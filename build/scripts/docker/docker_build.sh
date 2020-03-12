02dfc2025a4ddf86c61a7029b1d031f92754865a
Pankaj Garg <pgarg2@apple.com>
2020-03-12 12:57:03 -0700

    fix path and address some review comments in build_docker.sh

--- build/scripts/docker/docker_build.sh
+++ build/scripts/docker/docker_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-here="$(dirname "$0")"
+here=$(dirname "$0")
 
-MAKE_TARGET="${1:-all}"
+MAKE_TARGET=${1:-all}
 
-docker run --rm -v "${PWD}/$here/..":/tmp/chip rsmmr/clang make -C /tmp/chip "${MAKE_TARGET}"
+docker run --rm -v "${PWD}/$here/../../..":/tmp/chip rsmmr/clang make -C /tmp/chip "${MAKE_TARGET}"
