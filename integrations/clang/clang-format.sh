f0a107e1962421c068599059deda022cc8a26cbb
Rob Walker <robert_g_walker@apple.com>
2020-03-23 21:27:17 -0700

    Shellcheck-proof clang-format scripts

--- integrations/clang/clang-format.sh
+++ integrations/clang/clang-format.sh
@@ -2,14 +2,15 @@
 
 CLANG_FORMAT_VERSION="clang-format version 9.0"
 
+# shellcheck disable=SC2120
 die() {
-    echo " *** ERROR: " $*
+    echo " *** ERROR: $*"
     exit 1
 }
 
-if which clang-format-6.0 > /dev/null; then
+if command -v clang-format-6.0 > /dev/null; then
     alias clang-format=clang-format-9.0
-elif which clang-format > /dev/null; then
+elif command -v clang-format > /dev/null; then
     case "$(clang-format --version)" in
         "$CLANG_FORMAT_VERSION"*)
             ;;
@@ -21,7 +22,7 @@ else
     die "clang-format 9.0 required"
 fi
 
-clang-format $@ || die
+clang-format "$@" || die "format failed"
 
 # ensure EOF newline
 REPLACE=no
@@ -36,8 +37,8 @@ done
 
 file=$arg
 
-[ $REPLACE != yes ] || {
-    [ -n "$(tail -c1 $file)" ] && echo >> $file
+[[ $REPLACE != yes ]] || {
+    [[ -n "$(tail -c1 "$file")" ]] && echo >> "$file"
 }
 
 exit 0
