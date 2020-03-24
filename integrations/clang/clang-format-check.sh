dd1af7c00ae641cf9c4ef332e859e52987bf59e9
Rob Walker <robert_g_walker@apple.com>
2020-03-23 21:27:17 -0700

    Shellcheck-proof clang-format scripts

--- integrations/clang/clang-format-check.sh
+++ integrations/clang/clang-format-check.sh
@@ -6,20 +6,21 @@
 # replacements.
 #
 
+# shellcheck disable=SC2120
 die() {
-    echo " *** ERROR: " $*
+    echo " *** ERROR: $*"
     exit 1
 }
 
 # from `man diff`:
 # Exit status is 0 if inputs are the same, 1 if different, 2 if trouble.
 
-$(dirname "$0")/clang-format.sh -style=file $@  | diff -u $@ - || die
+"$(dirname "$0")"/clang-format.sh -style=file "$@"  | diff -u "$@" - || die "diffs exist"
 
 for arg; do true; done
 file=$arg
-[ -n "$(tail -c1 $file)" ] && {
-    echo " *** ERROR: Missing EOF newline: " $file
+[[ -n "$(tail -c1 "$file")" ]] && {
+    echo " *** ERROR: Missing EOF newline: $file"
     exit 1
 }
 
