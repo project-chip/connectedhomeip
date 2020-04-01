c8edf5601c55e7e91c2296ca8691a26f210e274e
Ezra Hale <ezra.hale@silabs.com>
2020-04-01 16:38:24 -0400

    removed Silicon Labs license for purposes of PR

--- src/zap-plugin/illuminance-measurement-server/illuminance-measurement-server-tokens.h
+++ src/zap-plugin/illuminance-measurement-server/illuminance-measurement-server-tokens.h
@@ -2,18 +2,6 @@
  * @file
  * @brief Custom Application Tokens
  * TODO: Pick creator codes.
- *******************************************************************************
- * # License
- * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
- *
- * The licensor of this software is Silicon Laboratories Inc. Your use of this
- * software is governed by the terms of Silicon Labs Master Software License
- * Agreement (MSLA) available at
- * www.silabs.com/about-us/legal/master-software-license-agreement. This
- * software is distributed to you in Source Code format and is governed by the
- * sections of the MSLA applicable to Source Code.
- *
  ******************************************************************************/
 
 #define CREATOR_SI1141_MULTIPLIER  (0x000F)
