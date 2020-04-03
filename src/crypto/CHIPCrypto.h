e526f71888035528154946324af2b10aeba16aef
Bhaskar Sarma <bhaskars@apple.com>
2020-04-03 15:06:24 -0700

    Fix formatting in CHIPCrypto.h

--- src/crypto/CHIPCrypto.h
+++ src/crypto/CHIPCrypto.h
@@ -2,7 +2,7 @@
  *
  *    Copyright (c) 2020 Project CHIP Authors
  *    Copyright (c) 2015-2019 The HomeKit ADK Contributors
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
