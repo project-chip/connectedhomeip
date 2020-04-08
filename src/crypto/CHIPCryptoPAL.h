123bf865cccede7ed3ad5cd75310accb4fb7f45e
Bhaskar Sarma <61213083+bhaskar-apple@users.noreply.github.com>
2020-04-08 10:00:43 -0700

    Missing crypto PAL layer #287 (#292)
    * Missing crypto PAL layer #287
    https://github.com/project-chip/connectedhomeip/issues/287
    
    * PR Feedback - Add doxygen, make params const, update sizes to be size_t
    
    * Add @return to doxygen comment
    
    * Add tag_length to the encrypt API
    
    * Add ciphertext_length as an out param
    
    * Revert "Add ciphertext_length as an out param"
    
    This reverts commit 9817553eb4a5ef5829f06e6e7b76f37ea7c6ad14.
    
    Co-authored-by: Bhaskar Sarma <bhaskars@apple.com>

---
+++ src/crypto/CHIPCryptoPAL.h
@@ -0,0 +1,66 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *      Header that exposes the platform agnostic CHIP crypto primitives
+ */
+
+#ifndef _CHIP_CRYPTO_PAL_H_
+#define _CHIP_CRYPTO_PAL_H_
+
+#include <core/CHIPError.h>
+#include <stddef.h>
+
+/**
+ * @brief A function that implements 256-bit AES-CCM encryption
+ * @param plaintext Plaintext to encrypt
+ * @param plaintext_length Length of plain_text
+ * @param aad Additional authentication data
+ * @param aad_length Length of additional authentication data
+ * @param key Encryption key
+ * @param iv Initial vector
+ * @param iv_length Length of initial vector
+ * @param ciphertext Buffer to write ciphertext into. Caller must ensure this is large enough to hold the ciphertext
+ * @param tag Buffer to write tag into. Caller must ensure this is large enough to hold the tag
+ * @param tag_length Expected length of tag
+ * @return Returns a CHIP_ERROR on error, CHIP_NO_ERROR otherwise
+ * */
+CHIP_ERROR CHIP_aes_ccm_256_encrypt(const unsigned char * plaintext, size_t plaintext_length, const unsigned char * aad,
+                                    size_t aad_length, const unsigned char * key, const unsigned char * iv, size_t iv_length,
+                                    unsigned char * ciphertext, unsigned char * tag, size_t tag_length);
+
+/**
+ * @brief A function that implements 256-bit AES-CCM decryption
+ * @param ciphertext Ciphertext to decrypt
+ * @param ciphertext_length Length of ciphertext
+ * @param aad Additional authentical data.
+ * @param aad_length Length of additional authentication data
+ * @param tag Tag to use to decrypt
+ * @param tag_length Length of tag
+ * @param key Decryption key
+ * @param iv Initial vector
+ * @param iv_length Length of initial vector
+ * @param plaintext Buffer to write plaintext into
+ * @return Returns a CHIP_ERROR on error, CHIP_NO_ERROR otherwise
+ **/
+
+CHIP_ERROR CHIP_aes_ccm_256_decrypt(const unsigned char * ciphertext, size_t ciphertext_length, const unsigned char * aad,
+                                    size_t aad_length, const unsigned char * tag, size_t tag_length, const unsigned char * key,
+                                    const unsigned char * iv, size_t iv_length, unsigned char * plaintext);
+
+#endif
---
+++ src/crypto/CHIPCryptoPALmbedTLS.c
@@ -0,0 +1,39 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *      mbedTLS based implementation of CHIP crypto primitives
+ */
+
+#include "CHIPCryptoPAL.h"
+
+CHIP_ERROR CHIP_aes_ccm_256_encrypt(const unsigned char * plaintext, size_t plaintext_length, const unsigned char * aad,
+                                    size_t aad_length, const unsigned char * key, const unsigned char * iv, size_t iv_length,
+                                    unsigned char * ciphertext, unsigned char * tag, size_t tag_length)
+{
+    // TODO: Need mbedTLS based implementation for AES-CCM #264
+    return CHIP_ERROR_UNSUPPORTED_ENCRYPTION_TYPE;
+}
+
+CHIP_ERROR CHIP_aes_ccm_256_decrypt(const unsigned char * ciphertext, size_t ciphertext_len, const unsigned char * aad,
+                                    size_t aad_len, const unsigned char * tag, size_t tag_length, const unsigned char * key,
+                                    const unsigned char * iv, size_t iv_length, unsigned char * plaintext)
+{
+    // TODO: Need mbedTLS based implementation for AES-CCM #264
+    return CHIP_ERROR_UNSUPPORTED_ENCRYPTION_TYPE;
+}
