ca74a5753e2b885a8b5042f35bf7c565f8fc1835
Rob Walker <robert_g_walker@apple.com>
2020-04-09 11:07:19 -0700

    more grooming, remove obsolete

--- integrations/docker/run_docker_build.sh
+++ integrations/docker/run_docker_build.sh
@@ -12,4 +12,4 @@ ORGANIZATION="connectedhomeip"
 IMAGE="chip-build"
 VERSION=$(cat "${here}/images/${IMAGE}/version")
 
-docker run --rm -w "${SOURCE_PATH}" -v "${SOURCE_PATH}:${SOURCE_PATH}" "${ORGANIZATION}/${IMAGE}:${VERSION}" integrations/docker/run_build.sh
+docker run --rm -w "${SOURCE_PATH}" -v "${SOURCE_PATH}:${SOURCE_PATH}" "${ORGANIZATION}/${IMAGE}:${VERSION}" integrations/docker/run_build.sh "${@:-distcheck}"
