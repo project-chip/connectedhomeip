a0194a4d111e7a5491686330145945ee01babbf8
Rob Walker <robert_g_walker@apple.com>
2020-04-15 11:50:36 -0700

    shellcheck examples (#362)
    * shellcheck examples
    
    * restyled

--- examples/lock-app/nrf5/start-gdb.sh
+++ examples/lock-app/nrf5/start-gdb.sh
@@ -1,22 +1,22 @@
-:
+#!/usr/bin/env bash
 
-SCRIPT_ROOT=`dirname "$0"`
+SCRIPT_ROOT=$(dirname "$0")
 GNU_INSTALL_ROOT=${GNU_INSTALL_ROOT:-${HOME}/tools/arm/gcc-arm-none-eabi-7-2018-q2-update/bin}
-GDB=${GNU_INSTALL_ROOT}/arm-none-eabi-gdb
+GDB=$GNU_INSTALL_ROOT/arm-none-eabi-gdb
 DEFAULT_APP=./build/chip-nrf52840-lock-example.out
-STARTUP_CMDS=${SCRIPT_ROOT}/gdb-startup-cmds.txt
+STARTUP_CMDS=$SCRIPT_ROOT/gdb-startup-cmds.txt
 
-if [ $# -eq 0 ]; then
-    APP=${DEFAULT_APP}
+if [[ $# -eq 0 ]]; then
+  APP=$DEFAULT_APP
 else
-    APP=$1
+  APP=$1
 fi
 
 EXTRA_STARTUP_CMDS=()
-if [[ ! -z "$OPENTHREAD_ROOT" ]]; then
-    EXTRA_STARTUP_CMDS+=(
-        "set substitute-path /build/KNGP-TOLL1-JOB1/openthread/examples/.. ${OPENTHREAD_ROOT}"
-    )
+if [[ -n $OPENTHREAD_ROOT ]]; then
+  EXTRA_STARTUP_CMDS+=(
+    "set substitute-path /build/KNGP-TOLL1-JOB1/openthread/examples/.. $OPENTHREAD_ROOT"
+  )
 fi
 
-exec ${GDB} -q -x ${STARTUP_CMDS} "${EXTRA_STARTUP_CMDS[@]/#/-ex=}" ${APP}
+exec "$GDB" -q -x "$STARTUP_CMDS" "${EXTRA_STARTUP_CMDS[@]/#/-ex=}" "$APP"
