813152397e7f642ca7c58c5797cc467a316aeb0f
Rob Walker <robert_g_walker@apple.com>
2020-04-15 10:36:16 -0700

    move shellcheck to local script

---
+++ integrations/docker/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# run.sh   - utility for running a Docker image
+#
+# This script expects to live in a directory named after the image
+#  with a version file next to it.  So: use symlinks
+#
+here=$(cd "$(dirname "$0")" && pwd)
+me=$(basename "$0")
+
+ORG=${DOCKER_RUN_ORG:-connectedhomeip}
+
+# directory name is
+IMAGE=${DOCKER_RUN_IMAGE:-$(basename "$here")}
+
+# version
+VERSION=${DOCKER_RUN_VERSION:-$(cat "$here/version")}
+
+# where
+RUN_DIR=${DOCKER_RUN_DIR:-$(pwd)}
+
+[[ ${*/--help//} != "${*}" ]] && {
+  set +x
+  echo "Usage: $me command
+
+  Run a command in a docker image described by $here
+
+  Options:
+   --help     get this message
+
+"
+  exit 0
+}
+
+docker run --rm -w "${RUN_DIR}" -v "${RUN_DIR}:${RUN_DIR}" "${ORG}/${IMAGE}:${VERSION}" "$@"
---
+++ integrations/shellcheck_tree.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+me=$(basename "$0")
+
+# Run shellcheck on all the shell-ish files in the tree
+patterns=( '*.bash*' 'bash*'
+           '*.ksh*' 'ksh*'
+           '*.zsh*' 'zsh*'
+           '.zlogin*' 'zlogin*'
+           '.zlogout*' 'zlogout*'
+           '.zprofile*' 'zprofile*'
+           '*.sh' '.shlib*' 'shlib*'
+           '.profile*' 'profile*'
+           'suid_profile' )
+
+# excluding these directories (e.g. third_party/* )
+excludes=( )
+
+# shellcheck disable=SC1091
+[[ -f .shellcheck_tree ]] && . .shellcheck_tree
+
+[[ ${*/--help//} != "${*}" ]] && {
+  echo "Usage: $me <OPTIONS>
+
+  Shellcheck all scripts in the tree.
+
+  Options:
+   --git      shellcheck via git ls-files, default is 'find .'
+   --help     get this message
+
+"
+  exit 0
+}
+
+if [[ ${*/--git//} != "${*}" ]]
+then
+  #
+  #  To run on git-files only
+  #
+  git ls-files -- "${patterns[@]}" | while read -r file
+  do
+    for exclude in "${excludes[@]}"
+    do
+      [[ $file =~ $exclude ]] && continue 2
+    done
+    shellcheck -x -f gcc "$file"
+  done
+else
+  #
+  # use find
+  #
+  exclude_args=()
+  for exclude in "${excludes[@]}"
+  do
+    exclude_args+=( '!' -path "./$exclude" -a )
+  done
+
+  pattern_args=()
+  for pattern in "${patterns[@]}"
+  do
+    pattern_args+=( -o -name "$pattern" )
+  done
+
+  find . "${exclude_args[@]}" '(' "${pattern_args[@]:1}" ')' -exec shellcheck -x -f gcc {} +
+fi
