d55515717b7d59ff0cd8372bc77863a0137451a6
Justin Wood <woody@apple.com>
2020-04-15 19:16:45 +0000

    Initial checkin

---
+++ integrations/travis/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+case "${TASK}" in
+
+    build-linux)
+        ./integrations/docker/run_docker_build.sh
+        ;;
+
+    preflight-shell-check)
+        ./integrations/docker/images/chip-build/run.sh integrations/shellcheck_tree.sh
+        ;;
+
+    *)
+        die "Unknown task: \"${TASK}\"."
+        ;;
+        
+esac
