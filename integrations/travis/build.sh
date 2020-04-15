97ec9a42554088a86865693b3dc959a177014fe6
Restyled.io <commits@restyled.io>
2020-04-15 20:43:27 +0000

    Restyled by shfmt

--- integrations/travis/build.sh
+++ integrations/travis/build.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-die()
-{
-    echo " *** ERROR: " "${*}"
-    exit 1
+die() {
+  echo " *** ERROR: " "${*}"
+  exit 1
 }
 
 case "$TASK" in
 
-    build-linux)
-        ./integrations/docker/run_docker_build.sh
-        ;;
+  build-linux)
+    ./integrations/docker/run_docker_build.sh
+    ;;
 
-    preflight-shell-check)
-        ./integrations/docker/images/chip-build/run.sh "integrations/shellcheck_tree.sh"
-        ;;
+  preflight-shell-check)
+    ./integrations/docker/images/chip-build/run.sh "integrations/shellcheck_tree.sh"
+    ;;
 
-    *)
-        die "Unknown task: $TASK."
-        ;;
-        
+  *)
+    die "Unknown task: $TASK."
+    ;;
+
 esac
