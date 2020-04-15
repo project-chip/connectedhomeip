9812635f750b190d2f6e71bb663605075b19bac6
Justin Wood <woody@apple.com>
2020-04-15 21:44:26 +0000

    Testing this approach..

--- integrations/travis/build.sh
+++ integrations/travis/build.sh
@@ -5,10 +5,14 @@ die() {
   exit 1
 }
 
+echo "#\!/bin/bash\n\n" > ./build/scripts/build_command.sh
+chmod +x ./build/scripts/build_command.sh
+
 case "$TASK" in
 
   build-linux)
-    ./integrations/docker/run_docker_build.sh
+    echo "./bootstrap && mkdir -p build/default && (cd build/default && ../../configure --enable-coverage) && make -C build/default \"${@:-distcheck}\" && git clean -Xdf && (cd examples/lock-app/nrf5 && make)" >> ./build/scripts/build_command.sh 
+    ./integrations/docker/images/chip-build/run.sh "build/scripts/build_command.sh"
     ;;
 
   preflight-shell-check)
