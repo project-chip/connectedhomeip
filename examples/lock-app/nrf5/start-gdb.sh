765f24643ca6b4062777e2fca277ee8c76a49bfb
Restyled.io <commits@restyled.io>
2020-04-15 17:34:19 +0000

    Restyled by shfmt

--- examples/lock-app/nrf5/start-gdb.sh
+++ examples/lock-app/nrf5/start-gdb.sh
@@ -7,16 +7,16 @@ DEFAULT_APP=./build/chip-nrf52840-lock-example.out
 STARTUP_CMDS=$SCRIPT_ROOT/gdb-startup-cmds.txt
 
 if [[ $# -eq 0 ]]; then
-    APP=$DEFAULT_APP
+  APP=$DEFAULT_APP
 else
-    APP=$1
+  APP=$1
 fi
 
 EXTRA_STARTUP_CMDS=()
 if [[ -n $OPENTHREAD_ROOT ]]; then
-    EXTRA_STARTUP_CMDS+=(
-        "set substitute-path /build/KNGP-TOLL1-JOB1/openthread/examples/.. $OPENTHREAD_ROOT"
-    )
+  EXTRA_STARTUP_CMDS+=(
+    "set substitute-path /build/KNGP-TOLL1-JOB1/openthread/examples/.. $OPENTHREAD_ROOT"
+  )
 fi
 
 exec "$GDB" -q -x "$STARTUP_CMDS" "${EXTRA_STARTUP_CMDS[@]/#/-ex=}" "$APP"
