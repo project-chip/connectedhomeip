76014824690bc767462a22c512ec83cba2460987
Martin Turon <mturon@google.com>
2020-04-17 15:16:33 -0700

    [format] Fix efr32 header formatting issues noted in PR comments.

--- examples/lock-app/efr32/include/board_features.h
+++ examples/lock-app/efr32/include/board_features.h
@@ -1,22 +1,19 @@
-/***************************************************************************/ /**
-                                                                               * @file
-                                                                               * @brief board_features.h
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2018 Silicon Laboratories Inc.
-                                                                               *www.silabs.com</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * The licensor of this software is Silicon
-                                                                               *Laboratories Inc. Your use of this software is
-                                                                               *governed by the terms of Silicon Labs Master
-                                                                               *Software License Agreement (MSLA) available at
-                                                                               * www.silabs.com/about-us/legal/master-software-license-agreement.
-                                                                               *This software is distributed to you in Source Code
-                                                                               *format and is governed by the sections of the MSLA
-                                                                               *applicable to Source Code.
-                                                                               *
-                                                                               ******************************************************************************/
+/*******************************************************************************
+ * @file
+ * @brief board_features.h
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
 #ifndef BOARD_FEATURES_H
 #define BOARD_FEATURES_H
--- examples/lock-app/efr32/include/init_mcu.h
+++ examples/lock-app/efr32/include/init_mcu.h
@@ -1,22 +1,19 @@
-/***************************************************************************/ /**
-                                                                               * @file
-                                                                               * @brief init_mcu.h
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2018 Silicon Laboratories Inc.
-                                                                               *www.silabs.com</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * The licensor of this software is Silicon
-                                                                               *Laboratories Inc. Your use of this software is
-                                                                               *governed by the terms of Silicon Labs Master
-                                                                               *Software License Agreement (MSLA) available at
-                                                                               * www.silabs.com/about-us/legal/master-software-license-agreement.
-                                                                               *This software is distributed to you in Source Code
-                                                                               *format and is governed by the sections of the MSLA
-                                                                               *applicable to Source Code.
-                                                                               *
-                                                                               ******************************************************************************/
+/*******************************************************************************
+ * @file
+ * @brief init_mcu.h
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
 #ifndef INIT_MCU_H
 #define INIT_MCU_H
