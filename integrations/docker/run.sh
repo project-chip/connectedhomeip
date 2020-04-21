fe1624de0c13f377fb43e264d967e7e2180f0dc6
Rob Walker <robert_g_walker@apple.com>
2020-04-21 08:01:11 -0700

    enhance run.sh (#441)
    * enhance run.sh
    
    * Restyled by shfmt
    
    * more friendliness and fun
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- integrations/docker/run.sh
+++ integrations/docker/run.sh
@@ -8,28 +8,81 @@
 here=$(cd "$(dirname "$0")" && pwd)
 me=$(basename "$0")
 
+die() {
+  echo "$me: *** ERROR: $*"
+  exit 1
+}
+
 ORG=${DOCKER_RUN_ORG:-connectedhomeip}
 
 # directory name is
 IMAGE=${DOCKER_RUN_IMAGE:-$(basename "$here")}
 
 # version
-VERSION=${DOCKER_RUN_VERSION:-$(cat "$here/version")}
+VERSION=${DOCKER_RUN_VERSION:-$(cat "$here/version")} ||
+  die "please run me from an image directory or set environment variables:
+          DOCKER_RUN_ORG
+          DOCKER_RUN_IMAGE
+          DOCKER_RUN_VERSION"
 
 # where
 RUN_DIR=${DOCKER_RUN_DIR:-$(pwd)}
 
-[[ ${*/--help//} != "${*}" ]] && {
+help() {
   set +x
-  echo "Usage: $me command
+  echo "Usage: $me [RUN_OPTIONS -- ] command
 
   Run a command in a docker image described by $here
 
   Options:
-   --help     get this message
+   --help        get this message
+
+  Any number of 'docker run' options can be passed
+     through to the invocation.  Terminate this list of
+     options with '--' to begin command and arguments.
+
+  Examples:
+    To run bash interactively:
+      $ $me -i -- bash
+     note the terminating '--' for run options
+
+    To just tell me about the image
+      $ $me uname -a
+
+    Add /tmp as an additional volumeand run make
+      $ $me --volume /tmp:/tmp -- make -C src
 
 "
-  exit 0
+
 }
 
-docker run --rm -w "$RUN_DIR" -v "$RUN_DIR:$RUN_DIR" "$ORG/$IMAGE:$VERSION" "$@"
+runargs=()
+
+# extract run options
+for arg in "$@"; do
+  case "$arg" in
+    --help)
+      help
+      exit
+      ;;
+
+    --)
+      shift
+      break
+      ;;
+
+    -*)
+      runargs+=("$arg")
+      shift
+      ;;
+
+    *)
+      ((!${#runargs[*]})) && break
+      runargs+=("$arg")
+      shift
+      ;;
+
+  esac
+done
+
+docker run "${runargs[@]}" --rm -w "$RUN_DIR" -v "$RUN_DIR:$RUN_DIR" "$ORG/$IMAGE${VERSION:+:${VERSION}}" "$@"
