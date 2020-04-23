6cfc5d83eb3c13df6f7b61833bfbbe79eaf97e69
Justin Wood <woody@apple.com>
2020-04-23 12:21:37 -0700

    Updating bootstrap

--- scripts/build/bootstrap_mbedtls.sh
+++ scripts/build/bootstrap_mbedtls.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 git clean -xdf
-mkdir -p build/default
-(cd build/default && ../../bootstrap-configure --enable-debug --enable-coverage --with-crypto=mbedtls)
+mkdir -p build/default (cd build/default && ../../bootstrap-configure --enable-debug --enable-coverage --with-crypto=mbedtls)
