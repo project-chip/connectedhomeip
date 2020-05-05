729b8e5363a15e0cfbaeb0fdf71bba8d0b3eac9e
Restyled.io <commits@restyled.io>
2020-05-05 08:34:25 +0000

    Restyled by shellharden

--- scripts/build/bootstrap.sh
+++ scripts/build/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$config_options = $@
+"$config_options" = "$@"
 
 if [[ ! -f build/default/config.status ]]; then
   mkdir -p build/default
   (cd build/default &&
-    ../../bootstrap-configure --enable-debug --enable-coverage $config_options)
+    ../../bootstrap-configure --enable-debug --enable-coverage "$config_options")
 elif [[ configure.ac -nt configure ]]; then
   ./bootstrap
 else
