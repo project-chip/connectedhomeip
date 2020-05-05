f135fb73ae024321495b78a9f98f826e5a2829ce
Andrei Litvin <andreilitvin@google.com>
2020-05-05 14:40:38 -0400

    Ran automated weave->chip conversion script.
    This should have renamed includes and class members to convert
    weave to chip.

--- src/lib/core/CHIPConnection.h
+++ src/lib/core/CHIPConnection.h
@@ -19,48 +19,47 @@
 
 /**
  *    @file
- *      This file defines the Weave Connection class.
+ *      This file defines the CHIP Connection class.
  *
  */
 
-#ifndef WEAVE_CONNECTION_H
-#define WEAVE_CONNECTION_H
+#ifndef CHIP_CONNECTION_H
+#define CHIP_CONNECTION_H
 
 #include <stdint.h>
 #include <string.h>
 
-#include <Weave/Support/NLDLLUtil.h>
+#include <support/DLLUtil.h>
 #include "HostPortList.h"
 #include <SystemLayer/SystemStats.h>
 
-namespace nl {
-namespace Weave {
+namespace chip {
 
 using System::PacketBuffer;
 
 /**
- *  @class WeaveConnection
+ *  @class ChipConnection
  *
  *  @brief
- *    The definition of the Weave Connection class. It represents a TCP or BLE
- *    connection to another Weave node.
+ *    The definition of the CHIP Connection class. It represents a TCP or BLE
+ *    connection to another CHIP node.
  *
  */
-class WeaveConnection
+class ChipConnection
 {
-    friend class WeaveMessageLayer;
+    friend class ChipMessageLayer;
 
 public:
     /**
      *  @enum State
      *
      *  @brief
-     *    The State of the Weave connection object.
+     *    The State of the CHIP connection object.
      *
      */
     enum State
     {
-        kState_ReadyToConnect                           = 0,  /**< State after initialization of the Weave connection. */
+        kState_ReadyToConnect                           = 0,  /**< State after initialization of the CHIP connection. */
         kState_Resolving                                = 1,  /**< State when DNS name resolution is being performed. */
         kState_Connecting                               = 2,  /**< State when connection is being attempted. */
         kState_EstablishingSession                      = 3,  /**< State when a secure session is being established. */
@@ -73,7 +72,7 @@ public:
      *  @enum NetworkType
      *
      *  @brief
-     *    The network type of the Weave connection object.
+     *    The network type of the CHIP connection object.
      *
      */
     enum NetworkType
@@ -85,14 +84,14 @@ public:
 
     uint64_t PeerNodeId;                                /**< [READ ONLY] The node identifier of the peer. */
     IPAddress PeerAddr;                                 /**< [READ ONLY] The IP address of the peer node. */
-    WeaveMessageLayer *MessageLayer;                    /**< [READ ONLY] The associated WeaveMessageLayer object. */
+    ChipMessageLayer *MessageLayer;                    /**< [READ ONLY] The associated ChipMessageLayer object. */
     void *AppState;                                     /**< A pointer to the application-specific state object. */
     uint16_t PeerPort;                                  /**< [READ ONLY] The port number of the peer node. */
     uint16_t DefaultKeyId;                              /**< The default encryption key to use when sending messages. */
-    WeaveAuthMode AuthMode;                             /**< [READ ONLY] The authentication mode used to establish the
+    ChipAuthMode AuthMode;                             /**< [READ ONLY] The authentication mode used to establish the
                                                              default encryption keys for the connection. */
     uint8_t DefaultEncryptionType;                      /**< The default encryption type for messages. */
-    uint8_t State;                                      /**< [READ ONLY] The state of the WeaveConnection object. */
+    uint8_t State;                                      /**< [READ ONLY] The state of the ChipConnection object. */
     uint8_t NetworkType;                                /**< [READ ONLY] The network type of the associated end point. */
     bool ReceiveEnabled;                                /**< [READ ONLY] True if receiving is enabled, false otherwise. */
     bool SendSourceNodeId;                              /**< True if all messages sent via this connection must include
@@ -105,54 +104,54 @@ public:
     void Release(void);
 
     // Note: a downcall to Connect() may call OnConnectionComplete before it returns.
-    WEAVE_ERROR Connect(uint64_t peerNodeId);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, const IPAddress &peerAddr, uint16_t peerPort = 0);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, const IPAddress &peerAddr, uint16_t peerPort = 0, InterfaceId intf = INET_NULL_INTERFACEID);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, const char *peerAddr, uint16_t defaultPort = 0);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, const char *peerAddr, uint16_t peerAddrLen, uint16_t defaultPort = 0);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, const char *peerAddr, uint16_t peerAddrLen, uint8_t dnsOptions, uint16_t defaultPort);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, HostPortList hostPortList, InterfaceId intf = INET_NULL_INTERFACEID);
-    WEAVE_ERROR Connect(uint64_t peerNodeId, WeaveAuthMode authMode, HostPortList hostPortList, uint8_t dnsOptions, InterfaceId intf);
+    CHIP_ERROR Connect(uint64_t peerNodeId);
+    CHIP_ERROR Connect(uint64_t peerNodeId, const IPAddress &peerAddr, uint16_t peerPort = 0);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, const IPAddress &peerAddr, uint16_t peerPort = 0, InterfaceId intf = INET_NULL_INTERFACEID);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, const char *peerAddr, uint16_t defaultPort = 0);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, const char *peerAddr, uint16_t peerAddrLen, uint16_t defaultPort = 0);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, const char *peerAddr, uint16_t peerAddrLen, uint8_t dnsOptions, uint16_t defaultPort);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, HostPortList hostPortList, InterfaceId intf = INET_NULL_INTERFACEID);
+    CHIP_ERROR Connect(uint64_t peerNodeId, ChipAuthMode authMode, HostPortList hostPortList, uint8_t dnsOptions, InterfaceId intf);
 
-    WEAVE_ERROR GetPeerAddressInfo(IPPacketInfo& addrInfo);
+    CHIP_ERROR GetPeerAddressInfo(IPPacketInfo& addrInfo);
 
     enum
     {
-        kGetPeerDescription_MaxLength = nl::Weave::kWeavePeerDescription_MaxLength,
+        kGetPeerDescription_MaxLength = chip::kChipPeerDescription_MaxLength,
                                                         /**< Maximum length of string (including NUL character) returned
                                                              by GetPeerDescription(). */
     };
 
     void GetPeerDescription(char * buf, size_t bufSize) const;
 
-    WEAVE_ERROR SendMessage(WeaveMessageInfo *msgInfo, PacketBuffer *msgBuf);
-#if WEAVE_CONFIG_ENABLE_TUNNELING
+    CHIP_ERROR SendMessage(ChipMessageInfo *msgInfo, PacketBuffer *msgBuf);
+#if CHIP_CONFIG_ENABLE_TUNNELING
 /**
- * Function to send a Tunneled packet over a Weave connection.
+ * Function to send a Tunneled packet over a CHIP connection.
  */
-    WEAVE_ERROR SendTunneledMessage(WeaveMessageInfo *msgInfo, PacketBuffer *msgBuf);
+    CHIP_ERROR SendTunneledMessage(ChipMessageInfo *msgInfo, PacketBuffer *msgBuf);
 #endif
 
-    // TODO COM-311: implement EnableReceived/DisableReceive for BLE WeaveConnections.
+    // TODO COM-311: implement EnableReceived/DisableReceive for BLE ChipConnections.
     void EnableReceive(void);
     void DisableReceive(void);
 
-    WEAVE_ERROR Shutdown(void);
+    CHIP_ERROR Shutdown(void);
 
-    WEAVE_ERROR Close(void);
-    WEAVE_ERROR Close(bool suppressCloseLog);
+    CHIP_ERROR Close(void);
+    CHIP_ERROR Close(bool suppressCloseLog);
 
     void Abort(void);
 
     void SetConnectTimeout(const uint32_t connTimeoutMsecs);
 
-    WEAVE_ERROR SetIdleTimeout(uint32_t timeoutMS);
+    CHIP_ERROR SetIdleTimeout(uint32_t timeoutMS);
 
-    WEAVE_ERROR EnableKeepAlive(uint16_t interval, uint16_t timeoutCount);
-    WEAVE_ERROR DisableKeepAlive(void);
+    CHIP_ERROR EnableKeepAlive(uint16_t interval, uint16_t timeoutCount);
+    CHIP_ERROR DisableKeepAlive(void);
 
-    WEAVE_ERROR SetUserTimeout(uint32_t userTimeoutMillis);
-    WEAVE_ERROR ResetUserTimeout(void);
+    CHIP_ERROR SetUserTimeout(uint32_t userTimeoutMillis);
+    CHIP_ERROR ResetUserTimeout(void);
     uint16_t LogId(void) const { return static_cast<uint16_t>(reinterpret_cast<intptr_t>(this)); }
 
     TCPEndPoint * GetTCPEndPoint(void) const { return mTcpEndPoint; }
@@ -160,65 +159,65 @@ public:
     /**
      *  This function is the application callback that is invoked when a connection setup is complete.
      *
-     *  @param[in]    con           A pointer to the WeaveConnection object.
+     *  @param[in]    con           A pointer to the ChipConnection object.
      *
-     *  @param[in]    conErr        The WEAVE_ERROR encountered during connection setup.
+     *  @param[in]    conErr        The CHIP_ERROR encountered during connection setup.
      *
      */
-    typedef void (*ConnectionCompleteFunct)(WeaveConnection *con, WEAVE_ERROR conErr);
+    typedef void (*ConnectionCompleteFunct)(ChipConnection *con, CHIP_ERROR conErr);
     ConnectionCompleteFunct OnConnectionComplete;
 
     /**
      *  This function is the application callback that is invoked when a connection is closed.
      *
-     *  @param[in]    con           A pointer to the WeaveConnection object.
+     *  @param[in]    con           A pointer to the ChipConnection object.
      *
-     *  @param[in]    conErr        The WEAVE_ERROR encountered when the connection was closed.
+     *  @param[in]    conErr        The CHIP_ERROR encountered when the connection was closed.
      *
      */
-    typedef void (*ConnectionClosedFunct)(WeaveConnection *con, WEAVE_ERROR conErr);
+    typedef void (*ConnectionClosedFunct)(ChipConnection *con, CHIP_ERROR conErr);
     ConnectionClosedFunct OnConnectionClosed;
 
     /**
      *  This function is the application callback that is invoked when a message is received over a
-     *  Weave connection.
+     *  CHIP connection.
      *
-     *  @param[in]    con           A pointer to the WeaveConnection object.
+     *  @param[in]    con           A pointer to the ChipConnection object.
      *
-     *  @param[in]    msgInfo       A pointer to a WeaveMessageInfo structure containing information about the message.
+     *  @param[in]    msgInfo       A pointer to a ChipMessageInfo structure containing information about the message.
      *
      *  @param[in]    msgBuf        A pointer to the PacketBuffer object holding the message.
      *
      */
-    typedef void (*MessageReceiveFunct)(WeaveConnection *con, WeaveMessageInfo *msgInfo, PacketBuffer *msgBuf);
+    typedef void (*MessageReceiveFunct)(ChipConnection *con, ChipMessageInfo *msgInfo, PacketBuffer *msgBuf);
     MessageReceiveFunct OnMessageReceived;
 
-#if WEAVE_CONFIG_ENABLE_TUNNELING
+#if CHIP_CONFIG_ENABLE_TUNNELING
     /**
      *  This function is the application callback that is invoked upon receipt of a Tunneled data packet over the
-     *  Weave connection.
+     *  CHIP connection.
      *
-     *  @param[in]     con            A pointer to the WeaveConnection object.
+     *  @param[in]     con            A pointer to the ChipConnection object.
      *
-     *  @param[in]     msgInfo        A pointer to the WeaveMessageInfo object.
+     *  @param[in]     msgInfo        A pointer to the ChipMessageInfo object.
      *
      *  @param[in]     msgBuf         A pointer to the PacketBuffer object containing the tunneled packet received.
      *
      */
-    typedef void (*TunneledMsgReceiveFunct)(WeaveConnection *con, const WeaveMessageInfo *msgInfo, PacketBuffer *msgBuf);
+    typedef void (*TunneledMsgReceiveFunct)(ChipConnection *con, const ChipMessageInfo *msgInfo, PacketBuffer *msgBuf);
     TunneledMsgReceiveFunct OnTunneledMessageReceived;
 #endif
 
     /**
      *  This function is the application callback invoked upon encountering an error when receiving
-     *  a Weave message.
+     *  a CHIP message.
      *
-     *  @param[in]     con            A pointer to the WeaveConnection object.
+     *  @param[in]     con            A pointer to the ChipConnection object.
      *
-     *  @param[in]     err            The WEAVE_ERROR encountered when receiving data over the connection.
+     *  @param[in]     err            The CHIP_ERROR encountered when receiving data over the connection.
      *
      */
-    typedef void (*ReceiveErrorFunct)(WeaveConnection *con, WEAVE_ERROR err);
+    typedef void (*ReceiveErrorFunct)(ChipConnection *con, CHIP_ERROR err);
     ReceiveErrorFunct OnReceiveError;
 
     bool IsIncoming(void) const { return GetFlag(mFlags, kFlag_IsIncoming); }
@@ -231,13 +230,13 @@ private:
         kDoCloseFlag_SuppressLogging    = 0x02
     } DoCloseFlags;
 
-    IPAddress mPeerAddrs[WEAVE_CONFIG_CONNECT_IP_ADDRS];
+    IPAddress mPeerAddrs[CHIP_CONFIG_CONNECT_IP_ADDRS];
     TCPEndPoint *mTcpEndPoint;
     HostPortList mPeerHostPortList;
     InterfaceId mTargetInterface;
     uint32_t mConnectTimeout;
     uint8_t mRefCount;
-#if WEAVE_CONFIG_ENABLE_DNS_RESOLVER
+#if CHIP_CONFIG_ENABLE_DNS_RESOLVER
     uint8_t mDNSOptions;
 #endif
 
@@ -248,29 +247,29 @@ private:
 
     uint8_t mFlags;                                     /**< Various flags associated with the connection. */
 
-    void Init(WeaveMessageLayer *msgLayer);
+    void Init(ChipMessageLayer *msgLayer);
     void MakeConnectedTcp(TCPEndPoint *endPoint, const IPAddress &localAddr, const IPAddress &peerAddr);
-    WEAVE_ERROR StartConnect(void);
-    void DoClose(WEAVE_ERROR err, uint8_t flags);
-    WEAVE_ERROR TryNextPeerAddress(WEAVE_ERROR lastErr);
+    CHIP_ERROR StartConnect(void);
+    void DoClose(CHIP_ERROR err, uint8_t flags);
+    CHIP_ERROR TryNextPeerAddress(CHIP_ERROR lastErr);
     void StartSession(void);
     bool StateAllowsSend(void) const { return State == kState_EstablishingSession || State == kState_Connected; }
     bool StateAllowsReceive(void) const { return State == kState_EstablishingSession || State == kState_Connected || State == kState_SendShutdown; }
-    void DisconnectOnError(WEAVE_ERROR err);
-    WEAVE_ERROR StartConnectToAddressLiteral(const char *peerAddr, size_t peerAddrLen);
+    void DisconnectOnError(CHIP_ERROR err);
+    CHIP_ERROR StartConnectToAddressLiteral(const char *peerAddr, size_t peerAddrLen);
 
     static void HandleResolveComplete(void *appState, INET_ERROR err, uint8_t addrCount, IPAddress *addrArray);
     static void HandleConnectComplete(TCPEndPoint *endPoint, INET_ERROR conRes);
     static void HandleDataReceived(TCPEndPoint *endPoint, PacketBuffer *data);
     static void HandleTcpConnectionClosed(TCPEndPoint *endPoint, INET_ERROR err);
-    static void HandleSecureSessionEstablished(WeaveSecurityManager *sm, WeaveConnection *con, void *reqState, uint16_t sessionKeyId, uint64_t peerNodeId, uint8_t encType);
-    static void HandleSecureSessionError(WeaveSecurityManager *sm, WeaveConnection *con, void *reqState, WEAVE_ERROR localErr, uint64_t peerNodeId,
+    static void HandleSecureSessionEstablished(ChipSecurityManager *sm, ChipConnection *con, void *reqState, uint16_t sessionKeyId, uint64_t peerNodeId, uint8_t encType);
+    static void HandleSecureSessionError(ChipSecurityManager *sm, ChipConnection *con, void *reqState, CHIP_ERROR localErr, uint64_t peerNodeId,
                                          Profiles::StatusReporting::StatusReport *statusReport);
-    static void DefaultConnectionClosedHandler(WeaveConnection *con, WEAVE_ERROR conErr);
+    static void DefaultConnectionClosedHandler(ChipConnection *con, CHIP_ERROR conErr);
 
 #if CONFIG_NETWORK_LAYER_BLE
 public:
-    WEAVE_ERROR ConnectBle(BLE_CONNECTION_OBJECT connObj, WeaveAuthMode authMode, bool autoClose = true);
+    CHIP_ERROR ConnectBle(BLE_CONNECTION_OBJECT connObj, ChipAuthMode authMode, bool autoClose = true);
 
 private:
     BLEEndPoint *mBleEndPoint;
@@ -284,7 +283,6 @@ private:
 
 };
 
-} // namespace nl
-} // namespace Weave
+} // namespace chip
 
-#endif // WEAVE_CONNECTION_H
+#endif // CHIP_CONNECTION_H
