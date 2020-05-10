9c87ddf1f17708604389f199fdc83cf298ca742d
Restyled.io <commits@restyled.io>
2020-05-10 04:12:39 +0000

    Restyled by clang-format

--- src/lib/support/TestUtils.cpp
+++ src/lib/support/TestUtils.cpp
@@ -21,23 +21,26 @@
 
 const size_t kTest_Suite_Count_Increments = 16;
 
-typedef struct {
-    nlTestSuite* test_suites;
+typedef struct
+{
+    nlTestSuite * test_suites;
     uint32_t test_suites_size;
     uint32_t num_test_suites;
 } test_suites_t;
 
-static test_suites_t gs_test_suites = {NULL, 0, 0};
+static test_suites_t gs_test_suites = { NULL, 0, 0 };
 
-CHIP_ERROR deploy_device_unit_tests(nlTestSuite *tests) {
-    if (gs_test_suites.test_suites_size <= gs_test_suites.num_test_suites) {
-        uint32_t new_size = gs_test_suites.test_suites_size + kTest_Suite_Count_Increments;
-        nlTestSuite* new_suites = (nlTestSuite*)realloc(gs_test_suites.test_suites,
-                                   sizeof(nlTestSuite) * new_size);
-        if (new_suites == NULL) {
+CHIP_ERROR deploy_device_unit_tests(nlTestSuite * tests)
+{
+    if (gs_test_suites.test_suites_size <= gs_test_suites.num_test_suites)
+    {
+        uint32_t new_size        = gs_test_suites.test_suites_size + kTest_Suite_Count_Increments;
+        nlTestSuite * new_suites = (nlTestSuite *) realloc(gs_test_suites.test_suites, sizeof(nlTestSuite) * new_size);
+        if (new_suites == NULL)
+        {
             return CHIP_ERROR_NO_MEMORY;
         }
-        gs_test_suites.test_suites = new_suites;
+        gs_test_suites.test_suites      = new_suites;
         gs_test_suites.test_suites_size = new_size;
     }
     memcpy(&gs_test_suites.test_suites[gs_test_suites.num_test_suites], tests, sizeof(nlTestSuite));
@@ -45,9 +48,11 @@ CHIP_ERROR deploy_device_unit_tests(nlTestSuite *tests) {
     return CHIP_NO_ERROR;
 }
 
-int run_device_unit_tests() {
+int run_device_unit_tests()
+{
     int status = 0;
-    for (uint32_t i = 0; i < gs_test_suites.num_test_suites; i++) {
+    for (uint32_t i = 0; i < gs_test_suites.num_test_suites; i++)
+    {
         nlTestRunner(&gs_test_suites.test_suites[i], NULL);
         status |= nlTestRunnerStats(&gs_test_suites.test_suites[i]);
     }
