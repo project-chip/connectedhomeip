d6f9263b4f3b5fe5ec42ce44aa404164952b9e41
Restyled.io <commits@restyled.io>
2020-05-12 15:18:16 +0000

    Restyled by clang-format

--- src/lib/core/HostPortList.h
+++ src/lib/core/HostPortList.h
@@ -44,9 +44,9 @@ namespace chip {
  */
 class HostPortList
 {
- public:
+public:
     HostPortList(void);
-    inline HostPortList(const uint8_t *hostPortList, uint8_t hostPortCount, const uint8_t *suffixList, const uint8_t suffixCount)
+    inline HostPortList(const uint8_t * hostPortList, uint8_t hostPortCount, const uint8_t * suffixList, const uint8_t suffixCount)
     {
         mElements    = hostPortList;
         mCount       = hostPortCount;
@@ -54,8 +54,8 @@ class HostPortList
         mSuffixCount = suffixCount;
     }
 
-    CHIP_ERROR Get(uint8_t index, char *hostBuf, uint32_t hostBufSize, uint16_t& port) const;
-    CHIP_ERROR Pop(char *hostBuf, uint32_t hostBufSize, uint16_t& port);
+    CHIP_ERROR Get(uint8_t index, char * hostBuf, uint32_t hostBufSize, uint16_t & port) const;
+    CHIP_ERROR Pop(char * hostBuf, uint32_t hostBufSize, uint16_t & port);
 
     void Clear(void);
 
@@ -64,23 +64,23 @@ class HostPortList
 private:
     enum
     {
-        kHostPortControl_TypeMask       = 0x03,  ///< Mask for the type of entry flags.
-        kHostPortControl_HasSuffixIndex = 0x04,  ///< Flag indicating whether the host port tuple has an associated suffix index.
-        kHostPortControl_HasPort        = 0x08,  ///< Flag indicating whether the host port tuple has a port number.
+        kHostPortControl_TypeMask       = 0x03, ///< Mask for the type of entry flags.
+        kHostPortControl_HasSuffixIndex = 0x04, ///< Flag indicating whether the host port tuple has an associated suffix index.
+        kHostPortControl_HasPort        = 0x08, ///< Flag indicating whether the host port tuple has a port number.
 
-        kHostPortType_FullyQualified    = 0x00,  ///< Flag indicating whether the host name is fully-qualified.
-        kHostPortType_Indexed           = 0x01,  ///< Flag indicating whether the host name is index (i.e. has a suffix).
+        kHostPortType_FullyQualified = 0x00, ///< Flag indicating whether the host name is fully-qualified.
+        kHostPortType_Indexed        = 0x01, ///< Flag indicating whether the host name is index (i.e. has a suffix).
     };
 
-    CHIP_ERROR Get(const uint8_t *& elem, char *hostBuf, uint32_t hostBufSize, uint16_t& port) const;
+    CHIP_ERROR Get(const uint8_t *& elem, char * hostBuf, uint32_t hostBufSize, uint16_t & port) const;
     CHIP_ERROR Skip(const uint8_t *& elem) const;
-    CHIP_ERROR GetSuffix(uint8_t index, char *buf, uint32_t bufSize, uint8_t& suffixLen) const;
+    CHIP_ERROR GetSuffix(uint8_t index, char * buf, uint32_t bufSize, uint8_t & suffixLen) const;
 
- private:
-    const uint8_t  *mElements;     ///< A read-only pointer to the host and port tuples list.
-    const uint8_t  *mSuffixTable;  ///< A read-only pointer to the host suffix list.
-    uint8_t         mCount;        ///< The number of host and port tuples in the list.
-    uint8_t         mSuffixCount;  ///< The number of host suffixes in the list.
+private:
+    const uint8_t * mElements;    ///< A read-only pointer to the host and port tuples list.
+    const uint8_t * mSuffixTable; ///< A read-only pointer to the host suffix list.
+    uint8_t mCount;               ///< The number of host and port tuples in the list.
+    uint8_t mSuffixCount;         ///< The number of host suffixes in the list.
 };
 
 } /* namespace chip */
