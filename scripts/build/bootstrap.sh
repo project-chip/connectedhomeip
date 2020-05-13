485f1441cb12f3f2ba678fa2ef97003adcf8354c
Restyled.io <commits@restyled.io>
2020-05-13 19:57:25 +0000

    Restyled by shfmt

--- scripts/build/bootstrap.sh
+++ scripts/build/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ ! -f build/default/config.status ]]; then
-  mkdir -p build/default
-  (cd build/default &&
-    ../../bootstrap-configure -C --enable-debug --enable-coverage)
+    mkdir -p build/default
+    (cd build/default &&
+        ../../bootstrap-configure -C --enable-debug --enable-coverage)
 elif [[ configure.ac -nt configure ]]; then
     ./bootstrap
 else
