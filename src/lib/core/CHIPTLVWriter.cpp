78fcd4ee8b623b673226f89ea931ca23cfe427bb
Grant Erickson <gerickson@users.noreply.github.com>
2020-05-19 13:01:47 -0700

    Migrate over Recent List TLV Changes from OpenWeave Core (#760)
    * -- Renamed path type to list and clarified text in corresponding section.
    -- Added section on canonical ordering of tags.
    -- Added section describing anonymous tags.
    -- Clarified descriptions of structure and array types.
    -- Clarified encoding of floating point values with reference to RFC-4506.
    
    * Relax Check for TLV Path Element in the VerifyElement() to reflect TLV specification update regarding paths and lists..
    
    -- Elements in the list (path) can also be of an AnonymousTag.
    -- Updated unit test to verify new behaviour.
    
    * Update for style compliance.

--- src/lib/core/CHIPTLVWriter.cpp
+++ src/lib/core/CHIPTLVWriter.cpp
@@ -1680,7 +1680,7 @@ CHIP_ERROR TLVWriter::WriteElementHead(TLVElementType elemType, uint64_t tag, ui
         else
         {
             if (elemType != kTLVElementType_EndOfContainer && mContainerType != kTLVType_NotSpecified &&
-                mContainerType != kTLVType_Array)
+                mContainerType != kTLVType_Array && mContainerType != kTLVType_Path)
                 return CHIP_ERROR_INVALID_TLV_TAG;
 
             Write8(p, kTLVTagControl_Anonymous | elemType);
