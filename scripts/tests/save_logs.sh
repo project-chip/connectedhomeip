95342303d1b8616609dd9e896d1f3e3f50b36cd7
Restyled.io <commits@restyled.io>
2020-05-21 19:16:39 +0000

    Restyled by shellharden

--- scripts/tests/save_logs.sh
+++ scripts/tests/save_logs.sh
@@ -4,7 +4,7 @@ TARGET_DIR=$1
 
 mkdir -p "$1"
 
-for name in $(find . -name test-suite.log); do
+for name in "$(find . -name test-suite.log)"; do
     destination="$1"/$(echo "$name" | sed -E s-\^.\/-- | sed s-/-_-g)
     echo "Saving $name to $destination"
     cp "$name" "$destination"
