8e742113b2d4d6e5676acc50d62f7ddefc06c619
Andrei Litvin <andreilitvin@google.com>
2020-05-25 09:29:56 -0400

    Preserve test logs as CI artifacts in case of CI failure.
    This makes it easier to debug CI failures.

---
+++ scripts/tests/save_logs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+
+mkdir -p "$1"
+
+for name in $(find . -name test-suite.log); do
+    destination="$1"/$(echo "$name" | sed -E s-\^.\/-- | sed s-/-_-g)
+    echo "Saving $name to $destination"
+    cp "$name" "$destination"
+done
