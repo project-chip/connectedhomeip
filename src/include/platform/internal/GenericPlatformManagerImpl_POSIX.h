0c03fa83f2fce2530a8f043d553edf5f67e91348
Martin Turon <mturon@google.com>
2020-05-26 15:43:11 -0700

    [linux] Land the First (1st) of Nine (9) Phases for the Linux DeviceLayer Adapation (#614)
    * [linux] Add initial Linux platform DeviceLayer.
    
    - Adds Device Layer for Linux.
    - Uses SystemLayer Sockets.
    - Entropy and time implementations.
    - Stub out ble, connectivity, and configuration / storage for follow-on PR.
    - Uses pthreads for mutex and tasks.
    - Uses std::chrono for monotonic system time.
    
    * [linux] Add PlatformMgr tests.
    
    * [linux] Add CircleCI target for linux device layer.
    
    * [linux] Add fixme reference to LWIP ifdef.
    
    * Restyled by clang-format
    
    * [linux] fix header order; fix build
    
    After the latest rebase, the following error was coming up:
    
    ```
    ../../src/include/platform/ConnectivityManager.h:252:40: fatal error: platform/CHIP_DEVICE_LAYER_TARGET/ConnectivityManagerImpl.h: No such file or directory
      252 | #define CONNECTIVITYMANAGERIMPL_HEADER <platform/CHIP_DEVICE_LAYER_TARGET/ConnectivityManagerImpl.h>
    ```
    
    Though CHIP_DEVICE_LAYER_TARGET=Linux is set using AC_SUBST in configure.ac,
    the definition, which was working fine before, was mysteriously getting lost.
    It turns out the header re-ordering PR was the root cause of the issue.
    
    The fix was to change all sources in src/platform/Linux to
    move this include first:
        #include <platform/internal/CHIPDeviceLayerInternal.h>
    
    The error did not lead to the fix in an obvious way, so the issue
    is being documented here.
    
    * [linux] Update linux-embedded target to follow circleci matrix template.
    
    * [linux] Fix naming nits.
    
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/include/platform/internal/GenericPlatformManagerImpl_POSIX.h
@@ -0,0 +1,105 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2018 Nest Labs, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *          Provides an generic implementation of PlatformManager features
+ *          for use on Linux platforms.
+ */
+
+#ifndef GENERIC_PLATFORM_MANAGER_IMPL_POSIX_H
+#define GENERIC_PLATFORM_MANAGER_IMPL_POSIX_H
+
+#include <platform/internal/GenericPlatformManagerImpl.h>
+
+#include <fcntl.h>
+#include <sched.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include <pthread.h>
+#include <queue>
+
+namespace chip {
+namespace DeviceLayer {
+namespace Internal {
+
+/**
+ * Provides a generic implementation of PlatformManager features that works on any OSAL platform.
+ *
+ * This template contains implementations of select features from the PlatformManager abstract
+ * interface that are suitable for use on OSAL-based platforms.  It is intended to be inherited
+ * (directly or indirectly) by the PlatformManagerImpl class, which also appears as the template's
+ * ImplClass parameter.
+ */
+template <class ImplClass>
+class GenericPlatformManagerImpl_POSIX : public GenericPlatformManagerImpl<ImplClass>
+{
+protected:
+    // Members for select loop
+    int mMaxFd;
+    fd_set mReadSet;
+    fd_set mWriteSet;
+    fd_set mErrorSet;
+    struct timeval mNextTimeout;
+
+    // OS-specific members (pthread)
+    pthread_mutex_t mChipStackLock;
+    std::queue<ChipDeviceEvent> mChipEventQueue;
+
+    pthread_t mChipTask;
+    pthread_attr_t mChipTaskAttr;
+    struct sched_param mChipTaskSchedParam;
+
+    // ===== Methods that implement the PlatformManager abstract interface.
+
+    CHIP_ERROR
+    _InitChipStack();
+    void _LockChipStack(void);
+    bool _TryLockChipStack(void);
+    void _UnlockChipStack(void);
+    void _PostEvent(const ChipDeviceEvent * event);
+    void _RunEventLoop(void);
+    CHIP_ERROR _StartEventLoopTask(void);
+    CHIP_ERROR _StartChipTimer(uint32_t durationMS);
+
+    // ===== Methods available to the implementation subclass.
+
+private:
+    // ===== Private members for use by this class only.
+
+    inline ImplClass * Impl() { return static_cast<ImplClass *>(this); }
+
+    void SysUpdate();
+    void SysProcess();
+    static void SysOnEventSignal(void * arg);
+
+    void ProcessDeviceEvents();
+
+    static void * EventLoopTaskMain(void * arg);
+};
+
+// Instruct the compiler to instantiate the template only when explicitly told to do so.
+extern template class GenericPlatformManagerImpl_POSIX<PlatformManagerImpl>;
+
+} // namespace Internal
+} // namespace DeviceLayer
+} // namespace chip
+
+#endif // GENERIC_PLATFORM_MANAGER_IMPL_POSIX_H
