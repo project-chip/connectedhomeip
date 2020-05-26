dffda890c7d6b9d077e42babe8e558eabd525ab7
Rob Walker <robert_g_walker@apple.com>
2020-05-26 13:39:11 -0700

    restyle ObjCplusplus

--- src/darwin/Framework/CHIP/CHIPEchoClient.mm
+++ src/darwin/Framework/CHIP/CHIPEchoClient.mm
@@ -15,47 +15,51 @@
  *    limitations under the License.
  */
 
-
 #import <Foundation/Foundation.h>
 #import <os/log.h>
 
-#import "CHIPEchoClient.h"
 #import "CHIPDeviceController.h"
+#import "CHIPEchoClient.h"
 
 static UInt32 const kDefaultFrequencySeconds = 10;
 static NSString * const kEchoString = @"Hello from darwin!";
 
-@interface CHIPEchoClient()  {
+@interface CHIPEchoClient () {
     dispatch_queue_t _echoCallbackQueue;
     dispatch_source_t _sendTimer;
-    CHIPDeviceController* _chipController;
+    CHIPDeviceController * _chipController;
 }
 
 @property (readonly) dispatch_queue_t echoCallbackQueue;
 @property (readonly) dispatch_source_t sendTimer;
-@property (readonly) CHIPDeviceController* chipController;
+@property (readonly) CHIPDeviceController * chipController;
 
 @end
 
 @implementation CHIPEchoClient
 
-- (instancetype)initWithServerAddress:(NSString *)ipAddress port:(UInt16)port {
+- (instancetype)initWithServerAddress:(NSString *)ipAddress port:(UInt16)port
+{
     if (self = [super init]) {
         NSError * error;
         _echoCallbackQueue = dispatch_queue_create("com.zigbee.chip.echo", NULL);
         _chipController = [[CHIPDeviceController alloc] initWithCallbackQueue:_echoCallbackQueue];
 
-        BOOL didInit = [_chipController connect:ipAddress port:port error:&error onMessage:^(NSData *  _Nonnull message, NSString * _Nonnull ipAddress, UInt16 port) {
-            NSData* sentMessage = [kEchoString dataUsingEncoding:NSUTF8StringEncoding];
-            if ([sentMessage isEqualToData:message]) {
-                os_log(OS_LOG_DEFAULT, "Received expected echo response...");
-            } else {
-                os_log(OS_LOG_DEFAULT, "Unexpected echo response. \nExpected: %@ \nReceived: %@", sentMessage, message);
+        BOOL didInit = [_chipController connect:ipAddress
+            port:port
+            error:&error
+            onMessage:^(NSData * _Nonnull message, NSString * _Nonnull ipAddress, UInt16 port) {
+                NSData * sentMessage = [kEchoString dataUsingEncoding:NSUTF8StringEncoding];
+                if ([sentMessage isEqualToData:message]) {
+                    os_log(OS_LOG_DEFAULT, "Received expected echo response...");
+                } else {
+                    os_log(OS_LOG_DEFAULT, "Unexpected echo response. \nExpected: %@ \nReceived: %@", sentMessage, message);
+                }
             }
-        } onError:^(NSError * _Nonnull error) {
-            os_log(OS_LOG_DEFAULT, "Received error %@. Stopping...", error);
-            [self stop];
-        }];
+            onError:^(NSError * _Nonnull error) {
+                os_log(OS_LOG_DEFAULT, "Received error %@. Stopping...", error);
+                [self stop];
+            }];
 
         if (!didInit) {
             os_log(OS_LOG_DEFAULT, "Failed to init with error %@", error);
@@ -66,12 +70,12 @@ static NSString * const kEchoString = @"Hello from darwin!";
             os_log(OS_LOG_DEFAULT, "Could not create dispatch source");
             return nil;
         }
-
     }
     return self;
 }
 
-- (BOOL)startWithFrequency:(UInt32)frequency withError:(NSError * __autoreleasing *)error  {
+- (BOOL)startWithFrequency:(UInt32)frequency withError:(NSError * __autoreleasing *)error
+{
     dispatch_source_set_timer(self.sendTimer, dispatch_walltime(NULL, 0), frequency * NSEC_PER_SEC, 1 * NSEC_PER_SEC);
     dispatch_source_set_event_handler(self.sendTimer, ^{
         os_log(OS_LOG_DEFAULT, "Sending echo...");
@@ -79,17 +83,18 @@ static NSString * const kEchoString = @"Hello from darwin!";
         if (![self.chipController sendMessage:[kEchoString dataUsingEncoding:NSUTF8StringEncoding] error:&error]) {
             os_log(OS_LOG_DEFAULT, "Failed to send with error %@", error);
         }
-
     });
     dispatch_activate(self.sendTimer);
     return YES;
 }
 
-- (BOOL)startWithError:(NSError * __autoreleasing *)error {
+- (BOOL)startWithError:(NSError * __autoreleasing *)error
+{
     return [self startWithFrequency:kDefaultFrequencySeconds withError:error];
 }
 
-- (BOOL)startWithFrequency:(UInt32)frequency stopAfter:(UInt32)stopAfter withError:(NSError * __autoreleasing *)error {
+- (BOOL)startWithFrequency:(UInt32)frequency stopAfter:(UInt32)stopAfter withError:(NSError * __autoreleasing *)error
+{
     BOOL result = [self startWithFrequency:frequency withError:error];
     dispatch_after(dispatch_walltime(nil, stopAfter * NSEC_PER_SEC), self.echoCallbackQueue, ^{
         [self stop];
@@ -97,13 +102,15 @@ static NSString * const kEchoString = @"Hello from darwin!";
     return result;
 }
 
-- (void)stop {
+- (void)stop
+{
     dispatch_suspend(self->_sendTimer);
     [self.chipController disconnect:nil];
     os_log(OS_LOG_DEFAULT, "Echo client stopped...");
 }
 
-- (void)dealloc {
+- (void)dealloc
+{
     [self stop];
 }
 
