f707e84d9a00a8ba37bac2403112744da4bbf29a
Martin Turon <mturon@google.com>
2020-05-28 21:28:32 -0700

    [build] Wrap BuildConfig.h in HAVE_CONFIG_H.

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -38,7 +38,9 @@
 #define SYSTEMCONFIG_H
 
 /* Platform include headers */
+#ifdef HAVE_CONFIG_H
 #include <BuildConfig.h>
+#endif
 
 /* Include a CHIP project-specific configuration file, if defined.
  *
