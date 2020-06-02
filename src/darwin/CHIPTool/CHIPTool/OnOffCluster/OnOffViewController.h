362a8e81780999382fae64f2c4f2f371515b0119
Bhaskar Sarma <61213083+bhaskar-apple@users.noreply.github.com>
2020-06-02 07:03:42 -0700

    Use navbar instead of tab bar controller.  (#941)
    * Use navbar
    
    * Restyled by clang-format
    
    * Restyled by shellharden
    
    * Restyled by shfmt.
    
    Co-authored-by: Bhaskar Sarma <bhaskars@apple.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/darwin/CHIPTool/CHIPTool/OnOffCluster/OnOffViewController.h
@@ -0,0 +1,17 @@
+//
+//  OnOffViewController.h
+//  CHIPTool
+//
+//  Created by Bhaskar on 6/1/20.
+//  Copyright © 2020 CHIP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OnOffViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
