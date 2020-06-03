e8b4b2922127c31f3944bb8e881124ebb77a0008
Andrei Litvin <andreilitvin@google.com>
2020-06-03 18:21:03 -0400

    Make restyler happy

--- scripts/helpers/bloat-check.sh
+++ scripts/helpers/bloat-check.sh
@@ -7,16 +7,16 @@ SOURCEDIR=$2
 OUTPUTDIR=$3
 REPORTFILE=$4
 
-mkdir -p ${OUTPUTDIR}
+mkdir -p "$OUTPUTDIR"
 scripts/helpers/ci-fetch-artifacts.py \
-    --token "${CIRCLECI_API_TOKEN}" \
-    --job "${JOBNAME}" \
-    --download-dir "${OUTPUTDIR}"
+    --token "$CIRCLECI_API_TOKEN" \
+    --job "$JOBNAME" \
+    --download-dir "$OUTPUTDIR"
 
-echo "BLOAT REPORT" >${REPORTFILE}
+echo "BLOAT REPORT" >"$REPORTFILE"
 
-for masterbinary in ${OUTPUTDIR}/*; do
-    newbinary=${SOURCEDIR}/$(basename $masterbinary)
-    echo "Checking $newbinary against $masterbinary" >>${REPORTFILE}
-    bloaty $newbinary -- $masterbinary 2>&1 >>${REPORTFILE}
+for masterbinary in "$OUTPUTDIR"/*; do
+    newbinary=$SOURCEDIR/$(basename "$masterbinary")
+    echo "Checking $newbinary against $masterbinary" >>"$REPORTFILE"
+    bloaty "$newbinary" -- "$masterbinary" 2>&1 >>"$REPORTFILE"
 done
