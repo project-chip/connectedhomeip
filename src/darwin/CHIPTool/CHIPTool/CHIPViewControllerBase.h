679e0c4ddac800596f5f513aaaec290752833a43
Pankaj Garg <pgarg2@apple.com>
2020-06-11 14:00:29 -0700

    Add a switch to CHIPTool UI to choose encryption key (#1093)
    * Add a switch to CHIPTool UI to choose encryption key
    
    * Restyled by clang-format
    
    * Fix bounds of UI elements
    
    * more UI constraints
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/darwin/CHIPTool/CHIPTool/CHIPViewControllerBase.h
+++ src/darwin/CHIPTool/CHIPTool/CHIPViewControllerBase.h
@@ -22,12 +22,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CHIPViewControllerBase : UIViewController
 
+@property (readwrite) BOOL useCorrectKey;
+@property (readwrite) BOOL useCorrectKeyStateChanged;
 @property (readwrite) CHIPDeviceController * chipController;
 @property (weak, nonatomic) IBOutlet UILabel * resultLabel;
+@property (weak, nonatomic) IBOutlet UISwitch * encryptionKeySwitch;
 
 - (void)reconnectIfNeeded;
 - (void)dismissKeyboard;
 - (void)postResult:(NSString *)result;
+- (IBAction)encryptionKey:(id)sender;
 
 @end
 
