43a6305989fb39efd2b137b5a4f39d68659a2a4a
Restyled.io <commits@restyled.io>
2020-06-19 13:25:38 +0000

    Restyled by shfmt

--- src/app/silabs/framework/plugin/ota-storage-simple-ram/ota-static-file-rebuild.sh
+++ src/app/silabs/framework/plugin/ota-storage-simple-ram/ota-static-file-rebuild.sh
@@ -12,7 +12,7 @@
 /bin/pwd | grep app/framework/cluster
 STATUS=$?
 if [ "$STATUS" == 0 ]; then
-  echo "Error:  This script must be run from the root of the branch"
+    echo "Error:  This script must be run from the root of the branch"
 fi
 
 OTA_FILE=app/framework/plugin/ota-storage-simple-ram/ota-static-sample.ota
@@ -22,9 +22,9 @@ OTA_HEADER_SCRIPT=app/framework/plugin/ota-storage-simple-ram/ota-static-file.pl
 echo ""
 
 if [ "$CYGWIN" = 1 ]; then
-  IMAGE_BUILDER_EXE=./build/image-builder/build.windows/image-builder-ecc.exe
+    IMAGE_BUILDER_EXE=./build/image-builder/build.windows/image-builder-ecc.exe
 else
-  IMAGE_BUILDER_EXE=./build/image-builder/build.linux/image-builder-ecc
+    IMAGE_BUILDER_EXE=./build/image-builder/build.linux/image-builder-ecc
 fi
 
 VERSION=0x05
@@ -33,32 +33,32 @@ IMAGE_ID=0x5678
 STRING="The latest and greatest upgrade."
 
 echo "Creating $OTA_FILE"
-"$IMAGE_BUILDER_EXE"     \
-  --create "$OTA_FILE"   \
-  --version "$VERSION"   \
-  --manuf-id "$MANUF_ID" \
-  --image-type "$IMAGE_ID" \
-  --string "$STRING"     \
-  --tag-id 0xF000        \
-  --tag-length 10        \
-  --test-sign 1
+"$IMAGE_BUILDER_EXE" \
+    --create "$OTA_FILE" \
+    --version "$VERSION" \
+    --manuf-id "$MANUF_ID" \
+    --image-type "$IMAGE_ID" \
+    --string "$STRING" \
+    --tag-id 0xF000 \
+    --tag-length 10 \
+    --test-sign 1
 
 STATUS=$?
 if [ "$STATUS" != 0 ]; then
-  echo "Error: Image-builder failed."
-  exit "$STATUS"
+    echo "Error: Image-builder failed."
+    exit "$STATUS"
 fi
 
 echo -e "Creating $OTA_HEADER_FILE"
 "$OTA_HEADER_SCRIPT" \
-  --input "$OTA_FILE" \
-  --output "$OTA_HEADER_FILE" \
-  --extra-comments "$IMAGE_BUILDER_EXE"
+    --input "$OTA_FILE" \
+    --output "$OTA_HEADER_FILE" \
+    --extra-comments "$IMAGE_BUILDER_EXE"
 
 STATUS=$?
 if [ "$STATUS" != 0 ]; then
-  echo "Error: Header generation script failed."
-  exit "$STATUS"
+    echo "Error: Header generation script failed."
+    exit "$STATUS"
 fi
 
 echo -e "\nScript Success!"
