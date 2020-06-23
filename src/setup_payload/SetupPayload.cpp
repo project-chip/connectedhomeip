6a19765684a1f0f79fa8d2b0b4e7356176d82cf8
shana-apple <61782012+shana-apple@users.noreply.github.com>
2020-06-23 23:06:23 +0200

    Allow serial number to be both an unsigned int and a string (#1043)
    * [SetupPayload] Allow serial number to be string/int
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/setup_payload/SetupPayload.cpp
+++ src/setup_payload/SetupPayload.cpp
@@ -36,6 +36,16 @@ using namespace chip::TLV;
 
 namespace chip {
 
+bool IsCHIPTag(uint8_t tag)
+{
+    return tag >= (1 << kRawVendorTagLengthInBits);
+}
+
+bool IsVendorTag(uint8_t tag)
+{
+    return tag < (1 << kRawVendorTagLengthInBits);
+}
+
 // Check the Setup Payload for validity
 //
 // `vendor_id` and `product_id` are allowed all of uint16_t
@@ -90,49 +100,230 @@ bool SetupPayload::isValidManualCode()
     return true;
 }
 
-CHIP_ERROR SetupPayload::addVendorOptionalData(OptionalQRCodeInfo info)
+CHIP_ERROR SetupPayload::addOptionalVendorData(uint8_t tag, string data)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
-    VerifyOrExit(info.tag < 1 << kRawVendorTagLengthInBits, err = CHIP_ERROR_INVALID_ARGUMENT);
-    optionalData[info.tag] = info;
+    OptionalQRCodeInfo info;
+    info.tag  = tag;
+    info.type = optionalQRCodeInfoTypeString;
+    info.data = data;
+
+    err = addOptionalVendorData(info);
+    SuccessOrExit(err);
+
 exit:
     return err;
 }
 
-CHIP_ERROR SetupPayload::addCHIPOptionalData(OptionalQRCodeInfo info)
+CHIP_ERROR SetupPayload::addOptionalVendorData(uint8_t tag, int32_t data)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
-    VerifyOrExit(info.tag >= 1 << kRawVendorTagLengthInBits, err = CHIP_ERROR_INVALID_ARGUMENT);
-    optionalData[info.tag] = info;
+    OptionalQRCodeInfo info;
+    info.tag   = tag;
+    info.type  = optionalQRCodeInfoTypeInt32;
+    info.int32 = data;
+
+    err = addOptionalVendorData(info);
+    SuccessOrExit(err);
+
 exit:
     return err;
 }
 
-vector<OptionalQRCodeInfo> SetupPayload::getAllOptionalData()
+vector<OptionalQRCodeInfo> SetupPayload::getAllOptionalVendorData()
 {
     vector<OptionalQRCodeInfo> returnedOptionalInfo;
-    for (map<uint64_t, OptionalQRCodeInfo>::iterator it = optionalData.begin(); it != optionalData.end(); ++it)
+    for (map<uint8_t, OptionalQRCodeInfo>::iterator it = optionalVendorData.begin(); it != optionalVendorData.end(); ++it)
     {
         returnedOptionalInfo.push_back(it->second);
     }
     return returnedOptionalInfo;
 }
 
-CHIP_ERROR SetupPayload::removeOptionalData(uint8_t tag)
+CHIP_ERROR SetupPayload::removeOptionalVendorData(uint8_t tag)
 {
-    if (optionalData.find(tag) == optionalData.end())
-    {
-        return CHIP_ERROR_KEY_NOT_FOUND;
-    }
-    optionalData.erase(tag);
-    return CHIP_NO_ERROR;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    VerifyOrExit(optionalVendorData.find(tag) != optionalVendorData.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+    optionalVendorData.erase(tag);
+
+exit:
+    return err;
 }
 
-bool SetupPayload::operator==(const SetupPayload & input)
+CHIP_ERROR SetupPayload::addSerialNumber(string serialNumber)
 {
-    return this->version == input.version && this->vendorID == input.vendorID && this->productID == input.productID &&
-        this->requiresCustomFlow == input.requiresCustomFlow && this->rendezvousInformation == input.rendezvousInformation &&
-        this->discriminator == input.discriminator && this->setUpPINCode == input.setUpPINCode;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    OptionalQRCodeInfoExtension info;
+    info.tag  = kSerialNumberTag;
+    info.type = optionalQRCodeInfoTypeString;
+    info.data = serialNumber;
+
+    err = addOptionalExtensionData(info);
+    SuccessOrExit(err);
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::addSerialNumber(uint32_t serialNumber)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    OptionalQRCodeInfoExtension info;
+    info.tag    = kSerialNumberTag;
+    info.type   = optionalQRCodeInfoTypeUInt32;
+    info.uint32 = serialNumber;
+
+    err = addOptionalExtensionData(info);
+    SuccessOrExit(err);
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::getSerialNumber(string & outSerialNumber)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    OptionalQRCodeInfoExtension info;
+    err = getOptionalExtensionData(kSerialNumberTag, info);
+    SuccessOrExit(err);
+
+    switch (info.type)
+    {
+    case (optionalQRCodeInfoTypeString):
+        outSerialNumber = info.data;
+        break;
+    case (optionalQRCodeInfoTypeUInt32):
+        outSerialNumber = to_string(info.uint32);
+        break;
+    default:
+        err = CHIP_ERROR_INVALID_ARGUMENT;
+    }
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::removeSerialNumber(void)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    VerifyOrExit(optionalExtensionData.find(kSerialNumberTag) != optionalExtensionData.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+    optionalExtensionData.erase(kSerialNumberTag);
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::addOptionalVendorData(OptionalQRCodeInfo info)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    VerifyOrExit(IsVendorTag(info.tag), err = CHIP_ERROR_INVALID_ARGUMENT);
+    optionalVendorData[info.tag] = info;
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::addOptionalExtensionData(OptionalQRCodeInfoExtension info)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    VerifyOrExit(IsCHIPTag(info.tag), err = CHIP_ERROR_INVALID_ARGUMENT);
+    optionalExtensionData[info.tag] = info;
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::getOptionalVendorData(uint8_t tag, OptionalQRCodeInfo & info)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    VerifyOrExit(optionalVendorData.find(tag) != optionalVendorData.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+    info = optionalVendorData[tag];
+
+exit:
+    return err;
+}
+
+CHIP_ERROR SetupPayload::getOptionalExtensionData(uint8_t tag, OptionalQRCodeInfoExtension & info)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    VerifyOrExit(optionalExtensionData.find(tag) != optionalExtensionData.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+    info = optionalExtensionData[tag];
+
+exit:
+    return err;
+}
+
+optionalQRCodeInfoType SetupPayload::getNumericTypeFor(uint8_t tag)
+{
+    optionalQRCodeInfoType elemType = optionalQRCodeInfoTypeUnknown;
+
+    if (IsVendorTag(tag))
+    {
+        elemType = optionalQRCodeInfoTypeInt32;
+    }
+    else if (tag == kSerialNumberTag)
+    {
+        elemType = optionalQRCodeInfoTypeUInt32;
+    }
+
+    return elemType;
+}
+
+vector<OptionalQRCodeInfoExtension> SetupPayload::getAllOptionalExtensionData()
+{
+    vector<OptionalQRCodeInfoExtension> returnedOptionalInfo;
+    for (map<uint8_t, OptionalQRCodeInfoExtension>::iterator it = optionalExtensionData.begin(); it != optionalExtensionData.end();
+         ++it)
+    {
+        returnedOptionalInfo.push_back(it->second);
+    }
+    return returnedOptionalInfo;
+}
+
+bool SetupPayload::operator==(SetupPayload & input)
+{
+    bool isIdentical = true;
+    vector<OptionalQRCodeInfo> inputOptionalVendorData;
+    vector<OptionalQRCodeInfoExtension> inputOptionalExtensionData;
+
+    VerifyOrExit(this->version == input.version && this->vendorID == input.vendorID && this->productID == input.productID &&
+                     this->requiresCustomFlow == input.requiresCustomFlow &&
+                     this->rendezvousInformation == input.rendezvousInformation && this->discriminator == input.discriminator &&
+                     this->setUpPINCode == input.setUpPINCode,
+                 isIdentical = false);
+
+    inputOptionalVendorData = input.getAllOptionalVendorData();
+    VerifyOrExit(optionalVendorData.size() == inputOptionalVendorData.size(), isIdentical = false);
+
+    for (OptionalQRCodeInfo inputInfo : inputOptionalVendorData)
+    {
+        OptionalQRCodeInfo info;
+        CHIP_ERROR err = getOptionalVendorData(inputInfo.tag, info);
+        VerifyOrExit(err == CHIP_NO_ERROR, isIdentical = false);
+        VerifyOrExit(inputInfo.type == info.type, isIdentical = false);
+        VerifyOrExit(inputInfo.data.compare(info.data) == 0, isIdentical = false);
+        VerifyOrExit(inputInfo.int32 == info.int32, isIdentical = false);
+    }
+
+    inputOptionalExtensionData = input.getAllOptionalExtensionData();
+    VerifyOrExit(optionalExtensionData.size() == inputOptionalExtensionData.size(), isIdentical = false);
+
+    for (OptionalQRCodeInfoExtension inputInfo : inputOptionalExtensionData)
+    {
+        OptionalQRCodeInfoExtension info;
+        CHIP_ERROR err = getOptionalExtensionData(inputInfo.tag, info);
+        VerifyOrExit(err == CHIP_NO_ERROR, isIdentical = false);
+        VerifyOrExit(inputInfo.type == info.type, isIdentical = false);
+        VerifyOrExit(inputInfo.data.compare(info.data) == 0, isIdentical = false);
+        VerifyOrExit(inputInfo.int32 == info.int32, isIdentical = false);
+        VerifyOrExit(inputInfo.int64 == info.int64, isIdentical = false);
+        VerifyOrExit(inputInfo.uint32 == info.uint32, isIdentical = false);
+        VerifyOrExit(inputInfo.uint64 == info.uint64, isIdentical = false);
+    }
+
+exit:
+    return isIdentical;
 }
 
 } // namespace chip
