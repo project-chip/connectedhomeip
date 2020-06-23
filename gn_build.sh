15cf1ca6781476fd3ffc68f1a2103b61df82a380
Michael Spang <spang@google.com>
2020-06-22 22:17:35 -0400

    Add tips to gn_build.sh
    To run a GN build of chip:
      ./gn_build.sh

--- gn_build.sh
+++ gn_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Copyright (c) 2020 Project CHIP Authors
 #
@@ -18,37 +18,63 @@
 CHIP_ROOT="$(dirname "$0")"
 
 _chip_red() {
-  echo -e "\033[0;31m$*\033[0m"
+    echo -e "\033[0;31m$*\033[0m"
 }
 
 _chip_yellow() {
-  echo -e "\033[0;33m$*\033[0m"
+    echo -e "\033[0;33m$*\033[0m"
 }
 
 _chip_banner() {
-  _chip_yellow '.--------------------------------'
-  _chip_yellow "-- $1"
-  _chip_yellow "'--------------------------------"
+    _chip_yellow '.--------------------------------'
+    _chip_yellow "-- $1"
+    _chip_yellow "'--------------------------------"
 }
 
 _chip_banner "Environment bringup"
 
-git -C "${CHIP_ROOT}" submodule update --init
+git -C "$CHIP_ROOT" submodule update --init
 
-source "${CHIP_ROOT}/activate.sh"
+set +e
+source "$CHIP_ROOT/activate.sh"
+set -e
 
 _chip_banner "Build: GN configure"
 
-gn --root="${CHIP_ROOT}" gen --check "${CHIP_ROOT}/out/debug"
-gn --root="${CHIP_ROOT}" gen --check "${CHIP_ROOT}/out/release" --args='is_debug=false'
+gn --root="$CHIP_ROOT" gen --check "$CHIP_ROOT/out/debug" --args='target_os="all"'
+gn --root="$CHIP_ROOT" gen --check "$CHIP_ROOT/out/release" --args='target_os="all" is_debug=false'
 
 _chip_banner "Build: Ninja build"
 
-time ninja -C "${CHIP_ROOT}/out/debug" check
+time ninja -C "$CHIP_ROOT/out/debug" check
 
-# Release build.
-# time ninja -C "${CHIP_ROOT}/out/release" check
+echo
+echo 'To re-bootstrap build environment in your shell, run:'
+echo source "$CHIP_ROOT/bootstrap.sh"
+echo
 
-# Standalone example app.
-# gn --root="${CHIP_ROOT}/examples/lock-app/nrf5" gen --check "${CHIP_ROOT}/out/nrf5_lock_app" --args=''
-# time ninja -C "${CHIP_ROOT}/out/nrf5_lock_app"
+echo 'To activate existing build environment in your shell, run (do this first):'
+echo source "$CHIP_ROOT/activate.sh"
+
+echo
+echo 'To build a debug build:'
+echo gn gen "$CHIP_ROOT/out/debug" --args=\''target_os="all"'\'
+echo ninja -C "$CHIP_ROOT/out/debug"
+
+echo
+echo 'To run tests (idempotent):'
+echo ninja -C "$CHIP_ROOT/out/debug" check
+
+echo
+echo 'To build & test an optimized build:'
+echo gn gen "$CHIP_ROOT/out/release" --args=\''target_os="all" is_debug=false'\'
+echo ninja -C "$CHIP_ROOT/out/release" check
+
+echo
+echo 'To build a custom build (for help run "gn args --list out/debug")'
+echo gn args "$CHIP_ROOT/out/custom"
+echo ninja -C "$CHIP_ROOT/out/custom"
+
+echo
+echo 'To build the nRF5 lock sample as a standalone project':
+echo "(cd $CHIP_ROOT/examples/lock-app/nrf5; gn gen out/debug; ninja -C out/debug)"
