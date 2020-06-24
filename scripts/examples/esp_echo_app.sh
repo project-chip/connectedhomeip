649d080416639cd2e52467ca805bb61cb1d956b9
Restyled.io <commits@restyled.io>
2020-06-24 20:31:19 +0000

    Restyled by shfmt

--- scripts/examples/esp_echo_app.sh
+++ scripts/examples/esp_echo_app.sh
@@ -9,7 +9,13 @@ make -j -f Makefile-bootstrap repos
 source "$root"/idf.sh
 
 SDKCONFIG_DEFAULTS=$root/sdkconfig_devkit.defaults idf make -j V=1 -C "$root" defconfig
-idf make -j V=1 -C "$root" || { echo 'build DevKit-C failed' ; exit 1; }
+idf make -j V=1 -C "$root" || {
+    echo 'build DevKit-C failed'
+    exit 1
+}
 
 SDKCONFIG_DEFAULTS=$root/sdkconfig_m5stack.defaults idf make -j V=1 -C "$root" defconfig
-idf make -j V=1 -C "$root" || { echo 'build M5Stack failed' ; exit 1; }
+idf make -j V=1 -C "$root" || {
+    echo 'build M5Stack failed'
+    exit 1
+}
