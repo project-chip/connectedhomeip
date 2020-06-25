899bd2e979073444c0884e7377b5d6124cd2716f
Restyled.io <commits@restyled.io>
2020-06-25 08:37:18 +0000

    Restyled by shellharden

--- activate.sh
+++ activate.sh
@@ -1,6 +1,6 @@
 CHIP_ROOT="$(dirname "$0")"
 
-export PW_BRANDING_BANNER="${CHIP_ROOT}/.chip-banner.txt"
+export PW_BRANDING_BANNER="$CHIP_ROOT/.chip-banner.txt"
 export PW_BRANDING_BANNER_COLOR="bold_white"
 
-source "${CHIP_ROOT}/third_party/pigweed/activate.sh"
+source "$CHIP_ROOT/third_party/pigweed/activate.sh"
