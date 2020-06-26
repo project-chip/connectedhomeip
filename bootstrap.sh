871ce0be2c5405ddd32ab919ab9fb17712ecf994
Michael Spang <spang@google.com>
2020-06-26 11:47:03 -0400

    Add dependency on pigweed (#1264)
    * Add dependency on pigweed
    
    Pigweed is a open source project containing libraries and tools for
    embedded development. It provides a package management system that
    downloads versioned development tools including ARM GCC, cmake, GN,
    ninja, openocd, and python on Linux, Mac, & Windows. These packages are
    hosted by Google in CIPD, which is part of Chrome's open source
    infrastructure.
    
    Using the tools provided by pigweed is not required, but having the
    option of using a reproducible development environment is useful,
    especially for new developers.
    
    Usage:
      source bootstrap.sh  # first time and after an update
      source activate.sh  # setup environment from prior bootstrap
    
    * Add CHIP branding to bootstrap
    
    Co-authored-by: Keir Mierle <keir@google.com>

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+CHIP_ROOT="$(dirname "$0")"
+
+export PW_BRANDING_BANNER="$CHIP_ROOT/.chip-banner.txt"
+export PW_BRANDING_BANNER_COLOR="bold_white"
+
+git submodule update --init
+
+source "$CHIP_ROOT/third_party/pigweed/bootstrap.sh"
