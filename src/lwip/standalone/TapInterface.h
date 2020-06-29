38cf73104b25efed6489f5c4658910a7a65cad9b
Robert Szewczyk <szewczyk@google.com>
2020-06-29 09:07:49 -0700

    Cleanup/license headers (#1322)
    * Attach missing licenses
    
    Attached missing licenses to files that were missing the license
    header.
    
    * Apache license additions
    
    Files in examples/lock-app/efr32/ must carry Apache license or a
    suitable open source license.
    
    Files in src/app/gen/gen/* are generated; attaching the copyright
    notice to those files to satisfy the linter.

--- src/lwip/standalone/TapInterface.h
+++ src/lwip/standalone/TapInterface.h
@@ -2,7 +2,8 @@
  *
  *    Copyright (c) 2020 Project CHIP Authors
  *    Copyright (c) 2014-2017 Nest Labs, Inc.
- * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ *    Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
