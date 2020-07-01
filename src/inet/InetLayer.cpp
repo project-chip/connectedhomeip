369276f08715d68c603d59462926a065dfeedb7b
Vidhi Shah <vidhi.s88@gmail.com>
2020-06-30 23:41:31 -0700

    Adding back ifaddrs-android.h 'include's to support the min Android API=21 in CHIP.

--- src/inet/InetLayer.cpp
+++ src/inet/InetLayer.cpp
@@ -65,7 +65,11 @@
 #include <fcntl.h>
 #include <net/if.h>
 #include <unistd.h>
+#ifdef __ANDROID__
+#include "ifaddrs-android.h"
+#else
 #include <ifaddrs.h>
+#endif // __ANDROID__
 #endif // CHIP_SYSTEM_CONFIG_USE_LWIP
 
 namespace chip {
