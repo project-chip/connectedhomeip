21930c58abe224f769fdc2fb87fb4187b5aa1e8d
Restyled.io <commits@restyled.io>
2020-07-07 07:15:27 +0000

    Restyled by google-java-format

--- src/setup_payload/java/src/chip/setuppayload/SetupPayloadParser.java
+++ src/setup_payload/java/src/chip/setuppayload/SetupPayloadParser.java
@@ -3,20 +3,21 @@ package chip.setuppayload;
 /** Parser for scanned QR code or manual entry code. */
 public class SetupPayloadParser {
 
-    /** Returns {@link SetupPayload} parsed from the QR code string. */
-    public SetupPayload parseQrCode(String qrCodeString) {
-        return fetchPayloadFromQrCode(qrCodeString);
-    }
+  /** Returns {@link SetupPayload} parsed from the QR code string. */
+  public SetupPayload parseQrCode(String qrCodeString) {
+    return fetchPayloadFromQrCode(qrCodeString);
+  }
 
-    /** Returns {@link SetupPayload} parsed from the manual entry code string. */
-    public SetupPayload parseManualEntryCode(String entryCodeString) {
-        return fetchPayloadFromManualEntryCode(entryCodeString);
-    }
+  /** Returns {@link SetupPayload} parsed from the manual entry code string. */
+  public SetupPayload parseManualEntryCode(String entryCodeString) {
+    return fetchPayloadFromManualEntryCode(entryCodeString);
+  }
 
-    private native SetupPayload fetchPayloadFromQrCode(String qrCodeString);
-    private native SetupPayload fetchPayloadFromManualEntryCode(String entryCodeString);
+  private native SetupPayload fetchPayloadFromQrCode(String qrCodeString);
 
-    static {
-        System.loadLibrary("SetupPayloadParser");
-    }
+  private native SetupPayload fetchPayloadFromManualEntryCode(String entryCodeString);
+
+  static {
+    System.loadLibrary("SetupPayloadParser");
+  }
 }
