8b02e269b9a9ff4bbe5d0c7bc92e3aade566f623
Andrei Litvin <andy314@gmail.com>
2020-07-09 09:07:45 -0400

    Fix the free of packet buffers

--- src/transport/Tuple.h
+++ src/transport/Tuple.h
@@ -157,7 +157,7 @@ private:
     template <size_t N, typename std::enable_if<(N >= sizeof...(TransportTypes))>::type * = nullptr>
     CHIP_ERROR SendMessageImpl(const MessageHeader & header, const PeerAddress & address, System::PacketBuffer * msgBuf)
     {
-        msgBuf->Free();
+        System::PacketBuffer::Free(msgBuf);
         return CHIP_ERROR_NO_MESSAGE_HANDLER;
     }
 
