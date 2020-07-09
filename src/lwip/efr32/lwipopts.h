efa630b58e514fa7a3240dcbeb2ffebd5adc68c4
Michael Spang <spang@google.com>
2020-07-08 22:23:08 -0400

    Support building CHIP with GN (#1456)
    * Add nlunit-test to GN build
    
    * Add nlassert to GN build
    
    * Add nlfaultinjection to GN build
    
    * Add nlio to GN build
    
    * Add GN files to gitignore
    
    * Add GN build support using pw_toolchain & pw_build
    
    This uses build infrastructure from pigweed to enable building CHIP with
    GN.
    
    * Add lwIP to GN build
    
    * Add Mbed TLS to GN build
    
    * Add SystemConfig to GN build
    
    * Add CHIPConfig to GN build
    
    * Add src/lib/support to GN build
    
    * Add src/system to GN build
    
    * Add src/inet to GN build
    
    * Add BLE to GN build
    
    * Add src/lib/core to GN build
    
    * Add src/controller to GN build
    
    * Add libCHIP.a to GN build
    
    * Add src/system/tests to GN build
    
    * Add CHIPVersion.h generation to GN build
    
    * Add src/lib/support/tests to GN build
    
    * Add src/inet/tests to GN build
    
    * Add src/ble/tests to GN build
    
    * Add src/lib/core/tests to GN build
    
    * Add src/crypto to GN build
    
    * Add src/app to GN build
    
    * Add src/crypto/tests to GN build
    
    * Add support for building nRF5 lock app
    
    * Run unit tests with some help from pigweed
    
    This enables running tests during the build. Just run
    
      gn gen out/debug
      ninja -C out/debug check
    
    * Add qrcodetool to GN build
    
    * Add transport to GN build
    
    * Add src/setup_payout/tests to GN build
    
    * Add src/setup_payload to GN build
    
    * Add ARM toolchain support
    
    * Make the build relocatable
    
    * Add nRF5 lock example superproject build
    
    * Add support for combined build via target_os="all"
    
    This configures host_clang, host_gcc, and nRF5 lock app.
    
    To run a maximal build:
    
      gn gen out/debug --args='target_os="all"'
      ninja -C out/debug check
    
    * Make bootstrap display cleaner
    
    * Add GN build wrapper script
    
    This script does an end to end bootstrap & build with GN from a fresh
    clone.
    
    Usage: ./gn_build.sh
    
    * Add examples/chip-tool to GN build
    
    * Remove shared imports
    
    Importing everything from chip.gni will require some dependencies to be
    present even if they are not used. Import overrides separately.
    
    Also add args to disable various builds at the top level.
    
    * Add transport tests to GN build
    
    * Add examples/shell to GN build
    
    * Add tips to gn_build.sh
    
    To run a GN build of chip:
      ./gn_build.sh
    
    * Enable building with homebrew openssl on Mac via pkg-config
    
    * Add mbedtls build to top level build
    
    * Add nRF5 lock app back to example build
    
    But only if NRF5_SDK_ROOT is set in the environment.
    
    * Harmonize library outputs with automake
    
    This changes most libraries to match the names used by automake.
    
    * Restyled by shfmt
    
    * Move the GN build files to //gn
    
    And use the secondary_source feature to still allow loading them via
    //build. This breaks using ${chip_root} in superproject builds, but that
    was unnecessary anyway.
    
    We should try to unwind this later as it causes some confusion about
    what path to specify.
    
    * Fix lint warning "Except block directly handles BaseException"
    
    * Move activate above bootstrap
    
    The usual case is to source activate.sh, not bootstrap.sh, so move that
    first. bootstrap.sh is only needed after an update.
    
    * Add github actions for GN
    
    * Move project specific build logic to gn/chip
    
    * Add comments to GN build GitHub workflow helpers
    
    * Fix the nRF5 lock app CI build
    
    Remove a broken link that was left behind after moving build files to //gn.
    
    * Updating Build Version
    
    * Re-merge repos.conf with .gitmodules
    
    This was unmerged by a recent commit.
    
    * Move bootstrap.sh & activate.sh to scripts
    
    * Fixup activate.sh & bootstrap.sh after moving
    
    Co-authored-by: Keir Mierle <keir@google.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Justin Wood <woody@apple.com>

--- src/lwip/efr32/lwipopts.h
+++ src/lwip/efr32/lwipopts.h
@@ -142,9 +142,10 @@
 #define DEFAULT_UDP_RECVMBOX_SIZE 6
 #define DEFAULT_TCP_RECVMBOX_SIZE 6
 
-// TODO: make LWIP_DEBUG conditional on build type
-
+#ifndef LWIP_DEBUG
 #define LWIP_DEBUG 1
+#endif
+
 #define MEMP_OVERFLOW_CHECK (0)
 #define MEMP_SANITY_CHECK (0)
 #define MEM_DEBUG (LWIP_DBG_OFF)
