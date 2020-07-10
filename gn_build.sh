35e4eeea7e3d10ac9f077f753fa2dcbde2b067c4
Rob Walker <robert_g_walker@apple.com>
2020-07-09 19:16:27 -0700

    bin/bash to /usr/bin/env bash

--- gn_build.sh
+++ gn_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 #
 # Copyright (c) 2020 Project CHIP Authors
 #
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+set -e
 
 CHIP_ROOT="$(dirname "$0")"
 
