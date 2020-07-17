3feae0d2948da04c41c962fa5f92a9ed1c96c8e4
Vivien Nicolas <vnicolas@apple.com>
2020-07-17 18:41:38 +0200

    Add BlePlatformDelegate, BleApplicationDelegate, BleConnectionDelegate code to src/platform/Darwin
    The delegates use CoreBluetooth which, unlike Network, is not directly available as C++ headers.
    As a result the mentioned delegates are implemented using Objective-c++.

---
+++ src/platform/Darwin/BLEManagerImpl.cpp
@@ -0,0 +1,133 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2018 Nest Labs, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *          Provides an implementation of the BLEManager singleton object
+ *          for Darwin platforms.
+ */
+#include <platform/internal/CHIPDeviceLayerInternal.h>
+
+#include <ble/CHIPBleServiceData.h>
+#include <platform/Darwin/BleApplicationDelegate.h>
+#include <platform/Darwin/BleConnectionDelegate.h>
+#include <platform/Darwin/BlePlatformDelegate.h>
+#include <support/logging/CHIPLogging.h>
+
+#include <new>
+
+#if CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
+
+using namespace ::chip;
+using namespace ::chip::Ble;
+
+namespace chip {
+namespace DeviceLayer {
+namespace Internal {
+
+BLEManagerImpl BLEManagerImpl::sInstance;
+
+CHIP_ERROR BLEManagerImpl::_Init()
+{
+    CHIP_ERROR err;
+
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+
+    // Initialize the Chip BleLayer.
+    BleApplicationDelegateImpl * appDelegate   = new BleApplicationDelegateImpl();
+    BleConnectionDelegateImpl * connDelegate   = new BleConnectionDelegateImpl();
+    BlePlatformDelegateImpl * platformDelegate = new BlePlatformDelegateImpl();
+    err                                        = BleLayer::Init(platformDelegate, connDelegate, appDelegate, &SystemLayer);
+    return err;
+}
+
+ConnectivityManager::CHIPoBLEServiceMode BLEManagerImpl::_GetCHIPoBLEServiceMode(void)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return ConnectivityManager::kCHIPoBLEServiceMode_NotSupported;
+}
+
+CHIP_ERROR BLEManagerImpl::_SetCHIPoBLEServiceMode(ConnectivityManager::CHIPoBLEServiceMode val)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+bool BLEManagerImpl::_IsAdvertisingEnabled(void)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return false;
+}
+
+CHIP_ERROR BLEManagerImpl::_SetAdvertisingEnabled(bool val)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+bool BLEManagerImpl::_IsFastAdvertisingEnabled(void)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return false;
+}
+
+CHIP_ERROR BLEManagerImpl::_SetFastAdvertisingEnabled(bool val)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+bool BLEManagerImpl::_IsAdvertising(void)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return false;
+}
+
+CHIP_ERROR BLEManagerImpl::_GetDeviceName(char * buf, size_t bufSize)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+CHIP_ERROR BLEManagerImpl::_SetDeviceName(const char * deviceName)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+BleLayer * BLEManagerImpl::_GetBleLayer() const
+{
+    return (BleLayer *) (this);
+}
+
+uint16_t BLEManagerImpl::_NumConnections(void)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+    return 0;
+}
+
+void BLEManagerImpl::_OnPlatformEvent(const ChipDeviceEvent * event)
+{
+    ChipLogProgress(DeviceLayer, "%s", __FUNCTION__);
+}
+
+} // namespace Internal
+} // namespace DeviceLayer
+} // namespace chip
+
+#endif // CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
---
+++ src/platform/Darwin/BLEManagerImpl.h
@@ -0,0 +1,97 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *          Provides an implementation of the BLEManager singleton object
+ *          for the Darwin platforms.
+ */
+
+#ifndef BLE_MANAGER_IMPL_H
+#define BLE_MANAGER_IMPL_H
+
+#if CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
+
+namespace chip {
+namespace DeviceLayer {
+namespace Internal {
+
+using namespace chip::Ble;
+
+/**
+ * Concrete implementation of the BLEManagerImpl singleton object for the Darwin platforms.
+ */
+class BLEManagerImpl final : public BLEManager, private BleLayer
+{
+    // Allow the BLEManager interface class to delegate method calls to
+    // the implementation methods provided by this class.
+    friend BLEManager;
+
+private:
+    // ===== Members that implement the BLEManager internal interface.
+
+    CHIP_ERROR _Init(void);
+    CHIPoBLEServiceMode _GetCHIPoBLEServiceMode(void);
+    CHIP_ERROR _SetCHIPoBLEServiceMode(CHIPoBLEServiceMode val);
+    bool _IsAdvertisingEnabled(void);
+    CHIP_ERROR _SetAdvertisingEnabled(bool val);
+    bool _IsFastAdvertisingEnabled(void);
+    CHIP_ERROR _SetFastAdvertisingEnabled(bool val);
+    bool _IsAdvertising(void);
+    CHIP_ERROR _GetDeviceName(char * buf, size_t bufSize);
+    CHIP_ERROR _SetDeviceName(const char * deviceName);
+    uint16_t _NumConnections(void);
+    void _OnPlatformEvent(const ChipDeviceEvent * event);
+    BleLayer * _GetBleLayer(void) const;
+
+    // ===== Members for internal use by the following friends.
+
+    friend BLEManager & BLEMgr(void);
+    friend BLEManagerImpl & BLEMgrImpl(void);
+
+    static BLEManagerImpl sInstance;
+};
+
+/**
+ * Returns a reference to the public interface of the BLEManager singleton object.
+ *
+ * Internal components should use this to access features of the BLEManager object
+ * that are common to all platforms.
+ */
+inline BLEManager & BLEMgr(void)
+{
+    return BLEManagerImpl::sInstance;
+}
+
+/**
+ * Returns the platform-specific implementation of the BLEManager singleton object.
+ *
+ * Internal components can use this to gain access to features of the BLEManager
+ * that are specific to the Darwin platforms.
+ */
+inline BLEManagerImpl & BLEMgrImpl(void)
+{
+    return BLEManagerImpl::sInstance;
+}
+
+} // namespace Internal
+} // namespace DeviceLayer
+} // namespace chip
+
+#endif // CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
+
+#endif // BLE_MANAGER_IMPL_H
