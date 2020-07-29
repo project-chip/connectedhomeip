5fb5fd4e1967d44b0f6b37b1f33429bfdc2be205
Boris Zbarsky <bzbarsky@apple.com>
2020-07-29 18:58:06 -0400

    Implement handling of DefaultResponse in chip-tool command-line client.

--- src/app/chip-zcl-zpro/command-encoder/decoder.c
+++ src/app/chip-zcl-zpro/command-encoder/decoder.c
@@ -105,14 +105,14 @@ void printApsFrame(EmberApsFrame * frame)
 
 uint16_t extractMessage(uint8_t * buffer, uint16_t buffer_length, uint8_t ** msg)
 {
-    // buffer is at least 16 bytes long and the msg is 3 bytes long and 13 bytes offset into the buffer.
-    // This is hard coded now till we refactor encoder.c to not have this be hardcoded.
+    // The size of an encoded EmberApsFrame is currently 13 bytes.  The message
+    // starts after that.
     uint16_t result = 0;
-    if (msg && buffer_length >= 16)
+    if (msg && buffer_length > 13)
     {
         // These are hard coded for now.
         *msg   = buffer + 13;
-        result = 3;
+        result = buffer_length - 13;
     }
     else if (msg)
     {
