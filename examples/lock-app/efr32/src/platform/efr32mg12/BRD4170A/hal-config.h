63d0046bad8afa461465f4ace9186d85d897b817
jmartinez-silabs <junior.martinez@silabs.com>
2020-08-03 11:38:27 -0400

    Add platform subdirectories in lock-app/efr32 to allow succesful builds with different boards on efr32 family MG12 and MG21 Add a board_id define in the makefile to match board capability in the code Add files for the mbedtls crypto on  MG21 family

---
+++ examples/lock-app/efr32/src/platform/efr32mg12/BRD4170A/hal-config.h
@@ -0,0 +1,86 @@
+/***************************************************************************/
+/**
+ * @file
+ * @brief hal-config.h
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc.
+ *www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon
+ *Laboratories Inc. Your use of this software is
+ *governed by the terms of Silicon Labs Master
+ *Software License Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement.
+ *This software is distributed to you in Source Code
+ *format and is governed by the sections of the MSLA
+ *applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#ifndef HAL_CONFIG_H
+#define HAL_CONFIG_H
+
+#include "board_features.h"
+#include "hal-config-app-common.h"
+#include "hal-config-board.h"
+
+#ifndef HAL_VCOM_ENABLE
+#define HAL_VCOM_ENABLE (1)
+#endif
+#ifndef HAL_I2CSENSOR_ENABLE
+#define HAL_I2CSENSOR_ENABLE (0)
+#endif
+
+#ifndef HAL_SPIDISPLAY_ENABLE
+#define HAL_SPIDISPLAY_ENABLE (1)
+#endif
+#define HAL_SPIDISPLAY_EXTCOMIN_CALLBACK
+#if defined(FEATURE_IOEXPANDER)
+#define HAL_SPIDISPLAY_EXTMODE_EXTCOMIN (0)
+#else
+#define HAL_SPIDISPLAY_EXTMODE_EXTCOMIN (1)
+#endif
+#define HAL_SPIDISPLAY_EXTMODE_SPI (0)
+#define HAL_SPIDISPLAY_EXTCOMIN_USE_PRS (0)
+#define HAL_SPIDISPLAY_EXTCOMIN_USE_CALLBACK (0)
+#define HAL_SPIDISPLAY_FREQUENCY (1000000)
+
+// $[CMU]
+#define HAL_CLK_HFCLK_SOURCE (HAL_CLK_HFCLK_SOURCE_HFXO)
+#define HAL_CLK_LFECLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
+#define HAL_CLK_LFBCLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
+#define HAL_CLK_LFACLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
+#define HAL_CLK_HFXO_AUTOSTART (HAL_CLK_HFXO_AUTOSTART_NONE)
+
+// $[DCDC]
+#define HAL_DCDC_BYPASS (0)
+
+// $[PA]
+#define HAL_PA_ENABLE (1)
+
+// $[SERIAL]
+#define HAL_SERIAL_USART0_ENABLE (0)
+#define HAL_SERIAL_LEUART0_ENABLE (0)
+#define HAL_SERIAL_USART1_ENABLE (0)
+#define HAL_SERIAL_USART2_ENABLE (0)
+#define HAL_SERIAL_USART3_ENABLE (0)
+#define HAL_SERIAL_RXWAKE_ENABLE (0)
+#define HAL_SERIAL_IDLE_WAKE_ENABLE (1)
+
+#define HAL_SERIAL_APP_RX_QUEUE_SIZE (128UL)
+#define HAL_SERIAL_APP_BAUD_RATE (115200UL)
+#define HAL_SERIAL_APP_RXSTOP (16UL)
+#define HAL_SERIAL_APP_RXSTART (16UL)
+#define HAL_SERIAL_APP_TX_QUEUE_SIZE (128UL)
+#define HAL_SERIAL_APP_FLOW_CONTROL (HAL_USART_FLOW_CONTROL_HWUART)
+#define HAL_USART0_ENABLE (1)
+#define HAL_USART0_RX_QUEUE_SIZE (128UL)
+#define HAL_USART0_BAUD_RATE (115200UL)
+#define HAL_USART0_RXSTOP (16UL)
+#define HAL_USART0_RXSTART (16UL)
+#define HAL_USART0_TX_QUEUE_SIZE (128UL)
+#define HAL_USART0_FLOW_CONTROL (HAL_USART_FLOW_CONTROL_NONE)
+
+#endif
---
+++ examples/lock-app/efr32/src/platform/efr32mg12/BRD4304A/init_mcu.c
@@ -0,0 +1,167 @@
+/***************************************************************************//**
+ * @file
+ * @brief init_mcu.c
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#if defined(HAL_CONFIG)
+#include "bsphalconfig.h"
+#include "hal-config.h"
+#else
+#include "bspconfig.h"
+#endif
+
+#include "board_features.h"
+
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_rtcc.h"
+
+#include "bsp.h"
+
+#include "init_mcu.h"
+
+
+// Bit [19] in MODULEINFO is the HFXOCALVAL:
+// 1=No factory cal, use default XO tunning value in FW
+// 0=Factory Cal, use XO tunning value in DI 
+#define DEVINFO_MODULEINFO_HFXOCALVAL_MASK  0x00080000UL
+// Calibration value for HFXO CTUNE is at DEVINFO Offset 0x08
+#define DEVINFO_MODULEINFO_CRYSTALOSCCALVAL  (*((uint16_t *) (uint32_t)(DEVINFO_BASE+0x8UL)))
+// [15:9] : (LFXOTUNING) Calibration for LFXO TUNING
+// [8:0]  : (HFXOCTUNE) Calibration for HFXO CTUNE
+#define DEVINFO_HFXOCTUNE_MASK  0x01FFUL
+
+#define set_HFXO_CTUNE(val) do {hfxoInit.ctuneSteadyState = (val);} while (0)
+
+static void initMcu_clocks(void);
+static void initHFXO(void);
+
+void initMcu(void)
+{
+  // Device errata
+  CHIP_Init();
+
+  // Set up DC-DC converter
+  EMU_DCDCInit_TypeDef dcdcInit = BSP_DCDC_INIT;
+  #if HAL_DCDC_BYPASS
+  dcdcInit.dcdcMode = emuDcdcMode_Bypass;
+  #endif
+  EMU_DCDCInit(&dcdcInit);
+
+  // Set up clocks
+  initMcu_clocks();
+
+
+  RTCC_Init_TypeDef rtccInit = RTCC_INIT_DEFAULT;
+  rtccInit.enable                = true;
+  rtccInit.debugRun              = false;
+  rtccInit.precntWrapOnCCV0      = false;
+  rtccInit.cntWrapOnCCV1         = false;
+  rtccInit.prescMode             = rtccCntTickPresc;
+  rtccInit.presc                 = rtccCntPresc_1;
+  rtccInit.enaOSCFailDetect      = false;
+  rtccInit.cntMode               = rtccCntModeNormal;
+  RTCC_Init(&rtccInit);
+
+#if defined(EMU_VSCALE_PRESENT)
+  // Set up EM0, EM1 energy mode configuration
+  EMU_EM01Init_TypeDef em01Init = EMU_EM01INIT_DEFAULT;
+  EMU_EM01Init(&em01Init);
+#endif // EMU_VSCALE_PRESENT
+  // Set up EM2, EM3 energy mode configuration
+  EMU_EM23Init_TypeDef em23init = EMU_EM23INIT_DEFAULT;
+#if defined(EMU_VSCALE_PRESENT)
+  em23init.vScaleEM23Voltage = emuVScaleEM23_LowPower;
+#endif // EMU_VSCALE_PRESENT
+  EMU_EM23Init(&em23init);
+
+
+#if defined(RMU_PRESENT)
+  // Set reset mode for sysreset back to DEFAULT (extended), this might have
+  // been changed by the bootloader to FULL reset.
+  RMU->CTRL = (RMU->CTRL & ~_RMU_CTRL_SYSRMODE_MASK) | RMU_CTRL_SYSRMODE_DEFAULT;
+#endif
+
+}
+
+static void initMcu_clocks(void)
+{
+  // Initialize HFXO
+  initHFXO();
+
+  // Set system HFXO frequency
+  SystemHFXOClockSet(BSP_CLK_HFXO_FREQ);
+
+  // Enable HFXO oscillator, and wait for it to be stable
+  CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
+
+  // Enable HFXO Autostart only if EM2 voltage scaling is disabled.
+  // In 1.0 V mode the chip does not support frequencies > 21 MHz,
+  // this is why HFXO autostart is not supported in this case.
+#if!defined(_EMU_CTRL_EM23VSCALE_MASK)
+  // Automatically start and select HFXO
+  CMU_HFXOAutostartEnable(0, true, true);
+#else
+  CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+#endif//_EMU_CTRL_EM23VSCALE_MASK
+
+  // HFRCO not needed when using HFXO
+  CMU_OscillatorEnable(cmuOsc_HFRCO, false, false);
+
+  // Enabling HFBUSCLKLE clock for LE peripherals
+  CMU_ClockEnable(cmuClock_HFLE, true);
+
+
+  // Initialize LFXO
+  CMU_LFXOInit_TypeDef lfxoInit = BSP_CLK_LFXO_INIT;
+  lfxoInit.ctune = BSP_CLK_LFXO_CTUNE;
+  CMU_LFXOInit(&lfxoInit);
+  // Set system LFXO frequency
+  SystemLFXOClockSet(BSP_CLK_LFXO_FREQ);
+
+  // Set LFXO if selected as LFCLK
+  CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+  CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
+  CMU_ClockSelectSet(cmuClock_LFE, cmuSelect_LFXO);
+}
+
+static void initHFXO(void)
+{
+  // Initialize HFXO
+  // Use BSP_CLK_HFXO_INIT as last result (4th)
+  CMU_HFXOInit_TypeDef hfxoInit = BSP_CLK_HFXO_INIT;
+  // if Factory Cal exists in DEVINFO then use it above all (1st)
+  if (0 == (DEVINFO->MODULEINFO & DEVINFO_MODULEINFO_HFXOCALVAL_MASK)) {
+#if defined(_SILICON_LABS_32B_SERIES_1)
+    set_HFXO_CTUNE(DEVINFO_MODULEINFO_CRYSTALOSCCALVAL);
+#elif defined(_SILICON_LABS_32B_SERIES_2)
+    set_HFXO_CTUNE(DEVINFO->MODXOCAL & _DEVINFO_MODXOCAL_HFXOCTUNEXIANA_MASK);
+#endif
+  }
+  // if User page has CTUNE from studio use that in 2nd place
+#if (MFG_CTUNE_EN == 1)
+  else if (MFG_CTUNE_VAL != 0xFFFF) {
+    set_HFXO_CTUNE(MFG_CTUNE_VAL);
+  }
+#endif
+  // 3rd option, get data from header defined for product/board
+#if defined(BSP_CLK_HFXO_CTUNE) && BSP_CLK_HFXO_CTUNE >= 0
+  else {
+    set_HFXO_CTUNE(BSP_CLK_HFXO_CTUNE);
+  }
+#endif
+  CMU_HFXOInit(&hfxoInit);
+}
