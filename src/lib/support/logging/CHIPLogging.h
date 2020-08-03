0110b6497a7b83a6a91e083114197471bc8169d4
Jiacheng Guo <gjc@google.com>
2020-08-03 13:29:13 +0800

    [misc] make IsCategoryEnabled an inline function
    This will make this symbol to be under namespace to avoid conflict with
    other codebases.

--- src/lib/support/logging/CHIPLogging.h
+++ src/lib/support/logging/CHIPLogging.h
@@ -327,11 +327,11 @@ static inline void GetModuleName(char * buf, uint8_t module)
 
 extern uint8_t gLogFilter;
 
-#define IsCategoryEnabled(CAT) ((CAT) <= gLogFilter)
+inline bool IsCategoryEnabled(uint8_t CAT) { return (CAT <= gLogFilter); }
 
 #else // CHIP_LOG_FILTERING
 
-#define IsCategoryEnabled(CAT) (true)
+inline bool IsCategoryEnabled(uint8_t CAT) { return true; }
 
 #endif // CHIP_LOG_FILTERING
 
