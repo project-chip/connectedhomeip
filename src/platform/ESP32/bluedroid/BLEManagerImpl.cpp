0671c4a80f95e5afc3b66b02f5ea486bc51d9636
Prasad Alatkar <prasad.alatkar@espressif.com>
2020-08-04 23:37:32 +0530

    ESP32: Add optional NimBLE support for BLE transport
    - Refactor `ESP32/BLEManagerImpl.cpp` into `ESP32/bluedroid/` and
      `ESP32/nimble/` sub-directories.
    - Keep `BLEManagerImpl.h` common for both bluedroid and nimble.
    - Modified BUILD.gn accordingly.
    - Make NimBLE as default BLE stack for wifi-echo example.
    
    Restyled

--- src/platform/ESP32/bluedroid/BLEManagerImpl.cpp
+++ src/platform/ESP32/bluedroid/BLEManagerImpl.cpp
@@ -27,6 +27,10 @@
 
 #if CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
 
+#include "sdkconfig.h"
+
+#if CONFIG_BT_BLUEDROID_ENABLED
+
 #include <ble/CHIPBleServiceData.h>
 #include <platform/internal/BLEManager.h>
 #include <support/CodeUtils.h>
@@ -1305,5 +1309,7 @@ void BLEManagerImpl::DriveBLEState(intptr_t arg)
 } // namespace Internal
 } // namespace DeviceLayer
 } // namespace chip
+
+#endif // CONFIG_BT_BLUEDROID_ENABLED
 
 #endif // CHIP_DEVICE_CONFIG_ENABLE_CHIPOBLE
