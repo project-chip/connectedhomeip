9d84c482304e7e474cdca5dc98586c2a6740f150
jmartinez-silabs <junior.martinez@silabs.com>
2020-08-05 15:39:44 -0400

    Add the IRQ priority init for all EFR32 boards init Restyle some file headers and copyright mentions

--- examples/lock-app/efr32/include/hal-config-app-common.h
+++ examples/lock-app/efr32/include/hal-config-app-common.h
@@ -1,22 +1,36 @@
-/***************************************************************************/ /**
-                                                                               * @file
-                                                                               * @brief hal-config-app-common.h
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2018 Silicon Laboratories Inc.
-                                                                               *www.silabs.com</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * The licensor of this software is Silicon
-                                                                               *Laboratories Inc. Your use of this software is
-                                                                               *governed by the terms of Silicon Labs Master
-                                                                               *Software License Agreement (MSLA) available at
-                                                                               * www.silabs.com/about-us/legal/master-software-license-agreement.
-                                                                               *This software is distributed to you in Source Code
-                                                                               *format and is governed by the sections of the MSLA
-                                                                               *applicable to Source Code.
-                                                                               *
-                                                                               ******************************************************************************/
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/******************************************************************************
+ * @file
+ * @brief hal-config-app-common.h
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
 #ifndef HAL_CONFIG_APP_COMMON_H
 #define HAL_CONFIG_APP_COMMON_H
--- examples/lock-app/efr32/src/platform/efr32mg12/BRD4166A/init_board.c
+++ examples/lock-app/efr32/src/platform/efr32mg12/BRD4166A/init_board.c
@@ -15,7 +15,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 /*******************************************************************************
  * @file
  * @brief init_board.c
