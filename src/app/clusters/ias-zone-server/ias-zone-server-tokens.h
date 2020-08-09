2b69ff1ff1a176d3d520d10f61e79ff223fe1404
Rob Walker <robert_g_walker@apple.com>
2020-08-09 16:53:20 -0700

    restyled

--- src/app/clusters/ias-zone-server/ias-zone-server-tokens.h
+++ src/app/clusters/ias-zone-server/ias-zone-server-tokens.h
@@ -31,24 +31,24 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-/***************************************************************************//**
+/****************************************************************************
  * @file
  * @brief Tokens for the IAS Zone Server plugin.
  *******************************************************************************
-   ******************************************************************************/
+ ******************************************************************************/
 
 /**
  * Custom Application Tokens
  */
-#define CREATOR_PLUGIN_IAS_ZONE_SERVER_ENROLLMENT_METHOD  (0x0020)
+#define CREATOR_PLUGIN_IAS_ZONE_SERVER_ENROLLMENT_METHOD (0x0020)
 #define NVM3KEY_PLUGIN_IAS_ZONE_SERVER_ENROLLMENT_METHOD (NVM3KEY_DOMAIN_USER | 0x0020)
 
 #ifdef DEFINETYPES
 // Include or define any typedef for tokens here
-#endif  // DEFINETYPES
+#endif // DEFINETYPES
 
 #ifdef DEFINETOKENS
 // Define the actual token storage information here
 DEFINE_BASIC_TOKEN(PLUGIN_IAS_ZONE_SERVER_ENROLLMENT_METHOD, uint8_t, 0xFF)
 
-#endif  // DEFINETOKENS
+#endif // DEFINETOKENS
