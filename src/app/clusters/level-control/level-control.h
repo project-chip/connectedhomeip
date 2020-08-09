f88ebacff5bb041df2c5002ec35b669b41ece824
Sergei Lissianoi <selissia@silabs.com>
2020-08-09 16:11:26 -0700

    Add missing level-control content

--- src/app/clusters/level-control/level-control.h
+++ src/app/clusters/level-control/level-control.h
@@ -14,7 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 /**
  *
  *    Copyright (c) 2020 Silicon Labs
@@ -31,3 +30,18 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+/***************************************************************************/
+/**
+ * @file
+ * @brief APIs and defines for the Level Control plugin, which implements the
+ *        Level Control cluster.
+ ******************************************************************************/
+ 
+// Rate of level control tick execution.
+// To increase tick frequency (for more granular updates of device state based
+// on level), redefine EMBER_AF_PLUGIN_LEVEL_CONTROL_TICKS_PER_SECOND.
+#ifndef EMBER_AF_PLUGIN_LEVEL_CONTROL_TICKS_PER_SECOND
+  #define EMBER_AF_PLUGIN_LEVEL_CONTROL_TICKS_PER_SECOND 32
+#endif
+#define EMBER_AF_PLUGIN_LEVEL_CONTROL_TICK_TIME \
+  (MILLISECOND_TICKS_PER_SECOND / EMBER_AF_PLUGIN_LEVEL_CONTROL_TICKS_PER_SECOND)
