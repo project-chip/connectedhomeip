6827fcefece1ef9104b4ac69f1649933c4cdba43
Sergei Lissianoi <selissia@silabs.com>
2020-08-09 16:11:27 -0700

    Add missing content in several plugins

--- src/app/clusters/ias-zone-client/ias-zone-client.h
+++ src/app/clusters/ias-zone-client/ias-zone-client.h
@@ -31,3 +31,41 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+/***************************************************************************//**
+ * @file
+ * @brief APIs and defines for the IAS Zone Client plugin, which keeps track of
+ *        IAS Zone servers.
+ *******************************************************************************
+   ******************************************************************************/
+
+typedef struct {
+  EmberEUI64 ieeeAddress;
+  EmberNodeId nodeId;
+  uint16_t zoneType;
+  uint16_t zoneStatus;
+  uint8_t zoneState;
+  uint8_t endpoint;
+  uint8_t zoneId;
+} IasZoneDevice;
+
+extern IasZoneDevice emberAfIasZoneClientKnownServers[];
+
+#define NO_INDEX 0xFF
+#define UNKNOWN_ENDPOINT 0
+
+#define UNKNOWN_ZONE_ID 0xFF
+
+void emAfClearServers(void);
+
+void emberAfPluginIasZoneClientZdoCallback(EmberNodeId emberNodeId,
+                                           EmberApsFrame* apsFrame,
+                                           uint8_t* message,
+                                           uint16_t length);
+
+void emberAfPluginIasZoneClientWriteAttributesResponseCallback(EmberAfClusterId clusterId,
+                                                               uint8_t * buffer,
+                                                               uint16_t bufLen);
+
+void emberAfPluginIasZoneClientReadAttributesResponseCallback(EmberAfClusterId clusterId,
+                                                              uint8_t * buffer,
+                                                              uint16_t bufLen);
