6da1e81d839be71ecf53bbf5c0e199d50056f788
Song Guo <songguo@google.com>
2020-08-20 08:19:06 +0800

    Fix format

--- scripts/tests/cirque_tests.sh
+++ scripts/tests/cirque_tests.sh
@@ -23,76 +23,76 @@ SOURCE_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 REPO_DIR="$SOURCE_DIR/../../"
 
 function __flask_clean() {
-	flask_pid=$(ps aux | grep "[f]lask" | awk '{print $2}' | sort -k2 -rn)
-	if [ ! -z "$flask_pid" ]; then
-		for pid in "$flask_pid"; do
-			kill -2 "$pid" >/dev/null 2>&1
-		done
-	fi
+    flask_pid=$(ps aux | grep "[f]lask" | awk '{print $2}' | sort -k2 -rn)
+    if [ ! -z "$flask_pid" ]; then
+        for pid in "$flask_pid"; do
+            kill -2 "$pid" >/dev/null 2>&1
+        done
+    fi
 }
 
 function __socat_clean() {
-	socat_pid=$(ps aux | grep "[s]ocat" | awk '{print $2}')
-	if [ ! -z "$socat_pid" ]; then
-		for pid in "$socat_pid"; do
-			kill -2 "$pid" >/dev/null 2>&1
-		done
-	fi
+    socat_pid=$(ps aux | grep "[s]ocat" | awk '{print $2}')
+    if [ ! -z "$socat_pid" ]; then
+        for pid in "$socat_pid"; do
+            kill -2 "$pid" >/dev/null 2>&1
+        done
+    fi
 }
 
 function __virtual_thread_clean() {
-	vthread_pid=$(ps aux | grep "[o]t-ncp-ftd" | awk '{print $2}')
-	if [ ! -z "$vthread_pid" ]; then
-		for pid in "$vthread_pid"; do
-			kill -2 "$pid" >/dev/null 2>&1
-		done
-	fi
+    vthread_pid=$(ps aux | grep "[o]t-ncp-ftd" | awk '{print $2}')
+    if [ ! -z "$vthread_pid" ]; then
+        for pid in "$vthread_pid"; do
+            kill -2 "$pid" >/dev/null 2>&1
+        done
+    fi
 }
 
 __cirquetest_start_flask() {
-	cd "$REPO_DIR"/third_party/cirque
-	FLASK_APP='cirque/restservice/service.py' \
-		PATH="$PATH":"$REPO_DIR"/third_party/cirque/openthread/output/x86_64-unknown-linux-gnu/bin/ \
-		python3 -m flask run
+    cd "$REPO_DIR"/third_party/cirque
+    FLASK_APP='cirque/restservice/service.py' \
+        PATH="$PATH":"$REPO_DIR"/third_party/cirque/openthread/output/x86_64-unknown-linux-gnu/bin/ \
+        python3 -m flask run
 }
 
 __cirquetest_clean_flask() {
-	__flask_clean
-	__socat_clean
-	__virtual_thread_clean
+    __flask_clean
+    __socat_clean
+    __virtual_thread_clean
 }
 
 cirquetest_bootstrap() {
-	cd "$REPO_DIR"/third_party/cirque
-	sudo apt-get install -y bazel socat psmisc tigervnc-standalone-server tigervnc-viewer python3-pip python3-venv python3-setuptools libdbus-glib-1-dev libgirepository1.0-dev
-	pip3 install pycodestyle==2.5.0 wheel
-	make NO_GRPC=1 install -j
-	./dependency_modules.sh
-	pip3 install -r requirements_nogrpc.txt
+    cd "$REPO_DIR"/third_party/cirque
+    sudo apt-get install -y bazel socat psmisc tigervnc-standalone-server tigervnc-viewer python3-pip python3-venv python3-setuptools libdbus-glib-1-dev libgirepository1.0-dev
+    pip3 install pycodestyle==2.5.0 wheel
+    make NO_GRPC=1 install -j
+    ./dependency_modules.sh
+    pip3 install -r requirements_nogrpc.txt
 }
 
 cirquetest_run_test() {
-	# Start Cirque flash server
-	__cirquetest_start_flask &
-	sleep 5
-	cd "$REPO_DIR"/src/test_driver/linux-cirque
-	./do_on-off-cluster-test.sh
-	__cirquetest_clean_flask
-	sleep 10
-	# Cleanup to run more tests
-	yes | docker system prune
+    # Start Cirque flash server
+    __cirquetest_start_flask &
+    sleep 5
+    cd "$REPO_DIR"/src/test_driver/linux-cirque
+    ./do_on-off-cluster-test.sh
+    __cirquetest_clean_flask
+    sleep 10
+    # Cleanup to run more tests
+    yes | docker system prune
 }
 
 subcommand="$1"
 shift
 
 case $subcommand in
-*)
-	cirquetest_"$subcommand" "$@"
-	exitcode=$?
-	if [ $? = 127 ]; then
-		echo "Unknown command: $subcommand" >&2
-	fi
-	exit "$exitcode"
-	;;
+    *)
+        cirquetest_"$subcommand" "$@"
+        exitcode=$?
+        if [ $? = 127 ]; then
+            echo "Unknown command: $subcommand" >&2
+        fi
+        exit "$exitcode"
+        ;;
 esac
