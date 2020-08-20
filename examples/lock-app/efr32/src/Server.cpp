32f990344a7b47cb0e5c21b2a78dced1b94ec787
jmartinez-silabs <67972863+jmartinez-silabs@users.noreply.github.com>
2020-08-20 13:05:51 -0400

    Add BoltLockManager and Silabs Cluster implementation  (#2203)
    * -Include the correct FreeRTOS Cortex files in the EFR32 makefiles correspoding to the defined MCU family (MG12 vs MG21)
    -Init all IRQs priority to a lower priority valid for FreeRTOS API. An IRQ in the gecko radio libs
     with the default priority 0 (highest) was causing a assert failure in FreeRTOS
    
    * Add the IRQ priority init for all EFR32 boards init
    Restyle some file headers and copyright mentions
    
    * Add BoltLockManager to manage the lock and unlock request and state
    Add DataModelHandler to handle bolt actions from the cluster messages
    Add Gen folder with the files for silicon lab cluster implementation
    Strt a server session for UDP messages
    Include some mbedtls sources  files from gsdk 2.7 in gni . TO BE FIX
    
    * merge upstream
    
    * Add BoltLockManager to manage the lock and unlock request and state
    Add DataModelHandler to handle bolt actions from the cluster messages
    Add Gen folder with the files for silicon lab cluster implementation
    Start a server session for UDP messages
    Include some mbedtls sources  files from gsdk 2.7 in gni . TO BE FIX
    
    merge upstream
    
    * Add support for Silabs dev board BRD4163A and BRD4164A
    
    Merge Upstream into branch
    
    Add BoltLockManager to manage the lock and unlock request and state
    Add DataModelHandler to handle bolt actions from the cluster messages
    Add Gen folder with the files for silicon lab cluster implementation
    Strt a server session for UDP messages
    Include some mbedtls sources  files from gsdk 2.7 in gni . TO BE FIX
    
    merge upstream
    
    Add the IRQ priority init for all EFR32 boards init
    Restyle some file headers and copyright mentions
    
    * Restyled by whitespace
    
    * Fix submodules
    
    * Clear up code and comment based on Andrei comments on PR
    
    * Restyled by gn
    
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ examples/lock-app/efr32/src/Server.cpp
@@ -0,0 +1,186 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "FreeRTOS.h"
+#include "appConfig.h"
+#include "task.h"
+#include <string.h>
+#include <sys/param.h>
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#include <inet/IPAddress.h>
+#include <inet/InetError.h>
+#include <inet/InetLayer.h>
+#include <platform/CHIPDeviceLayer.h>
+#include <support/CodeUtils.h>
+#include <support/ErrorStr.h>
+#include <system/SystemPacketBuffer.h>
+#include <transport/SecureSessionMgr.h>
+#include <transport/UDP.h>
+
+#include "Server.h"
+
+#include "attribute-storage.h"
+#include "chip-zcl/chip-zcl-zpro-codec.h"
+#include "gen/znet-bookkeeping.h"
+#include "util.h"
+
+using namespace ::chip;
+using namespace ::chip::Inet;
+using namespace ::chip::Transport;
+
+// Transport Callbacks
+namespace {
+
+#ifndef EXAMPLE_SERVER_NODEID
+// Use this hardcoded NODEID for the lock app example if none was provided
+#define EXAMPLE_SERVER_NODEID 0x3546526e
+#endif // EXAMPLE_SERVER_NODEID
+
+const uint8_t local_private_key[] = { 0xc6, 0x1a, 0x2f, 0x89, 0x36, 0x67, 0x2b, 0x26, 0x12, 0x47, 0x4f,
+                                      0x11, 0x0e, 0x34, 0x15, 0x81, 0x81, 0x12, 0xfc, 0x36, 0xeb, 0x65,
+                                      0x61, 0x07, 0xaa, 0x63, 0xe8, 0xc5, 0x22, 0xac, 0x52, 0xa1 };
+
+const uint8_t remote_public_key[] = { 0x04, 0x30, 0x77, 0x2c, 0xe7, 0xd4, 0x0a, 0xf2, 0xf3, 0x19, 0xbd, 0xfb, 0x1f,
+                                      0xcc, 0x88, 0xd9, 0x83, 0x25, 0x89, 0xf2, 0x09, 0xf3, 0xab, 0xe4, 0x33, 0xb6,
+                                      0x7a, 0xff, 0x73, 0x3b, 0x01, 0x35, 0x34, 0x92, 0x73, 0x14, 0x59, 0x0b, 0xbd,
+                                      0x44, 0x72, 0x1b, 0xcd, 0xb9, 0x02, 0x53, 0xd9, 0xaf, 0xcc, 0x1a, 0xcd, 0xae,
+                                      0xe8, 0x87, 0x2e, 0x52, 0x3b, 0x98, 0xf0, 0xa1, 0x88, 0x4a, 0xe3, 0x03, 0x75 };
+
+class ServerCallback : public SecureSessionMgrCallback
+{
+public:
+    virtual void OnMessageReceived(const MessageHeader & header, Transport::PeerConnectionState * state,
+                                   System::PacketBuffer * buffer, SecureSessionMgrBase * mgr)
+    {
+        const size_t data_len = buffer->DataLength();
+        char src_addr[PeerAddress::kMaxToStringSize];
+
+        // as soon as a client connects, assume it is connected
+        VerifyOrExit(buffer != NULL, EFR32_LOG("Received data but couldn't process it..."));
+        VerifyOrExit(header.GetSourceNodeId().HasValue(), EFR32_LOG("Unknown source for received message"));
+
+        VerifyOrExit(state->GetPeerNodeId() != kUndefinedNodeId, EFR32_LOG("Unknown source for received message"));
+
+        state->GetPeerAddress().ToString(src_addr, sizeof(src_addr));
+
+        EFR32_LOG("Packet received from %s: %zu bytes", src_addr, static_cast<size_t>(data_len));
+
+        HandleDataModelMessage(header, buffer, mgr);
+        buffer = NULL;
+
+    exit:
+        // SendTo calls Free on the buffer without an AddRef, if SendTo was not called, free the buffer.
+        if (buffer != NULL)
+        {
+            System::PacketBuffer::Free(buffer);
+        }
+    }
+
+    virtual void OnNewConnection(Transport::PeerConnectionState * state, SecureSessionMgrBase * mgr)
+    {
+        CHIP_ERROR err;
+
+        EFR32_LOG("Received a new connection.");
+
+        err = state->GetSecureSession().TemporaryManualKeyExchange(remote_public_key, sizeof(remote_public_key), local_private_key,
+                                                                   sizeof(local_private_key));
+        VerifyOrExit(err == CHIP_NO_ERROR, EFR32_LOG("Failed to setup encryption"));
+
+    exit:
+        return;
+    }
+
+private:
+    /**
+     * Handle a message that should be processed via our data model processing
+     * codepath.
+     *
+     * @param [in] buffer The buffer holding the message.  This function guarantees
+     *                    that it will free the buffer before returning.
+     */
+    void HandleDataModelMessage(const MessageHeader & header, System::PacketBuffer * buffer, SecureSessionMgrBase * mgr)
+    {
+        EmberApsFrame frame;
+        bool ret = extractApsFrame(buffer->Start(), buffer->DataLength(), &frame);
+        if (ret)
+        {
+            EFR32_LOG("APS framprocessing success!");
+        }
+        else
+        {
+            EFR32_LOG("APS processing failure!");
+            System::PacketBuffer::Free(buffer);
+            return;
+        }
+        ChipResponseDestination responseDest(header.GetSourceNodeId().Value(), mgr);
+        uint8_t * message;
+        uint16_t messageLen = extractMessage(buffer->Start(), buffer->DataLength(), &message);
+        ret                 = emberAfProcessMessage(&frame,
+                                    0, // type
+                                    message, messageLen,
+                                    &responseDest, // source identifier
+                                    NULL);
+
+        System::PacketBuffer::Free(buffer);
+
+        if (ret)
+        {
+            EFR32_LOG("Data model processing success!");
+        }
+        else
+        {
+            EFR32_LOG("Data model processing failure!");
+        }
+    }
+};
+
+static ServerCallback gCallbacks;
+
+} // namespace
+
+void InitDataModelHandler()
+{
+    emberAfEndpointConfigure();
+    emAfInit();
+}
+
+// The echo server assumes the platform's networking has been setup already
+void StartServer(DemoSessionManager * sessions)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    err = sessions->Init(EXAMPLE_SERVER_NODEID, &DeviceLayer::SystemLayer,
+                         UdpListenParameters(&DeviceLayer::InetLayer).SetAddressType(kIPAddressType_IPv6));
+    SuccessOrExit(err);
+
+    sessions->SetDelegate(&gCallbacks);
+
+exit:
+    if (err != CHIP_NO_ERROR)
+    {
+        EFR32_LOG("ERROR setting up transport: %s", ErrorStr(err));
+    }
+    else
+    {
+        EFR32_LOG("Lock Server Listening...");
+    }
+}
