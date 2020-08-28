43033e67d82f7bdb574f9518d33d3fec929a6bcd
Martin Turon <mturon@google.com>
2020-08-28 15:34:56 -0700

    [shell] [otcli] Switch to fork()/exec(). Add README for otcli command.

--- examples/shell/cmd_otcli.cpp
+++ examples/shell/cmd_otcli.cpp
@@ -36,6 +36,7 @@
 #include <openthread/thread.h>
 #else
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #endif
 
@@ -60,30 +61,17 @@ int cmd_otcli_help(int argc, char ** argv)
     return 0;
 }
 
+#if CHIP_TARGET_STYLE_EMBEDDED
+
 int cmd_otcli_dispatch(int argc, char ** argv)
 {
     CHIP_ERROR error = CHIP_NO_ERROR;
+
 // From OT CLI internal lib, kMaxLineLength = 128
 #define kMaxLineLength 128
     char buff[kMaxLineLength] = { 0 };
     char * buff_ptr           = buff;
     int i                     = 0;
-
-#if CHIP_TARGET_STYLE_UNIX
-    uid_t euid         = geteuid();
-    char ctl_command[] = "/usr/local/sbin/ot-ctl ";
-
-    // Must run as sudo.
-    if (euid != 0)
-    {
-        streamer_printf(streamer_get(), "Error otcli: requires running chip-shell as sudo\n\r");
-        error = CHIP_ERROR_INCORRECT_STATE;
-        ExitNow();
-    }
-
-    memcpy(buff, ctl_command, sizeof(ctl_command));
-    buff_ptr += sizeof(ctl_command) - 1;
-#endif
 
     VerifyOrExit(argc > 0, error = CHIP_ERROR_INVALID_ARGUMENT);
 
@@ -106,16 +94,54 @@ int cmd_otcli_dispatch(int argc, char ** argv)
     }
     buff_ptr = 0;
 
-#if CHIP_TARGET_STYLE_EMBEDDED
     otCliConsoleInputLine(buff, buff_ptr - buff);
-#else
-    streamer_printf(streamer_get(), "Exec: %s\n\r", buff);
-    system(buff);
-#endif
+exit:
+    return error;
+}
+
+#elif CHIP_TARGET_STYLE_UNIX
+
+int cmd_otcli_dispatch(int argc, char ** argv)
+{
+    CHIP_ERROR error = CHIP_NO_ERROR;
+
+    int pid, status;
+    uid_t euid         = geteuid();
+    char ctl_command[] = "/usr/local/sbin/ot-ctl";
+
+    // Must run as sudo.
+    if (euid != 0)
+    {
+        streamer_printf(streamer_get(), "Error otcli: requires running chip-shell as sudo\n\r");
+        error = CHIP_ERROR_INCORRECT_STATE;
+        ExitNow();
+    }
+
+    VerifyOrExit(argc > 0, error = CHIP_ERROR_INVALID_ARGUMENT);
+
+    // Fork and execute the command.
+    pid = fork();
+    VerifyOrExit(pid != -1, error = CHIP_ERROR_INCORRECT_STATE);
+
+    if (pid == 0)
+    {
+        // Child process to execute the command with provided arguments
+        --argv; // Restore access to entry [0] containing the command;
+        argv[0] = ctl_command;
+        execv(ctl_command, argv);
+        exit(0);
+    }
+    else
+    {
+        // Parent process to wait on child.
+        error = (waitpid(pid, &status, 0) == -1) ? CHIP_ERROR_INCORRECT_STATE : CHIP_NO_ERROR;
+    }
 
 exit:
     return error;
 }
+
+#endif // CHIP_TARGET_STYLE_UNIX
 
 static const shell_command_t cmds_otcli_root = { &cmd_otcli_dispatch, "otcli", "Dispatch OpenThread CLI command" };
 
