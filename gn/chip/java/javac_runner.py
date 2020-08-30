d74a0b6047a25d075d0215ccba4459c18f90bffb
Andrei Litvin <andrei@andy314.com>
2020-08-30 19:58:00 -0400

    GN Compilation for android JNI library (#2322)
    * Add ability to generate jar files (tested at least on android)
    
    * A few comments were updated
    
    * Support extra arguments for the javac runner. Code is cleaner now
    
    * conditionally compile java
    
    * Place java build rules in separate directory
    
    * Add build file for java
    
    * Compile JNI so
    
    * Compile for jni cpp files seems to work now
    
    * Run gn format
    
    * Run gn format on gni files as well
    
    * Moved the java compile to be android specific
    
    * Add libSetupPayloadParser as a java build
    
    * Reformat build.gn files
    
    * Fix LGTM.com warning for python package not used
    
    * Add setuppayload dependency to java setup payload, to match autotools deps
    
    * Fix LWIP compile error by including build config from the chip generated code
    
    Co-authored-by: Michael Spang <spang@google.com>

---
+++ gn/chip/java/javac_runner.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Wrapper script to run javac command as an action with gn."""
+
+import argparse
+import os
+import subprocess
+import sys
+
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+
+def IsExecutable(path):
+  """Returns whether file at |path| exists and is executable.
+
+  Args:
+    path: absolute or relative path to test.
+
+  Returns:
+    True if the file at |path| exists, False otherwise.
+  """
+  return os.path.isfile(path) and os.access(path, os.X_OK)
+
+
+def FindCommand(command):
+  """Looks up for |command| in PATH.
+
+  Args:
+    command: name of the command to lookup, if command is a relative or absolute
+      path (i.e. contains some path separator) then only that path will be
+      tested.
+
+  Returns:
+    Full path to command or None if the command was not found.
+
+    On Windows, this respects the PATHEXT environment variable when the
+    command name does not have an extension.
+  """
+  fpath, _ = os.path.split(command)
+  if fpath:
+    if IsExecutable(command):
+      return command
+
+  if sys.platform == 'win32':
+    # On Windows, if the command does not have an extension, cmd.exe will
+    # try all extensions from PATHEXT when resolving the full path.
+    command, ext = os.path.splitext(command)
+    if not ext:
+      exts = os.environ['PATHEXT'].split(os.path.pathsep)
+    else:
+      exts = [ext]
+  else:
+    exts = ['']
+
+  for path in os.environ['PATH'].split(os.path.pathsep):
+    for ext in exts:
+      path = os.path.join(path, command) + ext
+      if IsExecutable(path):
+        return path
+
+  return None
+
+
+def main():
+  java_path = FindCommand('javac')
+  if not java_path:
+    sys.stderr.write('javac: command not found\n')
+    sys.exit(EXIT_FAILURE)
+
+  parser = argparse.ArgumentParser('Javac runner')
+  parser.add_argument(
+      '--classdir',
+      dest='classdir',
+      required=True,
+      help='Directory that will contain class files')
+  parser.add_argument(
+      '--outfile',
+      dest='outfile',
+      required=True,
+      help='Output file containing a list of classes')
+  parser.add_argument(
+      'rest', metavar='JAVAC_ARGS', nargs='*', help='Argumets to pass to javac')
+
+  args = parser.parse_args()
+  retcode = subprocess.check_call([java_path] + args.rest)
+  if retcode != EXIT_SUCCESS:
+    return retcode
+
+  with open(args.outfile, 'wt') as f:
+    prefixlen = len(args.classdir) + 1
+    for root, dirnames, filenames in os.walk(args.classdir):
+      for filename in filenames:
+        if filename.endswith('.class'):
+          f.write(os.path.join(root[prefixlen:], filename))
+          f.write('\n')
+
+  return EXIT_SUCCESS
+
+
+if __name__ == '__main__':
+  sys.exit(main())
