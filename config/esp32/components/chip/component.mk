4e026defe482bc485d812502daf87da7ace02358
Rob Walker <robert_g_walker@apple.com>
2020-08-31 15:18:32 -0700

    fix build esp32 with gn

--- config/esp32/components/chip/component.mk
+++ config/esp32/components/chip/component.mk
@@ -221,8 +221,7 @@ install-chip-with-gn : $(OUTPUT_DIR)
 	echo esp32_cxx = \"$(CXX)\"              >> $(OUTPUT_DIR)/args.gn
 	echo esp32_cpu = \"esp32\"               >> $(OUTPUT_DIR)/args.gn
 	echo "Written file $(OUTPUT_DIR)/args.gn"
-#	MAKEFLAGS= make -C $(OUTPUT_DIR) --no-print-directory install
-	cd $(COMPONENT_PATH); gn gen $(OUTPUT_DIR)
+	cd $(CHIP_ROOT) && PW_ENVSETUP_QUIET=1 . scripts/activate.sh && cd $(COMPONENT_PATH) && gn gen $(OUTPUT_DIR)
 	cd $(COMPONENT_PATH); ninja -C $(OUTPUT_DIR) esp32
 
 install-chip-with-automake: configure-chip
