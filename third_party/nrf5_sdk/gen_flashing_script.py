6fcabc5adf66ad3dede2783f2a0af6d63c387caa
Kevin Schoedel <67607049+kpschoedel@users.noreply.github.com>
2020-08-31 14:37:45 -0400

    [GN] Flashing for nrf5 examples (#2344)
    * WIP: Demonstration of action()-based binary flashing.
    
    Strawman for one method of building flashable binaries and a consistent
    method of flashing them, for parity with some existing `make flash`
    commands and ultimately for automated testing.
    
    This is temporarily wholly inside examples/lock-app/nrf5 in order to
    experiment with minimal disruption but is intended to become generic.
    
    issue #1520 GN build parity - flashing
    
    * Remove include dependency on underlying crypto libraries (#2310)
    
    * Remove include dependency on underlying crypto libraries
    
    * Restyled by clang-format
    
    * Use nlassert, cpp style cast
    
    * Restyled by clang-format
    
    * use inline methods to cleanup the code
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * [shell] add missing inttypes.h header (#2357)
    
    * Address feedback and generalize.
    
    * WIP: Demonstration of action()-based binary flashing.
    
    Strawman for one method of building flashable binaries and a consistent
    method of flashing them, for parity with some existing `make flash`
    commands and ultimately for automated testing.
    
    This is temporarily wholly inside examples/lock-app/nrf5 in order to
    experiment with minimal disruption but is intended to become generic.
    
    issue #1520 GN build parity - flashing
    
    * Address feedback and generalize.
    
    * Add missing (ignored) symbolic link for `build`.
    
    * Remove unused import from gn_run_binary.py
    
    * Fix paths in flashing rules
    
    * Remove the build symlinks
    
    On second thought, we need to wait for automake removal for this.
    
    * Revised flashing script generation and execution.
    
    * Streamlined scripts and BUILD.gn files.
    
    * Script/generator cleanup and description.
    
    * Restyled by gn
    
    Co-authored-by: Pankaj Garg <pgarg2@apple.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Jiacheng Guo <gjc@google.com>
    Co-authored-by: Michael Spang <spang@google.com>

---
+++ third_party/nrf5_sdk/gen_flashing_script.py
@@ -0,0 +1,101 @@
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.!/usr/bin/env python
+
+"""Generate script to flash or erase an NRF5 device."""
+
+import argparse
+import os
+import stat
+import sys
+
+# Script to generate. This is a minimal wrapper around nrf_firmware_utils.
+# The substitutions are:
+#   scripts_dir - The location of nrf5_firmware_utils.
+#   defaults    - Options passed to nrf5_firmware_utils, including
+#                 in particular the image file to flash.
+
+SCRIPT = """#!/usr/bin/env python
+
+import sys
+
+SCRIPTS_DIR = '{scripts_dir}'
+DEFAULTS = {{
+{defaults}
+}}
+
+sys.path.append(SCRIPTS_DIR)
+import nrf5_firmware_utils
+
+if __name__ == '__main__':
+    sys.exit(nrf5_firmware_utils.flash_command(sys.argv[1:], DEFAULTS))
+"""
+
+
+# Generate flashing script.
+
+def main(argv):
+    """Generate script to flash or erase an NRF5 device."""
+    parser = argparse.ArgumentParser(description='Generate a flashing script')
+    parser.add_argument(
+        '--output',
+        metavar='FILENAME',
+        required=True,
+        help='flashing script name')
+    parser.add_argument(
+        '--scripts-dir',
+        metavar='DIR',
+        required=True,
+        help='nrf5 script utilities directory')
+    parser.add_argument(
+        '--nrfjprog',
+        metavar='FILENAME',
+        help='nrfjprog command')
+    parser.add_argument(
+        '--family',
+        metavar='FAMILY',
+        help='device family')
+    parser.add_argument(
+        '--softdevice',
+        metavar='GLOB',
+        help='softdevice file pattern')
+    parser.add_argument(
+        '--application',
+        metavar='FILENAME',
+        help='program to flash')
+
+    args = parser.parse_args(argv)
+    options = vars(args)
+
+    defaults = []
+    for key in ['nrfjprog', 'family', 'softdevice', 'application']:
+        if options.get(key):
+            defaults.append('  {}: {},'.format(repr(key), repr(options[key])))
+
+    script = SCRIPT.format(
+        scripts_dir=args.scripts_dir, defaults='\n'.join(defaults))
+    try:
+        with open(args.output, 'w') as script_file:
+            script_file.write(script)
+        os.chmod(args.output, (stat.S_IXUSR | stat.S_IRUSR | stat.S_IWUSR
+                             | stat.S_IXGRP | stat.S_IRGRP
+                             | stat.S_IXOTH | stat.S_IROTH))
+    except OSError as exception:
+        print(exception, sys.stderr)
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ third_party/nrf5_sdk/nrf5_firmware_utils.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Flash an NRF5 device.
+
+This is layered so that a caller can perform individual operations
+through a `Flasher` instance, or operations according to a command line.
+For `Flasher`, see the class documentation. For the parse_command()
+interface or standalone execution:
+
+usage: nrf5_firmware_utils.py [-h] [--verbose] [--nrfjprog FILE]
+                              [--family FAMILY] [--eraseall]
+                              [--softdevice FILE] [--skip-softdevice]
+                              [--application FILE] [--reset] [--skip-reset]
+
+Flash device
+
+optional arguments:
+  -h, --help          show this help message and exit
+
+configuration:
+  --verbose           Report more verbosely
+  --nrfjprog FILE     File name of the nrfjprog executable
+  --family FAMILY     NRF5 device famity
+
+operations:
+  --eraseall          Erase device before flashing
+  --softdevice FILE   Softdevice image file name
+  --skip-softdevice   Do not flash softdevice even if softdevice is set
+  --application FILE  Application image file name
+  --reset             Reset device after flashing
+  --skip-reset        Do not reset device after flashing
+"""
+
+import argparse
+import subprocess
+import sys
+
+# Here are the options that can be use to configure a `Flasher` object
+# (as dictionary keys) and/or passed as command line options.
+
+OPTIONS = {
+    # Configuration options define properties used in flashing operations.
+    'configuration': {
+        # Script configuration options.
+        'verbose': {
+            'help': 'Report more verbosely',
+            'default': 0,
+            'argument': {
+                'action': 'count'
+            },
+            # Levels:
+            #   0   - error message
+            #   1   - action to be taken
+            #   2   - results of action, even if successful
+            #   3+  - details
+        },
+
+        # Tool configuration options.
+        'nrfjprog': {
+            'help': 'File name of the nrfjprog executable',
+            'default': 'nrfjprog',
+            'argument': {
+                'metavar': 'FILE'
+            },
+        },
+
+        # Device configuration options.
+        'family': {
+            'help': 'NRF5 device famity',
+            'default': None,
+            'argument': {
+                'metavar': 'FAMILY'
+            },
+        },
+    },
+
+    # Action control options specify operations that Flasher.action() or
+    # the function interface flash_command() will perform.
+    'operations': {
+        # Action control options.
+        'eraseall': {
+            'help': 'Erase device before flashing',
+            'default': False,
+            'argument': {
+                'action': 'store_true'
+            },
+        },
+        'softdevice': {
+            'help': 'Softdevice image file name',
+            'default': None,
+            'argument': {
+                'metavar': 'FILE'
+            },
+        },
+        'skip-softdevice': {
+            'help': 'Do not flash softdevice even if softdevice is set',
+            'default': False,
+            'argument': {
+                'action': 'store_true'
+            },
+        },
+        'application': {
+            'help': 'Application image file name',
+            'default': None,
+            'argument': {
+                'metavar': 'FILE'
+            },
+        },
+        'reset': {
+            'help': 'Reset device after flashing',
+            'default': None,  # None = Reset iff application was flashed.
+            'argument': {
+                'action': 'store_true'
+            },
+        },
+    },
+}
+
+
+class Flasher:
+    """Manage nrf5 flashing."""
+
+    def __init__(self, options=None):
+        self.options = options or {}
+        # Obtain option defaults from `OPTIONS`.
+        for group_options in OPTIONS.values():
+            for key, info in group_options.items():
+                if key not in self.options:
+                    self.options[key] = info['default']
+        self.err = 0
+
+    def status(self):
+        """Return the current error code."""
+        return self.err
+
+    def log(self, level, *args):
+        """Optionally log a message to stderr."""
+        if self.options['verbose'] >= level:
+            print(*args, file=sys.stderr)
+
+    def nrfjprog(self, arguments):
+        """Run nrfjprog."""
+        command = [self.options['nrfjprog']]
+        family = self.options['family']
+        if family:
+            command += ['--family', family]
+        command += arguments
+        self.log(3, 'Execute:', *command)
+        self.err = subprocess.call(command)
+        return self
+
+    def nrfjprog_logging(self, arguments, name,
+                         pass_message=None, fail_message=None, fail_level=0):
+        """Run nrfjprog with log messages."""
+        self.log(1, name)
+        if self.nrfjprog(arguments).err:
+            self.log(fail_level, fail_message or ('FAILED: ' + name))
+        else:
+            self.log(2, pass_message or (name + ' complete'))
+        return self
+
+    def eraseall(self):
+        """Perform nrfjprog --eraseall"""
+        return self.nrfjprog_logging(['--eraseall'], 'Erase all')
+
+    def verify(self, image):
+        """Verify image."""
+        return self.nrfjprog_logging(['--quiet', '--verify', image],
+                                     'Verify', 'Verified', 'Not verified', 2)
+
+    def flash(self, image):
+        """Flash image."""
+        return self.nrfjprog_logging(['--program', image, '--sectorerase'],
+                                     'Flash', 'Flashed')
+
+    def reset(self):
+        """Reset the device."""
+        return self.nrfjprog_logging(['--reset'], 'Reset')
+
+    def actions(self):
+        """Perform actions on the device according to self.options."""
+        self.log(3, 'OPTIONS:', self.options)
+
+        if self.options['eraseall']:
+            if self.eraseall().err:
+                return self
+
+        softdevice = self.options['softdevice']
+        if softdevice and not self.options['skip_softdevice']:
+            if self.verify(softdevice).err:
+                if self.flash(softdevice).err:
+                    return self
+
+        application = self.options['application']
+        if application:
+            if self.flash(application).err:
+                return self
+            if self.options['reset'] is None:
+                self.options['reset'] = True
+
+        if self.options['reset']:
+            if self.reset().err:
+                return self
+
+        return self
+
+    def parse_argv(self, argv):
+        """Handle command line options."""
+        parser = argparse.ArgumentParser(description='Flash device')
+
+        # Command line options mirroring Flasher options.
+        group_parser = {}
+        for group, group_options in OPTIONS.items():
+            group_parser[group] = parser.add_argument_group(group)
+            for key, info in group_options.items():
+                group_parser[group].add_argument(
+                    '--' + key,
+                    help=info['help'],
+                    default=self.options[key],
+                    **info['argument'])
+
+        # Additional command line options.
+
+        # 'reset' is a three-way switch; if None, action() will reset the
+        # device if and only if an application image is flashed. So, we add
+        # an explicit option to set it false.
+        group_parser['operations'].add_argument(
+            '--skip-reset',
+            dest='reset',
+            action='store_false',
+            help='Do not reset device after flashing')
+
+        args = parser.parse_args(argv)
+        self.options.update(vars(args))
+        return self
+
+
+def flash_command(argv, defaults=None):
+    """Perform device actions according to the command line and defaults."""
+    return Flasher(defaults).parse_argv(argv).actions().status()
+
+
+if __name__ == '__main__':
+    sys.exit(flash_command(sys.argv[1:]))
