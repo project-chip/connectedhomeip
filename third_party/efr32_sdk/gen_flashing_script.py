493c1ad5101c06dc6fcd7d0aecbede5213846a2e
Kevin Schoedel <67607049+kpschoedel@users.noreply.github.com>
2020-09-01 21:50:47 -0400

    Flashing for efr32 lock-app example (#2403)
    * Flashing for efr32 examples
    
    Make-based builds for EFR32 lock-app had a `make flash` command to flash
    the device. GN builds have not had this functionality.
    
    With this change, GN builds produce a Python script with the executable
    name followed by .flash.py which when run flashes an attached device
    in the same way that make flash does for Make builds.
    
    The generated .flash.py script is a minimal wrapper that passes file
    names to a more generic efr32 flashing utility script. Similarly, changes
    to the application BUILD.gn are minimal.
    
    (It is intended that the platform-independent part of efr32_firmware_utils.py
    move to a module shared with other ..._firmware_utils.py scripts.)
    
    fixes #2384 - GN build flashing - EFR32
    
    * Restyled by prettier-markdown
    
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ third_party/efr32_sdk/gen_flashing_script.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.!/usr/bin/env python
+"""Generate script to flash or erase an EFR32 device."""
+
+import sys
+import efr32_firmware_utils
+
+if __name__ == '__main__':
+    sys.exit(efr32_firmware_utils.EFR32Flasher().make_wrapper(sys.argv[1:]))
