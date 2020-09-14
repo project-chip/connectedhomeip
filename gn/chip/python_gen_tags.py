35a1922d886373d506f653b76cf77fa18d6150b7
yunhanw <yunhanw@google.com>
2020-09-14 11:29:29 -0700

    Add initial python chip device controller and Bluez Manager for Linux
    Problem:
    We need python CLI to create BLE connection and do BTP traffic and
    beyond.
    
    Summary of Changes
    
    -- Add python chip device controller and Bluez Manager
    -- Add initial python wheel generation for test use
    -- Test using BLE scan, connect, BTP connection
    -- In BLE BTP, BLE_CONNECTION_OBJECT has been broadly used as void *, in central Linux controler tool side, we use python bluez and
    link to BTP using ctype, BLE_CONNECTION_OBJECT cannot be chip::DeviceLayer::Internal::BluezConnection.
    -- Minor fix for ble uuid dbus value in linux peripheral implementation
    
    fix #2027

---
+++ gn/chip/python_gen_tags.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Utility script to genertae the python tag + the abi tag + platform tag for a Python
+
+import distutils.util
+import sys
+import sysconfig
+
+# TODO: pigwee virtualenv don't have pep425tags, will revisit it after figuring out with pigwee
+# The below is temporary solution.
+#from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag, get_platform
+#print(get_abbr_impl() + get_impl_ver() + "-" + get_abi_tag() + "-" + get_platform())
+
+abbr_ver = "cp"
+impl_ver = str(sys.version_info[0]) + str(sys.version_info[1])
+if sysconfig.get_config_var('SOABI') is None:
+    abi_tag = abbr_ver + impl_ver
+else:
+    abi_tag = 'cp' + sysconfig.get_config_var('SOABI').split('-')[1]
+platform_tag = distutils.util.get_platform().replace('.', '_').replace('-', '_')
+
+print(abbr_ver + impl_ver + "-" + abi_tag + "-" + platform_tag)
