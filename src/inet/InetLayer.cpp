d5c67e40082e9afd3803324850eb835d15c5dab6
Restyled.io <commits@restyled.io>
2020-09-14 17:02:23 +0000

    Restyled by clang-format

--- src/inet/InetLayer.cpp
+++ src/inet/InetLayer.cpp
@@ -157,7 +157,7 @@ INET_ERROR InetLayer::InitQueueLimiter(void)
     const unsigned portBASE_TYPE initial = INET_CONFIG_MAX_DROPPABLE_EVENTS;
 
 #if (configSUPPORT_STATIC_ALLOCATION == 1)
-    mDroppableEvents = xSemaphoreCreateCountingStatic(maximum, initial, &mDroppableEventsObj);
+    mDroppableEvents                     = xSemaphoreCreateCountingStatic(maximum, initial, &mDroppableEventsObj);
 #else
     mDroppableEvents = xSemaphoreCreateCounting(maximum, initial);
 #endif
