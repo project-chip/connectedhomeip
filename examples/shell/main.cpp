c9025fee5c2a59db957d7dfabf89dc0851f424fc
oleggladyshev <67104610+oleggladyshev@users.noreply.github.com>
2020-09-15 22:35:51 +0300

    Port shell example to Nordic platform, add BLE transport commands. (#2010)
    * Small BLE changes to compile with BLE turned on
    
    * Building error fixed
    
    * New command stubs for BTP were added
    
    * Porting of the shell application to Nordic hardware was started
    
    * The shell application for nRF52 architecture was compiled
    
    * Shell app for nrf52 is running now
    
    * Thread comands were merged
    
    * Some code was removed
    
    * Some comments were changed
    
    * Small changes after a code review
    
    * The shall application was successfully compiled both for nRF52 and Linux
    
    * [shell] Project configuration was changed
    
    * [shell] New GN scripts for building the Shell app for an NRF5 were added
    
    * [shell] UART configuration for lighting and lock apps moved to app_config.h to aviod conflict with the shell app.
    
    * Restyled by gn
    
    * Restyled by gn
    
    * [shell][nrf5] The project can be built now
    
    * [shell] Logging initialization removed from the BLEManagerImpl
    
    * [shell] GN build scripts reverted to linux-only version
    
    * [shell] unnecessary GN build scripts were removed
    
    * [shell] vscode files reverted, build for Android was fixed
    
    * [shell] Shell application can be built with GN for both Linux and nRF archs
    
    * [shell] BLE initialization moved to the main.cpp
    
    * Restyled by gn
    
    * Restyled by shellharden
    
    * Add TODOs for softdevice & build cleanups
    
    * Fix the shell build
    
    * Fix nrfconnect build
    
    * appended to public_deps instead of redefining
    
    * [Shell] BUILD.gn was updated
    
    * [Shell] Compilation errors were fixed
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Michael Spang <spang@google.com>
    Co-authored-by: Viktor Strukov <viktor.strukov@dsr-corporation.com>

--- examples/shell/main.cpp
+++ examples/shell/main.cpp
@@ -24,16 +24,77 @@
 #include <lib/support/RandUtils.h>
 #include <support/logging/CHIPLogging.h>
 
+#if CHIP_DEVICE_LAYER_TARGET_NRF5
+#ifdef SOFTDEVICE_PRESENT
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
+#include "nrf_sdh_soc.h"
+#endif //#ifdef SOFTDEVICE_PRESENT
+
+#if CHIP_ENABLE_OPENTHREAD
+extern "C" {
+#include <openthread/platform/platform-softdevice.h>
+}
+#endif // CHIP_ENABLE_OPENTHREAD
+#endif
+
 using namespace chip;
 using namespace chip::Shell;
 
 void cmd_misc_init();
 void cmd_base64_init();
+void cmd_btp_init();
 void cmd_device_init();
 void cmd_otcli_init();
 
+#if defined(CHIP_DEVICE_LAYER_TARGET_NRF5)
+// TODO: Move softdevice initialization behind platform interface.
+static void OnSoCEvent(uint32_t sys_evt, void * p_context)
+{
+#if CHIP_ENABLE_OPENTHREAD
+    otSysSoftdeviceSocEvtHandler(sys_evt);
+#endif
+    UNUSED_PARAMETER(p_context);
+}
+
+CHIP_ERROR soft_device_init()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    err = nrf_sdh_enable_request();
+    SuccessOrExit(err);
+
+    while (!nrf_sdh_is_enabled())
+    {
+    }
+
+    // Register a handler for SOC events.
+    NRF_SDH_SOC_OBSERVER(m_soc_observer, NRF_SDH_SOC_STACK_OBSERVER_PRIO, OnSoCEvent, NULL);
+
+    {
+        uint32_t appRAMStart = 0;
+
+        // Configure the BLE stack using the default settings.
+        // Fetch the start address of the application RAM.
+        err = nrf_sdh_ble_default_cfg_set(CHIP_DEVICE_LAYER_BLE_CONN_CFG_TAG, &appRAMStart);
+        SuccessOrExit(err);
+
+        // Enable BLE stack.
+        err = nrf_sdh_ble_enable(&appRAMStart);
+        SuccessOrExit(err);
+    }
+
+exit:
+    return err;
+}
+#endif // defined(CHIP_DEVICE_LAYER_TARGET_NRF5)
+
 int main(void)
 {
+#if defined(CHIP_DEVICE_LAYER_TARGET_NRF5)
+    soft_device_init();
+#endif
+
     // Initialize the default streamer that was linked.
     const int rc = streamer_init(streamer_get());
 
@@ -46,6 +107,7 @@ int main(void)
     cmd_misc_init();
     cmd_base64_init();
     cmd_device_init();
+    cmd_btp_init();
     cmd_otcli_init();
 
     shell_task(NULL);
