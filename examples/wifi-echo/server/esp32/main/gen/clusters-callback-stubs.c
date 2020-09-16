b6061bbe4462df296fecff6c7fbd163c86405313
Bhaskar Sarma <bhaskars@apple.com>
2020-09-16 10:51:11 -0700

    Fix issue #2516
    Move emberAfPluginIdentifyStartFeedbackCallback and emberAfPluginIdentifyStopFeedbackCallback to callback-stub.c

--- examples/wifi-echo/server/esp32/main/gen/clusters-callback-stubs.c
+++ examples/wifi-echo/server/esp32/main/gen/clusters-callback-stubs.c
@@ -33,29 +33,6 @@ bool emberAfIdentifyClusterIdentifyQueryResponseCallback(uint16_t timeout)
     return false;
 }
 
-/** @brief Identify Cluster Start Feedback Callback
- *
- *
- *
- * @param endpoint Endpoint id
- * @param identifyTime Identify time
- */
-bool emberAfPluginIdentifyStartFeedbackCallback(uint8_t endpoint, uint16_t identifyTime)
-{
-    return false;
-}
-
-/** @brief Identify Cluster Stop Feedback Callback
- *
- *
- *
- * @param endpoint Endpoint id
- */
-bool emberAfPluginIdentifyStopFeedbackCallback(uint8_t endpoint)
-{
-    return false;
-}
-
 /** @brief Door Lock Cluster Clear All Pins
  *
  *
