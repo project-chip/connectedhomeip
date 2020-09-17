367ad722b20fda7d5242c92b2de5b86986e36749
Michael Spang <spang@google.com>
2020-09-17 14:37:38 -0400

    Remove the Android automake build (#2694)
    * Remove the Android automake build
    
    GN build is tested & working for Android, so remove automake.
    
    fixes #2251
    
    * Restyled by prettier-markdown
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    * Update docs
    
    * Update docs
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- third_party/ot-commissioner/build-android-libs.sh
+++ third_party/ot-commissioner/build-android-libs.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-readonly CUR_DIR=$(dirname "$(realpath -s $0)")
+readonly CUR_DIR="$(dirname "$(realpath -s "$0")")"
 
 set -e
 
-case "${TARGET}" in
-    armv7a-linux-androideabi)
-        ABI="armeabi-v7a"
-        ;;
-    aarch64-linux-android)
-        ABI="arm64-v8a"
-        ;;
-    i686-linux-android)
-        ABI="x86"
-        ;;
-    x86_64-linux-android)
-        ABI="x86-64"
-        ;;
-    *)
-        echo "invalid TARGET value: ${TARGET}"
-        exit 1
-esac
-
-cd "${CUR_DIR}"
+cd "$CUR_DIR"
 
 ## Install dependencies.
 [ ! -d build/ ] && ./repo/script/bootstrap.sh
 
 mkdir -p build && cd build
-cmake -GNinja                                                                               \
-    -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK_HOME}"/build/cmake/android.toolchain.cmake \
-    -DANDROID_ABI="${ABI}"                                                                  \
-    -DANDROID_ARM_NEON=ON                                                                   \
-    -DANDROID_NATIVE_API_LEVEL="${API}"                                                     \
-    -DBUILD_SHARED_LIBS=ON                                                                  \
-    -DCMAKE_CXX_STANDARD=11                                                                 \
-    -DCMAKE_CXX_STANDARD_REQUIRED=ON                                                        \
-    -DCMAKE_BUILD_TYPE=Release                                                              \
-    -DOT_COMM_ANDROID=ON                                                                    \
-    -DOT_COMM_JAVA_BINDING=ON                                                               \
-    -DOT_COMM_APP=OFF                                                                       \
-    -DOT_COMM_TEST=OFF                                                                      \
-    -DOT_COMM_CCM=OFF                                                                \
+cmake -GNinja \
+    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME"/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI="$ABI" \
+    -DANDROID_ARM_NEON=ON \
+    -DANDROID_NATIVE_API_LEVEL="$API" \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_CXX_STANDARD=11 \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DOT_COMM_ANDROID=ON \
+    -DOT_COMM_JAVA_BINDING=ON \
+    -DOT_COMM_APP=OFF \
+    -DOT_COMM_TEST=OFF \
+    -DOT_COMM_CCM=OFF \
     ../repo
 
 ninja
@@ -57,7 +39,7 @@ find ./io/openthread/commissioner -name "*.class" | xargs jar cvf ../../../libs/
 cd ../../../
 
 ## Copy shared native libraries
-for lib in $(find ./build -name "*.so"); do
+for lib in "$(find ./build -name "*.so")"; do
     ## Avoid copying symblink files.
     [ ! -L "$lib" ] && cp "$lib" libs
 done
