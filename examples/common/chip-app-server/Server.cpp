9945218fdbb17634c093bd02a9d5b4f22aeca61e
Jiacheng Guo <gjc@google.com>
2020-09-18 11:04:11 +0800

    add buffer size check

--- examples/common/chip-app-server/Server.cpp
+++ examples/common/chip-app-server/Server.cpp
@@ -80,26 +80,46 @@ void HandleBLEMessageReceived(chip::Ble::BLEEndPoint * endPoint, chip::System::P
     chip::DeviceLayer::Internal::DeviceNetworkInfo networkInfo;
     ChipLogProgress(AppServer, "Receive BLE message size=%u", bufferLen);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadNetworkName),
+                 ChipLogProgress(AppServer, "Invalid network provision message"));
     memcpy(networkInfo.ThreadNetworkName, data, sizeof(networkInfo.ThreadNetworkName));
     data += sizeof(networkInfo.ThreadNetworkName);
+    bufferLen -= sizeof(networkInfo.ThreadNetworkName);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadExtendedPANId),
+                 ChipLogProgress(AppServer, "Invalid network provision message"));
     memcpy(networkInfo.ThreadExtendedPANId, data, sizeof(networkInfo.ThreadExtendedPANId));
     data += sizeof(networkInfo.ThreadExtendedPANId);
+    bufferLen -= sizeof(networkInfo.ThreadExtendedPANId);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadMeshPrefix),
+                 ChipLogProgress(AppServer, "Invalid network provision message"));
     memcpy(networkInfo.ThreadMeshPrefix, data, sizeof(networkInfo.ThreadMeshPrefix));
     data += sizeof(networkInfo.ThreadMeshPrefix);
+    bufferLen -= sizeof(networkInfo.ThreadMeshPrefix);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadNetworkKey),
+                 ChipLogProgress(AppServer, "Invalid network provision message"));
     memcpy(networkInfo.ThreadNetworkKey, data, sizeof(networkInfo.ThreadNetworkKey));
     data += sizeof(networkInfo.ThreadNetworkKey);
+    bufferLen -= sizeof(networkInfo.ThreadNetworkKey);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadPSKc), ChipLogProgress(AppServer, "Invalid network provision message"));
     memcpy(networkInfo.ThreadPSKc, data, sizeof(networkInfo.ThreadPSKc));
     data += sizeof(networkInfo.ThreadPSKc);
+    bufferLen -= sizeof(networkInfo.ThreadPSKc);
 
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadPANId), ChipLogProgress(AppServer, "Invalid network provision message"));
     networkInfo.ThreadPANId = data[0] | (data[1] << 8);
     data += sizeof(networkInfo.ThreadPANId);
+    bufferLen -= sizeof(networkInfo.ThreadPANId);
+
+    VerifyOrExit(bufferLen >= sizeof(networkInfo.ThreadChannel), ChipLogProgress(AppServer, "Invalid network provision message"));
     networkInfo.ThreadChannel = data[0];
     data += sizeof(networkInfo.ThreadChannel);
+    bufferLen -= sizeof(networkInfo.ThreadChannel);
 
+    VerifyOrExit(bufferLen >= 3, ChipLogProgress(AppServer, "Invalid network provision message"));
     networkInfo.FieldPresent.ThreadExtendedPANId = *data;
     data++;
     networkInfo.FieldPresent.ThreadMeshPrefix = *data;
@@ -114,6 +134,7 @@ void HandleBLEMessageReceived(chip::Ble::BLEEndPoint * endPoint, chip::System::P
     ThreadStackMgr().SetThreadEnabled(true);
 
 #endif
+exit:
     endPoint->Close();
     chip::System::PacketBuffer::Free(buffer);
 }
