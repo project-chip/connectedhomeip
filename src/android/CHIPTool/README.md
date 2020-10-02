92fef0a1d26127f31f781f003a108c4e1ef5bfc1
jlportillo <72048955+jlportillo@users.noreply.github.com>
2020-10-02 16:26:13 -0700

    Add BLE scan and connection after QR Code (#2963)

--- src/android/CHIPTool/README.md
+++ src/android/CHIPTool/README.md
@@ -48,11 +48,11 @@ rsync -a out/android_arm64/lib/*.jar src/android/CHIPTool/app/libs
 rsync -a out/android_arm64/lib/jni/* src/android/CHIPTool/app/src/main/jniLibs
 ```
 
-'Gradle sync' the Android project and run.
+5. 'Gradle sync' the Android project and run.
 
-10. You will also need the "libc++\_shared.so" file in the jniLibs folder. This
-    file comes packaged with Android NDK and can be found under
-    `$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/$TARGET`, e.g.
+6. You will also need the "libc++\_shared.so" file in the jniLibs folder. This
+   file comes packaged with Android NDK and can be found under
+   `$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/$TARGET`, e.g.
 
     `rsync -a "${ANDROID_NDK_HOME}/sources/cxx-stl/llvm-libc++/libs/arm64-v8a/libc++_shared.so" src/android/CHIPTool/app/src/main/jniLibs/arm64-v8a/`
 
@@ -60,7 +60,7 @@ rsync -a out/android_arm64/lib/jni/* src/android/CHIPTool/app/src/main/jniLibs
     tweaking of the Android automake build rules. Include it in the interim to
     be able to build the Android app).
 
-11. Build OT Commissioner
+7. Build OT Commissioner
 
     ```shell
     git submodule update --init --recursive third_party/ot-commissioner/repo
---
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/bluetooth/BluetoothManager.kt
@@ -0,0 +1,110 @@
+package com.google.chip.chiptool.bluetooth
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.os.ParcelUuid
+import android.util.Log
+import java.util.UUID
+import kotlin.coroutines.resume
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeoutOrNull
+
+@ExperimentalCoroutinesApi
+class BluetoothManager {
+    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+    suspend fun getBluetoothDevice(discriminator: Int): BluetoothDevice? {
+        val scanner = bluetoothAdapter.bluetoothLeScanner ?: run {
+            Log.e(TAG, "No bluetooth scanner found")
+            return null
+        }
+
+        return withTimeoutOrNull(10000) {
+            callbackFlow {
+                val scanCallback = object : ScanCallback() {
+                    override fun onScanResult(callbackType: Int, result: ScanResult) {
+                        val device = result.device
+                        Log.i(TAG, "Bluetooth Device Scanned Addr: ${device.address}, Name ${device.name}")
+
+                        offer(device)
+                    }
+
+                    override fun onScanFailed(errorCode: Int) {
+                        Log.e(TAG, "Scan failed $errorCode")
+                    }
+                }
+
+                val scanFilter = ScanFilter.Builder()
+                    .setServiceData(
+                      ParcelUuid(UUID.fromString(CHIP_UUID)),
+                      discriminator.toByteArray(3)
+                    )
+                    .build()
+                val scanSettings = ScanSettings.Builder()
+                    .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                    .build()
+
+                Log.i(TAG, "Starting Bluetooth scan")
+                scanner.startScan(listOf(scanFilter), scanSettings, scanCallback)
+                awaitClose { scanner.stopScan(scanCallback) }
+            }.first()
+        }
+    }
+
+    /**
+     * Connects to a [BluetoothDevice] and suspends until [BluetoothGattCallback.onServicesDiscovered]
+     */
+    suspend fun connect(context: Context, device: BluetoothDevice): BluetoothGatt? {
+        return suspendCancellableCoroutine { continuation ->
+            val bluetoothGattCallback = object : BluetoothGattCallback() {
+                override fun onConnectionStateChange(
+                  gatt: BluetoothGatt?,
+                  status: Int,
+                  newState: Int
+                ) {
+                    super.onConnectionStateChange(gatt, status, newState)
+
+                    if (newState == BluetoothProfile.STATE_CONNECTED && status == BluetoothGatt.GATT_SUCCESS) {
+                        Log.i("$TAG|onConnectionStateChange", "Discovering Services...")
+                        gatt?.discoverServices()
+                    }
+                }
+
+                override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                    super.onServicesDiscovered(gatt, status)
+
+                    Log.i("$TAG|onServicesDiscovered", "Services Discovered")
+                    if (continuation.isActive) {
+                        continuation.resume(gatt)
+                    }
+                }
+            }
+
+            Log.i(TAG, "Connecting")
+            val gatt = device.connectGatt(context, false, bluetoothGattCallback)
+            continuation.invokeOnCancellation { gatt.disconnect() }
+        }
+    }
+
+    /** Converts an integer to a ByteArray with the first [size] bytes of the [Int]. */
+    private fun Int.toByteArray(size: Int): ByteArray {
+        return ByteArray(size) { i -> (this shr (8 * i)).toByte() }
+    }
+
+    companion object {
+        private const val TAG = "chip.BluetoothManager"
+        private const val CHIP_UUID = "0000FEAF-0000-1000-8000-00805F9B34FB"
+    }
+}
