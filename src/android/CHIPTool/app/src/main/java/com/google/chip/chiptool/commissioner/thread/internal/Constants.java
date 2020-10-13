eb9aa6ae0f71259a93f86406a475a341bb8c989c
kangping <wgtdkp@google.com>
2020-10-13 19:21:42 +0800

    [android] fetch Thread credential with Thread Commissioning (#2492)
    * [android] fetch Thread credential with Thread Commissioning
    
    * [android] update for revised ByteArray generated by OT Commissioner
    
    * [3rd-party] update OT Commissioner to include fixes regards the generated ByteArray
    
    * [3rd-party] revert changes to commissioner build script
    
    * [android] ignore shared libs and JAR libs in Android CHIPTool directory
    
    * [android] fix Java code style

--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/thread/internal/Constants.java
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/commissioner/thread/internal/Constants.java
@@ -16,12 +16,14 @@
  *
  */
 
-package com.google.chip.chiptool.commissioner;
+package com.google.chip.chiptool.commissioner.thread.internal;
 
-public class Constants {
+class Constants {
   public static final String KEY_DEVICE_INFO = "deviceinfo";
 
   public static final String KEY_NETWORK_INFO = "networkinfo";
+
+  public static final String KEY_PSKC = "pskc";
 
   public static final String KEY_COMMISSIONING_STATUS = "commissioning_status";
 
