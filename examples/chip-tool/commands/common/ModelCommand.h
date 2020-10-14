cf839e60380b2bc297a8a9e19cfcab39c7f75e57
Vivien Nicolas <vnicolas@apple.com>
2020-10-14 16:01:40 +0200

    Generate Global Commands Response code and use it inside chip-tool

--- examples/chip-tool/commands/common/ModelCommand.h
+++ examples/chip-tool/commands/common/ModelCommand.h
@@ -37,10 +37,11 @@
 class ModelCommand : public NetworkCommand
 {
 public:
-    ModelCommand(const char * commandName, const uint16_t clusterId) : NetworkCommand(commandName, NetworkType::UDP)
+    ModelCommand(const char * commandName, uint16_t clusterId, uint8_t commandId) : NetworkCommand(commandName, NetworkType::UDP)
     {
         AddArgument("endpoint-id", CHIP_ZCL_ENDPOINT_MIN, CHIP_ZCL_ENDPOINT_MAX, &mEndPointId);
         mClusterId = clusterId;
+        mCommandId = commandId;
     }
 
     /////////// Command Interface /////////
@@ -52,18 +53,12 @@ public:
     void OnMessage(ChipDeviceController * dc, chip::System::PacketBuffer * buffer) override;
 
     virtual uint16_t EncodeCommand(chip::System::PacketBuffer * buffer, uint16_t bufferSize, uint8_t endPointId) = 0;
-    virtual bool HandleClusterResponse(uint8_t * message, uint16_t messageLen) const { return false; }
+    virtual bool HandleGlobalResponse(uint8_t commandId, uint8_t * message, uint16_t messageLen) const { return false; }
+    virtual bool HandleSpecificResponse(uint8_t commandId, uint8_t * message, uint16_t messageLen) const { return false; }
 
 private:
     bool SendCommand(ChipDeviceController * dc);
     void ReceiveCommandResponse(ChipDeviceController * dc, chip::System::PacketBuffer * buffer) const;
-
-    void ParseGlobalResponseCommand(uint8_t commandId, uint8_t * message, uint16_t messageLen) const;
-    void ParseDefaultResponseCommand(uint8_t * message, uint16_t messageLen) const;
-    void ParseReadAttributeResponseCommand(uint8_t * message, uint16_t messageLen) const;
-    void ParseReadAttributeResponseCommandSuccess(uint16_t attrId, uint8_t * message, uint16_t messageLen) const;
-    void ParseReadAttributeResponseCommandFailure(uint16_t attrId, uint8_t status, uint16_t messageLen) const;
-    void ParseSpecificClusterResponseCommand(uint8_t commandId, uint8_t * message, uint16_t messageLen) const;
 
     void UpdateWaitForResponse(bool value);
     void WaitForResponse(void);
@@ -73,5 +68,6 @@ private:
     std::mutex cvWaitingForResponseMutex;
     bool mWaitingForResponse{ false };
     uint16_t mClusterId;
+    uint8_t mCommandId;
     uint8_t mEndPointId;
 };
---
+++ examples/chip-tool/commands/common/ModelCommandResponse.h
@@ -0,0 +1,40 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+#pragma once
+
+class ModelCommandResponse
+{
+public:
+    ModelCommandResponse(uint8_t commandId) { mCommandId = commandId; }
+
+    bool HandleCommandResponse(uint8_t commandId, uint8_t * message, uint16_t messageLen)
+    {
+        VerifyOrExit(commandId == mCommandId,
+                     ChipLogError(chipTool, "Unexpected command response id: '0x%02x'. Expecting: '0x%02x'", commandId, mCommandId));
+        return HandleResponse(message, messageLen);
+
+    exit:
+        return false;
+    }
+
+    virtual bool HandleResponse(uint8_t * message, uint16_t messageLen) const = 0;
+
+private:
+    uint8_t mCommandId;
+};
