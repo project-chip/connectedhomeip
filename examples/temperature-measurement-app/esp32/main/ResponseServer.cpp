ac588758c239297e50825e3eb2ac344c6a9da4ee
Shana Azria <sazria@apple.com>
2020-10-19 11:17:09 +0200

    Renamed QifiEcho file

--- examples/temperature-measurement-app/esp32/main/ResponseServer.cpp
+++ examples/temperature-measurement-app/esp32/main/ResponseServer.cpp
@@ -57,68 +57,7 @@ extern const NodeId kLocalNodeId = 12344321;
 
 namespace {
 
-/**
- * @brief implements something like "od -c", changes an arbitrary byte string
- *   into a single-line of ascii.  Destroys any byte-wise encoding that
- *   might be present, e.g. utf-8.
- *
- * @param bytes     potentially unprintable buffer
- * @param bytes_len length of bytes
- * @param out       where to put the printable string
- * @param out_len   length of out
- * @return size_t required size of output buffer, including null-termination
- */
-static size_t odc(const uint8_t * bytes, size_t bytes_len, char * out, size_t out_len)
-{
-    size_t required = 1; // always need null termination
-    memset(out, 0, out_len);
-    // count and print
-    for (; bytes_len > 0; bytes_len--, bytes++)
-    {
-        uint8_t byte = *bytes;
-
-        if ((byte >= '\t' && byte <= '\r') || byte == '\\')
-        {
-            static const char * kCodes = "tnvfr";
-            char code                  = (byte == '\\') ? '\\' : kCodes[byte - '\t'];
-            required += 2;
-            if (out_len > 2)
-            {
-                *out++ = '\\';
-                *out++ = code;
-                out_len -= 2;
-            }
-        }
-        else if (byte >= ' ' && byte <= '~')
-        {
-            required += 1;
-            if (out_len > 1)
-            {
-                *out++ = byte;
-                out_len--;
-            }
-        }
-        else
-        {
-            static const size_t kBinCodeLen = sizeof("\\xFF") - 1;
-            static const char * kCodes      = "0123456789ABCDEF";
-
-            required += kBinCodeLen;
-            if (out_len > kBinCodeLen)
-            {
-                *out++ = '\\';
-                *out++ = 'x';
-                *out++ = kCodes[(byte & 0xf0) >> 4];
-                *out++ = kCodes[byte & 0xf];
-                out_len -= kBinCodeLen;
-            }
-        }
-    }
-
-    return required;
-}
-
-class EchoServerCallback : public SecureSessionMgrCallback
+class ResponseServerCallback : public SecureSessionMgrDelegate
 {
 public:
     void OnMessageReceived(const PacketHeader & header, Transport::PeerConnectionState * state, System::PacketBuffer * buffer,
@@ -139,36 +78,8 @@ public:
             ESP_LOGI(TAG, "Packet received from %s: %zu bytes", src_addr, static_cast<size_t>(data_len));
         }
 
-        // FIXME: Long-term we shouldn't be guessing what sort of message this is
-        // based on the message bytes.  We're doing this for now to support both
-        // data model messages and text echo messages, but in the long term we
-        // should either do echo via a data model command or do echo on a separate
-        // port from data model processing.
-        if (ContentMayBeADataModelMessage(buffer))
-        {
-            HandleDataModelMessage(header, buffer, mgr);
-            buffer = nullptr;
-        }
-        else
-        {
-            char logmsg[512];
-
-            odc(buffer->Start(), data_len, logmsg, sizeof(logmsg));
-
-            ESP_LOGI(TAG, "Client sent: %s", logmsg);
-
-            // Attempt to echo back
-            err    = mgr->SendMessage(header.GetSourceNodeId().Value(), buffer);
-            buffer = nullptr;
-            if (err != CHIP_NO_ERROR)
-            {
-                ESP_LOGE(TAG, "Unable to echo back to client: %s", ErrorStr(err));
-            }
-            else
-            {
-                ESP_LOGI(TAG, "Echo sent");
-            }
-        }
+        HandleDataModelMessage(header, buffer, mgr);
+        buffer = nullptr;
 
     exit:
 
@@ -215,7 +126,7 @@ private:
     }
 };
 
-EchoServerCallback gCallbacks;
+ResponseServerCallback gCallbacks;
 
 SecureSessionMgr<Transport::UDP, // IPV6
                  Transport::UDP  // IPV4
