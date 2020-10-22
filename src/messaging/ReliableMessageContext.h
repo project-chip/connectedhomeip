36cde083f652ad579310fe4a025311b45e094044
Zang MingJie <zealot0630@gmail.com>
2020-10-23 01:38:41 +0800

    CRMP: Add unit tests

--- src/messaging/ReliableMessageContext.h
+++ src/messaging/ReliableMessageContext.h
@@ -50,7 +50,7 @@ public:
 
     /* Application callbacks */
     virtual void OnThrottleRcvd(uint32_t pauseTime) = 0; /**< Application callback for received Throttle message. */
-    virtual void OnDDRcvd(uint32_t pauseTime)       = 0; /**< Application callback for received Delayed Delivery message. */
+    virtual void OnDelayedDeliveryRcvd(uint32_t pauseTime) = 0; /**< Application callback for received Delayed Delivery message. */
     virtual void OnSendError(CHIP_ERROR err)        = 0; /**< Application callback for error while sending. */
     virtual void OnAckRcvd()                        = 0; /**< Application callback for received acknowledgment. */
 };
@@ -66,7 +66,10 @@ class ReliableMessageContext : public ReferenceCounted<ReliableMessageContext, R
 public:
     friend class ReliableMessageContextDeletor;
 
-    ReliableMessageContext(ReliableMessageDelegate & delegate);
+    ReliableMessageContext();
+
+    void SetConfig(ReliableMessageProtocolConfig config) { mConfig = config; }
+    void SetDelegate(ReliableMessageDelegate * delegate) { mDelegate = delegate; }
 
     CHIP_ERROR FlushAcks();
     uint64_t GetCurrentRetransmitTimeoutTick();
@@ -110,8 +113,7 @@ private:
 
     BitFlags<uint16_t, Flags> mFlags; // Internal state flags
 
-    bool IsNode(NodeId id);
-
+    CHIP_ERROR HandleDelayedDeliveryMessage(uint32_t PauseTimeMillis);
     CHIP_ERROR HandleRcvdAck(uint32_t AckMsgId);
     CHIP_ERROR HandleNeedsAck(uint32_t MessageId, BitFlags<uint32_t, MessageFlagValues> Flags);
     CHIP_ERROR HandleThrottleFlow(uint32_t PauseTimeMillis);
@@ -124,7 +126,7 @@ private:
     uint16_t mThrottleTimeoutTick;         // Timeout until when Throttle is On when ThrottleEnabled is set
     uint32_t mPendingPeerAckId;
 
-    ReliableMessageDelegate & mDelegate;
+    ReliableMessageDelegate * mDelegate;
 };
 
 } // namespace messaging
