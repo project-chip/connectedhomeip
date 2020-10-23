1b2d3a32c4c7b51ce2ffeaa8edd2797e5312499d
Song Guo <songguo@google.com>
2020-10-23 13:56:42 +0800

    add ble event handler

--- examples/lighting-app/linux/main.cpp
+++ examples/lighting-app/linux/main.cpp
@@ -78,13 +78,27 @@ void emberAfPluginOnOffClusterServerPostInitCallback(uint8_t endpoint)
 }
 }
 
+void EventHandler(const chip::DeviceLayer::ChipDeviceEvent * event, intptr_t arg)
+{
+    (void) arg;
+    if (event->Type == chip::DeviceLayer::DeviceEventType::kCHIPoBLEConnectionEstablished)
+    {
+        ChipLogProgress(DeviceLayer, "Receive kCHIPoBLEConnectionEstablished");
+        // exit(0);
+    }
+}
+
 int main()
 {
     chip::DeviceLayer::PlatformMgr().InitChipStack();
 
+    chip::Platform::MemoryInit();
+
+    chip::DeviceLayer::PlatformMgrImpl().AddEventHandler(EventHandler, 0);
+
     chip::DeviceLayer::ConnectivityMgr().SetBLEDeviceName("CHIP0001");
 
-    chip::DeviceLayer::Internal::BLEMgrImpl().ConfigureBle(1, false);
+    chip::DeviceLayer::Internal::BLEMgrImpl().ConfigureBle(0, false);
 
     chip::DeviceLayer::ConnectivityMgr().SetBLEAdvertisingEnabled(
         chip::DeviceLayer::ConnectivityManager::kCHIPoBLEServiceMode_Enabled);
