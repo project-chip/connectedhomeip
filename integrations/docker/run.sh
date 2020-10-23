eee4ded54b3e8f2e255304738f9af14cb9420114
Jiacheng Guo <gjc@google.com>
2020-10-23 11:37:57 +0800

    [build] update cirque base image version

--- integrations/docker/run.sh
+++ integrations/docker/run.sh
@@ -101,4 +101,6 @@ for arg in "$@"; do
     esac
 done
 
-docker run "${runargs[@]}" --rm --mount "source=/var/run/docker.sock,target=/var/run/docker.sock,type=bind" -w "$RUN_DIR" -v "$RUN_DIR:$RUN_DIR" "$ORG/$IMAGE${VERSION:+:${VERSION}}" "$@"
+docker pull "$ORG/$IMAGE${VERSION:+:${VERSION}}" || docker build -t "$IMAGE" "$here"
+
+docker run "${runargs[@]}" --rm --mount "source=/var/run/docker.sock,target=/var/run/docker.sock,type=bind" -w "$RUN_DIR" -v "$RUN_DIR:$RUN_DIR" "$IMAGE" "$@"
