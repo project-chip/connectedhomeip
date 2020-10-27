84994f2eed9ba4103dd76452abb824297a21363a
Boris Zbarsky <bzbarsky@apple.com>
2020-10-27 12:26:12 -0400

    Add a basic-types.h header. (#3451)
    The current types_stub.h and af-types.h headers depend on generated
    headers, which means they can't be used in code compiled into libCHIP
    instead of directly into the app.  This new header contains type
    definitions that are not dependent on app configuration and hence does
    not depend on the generated headers.
    
    The EmberAf* type names can be incrementally replaced with the CHIP
    type names that they are typedefs for at the moment.
    
    We need to support both C and C++ inclusion of this header until we
    convert generated code from C to C++, which is why the #ifdef noise is
    there.  Once we convert all the relevant code to C++ we can drop the
    ifdefs and the non-namespaced type names.

---
+++ src/app/util/basic-types.h
@@ -0,0 +1,54 @@
+/**
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ * @file
+ * @brief The include file for all the types for the data model that are not
+ *        dependent on an individual application configuration.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/**
+ * Types for use by CHIP C code, until we switch to C++.
+ */
+typedef uint8_t CHIPEndpointId;
+typedef uint16_t CHIPClusterId;
+typedef uint16_t CHIPAttributeId;
+
+/**
+ * Types for use by generated Silicon Labs code until we convert the generator
+ * to using CHIP types.
+ */
+typedef CHIPClusterId EmberAfClusterId;
+typedef CHIPAttributeId EmberAfAttributeId;
+/**
+ * @brief Type for referring to zigbee application profile id
+ *        TODO: This is probably not needed for CHIP and should be removed.
+ *        https://github.com/project-chip/connectedhomeip/issues/3444
+ */
+typedef uint16_t EmberAfProfileId;
+
+#ifdef __cplusplus
+namespace chip {
+typedef CHIPEndpointId EndpointId;
+typedef CHIPClusterId ClusterId;
+typedef CHIPAttributeId AttributeId;
+} // namespace chip
+#endif // __cplusplus
