b4e37f2caf0610502b1ea1aa509966d67b263c89
Boris Zbarsky <bzbarsky@apple.com>
2020-11-02 00:44:54 -0500

    Change to using a builder pattern for buffer reader. (#3497)
    For callers that read multiple things in a row with the reads not
    being conditional on what was already read, this leads to slightly
    smaller code.  And in general it can lead to more readable code.

---
+++ src/lib/support/BufferReader.cpp
@@ -0,0 +1,73 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "BufferReader.h"
+
+namespace chip {
+namespace Encoding {
+namespace LittleEndian {
+
+namespace {
+// These helper methods return void and put the value being read into an
+// outparam because that allows us to easily overload on the type of the
+// thing being read.
+void ReadHelper(const uint8_t *& p, uint8_t * dest)
+{
+    *dest = Read8(p);
+}
+void ReadHelper(const uint8_t *& p, uint16_t * dest)
+{
+    *dest = Read16(p);
+}
+void ReadHelper(const uint8_t *& p, uint32_t * dest)
+{
+    *dest = Read32(p);
+}
+void ReadHelper(const uint8_t *& p, uint64_t * dest)
+{
+    *dest = Read64(p);
+}
+} // anonymous namespace
+
+template <typename T>
+void Reader::Read(T * retval)
+{
+    static_assert(CHAR_BIT == 8, "Our various sizeof checks rely on bytes and octets being the same thing");
+
+    static constexpr size_t data_size = sizeof(T);
+
+    if (mAvailable < data_size)
+    {
+        mStatus = CHIP_ERROR_BUFFER_TOO_SMALL;
+        // Ensure that future reads all fail.
+        mAvailable = 0;
+        return;
+    }
+
+    ReadHelper(mReadPtr, retval);
+    mAvailable = static_cast<uint16_t>(mAvailable - data_size);
+}
+
+// Explicit Read instantiations for the data types we want to support.
+template void Reader::Read(uint8_t *);
+template void Reader::Read(uint16_t *);
+template void Reader::Read(uint32_t *);
+template void Reader::Read(uint64_t *);
+
+} // namespace LittleEndian
+} // namespace Encoding
+} // namespace chip
