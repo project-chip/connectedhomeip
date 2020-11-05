c6074f182f9186857c066a3552d0183e5ede6178
Andrei Litvin <andy314@gmail.com>
2020-11-05 15:52:25 -0500

    Initialize and run the even loop for the mdns test

--- examples/minimal-mdns/main.cpp
+++ examples/minimal-mdns/main.cpp
@@ -17,19 +17,29 @@
 
 #include <cstdio>
 
+#include <inet/UDPEndPoint.h>
 #include <mdns/minimal/DnsHeader.h>
 #include <platform/CHIPDeviceLayer.h>
 #include <support/CHIPMem.h>
+#include <system/SystemTimer.h>
+
+using namespace chip;
+
+namespace {
+
+constexpr uint32_t kRunTimeMs = 3'000;
+
+}
 
 int main(int argc, char ** args)
 {
-    if (chip::Platform::MemoryInit() != CHIP_NO_ERROR)
+    if (Platform::MemoryInit() != CHIP_NO_ERROR)
     {
         printf("FAILED to initialize memory");
         return 1;
     }
 
-    if (chip::DeviceLayer::PlatformMgr().InitChipStack() != CHIP_NO_ERROR)
+    if (DeviceLayer::PlatformMgr().InitChipStack() != CHIP_NO_ERROR)
     {
         printf("FAILED to initialize chip stack");
         return 1;
@@ -37,10 +47,32 @@ int main(int argc, char ** args)
 
     printf("Running...\n");
 
-    // TODO:
-    //   - send mDNS requests
-    //   - receive mDNS replies
+    Inet::UDPEndPoint * udp = nullptr;
 
+    if (DeviceLayer::InetLayer.NewUDPEndPoint(&udp) != CHIP_NO_ERROR)
+    {
+        printf("FAILED to create new udp endpoint");
+        return 1;
+    }
+
+    System::Timer * timer = nullptr;
+
+    if (DeviceLayer::SystemLayer.NewTimer(timer) == CHIP_NO_ERROR)
+    {
+        timer->Start(
+            kRunTimeMs, [](System::Layer *, void *, System::Error err) { DeviceLayer::PlatformMgr().Shutdown(); }, nullptr);
+    }
+
+    // TODO:
+    //   - MDNS:
+    //     - build a query (TODO header + query)
+    //     - Send it out (where? IPV4 works but what about V6?)
+    //     - Listen
+    //     - timeout
+
+    DeviceLayer::PlatformMgr().RunEventLoop();
+
+    udp->Free();
     printf("Done...\n");
     return 0;
 }
