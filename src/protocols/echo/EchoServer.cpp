aaacc1c5127a1c427248007c23de911ec518a38b
Restyled.io <commits@restyled.io>
2020-11-05 05:30:05 +0000

    Restyled by clang-format

--- src/protocols/echo/EchoServer.cpp
+++ src/protocols/echo/EchoServer.cpp
@@ -52,13 +52,15 @@ void EchoServer::Shutdown()
     }
 }
 
-void EchoServer::OnMessageReceived(ExchangeContext * ec, const PacketHeader & packetHeader, uint32_t protocolId, uint8_t msgType, System::PacketBuffer * payload)
+void EchoServer::OnMessageReceived(ExchangeContext * ec, const PacketHeader & packetHeader, uint32_t protocolId, uint8_t msgType,
+                                   System::PacketBuffer * payload)
 {
     // NOTE: we already know this is an Echo Request message because we explicitly registered with the
     // Exchange Manager for unsolicited Echo Requests.
 
     // Call the registered OnEchoRequestReceived handler, if any.
-    if (OnEchoRequestReceived != nullptr) OnEchoRequestReceived(ec->GetPeerNodeId(), payload);
+    if (OnEchoRequestReceived != nullptr)
+        OnEchoRequestReceived(ec->GetPeerNodeId(), payload);
 
     // Since we are re-using the inbound EchoRequest buffer to send the EchoResponse, if necessary,
     // adjust the position of the payload within the buffer to ensure there is enough room for the
