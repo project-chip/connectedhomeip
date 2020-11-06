e73cb06fa6e13b11fff03b5752b62997fd452db6
Restyled.io <commits@restyled.io>
2020-11-06 08:35:04 +0000

    Restyled by clang-format

--- src/messaging/ExchangeContext.cpp
+++ src/messaging/ExchangeContext.cpp
@@ -192,7 +192,7 @@ void ExchangeContext::Reset()
 }
 
 ExchangeContext * ExchangeContext::Alloc(ExchangeManager * em, uint16_t ExchangeId, uint64_t PeerNodeId, bool Initiator,
-                            ExchangeContextDelegateFactory * delegateFactory)
+                                         ExchangeContextDelegateFactory * delegateFactory)
 {
     VerifyOrDie(mExchangeMgr == nullptr && GetReferenceCount() == 0);
 
