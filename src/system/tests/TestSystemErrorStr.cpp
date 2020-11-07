ea6b8f2d2264beb7d68b5ca8e0838394748dcfaa
Vivien Nicolas <vnicolas@apple.com>
2020-11-07 15:16:00 +0100

    Remove '-Wno-unused-variable' from compiler flags

--- src/system/tests/TestSystemErrorStr.cpp
+++ src/system/tests/TestSystemErrorStr.cpp
@@ -63,8 +63,6 @@ static int32_t sContext[] =
 };
 // clang-format on
 
-static const size_t kTestElements = sizeof(sContext) / sizeof(sContext[0]);
-
 static void CheckSystemErrorStr(nlTestSuite * inSuite, void * inContext)
 {
     // Register the layer error formatter
