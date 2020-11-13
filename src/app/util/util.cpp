528ac73455af8334e4383b5aaf60c87d0153c967
Vivien Nicolas <vnicolas@apple.com>
2020-11-13 19:42:47 +0100

    Add missing '#ifdef EMBER_AF_PLUGIN_REPORTING' to src/app/util/util.cpp (#3825)

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -52,6 +52,10 @@
 #include <app/clusters/groups-server/groups-server.h>
 #endif // EMBER_AF_PLUGIN_GROUPS_SERVER
 
+#ifdef EMBER_AF_PLUGIN_REPORTING
+#include <app/reporting/reporting.h>
+#endif // EMBER_AF_PLUGIN_REPORTING
+
 using namespace chip;
 
 // TODO: Need to figure out what needs to happen wrt HAL tokens here, but for
@@ -311,10 +315,12 @@ void emberAfStackDown(void)
         // && emberNetworkState() == EMBER_NO_NETWORK
     )
     {
+#ifdef EMBER_AF_PLUGIN_REPORTING
         // the report table should be cleared when the stack comes down.
         // going to a new network means new report devices should be discovered.
         // if the table isnt cleared the device keeps trying to send messages.
         emberAfClearReportTableCallback();
+#endif // EMBER_AF_PLUGIN_REPORTING
     }
 
     emberAfRegistrationAbortCallback();
