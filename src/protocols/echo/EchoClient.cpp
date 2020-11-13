42dad85c298ec5fa010c11909e8a6f99a0394705
Yufeng Wang <44623591+yufengwangca@users.noreply.github.com>
2020-11-13 10:44:20 -0800

    Set EchoClient as the exchange delegate to receive the incoming messages (#3821)

--- src/protocols/echo/EchoClient.cpp
+++ src/protocols/echo/EchoClient.cpp
@@ -68,6 +68,9 @@ CHIP_ERROR EchoClient::SendEchoRequest(NodeId nodeId, System::PacketBuffer * pay
         return CHIP_ERROR_NO_MEMORY;
     }
 
+    // Set EchoClient itself as the exchange delegate to receive the message from exchange.
+    mExchangeCtx->SetDelegate(this);
+
     return SendEchoRequest(payload);
 }
 
