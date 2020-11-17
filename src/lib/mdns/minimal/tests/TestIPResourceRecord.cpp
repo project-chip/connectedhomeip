30ce0bc235497238f344f51c280adf99cefb6c55
Andrei Litvin <andy314@gmail.com>
2020-11-17 13:53:49 -0500

    Add writing tests for IPv6 records

--- src/lib/mdns/minimal/tests/TestIPResourceRecord.cpp
+++ src/lib/mdns/minimal/tests/TestIPResourceRecord.cpp
@@ -42,10 +42,10 @@ void WriteIPv4(nlTestSuite * inSuite, void * inContext)
     uint8_t headerBuffer[HeaderRef::kSizeBytes];
     uint8_t dataBuffer[128];
 
-    {
-        HeaderRef header(headerBuffer);
-        BufBound output(dataBuffer, sizeof(dataBuffer));
+    HeaderRef header(headerBuffer);
 
+    {
+        BufBound output(dataBuffer, sizeof(dataBuffer));
         IPResourceRecord ipResourceRecord(kTestQnames, kTestQnameCount, ipAddress);
 
         ipResourceRecord.SetTtl(123);
@@ -67,16 +67,112 @@ void WriteIPv4(nlTestSuite * inSuite, void * inContext)
 
         NL_TEST_ASSERT(inSuite, ipResourceRecord.Append(header, ResourceType::kAnswer, output));
         NL_TEST_ASSERT(inSuite, header.GetAnswerCount() == 1);
+        NL_TEST_ASSERT(inSuite, header.GetAuthorityCount() == 0);
+        NL_TEST_ASSERT(inSuite, header.GetAdditionalCount() == 0);
         NL_TEST_ASSERT(inSuite, output.Written() == sizeof(expectedOutput));
         NL_TEST_ASSERT(inSuite, memcmp(dataBuffer, expectedOutput, sizeof(expectedOutput)) == 0);
     }
 
-    // FIXME: implement
+    {
+        BufBound output(dataBuffer, sizeof(dataBuffer));
+
+        IPResourceRecord ipResourceRecord(kTestQnames, kTestQnameCount, ipAddress);
+
+        ipResourceRecord.SetTtl(234);
+
+        header.Clear();
+
+        const uint8_t expectedOutput[] = {
+            //
+            4,  's', 'o', 'm', 'e',      // QNAME part: some
+            4,  't', 'e', 's', 't',      // QNAME part: test
+            5,  'l', 'o', 'c', 'a', 'l', // QNAME part: local
+            0,                           // QNAME ends
+            0,  1,                       // QClass IN
+            0,  1,                       // QType A
+            0,  0,   0,   234,           // TTL
+            0,  4,                       // data size - size for IPv4
+            10, 20,  30,  40             // IP Address
+        };
+
+        NL_TEST_ASSERT(inSuite, ipResourceRecord.Append(header, ResourceType::kAuthority, output));
+        NL_TEST_ASSERT(inSuite, header.GetAnswerCount() == 0);
+        NL_TEST_ASSERT(inSuite, header.GetAuthorityCount() == 1);
+        NL_TEST_ASSERT(inSuite, header.GetAdditionalCount() == 0);
+        NL_TEST_ASSERT(inSuite, output.Written() == sizeof(expectedOutput));
+        NL_TEST_ASSERT(inSuite, memcmp(dataBuffer, expectedOutput, sizeof(expectedOutput)) == 0);
+    }
+
+    {
+        BufBound output(dataBuffer, sizeof(dataBuffer));
+
+        IPResourceRecord ipResourceRecord(kTestQnames, kTestQnameCount, ipAddress);
+
+        ipResourceRecord.SetTtl(0x1234);
+
+        header.Clear();
+
+        const uint8_t expectedOutput[] = {
+            //
+            4,  's', 'o',  'm',  'e',      // QNAME part: some
+            4,  't', 'e',  's',  't',      // QNAME part: test
+            5,  'l', 'o',  'c',  'a', 'l', // QNAME part: local
+            0,                             // QNAME ends
+            0,  1,                         // QClass IN
+            0,  1,                         // QType A
+            0,  0,   0x12, 0x34,           // TTL
+            0,  4,                         // data size - size for IPv4
+            10, 20,  30,   40              // IP Address
+        };
+
+        NL_TEST_ASSERT(inSuite, ipResourceRecord.Append(header, ResourceType::kAdditional, output));
+        NL_TEST_ASSERT(inSuite, header.GetAnswerCount() == 0);
+        NL_TEST_ASSERT(inSuite, header.GetAuthorityCount() == 0);
+        NL_TEST_ASSERT(inSuite, header.GetAdditionalCount() == 1);
+        NL_TEST_ASSERT(inSuite, output.Written() == sizeof(expectedOutput));
+        NL_TEST_ASSERT(inSuite, memcmp(dataBuffer, expectedOutput, sizeof(expectedOutput)) == 0);
+    }
 }
 
 void WriteIPv6(nlTestSuite * inSuite, void * inContext)
 {
-    // FIXME: implement
+    IPAddress ipAddress;
+
+    NL_TEST_ASSERT(inSuite, IPAddress::FromString("fe80::224:32ff:fe19:359b", ipAddress));
+
+    uint8_t headerBuffer[HeaderRef::kSizeBytes];
+    uint8_t dataBuffer[128];
+
+    HeaderRef header(headerBuffer);
+
+    BufBound output(dataBuffer, sizeof(dataBuffer));
+    IPResourceRecord ipResourceRecord(kTestQnames, kTestQnameCount, ipAddress);
+
+    ipResourceRecord.SetTtl(0x12345678);
+
+    header.Clear();
+
+    const uint8_t expectedOutput[] = {                                   //
+                                       4,    's',  'o',  'm',  'e',      // QNAME part: some
+                                       4,    't',  'e',  's',  't',      // QNAME part: test
+                                       5,    'l',  'o',  'c',  'a', 'l', // QNAME part: local
+                                       0,                                // QNAME ends
+                                       0,    1,                          // QClass IN
+                                       0,    28,                         // QType AAAA
+                                       0x12, 0x34, 0x56, 0x78,           // TTL
+                                       0,    16,                         // data size - size for IPv4
+                                       0xfe, 0x80, 0x00, 0x00,           // IPv6
+                                       0x00, 0x00, 0x00, 0x00,           //
+                                       0x02, 0x24, 0x32, 0xff,           //
+                                       0xfe, 0x19, 0x35, 0x9b
+    };
+
+    NL_TEST_ASSERT(inSuite, ipResourceRecord.Append(header, ResourceType::kAnswer, output));
+    NL_TEST_ASSERT(inSuite, header.GetAnswerCount() == 1);
+    NL_TEST_ASSERT(inSuite, header.GetAuthorityCount() == 0);
+    NL_TEST_ASSERT(inSuite, header.GetAdditionalCount() == 0);
+    NL_TEST_ASSERT(inSuite, output.Written() == sizeof(expectedOutput));
+    NL_TEST_ASSERT(inSuite, memcmp(dataBuffer, expectedOutput, sizeof(expectedOutput)) == 0);
 }
 
 } // namespace
