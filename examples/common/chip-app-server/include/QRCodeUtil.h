3c82905f1b0b5923cabd0757404a4d33f785a9d0
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2020-11-20 11:05:43 +0100

    Extracted ESP32 Url encoding method to the common QRCodeUtil module.
    ESP32 platform has implemented module enabling generating url link
    to the webpage displaying QR code, what could be useful for other
    platforms not having displays.
    
    * Moved url encoding method from ESP32 to the common QRCodeUtil module.
    * Refactored methods a bit, as they were bug prone (e.g. unreserved
    array was not /0 terminated and then strchr was using it).

--- examples/common/chip-app-server/include/QRCodeUtil.h
+++ examples/common/chip-app-server/include/QRCodeUtil.h
@@ -21,3 +21,18 @@
 
 void PrintQRCode(chip::RendezvousInformationFlags rendezvousFlags);
 CHIP_ERROR GetQRCode(uint32_t & setupPinCode, std::string & QRCode, chip::RendezvousInformationFlags rendezvousFlags);
+
+/**
+ * Initialize DataModelHandler and start CHIP datamodel server, the server
+ * assumes the platform's networking has been setup already.
+ *
+ * Method verifies if every character of the QR Code is valid for the url encoding
+ * and otherwise it encodes the invalid character using available ones. 
+ *
+ * @param QRCode address of the array storing QR Code to encode.
+ * @param len length of the given QR Code.
+ * @param url address of the location where encoded url should be stored.
+ * @param maxSize maximal size of the array where encoded url should be stored.
+ * @return CHIP_NO_ERROR on success and other values on error.
+ */
+CHIP_ERROR EncodeQRCodeToUrl(const char * QRCode, size_t len, char * url, size_t maxSize);
