99555f045f207d6bf0f60acb9ada4aafbce8ab4e
Zang MingJie <zealot0630@gmail.com>
2020-11-30 17:42:56 +0800

    Add APIs of Channel in message layer

--- src/messaging/ExchangeMgr.h
+++ src/messaging/ExchangeMgr.h
@@ -26,11 +26,15 @@
 
 #include <array>
 
+#include <messaging/Channel.h>
+#include <messaging/ChannelContext.h>
 #include <messaging/ExchangeContext.h>
 #include <messaging/MessageCounterSync.h>
 #include <messaging/ReliableMessageMgr.h>
 #include <support/DLLUtil.h>
+#include <support/Pool.h>
 #include <transport/SecureSessionMgr.h>
+#include <transport/TransportMgr.h>
 
 namespace chip {
 namespace Messaging {
@@ -46,7 +50,7 @@ static constexpr int16_t kAnyMessageType = -1;
  *    It works on be behalf of higher layers, creating ExchangeContexts and
  *    handling the registration/unregistration of unsolicited message handlers.
  */
-class DLL_EXPORT ExchangeManager : public SecureSessionMgrDelegate
+class DLL_EXPORT ExchangeManager : public SecureSessionMgrDelegate, public TransportMgrDelegate
 {
 public:
     ExchangeManager();
@@ -66,7 +70,7 @@ public:
      *  @retval #CHIP_NO_ERROR On success.
      *
      */
-    CHIP_ERROR Init(SecureSessionMgr * sessionMgr);
+    CHIP_ERROR Init(NodeId localNodeId, TransportMgrBase * transportMgr, SecureSessionMgr * sessionMgr);
 
     /**
      *  Shutdown the ExchangeManager. This terminates this instance
@@ -171,15 +175,36 @@ public:
                                                           static_cast<uint8_t>(msgType));
     }
 
+    // Channel public APIs
+    ChannelHandle EstablishChannel(const ChannelBuilder & builder, ChannelDelegate * delegate);
+
+    // Internal APIs used for channel
+    void ReleaseChannelContext(ChannelContext * channel) { mChannelContexts.ReleaseObject(channel); }
+
+    void ReleaseChannelHandle(ChannelContextHandleAssociation * association) { mChannelHandles.ReleaseObject(association); }
+
+    template <typename Event>
+    void NotifyChannelEvent(ChannelContext * channel, Event event)
+    {
+        mChannelHandles.ForEachActiveObject([&](ChannelContextHandleAssociation * association) {
+            if (association->mChannelContext == channel)
+                event(association->mChannelDelegate);
+            return true;
+        });
+    }
+
     void IncrementContextsInUse();
     void DecrementContextsInUse();
 
+    TransportMgrBase * GetTransportManager() const { return mTransportMgr; }
     SecureSessionMgr * GetSessionMgr() const { return mSessionMgr; }
 
     ReliableMessageMgr * GetReliableMessageMgr() { return &mReliableMessageMgr; };
 
     MessageCounterSyncMgr * GetMessageCounterSyncMgr() { return &mMessageCounterSyncMgr; };
 
+    NodeId GetLocalNodeId() { return mLocalNodeId; }
+    uint16_t GetNextKeyId() { return ++mNextKeyId; }
     size_t GetContextsInUse() const { return mContextsInUse; }
 
 private:
@@ -196,8 +221,11 @@ private:
         int16_t MessageType;
     };
 
+    NodeId mLocalNodeId; // < Id of the current node
     uint16_t mNextExchangeId;
+    uint16_t mNextKeyId;
     State mState;
+    TransportMgrBase * mTransportMgr;
     SecureSessionMgr * mSessionMgr;
     ReliableMessageMgr mReliableMessageMgr;
     MessageCounterSyncMgr mMessageCounterSyncMgr;
@@ -206,7 +234,8 @@ private:
     size_t mContextsInUse;
 
     UnsolicitedMessageHandler UMHandlerPool[CHIP_CONFIG_MAX_UNSOLICITED_MESSAGE_HANDLERS];
-    void (*OnExchangeContextChanged)(size_t numContextsInUse);
+    BitMapObjectPool<ChannelContext, CHIP_CONFIG_MAX_ACTIVE_CHANNELS> mChannelContexts;
+    BitMapObjectPool<ChannelContextHandleAssociation, CHIP_CONFIG_MAX_CHANNEL_HANDLES> mChannelHandles;
 
     ExchangeContext * AllocContext(uint16_t ExchangeId, SecureSessionHandle session, bool Initiator, ExchangeDelegate * delegate);
 
@@ -218,7 +247,12 @@ private:
     void OnMessageReceived(const PacketHeader & packetHeader, const PayloadHeader & payloadHeader, SecureSessionHandle session,
                            System::PacketBufferHandle msgBuf, SecureSessionMgr * msgLayer) override;
 
+    void OnNewConnection(SecureSessionHandle session, SecureSessionMgr * mgr) override;
     void OnConnectionExpired(SecureSessionHandle session, SecureSessionMgr * mgr) override;
+
+    // TransportMgrDelegate interface for rendezvous sessions
+    void OnMessageReceived(const PacketHeader & header, const Transport::PeerAddress & source,
+                           System::PacketBufferHandle msgBuf) override;
 };
 
 } // namespace Messaging
