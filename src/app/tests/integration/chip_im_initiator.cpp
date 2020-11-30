f73f8fe0ec46aaf6a0b0d9c8458acf9226d00532
Zang MingJie <zealot0630@gmail.com>
2020-11-30 17:42:56 +0800

    Add APIs of Channel in message layer

--- src/app/tests/integration/chip_im_initiator.cpp
+++ src/app/tests/integration/chip_im_initiator.cpp
@@ -222,7 +222,7 @@ int main(int argc, char * argv[])
     err = gSessionManager.Init(chip::kTestControllerNodeId, &chip::DeviceLayer::SystemLayer, &gTransportManager, &admins);
     SuccessOrExit(err);
 
-    err = gExchangeManager.Init(&gSessionManager);
+    err = gExchangeManager.Init(chip::kTestControllerNodeId, &gTransportManager, &gSessionManager);
     SuccessOrExit(err);
 
     err = chip::app::InteractionModelEngine::GetInstance()->Init(&gExchangeManager);
