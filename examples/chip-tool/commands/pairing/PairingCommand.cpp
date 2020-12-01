vivien-apple/connectedhomeip-1

Use CHIPDeviceController instead of CHIPDeviceController_deprecated

examples/chip-tool/commands/pairing/PairingCommand.cpp
@@ -1,0 +1,148 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+#include "PairingCommand.h"
+
+using namespace ::chip;
+
+constexpr uint16_t kWaitDurationInSeconds = 120;
+
+CHIP_ERROR PairingCommand::Run(PersistentStorage & storage, NodeId localId, NodeId remoteId)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    err = mCommissioner.Init(localId, &storage, this);
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(Controller, "Init failure! Commissioner: %s", chip::ErrorStr(err)));
+
+    err = mCommissioner.ServiceEvents();
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(Controller, "Init failure! Run Loop: %s", chip::ErrorStr(err)));
+
+    err = RunInternal(remoteId);
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(chipTool, "Init Failure! PairDevice: %s", chip::ErrorStr(err)));
+
+exit:
+    mCommissioner.ServiceEventSignal();
+    mCommissioner.Shutdown();
+    return err;
+}
+
+CHIP_ERROR PairingCommand::RunInternal(NodeId remoteId)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    switch (mPairingMode)
+    {
+    case PairingMode::None:
+        err = Unpair(remoteId);
+        break;
+    case PairingMode::Bypass:
+        err = PairWithoutSecurity(remoteId, PeerAddress::UDP(mRemoteAddr.address, mRemotePort));
+        break;
+    case PairingMode::Ble:
+        err = Pair(remoteId, PeerAddress::BLE());
+        break;
+    case PairingMode::SoftAP:
+        err = Pair(remoteId, PeerAddress::UDP(mRemoteAddr.address, mRemotePort));
+        break;
+    }
+
+    UpdateWaitForResponse(true);
+    WaitForResponse(kWaitDurationInSeconds);
+
+    return err;
+}
+
+CHIP_ERROR PairingCommand::Pair(NodeId remoteId, PeerAddress address)
+{
+    RendezvousParameters params =
+        RendezvousParameters().SetSetupPINCode(mSetupPINCode).SetDiscriminator(mDiscriminator).SetPeerAddress(address);
+
+    return mCommissioner.PairDevice(remoteId, params);
+}
+
+CHIP_ERROR PairingCommand::PairWithoutSecurity(NodeId remoteId, PeerAddress address)
+{
+    ChipSerializedDevice serializedTestDevice;
+    return mCommissioner.PairTestDeviceWithoutSecurity(remoteId, address.GetIPAddress(), serializedTestDevice, address.GetPort());
+}
+
+CHIP_ERROR PairingCommand::Unpair(NodeId remoteId)
+{
+    return mCommissioner.UnpairDevice(remoteId);
+}
+
+void PairingCommand::OnStatusUpdate(RendezvousSessionDelegate::Status status)
+{
+    switch (status)
+    {
+    case RendezvousSessionDelegate::Status::SecurePairingSuccess:
+        ChipLogProgress(chipTool, "Secure Pairing Success");
+        break;
+    case RendezvousSessionDelegate::Status::SecurePairingFailed:
+        ChipLogProgress(chipTool, "Secure Pairing Failed");
+        break;
+    case RendezvousSessionDelegate::Status::NetworkProvisioningSuccess:
+        ChipLogProgress(chipTool, "Network Provisioning Success");
+        break;
+    case RendezvousSessionDelegate::Status::NetworkProvisioningFailed:
+        ChipLogProgress(chipTool, "Network Provisioning Failed");
+        break;
+    }
+}
+
+void PairingCommand::OnNetworkCredentialsRequested(RendezvousDeviceCredentialsDelegate * callback)
+{
+    ChipLogProgress(chipTool, "OnNetworkCredentialsRequested");
+}
+
+void PairingCommand::OnOperationalCredentialsRequested(const char * csr, size_t csr_length,
+                                                       RendezvousDeviceCredentialsDelegate * callback)
+{
+    // TODO Implement this
+    ChipLogProgress(chipTool, "OnOperationalCredentialsRequested");
+}
+
+void PairingCommand::OnPairingComplete(CHIP_ERROR err)
+{
+    if (err == CHIP_NO_ERROR)
+    {
+        ChipLogProgress(chipTool, "Pairing Success");
+    }
+    else
+    {
+        ChipLogProgress(chipTool, "Pairing Failure: %s", chip::ErrorStr(err));
+    }
+
+    SetCommandExitStatus(err == CHIP_NO_ERROR);
+    UpdateWaitForResponse(false);
+}
+
+void PairingCommand::OnPairingDeleted(CHIP_ERROR err)
+{
+    if (err == CHIP_NO_ERROR)
+    {
+        ChipLogProgress(chipTool, "Pairing Deleted Success");
+    }
+    else
+    {
+        ChipLogProgress(chipTool, "Pairing Deleted Failure: %s", chip::ErrorStr(err));
+    }
+
+    SetCommandExitStatus(err == CHIP_NO_ERROR);
+    UpdateWaitForResponse(false);
+}

examples/chip-tool/config/PersistentStorage.cpp
@@ -1,0 +1,110 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+#include "PersistentStorage.h"
+
+#include <fstream>
+
+using String   = std::basic_string<char>;
+using Section  = std::map<String, String>;
+using Sections = std::map<String, Section>;
+
+using namespace ::chip;
+using namespace ::chip::Controller;
+
+constexpr const char kFilename[]           = "/tmp/chip_tool_config.ini";
+constexpr const char kDefaultSectionName[] = "Default";
+
+CHIP_ERROR PersistentStorage::Init()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    std::ifstream ifs;
+    ifs.open(kFilename, std::ifstream::in);
+    if (!ifs.good())
+    {
+        CommitConfig();
+        ifs.open(kFilename, std::ifstream::in);
+    }
+    VerifyOrExit(ifs.is_open(), err = CHIP_ERROR_OPEN_FAILED);
+
+    mConfig.parse(ifs);
+    ifs.close();
+
+exit:
+    return err;
+}
+
+void PersistentStorage::SetDelegate(PersistentStorageResultDelegate * delegate) {}
+
+void PersistentStorage::GetKeyValue(const char * key) {}
+
+CHIP_ERROR PersistentStorage::GetKeyValue(const char * key, char * value, uint16_t & size)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    std::string iniValue;
+    size_t iniValueLength = 0;
+
+    auto section = mConfig.sections[kDefaultSectionName];
+    auto it      = section.find(key);
+    VerifyOrExit(it != section.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+
+    VerifyOrExit(inipp::extract(section[key], iniValue), err = CHIP_ERROR_INVALID_ARGUMENT);
+
+    iniValueLength = iniValue.size();
+    VerifyOrExit(iniValueLength < static_cast<size_t>(size) - 1, err = CHIP_ERROR_BUFFER_TOO_SMALL);
+
+    iniValueLength        = iniValue.copy(value, iniValueLength);
+    value[iniValueLength] = '\0';
+
+exit:
+    return err;
+}
+
+void PersistentStorage::SetKeyValue(const char * key, const char * value)
+{
+    auto section = mConfig.sections[kDefaultSectionName];
+    section[key] = std::string(value);
+
+    mConfig.default_section(section);
+    CommitConfig();
+}
+
+void PersistentStorage::DeleteKeyValue(const char * key)
+{
+    auto section = mConfig.sections[kDefaultSectionName];
+    section.erase(key);
+
+    mConfig.default_section(section);
+    CommitConfig();
+}
+
+CHIP_ERROR PersistentStorage::CommitConfig()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    std::ofstream ofs;
+    ofs.open(kFilename, std::ofstream::out | std::ofstream::trunc);
+    VerifyOrExit(ofs.good(), err = CHIP_ERROR_WRITE_FAILED);
+
+    mConfig.generate(ofs);
+    ofs.close();
+    VerifyOrExit(ofs.good(), err = CHIP_ERROR_WRITE_FAILED);
+
+exit:
+    return err;
+}

project-chip/connectedhomeip

examples/chip-tool/commands/pairing/PairingCommand.cpp
@@ -1,0 +1,147 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+#include "PairingCommand.h"
+
+using namespace ::chip;
+
+constexpr uint16_t kWaitDurationInSeconds = 120;
+
+CHIP_ERROR PairingCommand::Run(PersistentStorage & storage, NodeId localId, NodeId remoteId)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    err = mCommissioner.Init(localId, &storage, this);
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(Controller, "Init failure! Commissioner: %s", chip::ErrorStr(err)));
+
+    err = mCommissioner.ServiceEvents();
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(Controller, "Init failure! Run Loop: %s", chip::ErrorStr(err)));
+
+    err = RunInternal(remoteId);
+    VerifyOrExit(err == CHIP_NO_ERROR, ChipLogError(chipTool, "Init Failure! PairDevice: %s", chip::ErrorStr(err)));
+
+exit:
+    mCommissioner.ServiceEventSignal();
+    mCommissioner.Shutdown();
+    return err;
+}
+
+CHIP_ERROR PairingCommand::RunInternal(NodeId remoteId)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    UpdateWaitForResponse(true);
+    switch (mPairingMode)
+    {
+    case PairingMode::None:
+        err = Unpair(remoteId);
+        break;
+    case PairingMode::Bypass:
+        err = PairWithoutSecurity(remoteId, PeerAddress::UDP(mRemoteAddr.address, mRemotePort));
+        break;
+    case PairingMode::Ble:
+        err = Pair(remoteId, PeerAddress::BLE());
+        break;
+    case PairingMode::SoftAP:
+        err = Pair(remoteId, PeerAddress::UDP(mRemoteAddr.address, mRemotePort));
+        break;
+    }
+    WaitForResponse(kWaitDurationInSeconds);
+
+    return err;
+}
+
+CHIP_ERROR PairingCommand::Pair(NodeId remoteId, PeerAddress address)
+{
+    RendezvousParameters params =
+        RendezvousParameters().SetSetupPINCode(mSetupPINCode).SetDiscriminator(mDiscriminator).SetPeerAddress(address);
+
+    return mCommissioner.PairDevice(remoteId, params);
+}
+
+CHIP_ERROR PairingCommand::PairWithoutSecurity(NodeId remoteId, PeerAddress address)
+{
+    ChipSerializedDevice serializedTestDevice;
+    return mCommissioner.PairTestDeviceWithoutSecurity(remoteId, address.GetIPAddress(), serializedTestDevice, address.GetPort());
+}
+
+CHIP_ERROR PairingCommand::Unpair(NodeId remoteId)
+{
+    return mCommissioner.UnpairDevice(remoteId);
+}
+
+void PairingCommand::OnStatusUpdate(RendezvousSessionDelegate::Status status)
+{
+    switch (status)
+    {
+    case RendezvousSessionDelegate::Status::SecurePairingSuccess:
+        ChipLogProgress(chipTool, "Secure Pairing Success");
+        break;
+    case RendezvousSessionDelegate::Status::SecurePairingFailed:
+        ChipLogError(chipTool, "Secure Pairing Failed");
+        break;
+    case RendezvousSessionDelegate::Status::NetworkProvisioningSuccess:
+        ChipLogProgress(chipTool, "Network Provisioning Success");
+        break;
+    case RendezvousSessionDelegate::Status::NetworkProvisioningFailed:
+        ChipLogError(chipTool, "Network Provisioning Failed");
+        break;
+    }
+}
+
+void PairingCommand::OnNetworkCredentialsRequested(RendezvousDeviceCredentialsDelegate * callback)
+{
+    ChipLogProgress(chipTool, "OnNetworkCredentialsRequested");
+}
+
+void PairingCommand::OnOperationalCredentialsRequested(const char * csr, size_t csr_length,
+                                                       RendezvousDeviceCredentialsDelegate * callback)
+{
+    // TODO Implement this
+    ChipLogProgress(chipTool, "OnOperationalCredentialsRequested");
+}
+
+void PairingCommand::OnPairingComplete(CHIP_ERROR err)
+{
+    if (err == CHIP_NO_ERROR)
+    {
+        ChipLogProgress(chipTool, "Pairing Success");
+    }
+    else
+    {
+        ChipLogProgress(chipTool, "Pairing Failure: %s", chip::ErrorStr(err));
+    }
+
+    SetCommandExitStatus(err == CHIP_NO_ERROR);
+    UpdateWaitForResponse(false);
+}
+
+void PairingCommand::OnPairingDeleted(CHIP_ERROR err)
+{
+    if (err == CHIP_NO_ERROR)
+    {
+        ChipLogProgress(chipTool, "Pairing Deleted Success");
+    }
+    else
+    {
+        ChipLogProgress(chipTool, "Pairing Deleted Failure: %s", chip::ErrorStr(err));
+    }
+
+    SetCommandExitStatus(err == CHIP_NO_ERROR);
+    UpdateWaitForResponse(false);
+}

examples/chip-tool/config/PersistentStorage.cpp
@@ -1,0 +1,114 @@
+/*
+ *   Copyright (c) 2020 Project CHIP Authors
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+#include "PersistentStorage.h"
+
+#include <fstream>
+
+using String   = std::basic_string<char>;
+using Section  = std::map<String, String>;
+using Sections = std::map<String, Section>;
+
+using namespace ::chip;
+using namespace ::chip::Controller;
+
+constexpr const char kFilename[]           = "/tmp/chip_tool_config.ini";
+constexpr const char kDefaultSectionName[] = "Default";
+
+CHIP_ERROR PersistentStorage::Init()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    std::ifstream ifs;
+    ifs.open(kFilename, std::ifstream::in);
+    if (!ifs.good())
+    {
+        CommitConfig();
+        ifs.open(kFilename, std::ifstream::in);
+    }
+    VerifyOrExit(ifs.is_open(), err = CHIP_ERROR_OPEN_FAILED);
+
+    mConfig.parse(ifs);
+    ifs.close();
+
+exit:
+    return err;
+}
+
+void PersistentStorage::SetDelegate(PersistentStorageResultDelegate * delegate) {}
+
+void PersistentStorage::GetKeyValue(const char * key) {}
+
+CHIP_ERROR PersistentStorage::GetKeyValue(const char * key, char * value, uint16_t & size)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    std::string iniValue;
+    size_t iniValueLength = 0;
+
+    auto section = mConfig.sections[kDefaultSectionName];
+    auto it      = section.find(key);
+    VerifyOrExit(it != section.end(), err = CHIP_ERROR_KEY_NOT_FOUND);
+
+    VerifyOrExit(inipp::extract(section[key], iniValue), err = CHIP_ERROR_INVALID_ARGUMENT);
+
+    iniValueLength = iniValue.size();
+    VerifyOrExit(iniValueLength < static_cast<size_t>(size) - 1, err = CHIP_ERROR_BUFFER_TOO_SMALL);
+
+    iniValueLength        = iniValue.copy(value, iniValueLength);
+    value[iniValueLength] = '\0';
+
+exit:
+    return err;
+}
+
+void PersistentStorage::SetKeyValue(const char * key, const char * value)
+{
+    auto section = mConfig.sections[kDefaultSectionName];
+    section[key] = std::string(value);
+
+    mConfig.default_section(section);
+    CommitConfig();
+}
+
+void PersistentStorage::DeleteKeyValue(const char * key)
+{
+    auto section = mConfig.sections[kDefaultSectionName];
+    section.erase(key);
+
+    mConfig.default_section(section);
+    CommitConfig();
+}
+
+CHIP_ERROR PersistentStorage::CommitConfig()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    std::ofstream ofs;
+    std::string tmpPath = kFilename;
+    tmpPath.append(".tmp");
+    ofs.open(tmpPath, std::ofstream::out | std::ofstream::trunc);
+    VerifyOrExit(ofs.good(), err = CHIP_ERROR_WRITE_FAILED);
+
+    mConfig.generate(ofs);
+    ofs.close();
+    VerifyOrExit(ofs.good(), err = CHIP_ERROR_WRITE_FAILED);
+
+    VerifyOrExit(rename(tmpPath.c_str(), kFilename) == 0, err = CHIP_ERROR_WRITE_FAILED);
+
+exit:
+    return err;
+}