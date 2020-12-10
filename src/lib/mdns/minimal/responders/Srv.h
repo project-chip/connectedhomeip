d6a30995fcbc9084e89d7e4f481b4df2e400ca30
Andrei Litvin <andy314@gmail.com>
2020-12-10 09:08:49 -0500

    Order of init fix

--- src/lib/mdns/minimal/responders/Srv.h
+++ src/lib/mdns/minimal/responders/Srv.h
@@ -26,7 +26,7 @@ namespace Minimal {
 class SrvResponder : public Responder
 {
 public:
-    SrvResponder(const SrvResourceRecord & record) : Responder(QType::SRV, mRecord.GetName()), mRecord(record) {}
+    SrvResponder(const SrvResourceRecord & record) : Responder(QType::SRV, record.GetName()), mRecord(record) {}
 
     void AddAllResponses(const chip::Inet::IPPacketInfo * source, ResponderDelegate * delegate) override
     {
