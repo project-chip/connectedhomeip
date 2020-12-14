1c6ade58971605ddd917930398fd25df8c35cb67
Restyled.io <commits@restyled.io>
2020-12-14 16:16:12 +0000

    Restyled by whitespace

--- src/platform/nrfconnect/CHIPDevicePlatformEvent.h
+++ src/platform/nrfconnect/CHIPDevicePlatformEvent.h
@@ -18,7 +18,7 @@
 /**
  *    @file
  *          Provides platform-specific event types and data for the chip
- *          Device Layer on nRF Connect SDK platforms, by including Zephyr 
+ *          Device Layer on nRF Connect SDK platforms, by including Zephyr
  *          platform definitions.
  */
 
