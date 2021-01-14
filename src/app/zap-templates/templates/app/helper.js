38190d26f1c4a6fb37f4d556cb1393f93eb93da8
Restyled.io <commits@restyled.io>
2021-01-14 23:54:59 +0000

    Restyled by clang-format

--- src/app/zap-templates/templates/app/helper.js
+++ src/app/zap-templates/templates/app/helper.js
@@ -23,9 +23,8 @@ const zclHelper    = require(zapPath + 'generator/helper-zcl.js')
 const zclQuery     = require(zapPath + 'db/query-zcl.js')
 const cHelper      = require(zapPath + 'generator/helper-c.js')
 
-const StringHelper = require('../../common/StringHelper.js');
+const StringHelper    = require('../../common/StringHelper.js');
 const ChipTypesHelper = require('../../common/ChipTypesHelper.js');
-
 
 /**
  * Check if the cluster (name) has any enabled manufacturer commands. This works only inside
