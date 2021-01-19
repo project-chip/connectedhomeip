fcb994bf382a0cc5d656e9f7209a66ab83faf4e9
Michael Spang <spang@google.com>
2021-01-19 16:31:08 -0500

    Fix crashes when BLE rendezvous disconnects unexpectedly
    In BLE::OnBleEndPointConnectionClosed, the endpoint is already closed.
    We try to close it again in the Transport::BLE destructor, causing a
    crash.  Clear the endpoint in OnBleEndPointConnectionClosed to avoid
    this case.
    
    In addition, the OnRendezvousError callback ends up calling
    RendezvousCleanup, which destroys the transport. We therefore cannot
    call OnRendezvousConnectionClosed  after OnRendezvousError; |this| was
    already freed in OnRendezvousError and we just need to return.

--- src/transport/BLE.cpp
+++ src/transport/BLE.cpp
@@ -230,14 +230,18 @@ void BLE::OnBleEndPointConnectionClosed(BLEEndPoint * endPoint, BLE_ERROR err)
     BLE * ble   = reinterpret_cast<BLE *>(endPoint->mAppState);
     ble->mState = State::kNotReady;
 
+    // Already closed, avoid closing again in our destructor.
+    ble->mBleEndPoint = nullptr;
+
     if (ble->mDelegate)
     {
         if (err != BLE_NO_ERROR)
         {
             ble->mDelegate->OnRendezvousError(err);
+        } else {
+            // OnRendezvousError may delete |ble|; don't call both callbacks.
+            ble->mDelegate->OnRendezvousConnectionClosed();
         }
-
-        ble->mDelegate->OnRendezvousConnectionClosed();
     }
 }
 
