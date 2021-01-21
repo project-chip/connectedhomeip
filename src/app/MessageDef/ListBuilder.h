17e0b8f8bbdb58eb0dd1328eca7ea200c3964118
yunhanw <yunhanw@google.com>
2021-01-21 09:05:56 -0800

    Refactor MeessageDef
    Split MessageDef into multiple files without logic change for better readability and
    maintability
    
    fixes #3644

---
+++ src/app/MessageDef/ListBuilder.h
@@ -0,0 +1,70 @@
+/**
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2016-2017 Nest Labs, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ *    @file
+ *      This file defines List builder in CHIP interaction model
+ *
+ */
+
+#pragma once
+
+#ifndef _CHIP_INTERACTION_MODEL_MESSAGE_DEF_LIST_BUILDER_H
+#define _CHIP_INTERACTION_MODEL_MESSAGE_DEF_LIST_BUILDER_H
+
+#include "Builder.h"
+#include "Parser.h"
+#include <core/CHIPCore.h>
+#include <core/CHIPTLV.h>
+#include <support/CodeUtils.h>
+#include <support/logging/CHIPLogging.h>
+#include <util/basic-types.h>
+
+namespace chip {
+namespace app {
+class ListBuilder : public chip::app::Builder
+{
+protected:
+    ListBuilder();
+
+public:
+    /**
+     * Init the TLV array container with an particular context tag.
+     * Required to implement arrays of arrays, and to test ListBuilder.
+     *
+     * @param[in]   apWriter    Pointer to the TLVWriter that is encoding the message.
+     * @param[in]   aContextTagToUse    A contextTag to use.
+     *
+     * @return                  CHIP_ERROR codes returned by chip::TLV objects.
+     */
+
+    CHIP_ERROR Init(chip::TLV::TLVWriter * const apWriter, const uint8_t aContextTagToUse);
+    /**
+     * Init the TLV array container with an anonymous tag.
+     * Required to implement arrays of arrays, and to test ListBuilder.
+     *
+     * @param[in]   apWriter    Pointer to the TLVWriter that is encoding the message.
+     *
+     * @return                  CHIP_ERROR codes returned by chip::TLV objects.
+     */
+    CHIP_ERROR Init(chip::TLV::TLVWriter * const apWriter);
+};
+
+}; // namespace app
+}; // namespace chip
+
+#endif // _CHIP_INTERACTION_MODEL_MESSAGE_DEF_LIST_BUILDER_H
---
+++ src/app/MessageDef/ReadRequest.h
@@ -0,0 +1,176 @@
+/**
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2016-2017 Nest Labs, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ *    @file
+ *      This file defines ReadRequest parser and builder in CHIP interaction model
+ *
+ */
+
+#pragma once
+
+#ifndef _CHIP_INTERACTION_MODEL_MESSAGE_DEF_READ_REQUEST_H
+#define _CHIP_INTERACTION_MODEL_MESSAGE_DEF_READ_REQUEST_H
+
+#include "AttributeDataVersionList.h"
+#include "AttributePathList.h"
+#include "Builder.h"
+#include "EventPathList.h"
+
+#include "Parser.h"
+#include <core/CHIPCore.h>
+#include <core/CHIPTLV.h>
+#include <support/CodeUtils.h>
+#include <support/logging/CHIPLogging.h>
+#include <util/basic-types.h>
+
+namespace chip {
+namespace app {
+namespace ReadRequest {
+enum
+{
+    kCsTag_AttributePathList        = 0,
+    kCsTag_EventPathList            = 1,
+    kCsTag_AttributeDataVersionList = 2,
+    kCsTag_EventNumber              = 3,
+};
+
+class Parser : public chip::app::Parser
+{
+public:
+    /**
+     *  @brief Initialize the parser object with TLVReader
+     *
+     *  @param [in] aReader A pointer to a TLVReader, which should point to the beginning of this request
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR Init(const chip::TLV::TLVReader & aReader);
+
+    /**
+     *  @brief Roughly verify the message is correctly formed
+     *   1) all mandatory tags are present
+     *   2) all elements have expected data type
+     *   3) any tag can only appear once
+     *   4) At the top level of the structure, unknown tags are ignored for forward compatibility
+     *  @note The main use of this function is to print out what we're
+     *    receiving during protocol development and debugging.
+     *    The encoding rule has changed in IM encoding spec so this
+     *    check is only "roughly" conformant now.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR CheckSchemaValidity() const;
+
+    /**
+     *  @brief Get a TLVReader for the AttributePathList. Next() must be called before accessing them.
+     *
+     *  @param [in] apAttributePath    A pointer to apAttributePath
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetAttributePathList(AttributePathList::Parser * const apAttributePathList) const;
+
+    /**
+     *  @brief Get a TLVReader for the EventPathList. Next() must be called before accessing them.
+     *
+     *  @param [in] apEventPathList    A pointer to apEventPathList
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetEventPathList(EventPathList::Parser * const apEventPathList) const;
+
+    /**
+     *  @brief Get a parser for the AttributeDataVersionList. Next() must be called before accessing them.
+     *
+     *  @param [in] apAttributeDataVersionList    A pointer to apAttributeDataVersionList
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetAttributeDataVersionList(AttributeDataVersionList::Parser * const apAttributeDataVersionList) const;
+
+    /**
+     *  @brief Get Event Number. Next() must be called before accessing them.
+     *
+     *  @param [in] apEventNumber    A pointer to apEventNumber
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetEventNumber(uint64_t * const apEventNumber) const;
+};
+
+class Builder : public chip::app::Builder
+{
+public:
+    /**
+     *  @brief Initialize a ReadRequest::Builder for writing into a TLV stream
+     *
+     *  @param [in] apWriter    A pointer to TLVWriter
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR Init(chip::TLV::TLVWriter * const apWriter);
+
+    /**
+     *  @brief Initialize a AttributePathList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to AttributePathList::Builder
+     */
+    AttributePathList::Builder & CreateAttributePathListBuilder();
+
+    /**
+     *  @brief Initialize a EventPathList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to EventPathList::Builder
+     */
+    EventPathList::Builder & CreateEventPathListBuilder();
+
+    /**
+     *  @brief Initialize a AttributeDataVersionList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to AttributeDataVersionList::Builder
+     */
+    AttributeDataVersionList::Builder & CreateAttributeDataVersionListBuilder();
+
+    /**
+     *  @brief An initiator can optionally specify an EventNumber it has already to limit the
+     *  set of retrieved events on the server for optimization purposes.
+     *  @param [in] aEventNumber The event number
+     *  @return A reference to *this
+     */
+    ReadRequest::Builder & EventNumber(const uint64_t aEventNumber);
+    /**
+     *  @brief Mark the end of this ReadRequest
+     *
+     *  @return A reference to *this
+     */
+    ReadRequest::Builder & EndOfReadRequest();
+
+private:
+    AttributePathList::Builder mAttributePathListBuilder;
+    EventPathList::Builder mEventPathListBuilder;
+    AttributeDataVersionList::Builder mAttributeDataVersionListBuilder;
+};
+}; // namespace ReadRequest
+}; // namespace app
+}; // namespace chip
+
+#endif // _CHIP_INTERACTION_MODEL_MESSAGE_DEF_READ_REQUEST_H
