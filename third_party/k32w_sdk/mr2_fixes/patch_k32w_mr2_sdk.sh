159809577b6a5dd2f88fcdd4686ddab306975b09
Restyled.io <commits@restyled.io>
2021-01-22 09:40:34 +0000

    Restyled by shellharden

--- third_party/k32w_sdk/mr2_fixes/patch_k32w_mr2_sdk.sh
+++ third_party/k32w_sdk/mr2_fixes/patch_k32w_mr2_sdk.sh
@@ -5,13 +5,13 @@ if [[ ! -v K32W061_SDK_ROOT ]]; then
     exit 1
 fi
 
-cp -r ./third_party/k32w_sdk/mr2_fixes/chip                 $K32W061_SDK_ROOT/boards/k32w061dk6/wireless_examples/hybrid/ble_ot/
-cp ./third_party/k32w_sdk/mr2_fixes/PWR.c                   $K32W061_SDK_ROOT/middleware/wireless/framework/LowPower/Source/k32w061dk6/
-cp ./third_party/k32w_sdk/mr2_fixes/controller_interface.h  $K32W061_SDK_ROOT/middleware/wireless/ble_controller/interface/
-cp ./third_party/k32w_sdk/mr2_fixes/Messaging.h             $K32W061_SDK_ROOT/middleware/wireless/framework/Messaging/Interface/
-cp ./third_party/k32w_sdk/mr2_fixes/ble_conn_manager.h      $K32W061_SDK_ROOT/middleware/wireless/bluetooth/application/common/
-cp ./third_party/k32w_sdk/mr2_fixes/GenericList.h           $K32W061_SDK_ROOT/middleware/wireless/framework/Lists/
-cp ./third_party/k32w_sdk/mr2_fixes/pin_mux.c               $K32W061_SDK_ROOT/boards/k32w061dk6/wireless_examples/openthread/enablement/
+cp -r ./third_party/k32w_sdk/mr2_fixes/chip                 "$K32W061_SDK_ROOT"/boards/k32w061dk6/wireless_examples/hybrid/ble_ot/
+cp ./third_party/k32w_sdk/mr2_fixes/PWR.c                   "$K32W061_SDK_ROOT"/middleware/wireless/framework/LowPower/Source/k32w061dk6/
+cp ./third_party/k32w_sdk/mr2_fixes/controller_interface.h  "$K32W061_SDK_ROOT"/middleware/wireless/ble_controller/interface/
+cp ./third_party/k32w_sdk/mr2_fixes/Messaging.h             "$K32W061_SDK_ROOT"/middleware/wireless/framework/Messaging/Interface/
+cp ./third_party/k32w_sdk/mr2_fixes/ble_conn_manager.h      "$K32W061_SDK_ROOT"/middleware/wireless/bluetooth/application/common/
+cp ./third_party/k32w_sdk/mr2_fixes/GenericList.h           "$K32W061_SDK_ROOT"/middleware/wireless/framework/Lists/
+cp ./third_party/k32w_sdk/mr2_fixes/pin_mux.c               "$K32W061_SDK_ROOT"/boards/k32w061dk6/wireless_examples/openthread/enablement/
 
 echo "K32W MR2 patched"
 exit 0
