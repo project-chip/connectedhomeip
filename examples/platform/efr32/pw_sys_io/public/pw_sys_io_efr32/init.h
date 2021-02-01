9aae93d623ba261241de8327dee42537f8dfe2de
jmartinez-silabs <junior.martinez@silabs.com>
2021-02-01 16:32:15 -0500

    Implement the Pigweed-app example on EFR32 - Init uart driver and create function to read and write a character - link those  functions with pigweed backend - move some files that can be shared for all EFR32 examples to efr32/platform - create GN build structure for pigweed usage with EFR32 platform
    Implement the Pigweed-app example on EFR32
    - Init uart driver and create function to read and write a character
    - link those  functions with pigweed backend
    - move some files that can be shared for all EFR32 examples to efr32/platform
    - create GN build structure for pigweed usage with EFR32 platform
    
    Change uart driver from uartdrv to retargetSerial, using the dma was causing issues reading with pigweed proto
    Cleanup in the build GN files
    
    Some cleanup for the build

---
+++ examples/platform/efr32/pw_sys_io/public/pw_sys_io_efr32/init.h
@@ -0,0 +1,27 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include "pw_preprocessor/util.h"
+
+PW_EXTERN_C_START
+
+// The actual implement of PreMainInit() in sys_io_BACKEND.
+void pw_sys_io_Init();
+
+PW_EXTERN_C_END
--- src/platform/EFR32/Logging.cpp
+++ src/platform/EFR32/Logging.cpp
@@ -43,10 +43,25 @@
 #define LOG_RTT_BUFFER_INDEX 0
 #endif
 
+/**
+ * @def LOG_RTT_BUFFER_NAME
+ *
+ * RTT's name. Only used if LOG_RTT_BUFFER_INDEX is not 0. Otherwise,
+ * the buffer name is fixed to "Terminal".
+ *
+ */
 #ifndef LOG_RTT_BUFFER_NAME
 #define LOG_RTT_BUFFER_NAME "Terminal"
 #endif
 
+/**
+ * @def LOG_RTT_BUFFER_SIZE
+ *
+ * LOG RTT's buffer size. Only used if LOG_RTT_BUFFER_INDEX is not 0. To
+ * configure buffer #0 size, check the BUFFER_SIZE_UP definition in
+ * SEGGER_RTT_Conf.h
+ *
+ */
 #ifndef LOG_RTT_BUFFER_SIZE
 #define LOG_RTT_BUFFER_SIZE 256
 #endif
@@ -67,7 +82,10 @@ using namespace ::chip::DeviceLayer;
 using namespace ::chip::DeviceLayer::Internal;
 
 static bool sLogInitialized = false;
+#if LOG_RTT_BUFFER_INDEX != 0
 static uint8_t sLogBuffer[LOG_RTT_BUFFER_SIZE];
+static uint8_t sCmdLineBuffer[LOG_RTT_BUFFER_SIZE];
+#endif
 
 /**
  * Print a log message to RTT
@@ -79,11 +97,11 @@ static void PrintLog(const char * msg)
     {
         size_t sz;
         sz = strlen(msg);
-        SEGGER_RTT_WriteNoLock(0, msg, sz);
+        SEGGER_RTT_WriteNoLock(LOG_RTT_BUFFER_INDEX, msg, sz);
 
         const char * newline = "\r\n";
         sz                   = strlen(newline);
-        SEGGER_RTT_WriteNoLock(0, newline, sz);
+        SEGGER_RTT_WriteNoLock(LOG_RTT_BUFFER_INDEX, newline, sz);
     }
 #endif // EFR32_LOG_ENABLED
 }
@@ -94,8 +112,15 @@ static void PrintLog(const char * msg)
 extern "C" void efr32LogInit(void)
 {
 #if EFR32_LOG_ENABLED
+#if LOG_RTT_BUFFER_INDEX != 0
     SEGGER_RTT_ConfigUpBuffer(LOG_RTT_BUFFER_INDEX, LOG_RTT_BUFFER_NAME, sLogBuffer, LOG_RTT_BUFFER_SIZE,
                               SEGGER_RTT_MODE_NO_BLOCK_TRIM);
+
+    SEGGER_RTT_ConfigDownBuffer(LOG_RTT_BUFFER_INDEX, LOG_RTT_BUFFER_NAME, sCmdLineBuffer, LOG_RTT_BUFFER_SIZE,
+                                SEGGER_RTT_MODE_NO_BLOCK_SKIP);
+#else
+    SEGGER_RTT_SetFlagsUpBuffer(LOG_RTT_BUFFER_INDEX, SEGGER_RTT_MODE_NO_BLOCK_TRIM);
+#endif
     sLogInitialized = true;
 #endif // EFR32_LOG_ENABLED
 }
