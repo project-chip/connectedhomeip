2622313129682bad8007a1df3194bad19dcadc74
Andrei Litvin <andy314@gmail.com>
2021-02-02 10:21:33 -0500

    Restyle fixes

--- scripts/setup/raspberry_pi/setup_ubuntu_20_04_lts.sh
+++ scripts/setup/raspberry_pi/setup_ubuntu_20_04_lts.sh
@@ -24,23 +24,23 @@
 set -ex
 
 sudo apt-get install -fy \
-   g++ \
-   gcc \
-   git \
-   libavahi-client-dev \
-   libcairo2-dev \
-   libdbus-1-dev \
-   libgirepository1.0-dev \
-   libglib2.0-dev \
-   libssl-dev \
-   ninja-build \
-   pi-bluetooth \
-   pkg-config \
-   protobuf-compiler \
-   python \
-   python3-dev \
-   python3-venv \
-   unzip &&
-   true
+    g++ \
+    gcc \
+    git \
+    libavahi-client-dev \
+    libcairo2-dev \
+    libdbus-1-dev \
+    libgirepository1.0-dev \
+    libglib2.0-dev \
+    libssl-dev \
+    ninja-build \
+    pi-bluetooth \
+    pkg-config \
+    protobuf-compiler \
+    python \
+    python3-dev \
+    python3-venv \
+    unzip &&
+    true
 
 echo "You must REBOOT after pi-bluetooth has been installed"
