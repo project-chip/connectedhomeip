14faa5d96a96cf3e066a5a45c46b7c2072d68634
Restyled.io <commits@restyled.io>
2021-02-03 20:40:40 +0000

    Restyled by clang-format

--- src/controller/CHIPDevice.h
+++ src/controller/CHIPDevice.h
@@ -373,7 +373,7 @@ typedef struct SerializableDevice
     uint64_t mDeviceId; /* This field is serialized in LittleEndian byte order */
     uint8_t mDeviceAddr[INET6_ADDRSTRLEN];
     uint16_t mDevicePort; /* This field is serealized in LittelEndian byte order */
-    uint16_t mAdminId; /* This field is serealized in LittelEndian byte order */
+    uint16_t mAdminId;    /* This field is serealized in LittelEndian byte order */
     uint8_t mInterfaceName[kMaxInterfaceName];
 } SerializableDevice;
 
