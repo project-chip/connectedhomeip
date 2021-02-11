0c75b58a2cbc22d3cd8468be271b52f9cfe5eca8
Andrei Litvin <andy314@gmail.com>
2021-02-11 09:29:57 -0500

    Also fix android include - missed last time because of quotes used for those includes

--- src/platform/logging/impl/android/Logging.cpp
+++ src/platform/logging/impl/android/Logging.cpp
@@ -1,6 +1,6 @@
 /* See Project chip LICENSE file for licensing information. */
 
-#include "logging/LogV.h"
+#include "platform/logging/LogV.h"
 #include "support/logging/Constants.h"
 
 #include <android/log.h>
