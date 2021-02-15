36217724e40aa6e05db1a780caa90a005b2979c1
Vincent Coubard <vincent.coubard@arm.com>
2021-02-15 18:26:45 +0000

    Mbed: Implement platform manager.
    The class contains three members:
    - An event queue to store events.
    - A mutex to protect calls into the CHIP stack.
    - A thread to run processing in an isolated context.
    
    There is room for optimization; it is documented in the code.

--- src/platform/mbed/PlatformManagerImpl.cpp
+++ src/platform/mbed/PlatformManagerImpl.cpp
@@ -1,3 +1,5 @@
+#include <new>
+
 #include "platform/internal/CHIPDeviceLayerInternal.h"
 
 #include <platform/PlatformManager.h>
@@ -6,14 +8,40 @@
 namespace chip {
 namespace DeviceLayer {
 
+// TODO: Event and timer processing is not efficient from a memory perspective.
+// Both occupy at least 24 bytes when only 4 bytes is required.
+// An optimized designed could use a separate circular buffer to store events
+// and register a single mbed event in the event queue to process all of them.
+// A similar design can be used for timers.
+
 CHIP_ERROR PlatformManagerImpl::_InitChipStack(void)
 {
     // Members are initialized by the stack
-    // TODO: understand if they should be dynamicaly allocated or not.
+    if (!mInitialized)
+    {
+        // reinitialize a new thread if it was terminated earlier
+        mLoopTask.~Thread();
+        new (&mLoopTask) rtos::Thread(osPriorityNormal, CHIP_DEVICE_CONFIG_CHIP_TASK_STACK_SIZE,
+                                      /* memory provided */ nullptr, CHIP_DEVICE_CONFIG_CHIP_TASK_NAME);
+
+        // Reinitialize the EventQueue
+        mQueue.~EventQueue();
+        new (&mQueue) events::EventQueue(event_size * CHIP_DEVICE_CONFIG_MAX_EVENT_QUEUE_SIZE);
+
+        // Reinitialize the Mutex
+        mChipStackMutex.~Mutex();
+        new (&mChipStackMutex) rtos::Mutex();
+    }
+    else
+    {
+        ChipLogError(DeviceLayer, "Trying to reinitialize the stack");
+        return CHIP_ERROR_INCORRECT_STATE;
+    }
 
     // Call up to the base class _InitChipStack() to perform the bulk of the initialization.
     auto err = GenericPlatformManagerImpl<ImplClass>::_InitChipStack();
     SuccessOrExit(err);
+    mInitialized = true;
 
 exit:
     return err;
@@ -33,23 +61,111 @@ void PlatformManagerImpl::_UnlockChipStack()
     mChipStackMutex.unlock();
 }
 
-void PlatformManagerImpl::_PostEvent(const ChipDeviceEvent * event) {}
+void PlatformManagerImpl::_PostEvent(const ChipDeviceEvent * event)
+{
+    auto handle = mQueue.call([event, this] {
+        LockChipStack();
+        DispatchEvent(event);
+        UnlockChipStack();
+    });
 
-void PlatformManagerImpl::_RunEventLoop() {}
+    if (!handle)
+    {
+        ChipLogError(DeviceLayer, "Error posting event: Not enough memory");
+    }
+}
+
+void PlatformManagerImpl::_RunEventLoop()
+{
+    // Note: no reason to lock the chip task here, it is locked by the
+    // event and timer callback
+    mQueue.dispatch_forever();
+}
 
 CHIP_ERROR PlatformManagerImpl::_StartEventLoopTask()
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    // This function start the Thread that run the chip event loop.
+    // If no threads are needed, the application can directly call RunEventLoop.
+    auto error = mLoopTask.start([this] {
+        ChipLogDetail(DeviceLayer, "CHIP task running");
+        RunEventLoop();
+    });
+
+    CHIP_ERROR err = TranslateOsStatus(error);
+    SuccessOrExit(err);
+
+exit:
+    return err;
 }
 
 CHIP_ERROR PlatformManagerImpl::_StartChipTimer(int64_t durationMS)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    int event      = 0;
+
+    VerifyOrExit(mInitialized == true, err = CHIP_ERROR_INCORRECT_STATE);
+
+    event = mQueue.call_in(std::chrono::milliseconds(durationMS), [this] {
+        LockChipStack();
+        auto err = SystemLayer.HandlePlatformTimer();
+        if (err != CHIP_SYSTEM_NO_ERROR)
+        {
+            ChipLogError(DeviceLayer, "Error handling CHIP timers: %s", ErrorStr(err));
+        }
+        UnlockChipStack();
+    });
+
+    VerifyOrExit(event != 0, err = CHIP_ERROR_NO_MEMORY);
+
+exit:
+    return err;
 }
 
 CHIP_ERROR PlatformManagerImpl::_Shutdown()
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    LockChipStack();
+
+    // If running, break out of the loop
+    if (IsLoopActive())
+    {
+        mQueue.break_dispatch();
+        mLoopTask.join();
+    }
+
+    // Note: we leave the event queue as is. It will be reinitialized in Init()
+    mInitialized = false;
+    UnlockChipStack();
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR PlatformManagerImpl::TranslateOsStatus(osStatus error)
+{
+    switch (error)
+    {
+    case osErrorNoMemory:
+        return CHIP_ERROR_NO_MEMORY;
+
+    case osOK:
+        return CHIP_NO_ERROR;
+
+    default:
+        return CHIP_ERROR_INTERNAL;
+    }
+}
+
+bool PlatformManagerImpl::IsLoopActive()
+{
+    switch (mLoopTask.get_state())
+    {
+    case rtos::Thread::Inactive:
+    case rtos::Thread::Ready:
+    case rtos::Thread::Deleted:
+        return false;
+
+    default:
+        return true;
+    }
 }
 
 // ===== Members for internal use by the following friends.
