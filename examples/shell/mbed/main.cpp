e39fe16de3fe599d3575fc3454c13ba7094a3559
Lukasz Gniadzik <lukasz.gniadzik@mobica.com>
2021-02-16 02:00:55 +0100

    Add |date| command to mbed shell example.
    This command can be used to verify SystemTimeSupport implementation
    for Mbed-OS platform.
    
    Usage example:
    
    date set YYYY-MM-DD HH:MM:SS
    date

---
+++ examples/shell/mbed/main.cpp
@@ -0,0 +1,52 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <lib/shell/shell.h>
+
+#include <lib/core/CHIPCore.h>
+#include <lib/support/Base64.h>
+#include <lib/support/CHIPArgParser.hpp>
+#include <lib/support/CodeUtils.h>
+#include <lib/support/RandUtils.h>
+#include <support/logging/CHIPLogging.h>
+
+#include <ChipShellCollection.h>
+
+using namespace chip;
+using namespace chip::Shell;
+
+int main()
+{
+    // Initialize the default streamer that was linked.
+    const int rc = streamer_init(streamer_get());
+
+    if (rc != 0)
+    {
+        ChipLogError(Shell, "Streamer initialization failed: %d", rc);
+        return rc;
+    }
+
+    cmd_misc_init();
+    cmd_base64_init();
+    cmd_device_init();
+    cmd_btp_init();
+    cmd_otcli_init();
+    cmd_mbed_utils_init();
+
+    shell_task(nullptr);
+    return 0;
+}
