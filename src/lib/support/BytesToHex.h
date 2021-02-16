741c5e3a181668bc6b889dde06b4495b4e69544a
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2021-02-16 17:13:04 -0500

    Add support/BytesToHex.h -> lightweight hex conversion
    * Lean on stack
    * Supports safe output to buffers of any size
    * Support uppercase/lowercase
    * Support non-nul-terminated and nul-terminated

---
+++ src/lib/support/BytesToHex.h
@@ -0,0 +1,62 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <core/CHIPError.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace chip {
+namespace Encoding {
+
+/**
+ * Encode a buffer of bytes into hexadecimal, with or without nul-termination
+ * and using either lowercase or uppercase hex.
+ *
+ * If `nul_terminate` is true, treat `dest_hex` as a nul-terminated string buffer.
+ * The function returns CHIP_ERROR_BUFFER_TOO_SMALL if dest_size_max can't fit
+ * the entire encoded buffer, and the nul-terminator if enabled. This function
+ * will never output truncated data. The result either fits and is written,
+ * or does not fit and nothing is written to dest_hex.
+ *
+ * If src_bytes and dest_hex overlap, the results may be incorrect, depending
+ * on overlap, but only the core validity checks are done and it's possible to
+ * get CHIP_NO_ERROR with erroneous output.
+ *
+ * On success, number of writes written to destination is always:
+ *   output_size = (src_size * 2) + (nul_terminate ? 1 : 0);
+ *
+ * @param src_bytes Pointer to non-null buffer to convert
+ * @param src_size Number of bytes to convert from src_bytes
+ * @param [out] dest_hex Destination buffer to receive hex encoding
+ * @param dest_size_max Maximum buffer size for the hex encoded `dest_hex` buffer
+ *                      including nul-terminator if needed.
+ * @param uppercase Use upper-case hex if true, otherwise lower-case hex
+ * @param nul_terminate Add nul terminator if true. See description
+ *
+ * @return CHIP_ERROR_BUFFER_TOO_SMALL on dest_max_size too small to fit output
+ * @return CHIP_ERROR_INVALID_ARGUMENT if either src_bytes or dest_hex is nullptr
+ * @return CHIP_NO_ERROR on success
+ */
+
+CHIP_ERROR BytesToHex(const uint8_t * src_bytes, size_t src_size,
+                      char * dest_hex, size_t dest_size_max,
+                      bool uppercase, bool nul_terminate);
+
+} // namespace Encoding
+} // namespace chip
---
+++ src/lib/support/tests/TestBytesToHex.cpp
@@ -0,0 +1,174 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <support/BytesToHex.h>
+#include <support/UnitTestRegistration.h>
+
+#include <nlunit-test.h>
+
+namespace {
+
+using namespace chip::Encoding;
+
+void TestBytesToHexNotNulTerminated(nlTestSuite * inSuite, void * inContext)
+{
+    // Uppercase
+    {
+        uint8_t src[] = {0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'F', 'E', 'D', 'C', 'B', 'A', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '!', '@'};
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], sizeof(src) * 2u,
+                                           /*uppercase=*/true, /*nul_terminate=*/false) == CHIP_NO_ERROR);
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Lowercase
+    {
+        uint8_t src[] = {0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'f', 'e', 'd', 'c', 'b', 'a', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '!', '@'};
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], sizeof(src) * 2u,
+                                           /*uppercase=*/false, /*nul_terminate=*/false) == CHIP_NO_ERROR);
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Trivial: Zero size input
+    {
+        uint8_t src[] = {};
+        char dest[2] =     {'!', '@'};
+        char expected[2] = {'!', '@'};
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], sizeof(src) * 2u,
+                                           /*uppercase=*/false, /*nul_terminate=*/false) == CHIP_NO_ERROR);
+        // Nothing should have been touched.
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+}
+
+void TestBytesToHexNulTerminated(nlTestSuite * inSuite, void * inContext)
+{
+    // Uppercase
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '\0', '@'};
+        NL_TEST_ASSERT(inSuite, ((sizeof(src) * 2u) + 1u) <= sizeof(dest));
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], (sizeof(src) * 2u) + 1,
+                                           /*uppercase=*/true, /*nul_terminate=*/true) == CHIP_NO_ERROR);
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Lowercase
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '\0', '@'};
+        NL_TEST_ASSERT(inSuite, ((sizeof(src) * 2u) + 1u) <= sizeof(dest));
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], (sizeof(src) * 2u) + 1,
+                                           /*uppercase=*/false, /*nul_terminate=*/true) == CHIP_NO_ERROR);
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Trivial: Zero size input
+    {
+        uint8_t src[] = {};
+        char dest[2] =     {'!', '@'};
+        char expected[2] = {'\0', '@'};
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], sizeof(dest),
+                                           /*uppercase=*/false, /*nul_terminate=*/true) == CHIP_NO_ERROR);
+        // Expect nul termination
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+}
+
+void TestBytesToHexErrors(nlTestSuite * inSuite, void * inContext)
+{
+    // NULL source
+    {
+        const uint8_t * src = nullptr;
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], 0, &dest[0], sizeof(dest),
+                                           /*uppercase=*/true, /*nul_terminate=*/true) == CHIP_ERROR_INVALID_ARGUMENT);
+
+        // Buffers should not have been touched
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // NULL destination
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char * dest = nullptr;
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], 0, dest, sizeof(src) * 2u,
+                                           /*uppercase=*/true, /*nul_terminate=*/false) == CHIP_ERROR_INVALID_ARGUMENT);
+    }
+
+    // Destination buffer too small for non-nul-terminated
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        NL_TEST_ASSERT(inSuite, ((sizeof(src) * 2u) + 1u) <= sizeof(dest));
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], (sizeof(src) * 2u) - 1,
+                                           /*uppercase=*/true, /*nul_terminate=*/false) == CHIP_ERROR_BUFFER_TOO_SMALL);
+        // Ensure output not touched
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Destination buffer too small for nul-terminated
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        NL_TEST_ASSERT(inSuite, ((sizeof(src) * 2u) + 1u) <= sizeof(dest));
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], (sizeof(src) * 2u),
+                                           /*uppercase=*/true, /*nul_terminate=*/true) == CHIP_ERROR_BUFFER_TOO_SMALL);
+        // Ensure output not touched
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+
+    // Writing in a larger buffer is fine, bytes past the nul terminator (when requested) are untouched.
+    {
+        uint8_t src[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+        char dest[18] =     {'?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '!', '@'};
+        char expected[18] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '\0', '@'};
+        NL_TEST_ASSERT(inSuite, ((sizeof(src) * 2u) + 1u) < sizeof(dest));
+        NL_TEST_ASSERT(inSuite, BytesToHex(&src[0], sizeof(src), &dest[0], sizeof(dest),
+                                           /*uppercase=*/false, /*nul_terminate=*/true) == CHIP_NO_ERROR);
+        NL_TEST_ASSERT(inSuite, memcmp(&dest[0], &expected[0], sizeof(expected)) == 0);
+    }
+}
+
+const nlTest sTests[] = {
+    NL_TEST_DEF("TestBytesToHexNotNulTerminated", TestBytesToHexNotNulTerminated),  //
+    NL_TEST_DEF("TestBytesToHexNulTerminated", TestBytesToHexNulTerminated),        //
+    NL_TEST_DEF("TestBytesToHexErrors", TestBytesToHexErrors),                      //
+    NL_TEST_SENTINEL()                                       //
+};
+
+} // namespace
+
+int TestBytesToHex(void)
+{
+    nlTestSuite theSuite = { "BytesToHex", sTests, nullptr, nullptr };
+    nlTestRunner(&theSuite, nullptr);
+    return nlTestRunnerStats(&theSuite);
+}
+
+CHIP_REGISTER_TEST_SUITE(TestBytesToHex)
