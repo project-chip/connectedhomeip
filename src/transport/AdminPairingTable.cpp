7e2a87a23899908971f9a86a00b1e8e65c80f943
Boris Zbarsky <bzbarsky@apple.com>
2021-02-25 13:19:48 -0500

    Remove unnecessary checking and casting of sizeof when we need uint16_t sizes. (#4997)
    Compilers generate a constant for sizeof(), which converts cleanly to
    whatever integer size we need as long as the constant is small enough.

--- src/transport/AdminPairingTable.cpp
+++ src/transport/AdminPairingTable.cpp
@@ -37,9 +37,7 @@ CHIP_ERROR AdminPairingInfo::StoreIntoKVS(PersistentStorageDelegate & kvs)
     info.mNodeId = Encoding::LittleEndian::HostSwap64(mNodeId);
     info.mAdmin  = Encoding::LittleEndian::HostSwap16(mAdmin);
 
-    VerifyOrReturnError(CanCastTo<uint16_t>(sizeof(info)), CHIP_ERROR_INTERNAL);
-    uint16_t size = static_cast<uint16_t>(sizeof(info));
-    return kvs.SetKeyValue(key, &info, size);
+    return kvs.SetKeyValue(key, &info, sizeof(info));
 }
 
 CHIP_ERROR AdminPairingInfo::FetchFromKVS(PersistentStorageDelegate & kvs)
@@ -49,8 +47,7 @@ CHIP_ERROR AdminPairingInfo::FetchFromKVS(PersistentStorageDelegate & kvs)
 
     StorableAdminPairingInfo info;
 
-    VerifyOrReturnError(CanCastTo<uint16_t>(sizeof(info)), CHIP_ERROR_INTERNAL);
-    uint16_t size = static_cast<uint16_t>(sizeof(info));
+    uint16_t size = sizeof(info);
     ReturnErrorOnFailure(kvs.GetKeyValue(key, &info, size));
 
     mNodeId    = Encoding::LittleEndian::HostSwap64(info.mNodeId);
