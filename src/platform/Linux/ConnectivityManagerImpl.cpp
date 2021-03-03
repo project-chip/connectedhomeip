4120787a86f8a5bf49ac72078a136f7ae8c079b1
Song Guo <songguo@google.com>
2021-03-03 17:10:11 +0800

    Run dhclient after connected to wifi

--- src/platform/Linux/ConnectivityManagerImpl.cpp
+++ src/platform/Linux/ConnectivityManagerImpl.cpp
@@ -21,6 +21,7 @@
 #include <platform/ConnectivityManager.h>
 #include <platform/internal/BLEManager.h>
 
+#include <cstdlib>
 #include <new>
 
 #include <support/CodeUtils.h>
@@ -676,7 +677,7 @@ void ConnectivityManagerImpl::DriveAPState()
 
                 // Compute the amount of idle time before the AP should be deactivated and
                 // arm a timer to fire at that time.
-                apTimeout = (uint32_t)((mLastAPDemandTime + mWiFiAPIdleTimeoutMS) - now);
+                apTimeout = (uint32_t) ((mLastAPDemandTime + mWiFiAPIdleTimeoutMS) - now);
                 err       = SystemLayer.StartTimer(apTimeout, DriveAPState, NULL);
                 SuccessOrExit(err);
                 ChipLogProgress(DeviceLayer, "Next WiFi AP timeout in %" PRIu32 " s", apTimeout / 1000);
@@ -899,6 +900,38 @@ CHIP_ERROR ConnectivityManagerImpl::ProvisionWiFiNetwork(const char * ssid, cons
             if (result)
             {
                 ChipLogProgress(DeviceLayer, "wpa_supplicant: save config succeeded!");
+
+                // Iterate on the network interface to see if we already have beed assigned addresses.
+                // The temporary hack for getting IP address change on linux for network provisioning in the rendezvous session.
+                // This should be removed or find a better place once we depercate the rendezvous session.
+                for (chip::Inet::InterfaceAddressIterator it; it.HasCurrent(); it.Next())
+                {
+                    char ifName[chip::Inet::InterfaceIterator::kMaxIfNameLength];
+                    if (it.IsUp() && CHIP_NO_ERROR == it.GetInterfaceName(ifName, sizeof(ifName)) &&
+                        strncmp(ifName, CHIP_DEVICE_CONFIG_WIFI_STATION_IF_NAME, sizeof(ifName)) == 0)
+                    {
+                        chip::Inet::IPAddress addr = it.GetAddress();
+                        if (addr.IsIPv4())
+                        {
+                            ChipDeviceEvent event;
+                            event.Type                            = DeviceEventType::kInternetConnectivityChange;
+                            event.InternetConnectivityChange.IPv4 = kConnectivity_Established;
+                            event.InternetConnectivityChange.IPv6 = kConnectivity_NoChange;
+                            addr.ToString(event.InternetConnectivityChange.address);
+
+                            ChipLogDetail(DeviceLayer, "Got IP address on interface: %s IP: %s", ifName,
+                                          event.InternetConnectivityChange.address);
+
+                            PlatformMgr().PostEvent(&event);
+                        }
+                    }
+                }
+
+                // Run dhclient for IP on WiFi.
+                // TODO: The wifi can be managed by networkmanager on linux so we don't have to care about this.
+                char cmdBuffer[128];
+                sprintf(cmdBuffer, "dhclient %s", CHIP_DEVICE_CONFIG_WIFI_STATION_IF_NAME);
+                system(cmdBuffer);
             }
             else
             {
@@ -908,32 +941,6 @@ CHIP_ERROR ConnectivityManagerImpl::ProvisionWiFiNetwork(const char * ssid, cons
 
             if (gerror != nullptr)
                 g_error_free(gerror);
-
-            // Iterate on the network interface to see if we already have beed assigned addresses.
-            // The temporary hack for getting IP address change on linux for network provisioning in the rendezvous session.
-            // This should be removed or find a better place once we depercate the rendezvous session.
-            for (chip::Inet::InterfaceAddressIterator it; it.HasCurrent(); it.Next())
-            {
-                char ifName[chip::Inet::InterfaceIterator::kMaxIfNameLength];
-                if (it.IsUp() && CHIP_NO_ERROR == it.GetInterfaceName(ifName, sizeof(ifName)) &&
-                    strncmp(ifName, CHIP_DEVICE_CONFIG_WIFI_STATION_IF_NAME, sizeof(ifName)) == 0)
-                {
-                    chip::Inet::IPAddress addr = it.GetAddress();
-                    if (addr.IsIPv4())
-                    {
-                        ChipDeviceEvent event;
-                        event.Type                            = DeviceEventType::kInternetConnectivityChange;
-                        event.InternetConnectivityChange.IPv4 = kConnectivity_Established;
-                        event.InternetConnectivityChange.IPv6 = kConnectivity_NoChange;
-                        addr.ToString(event.InternetConnectivityChange.address);
-
-                        ChipLogDetail(DeviceLayer, "Got IP address on interface: %s IP: %s", ifName,
-                                      event.InternetConnectivityChange.address);
-
-                        PlatformMgr().PostEvent(&event);
-                    }
-                }
-            }
 
             // Return success as long as the device is connected to the network
             ret = CHIP_NO_ERROR;
