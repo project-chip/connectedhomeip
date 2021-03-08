c31ee448f0bceb0ed18443da9c82da0c543c1d79
Andrei Litvin <andy314@gmail.com>
2021-03-08 15:25:14 -0500

    Replace semaphores with dispatch semaphore for apple (unnamed semaphores not supported on darwin)

--- src/controller/python/chip/internal/ChipThreadWork.cpp
+++ src/controller/python/chip/internal/ChipThreadWork.cpp
@@ -16,9 +16,15 @@
  */
 
 #include "ChipThreadWork.h"
+
+#ifdef __APPLE__
+#include <dispatch/dispatch.h>
+#else
 #include <semaphore.h>
+#endif
 
 #include <platform/CHIPDeviceLayer.h>
+
 
 namespace chip {
 namespace python {
@@ -27,11 +33,20 @@ namespace {
 struct WorkData
 {
     WorkCallback callback;
+#ifdef __APPLE__
+    dispatch_semaphore_t done;
+
+    WorkData() { done = dispatch_semaphore_create(0); }
+    ~WorkData() { dispatch_release(done); }
+    void Post() { dispatch_semaphore_signal(done); }
+    void Wait() { dispatch_semaphore_wait(done, DISPATCH_TIME_FOREVER); }
+#else
     sem_t done;
 
     WorkData() { sem_init(&done, 0 /* shared */, 0); }
     void Post() { sem_post(&done); }
     void Wait() { sem_wait(&done); }
+#endif
 };
 
 void PerformWork(intptr_t arg)
