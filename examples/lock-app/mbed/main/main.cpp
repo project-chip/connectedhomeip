2a3a98d5f49c412997c4acc22e263a286780b66d
Filip Jagodzinski <filip.jagodzinski@arm.com>
2021-03-08 17:47:01 +0100

    Mbed: Update Cmake files and add main stub for the lock-app

---
+++ examples/lock-app/mbed/main/main.cpp
@@ -0,0 +1,35 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2019 Google LLC.
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <platform/CHIPDeviceLayer.h>
+#include <support/logging/CHIPLogging.h>
+
+using namespace ::chip;
+using namespace ::chip::Inet;
+using namespace ::chip::DeviceLayer;
+
+int main()
+{
+    ChipLogProgress(NotSpecified, "Init CHIP Stack\r\n");
+    int ret = PlatformMgr().InitChipStack();
+    if (ret != CHIP_NO_ERROR)
+    {
+        ChipLogError(NotSpecified, "PlatformMgr().InitChipStack() failed");
+    }
+}
