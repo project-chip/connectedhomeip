6e3372e7581943f6f8bd4d111a409adb3404f3b3
doru91 <doru-cristian.gucea@nxp.com>
2021-03-12 17:12:06 +0200

    [K32W] Fix memory allocation (#5242)
    * [K32W] Fix memory allocation
    
    We had to fix the alloc/free pointers because OpenThread API changed.
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>
    
    * Restyled by clang-format
    
    * [K32W] Fix possible buffer overflow
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- examples/lock-app/k32w/main/main.cpp
+++ examples/lock-app/k32w/main/main.cpp
@@ -22,10 +22,10 @@
 
 #include "openthread/platform/logging.h"
 #include "openthread/platform/uart.h"
+#include <mbedtls/platform.h>
 #include <openthread-system.h>
 #include <openthread/cli.h>
 #include <openthread/error.h>
-#include <openthread/heap.h>
 
 #include <core/CHIPError.h>
 #include <platform/CHIPDeviceLayer.h>
@@ -43,8 +43,6 @@ using namespace ::chip::Inet;
 using namespace ::chip::DeviceLayer;
 using namespace ::chip::Logging;
 
-extern "C" void * pvPortCallocRtos(size_t num, size_t size);
-
 #include <AppTask.h>
 
 typedef void (*InitFunc)(void);
@@ -53,6 +51,8 @@ extern InitFunc __init_array_end;
 
 /* needed for FreeRtos Heap 4 */
 uint8_t __attribute__((section(".heap"))) ucHeap[0xF000];
+
+extern "C" void * pvPortCallocRtos(size_t num, size_t size);
 
 extern "C" void main_task(void const * argument)
 {
@@ -64,6 +64,8 @@ extern "C" void main_task(void const * argument)
     {
         (*pFunc)();
     }
+
+    mbedtls_platform_set_calloc_free(pvPortCallocRtos, vPortFree);
 
     /* Used for HW initializations */
     otSysInit(0, NULL);
