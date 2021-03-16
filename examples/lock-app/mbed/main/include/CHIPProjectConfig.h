e4187543a93df44b42f7858e3c11223290abeefb
Lukasz Gniadzik <lukasz.gniadzik@mobica.com>
2021-03-16 18:53:25 +0100

    Mbed: lock-app: Remove duplicated ChipProjectConfig.h file.
    In mbed lock-app example we already use one ChipProjectConfig.h
    which is inside include/directory.
    Additionally config.in is configured to point to that file.
    For that reason wifi station and ap mode enabling was moved to that file
    as well.

--- examples/lock-app/mbed/main/include/CHIPProjectConfig.h
+++ examples/lock-app/mbed/main/include/CHIPProjectConfig.h
@@ -27,8 +27,8 @@
 
 #pragma once
 
-// #define CHIP_DEVICE_CONFIG_ENABLE_WIFI_STATION 0
-// #define CHIP_DEVICE_CONFIG_ENABLE_WIFI_AP 0
+#define CHIP_DEVICE_CONFIG_ENABLE_WIFI_STATION 1
+#define CHIP_DEVICE_CONFIG_ENABLE_WIFI_AP 1
 
 // Use a default pairing code if one hasn't been provisioned in flash.
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_PIN_CODE 12345678
