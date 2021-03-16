3846103ba42d3cc59f332977c5c070b6200cd0a8
Vincent Coubard <vincent.coubard@arm.com>
2021-03-16 12:56:35 +0000

    Add event fd support: The file handle that provides event support is EventFileHandle. It has some limitations: - Flags are not supported - Counter is initialized at 0. - It only works in non blocking mode. To facilitate integration, a function that allocate a FileHandle and convert it into a file descriptor as been added. The FileHandle to use is allocated dynamically and the memory is released when the file it is closed.

--- examples/shell/mbed/arch.c
+++ examples/shell/mbed/arch.c
@@ -8,6 +8,24 @@ void __sync_synchronize() {
     MBED_BARRIER();
 }
 
-unsigned int __atomic_fetch_add_4(volatile uint32_t *ptr, uint32_t val, int memorder) { 
-    return core_util_atomic_fetch_add_explicit_u32(ptr, val, memorder);
+unsigned int __atomic_fetch_add_4(volatile void *ptr, unsigned int val, int memorder) { 
+    return core_util_atomic_fetch_add_explicit_u32((volatile uint32_t*)ptr, val, memorder);
+}
+
+uint64_t __atomic_load_8(const volatile void* ptr, int memorder) {
+    return core_util_atomic_load_explicit_u64((const volatile uint64_t*)ptr, memorder);
+}
+
+uint64_t __atomic_exchange_8(volatile void* ptr, uint64_t val, int memorder) {
+    return core_util_atomic_exchange_explicit_u64((volatile uint64_t*)ptr, val, memorder);
+}
+
+
+bool __atomic_compare_exchange_8(volatile void *ptr, void*expected, uint64_t desired, bool weak, int success_memorder, int failure_memorder)
+{
+    if (weak) {
+        return core_util_atomic_compare_exchange_weak_explicit_u64((volatile uint64_t*)ptr, (uint64_t*)expected, desired, success_memorder, failure_memorder);
+    } else {
+        return core_util_atomic_cas_explicit_u64((volatile uint64_t*)ptr, (uint64_t*)expected, desired, success_memorder, failure_memorder);
+    }
 }
--- src/platform/mbed/posix/src/eventfd.cpp
+++ src/platform/mbed/posix/src/eventfd.cpp
@@ -1,17 +1,24 @@
 #include <net_if.h>
 #include <sys/eventfd.h>
 
+#include "EventFileHandle.h"
+#include "OpenFileHandleAsFileDescriptor.h"
+
 int eventfd(unsigned int initval, int flags)
 {
-    return 0;
+    if (initval || flags)
+    {
+        return -1;
+    }
+    return mbed::open_fh_as_fd<mbed::EventFileHandle>();
 }
 
 int eventfd_read(int fd, eventfd_t * value)
 {
-    return 0;
+    return read(fd, value, sizeof(*value));
 }
 
 int eventfd_write(int fd, eventfd_t value)
 {
-    return 0;
+    return write(fd, &value, sizeof(value));
 }
