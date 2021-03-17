83e9c6ce3dc41dbbd6d394b49853289273e01c20
Trevor Holbrook <tholbrook@google.com>
2021-03-17 21:31:15 +0000

    remove experimental 0x0000 value in GeneralStatusCode

--- src/protocols/secure_channel/Constants.h
+++ src/protocols/secure_channel/Constants.h
@@ -82,23 +82,23 @@ constexpr uint16_t kProtocolCodeGeneralFailure = 0xFFFF;
  */
 enum class GeneralStatusCode : uint16_t
 {
-    kSuccess           = 0x0000, /**< Operation completed successfully. */
-    kFailure           = 1,      /**< Generic failure, additional details may be included in the protocol specific status. */
-    kBadPrecondition   = 2,      /**< Operation was rejected by the system because the system is in an invalid state. */
-    kOutOfRange        = 3,      /**< A value was out of a required range. */
-    kBadRequest        = 4,      /**< A request was unrecognized or malformed. */
-    kUnsupported       = 5,      /**< An unrecognized or unsupported request was received. */
-    kUnexpected        = 6,      /**< A request was not expected at this time. */
-    kResourceExhausted = 7,      /**< Insufficient resources to process the given request. */
-    kBusy              = 8,      /**< Device is busy and cannot handle this request at this time. */
-    kTimeout           = 9,      /**< A timeout occurred. */
-    kContinue          = 10,     /**< Context-specific signal to proceed. */
-    kAborted           = 11,     /**< Failure, often due to a concurrency error. */
-    kInvalidArgument   = 12,     /**< An invalid/unsupported argument was provided. */
-    kNotFound          = 13,     /**< Some requested entity was not found. */
-    kAlreadyExists     = 14,     /**< The caller attempted to create something that already exists. */
-    kPermissionDenied  = 15,     /**< Caller does not have sufficient permissions to execute the requested operations. */
-    kDataLoss          = 16,     /**< Unrecoverable data loss or corruption has occurred. */
+    kSuccess           = 0,  /**< Operation completed successfully. */
+    kFailure           = 1,  /**< Generic failure, additional details may be included in the protocol specific status. */
+    kBadPrecondition   = 2,  /**< Operation was rejected by the system because the system is in an invalid state. */
+    kOutOfRange        = 3,  /**< A value was out of a required range. */
+    kBadRequest        = 4,  /**< A request was unrecognized or malformed. */
+    kUnsupported       = 5,  /**< An unrecognized or unsupported request was received. */
+    kUnexpected        = 6,  /**< A request was not expected at this time. */
+    kResourceExhausted = 7,  /**< Insufficient resources to process the given request. */
+    kBusy              = 8,  /**< Device is busy and cannot handle this request at this time. */
+    kTimeout           = 9,  /**< A timeout occurred. */
+    kContinue          = 10, /**< Context-specific signal to proceed. */
+    kAborted           = 11, /**< Failure, often due to a concurrency error. */
+    kInvalidArgument   = 12, /**< An invalid/unsupported argument was provided. */
+    kNotFound          = 13, /**< Some requested entity was not found. */
+    kAlreadyExists     = 14, /**< The caller attempted to create something that already exists. */
+    kPermissionDenied  = 15, /**< Caller does not have sufficient permissions to execute the requested operations. */
+    kDataLoss          = 16, /**< Unrecoverable data loss or corruption has occurred. */
 };
 
 /**
