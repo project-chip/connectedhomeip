035748435fdf4bf420a8f955c73145e3c7c052b8
Thomas Lykkeberg <tlykkeberg@grundfos.com>
2021-03-29 16:04:54 +0200

    Re-added the chip-tool generated files.

--- examples/chip-tool/gen/endpoint_config.h
+++ examples/chip-tool/gen/endpoint_config.h
@@ -149,7 +149,7 @@
     }
 
 // Largest attribute size is needed for various buffers
-#define ATTRIBUTE_LARGEST (3)
+#define ATTRIBUTE_LARGEST (2)
 
 // Total size of singleton attributes
 #define ATTRIBUTE_SINGLETONS_SIZE (2)
