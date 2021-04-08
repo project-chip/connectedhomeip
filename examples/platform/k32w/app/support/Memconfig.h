dfe2bbaeb4b97332a21f44460975552e27d262bf
PetruSicoe <petru-georgian.sicoe@nxp.com>
2021-04-08 18:13:50 +0300

    Implement malloc_usable_size() version for heap_4
    The function xPortMallocUsableSize from Memconfig.cpp
    returns the size of an allocated block and is
    compatible with heap_4.c, unlike the gcc/glibc
    function malloc_usable_size
    
    Remove unnecessary #include and #define lines
    from CHIPMem-Malloc
    
    Signed-off-by: PetruSicoe <petru-georgian.sicoe@nxp.com>

--- examples/platform/k32w/app/support/Memconfig.h
+++ examples/platform/k32w/app/support/Memconfig.h
@@ -3,10 +3,8 @@
 #ifndef SRC_PLATFORM_K32W_MEMCONFIG_H_
 #define SRC_PLATFORM_K32W_MEMCONFIG_H_
 
-namespace chip {
-namespace Platform {
-
 extern "C" {
+size_t xPortMallocUsableSize( void *pv );
 void * __wrap_malloc(size_t size);
 void __wrap_free(void * ptr);
 void * __wrap_calloc(size_t num, size_t size);
@@ -15,8 +13,5 @@ void * __wrap__malloc_r(void * REENT, size_t size);
 void __wrap__free_r(void * REENT, void * ptr);
 void * __wrap__realloc_r(void * REENT, void * ptr, size_t new_size);
 }
-
-} // namespace Platform
-} // namespace chip
 
 #endif /* SRC_PLATFORM_K32W_MEMCONFIG_H_ */
