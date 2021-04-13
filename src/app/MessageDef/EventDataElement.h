d644bb03158ed3b1dcd32096fb786705c01f02c0
yunhanw-google <yunhanw@google.com>
2021-04-13 07:26:17 -0700

    Cleanup IM message def (#5967)
    Summary of Changes:
    --Remove extra header check in all im message def headers.
    --Run im schema check only under CheckSchemaValidity in debug linux/mac build
    --Remove redundant schema check

--- src/app/MessageDef/EventDataElement.h
+++ src/app/MessageDef/EventDataElement.h
@@ -23,9 +23,6 @@
 
 #pragma once
 
-#ifndef _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_DATA_ELEMENT_H
-#define _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_DATA_ELEMENT_H
-
 #include "Builder.h"
 #include "EventPath.h"
 
@@ -63,6 +60,7 @@ public:
      */
     CHIP_ERROR Init(const chip::TLV::TLVReader & aReader);
 
+#if CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
     /**
      *  @brief Roughly verify the message is correctly formed
      *   1) all mandatory tags are present
@@ -77,6 +75,7 @@ public:
      *  @return #CHIP_NO_ERROR on success
      */
     CHIP_ERROR CheckSchemaValidity() const;
+#endif
 
     /**
      *  @brief Get a TLVReader for the EventPath. Next() must be called before accessing them.
@@ -265,5 +264,3 @@ private:
 }; // namespace EventDataElement
 }; // namespace app
 }; // namespace chip
-
-#endif // _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_DATA_ELEMENT_H
--- src/app/MessageDef/EventPath.h
+++ src/app/MessageDef/EventPath.h
@@ -23,9 +23,6 @@
 
 #pragma once
 
-#ifndef _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_PATH_H
-#define _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_PATH_H
-
 #include "Builder.h"
 #include "Parser.h"
 #include <core/CHIPCore.h>
@@ -57,6 +54,7 @@ public:
      */
     CHIP_ERROR Init(const chip::TLV::TLVReader & aReader);
 
+#if CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
     /**
      *  @brief Roughly verify the message is correctly formed
      *   1) all mandatory tags are present
@@ -71,6 +69,7 @@ public:
      *  @return #CHIP_NO_ERROR on success
      */
     CHIP_ERROR CheckSchemaValidity() const;
+#endif
 
     /**
      *  @brief Get a TLVReader for the NodeId. Next() must be called before accessing them.
@@ -189,5 +188,3 @@ private:
 }; // namespace EventPath
 }; // namespace app
 }; // namespace chip
-
-#endif // _CHIP_INTERACTION_MODEL_MESSAGE_DEF_EVENT_PATH_H
