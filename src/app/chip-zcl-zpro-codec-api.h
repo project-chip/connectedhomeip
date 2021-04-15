bb90db8e8f49eaa0f2005993ed68854a2a6e108f
Restyled.io <commits@restyled.io>
2021-04-15 22:37:41 +0000

    Restyled by whitespace

--- src/app/chip-zcl-zpro-codec-api.h
+++ src/app/chip-zcl-zpro-codec-api.h
@@ -2140,4 +2140,3 @@ chip::System::PacketBufferHandle encodeWindowCoveringClusterWriteModeAttribute(u
  *    Encode a Window Covering server read command for the cluster revision attribute into buffer including the APS frame
  */
 chip::System::PacketBufferHandle encodeWindowCoveringClusterReadClusterRevisionAttribute(uint8_t seqNum, chip::EndpointId destinationEndpoint);
-
