f5f913408cbf3efc81085baf182d47182aeb1853
Damian Królik <66667989+Damian-Nordic@users.noreply.github.com>
2021-04-15 16:29:01 +0200

    [python/dns-sd] Fix "resolve" command in Python CHIP Controller (#6041)
    After #5956 has been merged, the device controller works as
    a Resolver delegate, so no separate DeviceAddressUpdater is
    needed. In fact, they interfere which each other.
    
    Switch Python CHIP controller to using the mechanisms
    introduced by #5956, fix a couple of issues and remove no
    longer necessary code.

--- src/controller/DeviceAddressUpdateDelegate.h
+++ src/controller/DeviceAddressUpdateDelegate.h
@@ -18,14 +18,11 @@
 
 #pragma once
 
-#include <mdns/Resolver.h>
 #include <support/DLLUtil.h>
 #include <transport/raw/MessageHeader.h>
 
 namespace chip {
 namespace Controller {
-
-class DeviceController;
 
 /// Callbacks for CHIP device address resolution
 class DLL_EXPORT DeviceAddressUpdateDelegate
@@ -33,21 +30,6 @@ class DLL_EXPORT DeviceAddressUpdateDelegate
 public:
     virtual ~DeviceAddressUpdateDelegate() {}
     virtual void OnAddressUpdateComplete(NodeId nodeId, CHIP_ERROR error) = 0;
-};
-
-/// Class for updating CHIP devices' addresses based on responses from mDNS Resolver
-class DLL_EXPORT DeviceAddressUpdater : public Mdns::ResolverDelegate
-{
-public:
-    CHIP_ERROR Init(DeviceController * controller, DeviceAddressUpdateDelegate * delegate = nullptr);
-
-private:
-    // Mdns::ResolverDelegate Implementation
-    void OnNodeIdResolved(NodeId nodeId, const Mdns::ResolvedNodeData & nodeData) override;
-    void OnNodeIdResolutionFailed(NodeId nodeId, CHIP_ERROR error) override;
-
-    DeviceController * mController          = nullptr;
-    DeviceAddressUpdateDelegate * mDelegate = nullptr;
 };
 
 } // namespace Controller
