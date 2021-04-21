c9464fc64a3393c763290fc98bd0d6c754e9920a
Lukasz Gniadzik <lukasz.gniadzik@mobica.com>
2021-04-21 11:31:19 +0200

    Mbed: Add conditional compilation for STM32L4 target in SystemConfig.h
    Due to conflict between 'SUCCESS' macro and 'ErrorStatus' enum values
    from stm32l4xx.h, undefining 'SUCCESS' has been guarded by
    'ifdef TARGET_MCU_STM32L4' condition, just to be sure that change will
    not interact with any other mbed targets.

--- src/system/SystemConfig.h
+++ src/system/SystemConfig.h
@@ -151,11 +151,13 @@
 #endif /* CHIP_SYSTEM_CONFIG_ERROR_TYPE */
 
 #if CHIP_SYSTEM_CONFIG_USE_LWIP
+#ifdef TARGET_MCU_STM32L4
 // [ MBED HACK ] 
 // We have to undefine |SUCCESS| here to prevent compilation error due to 
 // conflict with |ErrorStatus| enum type values defined in CMSIS/stm32l4xx.h.
 // This problem is only related for when tests are build and nlunit-test.h is used. 
 #undef SUCCESS
+#endif
 #include <lwip/opt.h>
 #endif // CHIP_SYSTEM_CONFIG_USE_LWIP
 
