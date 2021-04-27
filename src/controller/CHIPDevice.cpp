3f3630ab0507e7ec61b8869e297c4fdc14dc3f6d
Song Guo <songguo@google.com>
2021-04-27 16:38:43 +0800

    Add DeviceControllerIMDelegate to make current applications work

--- src/controller/CHIPDevice.cpp
+++ src/controller/CHIPDevice.cpp
@@ -398,6 +398,23 @@ void Device::CancelResponseHandler(uint8_t seqNum)
     mCallbacksMgr.CancelResponseCallback(mDeviceId, seqNum);
 }
 
+void Device::AddIMResponseHandler(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback)
+{
+    // This is a bit tricky, we try to assume that chip::NodeId is uint64_t so the pointer can be used as a NodeId for CallbackMgr.
+    static_assert(std::is_same<chip::NodeId, uint64_t>::value, "chip::NodeId is not uint64_t");
+    chip::NodeId transactionId = reinterpret_cast<chip::NodeId>(static_cast<app::Command *>(mCommandSender));
+    mCallbacksMgr.AddResponseCallback(transactionId, 0 /* seqNum, always 0 for IM before #6559 */, onSuccessCallback,
+                                      onFailureCallback);
+}
+
+void Device::CancelIMResponseHandler()
+{
+    // This is a bit tricky, we try to assume that chip::NodeId is uint64_t so the pointer can be used as a NodeId for CallbackMgr.
+    static_assert(std::is_same<chip::NodeId, uint64_t>::value, "chip::NodeId is not uint64_t");
+    chip::NodeId transactionId = reinterpret_cast<chip::NodeId>(static_cast<app::Command *>(mCommandSender));
+    mCallbacksMgr.CancelResponseCallback(transactionId, 0 /* seqNum, always 0 for IM before #6559 */);
+}
+
 void Device::AddReportHandler(EndpointId endpoint, ClusterId cluster, AttributeId attribute,
                               Callback::Cancelable * onReportCallback)
 {
