fcc9c603c7be72ed5c002d497c640f353672ef04
Andrei Litvin <andy314@gmail.com>
2021-04-28 17:05:32 -0400

    Add back KVS operation messages as progress (rather than erro like they were during debugging)

--- src/controller/java/AndroidDeviceControllerWrapper.cpp
+++ src/controller/java/AndroidDeviceControllerWrapper.cpp
@@ -274,6 +274,8 @@ void AndroidDeviceControllerWrapper::OnStatusChange(void) {}
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncGetKeyValue(const char * key, void * value, uint16_t & size)
 {
+    ChipLogProgress(chipTool, "KVS: Getting key %s", key);
+
     size_t read_size = 0;
 
     CHIP_ERROR err = chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr().Get(key, value, size, &read_size);
@@ -285,10 +287,12 @@ CHIP_ERROR AndroidDeviceControllerWrapper::SyncGetKeyValue(const char * key, voi
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncSetKeyValue(const char * key, const void * value, uint16_t size)
 {
+    ChipLogProgress(chipTool, "KVS: Setting key %s", key);
     return chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr().Put(key, value, size);
 }
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncDeleteKeyValue(const char * key)
 {
+    ChipLogProgress(chipTool, "KVS: Deleting key %s", key);
     return chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr().Delete(key);
 }
