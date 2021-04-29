e9739a0b6989d21751a9f359325bca2590ae8a15
Vivien Nicolas <vnicolas@apple.com>
2021-04-29 15:46:46 +0200

    Enable optional Basic Cluster attributes and Pump Configuration And Control cluster into the all-clusters-app

--- examples/all-clusters-app/all-clusters-common/gen/callback-stub.cpp
+++ examples/all-clusters-app/all-clusters-common/gen/callback-stub.cpp
@@ -86,6 +86,9 @@ void emberAfClusterInitCallback(EndpointId endpoint, ClusterId clusterId)
     case ZCL_OPERATIONAL_CREDENTIALS_CLUSTER_ID:
         emberAfOperationalCredentialsClusterInitCallback(endpoint);
         break;
+    case ZCL_PUMP_CONFIG_CONTROL_CLUSTER_ID:
+        emberAfPumpConfigurationAndControlClusterInitCallback(endpoint);
+        break;
     case ZCL_SCENES_CLUSTER_ID:
         emberAfScenesClusterInitCallback(endpoint);
         break;
@@ -195,6 +198,11 @@ void __attribute__((weak)) emberAfOnOffClusterInitCallback(EndpointId endpoint)
     (void) endpoint;
 }
 void __attribute__((weak)) emberAfOperationalCredentialsClusterInitCallback(EndpointId endpoint)
+{
+    // To prevent warning
+    (void) endpoint;
+}
+void __attribute__((weak)) emberAfPumpConfigurationAndControlClusterInitCallback(EndpointId endpoint)
 {
     // To prevent warning
     (void) endpoint;
