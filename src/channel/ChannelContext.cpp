18730322638490bfbacfb0f1952de28fce48fb49
Yufeng Wang <yufengwang@google.com>
2021-05-03 12:40:15 -0700

    remove extra return check.

--- src/channel/ChannelContext.cpp
+++ src/channel/ChannelContext.cpp
@@ -260,7 +260,6 @@ CHIP_ERROR ChannelContext::SendSessionEstablishmentMessage(const PacketHeader & 
     CHIP_ERROR err = CHIP_NO_ERROR;
 
     ReturnErrorOnFailure(header.EncodeBeforeData(msgIn));
-    ReturnErrorOnFailure(err);
 
     err = mExchangeManager->GetSessionMgr()->GetTransportManager()->SendMessage(peerAddress, std::move(msgIn));
     ReturnErrorOnFailure(err);
