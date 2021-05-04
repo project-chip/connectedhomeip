074dbee90a888ab0d379e99c185ff849464a4012
Yufeng Wang <yufengwang@google.com>
2021-05-04 13:39:06 -0700

    Update src/transport/SecureSessionMgr.h
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/transport/SecureSessionMgr.h
+++ src/transport/SecureSessionMgr.h
@@ -318,7 +318,7 @@ protected:
      *   Handle received secure message. Implements TransportMgrDelegate
      *
      * @param source    the source address of the package
-     * @param msgBuf    the buffer of (encrypted) payload
+     * @param msgBuf    the buffer containing a full CHIP message (except for the optional length field).
      */
     void OnMessageReceived(const Transport::PeerAddress & source, System::PacketBufferHandle msgBuf) override;
 
