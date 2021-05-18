32425b5171e631aa9d4686cda6041584273b3776
Yufeng Wang <yufengwang@google.com>
2021-05-17 23:00:36 -0700

    Add initial Software Diagnostics Cluster definition

--- src/darwin/Framework/CHIP/templates/helper.js
+++ src/darwin/Framework/CHIP/templates/helper.js
@@ -32,6 +32,7 @@ function asExpectedEndpointForCluster(clusterName)
   case 'Descriptor':
   case 'GeneralCommissioning':
   case 'GeneralDiagnostics':
+  case 'SoftwareDiagnostics':  
   case 'EthernetNetworkDiagnostics':
   case 'GroupKeyManagement':
   case 'NetworkCommissioning':
