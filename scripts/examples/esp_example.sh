d507b6ca4af10a5af34987a834048d8b88ac4c06
Switi Mhaiske <switi.mhaiske@espressif.com>
2021-05-21 13:46:32 +0530

    ESP32: Deprecate make support

--- scripts/examples/esp_example.sh
+++ scripts/examples/esp_example.sh
@@ -20,7 +20,30 @@ set -x
 env
 
 app="$1"
-root=examples/$app/esp32/
+root=examples/$app/esp32
+
+IDF_REPO_URL="https://github.com/espressif/esp-idf.git"
+IDF_BRANCH="release/v4.2"
+
+#
+# CLONE ESP-IDF
+#
+
+if [ -z "$IDF_PATH" ]; then
+    echo "ESP-IDF is not installed! Installing local copy"
+    idf_was_installed="1"
+    if ! [ -d esp-idf ]; then
+        git clone "$IDF_REPO_URL" -b "$IDF_BRANCH"
+    fi
+    export IDF_PATH="$PWD/esp-idf"
+fi
+
+if [ -x $idf_was_installed ]; then
+    git -C "$IDF_PATH" submodule update --init --recursive
+    cd "$IDF_PATH"
+    ./install.sh
+    . ./export.sh
+fi
 
 shift 1
 
@@ -29,18 +52,20 @@ if [ -z "$app" ]; then
     exit 1
 fi
 
-source "scripts/activate.sh"
+source "../activate.sh"
 # shellcheck source=/dev/null
-source "$root"/idf.sh
 
 for sdkconfig in "$root"/sdkconfig*.defaults; do
     # remove root path to get sdkconfig*.defaults name
     sdkconfig_name=${sdkconfig#"$root"/}
     rm -f "$root"/sdkconfig
-    SDKCONFIG_DEFAULTS=$sdkconfig_name idf make -j8 -C "$root" defconfig "$@"
-    idf make -j8 -C "$root" "$@" || {
+    SDKCONFIG_DEFAULTS=$sdkconfig_name idf.py -C "$root" defconfig "$@"
+    cd "$root"
+    idf.py build "$@" || {
+        cd -
         echo "build $sdkconfig_name failed"
         exit 1
     }
+    cd -
     cp "$root"/build/chip-"$app".elf "$root"/build/"${sdkconfig_name%".defaults"}"-chip-"$app".elf
 done
