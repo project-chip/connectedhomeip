4afbe71e67e7f590a809f18c93dfa78c27ae919b
Lukasz Gniadzik <lukasz.gniadzik@mobica.com>
2021-05-25 17:33:03 +0200

    Mbed: all-clusters-app: Add directory and source files.
    The all-clusters-app works in very symiliar way to mbed lock and
    lighting app ports. The only difference here is that all-clusters-app
    example supports all ZCL clusters by defining general stubs in
    all-clusters-common directory.
    
    Because this example will be used mainly for testing ZCL clusters
    support, there is no need for handling user buttons in the same way how
    it is done in lock/lighting app examples.
    
    However, status led functionality has been left unchanged - just for
    easy indicationt about connection status.

---
+++ examples/all-clusters-app/mbed/main/AppTask.cpp
@@ -0,0 +1,141 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "AppTask.h"
+#include "LEDWidget.h"
+#include "OnboardingCodesUtil.h"
+
+// FIXME: Undefine the `sleep()` function included by the CHIPDeviceLayer.h
+// from unistd.h to avoid a conflicting declaration with the `sleep()` provided
+// by Mbed-OS in mbed_power_mgmt.h.
+#define sleep unistd_sleep
+#include "Server.h"
+#include <platform/CHIPDeviceLayer.h>
+#undef sleep
+
+#include <support/logging/CHIPLogging.h>
+
+// mbed-os headers
+#include "platform/Callback.h"
+
+static LEDWidget sStatusLED(MBED_CONF_APP_SYSTEM_STATE_LED);
+
+static bool sIsWiFiStationProvisioned = false;
+static bool sIsWiFiStationEnabled     = false;
+static bool sIsWiFiStationConnected   = false;
+static bool sIsPairedToAccount        = false;
+static bool sHaveBLEConnections       = false;
+static bool sHaveServiceConnectivity  = false;
+
+// TODO: change EventQueue default event size
+static events::EventQueue sAppEventQueue;
+
+using namespace ::chip::DeviceLayer;
+
+AppTask AppTask::sAppTask;
+
+int AppTask::Init()
+{
+    // Start BLE advertising if needed
+    if (!CHIP_DEVICE_CONFIG_CHIPOBLE_ENABLE_ADVERTISING_AUTOSTART)
+    {
+        ChipLogProgress(NotSpecified, "Enabling BLE advertising.");
+        ConnectivityMgr().SetBLEAdvertisingEnabled(true);
+    }
+#ifdef MBED_CONF_APP_DEVICE_NAME
+    ConnectivityMgr().SetBLEDeviceName(MBED_CONF_APP_DEVICE_NAME);
+#endif
+
+    chip::DeviceLayer::ConnectivityMgrImpl().StartWiFiManagement();
+
+    // Init ZCL Data Model and start server
+    InitServer();
+    ConfigurationMgr().LogDeviceConfig();
+    // QR code will be used with CHIP Tool
+    PrintOnboardingCodes(chip::RendezvousInformationFlags(chip::RendezvousInformationFlag::kBLE));
+
+    return 0;
+}
+
+int AppTask::StartApp()
+{
+    int ret = Init();
+
+    if (ret)
+    {
+        ChipLogError(NotSpecified, "AppTask.Init() failed");
+        return ret;
+    }
+
+    while (true)
+    {
+        sAppEventQueue.dispatch(100);
+
+        // Collect connectivity and configuration state from the CHIP stack.  Because the
+        // CHIP event loop is being run in a separate task, the stack must be locked
+        // while these values are queried.  However we use a non-blocking lock request
+        // (TryLockChipStack()) to avoid blocking other UI activities when the CHIP
+        // task is busy (e.g. with a long crypto operation).
+
+        if (PlatformMgr().TryLockChipStack())
+        {
+            sIsWiFiStationProvisioned = ConnectivityMgr().IsWiFiStationProvisioned();
+            sIsWiFiStationEnabled     = ConnectivityMgr().IsWiFiStationEnabled();
+            sIsWiFiStationConnected   = ConnectivityMgr().IsWiFiStationConnected();
+            sHaveBLEConnections       = (ConnectivityMgr().NumBLEConnections() != 0);
+            sHaveServiceConnectivity  = ConnectivityMgr().HaveServiceConnectivity();
+            PlatformMgr().UnlockChipStack();
+        }
+
+        // Consider the system to be "fully connected" if it has service
+        // connectivity and it is able to interact with the service on a regular basis.
+        bool isFullyConnected = sHaveServiceConnectivity;
+
+        // Update the status LED
+        //
+        // If system has "full connectivity", keep the LED On constantly.
+        //
+        // If thread and service provisioned, but not attached to the thread network yet OR no
+        // connectivity to the service OR subscriptions are not fully established
+        // THEN blink the LED Off for a short period of time.
+        //
+        // If the system has ble connection(s) uptill the stage above, THEN blink the LEDs at an even
+        // rate of 100ms.
+        //
+        // Otherwise, blink the LED ON for a very short time.
+        if (isFullyConnected)
+        {
+            sStatusLED.Set(true);
+        }
+        else if (sIsWiFiStationProvisioned && sIsWiFiStationEnabled && sIsPairedToAccount &&
+                 (!sIsWiFiStationConnected || !isFullyConnected))
+        {
+            sStatusLED.Blink(950, 50);
+        }
+        else if (sHaveBLEConnections)
+        {
+            sStatusLED.Blink(100, 100);
+        }
+        else
+        {
+            sStatusLED.Blink(50, 950);
+        }
+
+        sStatusLED.Animate();
+    }
+}
---
+++ examples/all-clusters-app/mbed/main/ZclCallbacks.cpp
@@ -0,0 +1,41 @@
+/*
+ *
+ *    Copyright (c) 2020-2021 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "af.h"
+#include "gen/attribute-id.h"
+#include "gen/cluster-id.h"
+#include <app/chip-zcl-zpro-codec.h>
+#include <app/server/Mdns.h>
+#include <app/util/af-types.h>
+#include <app/util/attribute-storage.h>
+#include <app/util/util.h>
+#include <core/CHIPError.h>
+#include <support/CHIPMem.h>
+#include <support/RandUtils.h>
+
+using namespace ::chip;
+
+void emberAfPostAttributeChangeCallback(EndpointId endpoint, ClusterId clusterId, AttributeId attributeId, uint8_t mask,
+                                        uint16_t manufacturerCode, uint8_t type, uint8_t size, uint8_t * value)
+{}
+
+bool emberAfBasicClusterMfgSpecificPingCallback(void)
+{
+    emberAfSendDefaultResponse(emberAfCurrentCommand(), EMBER_ZCL_STATUS_SUCCESS);
+    return true;
+}
