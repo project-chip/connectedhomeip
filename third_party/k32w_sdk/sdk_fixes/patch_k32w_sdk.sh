b5c0c19ff1f74344e82e8d474302a4fe14d7a985
Doru Gucea <doru-cristian.gucea@nxp.com>
2021-05-27 06:11:34 -0700

    [K32W] Save PDM to external flash on the DK6 board
    This will free up 32.5K of internal flash space.
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

--- third_party/k32w_sdk/sdk_fixes/patch_k32w_sdk.sh
+++ third_party/k32w_sdk/sdk_fixes/patch_k32w_sdk.sh
@@ -12,6 +12,7 @@ cp ./third_party/k32w_sdk/sdk_fixes/pin_mux.c "$K32W061_SDK_ROOT"/boards/k32w061
 cp ./third_party/k32w_sdk/sdk_fixes/ntag_driver.h "$K32W061_SDK_ROOT"/middleware/ntag_i2c_plus/HAL_NTAG/inc/
 cp ./third_party/k32w_sdk/sdk_fixes/HAL_I2C_jn_fsl.h "$K32W061_SDK_ROOT"/middleware/ntag_i2c_plus/HAL_I2C/inc/
 cp ./third_party/k32w_sdk/sdk_fixes/SerialManager.c "$K32W061_SDK_ROOT"/middleware/wireless/framework/SerialManager/Source/
+cp ./third_party/k32w_sdk/sdk_fixes/Eeprom_MX25R8035F.c "$K32W061_SDK_ROOT"/middleware/wireless/framework/Flash/External/Source
 cp ./third_party/k32w_sdk/sdk_fixes/system.c ./third_party/openthread/ot-nxp/src/k32w/platform/
 cp ./third_party/k32w_sdk/sdk_fixes/openthread-core-k32w061-config.h ./third_party/openthread/ot-nxp/src/k32w/k32w061
 
