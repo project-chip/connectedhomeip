18e34faadeeabb7b3015df4ff9a24adc894ecf33
Shana Azria <sazria@apple.com>
2021-05-27 19:02:59 +0200

    switch from 14 to 17

--- config/nrfconnect/app/enable-gnu-std.cmake
+++ config/nrfconnect/app/enable-gnu-std.cmake
@@ -1,4 +1,3 @@
-add_library(gnu14 INTERFACE)
-target_compile_options(gnu14 INTERFACE -std=gnu++14 -D_SYS__PTHREADTYPES_H_)
-target_link_libraries(app PRIVATE gnu14)
-
+add_library(gnu17 INTERFACE)
+target_compile_options(gnu17 INTERFACE -std=gnu++17 -D_SYS__PTHREADTYPES_H_)
+target_link_libraries(app PRIVATE gnu17)
