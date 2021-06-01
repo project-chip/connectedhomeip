94afb011dacd6e708ee9c033276b286178440331
Ritika Nanda <ritikananda@google.com>
2021-06-01 12:59:07 -0700

    Extracting OpCSR to be used to generate the OpCert

--- src/controller/java/AndroidDeviceControllerWrapper.h
+++ src/controller/java/AndroidDeviceControllerWrapper.h
@@ -53,8 +53,8 @@ public:
     void OnPairingDeleted(CHIP_ERROR error) override;
 
     // OperationalCredentialsDelegate implementation
-    CHIP_ERROR GenerateNodeOperationalCertificate(const PeerId & peerId, const ByteSpan & csr, int64_t serialNumber,
-                                                      uint8_t * certBuf, uint32_t certBufSize, uint32_t & outCertLen) override;
+    GenerateNodeOperationalCertificate(const PeerId & peerId, const ByteSpan & csr, int64_t serialNumber,
+                                                              uint8_t * certBuf, uint32_t certBufSize, uint32_t & outCertLen)
 
 
 
