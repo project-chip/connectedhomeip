38803be1925e19cfd78f00d8241f5e775a354e1b
Restyled.io <commits@restyled.io>
2021-06-03 04:33:32 +0000

    Restyled by whitespace

--- src/app/clusters/scenes/scenes.cpp
+++ src/app/clusters/scenes/scenes.cpp
@@ -500,7 +500,7 @@ bool emberAfScenesClusterGetSceneMembershipCallback(chip::app::Command * command
         SuccessOrExit(err = commandObj->PrepareCommand(&cmdParams));
         VerifyOrExit((writer = commandObj->GetCommandDataElementTLVWriter()) != nullptr, err = CHIP_ERROR_INCORRECT_STATE);
         SuccessOrExit(err = writer->Put(TLV::ContextTag(0), static_cast<int32_t>(status)));
-        SuccessOrExit(err = writer->Put(TLV::ContextTag(1), 
+        SuccessOrExit(err = writer->Put(TLV::ContextTag(1),
                                         static_cast<int32_t>(EMBER_AF_PLUGIN_SCENES_TABLE_SIZE -
                                                              emberAfPluginScenesServerNumSceneEntriesInUse())));
         SuccessOrExit(err = writer->Put(TLV::ContextTag(2), static_cast<int32_t>(groupId)));
