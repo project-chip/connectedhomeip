2706ac451e0c03c26961d39e288d8471c0d2be76
Seth Rickard <s-rickard@ti.com>
2021-06-09 21:54:59 +0000

    Implement parallel key value storage
    Create a key itemID and a value itemID within the config module. The subIDs of
    each of these are kept in synch to allow arbitrary key creation.

--- src/platform/cc13x2_26x2/KeyValueStoreManagerImpl.cpp
+++ src/platform/cc13x2_26x2/KeyValueStoreManagerImpl.cpp
@@ -38,65 +38,13 @@ using namespace ::chip::DeviceLayer::Internal;
 
 KeyValueStoreManagerImpl KeyValueStoreManagerImpl::sInstance;
 
-/**
- * Calculate crc16 CCITT from the c string key.
- *
- * Code generated with the following commands. Output was modified to change
- * the type `crc_t` to `uint16_t`.
- *
- * ```
- * $ git clone https://github.com/tpircher/pycrc
- * $ python3 pycrc/pycrc.py --generate c --algorithm=bbf --model=crc-16-ccitt
- * ```
- *
- * Generated on Wed Jun  9 17:09:19 2021
- * by pycrc v0.9.2, https://pycrc.org
- * using the configuration:
- *  - Width         = 16
- *  - Poly          = 0x1021
- *  - XorIn         = 0x1d0f
- *  - ReflectIn     = False
- *  - XorOut        = 0x0000
- *  - ReflectOut    = False
- *  - Algorithm     = bit-by-bit-fast
- */
-uint16_t crc_update(uint16_t crc, const void * data, size_t data_len)
-{
-    const unsigned char * d = (const unsigned char *) data;
-    unsigned int i;
-    bool bit;
-    unsigned char c;
-
-    while (data_len--)
-    {
-        c = *d++;
-        for (i = 0x80; i > 0; i >>= 1)
-        {
-            bit = crc & 0x8000;
-            if (c & i)
-            {
-                bit = !bit;
-            }
-            crc <<= 1;
-            if (bit)
-            {
-                crc ^= 0x1021;
-            }
-        }
-        crc &= 0xffff;
-    }
-    return crc & 0xffff;
-}
-
-#define CRC_INITIAL (0xFFFF)
-
 CHIP_ERROR KeyValueStoreManagerImpl::_Get(const char * key, void * value, size_t value_size, size_t * read_bytes_size,
                                           size_t offset_bytes) const
 {
     VerifyOrReturnError(key, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(value, CHIP_ERROR_INVALID_ARGUMENT);
 
-    return CC13X2_26X2Config::ReadKVS(crc_update(CRC_INITIAL, key, strlen(key)), value, value_size, read_bytes_size, offset_bytes);
+    return CC13X2_26X2Config::ReadKVS(key, value, value_size, read_bytes_size, offset_bytes);
 }
 
 CHIP_ERROR KeyValueStoreManagerImpl::_Put(const char * key, const void * value, size_t value_size)
@@ -105,12 +53,12 @@ CHIP_ERROR KeyValueStoreManagerImpl::_Put(const char * key, const void * value, 
     VerifyOrReturnError(value, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(value_size > 0, CHIP_ERROR_INVALID_ARGUMENT);
 
-    return CC13X2_26X2Config::WriteKVS(crc_update(CRC_INITIAL, key, strlen(key)), value, value_size);
+    return CC13X2_26X2Config::WriteKVS(key, value, value_size);
 }
 
 CHIP_ERROR KeyValueStoreManagerImpl::_Delete(const char * key)
 {
-    return CC13X2_26X2Config::ClearKVS(crc_update(CRC_INITIAL, key, strlen(key)));
+    return CC13X2_26X2Config::ClearKVS(key);
 }
 
 } // namespace PersistedStorage
