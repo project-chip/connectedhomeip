18597e0d55b2796364f5cbca3cfd64eba69142fb
Boris Zbarsky <bzbarsky@apple.com>
2021-06-14 12:34:56 -0400

    Fix tv-app build on darwin
    This was broken by https://github.com/project-chip/connectedhomeip/pull/7266

--- examples/tv-app/linux/include/keypad-input/KeypadInputManager.cpp
+++ examples/tv-app/linux/include/keypad-input/KeypadInputManager.cpp
@@ -51,10 +51,13 @@ EmberAfKeypadInputStatus KeypadInputManager::proxyKeypadInputRequest(EmberAfKeyp
     return EMBER_ZCL_KEYPAD_INPUT_STATUS_SUCCESS;
 }
 
-static void sendResponse(const char * responseName, uint8_t keypadInputStatus)
+static void sendResponse(const char * responseName, EmberAfKeypadInputStatus keypadInputStatus)
 {
+    // TODO: Once our enums are sized properly, or once we stop depending on the
+    // value being a certain type, we can remove the static_cast.  For now the
+    // cast is safe because all EmberAfKeypadInputStatus values fit in uint32_t.
     emberAfFillExternalBuffer((ZCL_CLUSTER_SPECIFIC_COMMAND | ZCL_FRAME_CONTROL_SERVER_TO_CLIENT), ZCL_KEYPAD_INPUT_CLUSTER_ID,
-                              ZCL_SEND_KEY_RESPONSE_COMMAND_ID, "u", keypadInputStatus);
+                              ZCL_SEND_KEY_RESPONSE_COMMAND_ID, "u", static_cast<uint8_t>(keypadInputStatus));
 
     EmberStatus status = emberAfSendResponse();
     if (status != EMBER_SUCCESS)
