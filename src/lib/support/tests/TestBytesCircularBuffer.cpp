933aa80060c021eb2619083768bdf1b4daf3da1f
Zang MingJie <zealot0630@gmail.com>
2021-06-15 19:04:44 +0800

    Resolve comments

--- src/lib/support/tests/TestBytesCircularBuffer.cpp
+++ src/lib/support/tests/TestBytesCircularBuffer.cpp
@@ -18,7 +18,7 @@
 
 #include <string.h>
 
-#include <support/StringCircularBuffer.h>
+#include <support/BytesCircularBuffer.h>
 #include <support/UnitTestRegistration.h>
 
 #include <nlunit-test.h>
@@ -30,10 +30,11 @@ using namespace chip;
 void TestPushPop(nlTestSuite * inSuite, void * inContext)
 {
     uint8_t storage[10];
-    const unsigned char s[] = { 'T', 'e', 's', 't' };
-    unsigned char o[sizeof(s)];
+    const uint8_t s[] = { 'T', 'e', 's', 't' };
+    uint8_t o[sizeof(s)];
+    MutableByteSpan readSpan(o);
 
-    StringCircularBuffer buffer(storage, sizeof(storage));
+    BytesCircularBuffer buffer(storage, sizeof(storage));
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
 
     // Push a string
@@ -41,8 +42,8 @@ void TestPushPop(nlTestSuite * inSuite, void * inContext)
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
 
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == sizeof(s));
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, memcmp(s, o, sizeof(s)) == 0);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s, readSpan.data(), sizeof(s)) == 0);
 
     NL_TEST_ASSERT(inSuite, buffer.Pop() == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
@@ -52,7 +53,8 @@ void TestPushPop(nlTestSuite * inSuite, void * inContext)
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
 
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 0);
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, readSpan.size() == 0);
 
     NL_TEST_ASSERT(inSuite, buffer.Pop() == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
@@ -61,9 +63,13 @@ void TestPushPop(nlTestSuite * inSuite, void * inContext)
 void TestPushInvalid(nlTestSuite * inSuite, void * inContext)
 {
     uint8_t storage[10];
-    StringCircularBuffer buffer(storage, sizeof(storage));
-    const unsigned char s[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    BytesCircularBuffer buffer(storage, sizeof(storage));
+    const uint8_t s[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
+
+    NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s, static_cast<size_t>(-1))) == CHIP_ERROR_INVALID_ARGUMENT);
+    NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
+
     NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s, 8)) == CHIP_ERROR_INVALID_ARGUMENT);
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
     NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s, 9)) == CHIP_ERROR_INVALID_ARGUMENT);
@@ -75,41 +81,83 @@ void TestPushInvalid(nlTestSuite * inSuite, void * inContext)
 void TestPushWrap(nlTestSuite * inSuite, void * inContext)
 {
     uint8_t storage[10];
-    const unsigned char s1[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-    const unsigned char s2[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
-    unsigned char o[sizeof(s1)];
+    const uint8_t s1[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    const uint8_t s2[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+    uint8_t o[sizeof(s1)];
+    MutableByteSpan readSpan(o);
 
     // Total space = 9
-    StringCircularBuffer buffer(storage, sizeof(storage));
+    BytesCircularBuffer buffer(storage, sizeof(storage));
     NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
 
     // Used = 6, Free = 3
     NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s1, 4)) == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 4);
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, memcmp(s1, o, 4) == 0);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s1, readSpan.data(), 4) == 0);
 
     // Used = 6, Free = 3, s1 was discarded due to lack of storage
     NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s2, 4)) == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 4);
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, memcmp(s2, o, 4) == 0);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s2, readSpan.data(), 4) == 0);
 
     // Used = 9, Free = 0
     NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s1, 1)) == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 4);
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, memcmp(s2, o, 4) == 0);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s2, readSpan.data(), 4) == 0);
 
     // Used = 3, Free = 6
     NL_TEST_ASSERT(inSuite, buffer.Pop() == CHIP_NO_ERROR);
     NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
     NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 1);
-    NL_TEST_ASSERT(inSuite, buffer.ReadFront(o) == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, memcmp(s1, o, 1) == 0);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s1, readSpan.data(), 1) == 0);
+}
+
+void TestPushWrapAtEdge(nlTestSuite * inSuite, void * inContext)
+{
+    uint8_t storage[10];
+    const uint8_t s1[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    const uint8_t s2[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+    uint8_t o[sizeof(s1)];
+    MutableByteSpan readSpan(o);
+
+    // Total space = 9
+    BytesCircularBuffer buffer(storage, sizeof(storage));
+    NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
+
+    // Used = 6, Free = 3
+    NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s1, 4)) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
+    NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 4);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s1, readSpan.data(), 4) == 0);
+
+    // Used = 9, Free = 0, mDataEnd wrap to 0
+    NL_TEST_ASSERT(inSuite, buffer.Push(ByteSpan(s2, 1)) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
+    NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 4);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s1, readSpan.data(), 4) == 0);
+
+    // Used = 3, Free = 6
+    NL_TEST_ASSERT(inSuite, buffer.Pop() == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, !buffer.IsEmpty());
+    NL_TEST_ASSERT(inSuite, buffer.GetFrontSize() == 1);
+    NL_TEST_ASSERT(inSuite, buffer.ReadFront(readSpan) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, memcmp(s2, readSpan.data(), 1) == 0);
+
+    // All freed
+    NL_TEST_ASSERT(inSuite, buffer.Pop() == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite, buffer.IsEmpty());
+
+    // Try normal push/pop again
+    TestPushWrap(inSuite, inContext);
 }
 
 int Setup(void * inContext)
@@ -129,15 +177,15 @@ int Teardown(void * inContext)
  *   Test Suite. It lists all the test functions.
  */
 static const nlTest sTests[] = { NL_TEST_DEF_FN(TestPushPop), NL_TEST_DEF_FN(TestPushInvalid), NL_TEST_DEF_FN(TestPushWrap),
-                                 NL_TEST_SENTINEL() };
+                                 NL_TEST_DEF_FN(TestPushWrapAtEdge), NL_TEST_SENTINEL() };
 
-int TestStringCircularBuffer()
+int TestBytesCircularBuffer()
 {
-    nlTestSuite theSuite = { "CHIP StringCircularBuffer tests", &sTests[0], Setup, Teardown };
+    nlTestSuite theSuite = { "CHIP BytesCircularBuffer tests", &sTests[0], Setup, Teardown };
 
     // Run test suit againt one context.
     nlTestRunner(&theSuite, nullptr);
     return nlTestRunnerStats(&theSuite);
 }
 
-CHIP_REGISTER_TEST_SUITE(TestStringCircularBuffer);
+CHIP_REGISTER_TEST_SUITE(TestBytesCircularBuffer);
