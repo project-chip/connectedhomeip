f809671465dc8b49b7cd6198277d8d002c7eaad3
sujaygkulkarni <sujayg.kulkarni@nxp.com>
2021-06-23 23:10:26 +0530

    Added logs

--- src/crypto/hsm/nxp/CHIPCryptoPALHsm_SE05X_Spake2p.cpp
+++ src/crypto/hsm/nxp/CHIPCryptoPALHsm_SE05X_Spake2p.cpp
@@ -150,6 +150,8 @@ CHIP_ERROR Spake2p_ComputeRoundOne_HSM(hsm_pake_context_t * phsm_pake_context, c
 #endif
     SE05x_CryptoObjectID_t spakeObjectId = phsm_pake_context->spake_objId;
 
+    ChipLogProgress(Crypto, "Using HSM for spake ComputeRoundOne \n");
+
     VerifyOrReturnError(out != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(out_len != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
     if (pab_len > 0)
@@ -207,6 +209,8 @@ CHIP_ERROR Spake2p_ComputeRoundTwo_HSM(hsm_pake_context_t * phsm_pake_context, c
     VerifyOrReturnError(pKeyKe != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(pkeyKeLen != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
 
+    ChipLogProgress(Crypto, "Using HSM for spake ComputeRoundTwo \n");
+
     const uint8_t * const pab = (role == chip::Crypto::CHIP_SPAKE2P_ROLE::VERIFIER) ? NULL : in;
     const size_t pab_len      = (role == chip::Crypto::CHIP_SPAKE2P_ROLE::VERIFIER) ? 0 : in_len;
 
@@ -226,6 +230,8 @@ CHIP_ERROR Spake2p_KeyConfirm_HSM(hsm_pake_context_t * phsm_pake_context, chip::
 {
     VerifyOrReturnError(in != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(gex_sss_chip_ctx.ks.session != NULL, CHIP_ERROR_INTERNAL);
+
+    ChipLogProgress(Crypto, "Using HSM for spake KeyConfirm \n");
 
     uint8_t presult                            = 0;
     const SE05x_CryptoObjectID_t spakeObjectId = phsm_pake_context->spake_objId;
@@ -297,6 +303,8 @@ CHIP_ERROR Spake2pHSM_P256_SHA256_HKDF_HMAC::BeginVerifier(const uint8_t * my_id
     {
         VerifyOrReturnError(peer_identity != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
     }
+
+    ChipLogProgress(Crypto, "HSM - BeginVerifier \n");
 
     error = FELoad(w0in, w0in_len, w0);
     VerifyOrReturnError(error == CHIP_NO_ERROR, CHIP_ERROR_INTERNAL);
@@ -375,6 +383,8 @@ CHIP_ERROR Spake2pHSM_P256_SHA256_HKDF_HMAC::BeginProver(const uint8_t * my_iden
     }
 
     VerifyOrReturnError(state == CHIP_SPAKE2P_STATE::INIT, CHIP_ERROR_INTERNAL);
+
+    ChipLogProgress(Crypto, "HSM - BeginProver \n");
 
     error = FELoad(w0in, w0in_len, w0);
     VerifyOrReturnError(error == CHIP_NO_ERROR, CHIP_ERROR_INTERNAL);
