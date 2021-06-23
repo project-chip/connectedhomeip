ceff3d7252b6666ab58209b869d39311a07287fd
Boris Zbarsky <bzbarsky@apple.com>
2021-06-23 17:18:18 -0400

    Fix Darwin implementation of RunEventLoop to return when StopEventLoopTask is called. (#7854)
    Co-authored-by: Jerry Johns <johnsj@google.com>

--- src/include/platform/PlatformManager.h
+++ src/include/platform/PlatformManager.h
@@ -85,12 +85,63 @@ public:
 
     typedef void (*EventHandlerFunct)(const ChipDeviceEvent * event, intptr_t arg);
 
+    /**
+     * InitChipStack() initializes the PlatformManager.  After calling that, a
+     * consumer is allowed to call either StartEventLoopTask or RunEventLoop to
+     * process pending work.  Calling both is not allowed: it must be one or the
+     * other.
+     */
     CHIP_ERROR InitChipStack();
     CHIP_ERROR AddEventHandler(EventHandlerFunct handler, intptr_t arg = 0);
     void RemoveEventHandler(EventHandlerFunct handler, intptr_t arg = 0);
+    /**
+     * ScheduleWork can be called after InitChipStack has been called.  Calls
+     * that happen before either StartEventLoopTask or RunEventLoop will queue
+     * the work up but that work will NOT run until one of those functions is
+     * called.
+     */
     void ScheduleWork(AsyncWorkFunct workFunct, intptr_t arg = 0);
+    /**
+     * Process work items until StopEventLoopTask is called.  RunEventLoop will
+     * not return until work item processing is stopped.  Once it returns it
+     * guarantees that no more work items will be processed unless there's
+     * another call to RunEventLoop.
+     *
+     * Consumers that call RunEventLoop must not call StartEventLoopTask.
+     *
+     * Consumers that call RunEventLoop must ensure that RunEventLoop returns
+     * before calling Shutdown.
+     */
     void RunEventLoop();
+    /**
+     * Process work items until StopEventLoopTask is called.
+     *
+     * StartEventLoopTask processes items asynchronously.  It can return before
+     * any items are processed, or after some items have been processed, or
+     * while an item is being processed, or even after StopEventLoopTask() has
+     * been called (e.g. if ScheduleWork() was called with a work item that
+     * calls StopEventLoopTask before StartEventLoopTask was called).
+     *
+     * Consumers that call StartEventLoopTask must not call RunEventLoop.
+     *
+     * Consumers that call StartEventLoopTask must ensure that they call
+     * StopEventLoopTask before calling Shutdown.
+     */
     CHIP_ERROR StartEventLoopTask();
+    /**
+     * Stop processing of work items by the event loop.
+     *
+     * If called from outside work item processing, StopEventLoopTask guarantees
+     * that any currently-executing work item completes execution and no more
+     * work items will run before it returns.  This is generally how
+     * StopEventLoopTask is used in conjunction with StartEventLoopTask.
+     *
+     * If called from inside work item processing, StopEventLoopTask makes no
+     * guarantees about exactly when work item processing will stop.  What it
+     * does guarantee is that if it is used this way in conjunction with
+     * RunEventLoop then all work item processing will stop before RunEventLoop
+     * returns.
+     */
     CHIP_ERROR StopEventLoopTask();
     void LockChipStack();
     bool TryLockChipStack();
