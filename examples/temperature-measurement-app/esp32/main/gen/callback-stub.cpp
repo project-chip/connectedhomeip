b3ee4415392f42778996f3f08542777c8aefde6d
Song Guo <songguo@google.com>
2021-06-28 09:52:05 +0800

    Run codegen

--- examples/temperature-measurement-app/esp32/main/gen/callback-stub.cpp
+++ examples/temperature-measurement-app/esp32/main/gen/callback-stub.cpp
@@ -448,22 +448,6 @@ emberAfPostAttributeChangeCallback(EndpointId endpoint, ClusterId clusterId, Att
                                    uint16_t manufacturerCode, uint8_t type, uint16_t size, uint8_t * value)
 {}
 
-/** @brief Read Attributes Response
- *
- * This function is called by the application framework when a Read Attributes
- * Response command is received from an external device.  The application should
- * return true if the message was processed or false if it was not.
- *
- * @param clusterId The cluster identifier of this response.  Ver.: always
- * @param buffer Buffer containing the list of read attribute status records.
- * Ver.: always
- * @param bufLen The length in bytes of the list.  Ver.: always
- */
-bool __attribute__((weak)) emberAfReadAttributesResponseCallback(ClusterId clusterId, uint8_t * buffer, uint16_t bufLen)
-{
-    return false;
-}
-
 /** @brief External Attribute Read
  *
  * Like emberAfExternalAttributeWriteCallback above, this function is called
