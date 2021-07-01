bbec7f94d58db3618489840b2888fbda0785e245
Song Guo <songguo@google.com>
2021-07-01 15:41:00 +0800

    Fix findIndexFromEndpoint

--- src/app/util/attribute-storage.cpp
+++ src/app/util/attribute-storage.cpp
@@ -884,7 +884,7 @@ static uint16_t findClusterEndpointIndex(EndpointId endpoint, ClusterId clusterI
 
     if (emberAfFindClusterWithMfgCode(endpoint, clusterId, mask, manufacturerCode) == NULL)
     {
-        return 0xFF;
+        return 0xFFFF;
     }
 
     for (i = 0; i < emberAfEndpointCount(); i++)
@@ -914,7 +914,7 @@ static uint16_t findIndexFromEndpoint(EndpointId endpoint, bool ignoreDisabledEn
             return epi;
         }
     }
-    return 0xFF;
+    return 0xFFFF;
 }
 
 bool emberAfEndpointIsEnabled(EndpointId endpoint)
