29af46e4ea169315ec9b2603632e052399511ac0
Restyled.io <commits@restyled.io>
2021-07-01 23:50:17 +0000

    Restyled by clang-format

--- src/controller/java/templates/helper.js
+++ src/controller/java/templates/helper.js
@@ -116,8 +116,9 @@ function asJniBasicTypeForZclType(type)
   function fn(pkgId)
   {
     const options = { 'hash' : {} };
-    return zclHelper.asUnderlyingZclType.call(this, type, options)
-        .then(zclType => { return convertBasicCTypeToJniType(ChipTypesHelper.asBasicType(zclType)); })
+    return zclHelper.asUnderlyingZclType.call(this, type, options).then(zclType => {
+      return convertBasicCTypeToJniType(ChipTypesHelper.asBasicType(zclType));
+    })
   }
 
   const promise = templateUtil.ensureZclPackageId(this).then(fn.bind(this)).catch(err => console.log(err));
