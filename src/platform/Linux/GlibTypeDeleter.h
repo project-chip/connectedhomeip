6bbabf4189d9a571c63ba7e8ec6e774af8f9132c
Zang MingJie <zealot0630@gmail.com>
2021-07-06 00:48:46 +0800

    Add license header

--- src/platform/Linux/GlibTypeDeleter.h
+++ src/platform/Linux/GlibTypeDeleter.h
@@ -19,25 +19,22 @@
 
 #include <gio/gio.h>
 
-template<typename T, typename Deleter>
+template <typename T, typename Deleter>
 class UniquePointerReceiver
 {
 public:
     UniquePointerReceiver(std::unique_ptr<T, Deleter> & target) : mTarget(target) {}
 
-    ~UniquePointerReceiver() {
-        mTarget.reset(mValue);
-    }
+    ~UniquePointerReceiver() { mTarget.reset(mValue); }
 
-    T*& Get() {
-        return mValue;
-    }
+    T *& Get() { return mValue; }
+
 private:
     std::unique_ptr<T, Deleter> & mTarget;
-    T* mValue = nullptr;
+    T * mValue = nullptr;
 };
 
-template<typename T, typename Deleter>
+template <typename T, typename Deleter>
 UniquePointerReceiver<T, Deleter> MakeUniquePointerReceiver(std::unique_ptr<T, Deleter> & target)
 {
     return UniquePointerReceiver<T, Deleter>(target);
@@ -45,42 +42,30 @@ UniquePointerReceiver<T, Deleter> MakeUniquePointerReceiver(std::unique_ptr<T, D
 
 struct GFree
 {
-    void operator()(gpointer object) {
-        g_free(object);
-    }
+    void operator()(gpointer object) { g_free(object); }
 };
 
 struct GObjectDeleter
 {
-    void operator()(gpointer object) {
-        g_object_unref(object);
-    }
+    void operator()(gpointer object) { g_object_unref(object); }
 };
 
 struct GErrorDeleter
 {
-    void operator()(GError * object) {
-        g_error_free(object);
-    }
+    void operator()(GError * object) { g_error_free(object); }
 };
 
 struct GVariantDeleter
 {
-    void operator()(GVariant * object) {
-        g_variant_unref(object);
-    }
+    void operator()(GVariant * object) { g_variant_unref(object); }
 };
 
 struct GVariantIterDeleter
 {
-    void operator()(GVariantIter * object) {
-        g_variant_iter_free(object);
-    }
+    void operator()(GVariantIter * object) { g_variant_iter_free(object); }
 };
 
 struct GBytesDeleter
 {
-    void operator()(GBytes* object) {
-        g_bytes_unref(object);
-    }
+    void operator()(GBytes * object) { g_bytes_unref(object); }
 };
