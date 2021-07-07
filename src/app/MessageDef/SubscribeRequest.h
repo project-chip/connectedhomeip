113f620bfd324ca4814783c0848306d91410286f
yunhanw <yunhanw@google.com>
2021-07-07 16:21:53 -0700

    Add IM subscribe request/response builder and parser

---
+++ src/app/MessageDef/SubscribeRequest.h
@@ -0,0 +1,220 @@
+/**
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2016-2017 Nest Labs, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <app/AppBuildConfig.h>
+#include "AttributeDataVersionList.h"
+#include "AttributePathList.h"
+#include "Builder.h"
+#include "EventPathList.h"
+#include "Parser.h"
+#include <app/util/basic-types.h>
+#include <core/CHIPCore.h>
+#include <core/CHIPTLV.h>
+#include <support/CodeUtils.h>
+#include <support/logging/CHIPLogging.h>
+
+namespace chip {
+namespace app {
+namespace SubscribeRequest {
+enum
+{
+    kCsTag_AttributePathList         = 0,
+    kCsTag_EventPathList             = 1,
+    kCsTag_AttributeDataVersionList  = 2,
+    kCsTag_EventNumber               = 3,
+    kCsTag_MinInterval               = 4,
+    kCsTag_MaxInterval               = 5,
+    kCsTag_KeepExistingSubscriptions = 6,
+    kCsTag_IsProxy                   = 7,
+};
+
+class Parser : public chip::app::Parser
+{
+public:
+    /**
+     *  @brief Initialize the parser object with TLVReader
+     *
+     *  @param [in] aReader A pointer to a TLVReader, which should point to the beginning of this request
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR Init(const chip::TLV::TLVReader & aReader);
+#if CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
+    /**
+     *  @brief Roughly verify the message is correctly formed
+     *   1) all mandatory tags are present
+     *   2) all elements have expected data type
+     *   3) any tag can only appear once
+     *   4) At the top level of the structure, unknown tags are ignored for forward compatibility
+     *  @note The main use of this function is to print out what we're
+     *    receiving during protocol development and debugging.
+     *    The encoding rule has changed in IM encoding spec so this
+     *    check is only "roughly" conformant now.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR CheckSchemaValidity() const;
+#endif
+
+    /**
+     *  @brief Get a TLVReader for the AttributePathList. Next() must be called before accessing them.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetAttributePathList(AttributePathList::Parser * const apAttributePathList) const;
+
+    /**
+     *  @brief Get a TLVReader for the EventPathList. Next() must be called before accessing them.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetEventPathList(EventPathList::Parser * const apEventPathList) const;
+
+    /**
+     *  @brief Get a parser for the AttributeDataVersionList. Next() must be called before accessing them.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetAttributeDataVersionList(AttributeDataVersionList::Parser * const apAttributeDataVersionList) const;
+
+    /**
+     *  @brief Get Event Number. Next() must be called before accessing them.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetEventNumber(uint64_t * const apEventNumber) const;
+
+    /**
+     *  @brief Get Min Interval. Next() must be called before accessing them.
+     *
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetMinInterval(uint16_t * const apMinInterval) const;
+
+    /**
+     *  @brief Get Max Interval. Next() must be called before accessing them.
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetMaxInterval(uint16_t * const apMaxInterval) const;
+
+    /**
+     *  @brief Check if subscription is kept. Next() must be called before accessing them.
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetKeepExistingSubscriptions(bool * const apKeepExistingSubscription) const;
+
+    /**
+     *  @brief Check if subscription is kept. Next() must be called before accessing them.
+     *  @return #CHIP_NO_ERROR on success
+     *          #CHIP_END_OF_TLV if there is no such element
+     */
+    CHIP_ERROR GetIsProxy(bool * const apIsProxy) const;
+};
+
+class Builder : public chip::app::Builder
+{
+public:
+    /**
+     *  @brief Initialize a SubscribeRequest::Builder for writing into a TLV stream
+     *
+     *  @param [in] apWriter    A pointer to TLVWriter
+     *
+     *  @return #CHIP_NO_ERROR on success
+     */
+    CHIP_ERROR Init(chip::TLV::TLVWriter * const apWriter);
+
+    /**
+     *  @brief Initialize a AttributePathList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to AttributePathList::Builder
+     */
+    AttributePathList::Builder & CreateAttributePathListBuilder();
+
+    /**
+     *  @brief Initialize a EventPathList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to EventPathList::Builder
+     */
+    EventPathList::Builder & CreateEventPathListBuilder();
+
+    /**
+     *  @brief Initialize a AttributeDataVersionList::Builder for writing into the TLV stream
+     *
+     *  @return A reference to AttributeDataVersionList::Builder
+     */
+    AttributeDataVersionList::Builder & CreateAttributeDataVersionListBuilder();
+
+    /**
+     *  @brief An initiator can optionally specify an EventNumber it has already to limit the
+     *  set of retrieved events on the server for optimization purposes.
+     *  @param [in] aEventNumber The event number
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & EventNumber(const uint64_t aEventNumber);
+
+    /**
+     *  @brief The requested minimum interval boundary floor in seconds
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & MinInterval(const uint16_t aMinInterval);
+
+    /**
+     *  @brief  the requested maximum interval boundary ceiling in seconds
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & MaxInterval(const uint16_t aMinInterval);
+
+    /**
+     *  @brief This is set to 'true' by the subscriber to indicate preservation of previous subscriptions. If omitted, it implies
+     * 'false' as a value.
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & KeepExistingSubscriptions(const bool aKeepExistingSubscriptions);
+
+    /**
+     *  @brief This is set to true by the subscriber if it is a proxy-type device proxying for another client. This
+     *  confers it special privileges on the publisher that might result in evictions of other non-proxy subscriptions
+     *  to make way for the proxy.
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & IsProxy(const bool aIsProxy);
+
+    /**
+     *  @brief Mark the end of this SubscribeRequest
+     *
+     *  @return A reference to *this
+     */
+    SubscribeRequest::Builder & EndOfSubscribeRequest();
+
+private:
+    AttributePathList::Builder mAttributePathListBuilder;
+    EventPathList::Builder mEventPathListBuilder;
+    AttributeDataVersionList::Builder mAttributeDataVersionListBuilder;
+};
+}; // namespace SubscribeRequest
+}; // namespace app
+}; // namespace chip
---
+++ src/app/MessageDef/SubscribeResponse.cpp
@@ -0,0 +1,151 @@
+/**
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "SubscribeResponse.h"
+#include "MessageDefHelper.h"
+
+namespace chip {
+namespace app {
+CHIP_ERROR SubscribeResponse::Parser::Init(const chip::TLV::TLVReader & aReader)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    // make a copy of the reader here
+    mReader.Init(aReader);
+
+    VerifyOrExit(chip::TLV::kTLVType_Structure == mReader.GetType(), err = CHIP_ERROR_WRONG_TLV_TYPE);
+
+    err = mReader.EnterContainer(mOuterContainerType);
+
+exit:
+    ChipLogFunctError(err);
+
+    return err;
+}
+
+#if CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
+CHIP_ERROR SubscribeResponse::Parser::CheckSchemaValidity() const
+{
+    CHIP_ERROR err           = CHIP_NO_ERROR;
+    uint16_t TagPresenceMask = 0;
+    chip::TLV::TLVReader reader;
+    PRETTY_PRINT("SubscribeResponse =");
+    PRETTY_PRINT("{");
+
+    // make a copy of the reader
+    reader.Init(mReader);
+
+    while (CHIP_NO_ERROR == (err = reader.Next()))
+    {
+        VerifyOrExit(chip::TLV::IsContextTag(reader.GetTag()), err = CHIP_ERROR_INVALID_TLV_TAG);
+        switch (chip::TLV::TagNumFromTag(reader.GetTag()))
+        {
+        case kCsTag_SubscriptionId:
+            VerifyOrExit(!(TagPresenceMask & (1 << kCsTag_SubscriptionId)), err = CHIP_ERROR_INVALID_TLV_TAG);
+            TagPresenceMask |= (1 << kCsTag_SubscriptionId);
+            VerifyOrExit(chip::TLV::kTLVType_UnsignedInteger == reader.GetType(), err = CHIP_ERROR_WRONG_TLV_TYPE);
+#if CHIP_DETAIL_LOGGING
+            {
+                uint64_t subscriptionId;
+                err = reader.Get(subscriptionId);
+                SuccessOrExit(err);
+                PRETTY_PRINT("\tSubscriptionId = 0x%" PRIx64 ",", subscriptionId);
+            }
+#endif // CHIP_DETAIL_LOGGING
+            break;
+        case kCsTag_FinalSyncInterval:
+            VerifyOrExit(!(TagPresenceMask & (1 << kCsTag_FinalSyncInterval)), err = CHIP_ERROR_INVALID_TLV_TAG);
+            TagPresenceMask |= (1 << kCsTag_FinalSyncInterval);
+            VerifyOrExit(chip::TLV::kTLVType_UnsignedInteger == reader.GetType(), err = CHIP_ERROR_WRONG_TLV_TYPE);
+#if CHIP_DETAIL_LOGGING
+            {
+                uint16_t finalSyncInterval;
+                err = reader.Get(finalSyncInterval);
+                SuccessOrExit(err);
+                PRETTY_PRINT("\tFinalSyncInterval = 0x%" PRIx16 ",", finalSyncInterval);
+            }
+#endif // CHIP_DETAIL_LOGGING
+            break;
+        default:
+            ExitNow(err = CHIP_ERROR_INVALID_TLV_TAG);
+        }
+    }
+    PRETTY_PRINT("}");
+    PRETTY_PRINT("");
+
+    if (CHIP_END_OF_TLV == err)
+    {
+        const uint16_t RequiredFields = (1 << kCsTag_SubscriptionId) | (1 << kCsTag_FinalSyncInterval);
+
+        if ((TagPresenceMask & RequiredFields) == RequiredFields)
+        {
+            err = CHIP_NO_ERROR;
+        }
+    }
+    SuccessOrExit(err);
+    err = reader.ExitContainer(mOuterContainerType);
+
+exit:
+    ChipLogFunctError(err);
+
+    return err;
+}
+#endif // CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
+
+CHIP_ERROR SubscribeResponse::Parser::GetSubscriptionId(uint64_t * const apSubscribeId) const
+{
+    return GetUnsignedInteger(kCsTag_SubscriptionId, apSubscribeId);
+}
+
+CHIP_ERROR SubscribeResponse::Parser::GetFinalSyncInterval(uint16_t * const apFinalSyncInterval) const
+{
+    return GetUnsignedInteger(kCsTag_FinalSyncInterval, apFinalSyncInterval);
+}
+
+CHIP_ERROR SubscribeResponse::Builder::Init(chip::TLV::TLVWriter * const apWriter)
+{
+    return InitAnonymousStructure(apWriter);
+}
+
+SubscribeResponse::Builder & SubscribeResponse::Builder::SubscriptionId(const uint64_t aSubscribeId)
+{
+    // skip if error has already been set
+    if (mError == CHIP_NO_ERROR)
+    {
+        mError = mpWriter->Put(chip::TLV::ContextTag(kCsTag_SubscriptionId), aSubscribeId);
+        ChipLogFunctError(mError);
+    }
+    return *this;
+}
+
+SubscribeResponse::Builder & SubscribeResponse::Builder::FinalSyncInterval(const uint16_t aFinalSyncInterval)
+{
+    // skip if error has already been set
+    if (mError == CHIP_NO_ERROR)
+    {
+        mError = mpWriter->Put(chip::TLV::ContextTag(kCsTag_FinalSyncInterval), aFinalSyncInterval);
+        ChipLogFunctError(mError);
+    }
+    return *this;
+}
+
+SubscribeResponse::Builder & SubscribeResponse::Builder::EndOfSubscribeResponse()
+{
+    EndOfContainer();
+    return *this;
+}
+}; // namespace app
+}; // namespace chip
