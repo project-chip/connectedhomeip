3dcc8988c0e996f686bd7a29ddb88b8e414768e8
ATmobica <artur.tynecki@mobica.com>
2021-07-08 04:11:26 +0000

    Add smoke check for unit testing

--- src/test_driver/mbed/unit_tests/pytest/test_unittests.py
+++ src/test_driver/mbed/unit_tests/pytest/test_unittests.py
@@ -2,6 +2,9 @@ import pytest
 import re
 
 def test_chip_unit_tests(device):
+    # Check if application running
+    ret=device.wait_for_output("CHIP:-: Starting CHIP tests!")
+    assert ret != None and len(ret) > 0
     lines = device.wait_for_output("CHIP:-: CHIP test status:", 500)
     # extract number of failures: 
     last_line = lines[-1]
