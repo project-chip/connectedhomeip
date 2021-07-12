d3d2586b4ace0faf88be1b314eef4595eede8157
Kevin Schoedel <67607049+kpschoedel@users.noreply.github.com>
2021-07-12 09:46:56 -0400

    Additional PacketBuffer size fixes. (#8259)
    * Additional PacketBuffer size fixes.
    
    #### Problem
    
    1. After #8226, `PacketBuffer::kMaxSizeWithoutReserve` was wrong on LwIP builds.
    
    2. It is possible for a `PacketBufferHandle::New()` to return a larger
       buffer than requested (e.g. when using a shared pool allocator), and
       in particular for it to return a block that is larger than *can* be
       requested. Attempting `CloneData()` on such a buffer fails with an
       error message logged by `PacketBufferHandle::New()`.
    
    #### Change overview
    
    1. Fix `PacketBuffer::kMaxSizeWithoutReserve`.
    
    2. As long as the excess space is not actually occupied (which would be
       incorrect, since it exceeds the spec size limit), `CloneData()`
       copies correctly to a maximum-size buffer.
    
    #### Testing
    
    Added a unit test to verify that a buffer with excess unused space is
    clonable, and a buffer with oversize space in use is not.
    
    * dynamically allocate test buffer
    
    * cast narrowing conversion

--- src/system/SystemPacketBuffer.h
+++ src/system/SystemPacketBuffer.h
@@ -146,7 +146,7 @@ public:
      * The maximum size buffer an application can allocate with no protocol header reserve.
      */
 #if CHIP_SYSTEM_CONFIG_USE_LWIP
-    static constexpr uint16_t kMaxSizeWithoutReserve = (LWIP_MEM_ALIGN_SIZE(PBUF_POOL_BUFSIZE) - PacketBuffer::kStructureSize);
+    static constexpr uint16_t kMaxSizeWithoutReserve = LWIP_MEM_ALIGN_SIZE(PBUF_POOL_BUFSIZE);
 #else
     static constexpr uint16_t kMaxSizeWithoutReserve = CHIP_SYSTEM_CONFIG_PACKETBUFFER_CAPACITY_MAX;
 #endif
