19581e7bea627f76e3069446faa0ad442c13a83c
ATmobica <artur.tynecki@mobica.com>
2021-07-12 05:54:01 +0000

    Merge lock-app test to single file Add lock-app smoke test Mark tests as expected to fail

--- src/test_driver/mbed/integration_tests/lock-app/test_app.py
+++ src/test_driver/mbed/integration_tests/lock-app/test_app.py
@@ -26,7 +26,65 @@ log = logging.getLogger(__name__)
 
 DEVICE_NODE_ID=1234
 
+@pytest.mark.smoketest
+def test_smoke_test(device):
+    device.reset(duration=1)
+    ret = device.wait_for_output("Starting CHIP task")
+    assert ret != None and len(ret) > 0
+
 @pytest.mark.deviceControllerTest
+@pytest.mark.xfail
+def test_wifi_provisioning_dev_ctrl(device, network, device_controller):
+    network_ssid = network[0]
+    network_pass = network[1]
+    
+    ret = device.wait_for_output("SetupQRCode")
+    assert ret != None and len(ret) > 1
+
+    qr_code = ret[-1].split('[', 1)[1].split(']')[0]
+    device_details = dict(SetupPayload().ParseQrCode("VP:vendorpayload%{}".format(qr_code)).attributes)
+    assert device_details != None and len(device_details) != 0
+
+    ble_chip_device = scan_chip_ble_devices(device_controller)
+    assert ble_chip_device != None and len(ble_chip_device) != 0
+
+    chip_device_found = False
+
+    for ble_device in ble_chip_device:
+        if (int(ble_device.discriminator) == int(device_details["Discriminator"]) and
+            int(ble_device.vendorId) == int(device_details["VendorID"]) and
+            int(ble_device.productId) == int(device_details["ProductID"])):
+            chip_device_found = True
+            break
+
+    assert chip_device_found
+
+    ret = run_wifi_provisioning(device_controller, network_ssid, network_pass, int(device_details["Discriminator"]), int(device_details["SetUpPINCode"]), DEVICE_NODE_ID)
+    assert ret != None and ret == DEVICE_NODE_ID
+
+@pytest.mark.chipToolTest
+@pytest.mark.skip(reason="CLI chip-tool is unstable")
+def test_wifi_provisioning_chip_tool(device, network, chip_tools_dir):
+    network_ssid = network[0]
+    network_pass = network[1]
+    
+    ret = device.wait_for_output("SetupQRCode")
+    assert ret != None and len(ret) > 1
+
+    qr_code = ret[-1].split('[', 1)[1].split(']')[0]
+    device_details = dict(SetupPayload().ParseQrCode("VP:vendorpayload%{}".format(qr_code)).attributes)
+    assert device_details != None and len(device_details) != 0
+
+    process = subprocess.Popen(["./chip-tool", "pairing", "ble", network_ssid, network_pass, str(device_details["SetUpPINCode"]), str(device_details["Discriminator"])], cwd=chip_tools_dir, stdout=subprocess.PIPE, universal_newlines=True)
+    out = process.stdout.read()
+    process.wait()
+    assert process.returncode == 0
+    print(out)
+    assert "ConnectDevice complete" in out and "Network Provisioning Success" in out
+
+
+@pytest.mark.deviceControllerTest
+@pytest.mark.xfail
 def test_lock_ctrl_dev_ctrl(device, network, device_controller):
     network_ssid = network[0]
     network_pass = network[1]
@@ -75,6 +133,7 @@ def test_lock_ctrl_dev_ctrl(device, network, device_controller):
         assert False
 
 @pytest.mark.chipToolTest
+@pytest.mark.skip(reason="CLI chip-tool is unstable")
 def test_lock_ctrl_chip_tool(device, network, chip_tools_dir):
     network_ssid = network[0]
     network_pass = network[1]
