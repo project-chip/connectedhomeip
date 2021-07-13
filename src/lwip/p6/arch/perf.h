863469dd7806bae71c66792b55c67e4e2169930c
Praveen Babu Chandran <PraveenBabu.Chandran@infineon.com>
2021-07-13 16:06:47 -0700

    Update Code changes to support new Clock modifications

--- src/lwip/p6/arch/perf.h
+++ src/lwip/p6/arch/perf.h
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2021 Project CHIP Authors
  *    Copyright (c) 2019 Nest Labs, Inc.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
