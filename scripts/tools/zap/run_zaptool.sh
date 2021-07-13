cb68dcf482a797e67b988b4634e16fd438ef2e50
Restyled.io <commits@restyled.io>
2021-07-13 10:30:13 +0000

    Restyled by shfmt

--- scripts/tools/zap/run_zaptool.sh
+++ scripts/tools/zap/run_zaptool.sh
@@ -16,38 +16,37 @@
 # limitations under the License.
 #
 
-function _get_fullpath {
-	cd "$(dirname "$1")" && echo "$PWD/$(basename "$1")"
+function _get_fullpath() {
+    cd "$(dirname "$1")" && echo "$PWD/$(basename "$1")"
 }
 
 set -e
 
 [[ "$1" == "--help" ]] && {
-  echo "Usage: $0 [ZAP-file-path]" >&2
-  exit 0
+    echo "Usage: $0 [ZAP-file-path]" >&2
+    exit 0
 }
-
 
 CHIP_ROOT=$(_get_fullpath "${BASH_SOURCE%/scripts/tools/zap/run_zaptool.sh}")
 [[ -n "$1" ]] && ZAP_ARGS=(-i "$(_get_fullpath "$1")") || ZAP_ARGS=()
 
 (
 
-cd "$CHIP_ROOT" &&
-    git submodule update --init third_party/zap/repo
+    cd "$CHIP_ROOT" &&
+        git submodule update --init third_party/zap/repo
 
-cd "third_party/zap/repo"
-if ! npm list installed-check &>/dev/null; then
-    npm install installed-check
-fi
+    cd "third_party/zap/repo"
+    if ! npm list installed-check &>/dev/null; then
+        npm install installed-check
+    fi
 
-if ! ./node_modules/.bin/installed-check -c &>/dev/null; then
-    npm install
-fi
+    if ! ./node_modules/.bin/installed-check -c &>/dev/null; then
+        npm install
+    fi
 
-node src-script/zap-start.js --logToStdout \
-    --gen "$CHIP_ROOT/src/app/zap-templates/app-templates.json" \
-    --zcl "$CHIP_ROOT/src/app/zap-templates/zcl/zcl.json" \
-    "${ZAP_ARGS[@]}"
+    node src-script/zap-start.js --logToStdout \
+        --gen "$CHIP_ROOT/src/app/zap-templates/app-templates.json" \
+        --zcl "$CHIP_ROOT/src/app/zap-templates/zcl/zcl.json" \
+        "${ZAP_ARGS[@]}"
 
 )
