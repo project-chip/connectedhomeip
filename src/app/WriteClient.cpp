2990f90d010971249cce79d13b7f6d794b61f204
Yufeng Wang <yufengwang@google.com>
2021-07-15 12:47:44 -0700

    Update the exchange match logic to align with the latest spec proposal

--- src/app/WriteClient.cpp
+++ src/app/WriteClient.cpp
@@ -262,11 +262,9 @@ CHIP_ERROR WriteClient::SendWriteRequest(NodeId aNodeId, FabricIndex aFabricInde
     ClearExistingExchangeContext();
 
     // Create a new exchange context.
-    // TODO: we temporarily create a SecureSessionHandle from node id, this will be fixed in PR 3602
-    // TODO: Hard code keyID to 0 to unblock IM end-to-end test. Complete solution is tracked in issue:4451
     if (apSecureSession == nullptr)
     {
-        mpExchangeCtx = mpExchangeMgr->NewContext({ aNodeId, 0, aFabricIndex }, this);
+        mpExchangeCtx = mpExchangeMgr->NewContext(SecureSessionHandle(aNodeId, 0, aFabricIndex), this);
     }
     else
     {
