32c1393bd8a93ca1ad9097fd43a97922aba5c678
Zang MingJie <zealot0630@gmail.com>
2021-07-20 22:18:50 +0800

    Remove goto exit in messaging layer (#8106)
    * Remove goto exit in messaging layer
    
    * Resolve comments
    
    * Fix rebase conflict
    
    * Resolve comments

---
+++ src/lib/support/ReferenceCountedHandle.h
@@ -0,0 +1,43 @@
+/*
+ *
+ *    Copyright (c) 2020-2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+namespace chip {
+
+/** ReferenceCountedHandle acts like a shared_ptr to an object derived from ReferenceCounted. In contrast to shared_ptr, the handle
+ * will always hold a valid target */
+template <typename Target>
+class ReferenceCountedHandle
+{
+public:
+    explicit ReferenceCountedHandle(Target & target) : mTarget(target) { mTarget.Retain(); }
+    ~ReferenceCountedHandle() { mTarget.Release(); }
+
+    ReferenceCountedHandle(const ReferenceCountedHandle & that) = delete;
+    ReferenceCountedHandle & operator=(const ReferenceCountedHandle & that) = delete;
+    ReferenceCountedHandle(ReferenceCountedHandle && that)                  = delete;
+    ReferenceCountedHandle & operator=(ReferenceCountedHandle && that) = delete;
+
+    bool operator==(const ReferenceCountedHandle & that) const { return &mTarget == &that.mTarget; }
+    bool operator!=(const ReferenceCountedHandle & that) const { return !(*this == that); }
+
+private:
+    Target & mTarget;
+};
+
+} // namespace chip
