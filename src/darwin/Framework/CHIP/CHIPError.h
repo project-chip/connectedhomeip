245ec907c84d7c56e3b26f1101cbd0fee80b2eb8
Restyled.io <commits@restyled.io>
2021-07-22 18:51:43 +0000

    Restyled by clang-format

--- src/darwin/Framework/CHIP/CHIPError.h
+++ src/darwin/Framework/CHIP/CHIPError.h
@@ -20,17 +20,17 @@
 NS_ASSUME_NONNULL_BEGIN
 FOUNDATION_EXPORT NSErrorDomain const CHIPErrorDomain;
 
-typedef NS_ERROR_ENUM(CHIPErrorDomain, CHIPErrorCode) {
-    CHIPSuccess = 0,
-    CHIPErrorCodeUndefinedError = 1,
-    CHIPErrorCodeInvalidStringLength = 2,
-    CHIPErrorCodeInvalidIntegerValue = 3,
-    CHIPErrorCodeInvalidArgument = 4,
+typedef NS_ERROR_ENUM(CHIPErrorDomain, CHIPErrorCode){
+    CHIPSuccess                       = 0,
+    CHIPErrorCodeUndefinedError       = 1,
+    CHIPErrorCodeInvalidStringLength  = 2,
+    CHIPErrorCodeInvalidIntegerValue  = 3,
+    CHIPErrorCodeInvalidArgument      = 4,
     CHIPErrorCodeInvalidMessageLength = 5,
-    CHIPErrorCodeInvalidState = 6,
-    CHIPErrorCodeWrongAddressType = 7,
+    CHIPErrorCodeInvalidState         = 6,
+    CHIPErrorCodeWrongAddressType     = 7,
     CHIPErrorCodeIntegrityCheckFailed = 8,
-    CHIPErrorCodeDuplicateExists = 9,
+    CHIPErrorCodeDuplicateExists      = 9,
     CHIPErrorCodeUnsupportedAttribute = 10,
 };
 
