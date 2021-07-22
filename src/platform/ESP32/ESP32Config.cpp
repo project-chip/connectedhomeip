project-chip/connectedhomeip

commit 162366f07969412131464768ea519ea4f00f0d93

Make CHIP_ERROR a class type on ESP32 (#8469)
* Make CHIP_ERROR a class type on ESP32.

#### Problem

Having CHIP_ERROR be a class type would provide (a) type safety,
and (b) the ability to trace the source of errors (issue #8340).

#### Change overview

- Enable `CHIP_CONFIG_ERROR_CLASS` on ESP32.

#### Testing

Passes esp32_qemu_tests.sh

* review

src/platform/ESP32/ESP32Config.cpp
@@ -29,6 +29,7 @@
 
 #include <core/CHIPEncoding.h>
 #include <platform/ESP32/ESP32Utils.h>
+#include <platform/ESP32/ScopedNvsHandle.h>
 #include <support/CHIPMem.h>
 #include <support/CHIPMemString.h>
 #include <support/CodeUtils.h>
@@ -81,66 +82,44 @@ const char ESP32Config::kGroupKeyNamePrefix[] = "gk-";
 
 CHIP_ERROR ESP32Config::ReadConfigValue(Key key, bool & val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
     uint32_t intVal;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READONLY));
 
-    err = nvs_get_u32(handle, key.Name, &intVal);
+    esp_err_t err = nvs_get_u32(handle, key.Name, &intVal);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
-        err = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
     val = (intVal != 0);
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::ReadConfigValue(Key key, uint32_t & val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READONLY));
 
-    err = nvs_get_u32(handle, key.Name, &val);
+    esp_err_t err = nvs_get_u32(handle, key.Name, &val);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
-        err = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::ReadConfigValue(Key key, uint64_t & val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READONLY));
 
     // Special case the MfrDeviceId value, optionally allowing it to be read as a blob containing
     // a 64-bit big-endian integer, instead of a u64 value.
@@ -154,312 +133,192 @@ CHIP_ERROR ESP32Config::ReadConfigValue(Key key, uint64_t & val)
     {
         uint8_t deviceIdBytes[sizeof(uint64_t)];
         size_t deviceIdLen = sizeof(deviceIdBytes);
-        err                = nvs_get_blob(handle, key.Name, deviceIdBytes, &deviceIdLen);
+        esp_err_t err      = nvs_get_blob(handle, key.Name, deviceIdBytes, &deviceIdLen);
         if (err == ESP_OK)
         {
-            VerifyOrExit(deviceIdLen == sizeof(deviceIdBytes), err = ESP_ERR_NVS_INVALID_LENGTH);
+            VerifyOrReturnError(deviceIdLen == sizeof(deviceIdBytes), ESP32Utils::MapError(ESP_ERR_NVS_INVALID_LENGTH));
             val = Encoding::BigEndian::Get64(deviceIdBytes);
-            ExitNow();
+            return CHIP_NO_ERROR;
         }
     }
 
-    err = nvs_get_u64(handle, key.Name, &val);
+    esp_err_t err = nvs_get_u64(handle, key.Name, &val);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
-        err = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::ReadConfigValueStr(Key key, char * buf, size_t bufSize, size_t & outLen)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READONLY));
 
-    outLen = bufSize;
-    err    = nvs_get_str(handle, key.Name, buf, &outLen);
+    outLen        = bufSize;
+    esp_err_t err = nvs_get_str(handle, key.Name, buf, &outLen);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
         outLen = 0;
-        err    = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
     }
-    else if (err == ESP_ERR_NVS_INVALID_LENGTH)
+    if (err == ESP_ERR_NVS_INVALID_LENGTH && buf != NULL)
     {
-        err = (buf == NULL) ? CHIP_NO_ERROR : CHIP_ERROR_BUFFER_TOO_SMALL;
+        return CHIP_ERROR_BUFFER_TOO_SMALL;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
     outLen -= 1; // Don't count trailing nul.
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::ReadConfigValueBin(Key key, uint8_t * buf, size_t bufSize, size_t & outLen)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READONLY));
 
-    outLen = bufSize;
-    err    = nvs_get_blob(handle, key.Name, buf, &outLen);
+    outLen        = bufSize;
+    esp_err_t err = nvs_get_blob(handle, key.Name, buf, &outLen);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
         outLen = 0;
-        err    = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
     }
-    else if (err == ESP_ERR_NVS_INVALID_LENGTH)
+    else if (err == ESP_ERR_NVS_INVALID_LENGTH && buf != NULL)
     {
-        err = (buf == NULL) ? CHIP_NO_ERROR : CHIP_ERROR_BUFFER_TOO_SMALL;
+        return CHIP_ERROR_BUFFER_TOO_SMALL;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValue(Key key, bool val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-    SuccessOrExit(err);
-    needClose = true;
-
-    err = nvs_set_u32(handle, key.Name, val ? 1 : 0);
-    SuccessOrExit(err);
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
+    ReturnMappedErrorOnFailure(nvs_set_u32(handle, key.Name, val ? 1 : 0));
 
     // Commit the value to the persistent store.
-    err = nvs_commit(handle);
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(nvs_commit(handle));
 
     ChipLogProgress(DeviceLayer, "NVS set: %s/%s = %s", key.Namespace, key.Name, val ? "true" : "false");
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValue(Key key, uint32_t val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-    SuccessOrExit(err);
-    needClose = true;
-
-    err = nvs_set_u32(handle, key.Name, val);
-    SuccessOrExit(err);
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
+    ReturnMappedErrorOnFailure(nvs_set_u32(handle, key.Name, val));
 
     // Commit the value to the persistent store.
-    err = nvs_commit(handle);
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(nvs_commit(handle));
 
     ChipLogProgress(DeviceLayer, "NVS set: %s/%s = %" PRIu32 " (0x%" PRIX32 ")", key.Namespace, key.Name, val, val);
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValue(Key key, uint64_t val)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-    SuccessOrExit(err);
-    needClose = true;
-
-    err = nvs_set_u64(handle, key.Name, val);
-    SuccessOrExit(err);
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
+    ReturnMappedErrorOnFailure(nvs_set_u64(handle, key.Name, val));
 
     // Commit the value to the persistent store.
-    err = nvs_commit(handle);
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(nvs_commit(handle));
 
     ChipLogProgress(DeviceLayer, "NVS set: %s/%s = %" PRIu64 " (0x%" PRIX64 ")", key.Namespace, key.Name, val, val);
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValueStr(Key key, const char * str)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
-
     if (str != NULL)
     {
-        err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-        SuccessOrExit(err);
-        needClose = true;
+        ScopedNvsHandle handle;
 
-        err = nvs_set_str(handle, key.Name, str);
-        SuccessOrExit(err);
+        ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
+        ReturnMappedErrorOnFailure(nvs_set_str(handle, key.Name, str));
 
         // Commit the value to the persistent store.
-        err = nvs_commit(handle);
-        SuccessOrExit(err);
+        ReturnMappedErrorOnFailure(nvs_commit(handle));
 
         ChipLogProgress(DeviceLayer, "NVS set: %s/%s = \"%s\"", key.Namespace, key.Name, str);
+        return CHIP_NO_ERROR;
     }
 
-    else
-    {
-        err = ClearConfigValue(key);
-        SuccessOrExit(err);
-    }
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return ClearConfigValue(key);
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValueStr(Key key, const char * str, size_t strLen)
 {
-    CHIP_ERROR err;
     chip::Platform::ScopedMemoryBuffer<char> strCopy;
 
     if (str != NULL)
     {
         strCopy.Calloc(strLen + 1);
-        VerifyOrExit(strCopy, err = CHIP_ERROR_NO_MEMORY);
+        VerifyOrReturnError(strCopy, CHIP_ERROR_NO_MEMORY);
         strncpy(strCopy.Get(), str, strLen);
     }
-    err = ESP32Config::WriteConfigValueStr(key, strCopy.Get());
-
-exit:
-    return err;
+    return ESP32Config::WriteConfigValueStr(key, strCopy.Get());
 }
 
 CHIP_ERROR ESP32Config::WriteConfigValueBin(Key key, const uint8_t * data, size_t dataLen)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
     if (data != NULL)
     {
-        err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-        SuccessOrExit(err);
-        needClose = true;
-
-        err = nvs_set_blob(handle, key.Name, data, dataLen);
-        SuccessOrExit(err);
+        ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
+        ReturnMappedErrorOnFailure(nvs_set_blob(handle, key.Name, data, dataLen));
 
         // Commit the value to the persistent store.
-        err = nvs_commit(handle);
-        SuccessOrExit(err);
+        ReturnMappedErrorOnFailure(nvs_commit(handle));
 
         ChipLogProgress(DeviceLayer, "NVS set: %s/%s = (blob length %" PRId32 ")", key.Namespace, key.Name, dataLen);
+        return CHIP_NO_ERROR;
     }
 
-    else
-    {
-        err = ClearConfigValue(key);
-        SuccessOrExit(err);
-    }
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return ClearConfigValue(key);
 }
 
 CHIP_ERROR ESP32Config::ClearConfigValue(Key key)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READWRITE, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    ReturnErrorOnFailure(handle.Open(key.Namespace, NVS_READWRITE));
 
-    err = nvs_erase_key(handle, key.Name);
+    esp_err_t err = nvs_erase_key(handle, key.Name);
     if (err == ESP_ERR_NVS_NOT_FOUND)
     {
-        ExitNow(err = CHIP_NO_ERROR);
+        return CHIP_NO_ERROR;
     }
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(err);
 
     // Commit the value to the persistent store.
-    err = nvs_commit(handle);
-    SuccessOrExit(err);
+    ReturnMappedErrorOnFailure(nvs_commit(handle));
 
     ChipLogProgress(DeviceLayer, "NVS erase: %s/%s", key.Namespace, key.Name);
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 bool ESP32Config::ConfigValueExists(Key key)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(key.Namespace, NVS_READONLY, &handle);
-    SuccessOrExit(err);
-    needClose = true;
+    if (handle.Open(key.Namespace, NVS_READONLY) != CHIP_NO_ERROR)
+    {
+        return false;
+    }
 
     // This code is a rather unfortunate consequence of the limitations
     // in the ESP NVS API.  As defined, there is no API for determining
@@ -471,6 +330,7 @@ bool ESP32Config::ConfigValueExists(Key key)
     //
     // Thus the solution is to exhaustively check for the key using
     // each possible value type.
+    esp_err_t err;
     {
         uint8_t v;
         err = nvs_get_u8(handle, key.Name, &v);
@@ -528,63 +388,34 @@ bool ESP32Config::ConfigValueExists(Key key)
         err = ESP_OK;
     }
 
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
     return err == ESP_OK;
 }
 
 CHIP_ERROR ESP32Config::EnsureNamespace(const char * ns)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(ns, NVS_READONLY, &handle);
-    if (err == ESP_ERR_NVS_NOT_FOUND)
+    CHIP_ERROR err = handle.Open(ns, NVS_READONLY);
+    if (err == CHIP_NO_ERROR)
     {
-        err = nvs_open(ns, NVS_READWRITE, &handle);
-        SuccessOrExit(err);
-        needClose = true;
-
-        err = nvs_commit(handle);
-        SuccessOrExit(err);
+        return CHIP_NO_ERROR;
     }
-    SuccessOrExit(err);
-    needClose = true;
-
-exit:
-    if (needClose)
+    if (err == ESP32Utils::MapError(ESP_ERR_NVS_NOT_FOUND))
     {
-        nvs_close(handle);
+        ReturnErrorOnFailure(handle.Open(ns, NVS_READWRITE));
+        ReturnMappedErrorOnFailure(nvs_commit(handle));
     }
     return err;
 }
 
 CHIP_ERROR ESP32Config::ClearNamespace(const char * ns)
 {
-    CHIP_ERROR err;
-    nvs_handle handle;
-    bool needClose = false;
+    ScopedNvsHandle handle;
 
-    err = nvs_open(ns, NVS_READWRITE, &handle);
-    SuccessOrExit(err);
-    needClose = true;
-
-    err = nvs_erase_all(handle);
-    SuccessOrExit(err);
-
-    err = nvs_commit(handle);
-    SuccessOrExit(err);
-
-exit:
-    if (needClose)
-    {
-        nvs_close(handle);
-    }
-    return err;
+    ReturnErrorOnFailure(handle.Open(ns, NVS_READWRITE));
+    ReturnMappedErrorOnFailure(nvs_erase_all(handle));
+    ReturnMappedErrorOnFailure(nvs_commit(handle));
+    return CHIP_NO_ERROR;
 }
 
 void ESP32Config::RunConfigUnitTest() {}

src/platform/ESP32/ESP32Utils.cpp
@@ -39,14 +39,13 @@ using chip::DeviceLayer::Internal::DeviceNetworkInfo;
 
 CHIP_ERROR ESP32Utils::IsAPEnabled(bool & apEnabled)
 {
-    CHIP_ERROR err;
     wifi_mode_t curWiFiMode;
 
-    err = esp_wifi_get_mode(&curWiFiMode);
+    esp_err_t err = esp_wifi_get_mode(&curWiFiMode);
     if (err != ESP_OK)
     {
-        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", chip::ErrorStr(err));
-        return err;
+        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", esp_err_to_name(err));
+        return ESP32Utils::MapError(err);
     }
 
     apEnabled = (curWiFiMode == WIFI_MODE_AP || curWiFiMode == WIFI_MODE_APSTA);
@@ -69,14 +68,13 @@ CHIP_ERROR ESP32Utils::IsStationConnected(bool & connected)
 
 CHIP_ERROR ESP32Utils::StartWiFiLayer(void)
 {
-    CHIP_ERROR err;
     int8_t ignored;
     bool wifiStarted;
 
     // There appears to be no direct way to ask the ESP WiFi layer if esp_wifi_start()
     // has been called.  So use the ESP_ERR_WIFI_NOT_STARTED error returned by
     // esp_wifi_get_max_tx_power() to detect this.
-    err = esp_wifi_get_max_tx_power(&ignored);
+    esp_err_t err = esp_wifi_get_max_tx_power(&ignored);
     switch (err)
     {
     case ESP_OK:
@@ -84,10 +82,9 @@ CHIP_ERROR ESP32Utils::StartWiFiLayer(void)
         break;
     case ESP_ERR_WIFI_NOT_STARTED:
         wifiStarted = false;
-        err         = ESP_OK;
         break;
     default:
-        ExitNow();
+        return ESP32Utils::MapError(err);
     }
 
     if (!wifiStarted)
@@ -97,26 +94,25 @@ CHIP_ERROR ESP32Utils::StartWiFiLayer(void)
         err = esp_wifi_start();
         if (err != ESP_OK)
         {
-            ChipLogError(DeviceLayer, "esp_wifi_start() failed: %s", chip::ErrorStr(err));
+            ChipLogError(DeviceLayer, "esp_wifi_start() failed: %s", esp_err_to_name(err));
+            return ESP32Utils::MapError(err);
         }
     }
 
-exit:
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Utils::EnableStationMode(void)
 {
-    CHIP_ERROR err;
     wifi_mode_t curWiFiMode;
 
     // Get the current ESP WiFI mode.
-    err = esp_wifi_get_mode(&curWiFiMode);
+    esp_err_t err = esp_wifi_get_mode(&curWiFiMode);
     if (err != ESP_OK)
     {
-        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", chip::ErrorStr(err));
+        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", esp_err_to_name(err));
+        return ESP32Utils::MapError(err);
     }
-    SuccessOrExit(err);
 
     // If station mode is not already enabled (implying the current mode is WIFI_MODE_AP), change
     // the mode to WIFI_MODE_APSTA.
@@ -128,29 +124,27 @@ CHIP_ERROR ESP32Utils::EnableStationMode(void)
         err = esp_wifi_set_mode(WIFI_MODE_APSTA);
         if (err != ESP_OK)
         {
-            ChipLogError(DeviceLayer, "esp_wifi_set_mode() failed: %s", chip::ErrorStr(err));
+            ChipLogError(DeviceLayer, "esp_wifi_set_mode() failed: %s", esp_err_to_name(err));
+            return ESP32Utils::MapError(err);
         }
-        SuccessOrExit(err);
     }
 
-exit:
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Utils::SetAPMode(bool enabled)
 {
-    CHIP_ERROR err;
     wifi_mode_t curWiFiMode, targetWiFiMode;
 
     targetWiFiMode = (enabled) ? WIFI_MODE_APSTA : WIFI_MODE_STA;
 
     // Get the current ESP WiFI mode.
-    err = esp_wifi_get_mode(&curWiFiMode);
+    esp_err_t err = esp_wifi_get_mode(&curWiFiMode);
     if (err != ESP_OK)
     {
-        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", chip::ErrorStr(err));
+        ChipLogError(DeviceLayer, "esp_wifi_get_mode() failed: %s", esp_err_to_name(err));
+        return ESP32Utils::MapError(err);
     }
-    SuccessOrExit(err);
 
     // If station mode is not already enabled (implying the current mode is WIFI_MODE_AP), change
     // the mode to WIFI_MODE_APSTA.
@@ -161,13 +155,12 @@ CHIP_ERROR ESP32Utils::SetAPMode(bool enabled)
         err = esp_wifi_set_mode(targetWiFiMode);
         if (err != ESP_OK)
         {
-            ChipLogError(DeviceLayer, "esp_wifi_set_mode() failed: %s", chip::ErrorStr(err));
+            ChipLogError(DeviceLayer, "esp_wifi_set_mode() failed: %s", esp_err_to_name(err));
+            return ESP32Utils::MapError(err);
         }
-        SuccessOrExit(err);
     }
 
-exit:
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 int ESP32Utils::OrderScanResultsByRSSI(const void * _res1, const void * _res2)
@@ -231,13 +224,15 @@ bool ESP32Utils::HasIPv6LinkLocalAddress(const char * ifKey)
 
 CHIP_ERROR ESP32Utils::GetWiFiStationProvision(Internal::DeviceNetworkInfo & netInfo, bool includeCredentials)
 {
-    CHIP_ERROR err = CHIP_NO_ERROR;
     wifi_config_t stationConfig;
 
-    err = esp_wifi_get_config(WIFI_IF_STA, &stationConfig);
-    SuccessOrExit(err);
+    esp_err_t err = esp_wifi_get_config(WIFI_IF_STA, &stationConfig);
+    if (err != ESP_OK)
+    {
+        return ESP32Utils::MapError(err);
+    }
 
-    VerifyOrExit(stationConfig.sta.ssid[0] != 0, err = CHIP_ERROR_INCORRECT_STATE);
+    VerifyOrReturnError(stationConfig.sta.ssid[0] != 0, CHIP_ERROR_INCORRECT_STATE);
 
     netInfo.NetworkId              = kWiFiStationNetworkId;
     netInfo.FieldPresent.NetworkId = true;
@@ -254,13 +249,11 @@ CHIP_ERROR ESP32Utils::GetWiFiStationProvision(Internal::DeviceNetworkInfo & net
         memcpy(netInfo.WiFiKey, stationConfig.sta.password, netInfo.WiFiKeyLen);
     }
 
-exit:
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Utils::SetWiFiStationProvision(const Internal::DeviceNetworkInfo & netInfo)
 {
-    CHIP_ERROR err = CHIP_NO_ERROR;
     wifi_config_t wifiConfig;
 
     char wifiSSID[kMaxWiFiSSIDLength + 1];
@@ -268,8 +261,7 @@ CHIP_ERROR ESP32Utils::SetWiFiStationProvision(const Internal::DeviceNetworkInfo
 
     // Ensure that ESP station mode is enabled.  This is required before esp_wifi_set_config(ESP_IF_WIFI_STA,...)
     // can be called.
-    err = ESP32Utils::EnableStationMode();
-    SuccessOrExit(err);
+    ReturnErrorOnFailure(ESP32Utils::EnableStationMode());
 
     // Enforce that wifiSSID is null terminated before copying it
     memcpy(wifiSSID, netInfo.WiFiSSID, min(netInfoSSIDLen + 1, sizeof(wifiSSID)));
@@ -290,27 +282,34 @@ CHIP_ERROR ESP32Utils::SetWiFiStationProvision(const Internal::DeviceNetworkInfo
     wifiConfig.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
 
     // Configure the ESP WiFi interface.
-    err = esp_wifi_set_config(WIFI_IF_STA, &wifiConfig);
+    esp_err_t err = esp_wifi_set_config(WIFI_IF_STA, &wifiConfig);
     if (err != ESP_OK)
     {
-        ChipLogError(DeviceLayer, "esp_wifi_set_config() failed: %s", chip::ErrorStr(err));
+        ChipLogError(DeviceLayer, "esp_wifi_set_config() failed: %s", esp_err_to_name(err));
+        return ESP32Utils::MapError(err);
     }
-    SuccessOrExit(err);
 
     ChipLogProgress(DeviceLayer, "WiFi station provision set (SSID: %s)", netInfo.WiFiSSID);
 
-exit:
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ESP32Utils::ClearWiFiStationProvision(void)
 {
-    CHIP_ERROR err = CHIP_NO_ERROR;
     wifi_config_t stationConfig;
 
     // Clear the ESP WiFi station configuration.
     memset(&stationConfig, 0, sizeof(stationConfig));
     esp_wifi_set_config(WIFI_IF_STA, &stationConfig);
 
-    return err;
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR ESP32Utils::MapError(esp_err_t error)
+{
+    if (error == ESP_OK)
+    {
+        return CHIP_NO_ERROR;
+    }
+    return ChipError::Encapsulate(ChipError::Range::kPlatform, error);
 }

src/platform/ESP32/ESP32Utils.h
@@ -45,7 +45,19 @@ public:
     static CHIP_ERROR GetWiFiStationProvision(Internal::DeviceNetworkInfo & netInfo, bool includeCredentials);
     static CHIP_ERROR SetWiFiStationProvision(const Internal::DeviceNetworkInfo & netInfo);
     static CHIP_ERROR ClearWiFiStationProvision(void);
+
+    static CHIP_ERROR MapError(esp_err_t error);
 };
+
+#define ReturnMappedErrorOnFailure(expr)                                                                                           \
+    do                                                                                                                             \
+    {                                                                                                                              \
+        esp_err_t __err = (expr);                                                                                                  \
+        if (__err != ESP_OK)                                                                                                       \
+        {                                                                                                                          \
+            return chip::DeviceLayer::Internal::ESP32Utils::MapError(__err);                                                       \
+        }                                                                                                                          \
+    } while (false)
 
 } // namespace Internal
 } // namespace DeviceLayer

commit 6cb488591411aac506cd3c0364f55b0ba9b7d640

Fix ESP32 storage
#### Problem

Commit 162366f0 (#8469) broke persistent storage on ESP32.

#### Change overview

- Fixed return value of `ESP32Config::EnsureNamespace()`.
- Fixed a copy-paste mistake (`NVS_READONLY` for `NVS_READWRITE`)
  in `KeyValueStoreManagerImpl::_Put()`.
- Added string presentation of ESP32 errors.

Fixes #8572 _ESP32 storage probably broken_
Fixes #8569 _ESP32: All clusters app fails to boot up._

#### Testing

Manual on M5Stack

src/platform/ESP32/ESP32Config.cpp
@@ -404,6 +404,7 @@ CHIP_ERROR ESP32Config::EnsureNamespace(const char * ns)
     {
         ReturnErrorOnFailure(handle.Open(ns, NVS_READWRITE));
         ReturnMappedErrorOnFailure(nvs_commit(handle));
+        return CHIP_NO_ERROR;
     }
     return err;
 }

src/platform/ESP32/ESP32Utils.cpp
@@ -313,3 +313,43 @@ CHIP_ERROR ESP32Utils::MapError(esp_err_t error)
     }
     return ChipError::Encapsulate(ChipError::Range::kPlatform, error);
 }
+
+/**
+ * Given an OpenChip error value that represents an OpenThread error, returns a
+ * human-readable NULL-terminated C string describing the error.
+ *
+ * @param[in] buf                   Buffer into which the error string will be placed.
+ * @param[in] bufSize               Size of the supplied buffer in bytes.
+ * @param[in] err                   The error to be described.
+ *
+ * @return true                     If a description string was written into the supplied buffer.
+ * @return false                    If the supplied error was not an OpenThread error.
+ *
+ */
+bool ESP32Utils::FormatError(char * buf, uint16_t bufSize, CHIP_ERROR err)
+{
+    if (!ChipError::IsRange(ChipError::Range::kPlatform, err))
+    {
+        return false;
+    }
+
+#if CHIP_CONFIG_SHORT_ERROR_STR
+    const char * desc = NULL;
+#else  // CHIP_CONFIG_SHORT_ERROR_STR
+    const char * desc = esp_err_to_name((esp_err_t) ChipError::GetValue(err));
+#endif // CHIP_CONFIG_SHORT_ERROR_STR
+
+    chip::FormatError(buf, bufSize, "OpenThread", err, desc);
+
+    return true;
+}
+
+/**
+ * Register a text error formatter for OpenThread errors.
+ */
+void ESP32Utils::RegisterESP32ErrorFormatter()
+{
+    static ErrorFormatter sErrorFormatter = { ESP32Utils::FormatError, NULL };
+
+    RegisterErrorFormatter(&sErrorFormatter);
+}

src/platform/ESP32/ESP32Utils.h
@@ -47,6 +47,8 @@ public:
     static CHIP_ERROR ClearWiFiStationProvision(void);
 
     static CHIP_ERROR MapError(esp_err_t error);
+    static void RegisterESP32ErrorFormatter();
+    static bool FormatError(char * buf, uint16_t bufSize, CHIP_ERROR err);
 };
 
 #define ReturnMappedErrorOnFailure(expr)                                                                                           \

src/platform/ESP32/KeyValueStoreManagerImpl.cpp
@@ -63,7 +63,7 @@ CHIP_ERROR KeyValueStoreManagerImpl::_Put(const char * key, const void * value, 
     VerifyOrReturnError(value, CHIP_ERROR_INVALID_ARGUMENT);
 
     Internal::ScopedNvsHandle handle;
-    ReturnErrorOnFailure(handle.Open(kNamespace, NVS_READONLY));
+    ReturnErrorOnFailure(handle.Open(kNamespace, NVS_READWRITE));
 
     ReturnMappedErrorOnFailure(nvs_set_blob(handle, key, value, value_size));
 

src/platform/ESP32/PlatformManagerImpl.cpp
@@ -61,6 +61,9 @@ CHIP_ERROR PlatformManagerImpl::_InitChipStack(void)
     uint8_t ap_mac[6];
     wifi_mode_t mode;
 
+    // Arrange for CHIP-encapsulated ESP32 errors to be translated to text
+    Internal::ESP32Utils::RegisterESP32ErrorFormatter();
+
     // Make sure the LwIP core lock has been initialized
     ReturnErrorOnFailure(Internal::InitLwIPCoreLock());
 