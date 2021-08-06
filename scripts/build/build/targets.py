4f75167f5674ada5f7f742bd2a67c3edd5743212
Andrei Litvin <andrei@andy314.com>
2021-08-06 12:58:56 -0400

    Add esp32 flash script generation on more apps, update example_build (#8813)
    * Add flash script to esp32 lock app
    
    * Add C3devkit for esp32 boars compilation targets, added shell and flash bundle for esp32 shell
    
    * Add bridge app compilation, fix filename in shell app for esp32 builds
    
    * Add gitignore for esp32 bridgeapp - SDK config is modified at build time
    
    * Add newline at end  of cmake files
    
    * Add build to gitignore

--- scripts/build/build/targets.py
+++ scripts/build/build/targets.py
@@ -50,6 +50,7 @@ class Board(IntEnum):
   # ESP32 platform
   M5STACK = auto()
   DEVKITC = auto()
+  C3DEVKIT = auto()
 
   # EFR32 platform
   BRD4161A = auto()
@@ -78,6 +79,7 @@ class Application(IntEnum):
   WINDOW_COVERING = auto()
   SHELL = auto()
   CHIP_TOOL = auto()
+  BRIDGE = auto()
 
   @property
   def ArgName(self):
