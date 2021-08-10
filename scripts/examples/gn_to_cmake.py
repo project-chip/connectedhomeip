08929cbc3bd2d4051657e81a0677eb1e6739e288
Hui Li <xylophone21@gmail.com>
2021-08-10 20:46:58 +0800

    follow matter Style

--- scripts/examples/gn_to_cmake.py
+++ scripts/examples/gn_to_cmake.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 
-"""
-Usage: gn_to_cmake.py <json_file_name>
+#
+#    Copyright (c) 2021-2021 Project CHIP Authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
 
-gn gen out/config --ide=json --json-ide-script=//scripts/examples/gn_to_cmake.py
+##
+#    @file
+# Usage: gn_to_cmake.py <json_file_name>
 
-or
+# gn gen out/config --ide=json --json-ide-script=//scripts/examples/gn_to_cmake.py
 
-gn gen out/config --ide=json
-python gn/gn_to_cmake.py out/project.json
-"""
+# or
+
+# gn gen out/config --ide=json
+# python gn/gn_to_cmake.py out/project.json
+#
 
 
 import itertools
