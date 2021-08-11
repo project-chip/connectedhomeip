16d60dea9552e8ff53ba3d7b62990bce4d6a52ae
ATmobica <artur.tynecki@mobica.com>
2021-08-11 13:24:41 +0000

    Revert InetLayer changes

--- src/inet/InetLayer.cpp
+++ src/inet/InetLayer.cpp
@@ -348,7 +348,7 @@ CHIP_ERROR InetLayer::Shutdown()
         TCPEndPoint::sPool.ForEachActiveObject([&](TCPEndPoint * lEndPoint) {
             if ((lEndPoint != nullptr) && lEndPoint->IsCreatedByInetLayer(*this))
             {
-                lEndPoint->Free();
+                lEndPoint->Abort();
             }
             return true;
         });
@@ -359,7 +359,7 @@ CHIP_ERROR InetLayer::Shutdown()
         UDPEndPoint::sPool.ForEachActiveObject([&](UDPEndPoint * lEndPoint) {
             if ((lEndPoint != nullptr) && lEndPoint->IsCreatedByInetLayer(*this))
             {
-                lEndPoint->Free();
+                lEndPoint->Close();
             }
             return true;
         });
