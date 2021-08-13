acaa77443335d8af605cbfbb03587b68bde209c1
Alex Kolosov <rikorsev@gmail.com>
2021-08-13 19:40:32 +0300

    Fix autopep8 check

--- scripts/build/builders/telink.py
+++ scripts/build/builders/telink.py
@@ -20,80 +20,93 @@ from enum import Enum, auto
 
 from .builder import Builder
 
+
 class TelinkApp(Enum):
-  LIGHT = auto()
-  LOCK = auto()
-  SHELL = auto()
+    LIGHT = auto()
+    LOCK = auto()
+    SHELL = auto()
 
-  def ExampleName(self):
-    if self == TelinkApp.LIGHT:
-      return 'lighting-app'
-    else:
-      raise Exception('Unknown app type: %r' % self)
+    def ExampleName(self):
+        if self == TelinkApp.LIGHT:
+            return 'lighting-app'
+        else:
+            raise Exception('Unknown app type: %r' % self)
 
-  def AppNamePrefix(self):
-    if self == TelinkApp.LIGHT:
-      return 'chip-telink-lighting-example'
-    else:
-      raise Exception('Unknown app type: %r' % self)
+    def AppNamePrefix(self):
+        if self == TelinkApp.LIGHT:
+            return 'chip-telink-lighting-example'
+        else:
+            raise Exception('Unknown app type: %r' % self)
+
 
 class TelinkBoard(Enum):
-  TLSR9518ADK80D = auto()
+    TLSR9518ADK80D = auto()
 
-  def GnArgName(self):
-    if self == TelinkBoard.TLSR9518ADK80D:
-      return 'tlsr9518adk80d'
-    else:
-      raise Exception('Unknown board type: %r' % self)
+    def GnArgName(self):
+        if self == TelinkBoard.TLSR9518ADK80D:
+            return 'tlsr9518adk80d'
+        else:
+            raise Exception('Unknown board type: %r' % self)
 
 
 class TelinkBuilder(Builder):
 
-  def __init__(self,
-               root,
-               runner,
-               output_prefix: str,
-               app: TelinkApp = TelinkApp.LIGHT,
-               board: TelinkBoard = TelinkBoard.TLSR9518ADK80D):
-    super(TelinkBuilder, self).__init__(root, runner, output_prefix)
-    self.app = app
-    self.board = board
+    def __init__(self,
+                 root,
+                 runner,
+                 output_prefix: str,
+                 app: TelinkApp = TelinkApp.LIGHT,
+                 board: TelinkBoard = TelinkBoard.TLSR9518ADK80D):
+        super(TelinkBuilder, self).__init__(root, runner, output_prefix)
+        self.app = app
+        self.board = board
 
-  def generate(self):
+    def generate(self):
 
-    if not os.path.exists(self.output_dir):
+        if not os.path.exists(self.output_dir):
 
-        if not self._runner.dry_run:
+            if not self._runner.dry_run:
 
-          # Check Zephyr base
-          if 'TELINK_ZEPHYR_BASE' not in os.environ:
-              raise Exception("Telink builds require TELINK_ZEPHYR_BASE to be set")
+                # Check Zephyr base
+                if 'TELINK_ZEPHYR_BASE' not in os.environ:
+                    raise Exception(
+                        "Telink builds require TELINK_ZEPHYR_BASE to be set")
 
-          # Check Telink toolchain
-          if 'TELINK_TOOLCHAIN_PATH' not in os.environ:
-              raise Exception("Telink requires TELINK_TOOLCHAIN_PATH to be set")
+                # Check Telink toolchain
+                if 'TELINK_TOOLCHAIN_PATH' not in os.environ:
+                    raise Exception(
+                        "Telink requires TELINK_TOOLCHAIN_PATH to be set")
 
-        cmd = '''
+            cmd = '''
 source "$TELINK_ZEPHYR_BASE/zephyr-env.sh";
 export ZEPHYR_TOOLCHAIN_VARIANT=cross-compile;
 export CROSS_COMPILE=$TELINK_TOOLCHAIN_PATH/riscv32-elf-;
 west build -d {outdir} -b {board} {sourcedir}
         '''.format(
-            outdir = shlex.quote(self.output_dir),
-            board = self.board.GnArgName(),
-            sourcedir=shlex.quote(os.path.join(self.root, 'examples', self.app.ExampleName(), 'telink'))
-        ).strip()
+                outdir=shlex.quote(
+                    self.output_dir), board=self.board.GnArgName(), sourcedir=shlex.quote(
+                    os.path.join(
+                        self.root, 'examples', self.app.ExampleName(), 'telink'))).strip()
 
-        self._Execute(['bash', '-c', cmd], title='Generating ' + self.identifier)
+            self._Execute(['bash', '-c', cmd],
+                          title='Generating ' + self.identifier)
 
+    def _build(self):
+        logging.info('Compiling Telink at %s', self.output_dir)
 
-  def _build(self):
-    logging.info('Compiling Telink at %s', self.output_dir)
+        self._Execute(['ninja', '-C', self.output_dir],
+                      title='Building ' + self.identifier)
 
-    self._Execute(['ninja', '-C', self.output_dir], title='Building ' + self.identifier)
-
-  def build_outputs(self):
-    return {
-        '%s.elf' % self.app.AppNamePrefix(): os.path.join(self.output_dir, 'zephyr', 'zephyr.elf'),
-        '%s.map' % self.app.AppNamePrefix(): os.path.join(self.output_dir, 'zephyr', 'zephyr.map'),
-    }
+    def build_outputs(self):
+        return {
+            '%s.elf' %
+            self.app.AppNamePrefix(): os.path.join(
+                self.output_dir,
+                'zephyr',
+                'zephyr.elf'),
+            '%s.map' %
+            self.app.AppNamePrefix(): os.path.join(
+                self.output_dir,
+                'zephyr',
+                'zephyr.map'),
+        }
