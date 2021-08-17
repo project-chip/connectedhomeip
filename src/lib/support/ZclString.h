3fe1171284e9bce5e1c2abdf2e9e7ba474db7b7d
Restyled.io <commits@restyled.io>
2021-08-17 18:33:47 +0000

    Restyled by whitespace

--- src/lib/support/ZclString.h
+++ src/lib/support/ZclString.h
@@ -23,7 +23,7 @@ namespace chip {
 /**
  * @brief Create ZCL string (pascal string) from char string.
  * The maximum size of the ZCL string is defined by the kBufferMaximumSize.
- * In case char string exceeds the maximum of size ZCL string, the string will be 0-sized 
+ * In case char string exceeds the maximum of size ZCL string, the string will be 0-sized
  * and the method will return an error.
  */
 extern CHIP_ERROR MakeZclCharString(MutableByteSpan & buffer, const char * cString);
