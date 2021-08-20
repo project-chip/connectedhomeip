cee437a495f451358172787abbe67b98e9fbdfaf
Alex Kolosov <rikorsev@gmail.com>
2021-08-20 12:16:28 +0300

    [Telink]: Fix deprecated macro usage warning

--- examples/lighting-app/telink/include/AppConfig.h
+++ examples/lighting-app/telink/include/AppConfig.h
@@ -19,6 +19,8 @@
 #pragma once
 
 // ---- Lighting Example App Config ----
+
+// Buttons config
 #define BUTTON_PORT DT_LABEL(DT_NODELABEL(gpioc))
 
 #define BUTTON_PIN_1 2
@@ -26,6 +28,10 @@
 #define BUTTON_PIN_4 1
 #define BUTTON_PIN_2 0
 
+// LEDs config
+// System led config
 #define SYSTEM_STATE_LED_PIN 7
-#define LIGHTING_PWM_DEVICE DT_PWMS_LABEL(DT_ALIAS(pwm_led0))
+
+// Lighting LED config
+#define LIGHTING_PWM_DEVICE DEVICE_DT_GET(DT_PWMS_CTLR(DT_ALIAS(pwm_led0)))
 #define LIGHTING_PWM_CHANNEL DT_PWMS_CHANNEL(DT_ALIAS(pwm_led0))
