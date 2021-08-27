f4c307fc025825f56773fc3ab44fbbf250269bf6
Boris Zbarsky <bzbarsky@apple.com>
2021-08-27 11:39:00 -0400

    Add non-copying string and byte getters on a TLV reader. (#9249)
    * Add non-copying string and byte getters on a TLV reader.
    
    We have existing callsites that are doing what GetString/GetBytes say to do and call GetLength() to decide how big a buffer to allocate.  But the return value from GetLength() doesn't have to be sane, so we end up allocating buffers with attacker-controlled sizes.
    
    The changes here are:
    
    1) Add ContiguousBufferTLVReader that is always backed by a single
       buffer, so it can guarantee that the in-place view makes sense.
    
    2) Use the new class in setup payload parsers, fixing various bugs in
       the process.
    
    Fixes https://github.com/project-chip/connectedhomeip/issues/9009
    
    * Addressing review comments
    
    * More review comments
    
    * Bump Darwin build job bootstrap timeout to match other Darwin jobs.
    
    10 minutes keeps timing out.

--- src/setup_payload/QRCodeSetupPayloadParser.cpp
+++ src/setup_payload/QRCodeSetupPayloadParser.cpp
@@ -65,7 +65,8 @@ static CHIP_ERROR readBits(std::vector<uint8_t> buf, size_t & index, uint64_t & 
     return CHIP_NO_ERROR;
 }
 
-static CHIP_ERROR openTLVContainer(TLV::TLVReader & reader, TLV::TLVType type, uint64_t tag, TLV::TLVReader & containerReader)
+static CHIP_ERROR openTLVContainer(TLV::ContiguousBufferTLVReader & reader, TLV::TLVType type, uint64_t tag,
+                                   TLV::ContiguousBufferTLVReader & containerReader)
 {
     VerifyOrReturnError(reader.GetType() == type, CHIP_ERROR_INVALID_ARGUMENT);
     VerifyOrReturnError(reader.GetTag() == tag, CHIP_ERROR_INVALID_ARGUMENT);
@@ -77,17 +78,13 @@ static CHIP_ERROR openTLVContainer(TLV::TLVReader & reader, TLV::TLVType type, u
     return CHIP_NO_ERROR;
 }
 
-static CHIP_ERROR retrieveOptionalInfoString(TLV::TLVReader & reader, OptionalQRCodeInfo & info)
+static CHIP_ERROR retrieveOptionalInfoString(TLV::ContiguousBufferTLVReader & reader, OptionalQRCodeInfo & info)
 {
-    const uint32_t valLength = reader.GetLength();
-    chip::Platform::ScopedMemoryBuffer<char> value;
-    value.Alloc(valLength + 1);
-    VerifyOrReturnError(value, CHIP_ERROR_NO_MEMORY);
-
-    ReturnErrorOnFailure(reader.GetString(value.Get(), valLength + 1));
+    Span<const char> data;
+    ReturnErrorOnFailure(reader.GetStringView(data));
 
     info.type = optionalQRCodeInfoTypeString;
-    info.data = std::string(value.Get());
+    info.data = std::string(data.data(), data.size());
 
     return CHIP_NO_ERROR;
 }
@@ -136,7 +133,8 @@ static CHIP_ERROR retrieveOptionalInfoUInt64(TLV::TLVReader & reader, OptionalQR
     return CHIP_NO_ERROR;
 }
 
-static CHIP_ERROR retrieveOptionalInfo(TLV::TLVReader & reader, OptionalQRCodeInfo & info, optionalQRCodeInfoType type)
+static CHIP_ERROR retrieveOptionalInfo(TLV::ContiguousBufferTLVReader & reader, OptionalQRCodeInfo & info,
+                                       optionalQRCodeInfoType type)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
 
@@ -156,7 +154,8 @@ static CHIP_ERROR retrieveOptionalInfo(TLV::TLVReader & reader, OptionalQRCodeIn
     return err;
 }
 
-static CHIP_ERROR retrieveOptionalInfo(TLV::TLVReader & reader, OptionalQRCodeInfoExtension & info, optionalQRCodeInfoType type)
+static CHIP_ERROR retrieveOptionalInfo(TLV::ContiguousBufferTLVReader & reader, OptionalQRCodeInfoExtension & info,
+                                       optionalQRCodeInfoType type)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
 
@@ -184,7 +183,7 @@ static CHIP_ERROR retrieveOptionalInfo(TLV::TLVReader & reader, OptionalQRCodeIn
     return err;
 }
 
-CHIP_ERROR QRCodeSetupPayloadParser::retrieveOptionalInfos(SetupPayload & outPayload, TLV::TLVReader & reader)
+CHIP_ERROR QRCodeSetupPayloadParser::retrieveOptionalInfos(SetupPayload & outPayload, TLV::ContiguousBufferTLVReader & reader)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
     while (err == CHIP_NO_ERROR)
@@ -242,7 +241,7 @@ CHIP_ERROR QRCodeSetupPayloadParser::parseTLVFields(SetupPayload & outPayload, u
     {
         return CHIP_ERROR_INVALID_ARGUMENT;
     }
-    TLV::TLVReader rootReader;
+    TLV::ContiguousBufferTLVReader rootReader;
     rootReader.Init(tlvDataStart, tlvDataLengthInBytes);
     ReturnErrorOnFailure(rootReader.Next());
 
@@ -251,7 +250,7 @@ CHIP_ERROR QRCodeSetupPayloadParser::parseTLVFields(SetupPayload & outPayload, u
         return CHIP_ERROR_INVALID_ARGUMENT;
     }
 
-    TLV::TLVReader innerStructureReader;
+    TLV::ContiguousBufferTLVReader innerStructureReader;
     ReturnErrorOnFailure(openTLVContainer(rootReader, TLV::kTLVType_Structure, TLV::AnonymousTag, innerStructureReader));
     ReturnErrorOnFailure(innerStructureReader.Next());
     err = retrieveOptionalInfos(outPayload, innerStructureReader);
