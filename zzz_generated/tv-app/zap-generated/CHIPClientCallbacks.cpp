a8f02e21d89aa1df38e352a064ae2f0cfe874c88
Trevor Holbrook <tholbrook@google.com>
2021-09-02 07:20:46 -0700

    move PrepareListFromTLV into its own file (#9346)
    * move PrepareListFromTLV into its own file
    
    some applications might not use this function, which could cause
    compiler errors. Moving it to its own file avoids this issue.
    
    * regenerate to undo non-deterministic changes to lighting-app
    
    * move PrepareListFromTLV definition into a cpp file
    
    * modify darwin file to include new prepare-list.cpp
    
    * merge and regen to fix zap CI fail

--- zzz_generated/tv-app/zap-generated/CHIPClientCallbacks.cpp
+++ zzz_generated/tv-app/zap-generated/CHIPClientCallbacks.cpp
@@ -28,6 +28,7 @@
 #include <app/util/af.h>
 #include <app/util/attribute-list-byte-span.h>
 #include <app/util/basic-types.h>
+#include <app/util/prepare-list.h>
 #include <core/CHIPEncoding.h>
 #include <support/SafeInt.h>
 #include <support/TypeTraits.h>
@@ -348,31 +349,6 @@ bool emberAfDiscoverCommandsReceivedResponseCallback(ClusterId clusterId, uint16
     Callback::Callback<DefaultSuccessCallback> * cb = Callback::Callback<DefaultSuccessCallback>::FromCancelable(onSuccessCallback);
     cb->mCall(cb->mContext);
     return true;
-}
-
-static EmberAfStatus PrepareListFromTLV(TLV::TLVReader * tlvData, const uint8_t *& message, uint16_t & messageLen)
-{
-    CHIP_ERROR tlvError = CHIP_NO_ERROR;
-    TLV::TLVReader reader;
-    TLV::TLVType type;
-    reader.Init(*tlvData);
-    reader.EnterContainer(type);
-    tlvError = reader.Next();
-    if (tlvError != CHIP_NO_ERROR && tlvError != CHIP_END_OF_TLV && CanCastTo<uint16_t>(reader.GetLength()))
-    {
-        return EMBER_ZCL_STATUS_INVALID_VALUE;
-    }
-    if (tlvError == CHIP_NO_ERROR)
-    {
-        tlvError   = reader.GetDataPtr(message);
-        messageLen = static_cast<uint16_t>(reader.GetLength());
-    }
-    if (tlvError != CHIP_NO_ERROR)
-    {
-        return EMBER_ZCL_STATUS_INVALID_VALUE;
-    }
-    reader.ExitContainer(type);
-    return EMBER_ZCL_STATUS_SUCCESS;
 }
 
 void GeneralCommissioningClusterBasicCommissioningInfoListListAttributeFilter(TLV::TLVReader * tlvData,
