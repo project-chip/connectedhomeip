6e17e04172c7ed22965fde4c1c7845870f8e4077
Andrei Litvin <andy314@gmail.com>
2021-09-02 16:45:02 -0400

    Add trace.h header

---
+++ src/lib/support/logging/Trace.h
@@ -0,0 +1,75 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <lib/support/logging/CHIPLogging.h>
+
+namespace chip {
+namespace Logging {
+
+/// Provides the ability to add tracing code to methods. Will do
+/// ChipLog tracing if CHIP_CONFIG_ENABLE_FUNCT_ERROR_LOGGING is set
+/// and will be optimized away by the compiler if the define is not set
+///
+/// Typical usage:
+///     LogTracer tracer(__func__);
+///
+///     VerifyOrReturnError(somecondition, tracer.log(CHIP_ERROR_INVALID_ARGUMENT));
+///     VerifyOrReturnError(otherCondition, tracer.log(CHIP_ERROR_INVALID_ARGUMENT, "othercondition"));
+///
+///     return CHIP_NO_ERROR;
+class LogTracer
+{
+#if CHIP_CONFIG_ENABLE_FUNCT_ERROR_LOGGING
+public:
+    LogTracer(const char * name) : mName(name) {}
+    inline CHIP_ERROR logError(CHIP_ERROR err)
+    {
+        ChipLogError(NotSpecified, "%s in %s", ErrorStr(err), mName);
+        return err;
+    }
+
+    inline CHIP_ERROR logError(CHIP_ERROR err, const char * context)
+    {
+        ChipLogError(NotSpecified, "%s/%s in %s", ErrorStr(err), context, mName);
+        return err;
+    }
+
+private:
+    const char * mName;
+
+#else
+public:
+    LogTracer(const char *) {}
+    inline constexpr CHIP_ERROR logError(CHIP_ERROR err) { return err; }
+    inline constexpr CHIP_ERROR logError(CHIP_ERROR err, const char *) { return err; }
+#endif
+};
+
+} // namespace Logging
+} // namespace chip
+
+#define ReturnTracedErrorOnFailure(expr, tracer)                                                                                   \
+    do                                                                                                                             \
+    {                                                                                                                              \
+        auto __err = (expr);                                                                                                       \
+        if (!::chip::ChipError::IsSuccess(__err))                                                                                  \
+        {                                                                                                                          \
+            return tracer.logError(__err);                                                                                         \
+        }                                                                                                                          \
+    } while (false)
