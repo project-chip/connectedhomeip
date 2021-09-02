bea9e2d64d00509863d6017be0cee547d10168be
ATmobica <artur.tynecki@mobica.com>
2021-09-02 11:50:27 +0000

    Udpate unit tests app with a new logging approach

--- src/test_driver/mbed/main/main.cpp
+++ src/test_driver/mbed/main/main.cpp
@@ -17,33 +17,48 @@
  */
 
 #include "netsocket/WiFiInterface.h"
-#include <platform/CHIPDeviceLayer.h>
+#include <platform/mbed/Logging.h>
 #include <support/UnitTestRegistration.h>
 #include <support/logging/CHIPLogging.h>
 
 using namespace ::chip;
-using namespace ::chip::Inet;
-using namespace ::chip::DeviceLayer;
-WiFiInterface * _interface;
+using namespace ::chip::Logging::Platform;
+
 #define concat_(x, y) x##y
 #define concat(x, y) concat_(x, y)
 #define SECURITY concat(NSAPI_SECURITY_, MBED_CONF_NSAPI_DEFAULT_WIFI_SECURITY)
 
-#ifdef MBED_CONF_MBED_TRACE_ENABLE
-#include "mbed-trace/mbed_trace.h"
-#endif
-
 int main()
 {
-#ifdef MBED_CONF_MBED_TRACE_ENABLE
-    mbed_trace_init();
-    mbed_trace_config_set(TRACE_ACTIVE_LEVEL_ALL | TRACE_MODE_COLOR);
-#endif
+    mbed_logging_init();
 
     ChipLogProgress(NotSpecified, "Starting CHIP tests!");
-    _interface = WiFiInterface::get_default_instance();
-    _interface->connect(MBED_CONF_NSAPI_DEFAULT_WIFI_SSID, MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD, SECURITY);
-    _interface->get_connection_status();
+    ChipLogProgress(NotSpecified, "Connect to the network...");
+    auto interface = WiFiInterface::get_default_instance();
+    if (interface != nullptr)
+    {
+        auto err = interface->connect(MBED_CONF_NSAPI_DEFAULT_WIFI_SSID, MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD, SECURITY);
+        if (err != NSAPI_ERROR_OK)
+        {
+            ChipLogProgress(NotSpecified, "WARNING: WiFi network connection failed [%d]", err);
+        }
+        else
+        {
+            auto connection_status = interface->get_connection_status();
+            if (connection_status != NSAPI_STATUS_LOCAL_UP && connection_status != NSAPI_STATUS_GLOBAL_UP)
+            {
+                ChipLogProgress(NotSpecified, "WARNING: WiFi network connection failed. Connection status [%d]", connection_status);
+            }
+            else
+            {
+                ChipLogProgress(NotSpecified, "WiFi network connection success");
+            }
+        }
+    }
+    else
+    {
+        ChipLogProgress(NotSpecified, "WARNING: WiFi interface not available");
+    }
 
     int status = RunRegisteredUnitTests();
     ChipLogProgress(NotSpecified, "CHIP test status: %d", status);
