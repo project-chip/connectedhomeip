8a76647049ba4e4159a4f5e31a9449edfaa26bbc
Thomas Lykkeberg <tlykkeberg@grundfos.com>
2021-09-03 11:36:27 +0200

    Added the Linux platform as a target for the pump-app

---
+++ examples/pump-app/linux/main.cpp
@@ -0,0 +1,103 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <platform/CHIPDeviceLayer.h>
+#include <platform/PlatformManager.h>
+
+#include <app/common/gen/enums.h>
+#include <app/common/gen/ids/Attributes.h>
+#include <app/common/gen/ids/Clusters.h>
+#include <app/chip-zcl-zpro-codec.h>
+#include <app/util/af-types.h>
+#include <app/util/af.h>
+#include <app/util/attribute-storage.h>
+#include <app/util/util.h>
+
+#include "PumpManager.h"
+
+#include <AppMain.h>
+
+#if defined(PW_RPC_ENABLED)
+#include "Rpc.h"
+#endif // PW_RPC_ENABLED
+
+#include <cassert>
+#include <iostream>
+
+using namespace chip;
+using namespace chip::app::Clusters;
+using namespace chip::DeviceLayer;
+
+void emberAfPostAttributeChangeCallback(EndpointId endpoint, ClusterId clusterId, AttributeId attributeId, uint8_t mask,
+                                        uint16_t manufacturerCode, uint8_t type, uint16_t size, uint8_t * value)
+{
+    if (clusterId == OnOff::Id && attributeId == OnOff::Attributes::Ids::OnOff)
+    {
+        if (*value)
+        {
+            PumpMgr().InitiateAction(0, PumpManager::START_ACTION);
+        }
+        else
+        {
+            PumpMgr().InitiateAction(0, PumpManager::STOP_ACTION);
+        }
+    }
+    else if (clusterId == LevelControl::Id && attributeId == LevelControl::Attributes::Ids::CurrentLevel)
+    {
+        ChipLogProgress(Zcl, "[pump-app] Cluster LevelControl: attribute CurrentLevel set to %" PRIu8, *value);
+    }
+}
+
+/** @brief OnOff Cluster Init
+ *
+ * This function is called when a specific cluster is initialized. It gives the
+ * application an opportunity to take care of cluster initialization procedures.
+ * It is called exactly once for each endpoint where cluster is present.
+ *
+ * @param endpoint   Ver.: always
+ *
+ * TODO Issue #3841
+ * emberAfOnOffClusterInitCallback happens before the stack initialize the cluster
+ * attributes to the default value.
+ * The logic here expects something similar to the deprecated Plugins callback
+ * emberAfPluginOnOffClusterServerPostInitCallback.
+ *
+ */
+void emberAfOnOffClusterInitCallback(EndpointId endpoint)
+{
+    //GetAppTask().UpdateClusterState();
+}
+
+void emberAfPumpConfigurationAndControlClusterInitCallback(chip::EndpointId endpoint)
+{
+    // TODO: Setup the default values in the cluster for this specific application
+    //GetAppTask().UpdateClusterState();
+}
+
+int main(int argc, char * argv[])
+{
+    if (ChipLinuxAppInit(argc, argv) != 0)
+    {
+        return -1;
+    }
+
+    PumpMgr().Init();
+    ChipLinuxAppMainLoop();
+
+    return 0;
+}
