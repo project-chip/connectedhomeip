45a56c4f03606dc47839d6f51d71dac4677119a5
Trevor Holbrook <tholbrook@google.com>
2021-09-08 00:22:28 +0000

    update copyright dates

--- examples/ota-requestor-app/linux/PersistentStorage.h
+++ examples/ota-requestor-app/linux/PersistentStorage.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2020 Project CHIP Authors
+ *   Copyright (c) 2021 Project CHIP Authors
  *   All rights reserved.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
