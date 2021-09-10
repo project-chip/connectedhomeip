db1d87fbd43d9111c0f2ee618f1f3a5bdc7ae13d
Sharad Binjola <sharadb@amazon.com>
2021-09-10 19:22:32 +0000

    Fixing compiler errors

--- examples/tv-casting-app/linux/main.cpp
+++ examples/tv-casting-app/linux/main.cpp
@@ -123,7 +123,7 @@ void RequestUserDirectedCommissioning(System::SocketEvents events, intptr_t data
     int selectedCommissionerNumber = CHIP_DEVICE_CONFIG_MAX_DISCOVERED_NODES;
     scanf("%d", &selectedCommissionerNumber);
     printf("%d\n", selectedCommissionerNumber);
-    chip::DeviceLayer::SystemLayerSockets().StopWatchingSocket(&token);
+    chip::DeviceLayer::SystemLayerSockets().StopWatchingSocket(&gToken);
 
     const Mdns::DiscoveredNodeData * selectedCommissioner =
         gCommissionableNodeController.GetDiscoveredCommissioner(selectedCommissionerNumber - 1);
@@ -155,10 +155,10 @@ void InitCommissioningFlow(intptr_t commandArg)
         int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
         VerifyOrReturn(fcntl(0, F_SETFL, flags | O_NONBLOCK) == 0,
                        ChipLogError(AppServer, "Could not set non-blocking mode for user input!"));
-        ReturnOnFailure(chip::DeviceLayer::SystemLayerSockets().StartWatchingSocket(STDIN_FILENO, &token));
+        ReturnOnFailure(chip::DeviceLayer::SystemLayerSockets().StartWatchingSocket(STDIN_FILENO, &gToken));
         ReturnOnFailure(
-            chip::DeviceLayer::SystemLayerSockets().SetCallback(token, RequestUserDirectedCommissioning, (intptr_t) NULL));
-        ReturnOnFailure(chip::DeviceLayer::SystemLayerSockets().RequestCallbackOnPendingRead(token));
+            chip::DeviceLayer::SystemLayerSockets().SetCallback(gToken, RequestUserDirectedCommissioning, (intptr_t) NULL));
+        ReturnOnFailure(chip::DeviceLayer::SystemLayerSockets().RequestCallbackOnPendingRead(gToken));
     }
     else
     {
