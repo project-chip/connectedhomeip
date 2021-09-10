9303f9ca260e6fbd00939d05d25f8d1169dff81f
ATmobica <artur.tynecki@mobica.com>
2021-09-10 13:21:00 +0000

    Changes for Mbed OS update

--- examples/shell/mbed/main/main.cpp
+++ examples/shell/mbed/main/main.cpp
@@ -15,20 +15,19 @@
  *    limitations under the License.
  */
 
-#include <lib/core/CHIPCore.h>
-#include <lib/shell/Engine.h>
-#include <platform/mbed/Logging.h>
-#include <support/logging/CHIPLogging.h>
-
 #include "ChipShellMbedCollection.h"
+#include "mbedtls/platform.h"
 #include "rtos/Thread.h"
 #include <ChipShellCollection.h>
+#include <lib/core/CHIPCore.h>
+#include <lib/shell/Engine.h>
 #include <lib/support/CHIPMem.h>
+#include <lib/support/logging/CHIPLogging.h>
 #include <platform/CHIPDeviceLayer.h>
+#include <platform/mbed/Logging.h>
 
 using namespace ::chip;
 using namespace ::chip::Shell;
-using namespace ::chip::Platform;
 using namespace ::chip::DeviceLayer;
 using namespace ::chip::Logging::Platform;
 
@@ -37,21 +36,64 @@ rtos::Thread shellThread{ osPriorityNormal, CHIP_DEVICE_CONFIG_CHIP_TASK_STACK_S
 
 int main()
 {
+    int ret        = 0;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
     mbed_logging_init();
 
-    MemoryInit();
-    PlatformMgr().InitChipStack();
-    PlatformMgr().StartEventLoopTask();
+    ret = mbedtls_platform_setup(NULL);
+    if (ret)
+    {
+        ChipLogError(Shell, "Mbed TLS platform initialization failed [%d]", ret);
+        goto exit;
+    }
+
+    err = chip::Platform::MemoryInit();
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Memory initalization failed: %s", err.AsString());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+    ChipLogProgress(Shell, "Init CHIP Stack\r\n");
+    err = PlatformMgr().InitChipStack();
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Chip stack initalization failed: %s", err.AsString());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+#ifdef MBED_CONF_APP_BLE_DEVICE_NAME
+    err = ConnectivityMgr().SetBLEDeviceName(MBED_CONF_APP_BLE_DEVICE_NAME);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Set BLE device name failed: %s", err.AsString());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+#endif
+
 #if CHIP_DEVICE_CONFIG_ENABLE_WPA
     ConnectivityManagerImpl().StartWiFiManagement();
 #endif
 
-    // Initialize the default streamer that was linked.
-    const int rc = streamer_init(streamer_get());
-    if (rc != 0)
+    ChipLogProgress(NotSpecified, "Starting CHIP task");
+    err = PlatformMgr().StartEventLoopTask();
+    if (err != CHIP_NO_ERROR)
     {
-        ChipLogError(Shell, "Streamer initialization failed: %d", rc);
-        return rc;
+        ChipLogError(Shell, "Chip stack start failed: %s", err.AsString());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+    // Initialize the default streamer that was linked.
+    ret = streamer_init(streamer_get());
+    if (ret)
+    {
+        ChipLogError(Shell, "Streamer initialization failed [%d]", ret);
+        goto exit;
     }
 
     cmd_misc_init();
@@ -63,9 +105,14 @@ int main()
     auto error = shellThread.start([] { Engine::Root().RunMainLoop(); });
     if (error != osOK)
     {
-        ChipLogError(Shell, "Run shell thread failed: %d", (int) error);
-        return error;
+        ChipLogError(Shell, "Run shell thread failed [%d]", (int) error);
+        ret = EXIT_FAILURE;
+        goto exit;
     }
 
-    return 0;
+    ChipLogProgress(NotSpecified, "Mbed shell example application run");
+
+exit:
+    ChipLogProgress(NotSpecified, "Exited with code %d", ret);
+    return ret;
 }
