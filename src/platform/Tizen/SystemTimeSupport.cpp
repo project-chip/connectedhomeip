21936d7dbd55573306e076a0021751940853e371
hyunuktak <hyunuk.tak@samsung.com>
2021-09-13 15:04:21 +0900

    Implement system time support

--- src/platform/Tizen/SystemTimeSupport.cpp
+++ src/platform/Tizen/SystemTimeSupport.cpp
@@ -35,32 +35,92 @@ namespace Layer {
 
 uint64_t GetClock_Monotonic()
 {
-    return static_cast<uint64_t>(0);
+    std::chrono::microseconds epoch =
+        std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch());
+    // count() is nominally signed, but for a monotonic clock it cannot be
+    // negative.
+    return static_cast<uint64_t>(epoch.count());
 }
 
 uint64_t GetClock_MonotonicMS()
 {
-    return static_cast<uint64_t>(0);
+    std::chrono::milliseconds epoch =
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch());
+    // count() is nominally signed, but for a monotonic clock it cannot be
+    // negative.
+    return static_cast<uint64_t>(epoch.count());
 }
 
 uint64_t GetClock_MonotonicHiRes()
 {
-    return static_cast<uint64_t>(0);
+    std::chrono::microseconds epoch =
+        std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch());
+    // count() is nominally signed, but for a monotonic clock it cannot be
+    // negative.
+    return static_cast<uint64_t>(epoch.count());
 }
 
 CHIP_ERROR GetClock_RealTime(uint64_t & curTime)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    struct timeval tv;
+    if (gettimeofday(&tv, nullptr) != 0)
+    {
+        return MapErrorPOSIX(errno);
+    }
+    if (tv.tv_sec < CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    if (tv.tv_usec < 0)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    static_assert(CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD >= 0, "We might be letting through negative tv_sec values!");
+    curTime = (static_cast<uint64_t>(tv.tv_sec) * UINT64_C(1000000)) + static_cast<uint64_t>(tv.tv_usec);
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR GetClock_RealTimeMS(uint64_t & curTime)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    struct timeval tv;
+    if (gettimeofday(&tv, nullptr) != 0)
+    {
+        return MapErrorPOSIX(errno);
+    }
+    if (tv.tv_sec < CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    if (tv.tv_usec < 0)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    static_assert(CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD >= 0, "We might be letting through negative tv_sec values!");
+    curTime = (static_cast<uint64_t>(tv.tv_sec) * UINT64_C(1000)) + (static_cast<uint64_t>(tv.tv_usec) / 1000);
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR SetClock_RealTime(uint64_t newCurTime)
 {
-    return CHIP_ERROR_NOT_IMPLEMENTED;
+    struct timeval tv;
+    tv.tv_sec  = static_cast<time_t>(newCurTime / UINT64_C(1000000));
+    tv.tv_usec = static_cast<long>(newCurTime % UINT64_C(1000000));
+    if (settimeofday(&tv, nullptr) != 0)
+    {
+        return (errno == EPERM) ? CHIP_ERROR_ACCESS_DENIED : MapErrorPOSIX(errno);
+    }
+#if CHIP_PROGRESS_LOGGING
+    {
+        const time_t timep = tv.tv_sec;
+        struct tm calendar;
+        localtime_r(&timep, &calendar);
+        ChipLogProgress(
+            DeviceLayer,
+            "Real time clock set to %ld (%04" PRId16 "/%02" PRId8 "/%02" PRId8 " %02" PRId8 ":%02" PRId8 ":%02" PRId8 " UTC)",
+            tv.tv_sec, calendar.tm_year, calendar.tm_mon, calendar.tm_mday, calendar.tm_hour, calendar.tm_min, calendar.tm_sec);
+    }
+#endif // CHIP_PROGRESS_LOGGING
+    return CHIP_NO_ERROR;
 }
 
 } // namespace Layer
