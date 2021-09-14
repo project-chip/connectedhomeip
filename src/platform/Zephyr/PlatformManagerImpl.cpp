4f1bee1f8209d495b82095eb95428a85521437d3
Alex Kolosov <rikorsev@gmail.com>
2021-09-14 16:54:28 +0300

    [Telink]: Fix review remarks

--- src/platform/Zephyr/PlatformManagerImpl.cpp
+++ src/platform/Zephyr/PlatformManagerImpl.cpp
@@ -38,29 +38,43 @@ static K_THREAD_STACK_DEFINE(sChipThreadStack, CHIP_DEVICE_CONFIG_CHIP_TASK_STAC
 
 PlatformManagerImpl PlatformManagerImpl::sInstance{ sChipThreadStack };
 
+#if !CONFIG_NORDIC_SECURITY_BACKEND
 static int app_entropy_source(void * data, unsigned char * output, size_t len, size_t * olen)
 {
     const struct device * entropy = device_get_binding(DT_CHOSEN_ZEPHYR_ENTROPY_LABEL);
+    int ret                       = entropy_get_entropy(entropy, output, len);
 
-    SuccessOrExit(entropy_get_entropy(entropy, output, len));
+    if (ret == 0)
+    {
+        *olen = len;
+    }
+    else
+    {
+        *olen = 0;
+    }
 
-    *olen = len;
-
-exit:
-    return 0;
+    return ret;
 }
+#endif // !CONFIG_NORDIC_SECURITY_BACKEND
 
 CHIP_ERROR PlatformManagerImpl::_InitChipStack(void)
 {
     CHIP_ERROR err;
 
+#if !CONFIG_NORDIC_SECURITY_BACKEND
+    // Minimum required from source before entropy is released ( with mbedtls_entropy_func() ) (in bytes)
+    const size_t kThreshold = 16;
+#endif // !CONFIG_NORDIC_SECURITY_BACKEND
+
     // Initialize the configuration system.
     err = Internal::ZephyrConfig::Init();
     SuccessOrExit(err);
 
+#if !CONFIG_NORDIC_SECURITY_BACKEND
     // Add entropy source based on Zephyr entropy driver
-    err = chip::Crypto::add_entropy_source(app_entropy_source, NULL, 16);
+    err = chip::Crypto::add_entropy_source(app_entropy_source, NULL, kThreshold);
     SuccessOrExit(err);
+#endif // !CONFIG_NORDIC_SECURITY_BACKEND
 
     // Call _InitChipStack() on the generic implementation base class to finish the initialization process.
     err = Internal::GenericPlatformManagerImpl_Zephyr<PlatformManagerImpl>::_InitChipStack();
