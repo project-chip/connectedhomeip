885ac81e4941c8066b41ff4a699961178cad7be2
Boris Zbarsky <bzbarsky@apple.com>
2021-09-13 20:20:31 -0400

    Stop using the external attribute callback in lighting-app.
    Instead handle ethernet network diagnostics via an attribute interceptor.

--- src/app/clusters/ethernet_network_diagnostics_server/ethernet_network_diagnostics_server.cpp
+++ src/app/clusters/ethernet_network_diagnostics_server/ethernet_network_diagnostics_server.cpp
@@ -16,16 +16,21 @@
  */
 
 #include <app-common/zap-generated/attributes/Accessors.h>
+#include <app-common/zap-generated/ids/Attributes.h>
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/AttributeAccessInterceptor.h>
 #include <app/CommandHandler.h>
+#include <app/MessageDef/AttributeDataElement.h>
 #include <app/util/af.h>
 #include <app/util/attribute-storage.h>
 #include <lib/core/Optional.h>
+#include <platform/ConnectivityManager.h>
 
 using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::EthernetNetworkDiagnostics::Attributes;
+using chip::DeviceLayer::ConnectivityManager;
 
 namespace {
 
@@ -38,6 +43,9 @@ public:
     {}
 
     CHIP_ERROR ReadAttributeData(ClusterInfo & aClusterInfo, TLV::TLVWriter * aWriter, bool * aDataRead) override;
+
+private:
+    CHIP_ERROR ReadDataIfSupported(CHIP_ERROR (ConnectivityManager::*getter)(uint64_t &), TLV::TLVWriter * aWriter);
 };
 
 EthernetDiagosticsAttrInterceptor gAttrInterceptor;
@@ -45,10 +53,54 @@ EthernetDiagosticsAttrInterceptor gAttrInterceptor;
 CHIP_ERROR EthernetDiagosticsAttrInterceptor::ReadAttributeData(ClusterInfo & aClusterInfo, TLV::TLVWriter * aWriter,
                                                                 bool * aDataRead)
 {
-    *aDataRead = false;
+    if (aClusterInfo.mClusterId != EthernetNetworkDiagnostics::Id)
+    {
+        // We shouldn't have been called at all.
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    }
+
+    *aDataRead = true;
+    switch (aClusterInfo.mFieldId)
+    {
+    case Ids::PacketRxCount: {
+        return ReadDataIfSupported(&ConnectivityManager::GetEthPacketRxCount, aWriter);
+    }
+    case Ids::PacketTxCount: {
+        return ReadDataIfSupported(&ConnectivityManager::GetEthPacketTxCount, aWriter);
+    }
+    case Ids::TxErrCount: {
+        return ReadDataIfSupported(&ConnectivityManager::GetEthTxErrCount, aWriter);
+    }
+    case Ids::CollisionCount: {
+        return ReadDataIfSupported(&ConnectivityManager::GetEthCollisionCount, aWriter);
+    }
+    case Ids::OverrunCount: {
+        return ReadDataIfSupported(&ConnectivityManager::GetEthOverrunCount, aWriter);
+    }
+    default: {
+        *aDataRead = false;
+        break;
+    }
+    }
     return CHIP_NO_ERROR;
 }
 
+CHIP_ERROR EthernetDiagosticsAttrInterceptor::ReadDataIfSupported(CHIP_ERROR (ConnectivityManager::*getter)(uint64_t &),
+                                                                  TLV::TLVWriter * aWriter)
+{
+    uint64_t data;
+    CHIP_ERROR err = (DeviceLayer::ConnectivityMgr().*getter)(data);
+    if (err == CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE)
+    {
+        data = 0;
+    }
+    else if (err != CHIP_NO_ERROR)
+    {
+        return err;
+    }
+
+    return aWriter->Put(TLV::ContextTag(AttributeDataElement::kCsTag_Data), data);
+}
 } // anonymous namespace
 
 bool emberAfEthernetNetworkDiagnosticsClusterResetCountsCallback(EndpointId endpoint, app::CommandHandler * commandObj)
