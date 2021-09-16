5f931e0197683f0f24d977b4aa7763c7ecd6409b
sanghyukko <sanghyuk.ko@samsung.com>
2021-09-16 19:23:50 +0900

    Add basic cluster command.

--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/BasicClientFragment.kt
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/BasicClientFragment.kt
@@ -10,15 +10,12 @@ import chip.devicecontroller.ChipClusters
 import chip.devicecontroller.ChipClusters.BasicCluster
 import chip.devicecontroller.ChipDeviceController
 import com.google.chip.chiptool.ChipClient
+import com.google.chip.chiptool.GenericChipDeviceListener
 import com.google.chip.chiptool.R
 import com.google.chip.chiptool.util.DeviceIdUtil
-import kotlinx.android.synthetic.main.basic_client_fragment.basicClustCommandStatus
-import kotlinx.android.synthetic.main.basic_client_fragment.basicClusterDeviceIdEd
-import kotlinx.android.synthetic.main.basic_client_fragment.basicClusterFabricIdEd
-import kotlinx.android.synthetic.main.basic_client_fragment.view.basicClusterUpdateAddressBtn
-import kotlinx.android.synthetic.main.basic_client_fragment.view.readProducNameBtn
-import kotlinx.android.synthetic.main.basic_client_fragment.view.readUserLabelBtn
-import kotlinx.android.synthetic.main.basic_client_fragment.view.writeUserLabelBtn
+import kotlinx.android.synthetic.main.basic_client_fragment.*
+import kotlinx.android.synthetic.main.basic_client_fragment.view.*
+import kotlinx.android.synthetic.main.on_off_client_fragment.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -26,6 +23,9 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class BasicClientFragment : Fragment() {
+  private val deviceController: ChipDeviceController
+    get() = ChipClient.getDeviceController(requireContext())
+
   private val scope = CoroutineScope(Dispatchers.Main + Job())
 
   override fun onCreateView(
@@ -34,11 +34,44 @@ class BasicClientFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View {
     return inflater.inflate(R.layout.basic_client_fragment, container, false).apply {
+      deviceController.setCompletionListener(ChipControllerCallback())
 
-      basicClusterUpdateAddressBtn.setOnClickListener { scope.launch{ } }
-      readUserLabelBtn.setOnClickListener { scope.launch{ } }
-      writeUserLabelBtn.setOnClickListener { scope.launch{ } }
-      readProducNameBtn.setOnClickListener { scope.launch{ } }
+      basicClusterUpdateAddressBtn.setOnClickListener { scope.launch { updateAddressClick() }}
+      readUserLabelBtn.setOnClickListener { scope.launch { sendReadUserLabelCommandClick() }}
+      writeUserLabelBtn.setOnClickListener { scope.launch { sendWriteUserLabelCommandClick() }}
+      readProductNameBtn.setOnClickListener { scope.launch { sendReadProductNameCommandClick() }}
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    // TODO: use the fabric ID that was used to commission the device
+    val testFabricId = "0"//5544332211"
+    basicClusterFabricIdEd.setText(testFabricId)
+    basicClusterDeviceIdEd.setText(DeviceIdUtil.getLastDeviceId(requireContext()).toString())
+  }
+
+  inner class ChipControllerCallback : GenericChipDeviceListener() {
+    override fun onConnectDeviceComplete() {}
+
+    override fun onCommissioningComplete(nodeId: Long, errorCode: Int) {
+      Log.d(TAG, "onCommissioningComplete for nodeId $nodeId: $errorCode")
+    }
+
+    override fun onSendMessageComplete(message: String?) {
+      commandStatusTv.text = requireContext().getString(R.string.echo_status_response, message)
+    }
+
+    override fun onNotifyChipConnectionClosed() {
+      Log.d(TAG, "onNotifyChipConnectionClosed")
+    }
+
+    override fun onCloseBleComplete() {
+      Log.d(TAG, "onCloseBleComplete")
+    }
+
+    override fun onError(error: Throwable?) {
+      Log.d(TAG, "onError: $error")
     }
   }
 
@@ -47,10 +80,68 @@ class BasicClientFragment : Fragment() {
     scope.cancel()
   }
 
+  private fun updateAddressClick() {
+    try{
+      deviceController.updateDevice(
+              basicClusterFabricIdEd.text.toString().toULong().toLong(),
+              basicClusterDeviceIdEd.text.toString().toULong().toLong()
+      )
+      showMessage("Address update started")
+    } catch (ex: Exception) {
+      showMessage("Address update failed: $ex")
+    }
+  }
+
+  private suspend fun sendReadProductNameCommandClick() {
+    getBasicClusterForDevice().readProductNameAttribute(object : ChipClusters.CharStringAttributeCallback {
+      override fun onSuccess(value: String) {
+        showMessage("Read ProductName command success: value: $value")
+      }
+
+      override fun onError(ex: Exception) {
+        showMessage("Read ProductName command failure $ex")
+        Log.e(TAG, "Read ProductName command failure", ex)
+      }
+    })
+  }
+
+  private suspend fun sendReadUserLabelCommandClick() {
+    getBasicClusterForDevice().readUserLabelAttribute(object : ChipClusters.CharStringAttributeCallback {
+      override fun onSuccess(value: String) {
+        showMessage("Read UserLabel command success: value: $value")
+      }
+
+      override fun onError(ex: Exception) {
+        showMessage("Read UserLabel command failure $ex")
+        Log.e(TAG, "Read UserLabel command failure", ex)
+      }
+    })
+  }
+
+  private suspend fun sendWriteUserLabelCommandClick() {
+    getBasicClusterForDevice().writeUserLabelAttribute(object : ChipClusters.DefaultClusterCallback {
+      override fun onSuccess() {
+        showMessage("Write UserLabel command success")
+      }
+
+      override fun onError(ex: Exception) {
+        showMessage("Write UserLabel command failure $ex")
+        Log.e(TAG, "Write UserLabel command failure", ex)
+      }
+
+    }, userLabelEd.text.toString())
+  }
+
   private fun showMessage(msg: String) {
     requireActivity().runOnUiThread {
-      basicClustCommandStatus.text = msg
+      basicClusterCommandStatus.text = msg
     }
+  }
+
+  private suspend fun getBasicClusterForDevice(): BasicCluster {
+    return BasicCluster(
+            ChipClient.getConnectedDevicePointer(requireContext(), basicClusterDeviceIdEd.text.toString().toLong()), 0
+    )
   }
 
   companion object {
