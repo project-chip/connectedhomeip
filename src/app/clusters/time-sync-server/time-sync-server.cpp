0aac0473def32ca290844898758ee216dba30b74
Dimitri Mizenko <dimitri.mizenko@evehome.com>
2021-09-29 14:20:28 +0200

    add first minimal version of time sync cluster
    This has only support for the UTCTime. No NTP and co is supported.

--- src/app/clusters/time-sync-server/time-sync-server.cpp
+++ src/app/clusters/time-sync-server/time-sync-server.cpp
@@ -15,19 +15,107 @@
  *    limitations under the License.
  */
 
-#include <app/util/af.h>
-
-#include <app-common/zap-generated/attribute-id.h>
-#include <app-common/zap-generated/attribute-type.h>
-#include <app-common/zap-generated/cluster-id.h>
+#include <app-common/app-common/zap-generated/enums.h>
+#include <app-common/zap-generated/attributes/Accessors.h>
+#include <app-common/zap-generated/ids/Attributes.h>
+#include <app-common/zap-generated/ids/Clusters.h>
 #include <app/CommandHandler.h>
 #include <app/util/af-event.h>
+#include <app/util/af.h>
 #include <app/util/attribute-storage.h>
+#include <lib/support/logging/CHIPLogging.h>
+#include <system/SystemClock.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+
+struct TimeSyncTimeSource
+{
+    uint64_t GetUtcTime(void) { return GetLocalTime() + mTimeBase; }
+    uint64_t GetLocalTime(void) { return System::Clock::GetMonotonicMicroseconds(); }
+    void SetTimeBase(uint64_t timeBase) { mTimeBase = timeBase; }
+
+private:
+    // TODO store TimeBase in Flash ?
+    uint64_t mTimeBase;
+};
+
+namespace {
+
+TimeSyncTimeSource gTimeSource;
+
+class TimeSyncAttrAccess : public AttributeAccessInterface
+{
+public:
+    // Register for the TimeSync cluster on all endpoints.
+    TimeSyncAttrAccess() : AttributeAccessInterface(Optional<EndpointId>::Missing(), TimeSync::Id) {}
+    CHIP_ERROR Read(ClusterInfo & aClusterInfo, const AttributeValueEncoder & aEncoder, bool * aDataRead) override;
+};
+
+TimeSyncAttrAccess gAttrAccess;
+
+CHIP_ERROR TimeSyncAttrAccess::Read(ClusterInfo & aClusterInfo, const AttributeValueEncoder & aEncoder, bool * aDataRead)
+{
+    if (aClusterInfo.mClusterId != TimeSync::Id)
+    {
+        // We shouldn't have been called at all.
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    }
+
+    *aDataRead = true;
+    switch (aClusterInfo.mFieldId)
+    {
+    case TimeSync::Attributes::Ids::UTCTime: {
+        uint64_t utcTime = gTimeSource.GetUtcTime();
+        return aEncoder.Encode(utcTime);
+        break;
+    }
+    case TimeSync::Attributes::Ids::LocalTime: {
+        uint64_t localTime = gTimeSource.GetLocalTime();
+        return aEncoder.Encode(localTime);
+        break;
+    }
+    default: {
+        *aDataRead = false;
+        break;
+    }
+    }
+
+    return CHIP_NO_ERROR;
+}
+} // anonymous namespace
 
 bool emberAfTimeSyncClusterSetUtcTimeCallback(chip::EndpointId endpoint, chip::app::CommandHandler * commandObj, uint64_t UtcTime,
                                               uint8_t Granularity, uint8_t TimeSource)
 {
     EmberAfStatus status = EMBER_ZCL_STATUS_SUCCESS;
-    (void) emberAfSendImmediateDefaultResponse(status);
-    return true;
+    uint8_t granularity  = EMBER_ZCL_TIME_SYNC_GRANULARITY_NO_TIME_GRANULARITY;
+
+    if (EMBER_ZCL_STATUS_SUCCESS == TimeSync::Attributes::SetGranularity(endpoint, granularity) &&
+        (EMBER_ZCL_TIME_SYNC_GRANULARITY_NO_TIME_GRANULARITY == granularity || Granularity <= granularity))
+    {
+        gTimeSource.SetTimeBase(UtcTime);
+        status = TimeSync::Attributes::SetGranularity(endpoint, Granularity);
+        (void) TimeSync::Attributes::SetTimeSource(endpoint, TimeSource);
+    }
+    else
+    {
+        status = EMBER_ZCL_STATUS_FAILURE;
+    }
+
+    return EMBER_SUCCESS == emberAfSendImmediateDefaultResponse(status);
+}
+
+void emberAfTimeSyncClusterServerInitCallback(EndpointId endpoint)
+{
+    static bool attrAccessRegistered = false;
+    if (!attrAccessRegistered)
+    {
+        attrAccessRegistered = true;
+        registerAttributeAccessOverride(&gAttrAccess);
+
+        EmberAfTimeSyncGranularity granularity = EMBER_ZCL_TIME_SYNC_GRANULARITY_NO_TIME_GRANULARITY;
+        (void) TimeSync::Attributes::SetGranularity(endpoint, (uint8_t) granularity);
+    }
 }
