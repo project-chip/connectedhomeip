5a8c7d12ac0651a2bb037adc864e7b9673ab8b50
Andrei Litvin <andy314@gmail.com>
2021-09-30 20:09:49 -0400

    Make tizen build be a standard gn build

--- scripts/build/builders/tizen.py
+++ scripts/build/builders/tizen.py
@@ -17,7 +17,7 @@ import os
 
 from enum import Enum, auto
 
-from .builder import Builder
+from .gn import GnBuilder
 
 
 class TizenApp(Enum):
@@ -46,7 +46,7 @@ class TizenBoard(Enum):
             raise Exception('Unknown board type: %r' % self)
 
 
-class TizenBuilder(Builder):
+class TizenBuilder(GnBuilder):
 
     def __init__(self,
                  root,
@@ -59,33 +59,16 @@ class TizenBuilder(Builder):
         self.app = app
         self.board = board
 
-    def generate(self):
-        if not os.path.exists(self.output_dir):
-            if not self._runner.dry_run:
-                if 'TIZEN_HOME' not in os.environ:
-                    raise Exception(
-                        "Environment TIZEN_HOME missing, cannot build tizen libraries")
+    def GnBuildArgs(self):
+        if 'TIZEN_HOME' not in os.environ:
+            raise Exception(
+                "Environment TIZEN_HOME missing, cannot build tizen libraries")
 
-            cmd = '''\
-gn gen --check --fail-on-unused-args --root=%s '--args=''' % self.root
-
-            gn_args = {}
-            gn_args['target_os'] = 'tizen'
-            gn_args['target_cpu'] = self.board.TargetCpuName()
-            gn_args['sysroot'] = os.environ['TIZEN_HOME']
-
-            cmd += ' %s\' %s' % (' '.join([
-                '%s="%s"' % (key, value)
-                for key, value in gn_args.items()]), self.output_dir)
-
-            self._Execute(['bash', '-c', cmd],
-                          title='Generating ' + self.identifier)
-
-    def _build(self):
-        logging.info('Compiling Tizen at %s', self.output_dir)
-
-        self._Execute(['ninja', '-C', self.output_dir],
-                      title='Building ' + self.identifier)
+        return [
+            'target_os="tizen"',
+            'target_cpu="%s"' % self.board.TargetCpuName(),
+            'sysroot="%s"' % os.environ['TIZEN_HOME'],
+        ]
 
     def build_outputs(self):
         items = {
