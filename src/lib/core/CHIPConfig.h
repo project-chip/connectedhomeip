1f9e4b5e38deb5c21556e974d19301c0713ee39d
Restyled.io <commits@restyled.io>
2021-10-05 22:02:03 +0000

    Restyled by clang-format

--- src/lib/core/CHIPConfig.h
+++ src/lib/core/CHIPConfig.h
@@ -1397,7 +1397,7 @@
  *    "Test secret for key derivation." results in the following encryption keys:
  *
  *              5E DE D2 44 E5 53 2B 3C DC 23 40 9D BA D0 52 D2
-*               A9 E0 11 B1 73 7C 6D 4B 70 E4 C0 A2 FE 66 04 76
+ *               A9 E0 11 B1 73 7C 6D 4B 70 E4 C0 A2 FE 66 04 76
  */
 #ifndef CHIP_CONFIG_TEST_SHARED_SECRET_VALUE
 #define CHIP_CONFIG_TEST_SHARED_SECRET_VALUE "Test secret for key derivation."
--- src/transport/CryptoContext.cpp
+++ src/transport/CryptoContext.cpp
@@ -77,15 +77,19 @@ CHIP_ERROR CryptoContext::InitFromSecret(const ByteSpan & secret, const ByteSpan
     // If enabled, override the generated session key with a known key pair
     // to allow man-in-the-middle session key recovery for testing purposes.
 
-    #define TEST_SECRET_SIZE 32
+#define TEST_SECRET_SIZE 32
     constexpr uint8_t kTestSharedSecret[TEST_SECRET_SIZE] = CHIP_CONFIG_TEST_SHARED_SECRET_VALUE;
-    static_assert(sizeof(CHIP_CONFIG_TEST_SHARED_SECRET_VALUE) == TEST_SECRET_SIZE, "CHIP_CONFIG_TEST_SHARED_SECRET_VALUE must be 32 bytes");
+    static_assert(sizeof(CHIP_CONFIG_TEST_SHARED_SECRET_VALUE) == TEST_SECRET_SIZE,
+                  "CHIP_CONFIG_TEST_SHARED_SECRET_VALUE must be 32 bytes");
     const ByteSpan & testSalt = ByteSpan(nullptr, 0);
     (void) info;
     (void) infoLen;
 
-    #pragma message "Warning: CONFIG_SECURITY_TEST_MODE=1 bypassing key negotiation... All sessions will use known, fixed test key.  Node can only communicate with other nodes built with this flag set."
-    ChipLogError(SecureChannel, "Warning: CONFIG_SECURITY_TEST_MODE=1 bypassing key negotiation... All sessions will use known, fixed test key.  Node can only communicate with other nodes built with this flag set.");
+#pragma message                                                                                                                    \
+    "Warning: CONFIG_SECURITY_TEST_MODE=1 bypassing key negotiation... All sessions will use known, fixed test key.  Node can only communicate with other nodes built with this flag set."
+    ChipLogError(SecureChannel,
+                 "Warning: CONFIG_SECURITY_TEST_MODE=1 bypassing key negotiation... All sessions will use known, fixed test key.  "
+                 "Node can only communicate with other nodes built with this flag set.");
 
     ReturnErrorOnFailure(mHKDF.HKDF_SHA256(kTestSharedSecret, TEST_SECRET_SIZE, testSalt.data(), testSalt.size(), SEKeysInfo,
                                            sizeof(SEKeysInfo), &mKeys[0][0], sizeof(mKeys)));
