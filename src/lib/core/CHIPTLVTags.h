838bc413abc9c8985e179ff28ded2c2b94783f46
Evgeni Margolis <emargolis@google.com>
2021-10-05 09:39:32 -0700

    Added IsContextTag Check to the DecodeConvertExtension() Function.
      -- Added test to check this error scenario.
      -- Also minor (non-functional) update in the CHIP TLV encoding logic.

--- src/lib/core/CHIPTLVTags.h
+++ src/lib/core/CHIPTLVTags.h
@@ -50,7 +50,7 @@ enum TLVTagFields
     kProfileNumShift  = 32,
     kTagNumMask       = 0x00000000FFFFFFFFULL,
     kSpecialTagMarker = 0xFFFFFFFF00000000ULL,
-    kContextTagMaxNum = 256
+    kContextTagMaxNum = UINT8_MAX
 };
 
 // TODO: Move to private namespace
@@ -208,7 +208,7 @@ inline bool IsProfileTag(Tag tag)
  */
 inline bool IsContextTag(Tag tag)
 {
-    return (tag & kProfileIdMask) == kSpecialTagMarker && TagNumFromTag(tag) < kContextTagMaxNum;
+    return (tag & kProfileIdMask) == kSpecialTagMarker && TagNumFromTag(tag) <= kContextTagMaxNum;
 }
 
 // TODO: move to private namespace
