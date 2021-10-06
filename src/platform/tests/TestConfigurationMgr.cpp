327c491941dad89018cc40aa30ab40c6df72c355
Restyled.io <commits@restyled.io>
2021-10-06 09:28:04 +0000

    Restyled by clang-format

--- src/platform/tests/TestConfigurationMgr.cpp
+++ src/platform/tests/TestConfigurationMgr.cpp
@@ -443,31 +443,41 @@ static void TestConfigurationMgr_Breadcrumb(nlTestSuite * inSuite, void * inCont
 
 static void TestConfigurationMgr_GetPrimaryMACAddress(nlTestSuite * inSuite, void * inContext)
 {
-    CHIP_ERROR err      = CHIP_NO_ERROR;
-    const uint8_t defaultMacAddress[8] = {0xEE, 0xAA, 0xBA, 0xDA, 0xBA, 0xD0, 0xDD, 0xCA };
+    CHIP_ERROR err                     = CHIP_NO_ERROR;
+    const uint8_t defaultMacAddress[8] = { 0xEE, 0xAA, 0xBA, 0xDA, 0xBA, 0xD0, 0xDD, 0xCA };
     uint8_t macBuffer8Bytes[8];
     uint8_t macBuffer6Bytes[6];
     MutableByteSpan mac8Bytes(macBuffer8Bytes);
     MutableByteSpan mac6Bytes(macBuffer6Bytes);
 
     err = ConfigurationMgr().GetPrimaryMACAddress(mac8Bytes);
-    if (mac8Bytes.size() != ConfigurationManager::kMACAddressLength) {
+    if (mac8Bytes.size() != ConfigurationManager::kMACAddressLength)
+    {
         NL_TEST_ASSERT(inSuite, err == CHIP_ERROR_INVALID_ARGUMENT);
-    } else {
+    }
+    else
+    {
         NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
 
         // Verify default MAC address value
-        NL_TEST_ASSERT(inSuite, strncmp(reinterpret_cast<char*>(mac8Bytes.data()), reinterpret_cast<const char*>(defaultMacAddress), mac8Bytes.size()) == 0);
+        NL_TEST_ASSERT(inSuite,
+                       strncmp(reinterpret_cast<char *>(mac8Bytes.data()), reinterpret_cast<const char *>(defaultMacAddress),
+                               mac8Bytes.size()) == 0);
     }
 
     err = ConfigurationMgr().GetPrimaryMACAddress(mac6Bytes);
-    if (mac6Bytes.size() != ConfigurationManager::kMACAddressLength) {
+    if (mac6Bytes.size() != ConfigurationManager::kMACAddressLength)
+    {
         NL_TEST_ASSERT(inSuite, err == CHIP_ERROR_INVALID_ARGUMENT);
-    } else {
+    }
+    else
+    {
         NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
 
         // Verify default MAC address value
-        NL_TEST_ASSERT(inSuite, strncmp(reinterpret_cast<char*>(mac6Bytes.data()), reinterpret_cast<const char*>(defaultMacAddress), mac6Bytes.size()) == 0);
+        NL_TEST_ASSERT(inSuite,
+                       strncmp(reinterpret_cast<char *>(mac6Bytes.data()), reinterpret_cast<const char *>(defaultMacAddress),
+                               mac6Bytes.size()) == 0);
     }
 }
 
