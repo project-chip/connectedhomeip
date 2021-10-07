f915cbaf1d8bc8bbbf1b3224dcae967a46f5cd2e
pankore <86098180+pankore@users.noreply.github.com>
2021-10-07 19:11:21 +0800

    Support commissioning over BLE

--- examples/all-clusters-app/ameba/main/chipinterface.cpp
+++ examples/all-clusters-app/ameba/main/chipinterface.cpp
@@ -1,10 +1,38 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #include <platform_stdlib.h>
 
+#include "chip_porting.h"
 #include "CHIPDeviceManager.h"
 #include "DeviceCallbacks.h"
-#include "Server.h"
+#include "Globals.h"
+#include <lwip_netconf.h>
+#include "LEDWidget.h"
 
+#include <app/server/OnboardingCodesUtil.h>
+#include <app/server/Server.h>
+#include <credentials/DeviceAttestationCredsProvider.h>
+#include <credentials/examples/DeviceAttestationCredsExample.h>
+#include <lib/support/ErrorStr.h>
+#include <platform/Ameba/AmebaConfig.h>
 #include <platform/CHIPDeviceLayer.h>
+#include <setup_payload/ManualSetupPayloadGenerator.h>
+#include <setup_payload/QRCodeSetupPayloadGenerator.h>
 #include <support/CHIPMem.h>
 
 extern "C" {
@@ -12,30 +40,173 @@ void * __dso_handle = 0;
 }
 
 using namespace ::chip;
+using namespace ::chip::Credentials;
 using namespace ::chip::DeviceManager;
 using namespace ::chip::DeviceLayer;
 
+#define QRCODE_BASE_URL "https://dhrishi.github.io/connectedhomeip/qrcode.html"
+#define EXAMPLE_VENDOR_TAG_IP 1
+
+#ifdef CONFIG_PLATFORM_8721D
+#define STATUS_LED_GPIO_NUM PB_5
+#elif defined(CONFIG_PLATFORM_8710C)
+#define STATUS_LED_GPIO_NUM PA_20
+#else
+#define STATUS_LED_GPIO_NUM NC
+#endif
+
 static DeviceCallbacks EchoCallbacks;
+
+void GetGatewayIP(char * ip_buf, size_t ip_len)
+{
+    uint8_t *gateway = LwIP_GetGW(&xnetif[0]);
+    sprintf(ip_buf, "%d.%d.%d.%d", gateway[0], gateway[1], gateway[2], gateway[3]);
+    ChipLogProgress(DeviceLayer, "Got gateway ip: %s\r\n", ip_buf);
+}
+
+// need to check CONFIG_RENDEZVOUS_MODE
+bool isRendezvousBLE()
+{
+    RendezvousInformationFlags flags = RendezvousInformationFlags(CONFIG_RENDEZVOUS_MODE);
+    return flags.Has(RendezvousInformationFlag::kBLE);
+}
+
+std::string createSetupPayload()
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+    std::string result;
+
+    uint16_t discriminator;
+    err = ConfigurationMgr().GetSetupDiscriminator(discriminator);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "Couldn't get discriminator: %s\r\n", ErrorStr(err));
+        return result;
+    }
+    ChipLogProgress(DeviceLayer, "Setup discriminator: %u (0x%x)\r\n", discriminator, discriminator);
+
+    uint32_t setupPINCode;
+    err = ConfigurationMgr().GetSetupPinCode(setupPINCode);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "Couldn't get setupPINCode: %s\r\n", ErrorStr(err));
+        return result;
+    }
+    ChipLogProgress(DeviceLayer, "Setup PIN code: %u (0x%x)\r\n", setupPINCode, setupPINCode);
+
+    uint16_t vendorId;
+    err = ConfigurationMgr().GetVendorId(vendorId);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "Couldn't get vendorId: %s\r\n", ErrorStr(err));
+        return result;
+    }
+
+    uint16_t productId;
+    err = ConfigurationMgr().GetProductId(productId);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "Couldn't get productId: %s\r\n", ErrorStr(err));
+        return result;
+    }
+
+    SetupPayload payload;
+    payload.version               = 0;
+    payload.discriminator         = discriminator;
+    payload.setUpPINCode          = setupPINCode;
+    payload.rendezvousInformation = RendezvousInformationFlags(CONFIG_RENDEZVOUS_MODE);
+    payload.vendorID              = vendorId;
+    payload.productID             = productId;
+
+    if (!isRendezvousBLE())
+    {
+        char gw_ip[INET6_ADDRSTRLEN];
+        GetGatewayIP(gw_ip, sizeof(gw_ip));
+        payload.addOptionalVendorData(EXAMPLE_VENDOR_TAG_IP, gw_ip);
+
+        QRCodeSetupPayloadGenerator generator(payload);
+
+        size_t tlvDataLen = sizeof(gw_ip);
+        uint8_t tlvDataStart[tlvDataLen];
+        err = generator.payloadBase38Representation(result, tlvDataStart, tlvDataLen);
+    }
+    else
+    {
+        QRCodeSetupPayloadGenerator generator(payload);
+        err = generator.payloadBase38Representation(result);
+    }
+
+    {
+        ManualSetupPayloadGenerator generator(payload);
+        std::string outCode;
+
+        if (generator.payloadDecimalStringRepresentation(outCode) == CHIP_NO_ERROR)
+        {
+            ChipLogProgress(DeviceLayer, "Short Manual(decimal) setup code: %s\r\n", outCode.c_str());
+        }
+        else
+        {
+            ChipLogError(DeviceLayer, "Failed to get decimal setup code\r\n");
+        }
+
+        payload.commissioningFlow = CommissioningFlow::kCustom;
+        generator                 = ManualSetupPayloadGenerator(payload);
+
+        if (generator.payloadDecimalStringRepresentation(outCode) == CHIP_NO_ERROR)
+        {
+            // intentional extra space here to align the log with the short code
+            ChipLogProgress(DeviceLayer, "Long Manual(decimal) setup code:  %s\r\n", outCode.c_str());
+        }
+        else
+        {
+            ChipLogError(DeviceLayer, "Failed to get decimal setup code\r\n");
+        }
+    }
+
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "Couldn't get payload string %\r\n" CHIP_ERROR_FORMAT, err.Format());
+    }
+    return result;
+};
 
 extern "C" void ChipTest(void)
 {
-    printf("In ChipTest()\r\n");
+    ChipLogProgress(DeviceLayer, "All Clusters Demo!");
     CHIP_ERROR err = CHIP_NO_ERROR;
+
+    initPref();
 
     CHIPDeviceManager & deviceMgr = CHIPDeviceManager::GetInstance();
 
     err = deviceMgr.Init(&EchoCallbacks);
-
     if (err != CHIP_NO_ERROR)
     {
-        printf("DeviceManagerInit() - ERROR!\r\n");
+        ChipLogError(DeviceLayer, "DeviceManagerInit() - ERROR!\r\n");
     }
     else
     {
-        printf("DeviceManagerInit() - OK\r\n");
+        ChipLogProgress(DeviceLayer, "DeviceManagerInit() - OK\r\n");
     }
 
     chip::Server::GetInstance().Init();
+
+    // Initialize device attestation config
+    SetDeviceAttestationCredentialsProvider(Examples::GetExampleDACProvider());
+
+    std::string qrCodeText = createSetupPayload();
+    ChipLogProgress(DeviceLayer, "QR CODE Text: '%s'\r\n", qrCodeText.c_str());
+
+    {
+        std::vector<char> qrCode(3 * qrCodeText.size() + 1);
+        err = EncodeQRCodeToUrl(qrCodeText.c_str(), qrCodeText.size(), qrCode.data(), qrCode.max_size());
+        if (err == CHIP_NO_ERROR)
+        {
+            ChipLogProgress(DeviceLayer, "Copy/paste the below URL in a browser to see the QR CODE:\n\t%s?data=%s \r\n", QRCODE_BASE_URL, qrCode.data());
+        }
+    }
+
+    statusLED1.Init(STATUS_LED_GPIO_NUM);
 
     while (true)
         vTaskDelay(pdMS_TO_TICKS(50));
--- src/platform/Ameba/ConfigurationManagerImpl.cpp
+++ src/platform/Ameba/ConfigurationManagerImpl.cpp
@@ -29,6 +29,7 @@
 #include <platform/internal/GenericConfigurationManagerImpl.cpp>
 #include <support/CodeUtils.h>
 #include <support/logging/CHIPLogging.h>
+#include <wifi_conf.h>
 
 namespace chip {
 namespace DeviceLayer {
@@ -44,11 +45,17 @@ CHIP_ERROR ConfigurationManagerImpl::Init()
     CHIP_ERROR err;
     bool failSafeArmed;
 
+    // Force initialization of NVS namespaces if they doesn't already exist.
+    err = EnsureNamespace(kConfigNamespace_ChipFactory);
+    SuccessOrExit(err);
+    err = EnsureNamespace(kConfigNamespace_ChipConfig);
+    SuccessOrExit(err);
+    err = EnsureNamespace(kConfigNamespace_ChipCounters);
+    SuccessOrExit(err);
+
     // Initialize the generic implementation base class.
     err = Internal::GenericConfigurationManagerImpl<ConfigurationManagerImpl>::Init();
     SuccessOrExit(err);
-
-    // TODO: initialize NVM component
 
     // If the fail-safe was armed when the device last shutdown, initiate a factory reset.
     if (GetFailSafeArmed(failSafeArmed) == CHIP_NO_ERROR && failSafeArmed)
@@ -64,7 +71,16 @@ exit:
 
 CHIP_ERROR ConfigurationManagerImpl::GetPrimaryWiFiMACAddress(uint8_t * buf)
 {
-    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+    char temp[32];
+    uint32_t mac[ETH_ALEN];
+    int i = 0;
+
+    wifi_get_mac_address(temp);
+    sscanf(temp, "%02x:%02x:%02x:%02x:%02x:%02x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
+    for(i=0; i<ETH_ALEN; i++)
+        buf[i] = mac[i]&0xFF;
+
+    return CHIP_NO_ERROR;
 }
 
 bool ConfigurationManagerImpl::CanFactoryReset()
@@ -78,24 +94,40 @@ void ConfigurationManagerImpl::InitiateFactoryReset()
     PlatformMgr().ScheduleWork(DoFactoryReset);
 }
 
-CHIP_ERROR ConfigurationManagerImpl::ReadPersistedStorageValue(::chip::Platform::PersistedStorage::Key persistedStorageKey,
-                                                               uint32_t & value)
+CHIP_ERROR ConfigurationManagerImpl::_ReadPersistedStorageValue(::chip::Platform::PersistedStorage::Key key, uint32_t & value)
 {
-    // TODO
-    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+    AmebaConfig::Key configKey{ kConfigNamespace_ChipCounters, key };
+
+    CHIP_ERROR err = ReadConfigValue(configKey, value);
+    if (err == CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND)
+    {
+        err = CHIP_ERROR_PERSISTED_STORAGE_VALUE_NOT_FOUND;
+    }
+    return err;
 }
 
-CHIP_ERROR ConfigurationManagerImpl::WritePersistedStorageValue(::chip::Platform::PersistedStorage::Key persistedStorageKey,
-                                                                uint32_t value)
+CHIP_ERROR ConfigurationManagerImpl::_WritePersistedStorageValue(::chip::Platform::PersistedStorage::Key key, uint32_t value)
 {
-    // TODO
-    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+    AmebaConfig::Key configKey{ kConfigNamespace_ChipCounters, key };
+    return WriteConfigValue(configKey, value);
 }
 
 void ConfigurationManagerImpl::DoFactoryReset(intptr_t arg)
 {
-    // TODO
+    CHIP_ERROR err;
+
+    ChipLogProgress(DeviceLayer, "Performing factory reset");
+
+    // Erase all values in the chip-config NVS namespace.
+    err = ClearNamespace(kConfigNamespace_ChipConfig);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "ClearNamespace(ChipConfig) failed: %s", chip::ErrorStr(err));
+    }
+
+    // Restart the system.
     ChipLogProgress(DeviceLayer, "System restarting");
+    // sys_reset();
 }
 
 } // namespace DeviceLayer
