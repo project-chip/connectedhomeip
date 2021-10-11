5b8096ff94fcaedc5a29317204bda234e4b593be
JasonLiuZhuoCheng <zhuocheng@google.com>
2021-10-11 18:28:49 -0400

    create cluster interaction tool basic ui (#10324)
    * first commit
    
    * create two editText field and a enroll device button on the new cluster interaction tool page
    
    * change back the unwanted changes
    
    * revert one more change
    
    * remove zapt generation code, also added a toast to indicate status
    
    * fix naming for updateAddressBtn

---
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/ClusterInteractionFragment.kt
@@ -0,0 +1,101 @@
+package com.google.chip.chiptool.clusterclient
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import chip.devicecontroller.ChipClusters
+import chip.devicecontroller.ChipDeviceController
+import com.google.chip.chiptool.ChipClient
+import com.google.chip.chiptool.GenericChipDeviceListener
+import com.google.chip.chiptool.R
+import com.google.chip.chiptool.SelectActionFragment
+import com.google.chip.chiptool.util.DeviceIdUtil
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlinx.android.synthetic.main.cluster_interaction_fragment.*
+import kotlinx.android.synthetic.main.cluster_interaction_fragment.view.updateAddressBtn
+import kotlinx.android.synthetic.main.on_off_client_fragment.commandStatusTv
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+
+class ClusterInteractionFragment: Fragment() {
+  private val deviceController: ChipDeviceController
+    get() = ChipClient.getDeviceController(requireContext())
+
+  private val scope = CoroutineScope(Dispatchers.Main + Job())
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    return inflater.inflate(R.layout.cluster_interaction_fragment, container, false).apply {
+      deviceController.setCompletionListener(ChipControllerCallback())
+      updateAddressBtn.setOnClickListener { updateAddressClick() }
+} }
+
+  private fun updateAddressClick() {
+    try{
+      deviceController.updateDevice(
+        fabricId.text.toString().toULong().toLong(),
+        deviceId.text.toString().toULong().toLong()
+      )
+      showMessage("Address update started")
+    } catch (ex: Exception) {
+      showMessage("Address update failed: $ex")
+    }
+  }
+
+  private fun showMessage(msg: String) {
+    requireActivity().runOnUiThread {
+      Toast.makeText(
+        requireContext(),
+        msg,
+        Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    // TODO: use the fabric ID that was used to commission the device
+    val testFabricId = "5544332211"
+    fabricId.setText(testFabricId)
+    deviceId.setText(DeviceIdUtil.getLastDeviceId(requireContext()).toString())
+  }
+
+  inner class ChipControllerCallback : GenericChipDeviceListener() {
+    override fun onConnectDeviceComplete() {}
+
+    override fun onCommissioningComplete(nodeId: Long, errorCode: Int) {
+    }
+
+    override fun onNotifyChipConnectionClosed() {
+      Log.d(TAG, "onNotifyChipConnectionClosed")
+    }
+
+    override fun onCloseBleComplete() {
+      Log.d(TAG, "onCloseBleComplete")
+    }
+
+    override fun onError(error: Throwable?) {
+      Log.d(TAG, "onError: $error")
+    }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    scope.cancel()
+  }
+
+  companion object {
+    private const val TAG = "ClusterInteractionFragment"
+    fun newInstance(): ClusterInteractionFragment = ClusterInteractionFragment()
+  }
+}
