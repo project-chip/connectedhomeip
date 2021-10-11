9f4928ef135d7bcdb28b06e5f0e397065ab4ff19
Restyled.io <commits@restyled.io>
2021-10-11 11:44:07 +0000

    Restyled by clang-format

--- src/platform/ESP32/PlatformManagerImpl.cpp
+++ src/platform/ESP32/PlatformManagerImpl.cpp
@@ -184,21 +184,21 @@ CHIP_ERROR PlatformManagerImpl::_GetTotalOperationalHours(uint32_t & totalOperat
 
 CHIP_ERROR PlatformManagerImpl::_GetBootReasons(uint8_t & bootReasons)
 {
-/*
-    typedef enum {
-    ESP_RST_UNKNOWN,    //!< Reset reason can not be determined
-    ESP_RST_POWERON,    //!< Reset due to power-on event
-    ESP_RST_EXT,        //!< Reset by external pin (not applicable for ESP32)
-    ESP_RST_SW,         //!< Software reset via esp_restart
-    ESP_RST_PANIC,      //!< Software reset due to exception/panic
-    ESP_RST_INT_WDT,    //!< Reset (software or hardware) due to interrupt watchdog
-    ESP_RST_TASK_WDT,   //!< Reset due to task watchdog
-    ESP_RST_WDT,        //!< Reset due to other watchdogs
-    ESP_RST_DEEPSLEEP,  //!< Reset after exiting deep sleep mode
-    ESP_RST_BROWNOUT,   //!< Brownout reset (software or hardware)
-    ESP_RST_SDIO,       //!< Reset over SDIO
-} esp_reset_reason_t;
-*/
+    /*
+        typedef enum {
+        ESP_RST_UNKNOWN,    //!< Reset reason can not be determined
+        ESP_RST_POWERON,    //!< Reset due to power-on event
+        ESP_RST_EXT,        //!< Reset by external pin (not applicable for ESP32)
+        ESP_RST_SW,         //!< Software reset via esp_restart
+        ESP_RST_PANIC,      //!< Software reset due to exception/panic
+        ESP_RST_INT_WDT,    //!< Reset (software or hardware) due to interrupt watchdog
+        ESP_RST_TASK_WDT,   //!< Reset due to task watchdog
+        ESP_RST_WDT,        //!< Reset due to other watchdogs
+        ESP_RST_DEEPSLEEP,  //!< Reset after exiting deep sleep mode
+        ESP_RST_BROWNOUT,   //!< Brownout reset (software or hardware)
+        ESP_RST_SDIO,       //!< Reset over SDIO
+    } esp_reset_reason_t;
+    */
 
     bootReasons = static_cast<uint8_t>(esp_reset_reason());
     return CHIP_NO_ERROR;
