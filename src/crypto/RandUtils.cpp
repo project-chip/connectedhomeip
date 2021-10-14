267951cf6ed6f980954cb71458dba6b6207b4ffc
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2021-10-13 22:05:45 -0400

    Make RandUtils.h use DRBG_get_bytes() (#10465)
    * Make RandUtils.h use DRBG_get_bytes()
    
    - Move RandUtils.* from lib/support to src/crypto to break
      dependency cycle w/ CHIPCryptoPAL.h
    - Make RandUtils functions use DRBG_get_bytes(), which
      avoids issues of `srand()` improperly initialized or
      dependencies on `rand()` in core SDK code. All random numbers
      in core protocols are supposed, by spec, to be generated using
      a crypto-based DRBG
    - Fix needless inclusions of RandUtils.h
    - Move all uses of `GetRandU*` to the new location/version
    - Fix minor usage of snprintf() for large hex strings in DnsSd
      to use BytesToHex().
    
    Testing done: unit tests still pass. Cert tests still pass.
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/crypto/RandUtils.cpp
+++ src/crypto/RandUtils.cpp
@@ -18,7 +18,7 @@
 
 /**
  *    @file
- *      This file defines utility functions for deriving random integers.
+ *      This file implements utility functions for deriving random integers.
  *
  *  @note These utility functions do not generate cryptographically strong
  *        random number. To get cryptographically strong random data use
@@ -26,42 +26,44 @@
  *
  */
 
-#pragma once
+#include "RandUtils.h"
 
 #include <stdint.h>
+#include <stdlib.h>
+
+#include <crypto/CHIPCryptoPAL.h>
+#include <lib/support/CodeUtils.h>
 
 namespace chip {
+namespace Crypto {
 
-/**
- *  This function generates 64-bit unsigned random number.
- *
- * @return  64-bit unsigned random integer.
- *
- */
-extern uint64_t GetRandU64();
+uint64_t GetRandU64()
+{
+    uint64_t tmp = 0;
+    VerifyOrDie(CHIP_NO_ERROR == DRBG_get_bytes(reinterpret_cast<uint8_t *>(&tmp), sizeof(tmp)));
+    return tmp;
+}
 
-/**
- *  This function generates 32-bit unsigned random number.
- *
- * @return  32-bit unsigned random integer.
- *
- */
-extern uint32_t GetRandU32();
+uint32_t GetRandU32()
+{
+    uint32_t tmp = 0;
+    VerifyOrDie(CHIP_NO_ERROR == DRBG_get_bytes(reinterpret_cast<uint8_t *>(&tmp), sizeof(tmp)));
+    return tmp;
+}
 
-/**
- *  This function generates 16-bit unsigned random number.
- *
- * @return  16-bit unsigned random integer.
- *
- */
-extern uint16_t GetRandU16();
+uint16_t GetRandU16()
+{
+    uint16_t tmp = 0;
+    VerifyOrDie(CHIP_NO_ERROR == DRBG_get_bytes(reinterpret_cast<uint8_t *>(&tmp), sizeof(tmp)));
+    return tmp;
+}
 
-/**
- *  This function generates 8-bit unsigned random number.
- *
- * @return  8-bit unsigned random integer.
- *
- */
-extern uint8_t GetRandU8();
+uint8_t GetRandU8()
+{
+    uint8_t tmp = 0;
+    VerifyOrDie(CHIP_NO_ERROR == DRBG_get_bytes(&tmp, sizeof(tmp)));
+    return tmp;
+}
 
+} // namespace Crypto
 } // namespace chip
