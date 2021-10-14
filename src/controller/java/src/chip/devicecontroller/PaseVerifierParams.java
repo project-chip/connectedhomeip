c6d81044de78b2a833230a6363c59c2d2a2e4686
Kevin Coppock <coppock@google.com>
2021-10-14 11:21:10 -0500

    Surface a Java method to generate a PASE Verifier
    Currently, ChipDeviceController does provide an openPairingWindow() API,
    but this does not allow us to observe success or failure of the cluster
    command to open the commissioning window. With this change, we can use
    the cluster commands directly, using the generated PASE Verifier.

---
+++ src/controller/java/src/chip/devicecontroller/PaseVerifierParams.java
@@ -0,0 +1,65 @@
+package chip.devicecontroller;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/** Parameters representing a generated PASE verifier for a setup PIN code. */
+public final class PaseVerifierParams {
+  private final long setupPincode;
+  private final int passcodeId;
+  private final byte[] pakeVerifier;
+
+  /**
+   * Constructor
+   *
+   * @param setupPincode the PIN code associated with this verifier
+   * @param passcodeId the passcode ID for this generated verifier
+   * @param pakeVerifier the encoded verifier (w0 || L)
+   */
+  public PaseVerifierParams(long setupPincode, int passcodeId, byte[] pakeVerifier) {
+    this.setupPincode = setupPincode;
+    this.passcodeId = passcodeId;
+    this.pakeVerifier = pakeVerifier.clone();
+  }
+
+  /** Returns the PIN code associated with this verifier. */
+  public long getSetupPincode() {
+    return setupPincode;
+  }
+
+  /** Returns the passcode ID for this generated verifier. */
+  public int getPasscodeId() {
+    return passcodeId;
+  }
+
+  /** Returns the encoded verifier (w0 || L). */
+  public byte[] getPakeVerifier() {
+    return pakeVerifier.clone();
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    } else if (!(other instanceof PaseVerifierParams)) {
+      return false;
+    } else {
+      PaseVerifierParams that = (PaseVerifierParams) other;
+      return setupPincode == that.setupPincode
+          && passcodeId == that.passcodeId
+          && Arrays.equals(pakeVerifier, that.pakeVerifier);
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(setupPincode, passcodeId);
+    result = 31 * result + Arrays.hashCode(pakeVerifier);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "PaseVerifierParams{ passcodeId=" + passcodeId + " }";
+  }
+}
