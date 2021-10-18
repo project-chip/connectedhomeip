8bae150c98179ebd0f411b7ff2f1883d712f4311
Vivien Nicolas <vnicolas@apple.com>
2021-10-18 17:13:59 +0200

    Build bustage. Generated files are out of sync after 10497

--- zzz_generated/chip-tool/zap-generated/test/Commands.h
+++ zzz_generated/chip-tool/zap-generated/test/Commands.h
@@ -9816,6 +9816,8 @@ public:
             return;
         }
 
+        Wait();
+
         // Ensure we increment mTestIndex before we start running the relevant
         // command.  That way if we lose the timeslice after we send the message
         // but before our function call returns, we won't end up with an
@@ -10171,6 +10173,8 @@ public:
             SetCommandExitStatus(CHIP_NO_ERROR);
             return;
         }
+
+        Wait();
 
         // Ensure we increment mTestIndex before we start running the relevant
         // command.  That way if we lose the timeslice after we send the message
