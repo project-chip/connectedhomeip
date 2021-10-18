bd0689748d8762c4ad1b7244cd559f715e29ae0e
Restyled.io <commits@restyled.io>
2021-10-18 21:37:07 +0000

    Restyled by google-java-format

--- src/controller/java/src/chip/clusterinfo/DelegatedClusterCallback.java
+++ src/controller/java/src/chip/clusterinfo/DelegatedClusterCallback.java
@@ -1,6 +1,5 @@
 package chip.clusterinfo;
 
-import chip.clusterinfo.ClusterCommandCallback;
 
 public interface DelegatedClusterCallback {
   void setCallbackDelegate(ClusterCommandCallback callback);
