685e556cb56a76b2d60095f76fff8768399e4034
jmartinez-silabs <67972863+jmartinez-silabs@users.noreply.github.com>
2021-10-22 15:12:54 -0400

    Fix disparity in OnOff cluster revision (#10789)
    * Update OnOff Cluster revision to 4, Update yaml test, Enable onoff featuremap attribute in lighting-app and required onoff attributes in lock-app
    
    * regen all
    
    * regen after rebase conflicts

--- zzz_generated/bridge-app/zap-generated/endpoint_config.h
+++ zzz_generated/bridge-app/zap-generated/endpoint_config.h
@@ -1096,7 +1096,7 @@
                                                                                                                                    \
             /* Endpoint: 1, Cluster: On/Off (server) */                                                                            \
             { 0x0000, ZAP_TYPE(BOOLEAN), 1, 0, ZAP_SIMPLE_DEFAULT(0x00) }, /* OnOff */                                             \
-            { 0xFFFD, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(2) },     /* ClusterRevision */                                   \
+            { 0xFFFD, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(4) },     /* ClusterRevision */                                   \
                                                                                                                                    \
             /* Endpoint: 1, Cluster: Level Control (server) */                                                                     \
             { 0x0000, ZAP_TYPE(INT8U), 1, 0, ZAP_SIMPLE_DEFAULT(0x00) },                              /* current level */          \
