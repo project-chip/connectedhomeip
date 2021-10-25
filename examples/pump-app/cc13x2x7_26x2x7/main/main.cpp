7fe5f6a61495ac41ed55268e39049cab76cff19a
Anders Bang Poulsen <apoulsen@grundfos.com>
2021-10-25 14:36:12 +0200

    Reduced ICALL heap size to free up RAM for the application. Added a typecast for debug print that made the TI build fail.

--- examples/pump-app/cc13x2x7_26x2x7/main/main.cpp
+++ examples/pump-app/cc13x2x7_26x2x7/main/main.cpp
@@ -37,7 +37,7 @@
 #include <ti/drivers/SHA2.h>
 
 #include <bget.h>
-#define TOTAL_ICALL_HEAP_SIZE (0xE000)
+#define TOTAL_ICALL_HEAP_SIZE (0xC000)
 
 using namespace ::chip;
 using namespace ::chip::Inet;
