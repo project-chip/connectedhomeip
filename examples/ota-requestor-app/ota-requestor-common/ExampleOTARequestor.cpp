20ec105e50b2450d6c6d045b4273bb5794b97b46
Sergei Lissianoi <selissia@silabs.com>
2021-10-26 19:55:58 -0400

    Add a log message to HandleAnnounceOTAProvider

--- examples/ota-requestor-app/ota-requestor-common/ExampleOTARequestor.cpp
+++ examples/ota-requestor-app/ota-requestor-common/ExampleOTARequestor.cpp
@@ -92,6 +92,10 @@ EmberAfStatus ExampleOTARequestor::HandleAnnounceOTAProvider(
     mProviderNodeId      = providerLocation;
     mProviderFabricIndex = commandObj->GetExchangeContext()->GetSessionHandle().GetFabricIndex();
 
+    ChipLogProgress(SoftwareUpdate,
+                    "Notified of Provider at NodeID: 0x" ChipLogFormatX64 " on FabricIndex 0x%" PRIu8 ,
+                    ChipLogValueX64(mProviderNodeId), mProviderFabricIndex);
+
     // If reason is URGENT_UPDATE_AVAILABLE, we start OTA immediately. Otherwise, respect the timer value set in mOtaStartDelayMs.
     // This is done to exemplify what a real-world OTA Requestor might do while also being configurable enough to use as a test app.
     uint32_t msToStart = 0;
