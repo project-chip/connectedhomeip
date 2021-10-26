9a14b21f9423374b9a54b7697ae95a061dccf004
JasonLiuZhuoCheng <jasonlzc2017@gmail.com>
2021-10-25 22:03:22 -0400

    recycler view for endpoint display and test cluster detail screen done

--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/clusterinteraction/EndpointAdapter.kt
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/clusterinteraction/EndpointAdapter.kt
@@ -3,16 +3,35 @@ package com.google.chip.chiptool.clusterclient.clusterinteraction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.chip.chiptool.R
 
-class EndpointAdapter(private val endpointList: List<EndpointItem>) : RecyclerView.Adapter<EndpointAdapter.EndpointViewHolder>() {
+class EndpointAdapter(private val endpointList: List<EndpointItem>,
+                      private val listener: OnItemClickListener
 
-  class EndpointViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+) : RecyclerView.Adapter<EndpointAdapter.EndpointViewHolder>() {
+
+  inner class EndpointViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
     val endpointId: TextView = itemView.findViewById(R.id.endpoint_id)
+    init {
+      itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+      val position = adapterPosition
+      if (position != RecyclerView.NO_POSITION) {
+        listener.onItemClick(position)
+      }
+    }
 
   }
+
+  interface OnItemClickListener {
+    fun onItemClick(position: Int)
+  }
+
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EndpointViewHolder {
     val itemView = LayoutInflater.from(parent.context).inflate(R.layout.endpoint_item, parent, false)
