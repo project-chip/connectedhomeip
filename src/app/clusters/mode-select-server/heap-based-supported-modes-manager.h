31c9e59a2db1ea1f2d230336445301d7c6035a6c
Li-An Ding <dingl@amazon.com>
2021-10-26 08:55:46 -0700

    Migrate to static allocation.

--- src/app/clusters/mode-select-server/heap-based-supported-modes-manager.h
+++ src/app/clusters/mode-select-server/heap-based-supported-modes-manager.h
@@ -18,9 +18,11 @@
 
 #pragma once
 
+#include <cstring>
 #include <app/clusters/mode-select-server/supported-modes-manager.h>
 #include <map>
 #include <vector>
+#include <app/util/af.h>
 
 namespace chip {
 namespace app {
@@ -28,51 +30,102 @@ namespace Clusters {
 namespace ModeSelectCluster {
 
 /**
- * This implementation uses the heap to store the supported options
+ * This implementation statically defines the options.
  */
+
 class HeapBasedSupportedModesManager : public chip::app::Clusters::ModeSelectCluster::SupportedModesManager
 {
     using ModeOptionStructType = Structs::ModeOptionStruct::Type;
+    using storage_value_type        = const ModeOptionStructType*;
+
+    static const ModeOptionStructType blackOption;
+    static const ModeOptionStructType cappuccinoOption;
+    static const ModeOptionStructType espressoOption;
+
+    static const ModeOptionStructType* coffeeOptions[];
+    static const Span<storage_value_type> coffeeOptionsSpan;
+    static const std::map<EndpointId, Span<storage_value_type>> optionsByEndpoints;
+
+
 
 public:
-    /**
-     * A builder for HeapBasedSupportedModesManager, for conveniently injecting the supported options and building the
-     * SupportedModesManager.
-     */
-    class Builder
-    {
-    public:
-        /**
-         * Fluent method for adding a ModeOptionStruct::Type.
-         * @param endpointId The endpoint to which to add the supported mode
-         * @return this builder instance.
-         */
-        Builder & addSupportedMode(EndpointId endpointId, ModeOptionStructType & modeOptionStruct);
-        /**
-         * Fluent method for adding a ModeOptionStruct::Type.
-         * @param endpointId The endpoint to which to add the supported mode
-         * @return this builder instance.
-         */
-        Builder & addSupportedMode(EndpointId endpointId, ModeOptionStructType && modeOptionStruct);
-        HeapBasedSupportedModesManager build();
 
-    private:
-        std::map<EndpointId, std::vector<ModeOptionStructType> *> mSupportedModesByEndpoints;
+    static const HeapBasedSupportedModesManager instance;
+
+    struct Iterator: public chip::app::Clusters::ModeSelectCluster::SupportedModesManager::ModeOptionStructIterator
+    {
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = storage_value_type*;
+        using base_iterator_type= chip::app::Clusters::ModeSelectCluster::SupportedModesManager::ModeOptionStructIterator;
+
+        Iterator(const pointer aPtr):mPtr(aPtr) {}
+        ~Iterator() = default;
+
+        const ModeOptionStructType& operator*() const override { return **mPtr; }
+        const ModeOptionStructType* operator->() override { return *mPtr; }
+        const ModeOptionStructType* operator->() const override { return *mPtr; }
+
+        // Prefix increment
+        base_iterator_type& operator++() override { ++mPtr; return *this; }
+
+        bool operator== (const base_iterator_type& other) const override
+        { 
+            // Warning: we are not doing type check
+            // TODO: use of typeid requires -frtti
+            // if (typeid(other) != typeid(*this))
+            // {
+            //     return false;
+            // }
+            return this->operator->() == other.operator->();
+        }
+        bool operator!= (const base_iterator_type& other) const override { return !((*this) == other); }
+
+        private:
+            pointer mPtr;
+
     };
 
-    const std::vector<ModeOptionStructType> getSupportedModesForEndpoint(EndpointId endpointId) const override;
+    struct IteratorFactory: public chip::app::Clusters::ModeSelectCluster::SupportedModesManager::ModeOptionStructIteratorFactory
+    {
+        using pointer = Iterator*;
+        using const_pointer = const pointer;
+
+        IteratorFactory(const Span<storage_value_type>& aSupportedOptions): _begin(Iterator(aSupportedOptions.data())), _end(Iterator(aSupportedOptions.data() + aSupportedOptions.size())) { }
+        ~IteratorFactory() = default;
+
+        const Iterator* begin() const override { return &_begin; }
+        const Iterator* end() const override { return &_end; }
+
+        private:
+            const Iterator _begin;
+            const Iterator _end;
+
+    };
+
+    const IteratorFactory* getIteratorFactory(EndpointId endpointId) const override;
 
     EmberAfStatus getModeOptionByMode(EndpointId endpointId, uint8_t mode, const ModeOptionStructType *& dataPtr) const override;
 
+    ~HeapBasedSupportedModesManager() {};
+
+    HeapBasedSupportedModesManager(): HeapBasedSupportedModesManager(&optionsByEndpoints) {}
+
+    static inline const HeapBasedSupportedModesManager& getHeapBasedSupportedModesManagerInstance() { return instance; }
+
 private:
-    HeapBasedSupportedModesManager(std::map<EndpointId, const std::vector<ModeOptionStructType>> supportedModes) :
-        _supportedModesForAllEndpoints(supportedModes)
-    {}
+    HeapBasedSupportedModesManager(const std::map<EndpointId, Span<storage_value_type>>* const supportedModes): _iteratorFactoriesByEndpoints(std::map<EndpointId, IteratorFactory>()) {
+        for(auto& entry: *supportedModes)
+        {
+            _iteratorFactoriesByEndpoints.insert(std::pair<EndpointId, IteratorFactory>(entry.first, IteratorFactory(entry.second)));
+        }
+    }
     // TODO: Implement move constructor?
 
-    // endpoint index -> **ModeOptionStruct;
-    std::map<EndpointId, const std::vector<ModeOptionStructType>> _supportedModesForAllEndpoints;
+    std::map<EndpointId, IteratorFactory> _iteratorFactoriesByEndpoints;
 };
+
+
 } // namespace ModeSelectCluster
 } // namespace Clusters
 } // namespace app
