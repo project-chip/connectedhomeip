0e052504c67ccdcba16547c920d79cfeb969d88b
Shubham Patil <shubham.patil@espressif.com>
2021-10-27 15:15:06 +0530

    Added OTA requester and provider app for esp32

--- examples/ota-provider-app/ota-provider-common/BdxOtaSender.h
+++ examples/ota-provider-app/ota-provider-common/BdxOtaSender.h
@@ -20,12 +20,23 @@
 
 #pragma once
 
+// Callback to handle block query
+typedef void (*OnBdxBlockQuery)(void * context, chip::System::PacketBufferHandle & blockBuf, size_t & size, bool & isEof,
+                                uint32_t offset);
+typedef void (*OnBdxTransferComplete)(void * context);
+typedef void (*OnBdxTransferFailed)(void * context);
+
+struct BdxOtaSenderCallbacks
+{
+    chip::Callback::Callback<OnBdxBlockQuery> * onBlockQuery             = nullptr;
+    chip::Callback::Callback<OnBdxTransferComplete> * onTransferComplete = nullptr;
+    chip::Callback::Callback<OnBdxTransferFailed> * onTransferFailed     = nullptr;
+};
+
 class BdxOtaSender : public chip::bdx::Responder
 {
 public:
-    BdxOtaSender();
-
-    void SetFilepath(const char * path);
+    void SetCallbacks(BdxOtaSenderCallbacks callbacks);
 
 private:
     // Inherited from bdx::TransferFacilitator
@@ -33,8 +44,9 @@ private:
 
     void Reset();
 
-    static constexpr size_t kFilepathMaxLength = 256;
-    char mFilepath[kFilepathMaxLength];
-
     uint32_t mNumBytesSent = 0;
+
+    chip::Callback::Callback<OnBdxBlockQuery> * mOnBlockQueryCallback             = nullptr;
+    chip::Callback::Callback<OnBdxTransferComplete> * mOnTransferCompleteCallback = nullptr;
+    chip::Callback::Callback<OnBdxTransferFailed> * mOnTransferFailedCallback     = nullptr;
 };
