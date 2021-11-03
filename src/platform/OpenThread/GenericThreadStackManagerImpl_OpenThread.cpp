1a23e9f3bfefbedd7c3a189acc964c98c7ed0a63
Junior Martinez <junior.martinez@silabs.com>
2021-11-03 14:00:43 -0400

    dns browse had the same deadlock. Not that it works, browse commissionable result was causing an overflow of the thread task in EFR32, Bump the task stack to 4k

--- src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.cpp
+++ src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.cpp
@@ -1971,8 +1971,6 @@ void GenericThreadStackManagerImpl_OpenThread<ImplClass>::OnDnsBrowseResult(otEr
         return;
     }
 
-    ThreadStackMgrImpl().LockThreadStack();
-
     VerifyOrExit(aError == OT_ERROR_NONE, error = MapOpenThreadError(aError));
 
     error = MapOpenThreadError(otDnsBrowseResponseGetServiceName(aResponse, type, sizeof(type)));
@@ -2004,8 +2002,6 @@ void GenericThreadStackManagerImpl_OpenThread<ImplClass>::OnDnsBrowseResult(otEr
     }
 
 exit:
-
-    ThreadStackMgrImpl().UnlockThreadStack();
 
     // In case no service was found invoke callback to notify about failure. In other case it was already called before.
     if (!wasAnythingBrowsed)
