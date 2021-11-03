aa317f4961187fcc26c7d1372a1dd8dfcd0c3e69
JasonLiuZhuoCheng <jasonlzc2017@gmail.com>
2021-11-03 15:50:03 -0400

    add responseValueInfo class instead of string split

--- src/controller/java/src/chip/clusterinfo/ClusterCommandCallback.java
+++ src/controller/java/src/chip/clusterinfo/ClusterCommandCallback.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Interface for making customized callback that implements both onSuccess and onFailure functions.
  */
 public interface ClusterCommandCallback {
-  void onSuccess(Map<String, Object> responseValues);
+  void onSuccess(Map<ResponseValueInfo, Object> responseValues);
 
   void onFailure(Exception exception);
 }
