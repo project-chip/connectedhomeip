60edbda80cad9a40c6f7a2e0b74d96401b5da5b8
Shubham Patil <shubham.patil@espressif.com>
2021-11-03 15:36:17 +0530

    Apply suggestions from code review
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- examples/ota-requestor-app/esp32/main/DeviceCallbacks.cpp
+++ examples/ota-requestor-app/esp32/main/DeviceCallbacks.cpp
@@ -68,11 +68,11 @@ void DeviceCallbacks::DeviceEventCallback(const ChipDeviceEvent * event, intptr_
 void DeviceCallbacks::PostAttributeChangeCallback(EndpointId endpointId, ClusterId clusterId, AttributeId attributeId, uint8_t mask,
                                                   uint8_t type, uint16_t size, uint8_t * value)
 {
-    ESP_LOGI(TAG, "PostAttributeChangeCallback - Cluster ID: '0x%04x', EndPoint ID: '0x%02x', Attribute ID: '0x%04x'", clusterId,
-             endpointId, attributeId);
+    ESP_LOGI(TAG, "PostAttributeChangeCallback - Cluster ID: '" ChipLogFormatMEI "', EndPoint ID: '0x%02x', Attribute ID: '" ChipLogFormatMEI "'", ChipLogValueMEI(clusterId),
+             endpointId, ChipLogValueMEI(attributeId));
 
     // TODO handle this callback in switch statement
-    ESP_LOGI(TAG, "Unhandled cluster ID: %d", clusterId);
+    ESP_LOGI(TAG, "Unhandled cluster ID: " ChipLogFormatMEI, ChipLogValueMEI(clusterId));
 
     ESP_LOGI(TAG, "Current free heap: %d\n", heap_caps_get_free_size(MALLOC_CAP_8BIT));
 }
