b0a75d3a09ed73d64c5b365f03a3bed050ee5103
Boris Zbarsky <bzbarsky@apple.com>
2021-11-09 03:08:06 -0500

    Convert ZCL null values to/from TLV null when attributes are read/written.
    This implements the following semantics, for attributes we store in
    the attribute store (integers, booleans, strings, octet strings):
    
    Writes:
    
    * An attempt to write TLV null for non-nullable attributes leads to an
      error.
    
    * Writing TLV null to a nullable string or octet string sets its length
      to 0xFFFF or 0xFF depending on whether it's a long string or not.
    
    * Writing TLV null to a nullable integer or boolean uses the "NaU" or
      "NaS" ZCL representation to represent it.
    
    * Writing a TLV integer to an integer attribute makes a
      CanRepresentValue check and returns an error if it returns false.
      For now CanRepresentValue only disallows the NaU/NaS value for
      nullable attributes.
    
    Reads:
    
    * When reading a nullable integer or boolean, NaU/NaS is converted to TLV null.
    
    * When reading an integer or boolean all other cases go through a
      CanRepresentValue check (in case the value in the attr store is not
      actually valid) and return error if it returns false.
    
    * When reading a nullable string or octet string, length 0xFFFF/0xFF
      is converted to TLV null.
    
    * When reading a non-nullable string or octet string, length
      0xFFFF/0xFF leads to an error.

---
+++ src/app/util/attribute-storage-helpers.h
@@ -0,0 +1,75 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ * Helpers for manipulating values that are going into and out of the attribute
+ * store.
+ */
+
+#pragma once
+
+#include <lib/support/TypeTraits.h>
+
+#include <limits>
+
+namespace chip {
+namespace app {
+
+template <typename T>
+struct NumericAttributeTraits
+{
+    // StorageType is the type used to represent this C++ type in the attribute
+    // store.
+    using StorageType = T;
+
+    // The value reserved in the value space of StorageType to represent null,
+    // for cases when we have a nullable value.
+    static constexpr StorageType kNullValue =
+        std::is_signed<T>::value ? std::numeric_limits<T>::min() : std::numeric_limits<T>::max();
+
+    static constexpr bool IsNullValue(StorageType value) { return value == kNullValue; }
+
+    // Test whether a value can be represented in a "not null" value of the
+    // given type, which may be a nullable value or not.  This needs to be
+    // implemented for both T and StorageType if the two are distinct.
+    static constexpr bool CanRepresentValue(bool isNullable, T value)
+    {
+        // For now, allow the null-marker value for non-nullable types.  It's
+        // not what the spec says to do at the moment, but that might well
+        // change, and we have quite a number of tests relying on this behavior
+        // for now that we should only change once the spec really decides what
+        // it's doing.
+        return !isNullable || !IsNullValue(value);
+    }
+};
+
+template <>
+struct NumericAttributeTraits<bool>
+{
+    using StorageType                       = uint8_t;
+    static constexpr StorageType kNullValue = 0xFF;
+    static constexpr bool IsNullValue(StorageType value) { return value == kNullValue; }
+    static constexpr bool CanRepresentValue(bool isNullable, StorageType value)
+    {
+        // This treats all nonzero values (except the null value) as true.
+        return !IsNullValue(value);
+    }
+    static constexpr bool CanRepresentValue(bool isNullable, bool value) { return true; }
+};
+
+} // namespace app
+} // namespace chip
