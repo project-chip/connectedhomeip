9fceb82ef87b699347c7a2893098e5fd18097f87
Restyled.io <commits@restyled.io>
2021-11-09 17:02:02 +0000

    Restyled by shfmt

--- scripts/tests/test_suites.sh
+++ scripts/tests/test_suites.sh
@@ -18,7 +18,7 @@
 
 set -e
 set -o pipefail # Fail if anything in a pipeline fails, not just the last
-                # command which for us tends to be 'tee'.
+# command which for us tends to be 'tee'.
 
 declare -i iterations=2
 declare -i delay=0
