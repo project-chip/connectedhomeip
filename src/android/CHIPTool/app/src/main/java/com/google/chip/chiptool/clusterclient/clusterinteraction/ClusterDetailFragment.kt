d0284b19b6d4e1bd4f561a7448b39c781682d846
JasonLiuZhuoCheng <jasonlzc2017@gmail.com>
2021-11-10 16:40:44 -0500

    improve ui to show list index and simplified class name

--- src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/clusterinteraction/ClusterDetailFragment.kt
+++ src/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/clusterclient/clusterinteraction/ClusterDetailFragment.kt
@@ -190,20 +190,21 @@ class ClusterDetailFragment : Fragment() {
           emptyCallback.clusterCallbackNameTv.text = "Result is empty"
           callbackList.addView(emptyCallback)
         } else {
-          response.forEach {
+          response.forEachIndexed { index, it ->
             val objectCallback =
               inflater.inflate(R.layout.cluster_callback_item, null, false) as ConstraintLayout
-            objectCallback.clusterCallbackNameTv.text = variableNameType.name
+            objectCallback.clusterCallbackNameTv.text = variableNameType.name + "[$index]"
             val objectString = it.toString()
-            objectCallback.clusterCallbackDataTv.text = it!!.javaClass.toString().split('$').last()
+            val callbackClassName = it!!.javaClass.toString().split('$').last()
+            objectCallback.clusterCallbackDataTv.text = callbackClassName
             objectCallback.clusterCallbackDataTv.setOnClickListener {
               AlertDialog.Builder(requireContext())
-                .setTitle(variableNameType.name)
+                .setTitle(callbackClassName)
                 .setMessage(objectString)
                 .create()
                 .show()
             }
-            objectCallback.clusterCallbackTypeTv.text = variableNameType.type
+            objectCallback.clusterCallbackTypeTv.text = "List<$callbackClassName>"
             callbackList.addView(objectCallback)
           }
         }
