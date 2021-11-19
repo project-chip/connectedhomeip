955c20b486deacafbd7d0592df85b4fe9c54e7d9
Boris Zbarsky <bzbarsky@apple.com>
2021-11-19 11:47:37 -0500

    Add an accessing fabric index to AttributeAccessInterface writes.
    We will need this for bindings.

--- src/app/util/ember-compatibility-functions.cpp
+++ src/app/util/ember-compatibility-functions.cpp
@@ -592,7 +592,7 @@ CHIP_ERROR WriteSingleClusterData(ClusterInfo & aClusterInfo, TLV::TLVReader & a
     AttributeAccessInterface * attrOverride = findAttributeAccessOverride(aClusterInfo.mEndpointId, aClusterInfo.mClusterId);
     if (attrOverride != nullptr)
     {
-        AttributeValueDecoder valueDecoder(aReader);
+        AttributeValueDecoder valueDecoder(aReader, apWriteHandler->GetAccessingFabricIndex());
         ReturnErrorOnFailure(attrOverride->Write(aPath, valueDecoder));
 
         if (valueDecoder.TriedDecode())
