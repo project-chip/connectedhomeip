81e4e38fadc0f16ffa48b7210987c572f7622839
Marc Lepage <mlepage@google.com>
2021-11-19 18:46:36 -0500

    Remove debug logs and fix assignment
    Last things I was working on while trying to get tests running and
    restyler working.

--- src/app/CommandHandler.cpp
+++ src/app/CommandHandler.cpp
@@ -90,7 +90,6 @@ CHIP_ERROR CommandHandler::ProcessInvokeRequest(System::PacketBufferHandle && pa
     TLV::TLVReader invokeRequestsReader;
     InvokeRequestMessage::Parser invokeRequestMessage;
     InvokeRequests::Parser invokeRequests;
-    ChipLogDetail(DataManagement, "========= D");
     reader.Init(std::move(payload));
     ReturnErrorOnFailure(reader.Next());
     ReturnErrorOnFailure(invokeRequestMessage.Init(reader));
@@ -100,20 +99,15 @@ CHIP_ERROR CommandHandler::ProcessInvokeRequest(System::PacketBufferHandle && pa
     ReturnErrorOnFailure(invokeRequestMessage.GetSuppressResponse(&mSuppressResponse));
     ReturnErrorOnFailure(invokeRequestMessage.GetTimedRequest(&mTimedRequest));
     ReturnErrorOnFailure(invokeRequestMessage.GetInvokeRequests(&invokeRequests));
-    ChipLogDetail(DataManagement, "========= E");
 
     invokeRequests.GetReader(&invokeRequestsReader);
     while (CHIP_NO_ERROR == (err = invokeRequestsReader.Next()))
     {
-        ChipLogDetail(DataManagement, "========= F");
         VerifyOrReturnError(TLV::AnonymousTag == invokeRequestsReader.GetTag(), CHIP_ERROR_INVALID_TLV_TAG);
         CommandDataIB::Parser commandData;
         ReturnErrorOnFailure(commandData.Init(invokeRequestsReader));
-        ChipLogDetail(DataManagement, "========= G");
         ReturnErrorOnFailure(ProcessCommandDataIB(commandData));
-        ChipLogDetail(DataManagement, "========= H");
     }
-    ChipLogDetail(DataManagement, "========= I");
 
     // if we have exhausted this container
     if (CHIP_END_OF_TLV == err)
@@ -199,7 +193,7 @@ CHIP_ERROR CommandHandler::ProcessCommandDataIB(CommandDataIB::Parser & aCommand
 
     if (mpExchangeCtx && mpExchangeCtx->HasSessionHandle())
     {
-        mpExchangeCtx->GetSessionHandle().GetSubjectDescriptor();
+        subjectDescriptor = mpExchangeCtx->GetSessionHandle().GetSubjectDescriptor();
     }
 
     err = aCommandElement.GetPath(&commandPath);
