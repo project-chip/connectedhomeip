9517caf4ed8e8cc7a4ed1d8cd6b0386ea4009d74
Restyled.io <commits@restyled.io>
2021-11-20 22:48:19 +0000

    Restyled by clang-format

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -812,7 +812,7 @@ EmberStatus emberAfSendDefaultResponseWithCallback(const EmberAfClusterCommand *
     appResponseLength = 0;
     frameControl      = static_cast<uint8_t>(ZCL_GLOBAL_COMMAND |
                                         (cmd->direction == ZCL_DIRECTION_CLIENT_TO_SERVER ? ZCL_FRAME_CONTROL_SERVER_TO_CLIENT
-                                                                                               : ZCL_FRAME_CONTROL_CLIENT_TO_SERVER));
+                                                                                          : ZCL_FRAME_CONTROL_CLIENT_TO_SERVER));
 
     if (!cmd->mfgSpecific)
     {
@@ -839,23 +839,23 @@ EmberStatus emberAfSendDefaultResponse(const EmberAfClusterCommand * cmd, EmberA
 
 void emberAfCopyInt16u(uint8_t * data, uint16_t index, uint16_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
 }
 
 void emberAfCopyInt24u(uint8_t * data, uint16_t index, uint32_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
-    data[index + 2] = (uint8_t) (((x) >> 16) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
+    data[index + 2] = (uint8_t)(((x) >> 16) & 0xFF);
 }
 
 void emberAfCopyInt32u(uint8_t * data, uint16_t index, uint32_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
-    data[index + 2] = (uint8_t) (((x) >> 16) & 0xFF);
-    data[index + 3] = (uint8_t) (((x) >> 24) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
+    data[index + 2] = (uint8_t)(((x) >> 16) & 0xFF);
+    data[index + 3] = (uint8_t)(((x) >> 24) & 0xFF);
 }
 
 void emberAfCopyString(uint8_t * dest, const uint8_t * src, size_t size)
