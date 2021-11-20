649ef9fa856eb9341e31f3c37de1d58ff05211f9
Robert Szewczyk <szewczyk@google.com>
2021-11-19 16:02:44 -0800

    Initial cut at the server-side event API
    Not yet completely hooked up to the EventManagement but the key aspect
    of the API present.

---
+++ src/app/EventLogging.h
@@ -0,0 +1,53 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <app/EventLoggingDelegate.h>
+#include <app/ConcreteEventPath.h>
+#include <app/data-model/Decode.h>
+#include <app/data-model/List.h> // So we can encode lists
+
+namespace chip {
+namespace app {
+
+template <typename T>
+class EventLogger : EventLoggingDelegate
+{
+  public:
+    EventLogger(const T &aEventData) : mEventData(aEventData) {};
+    CHIP_ERROR WriteEvent(chip::TLV::TLVWriter &aWriter) final override
+    {
+        return mEventData.Encode(aWriter, TLV::AnonymousTag);
+    }
+  private:
+    const T &mEventData;
+};
+
+template <typename T>
+CHIP_ERROR LogEvent(const T &aEventData, EndpointId aEndpoint, EventOptions aEventOptions, EventNumber &aEventNumber)
+{
+    EventLogger<T> eventData(aEventData);
+    ConcreteEventPath path(aEndpoint, aEventData.GetClusterId(), aEventData.GetEventId());
+    // log the actual event
+    aEventNumber = 0;
+    return CHIP_NO_ERROR;
+}
+
+} // namespace app
+} // namespace chip
