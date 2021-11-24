c7052b9d272365c8e0cf21c999375d8fdca7e456
Sergei Lissianoi <selissia@silabs.com>
2021-11-24 09:47:11 -0500

    Test commit, ignore

--- examples/ota-requestor-app/linux/main.cpp
+++ examples/ota-requestor-app/linux/main.cpp
@@ -16,6 +16,9 @@
  *    limitations under the License.
  */
 
+// Trivial change to test build. Do not push to master
+
+
 #include <app-common/zap-generated/callback.h>
 #include <app-common/zap-generated/cluster-objects.h>
 #include <app/OperationalDeviceProxy.h>
