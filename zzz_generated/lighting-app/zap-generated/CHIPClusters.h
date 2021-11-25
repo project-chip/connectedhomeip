df2f65690bd7024cab9e7b02f364a3f129959c2b
Andrei Litvin <andy314@gmail.com>
2021-11-25 16:04:28 -0500

    Regen again, different results

--- zzz_generated/lighting-app/zap-generated/CHIPClusters.h
+++ zzz_generated/lighting-app/zap-generated/CHIPClusters.h
@@ -58,11 +58,14 @@ public:
     ~OnOffCluster() {}
 
     // Cluster Commands
+    CHIP_ERROR Off(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback);
     CHIP_ERROR OffWithEffect(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback, uint8_t effectId,
                              uint8_t effectVariant);
+    CHIP_ERROR On(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback);
     CHIP_ERROR OnWithRecallGlobalScene(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback);
     CHIP_ERROR OnWithTimedOff(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback,
                               uint8_t onOffControl, uint16_t onTime, uint16_t offWaitTime);
+    CHIP_ERROR Toggle(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback);
 
     // Cluster Attributes
     CHIP_ERROR ReadAttributeOnOff(Callback::Cancelable * onSuccessCallback, Callback::Cancelable * onFailureCallback);
