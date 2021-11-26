b81111a57ee918e5f92792cce7cdb7fde3bbb90a
Restyled.io <commits@restyled.io>
2021-11-26 05:56:32 +0000

    Restyled by clang-format

--- src/app/util/attribute-storage.h
+++ src/app/util/attribute-storage.h
@@ -135,8 +135,8 @@ EmberAfCluster * emberAfFindClusterInTypeWithMfgCode(EmberAfEndpointType * endpo
 
 EmberAfCluster * emberAfFindClusterInType(EmberAfEndpointType * endpointType, chip::ClusterId clusterId, EmberAfClusterMask mask);
 
-// For a given cluster and mask, retrieves the list of endpoints sorted by endpoint that contain the matching cluster and returns the index
-// within that list that matches the given endpoint.
+// For a given cluster and mask, retrieves the list of endpoints sorted by endpoint that contain the matching cluster and returns
+// the index within that list that matches the given endpoint.
 //
 // Mask is either CLUSTER_MASK_CLIENT or CLUSTER_MASK_SERVER
 // For example, if you have 3 endpoints, 10, 11, 12, and cluster X server is
