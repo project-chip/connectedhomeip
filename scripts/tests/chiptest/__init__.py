32a910e46dc60092a027179ab9791cce5b8c64a1
Andrei Litvin <andy314@gmail.com>
2021-11-29 16:26:05 -0500

    Update to the test list command to finding out available tests

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -30,19 +30,9 @@ def AllTests(chip_tool: str):
     """Executes `chip_tool` binary to see what tests are available.
     """
 
-    result = subprocess.run([chip_tool, 'tests'], capture_output=True)
+    result = subprocess.run([chip_tool, 'tests', 'list'], capture_output=True)
 
-    # Unfortunately there is no 'LIST' command and result return code is 1 (i.e. error)
-    # however the above will return tests in stderr
-    test_re = re.compile('^  \\| \\* ([^ ]*) *\\|$')
-
-    for line in result.stderr.decode('utf8').split('\n'):
-        m = test_re.match(line)
-        if not m:
-            continue
-
-        name = m.group(1)
-
+    for name in result.stdout.decode('utf8').split('\n'):
         if name.startswith('TV_'):
             target = TestTarget.TV
         else:
