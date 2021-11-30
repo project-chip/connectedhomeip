e443e00198049ed0733c6be5f6c776247208ba0b
Mathieu Kardous <mathieu.kardous@silabs.com>
2021-11-30 10:49:16 -0500

    Added test for group command

--- src/app/zap-templates/common/ClusterTestGeneration.js
+++ src/app/zap-templates/common/ClusterTestGeneration.js
@@ -140,6 +140,10 @@ function setDefaultTypeForCommand(test)
   default:
     test.commandName = test.command;
     test.isCommand   = true;
+    if ((kGroupId in test)) {
+      test.isGroupCommand = true;
+      test.groupId        = parseInt(test[kGroupId], 10);
+    }
     break;
   }
 
