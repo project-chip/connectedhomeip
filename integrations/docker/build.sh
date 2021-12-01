2c0ce5b1e02134ed1406e77903579cfc105c3d77
Victor Morales <v.morales@samsung.com>
2021-12-01 15:18:40 -0800

    Add docker size statistics in build script
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- integrations/docker/build.sh
+++ integrations/docker/build.sh
@@ -59,6 +59,8 @@ set -ex
 
 [[ -n $VERSION ]] || die "version cannot be empty"
 
+mb_space_before=$(df -m /var/lib/docker/ | awk 'FNR==2{print $3}')
+
 # go find and build any CHIP images this image is "FROM"
 awk -F/ '/^FROM connectedhomeip/ {print $2}' Dockerfile | while read -r dep; do
     dep=${dep%:*}
@@ -87,5 +89,10 @@ docker build "${BUILD_ARGS[@]}" --build-arg VERSION="$VERSION" -t "$ORG/$IMAGE:$
         docker push "$ORG"/"$IMAGE":latest
     }
 }
+
+docker images --filter=reference="$ORG/*"
+df -h /var/lib/docker/
+mb_space_after=$(df -m /var/lib/docker/ | awk 'FNR==2{print $3}')
+printf "%'.f MB total used\n" "$((mb_space_before - mb_space_after))"
 
 exit 0
