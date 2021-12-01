cb0ff384e9a7f4ba667561ef1ceabe12b2330514
WanqQixiang <wangqixiang@espressif.com>
2021-12-01 18:20:28 +0800

    Temporarily fix the compilation errors for ESP OTA Requestor app

--- examples/ota-requestor-app/esp32/main/OTARequesterImpl.cpp
+++ examples/ota-requestor-app/esp32/main/OTARequesterImpl.cpp
@@ -32,7 +32,7 @@
 
 using chip::ByteSpan;
 using chip::CharSpan;
-using chip::DeviceProxy;
+using chip::OperationalDeviceProxy;
 using chip::EndpointId;
 using chip::FabricIndex;
 using chip::NodeId;
@@ -56,7 +56,7 @@ void OnQueryImageFailure(void * context, EmberAfStatus status);
 void OnApplyUpdateResponse(void * context, const ApplyUpdateResponse::DecodableType & response);
 void OnApplyUpdateRequestFailure(void * context, EmberAfStatus status);
 
-void OnConnected(void * context, chip::DeviceProxy * deviceProxy);
+void OnConnected(void * context, chip::OperationalDeviceProxy * deviceProxy);
 void OnConnectionFailure(void * context, NodeId deviceId, CHIP_ERROR error);
 
 void OnBlockReceived(void * context, const chip::bdx::TransferSession::BlockData & blockdata);
@@ -175,7 +175,7 @@ void OnApplyUpdateRequestFailure(void * context, EmberAfStatus status)
     ChipLogDetail(SoftwareUpdate, "ApplyUpdateRequest failure response %" PRIu8, status);
 }
 
-void OnConnected(void * context, chip::DeviceProxy * deviceProxy)
+void OnConnected(void * context, chip::OperationalDeviceProxy * deviceProxy)
 {
     ChipLogDetail(SoftwareUpdate, "Callback OnConnected");
     uint8_t * command = reinterpret_cast<uint8_t *>(context);
