b4b511294affc8acb4b1b7c214bebb44d2ead7c8
Jim Lyall <jim.lyall@qorvo.com>
2021-12-01 13:41:19 +0100

    Add Ethernet tests

--- src/darwin/Framework/CHIP/templates/tests.js
+++ src/darwin/Framework/CHIP/templates/tests.js
@@ -63,6 +63,11 @@ function getTests()
     'Test_TC_EMR_1_1',
   ];
 
+  const EthernetNetworkDiagnostics = [
+    'Test_TC_DIAGETH_1_1',
+    'Test_TC_DIAGETH_2_1',
+  ];
+
   const FlowMeasurement = [
     'Test_TC_FLW_1_1',
     'Test_TC_FLW_2_1',
@@ -191,6 +196,7 @@ function getTests()
     ColorControl, //
     DeviceManagement, //
     ElectricalMeasurement, //
+    EthernetNetworkDiagnostics, //
     FlowMeasurement, //
     IlluminanceMeasurement, //
     LevelControl, //
