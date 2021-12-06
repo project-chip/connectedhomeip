1ae6e4c7baac8d4583b8e34f1e847bb9de47f3e5
Dmitry Vasukov <dmitry.vasukov@dsr-corporation.com>
2021-12-06 22:20:02 +0300

    Cleanup Door lock cluster

--- src/app/clusters/door-lock-server/door-lock-server.h
+++ src/app/clusters/door-lock-server/door-lock-server.h
@@ -36,11 +36,11 @@ class DoorLockServer
 {
     static DoorLockServer & Instance();
 
-    void InitServer();
+    void InitServer(chip::EndpointId endpointId);
 
     bool SetLockState(chip::EndpointId endpointId, chip::app::Clusters::DoorLock::DlLockState newLockState);
-    bool SetActuatorState(chip::EndpointId endpointId, bool actuatorState);
-    bool SetDoorState(chip::EndpointId endpointId, chip::app::Clusters::DoorLock::DlLockState doorState);
+    bool SetActuatorState(chip::EndpointId endpointId, bool newActuatorState);
+    bool SetDoorState(chip::EndpointId endpointId, chip::app::Clusters::DoorLock::DlLockState newDoorState);
 
     bool SetLanguage(chip::EndpointId endpointId, const char * newLanguage);
     bool SetAutoRelockTime(chip::EndpointId, uint32_t newAutoRelockTimeSec);
@@ -53,78 +53,5 @@ private:
     static DoorLockServer instance;
 };
 
-#if 0
 bool emberAfPluginDoorLockOnDoorLockCommand(chip::EndpointId endpointId, const char * PINCOde);
 bool emberAfPluginDoorLockOnDoorUnlockCommand(chip::EndpointId endpointId, const char * PINCode);
-
-bool emberAfPluginDoorLockGetUsers(chip::EndpointId endpointId, ...);
-bool emberAfPluginDoorLockSetUser(chip::EndpointId endpointId, ...);
-bool emberAfPluginDoorLockClearUser(chip::EndpointId endpointId, ...);
-
-bool emberAfPluginDoorLockGetCredentials(chip::EndpointId endpointId, ...);
-bool emberAfPluginDoorLockSetCredential(chip::EndpointId endpointId, ...);
-bool emberAfPluginDoorLockClearCredential(chip::EndpointId endpointId, ...);
-#endif
-
-
-#if 0
-bool emberAfDoorLockClusterLockDoorCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                            const Commands::LockDoor::DecodableType & commandData);
-bool emberAfDoorLockClusterUnlockDoorCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                              const Commands::UnlockDoor::DecodableType & commandData);
-bool emberAfDoorLockClusterGetLogRecordCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                const Commands::GetLogRecord::DecodableType & commandData);
-bool emberAfDoorLockClusterSetWeekDayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::SetWeekDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterGetWeekDayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::GetWeekDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterClearWeekDayScheduleCallback(app::CommandHandler * commandObj,
-                                                        const app::ConcreteCommandPath & commandPath,
-                                                        const Commands::ClearWeekDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterSetYearDayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::SetYearDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterGetYearDayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::GetYearDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterClearYearDayScheduleCallback(app::CommandHandler * commandObj,
-                                                        const app::ConcreteCommandPath & commandPath,
-                                                        const Commands::ClearYearDaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterSetHolidayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::SetHolidaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterGetHolidayScheduleCallback(app::CommandHandler * commandObj,
-                                                      const app::ConcreteCommandPath & commandPath,
-                                                      const Commands::GetHolidaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterClearHolidayScheduleCallback(app::CommandHandler * commandObj,
-                                                        const app::ConcreteCommandPath & commandPath,
-                                                        const Commands::ClearHolidaySchedule::DecodableType & commandData);
-bool emberAfDoorLockClusterGetUserTypeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                               const Commands::GetUserType::DecodableType & commandData);
-bool emberAfDoorLockClusterSetUserTypeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                               const Commands::SetUserType::DecodableType & commandData);
-bool emberAfDoorLockClusterSetPINCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                              const Commands::SetPINCode::DecodableType & commandData);
-bool emberAfDoorLockClusterGetPINCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                              const Commands::GetPINCode::DecodableType & commandData);
-bool emberAfDoorLockClusterClearPINCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                const Commands::ClearPINCode::DecodableType & commandData);
-bool emberAfDoorLockClusterClearAllPINCodesCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                    const Commands::ClearAllPINCodes::DecodableType & commandData);
-bool emberAfDoorLockClusterSetRFIDCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                               const Commands::SetRFIDCode::DecodableType & commandData);
-bool emberAfDoorLockClusterGetRFIDCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                               const Commands::GetRFIDCode::DecodableType & commandData);
-bool emberAfDoorLockClusterClearRFIDCodeCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                 const Commands::ClearRFIDCode::DecodableType & commandData);
-bool emberAfDoorLockClusterClearAllRFIDCodesCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                     const Commands::ClearAllRFIDCodes::DecodableType & commandData);
-void emberAfPluginDoorLockServerLockoutEventHandler(void);
-void emberAfPluginDoorLockServerRelockEventHandler(void);
-bool emberAfDoorLockClusterUnlockWithTimeoutCallback(app::CommandHandler * commandObj, const app::ConcreteCommandPath & commandPath,
-                                                     const Commands::UnlockWithTimeout::DecodableType & commandData);
-void MatterDoorLockPluginServerInitCallback();
-void MatterDoorLockClusterServerAttributeChangedCallback(const app::ConcreteAttributePath & attributePath);
-#endif
