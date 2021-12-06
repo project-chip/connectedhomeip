675d9f50e0290bb4bba89f654989fd6c6d422b8f
Boris Zbarsky <bzbarsky@apple.com>
2021-12-06 09:23:40 -0500

    Remove manufacturer code arrays from endpoint_config. (#12535)
    We're storing the full MEI for cluster and attribute ids, so all this
    manufacturer code stuff is dead code.  There's a lot more to remove,
    but this is a start.

--- zzz_generated/lighting-app/zap-generated/endpoint_config.h
+++ zzz_generated/lighting-app/zap-generated/endpoint_config.h
@@ -996,21 +996,3 @@
     {                                                                                                                              \
         0, 0, 0                                                                                                                    \
     }
-
-// This is an array of EmberAfManufacturerCodeEntry structures for clusters.
-#define GENERATED_CLUSTER_MANUFACTURER_CODE_COUNT (0)
-#define GENERATED_CLUSTER_MANUFACTURER_CODES                                                                                       \
-    {                                                                                                                              \
-        {                                                                                                                          \
-            0x00, 0x00                                                                                                             \
-        }                                                                                                                          \
-    }
-
-// This is an array of EmberAfManufacturerCodeEntry structures for attributes.
-#define GENERATED_ATTRIBUTE_MANUFACTURER_CODE_COUNT (0)
-#define GENERATED_ATTRIBUTE_MANUFACTURER_CODES                                                                                     \
-    {                                                                                                                              \
-        {                                                                                                                          \
-            0x00, 0x00                                                                                                             \
-        }                                                                                                                          \
-    }
--- zzz_generated/thermostat/zap-generated/endpoint_config.h
+++ zzz_generated/thermostat/zap-generated/endpoint_config.h
@@ -1080,21 +1080,3 @@
     {                                                                                                                              \
         0, 0                                                                                                                       \
     }
-
-// This is an array of EmberAfManufacturerCodeEntry structures for clusters.
-#define GENERATED_CLUSTER_MANUFACTURER_CODE_COUNT (0)
-#define GENERATED_CLUSTER_MANUFACTURER_CODES                                                                                       \
-    {                                                                                                                              \
-        {                                                                                                                          \
-            0x00, 0x00                                                                                                             \
-        }                                                                                                                          \
-    }
-
-// This is an array of EmberAfManufacturerCodeEntry structures for attributes.
-#define GENERATED_ATTRIBUTE_MANUFACTURER_CODE_COUNT (0)
-#define GENERATED_ATTRIBUTE_MANUFACTURER_CODES                                                                                     \
-    {                                                                                                                              \
-        {                                                                                                                          \
-            0x00, 0x00                                                                                                             \
-        }                                                                                                                          \
-    }
