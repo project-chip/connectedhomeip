702206fe470aec2ce9510ccbbb3914571fc0de36
Junior Martinez <junior.martinez@silabs.com>
2021-12-14 11:53:09 -0500

    regen

--- zzz_generated/lighting-app/zap-generated/gen_config.h
+++ zzz_generated/lighting-app/zap-generated/gen_config.h
@@ -116,8 +116,8 @@
 #define EMBER_AF_PLUGIN_LEVEL_CONTROL_SERVER
 #define EMBER_AF_PLUGIN_LEVEL_CONTROL
 // User options for server plugin Level Control
-#define EMBER_AF_PLUGIN_LEVEL_CONTROL_MAXIMUM_LEVEL 254
-#define EMBER_AF_PLUGIN_LEVEL_CONTROL_MINIMUM_LEVEL 1
+#define EMBER_AF_PLUGIN_LEVEL_CONTROL_MAXIMUM_LEVEL 255
+#define EMBER_AF_PLUGIN_LEVEL_CONTROL_MINIMUM_LEVEL 0
 #define EMBER_AF_PLUGIN_LEVEL_CONTROL_RATE 0
 
 // Use this macro to check if the server side of the Localization Configuration cluster is included
