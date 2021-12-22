70307c511a7ff60c57c06686afd2a4e417940338
xylophone21 <xylophone21@gmail.com>
2021-12-22 16:47:38 +0800

    remove chip thread work on darwin because there is no "non-main" CHIP thread on darwin

--- src/lib/support/ChipThreadWork.cpp
+++ src/lib/support/ChipThreadWork.cpp
@@ -16,13 +16,7 @@
  */
 
 #include "ChipThreadWork.h"
-
-#ifdef __APPLE__
-#include <dispatch/dispatch.h>
-#else
 #include <semaphore.h>
-#endif
-
 #include <platform/CHIPDeviceLayer.h>
 
 namespace chip {
@@ -32,21 +26,12 @@ namespace {
 struct WorkData
 {
     WorkCallback callback;
-#ifdef __APPLE__
-    dispatch_semaphore_t done;
-
-    WorkData() { done = dispatch_semaphore_create(0); }
-    ~WorkData() { dispatch_release(done); }
-    void Post() { dispatch_semaphore_signal(done); }
-    void Wait() { dispatch_semaphore_wait(done, DISPATCH_TIME_FOREVER); }
-#else
     sem_t done;
 
     WorkData() { sem_init(&done, 0 /* shared */, 0); }
     ~WorkData() { sem_destroy(&done); }
     void Post() { sem_post(&done); }
     void Wait() { sem_wait(&done); }
-#endif
 };
 
 void PerformWork(intptr_t arg)
