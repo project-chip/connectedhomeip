714b14c8842a6b46ee7c8a9f2949153d37c734f3
Michael Sandstedt <michael.sandstedt@smartthings.com>
2022-01-07 10:33:12 -0600

    remove workarounds for #13327, fixed by #13321

--- src/commissioner/ExampleCommissioningStateMachine.cpp
+++ src/commissioner/ExampleCommissioningStateMachine.cpp
@@ -91,9 +91,7 @@ chip::StateMachine::Optional<State> Transitions::operator()(const State & state,
     {
         return mFactory.CreateInvokingArmFailSafe(event.Get<ArmFailSafe>());
     }
-    else if (state.Is<InvokingArmFailSafe>() &&
-             (event.Is<Success>() || event.Is<Failure>())) // TODO(#13327): accept failure; until #13327 is resolved, commissionees
-                                                           // will return ArmFailSafe instead of ArmFailSafeResponse
+    else if (state.Is<InvokingArmFailSafe>() && event.Is<Success>())
     {
         return mFactory.CreateFailSafeArmed();
     }
@@ -197,10 +195,7 @@ chip::StateMachine::Optional<State> Transitions::operator()(const State & state,
     {
         return mFactory.CreateInvokingCommissioningComplete();
     }
-    else if (state.Is<InvokingCommissioningComplete>() &&
-             (event.Is<Success>() ||
-              event.Is<Failure>())) // TODO(#13327): accept failure; until #13327 is resolved, commissionees will return
-                                    // CommissioningComplete instead of CommissioningCompleteResponse
+    else if (state.Is<InvokingCommissioningComplete>() && event.Is<Success>())
     {
         return mFactory.CreateCommissioningComplete();
     }
