12cf6a2a3ac58321085b9cd649ffe96d7dccb54a
Vivien Nicolas <vnicolas@apple.com>
2022-01-13 14:46:10 +0100

    [YAML] Add TestEvents.yaml

--- examples/chip-tool/templates/tests/tests.js
+++ examples/chip-tool/templates/tests/tests.js
@@ -536,6 +536,7 @@ function getTests()
     'TestClusterComplexTypes',
     'TestConstraints',
     'TestDelayCommands',
+    'TestEvents',
     'TestDiscovery',
     'TestLogCommands',
     'TestSaveAs',
