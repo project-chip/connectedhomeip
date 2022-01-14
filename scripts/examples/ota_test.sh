fa539dfd4079f15b0f30b573e31581a7531041a7
Michael Rupp <michael.rupp@silabs.com>
2022-01-14 09:47:10 -0500

    Add tee commands to OTA test

--- scripts/examples/ota_test.sh
+++ scripts/examples/ota_test.sh
@@ -9,23 +9,23 @@ pkill chip-ota-requestor-app
 
 scripts/examples/gn_build_example.sh examples/chip-tool out/
 
-./out/ota_provider_debug/chip-ota-provider-app -f tmp/ota.txt > /tmp/ota/provider-log.txt  &
+./out/ota_provider_debug/chip-ota-provider-app -f tmp/ota.txt | tee /tmp/ota/provider-log.txt  &
 provider_pid=$!
 
 echo  "Commissioning Provider "
 
-./out/chip-tool pairing onnetwork 1 "$ARG1" > /tmp/ota/chip-tool-commission-provider.txt 
+./out/chip-tool pairing onnetwork 1 "$ARG1" | tee /tmp/ota/chip-tool-commission-provider.txt 
 if grep "Device commissioning completed with success" /tmp/ota/chip-tool-commission-provider.txt;
 then echo Provider Commissioned;
 else echo Provider not commissioned properly;
 fi
 
-stdbuf -o0 ./out/ota_requestor_debug/chip-ota-requestor-app -u "$ARG3" -d "$ARG2" > /tmp/ota/requestor-log.txt & 
+stdbuf -o0 ./out/ota_requestor_debug/chip-ota-requestor-app -u "$ARG3" -d "$ARG2" | tee /tmp/ota/requestor-log.txt & 
 requestor_pid=$!
 
 echo  "Commissioning Requestor "
 
-./out/chip-tool pairing onnetwork-long 2 "$ARG1" "$ARG2"  > /tmp/ota/chip-tool-commission-requestor.txt 
+./out/chip-tool pairing onnetwork-long 2 "$ARG1" "$ARG2" | tee /tmp/ota/chip-tool-commission-requestor.txt 
 
 if grep "Device commissioning completed with success" /tmp/ota/chip-tool-commission-requestor.txt;
 then echo Requestor Commissioned;
@@ -34,7 +34,7 @@ fi
 
 echo  "Sending announce-ota-provider "
 
-./out/chip-tool otasoftwareupdaterequestor announce-ota-provider 1 0 0 0 2 0  > /tmp/ota/chip-tool-announce-ota.txt 
+./out/chip-tool otasoftwareupdaterequestor announce-ota-provider 1 0 0 0 2 0 | tee /tmp/ota/chip-tool-announce-ota.txt 
 
 echo  "Sleeping for 20 seconds "
 sleep 20
