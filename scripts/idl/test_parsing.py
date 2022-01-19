71eb8b6947a84c04dabc2f351bff22269a9a46c6
Andrei Litvin <andy314@gmail.com>
2022-01-19 14:46:36 -0500

    make the structs a bit more compact: easier to read

--- scripts/idl/test_parsing.py
+++ scripts/idl/test_parsing.py
@@ -27,68 +27,77 @@ def parseText(txt):
 class TestParser(unittest.TestCase):
 
     def test_skips_comments(self):
-        self.assertEqual(parseText("""
+        actual = parseText("""
             // this is a single line comment
             // repeated
 
             /* This is a C++ comment
                and also whitespace should be ignored
              */
-        """), Idl())
+        """)
+        expected = Idl()
+
+        self.assertEqual(actual, expected)
 
     def test_global_enum(self):
-        self.assertEqual(parseText("""
+        actual = parseText("""
             enum GlobalEnum : ENUM8 {
                kValue1 = 1;
                kOther = 0x12; /* hex numbers tested sporadically */
             }
-        """),
-                         Idl(enums=[
-                             Enum(name='GlobalEnum', base_type='ENUM8',
-                                  entries=[
-                                      EnumEntry(name="kValue1", code=1),
-                                      EnumEntry(name="kOther", code=0x12),
-                                  ])]
-                             ))
+        """)
+
+        expected = Idl(enums=[
+            Enum(name='GlobalEnum', base_type='ENUM8',
+                 entries=[
+                     EnumEntry(name="kValue1", code=1),
+                     EnumEntry(name="kOther", code=0x12),
+                 ])]
+        )
+        self.assertEqual(actual, expected)
 
     def test_global_struct(self):
-        self.assertEqual(parseText("""
+        actual = parseText("""
             struct Something {
                 CHAR_STRING astring = 1;
                 optional CLUSTER_ID idlist[] = 2;
                 nullable int valueThatIsNullable = 0x123;
             }
-        """),
-                         Idl(structs=[
-                             Struct(name='Something',
-                                    members=[
-                                        StructureMember(
-                                            data_type="CHAR_STRING", code=1, name="astring", ),
-                                        StructureMember(data_type="CLUSTER_ID", code=2, name="idlist", is_list=True, attributes=set(
-                                            [MemberAttribute.OPTIONAL])),
-                                        StructureMember(data_type="int", code=0x123, name="valueThatIsNullable", attributes=set(
-                                            [MemberAttribute.NULLABLE])),
-                                    ])]
-                             ))
+        """)
+
+        expected = Idl(structs=[
+            Struct(name='Something',
+                   members=[
+                        StructureMember(
+                            data_type="CHAR_STRING", code=1, name="astring", ),
+                        StructureMember(data_type="CLUSTER_ID", code=2, name="idlist", is_list=True, attributes=set(
+                            [MemberAttribute.OPTIONAL])),
+                        StructureMember(data_type="int", code=0x123, name="valueThatIsNullable", attributes=set(
+                            [MemberAttribute.NULLABLE])),
+                   ])]
+        )
+        self.assertEqual(actual, expected)
 
     def test_cluster_attribute(self):
-        self.assertEqual(parseText("""
+        actual = parseText("""
             server cluster MyCluster = 0x321 {
                 attribute(readonly) int8u roAttr = 1;
                 attribute(writable) int32u rwAttr[] = 123;
             }
-        """),
-                         Idl(clusters=[
-                             Cluster(side=ClusterSide.SERVER,
-                                     name="MyCluster",
-                                     code=0x321,
-                                     attributes=[
-                                         Attribute(access=AttributeAccess.READONLY, definition=StructureMember(
-                                             data_type="int8u", code=1, name="roAttr")),
-                                         Attribute(access=AttributeAccess.READWRITE, definition=StructureMember(
-                                             data_type="int32u", code=123, name="rwAttr", is_list=True)),
-                                     ]
-                                     )]))
+        """)
+
+        expected = Idl(clusters=[
+            Cluster(side=ClusterSide.SERVER,
+                    name="MyCluster",
+                    code=0x321,
+                    attributes=[
+                        Attribute(access=AttributeAccess.READONLY, definition=StructureMember(
+                            data_type="int8u", code=1, name="roAttr")),
+                        Attribute(access=AttributeAccess.READWRITE, definition=StructureMember(
+                            data_type="int32u", code=123, name="rwAttr", is_list=True)),
+                    ]
+                    )])
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
