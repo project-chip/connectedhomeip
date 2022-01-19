527467617c7a59ba3a25dde6970d13fb7a10ad2c
Yufeng Wang <yufengwang@google.com>
2022-01-19 08:48:23 -0800

    Remove unused configuration items from ConfigurationMgr (#13672)

--- src/platform/Ameba/AmebaConfig.h
+++ src/platform/Ameba/AmebaConfig.h
@@ -68,8 +68,6 @@ public:
     static const Key kConfigKey_CountryCode;
     static const Key kConfigKey_ActiveLocale;
     static const Key kConfigKey_Breadcrumb;
-    static const Key kConfigKey_HourFormat;
-    static const Key kConfigKey_CalendarType;
 
     // Counter keys
     static const Key kCounterKey_RebootCount;
