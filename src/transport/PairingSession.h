15e5ea3c90a2725204b3c05be815ece661794112
Evgeni Margolis <emargolis@google.com>
2022-01-20 09:26:06 -0800

    Don't Clear mSecureSessionType on PairingSession::Clear()

--- src/transport/PairingSession.h
+++ src/transport/PairingSession.h
@@ -177,10 +177,9 @@ protected:
     // TODO: remove Clear, we should create a new instance instead reset the old instance.
     void Clear()
     {
-        mSecureSessionType = Transport::SecureSession::Type::kUndefined;
-        mPeerNodeId        = kUndefinedNodeId;
-        mPeerCATs          = kUndefinedCATs;
-        mPeerAddress       = Transport::PeerAddress::Uninitialized();
+        mPeerNodeId  = kUndefinedNodeId;
+        mPeerCATs    = kUndefinedCATs;
+        mPeerAddress = Transport::PeerAddress::Uninitialized();
         mPeerSessionId.ClearValue();
         mLocalSessionId = kInvalidKeyId;
     }
