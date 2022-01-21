09aa47fc78c3bcf46f74500f09df96ac230053d5
jepenven-silabs <jean-francois.penven@silabs.com>
2022-01-21 11:59:29 -0500

    Added persistence for GroupDataProvider

--- src/lib/support/TestPersistentStorageDelegate.h
+++ src/lib/support/TestPersistentStorageDelegate.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <algorithm>
+#include <credentials/FabricTable.h>
 #include <lib/core/CHIPCore.h>
 #include <lib/core/CHIPPersistentStorageDelegate.h>
 #include <lib/support/DLLUtil.h>
@@ -28,7 +29,7 @@
 
 namespace chip {
 
-class TestPersistentStorageDelegate : public PersistentStorageDelegate
+class TestPersistentStorageDelegate : public PersistentStorageDelegate, public FabricStorage
 {
 public:
     TestPersistentStorageDelegate() {}
@@ -59,6 +60,18 @@ public:
         mStorage.erase(key);
         return CHIP_NO_ERROR;
     }
+
+    CHIP_ERROR SyncStore(FabricIndex fabricIndex, const char * key, const void * buffer, uint16_t size) override
+    {
+        return SyncSetKeyValue(key, buffer, size);
+    };
+
+    CHIP_ERROR SyncLoad(FabricIndex fabricIndex, const char * key, void * buffer, uint16_t & size) override
+    {
+        return SyncGetKeyValue(key, buffer, size);
+    };
+
+    CHIP_ERROR SyncDelete(FabricIndex fabricIndex, const char * key) override { return SyncDeleteKeyValue(key); };
 
 protected:
     std::map<std::string, std::vector<uint8_t>> mStorage;
