c3b0b0581cc68b1e608af09c66be69121b410c9f
Andrei Litvin <andy314@gmail.com>
2022-01-22 00:07:03 -0500

    Add license and help text to codegen.py

--- scripts/codegen.py
+++ scripts/codegen.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+# Copyright (c) 2022 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import click
 import logging
@@ -66,6 +79,10 @@ __GENERATORS__ = {
     'idl_path',
     type=click.Path(exists=True))
 def main(log_level, generator, output_dir, dry_run, idl_path):
+    """
+    Parses MATTER IDL files (.matter) and performs SDK code generation
+    as set up by the program arguments.
+    """
     coloredlogs.install(level=__LOG_LEVELS__[
                         log_level], fmt='%(asctime)s %(levelname)-7s %(message)s')
     logging.info("Parsing idl from %s" % idl_path)
