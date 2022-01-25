421a4f01ba05e97019f6c52d3a89be0e91ad1cdc
Vivien Nicolas <vnicolas@apple.com>
2022-01-25 15:16:51 +0100

    [Test Suites] Expose methods to start/stop/reboot the accessory used for testing

--- scripts/tests/chiptest/test_definition.py
+++ scripts/tests/chiptest/test_definition.py
@@ -29,6 +29,81 @@ from random import randrange
 TEST_NODE_ID = '0x12344321'
 
 
+class App:
+    def __init__(self, runner, command):
+        self.process = None
+        self.runner = runner
+        self.command = command
+        self.stopped = False
+
+    def start(self, discriminator):
+        if not self.process:
+            self.process = None
+            process, outpipe, errpipe = self.__startServer(
+                self.runner, self.command, discriminator)
+            self.__waitForServerReady(outpipe)
+            self.__updateSetUpCode(outpipe)
+            self.process = process
+            self.stopped = False
+            return True
+        return False
+
+    def stop(self):
+        if self.process:
+            self.stopped = True
+            self.process.kill()
+            self.process.wait(10)
+            self.process = None
+            return True
+        return False
+
+    def reboot(self, discriminator):
+        if self.process:
+            self.stop()
+            self.start(discriminator)
+            return True
+        return False
+
+    def poll(self):
+        # When the server is manually stopped, process polling is overriden so the other
+        # processes that depends on the accessory beeing alive does not stop.
+        if self.stopped:
+            return None
+        return self.process.poll()
+
+    def kill(self):
+        if self.process:
+            self.process.kill()
+
+    def wait(self, duration):
+        if self.process:
+            self.process.wait(duration)
+
+    def __startServer(self, runner, command, discriminator):
+        logging.debug(
+            'Executing application under test with discriminator %s.' % discriminator)
+        app_cmd = command + ['--discriminator', str(discriminator)]
+        return runner.RunSubprocess(app_cmd, name='APP ', wait=False)
+
+    def __waitForServerReady(self, outpipe):
+        logging.debug('Waiting for server to listen.')
+        start_time = time.time()
+        server_is_listening = outpipe.CapturedLogContains("Server Listening")
+        while not server_is_listening:
+            if time.time() - start_time > 10:
+                raise Exception('Timeout for server listening')
+            time.sleep(0.1)
+            server_is_listening = outpipe.CapturedLogContains(
+                "Server Listening")
+        logging.debug('Server is listening. Can proceed.')
+
+    def __updateSetUpCode(self, outpipe):
+        qrLine = outpipe.FindLastMatchingLine('.*SetupQRCode: *\\[(.*)]')
+        if not qrLine:
+            raise Exception("Unable to find QR code")
+        self.setupCode = qrLine.group(1)
+
+
 class TestTarget(Enum):
     ALL_CLUSTERS = auto()
     TV = auto()
@@ -87,9 +162,8 @@ class TestDefinition:
     run_name: str
     target: TestTarget
 
-    def Run(self, runner, paths: ApplicationPaths):
+    def Run(self, runner, apps_register, paths: ApplicationPaths):
         """Executes the given test case using the provided runner for execution."""
-        app_process = None
         runner.capture_delegate = ExecutionCapture()
 
         try:
@@ -127,37 +201,19 @@ class TestDefinition:
                 if os.path.exists(str(Path.home()) + '/Documents/chip.store'):
                     os.unlink(str(Path.home()) + '/Documents/chip.store')
 
-            discriminator = str(randrange(1, 4096))
-            logging.debug(
-                'Executing application under test with discriminator %s.' % discriminator)
-            app_process, outpipe, errpipe = runner.RunSubprocess(
-                app_cmd + ['--discriminator', discriminator], name='APP ', wait=False)
+            app = App(runner, app_cmd)
+            app.start(str(randrange(1, 4096)))
+            apps_register.add("default", app)
 
-            logging.debug('Waiting for server to listen.')
-            start_time = time.time()
-            server_is_listening = outpipe.CapturedLogContains(
-                "Server Listening")
-            while not server_is_listening:
-                if time.time() - start_time > 10:
-                    raise Exception('Timeout for server listening')
-                time.sleep(0.1)
-                server_is_listening = outpipe.CapturedLogContains(
-                    "Server Listening")
-            logging.debug('Server is listening. Can proceed.')
-            qrLine = outpipe.FindLastMatchingLine('.*SetupQRCode: *\\[(.*)]')
-            if not qrLine:
-                raise Exception("Unable to find QR code")
-
-            runner.RunSubprocess(tool_cmd + ['pairing', 'qrcode', TEST_NODE_ID, qrLine.group(1)],
-                                 name='PAIR', dependencies=[app_process])
+            runner.RunSubprocess(tool_cmd + ['pairing', 'qrcode', TEST_NODE_ID, app.setupCode],
+                                 name='PAIR', dependencies=[apps_register])
 
             runner.RunSubprocess(tool_cmd + ['tests', self.run_name, TEST_NODE_ID],
-                                 name='TEST', dependencies=[app_process])
+                                 name='TEST', dependencies=[apps_register])
         except:
             logging.error("!!!!!!!!!!!!!!!!!!!! ERROR !!!!!!!!!!!!!!!!!!!!!!")
             runner.capture_delegate.LogContents()
             raise
         finally:
-            if app_process:
-                app_process.kill()
-                app_process.wait(10)
+            apps_register.killAll()
+            apps_register.removeAll()
