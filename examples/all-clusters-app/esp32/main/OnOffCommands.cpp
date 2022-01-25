90855d32ed30869e5c1fbbbce39644e3cedc04f9
Restyled.io <commits@restyled.io>
2022-01-25 05:27:27 +0000

    Restyled by clang-format

--- examples/all-clusters-app/esp32/main/OnOffCommands.cpp
+++ examples/all-clusters-app/esp32/main/OnOffCommands.cpp
@@ -39,59 +39,63 @@ namespace {
 
 Shell::Engine sSubShell;
 
-CHIP_ERROR OnOffHandler(int argc, char **argv) {
-  if (argc == 0) {
-    sSubShell.ForEachCommand(PrintCommandHelp, nullptr);
+CHIP_ERROR OnOffHandler(int argc, char ** argv)
+{
+    if (argc == 0)
+    {
+        sSubShell.ForEachCommand(PrintCommandHelp, nullptr);
+        return CHIP_NO_ERROR;
+    }
+
+    CHIP_ERROR error = sSubShell.ExecCommand(argc, argv);
+
+    if (error != CHIP_NO_ERROR)
+    {
+        streamer_printf(streamer_get(), "Error: %" CHIP_ERROR_FORMAT "\r\n", error.Format());
+    }
+
+    return error;
+}
+
+static CHIP_ERROR OnLightHandler(int argc, char ** argv)
+{
+    if (argc != 1)
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), 1);
     return CHIP_NO_ERROR;
-  }
-
-  CHIP_ERROR error = sSubShell.ExecCommand(argc, argv);
-
-  if (error != CHIP_NO_ERROR) {
-    streamer_printf(streamer_get(), "Error: %" CHIP_ERROR_FORMAT "\r\n",
-                    error.Format());
-  }
-
-  return error;
 }
 
-static CHIP_ERROR OnLightHandler(int argc, char **argv) {
-  if (argc != 1)
-    return CHIP_ERROR_INVALID_ARGUMENT;
-  chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), 1);
-  return CHIP_NO_ERROR;
+static CHIP_ERROR OffLightHandler(int argc, char ** argv)
+{
+    if (argc != 1)
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), 0);
+    return CHIP_NO_ERROR;
 }
 
-static CHIP_ERROR OffLightHandler(int argc, char **argv) {
-  if (argc != 1)
-    return CHIP_ERROR_INVALID_ARGUMENT;
-  chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), 0);
-  return CHIP_NO_ERROR;
-}
-
-static CHIP_ERROR ToggleLightHandler(int argc, char **argv) {
-  if (argc != 1)
-    return CHIP_ERROR_INVALID_ARGUMENT;
-  bool value;
-  chip::app::Clusters::OnOff::Attributes::OnOff::Get(atoi(argv[0]), &value);
-  chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), !value);
-  return CHIP_NO_ERROR;
+static CHIP_ERROR ToggleLightHandler(int argc, char ** argv)
+{
+    if (argc != 1)
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    bool value;
+    chip::app::Clusters::OnOff::Attributes::OnOff::Get(atoi(argv[0]), &value);
+    chip::app::Clusters::OnOff::Attributes::OnOff::Set(atoi(argv[0]), !value);
+    return CHIP_NO_ERROR;
 }
 
 } // namespace
 
-void OnOffCommands::Register() {
-  static const shell_command_t subCommands[] = {
-      {&OnLightHandler, "on", "Usage: OnOff on endpoint-id"},
-      {&OffLightHandler, "off", "Usage: OnOff off endpoint-id"},
-      {&ToggleLightHandler, "toggle", "Usage: OnOff toggle endpoint-id"}};
-  sSubShell.RegisterCommands(subCommands, ArraySize(subCommands));
+void OnOffCommands::Register()
+{
+    static const shell_command_t subCommands[] = { { &OnLightHandler, "on", "Usage: OnOff on endpoint-id" },
+                                                   { &OffLightHandler, "off", "Usage: OnOff off endpoint-id" },
+                                                   { &ToggleLightHandler, "toggle", "Usage: OnOff toggle endpoint-id" } };
+    sSubShell.RegisterCommands(subCommands, ArraySize(subCommands));
 
-  // Register the root `OnOff` command in the top-level shell.
-  static const shell_command_t onOffCommand = {&OnOffHandler, "OnOff",
-                                               "OnOff commands"};
+    // Register the root `OnOff` command in the top-level shell.
+    static const shell_command_t onOffCommand = { &OnOffHandler, "OnOff", "OnOff commands" };
 
-  Engine::Root().RegisterCommands(&onOffCommand, 1);
+    Engine::Root().RegisterCommands(&onOffCommand, 1);
 }
 
 } // namespace Shell
