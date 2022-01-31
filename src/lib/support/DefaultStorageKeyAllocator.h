b43feb05280a9ee71cb6576aa6c2cd5747c91e9b
Andy Salisbury <harimau@google.com>
2022-01-31 14:03:35 -0500

    Add a comment why the cast is acceptable.

--- src/lib/support/DefaultStorageKeyAllocator.h
+++ src/lib/support/DefaultStorageKeyAllocator.h
@@ -40,7 +40,11 @@ public:
     // Access Control List
 
     const char * AccessControlList() { return Format("acl"); }
-    const char * AccessControlEntry(size_t index) { return Format("acl/%x", static_cast<unsigned int>(index)); }
+    const char * AccessControlEntry(size_t index)
+    {
+        // This cast will never overflow because the number of ACL entries will be low.
+        return Format("acl/%x", static_cast<unsigned int>(index));
+    }
 
     // Group Data Provider
 
