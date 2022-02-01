7c2589045276757ec62244ef31d3a086cdb7df37
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2022-01-31 19:22:13 -0500

    [Trivial] Fix Level Control cluster revision  (#14611)
    * Update level control cluster to rev 5
    
    * Regenerated ZAP

--- zzz_generated/placeholder/app1/zap-generated/endpoint_config.h
+++ zzz_generated/placeholder/app1/zap-generated/endpoint_config.h
@@ -295,7 +295,7 @@
                                                                                                                                    \
             /* Endpoint: 1, Cluster: Level Control (server) */                                                                     \
             { 0x00000000, ZAP_TYPE(INT8U), 1, 0, ZAP_SIMPLE_DEFAULT(0x00) }, /* current level */                                   \
-            { 0x0000FFFD, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(3) },   /* ClusterRevision */                                 \
+            { 0x0000FFFD, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(5) },   /* ClusterRevision */                                 \
                                                                                                                                    \
             /* Endpoint: 1, Cluster: Basic (server) */                                                                             \
             { 0x00000000, ZAP_TYPE(INT16U), 2, ZAP_ATTRIBUTE_MASK(SINGLETON),                                                      \
