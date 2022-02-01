2518e8259ea5d61cec29f449452eba3ae3ee4863
Restyled.io <commits@restyled.io>
2022-02-01 17:53:51 +0000

    Restyled by autopep8

--- examples/chip-tool-darwin/entitlements/codesign.py
+++ examples/chip-tool-darwin/entitlements/codesign.py
@@ -3,7 +3,6 @@ import subprocess
 import re
 
 
-
 def find_identity():
     fail_str = "0 valid identities found"
     cmd = "/usr/bin/security find-identity -v -p codesigning"
@@ -13,11 +12,12 @@ def find_identity():
     result = re.search(r'\b[0-9a-fA-F]{40}\b', find_result)
     return result.group()
 
+
 def codesign(args):
     identity = find_identity()
     cmd = "codesign --force -d --sign {identity} --entitlements {entitlement} {target}".format(
-        identity=identity, 
-        entitlement=args.entitlements_path, 
+        identity=identity,
+        entitlement=args.entitlements_path,
         target=args.target_path)
     print("COMMAND " + cmd)
     codesign_result = str(subprocess.check_output(cmd.split()))
@@ -25,12 +25,14 @@ def codesign(args):
     with open(args.log_path, "w") as f:
         f.write(codesign_result)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-    description='Codesign the chip-tool-darwin binary')
+        description='Codesign the chip-tool-darwin binary')
     parser.add_argument('--entitlements_path', default='entitlements/chip-tool-darwin.entitlements',
                         help='Set the entitlements for codesign', required=True)
-    parser.add_argument('--log_path', help='Output log file destination', required=True)
+    parser.add_argument(
+        '--log_path', help='Output log file destination', required=True)
     parser.add_argument('--target_path', help='Binary to sign', required=True)
 
     args = parser.parse_args()
