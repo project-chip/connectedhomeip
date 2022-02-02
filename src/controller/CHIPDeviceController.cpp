90bd7d6631a6b6a8a34160187aa23269346370ae
C Freeman <cecille@google.com>
2022-02-02 11:19:34 -0500

    Update src/controller/CHIPDeviceController.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/controller/CHIPDeviceController.cpp
+++ src/controller/CHIPDeviceController.cpp
@@ -1746,10 +1746,8 @@ void DeviceCommissioner::PerformCommissioningStep(DeviceProxy * proxy, Commissio
         ChipLogProgress(Controller, "Sending request for network cluster feature map");
         app::InteractionModelEngine * engine = app::InteractionModelEngine::GetInstance();
         app::ReadPrepareParams readParams(proxy->GetSecureSession().Value());
-        app::AttributePathParams paramList[1];
-        paramList[0]                            = app::AttributePathParams(endpoint, app::Clusters::NetworkCommissioning::Id,
-                                                app::Clusters::NetworkCommissioning::Attributes::FeatureMap::Id);
-        readParams.mpAttributePathParamsList    = paramList;
+        app::AttributePathParams readPath(app::Clusters::NetworkCommissioning::Id, app::Clusters::NetworkCommissioning::Attributes::FeatureMap::Id);
+        readParams.mpAttributePathParamsList    = &readPath;
         readParams.mAttributePathParamsListSize = 1;
         if (timeout.HasValue())
         {
