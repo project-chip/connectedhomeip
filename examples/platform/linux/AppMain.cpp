df0ceb0ce821401dd3dc8521460587b256197dbc
chrisdecenzo <decenzo77@yahoo.com>
2022-02-02 18:03:02 -0800

    fix build

--- examples/platform/linux/AppMain.cpp
+++ examples/platform/linux/AppMain.cpp
@@ -22,7 +22,6 @@
 #include <app/clusters/network-commissioning/network-commissioning.h>
 #include <app/server/OnboardingCodesUtil.h>
 #include <app/server/Server.h>
-#include <controller-clusters/zap-generated/CHIPClusters.h>
 #include <lib/core/CHIPError.h>
 #include <lib/core/NodeId.h>
 
