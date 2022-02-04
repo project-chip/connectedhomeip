632464e46c3d6d40f324cbfa1f5c51aa8bce6236
Leonard Zgrablic <32855262+lzgrablic02@users.noreply.github.com>
2022-02-03 23:54:14 -0500

    Removed duplicate Bridged Basic init callback

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -234,7 +234,6 @@ void emberAfTick(void)
 // Cluster init functions that don't have a cluster implementation to define
 // them in.
 void MatterBooleanStatePluginServerInitCallback() {}
-void MatterBridgedDeviceBasicPluginServerInitCallback() {}
 void MatterElectricalMeasurementPluginServerInitCallback() {}
 void MatterRelativeHumidityMeasurementPluginServerInitCallback() {}
 void MatterIlluminanceMeasurementPluginServerInitCallback() {}
