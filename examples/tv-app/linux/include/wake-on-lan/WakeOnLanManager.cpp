f6b49e50393e6547d857a76436e029f62fe56470
Lazar Kovacic <lkovacic@amazon.com>
2022-02-09 17:51:21 +0100

    Update code

--- examples/tv-app/linux/include/wake-on-lan/WakeOnLanManager.cpp
+++ examples/tv-app/linux/include/wake-on-lan/WakeOnLanManager.cpp
@@ -17,11 +17,27 @@
  */
 
 #include "WakeOnLanManager.h"
+#include <string>
+#include <iostream>
+#include <fstream>
 
 using namespace chip;
 using namespace chip::app::Clusters::WakeOnLan;
 
+std::string getMacAddress()
+{
+    std::ifstream input("/sys/class/net/eth0/address");
+    std::string line;
+    std::getline(input, line);
+    return line;
+}
+
 CHIP_ERROR WakeOnLanManager::HandleGetMacAddress(chip::app::AttributeValueEncoder & aEncoder)
 {
+    #if CHIP_ENABLE_WAKE_ON_LAN
+    return aEncoder.Encode(CharSpan::fromCharString(getMacAddress().c_str()));
+    #else
     return aEncoder.Encode(CharSpan::fromCharString("00:00:00:00:00"));
+    #endif // CHIP_ENABLE_WAKE_ON_LAN
+    
 }
