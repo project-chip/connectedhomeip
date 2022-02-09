d4835da1062456e25c65c3fb78245d2c5b484c39
Andrei Litvin <andy314@gmail.com>
2022-02-09 15:39:02 -0500

    Update python code - set single IP address is used for UDP

--- src/controller/python/ChipDeviceController-ScriptBinding.cpp
+++ src/controller/python/ChipDeviceController-ScriptBinding.cpp
@@ -324,7 +324,7 @@ ChipError::StorageType pychip_DeviceController_ConnectIP(chip::Controller::Devic
     VerifyOrReturnError(chip::Inet::IPAddress::FromString(peerAddrStr, peerAddr), CHIP_ERROR_INVALID_ARGUMENT.AsInteger());
 
     // TODO: IP rendezvous should use TCP connection.
-    addr.SetTransportType(chip::Transport::Type::kUdp).SetIPAddress(peerAddr);
+    addr.SetTransportType(chip::Transport::Type::kUdp).SetSingleIPAddress(peerAddr);
     params.SetPeerAddress(addr).SetDiscriminator(0);
 
     devCtrl->ReleaseOperationalDevice(nodeid);
