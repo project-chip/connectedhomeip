9b39b1db2b5dc4f01905eeddc721dfbd5ba80dd3
rgoliver <rgoliver@google.com>
2022-02-11 14:58:33 -0500

    RPC: Cleanup and add thread service (#14843)
    * RPC: Refactor wifi RPC as common service
    
    - Move WiFi into a common rpc service like others.
    - Add WiFi RPC service to the all-clusters app
    
    * RPC: Add thread rpc service to efr and nrf
    
    - Add a Thread RPC service similar to others.
    - Add to NRF and EFR32 platforms
    - Add to efr/lighting RPC example app
    - Add to efr/lock RPC example app
    - Add to nfr/lighting RPC example app
    
    * RPC: Add get/set pairing state
    
    * RPC: Add OTCLI to EFR32 and NRF32
    
    - Add the openthread cli as a new RPC service.
    - Add service to EFR32 and NRF32 platforms
    - Add to lighting/efr32 example
    - Add to lighting/nrf example
    
    * RPC: Refactor linux RPC into platform.
    
    Move common linux Rpc.* files into common platform to match refactoring
    done for other platforms.
    
    * RPC: Wake m5 display on RPC button press

--- examples/platform/esp32/Rpc.cpp
+++ examples/platform/esp32/Rpc.cpp
@@ -21,12 +21,10 @@
 #if CONFIG_ENABLE_PW_RPC
 #include "PigweedLoggerMutex.h"
 #include "RpcService.h"
-#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
-#include "pw_log/log.h"
 #include "pw_rpc/server.h"
 #include "pw_sys_io/sys_io.h"
 #include "support/CodeUtils.h"
@@ -52,6 +50,16 @@
 #include "pigweed/rpc_services/Locking.h"
 #endif // defined(PW_RPC_LOCKING_SERVICE) && PW_RPC_LOCKING_SERVICE
 
+#if defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+#include "NetworkCommissioningDriver.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "pigweed/rpc_services/Wifi.h"
+#include "pw_status/try.h"
+#endif // defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+
 #if defined(PW_RPC_TRACING_SERVICE) && PW_RPC_TRACING_SERVICE
 #define PW_TRACE_BUFFER_SIZE_BYTES 1024
 #include "pw_trace/trace.h"
@@ -72,6 +80,7 @@ size_t pw_trace_GetTraceTimeTicksPerSecond()
 
 namespace chip {
 namespace rpc {
+namespace {
 
 #if defined(PW_RPC_BUTTON_SERVICE) && PW_RPC_BUTTON_SERVICE
 class Esp32Button final : public Button
@@ -83,6 +92,7 @@ public:
         if (request.pushed)
         {
             ScreenManager::ButtonPressed(1 + request.idx);
+            WakeDisplay();
         }
         return pw::OkStatus();
 #else  // CONFIG_DEVICE_TYPE_M5STACK
@@ -112,7 +122,127 @@ private:
 };
 #endif // defined(PW_RPC_DEVICE_SERVICE) && PW_RPC_DEVICE_SERVICE
 
-namespace {
+#if defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+
+class Esp32WiFi final : public WiFi
+{
+public:
+    pw::Status GetChannel(const pw_protobuf_Empty & request, chip_rpc_Channel & response) override
+    {
+        uint8_t channel = 0;
+        wifi_second_chan_t second;
+        PW_TRY(EspToPwStatus(esp_wifi_get_channel(&channel, &second)));
+        response.channel = channel;
+        return pw::OkStatus();
+    }
+
+    pw::Status GetSsid(const pw_protobuf_Empty & request, chip_rpc_Ssid & response) override
+    {
+        wifi_config_t config;
+        PW_TRY(EspToPwStatus(esp_wifi_get_config(WIFI_IF_STA, &config)));
+        size_t size = std::min(sizeof(response.ssid.bytes), sizeof(config.sta.ssid));
+        memcpy(response.ssid.bytes, config.sta.ssid, size);
+        response.ssid.size = size;
+        return pw::OkStatus();
+    }
+
+    pw::Status GetState(const pw_protobuf_Empty & request, chip_rpc_State & response) override
+    {
+        wifi_ap_record_t ap_info;
+        esp_err_t err = esp_wifi_sta_get_ap_info(&ap_info);
+        PW_TRY(EspToPwStatus(err));
+        response.connected = (err != ESP_ERR_WIFI_NOT_CONNECT);
+        return pw::OkStatus();
+    }
+
+    pw::Status GetMacAddress(const pw_protobuf_Empty & request, chip_rpc_MacAddress & response) override
+    {
+        uint8_t mac[6];
+        PW_TRY(EspToPwStatus(esp_wifi_get_mac(WIFI_IF_STA, mac)));
+        snprintf(response.mac_address, sizeof(response.mac_address), "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2],
+                 mac[3], mac[4], mac[5]);
+        return pw::OkStatus();
+    }
+
+    pw::Status GetWiFiInterface(const pw_protobuf_Empty & request, chip_rpc_WiFiInterface & response) override
+    {
+        wifi_ap_record_t ap_info;
+        PW_TRY(EspToPwStatus(esp_wifi_sta_get_ap_info(&ap_info)));
+        snprintf(response.interface, sizeof(response.interface), "STA");
+        return pw::OkStatus();
+    }
+
+    pw::Status GetIP4Address(const pw_protobuf_Empty & request, chip_rpc_IP4Address & response) override
+    {
+        esp_netif_ip_info_t ip_info;
+        PW_TRY(EspToPwStatus(esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info)));
+        snprintf(response.address, sizeof(response.address), IPSTR, IP2STR(&ip_info.ip));
+        return pw::OkStatus();
+    }
+
+    pw::Status GetIP6Address(const pw_protobuf_Empty & request, chip_rpc_IP6Address & response) override
+    {
+        esp_ip6_addr_t ip6;
+        PW_TRY(EspToPwStatus(esp_netif_get_ip6_linklocal(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip6)));
+        snprintf(response.address, sizeof(response.address), IPV6STR, IPV62STR(ip6));
+        return pw::OkStatus();
+    }
+
+    pw::Status Connect(const chip_rpc_ConnectionData & request, chip_rpc_ConnectionResult & response) override
+    {
+        char ssid[sizeof(wifi_config_t().sta.ssid)];
+        char password[sizeof(wifi_config_t().sta.password)];
+        size_t ssid_size = std::min(sizeof(ssid) - 1, static_cast<size_t>(request.ssid.size));
+        memcpy(ssid, request.ssid.bytes, ssid_size);
+        ssid[ssid_size]      = '\0';
+        size_t password_size = std::min(sizeof(password) - 1, static_cast<size_t>(request.secret.size));
+        memcpy(password, request.secret.bytes, password_size);
+        password[password_size] = '\0';
+        if (chip::DeviceLayer::NetworkCommissioning::ESPWiFiDriver::GetInstance().ConnectWiFiNetwork(
+                ssid, strlen(ssid), password, strlen(password)) != CHIP_NO_ERROR)
+        {
+            return pw::Status::Internal();
+        }
+        return pw::OkStatus();
+    }
+
+    pw::Status Disconnect(const pw_protobuf_Empty & request, pw_protobuf_Empty & response) override
+    {
+        chip::DeviceLayer::ConnectivityMgr().ClearWiFiStationProvision();
+        chip::DeviceLayer::ConnectivityMgr().SetWiFiStationMode(chip::DeviceLayer::ConnectivityManager::kWiFiStationMode_Disabled);
+        return pw::OkStatus();
+    }
+
+private:
+    static constexpr pw::Status EspToPwStatus(esp_err_t err)
+    {
+        switch (err)
+        {
+        case ESP_OK:
+            return pw::OkStatus();
+        case ESP_ERR_WIFI_NOT_INIT:
+            return pw::Status::FailedPrecondition();
+        case ESP_ERR_INVALID_ARG:
+            return pw::Status::InvalidArgument();
+        case ESP_ERR_ESP_NETIF_INVALID_PARAMS:
+            return pw::Status::InvalidArgument();
+        case ESP_ERR_WIFI_IF:
+            return pw::Status::NotFound();
+        case ESP_ERR_WIFI_NOT_CONNECT:
+            return pw::Status::FailedPrecondition();
+        case ESP_ERR_WIFI_NOT_STARTED:
+            return pw::Status::FailedPrecondition();
+        case ESP_ERR_WIFI_CONN:
+            return pw::Status::Internal();
+        case ESP_FAIL:
+            return pw::Status::Internal();
+        default:
+            return pw::Status::Unknown();
+        }
+    }
+};
+
+#endif // defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
 
 #define RPC_TASK_STACK_SIZE (8 * 1024)
 #define RPC_TASK_PRIORITY 5
@@ -144,6 +274,10 @@ Locking locking;
 pw::trace::TraceService trace_service;
 #endif // defined(PW_RPC_TRACING_SERVICE) && PW_RPC_TRACING_SERVICE
 
+#if defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+Esp32WiFi wifi_service;
+#endif // defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+
 void RegisterServices(pw::rpc::Server & server)
 {
 #if defined(PW_RPC_ATTRIBUTE_SERVICE) && PW_RPC_ATTRIBUTE_SERVICE
@@ -170,6 +304,10 @@ void RegisterServices(pw::rpc::Server & server)
     server.RegisterService(trace_service);
     PW_TRACE_SET_ENABLED(true);
 #endif // defined(PW_RPC_TRACING_SERVICE) && PW_RPC_TRACING_SERVICE
+
+#if defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
+    server.RegisterService(wifi_service);
+#endif // defined(PW_RPC_WIFI_SERVICE) && PW_RPC_WIFI_SERVICE
 }
 
 } // namespace
