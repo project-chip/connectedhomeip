dac827ddefaf8af4bfbb40d1c224805b7b000679
Sagar Dhawan <sagar_dhawan@apple.com>
2022-02-12 16:05:58 -0800

    Rename ResponseTime->RequestTime

--- src/darwin/Framework/CHIP/CHIPCallbackBridgeBase_internal.h
+++ src/darwin/Framework/CHIP/CHIPCallbackBridgeBase_internal.h
@@ -36,7 +36,7 @@ public:
         , mSuccess(OnSuccessFn, this)
         , mFailure(OnFailureFn, this)
     {
-        mResponseTime = [NSDate date];
+        mRequestTime = [NSDate date];
         // Generate a unique cookie to track this operation
         mCookie = [NSString stringWithFormat:@"Response Time: %s+%u", typeid(T).name(), arc4random()];
         ChipLogDetail(Controller, "%s", mCookie.UTF8String);
@@ -86,7 +86,7 @@ private:
 
         dispatch_async(callbackBridge->mQueue, ^{
             ChipLogDetail(Controller, "%s %f seconds", callbackBridge->mCookie.UTF8String,
-                -[callbackBridge->mResponseTime timeIntervalSinceNow]);
+                -[callbackBridge->mRequestTime timeIntervalSinceNow]);
             callbackBridge->mHandler(value, error);
 
             if (!callbackBridge->mKeepAlive) {
@@ -102,6 +102,6 @@ private:
     chip::Callback::Callback<CHIPDefaultFailureCallbackType> mFailure;
 
     // Measure the time it took for the callback to trigger
-    NSDate * mResponseTime;
+    NSDate * mRequestTime;
     NSString * mCookie;
 };
