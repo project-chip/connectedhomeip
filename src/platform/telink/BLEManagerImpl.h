209da99a1ef3ee42cffae361c29d04c2476cf69c
Alex Kolosov <rikorsev@gmail.com>
2022-02-14 14:49:32 +0200

    [Telink]: Implement Matter BLE connection

--- src/platform/telink/BLEManagerImpl.h
+++ src/platform/telink/BLEManagerImpl.h
@@ -44,6 +44,7 @@ private:
     // Members that implement the BLEManager internal interface.
 
     CHIP_ERROR _Init(void);
+    CHIP_ERROR _InitGatt(void);
     CHIPoBLEServiceMode _GetCHIPoBLEServiceMode(void);
     CHIP_ERROR _SetCHIPoBLEServiceMode(CHIPoBLEServiceMode val);
     bool _IsAdvertisingEnabled(void);
