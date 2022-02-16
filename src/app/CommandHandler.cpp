b6eb122d883ba2104710212c596cbc245dbfcd3e
Marc Lepage <mlepage@google.com>
2022-02-16 10:50:35 -0500

    Add temp logs to debug nRF test failures on Zephyr

--- src/app/CommandHandler.cpp
+++ src/app/CommandHandler.cpp
@@ -87,6 +87,7 @@ CHIP_ERROR CommandHandler::OnInvokeCommandRequest(Messaging::ExchangeContext * e
 
 CHIP_ERROR CommandHandler::ProcessInvokeRequest(System::PacketBufferHandle && payload, bool isTimedInvoke)
 {
+    ChipLogDetail(DataManagement, "mlepage A");
     CHIP_ERROR err = CHIP_NO_ERROR;
     System::PacketBufferTLVReader reader;
     TLV::TLVReader invokeRequestsReader;
@@ -94,53 +95,69 @@ CHIP_ERROR CommandHandler::ProcessInvokeRequest(System::PacketBufferHandle && pa
     InvokeRequests::Parser invokeRequests;
     reader.Init(std::move(payload));
     ReturnErrorOnFailure(reader.Next());
+    ChipLogDetail(DataManagement, "mlepage A");
     ReturnErrorOnFailure(invokeRequestMessage.Init(reader));
+    ChipLogDetail(DataManagement, "mlepage B");
 #if CHIP_CONFIG_IM_ENABLE_SCHEMA_CHECK
     ReturnErrorOnFailure(invokeRequestMessage.CheckSchemaValidity());
 #endif
+    ChipLogDetail(DataManagement, "mlepage C");
     ReturnErrorOnFailure(invokeRequestMessage.GetSuppressResponse(&mSuppressResponse));
+    ChipLogDetail(DataManagement, "mlepage D");
     ReturnErrorOnFailure(invokeRequestMessage.GetTimedRequest(&mTimedRequest));
+    ChipLogDetail(DataManagement, "mlepage E");
     ReturnErrorOnFailure(invokeRequestMessage.GetInvokeRequests(&invokeRequests));
 
     VerifyOrReturnError(mpExchangeCtx != nullptr, CHIP_ERROR_INCORRECT_STATE);
+    ChipLogDetail(DataManagement, "mlepage F");
 
     if (mTimedRequest != isTimedInvoke)
     {
+        ChipLogDetail(DataManagement, "mlepage G");
         // The message thinks it should be part of a timed interaction but it's
         // not, or vice versa.  Spec says to Respond with UNSUPPORTED_ACCESS.
         err = StatusResponse::Send(Protocols::InteractionModel::Status::UnsupportedAccess, mpExchangeCtx,
                                    /* aExpectResponse = */ false);
+        ChipLogDetail(DataManagement, "mlepage H");
 
         if (err != CHIP_NO_ERROR)
         {
+            ChipLogDetail(DataManagement, "mlepage I");
             // We have to manually close the exchange, because we called
             // WillSendMessage already.
             mpExchangeCtx->Close();
         }
 
+        ChipLogDetail(DataManagement, "mlepage J");
         // Null out the (now-closed) exchange, so that when we try to
         // SendCommandResponse() later (when our holdoff count drops to 0) it
         // just fails and we don't double-respond.
         mpExchangeCtx = nullptr;
         return err;
     }
+    ChipLogDetail(DataManagement, "mlepage K");
 
     invokeRequests.GetReader(&invokeRequestsReader);
     while (CHIP_NO_ERROR == (err = invokeRequestsReader.Next()))
     {
+        ChipLogDetail(DataManagement, "mlepage L");
         VerifyOrReturnError(TLV::AnonymousTag() == invokeRequestsReader.GetTag(), CHIP_ERROR_INVALID_TLV_TAG);
         CommandDataIB::Parser commandData;
         ReturnErrorOnFailure(commandData.Init(invokeRequestsReader));
+        ChipLogDetail(DataManagement, "mlepage M");
 
         if (mpExchangeCtx->IsGroupExchangeContext())
         {
+            ChipLogDetail(DataManagement, "mlepage N");
             ReturnErrorOnFailure(ProcessGroupCommandDataIB(commandData));
         }
         else
         {
+            ChipLogDetail(DataManagement, "mlepage O");
             ReturnErrorOnFailure(ProcessCommandDataIB(commandData));
         }
     }
+    ChipLogDetail(DataManagement, "mlepage P");
 
     // if we have exhausted this container
     if (CHIP_END_OF_TLV == err)
@@ -266,30 +283,38 @@ CHIP_ERROR CommandHandler::ProcessCommandDataIB(CommandDataIB::Parser & aCommand
         }
     }
 
+    ChipLogDetail(DataManagement, "mlepage 1");
     VerifyOrExit(mpExchangeCtx != nullptr && mpExchangeCtx->HasSessionHandle(), err = CHIP_ERROR_INCORRECT_STATE);
+    ChipLogDetail(DataManagement, "mlepage 2");
 
     {
         Access::SubjectDescriptor subjectDescriptor = GetSubjectDescriptor();
         Access::RequestPath requestPath{ .cluster = concretePath.mClusterId, .endpoint = concretePath.mEndpointId };
         Access::Privilege requestPrivilege = RequiredPrivilege::ForInvokeCommand(concretePath);
         err                                = Access::GetAccessControl().Check(subjectDescriptor, requestPath, requestPrivilege);
+        ChipLogDetail(DataManagement, "mlepage 3");
         if (err != CHIP_NO_ERROR)
         {
+            ChipLogDetail(DataManagement, "mlepage 4");
             if (err != CHIP_ERROR_ACCESS_DENIED)
             {
+                ChipLogDetail(DataManagement, "mlepage 5");
                 return AddStatus(concretePath, Status::Failure);
             }
             // TODO: when wildcard invokes are supported, handle them to discard rather than fail with status
             return AddStatus(concretePath, Status::UnsupportedAccess);
         }
     }
+    ChipLogDetail(DataManagement, "mlepage 6");
 
     if (CommandNeedsTimedInvoke(concretePath.mClusterId, concretePath.mCommandId) && !IsTimedInvoke())
     {
+        ChipLogDetail(DataManagement, "mlepage 7");
         // TODO: when wildcard invokes are supported, discard a
         // wildcard-expanded path instead of returning a status.
         return AddStatus(concretePath, Protocols::InteractionModel::Status::NeedsTimedInteraction);
     }
+    ChipLogDetail(DataManagement, "mlepage 8");
 
     err = aCommandElement.GetData(&commandDataReader);
     if (CHIP_END_OF_TLV == err)
@@ -309,10 +334,12 @@ CHIP_ERROR CommandHandler::ProcessCommandDataIB(CommandDataIB::Parser & aCommand
         mpCallback->DispatchCommand(*this, concretePath, commandDataReader);
         MatterPostCommandReceivedCallback(concretePath);
     }
+    ChipLogDetail(DataManagement, "mlepage 9");
 
 exit:
     if (err != CHIP_NO_ERROR)
     {
+        ChipLogDetail(DataManagement, "mlepage X");
         return AddStatus(concretePath, Status::InvalidCommand);
     }
 
