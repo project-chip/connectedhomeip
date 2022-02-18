da0add6e5707a11ff5600762ce54f45a0f24e7c4
Restyled.io <commits@restyled.io>
2022-02-18 19:22:20 +0000

    Restyled by clang-format

--- src/platform/P6/MTBKeyValueStore.cpp
+++ src/platform/P6/MTBKeyValueStore.cpp
@@ -46,8 +46,8 @@ cy_rslt_t mtb_key_value_store_init(mtb_kvstore_t * kvstore_obj)
     {
         return result;
     }
-    sectorSize          = cy_serial_flash_qspi_get_erase_size(ext_mem_address);
-    uint32_t length     = sectorSize * 2;
+    sectorSize      = cy_serial_flash_qspi_get_erase_size(ext_mem_address);
+    uint32_t length = sectorSize * 2;
 #ifdef P6_OTA
     // If using OTA set the start address towards the end of the flash area so
     // an update doesn't clobber the persistent storage.
