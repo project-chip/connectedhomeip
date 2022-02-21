61b943311df2105eda33e95b81c7c202fc057266
jmartinez-silabs <67972863+jmartinez-silabs@users.noreply.github.com>
2022-02-21 13:38:26 -0500

    Cherry pick Generic Thread Network commissioning Driver (#15392)
    * Implement a Generic Network commissioning Thread Driver (#14713)
    
    * Create a generic Network commissioning Thread Driver
    
    * Add OnThreadAttachFinished to send Connect network response
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by gn
    
    * Fix linux build
    
    do not build GenericNetworkCommissioningThreadDriver for linux
    
    * Fix a few sample app builds
    
    * fix shell app for all
    
    * cleaner fix for shell
    
    * Remove OtScanResponseIterator, Scan handling not yet implement. Need rework to not use vector
    
    * address PR comments
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * support Thread Scan Network (#15105)
    
    fix malloc and release issue
    
    Cleanup + restyle
    
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/platform/OpenThread/GenericNetworkCommissioningThreadDriver.h
@@ -0,0 +1,114 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <platform/NetworkCommissioning.h>
+
+namespace chip {
+namespace DeviceLayer {
+namespace NetworkCommissioning {
+
+template <typename T>
+class otScanResponseIterator : public Iterator<T>
+{
+public:
+    otScanResponseIterator(T * apScanResponse) : mpScanResponse(apScanResponse) {}
+    size_t Count() override { return itemCount; }
+    bool Next(T & item) override
+    {
+        if (mpScanResponse == nullptr || currentIterating >= itemCount)
+        {
+            return false;
+        }
+        item = mpScanResponse[currentIterating];
+        currentIterating++;
+        return true;
+    }
+    void Release() override
+    {
+        itemCount = currentIterating = 0;
+        Platform::MemoryFree(mpScanResponse);
+        mpScanResponse = nullptr;
+    }
+
+    void Add(T * pResponse)
+    {
+        size_t tempCount = itemCount + 1;
+        mpScanResponse   = static_cast<T *>(Platform::MemoryRealloc(mpScanResponse, kItemSize * tempCount));
+        if (mpScanResponse)
+        {
+            // first item at index. update after the copy.
+            memcpy(&(mpScanResponse[itemCount]), pResponse, kItemSize);
+            itemCount = tempCount;
+        }
+    }
+
+private:
+    size_t currentIterating = 0;
+    size_t itemCount        = 0;
+    size_t kItemSize        = sizeof(T);
+    T * mpScanResponse;
+};
+
+class GenericThreadDriver final : public ThreadDriver
+{
+public:
+    class ThreadNetworkIterator final : public NetworkIterator
+    {
+    public:
+        ThreadNetworkIterator(GenericThreadDriver * aDriver) : driver(aDriver) {}
+        size_t Count() override;
+        bool Next(Network & item) override;
+        void Release() override { delete this; }
+        ~ThreadNetworkIterator() = default;
+
+    private:
+        GenericThreadDriver * driver;
+        bool exhausted = false;
+    };
+
+    // BaseDriver
+    NetworkIterator * GetNetworks() override { return new ThreadNetworkIterator(this); }
+    CHIP_ERROR Init() override;
+    CHIP_ERROR Shutdown() override { return CHIP_NO_ERROR; } // Nothing to do on EFR32 for shutdown.
+
+    // WirelessDriver
+    uint8_t GetMaxNetworks() override { return 1; }
+    uint8_t GetScanNetworkTimeoutSeconds() override { return 10; }
+    uint8_t GetConnectNetworkTimeoutSeconds() override { return 20; }
+
+    CHIP_ERROR CommitConfiguration() override;
+    CHIP_ERROR RevertConfiguration() override;
+
+    Status RemoveNetwork(ByteSpan networkId) override;
+    Status ReorderNetwork(ByteSpan networkId, uint8_t index) override;
+    void ConnectNetwork(ByteSpan networkId, ConnectCallback * callback) override;
+
+    // ThreadDriver
+    Status AddOrUpdateNetwork(ByteSpan operationalDataset) override;
+    void ScanNetworks(ScanCallback * callback) override;
+
+private:
+    ThreadNetworkIterator mThreadIterator      = ThreadNetworkIterator(this);
+    Thread::OperationalDataset mSavedNetwork   = {};
+    Thread::OperationalDataset mStagingNetwork = {};
+};
+
+} // namespace NetworkCommissioning
+} // namespace DeviceLayer
+} // namespace chip
