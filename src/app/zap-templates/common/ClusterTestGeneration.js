2b0004a8980ceaf3b75d5e1a80433b30e7ae358e
Vivien Nicolas <vnicolas@apple.com>
2022-02-21 10:17:25 +0100

    [YAML] Remove unused errorWrongValue

--- src/app/zap-templates/common/ClusterTestGeneration.js
+++ src/app/zap-templates/common/ClusterTestGeneration.js
@@ -44,7 +44,6 @@ const kArgumentsName          = 'arguments';
 const kResponseName           = 'response';
 const kDisabledName           = 'disabled';
 const kResponseErrorName      = 'error';
-const kResponseWrongErrorName = 'errorWrongValue';
 const kPICSName               = 'PICS';
 const kSaveAsName             = 'saveAs';
 const kFabricFiltered         = 'fabricFiltered';
@@ -242,11 +241,10 @@ function setDefaultResponse(test)
   const defaultResponse = {};
   setDefault(test, kResponseName, defaultResponse);
 
-  const hasResponseError = (kResponseErrorName in test[kResponseName]) || (kResponseWrongErrorName in test[kResponseName]);
+  const hasResponseError = (kResponseErrorName in test[kResponseName]);
 
   const defaultResponseError = 0;
   setDefault(test[kResponseName], kResponseErrorName, defaultResponseError);
-  setDefault(test[kResponseName], kResponseWrongErrorName, defaultResponseError);
 
   const defaultResponseValues = [];
   setDefault(test[kResponseName], kValuesName, defaultResponseValues);
@@ -275,7 +273,6 @@ function setDefaultResponse(test)
   }
 
   ensureValidError(test[kResponseName], kResponseErrorName);
-  ensureValidError(test[kResponseName], kResponseWrongErrorName);
 
   // Step that waits for a particular event does not requires constraints nor expected values.
   if (test.isWait) {
