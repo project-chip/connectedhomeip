95931a658194ad83d3accafa31970e8ed53f561b
Kevin Coppock <coppock@google.com>
2022-02-22 14:41:51 -0600

    Add a sanity check for attestation challenge length before returning
    The challenge should always be exactly 16 bytes.

--- src/controller/java/CHIPDeviceController-JNI.cpp
+++ src/controller/java/CHIPDeviceController-JNI.cpp
@@ -598,6 +598,7 @@ JNI_METHOD(jbyteArray, getAttestationChallenge)
     AndroidDeviceControllerWrapper * wrapper = AndroidDeviceControllerWrapper::FromJNIHandle(handle);
     err                                      = wrapper->Controller()->GetAttestationChallenge(attestationChallenge);
     SuccessOrExit(err);
+    VerifyOrExit(attestationChallenge.size() == 16, err = CHIP_ERROR_INVALID_ARGUMENT);
 
     err = JniReferences::GetInstance().N2J_ByteArray(env, attestationChallenge.data(), attestationChallenge.size(),
                                                      attestationChallengeJbytes);
