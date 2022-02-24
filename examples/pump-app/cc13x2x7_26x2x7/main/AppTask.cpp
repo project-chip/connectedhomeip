9402aec795639f6840e3b930a8c4c5df34508db0
Restyled.io <commits@restyled.io>
2022-02-24 15:47:29 +0000

    Restyled by clang-format

--- examples/pump-app/cc13x2x7_26x2x7/main/AppTask.cpp
+++ examples/pump-app/cc13x2x7_26x2x7/main/AppTask.cpp
@@ -31,7 +31,7 @@
 #include <app/util/af-types.h>
 #include <app/util/af.h>
 
-#if defined (CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
+#if defined(CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
 #include <app/clusters/ota-requestor/BDXDownloader.h>
 #include <app/clusters/ota-requestor/OTARequestor.h>
 #include <platform/GenericOTARequestorDriver.h>
@@ -74,7 +74,7 @@ static Button_Handle sAppRightHandle;
 
 AppTask AppTask::sAppTask;
 
-#if defined (CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
+#if defined(CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
 static OTARequestor sRequestorCore;
 static GenericOTARequestorDriver sRequestorUser;
 static BDXDownloader sDownloader;
@@ -212,7 +212,7 @@ int AppTask::Init()
 
     ConfigurationMgr().LogDeviceConfig();
 
-#if defined (CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
+#if defined(CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR)
     InitializeOTARequestor();
 #endif
 
