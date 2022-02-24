d2997de3ef672f2ae1135fa3d3b5c4cd21277065
Shu Chen <chenshu@espressif.com>
2022-02-25 06:15:53 +0800

    [ESP] Fix boot crash issue (#15507)
    Since #15031, it will do MemoryCalloc() in InitChipStack(), so need to move
    MemoryInit() to the front of InitChipStack().

--- examples/all-clusters-app/esp32/main/CHIPDeviceManager.cpp
+++ examples/all-clusters-app/esp32/main/CHIPDeviceManager.cpp
@@ -60,6 +60,9 @@ CHIP_ERROR CHIPDeviceManager::Init(CHIPDeviceManagerCallbacks * cb)
     mCB                              = cb;
     RendezvousInformationFlags flags = RendezvousInformationFlags(CONFIG_RENDEZVOUS_MODE);
 
+    err = Platform::MemoryInit();
+    SuccessOrExit(err);
+
     // Initialize the CHIP stack.
     err = PlatformMgr().InitChipStack();
     SuccessOrExit(err);
@@ -80,9 +83,6 @@ CHIP_ERROR CHIPDeviceManager::Init(CHIPDeviceManagerCallbacks * cb)
         // be communicated with via its SoftAP as needed.
         ConnectivityMgr().SetWiFiAPMode(ConnectivityManager::kWiFiAPMode_Enabled);
     }
-
-    err = Platform::MemoryInit();
-    SuccessOrExit(err);
 
     // Register a function to receive events from the CHIP device layer.  Note that calls to
     // this function will happen on the CHIP event loop thread, not the app_main thread.
