9729c5edb9d71dbd5cbfce7e1762be3178f9908d
Sergei Lissianoi <54454955+selissia@users.noreply.github.com>
2022-03-03 15:32:33 -0500

    Implement periodic timer for querying OTA providers, refactor OTARequestor and OTARequestorDriver (#15433)
    * Add quasi-state machine for the Requestor
    
    * Use the RequestorDriver scheduling method instead of the platform timer
    
    The Linux OTA scenario works with this change
    
    * Add a commissioning callback
    
    * Add periodic timer for querying the default OTA provider
    
    * Add calls to StartDefaultProvidersTimer()
    
    * Move all calls to StartDefaultProvidersTimer() to RecordNewUpdateState()
    
    * Move some of the Requestor logic to the GenericOTARequestorDriver class
    
    * Update comments
    
    * Fix StartDelayTimerHandler() call
    
    * Add DriverTriggerQuery() method
    
    * Remove obsolet methods from OTARequestor
    
    * Rearrange error logic flow
    
    * Use TriggerImmediateQuery() API when processing AnnounceOTAProviders
    
    * Use DetermineProviderLocation() to pick the provider location
    
    * Introduce mLastUsedProvider
    
    * Rename functions and add comments
    
    * Rework StartDelayTimerHandler
    
    * Cleanup unused parameter, comments
    
    * Clean up spelling
    
    * Fix override compile error
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Remove unused enum
    
    * Rename variable, fix comments, change private back to protected (merge artifact)
    
    * Restyled by clang-format
    
    * Make periodic query timeout configurable on Linux
    
    * Update examples/ota-requestor-app/linux/main.cpp
    
    Co-authored-by: Carol Yang <clyang@apple.com>
    
    * Update examples/ota-requestor-app/linux/README.md
    
    Co-authored-by: Carol Yang <clyang@apple.com>
    
    * Update wording
    
    Co-authored-by: Carol Yang <clyang@apple.com>
    
    * Use seconds for the delay, don't set the state prematurely, use lambda
    
    * Rework enums, query retry logic
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by prettier-markdown
    
    * Add word to disctionary
    
    * Delay initial query after commissioning
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Udate comment
    
    * Introduce GenericOTARequestorDriver::OTACommissioningCallback
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Rename and move around various OTARequestor and Driver APIs
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Resolve merge conflicts from the latest merge from master
    
    * Rename variables and update comments
    
    * Restyled by whitespace
    
    * Remove leftover code block
    
    * When connecting to the Provider change the state to kUnknown
    
    * Remove unused function
    
    * Check for kCommissioningComplete; clear current provider
    
    * Set the kQuerying state in TriggerImmediateQueryInternal
    
    * Restyled by clang-format
    
    * Restyled by prettier-markdown
    
    * Clean up missed merge conflicts
    
    * Restyled by prettier-markdown
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Carol Yang <clyang@apple.com>

--- examples/ota-requestor-app/linux/main.cpp
+++ examples/ota-requestor-app/linux/main.cpp
@@ -52,29 +52,27 @@ BDXDownloader gDownloader;
 OTAImageProcessorImpl gImageProcessor;
 
 bool HandleOptions(const char * aProgram, OptionSet * aOptions, int aIdentifier, const char * aName, const char * aValue);
-void OnStartDelayTimerHandler(Layer * systemLayer, void * appState);
 
-constexpr uint16_t kOptionDelayQuery          = 'q';
-constexpr uint16_t kOptionRequestorCanConsent = 'c';
-constexpr uint16_t kOptionOtaDownloadPath     = 'f';
-constexpr size_t kMaxFilePathSize             = 256;
+constexpr uint16_t kOptionPeriodicQueryTimeout = 'p';
+constexpr uint16_t kOptionRequestorCanConsent  = 'c';
+constexpr uint16_t kOptionOtaDownloadPath      = 'f';
+constexpr size_t kMaxFilePathSize              = 256;
 
-uint16_t delayQueryTimeInSec = 0;
+uint32_t gPeriodicQueryTimeoutSec = (24 * 60 * 60);
 chip::Optional<bool> gRequestorCanConsent;
 static char gOtaDownloadPath[kMaxFilePathSize] = "/tmp/test.bin";
 
 OptionDef cmdLineOptionsDef[] = {
-    { "delayQuery", chip::ArgParser::kArgumentRequired, kOptionDelayQuery },
+    { "periodicQueryTimeout", chip::ArgParser::kArgumentRequired, kOptionPeriodicQueryTimeout },
     { "requestorCanConsent", chip::ArgParser::kNoArgument, kOptionRequestorCanConsent },
     { "otaDownloadPath", chip::ArgParser::kArgumentRequired, kOptionOtaDownloadPath },
     {},
 };
 
 OptionSet cmdLineOptions = { HandleOptions, cmdLineOptionsDef, "PROGRAM OPTIONS",
-                             "  -q/--delayQuery <Time in seconds>\n"
-                             "        From boot up, the amount of time to wait before triggering the QueryImage\n"
-                             "        command. If none or zero is supplied, QueryImage will not be triggered automatically. At "
-                             "least one provider location must be written to the DefaultOTAProviders attribute.\n"
+                             "  -p/--periodicQueryTimeout <Time in seconds>\n"
+                             "        Periodic timeout for querying providers in the default OTA provider list\n"
+                             "        If none or zero is supplied the timeout is set to every 24 hours. \n"
                              "  -c/--requestorCanConsent\n"
                              "        If supplied, the RequestorCanConsent field of the QueryImage command is set to true.\n"
                              "        Otherwise, the value is determined by the driver.\n "
@@ -91,6 +89,7 @@ static void InitOTARequestor(void)
 
     gRequestorCore.Init(&(chip::Server::GetInstance()), &gRequestorUser, &gDownloader);
     gRequestorUser.Init(&gRequestorCore, &gImageProcessor);
+    gRequestorUser.SetPeriodicQueryTimeout(gPeriodicQueryTimeoutSec);
 
     // WARNING: this is probably not realistic to know such details of the image or to even have an OTADownloader instantiated at
     // the beginning of program execution. We're using hardcoded values here for now since this is a reference application.
@@ -109,8 +108,8 @@ bool HandleOptions(const char * aProgram, OptionSet * aOptions, int aIdentifier,
 
     switch (aIdentifier)
     {
-    case kOptionDelayQuery:
-        delayQueryTimeInSec = static_cast<uint16_t>(strtol(aValue, NULL, 0));
+    case kOptionPeriodicQueryTimeout:
+        gPeriodicQueryTimeoutSec = static_cast<uint32_t>(strtoul(aValue, NULL, 0));
         break;
     case kOptionRequestorCanConsent:
         gRequestorCanConsent.SetValue(true);
@@ -138,13 +137,6 @@ void ApplicationInit()
 
     // Initialize all OTA download components
     InitOTARequestor();
-
-    // If a delay is provided, after the timer expires, QueryImage from default OTA provider
-    if (delayQueryTimeInSec > 0)
-    {
-        chip::DeviceLayer::SystemLayer().StartTimer(chip::System::Clock::Milliseconds32(delayQueryTimeInSec * 1000),
-                                                    OnStartDelayTimerHandler, nullptr);
-    }
 }
 
 int main(int argc, char * argv[])
@@ -152,10 +144,4 @@ int main(int argc, char * argv[])
     VerifyOrDie(ChipLinuxAppInit(argc, argv, &cmdLineOptions) == 0);
     ChipLinuxAppMainLoop();
     return 0;
-}
-
-// Test mode operation
-void OnStartDelayTimerHandler(Layer * systemLayer, void * appState)
-{
-    static_cast<OTARequestor *>(GetRequestorInstance())->TriggerImmediateQuery();
 }
