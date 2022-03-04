7fd66c475260c2e45cc78c2fcc11548764533658
Restyled.io <commits@restyled.io>
2022-03-04 15:32:39 +0000

    Restyled by autopep8

--- examples/lighting-app/nxp/k32w/k32w0/detokenizer.py
+++ examples/lighting-app/nxp/k32w/k32w0/detokenizer.py
@@ -4,6 +4,7 @@ import os
 import serial
 import pw_tokenizer
 
+
 def parse_args():
     """Parse input arguments
 
@@ -16,16 +17,23 @@ def parse_args():
     subparsers.required = True
 
     parser_file = subparsers.add_parser('file')
-    parser_file.add_argument("-i", "--input", help="Input file name.", required=True)
-    parser_file.add_argument("-d", "--database", help="Token database.", required=True)
-    parser_file.add_argument("-o", "--output", help="Output file name.", required=True)
+    parser_file.add_argument(
+        "-i", "--input", help="Input file name.", required=True)
+    parser_file.add_argument(
+        "-d", "--database", help="Token database.", required=True)
+    parser_file.add_argument(
+        "-o", "--output", help="Output file name.", required=True)
 
     parser_file = subparsers.add_parser('serial')
-    parser_file.add_argument("-i", "--input", help="Input serial port name.", required=True)
-    parser_file.add_argument("-d", "--database", help="Token database.", required=True)
-    parser_file.add_argument("-o", "--output", help="Output file name. Write to stdout and to file.")
+    parser_file.add_argument(
+        "-i", "--input", help="Input serial port name.", required=True)
+    parser_file.add_argument(
+        "-d", "--database", help="Token database.", required=True)
+    parser_file.add_argument(
+        "-o", "--output", help="Output file name. Write to stdout and to file.")
 
     return parser.parse_args()
+
 
 def decode_string(tstr, detok):
     """Decodes a single token.
@@ -40,12 +48,13 @@ def decode_string(tstr, detok):
     try:
         t = bytes.fromhex(tstr)
         s = str(detok.detokenize(t))
-        
+
         if s.find('$') == 0:
             return None
         return s
     except:
         return None
+
 
 def decode_serial(serialport, outfile, database):
     """Decodes logs from serial port.
@@ -55,7 +64,7 @@ def decode_serial(serialport, outfile, database):
         outfile     - path to output file
         database    - path to token database
     """
-    
+
     detokenizer = pw_tokenizer.Detokenizer(database)
     input = serial.Serial(serialport, 115200, timeout=None)
 
@@ -70,7 +79,7 @@ def decode_serial(serialport, outfile, database):
                 if(input.in_waiting > 0):
                     # read line from serial port and ascii decode
                     line = input.readline().decode('ascii').strip()
-                    #find token start and detokenize
+                    # find token start and detokenize
                     idx = line.rfind(']')
                     dstr = decode_string(line[idx + 1:], detokenizer)
                     if dstr:
@@ -84,9 +93,10 @@ def decode_serial(serialport, outfile, database):
         if output:
             input.close()
             output.close()
-    
+
     else:
         print("Invalid or closed serial port.", file=sys.stderr)
+
 
 def decode_file(infile, outfile, database):
     """Decodes logs from input file.
@@ -111,7 +121,7 @@ def decode_file(infile, outfile, database):
                     line = line.decode('ascii').strip()
                 except:
                     continue
-                #find token start and detokenize
+                # find token start and detokenize
                 idx = line.rfind(']')
                 dstr = decode_string(line[idx + 1:], detokenizer)
                 if dstr:
@@ -122,14 +132,16 @@ def decode_file(infile, outfile, database):
     else:
         print("File does not exist or is not a file.", file=sys.stderr)
 
+
 def detokenize_input():
 
     args = parse_args()
-    
+
     if args.type == 'file':
         decode_file(args.input, args.output, args.database)
     if args.type == 'serial':
         decode_serial(args.input, args.output, args.database)
+
 
 if __name__ == '__main__':
     detokenize_input()
