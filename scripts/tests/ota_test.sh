a00a391e9387cbb06fbcf50c33b246ef4da7e5c5
Michael Rupp <95718139+mykrupp@users.noreply.github.com>
2022-03-07 12:01:19 -0500

    add random udp and discrim

--- scripts/tests/ota_test.sh
+++ scripts/tests/ota_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PASSCODE=${1:-20202021}
-DISCRIMINATOR=${2:-42}
-UDP_PORT=${3:-5560}
+DISCRIMINATOR=${2:-( $RANDOM % 59535 + 6000 )}
+UDP_PORT=${3:-( $RANDOM % 9000 + 1000 )}
 
 pkill chip-ota-provider-app
 pkill chip-ota-requestor-app
