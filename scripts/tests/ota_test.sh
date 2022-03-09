b8c3a6858f89cee1c9902f99f99ba7c599784280
Michael Rupp <95718139+mykrupp@users.noreply.github.com>
2022-03-09 10:30:12 -0500

    add -r

--- scripts/tests/ota_test.sh
+++ scripts/tests/ota_test.sh
@@ -18,7 +18,7 @@ scripts/examples/gn_build_example.sh examples/chip-tool out/
 
 echo "Test" >"$FIRMWARE_BIN"
 
-rm /tmp/chip_*
+rm -r /tmp/chip_*
 
 ./src/app/ota_image_tool.py create -v 0xDEAD -p 0xBEEF -vn 1 -vs "1.0" -da sha256 "$FIRMWARE_BIN" "$FIRMWARE_OTA"
 
