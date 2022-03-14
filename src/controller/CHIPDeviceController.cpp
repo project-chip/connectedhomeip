ba09530b9d13b5a9fd64b5279dc21077e99d9d29
C Freeman <cecille@google.com>
2022-03-14 09:32:15 -0400

    Update src/controller/CHIPDeviceController.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/controller/CHIPDeviceController.cpp
+++ src/controller/CHIPDeviceController.cpp
@@ -668,7 +668,7 @@ CHIP_ERROR DeviceCommissioner::Shutdown()
     }
 #endif // CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY
 
-    // Release everything from the commissionee device pool here. DeviceController::Shutdown releasese operational.
+    // Release everything from the commissionee device pool here. DeviceController::Shutdown releases operational.
     mCommissioneeDevicePool.ReleaseAll();
 
     DeviceController::Shutdown();
