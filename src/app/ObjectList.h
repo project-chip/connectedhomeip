fd7756b50904fe0983bcee9a49810947049108a4
Yunhan Wang <yunhanw@google.com>
2022-03-21 16:28:29 -0700

    Use AttributePathParams Pool, EventPathParams Pool and DataVersionFilter Pool instead. Roughly there is no logical change.
    Testing
    The existing test covers.

--- src/app/ObjectList.h
+++ src/app/ObjectList.h
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2021 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,26 +18,20 @@
 
 #pragma once
 
+#include <app/ConcreteAttributePath.h>
+#include <app/ConcreteEventPath.h>
 #include <app/util/basic-types.h>
+#include <assert.h>
+#include <lib/core/Optional.h>
 
 namespace chip {
 namespace app {
-struct DataVersionFilter
+
+template <typename T>
+struct ObjectList
 {
-    DataVersionFilter(EndpointId aEndpointId, ClusterId aClusterId, DataVersion aDataVersion) :
-        mClusterId(aClusterId), mDataVersion(aDataVersion), mEndpointId(aEndpointId)
-    {}
-
-    DataVersionFilter() {}
-
-    bool IsValidDataVersionFilter() const
-    {
-        return (mEndpointId != kInvalidEndpointId) && (mClusterId != kInvalidClusterId) && (mDataVersion.HasValue());
-    }
-
-    ClusterId mClusterId = kInvalidClusterId;    // uint32
-    Optional<DataVersion> mDataVersion;          // uint32
-    EndpointId mEndpointId = kInvalidEndpointId; // uint16
+    T mValue;
+    ObjectList * mpNext = nullptr;
 };
 } // namespace app
 } // namespace chip
