c13c54a8202670d19e535e2514172a245c28ec30
Andrei Litvin <andy314@gmail.com>
2022-03-25 23:46:01 -0400

    Dynamic response sender count (#16535)
    * Make tracking responders either static or dynamic based on compiler flags
    
    * Ensure linter does not complain about inclusion of std list
    
    * Change linux/android/darwin implementations and update tests
    
    * Restyle
    
    * Fix typo
    
    * Switch dynamic address setting to a bool
    
    * Review updates

--- src/lib/core/CHIPConfig.h
+++ src/lib/core/CHIPConfig.h
@@ -1771,6 +1771,20 @@ extern const char CHIP_NON_PRODUCTION_MARKER[];
 #define CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE 1003
 #endif // CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE
 
+/*
+ * @def CHIP_CONFIG_MINMDNS_DYNAMIC_OPERATIONAL_RESPONDER_LIST
+ *
+ * @brief Enables usage of heap in the minmdns DNSSD implementation
+ *        for tracking active operational responder lists.
+ *
+ *        When this is not set, CHIP_CONFIG_MAX_FABRICS is used to determine
+ *        and statically allocate pointers needed to track active
+ *        operational responder lists.
+ */
+#ifndef CHIP_CONFIG_MINMDNS_DYNAMIC_OPERATIONAL_RESPONDER_LIST
+#define CHIP_CONFIG_MINMDNS_DYNAMIC_OPERATIONAL_RESPONDER_LIST 0
+#endif // CHIP_CONFIG_MINMDNS_DYNAMIC_OPERATIONAL_RESPONDER_LIST
+
 /**
  * @}
  */
