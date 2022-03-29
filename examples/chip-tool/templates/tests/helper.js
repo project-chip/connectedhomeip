a653c4e359f2eebd8c996a4a92846dabdab13e61
Vivien Nicolas <vnicolas@apple.com>
2022-03-29 13:46:09 +0200

    [YAML] Update the template to use a decodable type for the response of simulated clusters

--- examples/chip-tool/templates/tests/helper.js
+++ examples/chip-tool/templates/tests/helper.js
@@ -15,7 +15,8 @@
  *    limitations under the License.
  */
 
-const { asLowerCamelCase }  = require('../../../../src/app/zap-templates/templates/app/helper.js');
+const { zapTypeToDecodableClusterObjectType, asUpperCamelCase, asLowerCamelCase }
+= require('../../../../src/app/zap-templates/templates/app/helper.js');
 const { isTestOnlyCluster } = require('../../../../src/app/zap-templates/common/simulated-clusters/SimulatedClusters.js');
 
 function utf8StringLength(str)
@@ -47,8 +48,36 @@ function asPropertyValue(options)
   return name;
 }
 
+async function asDecodableType()
+{
+  // Copy some properties needed by zapTypeToDecodableClusterObjectType
+  let target = { global : this.global, entryType : this.entryType };
+
+  let type;
+  if ('commandObject' in this) {
+    type = this.commandObject.responseName;
+  } else if ('attributeObject' in this) {
+    type              = this.attributeObject.type;
+    target.isArray    = this.attributeObject.isArray;
+    target.isOptional = this.attributeObject.isOptional;
+    target.isNullable = this.attributeObject.isNullable;
+  } else if ('eventObject' in this) {
+    type = this.eventObject.type;
+  } else {
+    throw new Error("Unsupported decodable type");
+  }
+
+  if (isTestOnlyCluster(this.cluster) || 'commandObject' in this) {
+    return `chip::app::Clusters::${asUpperCamelCase(this.cluster)}::Commands::${asUpperCamelCase(type)}::DecodableType`;
+  }
+
+  const options = { 'hash' : { ns : this.cluster } };
+  return await zapTypeToDecodableClusterObjectType.call(target, type, options);
+}
+
 //
 // Module exports
 //
 exports.utf8StringLength = utf8StringLength;
 exports.asPropertyValue  = asPropertyValue;
+exports.asDecodableType  = asDecodableType;
