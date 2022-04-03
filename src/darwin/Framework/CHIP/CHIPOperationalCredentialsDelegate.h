58f7d83f1de4b1ebd6bd5c686c7ed87eb05076e5
Cecille Freeman <cecille@google.com>
2022-04-03 12:03:40 -0400

    Bring back namespasing.
    I accidentally removed it.

--- src/darwin/Framework/CHIP/CHIPOperationalCredentialsDelegate.h
+++ src/darwin/Framework/CHIP/CHIPOperationalCredentialsDelegate.h
@@ -46,9 +46,9 @@ public:
      */
     CHIP_ERROR init(CHIPPersistentStorageDelegateBridge * storage, ChipP256KeypairPtr nocSigner, NSData * _Nullable ipk);
 
-    CHIP_ERROR GenerateNOCChain(const ByteSpan & csrElements, const ByteSpan & csrNonce, const ByteSpan & attestationSignature,
-        const ByteSpan & attestationChallenge, const ByteSpan & DAC, const ByteSpan & PAI,
-        Callback::Callback<OnNOCChainGeneration> * onCompletion) override;
+    CHIP_ERROR GenerateNOCChain(const chip::ByteSpan & csrElements, const chip::ByteSpan & csrNonce,
+        const chip::ByteSpan & attestationSignature, const chip::ByteSpan & attestationChallenge, const chip::ByteSpan & DAC,
+        const chip::ByteSpan & PAI, chip::Callback::Callback<chip::Controller::OnNOCChainGeneration> * onCompletion) override;
 
     void SetNodeIdForNextNOCRequest(chip::NodeId nodeId) override
     {
