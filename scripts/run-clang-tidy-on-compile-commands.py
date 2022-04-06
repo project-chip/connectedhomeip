3b846ec9f1b8904826e7104352bc4880e0125a8e
Andrei Litvin <andy314@gmail.com>
2022-04-06 17:07:36 -0400

    Deduplicate clang-tidy file names to check.
    Unified builds may compile the same file several times, which
    results in slower clang-tidy (specifically darwin runs everything
    twice: once for gcc_x64 and once for standalone).
    
    Even though arguments may be different, this should not have a
    significant enough impact on tidy results. If we want to run
    with specific arguments, we can run on a more focused compile
    database or update the script to allow skipping the dedup.

--- scripts/run-clang-tidy-on-compile-commands.py
+++ scripts/run-clang-tidy-on-compile-commands.py
@@ -189,6 +189,7 @@ class ClangTidyRunner:
         self.fixes_file = None
         self.fixes_temporary_file_dir = None
         self.gcc_sysroot = None
+        self.file_names_to_check = set()
 
         if sys.platform == 'darwin':
             # Darwin gcc invocation will auto select a system root, however clang requires an explicit path since
@@ -206,6 +207,11 @@ class ClangTidyRunner:
             if not item.valid:
                 continue
 
+            if item.file in self.file_names_to_check:
+                logging.info('Ignoring additional request for checking %s', item.file)
+                continue
+
+            self.file_names_to_check.add(item.file)
             self.entries.append(item)
 
     def Cleanup(self):
