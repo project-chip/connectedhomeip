dd942f2d699ef35f88dc1da8154686d617211044
Andrei Litvin <andy314@gmail.com>
2022-04-08 09:39:05 -0400

    Restyle

--- src/app/clusters/scenes/scenes.cpp
+++ src/app/clusters/scenes/scenes.cpp
@@ -801,7 +801,7 @@ bool emberAfPluginScenesServerParseAddScene(
     if (enhanced)
     {
         entry.transitionTime      = transitionTime / 10;
-        entry.transitionTime100ms = (uint8_t) (transitionTime - entry.transitionTime * 10);
+        entry.transitionTime100ms = (uint8_t)(transitionTime - entry.transitionTime * 10);
     }
     else
     {
