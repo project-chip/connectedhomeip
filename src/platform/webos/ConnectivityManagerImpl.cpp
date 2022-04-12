26be92862e8343cf64f51e5b0aff0d18dd03c591
sungmok.shin <sungmok.shin@lge.com>
2022-04-12 23:49:44 +0900

    Update license
    * This commit updates the license of ConenctivityManagerImpl.cpp

--- src/platform/webos/ConnectivityManagerImpl.cpp
+++ src/platform/webos/ConnectivityManagerImpl.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020-2021 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *    Copyright (c) 2019 Nest Labs, Inc.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
