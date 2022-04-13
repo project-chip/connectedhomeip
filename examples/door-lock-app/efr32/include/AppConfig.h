63ba3b7cd8116ebff24c2a86beca2cc188f1448b
Michael Rupp <95718139+mykrupp@users.noreply.github.com>
2022-04-13 13:36:56 -0400

    Door lock example for efr32

--- examples/door-lock-app/efr32/include/AppConfig.h
+++ examples/door-lock-app/efr32/include/AppConfig.h
@@ -1,6 +1,7 @@
 /*
  *
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2019 Google LLC.
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +17,30 @@
  *    limitations under the License.
  */
 
-#include "Globals.h"
+#pragma once
 
-LED_Handle sAppRedHandle;
-LED_Handle sAppGreenHandle;
+// ---- Door lock Example App Config ----
+
+#define APP_TASK_NAME "Lock"
+
+// Time it takes in ms for the simulated actuator to move from one
+// state to another.
+#define ACTUATOR_MOVEMENT_PERIOS_MS 10
+
+// EFR Logging
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void efr32LogInit(void);
+
+void efr32Log(const char * aFormat, ...);
+#define EFR32_LOG(...) efr32Log(__VA_ARGS__);
+void appError(int err);
+
+#ifdef __cplusplus
+}
+
+#include <lib/core/CHIPError.h>
+void appError(CHIP_ERROR error);
+#endif
