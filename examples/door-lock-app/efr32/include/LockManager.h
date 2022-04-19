07717b0022a83bd2a92fcb895d631fe0de5d5267
Michael Rupp <95718139+mykrupp@users.noreply.github.com>
2022-04-19 19:04:55 -0400

    Add user/credential functionality back to app. Save lockState in NVRam

--- examples/door-lock-app/efr32/include/LockManager.h
+++ examples/door-lock-app/efr32/include/LockManager.h
@@ -31,11 +31,32 @@
 
 using namespace ::chip;
 
-static constexpr size_t DOOR_LOCK_CREDENTIAL_INFO_MAX_DATA_SIZE = 20;
-class LockManager
+struct LockUserInfo
+{
+    char userName[DOOR_LOCK_USER_NAME_BUFFER_SIZE];
+    DlCredential credentials[DOOR_LOCK_MAX_CREDENTIALS_PER_USER];
+    size_t totalCredentials;
+    uint32_t userUniqueId;
+    DlUserStatus userStatus;
+    DlUserType userType;
+    DlCredentialRule credentialRule;
+    chip::FabricIndex createdBy;
+    chip::FabricIndex lastModifiedBy;
+};
 
+static constexpr size_t DOOR_LOCK_CREDENTIAL_INFO_MAX_DATA_SIZE = 20;
+
+struct LockCredentialInfo
+{
+    DlCredentialStatus status;
+    DlCredentialType credentialType;
+    uint8_t credentialData[DOOR_LOCK_CREDENTIAL_INFO_MAX_DATA_SIZE];
+    size_t credentialDataSize;
+};
+class LockManager
 {
 public:
+
     enum Action_t
     {
         LOCK_ACTION = 0,
@@ -61,6 +82,23 @@ public:
     typedef void (*Callback_fn_completed)(Action_t);
     void SetCallbacks(Callback_fn_initiated aActionInitiated_CB, Callback_fn_completed aActionCompleted_CB);
 
+    bool Lock(chip::EndpointId endpointId, const Optional<chip::ByteSpan> & pin, DlOperationError & err);
+    bool Unlock(chip::EndpointId endpointId, const Optional<chip::ByteSpan> & pin, DlOperationError & err);
+
+    bool GetUser(uint16_t userIndex, EmberAfPluginDoorLockUserInfo & user) const;
+    bool SetUser(uint16_t userIndex, chip::FabricIndex creator, chip::FabricIndex modifier, const chip::CharSpan & userName,
+                 uint32_t uniqueId, DlUserStatus userStatus, DlUserType usertype, DlCredentialRule credentialRule,
+                 const DlCredential * credentials, size_t totalCredentials);
+
+    bool GetCredential(chip::EndpointId endpointId, DlCredentialType credentialType,
+                       EmberAfPluginDoorLockCredentialInfo & credential) const;
+
+    bool SetCredential(chip::EndpointId endpointId, DlCredentialStatus credentialStatus, DlCredentialType credentialType,
+                       const chip::ByteSpan & credentialData);
+
+    bool setLockState(DlLockState lockState, const Optional<chip::ByteSpan> & pin, DlOperationError & err);
+    const char * lockStateToString(DlLockState lockState) const;
+    
 private:
 
     friend LockManager & LockMgr();
@@ -80,6 +118,9 @@ private:
     static void TimerEventHandler(TimerHandle_t xTimer);
     static void AutoTurnOffTimerEventHandler(AppEvent * aEvent);
     static void ActuatorMovementTimerEventHandler(AppEvent * aEvent);
+
+    LockUserInfo mLockUser;
+    LockCredentialInfo mLockCredentials;
 
     static LockManager sLock;
 };
