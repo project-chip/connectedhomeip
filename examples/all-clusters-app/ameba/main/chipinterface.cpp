1c3fe9e9fbf2e0f5cdfa1486e1a5b663523c3fa0
Restyled.io <commits@restyled.io>
2022-04-19 11:18:21 +0000

    Restyled by clang-format

--- examples/all-clusters-app/ameba/main/chipinterface.cpp
+++ examples/all-clusters-app/ameba/main/chipinterface.cpp
@@ -44,8 +44,8 @@
 #include <app/clusters/ota-requestor/BDXDownloader.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestor.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorDriver.h>
-#include <app/clusters/ota-requestor/ExtendedOTARequestorDriver.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorUserConsent.h>
+#include <app/clusters/ota-requestor/ExtendedOTARequestorDriver.h>
 #include <platform/Ameba/AmebaOTAImageProcessor.h>
 #endif
 
