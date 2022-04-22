b4c408e81a467fa7473fa538d05991ac92606acf
Restyled.io <commits@restyled.io>
2022-04-22 21:55:44 +0000

    Restyled by clang-format

--- src/app/zap-templates/common/StringHelper.js
+++ src/app/zap-templates/common/StringHelper.js
@@ -14,36 +14,36 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-const characterStringTypes = ['CHAR_STRING', 'LONG_CHAR_STRING']
-const octetStringTypes = ['OCTET_STRING', 'LONG_OCTET_STRING']
-const stringShortTypes = ['CHAR_STRING', 'OCTET_STRING']
-const stringLongTypes = ['LONG_CHAR_STRING', 'LONG_OCTET_STRING']
+const characterStringTypes                                         = [ 'CHAR_STRING', 'LONG_CHAR_STRING' ] const octetStringTypes =
+    [ 'OCTET_STRING', 'LONG_OCTET_STRING' ] const stringShortTypes = [ 'CHAR_STRING', 'OCTET_STRING' ] const stringLongTypes =
+        [ 'LONG_CHAR_STRING', 'LONG_OCTET_STRING' ]
 
-async function isString(type) {
-    return isCharString(type) || isOctetString(type)
-}
+        async function isString(type) {
+          return isCharString(type) || isOctetString(type)
+        }
 
-function isCharString(type) {
-    return characterStringTypes.includes(type.toUpperCase())
-}
+        function isCharString(type) {
+          return characterStringTypes.includes(type.toUpperCase())
+        }
 
-function isOctetString(type) {
-    return octetStringTypes.includes(type.toUpperCase())
-}
+        function isOctetString(type) {
+          return octetStringTypes.includes(type.toUpperCase())
+        }
 
-function isShortString(type) {
-    return stringShortTypes.includes(type.toUpperCase())
-}
+        function isShortString(type) {
+          return stringShortTypes.includes(type.toUpperCase())
+        }
 
-function isLongString(type) {
-    return stringLongTypes.includes(type.toUpperCase())
-}
+        function isLongString(type) {
+          return stringLongTypes.includes(type.toUpperCase())
+        }
 
-//
-// Module exports
-//
-exports.isString = isString;
-exports.isCharString = isCharString;
+        //
+        // Module exports
+        //
+        exports.isString
+    = isString;
+exports.isCharString  = isCharString;
 exports.isOctetString = isOctetString;
 exports.isShortString = isShortString;
-exports.isLongString = isLongString;
+exports.isLongString  = isLongString;
