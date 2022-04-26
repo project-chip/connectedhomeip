9c5074bcde0498a37603932531e2be85b10145a1
Chris Gravel <chrisgravel@google.com>
2022-04-26 09:37:50 -0700

    Chef - Convert variables to snake case per PEP8
    Change-Id: I52520eec1dfb8093c39ce4570aa99cdd0b0d854a

--- examples/chef/chef.py
+++ examples/chef/chef.py
@@ -51,7 +51,7 @@ def splash():
     print(splashText)
 
 
-def loadConfig():
+def load_config():
     config = dict()
     config["nrfconnect"] = dict()
     config["esp32"] = dict()
@@ -77,7 +77,7 @@ def loadConfig():
     return config
 
 
-def checkPythonVersion():
+def check_python_version():
     if sys.version_info[0] < 3:
         print('Must use Python 3. Current version is ' +
               str(sys.version_info[0]))
@@ -85,8 +85,8 @@ def checkPythonVersion():
 
 
 def main(argv):
-    checkPythonVersion()
-    config = loadConfig()
+    check_python_version()
+    config = load_config()
 
     #
     # Build environment switches
@@ -120,41 +120,41 @@ def main(argv):
     parser = optparse.OptionParser(usage=usage)
 
     parser.add_option("-u", "--update_toolchain", help="updates toolchain & installs zap",
-                      action="store_true", dest="doUpdateToolchain")
+                      action="store_true", dest="do_update_toolchain")
     parser.add_option("-b", "--build", help="builds",
-                      action="store_true", dest="doBuild")
+                      action="store_true", dest="do_build")
     parser.add_option("-c", "--clean", help="clean build. Only valid if also building",
-                      action="store_true", dest="doClean")
+                      action="store_true", dest="do_clean")
     parser.add_option("-f", "--flash", help="flashes device",
-                      action="store_true", dest="doFlash")
+                      action="store_true", dest="do_flash")
     parser.add_option("-e", "--erase", help="erases flash before flashing. Only valid if also flashing",
-                      action="store_true", dest="doErase")
+                      action="store_true", dest="do_erase")
     parser.add_option("-i", "--terminal", help="opens terminal to interact with with device",
-                      action="store_true", dest="doInteract")
+                      action="store_true", dest="do_interact")
     parser.add_option("-m", "--menuconfig", help="runs menuconfig on platforms that support it",
-                      action="store_true", dest="doMenuconfig")
+                      action="store_true", dest="do_menuconfig")
     parser.add_option("", "--bootstrap_zap", help="installs zap dependencies",
-                      action="store_true", dest="doBootstrapZap")
+                      action="store_true", dest="do_bootstrap_zap")
     parser.add_option("-z", "--zap", help="runs zap to generate data model & interaction model artifacts",
-                      action="store_true", dest="doRunZap")
+                      action="store_true", dest="do_run_zap")
     parser.add_option("-g", "--zapgui", help="runs zap GUI display to allow editing of data model",
-                      action="store_true", dest="doRunGui")
-    parser.add_option("-d", "--device", dest="sampleDeviceTypeName",
+                      action="store_true", dest="do_run_gui")
+    parser.add_option("-d", "--device", dest="sample_device_type_name",
                       help="specifies device type. Default is lighting. See info above for supported device types", metavar="TARGET", default="lighting")
     parser.add_option("-t", "--target", type='choice',
                       action='store',
-                      dest="buildTarget",
+                      dest="build_target",
                       help="specifies target platform. Default is esp32. See info below for currently supported target platforms",
                       choices=['nrfconnect', 'esp32', 'linux', ],
                       metavar="TARGET",
                       default="esp32")
-    parser.add_option("-r", "--rpc", help="enables Pigweed RPC interface. Enabling RPC disables the shell interface. Your sdkconfig configurations will be reverted to default. Default is PW RPC off. When enabling or disabling this flag, on the first build force a clean build with -c", action="store_true", dest="doRPC")
+    parser.add_option("-r", "--rpc", help="enables Pigweed RPC interface. Enabling RPC disables the shell interface. Your sdkconfig configurations will be reverted to default. Default is PW RPC off. When enabling or disabling this flag, on the first build force a clean build with -c", action="store_true", dest="do_rpc")
     parser.add_option("-v", "--vid", dest="vid", type=int,
                       help="specifies the Vendor ID. Default is 0xFFF1", metavar="VID", default=0xFFF1)
     parser.add_option("-p", "--pid", dest="pid", type=int,
                       help="specifies the Product ID. Default is 0x8000", metavar="PID", default=0x8000)
     parser.add_option("", "--rpc_console", help="Opens PW RPC Console",
-                      action="store_true", dest="doRPC_CONSOLE")
+                      action="store_true", dest="do_rpc_console")
     parser.add_option("-y", "--tty", help="Enumerated USB tty/serial interface enumerated for your physical device. E.g.: /dev/ACM0",
                       dest="tty", metavar="TTY", default=None)
 
@@ -166,29 +166,29 @@ def main(argv):
     # Platform Folder
     #
 
-    print(f"Target is set to {options.sampleDeviceTypeName}")
+    print(f"Target is set to {options.sample_device_type_name}")
     global gen_dir
     gen_dir = (
-        f"{_CHEF_SCRIPT_PATH}/out/{options.sampleDeviceTypeName}/zap-generated/")
+        f"{_CHEF_SCRIPT_PATH}/out/{options.sample_device_type_name}/zap-generated/")
 
     print("Setting up environment...")
-    if options.buildTarget == "esp32":
+    if options.build_target == "esp32":
         if config['esp32']['IDF_PATH'] is None:
             print('Path for esp32 SDK was not found. Make sure esp32.IDF_PATH is set on your config.yaml file')
             exit(1)
         plat_folder = os.path.normpath(f"{_CHEF_SCRIPT_PATH}/esp32")
         shell.run_cmd(f'source {config["esp32"]["IDF_PATH"]}/export.sh')
-    elif options.buildTarget == "nrfconnect":
+    elif options.build_target == "nrfconnect":
         if config['nrfconnect']['ZEPHYR_BASE'] is None:
             print('Path for nrfconnect SDK was not found. Make sure nrfconnect.ZEPHYR_BASE is set on your config.yaml file')
             exit(1)
         plat_folder = os.path.normpath(f"{_CHEF_SCRIPT_PATH}/nrfconnect")
         shell.run_cmd(f'source {config["nrfconnect"]["ZEPHYR_BASE"]}/zephyr-env.sh')
         shell.run_cmd("export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb")
-    elif options.buildTarget == "linux":
+    elif options.build_target == "linux":
         pass
     else:
-        print(f"Target {options.buildTarget} not supported")
+        print(f"Target {options.build_target} not supported")
 
     shell.run_cmd(f"source {_REPO_BASE_PATH}/scripts/activate.sh")
 
@@ -196,21 +196,21 @@ def main(argv):
     # Toolchain update
     #
 
-    if options.doUpdateToolchain:
-        if options.buildTarget == "esp32":
+    if options.do_update_toolchain:
+        if options.build_target == "esp32":
             print("ESP32 toolchain update not supported. Skipping")
-        elif options.buildTarget == "nrfconnect":
+        elif options.build_target == "nrfconnect":
             print("Updating toolchain")
             shell.run_cmd(
                 f"cd {_REPO_BASE_PATH} && python3 scripts/setup/nrfconnect/update_ncs.py --update")
-        elif options.buildTarget == "linux":
+        elif options.build_target == "linux":
             print("Linux toolchain update not supported. Skipping")
 
     #
     # ZAP bootstrapping
     #
 
-    if options.doBootstrapZap:
+    if options.do_bootstrap_zap:
         if sys.platform == "linux" or sys.platform == "linux2":
             print("Installing ZAP OS package dependencies")
             shell.run_cmd(
@@ -229,18 +229,18 @@ def main(argv):
     # Cluster customization
     #
 
-    if options.doRunGui:
+    if options.do_run_gui:
         print("Starting ZAP GUI editor")
         shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/devices")
         shell.run_cmd(
-            f"{_REPO_BASE_PATH}/scripts/tools/zap/run_zaptool.sh {options.sampleDeviceTypeName}.zap")
+            f"{_REPO_BASE_PATH}/scripts/tools/zap/run_zaptool.sh {options.sample_device_type_name}.zap")
 
-    if options.doRunZap:
+    if options.do_run_zap:
         print("Running ZAP script to generate artifacts")
         shell.run_cmd(f"mkdir -p {gen_dir}/")
         shell.run_cmd(f"rm {gen_dir}/*")
         shell.run_cmd(
-            f"{_REPO_BASE_PATH}/scripts/tools/zap/generate.py {_CHEF_SCRIPT_PATH}/devices/{options.sampleDeviceTypeName}.zap -o {gen_dir}")
+            f"{_REPO_BASE_PATH}/scripts/tools/zap/generate.py {_CHEF_SCRIPT_PATH}/devices/{options.sample_device_type_name}.zap -o {gen_dir}")
         # af-gen-event.h is not generated
         shell.run_cmd(f"touch {gen_dir}/af-gen-event.h")
 
@@ -248,23 +248,23 @@ def main(argv):
     # Menuconfig
     #
 
-    if options.doMenuconfig:
-        if options.buildTarget == "esp32":
+    if options.do_menuconfig:
+        if options.build_target == "esp32":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/esp32")
             shell.run_cmd("idf.py menuconfig")
-        elif options.buildTarget == "nrfconnect":
+        elif options.build_target == "nrfconnect":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/nrfconnect")
             shell.run_cmd("west build -t menuconfig")
-        elif options.buildTarget == "linux":
+        elif options.build_target == "linux":
             print("Menuconfig not available on Linux target. Skipping")
 
     #
     # Build
     #
 
-    if options.doBuild:
+    if options.do_build:
         print("Building...")
-        if options.doRPC:
+        if options.do_rpc:
             print("RPC PW enabled")
             shell.run_cmd(
                 f"export SDKCONFIG_DEFAULTS={_CHEF_SCRIPT_PATH}/esp32/sdkconfig_rpc.defaults")
@@ -276,30 +276,30 @@ def main(argv):
             f"Product ID 0x{options.pid:02X} / Vendor ID 0x{options.vid:02X}")
         shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}")
 
-        if (options.buildTarget == "esp32") or (options.buildTarget == "nrfconnect"):
+        if (options.build_target == "esp32") or (options.build_target == "nrfconnect"):
             with open("project_include.cmake", "w") as f:
                 f.write(textwrap.dedent(f"""\
                         set(CONFIG_DEVICE_VENDOR_ID {options.vid})
                         set(CONFIG_DEVICE_PRODUCT_ID {options.pid})
-                        set(CONFIG_ENABLE_PW_RPC {"1" if options.doRPC else "0"})
-                        set(SAMPLE_NAME {options.sampleDeviceTypeName})"""))
+                        set(CONFIG_ENABLE_PW_RPC {"1" if options.do_rpc else "0"})
+                        set(SAMPLE_NAME {options.sample_device_type_name})"""))
 
-        if options.buildTarget == "esp32":
+        if options.build_target == "esp32":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/esp32")
-            if options.doClean:
+            if options.do_clean:
                 shell.run_cmd(f"rm {_CHEF_SCRIPT_PATH}/esp32/sdkconfig")
                 shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/esp32")
                 shell.run_cmd(f"rm -rf {_CHEF_SCRIPT_PATH}/esp32/build")
                 shell.run_cmd("idf.py fullclean")
             shell.run_cmd("idf.py build")
-        elif options.buildTarget == "nrfconnect":
+        elif options.build_target == "nrfconnect":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/nrfconnect")
-            if options.doClean:
+            if options.do_clean:
                 # shell.run_cmd(f"rm -rf {paths['rootSampleFolder']}/nrfconnect/build")
                 shell.run_cmd("west build -b nrf52840dk_nrf52840")
             else:
                 shell.run_cmd("west build -b nrf52840dk_nrf52840")
-        elif options.buildTarget == "linux":
+        elif options.build_target == "linux":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/linux")
             with open(f"{_CHEF_SCRIPT_PATH}/linux/args.gni", "w") as f:
                 f.write(textwrap.dedent(f"""\
@@ -310,10 +310,10 @@ def main(argv):
                         """))
             with open(f"{_CHEF_SCRIPT_PATH}/linux/sample.gni", "w") as f:
                 f.write(textwrap.dedent(f"""\
-                        sample_zap_file = "{options.sampleDeviceTypeName}.zap"
-                        sample_name = "{options.sampleDeviceTypeName}"
+                        sample_zap_file = "{options.sample_device_type_name}.zap"
+                        sample_name = "{options.sample_device_type_name}"
                         """))
-            if options.doClean:
+            if options.do_clean:
                 shell.run_cmd(f"rm -rf out")
             shell.run_cmd("gn gen out")
             shell.run_cmd("ninja -C out")
@@ -326,20 +326,20 @@ def main(argv):
     # Flash
     #
 
-    if options.doFlash:
+    if options.do_flash:
         print("Flashing target")
-        if options.buildTarget == "esp32":
+        if options.build_target == "esp32":
             if config['esp32']['TTY'] is None:
                 print('The path for the serial enumeration for esp32 is not set. Make sure esp32.TTY is set on your config.yaml file')
                 exit(1)
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/esp32")
-            if options.doErase:
+            if options.do_erase:
                 shell.run_cmd(
                     f"idf.py -p {config['esp32']['TTY']} erase-flash")
             shell.run_cmd(f"idf.py -p {config['esp32']['TTY']} flash")
-        elif options.buildTarget == "nrfconnect":
+        elif options.build_target == "nrfconnect":
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/nrfconnect")
-            if options.doErase:
+            if options.do_erase:
                 shell.run_cmd("rm -rf build")
             else:
                 shell.run_cmd("west flash")
@@ -348,30 +348,30 @@ def main(argv):
     # Terminal interaction
     #
 
-    if options.doInteract:
+    if options.do_interact:
         print("Starting terminal...")
-        if options.buildTarget == "esp32":
+        if options.build_target == "esp32":
             if config['esp32']['TTY'] is None:
                 print('The path for the serial enumeration for esp32 is not set. Make sure esp32.TTY is set on your config.yaml file')
                 exit(1)
             shell.run_cmd(f"cd {_CHEF_SCRIPT_PATH}/esp32")
             shell.run_cmd(f"idf.py -p {config['esp32']['TTY']} monitor")
-        elif options.buildTarget == "nrfconnect":
+        elif options.build_target == "nrfconnect":
             if config['nrfconnect']['TTY'] is None:
                 print('The path for the serial enumeration for nordic is not set. Make sure nrfconnect.TTY is set on your config.yaml file')
                 exit(1)
             shell.run_cmd("killall screen")
             shell.run_cmd(f"screen {config['nrfconnect']['TTY']} 115200")
-        elif options.buildTarget == "linux":
+        elif options.build_target == "linux":
             print(
-                f"{_CHEF_SCRIPT_PATH}/linux/out/{options.sampleDeviceTypeName}")
+                f"{_CHEF_SCRIPT_PATH}/linux/out/{options.sample_device_type_name}")
             shell.run_cmd(
-                f"{_CHEF_SCRIPT_PATH}/linux/out/{options.sampleDeviceTypeName}")
+                f"{_CHEF_SCRIPT_PATH}/linux/out/{options.sample_device_type_name}")
 
     #
     # RPC Console
     #
-    if options.doRPC_CONSOLE:
+    if options.do_rpc_console:
         shell.run_cmd(
             f"python3 -m chip_rpc.console --device {config['esp32']['TTY']}")
 
