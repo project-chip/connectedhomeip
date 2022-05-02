769264aef02674c04dfadb76664af6173d795714
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2022-05-02 16:38:58 -0400

    Reduce RAM usage from CHIPDeviceEvent (#17959)
    * Reduce RAM usage from CHIPDeviceEvent
    
    - CHIPDeviceEvent has had a historical `InternetConnectivityChange`
      event that embeds a 46 byte string (instead of a 16 byte IPAddress)
      and is *only used for logging* in examples.=
    - This event is in a union and is the largest of all the sub-structs,
      therefore forcing max-sized events to that event.
    
    Issue #9261
    
    This PR:
    
    - Changes the `address` from a string to an IPAddress
    - Renames to `ipAddress` to break external clients using this struct
      so that they notice on the next roll to master (usage change is trivial).
    - Updates all examples to no longer log the address, since it was only
      logged for IPv4 and not IPv6, and IPv6 is the standard, so if it was
      good enough for IPv6 not to log, it's good enough for IPv4.
    - Make IPAddress trivially copyable by removing a non-trivial `operator=` that was actually implementing the
      trivial copy. Upstream OpenWeave code that was the basis for the IPAddress.h file removed it already in late
      2021, > 1.5 years after import (see
      https://github.com/openweave/openweave-core/commit/fbbb01c501a8516df871842403a4ea926f5e4996#diff-33121ed998c085b8dc0026f30f24aaadb8b8b36042e38b449cec15c32c8ba86e)
    - Update all platforms that populated the address to use the actual address,
      not the string
    
    Testing done:
    
    - All cert tests pass
    - All unit tests pass
    
    * Restyled by clang-format
    
    * Fix CI on newer compilers
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/platform/P6/ConnectivityManagerImpl.cpp
+++ src/platform/P6/ConnectivityManagerImpl.cpp
@@ -657,10 +657,10 @@ void ConnectivityManagerImpl::UpdateInternetConnectivityState(void)
 
         // Alert other components of the state change.
         ChipDeviceEvent event;
-        event.Type                            = DeviceEventType::kInternetConnectivityChange;
-        event.InternetConnectivityChange.IPv4 = GetConnectivityChange(hadIPv4Conn, haveIPv4Conn);
-        event.InternetConnectivityChange.IPv6 = GetConnectivityChange(hadIPv6Conn, haveIPv6Conn);
-        addr.ToString(event.InternetConnectivityChange.address);
+        event.Type                                 = DeviceEventType::kInternetConnectivityChange;
+        event.InternetConnectivityChange.IPv4      = GetConnectivityChange(hadIPv4Conn, haveIPv4Conn);
+        event.InternetConnectivityChange.IPv6      = GetConnectivityChange(hadIPv6Conn, haveIPv6Conn);
+        event.InternetConnectivityChange.ipAddress = addr;
         PlatformMgr().PostEventOrDie(&event);
 
         if (haveIPv4Conn != hadIPv4Conn)
