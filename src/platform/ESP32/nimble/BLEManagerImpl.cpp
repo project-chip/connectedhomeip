14f33de41c9e78ff35b11c966332979dac9c0424
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2022-05-11 14:16:57 +0200

    Created Device Instance Info Provider
    The new Device Instance Info Provider provides API for
    accessing factory-provisioned device instance identifiers.
    
    All removed Configuration Manager method occurrences
    were replaced with a new Device Instance Info Provider API.
    Added LegacyDeviceInstanceInfoProvider implementation that
    preserves the existing ConfigurationManager methods.

--- src/platform/ESP32/nimble/BLEManagerImpl.cpp
+++ src/platform/ESP32/nimble/BLEManagerImpl.cpp
@@ -34,6 +34,7 @@
 #include <lib/support/CodeUtils.h>
 #include <lib/support/logging/CHIPLogging.h>
 #include <platform/CommissionableDataProvider.h>
+#include <platform/DeviceInstanceInfoProvider.h>
 #include <platform/internal/BLEManager.h>
 #include <setup_payload/AdditionalDataPayloadGenerator.h>
 #include <system/SystemTimer.h>
@@ -1076,7 +1077,7 @@ void BLEManagerImpl::HandleC3CharRead(struct ble_gatt_char_context * param)
     uint8_t rotatingDeviceIdUniqueId[ConfigurationManager::kRotatingDeviceIDUniqueIDLength] = {};
     MutableByteSpan rotatingDeviceIdUniqueIdSpan(rotatingDeviceIdUniqueId);
 
-    err = ConfigurationMgr().GetRotatingDeviceIdUniqueId(rotatingDeviceIdUniqueIdSpan);
+    err = DeviceLayer::GetDeviceInstanceInfoProvider()->GetRotatingDeviceIdUniqueId(rotatingDeviceIdUniqueIdSpan);
     SuccessOrExit(err);
     err = ConfigurationMgr().GetLifetimeCounter(additionalDataPayloadParams.rotatingDeviceIdLifetimeCounter);
     SuccessOrExit(err);
