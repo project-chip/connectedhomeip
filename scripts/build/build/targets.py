9f755f91df316a4f8095d95882e2a9c25458e1d2
Seth Rickard <s-rickard@ti.com>
2022-05-13 17:04:02 -0500

    [TI] enable shell example (#18176)
    * enable shell example
    
    add shell to CI build
    
    * revert BLE command change
    
    This will cause the BLE advertisment command in the Shell example to not
    function properly.

--- scripts/build/build/targets.py
+++ scripts/build/build/targets.py
@@ -492,6 +492,7 @@ def cc13x2x7_26x2x7Targets():
     yield target.Extend('pump', app=cc13x2x7_26x2x7App.PUMP)
     yield target.Extend('pump-controller', app=cc13x2x7_26x2x7App.PUMP_CONTROLLER)
     yield target.Extend('all-clusters', app=cc13x2x7_26x2x7App.ALL_CLUSTERS)
+    yield target.Extend('shell', app=cc13x2x7_26x2x7App.SHELL)
 
 
 def Cyw30739Targets():
