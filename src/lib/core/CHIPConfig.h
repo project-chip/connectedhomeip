5a3371ae8927897ee333f35b9ba22dc6a5698179
Song Guo <songguo@google.com>
2022-05-13 10:14:55 +0800

    Revert "Update limit"
    This reverts commit 8cc0c463894c62b438e8b40f9f62103e630f4e5b.

--- src/lib/core/CHIPConfig.h
+++ src/lib/core/CHIPConfig.h
@@ -778,9 +778,12 @@ extern const char CHIP_NON_PRODUCTION_MARKER[];
  * @brief Defines the maximum number of ReadHandler, limits the number of active read transactions on server.
  *
  * The default value comes from 3sub per fabric * max number of fabrics, then reserve 1 read client for each fabric.
+ *
+ * TODO: (#17085) Should be changed to (CHIP_CONFIG_MAX_FABRICS * 4) after we can hold more read handlers on more concise
+ * devices.
  */
 #ifndef CHIP_IM_MAX_NUM_READ_HANDLER
-#define CHIP_IM_MAX_NUM_READ_HANDLER (CHIP_CONFIG_MAX_FABRICS * 4)
+#define CHIP_IM_MAX_NUM_READ_HANDLER (CHIP_CONFIG_MAX_FABRICS * 3)
 #endif
 
 /**
@@ -801,7 +804,9 @@ extern const char CHIP_NON_PRODUCTION_MARKER[];
  * paths for each fabric.
  */
 #ifndef CHIP_IM_SERVER_MAX_NUM_PATH_GROUPS
-#define CHIP_IM_SERVER_MAX_NUM_PATH_GROUPS (CHIP_CONFIG_MAX_FABRICS * 18)
+// #define CHIP_IM_SERVER_MAX_NUM_PATH_GROUPS (CHIP_CONFIG_MAX_FABRICS * 18)
+// TODO: (#17085) Should be 3 sub * 3 path + 9 path (for read) = 18
+#define CHIP_IM_SERVER_MAX_NUM_PATH_GROUPS (CHIP_CONFIG_MAX_FABRICS * 13)
 #endif
 
 /**
