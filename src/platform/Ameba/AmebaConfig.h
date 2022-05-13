32bf8b347a6c3d503774698bd054340ceb9d2e05
pankore <86098180+pankore@users.noreply.github.com>
2022-05-13 18:45:13 +0800

    [Connectivity] Use persistent storage wifi info to connect to wifi after reboot * Implement AmebaUtils for wifi helper functions * Update ConnectivityManagerImpl and NetworkCommissioningDriverWiFi to use AmebaUtils functions * Add ChipGroupMessageCounters, ChipOthers2 * Remove ChipSessionResumptionIndex, which will be part of ChipOthers

--- src/platform/Ameba/AmebaConfig.h
+++ src/platform/Ameba/AmebaConfig.h
@@ -46,6 +46,7 @@ public:
     static const char kConfigNamespace_ChipFabric4[];
     static const char kConfigNamespace_ChipFabric5[];
     static const char kConfigNamespace_ChipACL[];
+    static const char kConfigNamespace_ChipGroupMessageCounters[];
     static const char kConfigNamespace_ChipAttributes[];
     static const char kConfigNamespace_ChipBindingTable[];
     static const char kConfigNamespace_ChipOTA[];
@@ -54,6 +55,7 @@ public:
     static const char kConfigNamespace_ChipSessionResumption[];
     static const char kConfigNamespace_ChipDeviceInfoProvider[];
     static const char kConfigNamespace_ChipOthers[];
+    static const char kConfigNamespace_ChipOthers2[];
 
     // Key definitions for well-known keys.
     static const Key kConfigKey_SerialNum;
