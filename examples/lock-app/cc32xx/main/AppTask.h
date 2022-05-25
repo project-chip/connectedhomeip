189fba67347042c6ad79641d70e43e1dec388392
Anu Biradar (local) <a-biradar@ti.com>
2022-05-25 16:38:14 -0500

    inital cc32xx commit
    Co-authored-by: Suyash Jain <s-jain@ti.com>
    Co-authored-by: Kobi Leibovitch <kobile@ti.com>

---
+++ examples/lock-app/cc32xx/main/AppTask.h
@@ -0,0 +1,78 @@
+/*
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2019 Google LLC.
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef APP_TASK_H
+#define APP_TASK_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
+#include "AppEvent.h"
+#include "BoltLockManager.h"
+
+#include <ti/drivers/apps/Button.h>
+
+class AppTask
+{
+public:
+    int StartAppTask();
+    static void AppTaskMain(void * pvParameter);
+
+    void PostLockActionRequest(int32_t aActor, BoltLockManager::Action_t aAction);
+    void PostEvent(const AppEvent * event);
+
+private:
+    friend AppTask & GetAppTask(void);
+
+    int Init();
+
+    // should this be done by BoltLock Manager? I don't want to unravel this spaghetti quite yet
+    static void ActionInitiated(BoltLockManager::Action_t aAction, int32_t aActor);
+    static void ActionCompleted(BoltLockManager::Action_t aAction);
+
+    void DispatchEvent(AppEvent * event);
+
+    static void ButtonRightEventHandler(Button_Handle handle, Button_EventMask events);
+
+    static void TimerEventHandler(void * p_context);
+
+    enum Function_t
+    {
+        kFunction_NoneSelected   = 0,
+        kFunction_SoftwareUpdate = 0,
+        kFunction_FactoryReset,
+
+        kFunction_Invalid
+    } Function;
+
+    Function_t mFunction;
+    bool mFunctionTimerActive;
+
+    static AppTask sAppTask;
+};
+
+inline AppTask & GetAppTask(void)
+{
+    return AppTask::sAppTask;
+}
+
+#endif // APP_TASK_H
