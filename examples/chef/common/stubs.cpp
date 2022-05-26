58042005f6b5a20dbf1c8ca590d0b8b2d6827c2b
Doug Ferraz <rochaferraz@google.com>
2022-05-25 18:12:04 -0700

    chg: restyle
    Change-Id: I1647422e5d378e7e12be9285044dc22ba6e90dfe

--- examples/chef/common/stubs.cpp
+++ examples/chef/common/stubs.cpp
@@ -16,19 +16,19 @@
  */
 
 #include <app-common/zap-generated/af-structs.h>
+#include <app-common/zap-generated/attribute-id.h>
+#include <app-common/zap-generated/attribute-type.h>
+#include <app-common/zap-generated/attributes/Accessors.h>
+#include <app-common/zap-generated/callback.h>
+#include <app-common/zap-generated/cluster-id.h>
 #include <app-common/zap-generated/cluster-objects.h>
+#include <app-common/zap-generated/command-id.h>
 #include <app-common/zap-generated/ids/Attributes.h>
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/AttributeAccessInterface.h>
 #include <app/util/attribute-storage.h>
 #include <lib/support/CodeUtils.h>
 #include <lib/support/logging/CHIPLogging.h>
-#include <app-common/zap-generated/attribute-id.h>
-#include <app-common/zap-generated/attribute-type.h>
-#include <app-common/zap-generated/attributes/Accessors.h>
-#include <app-common/zap-generated/callback.h>
-#include <app-common/zap-generated/cluster-id.h>
-#include <app-common/zap-generated/command-id.h>
 
 using namespace chip;
 using namespace chip::app;
