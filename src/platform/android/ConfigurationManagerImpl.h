e02366717d6d989b8faa63f6ac935ae0def53337
Arkadiusz Bałys <arkadiusz.balys@nordicsemi.no>
2022-05-27 11:19:32 +0200

    Created Device Instance Info Provider (#18767)
    The new Device Instance Info Provider provides API for
    accessing factory-provisioned device instance identifiers.
    
    All removed Configuration Manager method occurrences
    were replaced with a new Device Instance Info Provider API.
    Added LegacyDeviceInstanceInfoProvider implementation that
    preserves the existing ConfigurationManager methods.

--- src/platform/android/ConfigurationManagerImpl.h
+++ src/platform/android/ConfigurationManagerImpl.h
@@ -42,7 +42,6 @@ public:
     static ConfigurationManagerImpl & GetDefaultInstance();
     CHIP_ERROR GetProductId(uint16_t & productId) override;
     CHIP_ERROR GetProductName(char * buf, size_t bufSize) override;
-    CHIP_ERROR GetHardwareVersionString(char * buf, size_t bufSize) override;
     CHIP_ERROR GetSoftwareVersionString(char * buf, size_t bufSize) override;
     CHIP_ERROR GetSoftwareVersion(uint32_t & softwareVer) override;
     CHIP_ERROR GetPartNumber(char * buf, size_t bufSize) override;
