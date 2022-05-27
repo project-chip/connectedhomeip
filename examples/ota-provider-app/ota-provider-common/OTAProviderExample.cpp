b22f7c60bbd3fd66a8f4c91128d58eb938518dde
Carol Yang <clyang@apple.com>
2022-05-27 14:11:16 -0700

    [OTA] Only supply DelayActionTime field for busy status response

--- examples/ota-provider-app/ota-provider-common/OTAProviderExample.cpp
+++ examples/ota-provider-app/ota-provider-common/OTAProviderExample.cpp
@@ -296,9 +296,13 @@ void OTAProviderExample::SendQueryImageResponse(app::CommandHandler * commandObj
         }
     }
 
+    // Delay action time is only applicable when the provider is busy
+    if (mQueryImageStatus == OTAQueryStatus::kBusy) {
+        response.delayedActionTime.Emplace(mDelayedQueryActionTimeSec);
+    }
+
     // Set remaining fields common to all status types
     response.status = mQueryImageStatus;
-    response.delayedActionTime.Emplace(mDelayedQueryActionTimeSec);
     if (mUserConsentNeeded && requestorCanConsent)
     {
         response.userConsentNeeded.Emplace(true);
