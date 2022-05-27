2ffa5b451eb927ad855cdeec1e93fdd6675ef933
Douglas Rocha Ferraz <rochaferraz@google.com>
2022-05-26 21:02:03 -0400

    new: added MatterBridgedActionsPluginServerInitCallback stub and Brid… (#18829)
    * new: added MatterBridgedActionsPluginServerInitCallback stub and BridgedActionsAttrAccess to chef (same stubs as in all-clusters)
    
    * chg: restyle
    
    Change-Id: I1647422e5d378e7e12be9285044dc22ba6e90dfe
    
    * chg: removed custom cluster implementation from this PR and left only stub for MatterBridgedActionsPluginServerInitCallback

--- examples/chef/common/stubs.cpp
+++ examples/chef/common/stubs.cpp
@@ -1,9 +1,43 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <app-common/zap-generated/af-structs.h>
 #include <app-common/zap-generated/attribute-id.h>
 #include <app-common/zap-generated/attribute-type.h>
 #include <app-common/zap-generated/attributes/Accessors.h>
 #include <app-common/zap-generated/callback.h>
 #include <app-common/zap-generated/cluster-id.h>
+#include <app-common/zap-generated/cluster-objects.h>
 #include <app-common/zap-generated/command-id.h>
+#include <app-common/zap-generated/ids/Attributes.h>
+#include <app-common/zap-generated/ids/Clusters.h>
+#include <app/AttributeAccessInterface.h>
+#include <app/util/attribute-storage.h>
+#include <lib/support/CodeUtils.h>
+#include <lib/support/logging/CHIPLogging.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::BridgedActions::Attributes;
+
+// ************************************
+// Door Lock Stubs
+// ************************************
 
 using namespace ::chip;
 using namespace chip::app::Clusters::DoorLock;
@@ -18,3 +52,9 @@ bool emberAfPluginDoorLockOnDoorUnlockCommand(EndpointId endpointId, const chip:
 {
     return true;
 }
+
+// ************************************
+// Bridge Stubs
+// ************************************
+
+void MatterBridgedActionsPluginServerInitCallback(void) {}
