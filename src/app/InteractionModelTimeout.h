9660e3b7b234b2aa59684a88e3afa6267b3a3c07
Song Guo <songguo@google.com>
2022-05-29 16:46:07 +0800

    Address comments

--- src/app/InteractionModelTimeout.h
+++ src/app/InteractionModelTimeout.h
@@ -26,7 +26,7 @@ namespace app {
 
 // This is an expected upper bound of time-to-first-byte for IM transactions, the actual processing time should never exceed this
 // value unless specified elsewhere (e.g. async command handling for some clusters).
-static constexpr System::Clock::Timeout kExpectedIMProcessingTime = System::Clock::Seconds16(5);
+static constexpr System::Clock::Timeout kExpectedIMProcessingTime = System::Clock::Seconds16(2);
 
 } // namespace app
 } // namespace chip
