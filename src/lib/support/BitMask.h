9f81babc0c84ee8684cb92af4314be75fa45879a
Andrei Litvin <andy314@gmail.com>
2022-05-31 13:30:36 -0400

    Add a BitMask class that adds more functionality beyond bit flags

---
+++ src/lib/support/BitMask.h
@@ -0,0 +1,96 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <assert.h>
+
+#include <lib/support/BitFlags.h>
+
+namespace chip {
+
+/**
+ * Stores bit masks and flags in a more typesafe manner.
+ *
+ * Extends BitFlags for boolean flags and also adds support for bit-mask set options.
+ *
+ */
+template <typename FlagsEnum, typename StorageType = typename std::underlying_type_t<FlagsEnum>>
+class BitMask : public BitFlags<FlagsEnum, StorageType>
+{
+public:
+    /**
+     * GetField to value via a mask shifting.
+     *
+     * @param mask      Typed flag(s) to used as mask, flag(s) must be contiguous. Any flags not in @a v are unaffected.
+     * @returns         Value from the underlying field/mask
+     */
+    constexpr BitFlags & SetField(FlagsEnum mask, IntegerType value)
+    {
+        IntegerType bitMask = static_cast<IntegerType>(mask);
+        IntegerType shift   = GetShiftToFirstSetBit(bitMask);
+
+        // Value should be fully contained within the shift mask.
+        assert((value & (mask >> shift)) == value);
+
+        // Clear bits overlayed by the mask
+        mValue = static_cast<IntegerType>(mValue & ~bitMask);
+        // Set the right bits
+        mValue |= static_cast<IntegerType>(bitMask & (value << shift));
+
+        return *this;
+    }
+
+    /**
+     * GetField to value via a mask shifting.
+     *
+     * @param mask      Typed flag(s) to used as mask, flag(s) must be contiguous. Any flags not in @a v are unaffected.
+     * @returns         Value from the underlying field/mask
+     */
+    IntegerType GetField(FlagsEnum mask) const
+    {
+        {
+            IntegerType bitMask = static_cast<IntegerType>(mask);
+            IntegerType shift   = GetShiftToFirstSetBit(bitMask);
+
+            // Forward the right bits
+            return static_cast<IntegerType>(((mValue & bitMask) >> shift));
+        }
+    }
+
+protected:
+    /// Get the shift amount to reach the first non-zero bit in a bitmask
+    ///
+    /// Examples:
+    ///    GetShiftToFirstSetBit(0b0000001) == 0
+    ///    GetShiftToFirstSetBit(0b0011100) == 2
+    ///    GetShiftToFirstSetBit(0b0110000) == 4
+    ///
+    /// Note: This does NOT validate if the given mask is a valid mask. So:
+    ///    GetShiftToFirstSetBit(0b0100010) == 1
+    ///    GetShiftToFirstSetBit(0b010110100) == 2
+    static constexpr IntegerType GetShiftToFirstSetBit(IntegerType bitMask)
+    {
+        IntegerType count = 0;
+        while (!(bitMask & 0x1) && (count < std::numeric_limits<IntegerType>::max()))
+        {
+            bitMask = static_cast<IntegerType>(bitMask >> 1);
+            count++;
+        }
+        return count;
+    }
+};
