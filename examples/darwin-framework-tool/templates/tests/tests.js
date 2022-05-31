3a0ed879e0bc95af42e9b4f934c4b3abe0e59b1f
kowsisoundhar12 <kowsalyadevi@graniteriverlabs.in>
2022-05-31 12:44:18 +0530

    Added upadted tests.js

--- examples/darwin-framework-tool/templates/tests/tests.js
+++ examples/darwin-framework-tool/templates/tests/tests.js
@@ -48,7 +48,7 @@ function getTests() {
   // TODO: Test_TC_DIAG_TH_NW_1_1 does not work on Darwin for now.
   tests.disable('Test_TC_DIAG_TH_NW_2_4');
 
-  // TODO: Test_TC_DIAG_TH_NW_1_2 does not work on Darwin for now.
+  // TODO: Test_TC_DIAG_TH_NW_2_1 does not work on Darwin for now.
   tests.disable('Test_TC_DIAG_TH_NW_2_1');
 
   // TODO: Test_TC_DIAG_TH_NW_2_2 does not work on Darwin for now.
