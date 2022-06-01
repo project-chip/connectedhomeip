0d07d074a6b623b7ef27117662feeb2c1a0954bc
Sharad Patil <sharad.patil@silabs.com>
2022-06-01 14:34:03 +0530

    Enable IPv6 for WiFi (rs911x)
    Added build flag for Enabling and disabling IPv4 and IPv6
    
    Signed-off-by: Sharad Patil <sharad.patil@silabs.com>

--- src/platform/EFR32/CHIPDevicePlatformConfig.h
+++ src/platform/EFR32/CHIPDevicePlatformConfig.h
@@ -48,6 +48,14 @@
 
 #define CHIP_DEVICE_CONFIG_ENABLE_CHIP_TIME_SERVICE_TIME_SYNC 0
 
+#if defined(WIFI_IPV4_DISABLED)
+#define CHIP_DEVICE_CONFIG_ENABLE_IPV4 0
+#define CHIP_DEVICE_CONFIG_ENABLE_IPV6 1
+#else
+#define CHIP_DEVICE_CONFIG_ENABLE_IPV4 1
+#define CHIP_DEVICE_CONFIG_ENABLE_IPV6 1
+#endif
+
 // ========== Platform-specific Configuration =========
 
 // These are configuration options that are unique to the EFR32 platform.
