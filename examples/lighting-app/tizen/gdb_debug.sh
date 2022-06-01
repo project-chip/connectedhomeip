1b01bf0885a01b4afadf8566fed3a14fc8e7c7b4
Restyled.io <commits@restyled.io>
2022-06-01 10:04:33 +0000

    Restyled by shellharden

--- examples/lighting-app/tizen/gdb_debug.sh
+++ examples/lighting-app/tizen/gdb_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SDB="${TIZEN_SDK_ROOT}/tools/sdb"
+SDB="$TIZEN_SDK_ROOT/tools/sdb"
 APP_NAME='org.tizen.matter.example.lighting'
 
 if [[ $($SDB devices | wc -l) -le 1 ]]; then
@@ -8,16 +8,16 @@ if [[ $($SDB devices | wc -l) -le 1 ]]; then
     exit 1
 fi
 
-$SDB root on
+"$SDB" root on
 re='^[0-9]+$'
-lauch_out=$($SDB shell app_launcher -s org.tizen.matter.example.lighting vendor-id 65521 product-id 32769 discriminator 42 passcode 1234)
+lauch_out=$("$SDB" shell app_launcher -s org.tizen.matter.example.lighting vendor-id 65521 product-id 32769 discriminator 42 passcode 1234)
 APP_PID=$(echo "$lauch_out" | cut -d ' ' -f6)
 re='^[0-9]+$'
-if ! [[ $APP_PID =~ $re ]]; then
+if ! [[ "$APP_PID" =~ "$re" ]]; then
     echo "Wrong pid number" >&2
     echo "Launch command out:"
     echo "$lauch_out"
     exit 1
 fi
 
-$SDB shell "gdbserver :9999 --attach $APP_PID"
+"$SDB" shell "gdbserver :9999 --attach $APP_PID"
