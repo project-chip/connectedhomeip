053a2a7285b533ae2bb6971b8dcdfcfcae796486
Yufeng Wang <yufengwang@google.com>
2022-06-06 14:16:41 -0700

    Update src/app/clusters/general-diagnostics-server/general-diagnostics-server.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/clusters/general-diagnostics-server/general-diagnostics-server.cpp
+++ src/app/clusters/general-diagnostics-server/general-diagnostics-server.cpp
@@ -274,8 +274,7 @@ void Server::OnHardwareFaultsDetect(GeneralFaults<kMaxHardwareFaults> & previous
 
         // Record HardwareFault event
         EventNumber eventNumber;
-        DataModel::List<const HardwareFaultType> currentList =
-            DataModel::List<const HardwareFaultType>(reinterpret_cast<const HardwareFaultType *>(current.data()), current.size());
+        DataModel::List<const HardwareFaultType> currentList(reinterpret_cast<const HardwareFaultType *>(current.data()), current.size());
         DataModel::List<const HardwareFaultType> previousList =
             DataModel::List<const HardwareFaultType>(reinterpret_cast<const HardwareFaultType *>(previous.data()), previous.size());
         Events::HardwareFaultChange::Type event{ currentList, previousList };
