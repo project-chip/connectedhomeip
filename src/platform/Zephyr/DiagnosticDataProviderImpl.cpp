a574744233ac3b0832e809656c9f64f11bb8846d
Qingsong Gou <gouqs@hotmail.com>
2022-06-07 16:11:44 +0800

    make platform zephyr stand alone

--- src/platform/Zephyr/DiagnosticDataProviderImpl.cpp
+++ src/platform/Zephyr/DiagnosticDataProviderImpl.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2021 Project CHIP Authors
+ *    Copyright (c) 2022 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 /**
  *    @file
  *          Provides an implementation of the DiagnosticDataProvider object
- *          for Zephy platform.
+ *          for Zephyr platform.
  */
 
 #include <platform/internal/CHIPDeviceLayerInternal.h>
