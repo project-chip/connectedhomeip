8c5ddeb50b20ab062cc17b8b6eada2a34fce4044
Andrei Litvin <andy314@gmail.com>
2022-06-07 16:36:57 -0400

    Restyle

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -434,23 +434,23 @@ EmberStatus emberAfSendDefaultResponse(const EmberAfClusterCommand * cmd, EmberA
 
 void emberAfCopyInt16u(uint8_t * data, uint16_t index, uint16_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
 }
 
 void emberAfCopyInt24u(uint8_t * data, uint16_t index, uint32_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
-    data[index + 2] = (uint8_t) (((x) >> 16) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
+    data[index + 2] = (uint8_t)(((x) >> 16) & 0xFF);
 }
 
 void emberAfCopyInt32u(uint8_t * data, uint16_t index, uint32_t x)
 {
-    data[index]     = (uint8_t) (((x)) & 0xFF);
-    data[index + 1] = (uint8_t) (((x) >> 8) & 0xFF);
-    data[index + 2] = (uint8_t) (((x) >> 16) & 0xFF);
-    data[index + 3] = (uint8_t) (((x) >> 24) & 0xFF);
+    data[index]     = (uint8_t)(((x)) & 0xFF);
+    data[index + 1] = (uint8_t)(((x) >> 8) & 0xFF);
+    data[index + 2] = (uint8_t)(((x) >> 16) & 0xFF);
+    data[index + 3] = (uint8_t)(((x) >> 24) & 0xFF);
 }
 
 void emberAfCopyString(uint8_t * dest, const uint8_t * src, size_t size)
