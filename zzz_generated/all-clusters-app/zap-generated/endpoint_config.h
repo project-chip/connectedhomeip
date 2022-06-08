c43c2a52b87a471a479f06edbe43542007ae4fa3
Damian Krolik <damian.krolik@nordicsemi.no>
2022-06-08 13:36:59 +0200

    [window-app] Set valid Type for Window Covering cluster
    If Feature Map of Window Covering cluster has both LF and TL
    bits set, Type should be either 6 or 8. Initialize the
    attribute to 8 in window-app and all-clusters-app so that
    they can pass certification tests.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- zzz_generated/all-clusters-app/zap-generated/endpoint_config.h
+++ zzz_generated/all-clusters-app/zap-generated/endpoint_config.h
@@ -947,7 +947,7 @@
             { 0x0000FFFD, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(6) },       /* ClusterRevision */                             \
                                                                                                                                    \
             /* Endpoint: 1, Cluster: Window Covering (server) */                                                                   \
-            { 0x00000000, ZAP_TYPE(ENUM8), 1, 0, ZAP_SIMPLE_DEFAULT(0x00) },    /* Type */                                         \
+            { 0x00000000, ZAP_TYPE(ENUM8), 1, 0, ZAP_SIMPLE_DEFAULT(0x08) },    /* Type */                                         \
             { 0x00000001, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(0xFFFF) }, /* PhysicalClosedLimitLift */                      \
             { 0x00000002, ZAP_TYPE(INT16U), 2, 0, ZAP_SIMPLE_DEFAULT(0xFFFF) }, /* PhysicalClosedLimitTilt */                      \
             { 0x00000003, ZAP_TYPE(INT16U), 2, ZAP_ATTRIBUTE_MASK(TOKENIZE) | ZAP_ATTRIBUTE_MASK(NULLABLE),                        \
