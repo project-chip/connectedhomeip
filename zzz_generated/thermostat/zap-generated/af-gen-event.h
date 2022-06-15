9a14ba919556fbcd1bb96580bc91ffbd6b4dd1ee
Restyled.io <commits@restyled.io>
2022-06-15 18:27:57 +0000

    Restyled by clang-format

--- zzz_generated/thermostat/zap-generated/af-gen-event.h
+++ zzz_generated/thermostat/zap-generated/af-gen-event.h
@@ -66,19 +66,17 @@
 
 // EmberEventData structs used to populate the EmberEventData table
 #define EMBER_AF_GENERATED_EVENTS                                                                                                  \
-        { &emberAfBarrierControlClusterServerTickCallbackControl1,                                                                 \
-          emberAfBarrierControlClusterServerTickCallbackWrapperFunction1 },
+    { &emberAfBarrierControlClusterServerTickCallbackControl1, emberAfBarrierControlClusterServerTickCallbackWrapperFunction1 },
 
 #define EMBER_AF_GENERATED_EVENT_STRINGS                                                                                           \
-    "Barrier Control Cluster Server EP 1",                                                    \
-        "Color Control Cluster Server Plugin HueSatTransition", "Color Control Cluster Server Plugin TempTransition",              \
-        "Color Control Cluster Server Plugin XyTransition", "Door Lock Server Cluster Plugin Lockout",                             \
-        "Door Lock Server Cluster Plugin Relock", "IAS Zone Server Plugin ManageQueue",
+    "Barrier Control Cluster Server EP 1", "Color Control Cluster Server Plugin HueSatTransition",                                 \
+        "Color Control Cluster Server Plugin TempTransition", "Color Control Cluster Server Plugin XyTransition",                  \
+        "Door Lock Server Cluster Plugin Lockout", "Door Lock Server Cluster Plugin Relock", "IAS Zone Server Plugin ManageQueue",
 
 // The length of the event context table used to track and retrieve cluster events
 #define EMBER_AF_EVENT_CONTEXT_LENGTH 2
 
 // EmberAfEventContext structs used to populate the EmberAfEventContext table
 #define EMBER_AF_GENERATED_EVENT_CONTEXT                                                                                           \
-        { 0x1, 0x103, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfBarrierControlClusterServerTickCallbackControl1 },
+    { 0x1, 0x103, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfBarrierControlClusterServerTickCallbackControl1 },
 #endif // __AF_GEN_EVENT__
