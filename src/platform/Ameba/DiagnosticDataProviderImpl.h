d1ae086f40e7cdad448e1aeadba56f133086eed4
pankore <86098180+pankore@users.noreply.github.com>
2022-06-16 21:25:16 +0800

    [Ameba] SoftwareDiagnostic update (#19147)
    * [DGSW] Implement GetThreadMetrics and ResetWaterMark
    
    * [DGSW] Align ThreadMetrics struct with spec

--- src/platform/Ameba/DiagnosticDataProviderImpl.h
+++ src/platform/Ameba/DiagnosticDataProviderImpl.h
@@ -40,6 +40,9 @@ public:
     CHIP_ERROR GetCurrentHeapFree(uint64_t & currentHeapFree) override;
     CHIP_ERROR GetCurrentHeapUsed(uint64_t & currentHeapUsed) override;
     CHIP_ERROR GetCurrentHeapHighWatermark(uint64_t & currentHeapHighWatermark) override;
+    CHIP_ERROR ResetWatermarks() override;
+    CHIP_ERROR GetThreadMetrics(ThreadMetrics ** threadMetricsOut) override;
+    void ReleaseThreadMetrics(ThreadMetrics * threadMetrics) override;
 
     CHIP_ERROR GetRebootCount(uint16_t & rebootCount) override;
     CHIP_ERROR GetUpTime(uint64_t & upTime) override;
