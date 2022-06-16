6b1d319fecf5070507c9b6ac4f6a0a7069041f06
Terence Hampson <thampson@google.com>
2022-06-16 16:36:26 +0000

    Rename CONFIG_IM_BUILD_FOR_UNIT_TEST to CONFIG_BUILD_FOR_UNIT_TEST
    CONFIG_IM_BUILD_FOR_UNIT_TEST started getting used in other unit tests
    as well. A more appropriate name is CONFIG_IM_BUILD_FOR_UNIT_TEST.

--- src/lib/core/CHIPConfig.h
+++ src/lib/core/CHIPConfig.h
@@ -878,13 +878,13 @@ extern const char CHIP_NON_PRODUCTION_MARKER[];
 #endif
 
 /**
- * @def CONFIG_IM_BUILD_FOR_UNIT_TEST
+ * @def CONFIG_BUILD_FOR_UNIT_TEST
  *
- * @brief Defines whether we're currently building the IM for unit testing, which enables a set of features
+ * @brief Defines whether we're currently building for unit testing, which enables a set of features
  *        that are only utilized in those tests.
  */
-#ifndef CONFIG_IM_BUILD_FOR_UNIT_TEST
-#define CONFIG_IM_BUILD_FOR_UNIT_TEST 0
+#ifndef CONFIG_BUILD_FOR_UNIT_TEST
+#define CONFIG_BUILD_FOR_UNIT_TEST 0
 #endif
 
 /**
