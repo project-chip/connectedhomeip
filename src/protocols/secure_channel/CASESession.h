e64de1166537d6ce0fd3ac62d2156e5eca973730
Terence Hampson <thampson@google.com>
2022-06-17 13:40:47 +0000

    Small refactor to TestCASESession to bring it inline with other unit test

--- src/protocols/secure_channel/CASESession.h
+++ src/protocols/secure_channel/CASESession.h
@@ -188,7 +188,7 @@ public:
     void Clear();
 
 private:
-    friend class CASESessionForTest;
+    friend class TestCASESession;
     enum class State : uint8_t
     {
         kInitialized       = 0,
