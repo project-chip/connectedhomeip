3cde454c7fc19c75de0c1ef8f45af58ecc5f5ed7
Restyled.io <commits@restyled.io>
2022-06-17 19:17:25 +0000

    Restyled by autopep8

--- examples/chef/hashtest.py
+++ examples/chef/hashtest.py
@@ -3,17 +3,18 @@ import hashlib
 
 
 def gen_hash_from_dir(path: str) -> str:
-  file_list = []
-  for dirpath, dirnames, files in os.walk(path):
-    for filename in files:
-      file_list.append(os.path.join(dirpath, filename))
-  file_list = sorted(file_list)
-  md5hash = hashlib.md5()
-  for filename in file_list:
-    with open(filename, "rb") as f:
-      md5hash.update(f.read())
-  md5hash.hexdigest()
-  return md5hash.hexdigest()
+    file_list = []
+    for dirpath, dirnames, files in os.walk(path):
+        for filename in files:
+            file_list.append(os.path.join(dirpath, filename))
+    file_list = sorted(file_list)
+    md5hash = hashlib.md5()
+    for filename in file_list:
+        with open(filename, "rb") as f:
+            md5hash.update(f.read())
+    md5hash.hexdigest()
+    return md5hash.hexdigest()
+
 
 r = gen_hash_from_dir("zzz_generated/rootnode_contactsensor_DreXRHtsq9")
 
