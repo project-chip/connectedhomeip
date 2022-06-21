6a5f81cdb4025e865d818792deeac1083e589343
Austin Bozowski <bozowski@google.com>
2022-06-21 05:05:51 +0000

    Remove chef exclusion from regen

--- scripts/tools/zap_regen_all.py
+++ scripts/tools/zap_regen_all.py
@@ -115,8 +115,8 @@ def getGlobalTemplatesTargets():
                 ZAPGenerateTarget(filepath, output_dir=output_dir, template=template))
             continue
 
-        if example_name == "chef":
-            continue
+        #if example_name == "chef":
+        #    continue
 
         logging.info("Found example %s (via %s)" %
                      (example_name, str(filepath)))
