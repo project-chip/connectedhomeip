6bf269c72fe8e5469d3a1788c01df90e8ca2b9b1
Amit Jain <amitnj@amazon.com>
2022-06-23 14:50:58 -0700

    Add functionality to read attributes from content apps

--- examples/tv-app/android/java/src/com/matter/tv/server/tvapp/ContentAppEndpointManager.java
+++ examples/tv-app/android/java/src/com/matter/tv/server/tvapp/ContentAppEndpointManager.java
@@ -3,4 +3,6 @@ package com.matter.tv.server.tvapp;
 public interface ContentAppEndpointManager {
 
   public String sendCommand(int endpointId, int clusterId, int commandId, String commandPayload);
+
+  public String readAttribute(int endpointId, int clusterId, int attributeId);
 }
