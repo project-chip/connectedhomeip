8c5435d001d91a69e59637ae4c0ea656cf230e20
Austin Bozowski <bozowski@google.com>
2022-06-22 18:15:16 -0700

    Make Chef CI use existing zzz_generated and complete Chef CD for existing platforms (#19478)
    * Squashed commit of the following:
    
    commit b5d59200c966bb74656c25d95ec0a09387eee831
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 18:17:34 2022 +0000
    
        [no ci] comment
    
    commit 7c5fb08d534f80c0356e8e9e26bc5bccc1e6d38e
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 18:15:37 2022 +0000
    
        [no ci] Make border nice
    
    commit d67a7bc58efe6aa865eb45023ee9baa2ae999608
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 18:09:29 2022 +0000
    
        [no ci] restyle
    
    commit 5783829bce34978942662910a92bdd9c866cb75f
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 17:48:39 2022 +0000
    
        Add error handling to build all
    
    commit 083b072b7045ad00eb8ce2e1ccb4a88e039be315
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 17:15:33 2022 +0000
    
        [no ci] update build image version
    
    commit 48805a8f41dd46c183e80645d019183a1b46e21b
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 08:20:49 2022 +0000
    
        [no ci] minor
    
    commit 3cae95cdc4e3ec7f94e2b3f0360066ed076a748b
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 08:15:22 2022 +0000
    
        [no ci] Minor
    
    commit df929b2e1426c3a654e1d371f6d7745ae668a9ce
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 07:37:49 2022 +0000
    
        Minor
    
    commit 759ddb05445a94ac5d91c54597ab7a42f82201e6
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 07:28:36 2022 +0000
    
        Minor
    
    commit d9ca2a642d195ebed1e9283c693d77fc337a486d
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 07:22:03 2022 +0000
    
        Style
    
    commit dbb9b30fad8bed9e00bc32db6af7c84822269913
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 06:50:11 2022 +0000
    
        Style
    
    commit 3daff46c591239227db63ee6f6fd54dd19888144
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Fri Jun 10 02:58:43 2022 +0000
    
        nrf bundle
    
    commit 7d38eb46b9b7febc94f52eea9059bedc85e58947
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Wed Jun 8 16:34:57 2022 +0000
    
        Linux bundle
    
    commit fd5689e194cd1588fe41a8dce1037ca800e098f6
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Wed Jun 8 09:08:42 2022 +0000
    
        Make esp32 generic
    
    commit 1af3a2ea7329cc6a98b7ea46d40ab302a00d1706
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Tue Jun 7 08:35:52 2022 +0000
    
        Missed comment
    
    commit 5f7c41c9a6168dcc980612fd9bd120c2c3fb7831
    Author: Austin Bozowski <bozowski@google.com>
    Date:   Tue Jun 7 08:32:07 2022 +0000
    
        Example bundle function
    
    * Bundle .matter, metadata & ensure .matter commit
    
    * Add partial check for .matter regen
    
    * Restyle and spelling
    
    * Address comments
    
    * Remove reflection
    
    * Generate zzz, .MATTERMD5 should go away in future
    
    * Temp del workflows
    
    * Remove chef exclusion from regen
    
    * Make regen all put chef exs in individual dirs
    
    * Remove validate zzz
    
    * Remove unused code from chef, change use_zzz
    
    * Skip util test files in gen all
    
    * Commit chef in root zzz
    
    * Delete chef zzz
    
    * Clean chef .matter files
    
    * Add post build for chef
    
    * Change .matter bundle rc to oot zzz
    
    * Generated files
    
    * Restore workflows
    
    * Change open to with
    
    * Restyle

--- scripts/tools/zap_regen_all.py
+++ scripts/tools/zap_regen_all.py
@@ -66,6 +66,13 @@ class ZAPGenerateTarget:
         cmd = self.build_cmd()
         logging.info("Generating target: %s" % " ".join(cmd))
         subprocess.check_call(cmd)
+        if "chef" in self.zap_config:
+            af_gen_event = os.path.join(self.output_dir, "af-gen-event.h")
+            with open(af_gen_event, "w+"):  # Empty file needed for linux
+                pass
+            idl_path = self.zap_config.replace(".zap", ".matter")
+            target_path = os.path.join(self.output_dir, os.path.basename(idl_path))
+            os.rename(idl_path, target_path)
 
 
 def checkPythonVersion():
@@ -116,7 +123,9 @@ def getGlobalTemplatesTargets():
             continue
 
         if example_name == "chef":
-            continue
+            if os.path.join("chef", "devices") not in str(filepath):
+                continue
+            example_name = "chef-"+os.path.basename(filepath)[:-len(".zap")]
 
         logging.info("Found example %s (via %s)" %
                      (example_name, str(filepath)))
