7cd8316c2affc9a0c3489794f813c161af076cb1
ATmobica <artur.tynecki@mobica.com>
2022-06-27 07:49:17 +0000

    Open IoT SDK shell example
    Improve shell example to link shell library and execute shell commands
    Improve mbedtls configuration
    
    Signed-off-by: ATmobica <artur.tynecki@mobica.com>

--- examples/shell/openiotsdk/main/main.cpp
+++ examples/shell/openiotsdk/main/main.cpp
@@ -15,59 +15,117 @@
  *    limitations under the License.
  */
 
-#include <lib/core/CHIPConfig.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "cmsis_os2.h"
+#include <lib/core/CHIPConfig.h>
+#include <lib/shell/Engine.h>
+#include <lib/support/CHIPMem.h>
+#include <lib/support/logging/CHIPLogging.h>
+#include <platform/CHIPDeviceLayer.h>
 
-static void demo_thread(void * argument)
+#include <ChipShellCollection.h>
+
+#include "cmsis_os2.h"
+#include "mbedtls/platform.h"
+
+using namespace ::chip;
+using namespace ::chip::Shell;
+using namespace ::chip::DeviceLayer;
+using namespace ::chip::Logging::Platform;
+
+static void app_thread(void * argument)
 {
-    for (int i = 0; i < 5; ++i)
+    // Initialize the default streamer that was linked.
+    auto ret = Engine::Root().Init();
+    if (ret)
     {
-        printf("Message from demo thread\r\n");
-        osDelay(500);
+        ChipLogError(Shell, "Streamer initialization failed [%d]", ret);
+        return;
     }
+
+    cmd_misc_init();
+
+    ChipLogProgress(Shell, "Open IoT SDK shell example application run");
+
+#if NDEBUG
+    chip::Logging::SetLogFilter(chip::Logging::LogCategory::kLogCategory_None);
+#endif
+
+    Engine::Root().RunMainLoop();
 }
 
 int main()
 {
-    printf("CHIP demo example\r\n");
-    printf("CHIP port: %d\r\n", CHIP_PORT);
+    int ret        = 0;
+    CHIP_ERROR err = CHIP_NO_ERROR;
 
-    osStatus_t ret = osKernelInitialize();
+    osThreadId_t appThread;
+    osKernelState_t state;
+
+    ChipLogProgress(Shell, "Open IoT SDK shell example application start");
+
+    ret = mbedtls_platform_setup(NULL);
+    if (ret)
+    {
+        ChipLogError(Shell, "Mbed TLS platform initialization failed: %d", ret);
+        return EXIT_FAILURE;
+    }
+
+    ret = osKernelInitialize();
     if (ret != osOK)
     {
-        printf("osKernelInitialize failed: %d\r\n", ret);
+        ChipLogError(Shell, "osKernelInitialize failed: %d", ret);
+        return EXIT_FAILURE;
+    }
+
+    err = chip::Platform::MemoryInit();
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Memory initialization failed: %s", err.AsString());
+        return EXIT_FAILURE;
+    }
+
+    err = PlatformMgr().InitChipStack();
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Chip stack initialization failed: %s", err.AsString());
+        return EXIT_FAILURE;
+    }
+
+    err = PlatformMgr().StartEventLoopTask();
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Shell, "Chip stack start failed: %s", err.AsString());
         return EXIT_FAILURE;
     }
 
     static const osThreadAttr_t thread_attr = {
-        .stack_size = 1024 // Allocate our threads with enough stack for printf
+        .stack_size = 8 * 1024 // Allocate enough stack for app thread
     };
-    osThreadId_t thread = osThreadNew(demo_thread, NULL, &thread_attr);
-    if (thread == NULL)
+
+    appThread = osThreadNew(app_thread, NULL, &thread_attr);
+    if (appThread == NULL)
     {
-        printf("Failed to create thread\r\n");
+        ChipLogError(Shell, "Failed to create app thread");
         return EXIT_FAILURE;
     }
 
-    osKernelState_t state = osKernelGetState();
+    state = osKernelGetState();
     if (state == osKernelReady)
     {
-        printf("Starting kernel\r\n");
         ret = osKernelStart();
         if (ret != osOK)
         {
-            printf("Failed to start kernel: %d\r\n", ret);
+            ChipLogError(Shell, "Failed to start kernel: %d", ret);
             return EXIT_FAILURE;
         }
     }
     else
     {
-        printf("Kernel not ready: %d\r\n", state);
+        ChipLogError(Shell, "Kernel not ready: %d", state);
         return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
