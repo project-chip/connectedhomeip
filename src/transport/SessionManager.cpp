cd6e9affc056fe8148cc6ddf5e867e12eee8c636
Andrei Litvin <andy314@gmail.com>
2022-06-29 15:07:51 -0400

    Log error on bad packets instead of silent return

--- src/transport/SessionManager.cpp
+++ src/transport/SessionManager.cpp
@@ -430,7 +430,12 @@ void SessionManager::OnMessageReceived(const PeerAddress & peerAddress, System::
     CHIP_TRACE_PREPARED_MESSAGE_RECEIVED(&peerAddress, &msg);
     PacketHeader packetHeader;
 
-    ReturnOnFailure(packetHeader.DecodeAndConsume(msg));
+    CHIP_ERROR err = packetHeader.DecodeAndConsume(msg);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(Inet, "Malformed message received: %" CHIP_ERROR_FORMAT, err.Format());
+        return;
+    }
 
     if (packetHeader.IsEncrypted())
     {
