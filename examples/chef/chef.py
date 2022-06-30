d6c351928977b0ec43b69386b822558b1de94535
Austin Bozowski <bozowski@google.com>
2022-06-30 09:45:18 -0700

    Move chef .matter files back to /chef/devices (#20142)
    * Remove matter files from root zzz
    
    * Move .matter regen for chef
    
    * Update bundle for changed .matter location

--- examples/chef/chef.py
+++ examples/chef/chef.py
@@ -150,10 +150,7 @@ def bundle(platform: str, device_name: str) -> None:
         flush_print(f"No bundle function for {platform}!")
         exit(1)
     flush_print(f"Copying {matter_file}")
-    src_item = os.path.join(_REPO_BASE_PATH,
-                            "zzz_generated",
-                            "chef-"+device_name,
-                            "zap-generated",
+    src_item = os.path.join(_DEVICE_FOLDER,
                             matter_file)
     dest_item = os.path.join(_CD_STAGING_DIR, matter_file)
     shutil.copy(src_item, dest_item)
