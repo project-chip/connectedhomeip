85f76f21716c945467ff9f9aca0827271c935c10
Andrei Litvin <andy314@gmail.com>
2022-06-30 16:04:28 -0400

    Pull latest pigweed version. (#20158)
    * Pull latest pigweed version.
    
    ```
    cd third_party/pigweed/repo
    git fetch https://github.com/google/pigweed.git main
    git checkout FETCH_HEAD
    ```
    
    * change std::span/std::as_bytes to pw::span/pw::as_bytes to make pigweed polyfill happy
    
    * Switch include: from span to pw_span.h
    
    * More replacing of std::span with pw::span
    
    * Restyle
    
    * Set pw_span_ENABLE_STD_SPAN_POLYFILL to false for anything that sets a sys_io_BACKEND
    
    * fix amebad-pigweed builds
    
    * Ensure import is done for polyfill settings for Amebad
    
    * Fix ESP32 builds with pw_span
    
    * Restyle

--- examples/platform/bouffalolab/bl602/PigweedLogger.cpp
+++ examples/platform/bouffalolab/bl602/PigweedLogger.cpp
@@ -31,9 +31,9 @@
 #include <pw_stream/sys_io_stream.h>
 #include <pw_sys_io_efr32/init.h>
 
+#include "pw_span/span.h"
 #include <cassert>
 #include <cstdint>
-#include <span>
 #include <string_view>
 
 namespace PigweedLogger {
@@ -52,7 +52,7 @@ static char sWriteBuffer[kWriteBufferSize];
 
 static void send(void)
 {
-    pw::hdlc::WriteUIFrame(kLogHdlcAddress, std::as_bytes(std::span(sWriteBuffer, sWriteBufferPos)), sWriter);
+    pw::hdlc::WriteUIFrame(kLogHdlcAddress, pw::as_bytes(pw::span(sWriteBuffer, sWriteBufferPos)), sWriter);
     sWriteBufferPos = 0;
 }
 
