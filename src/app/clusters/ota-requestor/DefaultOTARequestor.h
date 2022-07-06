b726656895695ea0ed5fa9751d5974b3cc8e33b3
Damian Królik <66667989+Damian-Nordic@users.noreply.github.com>
2022-07-06 04:27:44 +0200

    [ota] Fix exchange context leak in OTA requestor (#20304)
    * [ota] Fix exchange context leak
    
    It turns out that Exchange Context allocated for BDX
    transfer is not released on completion or connection abort.
    
    It is not seen in a happy path that results in applying
    and rebooting into a new firmware, but may lead to the
    exchange leak when the transfer is interrupted.
    
    Furthermore, if an exchange is never released, a Sleepy End
    Device never returns to the idle mode, needlessly draining
    the battery.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/app/clusters/ota-requestor/DefaultOTARequestor.h
+++ src/app/clusters/ota-requestor/DefaultOTARequestor.h
@@ -175,6 +175,13 @@ private:
             mDownloader  = downloader;
         }
 
+        void Reset()
+        {
+            VerifyOrReturn(mExchangeCtx != nullptr);
+            mExchangeCtx->Close();
+            mExchangeCtx = nullptr;
+        }
+
     private:
         chip::Messaging::ExchangeContext * mExchangeCtx;
         chip::BDXDownloader * mDownloader;
@@ -292,13 +299,12 @@ private:
      */
     static void OnCommissioningCompleteRequestor(const DeviceLayer::ChipDeviceEvent * event, intptr_t arg);
 
-    OTARequestorStorage * mStorage            = nullptr;
-    OTARequestorDriver * mOtaRequestorDriver  = nullptr;
-    CASESessionManager * mCASESessionManager  = nullptr;
-    OnConnectedAction mOnConnectedAction      = kQueryImage;
-    Messaging::ExchangeContext * mExchangeCtx = nullptr;
-    BDXDownloader * mBdxDownloader            = nullptr; // TODO: this should be OTADownloader
-    BDXMessenger mBdxMessenger;                          // TODO: ideally this is held by the application
+    OTARequestorStorage * mStorage           = nullptr;
+    OTARequestorDriver * mOtaRequestorDriver = nullptr;
+    CASESessionManager * mCASESessionManager = nullptr;
+    OnConnectedAction mOnConnectedAction     = kQueryImage;
+    BDXDownloader * mBdxDownloader           = nullptr; // TODO: this should be OTADownloader
+    BDXMessenger mBdxMessenger;                         // TODO: ideally this is held by the application
     uint8_t mUpdateTokenBuffer[kMaxUpdateTokenLen];
     ByteSpan mUpdateToken;
     uint32_t mCurrentVersion = 0;
