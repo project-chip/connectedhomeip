5bc59e913000e3d95e4ee4308b5559c3b262132e
AlvinHsiao <89836894+AlvinHsiao@users.noreply.github.com>
2022-07-06 10:38:42 +0800

    [Infineon] Lock-app updates, adding multiple users and credentials for CYW30739 (#20034)
    * [Infineon] Lock-app updates, adding multiple users and credentials.
    
        * Fix mCredentials, fix NVM flash saving
    
        * restyle
    
        * cleanup
    
        * remove unused array
    
        * remove TotalCredentials key
    
        * Check mMaxUsers against DOOR_LOCK_MAX_USERS
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- examples/lock-app/cyw30739/include/chip_lock.h
+++ examples/lock-app/cyw30739/include/chip_lock.h
@@ -23,6 +23,7 @@
 
 // Application-defined error codes in the CHIP_ERROR space
 #define APP_ERROR_INIT_TIMER_FAILED CHIP_ERROR_NO_MEMORY // should use CHIP_APPLICATION_ERROR when it's ready
+#define APP_ERROR_ALLOCATION_FAILED CHIP_APPLICATION_ERROR(0x07)
 
 // Application configurations
 #define ACTUATOR_MOVEMENT_PERIOS_MS 1000
