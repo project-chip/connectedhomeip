dcf50d2397787826e6792b50731bb1e4a55405ab
krypton36 <jvillasenor@apple.com>
2022-07-05 19:29:38 -0700

    Add payload parser to darwin framework tool (#19735)

--- examples/darwin-framework-tool/main.mm
+++ examples/darwin-framework-tool/main.mm
@@ -21,6 +21,7 @@
 #include "commands/common/Commands.h"
 #include "commands/interactive/Commands.h"
 #include "commands/pairing/Commands.h"
+#include "commands/payload/Commands.h"
 #include "commands/storage/Commands.h"
 
 #include <zap-generated/cluster/Commands.h>
@@ -31,6 +32,7 @@ int main(int argc, const char * argv[])
     Commands commands;
     registerCommandsPairing(commands);
     registerCommandsInteractive(commands);
+    registerCommandsPayload(commands);
     registerCommandsStorage(commands);
     registerCommandsTests(commands);
     registerClusters(commands);
