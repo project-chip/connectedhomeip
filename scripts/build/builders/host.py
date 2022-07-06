31b356438e2aad63a3c5bb3add80acbc0d2a29e3
Andrei Litvin <andy314@gmail.com>
2022-07-06 12:25:56 -0400

    Revert "Restyle"
    This reverts commit 8bd2f67d2a70ab8939d390b3e540cbcb0cb12140.

--- scripts/build/builders/host.py
+++ scripts/build/builders/host.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os
-from enum import auto
-from enum import Enum
+from enum import Enum, auto
 from platform import uname
 
 from .gn import GnBuilder
@@ -44,115 +44,110 @@ class HostApp(Enum):
 
     def ExamplePath(self):
         if self == HostApp.ALL_CLUSTERS:
-            return "all-clusters-app/linux"
+            return 'all-clusters-app/linux'
         elif self == HostApp.ALL_CLUSTERS_MINIMAL:
-            return "all-clusters-minimal-app/linux"
+            return 'all-clusters-minimal-app/linux'
         elif self == HostApp.CHIP_TOOL:
-            return "chip-tool"
+            return 'chip-tool'
         elif self == HostApp.CHIP_TOOL_DARWIN:
-            return "darwin-framework-tool"
+            return 'darwin-framework-tool'
         elif self == HostApp.THERMOSTAT:
-            return "thermostat/linux"
+            return 'thermostat/linux'
         elif self == HostApp.RPC_CONSOLE:
-            return "common/pigweed/rpc_console"
+            return 'common/pigweed/rpc_console'
         elif self == HostApp.MIN_MDNS:
-            return "minimal-mdns"
+            return 'minimal-mdns'
         elif self == HostApp.TV_APP:
-            return "tv-app/linux"
+            return 'tv-app/linux'
         elif self == HostApp.TV_CASTING_APP:
-            return "tv-casting-app/linux"
+            return 'tv-casting-app/linux'
         elif self == HostApp.LIGHT:
-            return "lighting-app/linux"
+            return 'lighting-app/linux'
         elif self == HostApp.LOCK:
-            return "lock-app/linux"
+            return 'lock-app/linux'
         elif self == HostApp.SHELL:
-            return "shell/standalone"
+            return 'shell/standalone'
         elif self == HostApp.OTA_PROVIDER:
-            return "ota-provider-app/linux"
+            return 'ota-provider-app/linux'
         elif self == HostApp.OTA_REQUESTOR:
-            return "ota-requestor-app/linux"
-        elif self in [
-                HostApp.ADDRESS_RESOLVE,
-                HostApp.TESTS,
-                HostApp.PYTHON_BINDINGS,
-                HostApp.CERT_TOOL,
-        ]:
-            return "../"
+            return 'ota-requestor-app/linux'
+        elif self in [HostApp.ADDRESS_RESOLVE, HostApp.TESTS, HostApp.PYTHON_BINDINGS, HostApp.CERT_TOOL]:
+            return '../'
         elif self == HostApp.NL_TEST_RUNNER:
-            return "../src/test_driver/efr32"
+            return '../src/test_driver/efr32'
         elif self == HostApp.TV_CASTING:
-            return "tv-casting-app/linux"
+            return 'tv-casting-app/linux'
         elif self == HostApp.BRIDGE:
-            return "bridge-app/linux"
+            return 'bridge-app/linux'
         else:
-            raise Exception("Unknown app type: %r" % self)
+            raise Exception('Unknown app type: %r' % self)
 
     def OutputNames(self):
         if self == HostApp.ALL_CLUSTERS:
-            yield "chip-all-clusters-app"
-            yield "chip-all-clusters-app.map"
+            yield 'chip-all-clusters-app'
+            yield 'chip-all-clusters-app.map'
         elif self == HostApp.ALL_CLUSTERS_MINIMAL:
-            yield "chip-all-clusters-minimal-app"
-            yield "chip-all-clusters-minimal-app.map"
+            yield 'chip-all-clusters-minimal-app'
+            yield 'chip-all-clusters-minimal-app.map'
         elif self == HostApp.CHIP_TOOL:
-            yield "chip-tool"
-            yield "chip-tool.map"
+            yield 'chip-tool'
+            yield 'chip-tool.map'
         elif self == HostApp.CHIP_TOOL_DARWIN:
-            yield "darwin-framework-tool"
-            yield "darwin-framework-tool.map"
+            yield 'darwin-framework-tool'
+            yield 'darwin-framework-tool.map'
         elif self == HostApp.THERMOSTAT:
-            yield "thermostat-app"
-            yield "thermostat-app.map"
+            yield 'thermostat-app'
+            yield 'thermostat-app.map'
         elif self == HostApp.RPC_CONSOLE:
-            yield "chip_rpc_console_wheels"
+            yield 'chip_rpc_console_wheels'
         elif self == HostApp.MIN_MDNS:
-            yield "mdns-advertiser"
-            yield "mdns-advertiser.map"
-            yield "minimal-mdns-client"
-            yield "minimal-mdns-client.map"
-            yield "minimal-mdns-server"
-            yield "minimal-mdns-server.map"
+            yield 'mdns-advertiser'
+            yield 'mdns-advertiser.map'
+            yield 'minimal-mdns-client'
+            yield 'minimal-mdns-client.map'
+            yield 'minimal-mdns-server'
+            yield 'minimal-mdns-server.map'
         elif self == HostApp.ADDRESS_RESOLVE:
-            yield "address-resolve-tool"
-            yield "address-resolve-tool.map"
+            yield 'address-resolve-tool'
+            yield 'address-resolve-tool.map'
         elif self == HostApp.TV_APP:
-            yield "chip-tv-app"
-            yield "chip-tv-app.map"
+            yield 'chip-tv-app'
+            yield 'chip-tv-app.map'
         elif self == HostApp.TV_CASTING_APP:
-            yield "chip-tv-casting-app"
-            yield "chip-tv-casting-app.map"
+            yield 'chip-tv-casting-app'
+            yield 'chip-tv-casting-app.map'
         elif self == HostApp.LIGHT:
-            yield "chip-lighting-app"
-            yield "chip-lighting-app.map"
+            yield 'chip-lighting-app'
+            yield 'chip-lighting-app.map'
         elif self == HostApp.LOCK:
-            yield "chip-lock-app"
-            yield "chip-lock-app.map"
+            yield 'chip-lock-app'
+            yield 'chip-lock-app.map'
         elif self == HostApp.TESTS:
             pass
         elif self == HostApp.SHELL:
-            yield "chip-shell"
-            yield "chip-shell.map"
+            yield 'chip-shell'
+            yield 'chip-shell.map'
         elif self == HostApp.CERT_TOOL:
-            yield "chip-cert"
-            yield "chip-cert.map"
+            yield 'chip-cert'
+            yield 'chip-cert.map'
         elif self == HostApp.OTA_PROVIDER:
-            yield "chip-ota-provider-app"
-            yield "chip-ota-provider-app.map"
+            yield 'chip-ota-provider-app'
+            yield 'chip-ota-provider-app.map'
         elif self == HostApp.OTA_REQUESTOR:
-            yield "chip-ota-requestor-app"
-            yield "chip-ota-requestor-app.map"
+            yield 'chip-ota-requestor-app'
+            yield 'chip-ota-requestor-app.map'
         elif self == HostApp.PYTHON_BINDINGS:
-            yield "controller/python"  # Directory containing WHL files
+            yield 'controller/python'  # Directory containing WHL files
         elif self == HostApp.NL_TEST_RUNNER:
-            yield "chip_nl_test_runner_wheels"
+            yield 'chip_nl_test_runner_wheels'
         elif self == HostApp.TV_CASTING:
-            yield "chip-tv-casting-app"
-            yield "chip-tv-casting-app.map"
+            yield 'chip-tv-casting-app'
+            yield 'chip-tv-casting-app.map'
         elif self == HostApp.BRIDGE:
-            yield "chip-bridge-app"
-            yield "chip-bridge-app.map"
+            yield 'chip-bridge-app'
+            yield 'chip-bridge-app.map'
         else:
-            raise Exception("Unknown app type: %r" % self)
+            raise Exception('Unknown app type: %r' % self)
 
 
 class HostBoard(Enum):
@@ -170,56 +165,40 @@ class HostBoard(Enum):
             arch = uname_result.machine
 
             # standardize some common platforms
-            if arch == "x86_64":
-                arch = "x64"
-            elif arch == "i386" or arch == "i686":
-                arch = "x86"
-            elif arch in ("aarch64", "aarch64_be", "armv8b", "armv8l"):
-                arch = "arm64"
+            if arch == 'x86_64':
+                arch = 'x64'
+            elif arch == 'i386' or arch == 'i686':
+                arch = 'x86'
+            elif arch in ('aarch64', 'aarch64_be', 'armv8b', 'armv8l'):
+                arch = 'arm64'
 
             return arch
         elif self == HostBoard.ARM64:
-            return "arm64"
+            return 'arm64'
         elif self == HostBoard.FAKE:
-            return "fake"
+            return 'fake'
         else:
-            raise Exception("Unknown host board type: %r" % self)
+            raise Exception('Unknown host board type: %r' % self)
 
     def PlatformName(self):
         if self == HostBoard.NATIVE:
             return uname().system.lower()
         elif self == HostBoard.FAKE:
-            return "fake"
+            return 'fake'
         else:
             # Cross compilation assumes linux currently
-            return "linux"
+            return 'linux'
 
 
 class HostBuilder(GnBuilder):
 
-    def __init__(
-        self,
-        root,
-        runner,
-        app: HostApp,
-        board=HostBoard.NATIVE,
-        enable_ipv4=True,
-        enable_ble=True,
-        enable_wifi=True,
-        use_tsan=False,
-        use_asan=False,
-        separate_event_loop=True,
-        use_libfuzzer=False,
-        use_clang=False,
-        interactive_mode=True,
-        extra_tests=False,
-        use_platform_mdns=False,
-        enable_rpcs=False,
-    ):
-        super(HostBuilder,
-              self).__init__(root=os.path.join(root, "examples",
-                                               app.ExamplePath()),
-                             runner=runner)
+    def __init__(self, root, runner, app: HostApp, board=HostBoard.NATIVE, enable_ipv4=True,
+                 enable_ble=True, enable_wifi=True, use_tsan=False,  use_asan=False, separate_event_loop=True,
+                 use_libfuzzer=False, use_clang=False, interactive_mode=True, extra_tests=False,
+                 use_platform_mdns=False, enable_rpcs=False):
+        super(HostBuilder, self).__init__(
+            root=os.path.join(root, 'examples', app.ExamplePath()),
+            runner=runner)
 
         self.app = app
         self.board = board
@@ -229,36 +208,36 @@ class HostBuilder(GnBuilder):
             self.extra_gn_options.append('import("//with_pw_rpc.gni")')
 
         if not enable_ipv4:
-            self.extra_gn_options.append("chip_inet_config_enable_ipv4=false")
+            self.extra_gn_options.append('chip_inet_config_enable_ipv4=false')
 
         if not enable_ble:
-            self.extra_gn_options.append("chip_config_network_layer_ble=false")
+            self.extra_gn_options.append('chip_config_network_layer_ble=false')
 
         if not enable_wifi:
-            self.extra_gn_options.append("chip_enable_wifi=false")
+            self.extra_gn_options.append('chip_enable_wifi=false')
 
         if use_tsan:
-            self.extra_gn_options.append("is_tsan=true")
+            self.extra_gn_options.append('is_tsan=true')
 
         if use_asan:
-            self.extra_gn_options.append("is_asan=true")
+            self.extra_gn_options.append('is_asan=true')
 
         if not separate_event_loop:
-            self.extra_gn_options.append("config_use_separate_eventloop=false")
+            self.extra_gn_options.append('config_use_separate_eventloop=false')
 
         if not interactive_mode:
-            self.extra_gn_options.append("config_use_interactive_mode=false")
+            self.extra_gn_options.append('config_use_interactive_mode=false')
 
         if use_libfuzzer:
-            self.extra_gn_options.append("is_libfuzzer=true")
+            self.extra_gn_options.append('is_libfuzzer=true')
 
         if use_clang:
-            self.extra_gn_options.append("is_clang=true")
+            self.extra_gn_options.append('is_clang=true')
 
             if self.board == HostBoard.FAKE:
                 # Fake uses "//build/toolchain/fake:fake_x64_gcc"
                 # so setting clang is not correct
-                raise Exception("Fake host board is always gcc (not clang)")
+                raise Exception('Fake host board is always gcc (not clang)')
 
         if use_platform_mdns:
             self.extra_gn_options.append('chip_mdns="platform"')
@@ -266,14 +245,14 @@ class HostBuilder(GnBuilder):
         if extra_tests:
             # Flag for testing purpose
             self.extra_gn_options.append(
-                "chip_im_force_fabric_quota_check=true")
+                'chip_im_force_fabric_quota_check=true')
 
         if app == HostApp.TESTS:
-            self.extra_gn_options.append("chip_build_tests=true")
-            self.build_command = "check"
+            self.extra_gn_options.append('chip_build_tests=true')
+            self.build_command = 'check'
 
         if app == HostApp.NL_TEST_RUNNER:
-            self.build_command = "runner"
+            self.build_command = 'runner'
 
         if app == HostApp.CERT_TOOL:
             # Certification only built for openssl
@@ -283,37 +262,40 @@ class HostBuilder(GnBuilder):
                 raise Exception(
                     "Cannot cross compile CERT TOOL: ssl library conflict")
             self.extra_gn_options.append('chip_crypto="openssl"')
-            self.build_command = "src/tools/chip-cert"
+            self.build_command = 'src/tools/chip-cert'
         elif app == HostApp.ADDRESS_RESOLVE:
-            self.build_command = "src/lib/address_resolve:address-resolve-tool"
+            self.build_command = 'src/lib/address_resolve:address-resolve-tool'
         elif app == HostApp.PYTHON_BINDINGS:
-            self.extra_gn_options.append("enable_rtti=false")
-            self.extra_gn_options.append(
-                'chip_project_config_include_dirs=["//config/python"]')
-            self.build_command = "python"
+            self.extra_gn_options.append('enable_rtti=false')
+            self.extra_gn_options.append('chip_project_config_include_dirs=["//config/python"]')
+            self.build_command = 'python'
 
     def GnBuildArgs(self):
         if self.board == HostBoard.NATIVE:
             return self.extra_gn_options
         elif self.board == HostBoard.ARM64:
-            self.extra_gn_options.extend([
-                'target_cpu="arm64"',
-                "is_clang=true",
-                'chip_crypto="mbedtls"',
-                'sysroot="%s"' % self.SysRootPath("SYSROOT_AARCH64"),
-            ])
+            self.extra_gn_options.extend(
+                [
+                    'target_cpu="arm64"',
+                    'is_clang=true',
+                    'chip_crypto="mbedtls"',
+                    'sysroot="%s"' % self.SysRootPath('SYSROOT_AARCH64')
+                ]
+            )
 
             return self.extra_gn_options
         elif self.board == HostBoard.FAKE:
-            self.extra_gn_options.extend([
-                'custom_toolchain="//build/toolchain/fake:fake_x64_gcc"',
-                "chip_link_tests=true",
-                'chip_device_platform="fake"',
-                "chip_fake_platform=true",
-            ])
+            self.extra_gn_options.extend(
+                [
+                    'custom_toolchain="//build/toolchain/fake:fake_x64_gcc"',
+                    'chip_link_tests=true',
+                    'chip_device_platform="fake"',
+                    'chip_fake_platform=true',
+                ]
+            )
             return self.extra_gn_options
         else:
-            raise Exception("Unknown host board type: %r" % self)
+            raise Exception('Unknown host board type: %r' % self)
 
     def GnBuildEnv(self):
         if self.board == HostBoard.NATIVE:
@@ -322,14 +304,12 @@ class HostBuilder(GnBuilder):
             return None
         elif self.board == HostBoard.ARM64:
             return {
-                "PKG_CONFIG_PATH":
-                os.path.join(
-                    self.SysRootPath("SYSROOT_AARCH64"),
-                    "lib/aarch64-linux-gnu/pkgconfig",
-                ),
+                'PKG_CONFIG_PATH': os.path.join(
+                    self.SysRootPath('SYSROOT_AARCH64'),
+                    'lib/aarch64-linux-gnu/pkgconfig'),
             }
         else:
-            raise Exception("Unknown host board type: %r" % self)
+            raise Exception('Unknown host board type: %r' % self)
 
     def SysRootPath(self, name):
         if name not in os.environ:
@@ -344,8 +324,12 @@ class HostBuilder(GnBuilder):
             if os.path.isdir(path):
                 for root, dirs, files in os.walk(path):
                     for file in files:
-                        outputs.update({file: os.path.join(root, file)})
+                        outputs.update({
+                            file: os.path.join(root, file)
+                        })
             else:
-                outputs.update({name: os.path.join(self.output_dir, name)})
+                outputs.update({
+                    name: os.path.join(self.output_dir, name)
+                })
 
         return outputs
