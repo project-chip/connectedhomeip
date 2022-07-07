e66a1f50ddc863e39016b63987ad4e8ff11239e8
Terence Hampson <thampson@google.com>
2022-07-07 13:49:03 +0000

    Restyle

--- src/controller/python/chip/utils/CommissioningUtils.py
+++ src/controller/python/chip/utils/CommissioningUtils.py
@@ -21,6 +21,7 @@ import chip.tlv
 
 _UINT16_MAX = 65535
 
+
 async def _IsBasicAttributeReadSuccessful(devCtrl, nodeId):
     resp = await devCtrl.ReadAttribute(nodeId, [(Clusters.Basic.Attributes.ClusterRevision)])
     clusterRevision = resp[0][Clusters.Basic][Clusters.Basic.Attributes.ClusterRevision]
@@ -33,15 +34,15 @@ async def AddNOC(commissionedDevCtrl, newDevCtrl, existingNodeId, newNodeId):
     resp = await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(60), timedRequestTimeoutMs=1000)
     if resp.errorCode is not Clusters.GeneralCommissioning.Enums.CommissioningError.kOk:
         return False
-  
+
     csrForAddNOC = await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.OperationalCredentials.Commands.CSRRequest(CSRNonce=b'1' * 32))
-  
+
     chainForAddNOC = newDevCtrl.IssueNOCChain(csrForAddNOC, newNodeId)
     if chainForAddNOC.rcacBytes is None or chainForAddNOC.icacBytes is None or chainForAddNOC.nocBytes is None or chainForAddNOC.ipkBytes is None:
         # Expiring the failsafe timer in an attempt to clean up.
         await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(0), timedRequestTimeoutMs=1000)
         return False
-  
+
     # TODO error check on the commands below
     await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.OperationalCredentials.Commands.AddTrustedRootCertificate(chainForAddNOC.rcacBytes))
     resp = await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.OperationalCredentials.Commands.AddNOC(chainForAddNOC.nocBytes, chainForAddNOC.icacBytes, chainForAddNOC.ipkBytes, newDevCtrl.GetNodeId(), 0xFFF1))
@@ -55,11 +56,12 @@ async def AddNOC(commissionedDevCtrl, newDevCtrl, existingNodeId, newNodeId):
         # Expiring the failsafe timer in an attempt to clean up.
         await commissionedDevCtrl.SendCommand(existingNodeId, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(0), timedRequestTimeoutMs=1000)
         return False
-    
+
     if not await _IsBasicAttributeReadSuccessful(newDevCtrl, newNodeId):
         return False
 
     return True
+
 
 async def UpdateNOC(devCtrl, existingNodeId, newNodeId):
     resp = await devCtrl.SendCommand(existingNodeId, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(600), timedRequestTimeoutMs=1000)
