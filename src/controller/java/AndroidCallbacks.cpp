d9745bce3a3372451ec018319b2aab6a657694fa
Restyled.io <commits@restyled.io>
2022-07-08 08:29:08 +0000

    Restyled by clang-format

--- src/controller/java/AndroidCallbacks.cpp
+++ src/controller/java/AndroidCallbacks.cpp
@@ -448,9 +448,9 @@ void ReportEventCallback::OnEventData(const app::EventHeader & aEventHeader, TLV
     readerForJavaTLV.Init(*apData);
     readerForJson.Init(*apData);
 
-    jlong eventNumber = static_cast<jlong>(aEventHeader.mEventNumber);
+    jlong eventNumber   = static_cast<jlong>(aEventHeader.mEventNumber);
     jlong priorityLevel = static_cast<jint>(aEventHeader.mPriorityLevel);
-    jlong timestamp = static_cast<jlong>(aEventHeader.mTimestamp.mValue);
+    jlong timestamp     = static_cast<jlong>(aEventHeader.mTimestamp.mValue);
 
     jobject value = DecodeEventValue(aEventHeader.mPath, readerForJavaObject, &err);
     // If we don't know this event, just skip it.
@@ -487,7 +487,8 @@ void ReportEventCallback::OnEventData(const app::EventHeader & aEventHeader, TLV
     chip::JniClass eventStateJniCls(eventStateCls);
     jmethodID eventStateCtor = env->GetMethodID(eventStateCls, "<init>", "(JIJLjava/lang/Object;[BLjava/lang/String;)V");
     VerifyOrReturn(eventStateCtor != nullptr, ChipLogError(Controller, "Could not find EventState constructor"));
-    jobject eventStateObj = env->NewObject(eventStateCls, eventStateCtor, eventNumber, priorityLevel, timestamp, value, jniByteArray.jniValue(), jsonString.jniValue());
+    jobject eventStateObj = env->NewObject(eventStateCls, eventStateCtor, eventNumber, priorityLevel, timestamp, value,
+                                           jniByteArray.jniValue(), jsonString.jniValue());
     VerifyOrReturn(eventStateObj != nullptr, ChipLogError(Controller, "Could not create EventState object"));
 
     // Add EventState to NodeState
