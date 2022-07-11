2ff17c12d466d40128aac56dcf559a3ef130eeab
Damian Królik <66667989+Damian-Nordic@users.noreply.github.com>
2022-07-11 11:34:13 +0200

    [power-source] Update cluster definitions (#20370)
    * [power-source] Update cluster definitions
    
    1. Align attribute names with the spec by replacing
       BateryXYZ names with BatXYZ.
    2. Use specific enum types instead of ENUM8.
    3. Add missing isNullable="true" definitions.
    4. Fix attribute value ranges.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>
    
    * Fix examples
    
    * Regen ZAP

--- zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
+++ zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
@@ -492,8 +492,8 @@ namespace PowerSourceConfiguration {
 
 namespace PowerSource {
 
-// Enum for BatChargeFaultType
-enum class BatChargeFaultType : uint8_t
+// Enum for BatChargeFault
+enum class BatChargeFault : uint8_t
 {
     kUnspecfied          = 0x00,
     kAmbientTooHot       = 0x01,
@@ -525,8 +525,8 @@ enum class BatChargeState : uint8_t
     kIsNotCharging  = 0x03,
 };
 
-// Enum for BatFaultType
-enum class BatFaultType : uint8_t
+// Enum for BatFault
+enum class BatFault : uint8_t
 {
     kUnspecfied = 0x00,
     kOverTemp   = 0x01,
@@ -558,8 +558,8 @@ enum class WiredCurrentType : uint8_t
     kDc = 0x01,
 };
 
-// Enum for WiredFaultType
-enum class WiredFaultType : uint8_t
+// Enum for WiredFault
+enum class WiredFault : uint8_t
 {
     kUnspecfied   = 0x00,
     kOverVoltage  = 0x01,
