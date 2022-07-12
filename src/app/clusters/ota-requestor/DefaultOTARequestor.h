aa7502389e8b8c236db07f67fdbcacaba2161e54
Boris Zbarsky <bzbarsky@apple.com>
2022-07-12 11:24:53 -0400

    Fix exchange lifetime management in BDXMessenger.

--- src/app/clusters/ota-requestor/DefaultOTARequestor.h
+++ src/app/clusters/ota-requestor/DefaultOTARequestor.h
@@ -136,9 +136,13 @@ private:
             {
                 sendFlags.Set(chip::Messaging::SendMessageFlags::kExpectResponse);
             }
-            ReturnErrorOnFailure(mExchangeCtx->SendMessage(event.msgTypeData.ProtocolId, event.msgTypeData.MessageType,
-                                                           event.MsgData.Retain(), sendFlags));
-            return CHIP_NO_ERROR;
+            CHIP_ERROR err = mExchangeCtx->SendMessage(event.msgTypeData.ProtocolId, event.msgTypeData.MessageType,
+                                                       event.MsgData.Retain(), sendFlags);
+            if (err != CHIP_NO_ERROR) {
+                Reset();
+            }
+
+            return err;
         }
 
         CHIP_ERROR OnMessageReceived(chip::Messaging::ExchangeContext * ec, const chip::PayloadHeader & payloadHeader,
@@ -150,13 +154,14 @@ private:
                 return CHIP_NO_ERROR;
             }
 
-            mDownloader->OnMessageReceived(payloadHeader, payload.Retain());
+            mDownloader->OnMessageReceived(payloadHeader, std::move(payload));
 
             // For a receiver using BDX Protocol, all received messages will require a response except for a StatusReport
             if (!payloadHeader.HasMessageType(chip::Protocols::SecureChannel::MsgType::StatusReport))
             {
                 ec->WillSendMessage();
             }
+
             return CHIP_NO_ERROR;
         }
 
@@ -167,6 +172,11 @@ private:
             {
                 mDownloader->OnDownloadTimeout();
             }
+        }
+
+        void OnExchangeClosing(Messaging::ExchangeContext * ec) override
+        {
+            mExchangeCtx = nullptr;
         }
 
         void Init(chip::BDXDownloader * downloader, chip::Messaging::ExchangeContext * ec)
