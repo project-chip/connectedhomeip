02ee3f0779775d8159bf88ef362170197271bbf9
Vivien Nicolas <vnicolas@apple.com>
2022-07-19 23:01:56 +0200

    [chip-tool] Add subscribe-all command (#20868)
    * [chip-tool] Update read-all command such that an arbitrary number of attributes/events can be used
    
    * [chip-tool] Add subscribe-all command to chip-tool
    
    * Update generated code

--- src/app/tests/suites/commands/interaction_model/InteractionModel.h
+++ src/app/tests/suites/commands/interaction_model/InteractionModel.h
@@ -108,7 +108,35 @@ protected:
                            const chip::Optional<std::vector<bool>> & isUrgents   = chip::NullOptional);
 
     CHIP_ERROR ReadAll(chip::DeviceProxy * device, std::vector<chip::EndpointId> endpointIds,
-                       const chip::Optional<bool> & fabricFiltered = chip::Optional<bool>(true));
+                       std::vector<chip::ClusterId> clusterIds, std::vector<chip::AttributeId> attributeIds,
+                       std::vector<chip::EventId> eventIds,
+                       const chip::Optional<bool> & fabricFiltered                         = chip::Optional<bool>(true),
+                       const chip::Optional<std::vector<chip::DataVersion>> & dataVersions = chip::NullOptional,
+                       const chip::Optional<chip::EventNumber> & eventNumber               = chip::NullOptional)
+    {
+        return ReportAll(device, endpointIds, clusterIds, attributeIds, eventIds, chip::app::ReadClient::InteractionType::Read, 0,
+                         0, fabricFiltered, dataVersions, eventNumber);
+    }
+
+    CHIP_ERROR SubscribeAll(chip::DeviceProxy * device, std::vector<chip::EndpointId> endpointIds,
+                            std::vector<chip::ClusterId> clusterIds, std::vector<chip::AttributeId> attributeIds,
+                            std::vector<chip::EventId> eventIds, uint16_t minInterval = 0, uint16_t maxInterval = 0,
+                            const chip::Optional<bool> & fabricFiltered           = chip::Optional<bool>(true),
+                            const chip::Optional<chip::EventNumber> & eventNumber = chip::NullOptional,
+                            const chip::Optional<bool> & keepSubscriptions        = chip::NullOptional)
+    {
+        return ReportAll(device, endpointIds, clusterIds, attributeIds, eventIds, chip::app::ReadClient::InteractionType::Subscribe,
+                         minInterval, maxInterval, fabricFiltered, chip::NullOptional, eventNumber, keepSubscriptions);
+    }
+
+    CHIP_ERROR ReportAll(chip::DeviceProxy * device, std::vector<chip::EndpointId> endpointIds,
+                         std::vector<chip::ClusterId> clusterIds, std::vector<chip::AttributeId> attributeIds,
+                         std::vector<chip::EventId> eventIds, chip::app::ReadClient::InteractionType interactionType,
+                         uint16_t minInterval = 0, uint16_t maxInterval = 0,
+                         const chip::Optional<bool> & fabricFiltered                         = chip::Optional<bool>(true),
+                         const chip::Optional<std::vector<chip::DataVersion>> & dataVersions = chip::NullOptional,
+                         const chip::Optional<chip::EventNumber> & eventNumber               = chip::NullOptional,
+                         const chip::Optional<bool> & keepSubscriptions                      = chip::NullOptional);
 
     void Shutdown() { mReadClients.clear(); }
 
