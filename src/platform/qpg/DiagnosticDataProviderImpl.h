579a276d7432764742d3ca26426fcac1225185ad
lucicop <lucian.copat@qorvo.com>
2022-07-20 12:52:00 +0200

    Add function to reset heap high watermarks

--- src/platform/qpg/DiagnosticDataProviderImpl.h
+++ src/platform/qpg/DiagnosticDataProviderImpl.h
@@ -39,9 +39,11 @@ public:
 
     // ===== Methods that implement the PlatformManager abstract interface.
 
+    bool SupportsWatermarks() override { return true; }
     CHIP_ERROR GetCurrentHeapFree(uint64_t & currentHeapFree) override;
     CHIP_ERROR GetCurrentHeapUsed(uint64_t & currentHeapUsed) override;
     CHIP_ERROR GetCurrentHeapHighWatermark(uint64_t & currentHeapHighWatermark) override;
+    CHIP_ERROR ResetWatermarks() override;
 };
 
 /**
