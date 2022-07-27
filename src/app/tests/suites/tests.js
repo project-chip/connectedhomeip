51555e5978833af21759358f39f2b53a2dec2078
Cecille Freeman <cecille@google.com>
2022-07-26 20:57:39 -0400

    TC-OPCREDS-1.2 yaml

--- src/app/tests/suites/tests.js
+++ src/app/tests/suites/tests.js
@@ -110,7 +110,6 @@ function getManualTests() {
     const DeviceManagement = [
         "Test_TC_BINFO_1_1",
         "Test_TC_OPCREDS_1_2",
-        "Test_TC_CNET_1_3",
         "Test_TC_BINFO_2_2",
         "Test_TC_BINFO_2_4",
         "Test_TC_OPCREDS_3_1",
@@ -595,6 +594,7 @@ function getTests() {
     ];
 
     const DeviceManagement = [
+        "Test_TC_CNET_1_3",
         "Test_TC_BINFO_2_1",
     ];
 
