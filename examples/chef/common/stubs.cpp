7c833ab6bf2daad00678308a0432a6250c018623
Morozov-5F <evgeniy.morozov@dsr-corporation.com>
2022-07-29 15:41:56 +0300

    [#19989] Implement lockout for door lock

--- examples/chef/common/stubs.cpp
+++ examples/chef/common/stubs.cpp
@@ -4,6 +4,9 @@
 #include <app-common/zap-generated/callback.h>
 #include <app-common/zap-generated/cluster-id.h>
 #include <app-common/zap-generated/command-id.h>
+
+// Include door lock callbacks only when the server is enabled
+#ifdef EMBER_AF_PLUGIN_DOOR_LOCK_SERVER
 #include <app/clusters/door-lock-server/door-lock-server.h>
 
 bool emberAfPluginDoorLockOnDoorLockCommand(chip::EndpointId endpointId, const chip::Optional<chip::ByteSpan> & pinCode,
@@ -19,3 +22,4 @@ bool emberAfPluginDoorLockOnDoorUnlockCommand(chip::EndpointId endpointId, const
     err = DlOperationError::kUnspecified;
     return true;
 }
+#endif /* EMBER_AF_PLUGIN_DOOR_LOCK_SERVER */
