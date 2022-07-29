cd9a857952aaf772c0f7a6c503562532d23a7648
Evgeniy Morozov <evgeniy.morozov@dsr-corporation.com>
2022-07-30 00:21:22 +0300

    Fix error code for the Set User command (#21281)
    * [#19991] Fix error code for the Set User command
    
    * Update auto-generated files

--- src/app/clusters/door-lock-server/door-lock-server.h
+++ src/app/clusters/door-lock-server/door-lock-server.h
@@ -348,6 +348,9 @@ private:
 
     bool RemoteOperationEnabled(chip::EndpointId endpointId) const;
 
+    static CHIP_ERROR sendClusterResponse(chip::app::CommandHandler * commandObj,
+                                          const chip::app::ConcreteCommandPath & commandPath, EmberAfStatus status);
+
     /**
      * @brief Common handler for LockDoor, UnlockDoor, UnlockWithTimeout commands
      *
