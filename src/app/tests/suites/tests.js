8719a90842fffb68894fe3c301e55e8c7549a5cb
kowsisoundhar12 <57476670+kowsisoundhar12@users.noreply.github.com>
2022-08-02 06:22:48 +0530

    Updated Manual Scripts-Aug1 (#21476)
    * updated Manual scripts
    
    * Added Auto generated file
    
    * Added updated tests.js file
    
    * Added Auto generated file
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by prettier-yaml
    
    * Update tests.js file
    
    * Added Auto generated file
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/app/tests/suites/tests.js
+++ src/app/tests/suites/tests.js
@@ -503,6 +503,12 @@ function getManualTests() {
         "Test_TC_ULABEL_3_1",
     ];
 
+    const BridgedDeviceBasicInformation = [
+        "Test_TC_BRBINFO_1_1",
+        "Test_TC_BRBINFO_2_1",
+        "Test_TC_BRBINFO_2_2",
+        "Test_TC_BRBINFO_2_3",
+    ];
 
     const tests = [
         DeviceDiscovery,
@@ -553,6 +559,7 @@ function getManualTests() {
         PumpConfigurationControl,
         AccessControl,
         UserLabel,
+        BridgedDeviceBasicInformation,
     ].flat(1);
 
     tests.disable = disable.bind(tests);
