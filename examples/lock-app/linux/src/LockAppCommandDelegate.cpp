fe7cf5e9e636c1a8e2acf92ec2e247f33dd4df97
Yufeng Wang <yufengwang@google.com>
2022-08-01 18:11:30 -0700

    Address review comments

--- examples/lock-app/linux/src/LockAppCommandDelegate.cpp
+++ examples/lock-app/linux/src/LockAppCommandDelegate.cpp
@@ -31,7 +31,7 @@ public:
 
     static void HandleCommand(intptr_t context);
 
-    LockAppCommandHandler(std::string & cmd, Json::Value & params) :
+    LockAppCommandHandler(std::string && cmd, Json::Value && params) :
         mCommandName(std::move(cmd)), mCommandParameters(std::move(params))
     {}
 
@@ -75,7 +75,7 @@ LockAppCommandHandler * LockAppCommandHandler::FromJSON(const char * json)
         params = value["Params"];
     }
     auto commandName = value["Cmd"].asString();
-    return chip::Platform::New<LockAppCommandHandler>(commandName, params);
+    return chip::Platform::New<LockAppCommandHandler>(std::move(commandName), std::move(params));
 }
 
 void LockAppCommandHandler::HandleCommand(intptr_t context)
