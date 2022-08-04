1a30cd2fa34c0c968329554bfc716d0c42392a38
chrisdecenzo <decenzo77@yahoo.com>
2022-08-04 14:41:40 -0700

    Draft: Add parsed attestation and csr info for Android apps

--- src/controller/java/src/chip/devicecontroller/ChipDeviceController.java
+++ src/controller/java/src/chip/devicecontroller/ChipDeviceController.java
@@ -350,26 +350,12 @@ public class ChipDeviceController {
   }
 
   public void onNOCChainGenerationNeeded(
-      byte[] csrElements,
-      byte[] csrNonce,
-      byte[] csrElementsSignature,
-      byte[] attestationChallenge,
-      byte[] attestationElements,
-      byte[] attestationNonce,
-      byte[] attestationElementsSignature,
-      byte[] dac,
-      byte[] pai) {
+      CSRInfo csrInfo,
+      AttestationInfo attestationInfo) {
     if (nocChainIssuer != null) {
       nocChainIssuer.onNOCChainGenerationNeeded(
-          csrElements,
-          csrNonce,
-          csrElementsSignature,
-          attestationChallenge,
-          attestationElements,
-          attestationNonce,
-          attestationElementsSignature,
-          dac,
-          pai);
+          csrInfo,
+          attestationInfo);
     }
   }
 
@@ -738,15 +724,8 @@ public class ChipDeviceController {
      * <p>All csr and attestation fields are provided to allow for custom attestestation checks.
      */
     void onNOCChainGenerationNeeded(
-        byte[] csrElements,
-        byte[] csrNonce,
-        byte[] csrElementsSignature,
-        byte[] attestationChallenge,
-        byte[] attestationElements,
-        byte[] attestationNonce,
-        byte[] attestationElementsSignature,
-        byte[] dac,
-        byte[] pai);
+        CSRInfo csrInfo,
+        AttestationInfo attestationInfo);
   }
 
   /**
