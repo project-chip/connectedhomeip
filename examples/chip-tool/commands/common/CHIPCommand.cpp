a0ba8abf36268506b7b69374cf554f0be88ec24a
Boris Zbarsky <bzbarsky@apple.com>
2022-08-05 00:58:43 -0400

    Make sure chip-tool interactive cleanup happens on the Matter thread.
    The interactive cleanup happens while the event loop is still running
    (during the execution of the "interactive" command), so queuing the
    cleanup on the event loop is reasonable.  And this ensures cleanups
    don't run into MAtter locking assertions.

--- examples/chip-tool/commands/common/CHIPCommand.cpp
+++ examples/chip-tool/commands/common/CHIPCommand.cpp
@@ -443,7 +443,7 @@ void CHIPCommand::StopWaiting()
 #endif // CONFIG_USE_SEPARATE_EVENTLOOP
 }
 
-void CHIPCommand::ExecuteDeferredCleanups()
+void CHIPCommand::ExecuteDeferredCleanups(intptr_t ignored)
 {
     for (auto * cmd : sDeferredCleanups)
     {
