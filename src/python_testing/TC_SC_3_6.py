0de568109a1dd5e0f989f32c7010c2b1699f5add
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2022-08-09 23:25:58 -0400

    Apply more review comments

--- src/python_testing/TC_SC_3_6.py
+++ src/python_testing/TC_SC_3_6.py
@@ -15,18 +15,46 @@
 #    limitations under the License.
 #
 
-from unicodedata import name
 from matter_testing_support import MatterBaseTest, default_matter_test_main, async_test_body
-from chip.interaction_model import Status
 import chip.clusters as Clusters
 import chip.FabricAdmin
 import logging
 from mobly import asserts
 from chip.utils import CommissioningBuildingBlocks
 from chip.clusters.Attribute import TypedAttributePath, SubscriptionTransaction
+import queue
 import asyncio
 from threading import Event
 import time
+
+# TODO: Overall, we need to add validation that session IDs have not changed throughout to be agnostic
+#       to some internal behavior assumptions of the SDK we are making relative to the write to
+#       the trigger the subscriptions not re-opening a new CASE session
+#
+
+
+class AttributeChangeAccumulator:
+    def __init__(self, name: str, expected_attribute: Clusters.ClusterAttributeDescriptor, output: queue.Queue):
+        self._name = name
+        self._output = output
+        self._expected_attribute = expected_attribute
+
+    def __call__(self, path: TypedAttributePath, transaction: SubscriptionTransaction):
+        if path.AttributeType == self._expected_attribute:
+            data = transaction.GetAttribute(path)
+
+            value = {
+                'name': self._name,
+                'endpoint': path.Path.EndpointId,
+                'attribute': path.AttributeType,
+                'value': data
+            }
+            logging.info("Got subscription report on client %s for %s: %s" % (self.name, path.AttributeType, data))
+            self._output.put(value)
+
+    @property
+    def name(self) -> str:
+        return self._name
 
 
 class ResubscriptionCatcher:
@@ -48,6 +76,14 @@ class ResubscriptionCatcher:
 
 
 class TC_SC_3_6(MatterBaseTest):
+    def setup_class(self):
+        self._subscriptions = []
+
+    def teardown_class(self):
+        logging.info("Teardown: shutting down all subscription to avoid racy callbacks")
+        for subscription in self._subscriptions:
+            subscription.Shutdown()
+
     @async_test_body
     async def test_TC_SC_3_6(self):
         dev_ctrl = self.default_controller
@@ -55,11 +91,15 @@ class TC_SC_3_6(MatterBaseTest):
         # Get overrides for debugging the test
         num_fabrics_to_commission = self.user_params.get("num_fabrics_to_commission", 5)
         num_controllers_per_fabric = self.user_params.get("num_controllers_per_fabric", 3)
-        min_report_interval_sec = self.user_params.get("min_report_interval_sec", 1)
-        max_report_interval_sec = self.user_params.get("max_report_interval_sec", 30)
-        # Time to wait after changing NodeLabel for subscriptions to all hit
-        stabilization_delay_sec = self.user_params.get("stabilization_delay_sec", 10)
-        sub_liveness_override_ms = self.user_params.get("sub_liveness_override_ms", None)
+        # Immediate reporting
+        min_report_interval_sec = self.user_params.get("min_report_interval_sec", 0)
+        # 10 minutes max reporting interval --> We don't care about keep-alives per-se and
+        # want to avoid resubscriptions
+        max_report_interval_sec = self.user_params.get("max_report_interval_sec", 10 * 60)
+        # Time to wait after changing NodeLabel for subscriptions to all hit. This is dependant
+        # on MRP params of subscriber and on actual min_report_interval.
+        # TODO: Determine the correct max value depending on target. Test plan doesn't say!
+        timeout_delay_sec = self.user_params.get("timeout_delay_sec", max_report_interval_sec * 2)
 
         BEFORE_LABEL = "Before Subscriptions"
         AFTER_LABEL = "After Subscriptions"
@@ -119,39 +159,64 @@ class TC_SC_3_6(MatterBaseTest):
         await client_list[0].WriteAttribute(self.dut_node_id, [(0, Clusters.Basic.Attributes.NodeLabel(value=BEFORE_LABEL))])
 
         # Subscribe with all clients to NodeLabel attribute
-        subscriptions = []
         sub_handlers = []
         resub_catchers = []
+        output_queue = queue.Queue()
 
         logging.info("Step 1 (first part): Establish subscription with all %d clients" % len(client_list))
-        for client in client_list:
-            logging.info("Establishing subscription from controller node %s" % client.name)
+        for sub_idx, client in enumerate(client_list):
+            logging.info("Establishing subscription %d/%d from controller node %s" % (sub_idx + 1, len(client_list), client.name))
+
             sub = await client.ReadAttribute(nodeid=self.dut_node_id, attributes=[(0, Clusters.Basic.Attributes.NodeLabel)],
                                              reportInterval=(min_report_interval_sec, max_report_interval_sec), keepSubscriptions=False)
-            subscriptions.append(sub)
+            self._subscriptions.append(sub)
 
+            attribute_handler = AttributeChangeAccumulator(
+                name=client.name, expected_attribute=Clusters.Basic.Attributes.NodeLabel, output=output_queue)
+            sub.SetAttributeUpdateCallback(attribute_handler)
+            sub_handlers.append(attribute_handler)
+
+            # TODO: Replace resubscription catcher with API to disable re-subscription on failure
             resub_catcher = ResubscriptionCatcher(name=client.name)
             sub.SetResubscriptionAttemptedCallback(resub_catcher)
             resub_catchers.append(resub_catcher)
 
-            if sub_liveness_override_ms is not None:
-                logging.warning("Overriding subscription liveness to check %dms! NOT FOR CERTIFICATION!" % sub_liveness_override_ms)
-                sub.OverrideLivenessTimeoutMs(sub_liveness_override_ms)
-
-        asserts.assert_equal(len(subscriptions), len(client_list), "Must have the right number of subscriptions")
+        asserts.assert_equal(len(self._subscriptions), len(client_list), "Must have the right number of subscriptions")
 
         # Trigger a change on NodeLabel
         logging.info(
-            "Step 1 (second part): Change attribute with one client, ensure no resubscriptions are triggered due to CASE session starvation")
+            "Step 1 (second part): Change attribute with one client, await all attributes changed within time")
         await asyncio.sleep(1)
         await client_list[0].WriteAttribute(self.dut_node_id, [(0, Clusters.Basic.Attributes.NodeLabel(value=AFTER_LABEL))])
 
+        all_changes = {client.name: False for client in client_list}
+
         # Await a stabilization delay in increments to let the event loops run
         start_time = time.time()
-        while (time.time() - start_time) < stabilization_delay_sec:
-            await asyncio.sleep(0.05)
+        elapsed = 0
+        time_remaining = timeout_delay_sec
 
-        # After stabilization, validate no resubscriptions
+        while time_remaining > 0:
+            try:
+                item = output_queue.get(block=True, timeout=time_remaining)
+                client_name, endpoint, attribute, value = item['name'], item['endpoint'], item['attribute'], item['value']
+
+                # Record arrival of an expected subscription change when seen
+                if endpoint == 0 and attribute == Clusters.Basic.Attributes.NodeLabel and value == AFTER_LABEL:
+                    if not all_changes[client_name]:
+                        logging.info("Got expected attribute change for client %s" % client_name)
+                        all_changes[client_name] = True
+
+                # We are done waiting when we have accumulated all results
+                if all(all_changes.values()):
+                    logging.info("All clients have reported, done waiting.")
+                    break
+            except queue.Empty:
+                # No error, we update timeouts and keep going
+                pass
+
+            elapsed = time.time() - start_time
+            time_remaining = timeout_delay_sec - elapsed
 
         logging.info("Validation of results")
         failed = False
@@ -161,7 +226,15 @@ class TC_SC_3_6(MatterBaseTest):
                 logging.error("Client %s saw a resubscription" % catcher.name)
                 failed = True
             else:
-                logging.error("Client %s correctly did not see a resubscription" % catcher.name)
+                logging.info("Client %s correctly did not see a resubscription" % catcher.name)
+
+        all_reports_gotten = all(all_changes.values())
+        if not all_reports_gotten:
+            logging.error("Missing reports from the following clients: %s" %
+                          ", ".join([name for name, value in all_changes.items() if value is False]))
+            failed = True
+        else:
+            logging.info("Got successful reports from all clients, meaning all concurrent CASE sessions worked")
 
         # Determine final result
         if failed:
