0528a4259e51f79aa7a853fef16d9e2df24b4c17
zhengyaohan <502106661@qq.com>
2022-08-16 06:52:33 +0800

    [BEKEN] add beken platform file (#21824)
    * [BEKEN] add beken platform file
    
    * add beken platform src file
    * support examples/lighting-app
    * add beken config file
    
    * [BEKEN] change the return type of InitClock_RealTime() and update some
    comments.
    
    * [BEKEN] del obsolete interface
    
    * del obsolete interface
    * fix copyright headers (2020 -> 2022) in all files
    
    Co-authored-by: Tao Yang <tao.yang@bekencorp.com>
    Co-authored-by: cenfang <fly92727@outlook.com>

---
+++ src/platform/Beken/BekenConfig.cpp
@@ -0,0 +1,316 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/* this file behaves like a config.h, comes first */
+#include <platform/internal/CHIPDeviceLayerInternal.h>
+
+#include <core/CHIPEncoding.h>
+#include <platform/Beken/BekenConfig.h>
+#include <support/CHIPMem.h>
+#include <support/CHIPMemString.h>
+#include <support/CodeUtils.h>
+#include <support/logging/CHIPLogging.h>
+
+#include "matter_pal.h"
+
+enum
+{
+    kPrefsTypeBoolean = 1,
+    kPrefsTypeInteger = 2,
+    kPrefsTypeString  = 3,
+    kPrefsTypeBuffer  = 4,
+    kPrefsTypeBinary  = 5
+};
+
+namespace chip {
+namespace DeviceLayer {
+namespace Internal {
+
+// *** CAUTION ***: Changing the names or namespaces of these values will *break* existing devices.
+
+// NVS namespaces used to store device configuration information.
+const char BekenConfig::kConfigNamespace_ChipFactory[]  = "chip-factory";
+const char BekenConfig::kConfigNamespace_ChipConfig[]   = "chip-config";
+const char BekenConfig::kConfigNamespace_ChipCounters[] = "chip-counters";
+
+// Keys stored in the chip-factory namespace
+const BekenConfig::Key BekenConfig::kConfigKey_SerialNum             = { kConfigNamespace_ChipFactory, "serial-num" };
+const BekenConfig::Key BekenConfig::kConfigKey_MfrDeviceId           = { kConfigNamespace_ChipFactory, "device-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_MfrDeviceCert         = { kConfigNamespace_ChipFactory, "device-cert" };
+const BekenConfig::Key BekenConfig::kConfigKey_MfrDeviceICACerts     = { kConfigNamespace_ChipFactory, "device-ca-certs" };
+const BekenConfig::Key BekenConfig::kConfigKey_MfrDevicePrivateKey   = { kConfigNamespace_ChipFactory, "device-key" };
+const BekenConfig::Key BekenConfig::kConfigKey_HardwareVersion       = { kConfigNamespace_ChipFactory, "hardware-ver" };
+const BekenConfig::Key BekenConfig::kConfigKey_ManufacturingDate     = { kConfigNamespace_ChipFactory, "mfg-date" };
+const BekenConfig::Key BekenConfig::kConfigKey_SetupPinCode          = { kConfigNamespace_ChipFactory, "pin-code" };
+const BekenConfig::Key BekenConfig::kConfigKey_SetupDiscriminator    = { kConfigNamespace_ChipFactory, "discriminator" };
+const BekenConfig::Key BekenConfig::kConfigKey_Spake2pIterationCount = { kConfigNamespace_ChipFactory, "iteration-count" };
+const BekenConfig::Key BekenConfig::kConfigKey_Spake2pSalt           = { kConfigNamespace_ChipFactory, "salt" };
+const BekenConfig::Key BekenConfig::kConfigKey_Spake2pVerifier       = { kConfigNamespace_ChipFactory, "verifier" };
+const BekenConfig::Key BekenConfig::kConfigKey_UniqueId              = { kConfigNamespace_ChipFactory, "uniqueId" };
+
+// Keys stored in the chip-config namespace
+const BekenConfig::Key BekenConfig::kConfigKey_FabricId                    = { kConfigNamespace_ChipConfig, "fabric-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_ServiceConfig               = { kConfigNamespace_ChipConfig, "service-config" };
+const BekenConfig::Key BekenConfig::kConfigKey_PairedAccountId             = { kConfigNamespace_ChipConfig, "account-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_ServiceId                   = { kConfigNamespace_ChipConfig, "service-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_GroupKeyIndex               = { kConfigNamespace_ChipConfig, "group-key-index" };
+const BekenConfig::Key BekenConfig::kConfigKey_LastUsedEpochKeyId          = { kConfigNamespace_ChipConfig, "last-ek-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_FailSafeArmed               = { kConfigNamespace_ChipConfig, "fail-safe-armed" };
+const BekenConfig::Key BekenConfig::kConfigKey_WiFiStationSecType          = { kConfigNamespace_ChipConfig, "sta-sec-type" };
+const BekenConfig::Key BekenConfig::kConfigKey_OperationalDeviceId         = { kConfigNamespace_ChipConfig, "op-device-id" };
+const BekenConfig::Key BekenConfig::kConfigKey_OperationalDeviceCert       = { kConfigNamespace_ChipConfig, "op-device-cert" };
+const BekenConfig::Key BekenConfig::kConfigKey_OperationalDeviceICACerts   = { kConfigNamespace_ChipConfig, "op-device-ca-certs" };
+const BekenConfig::Key BekenConfig::kConfigKey_OperationalDevicePrivateKey = { kConfigNamespace_ChipConfig, "op-device-key" };
+const BekenConfig::Key BekenConfig::kConfigKey_RegulatoryLocation          = { kConfigNamespace_ChipConfig, "regulatory-location" };
+const BekenConfig::Key BekenConfig::kConfigKey_CountryCode                 = { kConfigNamespace_ChipConfig, "country-code" };
+const BekenConfig::Key BekenConfig::kConfigKey_Breadcrumb                  = { kConfigNamespace_ChipConfig, "breadcrumb" };
+
+// Keys stored in the Chip-counters namespace
+const BekenConfig::Key BekenConfig::kCounterKey_RebootCount           = { kConfigNamespace_ChipCounters, "reboot-count" };
+const BekenConfig::Key BekenConfig::kCounterKey_UpTime                = { kConfigNamespace_ChipCounters, "up-time" };
+const BekenConfig::Key BekenConfig::kCounterKey_TotalOperationalHours = { kConfigNamespace_ChipCounters, "total-hours" };
+const BekenConfig::Key BekenConfig::kCounterKey_BootReason            = { kConfigNamespace_ChipCounters, "boot-reason" };
+
+CHIP_ERROR BekenConfig::ReadConfigValue(Key key, bool & val)
+{
+    uint32_t success    = 0;
+    uint32_t out_length = 0;
+    uint8_t intval      = 0;
+    success             = bk_read_data(key.Namespace, key.Name, (char *) &intval, 1, &out_length);
+
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "bk_read_data: %s  %s failed\n", (char *) key.Namespace, (char *) key.Name);
+
+    val = (intval != 0);
+    if (kNoErr == success)
+        return CHIP_NO_ERROR;
+    else
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+}
+
+CHIP_ERROR BekenConfig::ReadConfigValue(Key key, uint32_t & val)
+{
+    uint32_t success    = 0;
+    uint32_t out_length = 0;
+    uint32_t temp_data  = 0;
+
+    success = bk_read_data(key.Namespace, key.Name, (char *) &temp_data, sizeof(uint32_t), &out_length);
+
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "bk_read_data: %s  %s failed\n", (char *) key.Namespace, (char *) key.Name);
+    val = temp_data;
+
+    if (kNoErr == success)
+        return CHIP_NO_ERROR;
+    else
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+}
+
+CHIP_ERROR BekenConfig::ReadConfigValue(Key key, uint64_t & val)
+{
+    uint32_t success    = 0;
+    uint32_t out_length = 0;
+    uint64_t temp_data  = 0;
+
+    success = bk_read_data(key.Namespace, key.Name, (char *) &temp_data, sizeof(uint64_t), &out_length);
+
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "bk_read_data: %s  %s failed\n", (char *) key.Namespace, (char *) key.Name);
+    val = temp_data;
+
+    if (kNoErr == success)
+        return CHIP_NO_ERROR;
+    else
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+}
+
+CHIP_ERROR BekenConfig::ReadConfigValueStr(Key key, char * buf, size_t bufSize, size_t & outLen)
+{
+    uint32_t success    = 0;
+    uint32_t out_length = 0;
+
+    success = bk_read_data(key.Namespace, key.Name, buf, bufSize, &out_length);
+    outLen  = out_length;
+
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "bk_read_data: %s  %s failed\n", (char *) key.Namespace, (char *) key.Name);
+
+    if (kNoErr == success)
+    {
+        return CHIP_NO_ERROR;
+    }
+    else
+    {
+        outLen = 0;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+    }
+}
+
+CHIP_ERROR BekenConfig::ReadConfigValueBin(Key key, uint8_t * buf, size_t bufSize, size_t & outLen)
+{
+    uint32_t success    = 0;
+    uint32_t out_length = 0;
+
+    success = bk_read_data(key.Namespace, key.Name, (char *) buf, bufSize, &out_length);
+    outLen  = out_length;
+
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "bk_read_data: %s  %s failed\n", (char *) key.Namespace, (char *) key.Name);
+
+    if (kNoErr == success)
+    {
+        return CHIP_NO_ERROR;
+    }
+    else
+    {
+        outLen = 0;
+        return CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
+    }
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValue(Key key, bool val)
+{
+    uint32_t success = 0;
+    uint8_t value    = 0;
+
+    if (val == 1)
+    {
+        value = 1;
+    }
+    else
+    {
+        value = 0;
+    }
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) &value, 1);
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s  %s  %s failed\n", (char *) key.Namespace, (char *) key.Name,
+                     value ? "true" : "false");
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValue(Key key, uint32_t val)
+{
+    uint32_t success = 0;
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) &val, sizeof(val));
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s  %s = %lu(0x%lx) failed\n", (char *) key.Namespace, (char *) key.Name, val,
+                     val);
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValue(Key key, uint64_t val)
+{
+    uint32_t success = 0;
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) &val, sizeof(val));
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s  %s = %llu(0x%llx) failed\n", (char *) key.Namespace, (char *) key.Name, val,
+                     val);
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValueStr(Key key, const char * str)
+{
+    uint32_t success = 0;
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) str, strlen(str));
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s %s %s failed\n", (char *) key.Namespace, (char *) key.Name, (char *) str);
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValueStr(Key key, const char * str, size_t strLen)
+{
+    uint32_t success = 0;
+    strLen           = (strLen > strlen(str)) ? strlen(str) : strLen;
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) str, strLen);
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s %s %s failed\n", (char *) key.Namespace, (char *) key.Name, (char *) str);
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::WriteConfigValueBin(Key key, const uint8_t * data, size_t dataLen)
+{
+    uint32_t success = 0;
+
+    success = bk_write_data(key.Namespace, key.Name, (char *) data, dataLen);
+    if (kNoErr != success)
+        ChipLogError(DeviceLayer, "bk_write_data: %s  %s failed \r\n", key.Namespace, key.Name);
+
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::ClearConfigValue(Key key)
+{
+    uint32_t success = 0;
+
+    success = bk_clean_data(key.Namespace, key.Name);
+    if (kNoErr != success)
+        ChipLogProgress(DeviceLayer, "%s : %s  %s failed\n", __FUNCTION__, key.Namespace, key.Name);
+    return CHIP_NO_ERROR;
+}
+
+bool BekenConfig::ConfigValueExists(Key key)
+{
+    if (kNoErr == bk_ensure_name_data(key.Namespace, key.Name))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+CHIP_ERROR BekenConfig::EnsureNamespace(const char * ns)
+{
+    uint32_t success = 0;
+
+    success = bk_ensure_namespace(ns);
+    if (kNoErr != success)
+    {
+        ChipLogError(DeviceLayer, "dct_register_module failed\n");
+    }
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenConfig::ClearNamespace(const char * ns)
+{
+    uint32_t success = 0;
+
+    success = bK_clear_namespace(ns);
+    if (success != 0)
+    {
+        ChipLogError(DeviceLayer, "ClearNamespace failed\n");
+    }
+    return CHIP_NO_ERROR;
+}
+
+void BekenConfig::RunConfigUnitTest() {}
+
+} // namespace Internal
+} // namespace DeviceLayer
+} // namespace chip
---
+++ src/platform/Beken/NetworkCommissioningWiFiDriver.cpp
@@ -0,0 +1,366 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <lib/support/CodeUtils.h>
+#include <lib/support/SafeInt.h>
+#include <platform/Beken/NetworkCommissioningDriver.h>
+#include <platform/CHIPDeviceLayer.h>
+
+#include <limits>
+#include <string>
+
+#include "wlan_ui_pub.h"
+
+using namespace ::chip;
+#if CHIP_DEVICE_CONFIG_ENABLE_WIFI
+namespace chip {
+namespace DeviceLayer {
+namespace NetworkCommissioning {
+
+namespace {
+constexpr char kWiFiSSIDKeyName[]        = "wifi-ssid";
+constexpr char kWiFiCredentialsKeyName[] = "wifi-pass";
+} // namespace
+
+CHIP_ERROR BekenWiFiDriver::Init(NetworkStatusChangeCallback * networkStatusChangeCallback)
+{
+    CHIP_ERROR err;
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::Init\r\n");
+
+    err = GetSavedNetWorkConfig(&mSavedNetwork);
+
+    mStagingNetwork        = mSavedNetwork;
+    mpScanCallback         = nullptr;
+    mpConnectCallback      = nullptr;
+    mpStatusChangeCallback = networkStatusChangeCallback;
+    return err;
+}
+
+CHIP_ERROR BekenWiFiDriver::GetSavedNetWorkConfig(WiFiNetwork * WifiNetconf)
+{
+    CHIP_ERROR err;
+    size_t ssidLen        = 0;
+    size_t credentialsLen = 0;
+
+    memset(WifiNetconf, 0x0, sizeof(WiFiNetwork));
+    err = PersistedStorage::KeyValueStoreMgr().Get(kWiFiCredentialsKeyName, WifiNetconf->credentials,
+                                                   sizeof(WifiNetconf->credentials), &credentialsLen);
+    if (err == CHIP_ERROR_NOT_FOUND)
+    {
+        return CHIP_NO_ERROR;
+    }
+
+    err = PersistedStorage::KeyValueStoreMgr().Get(kWiFiSSIDKeyName, WifiNetconf->ssid, sizeof(WifiNetconf->ssid), &ssidLen);
+    if (err == CHIP_ERROR_NOT_FOUND)
+    {
+        return CHIP_NO_ERROR;
+    }
+    WifiNetconf->credentialsLen = credentialsLen;
+    WifiNetconf->ssidLen        = ssidLen;
+    return err;
+}
+
+void BekenWiFiDriver::Shutdown()
+{
+    mpStatusChangeCallback = nullptr;
+}
+
+CHIP_ERROR BekenWiFiDriver::CommitConfiguration()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::CommitConfiguration\r\n");
+    ReturnErrorOnFailure(PersistedStorage::KeyValueStoreMgr().Put(kWiFiSSIDKeyName, mStagingNetwork.ssid, mStagingNetwork.ssidLen));
+    ReturnErrorOnFailure(PersistedStorage::KeyValueStoreMgr().Put(kWiFiCredentialsKeyName, mStagingNetwork.credentials,
+                                                                  mStagingNetwork.credentialsLen));
+    mSavedNetwork = mStagingNetwork;
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR BekenWiFiDriver::RevertConfiguration()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::RevertConfiguration\r\n");
+    mStagingNetwork = mSavedNetwork;
+    return CHIP_NO_ERROR;
+}
+
+bool BekenWiFiDriver::NetworkMatch(const WiFiNetwork & network, ByteSpan networkId)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::NetworkMatch\r\n");
+    return networkId.size() == network.ssidLen && memcmp(networkId.data(), network.ssid, network.ssidLen) == 0;
+}
+
+Status BekenWiFiDriver::AddOrUpdateNetwork(ByteSpan ssid, ByteSpan credentials, MutableCharSpan & outDebugText,
+                                           uint8_t & outNetworkIndex)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::AddOrUpdateNetwork\r\n");
+
+    outDebugText.reduce_size(0);
+    outNetworkIndex = 0;
+    VerifyOrReturnError(mStagingNetwork.ssidLen == 0 || NetworkMatch(mStagingNetwork, ssid), Status::kBoundsExceeded);
+    VerifyOrReturnError(credentials.size() <= sizeof(mStagingNetwork.credentials), Status::kOutOfRange);
+    VerifyOrReturnError(ssid.size() <= sizeof(mStagingNetwork.ssid), Status::kOutOfRange);
+
+    memcpy(mStagingNetwork.credentials, credentials.data(), credentials.size());
+    mStagingNetwork.credentialsLen = static_cast<decltype(mStagingNetwork.credentialsLen)>(credentials.size());
+
+    memcpy(mStagingNetwork.ssid, ssid.data(), ssid.size());
+    mStagingNetwork.ssidLen = static_cast<decltype(mStagingNetwork.ssidLen)>(ssid.size());
+
+    return Status::kSuccess;
+}
+
+Status BekenWiFiDriver::RemoveNetwork(ByteSpan networkId, MutableCharSpan & outDebugText, uint8_t & outNetworkIndex)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::RemoveNetwork\r\n");
+
+    outDebugText.reduce_size(0);
+    outNetworkIndex = 0;
+    VerifyOrReturnError(NetworkMatch(mStagingNetwork, networkId), Status::kNetworkIDNotFound);
+
+    // Use empty ssid for representing invalid network
+    mStagingNetwork.ssidLen = 0;
+    return Status::kSuccess;
+}
+
+Status BekenWiFiDriver::ReorderNetwork(ByteSpan networkId, uint8_t index, MutableCharSpan & outDebugText)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::ReorderNetwork\r\n");
+    // Only one network is supported now
+    outDebugText.reduce_size(0);
+    VerifyOrReturnError(index == 0, Status::kOutOfRange);
+    VerifyOrReturnError(NetworkMatch(mStagingNetwork, networkId), Status::kNetworkIDNotFound);
+    return Status::kSuccess;
+}
+
+CHIP_ERROR BekenWiFiDriver::ConnectWiFiNetwork(const char * ssid, uint8_t ssidLen, const char * key, uint8_t keyLen)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::ConnectWiFiNetwork....ssid:%s", ssid);
+    ReturnErrorOnFailure(ConnectivityMgr().SetWiFiStationMode(ConnectivityManager::kWiFiStationMode_Disabled));
+
+    wifi_sta_config_t sta_config;
+    memset(&sta_config, 0x0, sizeof(sta_config));
+    sta_config.security = WIFI_SECURITY_AUTO; // can't use WIFI_DEFAULT_STA_CONFIG because of C99 designator error
+    strncpy(sta_config.ssid, ssid, ssidLen);
+    strncpy(sta_config.password, key, keyLen);
+
+    BK_LOG_ON_ERR(bk_wifi_sta_set_config(&sta_config));
+    BK_LOG_ON_ERR(bk_wifi_sta_start());
+    BK_LOG_ON_ERR(bk_wifi_sta_connect());
+
+    return ConnectivityMgr().SetWiFiStationMode(ConnectivityManager::kWiFiStationMode_Enabled);
+}
+
+void BekenWiFiDriver::OnConnectWiFiNetwork()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::OnConnectWiFiNetwork\r\n");
+    if (mpConnectCallback)
+    {
+        // chip::DeviceLayer::PlatformMgr().LockChipStack();
+        mpConnectCallback->OnResult(Status::kSuccess, CharSpan(), 0);
+        // chip::DeviceLayer::PlatformMgr().UnlockChipStack();
+        mpConnectCallback = nullptr;
+    }
+}
+
+void BekenWiFiDriver::ConnectNetwork(ByteSpan networkId, ConnectCallback * callback)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::ConnectNetwork\r\n");
+    CHIP_ERROR err          = CHIP_NO_ERROR;
+    Status networkingStatus = Status::kSuccess;
+
+    VerifyOrExit(NetworkMatch(mStagingNetwork, networkId), networkingStatus = Status::kNetworkIDNotFound);
+    VerifyOrExit(mpConnectCallback == nullptr, networkingStatus = Status::kUnknownError);
+
+    err               = ConnectWiFiNetwork(reinterpret_cast<const char *>(mStagingNetwork.ssid), mStagingNetwork.ssidLen,
+                             reinterpret_cast<const char *>(mStagingNetwork.credentials), mStagingNetwork.credentialsLen);
+    mpConnectCallback = callback;
+exit:
+    if (err != CHIP_NO_ERROR)
+    {
+        networkingStatus = Status::kUnknownError;
+    }
+    if (networkingStatus != Status::kSuccess)
+    {
+        ChipLogError(NetworkProvisioning, "Failed to connect to WiFi network:%s", chip::ErrorStr(err));
+        mpConnectCallback = nullptr;
+        // chip::DeviceLayer::PlatformMgr().LockChipStack();
+        callback->OnResult(networkingStatus, CharSpan(), 0);
+        // chip::DeviceLayer::PlatformMgr().UnlockChipStack();
+    }
+}
+
+CHIP_ERROR GetConnectedNetwork(Network & network)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::GetConnectedNetwork\r\n");
+    wifi_link_status_t wifi_setting;
+    memset(&wifi_setting, 0x0, sizeof(wifi_setting));
+    bk_wifi_sta_get_link_status(&wifi_setting);
+    uint8_t length = strnlen(reinterpret_cast<const char *>(wifi_setting.ssid), DeviceLayer::Internal::kMaxWiFiSSIDLength);
+
+    os_memcpy(network.networkID, wifi_setting.ssid, length);
+    ChipLogProgress(NetworkProvisioning, "networkID:[%s][%d]\r\n", network.networkID, length);
+    network.networkIDLen = length;
+    return CHIP_NO_ERROR;
+}
+
+void BekenWiFiDriver::OnNetworkStatusChange()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::OnNetworkStatusChange\r\n");
+    Network configuredNetwork = { 0 };
+
+    VerifyOrReturn(mpStatusChangeCallback != nullptr);
+    GetConnectedNetwork(configuredNetwork);
+
+    if (configuredNetwork.networkIDLen)
+    {
+        mpStatusChangeCallback->OnNetworkingStatusChange(
+            Status::kSuccess, MakeOptional(ByteSpan(configuredNetwork.networkID, configuredNetwork.networkIDLen)), NullOptional);
+        return;
+    }
+    mpStatusChangeCallback->OnNetworkingStatusChange(
+        Status::kUnknownError, MakeOptional(ByteSpan(configuredNetwork.networkID, configuredNetwork.networkIDLen)),
+        MakeOptional(GetLastDisconnectReason()));
+}
+
+CHIP_ERROR BekenWiFiDriver::SetLastDisconnectReason(const ChipDeviceEvent * event)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::SetLastDisconnectReason\r\n");
+    mLastDisconnectedReason = event->Platform.BKSystemEvent.Data.WiFiStaDisconnected;
+    return CHIP_NO_ERROR;
+}
+
+int32_t BekenWiFiDriver::GetLastDisconnectReason()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::GetLastDisconnectReason\r\n");
+    return mLastDisconnectedReason;
+}
+
+static beken_semaphore_t matter_scan_sema = NULL;
+
+int scan_done_handler(void * arg, event_module_t event_module, int event_id, void * event_data)
+{
+    if (matter_scan_sema)
+    {
+        rtos_set_semaphore(&matter_scan_sema);
+    }
+    return BK_OK;
+}
+
+CHIP_ERROR BekenWiFiDriver::StartScanWiFiNetworks(ByteSpan ssid)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::StartScanWiFiNetworks\r\n");
+    wifi_scan_config_t config = { 0 };
+    if (ssid.data() == NULL) // non-directed scanning
+    {
+        ChipLogProgress(NetworkProvisioning, "non-directed scanning...\r\n");
+        BK_LOG_ON_ERR(bk_wifi_scan_start(NULL));
+    }
+    else // directed scanning
+    {
+        os_memcpy(config.ssid, ssid.data(), ssid.size());
+        ChipLogProgress(NetworkProvisioning, "directed scanning... ssid:%s ; %d \r\n", config.ssid, ssid.size());
+        BK_LOG_ON_ERR(bk_wifi_scan_start(&config));
+    }
+    return CHIP_NO_ERROR;
+}
+
+void BekenWiFiDriver::OnScanWiFiNetworkDone()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::OnScanWiFiNetworkDone\r\n");
+    if (!GetInstance().mpScanCallback)
+    {
+        ChipLogProgress(NetworkProvisioning, "can't find the ScanCallback function\r\n");
+        return;
+    }
+    wifi_scan_result_t scan_result = { 0 };
+    int scan_rst_ap_num            = 0;
+    BK_LOG_ON_ERR(bk_wifi_scan_get_result(&scan_result));
+    scan_rst_ap_num = scan_result.ap_num;
+    if (scan_rst_ap_num < 2) // beken scan result > = 1
+    {
+        ChipLogProgress(NetworkProvisioning, "NULL AP\r\n");
+        GetInstance().mpScanCallback->OnFinished(Status::kNetworkNotFound, CharSpan(), nullptr);
+    }
+    else
+    {
+        ChipLogProgress(NetworkProvisioning, "AP num = %d\r\n", scan_rst_ap_num);
+        BKScanResponseIterator iter(scan_rst_ap_num, &scan_result);
+        GetInstance().mpScanCallback->OnFinished(Status::kSuccess, CharSpan(), &iter);
+    }
+    GetInstance().mpScanCallback = nullptr;
+    bk_wifi_scan_free_result(&scan_result);
+}
+
+void BekenWiFiDriver::ScanNetworks(ByteSpan ssid, WiFiDriver::ScanCallback * callback)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::ScanNetworks\r\n");
+    if (callback != nullptr)
+    {
+        mpScanCallback = callback;
+        if (matter_scan_sema == NULL)
+        {
+            BK_LOG_ON_ERR(rtos_init_semaphore(&matter_scan_sema, 1));
+        }
+        BK_LOG_ON_ERR(bk_event_register_cb(EVENT_MOD_WIFI, EVENT_WIFI_SCAN_DONE, scan_done_handler, NULL));
+        if (StartScanWiFiNetworks(ssid) != CHIP_NO_ERROR)
+        {
+            mpScanCallback = nullptr;
+            callback->OnFinished(Status::kUnknownError, CharSpan(), nullptr);
+        }
+        BK_LOG_ON_ERR(rtos_get_semaphore(&matter_scan_sema, 4000)); // timeout 4000ms
+        BK_LOG_ON_ERR(bk_event_unregister_cb(EVENT_MOD_WIFI, EVENT_WIFI_SCAN_DONE, scan_done_handler));
+        BK_LOG_ON_ERR(rtos_deinit_semaphore(&matter_scan_sema));
+        matter_scan_sema = NULL;
+        OnScanWiFiNetworkDone();
+    }
+}
+
+size_t BekenWiFiDriver::WiFiNetworkIterator::Count()
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::WiFiNetworkIterator::Count\r\n");
+    return mDriver->mStagingNetwork.ssidLen == 0 ? 0 : 1;
+}
+
+bool BekenWiFiDriver::WiFiNetworkIterator::Next(Network & item)
+{
+    ChipLogProgress(NetworkProvisioning, "BekenWiFiDriver::WiFiNetworkIterator::Next\r\n");
+    if (mExhausted || mDriver->mStagingNetwork.ssidLen == 0)
+    {
+        return false;
+    }
+    uint8_t length =
+        strnlen(reinterpret_cast<const char *>(mDriver->mStagingNetwork.ssid), DeviceLayer::Internal::kMaxWiFiSSIDLength);
+    memcpy(item.networkID, mDriver->mStagingNetwork.ssid, length);
+    item.networkIDLen        = length;
+    item.connected           = false;
+    mExhausted               = true;
+    Network connectedNetwork = { 0 };
+    CHIP_ERROR err           = GetConnectedNetwork(connectedNetwork);
+    if (err == CHIP_NO_ERROR)
+    {
+        if (connectedNetwork.networkIDLen == item.networkIDLen &&
+            memcmp(connectedNetwork.networkID, item.networkID, item.networkIDLen) == 0)
+        {
+            item.connected = true;
+        }
+    }
+    return true;
+}
+
+} // namespace NetworkCommissioning
+} // namespace DeviceLayer
+} // namespace chip
+#endif // CHIP_DEVICE_CONFIG_ENABLE_WIFI
