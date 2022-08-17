f3abf77f3f1a429dc8fe97dd8ac9825a0efe7f6f
Shubham Patil <shubham.patil@espressif.com>
2022-08-17 13:31:13 +0530

    [ESP32] Fix some incorrect log levels in lighting-app

--- examples/lighting-app/esp32/main/AppTask.cpp
+++ examples/lighting-app/esp32/main/AppTask.cpp
@@ -161,7 +161,7 @@ void AppTask::UpdateClusterState()
         ESP_LOGE(TAG, "Updating on/off cluster failed: %x", status);
     }
 
-    ESP_LOGE(TAG, "Writting to Current Level cluster");
+    ESP_LOGI(TAG, "Writting to Current Level cluster");
     status = Clusters::LevelControl::Attributes::CurrentLevel::Set(kLightEndpointId, AppLED.GetLevel());
 
     if (status != EMBER_ZCL_STATUS_SUCCESS)
