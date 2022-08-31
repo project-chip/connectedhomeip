ad09dd86d43d82378e9ddf93eafe2b7648523d6d
Boris Zbarsky <bzbarsky@apple.com>
2022-08-31 17:58:02 -0400

    Only do address updates for CASE delivery failures.
    When the handling of "failed to send message, so update the IP for this peer"
    moved out of DeviceCommissioner, we stopped checking whether the session is a
    CASE session.  Go back to checking that again.
    
    Fixes https://github.com/project-chip/connectedhomeip/issues/22326

--- src/app/OperationalSessionSetup.cpp
+++ src/app/OperationalSessionSetup.cpp
@@ -411,7 +411,7 @@ void OperationalSessionSetup::PerformAddressUpdate()
     CHIP_ERROR err = LookupPeerAddress();
     if (err != CHIP_NO_ERROR)
     {
-        ChipLogError(Controller, "PerformAddressUpdate could not perform lookup");
+        ChipLogError(Controller, "PerformAddressUpdate could not perform lookup: %" CHIP_ERROR_FORMAT, err.Format());
         DequeueConnectionCallbacks(err);
         // Do not touch `this` instance anymore; it has been destroyed in DequeueConnectionCallbacks.
         return;
