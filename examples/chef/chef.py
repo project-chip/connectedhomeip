6eee6266b702f430b74f7e59d5c32a605eb89954
pankore <86098180+pankore@users.noreply.github.com>
2022-09-14 14:38:58 +0800

    [Chef] Add make is command into z2 build

--- examples/chef/chef.py
+++ examples/chef/chef.py
@@ -681,6 +681,7 @@ def main() -> int:
                 if options.do_clean:
                     shell.run_cmd("make clean")
                 shell.run_cmd("make chef")
+                shell.run_cmd("make is")
 
 
         elif options.build_target == "linux":
