aa64ab3b63f9c2e134d444649de93874abbf78a1
Vivien Nicolas <vnicolas@apple.com>
2022-09-20 09:04:04 +0200

    [ZAP] Increase maximum memory usage to 9gb instead of 8gb

--- scripts/tools/zap/generate.py
+++ scripts/tools/zap/generate.py
@@ -212,8 +212,8 @@ def runJavaPrettifier(templates_file, output_dir):
 def main():
     checkPythonVersion()
 
-    # The maximum meory usage is over 4GB (#15620)
-    os.environ["NODE_OPTIONS"] = "--max-old-space-size=8192"
+    # The maximum memory usage is over 4GB (#15620)
+    os.environ["NODE_OPTIONS"] = "--max-old-space-size=9216"
     zap_file, zcl_file, templates_file, output_dir = runArgumentsParser()
     runGeneration(zap_file, zcl_file, templates_file, output_dir)
 
