5f5e6bed767171fd790632a2b57f75cd16e7b47e
Restyled.io <commits@restyled.io>
2022-09-27 17:36:29 +0000

    Restyled by shellharden

--- examples/chef/create_docker.sh
+++ examples/chef/create_docker.sh
@@ -9,8 +9,8 @@ mkdir /workspace/examples/chef/tmp/
 tar -xf /workspace/artifacts/linux_x86-rootnode_dimmablelight_bCwGYSDpoe.tar.gz -C /workspace/examples/chef/tmp
 
 # Build the docker image
-docker build -f /workspace/examples/chef/dockerfile -t $IMAGE_NAME:latest -t $IMAGE_NAME:short-sha_$SHORT_SHA \
-    -t $IMAGE_NAME:build-id_$BUILD_ID -t $IMAGE_NAME:commit-sha_$COMMIT_SHA -t $IMAGE_NAME:revision-id_$REVISION_ID .
+docker build -f /workspace/examples/chef/dockerfile -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:short-sha_$SHORT_SHA" \
+    -t "$IMAGE_NAME:build-id_$BUILD_ID" -t "$IMAGE_NAME:commit-sha_$COMMIT_SHA" -t "$IMAGE_NAME:revision-id_$REVISION_ID" .
 
 # Cleanup the extracted files
 rm -r /workspace/examples/chef/tmp
