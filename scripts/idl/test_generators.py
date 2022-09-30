b9352073410d7244a55a4e45588ea28a93025e22
Restyled.io <commits@restyled.io>
2022-09-30 18:56:14 +0000

    Restyled by gn & fix codegen test

--- scripts/idl/test_generators.py
+++ scripts/idl/test_generators.py
@@ -32,7 +32,7 @@ except:
 
 from idl.matter_idl_types import Idl
 from idl.generators.java import JavaGenerator
-from idl.generators.cpp import CppGenerator
+from idl.generators.bridge import BridgeGenerator
 from idl.generators import GeneratorStorage
 
 
@@ -101,8 +101,8 @@ class GeneratorTest:
     def _create_generator(self, storage: GeneratorStorage, idl: Idl):
         if self.generator_name.lower() == 'java':
             return JavaGenerator(storage, idl)
-        if self.generator_name.lower() == 'cpp':
-            return CppGenerator(storage, idl)
+        if self.generator_name.lower() == 'bridge':
+            return BridgeGenerator(storage, idl)
         else:
             raise Exception("Unknown generator for testing: %s",
                             self.generator_name.lower())
