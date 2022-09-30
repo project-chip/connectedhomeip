25eab4cd1f6df3a600d5501d94468beb934a2783
Restyled.io <commits@restyled.io>
2022-09-30 20:37:27 +0000

    Restyled by shellharden

--- examples/chef/create_docker.sh
+++ examples/chef/create_docker.sh
@@ -20,12 +20,12 @@ set -e # Exit on any script error.
 OUT_DIRECTORY="$WORKING_DIR/out/"
 
 # Clean up the out directory before extracting device files
-rm -r $OUT_DIRECTORY*
+rm -r "$OUT_DIRECTORY"*
 
 # Extract device files
-tar -xf $TAR_PATH -C $OUT_DIRECTORY
+tar -xf "$TAR_PATH" -C "$OUT_DIRECTORY"
 
 # Build the docker image
-docker build -f $WORKING_DIR/dockerfile -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:short-sha_$SHORT_SHA" \
+docker build -f "$WORKING_DIR"/dockerfile -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:short-sha_$SHORT_SHA" \
     -t "$IMAGE_NAME:build-id_$BUILD_ID" -t "$IMAGE_NAME:commit-sha_$COMMIT_SHA" -t "$IMAGE_NAME:revision-id_$REVISION_ID" \
-    --build-arg DEVICE_NAME=$DEVICE_NAME .
+    --build-arg DEVICE_NAME="$DEVICE_NAME" .
