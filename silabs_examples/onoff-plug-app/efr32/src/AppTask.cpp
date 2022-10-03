82c1f9d41b6ae09ebc9aa079af0701d23204c1ba
Mathieu Kardous <mathieu.kardous@silabs.com>
2022-10-03 09:03:56 -0400

    Remove old bluetooth header files

--- silabs_examples/onoff-plug-app/efr32/src/AppTask.cpp
+++ silabs_examples/onoff-plug-app/efr32/src/AppTask.cpp
@@ -42,8 +42,6 @@
 #include <setup_payload/QRCodeSetupPayloadGenerator.h>
 #include <setup_payload/SetupPayload.h>
 
-#include <platform/EFR32/freertos_bluetooth.h>
-
 #include <lib/support/CodeUtils.h>
 
 #include <platform/CHIPDeviceLayer.h>
