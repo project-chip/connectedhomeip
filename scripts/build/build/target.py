6f395586f00d0ad4e036e5494f9d269828c026c2
Andrei Litvin <andy314@gmail.com>
2022-10-05 18:02:32 -0400

    More updates

--- scripts/build/build/target.py
+++ scripts/build/build/target.py
@@ -77,12 +77,14 @@ class TargetPart:
     def Accept(self, full_input: str):
         if self.except_if_re:
             if self.except_if_re.search(full_input):
-                logging.warning(f"'{self.name}' does not support '{full_input}' due to rule EXCEPT IF '{self.except_if_re.pattern}'")
+                # likely nothing will match when we get such an error
+                logging.error(f"'{self.name}' does not support '{full_input}' due to rule EXCEPT IF '{self.except_if_re.pattern}'")
                 return False
 
         if self.only_if_re:
             if not self.only_if_re.search(full_input):
-                logging.warning(f"{self.name} does not support '{full_input}' due to rule ONLY IF '{self.only_if_re.pattern}'")
+                # likely nothing will match when we get such an error
+                logging.error(f"'{self.name}' does not support '{full_input}' due to rule ONLY IF '{self.only_if_re.pattern}'")
                 return False
 
         return True
