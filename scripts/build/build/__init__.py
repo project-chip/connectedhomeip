2bd76db9bef6c196b97fd1963a021f514903603c
Andrei Litvin <andy314@gmail.com>
2022-10-11 13:39:21 -0400

    Update target/variant/modifier logic in `build_examples.py` (#23054)
    * Starting to add some build target logic and unit tests
    
    * Restyle
    
    * make basic unit tests pass
    
    * More unit tests
    
    * Restyle
    
    * Update prefix logic: use the builder prefix as well
    
    * Prepare to move build targets for linux
    
    * More conversion tests
    
    * more conversions
    
    * Converted more targets
    
    * Converted more targets
    
    * Updted more targets
    
    * Simplify target part
    
    * Restyle
    
    * All applications are converted
    
    * Remove old target code - should not be needed anymore
    
    * Restyle
    
    * Building at least chip-tool works. No introspection though
    
    * Fix typo in m5 builds
    
    * A bit easier to parse logging
    
    * More updates
    
    * Add ability to output all variants of some target
    
    * Restyle
    
    * Add the ability to print all possible targets
    
    * Add esp32 qemu support
    
    * Restyle
    
    * Fix typo
    
    * Simplify target printout, make nrf tests work
    
    * Restyle
    
    * Remove some usages of target glob
    
    * Replace glob in build scripts
    
    * Restyle
    
    * convert some globs into instructions, fix some target compile bugs
    
    * make unit tests pass
    
    * more unit tests
    
    * Restyle
    
    * Remove the default of "all" in build targets since that does not mean anything anymore
    
    * Rename arm64-clang to arm64 in targets for cloudbuild smoke test
    
    * For arm64, clang is a modifier that is required
    
    * Remove some whitespace.Tested that targets are ok
    
    * Fix ameba
    
    * Fix tizen
    
    * Fix infineon
    
    * Fix qpg
    
    * Fix infineon, cc13x2x7 and update android a bit
    
    * Fix linux standalone
    
    * Fix efr32
    
    * Fix esp32
    
    * Remove glob option from tasks.json
    
    * Fix platform naming for targets - fixes darwin target naming
    
    * Fix k32w targets - no more release
    
    * Adjust arm64 cross compile target names
    
    * Fix boufallolab app target
    
    * All-clusters full thread device does not link (insufficient ram/flash) on cc13x2x7. Updating to minimal thread device only
    
    * Update k32w example builds: nologs is mandatory for low-power builds
    
    * Fix output dir for boufallolab: output should be all lowercase
    
    * Add back the test/extra_tests variant to host builds
    
    * Fix qpg size reports
    
    * Update unit tests for available outpust now that test variant was added
    
    * Fix expected paths for cc13x2x7 builds
    
    * Fix typo in infineon build
    
    * Fix BL702 path for output - duplicate boufallolab
    
    * Allow rpc modifier for BL706
    
    * Fix unit tests
    
    * Move nltestrunner to esp32 test runner and include in cloudbuild
    
    * Undo duplicate build target addition
    
    * Fix typo: esp32 -> efr32
    
    * Split out fake platform: only tests are really supported there

--- scripts/build/build/__init__.py
+++ scripts/build/build/__init__.py
@@ -4,9 +4,7 @@ import shutil
 from enum import Enum, auto
 from typing import Sequence
 
-from .targets import ALL, Target
-
-ALL_TARGETS = ALL
+from .targets import BUILD_TARGETS
 
 
 class BuildSteps(Enum):
@@ -26,7 +24,7 @@ class Context:
         self.output_prefix = output_prefix
         self.completed_steps = set()
 
-    def SetupBuilders(self, targets: Sequence[Target],
+    def SetupBuilders(self, targets: Sequence[str],
                       enable_flashbundle: bool):
         """
         Configures internal builders for the given platform/board/app
@@ -35,9 +33,15 @@ class Context:
 
         self.builders = []
         for target in targets:
-            self.builders.append(target.Create(
-                self.runner, self.repository_path, self.output_prefix,
-                enable_flashbundle))
+            found = False
+            for choice in BUILD_TARGETS:
+                builder = choice.Create(target, self.runner, self.repository_path, self.output_prefix, enable_flashbundle)
+                if builder:
+                    self.builders.append(builder)
+                    found = True
+
+            if not found:
+                logging.error(f"Target '{target}' could not be found. Nothing executed for it")
 
         # whenever builders change, assume generation is required again
         self.completed_steps.discard(BuildSteps.GENERATED)
