project-chip/connectedhomeip

commit 15ac91713ad51bee8fed30f956388eeec3f5f55a

Use CopyString() Instead strncpy() (#22708)

examples/chip-tool/commands/clusters/ComplexArgument.h
@@ -24,6 +24,7 @@
 #include <json/json.h>
 #include <lib/core/Optional.h>
 #include <lib/support/BytesToHex.h>
+#include <lib/support/CHIPMemString.h>
 #include <lib/support/SafeInt.h>
 
 #include "JsonParser.h"
@@ -195,7 +196,7 @@ public:
 
         size_t size = strlen(value.asCString());
         auto buffer = static_cast<char *>(chip::Platform::MemoryCalloc(size, sizeof(char)));
-        strncpy(buffer, value.asCString(), size);
+        chip::Platform::CopyString(buffer, size, value.asCString());
 
         request = chip::CharSpan(buffer, size);
         return CHIP_NO_ERROR;

examples/bridge-app/linux/Device.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2021 Project CHIP Authors
+ *    Copyright (c) 2021-2022 Project CHIP Authors
  *    Copyright (c) 2019 Google LLC.
  *    All rights reserved.
  *
@@ -24,11 +24,9 @@
 
 using namespace chip::app::Clusters::Actions;
 
-// LightingManager LightingManager::sLight;
-
 Device::Device(const char * szDeviceName, std::string szLocation)
 {
-    strncpy(mName, szDeviceName, sizeof(mName));
+    chip::Platform::CopyString(mName, szDeviceName);
     mLocation   = szLocation;
     mReachable  = false;
     mEndpointId = 0;
@@ -66,7 +64,7 @@ void Device::SetName(const char * szName)
 
     ChipLogProgress(DeviceLayer, "Device[%s]: New Name=\"%s\"", mName, szName);
 
-    strncpy(mName, szName, sizeof(mName));
+    chip::Platform::CopyString(mName, szName);
 
     if (changed)
     {

examples/ota-provider-app/linux/main.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -111,14 +111,13 @@ static bool ParseJsonFileAndPopulateCandidates(const char * filepath,
             candidate.vendorId        = static_cast<chip::VendorId>(iter.get("vendorId", 1).asUInt());
             candidate.productId       = static_cast<uint16_t>(iter.get("productId", 1).asUInt());
             candidate.softwareVersion = static_cast<uint32_t>(iter.get("softwareVersion", 10).asUInt64());
-            strncpy(candidate.softwareVersionString, iter.get("softwareVersionString", "1.0.0").asCString(),
-                    OTAProviderExample::SW_VER_STR_MAX_LEN);
+            chip::Platform::CopyString(candidate.softwareVersionString, iter.get("softwareVersionString", "1.0.0").asCString());
             candidate.cDVersionNumber              = static_cast<uint16_t>(iter.get("cDVersionNumber", 0).asUInt());
             candidate.softwareVersionValid         = iter.get("softwareVersionValid", true).asBool() ? true : false;
             candidate.minApplicableSoftwareVersion = static_cast<uint32_t>(iter.get("minApplicableSoftwareVersion", 0).asUInt64());
             candidate.maxApplicableSoftwareVersion =
                 static_cast<uint32_t>(iter.get("maxApplicableSoftwareVersion", 1000).asUInt64());
-            strncpy(candidate.otaURL, iter.get("otaURL", "https://test.com").asCString(), OTAProviderExample::OTA_URL_MAX_LEN);
+            chip::Platform::CopyString(candidate.otaURL, iter.get("otaURL", "https://test.com").asCString());
             candidates.push_back(candidate);
             ret = true;
         }

examples/platform/bouffalolab/bl602/Service.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ constexpr uint16_t kUDPBroadcastPort = 23367;
 
 void SetDeviceName(const char * newDeviceName)
 {
-    strncpy(sDeviceName, newDeviceName, sizeof(sDeviceName) - 1);
+    chip::Platform::CopyString(sDeviceName, newDeviceName);
 }
 
 void PublishService()

src/app/clusters/basic/basic.cpp
@@ -1,6 +1,6 @@
 /**
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -304,7 +304,7 @@ CHIP_ERROR BasicAttrAccess::ReadLocation(AttributeValueEncoder & aEncoder)
     CHIP_ERROR err = ConfigurationMgr().GetCountryCode(location, sizeof(location), codeLen);
     if ((err != CHIP_NO_ERROR) || (codeLen == 0))
     {
-        strncpy(&location[0], "XX", kMaxLen + 1);
+        Platform::CopyString(location, "XX");
         codeLen = strnlen(location, kMaxLen);
         err     = CHIP_NO_ERROR;
     }

src/app/tests/suites/commands/system/SystemCommands.cpp
@@ -18,6 +18,8 @@
 
 #include "SystemCommands.h"
 
+#include <lib/support/CHIPMemString.h>
+
 namespace {
 const char basePath[] = "./src/app/tests/suites/commands/system/scripts/";
 const char * getScriptsFolder()
@@ -45,8 +47,7 @@ CHIP_ERROR SystemCommands::Start(const char * identity, const chip::app::Cluster
     {
         VerifyOrReturnError(value.registerKey.Value().size() < 128, CHIP_ERROR_INVALID_ARGUMENT);
         char registerKey[128];
-        memset(registerKey, '\0', sizeof(registerKey));
-        strncpy(registerKey, value.registerKey.Value().data(), value.registerKey.Value().size());
+        chip::Platform::CopyString(registerKey, value.registerKey.Value());
         builder.Add(registerKey);
     }
     else
@@ -179,8 +180,7 @@ CHIP_ERROR SystemCommands::AddSystemCommandArgument(chip::StringBuilderBase & bu
     builder.Add(" ");
 
     char arg[kArgumentMaxLen];
-    memset(arg, 0, sizeof(arg));
-    strncpy(arg, argValue.data(), argValue.size());
+    chip::Platform::CopyString(arg, argValue);
     builder.Add(arg);
 
     return CHIP_NO_ERROR;

src/controller/CommissionerDiscoveryController.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020-2021 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *    Copyright (c) 2013-2017 Nest Labs, Inc.
  *    All rights reserved.
  *
@@ -51,7 +51,7 @@ void CommissionerDiscoveryController::OnUserDirectedCommissioningRequest(UDCClie
         return;
     }
     mReady = false;
-    strncpy(mCurrentInstance, state.GetInstanceName(), sizeof(mCurrentInstance));
+    Platform::CopyString(mCurrentInstance, state.GetInstanceName());
     mPendingConsent = true;
     char rotatingDeviceIdHexBuffer[RotatingDeviceId::kHexMaxLength];
     Encoding::BytesToUppercaseHexString(state.GetRotatingId(), state.GetRotatingIdLength(), rotatingDeviceIdHexBuffer,

src/controller/tests/TestCommissionableNodeController.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2020-2022 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,7 @@
  */
 
 #include <controller/CHIPCommissionableNodeController.h>
+#include <lib/support/CHIPMemString.h>
 #include <lib/support/UnitTestRegistration.h>
 #include <nlunit-test.h>
 
@@ -59,7 +60,7 @@ void TestGetDiscoveredCommissioner_HappyCase(nlTestSuite * inSuite, void * inCon
     MockResolver resolver;
     CommissionableNodeController controller(&resolver);
     chip::Dnssd::DiscoveredNodeData inNodeData;
-    strncpy(inNodeData.resolutionData.hostName, "mockHostName", sizeof(inNodeData.resolutionData.hostName));
+    Platform::CopyString(inNodeData.resolutionData.hostName, "mockHostName");
     Inet::IPAddress::FromString("192.168.1.10", inNodeData.resolutionData.ipAddress[0]);
     inNodeData.resolutionData.numIPs++;
     inNodeData.resolutionData.port = 5540;
@@ -97,12 +98,11 @@ void TestGetDiscoveredCommissioner_HappyCase_OneValidOneInvalidNode(nlTestSuite 
     MockResolver resolver;
     CommissionableNodeController controller(&resolver);
     chip::Dnssd::DiscoveredNodeData invalidNodeData, validNodeData;
-    // strncpy(inNodeData1.hostName, "mockHostName1", sizeof inNodeData1.hostName);
     Inet::IPAddress::FromString("192.168.1.10", invalidNodeData.resolutionData.ipAddress[0]);
     invalidNodeData.resolutionData.numIPs++;
     invalidNodeData.resolutionData.port = 5540;
 
-    strncpy(validNodeData.resolutionData.hostName, "mockHostName2", sizeof validNodeData.resolutionData.hostName);
+    Platform::CopyString(validNodeData.resolutionData.hostName, "mockHostName2");
     Inet::IPAddress::FromString("192.168.1.11", validNodeData.resolutionData.ipAddress[0]);
     validNodeData.resolutionData.numIPs++;
     validNodeData.resolutionData.port = 5540;

commit e090b956eb73218b321344ea983edfacb2a95d71

[chip-tool] Regression from #22708. Revert chip::Platform::CopyString to memcpy (it previously uses to be strncpy which was confusing)

examples/chip-tool/commands/clusters/ComplexArgument.h
@@ -196,7 +196,7 @@ public:
 
         size_t size = strlen(value.asCString());
         auto buffer = static_cast<char *>(chip::Platform::MemoryCalloc(size, sizeof(char)));
-        chip::Platform::CopyString(buffer, size, value.asCString());
+        memcpy(buffer, value.asCString(), size);
 
         request = chip::CharSpan(buffer, size);
         return CHIP_NO_ERROR;