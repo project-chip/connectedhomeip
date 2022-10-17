d9523e412b44b714cad643aadefdbc615dca750e
chrisdecenzo <decenzo77@yahoo.com>
2022-10-16 18:54:32 -0700

    Draft: Add configurable ep1 ACLs and bindings

--- examples/tv-app/linux/AppImpl.cpp
+++ examples/tv-app/linux/AppImpl.cpp
@@ -42,9 +42,11 @@
 #include <lib/support/CodeUtils.h>
 #include <lib/support/ZclString.h>
 #include <platform/CHIPDeviceLayer.h>
+#include <zap-generated/CHIPClusters.h>
 
 using namespace chip;
 using namespace chip::AppPlatform;
+using namespace chip::app::Clusters;
 
 #if CHIP_DEVICE_CONFIG_ENABLE_BOTH_COMMISSIONER_AND_COMMISSIONEE
 class MyUserPrompter : public UserPrompter
@@ -89,9 +91,44 @@ class MyPostCommissioningListener : public PostCommissioningListener
     void CommissioningCompleted(uint16_t vendorId, uint16_t productId, NodeId nodeId, Messaging::ExchangeManager & exchangeMgr,
                                 SessionHandle & sessionHandle) override
     {
+        // read current binding list
+        chip::Controller::BindingCluster cluster(exchangeMgr, sessionHandle, kTargetBindingClusterEndpointId);
 
-        ContentAppPlatform::GetInstance().ManageClientAccess(
-            exchangeMgr, sessionHandle, vendorId, GetDeviceCommissioner()->GetNodeId(), OnSuccessResponse, OnFailureResponse);
+        cacheContext(vendorId, productId, nodeId, exchangeMgr, sessionHandle);
+
+        CHIP_ERROR err =
+            cluster.ReadAttribute<Binding::Attributes::Binding::TypeInfo>(this, OnReadSuccessResponse, OnReadFailureResponse);
+        if (err != CHIP_NO_ERROR)
+        {
+            ChipLogError(Controller, "Failed in reading binding. Error %s", ErrorStr(err));
+            clearContext();
+        }
+    }
+
+    /* Callback when command results in success */
+    static void
+    OnReadSuccessResponse(void * context,
+                          const app::DataModel::DecodableList<Binding::Structs::TargetStruct::DecodableType> & responseData)
+    {
+        ChipLogProgress(Controller, "OnReadSuccessResponse - Binding Read Successfully");
+
+        MyPostCommissioningListener * listener = static_cast<MyPostCommissioningListener *>(context);
+        listener->finishTargetConfiguration(responseData);
+    }
+
+    /* Callback when command results in failure */
+    static void OnReadFailureResponse(void * context, CHIP_ERROR error)
+    {
+        ChipLogProgress(Controller, "OnReadFailureResponse - Binding Read Failed");
+
+        MyPostCommissioningListener * listener = static_cast<MyPostCommissioningListener *>(context);
+        listener->clearContext();
+
+        CommissionerDiscoveryController * cdc = GetCommissionerDiscoveryController();
+        if (cdc != nullptr)
+        {
+            cdc->PostCommissioningFailed(error);
+        }
     }
 
     /* Callback when command results in success */
@@ -115,6 +152,60 @@ class MyPostCommissioningListener : public PostCommissioningListener
             cdc->PostCommissioningFailed(error);
         }
     }
+
+    void
+    finishTargetConfiguration(const app::DataModel::DecodableList<Binding::Structs::TargetStruct::DecodableType> & responseList)
+    {
+        std::vector<app::Clusters::Binding::Structs::TargetStruct::Type> bindings;
+        NodeId localNodeId = GetDeviceCommissioner()->GetNodeId();
+
+        auto iter = responseList.begin();
+        while (iter.Next())
+        {
+            auto & binding = iter.GetValue();
+            ChipLogProgress(Controller, "Binding found nodeId=0x" ChipLogFormatX64 " my nodeId=0x" ChipLogFormatX64,
+                            ChipLogValueX64(binding.node.ValueOr(0)), ChipLogValueX64(localNodeId));
+            if (binding.node.ValueOr(0) != localNodeId)
+            {
+                ChipLogProgress(Controller, "Found a binding for a different node, preserving");
+                bindings.push_back(binding);
+            }
+            else
+            {
+                ChipLogProgress(Controller, "Found a binding for a matching node, dropping");
+            }
+        }
+
+        Optional<SessionHandle> opt   = mSecureSession.Get();
+        SessionHandle & sessionHandle = opt.Value();
+        ContentAppPlatform::GetInstance().ManageClientAccess(*mExchangeMgr, sessionHandle, mVendorId, localNodeId, bindings,
+                                                             OnSuccessResponse, OnFailureResponse);
+        clearContext();
+    }
+
+    void cacheContext(uint16_t vendorId, uint16_t productId, NodeId nodeId, Messaging::ExchangeManager & exchangeMgr,
+                      SessionHandle & sessionHandle)
+    {
+        mVendorId    = vendorId;
+        mProductId   = productId;
+        mNodeId      = nodeId;
+        mExchangeMgr = &exchangeMgr;
+        mSecureSession.ShiftToSession(sessionHandle);
+    }
+
+    void clearContext()
+    {
+        mVendorId    = 0;
+        mProductId   = 0;
+        mNodeId      = 0;
+        mExchangeMgr = nullptr;
+        mSecureSession.SessionReleased();
+    }
+    uint16_t mVendorId                        = 0;
+    uint16_t mProductId                       = 0;
+    NodeId mNodeId                            = 0;
+    Messaging::ExchangeManager * mExchangeMgr = nullptr;
+    SessionHolder mSecureSession;
 };
 
 MyPostCommissioningListener gMyPostCommissioningListener;
@@ -405,6 +496,30 @@ Access::Privilege ContentAppFactoryImpl::GetVendorPrivilege(uint16_t vendorId)
         }
     }
     return Access::Privilege::kOperate;
+}
+
+constexpr ClusterId kClusterIdDescriptor = 0x001d;
+constexpr ClusterId kClusterIdOnOff      = 0x0006;
+constexpr ClusterId kClusterIdWakeOnLAN  = 0x0503;
+// constexpr ClusterId kClusterIdChannel             = 0x0504;
+// constexpr ClusterId kClusterIdTargetNavigator     = 0x0505;
+constexpr ClusterId kClusterIdMediaPlayback = 0x0506;
+// constexpr ClusterId kClusterIdMediaInput          = 0x0507;
+constexpr ClusterId kClusterIdLowPower        = 0x0508;
+constexpr ClusterId kClusterIdKeypadInput     = 0x0509;
+constexpr ClusterId kClusterIdContentLauncher = 0x050a;
+constexpr ClusterId kClusterIdAudioOutput     = 0x050b;
+// constexpr ClusterId kClusterIdApplicationLauncher = 0x050c;
+// constexpr ClusterId kClusterIdAccountLogin        = 0x050e;
+
+std::list<ClusterId> ContentAppFactoryImpl::GetAllowedClusterListForStaticEndpoint(EndpointId endpointId, uint16_t vendorId)
+{
+    if (endpointId == kLocalVideoPlayerEndpointId)
+    {
+        return { kClusterIdDescriptor, kClusterIdOnOff,       kClusterIdWakeOnLAN,       kClusterIdMediaPlayback,
+                 kClusterIdLowPower,   kClusterIdKeypadInput, kClusterIdContentLauncher, kClusterIdAudioOutput };
+    }
+    return {};
 }
 
 } // namespace AppPlatform
