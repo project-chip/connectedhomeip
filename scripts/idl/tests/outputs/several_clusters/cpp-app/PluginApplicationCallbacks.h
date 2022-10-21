73881c1e144d5368300ec4d8a0ca03913faa74db
Andrei Litvin <andy314@gmail.com>
2022-10-21 14:28:24 -0400

    Remove `PluginCallbacks` generated file.
    This file is not really needed by anything except advance
    declaration by PluginApplicationCallbachs.h. We can codegen the
    callbacks in that file.

--- scripts/idl/tests/outputs/several_clusters/cpp-app/PluginApplicationCallbacks.h
+++ scripts/idl/tests/outputs/several_clusters/cpp-app/PluginApplicationCallbacks.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <app-common/zap-generated/callbacks/PluginCallbacks.h>
-
 #define MATTER_PLUGINS_INIT \
     MatterFirstPluginClientInitCallback(); \
     MatterSecondPluginClientInitCallback(); \
