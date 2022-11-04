f8128b097cb268241ef3ca8584853502e5b30e35
Andrei Litvin <andy314@gmail.com>
2022-11-04 11:18:37 -0400

    Fix unit tests, make sure acronyms are normalized for wifi

--- scripts/idl/tests/outputs/several_clusters/cpp-app/callback-stub.cpp
+++ scripts/idl/tests/outputs/several_clusters/cpp-app/callback-stub.cpp
@@ -10,13 +10,29 @@ void emberAfClusterInitCallback(EndpointId endpoint, ClusterId clusterId)
 {
     switch (clusterId)
     {
-     case ZCL_THIRD_CLUSTER_ID :
+    case ZCL_FIRST_CLUSTER_ID:
+        emberAfFirstClusterInitCallback(endpoint);
+        break;
+    case ZCL_SECOND_CLUSTER_ID:
+        emberAfSecondClusterInitCallback(endpoint);
+        break;
+    case ZCL_THIRD_CLUSTER_ID:
         emberAfThirdClusterInitCallback(endpoint);
         break;
     default:
         // Unrecognized cluster ID
         break;
     }
+}
+void __attribute__((weak)) emberAfFirstClusterInitCallback(EndpointId endpoint)
+{
+    // To prevent warning
+    (void) endpoint;
+}
+void __attribute__((weak)) emberAfSecondClusterInitCallback(EndpointId endpoint)
+{
+    // To prevent warning
+    (void) endpoint;
 }
 void __attribute__((weak)) emberAfThirdClusterInitCallback(EndpointId endpoint)
 {
