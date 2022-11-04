59668ddf5cee25f6fc17e8c5e8d20243335bfd6c
Andrei Litvin <andy314@gmail.com>
2022-11-04 13:21:24 -0400

    Correct cirque test to expect cluster name to be Test

--- src/controller/python/test/test_scripts/base.py
+++ src/controller/python/test/test_scripts/base.py
@@ -1173,9 +1173,9 @@ class BaseTestHelper:
         try:
             cluster = self.devCtrl.GetClusterHandler()
             clusterInfo = cluster.GetClusterInfoById(0xFFF1FC05)  # TestCluster
-            if clusterInfo["clusterName"] != "TestCluster":
+            if clusterInfo["clusterName"] != "Test":
                 raise Exception(
-                    f"Wrong cluster info clusterName: {clusterInfo['clusterName']} expected TestCluster")
+                    f"Wrong cluster info clusterName: {clusterInfo['clusterName']} expected 'Test'")
         except Exception as ex:
             self.logger.exception(f"Failed to finish API test: {ex}")
             return False
