f7077497671ad1b5942270ff59014b9f6137fd74
Terence Hampson <thampson@google.com>
2022-11-17 16:08:16 +0000

    Fix octet string parsing in yaml test parser
    Old parsing had issue since str.encode for utf-8 character would be
    improperly convert. For example `\xff` would become `b'\xc3\xbf'` when
    we wanted it to be `b'\xff`.
    
    Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

--- src/controller/python/chip/yaml/format_converter.py
+++ src/controller/python/chip/yaml/format_converter.py
@@ -20,9 +20,39 @@ from chip.clusters.Types import Nullable, NullValue
 from chip.tlv import uint, float32
 import enum
 from chip.yaml.errors import ValidationError
+from binascii import unhexlify
+import re
 
 
-_HEX_PREFIX = 'hex:'
+def convert_yaml_octet_string_to_bytes(s: str) -> bytes:
+    """Convert YAML octet string body to bytes, handling any c-style hex escapes (e.g. \x5a) and hex: prefix"""
+    is_hex_string_re = r"^hex:(?P<hex_content>[A-Fa-f0-9]*)$"
+
+    hex_prefixed_match = re.match(is_hex_string_re, s)
+
+    # Step 1: handle explicit "hex:" prefix
+    if hex_prefixed_match:
+        hex_content = hex_prefixed_match.group("hex_content")
+        if (len(hex_content) % 2) != 0:
+            raise ValueError("Hex literal is not even length!")
+        return unhexlify(hex_content)
+
+    # Step 2: convert non-hex-prefixed to bytes
+    # TODO(#23669): This does not properly support utf8 octet strings. We mimic
+    # javascript codegen behavior. Behavior or javascript is:
+    #   * Octet string character >= u+0200 errors out.
+    #   * Any character greater than 0xFF has the upper bytes chopped off.
+    known_javascript_max_char_value = 0x200
+    accumulated_hex = ""
+    for char in s:
+        char_value = ord(char)
+        if char_value >= known_javascript_max_char_value:
+            # If you got here see TODO #23669 mentioned above.
+            raise ValueError("Unsupport char in octet string")
+        char_value_lsb = char_value & 0xFF
+        hex_with_leading_0x = '{0:02x}'.format(char_value_lsb)
+        accumulated_hex += hex_with_leading_0x
+    return unhexlify(accumulated_hex)
 
 
 def convert_name_value_pair_to_dict(arg_values):
@@ -118,9 +148,7 @@ def convert_yaml_type(field_value, field_type, use_from_dict=False):
         return field_type(field_value)
     # YAML treats bytes as strings. Convert to a byte string.
     elif (field_type == bytes and type(field_value) != bytes):
-        if isinstance(field_value, str) and field_value.startswith(_HEX_PREFIX):
-            return bytes.fromhex(field_value[len(_HEX_PREFIX):])
-        return str.encode(field_value)
+        return convert_yaml_octet_string_to_bytes(field_value)
     # By default, just return the field_value casted to field_type.
     else:
         return field_type(field_value)
