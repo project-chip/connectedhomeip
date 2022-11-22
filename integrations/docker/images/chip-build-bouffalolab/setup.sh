c42c8923f5205ea0a6e38c0c77391fa5b1e5e598
wyhong <wyhong@bouffalolab.com>
2022-11-22 17:39:56 +0800

    setup toolchain under /opt/bouffalolab_sdk

--- integrations/docker/images/chip-build-bouffalolab/setup.sh
+++ integrations/docker/images/chip-build-bouffalolab/setup.sh
@@ -5,15 +5,25 @@ CURRENT_DIR=$(
     cd "$(dirname "$0")"
     pwd
 )
-SDK_ROOT_DIR=$CURRENT_DIR/../
 
-TOOLCHAIN_SETUP_ROOT=~/.bouffalolab/toolchain
+SDK_ROOT=/opt/bouffalolab_sdk
+# Currently, only setup toolchain under $SDK_ROOT
+TOOLCHAIN_SETUP_ROOT=$SDK_ROOT/toolchain
+
+TOOLCHAIN_SYMBOLIC_LINK_PATH=""
+git -C . rev-parse 2>/dev/null
+if [[ "$?" == "0" ]]; then
+    # Examples in Bouffalo Lab IOT SDK repo expect toolchain under repo,
+    # let's create a symbolic link to Bouffalo Lab toolchain, 
+    # if this script runs under repo
+    TOOLCHAIN_SYMBOLIC_LINK_PATH=$CURRENT_DIR/../toolchain
+fi
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     toolchains_url=(
         "riscv/Thead_riscv/Linux_x86_64" "https://dev.bouffalolab.com/media/upload/download/toolchain_riscv_thead_linux64.zip" "toolchain_riscv_thead_linux_x86_64"
         "riscv/Linux" "https://dev.bouffalolab.com/media/upload/download/toolchain_riscv_sifive_linux64.zip" "toolchain_riscv_sifive_linux"
     )
-
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     toolchains_url=(
         "riscv/Darwin" "https://dev.bouffalolab.com/media/upload/download/toolchain_riscv_sifive_macos.zip" "toolchain_riscv_sifive_macos"
@@ -37,20 +47,20 @@ for ((i = 0; i < ${#toolchains_url[@]}; i += 3)); do
     if [ ! -f "$TOOLCHAIN_SETUP_ROOT/$toolchain_zip" ]; then
         exit 1
     fi
-    rm -rf "$TOOLCHAIN_SETUP_ROOT/$path" 2>&1 >/dev/null
+    rm -rf "$TOOLCHAIN_SETUP_ROOT/$path"
     mkdir -p "$TOOLCHAIN_SETUP_ROOT/$path"
-    unzip "$TOOLCHAIN_SETUP_ROOT/$toolchain_zip" -d "$TOOLCHAIN_SETUP_ROOT/$path" 2>&1 >/dev/null
+    unzip "$TOOLCHAIN_SETUP_ROOT/$toolchain_zip" -d "$TOOLCHAIN_SETUP_ROOT/$path"
     mv "$TOOLCHAIN_SETUP_ROOT/$path/$output"/* "$TOOLCHAIN_SETUP_ROOT/$path"
-    rm -rf "$TOOLCHAIN_SETUP_ROOT/$toolchain_zip" 2>&1 >/dev/null
+    rm -rf "$TOOLCHAIN_SETUP_ROOT/$toolchain_zip"
 
     if [ -f "$TOOLCHAIN_SETUP_ROOT/$path"/chmod755.sh ]; then
         cd "$TOOLCHAIN_SETUP_ROOT/$path"/
         bash chmod755.sh
+        cd $CURRENT_DIR
     fi
 done
 
-cd "$SDK_ROOT_DIR"
-rm -rf toolchain
-ln -s "$TOOLCHAIN_SETUP_ROOT" toolchain
-
-cd "$CURRENT_DIR"
+if [[ "$TOOLCHAIN_SYMBOLIC_LINK_PATH" != "" ]]; then
+    rm -rf $TOOLCHAIN_SYMBOLIC_LINK_PATH
+    ln -s $TOOLCHAIN_SETUP_ROOT $TOOLCHAIN_SYMBOLIC_LINK_PATH  
+fi
