63b4614a5cc5f9fd4d1566acf0161f7e6dfe5d8b
Andrei Litvin <andy314@gmail.com>
2022-11-24 14:58:24 -0500

    Restyle

--- build/chip/copy_tree.py
+++ build/chip/copy_tree.py
@@ -4,6 +4,7 @@ import argparse
 import shutil
 import sys
 
+
 def main():
 
     parser = argparse.ArgumentParser(description="Copy a tree of files from a source directory to a destination one.")
@@ -12,8 +13,9 @@ def main():
     parser.add_argument('--dest-dir', required=True)
 
     args = parser.parse_args()
-    
+
     shutil.copytree(args.src_dir, args.dest_dir, dirs_exist_ok=True)
+
 
 if __name__ == '__main__':
     sys.exit(main())
