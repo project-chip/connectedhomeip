6cca33f5a032ba0e8885fb6d626863524fb0ee4b
Damian Krolik <damian.krolik@nordicsemi.no>
2022-12-01 13:16:37 +0100

    [crypto] Add Operational Keystore for PSA crypto API
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- src/crypto/CHIPCryptoPALPSA.h
+++ src/crypto/CHIPCryptoPALPSA.h
@@ -15,15 +15,48 @@
  *    limitations under the License.
  */
 
+/**
+ *    @file
+ *      Public constants and structures used by the crypto backend based on PSA crypto API
+ */
+
 #pragma once
 
 #include "CHIPCryptoPAL.h"
+#include <lib/core/DataModelTypes.h>
 #include <lib/support/SafePointerCast.h>
 
 #include <psa/crypto.h>
 
 namespace chip {
 namespace Crypto {
+
+/**
+ *  @def CHIP_CONFIG_CRYPTO_PSA_KEY_ID_BASE
+ *
+ *  @brief
+ *    Base for PSA key identifier range used by Matter.
+ *
+ *  Cryptographic keys stored in the PSA Internal Trusted Storage must have
+ *  a user-assigned identifer from the range PSA_KEY_ID_USER_MIN to
+ *  PSA_KEY_ID_USER_MAX. This option allows to override the base used to derive
+ *  key identifiers used by Matter to avoid overlapping with other firmware
+ *  components that also use PSA crypto API. The default value was selected
+ *  not to interfere with OpenThread's default base that is 0x20000.
+ */
+#ifndef CHIP_CONFIG_CRYPTO_PSA_KEY_ID_BASE
+#define CHIP_CONFIG_CRYPTO_PSA_KEY_ID_BASE 0x30000
+#endif // CHIP_CONFIG_CRYPTO_PSA_KEY_ID_BASE
+
+enum class KeyIdBase : psa_key_id_t
+{
+    Operational = CHIP_CONFIG_CRYPTO_PSA_KEY_ID_BASE
+};
+
+constexpr psa_key_id_t MakeOperationalKeyId(FabricIndex fabricIndex)
+{
+    return to_underlying(KeyIdBase::Operational) + fabricIndex;
+}
 
 struct PSAP256KeypairContext
 {
