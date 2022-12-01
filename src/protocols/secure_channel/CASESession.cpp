02a25fac46cdfb32687087af7f8c314ebc30fac3
Boris Zbarsky <bzbarsky@apple.com>
2022-12-01 17:31:24 -0500

    Fix status report message when destination id is unknown. (#23858)
    * Fix status report message when destination id is unknown.
    
    We were ignoring the fact that we failed to match the destination id, pressing
    on to SendSigma2, and failing out, but that clobbered the "err" variable with a
    diferent error, so we ended up sending an error status report with the wrong
    protocol code.
    
    * Address review comments

--- src/protocols/secure_channel/CASESession.cpp
+++ src/protocols/secure_channel/CASESession.cpp
@@ -628,6 +628,7 @@ CHIP_ERROR CASESession::HandleSigma1(System::PacketBufferHandle && msg)
         ChipLogError(SecureChannel, "CASE failed to match destination ID with local fabrics");
         ChipLogByteSpan(SecureChannel, destinationIdentifier);
     }
+    SuccessOrExit(err);
 
     // ParseSigma1 ensures that:
     // mRemotePubKey.Length() == initiatorPubKey.size() == kP256_PublicKey_Length.
