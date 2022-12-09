d7ddc28141d61dcd912b5e199ef494864cd47c32
shasaicha <shsaicha@silabs.com>
2022-12-09 18:13:13 +0530

    thermostat groups support zap changes

--- zzz_generated/thermostat/zap-generated/CHIPClusters.h
+++ zzz_generated/thermostat/zap-generated/CHIPClusters.h
@@ -33,19 +33,14 @@ namespace Controller {
 class DLL_EXPORT IdentifyCluster : public ClusterBase
 {
 public:
-    IdentifyCluster(Messaging::ExchangeManager & exchangeManager, const SessionHandle & session, EndpointId endpoint) :
-        ClusterBase(exchangeManager, session, app::Clusters::Identify::Id, endpoint)
-    {}
+    IdentifyCluster(Messaging::ExchangeManager & exchangeManager, const SessionHandle & session, EndpointId endpoint) : ClusterBase(exchangeManager, session, app::Clusters::Identify::Id, endpoint) {}
     ~IdentifyCluster() {}
 };
 
 class DLL_EXPORT OtaSoftwareUpdateProviderCluster : public ClusterBase
 {
 public:
-    OtaSoftwareUpdateProviderCluster(Messaging::ExchangeManager & exchangeManager, const SessionHandle & session,
-                                     EndpointId endpoint) :
-        ClusterBase(exchangeManager, session, app::Clusters::OtaSoftwareUpdateProvider::Id, endpoint)
-    {}
+    OtaSoftwareUpdateProviderCluster(Messaging::ExchangeManager & exchangeManager, const SessionHandle & session, EndpointId endpoint) : ClusterBase(exchangeManager, session, app::Clusters::OtaSoftwareUpdateProvider::Id, endpoint) {}
     ~OtaSoftwareUpdateProviderCluster() {}
 };
 
