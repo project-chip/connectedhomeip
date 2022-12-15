18ab6578b6dee5f917d8fb1f25d1baf8a58d3923
Mathieu Kardous <mathieu.kardous@silabs.com>
2022-12-15 12:24:37 -0500

    CM table tests

---
+++ src/app/tests/TestClientMonitoringRegistrationTable.cpp
@@ -0,0 +1,93 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <app/util/ClientMonitoringRegistrationTable.h>
+#include <lib/core/CHIPError.h>
+#include <lib/support/DefaultStorageKeyAllocator.h>
+#include <lib/support/TestPersistentStorageDelegate.h>
+#include <lib/support/UnitTestRegistration.h>
+#include <nlunit-test.h>
+
+using chip::ClientMonitoringRegistrationTable;
+using chip::NullOptional;
+
+namespace {
+
+constexpr chip::FabricIndex kTestFabricIndex = 1;
+constexpr uint64_t kTestICid                 = 10;
+constexpr chip::NodeId kTestClientNodeId     = 11;
+
+constexpr chip::FabricIndex kTestInvalidFabric  = 0;
+constexpr uint64_t kTestInvalidICid             = 0;
+constexpr chip::NodeId kTestInvalidClientNodeId = 0;
+
+void TestDefaultClientValues(nlTestSuite * aSuite, void * aContext)
+{
+    chip::TestPersistentStorageDelegate testStorage;
+    ClientMonitoringRegistrationTable registration(testStorage);
+
+    NL_TEST_ASSERT(aSuite, registration.getFaricIndex() == kTestInvalidFabric);
+    NL_TEST_ASSERT(aSuite, registration.getClientNodeId() == kTestInvalidClientNodeId);
+    NL_TEST_ASSERT(aSuite, registration.getICid() == kTestInvalidICid);
+}
+
+void TestLoadFromStorageEmptyValue(nlTestSuite * aSuite, void * aContext)
+{
+    chip::TestPersistentStorageDelegate testStorage;
+    ClientMonitoringRegistrationTable registration(testStorage);
+
+    CHIP_ERROR err = registration.LoadFromStorage(kTestFabricIndex);
+    NL_TEST_ASSERT(aSuite, err == CHIP_ERROR_PERSISTED_STORAGE_VALUE_NOT_FOUND);
+}
+
+void TestSaveAndLoadRegistrationValue(nlTestSuite * aSuite, void * aContext)
+{
+    chip::TestPersistentStorageDelegate testStorage;
+    ClientMonitoringRegistrationTable savedRegistration(testStorage);
+    ClientMonitoringRegistrationTable loadedRegistration(testStorage);
+
+    savedRegistration.setClientNodeId(kTestClientNodeId);
+    savedRegistration.setICid(kTestICid);
+    savedRegistration.setFabricIndex(kTestFabricIndex);
+
+    CHIP_ERROR err = savedRegistration.SaveToStorage();
+    NL_TEST_ASSERT(aSuite, err == CHIP_NO_ERROR);
+
+    err = loadedRegistration.LoadFromStorage(kTestFabricIndex);
+    NL_TEST_ASSERT(aSuite, err == CHIP_NO_ERROR);
+
+    NL_TEST_ASSERT(aSuite, loadedRegistration.getFaricIndex() == kTestFabricIndex);
+    NL_TEST_ASSERT(aSuite, loadedRegistration.getClientNodeId() == kTestClientNodeId);
+    NL_TEST_ASSERT(aSuite, loadedRegistration.getICid() == kTestICid);
+}
+
+} // namespace
+
+int TestClientMonitoringRegistrationTable()
+{
+    static nlTest sTests[] = { NL_TEST_DEF("TestDefaultClientValues", TestDefaultClientValues),
+                               NL_TEST_DEF("TestLoadFromStorageEmptyValue", TestLoadFromStorageEmptyValue),
+                               NL_TEST_DEF("TestSaveAndLoadRegistrationValue", TestSaveAndLoadRegistrationValue),
+                               NL_TEST_SENTINEL() };
+
+    nlTestSuite cmSuite = { "TestClientMonitoringRegistrationTable", &sTests[0], nullptr, nullptr };
+
+    nlTestRunner(&cmSuite, nullptr);
+    return (nlTestRunnerStats(&cmSuite));
+}
+
+CHIP_REGISTER_TEST_SUITE(TestClientMonitoringRegistrationTable)
