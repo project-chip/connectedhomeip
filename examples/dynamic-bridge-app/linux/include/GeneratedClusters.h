c8cc3c96a20b2dd3ff75b4ba90a898ed6c7898d4
Feras Muki <ferasmuki@google.com>
2022-12-19 16:22:35 +0000

    Change the incoming command list for generated clusters to const

--- examples/dynamic-bridge-app/linux/include/GeneratedClusters.h
+++ examples/dynamic-bridge-app/linux/include/GeneratedClusters.h
@@ -25,6 +25,6 @@ public:
     // Gets the list of available attributes for this cluster.
     virtual std::vector<AttributeInterface *> GetAttributes() = 0;
 
-    virtual chip::CommandId * GetIncomingCommandList() { return nullptr; }
-    virtual chip::CommandId * GetOutgoingCommandList() { return nullptr; };
+    virtual const chip::CommandId * GetIncomingCommandList() { return nullptr; }
+    virtual const chip::CommandId * GetOutgoingCommandList() { return nullptr; };
 };
