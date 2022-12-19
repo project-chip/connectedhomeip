adebca2764515d66ab0eee637b3b091803fa0500
Alex Tsitsiura <s07641069@gmail.com>
2022-12-19 14:45:07 +0200

    [Telink] Enable NVS lookup cache and add log levels

--- examples/lighting-app/telink/src/LightingManager.cpp
+++ examples/lighting-app/telink/src/LightingManager.cpp
@@ -115,7 +115,7 @@ bool LightingManager::InitiateAction(Action_t aAction, int32_t aActor, uint8_t s
 
 void LightingManager::SetLevel(uint8_t aLevel)
 {
-    LOG_INF("Setting brightness level to %u", aLevel);
+    LOG_DBG("Setting brightness level to %u", aLevel);
     mLevel = aLevel;
     UpdateLight();
 }
