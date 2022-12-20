5283105a4a2ecec576bc3f6011766f26ce0ff44d
Sandeep Mistry <sandeep.mistry@arm.com>
2022-12-20 14:46:27 -0500

    Add initial Arm Virtual Hardware (AVH) workflow with end to end test

---
+++ scripts/tests/avh/helpers/avh_chiptool_instance.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2022 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+
+from .avh_instance import AvhInstance
+
+APPLICATION_BINARY = "chip-tool"
+
+
+class AvhChiptoolInstance(AvhInstance):
+    def __init__(self, avh_client, name, application_binary_path):
+        super().__init__(avh_client, name)
+
+        self.application_binary_path = application_binary_path
+
+    def upload_application_binary(self):
+        super().upload_application_binary(
+            self.application_binary_path, APPLICATION_BINARY
+        )
+
+    def configure_system(self):
+        self.log_in_to_console()
+
+        # set current date and time
+        self.console_exec_command("sudo timedatectl set-ntp false", timeout=300)
+        self.console_exec_command(
+            f"sudo timedatectl set-time '{datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}'",
+            timeout=300,
+        )
+        self.console_exec_command("sudo timedatectl set-ntp true", timeout=300)
+
+        # install network manager
+        self.console_exec_command("sudo apt-get update", timeout=300)
+        self.console_exec_command(
+            "sudo apt-get -y install network-manager", timeout=300
+        )
+
+        # connect Wi-Fi to the Arm ssid
+        self.console_exec_command("sudo nmcli r wifi on")
+        self.console_exec_command("sudo nmcli d wifi connect Arm password password")
+
+        # disable eth0
+        self.console_exec_command("sudo nmcli dev set eth0 managed no")
+        self.console_exec_command("sudo ip link set dev eth0 down")
+
+    def pairing_ble_wifi(self, node_id, ssid, password, pin_code, discriminator):
+        output = self.console_exec_command(
+            f"./{APPLICATION_BINARY} pairing ble-wifi {node_id} {ssid} {password} {pin_code} {discriminator}",
+            timeout=120.0,
+        )
+
+        return output
+
+    def on(self, node_id):
+        output = self.console_exec_command(
+            f"./{APPLICATION_BINARY} onoff on {node_id} 1", timeout=30.0
+        )
+
+        return output
+
+    def off(self, node_id):
+        output = self.console_exec_command(
+            f"./{APPLICATION_BINARY} onoff off {node_id} 1", timeout=30.0
+        )
+
+        return output
---
+++ scripts/tests/avh/helpers/avh_instance.py
@@ -0,0 +1,240 @@
+# Copyright (c) 2022 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+
+import avh_api
+import paramiko
+import websocket
+
+DEFAULT_INSTANCE_FLAVOR = "rpi4b"
+DEFAULT_INSTANCE_OS = "Ubuntu Server"
+DEFAULT_INSTANCE_OS_VERSION = "22.04.1"
+
+DEFAULT_OS_BOOTED_OUTPUT = "-----END SSH HOST KEY KEYS-----"
+
+DEFAULT_SSH_USERNAME = "pi"
+DEFAULT_SSH_PASSWORD = "raspberry"
+
+
+class AvhInstance:
+    def __init__(
+        self,
+        avh_client,
+        name,
+        flavor=DEFAULT_INSTANCE_FLAVOR,
+        os=DEFAULT_INSTANCE_OS,
+        os_version=DEFAULT_INSTANCE_OS_VERSION,
+        username=DEFAULT_SSH_USERNAME,
+        password=DEFAULT_SSH_PASSWORD,
+    ):
+        self.avh_client = avh_client
+        self.name = name
+        self.flavor = flavor
+        self.os = os
+        self.os_version = os_version
+        self.username = username
+        self.password = password
+        self.instance_id = None
+        self.console = None
+        self.ssh_pkey = None
+        self.ssh_key_id = None
+
+    def create(self):
+        self.instance_id = self.avh_client.create_instance(
+            name=self.name, flavor=self.flavor, os=self.os_version, osbuild=self.os
+        )
+
+    def wait_for_state_on(self, timeout=240):
+        start_time = time.monotonic()
+
+        while True:
+            instance_state = self.avh_client.instance_state(self.instance_id)
+
+            if instance_state == "on":
+                break
+            elif instance_state == "error":
+                raise Exception("VM entered error state")
+            elif (time.monotonic() - start_time) > timeout:
+                raise Exception(
+                    f"Timed out waiting for state 'on' for instance id {self.instance_id}, current state is '{instance_state}'"
+                )
+
+            time.sleep(1.0)
+
+        self.console = websocket.create_connection(
+            self.avh_client.instance_console_url(self.instance_id)
+        )
+
+    def wait_for_os_boot(self, booted_output=DEFAULT_OS_BOOTED_OUTPUT, timeout=240):
+        start_time = time.monotonic()
+
+        while True:
+            console_log = self.avh_client.instance_console_log(self.instance_id)
+
+            if booted_output in console_log:
+                break
+            elif (time.monotonic() - start_time) > timeout:
+                raise Exception(
+                    f"Timed out waiting for OS to boot for instance id {self.instance_id}",
+                    f"Did not find {booted_output} in {console_log}",
+                )
+
+            time.sleep(1.0)
+
+    def ssh_client(self, timeout=30):
+        if self.ssh_pkey is None:
+            self.ssh_pkey = paramiko.ecdsakey.ECDSAKey.generate()
+
+            self.ssh_key_id = self.avh_client.create_ssh_project_key(
+                self.name, f"{self.ssh_pkey.get_name()} {self.ssh_pkey.get_base64()}"
+            )
+
+        instance_quick_connect_command = self.avh_client.instance_quick_connect_command(
+            self.instance_id
+        )
+
+        split_instance_quick_connect_command = instance_quick_connect_command.split()
+        proxy_username = split_instance_quick_connect_command[-2].split("@")[-2]
+        proxy_hostname = split_instance_quick_connect_command[-2].split("@")[-1]
+        instance_ip = split_instance_quick_connect_command[-1].split("@")[-1]
+
+        ssh_proxy_client = paramiko.SSHClient()
+        ssh_proxy_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh_proxy_client.connect(
+            hostname=proxy_hostname,
+            username=proxy_username,
+            pkey=self.ssh_pkey,
+            look_for_keys=False,
+            timeout=timeout,
+        )
+
+        try:
+            proxy_sock = ssh_proxy_client.get_transport().open_channel(
+                kind="direct-tcpip",
+                dest_addr=(instance_ip, 22),
+                src_addr=("", 0),
+                timeout=timeout,
+            )
+
+            ssh_client.connect(
+                hostname=instance_ip,
+                username=self.username,
+                password=self.password,
+                sock=proxy_sock,
+                timeout=timeout,
+                look_for_keys=False,
+            )
+        except Exception:
+            raise Exception(
+                f"Failled to connect to {instance_ip} via SSH proxy {proxy_username}@{proxy_hostname}"
+            )
+
+        return ssh_client
+
+    def delete(self):
+        if self.console is not None:
+            self.console.close()
+
+            self.console = None
+
+        if self.ssh_key_id is not None:
+            self.avh_client.delete_ssh_project_key(self.ssh_key_id)
+
+            self.ssh_key_id = None
+
+        if self.instance_id is not None:
+            self.avh_client.delete_instance(self.instance_id)
+
+    def wait_for_state_deleted(self, timeout=60):
+        if self.instance_id is None:
+            return
+
+        start_time = time.monotonic()
+
+        while True:
+            try:
+                self.avh_client.instance_state(self.instance_id)
+            except avh_api.exceptions.NotFoundException:
+                break
+
+            if (time.monotonic() - start_time) > timeout:
+                raise Exception(
+                    f"Timedout waiting for instance id {self.instance_id} to be deleted"
+                )
+
+            time.sleep(1.0)
+
+        self.instance_id = None
+
+    def upload_application_binary(self, local_path, remote_path):
+        ssh_client = self.ssh_client()
+
+        stfp_client = ssh_client.open_sftp()
+        stfp_client.put(local_path, remote_path)
+        stfp_client.close()
+
+        ssh_client.exec_command(f"chmod +x {remote_path}")
+        ssh_client.close()
+
+    def wait_for_console_output(self, expected_output, timeout=10.0):
+        self.console.settimeout(1.0)
+
+        start_time = time.monotonic()
+
+        output = b""
+        while True:
+            if (time.monotonic() - start_time) > timeout:
+                raise Exception(
+                    f"Timed out waiting for {expected_output} in console output"
+                    f"Current output is {output}"
+                )
+
+            try:
+                output += self.console.recv()
+            except websocket.WebSocketTimeoutException:
+                pass
+
+            if expected_output in output:
+                break
+
+        return output
+
+    def wait_for_console_prompt(self, timeout=10.0):
+        return self.wait_for_console_output(b"$ ", timeout)
+
+    def log_in_to_console(self):
+        self.console.recv()  # flush input
+        self.console.send("\n")
+
+        self.wait_for_console_output(b"login: ")
+        self.console.send(f"{self.username}\n")
+
+        self.wait_for_console_output(b"Password: ")
+        self.console.send(f"{self.password}\n")
+
+        self.wait_for_console_prompt()
+
+    def console_exec_command(self, command, timeout=10.0):
+        self.console.send("\03")  # CTRL-C
+        self.wait_for_console_prompt()
+        self.console.send(f"{command}\n")
+
+        output = self.wait_for_console_prompt(timeout)
+
+        return output
