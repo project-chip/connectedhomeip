b76318bea513207521fb296db34c6ab3c0730db7
panliming-tuya <smarxpan@163.com>
2023-01-05 19:39:08 +0800

    restyled.

--- src/controller/java/src/chip/devicecontroller/AttestationTrustStoreDelegate.java
+++ src/controller/java/src/chip/devicecontroller/AttestationTrustStoreDelegate.java
@@ -2,24 +2,20 @@ package chip.devicecontroller;
 
 /**
  * Delegate for attestation trust store for device attestation verifiers.
- * 
- * API is synchronous.
+ *
+ * <p>API is synchronous.
  */
 public interface AttestationTrustStoreDelegate {
   /**
-   * Look-up a product attestation authority (PAA) cert by subject key
-   * identifier (SKID).
-   * 
-   * The implementations of this interface must have access to a set of PAAs to
-   * trust.
+   * Look-up a product attestation authority (PAA) cert by subject key identifier (SKID).
    *
-   * Interface is synchronous, and therefore this should not be used unless to
-   * expose a PAA store that is both fully local and quick to access.
-   * 
-   * @param skid Buffer containing the subject key identifier (SKID) of the PAA to
-   *             look-up
-   * @return If found, the result should return paa cert in x.509 format, if not
-   *         found, return null.
+   * <p>The implementations of this interface must have access to a set of PAAs to trust.
+   *
+   * <p>Interface is synchronous, and therefore this should not be used unless to expose a PAA store
+   * that is both fully local and quick to access.
+   *
+   * @param skid Buffer containing the subject key identifier (SKID) of the PAA to look-up
+   * @return If found, the result should return paa cert in x.509 format, if not found, return null.
    */
   byte[] getProductAttestationAuthorityCert(byte[] skid);
 }
