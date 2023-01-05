a9150a42756d7b89b6f6511f9c4081f2a63d0d5a
Karsten Sperling <ksperling@apple.com>
2023-01-06 11:02:56 +1300

    Add MTRCertificateInfo / MTRDistinguishedNameInfo.
    These are wrappers around ChipCert and ChipDN.
    Fixes #23668

---
+++ src/darwin/Framework/CHIP/MTRCertificateInfo.h
@@ -0,0 +1,81 @@
+/**
+ *    Copyright (c) 2023 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+#import <Matter/MTRCertificates.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class MTRDistinguishedNameInfo;
+
+/**
+ * Exposes Matter-specific information from an operational X.509 certificate.
+ *
+ * Note: This class does not support parsing certifactes related to Device Attestation.
+ */
+@interface MTRCertificateInfo : NSObject <NSCopying>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/**
+ * Initializes the receiver with an operational certificate in Matter TLV format.
+ */
+- (nullable instancetype)initWithTLVBytes:(MTRCertificateTLVBytes)bytes;
+
+@property (readonly) MTRDistinguishedNameInfo * issuer;
+@property (readonly) MTRDistinguishedNameInfo * subject;
+
+@property (readonly) NSDate * notBefore;
+@property (readonly) NSDate * notAfter;
+
+@end
+
+/**
+ * Represents the Matter-specific components of an X.509 Distinguished Name.
+ */
+NS_SWIFT_SENDABLE
+@interface MTRDistinguishedNameInfo : NSObject <NSCopying>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/**
+ * The Node ID contained in the DN, if any.
+ */
+@property (readonly, nullable) NSNumber * nodeID;
+
+/**
+ * The Fabric ID contained in the DN, if any.
+ */
+@property (readonly, nullable) NSNumber * fabricID;
+
+/**
+ * The `RCAC` ID contained in the DN, if any.
+ */
+@property (readonly, nullable) NSNumber * rootCACertificateID;
+
+/**
+ * The `ICAC` ID contained in the DN, if any.
+ */
+@property (readonly, nullable) NSNumber * intermediateCACertificateID;
+
+/**
+ * The set of CASE Authenticated Tags contained in the DN.
+ */
+@property (readonly) NSSet<NSNumber *> * tags;
+
+@end
+
+NS_ASSUME_NONNULL_END
