27e304614c3babfef17daa4e93079ec40b7d2a44
René Josefsen <rjosefsen@grundfos.com>
2023-01-09 13:56:52 +0100

    Ran regen all on latest zap and master

--- src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
+++ src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
@@ -18298,14 +18298,14 @@ typedef NS_ENUM(uint8_t, MTRPumpConfigurationAndControlPumpOperationMode) {
 } API_AVAILABLE(ios(16.1), macos(13.0), watchos(9.1), tvos(16.1));
 
 typedef NS_OPTIONS(uint32_t, MTRPumpConfigurationAndControlPumpFeature) {
-    MTRPumpConfigurationAndControlPumpFeatureConstantPressure = 0x1,
-    MTRPumpConfigurationAndControlPumpFeatureCompensatedPressure = 0x2,
-    MTRPumpConfigurationAndControlPumpFeatureConstantFlow = 0x4,
-    MTRPumpConfigurationAndControlPumpFeatureConstantSpeed = 0x8,
-    MTRPumpConfigurationAndControlPumpFeatureConstantTemperature = 0x10,
-    MTRPumpConfigurationAndControlPumpFeatureAutomatic = 0x20,
-    MTRPumpConfigurationAndControlPumpFeatureLocal = 0x40,
-};
+    MTRPumpConfigurationAndControlPumpFeatureConstantPressure MTR_NEWLY_AVAILABLE = 0x1,
+    MTRPumpConfigurationAndControlPumpFeatureCompensatedPressure MTR_NEWLY_AVAILABLE = 0x2,
+    MTRPumpConfigurationAndControlPumpFeatureConstantFlow MTR_NEWLY_AVAILABLE = 0x4,
+    MTRPumpConfigurationAndControlPumpFeatureConstantSpeed MTR_NEWLY_AVAILABLE = 0x8,
+    MTRPumpConfigurationAndControlPumpFeatureConstantTemperature MTR_NEWLY_AVAILABLE = 0x10,
+    MTRPumpConfigurationAndControlPumpFeatureAutomatic MTR_NEWLY_AVAILABLE = 0x20,
+    MTRPumpConfigurationAndControlPumpFeatureLocal MTR_NEWLY_AVAILABLE = 0x40,
+} MTR_NEWLY_AVAILABLE;
 
 typedef NS_OPTIONS(uint16_t, MTRPumpConfigurationAndControlPumpStatus) {
     MTRPumpConfigurationAndControlPumpStatusDeviceFault API_AVAILABLE(ios(16.1), macos(13.0), watchos(9.1), tvos(16.1)) = 0x1,
