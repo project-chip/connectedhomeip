08d1ab5bc2949c089b49af0045310bc7daa3d488
Terence Hampson <thampson@google.com>
2023-01-10 14:23:22 +0000

    Restyle

--- src/controller/python/chip/yaml/runner.py
+++ src/controller/python/chip/yaml/runner.py
@@ -16,8 +16,8 @@
 #
 
 import asyncio as asyncio
-import queue
 import logging
+import queue
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
@@ -26,8 +26,7 @@ import chip.interaction_model
 import chip.yaml.format_converter as Converter
 import stringcase
 from chip import ChipDeviceCtrl
-from chip.clusters.Attribute import (
-    AttributeStatus, ValueDecodeFailure, TypedAttributePath, SubscriptionTransaction)
+from chip.clusters.Attribute import AttributeStatus, ValueDecodeFailure, TypedAttributePath, SubscriptionTransaction
 from chip.yaml.errors import ParsingError, UnexpectedParsingError
 
 from .data_model_lookup import *
