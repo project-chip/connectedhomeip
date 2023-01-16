25444ea2645c41793a5657f5e83c2d8582b5b0ec
ATmobica <artur.tynecki@arm.com>
2023-01-16 12:08:04 +0000

    [OIS] Update SDK version
    Udpate Open IoT SDK version to the latest available.
    Refactoring the Cmake build system for passing the SDK targets
    to the CHIP build in a more generic way.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>

--- config/openiotsdk/cmake/sdk.cmake
+++ config/openiotsdk/cmake/sdk.cmake
@@ -24,25 +24,13 @@ include(FetchContent)
 get_filename_component(OPEN_IOT_SDK_SOURCE ${CHIP_ROOT}/third_party/open-iot-sdk/sdk REALPATH)
 get_filename_component(OPEN_IOT_SDK_STORAGE_SOURCE ${CHIP_ROOT}/third_party/open-iot-sdk/storage REALPATH)
 
-# List of binary directories to Open IoT SDK sources
-list(APPEND SDK_SOURCES_BINARY_DIRS)
+# Open IoT SDK targets passed to CHIP build
+list(APPEND EXTERNAL_TARGETS)
 
-# Open IoT SDK configuration
-set(IOTSDK_MDH_ARM ON)
-set(MDH_PLATFORM "ARM_AN552_MPS3")
-set(MDH_ARM_BUILD_EXAMPLES OFF)
-set(IOTSDK_CMSIS_RTOS_API ON)
-set(IOTSDK_FREERTOS ON)
-set(IOTSDK_MBEDTLS ON)
-set(IOTSDK_LWIP ON)
-set(FETCHCONTENT_QUIET OFF)
-set(IOTSDK_EXAMPLES OFF)
-set(BUILD_TESTING NO)
-set(VARIANT "FVP")
+# Overwrite versions of Open IoT SDK components
 
 # Add a Matter specific version of Mbedtls
 FetchContent_Declare(
-
     mbedtls
     GIT_REPOSITORY https://github.com/ARMmbed/mbedtls
     GIT_TAG        v3.2.1
@@ -50,30 +38,69 @@ FetchContent_Declare(
     GIT_PROGRESS   ON
 )
 
+# Open IoT SDK configuration
+set(IOTSDK_FETCH_LIST
+    mcu-driver-reference-platforms-for-arm
+    cmsis-5
+    cmsis-freertos
+    mbedtls
+    lwip
+    cmsis-sockets-api
+)
+
+set(MDH_PLATFORM ARM_AN552_MPS3)
+set(VARIANT "FVP")
+set(FETCHCONTENT_QUIET OFF)
+
 # Add Open IoT SDK source
 add_subdirectory(${OPEN_IOT_SDK_SOURCE} ./sdk_build)
-list(APPEND SDK_SOURCES_BINARY_DIRS ${CMAKE_CURRENT_BINARY_DIR}/sdk_build)
 
 # Add Open IoT SDK modules to path
 list(APPEND CMAKE_MODULE_PATH ${open-iot-sdk_SOURCE_DIR}/cmake)
 
-# CMSIS-RTOS configuration
+# Configure component properties
+
 # CMSIS 5 require projects to provide configuration macros via RTE_Components.h
 # and CMSIS_device_header. The macro CMSIS_device_header is not automatically set
 # based on CMAKE_SYSTEM_PROCESSOR in the place where cmsis-core is first defined,
 # because a project may want to provide its own device header.
-if(TARGET cmsis-rtos-api)
-    target_include_directories(cmsis-rtos-api
-            PUBLIC
-                cmsis-config
-    )
-endif()
-
 if(TARGET cmsis-core)
     target_compile_definitions(cmsis-core
         INTERFACE
             $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},cortex-m55>:CMSIS_device_header="ARMCM55.h">
     )
+endif()
+
+# Add RTOS configuration headers
+# Link cmsis-rtos-api against a concrete implementation
+if(TARGET cmsis-rtos-api)
+    target_include_directories(cmsis-core 
+        INTERFACE 
+            cmsis-config
+    )
+
+    if(TARGET freertos-kernel)
+        target_include_directories(freertos-kernel 
+            PUBLIC 
+                freertos-config
+        )
+
+        target_link_libraries(freertos-kernel 
+            PUBLIC 
+                cmsis-core
+        )
+
+        target_link_libraries(cmsis-rtos-api
+            PUBLIC
+                freertos-cmsis-rtos
+                freertos-kernel-heap-3
+        )
+    elseif(TARGET cmsis-rtx)
+        target_link_libraries(cmsis-rtos-api
+            INTERFACE
+                cmsis-rtx
+        )
+    endif()
 endif()
 
 # LwIP configuration
@@ -127,28 +154,65 @@ if(TARGET mbedtls-config)
     )
 endif()
 
-# Declare RTOS interface target
-add_library(cmsis-rtos-implementation INTERFACE)
-
-if(TARGET freertos-kernel)
-    target_link_libraries(cmsis-rtos-implementation
-        INTERFACE
-            freertos-cmsis-rtos
-            freertos-kernel-heap-3
-    )
-    target_include_directories(cmsis-rtos-implementation
-        INTERFACE
-            ${CMAKE_CURRENT_SOURCE_DIR}/freertos-config
-    )
-elseif(TARGET cmsis-rtx)
-    target_link_libraries(cmsis-rtos-implementation
-        INTERFACE
-            cmsis-rtx
-            cmsis-rtos-api
-            cmsis-rtx-freertos-alloc-wrapper
+if("mcu-driver-reference-platforms-for-arm" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        mcu-driver-bootstrap
+        mcu-driver-hal
+        mdh-arm-corstone-300-common
+        target-interface
     )
 endif()
 
+if("cmsis-5" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        cmsis-core
+        cmsis-rtos-api
+        iotsdk-ip-network-api
+    )
+endif()
+
+if("cmsis-freertos" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        freertos-cmsis-rtos
+    )
+endif()
+
+if("mbedtls" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        mbedtls
+        mbedtls-config
+        mbedtls-threading-cmsis-rtos 
+    )
+endif()
+
+if("lwip" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        lwipcore
+        lwip-cmsis-port
+        lwip-cmsis-sys
+        lwip-cmsis-port-low-input-latency
+        lwipopts
+    )
+endif()
+
+if("cmsis-sockets-api" IN_LIST IOTSDK_FETCH_LIST)
+    list(APPEND EXTERNAL_TARGETS
+        cmsis-sockets-api
+        lwip-sockets
+    )
+endif()
+
+# Additional Open IoT SDK port components
+
 # Add Open IoT SDK storage source
 add_subdirectory(${OPEN_IOT_SDK_STORAGE_SOURCE} ./sdk_storage_build)
-list(APPEND SDK_SOURCES_BINARY_DIRS ${CMAKE_CURRENT_BINARY_DIR}/sdk_storage_build)
+list(APPEND EXTERNAL_TARGETS
+    iotsdk-blockdevice
+    iotsdk-tdbstore
+)
+
+# Add custom storage library
+add_subdirectory(${OPEN_IOT_SDK_CONFIG}/storage storage_build)
+list(APPEND EXTERNAL_TARGETS
+    openiotsdk-storage
+)
