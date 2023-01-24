project-chip/connectedhomeip

commit bba4182d61d56510f628307aff9844b75fb27b0d

[ESP32] Added door lock support for esp32. (#24547)
* [ESP32/Linux] Added support of door-lock for esp32 and moved linux
door-lock code to common

* Restored linux implementation, implemented door lock support for esp32

* Restyled by clang-format

* esp32/all-clusters-app's uses linux door-lock replace it with esp32 door-lock support

* Restyled by whitespace

* Restyled by clang-format

* Removed redundent handling of lock/unlock

* Rebased and fixed zap generated changes

* Restyled by clang-format

Co-authored-by: Restyled.io <commits@restyled.io>

examples/all-clusters-app/esp32/main/AppTask.cpp
@@ -27,7 +27,9 @@
 #include "esp_spi_flash.h"
 #include "freertos/FreeRTOS.h"
 #include <app/server/OnboardingCodesUtil.h>
+#include <lock/BoltLockManager.h>
 
+#define FACTORY_RESET_CANCEL_WINDOW_TIMEOUT 3000
 #define APP_TASK_NAME "APP"
 #define APP_EVENT_QUEUE_SIZE 10
 #define APP_TASK_STACK_SIZE (3072)
@@ -35,6 +37,7 @@
 static const char * TAG = "app-task";
 
 namespace {
+TimerHandle_t sFunctionTimer; // FreeRTOS app sw timer
 
 QueueHandle_t sAppEventQueue;
 TaskHandle_t sAppTaskHandle;
@@ -58,6 +61,106 @@ CHIP_ERROR AppTask::StartAppTask()
     return (xReturned == pdPASS) ? CHIP_NO_ERROR : APP_ERROR_CREATE_TASK_FAILED;
 }
 
+void AppTask::TimerEventHandler(TimerHandle_t xTimer)
+{
+    AppEvent event;
+    event.mType                = AppEvent::kEventType_Timer;
+    event.mTimerEvent.mContext = (void *) xTimer;
+    event.mHandler             = FunctionTimerEventHandler;
+    sAppTask.PostEvent(&event);
+}
+
+void AppTask::FunctionTimerEventHandler(AppEvent * aEvent)
+{
+    if (aEvent->mType != AppEvent::kEventType_Timer)
+    {
+        return;
+    }
+    // If we reached here, the button was held past FACTORY_RESET_TRIGGER_TIMEOUT,
+    // initiate factory reset
+    if (sAppTask.mFunctionTimerActive && sAppTask.mFunction == kFunction_StartBleAdv)
+    {
+        // ESP_LOGI(TAG, "Factory Reset Triggered. Release button within %ums to cancel.", FACTORY_RESET_CANCEL_WINDOW_TIMEOUT);
+        // Start timer for FACTORY_RESET_CANCEL_WINDOW_TIMEOUT to allow user to
+        // cancel, if required.
+        sAppTask.StartTimer(FACTORY_RESET_CANCEL_WINDOW_TIMEOUT);
+        sAppTask.mFunction = kFunction_FactoryReset;
+        // Turn off all LEDs before starting blink to make sure blink is
+        // co-ordinated.
+        // sStatusLED.Set(false);
+        // sLockLED.Set(false);
+        // sStatusLED.Blink(500);
+        // sLockLED.Blink(500);
+    }
+    else if (sAppTask.mFunctionTimerActive && sAppTask.mFunction == kFunction_FactoryReset)
+    {
+        // Actually trigger Factory Reset
+        sAppTask.mFunction = kFunction_NoneSelected;
+        chip::Server::GetInstance().ScheduleFactoryReset();
+    }
+}
+
+void AppTask::CancelTimer()
+{
+    if (xTimerStop(sFunctionTimer, 0) == pdFAIL)
+    {
+        ESP_LOGI(TAG, "app timer stop() failed");
+        return;
+    }
+    mFunctionTimerActive = false;
+}
+void AppTask::StartTimer(uint32_t aTimeoutInMs)
+{
+    if (xTimerIsTimerActive(sFunctionTimer))
+    {
+        ESP_LOGI(TAG, "app timer already started!");
+        CancelTimer();
+    }
+    // timer is not active, change its period to required value (== restart).
+    // FreeRTOS- Block for a maximum of 100 ticks if the change period command
+    // cannot immediately be sent to the timer command queue.
+    if (xTimerChangePeriod(sFunctionTimer, aTimeoutInMs / portTICK_PERIOD_MS, 100) != pdPASS)
+    {
+        ESP_LOGI(TAG, "app timer start() failed");
+        return;
+    }
+    mFunctionTimerActive = true;
+}
+void AppTask::ActionInitiated(BoltLockManager::Action_t aAction, int32_t aActor)
+{
+    // If the action has been initiated by the lock, update the bolt lock trait
+    // and start flashing the LEDs rapidly to indicate action initiation.
+    if (aAction == BoltLockManager::LOCK_ACTION)
+    {
+        ESP_LOGI(TAG, "Lock Action has been initiated");
+    }
+    else if (aAction == BoltLockManager::UNLOCK_ACTION)
+    {
+        ESP_LOGI(TAG, "Unlock Action has been initiated");
+    }
+    if (aActor == AppEvent::kEventType_Button)
+    {
+        sAppTask.mSyncClusterToButtonAction = true;
+    }
+    // sLockLED.Blink(50, 50);
+}
+void AppTask::ActionCompleted(BoltLockManager::Action_t aAction)
+{
+    // if the action has been completed by the lock, update the bolt lock trait.
+    // Turn on the lock LED if in a LOCKED state OR
+    // Turn off the lock LED if in an UNLOCKED state.
+    if (aAction == BoltLockManager::LOCK_ACTION)
+    {
+        ESP_LOGI(TAG, "Lock Action has been completed");
+        // sLockLED.Set(true);
+    }
+    else if (aAction == BoltLockManager::UNLOCK_ACTION)
+    {
+        ESP_LOGI(TAG, "Unlock Action has been completed");
+        // sLockLED.Set(false);
+    }
+}
+
 CHIP_ERROR AppTask::Init()
 {
     /* Print chip information */
@@ -69,7 +172,18 @@ CHIP_ERROR AppTask::Init()
     ESP_LOGI(TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
              (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    CHIP_ERROR err = CHIP_NO_ERROR;
+    // Create FreeRTOS sw timer for Function Selection
+    sFunctionTimer = xTimerCreate("FnTmr",          // Just a text name, not used by the RTOS kernel
+                                  1,                // == default timer period (mS)
+                                  false,            // no timer reload (==one-shot)
+                                  (void *) this,    // init timer id = app task obj context
+                                  TimerEventHandler // timer callback handler
+    );
+
+    CHIP_ERROR err = BoltLockMgr().InitLockState();
+
+    BoltLockMgr().SetCallbacks(ActionInitiated, ActionCompleted);
+
     statusLED1.Init(STATUS_LED_GPIO_NUM);
     // Our second LED doesn't map to any physical LEDs so far, just to virtual
     // "LED"s on devices with screens.

examples/all-clusters-app/esp32/main/include/AppTask.h
@@ -18,7 +18,8 @@
  */
 
 #pragma once
-#include "AppEvent.h"
+#include <lock/AppEvent.h>
+#include <lock/BoltLockManager.h>
 #include <platform/CHIPDeviceLayer.h>
 
 // Application-defined error codes in the CHIP_ERROR space.
@@ -28,6 +29,7 @@
 #define APP_ERROR_CREATE_TIMER_FAILED CHIP_APPLICATION_ERROR(0x04)
 #define APP_ERROR_START_TIMER_FAILED CHIP_APPLICATION_ERROR(0x05)
 #define APP_ERROR_STOP_TIMER_FAILED CHIP_APPLICATION_ERROR(0x06)
+#define APP_ERROR_ALLOCATION_FAILED CHIP_APPLICATION_ERROR(0x07)
 
 class AppTask
 {
@@ -40,7 +42,30 @@ public:
 
 private:
     CHIP_ERROR Init();
+
+    static void ActionInitiated(BoltLockManager::Action_t aAction, int32_t aActor);
+    static void ActionCompleted(BoltLockManager::Action_t aAction);
+
+    void StartTimer(uint32_t aTimeoutMs);
+    void CancelTimer(void);
+
+    static void FunctionTimerEventHandler(AppEvent * aEvent);
+    static void TimerEventHandler(TimerHandle_t xTimer);
+
     void DispatchEvent(AppEvent * event);
+
+    enum Function_t
+    {
+        kFunction_NoneSelected   = 0,
+        kFunction_SoftwareUpdate = 0,
+        kFunction_StartBleAdv    = 1,
+        kFunction_FactoryReset   = 2,
+        kFunction_Invalid
+    } Function;
+    Function_t mFunction;
+    bool mFunctionTimerActive;
+    bool mSyncClusterToButtonAction;
+
     static AppTask sAppTask;
     friend AppTask & GetAppTask(void);
 };

examples/lock-app/esp32/main/AppTask.cpp
@@ -16,13 +16,14 @@
  */
 
 #include "AppTask.h"
-#include "AppConfig.h"
-#include "AppEvent.h"
 #include "Button.h"
 #include "LEDWidget.h"
 #include "esp_log.h"
 #include <app-common/zap-generated/attribute-id.h>
 #include <app-common/zap-generated/attribute-type.h>
+#include <lock/AppConfig.h>
+#include <lock/AppEvent.h>
+//#include <app-common/zap-generated/attributes/Accessors.h>
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/server/OnboardingCodesUtil.h>
 #include <app/server/Server.h>
@@ -60,6 +61,7 @@ StackType_t appStack[APP_TASK_STACK_SIZE / sizeof(StackType_t)];
 
 using namespace ::chip::DeviceLayer;
 using namespace ::chip::System;
+// using namespace ESP32DoorLock::LockInitParams;
 
 AppTask AppTask::sAppTask;
 
@@ -79,19 +81,14 @@ CHIP_ERROR AppTask::StartAppTask()
 
 CHIP_ERROR AppTask::Init()
 {
-    // Create FreeRTOS sw timer for Function Selection.
+    // Create FreeRTOS sw timer for Function Selection
     sFunctionTimer = xTimerCreate("FnTmr",          // Just a text name, not used by the RTOS kernel
                                   1,                // == default timer period (mS)
                                   false,            // no timer reload (==one-shot)
                                   (void *) this,    // init timer id = app task obj context
                                   TimerEventHandler // timer callback handler
     );
-    CHIP_ERROR err = BoltLockMgr().Init();
-    if (err != CHIP_NO_ERROR)
-    {
-        ESP_LOGI(TAG, "BoltLockMgr().Init() failed");
-        return err;
-    }
+    CHIP_ERROR err = BoltLockMgr().InitLockState();
 
     BoltLockMgr().SetCallbacks(ActionInitiated, ActionCompleted);
 
@@ -201,12 +198,12 @@ void AppTask::LockActionEventHandler(AppEvent * aEvent)
     int32_t actor;
     CHIP_ERROR err = CHIP_NO_ERROR;
 
-    if (aEvent->Type == AppEvent::kEventType_Lock)
+    if (aEvent->mType == AppEvent::kEventType_Lock)
     {
-        action = static_cast<BoltLockManager::Action_t>(aEvent->LockEvent.Action);
-        actor  = aEvent->LockEvent.Actor;
+        action = static_cast<BoltLockManager::Action_t>(aEvent->mLockEvent.mAction);
+        actor  = aEvent->mLockEvent.mActor;
     }
-    else if (aEvent->Type == AppEvent::kEventType_Button)
+    else if (aEvent->mType == AppEvent::kEventType_Button)
     {
         if (BoltLockMgr().IsUnlocked())
         {
@@ -241,19 +238,19 @@ void AppTask::ButtonEventHandler(uint8_t btnIdx, uint8_t btnAction)
         return;
     }
 
-    AppEvent button_event           = {};
-    button_event.Type               = AppEvent::kEventType_Button;
-    button_event.ButtonEvent.PinNo  = btnIdx;
-    button_event.ButtonEvent.Action = btnAction;
+    AppEvent button_event             = {};
+    button_event.mType                = AppEvent::kEventType_Button;
+    button_event.mButtonEvent.mPinNo  = btnIdx;
+    button_event.mButtonEvent.mAction = btnAction;
 
     if (btnIdx == APP_LOCK_BUTTON && btnAction == APP_BUTTON_PRESSED)
     {
-        button_event.Handler = LockActionEventHandler;
+        button_event.mHandler = LockActionEventHandler;
         sAppTask.PostEvent(&button_event);
     }
     else if (btnIdx == APP_FUNCTION_BUTTON)
     {
-        button_event.Handler = FunctionHandler;
+        button_event.mHandler = FunctionHandler;
         sAppTask.PostEvent(&button_event);
     }
 }
@@ -261,15 +258,15 @@ void AppTask::ButtonEventHandler(uint8_t btnIdx, uint8_t btnAction)
 void AppTask::TimerEventHandler(TimerHandle_t xTimer)
 {
     AppEvent event;
-    event.Type               = AppEvent::kEventType_Timer;
-    event.TimerEvent.Context = (void *) xTimer;
-    event.Handler            = FunctionTimerEventHandler;
+    event.mType                = AppEvent::kEventType_Timer;
+    event.mTimerEvent.mContext = (void *) xTimer;
+    event.mHandler             = FunctionTimerEventHandler;
     sAppTask.PostEvent(&event);
 }
 
 void AppTask::FunctionTimerEventHandler(AppEvent * aEvent)
 {
-    if (aEvent->Type != AppEvent::kEventType_Timer)
+    if (aEvent->mType != AppEvent::kEventType_Timer)
     {
         return;
     }
@@ -304,7 +301,7 @@ void AppTask::FunctionTimerEventHandler(AppEvent * aEvent)
 
 void AppTask::FunctionHandler(AppEvent * aEvent)
 {
-    if (aEvent->ButtonEvent.PinNo != APP_FUNCTION_BUTTON)
+    if (aEvent->mButtonEvent.mPinNo != APP_FUNCTION_BUTTON)
     {
         return;
     }
@@ -316,7 +313,7 @@ void AppTask::FunctionHandler(AppEvent * aEvent)
     // FACTORY_RESET_TRIGGER_TIMEOUT to signal factory reset has been initiated.
     // To cancel factory reset: release the APP_FUNCTION_BUTTON once all LEDs
     // start blinking within the FACTORY_RESET_CANCEL_WINDOW_TIMEOUT
-    if (aEvent->ButtonEvent.Action == APP_BUTTON_PRESSED)
+    if (aEvent->mButtonEvent.mAction == APP_BUTTON_PRESSED)
     {
         if (!sAppTask.mFunctionTimerActive && sAppTask.mFunction == kFunction_NoneSelected)
         {
@@ -431,10 +428,10 @@ void AppTask::ActionCompleted(BoltLockManager::Action_t aAction)
 void AppTask::PostLockActionRequest(int32_t aActor, BoltLockManager::Action_t aAction)
 {
     AppEvent event;
-    event.Type             = AppEvent::kEventType_Lock;
-    event.LockEvent.Actor  = aActor;
-    event.LockEvent.Action = aAction;
-    event.Handler          = LockActionEventHandler;
+    event.mType              = AppEvent::kEventType_Lock;
+    event.mLockEvent.mActor  = aActor;
+    event.mLockEvent.mAction = aAction;
+    event.mHandler           = LockActionEventHandler;
     PostEvent(&event);
 }
 
@@ -451,9 +448,9 @@ void AppTask::PostEvent(const AppEvent * aEvent)
 
 void AppTask::DispatchEvent(AppEvent * aEvent)
 {
-    if (aEvent->Handler)
+    if (aEvent->mHandler)
     {
-        aEvent->Handler(aEvent);
+        aEvent->mHandler(aEvent);
     }
     else
     {

examples/lock-app/esp32/main/BoltLockManager.cpp
@@ -1,217 +1,0 @@
-/*
- *
- *    Copyright (c) 2020 Project CHIP Authors
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
-#include "BoltLockManager.h"
-
-#include "AppConfig.h"
-#include "AppTask.h"
-#include "esp_log.h"
-#include <freertos/FreeRTOS.h>
-static const char * TAG = "BoltLockManager";
-BoltLockManager BoltLockManager::sLock;
-
-TimerHandle_t sLockTimer;
-
-CHIP_ERROR BoltLockManager::Init()
-{
-    // Create FreeRTOS sw timer for lock timer.
-    sLockTimer = xTimerCreate("lockTmr",        // Just a text name, not used by the RTOS kernel
-                              1,                // == default timer period (mS)
-                              false,            // no timer reload (==one-shot)
-                              (void *) this,    // init timer id = lock obj context
-                              TimerEventHandler // timer callback handler
-    );
-
-    if (sLockTimer == NULL)
-    {
-        ESP_LOGE(TAG, "sLockTimer timer create failed");
-        return APP_ERROR_CREATE_TIMER_FAILED;
-    }
-
-    mState              = kState_LockingCompleted;
-    mAutoLockTimerArmed = false;
-    mAutoRelock         = false;
-    mAutoLockDuration   = 0;
-
-    return CHIP_NO_ERROR;
-}
-
-void BoltLockManager::SetCallbacks(Callback_fn_initiated aActionInitiated_CB, Callback_fn_completed aActionCompleted_CB)
-{
-    mActionInitiated_CB = aActionInitiated_CB;
-    mActionCompleted_CB = aActionCompleted_CB;
-}
-
-bool BoltLockManager::IsActionInProgress()
-{
-    return (mState == kState_LockingInitiated || mState == kState_UnlockingInitiated);
-}
-
-bool BoltLockManager::IsUnlocked()
-{
-    return (mState == kState_UnlockingCompleted);
-}
-
-void BoltLockManager::SetAutoLockDuration(uint32_t aDurationInSecs)
-{
-    mAutoLockDuration = aDurationInSecs;
-}
-
-bool BoltLockManager::InitiateAction(int32_t aActor, Action_t aAction)
-{
-    bool action_initiated = false;
-    State_t new_state;
-    // Initiate Lock/Unlock Action only when the previous one is complete.
-    if (mState == kState_LockingCompleted && aAction == UNLOCK_ACTION)
-    {
-        action_initiated = true;
-
-        new_state = kState_UnlockingInitiated;
-    }
-    else if (mState == kState_UnlockingCompleted && aAction == LOCK_ACTION)
-    {
-        action_initiated = true;
-
-        new_state = kState_LockingInitiated;
-    }
-
-    if (action_initiated)
-    {
-        if (mAutoLockTimerArmed && new_state == kState_LockingInitiated)
-        {
-            // If auto lock timer has been armed and someone initiates locking,
-            // cancel the timer and continue as normal.
-            mAutoLockTimerArmed = false;
-
-            CancelTimer();
-        }
-
-        StartTimer(ACTUATOR_MOVEMENT_PERIOS_MS);
-
-        // Since the timer started successfully, update the state and trigger callback
-        mState = new_state;
-
-        if (mActionInitiated_CB)
-        {
-            mActionInitiated_CB(aAction, aActor);
-        }
-    }
-
-    return action_initiated;
-}
-
-void BoltLockManager::StartTimer(uint32_t aTimeoutMs)
-{
-    if (xTimerIsTimerActive(sLockTimer))
-    {
-        ESP_LOGI(TAG, "app timer already started!");
-        CancelTimer();
-    }
-
-    // timer is not active, change its period to required value (== restart).
-    // FreeRTOS- Block for a maximum of 100 ticks if the change period command
-    // cannot immediately be sent to the timer command queue.
-    if (xTimerChangePeriod(sLockTimer, (aTimeoutMs / portTICK_PERIOD_MS), 100) != pdPASS)
-    {
-        ESP_LOGI(TAG, "sLockTimer timer start() failed");
-        return;
-    }
-}
-
-void BoltLockManager::CancelTimer(void)
-{
-    if (xTimerStop(sLockTimer, 0) == pdFAIL)
-    {
-        ESP_LOGI(TAG, "Lock timer timer stop() failed");
-        return;
-    }
-}
-void BoltLockManager::TimerEventHandler(TimerHandle_t xTimer)
-{
-    // Get lock obj context from timer id.
-    BoltLockManager * lock = static_cast<BoltLockManager *>(pvTimerGetTimerID(xTimer));
-
-    // The timer event handler will be called in the context of the timer task
-    // once sLockTimer expires. Post an event to apptask queue with the actual handler
-    // so that the event can be handled in the context of the apptask.
-    AppEvent event;
-    event.Type               = AppEvent::kEventType_Timer;
-    event.TimerEvent.Context = lock;
-    if (lock->mAutoLockTimerArmed)
-    {
-        event.Handler = AutoReLockTimerEventHandler;
-    }
-    else
-    {
-        event.Handler = ActuatorMovementTimerEventHandler;
-    }
-    GetAppTask().PostEvent(&event);
-}
-
-void BoltLockManager::AutoReLockTimerEventHandler(AppEvent * aEvent)
-{
-    BoltLockManager * lock = static_cast<BoltLockManager *>(aEvent->TimerEvent.Context);
-    int32_t actor          = 0;
-
-    // Make sure auto lock timer is still armed.
-    if (!lock->mAutoLockTimerArmed)
-    {
-        return;
-    }
-
-    lock->mAutoLockTimerArmed = false;
-
-    ESP_LOGI(TAG, "Auto Re-Lock has been triggered!");
-
-    lock->InitiateAction(actor, LOCK_ACTION);
-}
-
-void BoltLockManager::ActuatorMovementTimerEventHandler(AppEvent * aEvent)
-{
-    Action_t actionCompleted = INVALID_ACTION;
-
-    BoltLockManager * lock = static_cast<BoltLockManager *>(aEvent->TimerEvent.Context);
-
-    if (lock->mState == kState_LockingInitiated)
-    {
-        lock->mState    = kState_LockingCompleted;
-        actionCompleted = LOCK_ACTION;
-    }
-    else if (lock->mState == kState_UnlockingInitiated)
-    {
-        lock->mState    = kState_UnlockingCompleted;
-        actionCompleted = UNLOCK_ACTION;
-    }
-
-    if (actionCompleted != INVALID_ACTION)
-    {
-        if (lock->mActionCompleted_CB)
-        {
-            lock->mActionCompleted_CB(actionCompleted);
-        }
-
-        if (lock->mAutoRelock && actionCompleted == UNLOCK_ACTION)
-        {
-            // Start the timer for auto relock
-            lock->StartTimer(lock->mAutoLockDuration * 1000);
-
-            lock->mAutoLockTimerArmed = true;
-
-            ESP_LOGI(TAG, "Auto Re-lock enabled. Will be triggered in %u seconds", lock->mAutoLockDuration);
-        }
-    }
-}

examples/lock-app/esp32/main/include/AppTask.h
@@ -20,8 +20,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "AppEvent.h"
-#include "BoltLockManager.h"
+#include <lock/AppEvent.h>
+#include <lock/BoltLockManager.h>
 
 #include "freertos/FreeRTOS.h"
 #include <ble/BLEEndPoint.h>
@@ -35,6 +35,7 @@
 #define APP_ERROR_CREATE_TIMER_FAILED CHIP_APPLICATION_ERROR(0x04)
 #define APP_ERROR_START_TIMER_FAILED CHIP_APPLICATION_ERROR(0x05)
 #define APP_ERROR_STOP_TIMER_FAILED CHIP_APPLICATION_ERROR(0x06)
+#define APP_ERROR_ALLOCATION_FAILED CHIP_APPLICATION_ERROR(0x07)
 
 class AppTask
 {

examples/all-clusters-app/esp32/main/CMakeLists.txt
@@ -20,7 +20,6 @@ set(PRIV_INCLUDE_DIRS_LIST
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/zzz_generated/all-clusters-app"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/all-clusters-app/all-clusters-common/include"
                       "${CMAKE_CURRENT_LIST_DIR}/include"
-                      "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/lock-app/linux/include"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/providers"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/platform/esp32"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/third_party/nlfaultinjection/repo/include"                      
@@ -30,11 +29,11 @@ set(SRC_DIRS_LIST
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/zzz_generated/all-clusters-app/zap-generated"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/zzz_generated/app-common/app-common/zap-generated/attributes"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/zzz_generated/app-common/app-common/zap-generated"
-                      "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/lock-app/linux/src"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/providers"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/platform/esp32/ota"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/platform/esp32/common"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/platform/esp32/shell_extension"
+                      "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/platform/esp32/lock"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/src/app/server"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/src/app/util"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/src/app/reporting"
@@ -92,10 +91,6 @@ set(SRC_DIRS_LIST
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/src/app/clusters/power-source-configuration-server"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/src/app/clusters/power-source-server"
                       "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/all-clusters-app/all-clusters-common/src"
-)
-
-set(EXCLUDE_SRCS_LIST
-                      "${CMAKE_SOURCE_DIR}/third_party/connectedhomeip/examples/lock-app/linux/src/LockAppCommandDelegate.cpp"
 )
 
 if (CONFIG_ENABLE_PW_RPC)

examples/lock-app/esp32/main/Button.cpp
@@ -18,7 +18,7 @@
 #include "Button.h"
 #include "AppTask.h"
 
-#include "AppConfig.h"
+//#include <lock/AppConfig.h>
 
 esp_err_t Button::Init(gpio_num_t gpioNum, uint16_t debouncePeriod)
 {

commit 627ec988ab304654d6c2ec5a65cbf5362dad721c

Fix the boot loop crash on M5Stack
This is the regression from #24547, which is accessing the attributes
even before Server is ready. Moved the lock initialization after service is initialized

examples/all-clusters-app/esp32/main/AppTask.cpp
@@ -161,6 +161,13 @@ void AppTask::ActionCompleted(BoltLockManager::Action_t aAction)
     }
 }
 
+CHIP_ERROR AppTask::LockInit()
+{
+    ReturnErrorOnFailure(BoltLockMgr().InitLockState());
+    BoltLockMgr().SetCallbacks(ActionInitiated, ActionCompleted);
+    return CHIP_NO_ERROR;
+}
+
 CHIP_ERROR AppTask::Init()
 {
     /* Print chip information */
@@ -179,10 +186,7 @@ CHIP_ERROR AppTask::Init()
                                   (void *) this,    // init timer id = app task obj context
                                   TimerEventHandler // timer callback handler
     );
-
-    CHIP_ERROR err = BoltLockMgr().InitLockState();
-
-    BoltLockMgr().SetCallbacks(ActionInitiated, ActionCompleted);
+    VerifyOrReturnError(sFunctionTimer != NULL, CHIP_ERROR_NO_MEMORY, ESP_LOGE(TAG, "Failed to create function selection timer"));
 
     statusLED1.Init(STATUS_LED_GPIO_NUM);
     // Our second LED doesn't map to any physical LEDs so far, just to virtual
@@ -199,7 +203,7 @@ CHIP_ERROR AppTask::Init()
     InitDeviceDisplay();
 #endif
 
-    return err;
+    return CHIP_NO_ERROR;
 }
 
 void AppTask::AppTaskMain(void * pvParameter)

examples/all-clusters-app/esp32/main/include/AppTask.h
@@ -39,6 +39,7 @@ public:
     void PostEvent(const AppEvent * event);
     void ButtonEventHandler(uint8_t btnIdx, uint8_t btnAction);
     static void ButtonPressedAction(AppEvent * aEvent);
+    CHIP_ERROR LockInit();
 
 private:
     CHIP_ERROR Init();

examples/all-clusters-app/esp32/main/main.cpp
@@ -115,6 +115,13 @@ static void InitServer(intptr_t context)
     emberAfEndpointEnableDisable(kNetworkCommissioningEndpointSecondary, false);
 
     InitBindingHandlers();
+
+    CHIP_ERROR err = GetAppTask().LockInit();
+    if (err != CHIP_NO_ERROR)
+    {
+        ESP_LOGE(TAG, "Failed to initialize app task lock, err:%" CHIP_FORMAT_ERROR, err.Format());
+    }
+
 #if CONFIG_DEVICE_TYPE_M5STACK
     SetupPretendDevices();
 #endif

examples/platform/esp32/lock/BoltLockManager.cpp
@@ -764,7 +764,6 @@ CHIP_ERROR BoltLockManager::InitLockState()
     // Initial lock state
     chip::app::DataModel::Nullable<chip::app::Clusters::DoorLock::DlLockState> state;
     chip::EndpointId endpointId{ 1 };
-    chip::DeviceLayer::PlatformMgr().LockChipStack();
     chip::app::Clusters::DoorLock::Attributes::LockState::Get(endpointId, state);
 
     uint8_t numberOfCredentialsPerUser = 0;
@@ -815,8 +814,6 @@ CHIP_ERROR BoltLockManager::InitLockState()
             endpointId);
         numberOfHolidaySchedules = 10;
     }
-
-    chip::DeviceLayer::PlatformMgr().UnlockChipStack();
 
     CHIP_ERROR err = BoltLockMgr().Init(state,
                                         ParamBuilder()