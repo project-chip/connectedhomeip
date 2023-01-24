f1a0557011167b6c54b1f8061eda3e54d96f702f
Terence Hampson <thampson@google.com>
2023-01-24 14:23:38 +0000

    Have chip-repl yamltests parser use PICs

--- scripts/tests/chiptest/yamltest_with_chip_repl_tester.py
+++ scripts/tests/chiptest/yamltest_with_chip_repl_tester.py
@@ -66,7 +66,11 @@ def _sort_with_global_attribute_first(a, b):
     '--node-id',
     default=0x12344321,
     help='Node ID to use when commissioning device')
-def main(setup_code, yaml_path, node_id):
+@click.option(
+    '--pics-file',
+    default=None,
+    help='Optional PICS file')
+def main(setup_code, yaml_path, node_id, pics_file):
     # Setting up python environment for running YAML CI tests using python parser.
     with tempfile.NamedTemporaryFile() as chip_stack_storage:
         chip.native.Init()
@@ -95,20 +99,21 @@ def main(setup_code, yaml_path, node_id):
         clusters_definitions = SpecDefinitions(sources)
 
         # Parsing YAML test and setting up chip-repl yamltests runner.
-        yaml = TestParser(yaml_path, None, clusters_definitions)
+        yaml = TestParser(yaml_path, pics_file, clusters_definitions)
         runner = ReplTestRunner(clusters_definitions, certificate_authority_manager, dev_ctrl)
 
         # Executing and validating test
         for test_step in yaml.tests:
             test_action = runner.encode(test_step)
             # TODO if test_action is None we should see if it is a pseudo cluster.
-            if test_action is not None:
-                response = runner.execute(test_action)
-                decoded_response = runner.decode(response)
-                post_processing_result = test_step.post_process_response(decoded_response)
-                if not post_processing_result.is_success():
-                    exit(-2)
-            else:
+            if test_action is None:
+                exit(-2)
+            if not test_action.pics_enabled:
+                continue
+            response = runner.execute(test_action)
+            decoded_response = runner.decode(response)
+            post_processing_result = test_step.post_process_response(decoded_response)
+            if not post_processing_result.is_success():
                 exit(-2)
 
         runner.shutdown()
