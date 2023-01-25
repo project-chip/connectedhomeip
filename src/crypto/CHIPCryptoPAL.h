b7a89600fbf6b5c9433cf27fd9c1906b1ec2ea35
Ji Luo <ji.luo@nxp.com>
2023-01-25 17:14:02 +0800

    Support P256KeyPair based on Trusty OS
    Move the p256keypair operations to Trusty OS to enhance
    the crypto security.
    
    Change-Id: I47ec6b440f91adf3e717ed8915f35b7844731c90
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/21251
    Reviewed-by: Elven Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23005

--- src/crypto/CHIPCryptoPAL.h
+++ src/crypto/CHIPCryptoPAL.h
@@ -588,6 +588,10 @@ public:
     /** Release resources associated with this key pair */
     void Clear();
 
+#if CHIP_CRYPTO_TRUSTY_OS
+    uint64_t p256_handler = 0;
+#endif
+
 protected:
     P256PublicKey mPublicKey;
     mutable P256KeypairContext mKeypair;
