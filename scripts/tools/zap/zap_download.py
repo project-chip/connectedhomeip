767d7b26740595cb00a6aed4f0c96516650c1f47
Andrei Litvin <andy314@gmail.com>
2023-01-25 13:25:25 -0500

    Added some functionality - at least zap release download seems to work

--- scripts/tools/zap/zap_download.py
+++ scripts/tools/zap/zap_download.py
@@ -14,9 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import enum
 import logging
 import os
 import re
+import requests
+import zipfile
+import io
+import sys
+import shlex
+
 from typing import Optional
 
 import click
@@ -36,9 +43,10 @@ __LOG_LEVELS__ = {
     'fatal': logging.FATAL,
 }
 
-# TODO:
-#  - figure out "current zap version to use"
-#  - select what to download
+
+class DownloadType(enum.Enum):
+    RELEASE = enum.auto()  # asking for a zip file release download
+    SOURCE = enum.auto()  # asking for a source download (will work on arm64 for example)
 
 # Downloads zap into the current environment
 # Arguments:
@@ -55,6 +63,34 @@ def _GetDefaultExtractRoot():
         # Before bootstrap, this will pick a temporary directory. Probably
         # not ideal, but it likely just works
         return '/tmp/'
+
+def _SetupSourceZap(install_directory: str, zap_version: str):
+    # TODO:
+    #   - git checkout
+    #   - npm ci
+    raise Exception("NOT YET IMPLEMENTED")
+
+def _SetupReleaseZap(install_directory: str, zap_version: str):
+
+    if sys.platform == 'linux':
+        zap_platform = 'linux'
+    elif sys.platform == 'darwin':
+        zap_platform = 'mac'
+    else:
+        raise Exception('Unknown platform - do not know what zip file to download.')
+
+
+    url = f"https://github.com/project-chip/zap/releases/download/{zap_version}/zap-{zap_platform}.zip"
+
+    logging.info("Fetching: %s", url)
+
+    r = requests.get(url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+
+    logging.info("Data downloaded, extracting ...")
+    z.extractall(install_directory)
+    logging.info("Done extracting.")
+
 
 def _GetZapVersionToUse(project_root):
     """
@@ -81,6 +117,7 @@ def _GetZapVersionToUse(project_root):
 
     with open(docker_path, 'rt') as f:
         for l in f.readlines():
+            l = l.strip()
             m = match_re.match(l)
             if not m:
                 continue
@@ -95,6 +132,7 @@ def _GetZapVersionToUse(project_root):
     default='INFO',
     show_default=True,
     type=click.Choice(__LOG_LEVELS__.keys(), case_sensitive=False),
+    callback=lambda c, p, v: __LOG_LEVELS__[v],
     help='Determines the verbosity of script output')
 @click.option(
     '--sdk-root',
@@ -110,13 +148,19 @@ def _GetZapVersionToUse(project_root):
     '--zap-version',
     default=None,
     help='Force to checkout this zap version instead of trying to auto-detect')
-def main(log_level: str, sdk_root: str, extract_root: str, zap_version=Optional[str]):
+@click.option(
+    '--zap',
+    default='RELEASE',
+    show_default=True,
+    type=click.Choice(DownloadType.__members__, case_sensitive=False),
+    callback=lambda c, p, v: getattr(DownloadType, v),
+    help='What type of zap download to perform')
+def main(log_level: str, sdk_root: str, extract_root: str, zap_version: Optional[str], zap: DownloadType):
     if _has_coloredlogs:
-        coloredlogs.install(level=__LOG_LEVELS__[
-                            log_level], fmt='%(asctime)s %(levelname)-7s %(message)s')
+        coloredlogs.install(level=log_level, fmt='%(asctime)s %(levelname)-7s %(message)s')
     else:
         logging.basicConfig(
-            level=__LOG_LEVELS__[log_level],
+            level=log_level,
             format='%(asctime)s %(levelname)-7s %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S'
         )
@@ -125,8 +169,20 @@ def main(log_level: str, sdk_root: str, extract_root: str, zap_version=Optional[
         zap_version = _GetZapVersionToUse(sdk_root)
         logging.info('Found required zap version to be: %s' % zap_version)
 
-    # TODO: implement
-    pass
+    logging.debug('User requested to download a %s zap version %s into %s', zap, zap_version, extract_root)
+
+    install_directory = os.path.join(extract_root, f"zap-{zap_version}")
+    if zap == DownloadType.SOURCE:
+        install_directory = install_directory + "-src"
+        _SetupSourceZap(install_directory, zap_version)
+
+        # Make sure the results can be used in scripts
+        print(f"export ZAP_DEVELOPMENT_PATH={shlex.quote(install_directory)}")
+    else:
+        _SetupReleaseZap(install_directory, zap_version)
+
+        # Make sure the results can be used in scripts
+        print(f"export ZAP_INSTALL_PATH={shlex.quote(install_directory)}")
 
 
 if __name__ == '__main__':
