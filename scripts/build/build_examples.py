e22ba48cb7e18b39daa5526b1f04ba4791a82e9f
Arkadiusz Bokowy <a.bokowy@samsung.com>
2023-01-26 13:12:33 +0100

    Validate target names passed to build_examples.py (#24644)
    * Validate target names passed to build_examples.py
    
    Skipping invalid target names might lead to false positive CI passes in
    case when target name is invalid. In such case test was never executed.
    
    * Fix target names for Tizen examples CI checks
    
    * Restyled by isort
    
    * Add libwebsockets submodule to Tizen platform
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- scripts/build/build_examples.py
+++ scripts/build/build_examples.py
@@ -20,10 +20,10 @@ import sys
 
 import click
 import coloredlogs
-
-import build
 from builders.builder import BuilderOptions
 from runner import PrintOnlyRunner, ShellRunner
+
+import build
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
@@ -59,6 +59,18 @@ def ValidateRepoPath(context, parameter, value):
     return value
 
 
+def ValidateTargetNames(context, parameter, values):
+    """
+    Validates that the given target name is valid.
+    """
+    for value in values:
+        if not any(target.StringIntoTargetParts(value.lower())
+                   for target in build.targets.BUILD_TARGETS):
+            raise click.BadParameter(
+                "'%s' is not a valid target name." % value)
+    return values
+
+
 @click.group(chain=True)
 @click.option(
     '--log-level',
@@ -69,6 +81,7 @@ def ValidateRepoPath(context, parameter, value):
     '--target',
     default=[],
     multiple=True,
+    callback=ValidateTargetNames,
     help='Build target(s)'
 )
 @click.option(
