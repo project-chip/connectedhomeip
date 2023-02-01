0295ceb9a24323a497d863d4c791d9a9a127e4e2
Restyled.io <commits@restyled.io>
2023-02-01 15:53:21 +0000

    Restyled by isort

--- src/setup_payload/python/generate_setup_payload.py
+++ src/setup_payload/python/generate_setup_payload.py
@@ -18,8 +18,7 @@
 import argparse
 import sys
 
-from SetupPayload import SetupPayload, CommissioningFlow
-
+from SetupPayload import CommissioningFlow, SetupPayload
 
 INVALID_PASSCODES = [00000000, 11111111, 22222222, 33333333, 44444444, 55555555,
                      66666666, 77777777, 88888888, 99999999, 12345678, 87654321]
