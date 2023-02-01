f5b9d45b919dd2650c9c947d2602bdddc0e78cb3
Damian Krolik <damian.krolik@nordicsemi.no>
2023-02-01 20:33:15 +0100

    Code review p.2

--- src/controller/CHIPDeviceControllerFactory.h
+++ src/controller/CHIPDeviceControllerFactory.h
@@ -94,9 +94,9 @@ struct SetupParams
     CommissioningDelegate * defaultCommissioner                        = nullptr;
 };
 
-// TODO everything other than the fabric storage, group data provider, OperationalKeystore
-// and OperationalCertificateStore here should be removed. We're blocked because of the
-// need to support !CHIP_DEVICE_LAYER
+// TODO everything other than the fabric storage, group data provider, OperationalKeystore,
+// OperationalCertificateStore and SessionKeystore here should be removed. We're blocked
+// because of the need to support !CHIP_DEVICE_LAYER
 struct FactoryInitParams
 {
     System::Layer * systemLayer                                        = nullptr;
