239af08dde5a7be6f6ea5b381f0ba026e90dd329
Jeff Tung <100387939+jtung-apple@users.noreply.github.com>
2023-02-03 16:20:32 -0800

    Clarify NSTimeInterval conversion further

--- src/darwin/Framework/CHIP/MTRDevice.mm
+++ src/darwin/Framework/CHIP/MTRDevice.mm
@@ -118,9 +118,10 @@ NSTimeInterval MTRTimeIntervalForEventTimestampValue(uint64_t timeValue)
 
     // First convert the event timestamp value (in milliseconds) to NSTimeInterval - to minimize potential loss of precision
     // of uint64 => NSTimeInterval (double), convert whole seconds and remainder separately and then combine
-    NSTimeInterval eventTimestampValueSeconds = timeValue / chip::kMillisecondsPerSecond;
+    uint64_t eventTimestampValueSeconds = timeValue / chip::kMillisecondsPerSecond;
+    uint64_t eventTimestampValueRemainderMilliseconds = timeValue % chip::kMillisecondsPerSecond;
     NSTimeInterval eventTimestampValueRemainder
-        = ((NSTimeInterval)(timeValue % chip::kMillisecondsPerSecond)) / chip::kMillisecondsPerSecond;
+        = NSTimeInterval(eventTimestampValueRemainderMilliseconds) / chip::kMillisecondsPerSecond;
     NSTimeInterval eventTimestampValue = eventTimestampValueSeconds + eventTimestampValueRemainder;
 
     return eventTimestampValue;
