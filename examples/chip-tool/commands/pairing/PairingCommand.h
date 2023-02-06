2ea6afcc3ae0ac623b01f8266f2339d405d8db67
Yufeng Wang <yufengwang@google.com>
2023-02-06 14:48:00 -0800

    [chip-tool] Remove pairing ethernet command

--- examples/chip-tool/commands/pairing/PairingCommand.h
+++ examples/chip-tool/commands/pairing/PairingCommand.h
@@ -34,7 +34,6 @@ enum class PairingMode
     CodePaseOnly,
     Ble,
     SoftAP,
-    Ethernet,
     OnNetwork,
 };
 
@@ -43,7 +42,6 @@ enum class PairingNetworkType
     None,
     WiFi,
     Thread,
-    Ethernet,
 };
 
 class PairingCommand : public CHIPCommand,
@@ -69,7 +67,6 @@ public:
         switch (networkType)
         {
         case PairingNetworkType::None:
-        case PairingNetworkType::Ethernet:
             break;
         case PairingNetworkType::WiFi:
             AddArgument("ssid", &mSSID);
@@ -102,14 +99,6 @@ public:
             AddArgument("pase-only", 0, 1, &mPaseOnly);
             break;
         case PairingMode::SoftAP:
-            AddArgument("skip-commissioning-complete", 0, 1, &mSkipCommissioningComplete);
-            AddArgument("setup-pin-code", 0, 134217727, &mSetupPINCode);
-            AddArgument("discriminator", 0, 4096, &mDiscriminator);
-            AddArgument("device-remote-ip", &mRemoteAddr);
-            AddArgument("device-remote-port", 0, UINT16_MAX, &mRemotePort);
-            AddArgument("pase-only", 0, 1, &mPaseOnly);
-            break;
-        case PairingMode::Ethernet:
             AddArgument("skip-commissioning-complete", 0, 1, &mSkipCommissioningComplete);
             AddArgument("setup-pin-code", 0, 134217727, &mSetupPINCode);
             AddArgument("discriminator", 0, 4096, &mDiscriminator);
