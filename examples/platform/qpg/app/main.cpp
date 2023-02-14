ff8524f27c475d800822ba8881b7ac47ce472a05
lucicop <lucian.copat@qorvo.com>
2023-02-14 15:10:03 +0100

    Improve DNS SD implementation to trigger OTA requestor init when DNS SD connectivity changes

--- examples/platform/qpg/app/main.cpp
+++ examples/platform/qpg/app/main.cpp
@@ -81,7 +81,7 @@ constexpr int extDiscTimeoutSecs             = 20;
 CHIP_ERROR CHIP_Init(void);
 
 #if CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
-void InitOTARequestorHandler(System::Layer * systemLayer, void * appState)
+void InitOTARequestorHandler(void)
 {
     InitializeOTARequestor();
 }
@@ -130,13 +130,9 @@ void ChipEventHandler(const ChipDeviceEvent * aEvent, intptr_t /* arg */)
 {
     switch (aEvent->Type)
     {
-    case DeviceEventType::kThreadConnectivityChange:
+    case DeviceEventType::kDnssdInitialized:
 #if CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
-        if (aEvent->ThreadConnectivityChange.Result == kConnectivity_Established)
-        {
-            chip::DeviceLayer::SystemLayer().StartTimer(chip::System::Clock::Seconds32(kInitOTARequestorDelaySec),
-                                                        InitOTARequestorHandler, nullptr);
-        }
+        InitOTARequestorHandler();
 #endif
         break;
     default:
