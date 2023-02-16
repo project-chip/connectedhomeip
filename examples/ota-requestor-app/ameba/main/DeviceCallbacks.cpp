4ac3c3ad73e8d77c3d677a8cd1d1b549fcc4ea93
pankore <86098180+pankore@users.noreply.github.com>
2023-02-16 18:08:20 +0800

    restyle

--- examples/ota-requestor-app/ameba/main/DeviceCallbacks.cpp
+++ examples/ota-requestor-app/ameba/main/DeviceCallbacks.cpp
@@ -64,7 +64,6 @@ void InitOTARequestorHandler(System::Layer * systemLayer, void * appState)
 }
 #endif
 
-
 void DeviceCallbacks::DeviceEventCallback(const ChipDeviceEvent * event, intptr_t arg)
 {
     switch (event->Type)
