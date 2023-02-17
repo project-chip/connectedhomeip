48c0b906380402869771244bf3787d68d37a40c4
Vivien Nicolas <vnicolas@apple.com>
2023-02-18 00:10:39 +0100

    Rewrite discover-once to really stop right after the first PASE session failure

--- examples/darwin-framework-tool/commands/pairing/DeviceControllerDelegateBridge.mm
+++ examples/darwin-framework-tool/commands/pairing/DeviceControllerDelegateBridge.mm
@@ -36,7 +36,7 @@
         _commandBridge->SetCommandExitStatus(CHIP_ERROR_INCORRECT_STATE);
         break;
     case MTRCommissioningStatusDiscoveringMoreDevices:
-        ChipLogProgress(chipTool, "Secure Pairing Discovering More Devices");
+        ChipLogError(chipTool, "MTRCommissioningStatusDiscoveringMoreDevices: This should not happen.");
         break;
     case MTRCommissioningStatusUnknown:
         ChipLogError(chipTool, "Uknown Pairing Status");
--- src/controller/DevicePairingDelegate.h
+++ src/controller/DevicePairingDelegate.h
@@ -40,7 +40,6 @@ public:
     {
         SecurePairingSuccess = 0,
         SecurePairingFailed,
-        SecurePairingDiscoveringMoreDevices,
     };
 
     /**
