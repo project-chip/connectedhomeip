ad5dc95440d7f2db136724d630cac5550ef87ea1
swan-amazon <122404367+swan-amazon@users.noreply.github.com>
2023-02-24 07:39:10 -0800

    Added RegulatoryConfig support to AutoCommissioner for Android (#24413)

--- examples/java-matter-controller/java/src/com/matter/controller/Main.java
+++ examples/java-matter-controller/java/src/com/matter/controller/Main.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2022 Project CHIP Authors
+ *   Copyright (c) 2022-2023 Project CHIP Authors
  *   All rights reserved.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
@@ -105,6 +105,7 @@ public class Main {
             ControllerParams.newBuilder()
                 .setUdpListenPort(0)
                 .setControllerVendorId(0xFFF1)
+                .setCountryCode("US")
                 .build());
 
     CredentialsIssuer credentialsIssuer = new CredentialsIssuer();
