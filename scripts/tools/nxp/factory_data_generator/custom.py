5d3f2a77334724d53503ecba4784649a70ce05d8
Damian Michalak-Szmaciński <d.michalak@samsung.com>
2023-02-24 12:22:06 +0100

    [NXP] Add to flake8 in workflow and fix python files

--- scripts/tools/nxp/factory_data_generator/custom.py
+++ scripts/tools/nxp/factory_data_generator/custom.py
@@ -49,7 +49,7 @@ option:
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_der_private_key
-from default import *
+from default import StrArgument, FileArgument, IntArgument, Base64Argument
 
 
 class Verifier(Base64Argument):
