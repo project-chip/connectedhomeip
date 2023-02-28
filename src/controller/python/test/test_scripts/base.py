0e73f29998895cb7577ce0bfb60ed3e98a516c06
Yunhan Wang <yunhanw@google.com>
2023-02-28 14:54:11 -0800

    Fix the lint error

--- src/controller/python/test/test_scripts/base.py
+++ src/controller/python/test/test_scripts/base.py
@@ -37,7 +37,7 @@ import chip.FabricAdmin
 import chip.interaction_model as IM
 import chip.native
 from chip import ChipDeviceCtrl
-from chip.ChipStack import *
+from chip.ChipStack import ChipStack
 from chip.crypto import p256keypair
 from chip.utils import CommissioningBuildingBlocks
 
