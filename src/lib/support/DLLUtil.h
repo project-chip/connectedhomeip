4c154b45bb7435ab711e0c416bf18f4c9fb4a22c
Matt Swartwout <mwswartwout@google.com>
2023-03-08 11:13:34 -0700

    Remove DLL_LOCAL macro
    This macro doesn't appear to be providing any value, and make some
    compilation harder.

--- src/lib/support/DLLUtil.h
+++ src/lib/support/DLLUtil.h
@@ -30,13 +30,10 @@
 
 #if defined _WIN32 || defined __CYGWIN__
 #define DLL_EXPORT __declspec(dllexport)
-#define DLL_LOCAL
 #else
 #if __GNUC__ >= 4
 #define DLL_EXPORT __attribute__((visibility("default")))
-#define DLL_LOCAL __attribute__((visibility("hidden")))
 #else
 #define DLL_EXPORT
-#define DLL_LOCAL
 #endif
 #endif
