1f2f8820073f96c842869f693248dc2acba17dd0
Cliff Chung <116232729+cliffamzn@users.noreply.github.com>
2023-03-08 12:52:46 -0800

    Respect updated DNS SD broadcast names in TV app (#25355)
    * Respect updated DNS SD broadcast names
    
    When a device is advertising over dns-sd and the TX bits change (device
    name) a message is sent out to indicate that the old adverisment should
    be expired and the new one should be used.
    
    In the current implementation of the casting app, we stop listening to
    updates after the `discoveryDurationSeconds` timeout. This causes us to
    miss messages and Android seems to fall back to the cached value of the
    DNS advertisment.
    
    We need to change the usage pattern here so that clients stop the
    discovery manually after it is started and keep it active for the
    lifetime they need it for.
    
    I also updated the test apps to behave a bit nicer with discovered
    commissioners.

--- examples/tv-casting-app/android/App/app/src/main/jni/com/chip/casting/TvCastingApp.java
+++ examples/tv-casting-app/android/App/app/src/main/jni/com/chip/casting/TvCastingApp.java
@@ -32,8 +32,6 @@ import chip.platform.PreferencesConfigurationManager;
 import chip.platform.PreferencesKeyValueStoreManager;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class TvCastingApp {
   private static final String TAG = TvCastingApp.class.getSimpleName();
@@ -44,6 +42,11 @@ public class TvCastingApp {
   private Context applicationContext;
   private ChipAppServer chipAppServer;
   private NsdManagerServiceResolver.NsdManagerResolverAvailState nsdManagerResolverAvailState;
+  private boolean discoveryStarted = false;
+
+  private WifiManager.MulticastLock multicastLock;
+  private NsdManager nsdManager;
+  private NsdDiscoveryListener nsdDiscoveryListener;
 
   public boolean initApp(Context applicationContext, AppParameters appParameters) {
     if (applicationContext == null || appParameters == null) {
@@ -103,21 +106,25 @@ public class TvCastingApp {
   private native boolean initJni(AppParameters appParameters);
 
   public void discoverVideoPlayerCommissioners(
-      long discoveryDurationSeconds,
       SuccessCallback<DiscoveredNodeData> discoverySuccessCallback,
       FailureCallback discoveryFailureCallback) {
     Log.d(TAG, "TvCastingApp.discoverVideoPlayerCommissioners called");
+
+    if (this.discoveryStarted) {
+      Log.d(TAG, "Discovery already started, stopping before starting again");
+      stopVideoPlayerDiscovery();
+    }
 
     List<VideoPlayer> preCommissionedVideoPlayers = readCachedVideoPlayers();
 
     WifiManager wifiManager =
         (WifiManager) applicationContext.getSystemService(Context.WIFI_SERVICE);
-    WifiManager.MulticastLock multicastLock = wifiManager.createMulticastLock("multicastLock");
+    multicastLock = wifiManager.createMulticastLock("multicastLock");
     multicastLock.setReferenceCounted(true);
     multicastLock.acquire();
 
-    NsdManager nsdManager = (NsdManager) applicationContext.getSystemService(Context.NSD_SERVICE);
-    NsdDiscoveryListener nsdDiscoveryListener =
+    nsdManager = (NsdManager) applicationContext.getSystemService(Context.NSD_SERVICE);
+    nsdDiscoveryListener =
         new NsdDiscoveryListener(
             nsdManager,
             DISCOVERY_TARGET_SERVICE_TYPE,
@@ -129,22 +136,23 @@ public class TvCastingApp {
 
     nsdManager.discoverServices(
         DISCOVERY_TARGET_SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, nsdDiscoveryListener);
+    Log.d(TAG, "TvCastingApp.discoverVideoPlayerCommissioners started");
+    this.discoveryStarted = true;
+  }
 
-    Executors.newSingleThreadScheduledExecutor()
-        .schedule(
-            new Runnable() {
-              @Override
-              public void run() {
-                Log.d(TAG, "TvCastingApp stopping Video Player commissioner discovery");
-                nsdManager.stopServiceDiscovery(nsdDiscoveryListener);
-                if (multicastLock.isHeld()) {
-                  multicastLock.release();
-                }
-              }
-            },
-            discoveryDurationSeconds,
-            TimeUnit.SECONDS);
-    Log.d(TAG, "TvCastingApp.discoverVideoPlayerCommissioners ended");
+  public void stopVideoPlayerDiscovery() {
+    Log.d(TAG, "TvCastingApp trying to stop video player discovery");
+    if (this.discoveryStarted
+        && nsdManager != null
+        && multicastLock != null
+        && nsdDiscoveryListener != null) {
+      Log.d(TAG, "TvCastingApp stopping Video Player commissioner discovery");
+      nsdManager.stopServiceDiscovery(nsdDiscoveryListener);
+      if (multicastLock.isHeld()) {
+        multicastLock.release();
+      }
+      this.discoveryStarted = false;
+    }
   }
 
   public native boolean openBasicCommissioningWindow(
