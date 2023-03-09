3691c70526e0fc6b5f3900d3d0245d7d824cdb66
WanqQixiang <wangqixiang@espressif.com>
2023-03-09 19:46:16 +0800

    ESP32: fix compile errors related to screen framwork

--- examples/all-clusters-app/esp32/main/include/QRCodeScreen.h
+++ examples/all-clusters-app/esp32/main/include/QRCodeScreen.h
@@ -25,10 +25,11 @@
 
 #pragma once
 
+#include "sdkconfig.h"
+#if CONFIG_HAVE_DISPLAY
 #include "Screen.h"
 #include "ScreenManager.h"
 
-#if CONFIG_HAVE_DISPLAY
 
 #include <cstdint>
 #include <vector>
--- examples/lighting-app/esp32/main/include/StatusScreen.h
+++ examples/lighting-app/esp32/main/include/StatusScreen.h
@@ -18,10 +18,11 @@
 
 #pragma once
 
-#include "ListScreen.h"
-#include "ScreenManager.h"
+#include "sdkconfig.h"
 
 #if CONFIG_HAVE_DISPLAY
+#include "ListScreen.h"
+#include "ScreenManager.h"
 
 #include <cstdint>
 #include <vector>
