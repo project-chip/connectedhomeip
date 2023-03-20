440968be05fafd15a74c78b2f1fc5fdd1862c2eb
pankore <86098180+pankore@users.noreply.github.com>
2023-03-21 05:05:23 +0800

    [Ameba] Location Capability (#25647)
    * [Config] add location capability
    
    * restyle
    
    * Update src/platform/Ameba/ConfigurationManagerImpl.cpp
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    * Update src/platform/Ameba/ConfigurationManagerImpl.cpp
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    * restyle
    
    ---------
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/platform/Ameba/ConfigurationManagerImpl.cpp
+++ src/platform/Ameba/ConfigurationManagerImpl.cpp
@@ -79,6 +79,20 @@ CHIP_ERROR ConfigurationManagerImpl::Init()
         SuccessOrExit(err);
     }
 
+    if (!AmebaConfig::ConfigValueExists(AmebaConfig::kConfigKey_RegulatoryLocation))
+    {
+        uint32_t regulatoryLocation = to_underlying(app::Clusters::GeneralCommissioning::RegulatoryLocationType::kIndoor);
+        err                         = WriteConfigValue(AmebaConfig::kConfigKey_RegulatoryLocation, regulatoryLocation);
+        SuccessOrExit(err);
+    }
+
+    if (!AmebaConfig::ConfigValueExists(AmebaConfig::kConfigKey_LocationCapability))
+    {
+        uint32_t locationCapability = to_underlying(app::Clusters::GeneralCommissioning::RegulatoryLocationType::kIndoorOutdoor);
+        err                         = WriteConfigValue(AmebaConfig::kConfigKey_LocationCapability, locationCapability);
+        SuccessOrExit(err);
+    }
+
     // Initialize the generic implementation base class.
     err = Internal::GenericConfigurationManagerImpl<AmebaConfig>::Init();
     SuccessOrExit(err);
@@ -117,6 +131,21 @@ CHIP_ERROR ConfigurationManagerImpl::GetBootReason(uint32_t & bootReason)
 CHIP_ERROR ConfigurationManagerImpl::StoreBootReason(uint32_t bootReason)
 {
     return WriteConfigValue(AmebaConfig::kCounterKey_BootReason, bootReason);
+}
+
+CHIP_ERROR ConfigurationManagerImpl::GetLocationCapability(uint8_t & location)
+{
+    uint32_t value = 0;
+
+    CHIP_ERROR err = ReadConfigValue(AmebaConfig::kConfigKey_LocationCapability, value);
+
+    if (err == CHIP_NO_ERROR)
+    {
+        VerifyOrReturnError(value <= UINT8_MAX, CHIP_ERROR_INVALID_INTEGER_VALUE);
+        location = static_cast<uint8_t>(value);
+    }
+
+    return err;
 }
 
 CHIP_ERROR ConfigurationManagerImpl::GetPrimaryWiFiMACAddress(uint8_t * buf)
