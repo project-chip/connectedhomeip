454638569b12d794c1af7c47b1287a6a201d91d0
Andrei Litvin <andy314@gmail.com>
2023-03-20 14:48:02 -0400

    Most codegen looks ok. Java boxing logic is suspect still

--- scripts/py_matter_idl/matter_idl/generators/filters.py
+++ scripts/py_matter_idl/matter_idl/generators/filters.py
@@ -24,12 +24,14 @@ def normalize_acronyms(s: str) -> str:
     return s.replace('WiFi', 'Wifi').replace('WI_FI', 'WIFI')
 
 
-def lower_camel_case(s: str) -> str:
-    """Like camelcase, except it ensures the first letter is actually
-       lower case.
-    """
-    s = stringcase.camelcase(s)
+def lowfirst(s: str) -> str:
+    """Make the first letter lowercase. """
     return s[0].lower() + s[1:]
+
+
+def upfirst(s: str) -> str:
+    """Make the first letter uppercase """
+    return s[0].upper() + s[1:]
 
 
 def RegisterCommonFilters(filtermap):
@@ -50,4 +52,5 @@ def RegisterCommonFilters(filtermap):
     filtermap['spinalcase'] = stringcase.spinalcase
 
     filtermap['normalize_acronyms'] = normalize_acronyms
-    filtermap['lowercamelcase'] = lower_camel_case
+    filtermap['lowfirst'] = lowfirst
+    filtermap['upfirst'] = upfirst
