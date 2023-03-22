b388c3669ca7fb336591102926e8b3f298b3275a
Boris Zbarsky <bzbarsky@apple.com>
2023-03-22 16:10:54 -0400

    Fix failsafe extension when we have a device attestation delegate. (#25787)
    The boolean test in ExtendArmFailSafeForDeviceAttestation was backwards, so
    there were two failure cases:
    
    1. If we had a delegate but FailSafeExpiryTimeoutSecs() returned NullOptional,
    we would skip extending the fail-safe (expected), but set failSafeSkipped to
    _false_, and so not actually make our "continue commissioning" callback.
    
    2. If we had a delegate and FailSafeExpiryTimeoutSecs() returned a too-small
    value, we would also skip resetting the fail-safe, but end up setting
    failSafeSkipped to false, and hence again failing to make our "continue
    commissioning" callback.
    
    The fix is to reverse the sense of the "do we need to call back immediately"
    boolean, to make it a little clearer what the logic flow is here, and then check
    it appropriately.

---
+++ src/darwin/Framework/CHIPTests/MTRPairingTests.m
@@ -0,0 +1,258 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+// module headers
+#import <Matter/Matter.h>
+
+#import "MTRErrorTestUtils.h"
+#import "MTRTestKeys.h"
+#import "MTRTestResetCommissioneeHelper.h"
+#import "MTRTestStorage.h"
+
+// system dependencies
+#import <XCTest/XCTest.h>
+
+// Set the following to 1 in order to run individual test case manually.
+#define MANUAL_INDIVIDUAL_TEST 0
+
+static const uint16_t kPairingTimeoutInSeconds = 10;
+static const uint16_t kTimeoutInSeconds = 3;
+static uint64_t sDeviceId = 0x12344321;
+static NSString * kOnboardingPayload = @"MT:-24J0AFN00KA0648G00";
+static const uint16_t kLocalPort = 5541;
+static const uint16_t kTestVendorId = 0xFFF1u;
+
+// Singleton controller we use.
+static MTRDeviceController * sController = nil;
+
+// Keys we can use to restart the controller.
+static MTRTestKeys * sTestKeys = nil;
+
+// A no-op MTRDeviceAttestationDelegate which lets us test (by default, in CI)
+// commissioning flows that have such a delegate.
+@interface NoOpAttestationDelegate : NSObject <MTRDeviceAttestationDelegate>
+@property (nonatomic) XCTestExpectation * expectation;
+
+- (instancetype)initWithExpectation:(XCTestExpectation *)expectation;
+@end
+
+@implementation NoOpAttestationDelegate
+- (instancetype)initWithExpectation:(XCTestExpectation *)expectation
+{
+    if (!(self = [super init])) {
+        return nil;
+    }
+
+    _expectation = expectation;
+    return self;
+}
+
+- (void)deviceAttestationCompletedForController:(MTRDeviceController *)controller
+                             opaqueDeviceHandle:(void *)opaqueDeviceHandle
+                          attestationDeviceInfo:(MTRDeviceAttestationDeviceInfo *)attestationDeviceInfo
+                                          error:(NSError * _Nullable)error
+{
+    [self.expectation fulfill];
+    [controller continueCommissioningDevice:opaqueDeviceHandle ignoreAttestationFailure:NO error:nil];
+}
+
+@end
+
+@interface MTRPairingTestPairingDelegate : NSObject <MTRDevicePairingDelegate>
+@property (nonatomic, strong) XCTestExpectation * expectation;
+@property (nonatomic, nullable) id<MTRDeviceAttestationDelegate> attestationDelegate;
+@property (nonatomic, nullable) NSNumber * failSafeExtension;
+@end
+
+@implementation MTRPairingTestPairingDelegate
+- (id)initWithExpectation:(XCTestExpectation *)expectation
+      attestationDelegate:(id<MTRDeviceAttestationDelegate>)attestationDelegate
+        failSafeExtension:(NSNumber *)failSafeExtension
+{
+    self = [super init];
+    if (self) {
+        _expectation = expectation;
+        _attestationDelegate = attestationDelegate;
+        _failSafeExtension = failSafeExtension;
+    }
+    return self;
+}
+
+- (void)onPairingComplete:(NSError *)error
+{
+    XCTAssertEqual(error.code, 0);
+
+    __auto_type * params = [[MTRCommissioningParameters alloc] init];
+    params.deviceAttestationDelegate = self.attestationDelegate;
+    params.failSafeTimeout = self.failSafeExtension;
+
+    NSError * commissionError = nil;
+    [sController commissionDevice:sDeviceId commissioningParams:params error:&commissionError];
+    XCTAssertNil(commissionError);
+
+    // Keep waiting for onCommissioningComplete
+}
+
+- (void)onCommissioningComplete:(NSError *)error
+{
+    XCTAssertEqual(error.code, 0);
+    [_expectation fulfill];
+    _expectation = nil;
+}
+
+@end
+
+// attestationDelegate and failSafeExtension can both be nil
+static void DoPairingTest(XCTestCase * testcase, id<MTRDeviceAttestationDelegate> attestationDelegate, NSNumber * failSafeExtension)
+{
+    // Don't reuse node ids, because that will confuse us.
+    ++sDeviceId;
+    XCTestExpectation * expectation = [testcase expectationWithDescription:@"Pairing Complete"];
+    __auto_type * controller = sController;
+
+    __auto_type * pairing = [[MTRPairingTestPairingDelegate alloc] initWithExpectation:expectation
+                                                                   attestationDelegate:attestationDelegate
+                                                                     failSafeExtension:failSafeExtension];
+    dispatch_queue_t callbackQueue = dispatch_queue_create("com.chip.pairing", DISPATCH_QUEUE_SERIAL);
+
+    [controller setPairingDelegate:pairing queue:callbackQueue];
+
+    NSError * error;
+    __auto_type * payload = [MTRSetupPayload setupPayloadWithOnboardingPayload:kOnboardingPayload error:&error];
+    XCTAssertNotNil(payload);
+    XCTAssertNil(error);
+
+    [controller setupCommissioningSessionWithPayload:payload newNodeID:@(sDeviceId) error:&error];
+    XCTAssertNil(error);
+
+    [testcase waitForExpectations:@[ expectation ] timeout:kPairingTimeoutInSeconds];
+
+    ResetCommissionee([MTRBaseDevice deviceWithNodeID:@(sDeviceId) controller:controller], dispatch_get_main_queue(), testcase,
+        kTimeoutInSeconds);
+}
+
+@interface MTRPairingTests : XCTestCase
+@end
+
+@implementation MTRPairingTests
+
+- (void)setUp
+{
+    [super setUp];
+    [self setContinueAfterFailure:NO];
+}
+
+- (void)tearDown
+{
+#if MANUAL_INDIVIDUAL_TEST
+    [self shutdownStack];
+#endif
+    [super tearDown];
+}
+
+- (void)initStack
+{
+    __auto_type * factory = [MTRControllerFactory sharedInstance];
+    XCTAssertNotNil(factory);
+
+    __auto_type * storage = [[MTRTestStorage alloc] init];
+    __auto_type * factoryParams = [[MTRControllerFactoryParams alloc] initWithStorage:storage];
+    factoryParams.port = @(kLocalPort);
+
+    BOOL ok = [factory startup:factoryParams];
+    XCTAssertTrue(ok);
+
+    __auto_type * testKeys = [[MTRTestKeys alloc] init];
+    XCTAssertNotNil(testKeys);
+
+    sTestKeys = testKeys;
+
+    // Needs to match what startControllerOnExistingFabric calls elsewhere in
+    // this file do.
+    __auto_type * params = [[MTRDeviceControllerStartupParams alloc] initWithSigningKeypair:testKeys fabricId:1 ipk:testKeys.ipk];
+    params.vendorId = @(kTestVendorId);
+
+    MTRDeviceController * controller = [factory startControllerOnNewFabric:params];
+    XCTAssertNotNil(controller);
+
+    sController = controller;
+}
+
+- (void)shutdownStack
+{
+    MTRDeviceController * controller = sController;
+    XCTAssertNotNil(controller);
+
+    [controller shutdown];
+    XCTAssertFalse([controller isRunning]);
+
+    [[MTRControllerFactory sharedInstance] shutdown];
+}
+
+#if !MANUAL_INDIVIDUAL_TEST
+- (void)test000_SetUp
+{
+    [self initStack];
+}
+#endif
+
+- (void)test001_PairWithoutAttestationDelegate
+{
+    DoPairingTest(self, nil, nil);
+}
+
+- (void)test002_PairWithAttestationDelegateNoFailsafeExtension
+{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"Attestation delegate called"];
+
+    DoPairingTest(self, [[NoOpAttestationDelegate alloc] initWithExpectation:expectation], nil);
+
+    [self waitForExpectations:@[ expectation ] timeout:kTimeoutInSeconds];
+}
+
+- (void)test003_PairWithAttestationDelegateFailsafeExtensionShort
+{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"Attestation delegate called"];
+
+    // Extend by a time that is going to be smaller than the 60s default we
+    // already have set via CHIP_DEVICE_CONFIG_FAILSAFE_EXPIRY_LENGTH_SEC on the
+    // server side, minus whatever time that has likely passed.
+    DoPairingTest(self, [[NoOpAttestationDelegate alloc] initWithExpectation:expectation], @(30));
+
+    [self waitForExpectations:@[ expectation ] timeout:kTimeoutInSeconds];
+}
+
+- (void)test004_PairWithAttestationDelegateFailsafeExtensionLong
+{
+    XCTestExpectation * expectation = [self expectationWithDescription:@"Attestation delegate called"];
+
+    // Extend by a time that is going to be larger than the 60s default we
+    // already have set via CHIP_DEVICE_CONFIG_FAILSAFE_EXPIRY_LENGTH_SEC on the
+    // server side.
+    DoPairingTest(self, [[NoOpAttestationDelegate alloc] initWithExpectation:expectation], @(90));
+
+    [self waitForExpectations:@[ expectation ] timeout:kTimeoutInSeconds];
+}
+
+#if !MANUAL_INDIVIDUAL_TEST
+- (void)test999_TearDown
+{
+    [self shutdownStack];
+}
+#endif
+
+@end
