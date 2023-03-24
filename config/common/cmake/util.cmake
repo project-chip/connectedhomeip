e3cda8bfdcdf0d93e5839f2cc8ba2882f396009d
Artur Tynecki <77382963+ATmobica@users.noreply.github.com>
2023-03-24 20:02:11 +0100

    [cmake] Common Cmake sources for Matter build (#25654)
    * [lwip] Add skip LwIP init flag
    
    Add CHIP_SYSTEM_CONFIG_LWIP_SKIP_INIT define to skip
    LwIP initalization during network setup.
    The platform can provide its own LwIP initialization.
    This option allows skipping initialization steps in network setup
    e.g. in unit tests.
    Apply skipping LwIP initalization to Open IoT SDK platform.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [Fix] Fix unused functions call in unit-tests
    
    Add necessary defines in unit-tests code to fix unused-function
    issues.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [OIS] Create common cmake source in platform config
    
    Add Cmake files that create GN args file and build
    the CHIP library with ExternalProject.
    At the end the 'chip' is defined. It exposes CHIP headers
    and libraries to the application.
    Add CMake utilities for managing and retrieving build configuration.
    Add Python script to generate the args.gn file in a correct format.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [OIS] Adapt Open IoT SDK platform to common cmake sources
    
    Change OIS cmake files to  the common sources.
    Remove redundant files.
    Improve paths to Matter files.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [Mbed] Adapt Mbed platform to common cmake sources
    
    Change mbed cmake files to the common sources.
    Remove redundant files.
    Improve mbed GN project.
    Improve paths to Matter files.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [nrfconnect] Adapt nrfconnect platform to common cmake sources
    
    Change nrfconnect cmake files to the common sources.
    Remove redundant files.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [Zephyr] Adapt Zephyr platform to common cmake sources
    
    Change Zephyr cmake files to the common sources.
    Remove redundant files.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    * [Telink] Adapt Telink platform to common cmake sources
    
    Change Telink cmake files to the common sources.
    Remove redundant files.
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>
    
    ---------
    
    Signed-off-by: ATmobica <artur.tynecki@arm.com>

--- config/common/cmake/util.cmake
+++ config/common/cmake/util.cmake
@@ -1,5 +1,5 @@
 #
-#   Copyright (c) 2020 Project CHIP Authors
+#   Copyright (c) 2023 Project CHIP Authors
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,8 +16,12 @@
 
 #
 #   @file
-#     CMake utilities for managing and retrieving mbed build configuration
+#     CMake utilities for managing and retrieving build configuration
 #
+
+# ==============================================================================
+# Functions
+# ==============================================================================
 
 # Get compilation flags for specific lang
 # The flags might contains compile language generator expressions that
@@ -55,20 +59,20 @@ function(get_flags_for_lang lang input output)
   set(${output} ${tmp_list} PARENT_SCOPE)
 endfunction()
 
+
 # Get include directory of target build
 # Get target property of includes directories
 # For each flag add -I prefix and put it in quotation marks
 # [Args]:
 #   target - target name
 #   output - output variable name
-function(mbed_get_include_directories target output)
+# Note: Includes are returned as a generator expression which must be evalued 
+# at build time. 
+function(get_include_directories target output)
   get_property(flags TARGET ${target} PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
-  list(APPEND CFLAG_LIST)
-  foreach(flag ${flags})
-    list(APPEND CFLAG_LIST "\"-isystem${flag}\"")
-  endforeach()
-  set(${output} ${CFLAG_LIST} PARENT_SCOPE)
+  set(${output} "-isystem$<JOIN:$<TARGET_PROPERTY:${target},INTERFACE_INCLUDE_DIRECTORIES>, -isystem>" PARENT_SCOPE)
 endfunction()
+
 
 # Get compile definitions of target build
 # Get target property of compile definitions
@@ -76,25 +80,21 @@ endfunction()
 # [Args]:
 #   target - target name
 #   output - output variable name
-function(mbed_get_compile_definitions target output)
+# Note: Definitions are returned as a generator expression which must be evalued 
+# at build time. 
+function(get_compile_definitions target output)
   get_property(flags TARGET ${target} PROPERTY INTERFACE_COMPILE_DEFINITIONS)
-
-  list(APPEND CFLAG_LIST)
-  foreach(flag ${flags})
-    # Replace each quote with a '\"' - format required for the GN arguments
-    string(REPLACE "\""  "\\\\\""  output_flag ${flag})
-    list(APPEND CFLAG_LIST "\"-D${output_flag}\"")
-  endforeach()
-  set(${output} ${CFLAG_LIST} PARENT_SCOPE)
+  set(${output} "-D$<JOIN:$<TARGET_PROPERTY:${target},INTERFACE_COMPILE_DEFINITIONS>, -D>" PARENT_SCOPE)
 endfunction()
 
-# Get compile options of mbed build for specific language
-# Get mbed-core property of compile options
+
+# Get compile options of build for specific language
+# Get property of compile options
 # [Args]:
 #   lang - compilation languge (C, C++ or ASM)
 #   target - target name
 #   output - output variable name
-function(mbed_get_compile_options_for_lang lang target output)
+function(get_compile_options_for_lang lang target output)
   get_property(flags TARGET ${target} PROPERTY INTERFACE_COMPILE_OPTIONS)
   get_flags_for_lang(${lang} flags output_list)
   set(${output} ${output_list} PARENT_SCOPE)
@@ -105,32 +105,33 @@ endfunction()
 # [Args]:
 #   VAR - flags variable name
 #   TARGET - target name
-function(mbed_get_target_common_compile_flags VAR TARGET)
-  mbed_get_include_directories(${TARGET} INCLUDES)
-  mbed_get_compile_definitions(${TARGET} DEFINES)
+function(get_target_common_compile_flags VAR TARGET)
+  get_include_directories(${TARGET} INCLUDES)
+  get_compile_definitions(${TARGET} DEFINES)
   set(${VAR} ${INCLUDES} ${DEFINES} ${${VAR}} PARENT_SCOPE)
 endfunction()
+
 
 # Retrieve target compiler flags for the specific language (C or CXX)
 # [Args]:
 #   VAR - flags variable name
 #   TARGET - target name
 #   LANG - compilation languge (C, C++ or ASM) 
-function(mbed_get_lang_compile_flags VAR TARGET LANG)
-  mbed_get_compile_options_for_lang(${LANG} ${TARGET} FLAGS)
+function(get_lang_compile_flags VAR TARGET LANG)
+  get_compile_options_for_lang(${LANG} ${TARGET} FLAGS)
   set(${VAR} ${FLAGS} ${${VAR}} PARENT_SCOPE)
 endfunction()
+
 
 # Convert list of flags to string format
 # [Args]:
 #   ptr_list_of_flags - list fo flags
 #   string_of_flags - output string
-#   separator - flags separator
-function(convert_list_of_flags_to_string_of_flags ptr_list_of_flags string_of_flags separator)
+function(convert_list_of_flags_to_string_of_flags ptr_list_of_flags string_of_flags)
   # Convert the list to a string so we can do string replace
   # operations on it and replace the ";" list separators with a
-  # desirable one so the flags are spaced out
-  string(REPLACE ";"  ${separator} locally_scoped_string_of_flags "${${ptr_list_of_flags}}")
+  # whitespace so the flags are spaced out
+  string(REPLACE ";"  " " locally_scoped_string_of_flags "${${ptr_list_of_flags}}")
   # Removing excess spaces
   string(REPLACE "  "  " "  locally_scoped_string_of_flags "${locally_scoped_string_of_flags}")
 
