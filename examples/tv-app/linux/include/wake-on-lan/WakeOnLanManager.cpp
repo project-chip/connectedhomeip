ec5e6d82f10cab68c1669ea011d985a79958e941
chrisdecenzo <decenzo77@yahoo.com>
2023-03-29 13:03:16 -0700

    Address WoL issue 25793

--- examples/tv-app/linux/include/wake-on-lan/WakeOnLanManager.cpp
+++ examples/tv-app/linux/include/wake-on-lan/WakeOnLanManager.cpp
@@ -19,6 +19,7 @@
 #include "WakeOnLanManager.h"
 #include <fstream>
 #include <iostream>
+#include <platform/ConfigurationManager.h>
 #include <string>
 
 using namespace chip;
@@ -26,17 +27,33 @@ using namespace chip::app::Clusters::WakeOnLan;
 
 std::string getMacAddress()
 {
-    std::ifstream input("/sys/class/net/eth0/address");
-    std::string line;
-    std::getline(input, line);
-    return line;
+    uint8_t macBuffer[chip::DeviceLayer::ConfigurationManager::kPrimaryMACAddressLength];
+    MutableByteSpan mac(macBuffer);
+    if (chip::DeviceLayer::ConfigurationMgr().GetPrimaryMACAddress(mac) != CHIP_NO_ERROR)
+    {
+        ChipLogProgress(Zcl, "WakeOnLanManager::getMacAddress no primary MAC configured by DeviceLayer");
+        return "0000000000";
+    }
+
+    char macStr[chip::DeviceLayer::ConfigurationManager::kPrimaryMACAddressLength * 2 + 1] = { 0 }; // added null char
+
+    uint8_t j = 0;
+    for (uint8_t i = 0; i < sizeof(macBuffer); i++)
+    {
+        j         = i * 2;
+        macStr[j] = (((macBuffer[i] & 0xF0) >> 4) & 0xF);
+        macStr[j] += (macStr[j] <= 9) ? '0' : ('A' - 10);
+        j++;
+        macStr[j] = (macBuffer[i] & 0x0F);
+        macStr[j] += (macStr[j] <= 9) ? '0' : ('A' - 10);
+    }
+
+    return std::string(macStr);
 }
 
 CHIP_ERROR WakeOnLanManager::HandleGetMacAddress(chip::app::AttributeValueEncoder & aEncoder)
 {
-#if CHIP_ENABLE_WAKE_ON_LAN
+    ChipLogProgress(Zcl, "WakeOnLanManager::HandleGetMacAddress");
+
     return aEncoder.Encode(CharSpan::fromCharString(getMacAddress().c_str()));
-#else
-    return aEncoder.Encode(CharSpan::fromCharString("00:00:00:00:00"));
-#endif // CHIP_ENABLE_WAKE_ON_LAN
 }
