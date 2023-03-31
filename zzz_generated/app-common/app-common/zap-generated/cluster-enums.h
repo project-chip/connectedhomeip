70ddc7ccba4989e36e26ee5664a68560e1d7165e
Restyled.io <commits@restyled.io>
2023-03-31 15:41:30 +0000

    Restyled by clang-format

--- zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
+++ zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
@@ -180,7 +180,8 @@ enum class SceneFeatures : uint32_t
 };
 } // namespace OnOff
 
-namespace OnOffSwitchConfiguration {} // namespace OnOffSwitchConfiguration
+namespace OnOffSwitchConfiguration {
+} // namespace OnOffSwitchConfiguration
 
 namespace LevelControl {
 
@@ -230,13 +231,17 @@ enum class LevelControlOptions : uint8_t
 };
 } // namespace LevelControl
 
-namespace BinaryInputBasic {} // namespace BinaryInputBasic
+namespace BinaryInputBasic {
+} // namespace BinaryInputBasic
 
-namespace PulseWidthModulation {} // namespace PulseWidthModulation
+namespace PulseWidthModulation {
+} // namespace PulseWidthModulation
 
-namespace Descriptor {} // namespace Descriptor
+namespace Descriptor {
+} // namespace Descriptor
 
-namespace Binding {} // namespace Binding
+namespace Binding {
+} // namespace Binding
 
 namespace AccessControl {
 
@@ -330,7 +335,8 @@ enum class CommandBits : uint16_t
 };
 } // namespace Actions
 
-namespace BasicInformation {} // namespace BasicInformation
+namespace BasicInformation {
+} // namespace BasicInformation
 
 namespace OtaSoftwareUpdateProvider {
 
@@ -402,7 +408,8 @@ enum class OTAUpdateStateEnum : uint8_t
 };
 } // namespace OtaSoftwareUpdateRequestor
 
-namespace LocalizationConfiguration {} // namespace LocalizationConfiguration
+namespace LocalizationConfiguration {
+} // namespace LocalizationConfiguration
 
 namespace TimeFormatLocalization {
 
@@ -451,7 +458,8 @@ enum class UnitLocalizationFeature : uint32_t
 };
 } // namespace UnitLocalization
 
-namespace PowerSourceConfiguration {} // namespace PowerSourceConfiguration
+namespace PowerSourceConfiguration {
+} // namespace PowerSourceConfiguration
 
 namespace PowerSource {
 
@@ -928,7 +936,8 @@ enum class TimeSourceEnum : uint8_t
 };
 } // namespace TimeSynchronization
 
-namespace BridgedDeviceBasic {} // namespace BridgedDeviceBasic
+namespace BridgedDeviceBasic {
+} // namespace BridgedDeviceBasic
 
 namespace Switch {
 
@@ -1001,17 +1010,23 @@ enum class GroupKeySecurityPolicy : uint8_t
 };
 } // namespace GroupKeyManagement
 
-namespace FixedLabel {} // namespace FixedLabel
+namespace FixedLabel {
+} // namespace FixedLabel
 
-namespace UserLabel {} // namespace UserLabel
+namespace UserLabel {
+} // namespace UserLabel
 
-namespace ProxyConfiguration {} // namespace ProxyConfiguration
+namespace ProxyConfiguration {
+} // namespace ProxyConfiguration
 
-namespace ProxyDiscovery {} // namespace ProxyDiscovery
+namespace ProxyDiscovery {
+} // namespace ProxyDiscovery
 
-namespace ProxyValid {} // namespace ProxyValid
+namespace ProxyValid {
+} // namespace ProxyValid
 
-namespace BooleanState {} // namespace BooleanState
+namespace BooleanState {
+} // namespace BooleanState
 
 namespace ModeSelect {
 
@@ -1551,7 +1566,8 @@ enum class SafetyStatus : uint16_t
 };
 } // namespace WindowCovering
 
-namespace BarrierControl {} // namespace BarrierControl
+namespace BarrierControl {
+} // namespace BarrierControl
 
 namespace PumpConfigurationAndControl {
 
@@ -1735,7 +1751,8 @@ enum class WindSupportMask : uint8_t
 };
 } // namespace FanControl
 
-namespace ThermostatUserInterfaceConfiguration {} // namespace ThermostatUserInterfaceConfiguration
+namespace ThermostatUserInterfaceConfiguration {
+} // namespace ThermostatUserInterfaceConfiguration
 
 namespace ColorControl {
 
@@ -1895,7 +1912,8 @@ enum class ColorLoopUpdateFlags : uint8_t
 };
 } // namespace ColorControl
 
-namespace BallastConfiguration {} // namespace BallastConfiguration
+namespace BallastConfiguration {
+} // namespace BallastConfiguration
 
 namespace IlluminanceMeasurement {
 
@@ -1908,7 +1926,8 @@ enum class LightSensorType : uint8_t
 };
 } // namespace IlluminanceMeasurement
 
-namespace TemperatureMeasurement {} // namespace TemperatureMeasurement
+namespace TemperatureMeasurement {
+} // namespace TemperatureMeasurement
 
 namespace PressureMeasurement {
 
@@ -1919,13 +1938,17 @@ enum class PressureFeature : uint32_t
 };
 } // namespace PressureMeasurement
 
-namespace FlowMeasurement {} // namespace FlowMeasurement
+namespace FlowMeasurement {
+} // namespace FlowMeasurement
 
-namespace RelativeHumidityMeasurement {} // namespace RelativeHumidityMeasurement
+namespace RelativeHumidityMeasurement {
+} // namespace RelativeHumidityMeasurement
 
-namespace OccupancySensing {} // namespace OccupancySensing
+namespace OccupancySensing {
+} // namespace OccupancySensing
 
-namespace WakeOnLan {} // namespace WakeOnLan
+namespace WakeOnLan {
+} // namespace WakeOnLan
 
 namespace Channel {
 
@@ -2024,7 +2047,8 @@ enum class MediaInputFeature : uint32_t
 };
 } // namespace MediaInput
 
-namespace LowPower {} // namespace LowPower
+namespace LowPower {
+} // namespace LowPower
 
 namespace KeypadInput {
 
@@ -2243,11 +2267,14 @@ enum class ApplicationStatusEnum : uint8_t
 };
 } // namespace ApplicationBasic
 
-namespace AccountLogin {} // namespace AccountLogin
+namespace AccountLogin {
+} // namespace AccountLogin
 
-namespace ElectricalMeasurement {} // namespace ElectricalMeasurement
+namespace ElectricalMeasurement {
+} // namespace ElectricalMeasurement
 
-namespace ClientMonitoring {} // namespace ClientMonitoring
+namespace ClientMonitoring {
+} // namespace ClientMonitoring
 
 namespace UnitTesting {
 
