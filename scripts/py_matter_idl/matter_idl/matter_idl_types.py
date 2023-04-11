a5894d27161cdb43b866fc66b047fcce2fe31da7
Andrei Litvin <andreilitvin@google.com>
2023-04-11 18:02:04 -0400

    Support doc comment parsing in matter files

--- scripts/py_matter_idl/matter_idl/matter_idl_types.py
+++ scripts/py_matter_idl/matter_idl/matter_idl_types.py
@@ -9,16 +9,19 @@ from lark.tree import Meta
 # Helpful when referencing data items in logs when processing
 @dataclass
 class ParseMetaData:
-    line: int
-    column: int
+    line: Optional[int]
+    column: Optional[int]
+    start_pos: Optional[int]
 
-    def __init__(self, meta: Meta = None, line: int = None, column: int = None):
+    def __init__(self, meta: Meta = None, line: int = None, column: int = None, start_pos: int = None):
         if meta:
             self.line = meta.line
             self.column = meta.column
+            self.start_pos = meta.start_pos
         else:
             self.line = line
             self.column = column
+            self.start_pos = start_pos
 
 
 class StructQuality(enum.Flag):
@@ -193,6 +196,9 @@ class Command:
     qualities: CommandQuality = CommandQuality.NONE
     invokeacl: AccessPrivilege = AccessPrivilege.OPERATE
     description: Optional[str] = None
+
+    # Parsing meta data missing only when skip meta data is requested
+    parse_meta: Optional[ParseMetaData] = field(default=None)
 
     @property
     def is_timed_invoke(self):
