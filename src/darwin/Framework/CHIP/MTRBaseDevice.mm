debd9aa9f5338500e77f5cd0b5ed07817657a94b
joonhaengHeo <85541460+joonhaengHeo@users.noreply.github.com>
2023-04-13 08:26:08 +0900

    Update src/darwin/Framework/CHIP/MTRBaseDevice.mm
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/darwin/Framework/CHIP/MTRBaseDevice.mm
+++ src/darwin/Framework/CHIP/MTRBaseDevice.mm
@@ -151,6 +151,7 @@ static void PurgeReadClientContainers(
                     container.pathParams = nullptr;
                 }
                 if (container.eventPathParams) {
+                    static_assert(std::is_trivially_destructible<EventPathParams>::value, "EventPathParams destructors won't get run");
                     Platform::MemoryFree(container.eventPathParams);
                     container.eventPathParams = nullptr;
                 }
