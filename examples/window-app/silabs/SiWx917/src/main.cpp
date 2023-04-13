e1f306f84d815d3b49b10292c48880fa81662cb7
Jean-Francois Penven <67962328+jepenven-silabs@users.noreply.github.com>
2023-04-13 15:41:21 -0400

    Rename credentials define (#26089)

--- examples/window-app/silabs/SiWx917/src/main.cpp
+++ examples/window-app/silabs/SiWx917/src/main.cpp
@@ -25,7 +25,7 @@
 #include <app/server/Server.h>
 #include <credentials/DeviceAttestationCredsProvider.h>
 #include <matter_config.h>
-#ifdef SI917_ATTESTATION_CREDENTIALS
+#ifdef SILABS_ATTESTATION_CREDENTIALS
 #include <examples/platform/silabs/SilabsDeviceAttestationCreds.h>
 #else
 #include <credentials/examples/DeviceAttestationCredsExample.h>
@@ -60,7 +60,7 @@ int main(void)
     chip::DeviceLayer::PlatformMgr().LockChipStack();
     err = app.Init();
     // Initialize device attestation config
-#ifdef SI917_ATTESTATION_CREDENTIALS
+#ifdef SILABS_ATTESTATION_CREDENTIALS
     SetDeviceAttestationCredentialsProvider(SILABS::GetSILABSDacProvider());
 #else
     SetDeviceAttestationCredentialsProvider(Examples::GetExampleDACProvider());
