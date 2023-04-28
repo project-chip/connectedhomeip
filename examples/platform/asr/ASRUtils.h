d4f721199e0b081e9a739086ced0bf055c7e47fd
weicheng <weichengxu@asrmicro.com>
2023-04-28 17:20:09 +0800

    [ASR] add ASR582X & ASR595X support

---
+++ examples/platform/asr/ASRUtils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <app-common/zap-generated/attributes/Accessors.h>
+#include <app-common/zap-generated/ids/Attributes.h>
+#include <app-common/zap-generated/ids/Clusters.h>
+#include <app/server/Dnssd.h>
+#include <app/server/OnboardingCodesUtil.h>
+#include <app/server/Server.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
