5dbb3bb563e91812a9d070860db6db26c7df2bba
Andrei Litvin <andreilitvin@google.com>
2023-05-04 13:23:07 -0400

    Fix some import logic for tests to pass

--- scripts/py_matter_idl/matter_idl/matter_idl_parser.py
+++ scripts/py_matter_idl/matter_idl/matter_idl_parser.py
@@ -10,10 +10,10 @@ from lark.visitors import Transformer, v_args
 
 try:
     from matter_idl.matter_idl_types import AccessPrivilege
-except ImportError:
+except ModuleNotFoundError:
     import os
     import sys
-    sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
     from matter_idl.matter_idl_types import AccessPrivilege
 
