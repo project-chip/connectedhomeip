97918546c54b1871c78c275afc3a90927cdebb0d
Ricardo Casallas <Ricardo.Casallas@silabs.com>
2023-05-04 14:23:36 +0000

    Merge pull request #739 in WMN_TOOLS/matter from remove_credentials_example to silabs
    Auto-Merge: Pull request #739: [AUTO] CPMS: Remove the now obsolete credentials example.
    
    Merge in WMN_TOOLS/matter from remove_credentials_example to silabs
    
    Squashed commit of the following:
    
    commit 61d40e6c9577dc76cabd9d6c088054868c39da71
    Author: Ricardo Casallas <ricardo.casallas@silabs.com>
    Date:   Thu May 4 05:58:46 2023 -0400
    
        CPMS: Deprecated credentials example removed.

--- silabs_examples/credentials/host/host_creds.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATTER_HOST_CREDS_H
-#define MATTER_HOST_CREDS_H
-
-#include <stddef.h>
-#include <stdint.h>
-
-int host_creds_csr(const char * port, const char *common_name, uint32_t vendor_id, uint32_t product_id, const char * filename);
-
-#endif // MATTER_HOST_CREDS_H
