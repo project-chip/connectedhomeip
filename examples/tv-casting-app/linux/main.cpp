f39e477fbc89fd731d28811c594580f84290858f
Boris Zbarsky <bzbarsky@apple.com>
2023-05-05 12:19:03 -0400

    Fix tv-casting-app build

--- examples/tv-casting-app/linux/main.cpp
+++ examples/tv-casting-app/linux/main.cpp
@@ -161,7 +161,7 @@ int main(int argc, char * argv[])
     }
 
     registerClusters(gCommands, &gCredIssuerCommands);
-    registerClusterSubscriptions(gCommands, &gCredIssuerCommands);
+    registerCommandsSubscriptions(gCommands, &gCredIssuerCommands);
 
     if (argc > 1)
     {
