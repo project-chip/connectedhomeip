project-chip/connectedhomeip

commit 4850ad8ffa185543129e2d1d70d65fa56b64b65f

Suppressing the logs related to events' Long dispatch time and Log related changes in Logging.cpp. (#26227)
-Provided the config option specific to ESP32 to set the threshold for events dispatching time.
-Set the config option by default to 0 to suppress the Long dispatch time logs.
-The log related changes mainly changes the way the logs are converted into the idf specific format.
-The arguments are directly used from chip_log instead of converting them to a string and then calling the IDF API.
-This also removes the need for the extra buffer, and also removes the limit for the 256 length for the log + arguments
size.

src/platform/ESP32/Logging.cpp
@@ -26,20 +26,35 @@ void ENFORCE_FORMAT(3, 0) LogV(const char * module, uint8_t category, const char
     snprintf(tag, sizeof(tag), "chip[%s]", module);
     tag[sizeof(tag) - 1] = 0;
 
-    char formattedMsg[CHIP_CONFIG_LOG_MESSAGE_MAX_SIZE];
-    vsnprintf(formattedMsg, sizeof(formattedMsg), msg, v);
-
     switch (category)
     {
     case kLogCategory_Error:
-        ESP_LOGE(tag, "%s", formattedMsg);
+        if (esp_log_default_level >= ESP_LOG_ERROR)
+        {
+            printf(LOG_COLOR_E "E");                        // set color
+            printf(" (%u) %s: ", esp_log_timestamp(), tag); // add timestamp
+            esp_log_writev(ESP_LOG_ERROR, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
         break;
     case kLogCategory_Progress:
     default:
-        ESP_LOGI(tag, "%s", formattedMsg);
+        if (esp_log_default_level >= ESP_LOG_INFO)
+        {
+            printf(LOG_COLOR_I "I");                        // set color
+            printf(" (%u) %s: ", esp_log_timestamp(), tag); // add timestamp
+            esp_log_writev(ESP_LOG_INFO, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
         break;
     case kLogCategory_Detail:
-        ESP_LOGD(tag, "%s", formattedMsg);
+        if (esp_log_default_level >= ESP_LOG_DEBUG)
+        {
+            printf(LOG_COLOR_D "D");                        // set color
+            printf(" (%u) %s: ", esp_log_timestamp(), tag); // add timestamp
+            esp_log_writev(ESP_LOG_DEBUG, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
         break;
     }
 }

config/esp32/components/chip/Kconfig
@@ -108,6 +108,14 @@ menu "CHIP Core"
             help
                 Build CHIP test binaries.
 
+        config DISPATCH_EVENT_LONG_DISPATCH_TIME_WARNING_THRESHOLD_MS
+            int  "Set threshold in ms"
+            default 700
+            help
+                 Time threshold for events dispatching. By default set to 0 to
+                 to disable event dispatching time measurement and suppress the
+                 logs for Long dispatch time.
+
         # TODO: add log level selection
 
     endmenu # "General Options"

src/platform/ESP32/CHIPPlatformConfig.h
@@ -51,6 +51,7 @@
 
 // The following values are configured via the ESP-IDF Kconfig mechanism.
 
+#define CHIP_DISPATCH_EVENT_LONG_DISPATCH_TIME_WARNING_THRESHOLD_MS CONFIG_DISPATCH_EVENT_LONG_DISPATCH_TIME_WARNING_THRESHOLD_MS
 #define CHIP_CONFIG_MAX_UNSOLICITED_MESSAGE_HANDLERS CONFIG_MAX_UNSOLICITED_MESSAGE_HANDLERS
 #define CHIP_CONFIG_MAX_EXCHANGE_CONTEXTS CONFIG_MAX_EXCHANGE_CONTEXTS
 #define CHIP_CONFIG_SECURITY_TEST_MODE CONFIG_SECURITY_TEST_MODE

commit dd4b4d72b684a9e105b7cbc9bce5ba525ec4f771

[ESP32] Use esp_log_writev() for logging rather than ESP_LOGx macros
Basically this unreverts the #26282 with additional changes to fixed
the rpc logging problem introduced in #26227.

config/esp32/components/chip/CMakeLists.txt
@@ -373,8 +373,10 @@ target_link_libraries(${COMPONENT_LIB} INTERFACE -Wl,--start-group
 add_dependencies(${COMPONENT_LIB} chip_gn)
 
 if(CONFIG_ENABLE_PW_RPC)
-    set(WRAP_FUNCTIONS esp_log_write)
-    target_link_libraries(${COMPONENT_LIB} INTERFACE "-Wl,--wrap=${WRAP_FUNCTIONS}")
+    set(WRAP_FUNCTIONS esp_log_write esp_log_writev)
+    foreach(func ${WRAP_FUNCTIONS})
+        target_link_libraries(${COMPONENT_LIB} INTERFACE "-Wl,--wrap=${func}")
+    endforeach()
 endif()
 
 # Build Matter OTA image

examples/platform/esp32/PigweedLogger.cpp
@@ -86,6 +86,22 @@ SemaphoreHandle_t * getSemaphore()
     return &esp_log_mutex;
 }
 
+static const char * getLogColorForLevel(esp_log_level_t level)
+{
+    switch (level)
+    {
+    case ESP_LOG_ERROR:
+        return LOG_COLOR_E "E";
+
+    case ESP_LOG_INFO:
+        return LOG_COLOR_E "I";
+
+    default:
+        // default is kept as ESP_LOG_DEBUG
+        return LOG_COLOR_E "D";
+    }
+}
+
 extern "C" void __wrap_esp_log_write(esp_log_level_t level, const char * tag, const char * format, ...)
 {
     va_list v;
@@ -105,6 +121,32 @@ extern "C" void __wrap_esp_log_write(esp_log_level_t level, const char * tag, co
 #endif
 
     va_end(v);
+}
+
+extern "C" void __wrap_esp_log_writev(esp_log_level_t level, const char * tag, const char * format, va_list v)
+{
+#ifndef CONFIG_LOG_DEFAULT_LEVEL_NONE
+    if (uartInitialised && level <= CONFIG_LOG_MAXIMUM_LEVEL)
+    {
+        const char * logColor = getLogColorForLevel(level);
+        PigweedLogger::putString(logColor, strlen(logColor));
+
+        char formattedMsg[CHIP_CONFIG_LOG_MESSAGE_MAX_SIZE];
+        size_t len = snprintf(formattedMsg, sizeof formattedMsg, " (%u) %s: ", esp_log_timestamp(), tag);
+        PigweedLogger::putString(formattedMsg, len);
+
+        memset(formattedMsg, 0, sizeof formattedMsg);
+        len = vsnprintf(formattedMsg, sizeof formattedMsg, format, v);
+        if (len >= sizeof formattedMsg)
+        {
+            len = sizeof formattedMsg - 1;
+        }
+        PigweedLogger::putString(formattedMsg, len);
+
+        const char * logResetColor = LOG_RESET_COLOR "\n";
+        PigweedLogger::putString(logResetColor, strlen(logResetColor));
+    }
+#endif
 }
 
 } // namespace PigweedLogger

src/platform/ESP32/Logging.cpp
@@ -26,21 +26,38 @@ void ENFORCE_FORMAT(3, 0) LogV(const char * module, uint8_t category, const char
     snprintf(tag, sizeof(tag), "chip[%s]", module);
     tag[sizeof(tag) - 1] = 0;
 
-    char formattedMsg[CHIP_CONFIG_LOG_MESSAGE_MAX_SIZE];
-    vsnprintf(formattedMsg, sizeof(formattedMsg), msg, v);
-
     switch (category)
     {
-    case kLogCategory_Error:
-        ESP_LOGE(tag, "%s", formattedMsg);
-        break;
+    case kLogCategory_Error: {
+        if (esp_log_default_level >= ESP_LOG_ERROR)
+        {
+            printf(LOG_COLOR_E "E (%u) %s: ", esp_log_timestamp(), tag);
+            esp_log_writev(ESP_LOG_ERROR, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
+    }
+    break;
+
     case kLogCategory_Progress:
-    default:
-        ESP_LOGI(tag, "%s", formattedMsg);
-        break;
-    case kLogCategory_Detail:
-        ESP_LOGD(tag, "%s", formattedMsg);
-        break;
+    default: {
+        if (esp_log_default_level >= ESP_LOG_INFO)
+        {
+            printf(LOG_COLOR_I "I (%u) %s: ", esp_log_timestamp(), tag);
+            esp_log_writev(ESP_LOG_INFO, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
+    }
+    break;
+
+    case kLogCategory_Detail: {
+        if (esp_log_default_level >= ESP_LOG_DEBUG)
+        {
+            printf(LOG_COLOR_D "D (%u) %s: ", esp_log_timestamp(), tag);
+            esp_log_writev(ESP_LOG_DEBUG, tag, msg, v);
+            printf(LOG_RESET_COLOR "\n");
+        }
+    }
+    break;
     }
 }
 