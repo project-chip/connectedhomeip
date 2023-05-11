60119a6744676020674b706c1a31a3008382a95b
Mathieu Kardous <mathieu.kardous@silabs.com>
2023-05-11 15:17:05 -0400

    Creation of ICDManager

---
+++ src/include/platform/ICDEventManager.h
@@ -0,0 +1,46 @@
+/*
+ *
+ *    Copyright (c) 2023 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#pragma once
+
+/* this file behaves like a config.h, comes first */
+#include <platform/internal/CHIPDeviceLayerInternal.h>
+#include <platform/ICDManager.h>
+#include "lib/core/CHIPError.h"
+
+namespace chip {
+namespace DeviceLayer {
+
+/**
+ * @brief ICDEventManager class is responsible of processing Platform Events that affect an ICDs behaviour
+ * The class registers an Event Handler with the Platform Manager and dispatchs the processing to the ICDManager class. 
+ */
+class ICDEventManager {
+
+public :
+    static ICDEventManager & GetInstance();
+    CHIP_ERROR Init();
+
+private:
+    ICDEventManager() = default;
+
+    ICDManager icdManager;
+    static ICDEventManager instance;
+
+};
+
+} // namespace DeviceLayer
+} // namespace chip
