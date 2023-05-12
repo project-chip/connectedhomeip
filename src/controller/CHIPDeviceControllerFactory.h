83bbed27eb2bf678a0e890747b7e9bc11432404d
Boris Zbarsky <bzbarsky@apple.com>
2023-05-12 09:08:40 -0400

    Fix handling of certificate validity policy in CHIPDeviceControllerFactory. (#26526)
    When we created a new system state, we lost track of the certificate validity
    policy (since it's not stored in the system state and was not copied from the
    old one to the new one).
    
    Just store it in the controller factory like other things of that sort, so we
    make sure to propagate it to all system states.

--- src/controller/CHIPDeviceControllerFactory.h
+++ src/controller/CHIPDeviceControllerFactory.h
@@ -251,11 +251,12 @@ private:
     CHIP_ERROR InitSystemState();
 
     uint16_t mListenPort;
-    DeviceControllerSystemState * mSystemState              = nullptr;
-    PersistentStorageDelegate * mFabricIndependentStorage   = nullptr;
-    Crypto::OperationalKeystore * mOperationalKeystore      = nullptr;
-    Credentials::OperationalCertificateStore * mOpCertStore = nullptr;
-    bool mEnableServerInteractions                          = false;
+    DeviceControllerSystemState * mSystemState                          = nullptr;
+    PersistentStorageDelegate * mFabricIndependentStorage               = nullptr;
+    Crypto::OperationalKeystore * mOperationalKeystore                  = nullptr;
+    Credentials::OperationalCertificateStore * mOpCertStore             = nullptr;
+    Credentials::CertificateValidityPolicy * mCertificateValidityPolicy = nullptr;
+    bool mEnableServerInteractions                                      = false;
 };
 
 } // namespace Controller
