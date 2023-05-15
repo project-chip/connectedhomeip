3793dba3c257ce50da7ac6713ca2495c0ec10704
Lazar Kovacic <lkovacic@amazon.com>
2023-05-15 21:37:12 +0200

    Update logic & tests

--- examples/tv-app/linux/include/channel/ChannelManager.cpp
+++ examples/tv-app/linux/include/channel/ChannelManager.cpp
@@ -169,9 +169,18 @@ bool ChannelManager::HandleChangeChannelByNumber(const uint16_t & majorNumber, c
 
 bool ChannelManager::HandleSkipChannel(const int16_t & count)
 {
-    // TODO: Insert code here
-    uint16_t newChannelIndex = static_cast<uint16_t>((count + mCurrentChannelIndex) % mChannels.size());
-    mCurrentChannelIndex     = newChannelIndex;
+    int16_t newChannelIndex  = count + static_cast<int16_t>(mCurrentChannelIndex);
+    int16_t channelsSize = static_cast<int16_t>(mChannels.size());
+
+    if (newChannelIndex >= channelsSize) {
+        newChannelIndex = newChannelIndex % channelsSize;
+    }
+
+    if (newChannelIndex < 0) {
+        newChannelIndex = channelsSize + (newChannelIndex % channelsSize);
+    }
+
+    mCurrentChannelIndex     = static_cast<uint16_t>(newChannelIndex);
     mCurrentChannel          = mChannels[mCurrentChannelIndex];
     return true;
 }
