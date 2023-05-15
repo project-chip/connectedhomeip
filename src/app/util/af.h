bada0a13ef35555691de5ca951cbf69640b20783
Restyled.io <commits@restyled.io>
2023-05-15 19:29:27 +0000

    Restyled by clang-format

--- src/app/util/af.h
+++ src/app/util/af.h
@@ -220,7 +220,8 @@ uint16_t emberAfFindClusterServerEndpointIndex(chip::EndpointId endpoint, chip::
  *
  * @param endpoint Endpoint number
  * @param cluster Id the of the Cluster server you are interrested on
- * @param fixedClusterServerEndpointCount The number of fixed endpoints containing this cluster server.  Typically one of the  EMBER_AF_*_CLUSTER_SERVER_ENDPOINT_COUNT constants.
+ * @param fixedClusterServerEndpointCount The number of fixed endpoints containing this cluster server.  Typically one of the
+ EMBER_AF_*_CLUSTER_SERVER_ENDPOINT_COUNT constants.
  */
 uint16_t emberAfGetClusterServerEndpointIndex(chip::EndpointId endpoint, chip::ClusterId cluster,
                                               uint16_t fixedClusterServerEndpointCount);
