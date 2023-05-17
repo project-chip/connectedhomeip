56fcf2299560ce8d56384d47368c566065e91b33
William <hicklin@users.noreply.github.com>
2023-05-17 14:02:43 +0100

    Update src/app/clusters/mode-select-server/mode-select-delegate.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/clusters/mode-select-server/mode-select-delegate.cpp
+++ src/app/clusters/mode-select-server/mode-select-delegate.cpp
@@ -33,7 +33,7 @@ Status Delegate::HandleChangeToMode(uint8_t mode)
 
 void Delegate::HandleChangeToModeWitheStatus(uint8_t mode, ModeSelect::Commands::ChangeToModeResponse::Type &response)
 {
-    response.status = static_cast<uint8_t>(ChangeToModeResponseStatus::kSuccess);
+    response.status = to_underlying(ChangeToModeResponseStatus::kSuccess);
 }
 
 bool Delegate::IsSupportedMode(uint8_t modeValue)
