32173d541237c49701ec0356590e1275e7bcdbea
Jakub Latusek <j.latusek@samsung.com>
2023-05-22 16:36:47 +0200

    Add simple gpio implementation

--- examples/lighting-app/tizen/src/main.cpp
+++ examples/lighting-app/tizen/src/main.cpp
@@ -25,6 +25,8 @@
 #include <LightingManager.h>
 #include <TizenServiceAppMain.h>
 
+#include <peripheral_io.h>
+
 using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
@@ -45,6 +47,44 @@ void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & 
     }
 }
 
+constexpr int blue_pin  = 18;
+constexpr int green_pin = 19;
+constexpr int red_pin   = 20;
+
+static peripheral_gpio_h blue_gpio;
+static peripheral_gpio_h green_gpio;
+static peripheral_gpio_h red_gpio;
+
+void gpio_turn_on();
+
+int gpio_init()
+{
+    peripheral_gpio_open(blue_pin, &blue_gpio);
+    peripheral_gpio_set_direction(blue_gpio, PERIPHERAL_GPIO_DIRECTION_OUT_INITIALLY_LOW);
+
+    peripheral_gpio_open(green_pin, &green_gpio);
+    peripheral_gpio_set_direction(green_gpio, PERIPHERAL_GPIO_DIRECTION_OUT_INITIALLY_LOW);
+
+    peripheral_gpio_open(red_pin, &red_gpio);
+    peripheral_gpio_set_direction(red_gpio, PERIPHERAL_GPIO_DIRECTION_OUT_INITIALLY_LOW);
+    gpio_turn_on();
+    return 0;
+}
+
+void gpio_turn_on()
+{
+    peripheral_gpio_write(green_gpio, 1);
+    peripheral_gpio_write(blue_gpio, 1);
+    peripheral_gpio_write(red_gpio, 1);
+}
+
+void gpio_turn_off()
+{
+    peripheral_gpio_write(green_gpio, 0);
+    peripheral_gpio_write(blue_gpio, 0);
+    peripheral_gpio_write(red_gpio, 0);
+}
+
 void ApplicationInit()
 {
 #if CHIP_DEVICE_CONFIG_ENABLE_WIFI
@@ -59,7 +99,22 @@ int main(int argc, char * argv[])
     TizenServiceAppMain app;
     VerifyOrDie(app.Init(argc, argv) == 0);
 
+    VerifyOrDie(gpio_init() == 0);
     VerifyOrDie(LightingMgr().Init() == CHIP_NO_ERROR);
+    LightingMgr().SetCallbacks(
+        [](LightingManager::Action_t action) {
+            if (action == LightingManager::ON_ACTION)
+            {
+                gpio_turn_on();
+            }
+            else if (action == LightingManager::OFF_ACTION)
+            {
+                gpio_turn_off();
+            }
+        },
+        [](LightingManager::Action_t action) {
+
+        });
 
     return app.RunMainLoop();
 }
