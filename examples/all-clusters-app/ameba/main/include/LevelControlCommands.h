324fc2e82c18d797c0d6dbcff5a54f9267c44d50
Fesseha <fesseha@evehome.com>
2023-05-24 14:48:59 +0200

    trying to fix types

--- examples/all-clusters-app/ameba/main/include/LevelControlCommands.h
+++ examples/all-clusters-app/ameba/main/include/LevelControlCommands.h
@@ -515,7 +515,8 @@ CHIP_ERROR LevelControlReadCurrentLevel(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::CurrentLevel::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -526,7 +527,8 @@ CHIP_ERROR LevelControlReadRemainingTime(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::RemainingTime::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -537,7 +539,8 @@ CHIP_ERROR LevelControlReadMinLevel(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::MinLevel::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -548,7 +551,8 @@ CHIP_ERROR LevelControlReadMaxLevel(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::MaxLevel::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -559,7 +563,8 @@ CHIP_ERROR LevelControlReadCurrentFrequency(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::CurrentFrequency::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -570,7 +575,8 @@ CHIP_ERROR LevelControlReadMinFrequency(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::MinFrequency::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -581,7 +587,8 @@ CHIP_ERROR LevelControlReadMaxFrequency(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::MaxFrequency::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -592,7 +599,8 @@ CHIP_ERROR LevelControlReadOptions(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::Options::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -603,7 +611,8 @@ CHIP_ERROR LevelControlReadOnOffTransitionTime(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::OnOffTransitionTime::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -614,7 +623,8 @@ CHIP_ERROR LevelControlReadOnLevel(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::OnLevel::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -625,7 +635,8 @@ CHIP_ERROR LevelControlReadOnTransitionTime(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::OnTransitionTime::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -636,7 +647,8 @@ CHIP_ERROR LevelControlReadOffTransitionTime(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::OffTransitionTime::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -647,7 +659,8 @@ CHIP_ERROR LevelControlReadDefaultMoveRate(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::DefaultMoveRate::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
@@ -658,7 +671,8 @@ CHIP_ERROR LevelControlReadStartUpCurrentLevel(int argc, char ** argv)
     data->attributeId         = Clusters::LevelControl::Attributes::StartUpCurrentLevel::Id;
     data->clusterId           = Clusters::LevelControl::Id;
     data->isReadAttribute     = true;
-    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", to_underlying(data->clusterId), to_underlying(data->attributeId));
+    ChipLogProgress(NotSpecified, "Read cluster=0x%x, attribute=0x%08x", static_cast<uint32_t>(data->clusterId),
+                    static_cast<uint32_t>(data->attributeId));
     DeviceLayer::PlatformMgr().ScheduleWork(SwitchWorkerFunction, reinterpret_cast<intptr_t>(data));
     return CHIP_NO_ERROR;
 }
