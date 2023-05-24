e0ed8ca75bb73c46a9aa202937a738ba752e02e7
Restyled.io <commits@restyled.io>
2023-05-24 17:45:32 +0000

    Restyled by autopep8

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -137,7 +137,7 @@ def _GetInDevelopmentTests() -> Set[str]:
         "Test_AddNewFabricFromExistingFabric.yaml",     # chip-repl does not support GetCommissionerRootCertificate and IssueNocChain command
         "TestEqualities.yaml",              # chip-repl does not support pseudo-cluster commands that return a value
         "TestExampleCluster.yaml",          # chip-repl does not load custom pseudo clusters
-        "TestClientMonitoringCluster.yaml", # Client Monitoring Tests need a rework after the XML update
+        "TestClientMonitoringCluster.yaml",  # Client Monitoring Tests need a rework after the XML update
         "Test_TC_TIMESYNC_1_1.yaml"         # Time sync SDK is not yet ready
     }
 
