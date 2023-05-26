4cc0a2be51b416ecc1a811d452580c6b19d65092
thirupathi <ths@silabs.com>
2023-05-26 11:33:46 +0530

    removed 917 soc changes

--- examples/platform/silabs/SilabsDeviceDataProvider.cpp
+++ examples/platform/silabs/SilabsDeviceDataProvider.cpp
@@ -105,7 +105,7 @@ CHIP_ERROR SilabsDeviceDataProvider::FlashFactoryData()
             return err;
         }
     }
-    if (strlen(spake2Salt) != 0)
+    if (spake2Salt != 0)
     {
         err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_Spake2pSalt, spake2Salt);
         if (err != CHIP_NO_ERROR)
@@ -113,7 +113,7 @@ CHIP_ERROR SilabsDeviceDataProvider::FlashFactoryData()
             return err;
         }
     }
-    if (strlen(spake2Verifier) != 0)
+    if (spake2Verifier != 0)
     {
         err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_Spake2pVerifier, spake2Verifier);
         if (err != CHIP_NO_ERROR)
@@ -145,32 +145,6 @@ CHIP_ERROR SilabsDeviceDataProvider::FlashFactoryData()
             return err;
         }
     }
-#ifdef SL_DIC_NVM_EMBED_CERT
-    if (strlen(ca_cert) != 0)
-    {
-        err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_CACerts, ca_cert);
-        if (err != CHIP_NO_ERROR)
-        {
-            return err;
-        }
-    }
-    if (strlen(dev_cert) != 0)
-    {
-        err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_DeviceCerts, dev_cert);
-        if (err != CHIP_NO_ERROR)
-        {
-            return err;
-        }
-    }
-    if (strlen(dev_key) != 0)
-    {
-        err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_DeviceKey, dev_key);
-        if (err != CHIP_NO_ERROR)
-        {
-            return err;
-        }
-    }
-#endif // SL_DIC_NVM_EMBED_CERT
     if (strlen(vendorName) != 0)
     {
         err = SilabsConfig::WriteConfigValueStr(SilabsConfig::kConfigKey_VendorName, vendorName);
