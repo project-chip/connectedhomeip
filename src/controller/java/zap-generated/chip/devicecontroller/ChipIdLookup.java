2205eb658a76302f66c72fe83d085f6c3c70d732
Andrei Litvin <andy314@gmail.com>
2023-05-30 11:13:48 -0400

    stop running restyle on java zap-generated files (#26951)
    * Skip restyling java generated files
    
    * Run zap_regen_all
    
    * Fix restyle path to include a glob
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>

--- src/controller/java/zap-generated/chip/devicecontroller/ChipIdLookup.java
+++ src/controller/java/zap-generated/chip/devicecontroller/ChipIdLookup.java
@@ -20,8 +20,8 @@ package chip.devicecontroller;
 
 public final class ChipIdLookup {
   /**
-   * Translates cluster ID to a cluster name in upper camel case. If no matching ID is found,
-   * returns an empty string.
+   * Translates cluster ID to a cluster name in upper camel case. If no matching
+   * ID is found, returns an empty string.
    */
   public static String clusterIdToName(long clusterId) {
     if (clusterId == 3L) {
@@ -403,8 +403,8 @@ public final class ChipIdLookup {
   }
 
   /**
-   * Translates cluster ID and attribute ID to an attribute name in upper camel case. If no matching
-   * IDs are found, returns an empty string.
+   * Translates cluster ID and attribute ID to an attribute name in upper camel case.
+   * If no matching IDs are found, returns an empty string.
    */
   public static String attributeIdToName(long clusterId, long attributeId) {
     if (clusterId == 3L) {
@@ -6810,8 +6810,8 @@ public final class ChipIdLookup {
   }
 
   /**
-   * Translates cluster ID and event ID to an attribute name in upper camel case. If no matching IDs
-   * are found, returns an empty string.
+   * Translates cluster ID and event ID to an attribute name in upper camel case.
+   * If no matching IDs are found, returns an empty string.
    */
   public static String eventIdToName(long clusterId, long eventId) {
     if (clusterId == 3L) {
@@ -7429,3 +7429,4 @@ public final class ChipIdLookup {
     return "";
   }
 }
+
