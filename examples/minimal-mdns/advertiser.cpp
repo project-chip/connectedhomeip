853c8baabd62bce3d3cff814be471a9efdb9f336
Andrei Litvin <andreilitvin@google.com>
2023-06-15 12:32:54 -0400

    Update advertiser to make srv port unique

--- examples/minimal-mdns/advertiser.cpp
+++ examples/minimal-mdns/advertiser.cpp
@@ -314,7 +314,7 @@ int main(int argc, char ** args)
             err = chip::Dnssd::ServiceAdvertiser::Instance().Advertise(
                 chip::Dnssd::OperationalAdvertisingParameters()
                     .EnableIpV4(gOptions.enableIpV4)
-                    .SetPort(CHIP_PORT)
+                    .SetPort(CHIP_PORT+1)
                     .SetMac(chip::ByteSpan(gOptions.mac, 6))
                     .SetPeerId(PeerId().SetCompressedFabricId(gOptions.fabricId + 1).SetNodeId(gOptions.nodeId + 1)));
         }
@@ -324,7 +324,7 @@ int main(int argc, char ** args)
             err = chip::Dnssd::ServiceAdvertiser::Instance().Advertise(
                 chip::Dnssd::OperationalAdvertisingParameters()
                     .EnableIpV4(gOptions.enableIpV4)
-                    .SetPort(CHIP_PORT)
+                    .SetPort(CHIP_PORT+2)
                     .SetMac(chip::ByteSpan(gOptions.mac, 6))
                     .SetPeerId(PeerId().SetCompressedFabricId(gOptions.fabricId + 2).SetNodeId(gOptions.nodeId + 2)));
         }
