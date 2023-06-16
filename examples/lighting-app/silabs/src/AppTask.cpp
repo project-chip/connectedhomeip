8be97d882d2e513166261ddff7ee2e78d28284f1
chirag-silabs <100861685+chirag-silabs@users.noreply.github.com>
2023-06-16 19:43:50 +0530

    [Silabs][SiWx917] Led 1 enabled for 917 soc (#27146)
    * led 1 fix enabled for 917 soc
    
    * adding conditional based check for BRD4325B

--- examples/lighting-app/silabs/src/AppTask.cpp
+++ examples/lighting-app/silabs/src/AppTask.cpp
@@ -40,7 +40,7 @@
 
 #include <platform/CHIPDeviceLayer.h>
 
-#if defined(SL_CATALOG_SIMPLE_LED_LED1_PRESENT)
+#if (defined(SL_CATALOG_SIMPLE_LED_LED1_PRESENT) || defined(BRD4325B))
 #define LIGHT_LED 1
 #else
 #define LIGHT_LED 0
