3cdd5ee16a8f712496ee81d6902a6e302acf6644
manjunath-grl <102359958+manjunath-grl@users.noreply.github.com>
2023-06-23 20:58:15 +0530

    Modified scripts JUNE 01 (#27011)
    * Modified scripts:
    ci-pics-values
    ciTests.json
    app1/ciTests.json
    app1/config.zap
    app2/config.zap
    manualTests.json
    PICS
    TC_AUDIOOUTPUT_7_2
    TC_BINFO_2_1
    TC_BRBINFO_2_1
    TC_CC_2_2_Simulated
    TC_CC_6_5
    TC_CHANNEL_5_2
    TC_CHANNEL_5_3
    TC_DGTHREAD_3_2_Simulated
    TC_MEDIAINPUT_3_11
    TC_MEDIAINPUT_3_13
    TC_MOD_1_2
    TC_MOD_2_1
    TC_MOD_2_3
    TC_MOD_3_1
    TC_MOD_3_2
    TC_MOD_3_4
    TC_PRS_2_1
    TC_PS_1_1
    TC_TMP_2_1
    
    * Auto generated files
    
    * Restyled by whitespace
    
    * Restyled by prettier-json
    
    * Added anyOf keyword constraint check for TSTAT
    Removed TSTAT-2.2 test from ciTests.json file
    
    * Restyled by prettier-json
    
    * Modify CC-6.5 CI to pass all checks
    
    * Fix MediaInput-3.13
    
    * Fix issue 27087
    
    * Fixes issue 567
    
    * Modified tests
    TC-PRS-2.1
    TC-TMP-2.1
    TC-DRLK-2.1
    TC-WNCV-2.2
    TC-WNCV-2.4
    Auto generated files
    
    * Modified test TC-CC-6.3
    
    * Fixes issue 565
    
    * Modified TC-DRLK-2.1 & Ci-pics-value
    
    * Restyled by whitespace
    
    * Updated comment for DRLK-2.1
    Added test
    TC-DRLK-2.8
    Modified tests
    TC-DRLK-2.1
    TC-DRLK-2.9
    TC-TSTAT-2.2
    Auto generated files
    
    * Auto generated files
    
    * Modified Ci-pics-value for DRLK tests
    
    * Disabled DRLK-2.8 in init.py
    Fixes issue 870
    
    * Restyled by autopep8
    
    * Auto generated files
    
    * Automated test
    TC-FAN-2.5
    Simulated testcases
    TC-DRLK-3.1
    TC-DGTHREAD-3.1
    Modified tests
    TC-DRLK-2.3
    TC-DRLK-2.4
    TC-DRLK-2.12
    
    * Auto generated files
    
    * Restyled by prettier-json
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -137,6 +137,7 @@ def _GetInDevelopmentTests() -> Set[str]:
         "TestAttributesById.yaml",           # chip-repl does not support AnyCommands (06/06/2023)
         "TestCommandsById.yaml",             # chip-repl does not support AnyCommands (06/06/2023)
         "TestEventsById.yaml",               # chip-repl does not support AnyCommands (06/06/2023)
+        "Test_TC_DRLK_2_8.yaml",  # Test fails only in chip-repl: Refer--> https://github.com/project-chip/connectedhomeip/pull/27011#issuecomment-1593339855
     }
 
 
