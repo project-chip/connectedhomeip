e6b201c56e144f897087c3e8fe8b6b82f51d1a86
Andrei Litvin <andy314@gmail.com>
2023-06-28 14:00:36 -0400

    Restyle

--- scripts/py_matter_idl/matter_idl/generators/cpp/tlvmeta/__init__.py
+++ scripts/py_matter_idl/matter_idl/generators/cpp/tlvmeta/__init__.py
@@ -13,32 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-from typing import List, Optional, Generator
 from dataclasses import dataclass
+from typing import Generator, List, Optional
 
 from matter_idl.generators import CodeGenerator, GeneratorStorage
-from matter_idl.matter_idl_types import Cluster, ClusterSide, Idl, Field, StructTag
+from matter_idl.matter_idl_types import Cluster, ClusterSide, Field, Idl, StructTag
+
 
 @dataclass
 class TableEntry:
     code: int                # Unique code for the entry
     name: str                # human friendly name
-    reference: Optional[str] # reference to full name
+    reference: Optional[str]  # reference to full name
     real_type: str           # real type
 
 
 @dataclass
 class Table:
-    full_name: str            # Usable variable fully qualified name (like <Cluster>_<name>)
+    # Usable variable fully qualified name (like <Cluster>_<name>)
+    full_name: str
     entries: List[TableEntry]
 
 
 class ClusterTablesGenerator:
     """Handles conversion from a cluster to tables."""
+
     def __init__(self, cluster: Cluster):
         self.cluster = cluster
-        self.known_types = set() # all types where we create reference_to
-        self.list_types = set() # all types that require a list entry
+        self.known_types = set()  # all types where we create reference_to
+        self.list_types = set()  # all types that require a list entry
 
     def FieldEntry(self, field: Field) -> TableEntry:
         type_reference = "%s_%s" % (self.cluster.name, field.data_type.name)
@@ -70,8 +73,10 @@ class ClusterTablesGenerator:
         for c in self.cluster.commands:
             if c.input_param:
                 yield TableEntry(
-                    name=c.input_param, code=c.code, reference="%s_%s" % (self.cluster.name, c.input_param),
-                    real_type="%s::%s::%s" % (self.cluster.name, c.name, c.input_param)
+                    name=c.input_param, code=c.code, reference="%s_%s" % (
+                        self.cluster.name, c.input_param),
+                    real_type="%s::%s::%s" % (
+                        self.cluster.name, c.name, c.input_param)
                 )
 
         # yield entries for every command output. We use "respons struct"
@@ -80,8 +85,9 @@ class ClusterTablesGenerator:
             if c.tag != StructTag.RESPONSE:
                 continue
             yield TableEntry(
-                 name=c.name, code=c.code, reference="%s_%s" % (self.cluster.name, c.name),
-                 real_type="%s::%s" % (self.cluster.name, c.name),
+                name=c.name, code=c.code, reference="%s_%s" % (
+                    self.cluster.name, c.name),
+                real_type="%s::%s" % (self.cluster.name, c.name),
             )
 
     def GenerateTables(self) -> Generator[Table, None, None]:
@@ -90,52 +96,59 @@ class ClusterTablesGenerator:
         for s in self.cluster.structs:
             yield Table(
                 full_name="%s_%s" % (self.cluster.name, s.name),
-                entries = [self.FieldEntry(field) for field in s.fields]
+                entries=[self.FieldEntry(field) for field in s.fields]
             )
 
         for e in self.cluster.events:
             yield Table(
                 full_name="%s_%s" % (self.cluster.name, e.name),
-                entries = [self.FieldEntry(field) for field in s.fields]
+                entries=[self.FieldEntry(field) for field in s.fields]
             )
 
         # Clusters have attributes. They are direct descendants for
         # attributes
         yield Table(
             full_name=("%s_attributes" % self.cluster.name),
-            entries = [
+            entries=[
                 self.FieldEntry(a.definition) for a in self.cluster.attributes
             ]
         )
         yield Table(
             full_name=("%s_events" % self.cluster.name),
-            entries = [
+            entries=[
                 # events always reference an existing struct
-                TableEntry(coden=e.code, name=e.name, reference="%s_%s" % (self.cluster.name, e.name))
+                TableEntry(coden=e.code, name=e.name, reference="%s_%s" %
+                           (self.cluster.name, e.name))
                 for e in self.cluster.events
             ]
         )
 
         yield Table(
             full_name=("%s_commands" % self.cluster.name),
-            entries = [entry for entry in self.CommandEntries()],
+            entries=[entry for entry in self.CommandEntries()],
         )
 
         # TODO:
         #  - enums and bitmaps could be decoded with their value (by abusing tags a bit)
         #  - list indices require one extra indirection
 
+
 def ClusterToTables(cluster: Cluster) -> List[Table]:
 
     cluster_table = Table(full_name=cluster.name, entries=[
-        TableEntry(code=1, name="attributes", reference="%s_attributes" % cluster.name, real_type="%s/attr" % (cluster.name,)),
-        TableEntry(code=2, name="commands", reference="%s_commands" % cluster.name, real_type="%s/cmd" % (cluster.name,)),
-        TableEntry(code=3, name="events", reference="%s_events" % cluster.name, real_type="%s/ev" % (cluster.name,)),
+        TableEntry(code=1, name="attributes", reference="%s_attributes" %
+                   cluster.name, real_type="%s/attr" % (cluster.name,)),
+        TableEntry(code=2, name="commands", reference="%s_commands" %
+                   cluster.name, real_type="%s/cmd" % (cluster.name,)),
+        TableEntry(code=3, name="events", reference="%s_events" %
+                   cluster.name, real_type="%s/ev" % (cluster.name,)),
     ])
     result = [cluster_table]
-    result.extend([ table for table in ClusterTablesGenerator(cluster).GenerateTables()])
+    result.extend(
+        [table for table in ClusterTablesGenerator(cluster).GenerateTables()])
 
     return result
+
 
 def CreateTables(idl: Idl) -> List[Table]:
     result = []
@@ -176,7 +189,7 @@ class TLVMetaDataGenerator(CodeGenerator):
           src/lib/format/protocol_messages.matter
     """
 
-    def __init__(self, storage: GeneratorStorage, idl: Idl, table_name:str = "clusters_meta", **kargs):
+    def __init__(self, storage: GeneratorStorage, idl: Idl, table_name: str = "clusters_meta", **kargs):
         super().__init__(storage, idl, fs_loader_searchpath=os.path.dirname(__file__))
         self.table_name = table_name
         self.jinja_env.filters['indexInTable'] = IndexInTable
