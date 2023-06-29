972096ad3dc7969cd32a0f8755ca8b812c8feed8
Andrei Litvin <andy314@gmail.com>
2023-06-29 13:51:10 -0400

    Starting some decoding support. Still very much broken

--- src/lib/format/tests/TestDecoding.cpp
+++ src/lib/format/tests/TestDecoding.cpp
@@ -14,8 +14,10 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include <lib/core/TLVTags.h>
+#include <lib/core/TLVDebug.h>
+#include <lib/core/TLVReader.h>
 #include <lib/format/FlatTree.h>
+#include <lib/format/FlatTreePosition.h>
 #include <lib/support/UnitTestRegistration.h>
 
 #include "sample_data.h"
@@ -25,8 +27,121 @@
 
 namespace {
 
-void TestDecode(nlTestSuite * inSuite, void * inContext) {
-    // FIXME: implement
+using namespace chip::TestData;
+using namespace chip::TLV;
+
+using chip::TLVMeta::ClusterTag;
+using chip::TLVMeta::AttributeTag;
+
+class ByTag
+{
+public:
+    constexpr ByTag(Tag tag) : mTag(tag) {}
+    bool operator()(const chip::TLVMeta::ItemInfo & item) { return item.tag == mTag; }
+
+private:
+    const Tag mTag;
+};
+
+void ENFORCE_FORMAT(1, 2) SimpleDumpWriter(const char * aFormat, ...)
+{
+    va_list args;
+    va_start(args, aFormat);
+    vprintf(aFormat, args);
+    va_end(args);
+}
+
+using DecodePosition = chip::FlatTree::Position<chip::TLVMeta::ItemInfo, 16>;
+
+void NiceDecode(DecodePosition & position, TLVReader reader)
+{
+    CHIP_ERROR err = reader.Next(kTLVType_Structure, AnonymousTag());
+    if (err == CHIP_END_OF_TLV) {
+        printf("<empty>\n");
+        return;
+    } else if (err != CHIP_NO_ERROR) {
+        printf("UNEXPECTED DATA: %" CHIP_ERROR_FORMAT "\n", err.Format());
+        return;
+    }
+
+    auto data = position.Get();
+    if (data != nullptr) {
+        printf("%s:\n", data->name);
+    } else {
+        printf("UNKNOWN DATA POSITION\n");
+        return;
+    }
+
+    TLVType unused;
+    reader.EnterContainer(unused);
+    size_t depth = 1; // entered structure
+
+    while (true) {
+        err = reader.Next();
+        if (err == CHIP_END_OF_TLV) {
+            position.Exit();
+            if (--depth == 0) {
+                break;
+            }
+            continue;
+        }
+
+        // we likely have a tag, try to find its data
+        position.Enter(ByTag(reader.GetTag()));
+
+        for (size_t i = 0; i < depth; i++) {
+            printf("  ");
+        }
+
+        // FIXME: exit logic may be complex
+        auto data = position.Get();
+        if (data != nullptr) {
+            printf("%s:\n", data->name);
+        } else {
+            printf("TAG NOT FOUND\n");
+        }
+
+        position.Exit();
+    }
+}
+
+void TestSampleData(nlTestSuite * inSuite, void * inContext, const SamplePayload & data)
+{
+    // data.protocolId
+    // data.messageType
+
+    printf("*******************************************\n");
+    printf("* message type: %d\n", data.protocolId.ToFullyQualifiedSpecForm());
+    printf("* message type: %d\n", data.messageType);
+    printf("* DATA:\n");
+
+    TLVReader reader;
+    reader.Init(data.payload);
+
+    // FIXME: now what
+    Debug::Dump(reader, SimpleDumpWriter);
+    printf("* NEW DATA:\n");
+
+    DecodePosition position(chip::TLVMeta::protocols_meta.data(), chip::TLVMeta::protocols_meta.size());
+
+    // Find the right protocol (fake cluster id)
+    position.Enter(ByTag(ClusterTag(0xFFFF0000 | data.protocolId.ToFullyQualifiedSpecForm())));
+    position.Enter(ByTag(AttributeTag(data.messageType)));
+
+    NiceDecode(position, reader);
+}
+
+void TestDecode(nlTestSuite * inSuite, void * inContext)
+{
+    TestSampleData(inSuite, inContext, secure_channel_mrp_ack);
+    TestSampleData(inSuite, inContext, secure_channel_pkbdf_param_request);
+    TestSampleData(inSuite, inContext, secure_channel_pkbdf_param_response);
+    TestSampleData(inSuite, inContext, secure_channel_pase_pake1);
+    TestSampleData(inSuite, inContext, secure_channel_pase_pake2);
+    TestSampleData(inSuite, inContext, secure_channel_pase_pake3);
+    TestSampleData(inSuite, inContext, secure_channel_status_report);
+    TestSampleData(inSuite, inContext, im_protocol_read_request);
+    TestSampleData(inSuite, inContext, im_protocol_report_data);
 }
 
 const nlTest sTests[] = {
