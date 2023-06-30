6df00da5ac8829f7dba60fe703ac2eddc1dca961
Hare <renilr0@outlook.com>
2023-06-30 14:32:38 +0800

    Smoke/CO: Update zap
    Sync Silab changes

--- examples/smoke-co-alarm-app/silabs/src/AppTask.cpp
+++ examples/smoke-co-alarm-app/silabs/src/AppTask.cpp
@@ -21,7 +21,6 @@
 
 #include "LEDWidget.h"
 
-#include <app/clusters/identify-server/identify-server.h>
 #include <app/clusters/smoke-co-alarm-server/smoke-co-alarm-server.h>
 #include <app/server/OnboardingCodesUtil.h>
 #include <app/server/Server.h>
@@ -38,7 +37,7 @@
 
 #include <platform/CHIPDeviceLayer.h>
 
-#if defined(SL_CATALOG_SIMPLE_LED_LED1_PRESENT)
+#if (defined(SL_CATALOG_SIMPLE_LED_LED1_PRESENT) || defined(BRD4325B))
 #define LIGHT_LED 1
 #else
 #define LIGHT_LED 0
