031a840d62e91f556703482e1d9fffcf09be4ea9
Andrei Litvin <andy314@gmail.com>
2023-07-10 14:26:09 -0400

    Restyle

--- src/lib/core/TLVTags.cpp
+++ src/lib/core/TLVTags.cpp
@@ -24,22 +24,22 @@ namespace TLV {
 StringBuilderBase & Tag::AppendTo(StringBuilderBase & out)
 {
     if (TLV::IsProfileTag(*this))
-        {
-            out.AddFormat("ProfileTag(0x%X::0x%X::0x%" PRIX32 ")", TLV::VendorIdFromTag(*this), TLV::ProfileNumFromTag(*this),
-                          TLV::TagNumFromTag(*this));
-        }
+    {
+        out.AddFormat("ProfileTag(0x%X::0x%X::0x%" PRIX32 ")", TLV::VendorIdFromTag(*this), TLV::ProfileNumFromTag(*this),
+                      TLV::TagNumFromTag(*this));
+    }
     else if (TLV::IsContextTag(*this))
-        {
-            out.AddFormat("ContextTag(0x%" PRIX32 ")", TLV::TagNumFromTag(*this));
-        }
+    {
+        out.AddFormat("ContextTag(0x%" PRIX32 ")", TLV::TagNumFromTag(*this));
+    }
     else if (*this == TLV::AnonymousTag())
-        {
-            out.Add("AnonymousTag()");
-        }
+    {
+        out.Add("AnonymousTag()");
+    }
     else
-        {
-            out.AddFormat("UnknownTag(0x%" PRIX64 ")", mVal);
-        }
+    {
+        out.AddFormat("UnknownTag(0x%" PRIX64 ")", mVal);
+    }
 
     return out;
 }
