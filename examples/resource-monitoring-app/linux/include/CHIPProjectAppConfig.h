40a5369a4c47b167371b99ecd91b49a5d322fcb6
Tobias Graf <4622393+tobiasgraf@users.noreply.github.com>
2023-07-13 14:34:57 +0200

    Remove Options from CHIPProjectAppConfig.h
    not needed or wrong

--- examples/resource-monitoring-app/linux/include/CHIPProjectAppConfig.h
+++ examples/resource-monitoring-app/linux/include/CHIPProjectAppConfig.h
@@ -30,19 +30,6 @@
 // include the CHIPProjectConfig from config/standalone
 #include <CHIPProjectConfig.h>
 
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY 0
-
-// Bulbs do not typically use this - enabled so we can use shell to discover commissioners
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY_CLIENT 1
-
-#define CHIP_DEVICE_CONFIG_ENABLE_EXTENDED_DISCOVERY 1
-
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONABLE_DEVICE_TYPE 1
-
-#define CHIP_DEVICE_CONFIG_DEVICE_TYPE 257 // 0x0101 = 257 = Dimmable Bulb
-
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONABLE_DEVICE_NAME 1
-
-#define CHIP_DEVICE_ENABLE_PORT_PARAMS 1
+#define CHIP_DEVICE_CONFIG_DEVICE_TYPE 45 // 0x002D = 45 == Air Purifer
 
 #define CHIP_DEVICE_CONFIG_DEVICE_NAME "Test Resource Monitoring Device"
