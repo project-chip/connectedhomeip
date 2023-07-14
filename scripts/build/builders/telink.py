39c569bf62a8d94427f788fd96141d63169ec4f3
Alex Tsitsiura <s07641069@gmail.com>
2023-07-15 01:54:24 +0300

    [Telink] Add Smoke Sensor app demo (#27609)
    * [Telink]: Added draft smoke sensor
    
    Added draft smoke sensor app for telink platform
    
    Signed-off-by: dmytro.k <ur6lal@gmail.com>
    
    * [Telink]: Added draft smoke sensor
    
    Added draft smoke sensor app for telink platform
    
    Signed-off-by: dmytro.k <ur6lal@gmail.com>
    
    * [Telink]: Added draft smoke sensor
    
    Added smoke sensor app for telink platform
    
    Signed-off-by: dmytro.k <ur6lal@gmail.com>
    
    * [Teilnk] format fix
    
    * [Telink]: Added smoke sensor
    
    Added smoke sensor app for telink platform
    
    Signed-off-by: dmytro.k <ur6lal@gmail.com>
    
    * [Telink] remove spaces
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by prettier-markdown
    
    * code style fix
    
    * reviewer feedback fix
    
    ---------
    
    Signed-off-by: dmytro.k <ur6lal@gmail.com>
    Co-authored-by: dmytro.k <ur6lal@gmail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- scripts/build/builders/telink.py
+++ scripts/build/builders/telink.py
@@ -31,6 +31,7 @@ class TelinkApp(Enum):
     OTA_REQUESTOR = auto()
     PUMP = auto()
     PUMP_CONTROLLER = auto()
+    SMOKE_CO_ALARM = auto()
     TEMPERATURE_MEASUREMENT = auto()
     THERMOSTAT = auto()
     WINDOW_COVERING = auto()
@@ -56,6 +57,8 @@ class TelinkApp(Enum):
             return 'pump-app'
         elif self == TelinkApp.PUMP_CONTROLLER:
             return 'pump-controller-app'
+        elif self == TelinkApp.SMOKE_CO_ALARM:
+            return 'smoke-co-alarm-app'
         elif self == TelinkApp.TEMPERATURE_MEASUREMENT:
             return 'temperature-measurement-app'
         elif self == TelinkApp.THERMOSTAT:
@@ -86,6 +89,8 @@ class TelinkApp(Enum):
             return 'chip-telink-pump-example'
         elif self == TelinkApp.PUMP_CONTROLLER:
             return 'chip-telink-pump-controller-example'
+        elif self == TelinkApp.SMOKE_CO_ALARM:
+            return 'chip-telink-smoke-co-alarm-example'
         elif self == TelinkApp.TEMPERATURE_MEASUREMENT:
             return 'chip-telink-temperature-measurement-example'
         elif self == TelinkApp.THERMOSTAT:
