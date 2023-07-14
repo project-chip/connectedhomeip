7781f3be34dd83eee470431a6d054999094f16ba
Justin Wood <woody@apple.com>
2023-07-13 21:42:23 -0700

    Starting at version 1, and adding new jobs to roll

--- scripts/helpers/roll_docker_version.sh
+++ scripts/helpers/roll_docker_version.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 # TODO: Add a simple way to roll docker image numbers
 #image: xxxxxxxxxx:0.7.14
+CURRENT_VERSION=$(sed 's/ .*//' ./integrations/docker/images/base/chip-build/version)
+
+echo "Current version: $CURRENT_VERSION"
+
+NEXT_VERSION=$((CURRENT_VERSION + 1))
+
+echo "Next version: $NEXT_VERSION"
+
+sed -r -i "s|image: ghcr\.io/project-chip/(.*):[0-9]*(\..*)?|image: ghcr.io/project-chip/\1:$NEXT_VERSION|" .github/workflows/*.yaml
+
+echo "$NEXT_VERSION" >./integrations/docker/images/base/chip-build/version
