90db4fe2a578f7c18d02438726fbdb7b530801a1
mideayanghui <yanghui65@midea.com>
2023-07-14 16:48:20 +0800

    remove unuseful code

--- examples/dishwasher-app/linux/include/CHIPProjectAppConfig.h
+++ examples/dishwasher-app/linux/include/CHIPProjectAppConfig.h
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2023 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,19 +30,5 @@
 // include the CHIPProjectConfig from config/standalone
 #include <CHIPProjectConfig.h>
 
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY 0
-
-// Bulbs do not typically use this - enabled so we can use shell to discover commissioners
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY_CLIENT 1
-
-#define CHIP_DEVICE_CONFIG_ENABLE_EXTENDED_DISCOVERY 1
-
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONABLE_DEVICE_TYPE 1
-
-#define CHIP_DEVICE_CONFIG_DEVICE_TYPE 257 // 0x0101 = 257 = Dimmable Bulb
-
-#define CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONABLE_DEVICE_NAME 1
-
-#define CHIP_DEVICE_ENABLE_PORT_PARAMS 1
-
-#define CHIP_DEVICE_CONFIG_DEVICE_NAME "Test Bulb"
+#define CHIP_DEVICE_CONFIG_DEVICE_TYPE 117 // 0x0075 = 117 = Matter Dishwasher
+#define CHIP_DEVICE_CONFIG_DEVICE_NAME "Test Dishwasher"
--- examples/dishwasher-app/linux/main.cpp
+++ examples/dishwasher-app/linux/main.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2023 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,36 +21,14 @@
 #include <app-common/zap-generated/ids/Attributes.h>
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/ConcreteAttributePath.h>
-#include <app/clusters/network-commissioning/network-commissioning.h>
-#include <app/server/Server.h>
 #include <lib/support/logging/CHIPLogging.h>
-#include <platform/Linux/NetworkCommissioningDriver.h>
-
-#if defined(CHIP_IMGUI_ENABLED) && CHIP_IMGUI_ENABLED
-#include <imgui_ui/ui.h>
-#include <imgui_ui/windows/light.h>
-#include <imgui_ui/windows/occupancy_sensing.h>
-#include <imgui_ui/windows/qrcode.h>
-
-#endif
 
 using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
 
-#if CHIP_DEVICE_CONFIG_ENABLE_WPA
-namespace {
-DeviceLayer::NetworkCommissioning::LinuxWiFiDriver sLinuxWiFiDriver;
-Clusters::NetworkCommissioning::Instance sWiFiNetworkCommissioningInstance(0, &sLinuxWiFiDriver);
-} // namespace
-#endif
-
 void ApplicationInit()
 {
-#if CHIP_DEVICE_CONFIG_ENABLE_WPA
-    sWiFiNetworkCommissioningInstance.Init();
-#endif
-
     MatterOperationalStateServerInit();
 }
 
@@ -61,17 +39,6 @@ int main(int argc, char * argv[])
         return -1;
     }
 
-#if defined(CHIP_IMGUI_ENABLED) && CHIP_IMGUI_ENABLED
-    example::Ui::ImguiUi ui;
-
-    ui.AddWindow(std::make_unique<example::Ui::Windows::QRCode>());
-    ui.AddWindow(std::make_unique<example::Ui::Windows::OccupancySensing>(chip::EndpointId(1), "Occupancy"));
-    ui.AddWindow(std::make_unique<example::Ui::Windows::Light>(chip::EndpointId(1)));
-
-    ChipLinuxAppMainLoop(&ui);
-#else
     ChipLinuxAppMainLoop();
-#endif
-
     return 0;
 }
