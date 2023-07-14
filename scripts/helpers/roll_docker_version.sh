371393a997cdfa9b678cf7d5115c61cc90210c7a
Justin Wood <woody@apple.com>
2023-07-14 09:06:45 -0700

    Updating

--- scripts/helpers/roll_docker_version.sh
+++ scripts/helpers/roll_docker_version.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# TODO: Add a simple way to roll docker image numbers
-#image: xxxxxxxxxx:0.7.14
 CURRENT_VERSION=$(sed 's/ .*//' ./integrations/docker/images/base/chip-build/version)
 
 echo "Current version: $CURRENT_VERSION"
@@ -9,6 +7,16 @@ NEXT_VERSION=$((CURRENT_VERSION + 1))
 
 echo "Next version: $NEXT_VERSION"
 
+echo "Rolling workflows"
 sed -r -i "s|image: ghcr\.io/project-chip/(.*):[0-9]*(\..*)?|image: ghcr.io/project-chip/\1:$NEXT_VERSION|" .github/workflows/*.yaml
+
+echo "Rolling VSCode container"
 sed -r -i "s|matter-dev-environment:local --version [0-9]*|matter-dev-environment:local --version $NEXT_VERSION|" .devcontainer/devcontainer.json
+
+echo "Rolling docker images"
+sed -r -i "s|ARG VERSION=[0-9]*|ARG VERSION=$NEXT_VERSION|" integrations/docker/images/*/*/Dockerfile
+
+echo "Rolling main docker version"
 echo "$NEXT_VERSION" >./integrations/docker/images/base/chip-build/version
+
+echo "Done"
