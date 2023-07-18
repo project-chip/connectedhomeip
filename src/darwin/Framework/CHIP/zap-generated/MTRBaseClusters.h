41c79412d6b1abd8a41f508a7039109fb1ca5823
Boris Zbarsky <bzbarsky@apple.com>
2023-07-17 20:45:46 -0400

    Update ZAP to pick up swiftUnavailable changes.
    And mark some Darwin codegen bits as swiftUnavailable.

--- src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
+++ src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
@@ -19573,6 +19573,7 @@ typedef NS_OPTIONS(uint8_t, MTRNetworkCommissioningWiFiSecurity) {
     = 0x4,
     MTRNetworkCommissioningWiFiSecurityWpaPersonal MTR_DEPRECATED("Please use MTRNetworkCommissioningWiFiSecurityBitmapWPAPersonal",
         ios(16.1, 16.4), macos(13.0, 13.3), watchos(9.1, 9.4), tvos(16.1, 16.4))
+        NS_SWIFT_UNAVAILABLE("Please use MTRNetworkCommissioningWiFiSecurityBitmapWPAPersonal")
     = 0x4,
     MTRNetworkCommissioningWiFiSecurityWPA2Personal MTR_DEPRECATED(
         "Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA2Personal", ios(16.4, 17.0), macos(13.3, 14.0), watchos(9.4, 10.0),
@@ -19580,7 +19581,7 @@ typedef NS_OPTIONS(uint8_t, MTRNetworkCommissioningWiFiSecurity) {
     = 0x8,
     MTRNetworkCommissioningWiFiSecurityWpa2Personal MTR_DEPRECATED(
         "Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA2Personal", ios(16.1, 16.4), macos(13.0, 13.3), watchos(9.1, 9.4),
-        tvos(16.1, 16.4))
+        tvos(16.1, 16.4)) NS_SWIFT_UNAVAILABLE("Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA2Personal")
     = 0x8,
     MTRNetworkCommissioningWiFiSecurityWPA3Personal MTR_DEPRECATED(
         "Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA3Personal", ios(16.4, 17.0), macos(13.3, 14.0), watchos(9.4, 10.0),
@@ -19588,7 +19589,7 @@ typedef NS_OPTIONS(uint8_t, MTRNetworkCommissioningWiFiSecurity) {
     = 0x10,
     MTRNetworkCommissioningWiFiSecurityWpa3Personal MTR_DEPRECATED(
         "Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA3Personal", ios(16.1, 16.4), macos(13.0, 13.3), watchos(9.1, 9.4),
-        tvos(16.1, 16.4))
+        tvos(16.1, 16.4)) NS_SWIFT_UNAVAILABLE("Please use MTRNetworkCommissioningWiFiSecurityBitmapWPA3Personal")
     = 0x10,
 } MTR_DEPRECATED("Please use MTRNetworkCommissioningWiFiSecurityBitmap", ios(16.1, 17.0), macos(13.0, 14.0), watchos(9.1, 10.0),
     tvos(16.1, 17.0));
