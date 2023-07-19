b341816266fb70233f8a46a5b3cdd9dfef9fc5d5
Neethu <neethu.viswanath@sensorfy.ai>
2023-07-19 09:25:21 +0200

    TSBM-21: Modification for renesas platform

---
+++ src/platform/renesas/DnssdImpl.cpp
@@ -0,0 +1,146 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#include "DnssdImpl.h"
+#include "lib/dnssd/platform/Dnssd.h"
+
+namespace chip {
+namespace Dnssd {
+
+namespace test {
+namespace {
+static constexpr size_t kMaxExpectedCalls = 10;
+size_t numExpectedCalls                   = 0;
+ExpectedCall expectedCalls[kMaxExpectedCalls];
+bool foundCall[kMaxExpectedCalls] = {};
+} // namespace
+
+CHIP_ERROR AddExpectedCall(const ExpectedCall & call)
+{
+    if (!call.IsValid())
+    {
+        return CHIP_ERROR_INVALID_ARGUMENT;
+    }
+    if (numExpectedCalls >= kMaxExpectedCalls)
+    {
+        return CHIP_ERROR_NO_MEMORY;
+    }
+    expectedCalls[numExpectedCalls++] = call;
+    return CHIP_NO_ERROR;
+}
+
+void Reset()
+{
+    numExpectedCalls = 0;
+    for (auto & found : foundCall)
+    {
+        found = false;
+    }
+}
+
+CHIP_ERROR CheckExpected(CallType type, const DnssdService * service)
+{
+    for (size_t i = 0; i < test::numExpectedCalls; ++i)
+    {
+        if (test::foundCall[i])
+        {
+            continue;
+        }
+        if (test::expectedCalls[i].CheckMatch(type, service))
+        {
+            return CHIP_NO_ERROR;
+        }
+    }
+    ChipLogError(Discovery, "Unexpected event of type %d", static_cast<int>(type));
+    ChipLogProgress(Discovery, "mName = %s", service->mName);
+    ChipLogProgress(Discovery, "mHostName = %s", service->mHostName);
+    ChipLogProgress(Discovery, "mType = %s", service->mType);
+    ChipLogProgress(Discovery, "mProtocol = %d", static_cast<int>(service->mProtocol));
+    ChipLogProgress(Discovery, "num subtypes = %lu", static_cast<unsigned long>(service->mSubTypeSize));
+    for (size_t i = 0; i < service->mSubTypeSize; ++i)
+    {
+        ChipLogProgress(Discovery, "\t%s", service->mSubTypes[i]);
+    }
+    ChipLogProgress(Discovery, "num text entries = %lu", static_cast<unsigned long>(service->mTextEntrySize));
+    for (size_t i = 0; i < service->mTextEntrySize; ++i)
+    {
+        ChipLogProgress(Discovery, "\t%s", service->mTextEntries[i].mKey);
+    }
+    ChipLogProgress(Discovery, "\nExpected:");
+    for (size_t i = 0; i < test::numExpectedCalls; ++i)
+    {
+        ChipLogProgress(Discovery, "Call %lu", static_cast<unsigned long>(i));
+        test::expectedCalls[i].PrintForDebugging();
+    }
+    return CHIP_ERROR_UNEXPECTED_EVENT;
+}
+
+} // namespace test
+
+CHIP_ERROR ChipDnssdInit(DnssdAsyncReturnCallback initCallback, DnssdAsyncReturnCallback errorCallback, void * context)
+{
+    initCallback(context, CHIP_NO_ERROR);
+    return CHIP_NO_ERROR;
+}
+
+void ChipDnssdShutdown() {}
+
+CHIP_ERROR ChipDnssdPublishService(const DnssdService * service, DnssdPublishCallback callback, void * context)
+{
+    return test::CheckExpected(test::CallType::kStart, service);
+}
+
+CHIP_ERROR ChipDnssdRemoveServices()
+{
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR ChipDnssdFinalizeServiceUpdate()
+{
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR ChipDnssdBrowse(const char * type, DnssdServiceProtocol protocol, chip::Inet::IPAddressType addressType,
+                           chip::Inet::InterfaceId interface, DnssdBrowseCallback callback, void * context,
+                           intptr_t * browseIdentifier)
+{
+    return CHIP_ERROR_NOT_IMPLEMENTED;
+}
+
+CHIP_ERROR ChipDnssdStopBrowse(intptr_t browseIdentifier)
+{
+    return CHIP_ERROR_NOT_IMPLEMENTED;
+}
+
+CHIP_ERROR ChipDnssdResolve(DnssdService * browseResult, chip::Inet::InterfaceId interface, DnssdResolveCallback callback,
+                            void * context)
+
+{
+    return CHIP_ERROR_NOT_IMPLEMENTED;
+}
+
+void ChipDnssdResolveNoLongerNeeded(const char * instanceName) {}
+
+CHIP_ERROR ChipDnssdReconfirmRecord(const char * hostname, chip::Inet::IPAddress address, chip::Inet::InterfaceId interface)
+{
+    return CHIP_ERROR_NOT_IMPLEMENTED;
+}
+
+void GetMdnsTimeout(timeval & timeout) {}
+void HandleMdnsTimeout() {}
+
+} // namespace Dnssd
+} // namespace chip
--- src/platform/renesas/SystemTimeSupport.cpp
+++ src/platform/renesas/SystemTimeSupport.cpp
@@ -20,12 +20,15 @@
 /**
  *    @file
  *          Provides implementations of the CHIP System Layer platform
- *          time/clock functions.
+ *          time/clock functions based on the FreeRTOS tick counter.
  */
 /* this file behaves like a config.h, comes first */
 #include <platform/internal/CHIPDeviceLayerInternal.h>
 
 #include <lib/support/TimeUtils.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
 
 namespace chip {
 namespace System {
@@ -35,29 +38,135 @@ namespace Internal {
 ClockImpl gClockImpl;
 } // namespace Internal
 
+namespace {
+
+constexpr uint32_t kTicksOverflowShift = (configUSE_16_BIT_TICKS) ? 16 : 32;
+
+uint64_t sBootTimeUS = 0;
+
+#ifdef __CORTEX_M
+BaseType_t sNumOfOverflows;
+#endif
+} // unnamed namespace
+
+/**
+ * Returns the number of FreeRTOS ticks since the system booted.
+ *
+ * NOTE: The default implementation of this function uses FreeRTOS's
+ * vTaskSetTimeOutState() function to get the total number of ticks,
+ * irrespective of tick counter overflows.  Unfortunately, this function cannot
+ * be called in interrupt context, no equivalent ISR function exists, and
+ * FreeRTOS provides no portable way of determining whether a function is being
+ * called in an interrupt context.  Adaptations that need to use the Chip
+ * Get/SetClock methods from within an interrupt handler must override this
+ * function with a suitable alternative that works on the target platform.  The
+ * provided version is safe to call on ARM Cortex platforms with CMSIS
+ * libraries.
+ */
+
+uint64_t FreeRTOSTicksSinceBoot(void) __attribute__((weak));
+
+uint64_t FreeRTOSTicksSinceBoot(void)
+{
+    TimeOut_t timeOut;
+
+#ifdef __CORTEX_M
+    if (SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) // running in an interrupt context
+    {
+        // Note that sNumOverflows may be quite stale, and under those
+        // circumstances, the function may violate monotonicity guarantees
+        timeOut.xTimeOnEntering = xTaskGetTickCountFromISR();
+        timeOut.xOverflowCount  = sNumOfOverflows;
+    }
+    else
+    {
+#endif
+
+        vTaskSetTimeOutState(&timeOut);
+
+#ifdef __CORTEX_M
+        // BaseType_t is supposed to be atomic
+        sNumOfOverflows = timeOut.xOverflowCount;
+    }
+#endif
+
+    return static_cast<uint64_t>(timeOut.xTimeOnEntering) + (static_cast<uint64_t>(timeOut.xOverflowCount) << kTicksOverflowShift);
+}
+
 Clock::Microseconds64 ClockImpl::GetMonotonicMicroseconds64(void)
 {
-    return Clock::Microseconds64(clock_SystemTimeMicroseconds64());
+    return Clock::Microseconds64((FreeRTOSTicksSinceBoot() * kMicrosecondsPerSecond) / configTICK_RATE_HZ);
 }
 
 Clock::Milliseconds64 ClockImpl::GetMonotonicMilliseconds64(void)
 {
-    return Clock::Milliseconds64(clock_SystemTimeMicroseconds64() / 1000);
+    return Clock::Milliseconds64((FreeRTOSTicksSinceBoot() * kMillisecondsPerSecond) / configTICK_RATE_HZ);
+}
+
+uint64_t GetClock_Monotonic(void)
+{
+    return (FreeRTOSTicksSinceBoot() * kMicrosecondsPerSecond) / configTICK_RATE_HZ;
+}
+
+uint64_t GetClock_MonotonicMS(void)
+{
+    return (FreeRTOSTicksSinceBoot() * kMillisecondsPerSecond) / configTICK_RATE_HZ;
+}
+
+uint64_t GetClock_MonotonicHiRes(void)
+{
+    return GetClock_Monotonic();
 }
 
 CHIP_ERROR ClockImpl::GetClock_RealTime(Clock::Microseconds64 & aCurTime)
 {
+    // TODO(19081): This platform does not properly error out if wall clock has
+    //              not been set.  For now, short circuit this.
     return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+#if 0
+    if (sBootTimeUS == 0)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    aCurTime = Clock::Microseconds64(sBootTimeUS + GetClock_Monotonic());
+    return CHIP_NO_ERROR;
+#endif
 }
 
 CHIP_ERROR ClockImpl::GetClock_RealTimeMS(Clock::Milliseconds64 & aCurTime)
 {
-    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+    if (sBootTimeUS == 0)
+    {
+        return CHIP_ERROR_REAL_TIME_NOT_SYNCED;
+    }
+    aCurTime = Clock::Milliseconds64((sBootTimeUS + GetClock_Monotonic()) / 1000);
+    return CHIP_NO_ERROR;
 }
 
 CHIP_ERROR ClockImpl::SetClock_RealTime(Clock::Microseconds64 aNewCurTime)
 {
-    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+    uint64_t timeSinceBootUS = GetClock_Monotonic();
+    if (aNewCurTime.count() > timeSinceBootUS)
+    {
+        sBootTimeUS = aNewCurTime.count() - timeSinceBootUS;
+    }
+    else
+    {
+        sBootTimeUS = 0;
+    }
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR InitClock_RealTime()
+{
+    Clock::Microseconds64 curTime =
+        Clock::Microseconds64((static_cast<uint64_t>(CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD) * UINT64_C(1000000)));
+    // Use CHIP_SYSTEM_CONFIG_VALID_REAL_TIME_THRESHOLD as the initial value of RealTime.
+    // Then the RealTime obtained from GetClock_RealTime will be always valid.
+    //
+    // TODO(19081): This is broken because it causes the platform to report
+    //              that it does have wall clock time when it actually doesn't.
+    return System::SystemClock().SetClock_RealTime(curTime);
 }
 
 } // namespace Clock
