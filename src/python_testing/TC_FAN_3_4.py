987610a5fc0e588305784c45b77794d1e2266004
Restyled.io <commits@restyled.io>
2023-07-20 13:34:00 +0000

    Restyled by isort

--- src/python_testing/TC_FAN_3_4.py
+++ src/python_testing/TC_FAN_3_4.py
@@ -15,14 +15,13 @@
 #    limitations under the License.
 #
 
+import logging
 import time
 
 import chip.clusters as Clusters
 from chip.interaction_model import Status
 from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
 from mobly import asserts
-import logging
-
 
 logger = logging.getLogger(__name__)
 
