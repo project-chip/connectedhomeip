f64254d630d21c43256d06e9a0b3267acfae725c
Joonhaeng Heo <jh.heo@lge.com>
2023-07-24 17:26:08 +0900

    fix kotlin static check error

--- src/controller/java/tests/chip/devicecontroller/cluster/ChipClusterStructTest.kt
+++ src/controller/java/tests/chip/devicecontroller/cluster/ChipClusterStructTest.kt
@@ -14,7 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 package chip.devicecontroller.cluster
 
 import chip.devicecontroller.cluster.structs.UnitTestingClusterDoubleNestedStructList
@@ -32,7 +31,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class ChipClusterTest {
+class ChipClusterStructTest {
   @Test
   fun doubleNestedStructTlvTest() {
     val simpleStruct =
@@ -386,61 +385,6 @@ class ChipClusterTest {
     // Reparse to Struct
     val tlvReader = TlvReader(tlv)
     val compareStruct = UnitTestingClusterTestListStructOctet.fromTlv(AnonymousTag, tlvReader)
-
-    // For comparing, struct to re-generate to tlv bytearray
-    val compareTlvWriter = TlvWriter()
-    compareStruct.toTlv(AnonymousTag, compareTlvWriter)
-
-    assert(compareTlvWriter.getEncoded().contentEquals(tlvWriter.getEncoded()))
-  }
-
-  @Test
-  fun testEventEventTlvTest() {
-    val simpleStruct =
-      UnitTestingClusterSimpleStruct(1, true, 2, byteArrayOf(0x00, 0x01), "test", 3, 4.5f, 6.7)
-    val simpleStruct2 =
-      UnitTestingClusterSimpleStruct(8, false, 9, byteArrayOf(0x02, 0x03), "test2", 4, 5.6f, 7.8)
-    val struct =
-      UnitTestingClusterTestEventEvent(
-        1,
-        2,
-        true,
-        simpleStruct,
-        listOf(simpleStruct, simpleStruct2),
-        listOf(3, 4, 5)
-      )
-
-    val tlvWriter = TlvWriter()
-    struct.toTlv(AnonymousTag, tlvWriter)
-
-    // to TLV ByteArray
-    val tlv = tlvWriter.getEncoded()
-
-    // Reparse to Struct
-    val tlvReader = TlvReader(tlv)
-    val compareStruct = UnitTestingClusterTestEventEvent.fromTlv(AnonymousTag, tlvReader)
-
-    // For comparing, struct to re-generate to tlv bytearray
-    val compareTlvWriter = TlvWriter()
-    compareStruct.toTlv(AnonymousTag, compareTlvWriter)
-
-    assert(compareTlvWriter.getEncoded().contentEquals(tlvWriter.getEncoded()))
-  }
-
-  @Test
-  fun testFabricScopedEventEventTest() {
-    val struct = UnitTestingClusterTestFabricScopedEventEvent(1)
-
-    val tlvWriter = TlvWriter()
-    struct.toTlv(AnonymousTag, tlvWriter)
-
-    // to TLV ByteArray
-    val tlv = tlvWriter.getEncoded()
-
-    // Reparse to Struct
-    val tlvReader = TlvReader(tlv)
-    val compareStruct =
-      UnitTestingClusterTestFabricScopedEventEvent.fromTlv(AnonymousTag, tlvReader)
 
     // For comparing, struct to re-generate to tlv bytearray
     val compareTlvWriter = TlvWriter()
