5212ee7553744f6f4530f0db810f087cf2bf94a7
Lukas Zeller <luz@plan44.ch>
2023-07-26 19:41:45 +0200

    [attribute storage] allow setting up dynamic endpoints from templates
    Instead of re-creating dynamic endpoint's cluster declarations
    using the DECLARE_DYNAMIC_xxx macros, the new
    `setupDynamicEndpointDeclaration()` function allows setting
    up a dynamic endpoint by using clusters defined in another
    endpoint as templates. Usually that is a disabled endpoint created
    in ZAP with all possibly dynamically used clusters assigned.
    
    In combination with dynamic endpoint attribute storage, this
    greatly simplifies implementing dynamic endpoints and allows
    configuring them using the ZAP tool to ensure specs conformance,
    much the same way as with statically defined endpoints.

--- src/app/util/attribute-storage.cpp
+++ src/app/util/attribute-storage.cpp
@@ -265,6 +265,40 @@ uint16_t emberAfGetDynamicIndexFromEndpoint(EndpointId id)
     return kEmberInvalidEndpointIndex;
 }
 
+const EmberAfCluster * getClusterTypeDefinition(EndpointId endpointId, ClusterId clusterId, EmberAfClusterMask mask)
+{
+    uint16_t index = emberAfIndexFromEndpointIncludingDisabledEndpoints(endpointId);
+    if (index != kEmberInvalidEndpointIndex)
+    {
+        return emberAfFindClusterInType(emAfEndpoints[index].endpointType, clusterId, mask, nullptr);
+    }
+    // not found
+    return nullptr;
+}
+
+EmberAfStatus setupDynamicEndpointDeclaration(EmberAfEndpointType & endpointType, EndpointId templateEndpointId,
+                                              const Span<const ClusterId> & templateClusterIds)
+{
+    // allocate cluster list
+    endpointType.clusterCount = static_cast<uint8_t>(templateClusterIds.size());
+    endpointType.cluster      = new EmberAfCluster[endpointType.clusterCount];
+    endpointType.endpointSize = 0;
+    // get the actual cluster pointers and sum up memory size
+    for (size_t i = 0; i < templateClusterIds.size(); i++)
+    {
+        auto cluster = getClusterTypeDefinition(templateEndpointId, templateClusterIds.data()[i], 0);
+        VerifyOrDieWithMsg(cluster, Support, "cluster 0x%04x template in endpoint %u does not exist",
+                           (unsigned int) templateClusterIds.data()[i], (unsigned int) templateEndpointId);
+        // for now, we need to copy the cluster definition, unfortunately.
+        // TODO: make endpointType use a pointer to a list of EmberAfCluster* instead, so we can re-use cluster definitions
+        //   instead of duplicating them here once for every instance.
+        memcpy((void *) &endpointType.cluster[i], cluster, sizeof(EmberAfCluster));
+        // sum up the needed storage
+        endpointType.endpointSize = (uint16_t) (endpointType.endpointSize + cluster->clusterSize);
+    }
+    return EMBER_ZCL_STATUS_SUCCESS;
+}
+
 CHIP_ERROR emberAfSetDynamicEndpoint(uint16_t index, EndpointId id, const EmberAfEndpointType * ep,
                                      const chip::Span<chip::DataVersion> & dataVersionStorage,
                                      chip::Span<const EmberAfDeviceType> deviceTypeList, EndpointId parentEndpointId,
@@ -302,7 +336,7 @@ CHIP_ERROR emberAfSetDynamicEndpoint(uint16_t index, EndpointId id, const EmberA
     emAfEndpoints[index].dataVersions   = dataVersionStorage.data();
     // Start the endpoint off as disabled.
     emAfEndpoints[index].bitmask.Clear(EmberAfEndpointOptions::isEnabled);
-    emAfEndpoints[index].parentEndpointId = parentEndpointId;
+    emAfEndpoints[index].parentEndpointId        = parentEndpointId;
     emAfEndpoints[index].dynamicAttributeStorage = dynamicAttributeStorage;
 
     emberAfSetDynamicEndpointCount(MAX_ENDPOINT_COUNT - FIXED_ENDPOINT_COUNT);
@@ -316,6 +350,16 @@ CHIP_ERROR emberAfSetDynamicEndpoint(uint16_t index, EndpointId id, const EmberA
             // Now what?  At least 0-init it.
             memset(dataVersionStorage.data(), 0, dataSize);
         }
+    }
+
+    if (dynamicAttributeStorage != nullptr && ep->endpointSize > 0)
+    {
+        // Flag the endpoint as enabled here, because otherwise loading attributes cannot work
+        emAfEndpoints[index].bitmask.Set(EmberAfEndpointOptions::isEnabled);
+        // Load attributes from NVstorage or set defaults
+        emAfLoadAttributeDefaults(id, false);
+        // set disabled again, so enabling below will detect a transition
+        emAfEndpoints[index].bitmask.Clear(EmberAfEndpointOptions::isEnabled);
     }
 
     // Now enable the endpoint.
@@ -650,8 +694,8 @@ Status emAfReadOrWriteAttribute(const EmberAfAttributeSearchRecord * attRecord, 
                                     (am->mask & ATTRIBUTE_MASK_SINGLETON
                                          ? singletonAttributeLocation(am)
                                          : (hasDynamicAttributeStorage ? emAfEndpoints[ep].dynamicAttributeStorage
-                                                                       : attributeData) + attributeStorageOffset);
-                                    ;
+                                                                       : attributeData) +
+                                             attributeStorageOffset);
 
                                 uint8_t *src, *dst;
                                 if (write)
