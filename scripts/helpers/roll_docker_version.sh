6082105470f5f3f5e2abecb2069d368612086d0d
Andrei Litvin <andreilitvin@google.com>
2023-08-04 12:56:48 -0400

    Allow roll script to not update docker images

--- scripts/helpers/roll_docker_version.sh
+++ scripts/helpers/roll_docker_version.sh
@@ -20,14 +20,18 @@
 #
 
 CURRENT_VERSION=$(sed 's/ .*//' ./integrations/docker/images/base/chip-build/version)
-NEXT_VERSION=$((CURRENT_VERSION + 1))
 ROLL_REASON=""
 ALL_ARGS=""
+NO_DOCKER=""
+NEXT_VERSION=""
 
 for i in "$@"; do
     case $i in
         -v=* | --version=*)
             NEXT_VERSION="${i#*=}"
+            ;;
+        --no-docker)
+            NO_DOCKER="yes"
             ;;
         -r=* | --reason=*)
             ROLL_REASON="${i#*=}"
@@ -38,31 +42,46 @@ for i in "$@"; do
     esac
 done
 
-if [[ ! $ROLL_REASON ]]; then
-    echo 'Roll reason required, please add --reason="Why I am rolling"'
+if [[ -z "$NO_DOCKER" ]]; then
+  if [[ ! $ROLL_REASON ]]; then
+    echo 'Roll reason required, please add --reason="Why I am rolling" or use --no-docker'
     exit -1
+  fi
+  if [[ -z "$NEXT_VERSION" ]]; then
+    NEXT_VERSION=$((CURRENT_VERSION + 1))
+  fi
+  echo "Current version: $CURRENT_VERSION"
+  echo "Next version: $NEXT_VERSION"
+  echo "Reason: $ROLL_REASON"
+else
+  if [[ -z "$NEXT_VERSION" ]]; then
+    NEXT_VERSION="$CURRENT_VERSION"
+  fi
+  echo "Updating to version: $NEXT_VERSION"
 fi
 
-echo "Current version: $CURRENT_VERSION"
-echo "Next version: $NEXT_VERSION"
-echo "Reason: $ROLL_REASON"
 echo ""
 
 echo "Rolling workflows"
 sed -r -i "s|image: ghcr\.io/project-chip/(.*):[0-9,a-z,A-Z,-]*(\..*)?|image: ghcr.io/project-chip/\1:$NEXT_VERSION|" .github/workflows/*.yaml
 sed -r -i "s|image: ghcr\.io/project-chip/(.*):[0-9,a-z,A-Z,-]*(\..*)?|image: ghcr.io/project-chip/\1:$NEXT_VERSION|" .github/workflows/*.yml
 
+echo "Rolling cloudbuild"
+sed -r -i "s|ghcr\.io/project-chip/(.*):[0-9,a-z,A-Z,-]*(\..*)?|image: ghcr.io/project-chip/\1:$NEXT_VERSION|" integrations/cloudbuild/*.yaml
+
 echo "Rolling VSCode container"
 sed -r -i "s|matter-dev-environment:local --version [0-9,a-z,A-Z,-]*|matter-dev-environment:local --version $NEXT_VERSION|" .devcontainer/devcontainer.json
 
-echo "Rolling docker images"
-find . -iname Dockerfile -not -path "./third_party/*" | xargs sed -r -i "s|ARG VERSION=[0-9,a-z,A-Z,-]*|ARG VERSION=$NEXT_VERSION|"
-
-echo "Rolling main docker version"
-echo "$NEXT_VERSION : $ROLL_REASON" >./integrations/docker/images/base/chip-build/version
-
 echo "Rolling README.md files"
 find . -iname readme.md -not -path "./third_party/*" | xargs sed -r -i "s|ghcr\.io/project-chip/(.*):[0-9,a-z,A-Z,-]*(\..*)?|ghcr.io/project-chip/\1:$NEXT_VERSION|"
+
+if [[ ! -z "$NO_DOCKER" ]]; then
+  echo "Rolling docker images"
+  find . -iname Dockerfile -not -path "./third_party/*" | xargs sed -r -i "s|ARG VERSION=[0-9,a-z,A-Z,-]*|ARG VERSION=$NEXT_VERSION|"
+
+  echo "Rolling main docker version"
+  echo "$NEXT_VERSION : $ROLL_REASON" >./integrations/docker/images/base/chip-build/version
+fi
 
 echo ""
 echo "Done"
