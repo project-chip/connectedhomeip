456e1a078f5c1b9489fc0fe07406e4f955f9afac
Jaehoon You <jaehoon.you@samsung.com>
2023-08-06 23:14:21 +0900

    virtual-device-app: Add initialize api for OnOffManager
    Signed-off-by: Jaehoon You <jaehoon.you@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>

--- examples/virtual-device-app/android/java/src/com/matter/virtual/device/app/OnOffManager.java
+++ examples/virtual-device-app/android/java/src/com/matter/virtual/device/app/OnOffManager.java
@@ -1,11 +1,18 @@
 package com.matter.virtual.device.app;
 
 public interface OnOffManager {
-  /**
-   * Notify that the OnOff value be changed by matter and should effect it. Note, set by TvApp will
-   * also trigger this function, so must check if value is same
-   *
-   * @param value
-   */
-  void HandleOnOffChanged(boolean value);
+
+    /**
+     * initialize OnOff value by DeviceApp
+     */
+    void initAttributeValue();
+
+    /**
+     * Notify that the OnOff value be changed by matter and should effect it. Note,
+     * set by DeviceApp will
+     * also trigger this function, so must check if value is same
+     *
+     * @param value
+     */
+    void handleOnOffChanged(boolean value);
 }
