c17657e628a8ac3c838d1f457a5d329d196fd3c0
Jaehoon You <jaehoon.you@samsung.com>
2023-08-07 00:07:59 +0900

    virtual-device-app: Add SharedPreferences feature for managing commissioning state
    Signed-off-by: Jaehoon You <jaehoon.you@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>

--- examples/virtual-device-app/android/App/core/data/src/main/java/com/matter/virtual/device/app/core/data/di/DataModule.kt
+++ examples/virtual-device-app/android/App/core/data/src/main/java/com/matter/virtual/device/app/core/data/di/DataModule.kt
@@ -1,9 +1,6 @@
 package com.matter.virtual.device.app.core.data.di
 
-import com.matter.virtual.device.app.core.data.repository.MatterRepository
-import com.matter.virtual.device.app.core.data.repository.MatterRepositoryImpl
-import com.matter.virtual.device.app.core.data.repository.NetworkRepository
-import com.matter.virtual.device.app.core.data.repository.NetworkRepositoryImpl
+import com.matter.virtual.device.app.core.data.repository.*
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,4 +13,9 @@ internal abstract class DataModule {
   @Binds abstract fun bindMatterRepository(repository: MatterRepositoryImpl): MatterRepository
 
   @Binds abstract fun bindNetworkRepository(repository: NetworkRepositoryImpl): NetworkRepository
+
+  @Binds
+  abstract fun bindSharedPreferencesRepository(
+    repository: SharedPreferencesRepositoryImpl
+  ): SharedPreferencesRepository
 }
