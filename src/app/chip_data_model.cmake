d4e1bca8b088cda599da7e0f6d8fb534345a53c7
Boris Zbarsky <bzbarsky@apple.com>
2023-08-07 09:21:36 -0400

    Remove special-case in ToInteractionModelStatus. (#28538)
    EMBER_ZCL_STATUS_DUPLICATE_EXISTS is now only returned from
    emberAfSetDynamicEndpoint, which is never on any IM codepaths.

--- src/app/chip_data_model.cmake
+++ src/app/chip_data_model.cmake
@@ -140,7 +140,6 @@ function(chip_configure_data_model APP_TARGET)
         ${CHIP_APP_BASE_DIR}/icd/IcdManagementServer.cpp
         ${CHIP_APP_BASE_DIR}/util/DataModelHandler.cpp
         ${CHIP_APP_BASE_DIR}/util/ember-compatibility-functions.cpp
-        ${CHIP_APP_BASE_DIR}/util/error-mapping.cpp
         ${CHIP_APP_BASE_DIR}/util/generic-callback-stubs.cpp
         ${CHIP_APP_BASE_DIR}/util/message.cpp
         ${CHIP_APP_BASE_DIR}/util/privilege-storage.cpp
