dbd8440a52d77e4ae7acf32a15ceb7892d407fb6
Sharad Binjola <sharadb@amazon.com>
2023-08-07 16:14:53 -0700

    Linux tv-casting-app: refactored Initialization

---
+++ examples/tv-casting-app/tv-casting-common/core/CastingApp.h
@@ -0,0 +1,83 @@
+/*
+ *
+ *    Copyright (c) 2023 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include "support/AppParameters.h"
+
+namespace matter {
+namespace casting {
+namespace app {
+
+/**
+ * @brief Represents CastingApp state.
+ *
+ */
+enum CastingAppState
+{
+    UNINITIALIZED, // Before Initialize() success
+    NOT_RUNNING,   // After Initialize() success before Start()ing, OR After stop() success
+    RUNNING,       // After Start() success
+};
+
+/**
+ * @brief CastingApp represents an app that can cast content to a Casting Player.
+ * This class is a singleton.
+ */
+class CastingApp
+{
+private:
+    CastingApp();
+
+    CastingApp(CastingApp & other) = delete;
+    void operator=(const CastingApp &) = delete;
+
+    const matter::casting::support::AppParameters * mAppParameters;
+
+    CastingAppState mState = UNINITIALIZED;
+
+public:
+    static CastingApp * castingApp_;
+    static CastingApp * GetInstance();
+
+    /**
+     * @brief Initializes the CastingApp with appParameters
+     *
+     * @param appParameters
+     * @return CHIP_ERROR
+     */
+    CHIP_ERROR Initialize(const matter::casting::support::AppParameters & appParameters);
+
+    /**
+     * @brief Starts the Matter server that the CastingApp runs on
+     *
+     * @return CHIP_ERROR - CHIP_NO_ERROR if Matter server started successfully, specific error code otherwise.
+     */
+    CHIP_ERROR Start();
+
+    /**
+     * @brief Stops the Matter server that the CastingApp runs on
+     *
+     * @return CHIP_ERROR - CHIP_NO_ERROR if Matter server stopped successfully, specific error code otherwise.
+     */
+    CHIP_ERROR Stop();
+};
+
+}; // namespace app
+}; // namespace casting
+}; // namespace matter
