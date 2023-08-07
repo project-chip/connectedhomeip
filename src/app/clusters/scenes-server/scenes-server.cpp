3564e531e1832e85003532f72176f4cd0a26cb3b
Marianne Primeau Breton <marianne.breton@silabs.com>
2023-08-07 14:03:28 -0400

    Validate if the Current Group = RemovedGroup or check if CurrentScene is in RemovedGroup before invalidating the scene.

--- src/app/clusters/scenes-server/scenes-server.cpp
+++ src/app/clusters/scenes-server/scenes-server.cpp
@@ -510,7 +510,19 @@ void ScenesServer::GroupWillBeRemoved(FabricIndex aFabricIx, EndpointId aEndpoin
     SceneTable * sceneTable = scenes::GetSceneTableImpl(aEndpointId);
     VerifyOrReturn(nullptr != sceneTable);
 
-    MakeSceneInvalid(aEndpointId);
+    chip::GroupId * currentGroup;
+    Attributes::CurrentGroup::Get(aEndpointId, currentGroup);
+
+    chip::SceneId * currentScene;
+    Attributes::CurrentScene::Get(aEndpointId,currentScene);
+
+    //Validate if the Current Group = RemovedGroup or check if CurrentScene is in RemovedGroup before invalidating the scene.
+    SceneTableEntry entry;
+    if(aGroupId == *currentGroup || sceneTable->GetSceneTableEntry(aFabricIx,SceneStorageId(*currentScene,*currentGroup),entry) == CHIP_NO_ERROR)
+    {
+        MakeSceneInvalid(aEndpointId);
+    }
+
 
     VerifyOrReturn(nullptr != mGroupProvider);
     if (0 != aGroupId && !mGroupProvider->HasEndpoint(aFabricIx, aGroupId, aEndpointId))
