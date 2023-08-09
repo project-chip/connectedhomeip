fc044751da910f4608fd08e26d838b81e5cd94a3
Boris Zbarsky <bzbarsky@apple.com>
2023-08-09 00:58:44 -0400

    Reduce the amount of code generated for MTRClusters. (#28594)
    1. Push endpoint property up to MTRCluster, instead of having it be on every
       MTRCluster* and MTRBaseCluster* instance.
    2. Instead of duplicating all the command bridge code for MTRCluster* commands,
       just instantiate the relevant MTRBaseCluster and delegate to it.

--- src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters_Internal.h
+++ src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters_Internal.h
@@ -22,465 +22,372 @@
 
 @interface MTRBaseClusterIdentify ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterGroups ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterScenes ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOnOff ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOnOffSwitchConfiguration ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterLevelControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBinaryInputBasic ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPulseWidthModulation ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterDescriptor ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBinding ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterAccessControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterActions ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBasicInformation ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOTASoftwareUpdateProvider ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOTASoftwareUpdateRequestor ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterLocalizationConfiguration ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTimeFormatLocalization ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterUnitLocalization ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPowerSourceConfiguration ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPowerSource ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterGeneralCommissioning ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterNetworkCommissioning ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterDiagnosticLogs ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterGeneralDiagnostics ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterSoftwareDiagnostics ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterThreadNetworkDiagnostics ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterWiFiNetworkDiagnostics ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterEthernetNetworkDiagnostics ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTimeSynchronization ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBridgedDeviceBasicInformation ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterSwitch ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterAdministratorCommissioning ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOperationalCredentials ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterGroupKeyManagement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterFixedLabel ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterUserLabel ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBooleanState ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterICDManagement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterModeSelect ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterLaundryWasherMode ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRefrigeratorAndTemperatureControlledCabinetMode ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterLaundryWasherControls ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRVCRunMode ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRVCCleanMode ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTemperatureControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRefrigeratorAlarm ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterDishwasherMode ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterAirQuality ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterSmokeCOAlarm ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterDishwasherAlarm ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOperationalState ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRVCOperationalState ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterHEPAFilterMonitoring ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterActivatedCarbonFilterMonitoring ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterDoorLock ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterWindowCovering ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBarrierControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPumpConfigurationAndControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterThermostat ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterFanControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterThermostatUserInterfaceConfiguration ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterColorControl ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterBallastConfiguration ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterIlluminanceMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTemperatureMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPressureMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterFlowMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRelativeHumidityMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOccupancySensing ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterCarbonMonoxideConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterCarbonDioxideConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterNitrogenDioxideConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterOzoneConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPM25ConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterFormaldehydeConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPM1ConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterPM10ConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTotalVolatileOrganicCompoundsConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterRadonConcentrationMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterWakeOnLAN ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterChannel ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterTargetNavigator ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterMediaPlayback ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterMediaInput ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterLowPower ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterKeypadInput ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterContentLauncher ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterAudioOutput ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterApplicationLauncher ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterApplicationBasic ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterAccountLogin ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterElectricalMeasurement ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
 
 @interface MTRBaseClusterUnitTesting ()
 @property (nonatomic, strong, readonly) MTRBaseDevice * device;
-@property (nonatomic, assign, readonly) chip::EndpointId endpoint;
 @end
