a925566d8ccea501d4cc003ae58218fd8a31a9e9
Graf Tobias <4622393+tobiasgraf@users.noreply.github.com>
2023-08-15 16:15:50 +0200

    Rename "resource-monitoring-instances.h/cpp" To "resource-monitoring-delegates.h/cpp"

--- examples/all-clusters-app/all-clusters-common/src/resource-monitoring-delegates.cpp
+++ examples/all-clusters-app/all-clusters-common/src/resource-monitoring-delegates.cpp
@@ -19,7 +19,7 @@
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/clusters/resource-monitoring-server/resource-monitoring-cluster-objects.h>
 #include <app/clusters/resource-monitoring-server/resource-monitoring-server.h>
-#include <resource-monitoring-instances.h>
+#include <resource-monitoring-delegates.h>
 
 using namespace chip;
 using namespace chip::app;
