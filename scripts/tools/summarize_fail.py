16d2ce8122ca61f5b44ba25676da308577f354d1
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2023-08-17 14:01:02 -0600

    Update summarize_fail.py

--- scripts/tools/summarize_fail.py
+++ scripts/tools/summarize_fail.py
@@ -3,8 +3,8 @@ import pandas as pd
 df = pd.read_json("run_list.json")
 df.columns = ["Pull Request", "Workflow"]
 print("Recent Failures:")
-print(df.to_string(index = False))
-df.to_csv("recent_fails.csv", index = False)
+print(df.to_string(index=False))
+df.to_csv("recent_fails.csv", index=False)
 print()
 print("Percentage Frequency:")
 frequency = df["Workflow"].value_counts(normalize=True).mul(100).astype(str) + "%"
