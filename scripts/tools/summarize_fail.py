c51ede47d53fe8baeffe4cf2651f4c4739ee0b40
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2023-08-17 13:20:49 -0600

    Update summarize_fail.py

--- scripts/tools/summarize_fail.py
+++ scripts/tools/summarize_fail.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 df = pd.read_json("runlist.json")
+df.columns = ["Pull Request", "Workflow"]
 print("Recent Failures:")
-print(df)
+print(df["Pull Request", "Workflow"])
+print()
 print("Percentage Frequency:")
 print(df["workflowName"].value_counts(normalize=True) * 100)
