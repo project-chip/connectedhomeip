f0825b2b19e7f728b1c2e75d629ec415e1905940
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2023-08-17 13:52:19 -0600

    Update summarize_fail.py

--- scripts/tools/summarize_fail.py
+++ scripts/tools/summarize_fail.py
@@ -7,6 +7,6 @@ print(df.to_string(index = False))
 df.to_csv("recent_fails.csv", index = False)
 print()
 print("Percentage Frequency:")
-frequency = df["Workflow"].value_counts(normalize=True).mul(100).round().astype(str) + "%"
+frequency = df["Workflow"].value_counts(normalize=True).mul(100).astype(str) + "%"
 print(frequency.to_string())
 frequency.to_csv("recent_fails_frequency.csv")
