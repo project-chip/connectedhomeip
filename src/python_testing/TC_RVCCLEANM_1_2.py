7f1dd53a6dff22a2b14619de5770de3b123c2c5d
Petru Lauric <plauric@irobot.com>
2023-08-18 20:34:03 +0300

    fix trailing whitespace

--- src/python_testing/TC_RVCCLEANM_1_2.py
+++ src/python_testing/TC_RVCCLEANM_1_2.py
@@ -53,7 +53,7 @@ class TC_RVCCLEANM_1_2(MatterBaseTest):
             logging.info("SupportedModes: %s" % (supported_modes))
 
             asserts.assert_greater_equal(len(supported_modes), 2, "SupportedModes must have at least 2 entries!")
-            asserts.assert_less_equal(len(supported_modes), 255, "SupportedModes must have at most 255 entries!")                  
+            asserts.assert_less_equal(len(supported_modes), 255, "SupportedModes must have at most 255 entries!")
 
             modes = []
             for m in supported_modes:
