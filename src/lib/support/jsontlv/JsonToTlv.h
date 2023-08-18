512f6316022f20ca0b5052baeb99932e909fd796
yunhanw-google <yunhanw@google.com>
2023-08-18 15:32:41 -0700

    Update src/lib/support/jsontlv/JsonToTlv.h
    Co-authored-by: saurabhst <s.kumar9@samsung.com>

--- src/lib/support/jsontlv/JsonToTlv.h
+++ src/lib/support/jsontlv/JsonToTlv.h
@@ -33,7 +33,11 @@ CHIP_ERROR JsonToTlv(const std::string & jsonString, MutableByteSpan & tlv);
 CHIP_ERROR JsonToTlv(const std::string & jsonString, TLV::TLVWriter & writer);
 
 /*
- * Given a uint64_t tagNumber, transform it into a tlv tag. When tagNumber is less than UINT8_MAX, encode the tag using ContextTag, when tagNumber is
+ /*
+ * Convert a uint64_t tagNumber to a TLV tag. When tagNumber is less than UINT8_MAX,
+ * the tag is encoded using ContextTag. When tagNumber is less than UINT32_MAX,
+ * the tag is encoded using an implicit profile tag.
+ */
  * less than uint32_t, encode the tag using an implicit profile tag.
  */
  * Given a uint64_t tagNumber, and convert it a tlv tag. when tagNumber is less than UINT8_MAX, tag is encoded with ContextTag, when tagNumber is
