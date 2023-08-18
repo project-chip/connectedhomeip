08a2da5959379eb24fb35eb8ffad76e610650c79
Restyled.io <commits@restyled.io>
2023-08-18 13:43:12 +0000

    Restyled by clang-format

--- examples/platform/silabs/SiWx917/SiWx917/hal/rsi_hal_mcu_m4.c
+++ examples/platform/silabs/SiWx917/SiWx917/hal/rsi_hal_mcu_m4.c
@@ -82,7 +82,7 @@ void IRQ021_Handler(void)
     RSI_NPSSGPIO_ClrIntr(NPSS_GPIO_0_INTR);
     RSI_NPSSGPIO_ClrIntr(NPSS_GPIO_2_INTR);
     // if the btn is not pressed setting the state to 1
-    if (RSI_NPSSGPIO_GetPin(NPSS_GPIO_2)  && (!btn1))
+    if (RSI_NPSSGPIO_GetPin(NPSS_GPIO_2) && (!btn1))
     {
         btn1 = 1;
         sl_button_on_change(1, 0);
