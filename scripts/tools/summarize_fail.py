30e6530bd24367fb66b1ab6f6c49cbb834a019f4
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2023-08-22 16:08:11 -0600

    Update summarize_fail.py

--- scripts/tools/summarize_fail.py
+++ scripts/tools/summarize_fail.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import subprocess
 
-subprocess.run("gh run list -R project-chip/connectedhomeip -b master -s failure --json displayTitle,workflowName > run_list.json", shell=True)
+subprocess.run("gh run list -R project-chip/connectedhomeip -b master -s failure --json databaseId,displayTitle,workflowName > run_list.json", shell=True)
 df = pd.read_json("run_list.json")
-df.columns = ["Pull Request", "Workflow"]
+df.columns = ["ID", "Pull Request", "Workflow"]
 print("Recent Failures:")
 print(df.to_string(index=False))
 df.to_csv("recent_fails.csv", index=False)
@@ -12,3 +12,6 @@ print("Percentage Frequency:")
 frequency = df["Workflow"].value_counts(normalize=True).mul(100).astype(str) + "%"
 print(frequency.to_string())
 frequency.to_csv("recent_fails_frequency.csv")
+print()
+for run in df["ID"].tolist():
+  print(run)
