d986a02195ff268e0dc9dc960c68d830346d6e59
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2023-08-23 12:30:25 -0600

    Update summarize_fail.py

--- scripts/tools/summarize_fail.py
+++ scripts/tools/summarize_fail.py
@@ -1,17 +1,18 @@
 import logging
 import os
 import pandas as pd
+from slugify import slugify
 import subprocess
 
-def process_fail(id, pr, workflow):
-    os.makedirs(f"logs/{pr}/{workflow}")
-    subprocess.run(f"gh run view -R project-chip/connectedhomeip {id} --log-failed > logs/{pr}/{workflow}/fail_logs.txt", shell=True)
+def process_fail(id, pr, workflow, start_time):
+    output_path = f"logs/{slugify(pr)}/{slugify(workflow)}/{slugify(start_time)}"
+    subprocess.run(f"gh run view -R project-chip/connectedhomeip {id} --log-failed > {output_path}/fail_logs.txt", shell=True)
 
 def main():
     logging.info("Gathering recent run failure information into run_list.json.")
-    subprocess.run("gh run list -R project-chip/connectedhomeip -b master -s failure --json databaseId,displayTitle,workflowName > run_list.json", shell=True)
+    subprocess.run("gh run list -R project-chip/connectedhomeip -b master -s failure --json databaseId,displayTitle,workflowName,startedAt > run_list.json", shell=True)
     df = pd.read_json("run_list.json")
-    df.columns = ["ID", "Pull Request", "Workflow"]
+    df.columns = ["ID", "Pull Request", "Workflow", "Start Time"]
     logging.info("Recent Failures:")
     logging.info(df.to_string(index=False))
     df.to_csv("recent_fails.csv", index=False)
@@ -19,7 +20,7 @@ def main():
     frequency = df["Workflow"].value_counts(normalize=True).mul(100).astype(str).reset_index(name="Percentage")
     logging.info(frequency.to_string(index=False))
     frequency.to_csv("recent_workflow_fails_frequency.csv")
-    df.apply(lambda row: process_fail(row["ID"], row["Pull Request"], row["Workflow"]), axis=1)
+    df.apply(lambda row: process_fail(row["ID"], row["Pull Request"], row["Workflow"], row["Start Time"]), axis=1)
 
 if __name__ == "__main__":
     main()
