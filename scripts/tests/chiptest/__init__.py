095c5947a602e3c60fcafc5a9aa9f5c6172e04cc
manjunath-grl <manjunath.grl@outlook.com>
2023-08-25 18:13:37 +0530

    Test_TC_G_2_4.yaml test added in init.py as the chip-repl does not support EqualityCommands pseudo-cluster

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -173,6 +173,7 @@ def _GetChipReplUnsupportedTests() -> Set[str]:
         "Test_TC_DGGEN_2_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_DGGEN_2_3.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_LWM_3_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
+        "Test_TC_G_2_4.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
     }
 
 
