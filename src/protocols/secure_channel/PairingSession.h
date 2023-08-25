project-chip/connectedhomeip

commit 59a0b2f67e0827f41bae97ef9eec78c4dc171a7a

CASE: Send busy status report if we receive a sigma1 and we are in the middle of handshake (#28153)
* CASE: Send busy status report if we receive a sigma1 and we are in the
middle of handshake

* Apply suggestions from code review

Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

* Addressing review comments

* few more review comments

* Addressed reviews and added the unit tests

* Restyled by clang-format

* Few more review comments

* Apply suggestions from code review

Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

* Moved todo to better place

---------

Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
Co-authored-by: Restyled.io <commits@restyled.io>
Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

src/protocols/secure_channel/PairingSession.h
@@ -174,6 +174,29 @@ protected:
         else
         {
             err = OnFailureStatusReport(report.GetGeneralCode(), report.GetProtocolCode());
+
+            if (report.GetGeneralCode() == Protocols::SecureChannel::GeneralStatusCode::kBusy &&
+                report.GetProtocolCode() == Protocols::SecureChannel::kProtocolCodeBusy)
+            {
+                if (!report.GetProtocolData().IsNull())
+                {
+                    Encoding::LittleEndian::Reader reader(report.GetProtocolData()->Start(),
+                                                          report.GetProtocolData()->DataLength());
+
+                    uint16_t minimumWaitTime = 0;
+                    err                      = reader.Read16(&minimumWaitTime).StatusCode();
+                    if (err != CHIP_NO_ERROR)
+                    {
+                        ChipLogError(SecureChannel, "Failed to read the minimum wait time: %" CHIP_ERROR_FORMAT, err.Format());
+                    }
+                    else
+                    {
+                        // TODO: CASE: Notify minimum wait time to clients on receiving busy status report #28290
+                        ChipLogProgress(SecureChannel, "Received busy status report with minimum wait time: %u ms",
+                                        minimumWaitTime);
+                    }
+                }
+            }
         }
 
         return err;

src/protocols/secure_channel/CASEServer.cpp
@@ -84,10 +84,17 @@ CHIP_ERROR CASEServer::OnMessageReceived(Messaging::ExchangeContext * ec, const 
         bool watchdogFired = GetSession().InvokeBackgroundWorkWatchdog();
         if (!watchdogFired)
         {
-            // Handshake wasn't stuck, let it continue its work
-            // TODO: Send Busy response, #27473
-            ChipLogError(Inet, "CASE session is in establishing state, returning without responding");
-            return CHIP_NO_ERROR;
+            // Handshake wasn't stuck, send the busy status report and let the existing handshake continue.
+
+            // A successful CASE handshake can take several seconds and some may time out (30 seconds or more).
+            // TODO: Come up with better estimate: https://github.com/project-chip/connectedhomeip/issues/28288
+            // For now, setting minimum wait time to 5000 milliseconds.
+            CHIP_ERROR err = SendBusyStatusReport(ec, System::Clock::Milliseconds16(5000));
+            if (err != CHIP_NO_ERROR)
+            {
+                ChipLogError(Inet, "Failed to send the busy status report, err:%" CHIP_ERROR_FORMAT, err.Format());
+            }
+            return err;
         }
     }
 
@@ -181,4 +188,16 @@ void CASEServer::OnSessionEstablished(const SessionHandle & session)
                     ChipLogValueScopedNodeId(session->GetPeer()));
     PrepareForSessionEstablishment(session->GetPeer());
 }
+
+CHIP_ERROR CASEServer::SendBusyStatusReport(Messaging::ExchangeContext * ec, System::Clock::Milliseconds16 minimumWaitTime)
+{
+    ChipLogProgress(Inet, "Already in the middle of CASE handshake, sending busy status report");
+
+    System::PacketBufferHandle handle = Protocols::SecureChannel::StatusReport::MakeBusyStatusReportMessage(minimumWaitTime);
+    VerifyOrReturnError(!handle.IsNull(), CHIP_ERROR_NO_MEMORY);
+
+    ChipLogProgress(Inet, "Sending status report, exchange " ChipLogFormatExchange, ChipLogValueExchange(ec));
+    return ec->SendMessage(Protocols::SecureChannel::MsgType::StatusReport, std::move(handle));
+}
+
 } // namespace chip

src/protocols/secure_channel/CASEServer.h
@@ -22,6 +22,7 @@
 #include <messaging/ExchangeDelegate.h>
 #include <messaging/ExchangeMgr.h>
 #include <protocols/secure_channel/CASESession.h>
+#include <system/SystemClock.h>
 
 namespace chip {
 
@@ -107,6 +108,13 @@ private:
      *
      */
     void PrepareForSessionEstablishment(const ScopedNodeId & previouslyEstablishedPeer = ScopedNodeId());
+
+    // If we are in the middle of handshake and receive a Sigma1 then respond with Busy status code.
+    // @param[in] ec              Exchange Context
+    // @param[in] minimumWaitTime Minimum wait time reported to client before it can attempt to resend sigma1
+    //
+    // @return CHIP_NO_ERROR on success, error code otherwise
+    CHIP_ERROR SendBusyStatusReport(Messaging::ExchangeContext * ec, System::Clock::Milliseconds16 minimumWaitTime);
 };
 
 } // namespace chip

src/protocols/secure_channel/CASESession.cpp
@@ -1977,6 +1977,10 @@ CHIP_ERROR CASESession::OnFailureStatusReport(Protocols::SecureChannel::GeneralS
         err = CHIP_ERROR_NO_SHARED_TRUSTED_ROOT;
         break;
 
+    case kProtocolCodeBusy:
+        err = CHIP_ERROR_BUSY;
+        break;
+
     default:
         err = CHIP_ERROR_INTERNAL;
         break;

src/protocols/secure_channel/StatusReport.cpp
@@ -95,6 +95,34 @@ size_t StatusReport::Size() const
     return WriteToBuffer(emptyBuf).Needed();
 }
 
+System::PacketBufferHandle StatusReport::MakeBusyStatusReportMessage(System::Clock::Milliseconds16 minimumWaitTime)
+{
+    using namespace Protocols::SecureChannel;
+    constexpr uint8_t kBusyStatusReportProtocolDataSize = sizeof(minimumWaitTime.count()); // 16-bits
+
+    auto handle = System::PacketBufferHandle::New(kBusyStatusReportProtocolDataSize, 0);
+    VerifyOrReturnValue(!handle.IsNull(), handle,
+                        ChipLogError(SecureChannel, "Failed to allocate protocol data for busy status report"));
+
+    // Build the protocol data with minimum wait time
+    Encoding::LittleEndian::PacketBufferWriter protocolDataBufferWriter(std::move(handle));
+    protocolDataBufferWriter.Put16(minimumWaitTime.count());
+    handle = protocolDataBufferWriter.Finalize();
+    VerifyOrReturnValue(!handle.IsNull(), handle,
+                        ChipLogError(SecureChannel, "Failed to finalize protocol data for busy status report"));
+
+    // Build a busy status report
+    StatusReport statusReport(GeneralStatusCode::kBusy, Protocols::SecureChannel::Id, kProtocolCodeBusy, std::move(handle));
+
+    // Build the status report message
+    handle = System::PacketBufferHandle::New(statusReport.Size());
+    VerifyOrReturnValue(!handle.IsNull(), handle, ChipLogError(SecureChannel, "Failed to allocate status report message"));
+    Encoding::LittleEndian::PacketBufferWriter bbuf(std::move(handle));
+
+    statusReport.WriteToBuffer(bbuf);
+    return bbuf.Finalize();
+}
+
 } // namespace SecureChannel
 } // namespace Protocols
 } // namespace chip

src/protocols/secure_channel/StatusReport.h
@@ -21,6 +21,7 @@
 #include <lib/support/BufferWriter.h>
 #include <protocols/Protocols.h>
 #include <protocols/secure_channel/Constants.h>
+#include <system/SystemClock.h>
 #include <system/SystemPacketBuffer.h>
 
 namespace chip {
@@ -93,6 +94,15 @@ public:
     Protocols::Id GetProtocolId() const { return mProtocolId; }
     uint16_t GetProtocolCode() const { return mProtocolCode; }
     const System::PacketBufferHandle & GetProtocolData() const { return mProtocolData; }
+
+    /**
+     * Builds a busy status report with protocol data containing the minimum wait time.
+     *
+     * @param[in] minimumWaitTime Time in milliseconds before initiator retries the request
+     *
+     * @return Packet buffer handle which can be passed to SendMessage.
+     */
+    static System::PacketBufferHandle MakeBusyStatusReportMessage(System::Clock::Milliseconds16 minimumWaitTime);
 
 private:
     GeneralStatusCode mGeneralCode;

src/protocols/secure_channel/tests/TestStatusReport.cpp
@@ -16,6 +16,7 @@
  *    limitations under the License.
  */
 
+#include <lib/support/BufferReader.h>
 #include <lib/support/BufferWriter.h>
 #include <lib/support/CHIPMem.h>
 #include <lib/support/UnitTestRegistration.h>
@@ -104,6 +105,33 @@ void TestBadStatusReport(nlTestSuite * inSuite, void * inContext)
     NL_TEST_ASSERT(inSuite, err != CHIP_NO_ERROR);
 }
 
+void TestMakeBusyStatusReport(nlTestSuite * inSuite, void * inContext)
+{
+    GeneralStatusCode generalCode                 = GeneralStatusCode::kBusy;
+    auto protocolId                               = SecureChannel::Id;
+    uint16_t protocolCode                         = kProtocolCodeBusy;
+    System::Clock::Milliseconds16 minimumWaitTime = System::Clock::Milliseconds16(5000);
+
+    System::PacketBufferHandle handle = StatusReport::MakeBusyStatusReportMessage(minimumWaitTime);
+    NL_TEST_ASSERT(inSuite, !handle.IsNull());
+
+    StatusReport reportToParse;
+    CHIP_ERROR err = reportToParse.Parse(std::move(handle));
+    NL_TEST_ASSERT(inSuite, CHIP_NO_ERROR == err);
+    NL_TEST_ASSERT(inSuite, reportToParse.GetGeneralCode() == generalCode);
+    NL_TEST_ASSERT(inSuite, reportToParse.GetProtocolId() == protocolId);
+    NL_TEST_ASSERT(inSuite, reportToParse.GetProtocolCode() == protocolCode);
+
+    const System::PacketBufferHandle & rcvData = reportToParse.GetProtocolData();
+    NL_TEST_ASSERT(inSuite, !rcvData.IsNull());
+    NL_TEST_ASSERT(inSuite, rcvData->DataLength() == sizeof(minimumWaitTime));
+
+    uint16_t readMinimumWaitTime = 0;
+    Encoding::LittleEndian::Reader reader(rcvData->Start(), rcvData->DataLength());
+    NL_TEST_ASSERT(inSuite, CHIP_NO_ERROR == reader.Read16(&readMinimumWaitTime).StatusCode());
+    NL_TEST_ASSERT(inSuite, System::Clock::Milliseconds16(readMinimumWaitTime) == minimumWaitTime);
+}
+
 // Test Suite
 
 /**
@@ -115,6 +143,7 @@ static const nlTest sTests[] =
     NL_TEST_DEF("TestStatusReport_NoData", TestStatusReport_NoData),
     NL_TEST_DEF("TestStatusReport_WithData", TestStatusReport_WithData),
     NL_TEST_DEF("TestBadStatusReport", TestBadStatusReport),
+    NL_TEST_DEF("TestMakeBusyStatusReport", TestMakeBusyStatusReport),
 
     NL_TEST_SENTINEL()
 };

commit 8b09af7817a943f511256b59b59a84553db7f0d7

Make sure we handle BUSY responses during CASE properly.
If we got a BUSY response during a CASE handshake and successfully read the wait
time, we would treat that as a success case, not a failure case, and not realize
that our exchange has been closed.  That could lead to use-after-free when we
later tried to abort an already-closed exchange.

The problem was introduced in 59a0b2f67e0827f41bae97ef9eec78c4dc171a7a
(PR #28153).

The new unit test fails (both with ASAN failures and with incorrect state
because the client that got BUSY does not think the handshake failed) without
this fix.

src/protocols/secure_channel/PairingSession.h
@@ -162,44 +162,40 @@ protected:
     CHIP_ERROR HandleStatusReport(System::PacketBufferHandle && msg, bool successExpected)
     {
         Protocols::SecureChannel::StatusReport report;
-        CHIP_ERROR err = report.Parse(std::move(msg));
-        ReturnErrorOnFailure(err);
+        ReturnErrorOnFailure(report.Parse(std::move(msg)));
         VerifyOrReturnError(report.GetProtocolId() == Protocols::SecureChannel::Id, CHIP_ERROR_INVALID_ARGUMENT);
 
         if (report.GetGeneralCode() == Protocols::SecureChannel::GeneralStatusCode::kSuccess &&
             report.GetProtocolCode() == Protocols::SecureChannel::kProtocolCodeSuccess && successExpected)
         {
             OnSuccessStatusReport();
+            return CHIP_NO_ERROR;
         }
-        else
+
+        if (report.GetGeneralCode() == Protocols::SecureChannel::GeneralStatusCode::kBusy &&
+            report.GetProtocolCode() == Protocols::SecureChannel::kProtocolCodeBusy)
         {
-            err = OnFailureStatusReport(report.GetGeneralCode(), report.GetProtocolCode());
-
-            if (report.GetGeneralCode() == Protocols::SecureChannel::GeneralStatusCode::kBusy &&
-                report.GetProtocolCode() == Protocols::SecureChannel::kProtocolCodeBusy)
+            if (!report.GetProtocolData().IsNull())
             {
-                if (!report.GetProtocolData().IsNull())
-                {
-                    Encoding::LittleEndian::Reader reader(report.GetProtocolData()->Start(),
-                                                          report.GetProtocolData()->DataLength());
+                Encoding::LittleEndian::Reader reader(report.GetProtocolData()->Start(), report.GetProtocolData()->DataLength());
 
-                    uint16_t minimumWaitTime = 0;
-                    err                      = reader.Read16(&minimumWaitTime).StatusCode();
-                    if (err != CHIP_NO_ERROR)
-                    {
-                        ChipLogError(SecureChannel, "Failed to read the minimum wait time: %" CHIP_ERROR_FORMAT, err.Format());
-                    }
-                    else
-                    {
-                        // TODO: CASE: Notify minimum wait time to clients on receiving busy status report #28290
-                        ChipLogProgress(SecureChannel, "Received busy status report with minimum wait time: %u ms",
-                                        minimumWaitTime);
-                    }
+                uint16_t minimumWaitTime = 0;
+                CHIP_ERROR waitTimeErr   = reader.Read16(&minimumWaitTime).StatusCode();
+                if (waitTimeErr != CHIP_NO_ERROR)
+                {
+                    ChipLogError(SecureChannel, "Failed to read the minimum wait time: %" CHIP_ERROR_FORMAT, waitTimeErr.Format());
+                }
+                else
+                {
+                    // TODO: CASE: Notify minimum wait time to clients on receiving busy status report #28290
+                    ChipLogProgress(SecureChannel, "Received busy status report with minimum wait time: %u ms", minimumWaitTime);
                 }
             }
         }
 
-        return err;
+        // It's very important that we propagate the return value from
+        // OnFailureStatusReport out to the caller.  Make sure we return it directly.
+        return OnFailureStatusReport(report.GetGeneralCode(), report.GetProtocolCode());
     }
 
     /**

src/protocols/secure_channel/tests/TestCASESession.cpp
@@ -122,7 +122,14 @@ CHIP_ERROR InitFabricTable(chip::FabricTable & fabricTable, chip::TestPersistent
 class TestCASESecurePairingDelegate : public SessionEstablishmentDelegate
 {
 public:
-    void OnSessionEstablishmentError(CHIP_ERROR error) override { mNumPairingErrors++; }
+    void OnSessionEstablishmentError(CHIP_ERROR error) override
+    {
+        mNumPairingErrors++;
+        if (error == CHIP_ERROR_BUSY)
+        {
+            mNumBusyResponses++;
+        }
+    }
 
     void OnSessionEstablished(const SessionHandle & session) override
     {
@@ -137,6 +144,7 @@ public:
     // TODO: Rename mNumPairing* to mNumEstablishment*
     uint32_t mNumPairingErrors   = 0;
     uint32_t mNumPairingComplete = 0;
+    uint32_t mNumBusyResponses   = 0;
 };
 
 class TestOperationalKeystore : public chip::Crypto::OperationalKeystore
@@ -314,6 +322,7 @@ public:
     static void SecurePairingStartTest(nlTestSuite * inSuite, void * inContext);
     static void SecurePairingHandshakeTest(nlTestSuite * inSuite, void * inContext);
     static void SecurePairingHandshakeServerTest(nlTestSuite * inSuite, void * inContext);
+    static void ClientReceivesBusyTest(nlTestSuite * inSuite, void * inContext);
     static void Sigma1ParsingTest(nlTestSuite * inSuite, void * inContext);
     static void DestinationIdTest(nlTestSuite * inSuite, void * inContext);
     static void SessionResumptionStorage(nlTestSuite * inSuite, void * inContext);
@@ -536,6 +545,58 @@ void TestCASESession::SecurePairingHandshakeServerTest(nlTestSuite * inSuite, vo
 
     chip::Platform::Delete(pairingCommissioner);
     chip::Platform::Delete(pairingCommissioner1);
+
+    gPairingServer.Shutdown();
+}
+
+void TestCASESession::ClientReceivesBusyTest(nlTestSuite * inSuite, void * inContext)
+{
+    TestContext & ctx = *reinterpret_cast<TestContext *>(inContext);
+    TemporarySessionManager sessionManager(inSuite, ctx);
+
+    TestCASESecurePairingDelegate delegateCommissioner1, delegateCommissioner2;
+    CASESession pairingCommissioner1, pairingCommissioner2;
+
+    pairingCommissioner1.SetGroupDataProvider(&gCommissionerGroupDataProvider);
+    pairingCommissioner2.SetGroupDataProvider(&gCommissionerGroupDataProvider);
+
+    auto & loopback            = ctx.GetLoopback();
+    loopback.mSentMessageCount = 0;
+
+    NL_TEST_ASSERT(inSuite,
+                   gPairingServer.ListenForSessionEstablishment(&ctx.GetExchangeManager(), &ctx.GetSecureSessionManager(),
+                                                                &gDeviceFabrics, nullptr, nullptr,
+                                                                &gDeviceGroupDataProvider) == CHIP_NO_ERROR);
+
+    ExchangeContext * contextCommissioner1 = ctx.NewUnauthenticatedExchangeToBob(&pairingCommissioner1);
+    ExchangeContext * contextCommissioner2 = ctx.NewUnauthenticatedExchangeToBob(&pairingCommissioner2);
+
+    NL_TEST_ASSERT(inSuite,
+                   pairingCommissioner1.EstablishSession(sessionManager, &gCommissionerFabrics,
+                                                         ScopedNodeId{ Node01_01, gCommissionerFabricIndex }, contextCommissioner1,
+                                                         nullptr, nullptr, &delegateCommissioner1, NullOptional) == CHIP_NO_ERROR);
+    NL_TEST_ASSERT(inSuite,
+                   pairingCommissioner2.EstablishSession(sessionManager, &gCommissionerFabrics,
+                                                         ScopedNodeId{ Node01_01, gCommissionerFabricIndex }, contextCommissioner2,
+                                                         nullptr, nullptr, &delegateCommissioner2, NullOptional) == CHIP_NO_ERROR);
+
+    ServiceEvents(ctx);
+
+    // We should have one full handshake and one Sigma1 + Busy + ack.  If that
+    // ever changes (e.g. because our server starts supporting multiple parallel
+    // handshakes), this test needs to be fixed so that the server is still
+    // responding BUSY to the client.
+    NL_TEST_ASSERT(inSuite, loopback.mSentMessageCount == sTestCaseMessageCount + 3);
+    NL_TEST_ASSERT(inSuite, delegateCommissioner1.mNumPairingComplete == 1);
+    NL_TEST_ASSERT(inSuite, delegateCommissioner2.mNumPairingComplete == 0);
+
+    NL_TEST_ASSERT(inSuite, delegateCommissioner1.mNumPairingErrors == 0);
+    NL_TEST_ASSERT(inSuite, delegateCommissioner2.mNumPairingErrors == 1);
+
+    NL_TEST_ASSERT(inSuite, delegateCommissioner1.mNumBusyResponses == 0);
+    NL_TEST_ASSERT(inSuite, delegateCommissioner2.mNumBusyResponses == 1);
+
+    gPairingServer.Shutdown();
 }
 
 struct Sigma1Params
@@ -1115,6 +1176,7 @@ static const nlTest sTests[] =
     NL_TEST_DEF("Start",       chip::TestCASESession::SecurePairingStartTest),
     NL_TEST_DEF("Handshake",   chip::TestCASESession::SecurePairingHandshakeTest),
     NL_TEST_DEF("ServerHandshake", chip::TestCASESession::SecurePairingHandshakeServerTest),
+    NL_TEST_DEF("ClientReceivesBusy", chip::TestCASESession::ClientReceivesBusyTest),
     NL_TEST_DEF("Sigma1Parsing", chip::TestCASESession::Sigma1ParsingTest),
     NL_TEST_DEF("DestinationId", chip::TestCASESession::DestinationIdTest),
     NL_TEST_DEF("SessionResumptionStorage", chip::TestCASESession::SessionResumptionStorage),