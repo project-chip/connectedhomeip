c34eb167dcae98212ca2c439d637c1624cbd279a
Hussein Elsherbini <Hussein.Elsherbini@silabs.com>
2023-08-28 13:41:47 +0000

    Pull request #1091: MATTER-800 create workspace for user
    Merge in WMN_TOOLS/matter from feature/create_workspace_gn to silabs
    
    Squashed commit of the following:
    
    commit 293f38bf8d05fe761bc9c72d1e19b1b8550ece81
    Author: Sergei Lissianoi <Sergei.Lissianoi@silabs.com>
    Date:   Mon Aug 28 13:28:45 2023 +0000
    
        Applied suggestion
    
    commit 1bdd189195a30b28805a608c58ab5017788cc862
    Author: hussein elsherbini <hussein.elsherbini@silabs.com>
    Date:   Mon Aug 28 09:27:43 2023 -0400
    
        removed zzz_generated folder and zap templates folder
    
    commit 98ac18c6011cd7857a5154c574c32fb916c46673
    Author: hussein elsherbini <hussein.elsherbini@silabs.com>
    Date:   Sun Aug 27 20:10:55 2023 -0400
    
        MATTER-800 modified sl_new_app.sh script to successfully generate a seperate workspace by user and build examples

--- scripts/sl_new_app.sh
+++ scripts/sl_new_app.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 source_path=""
+source_path1=""
+source_path2=""
 dest_path=""
-efr32_board=""
-efr32_family=""
+board_name=""
+board_family=""
 app_name=""
 common_name=""
 root=""
+temp=""
+temp1=""
 
-MG12_boards=("BRD4161A" "BRD4162A" "BRD4163A" "BRD4164A" "BRD4166A" "BRD4170A")
-MG24_boards=("BRD4186C" "BRD4187C" "BRD2703A" "BRD2601B" "BRD4316A" "BRD4317A" "BRD4319A")
+EFR32MG12_boards=("BRD4161A" "BRD4162A" "BRD4163A" "BRD4164A" "BRD4166A" "BRD4170A" "BRD4304A")
+EFR32MG24_boards=("BRD2601B" "BRD2703A" "BRD4186A" "BRD4186C" "BRD4187C" "BRD4187A" "BRD4187C")
+MGM24_boards=("BRD2704A" "BRD4317A" "BRD4318A" "BRD4319A" "BRD4316A" "BRD4325B")
 
 function Print_Help {
     echo "This bash script is to create a new app according to source sample app, board and path to the new project directory."
     echo "Usage:"
-    echo "        newlightapp -h"
-    echo "        newlightapp -s <sourcepath> -d <destpath> -b <board>"
+    echo "        sl_new_app.sh -h"
+    echo "        sl_new_app.sh -s <sourcepath> -d <destpath> -b <board>"
     echo
     echo "Available options:"
     echo "        -h   Print this help."
@@ -24,6 +29,7 @@ function Print_Help {
     echo "        -b   The board that the new app builds for."
 }
 
+# Get arguments and store in corresponding variables
 while getopts "s:d:b:h" FLAG
 do
 	case "${FLAG}" in
@@ -34,7 +40,7 @@ do
 			dest_path=${OPTARG}
 			;;
 		b)
-			efr32_board=${OPTARG}
+			board_name=${OPTARG}
 			;;
 		h)
 			Print_Help
@@ -48,20 +54,31 @@ done
 
 # If the board is supported, function returns 0, otherwise 1 is returned
 function check_board {
-	if [[ -n "$efr32_board"  ]];
+	if [[ -n "${board_name}"  ]];
 	then
-		for i in "${MG24_boards[@]}"
+		for i in "${EFR32MG24_boards[@]}"
 		do
-		    if [[ "$i" == "$efr32_board"  ]]; then
-				efr32_family="efr32mg24"
+		    if [[ "${i}" == "${board_name}"  ]]; then
+				board_family="efr32mg24"
+				echo "${board_family}"
 	       		return  0
 	       fi
 		done
 
-		for k in "${MG12_boards[@]}"
+        for j in "${MGM24_boards[@]}"
 		do
-		    if [[ "$k" == "$efr32_board"  ]]; then
-				efr32_family="efr32mg12"
+			if [[ "${j}" == "${board_name}"  ]]; then
+				board_family="mgm24"
+				echo "${board_family}"
+	            return 0
+		    fi
+		done
+
+		for k in "${EFR32MG12_boards[@]}"
+		do
+		    if [[ "${k}" == "${board_name}"  ]]; then
+				board_family="efr32mg12"
+				echo "${board_family}"
 	            return 0
 		    fi
 		done
@@ -71,7 +88,7 @@ function check_board {
 
 function main {
 	# check if the using format of the script is wrong
-	[[ -z "$source_path" || -z "$dest_path" || -z "$efr32_board" ]] && echo "Wrong format" && Print_Help && return 1
+	[[ -z "${source_path}" || -z "${dest_path}" || -z "${board_name}" ]] && echo "Wrong format" && Print_Help && return 1
 		
 	# Check if the board is supported
 	if ! check_board;
@@ -81,110 +98,121 @@ function main {
 		return 1
 	fi
 	# Check if the source path is a directory and exist
-	[[ ! -d "$source_path"  ]] && echo "The source path is incorrect!" && Print_Help && return 1
+	[[ ! -d "${source_path}"  ]] && echo "The source path is incorrect!" && Print_Help && return 1
 
 	# Get the name of the selected app
-	app_name=$(basename $source_path)
-	
+	source_path1=$(dirname "${source_path}")
+	echo "${source_path1}"
+	app_name=$(basename "${source_path1}")
+	echo "${app_name}"
+
 	# create the common directory name from provided app name
-    if [[ "${app_name: -4}" != "-app" ]]; then 
+    if [[ "${app_name: -4}" -ne "-app" ]]; then
 		common_name="${app_name}-common" 
 	else 
 		common_name="${app_name:0:${#app_name}-4}-common"
 	fi
 
 	# Create the new destination path if it does not exist
-	mkdir -p "$dest_path/$source_path"
+	mkdir -p "${dest_path}/${source_path}"
 	# Get absolute path of the destination
-	current_path=$(pwd) && cd $dest_path && dest_path=$(pwd) &&	cd $current_path
+	current_path=$(pwd) && cd "${dest_path}" && dest_path=$(pwd) &&	cd "${current_path}"
 
-	[[ $? -ne 0 ]] && echo "Please check the validity of the destination path \"$dest_path\"" && return 1
+	[[ $? -ne 0 ]] && echo "Please check the validity of the destination path \"${dest_path}\"" && return 1
 	
-	echo "Start copy app from the \"$source_path\" to \"$dest_path\"."
+	echo "Copying application from  \"${source_path}\" to \"${dest_path}\"."
 	
-	# Copy efr32 and appname-common of the selected app
-	cp -PRvf "$source_path/efr32" "$source_path/$common_name" "$dest_path/$source_path"
+	# Copy app and appname-common of the selected app
+	source_path2=$(dirname "${source_path}")
+	cp -PRvf "${source_path}" "${dest_path}/${source_path2}"
+	cp -PRvf "${source_path1}/${common_name}" "${dest_path}/${source_path1}"
 
-	# Copy "./third_party/silabs/matter_support/matter/efr32/${efr32_family}/${efr32_board}"/config and autogen
-	copy_path1="third_party/silabs/matter_support/matter/efr32/$efr32_family/$efr32_board"
+	# Copy "./third_party/silabs/matter_support/matter/efr32/${board_family}/${board_name}"/config and autogen
+	copy_path1="third_party/silabs/matter_support/matter/efr32/${board_family}/${board_name}"
+	# Copy "./third_party/silabs/slc_gen/\${silabs_board}"/config and autogen
+	copy_path5="third_party/silabs/slc_gen/${board_name}"
+
 	# Create the new destination path if it does not exist
-	mkdir -p "$dest_path/$copy_path1"
-	cp -PRvf "./$copy_path1/config" "./$copy_path1/autogen" "$dest_path/$copy_path1"
+	mkdir -p "${dest_path}/${copy_path1}"
+	mkdir -p "${dest_path}/${copy_path5}"
+	cp -PRvf "${copy_path1}/autogen" "./${copy_path1}/config" "${dest_path}/${copy_path1}"
+	cp -PRvf "${copy_path5}/autogen" "./${copy_path5}/config" "${dest_path}/${copy_path5}"
 
     # Copy "./third_party/silabs/efr32_sdk.gni"
+	temp="efr32_sdk.gni"
+	if [[ "${board_family}" == "mgm24" ]]; then
+		temp="SiWx917_sdk.gni"
+	fi
+
 	copy_path2="third_party/silabs"
-	cp -PRvf "./$copy_path2/efr32_sdk.gni" "$dest_path/$copy_path2"
-
-	# Copy "./third_party/zap/repo/zcl-builtin/shared/manufacturers.xml"
-	copy_path5="third_party/zap/repo/zcl-builtin/shared"
-
-	# Create the new destination path if it does not exist
-	mkdir -p "$dest_path/$copy_path5"
-	cp -PRvf "./$copy_path5/manufacturers.xml" "$dest_path/$copy_path5"
-	
-	# Copy zzz_generated directory of the copied app
-	copy_path3="zzz_generated/$app_name"
-	mkdir -p "$dest_path/$copy_path3"
-	cp -PRvf "./$copy_path3/zap-generated" "$dest_path/$copy_path3"
-
-	# Copy zap-templates for the copied app
-	copy_path4="src/app"
-	mkdir -p "$dest_path/$copy_path4"
-	cp -PRvf "./$copy_path4/zap-templates" "$dest_path/$copy_path4"
+	cp -PRvf "./${copy_path2}/${temp}" "${dest_path}/${copy_path2}"
 
 	#1. Root directory of matter
-	if [ -n "${MATTER_ROOT}" ]; then	
+	if [[ -n "${MATTER_ROOT}" ]]; then
 		root="${MATTER_ROOT}"
 	else
 		root=$(pwd)
 	fi
 
 	#2. symlink the buildoverride folder
-	if [ -L "$dest_path/$source_path/efr32/build_overrides" ]; then
-		unlink "$dest_path/$source_path/efr32/build_overrides"
+	if [[ -L "${dest_path}/${source_path}/build_overrides" ]]; then
+		unlink "${dest_path}/${source_path}/build_overrides"
 	fi
-	ln -s "${root}/examples/build_overrides" "$dest_path/$source_path/efr32/build_overrides"
+	ln -s "${root}/examples/build_overrides" "${dest_path}/${source_path}/build_overrides"
 
 	#3. symlink the third_party to point to the actual location of the matter repo
-	if [ -L "$dest_path/$source_path/efr32/third_party/connectedhomeip" ]; then
-		unlink "$dest_path/$source_path/efr32/third_party/connectedhomeip"
+	if [[ -L "${dest_path}/${source_path}/third_party/connectedhomeip" ]]; then
+		unlink "${dest_path}/${source_path}/third_party/connectedhomeip"
 	fi	
-	ln -s "${root}" "$dest_path/$source_path/efr32/third_party/connectedhomeip"
+	ln -s "${root}" "${dest_path}/${source_path}/third_party/connectedhomeip"
 
-	#4. Modify app/efr32/BUILD.gn and app/app-common/BUILD.gn to correct paths corresponding to coppied app
-	# Modification in app/efr32/BUILD.gn
-	sed -i.bak -e "s+import(\"\${efr32_sdk_build_root}/efr32_sdk.gni\")+import(\"../../../$copy_path2/efr32_sdk.gni\")+" -- "$dest_path/$source_path/efr32/BUILD.gn"
-	sed -i.bak -e "s+\"\${chip_root}/examples/lighting-app/efr32\"+\"$dest_path/$source_path/efr32\"+" -- "$dest_path/$source_path/efr32/BUILD.gn"
-	sed -i.bak -e "s+\"\${chip_root}/examples/lighting-app/lighting-common\"+\"$dest_path/$source_path/$common_name\"+" -- "$dest_path/$source_path/efr32/BUILD.gn" &&
-	rm -- "$dest_path/$source_path/efr32/BUILD.gn.bak"
-	# Modification in app/app-common/BUILD.gn
-	sed -i.bak -e "s+\"\${chip_root}/zzz_generated/lighting-app/zap-generated\"+\"../../../zzz_generated/$app_name/zap-generated\"+" -- "$dest_path/$source_path/$common_name/BUILD.gn" &&
-	rm -- "$dest_path/$source_path/$common_name/BUILD.gn.bak"
+	#4. Modify app BUILD.gn and app-common BUILD.gn to correct paths corresponding to copied app
+	temp="efr32_sdk.gni"
+	temp1="silabs"
+	if [[ "${board_family}" == "mgm24" ]]; then
+		temp="SiWx917_sdk.gni"
+		temp1="silabs/SiWx917"
+	fi
+	# Modification in app BUILD.gn
+	sed -i.bak -e "s+import(\"\${efr32_sdk_build_root}/${temp}\")+import(\"../../../../${copy_path2}/${temp}\")+" -- "${dest_path}/${source_path}/BUILD.gn"
+	sed -i.bak -e "s+\"\${chip_root}/examples/${app_name}/${temp1}\"+\"${dest_path}/${source_path1}/${temp1}\"+" -- "${dest_path}/${source_path}/BUILD.gn"
+	sed -i.bak -e "s+import(\"../../../../third_party/silabs/efr32_sdk.gni\")+import(\"${dest_path}/third_party/silabs/efr32_sdk.gni\")+" -- "${dest_path}/${source_path}/BUILD.gn"
+	sed -i.bak -e "s+\"\${chip_root}/examples/${app_name}/${common_name}\"+\"${dest_path}/${source_path1}/${common_name}\"+" -- "${dest_path}/${source_path}/BUILD.gn" &&
+	rm -- "${dest_path}/${source_path}/BUILD.gn.bak"
 
-	#5. Modify efr32_sdk.gni
+	# Modification in app-common BUILD.gn
+	sed -i.bak -e "s+\"\${chip_root}/zzz_generated/${app_name}/zap-generated\"+\"../../../zzz_generated/${app_name}/zap-generated\"+" -- "${dest_path}/${source_path1}/${common_name}/BUILD.gn" &&
+	rm -- "${dest_path}/${source_path1}/${common_name}/BUILD.gn.bak"
+
+	#5. Modify efr32_sdk.gni or SiWx917_sdk.gni correspondingly
 	# Correct the import path
-	sed -i.bak -e "s+import(\"silabs_board.gni\")+import(\"../../$source_path/efr32/third_party/connectedhomeip/$copy_path2/silabs_board.gni\")+" -- "$dest_path/$copy_path2/efr32_sdk.gni"
+	sed -i.bak -e "s+import(\"silabs_board.gni\")+import(\"../../${source_path}/third_party/connectedhomeip/${copy_path2}/silabs_board.gni\")+" -- "${dest_path}/${copy_path2}/${temp}"
 	# Correct path to the coppied app
-    sed -i.bak -e "s+\"\${sdk_support_root}/matter/efr32/\${silabs_family}/\${silabs_board}/config\"+\"$dest_path/$copy_path2/matter_support/matter/efr32/\${silabs_family}/\${silabs_board}/config\"+" -- "$dest_path/$copy_path2/efr32_sdk.gni"
-	sed -i.bak -e "s+\"\${sdk_support_root}/matter/efr32/\${silabs_family}/\${silabs_board}/autogen\"+\"$dest_path/$copy_path2/matter_support/matter/efr32/\${silabs_family}/\${silabs_board}/autogen\"+" -- "$dest_path/$copy_path2/efr32_sdk.gni"
-	# Import the newly created file customized.gni
-	sed -i.bak -e "s+declare_args() {+import(\"customized.gni\")+" -- "$dest_path/$copy_path2/efr32_sdk.gni"
-	# Delete range of text
-	sed -i.bak -e "/# Location of the efr32 SDK./{N;N;N;N;N;N;N;N;N;N;N;N;N;N;d;}" -- "$dest_path/$copy_path2/efr32_sdk.gni" &&
-	rm -- "$dest_path/$copy_path2/efr32_sdk.gni.bak"
+	sed -i.bak -e "s+\"\${chip_root}/third_party/silabs/slc_gen/\${silabs_board}/\"+\"${dest_path}/${copy_path2}/slc_gen/\${silabs_board}/\"+" -- "${dest_path}/${copy_path2}/${temp}"
+	sed -i.bak -e "s+\"\${sdk_support_root}/matter/efr32/\${silabs_family}/\${silabs_board}/\"+\"${dest_path}/${copy_path2}/matter_support/matter/efr32/\${silabs_family}/\${silabs_board}/\"+" -- "$dest_path/$copy_path2/$temp"
 
-	#6. Create a customized.gni and move arg declaration from efr32_sdk.gni
-	# Copy text from line 1~38 of file efr32_sdk.gni and write to file customized.gni (new created file)
-	sed -n "1,38 p" "./$copy_path2/efr32_sdk.gni" > "$dest_path/$copy_path2/customized.gni"
-	# Delete line 16~22 and 38 of newly created customized.gni
-	sed -i.bak -e "16,22d;38d" -- "$dest_path/$copy_path2/customized.gni"
-	# Replace the patern with a blank line
-	sed -i.bak -e "s/declare_args() {/\n/" -- "$dest_path/$copy_path2/customized.gni"
-	# Remove leading spaces of all text lines
-	sed -i.bak -e "s/^[ \t]*//" -- "$dest_path/$copy_path2/customized.gni" &&
-	rm -- "$dest_path/$copy_path2/customized.gni.bak"
-    
-	[[ $? -eq 0 ]] && echo "Creation of selected app from matter to destination folder \"$dest_path\" is done."
+	# Fixing for building
+	declare -i num_of_curly_braces=0
+	declare -i line_number=0
+	declare -i x=0
+	gni_filename="${dest_path}/${copy_path2}/${temp}"
+	while IFS= read -r line
+	do 
+		((line_number++))
+		if [ "$line" = "}" ] && [[ "$num_of_curly_braces" -gt 0 ]]; then
+			sed -i.bak -e "$(expr ${line_number} - $x) d" -- "$gni_filename"
+			((num_of_curly_braces--))
+			((x++))
+		elif [ "$line" = "declare_args() {" ]; then
+			((num_of_curly_braces++))
+			sed -i.bak -e "$(expr ${line_number} - $x) d" -- "$gni_filename"
+			((x++))
+		fi 
+	done < "${gni_filename}"
+
+	rm -- "${dest_path}/${copy_path2}/${temp}.bak"
+
+	[[ $? -eq 0 ]] && echo "Creation of selected app from matter to destination folder \"${dest_path}\" is done."
 }
 
 main
