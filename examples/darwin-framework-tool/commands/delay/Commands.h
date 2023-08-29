b3a4d5cb87cec3c2ef9ba0b6c731fb0ce3016018
Vivien Nicolas <vnicolas@apple.com>
2023-08-29 17:50:59 +0200

    Add wait-for-commissionee command to darwin-framework-tool

--- examples/darwin-framework-tool/commands/delay/Commands.h
+++ examples/darwin-framework-tool/commands/delay/Commands.h
@@ -20,13 +20,14 @@
 
 #include "commands/common/Commands.h"
 #include "commands/delay/SleepCommand.h"
-// #include "commands/delay/WaitForCommissioneeCommand.h"
+#include "commands/delay/WaitForCommissioneeCommand.h"
 
 void registerCommandsDelay(Commands & commands)
 {
     const char * clusterName      = "Delay";
     commands_list clusterCommands = {
-        make_unique<SleepCommand>(), //
+        make_unique<SleepCommand>(),               //
+        make_unique<WaitForCommissioneeCommand>(), //
     };
 
     commands.RegisterCommandSet(clusterName, clusterCommands, "Commands for waiting for something to happen.");
