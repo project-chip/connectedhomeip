4d2b9ea5cfe43701c35da5aa9e4b7ef1607550aa
René Josefsen <rjosefsen@grundfos.com>
2023-08-31 12:55:24 +0200

    Reworked scripts based on review feedback

--- src/python_testing/TC_XX_1_1.py
+++ src/python_testing/TC_XX_1_1.py
@@ -20,82 +20,67 @@ import logging
 from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
 from mobly import asserts
 
-standard_range_prefix = 0x0000
-manufacturer_code_min_prefix = 0x0001
-manufacturer_code_max_prefix = 0xFFF0
-test_vendor_min_prefix = 0xFFF1
-test_vendor_max_prefix = 0xFFF4
-invalid_vendor_min_prefix = 0xFFF5
-invalid_vendor_max_prefix = 0xFFFF
+STANDARD_RANGE = 0x0000
+MANUFACTURER_CODE_RANGE = range(0x0001, 0xFFF0 + 1)
+TEST_VENDOR_RANGE = range(0xFFF1, 0xFFF4 + 1)
+INVALID_VENDOR_RANGE = range(0xFFF5, 0xFFFF + 1)
 
-attribute_id_range_min = 0x0000
-attribute_id_range_max = 0x4FFF
-attribute_global_range_min = 0xF000
-attribute_global_range_max = 0xFFFE
-attribute_id_invalid_max = 0xFFFF
+ATTRIBUTE_ID_RANGE = range(0x0000, 0x4FFF + 1)
+GLOBAL_ATTRIBUTE_ID_RANGE = range(0xF000, 0xFFFE)
+ATTRIBUTE_INVALID_MAX = 0xFFFF
 
-# event_id_range_min = 0x00
-# event_id_range_max = 0xFF
-# event_id_invalid_max = 0xFFFF
-
-command_id_range_min = 0x00
-command_id_range_max = 0xFF
-command_id_invalid_max = 0xFFFF
+COMMAND_ID_RANGE = range(0x00, 0xFF + 1)
+COMMAND_ID_INVALID_MAX = 0xFFFF
 
 ATTRIBUTE_LIST_ID = 0xFFFB
-# EVENT_LIST_ID = 0xFFFA
 ACCEPTED_COMMAND_LIST_ID = 0xFFF9
 GENERATED_COMMAND_LIST_ID = 0xFFF8
 
 
-def get_element_prefix_suffix(element):
-    prefix_mask = ~0xffff
-    suffix_mask = 0xffff
-
-    prefix = (element >> 16)
-    suffix = (element & suffix_mask)
-
-    return prefix, suffix
+def split_element_into_prefix_suffix(element):
+    return (element >> 16), (element & 0xFFFF)
 
 
-def is_attribute_id_in_valid_range(attribute_id):
-    prefix, suffix = get_element_prefix_suffix(attribute_id)
+def validate_attribute_id_range(attribute_id):
+    prefix, suffix = split_element_into_prefix_suffix(attribute_id)
 
-    if prefix == standard_range_prefix:
-        if suffix > attribute_id_range_max and suffix < attribute_global_range_min or suffix == attribute_id_invalid_max:
-            asserts.fail(f"Invalid attribute id {attribute_id:010x} in standard range")
+    if prefix == STANDARD_RANGE:
+        if suffix not in ATTRIBUTE_ID_RANGE and suffix not in GLOBAL_ATTRIBUTE_ID_RANGE or suffix == ATTRIBUTE_INVALID_MAX:
+            asserts.fail(f"Invalid attribute id (0x{attribute_id:08X}) in standard range")
 
-    elif prefix >= manufacturer_code_min_prefix and prefix <= manufacturer_code_max_prefix:
-        if suffix > attribute_id_range_max:
-            asserts.fail(f"Invalid attribute id {attribute_id:010x} in MC range")
+    elif prefix in MANUFACTURER_CODE_RANGE:
+        if suffix not in ATTRIBUTE_ID_RANGE:
+            asserts.fail(f"Invalid attribute id (0x{attribute_id:08X}) in MC range")
 
-    elif prefix >= test_vendor_min_prefix and prefix <= test_vendor_max_prefix:
-        if suffix >= attribute_id_range_min:
-            asserts.fail(f"Invalid attribute id {attribute_id:010x} in Test Vendor MC range")
+    elif prefix in TEST_VENDOR_RANGE:
+        if suffix in ATTRIBUTE_ID_RANGE:
+            logging.info(f"Warning: Attribute id (0x{attribute_id:08X}) found in test vendor range")
+        else:
+            asserts.fail(f"Invalid attribute id (0x{attribute_id:08X}) in Test Vendor MC range")
 
-    else:  # prefix >= invalid_vendor_min_prefix and prefix <= invalid_vendor_max_prefix
-        if suffix >= attribute_id_range_min:
-            asserts.fail(f"Invalid attribute id {attribute_id:010x} in invalid range")
+    else:  # prefix in INVALID_VENDOR_RANGE
+        asserts.fail(f"Invalid attribute id (0x{attribute_id:08X}) in invalid range")
 
 
-def is_command_id_in_valid_range(command_id):
-    prefix, suffix = get_element_prefix_suffix(command_id)
+def validate_command_id_range(command_id):
+    prefix, suffix = split_element_into_prefix_suffix(command_id)
 
-    if prefix == standard_range_prefix:
-        if suffix > command_id_range_max:
-            asserts.fail(f"Invalid command id {command_id:010x} in standard range")
+    if prefix == STANDARD_RANGE:
+        if suffix not in COMMAND_ID_RANGE:
+            asserts.fail(f"Invalid command id (0x{command_id:08X}) in standard range")
 
-    elif prefix >= manufacturer_code_min_prefix and prefix <= manufacturer_code_max_prefix:
-        if suffix > command_id_range_max:
-            asserts.fail(f"Invalid command id {command_id:010x} in MC range")
+    elif prefix in MANUFACTURER_CODE_RANGE:
+        if suffix not in COMMAND_ID_RANGE:
+            asserts.fail(f"Invalid command id (0x{command_id:08X}) in MC range")
 
-    elif prefix >= test_vendor_min_prefix and prefix <= test_vendor_max_prefix:
-        if suffix >= command_id_range_min:
-            asserts.fail(f"Invalid command id {command_id:010x} in Test Vendor MC range")
+    elif prefix in TEST_VENDOR_RANGE:
+        if suffix in COMMAND_ID_RANGE:
+            logging.info(f"Warning: Command id (0x{command_id:08X}) found in test vendor range")
+        else:
+            asserts.fail(f"Invalid command id (0x{command_id:08X}) in Test Vendor MC range")
 
-    else:  # prefix >= invalid_vendor_min_prefix and prefix >= invalid_vendor_max_prefix
-        if suffix >= command_id_range_min:
-            asserts.fail(f"Invalid command id {command_id:010x} in invalid range")
+    else:  # prefix in INVALID_VENDOR_RANGE
+        asserts.fail(f"Invalid command id (0x{command_id:08X}) in invalid range")
 
 
 class TC_XX_1_1(MatterBaseTest):
@@ -113,25 +98,26 @@ class TC_XX_1_1(MatterBaseTest):
             logging.info(f"Verifying Endpoint: {endpoint_id}")
 
             for cluster_id, cluster in endpoint.items():
-                logging.info(f"Verifying ClusterID: {cluster_id:#06x}")
+                logging.info(f"Verifying ClusterID: 0x{cluster_id:04X}")
 
                 attribute_list = cluster[ATTRIBUTE_LIST_ID]
                 accepted_command_list = cluster[ACCEPTED_COMMAND_LIST_ID]
                 generated_command_list = cluster[GENERATED_COMMAND_LIST_ID]
 
                 # -- Attributes
-                logging.info(f"AttributeList: {attribute_list}")
+                logging.info(f"AttributeList: {[f'0x{attribute_id:04X}' for attribute_id in attribute_list]}")
+
                 for attribute_id in attribute_list:
-                    is_attribute_id_in_valid_range(attribute_id)
+                    validate_attribute_id_range(attribute_id)
 
                 # -- Commands
                 logging.info(f"AcceptedCommandList: {accepted_command_list}")
                 for command_id in accepted_command_list:
-                    is_command_id_in_valid_range(command_id)
+                    validate_command_id_range(command_id)
 
                 logging.info(f"GeneratedCommandList: {generated_command_list}")
                 for command_id in generated_command_list:
-                    is_command_id_in_valid_range(command_id)
+                    validate_command_id_range(command_id)
 
 
 if __name__ == "__main__":
