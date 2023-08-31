3550536f100bc572a02cb54099560ec68afff984
manjunath-grl <manjunath.grl@outlook.com>
2023-08-31 20:19:24 +0530

    Disabled RVCCLEANM-3.1 test in init.py

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -175,6 +175,7 @@ def _GetChipReplUnsupportedTests() -> Set[str]:
         "Test_TC_LWM_3_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_G_2_4.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_RVCRUNM_3_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
+        "Test_TC_RVCCLEANM_3_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
     }
 
 
