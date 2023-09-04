43d479ca02d0c5b26585aa3b3d918a61c00433e3
William Hicklin <venturehicklin@gmail.com>
2023-09-04 17:43:34 +0100

    RvcApp: Added a FIFO pipe for out-of-band messages.

--- examples/rvc-app/linux/main.cpp
+++ examples/rvc-app/linux/main.cpp
@@ -15,12 +15,29 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+#include "RvcAppCommandDelegate.h"
 #include "rvc-device.h"
 #include <AppMain.h>
 
 using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
+
+// Todo s
+//  - Add a named pipe for out-of-band messages
+//  - Add and handle the following out-of-band messages
+//    - charged
+//    - docked?
+//    - charger found
+//    - low charge
+//    - activity complete
+//    - error event
+
+namespace {
+constexpr const char kChipEventFifoPathPrefix[] = "/tmp/chip_rvc_fifo_";
+NamedPipeCommands sChipNamedPipeCommands;
+RvcAppCommandDelegate sRvcAppCommandDelegate;
+} // namespace
 
 RvcDevice * rvcDevice = nullptr;
 
@@ -41,6 +58,14 @@ int main(int argc, char * argv[])
     if (ChipLinuxAppInit(argc, argv) != 0)
     {
         return -1;
+    }
+
+    std::string path = kChipEventFifoPathPrefix + std::to_string(getpid());
+
+    if (sChipNamedPipeCommands.Start(path, &sRvcAppCommandDelegate) != CHIP_NO_ERROR)
+    {
+        ChipLogError(NotSpecified, "Failed to start CHIP NamedPipeCommands");
+        sChipNamedPipeCommands.Stop();
     }
 
     ChipLinuxAppMainLoop();
