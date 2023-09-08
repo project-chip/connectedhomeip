1dc414b79eff1d2c72181f833fa39ceafcfae9be
Boris Zbarsky <bzbarsky@apple.com>
2023-09-08 13:38:14 -0400

    Implement RebootCount storage on Darwin.
    Fixes https://github.com/project-chip/connectedhomeip/issues/29108

--- src/platform/Darwin/DiagnosticDataProviderImpl.cpp
+++ src/platform/Darwin/DiagnosticDataProviderImpl.cpp
@@ -89,6 +89,21 @@ CHIP_ERROR DiagnosticDataProviderImpl::ResetWatermarks()
     return CHIP_NO_ERROR;
 }
 
+CHIP_ERROR DiagnosticDataProviderImpl::GetRebootCount(uint16_t & rebootCount)
+{
+    uint32_t count = 0;
+
+    CHIP_ERROR err = ConfigurationMgr().GetRebootCount(count);
+
+    if (err == CHIP_NO_ERROR)
+    {
+        VerifyOrReturnError(count <= UINT16_MAX, CHIP_ERROR_INVALID_INTEGER_VALUE);
+        rebootCount = static_cast<uint16_t>(count);
+    }
+
+    return err;
+}
+
 DiagnosticDataProvider & GetDiagnosticDataProviderImpl()
 {
     return DiagnosticDataProviderImpl::GetDefaultInstance();
