d5f2270e612acb2e495eab21873c0c74dfd8d819
Vivien Nicolas <vnicolas@apple.com>
2023-09-08 15:31:26 +0200

    [darwin-framework-tool] Expose --storage-directory argument

--- examples/darwin-framework-tool/commands/interactive/InteractiveCommands.mm
+++ examples/darwin-framework-tool/commands/interactive/InteractiveCommands.mm
@@ -380,7 +380,7 @@ bool InteractiveCommand::ParseCommand(char * command, int * status)
 
     ClearLine();
 
-    *status = mHandler->RunInteractive(command);
+    *status = mHandler->RunInteractive(command, GetStorageDirectory());
 
     return YES;
 }
