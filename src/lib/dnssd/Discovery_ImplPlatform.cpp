162ea958467a846131629f4a8bab44af30ec6f89
Deomid Ryabkov <rojer@rojer.me>
2023-09-22 14:37:31 +0100

    DiscoveryImplPlatform: Only publish IPv6 address if IPv4 is disabled (#29399)

--- src/lib/dnssd/Discovery_ImplPlatform.cpp
+++ src/lib/dnssd/Discovery_ImplPlatform.cpp
@@ -508,7 +508,11 @@ CHIP_ERROR DiscoveryImplPlatform::PublishService(const char * serviceType, TextE
                              ? MakeInstanceName(service.mName, sizeof(service.mName), peerId)
                              : GetCommissionableInstanceName(service.mName, sizeof(service.mName)));
     Platform::CopyString(service.mType, serviceType);
-    service.mAddressType   = Inet::IPAddressType::kAny;
+#if INET_CONFIG_ENABLE_IPV4
+    service.mAddressType = Inet::IPAddressType::kAny;
+#else
+    service.mAddressType = Inet::IPAddressType::kIPv6;
+#endif
     service.mInterface     = interfaceId;
     service.mProtocol      = protocol;
     service.mPort          = port;
