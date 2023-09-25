b0514c1cccfcdacf2f158652a3342ed28d623877
Restyled.io <commits@restyled.io>
2023-09-25 17:36:40 +0000

    Restyled by shfmt

--- src/tools/interop/idt/scripts/py_venv.sh
+++ src/tools/interop/idt/scripts/py_venv.sh
@@ -17,10 +17,10 @@
 
 cd idt
 if [ -d venv ]; then
-  source venv/bin/activate
+    source venv/bin/activate
 else
-  python3 -m venv venv
-  source venv/bin/activate
-  pip install -r requirements.txt
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
 fi
 cd ..
