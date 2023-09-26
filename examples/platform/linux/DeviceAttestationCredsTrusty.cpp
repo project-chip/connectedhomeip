fb83d2275b4c784f611f3c1dcecd3f5fc26f56fe
Ji Luo <ji.luo@nxp.com>
2023-09-26 10:33:04 +0800

    Add device attestation based on ELE
    Support device attestation based on EdgeLock Enclave(ELE).
    Attestation certifications and keys should be provisioned
    into device in advance.
    
    The official tool to provide signed content is not ready, so
    we disable the device attestation feature based on ELE first.
    
    It's currently only supported by i.MX 93 platform.
    
    Change-Id: I39c79efa17a99266113e2bf28204d0c23b81af1d
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23345
    Reviewed-by: Elven Wang <elven.wang@nxp.com>

--- examples/platform/linux/DeviceAttestationCredsTrusty.cpp
+++ examples/platform/linux/DeviceAttestationCredsTrusty.cpp
@@ -1,6 +1,6 @@
 /*
  *    Copyright (c) 2021-2022 Project CHIP Authors
- *    Copyright 2023 NXP
+ *    Copyright 2023, 2025 NXP
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "DeviceAttestationCreds.h"
+#include "DeviceAttestationCredsTrusty.h"
 
 #include <crypto/CHIPCryptoPAL.h>
 #include <lib/core/CHIPError.h>
