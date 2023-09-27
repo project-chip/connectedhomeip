9b92ed4965fa43ce3c8cf527c25ab3b7064fd033
Mark Trayer <m.trayer@samsung.com>
2023-09-27 08:09:50 -0500

    Remove TCCM 3-1, 3-2, 3-3 from being executed.

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -158,11 +158,14 @@ def _GetInDevelopmentTests() -> Set[str]:
                                              # TestEventTriggersEnabled is true, which it's not in CI.
         "Test_TC_SMOKECO_2_6.yaml",          # chip-repl does not support local timeout (07/20/2023) and test assumes
                                              # TestEventTriggersEnabled is true, which it's not in CI.
-        "Test_TC_IDM_1_2.yaml",  # Broken harness: https://github.com/project-chip/connectedhomeip/issues/29115
-        "Test_TC_S_2_4.yaml",  # https://github.com/project-chip/connectedhomeip/issues/29117
-        "Test_TC_LWM_3_1.yaml",  # Target attribute now provisional and removed from the XML
-        "Test_TC_LWM_3_2.yaml",  # Target attribute now provisional and removed from the XML
-        "Test_TC_LWM_3_3.yaml"   # Target attribute now provisional and removed from the XML
+        "Test_TC_IDM_1_2.yaml",   # Broken harness: https://github.com/project-chip/connectedhomeip/issues/29115
+        "Test_TC_S_2_4.yaml",     # https://github.com/project-chip/connectedhomeip/issues/29117
+        "Test_TC_LWM_3_1.yaml",   # Target attribute now provisional and removed from the XML
+        "Test_TC_LWM_3_2.yaml",   # Target attribute now provisional and removed from the XML
+        "Test_TC_LWM_3_3.yaml",   # Target attribute now provisional and removed from the XML
+        "Test_TC_TCCM_3_1.yaml",  # Target attribute now provisional and removed from the XML
+        "Test_TC_TCCM_3_2.yaml",  # Target attribute now provisional and removed from the XML
+        "Test_TC_TCCM_3_3.yaml"   # Target attribute now provisional and removed from the XML
     }
 
 
