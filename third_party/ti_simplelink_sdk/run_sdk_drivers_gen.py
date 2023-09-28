daf949f680a9f7c39a83a11e006a9067b66aacc5
Restyled.io <commits@restyled.io>
2023-09-28 22:15:29 +0000

    Restyled by autopep8

--- third_party/ti_simplelink_sdk/run_sdk_drivers_gen.py
+++ third_party/ti_simplelink_sdk/run_sdk_drivers_gen.py
@@ -9,11 +9,12 @@ Args:
 import subprocess
 import sys
 
-SDK_ROOT=sys.argv[1]
-CHIP_ROOT=sys.argv[2]
+SDK_ROOT = sys.argv[1]
+CHIP_ROOT = sys.argv[2]
 
-GCC_ARMCOMPILER_PATH= CHIP_ROOT + "/.environment/cipd/packages/arm/"
-make_command = ["make", "-C" ,SDK_ROOT,"CMAKE=cmake","GCC_ARMCOMPILER=" + GCC_ARMCOMPILER_PATH,"IAR_ARMCOMPILER=","TICLANG_ARMCOMPILER=","GENERATOR=Ninja"]
+GCC_ARMCOMPILER_PATH = CHIP_ROOT + "/.environment/cipd/packages/arm/"
+make_command = ["make", "-C", SDK_ROOT, "CMAKE=cmake", "GCC_ARMCOMPILER=" +
+                GCC_ARMCOMPILER_PATH, "IAR_ARMCOMPILER=", "TICLANG_ARMCOMPILER=", "GENERATOR=Ninja"]
 
 ret = subprocess.call(make_command)
 sys.exit(ret)
