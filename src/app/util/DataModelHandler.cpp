4fa372163b1c69a8697d5cbdd73e5ee91b85a75a
Andrei Litvin <andy314@gmail.com>
2023-10-02 11:02:00 -0400

    Remove usage of `__has_include` and usage of `USE_ZAP_CONFIG` (#29492)
    * Start removing split defines and setups for InitDataModelHandler
    
    * Restyle
    
    * Add EmptyDataModelHandler to Matter.framework.
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/util/DataModelHandler.cpp
+++ src/app/util/DataModelHandler.cpp
@@ -22,20 +22,15 @@
 
 #include <app/util/DataModelHandler.h>
 
-#if __has_include(<zap-generated/endpoint_config.h>)
-#define USE_ZAP_CONFIG 1
 #include <app/util/attribute-storage.h>
 #include <app/util/util.h>
-#endif
 #include <lib/support/logging/CHIPLogging.h>
 
 using namespace ::chip;
 
 void InitDataModelHandler()
 {
-#ifdef USE_ZAP_CONFIG
     ChipLogProgress(Zcl, "Using ZAP configuration...");
     emberAfEndpointConfigure();
     emberAfInit();
-#endif
 }
