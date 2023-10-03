19771ed7101321d68b87d05201d42d00adf5368f
Marius Tache <102153746+marius-alex-tache@users.noreply.github.com>
2023-10-03 18:32:48 +0300

    [NXP] Initial support for K32W1 (#28777)
    * [NXP] Introducing Matter support for K32W1
    
    Additional information:
    * SDK: 2.12.5 K32W148-EVK, downloadable from https://mcuxpresso.nxp.com
    * MCU overview: https://www.nxp.com/products/wireless/multiprotocol-mcus/secure-and-ultra-low-power-mcu-for-matter-over-thread-and-bluetooth-le-5-3:K32W148
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [NXP] Add CI/CV support for K32W1
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [NXP] Bump ot-nxp submodule
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W] Add OPENTHREAD_PLATFORM_CORE_CONFIG_FILE define
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Fix misspell
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by gn
    
    * Restyled by prettier-markdown
    
    * Restyled by autopep8
    
    * Restyled by isort
    
    * [K32W1] Fix lint errors
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Removed deprecated class
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * Restyled by clang-format
    
    * [test] Update testdata/all_targets_linux_x64 with K32W1 targets
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W] Fix target names in workflow
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Remove deprecated commented sections
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Add missing # to reference targets in README
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Fix spelling errors
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Update CSR crypto flag usage
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * [K32W1] Remove ResetWatermarks empty implementation
    
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    
    * Fix typo in device.gni after fixing conflict
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [K32W1] Remove anchors and backtick some words
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [K32W] Fix confusing naming in K32W builder
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [K32W] Remove commented option
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [K32W] Fix binary extension format
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Restyled by autopep8
    
    * [K32W] Fix binary name when copying
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Restyled by clang-format
    
    * [K32W1] Fix ErrorStr header inclusion
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    ---------
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>
    Signed-off-by: Marius Tache <marius.tache@nxp.com>
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    Co-authored-by: Doru Gucea <doru-cristian.gucea@nxp.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/platform/nxp/k32w/k32w1/k32w1-chip-mbedtls-config.h
@@ -0,0 +1,131 @@
+/*
+ *
+ *    Copyright (c) 2021 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+// Spans multiple lines to avoid being processed by unifdef
+#ifndef MBEDTLS_CONFIG_H
+#define MBEDTLS_CONFIG_H
+
+#include "openthread-core-config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <openthread/config.h>
+#include <openthread/platform/logging.h>
+#include <openthread/platform/memory.h>
+
+#undef MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
+#define MBEDTLS_PLATFORM_SNPRINTF_MACRO snprintf
+
+#define MBEDTLS_AES_C
+#define MBEDTLS_AES_ROM_TABLES
+#define MBEDTLS_ASN1_PARSE_C
+#define MBEDTLS_ASN1_WRITE_C
+#define MBEDTLS_BIGNUM_C
+#define MBEDTLS_CCM_C
+#define MBEDTLS_CIPHER_C
+#define MBEDTLS_CMAC_C
+#define MBEDTLS_CTR_DRBG_C
+#define MBEDTLS_ECJPAKE_C
+#define MBEDTLS_ECP_C
+#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+#define MBEDTLS_ECP_NIST_OPTIM
+#define MBEDTLS_ENTROPY_C
+#define MBEDTLS_HAVE_ASM
+#define MBEDTLS_HMAC_DRBG_C
+#define MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED
+#define MBEDTLS_MD_C
+#define MBEDTLS_NO_PLATFORM_ENTROPY
+#define MBEDTLS_PK_C
+#define MBEDTLS_PK_PARSE_C
+#define MBEDTLS_PLATFORM_C
+#define MBEDTLS_PLATFORM_MEMORY
+#define MBEDTLS_PLATFORM_NO_STD_FUNCTIONS
+#define MBEDTLS_SHA224_C
+#define MBEDTLS_SHA256_C
+#define MBEDTLS_SHA256_SMALLER
+#define MBEDTLS_SSL_CLI_C
+#define MBEDTLS_SSL_DTLS_ANTI_REPLAY
+#define MBEDTLS_SSL_DTLS_HELLO_VERIFY
+#define MBEDTLS_SSL_EXPORT_KEYS
+#define MBEDTLS_SSL_MAX_FRAGMENT_LENGTH
+#define MBEDTLS_SSL_PROTO_TLS1_2
+#define MBEDTLS_SSL_PROTO_DTLS
+#define MBEDTLS_SSL_TLS_C
+
+#if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE || OPENTHREAD_CONFIG_COMMISSIONER_ENABLE || OPENTHREAD_CONFIG_COAP_SECURE_API_ENABLE
+#define MBEDTLS_SSL_COOKIE_C
+#define MBEDTLS_SSL_SRV_C
+#endif
+
+#if OPENTHREAD_CONFIG_COAP_SECURE_API_ENABLE
+#define MBEDTLS_KEY_EXCHANGE_PSK_ENABLED
+#define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
+#endif
+
+#ifdef MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
+#define MBEDTLS_BASE64_C
+#define MBEDTLS_ECDH_C
+#define MBEDTLS_ECDSA_C
+#define MBEDTLS_OID_C
+#define MBEDTLS_PEM_PARSE_C
+#define MBEDTLS_X509_USE_C
+#define MBEDTLS_X509_CRT_PARSE_C
+#endif
+
+#if OPENTHREAD_CONFIG_ECDSA_ENABLE
+#define MBEDTLS_BASE64_C
+#define MBEDTLS_ECDH_C
+#define MBEDTLS_ECDSA_C
+#define MBEDTLS_ECDSA_DETERMINISTIC
+#define MBEDTLS_OID_C
+#define MBEDTLS_PEM_PARSE_C
+#define MBEDTLS_PK_WRITE_C
+#endif
+
+#define MBEDTLS_MPI_WINDOW_SIZE 1       /**< Maximum windows size used. */
+#define MBEDTLS_MPI_MAX_SIZE 32         /**< Maximum number of bytes for usable MPIs. */
+#define MBEDTLS_ECP_MAX_BITS 256        /**< Maximum bit size of groups */
+#define MBEDTLS_ECP_WINDOW_SIZE 2       /**< Maximum window size used */
+#define MBEDTLS_ECP_FIXED_POINT_OPTIM 0 /**< Enable fixed-point speed-up */
+#define MBEDTLS_ENTROPY_MAX_SOURCES 2   /**< Maximum number of sources supported */
+
+#if OPENTHREAD_CONFIG_HEAP_EXTERNAL_ENABLE
+#define MBEDTLS_PLATFORM_STD_CALLOC otPlatCAlloc /**< Default allocator to use, can be undefined */
+#define MBEDTLS_PLATFORM_STD_FREE otPlatFree     /**< Default free to use, can be undefined */
+#else
+#define MBEDTLS_MEMORY_BUFFER_ALLOC_C
+#endif
+
+#if OPENTHREAD_CONFIG_COAP_SECURE_API_ENABLE
+#define MBEDTLS_SSL_MAX_CONTENT_LEN 900 /**< Maxium fragment length in bytes */
+#else
+#define MBEDTLS_SSL_MAX_CONTENT_LEN 768 /**< Maxium fragment length in bytes */
+#endif
+
+#define MBEDTLS_SSL_IN_CONTENT_LEN MBEDTLS_SSL_MAX_CONTENT_LEN
+#define MBEDTLS_SSL_OUT_CONTENT_LEN MBEDTLS_SSL_MAX_CONTENT_LEN
+#define MBEDTLS_SSL_CIPHERSUITES MBEDTLS_TLS_ECJPAKE_WITH_AES_128_CCM_8
+
+// Spans multiple lines to avoid being processed by unifdef
+#if defined(MBEDTLS_USER_CONFIG_FILE)
+#include MBEDTLS_USER_CONFIG_FILE
+#endif
+
+#include "mbedtls/check_config.h"
+
+#endif /* MBEDTLS_CONFIG_H */
