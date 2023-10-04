2c36bdb8916db4449b4e89432ff139b646fd757d
Rohan Sahay <rohan.sahay@silabs.com>
2023-10-04 23:34:58 +0530

    adds thread check

--- gn_build_silabs_wifi.sh
+++ gn_build_silabs_wifi.sh
@@ -3,6 +3,8 @@
 set -e
 
 ## BRD4161A
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4161A disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
@@ -19,6 +21,8 @@ set -e
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_wf200 BRD4161A --wifi wf200 disable_lcd=false use_external_flash=false chip_logging=false silabs_log_enabled=false is_debug=false | tee lighting-app.build.log
 
 ## BRD4187C
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4187C disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
