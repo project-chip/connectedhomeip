0fba7ea6aec17c441c82c2687f218657a2d2ac80
shripad621git <shripad62001@gmail.com>
2023-10-04 14:55:52 +0530

    Made esp32_trace backend in sync with existing version

--- src/tracing/esp32_trace/esp32_tracing.cpp
+++ src/tracing/esp32_trace/esp32_tracing.cpp
@@ -17,52 +17,48 @@
  */
 
 #include "esp32_tracing.h"
+#include <esp_heap_caps.h>
 #include <esp_insights.h>
 #include <esp_log.h>
 #include <memory>
 #include <tracing/backend.h>
+
 namespace chip {
 namespace Tracing {
 namespace Insights {
 
-void ESP32Backend::LogMessageReceived(MessageReceivedInfo & info)
-{
-    ESP_LOGI("Matter", "Message Received");
-}
+#define LOG_HEAP_INFO(label, group, entry_exit)                                                                                    \
+    do                                                                                                                             \
+    {                                                                                                                              \
+        ESP_DIAG_EVENT("MTR_TRC", "%s - %s - %s Min Free heap - %u - LFB - %u Start free heap - %u", entry_exit, label, group,     \
+                       heap_caps_get_minimum_free_size(MALLOC_CAP_8BIT),                                                           \
+                       heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT),                                    \
+                       heap_caps_get_free_size(MALLOC_CAP_8BIT));                                                                  \
+    } while (0)
 
-void ESP32Backend::LogMessageSend(MessageSendInfo & info)
-{
-    ESP_LOGI("Data_Log", "Message Sent");
-}
+void ESP32Backend::LogMessageReceived(MessageReceivedInfo & info) {}
 
-void ESP32Backend::LogNodeLookup(NodeLookupInfo & info)
-{
-    ESP_LOGI("Data_Log", "Node Lookup");
-}
+void ESP32Backend::LogMessageSend(MessageSendInfo & info) {}
 
-void ESP32Backend::LogNodeDiscovered(NodeDiscoveredInfo & info)
-{
-    ESP_LOGI("Data_Log", "Log Node Discovered");
-}
+void ESP32Backend::LogNodeLookup(NodeLookupInfo & info) {}
 
-void ESP32Backend::LogNodeDiscoveryFailed(NodeDiscoveryFailedInfo & info)
-{
-    ESP_LOGI("Data_Log", "Log Node Discovery Failed");
-}
+void ESP32Backend::LogNodeDiscovered(NodeDiscoveredInfo & info) {}
+
+void ESP32Backend::LogNodeDiscoveryFailed(NodeDiscoveryFailedInfo & info) {}
 
 void ESP32Backend::TraceBegin(const char * label, const char * group)
 {
-    ESP_DIAG_EVENT("Trc", "Entry : %s -%s", label, group);
+    LOG_HEAP_INFO(label, group, "Entry");
 }
 
 void ESP32Backend::TraceEnd(const char * label, const char * group)
 {
-    ESP_DIAG_EVENT("Trc", "Exit : %s -%s", label, group);
+    LOG_HEAP_INFO(label, group, "Exit");
 }
 
 void ESP32Backend::TraceInstant(const char * label, const char * group)
 {
-    ESP_DIAG_EVENT("Trc", "Instant : %s -%s", label, group);
+    ESP_DIAG_EVENT("MTR_TRC", "Instant : %s -%s", label, group);
 }
 } // namespace Insights
 } // namespace Tracing
