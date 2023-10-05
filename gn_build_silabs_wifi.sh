32d8413f1a4f7a4b3f5e0a9ace0acdfc9e6ff507
Rohan S <3526930+brosahay@users.noreply.github.com>
2023-10-05 02:35:04 +0000

    updated build script

--- gn_build_silabs_wifi.sh
+++ gn_build_silabs_wifi.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 ## BRD4161A
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4161A disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/ BRD4161A disable_lcd=false use_external_flash=true | tee lighting-app.build.log
 
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_SiWx917 BRD4161A --wifi SiWx917 disable_lcd=false use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4161A --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4161A --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_SiWx917 BRD4161A --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=false | tee lighting-app.build.log
 
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_rs9116 BRD4161A --wifi rs9116 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_rs9116 BRD4161A --wifi rs9116 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_rs9116 BRD4161A --wifi rs9116 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_rs9116 BRD4161A --wifi rs9116 disable_lcd=false use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_rs9116 BRD4161A --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_rs9116 BRD4161A --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_rs9116 BRD4161A --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_rs9116 BRD4161A --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=false | tee lighting-app.build.log
 
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_wf200 BRD4161A --wifi wf200 disable_lcd=true use_external_flash=true chip_logging=false silabs_log_enabled=false is_debug=false | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_wf200 BRD4161A --wifi wf200 disable_lcd=true use_external_flash=false chip_logging=false silabs_log_enabled=false is_debug=false | tee lighting-app.build.log
@@ -21,17 +21,17 @@ set -e
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_wf200 BRD4161A --wifi wf200 disable_lcd=false use_external_flash=false chip_logging=false silabs_log_enabled=false is_debug=false | tee lighting-app.build.log
 
 ## BRD4187C
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4187C disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/ BRD4187C disable_lcd=false use_external_flash=true | tee lighting-app.build.log
 
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_SiWx917 BRD4187C --wifi SiWx917 disable_lcd=false use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_SiWx917 BRD4187C --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_SiWx917 BRD4187C --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_SiWx917 BRD4187C --wifi SiWx917 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=false | tee lighting-app.build.log
 
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_rs9116 BRD4187C --wifi rs9116 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_rs9116 BRD4187C --wifi rs9116 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_rs9116 BRD4187C --wifi rs9116 disable_lcd=false use_external_flash=true | tee lighting-app.build.log
-./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_rs9116 BRD4187C --wifi rs9116 disable_lcd=false use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_rs9116 BRD4187C --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_rs9116 BRD4187C --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=true use_external_flash=false | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_true_rs9116 BRD4187C --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=true | tee lighting-app.build.log
+./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_false_use_external_flash_false_rs9116 BRD4187C --wifi rs9116 chip_enable_ble_rs911x=true disable_lcd=false use_external_flash=false | tee lighting-app.build.log
 
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_true_wf200 BRD4187C --wifi wf200 disable_lcd=true use_external_flash=true | tee lighting-app.build.log
 ./scripts/examples/gn_silabs_example.sh ./examples/lighting-app/silabs ./out/debug/disable_lcd_true_use_external_flash_false_wf200 BRD4187C --wifi wf200 disable_lcd=true use_external_flash=false | tee lighting-app.build.log
