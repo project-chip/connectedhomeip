da127c4b698a0331d436e9834dba1b3a3c29d06f
Alex Tsitsiura <s07641069@gmail.com>
2023-10-08 14:18:14 +0300

    [Telink] fix builds

--- examples/lock-app/nrfconnect/main/include/CHIPProjectConfig.h
+++ examples/lock-app/nrfconnect/main/include/CHIPProjectConfig.h
@@ -30,3 +30,12 @@
 // Use a default pairing code if one hasn't been provisioned in flash.
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_PIN_CODE 20202021
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_DISCRIMINATOR 0xF00
+
+/**
+ * @def CHIP_DEVICE_CONFIG_ENABLE_SED
+ *
+ * @brief Defines if a matter device is acting as a Sleepy End Device(SED)
+ */
+#ifndef CHIP_DEVICE_CONFIG_ENABLE_SED
+#define CHIP_DEVICE_CONFIG_ENABLE_SED 0
+#endif
