f900f4694c79aba0a675e48ec81b2b2bd95e9d82
Alex Tsitsiura <s07641069@gmail.com>
2023-10-12 20:23:54 +0300

    [Telink] Fix OTA tests (#29725)
    * [Telink] Set default value of COMMON_LIBC_MALLOC_ARENA_SIZE
    
    * [Telink] InitBasicOTARequestor after kDnssdInitialized
    
    * [Telink] Move OTAUtil into lib
    
    * [Telink] restyled

--- src/platform/telink/OTAUtil.cpp
+++ src/platform/telink/OTAUtil.cpp
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2022-2023 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,15 +15,24 @@
  *    limitations under the License.
  */
 
+#ifdef CONFIG_CHIP_OTA_REQUESTOR
 #include <app/clusters/ota-requestor/BDXDownloader.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestor.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorDriver.h>
 #include <app/clusters/ota-requestor/DefaultOTARequestorStorage.h>
 #include <app/server/Server.h>
 #include <platform/telink/OTAImageProcessorImpl.h>
+#endif
+
+#if CONFIG_BOOTLOADER_MCUBOOT
+#include <app/server/Server.h>
+#include <zephyr/dfu/mcuboot.h>
+#endif
 
 using namespace chip;
 using namespace chip::DeviceLayer;
+
+#ifdef CONFIG_CHIP_OTA_REQUESTOR
 
 namespace {
 
@@ -44,4 +53,22 @@ void InitBasicOTARequestor()
     sOTARequestor.Init(Server::GetInstance(), sOTARequestorStorage, sOTARequestorDriver, sBDXDownloader);
     chip::SetRequestorInstance(&sOTARequestor);
     sOTARequestorDriver.Init(&sOTARequestor, &sOTAImageProcessor);
+}
+
+#endif
+
+void OtaConfirmNewImage()
+{
+    if (mcuboot_swap_type() == BOOT_SWAP_TYPE_REVERT)
+    {
+        CHIP_ERROR err = System::MapErrorZephyr(boot_write_img_confirmed());
+        if (CHIP_NO_ERROR == err)
+        {
+            ChipLogProgress(SoftwareUpdate, "New firmware image confirmed");
+        }
+        else
+        {
+            ChipLogError(SoftwareUpdate, "Failed to confirm firmware image, it will be reverted on the next boot");
+        }
+    }
 }
