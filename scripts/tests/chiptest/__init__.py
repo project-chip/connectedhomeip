fadb9d38334c4d8ffcc87de0308c539cbcc7db5c
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2023-10-13 11:23:19 -0400

    Disabled test 2_2 for Repl as it used unsupported commands for this test and fixed the yaml for Darwin-framework-tool

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -175,6 +175,7 @@ def _GetChipReplUnsupportedTests() -> Set[str]:
         "Test_TC_ACE_1_6.yaml",    # Test fails only in chip-repl: Refer--> https://github.com/project-chip/connectedhomeip/pull/27910#issuecomment-1632485584
         "Test_TC_IDM_1_2.yaml",              # chip-repl does not support AnyCommands (19/07/2023)
         "TestGroupKeyManagementCluster.yaml",  # chip-repl does not support EqualityCommands (2023-08-04)
+        "Test_TC_S_2_2.yaml",              # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_MOD_3_1.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_MOD_3_2.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
         "Test_TC_MOD_3_3.yaml",            # chip-repl does not support EqualityCommands pseudo-cluster
