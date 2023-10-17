82d79deb9259b8c5f957aa6d3c3cd5dd06bcfb92
Justin Wood <woody@apple.com>
2023-10-17 15:01:58 -0700

    Initial checkin

---
+++ scripts/tagging/tag_new_release.sh
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2020 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#!/bin/bash
+
+CURRENT_SPEC_VERSION=$(cat SPECIFICATION_VERSION)
+
+CURRENT_RELEASE=$(gh release list --exclude-pre-releases | grep -Fi $CURRENT_SPEC_VERSION | awk '{print $1}' | head -n1)
+
+if [ -z "$CURRENT_RELEASE" ]; then
+    SDK_RELEASE_REVISIONS=0
+    echo "No revision found for current release"
+else
+    SDK_RELEASE_REVISIONS="$(echo $CURRENT_RELEASE | cut -d'.' -f4)"
+fi
+
+if [ $SDK_RELEASE_REVISIONS != 0 ]; then
+    SDK_RELEASE_REVISIONS=$(($SDK_RELEASE_REVISIONS + 1))
+fi
+
+NEW_RELEASE_TAG="v$CURRENT_SPEC_VERSION.$SDK_RELEASE_REVISIONS"
+
+echo "Current release: $CURRENT_RELEASE"
+echo "SDK release revisions: $SDK_RELEASE_REVISIONS"
+echo "New release: $NEW_RELEASE_TAG"
+
+gh release create "$NEW_RELEASE_TAG" $@
