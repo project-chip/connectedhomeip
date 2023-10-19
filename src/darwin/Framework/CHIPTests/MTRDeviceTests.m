c8b79f100414f1401dd645512076cac9a684b024
Justin Wood <woody@apple.com>
2023-10-19 11:14:13 -0700

    Revert "Ensure that MTRDevice invokes provide a timed invoke timeout when needed. (#29855)"
    This reverts commit 4e13b93caa8ee5c12ca6422d89ee037695800de4.

--- src/darwin/Framework/CHIPTests/MTRDeviceTests.m
+++ src/darwin/Framework/CHIPTests/MTRDeviceTests.m
@@ -2240,15 +2240,12 @@ static void (^globalReportHandler)(id _Nullable values, NSError * _Nullable erro
                                  {
                                      XCTAssertTrue([values isKindOfClass:[NSArray class]]);
                                      NSArray * resultArray = values;
-                                     XCTAssertEqual(resultArray.count, 1);
                                      for (NSDictionary * result in resultArray) {
                                          MTRCommandPath * path = result[@"commandPath"];
                                          XCTAssertEqualObjects(path.endpoint, @1);
                                          XCTAssertEqualObjects(path.cluster, @6);
                                          XCTAssertEqualObjects(path.command, @1);
                                          XCTAssertNil(result[@"error"]);
-                                         // This command just has a status response.
-                                         XCTAssertNil(result[@"value"]);
                                      }
                                      XCTAssertEqual([resultArray count], 1);
                                  }
