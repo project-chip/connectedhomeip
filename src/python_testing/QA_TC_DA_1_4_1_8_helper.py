60d2f18990d3307a4f8af1d7a1fa8c49e1fc8f6e
cecille <cecille@google.com>
2023-10-23 14:10:33 -0400

    fixes

--- src/python_testing/QA_TC_DA_1_4_1_8_helper.py
+++ src/python_testing/QA_TC_DA_1_4_1_8_helper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S python3 -B
 #
-#    Copyright (c) 2022 Project CHIP Authors
+#    Copyright (c) 2023 Project CHIP Authors
 #    All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
@@ -51,13 +51,22 @@ def main(app: str, chip_tool: str, out_dir: str, test_case: str, manual: bool):
     success_tc = test_case == '1.8'
     cert_path = os.path.abspath(os.path.join(CHIP_ROOT, 'credentials/development/commissioner_dut'))
 
-    # struct_cd_version_number_wrong - excluded because this is a DCL test not covered by cert
-    # struct_cd_cert_id_mismatch - excluded because this is a DCL test not covered by cert
-    exclude_cases = ['struct_cd_version_number_wrong', 'struct_cd_cert_id_mismatch']
+    # The following test vectors are success conditions for an SDK commissioner for the following reasons:
+    # struct_cd_device_type_id_mismatch - requires DCL access, which the SDK does not have and is not required
+    # struct_cd_security_info_wrong - while devices are required to set this to 0, commissioners are required to ignore it
+    #                                 (see 6.3.1)
+    #                                 hence this is marked as a failure for devices, but should be a success case for
+    #                                 commissioners
+    # struct_cd_security_level_wrong - as with security info, commissioners are required to ignore this value (see 6.3.1)
+    # struct_cd_version_number_wrong - this value is not meant to be interpreted by commissioners, so errors here should be
+    #                                  ignored (6.3.1)
+    # struct_cd_cert_id_mismatch - requires DCL access, which the SDK does not have and is not required.
+    exclude_cases = ['struct_cd_device_type_id_mismatch', 'struct_cd_security_info_wrong',
+                     'struct_cd_security_level_wrong', 'struct_cd_version_number_wrong', 'struct_cd_cert_id_mismatch']
 
     subprocess.call(f'mkdir -p {out_dir}', shell=True)
     results = {}
-    for p in os.listdir(cert_path):
+    for p in sorted(os.listdir(cert_path)):
         if str(p) in exclude_cases:
             continue
 
@@ -82,7 +91,7 @@ def main(app: str, chip_tool: str, out_dir: str, test_case: str, manual: bool):
             if manual:
                 input_done = False
                 while not input_done:
-                    resp = input('Please commissiong the DUT. Please input Y if the commissioning is successful, N if it is unsuccessful')
+                    resp = input('Please commission the DUT. Please input Y if the commissioning is successful, N if it is unsuccessful')
                     if resp == 'y' or resp == "Y":
                         commissioning_ok = True
                         input_done = True
@@ -106,8 +115,10 @@ def main(app: str, chip_tool: str, out_dir: str, test_case: str, manual: bool):
 
     summary_file_name = f'{out_dir}/summary_{test_case}.log'
 
-    print(results)
+    test_passed = all(results.values())
+    print(f"\n Test passed? {test_passed}\n")
     with open(summary_file_name, 'w') as summary_file:
+        summary_file.write(f"Test passed? {test_passed}\n\n")
         for k, v in results.items():
             passed = "PASSED" if v is True else "FAILED"
             summary_file.write(f'{k}:{passed}\n')
