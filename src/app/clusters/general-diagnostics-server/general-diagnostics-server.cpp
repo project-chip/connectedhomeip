c331529e251f8b8d69777e4bc5d0f7de5b130a84
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2023-10-31 11:30:17 -0400

    Remove AverageWearCount

--- src/app/clusters/general-diagnostics-server/general-diagnostics-server.cpp
+++ src/app/clusters/general-diagnostics-server/general-diagnostics-server.cpp
@@ -204,9 +204,6 @@ CHIP_ERROR GeneralDiagosticsAttrAccess::Read(const ConcreteReadAttributePath & a
         bool isTestEventTriggersEnabled = IsTestEventTriggerEnabled();
         return aEncoder.Encode(isTestEventTriggersEnabled);
     }
-    case AverageWearCount::Id: {
-        return ReadIfSupported(&DiagnosticDataProvider::GetAverageWearCount, aEncoder);
-    }
     default: {
         break;
     }
