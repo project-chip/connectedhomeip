427ee2b757b0a7045d1a4b37fe7afcf577a94aee
Arkadiusz Bokowy <a.bokowy@samsung.com>
2023-11-03 09:11:11 +0100

    Add missing license header

--- src/controller/python/chip/ble/LinuxImpl.cpp
+++ src/controller/python/chip/ble/LinuxImpl.cpp
@@ -1,3 +1,20 @@
+/*
+ *
+ *    Copyright (c) 2020-2023 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 #include <lib/support/CHIPMem.h>
 #include <platform/CHIPDeviceLayer.h>
@@ -125,18 +142,12 @@ extern "C" void * pychip_ble_start_scanning(PyObject * context, void * adapter, 
         std::make_unique<ScannerDelegateImpl>(context, scanCallback, completeCallback, errorCallback);
 
     CHIP_ERROR err = delegate->ScannerInit(static_cast<BluezAdapter1 *>(adapter));
-    if (err != CHIP_NO_ERROR)
-    {
-        return nullptr;
-    }
+    VerifyOrReturnError(err == CHIP_NO_ERROR, nullptr);
 
     chip::DeviceLayer::PlatformMgr().LockChipStack();
     err = delegate->ScannerStartScan(chip::System::Clock::Milliseconds32(timeoutMs));
     chip::DeviceLayer::PlatformMgr().UnlockChipStack();
-    if (err != CHIP_NO_ERROR)
-    {
-        return nullptr;
-    }
+    VerifyOrReturnError(err == CHIP_NO_ERROR, nullptr);
 
     return delegate.release();
 }
