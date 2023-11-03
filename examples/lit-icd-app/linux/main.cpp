3efb8e4e5ab29b27c54ce591634f111e2c81a8a1
mkardous-silabs <84793247+mkardous-silabs@users.noreply.github.com>
2023-11-03 17:07:56 -0400

    [ICD] Add LIT ICD reference app for tests and certification (#30092)
    * Initial example creation
    
    * intial clean up
    
    * update build.gb
    
    * update build configs
    
    * correct build.gn
    
    * Add example to CI
    
    * zap regen
    
    * update linux targets
    
    * Add default config for user active trigger
    
    * remove clusters that shouldn't be on endpoint 0
    
    * fix device conformance

---
+++ examples/lit-icd-app/linux/main.cpp
@@ -0,0 +1,40 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "AppMain.h"
+#include <app-common/zap-generated/ids/Clusters.h>
+
+using namespace chip;
+using namespace chip::app;
+
+void ApplicationInit() {}
+
+void ApplicationShutdown() {}
+
+int main(int argc, char * argv[])
+{
+    VerifyOrDie(ChipLinuxAppInit(argc, argv) == 0);
+    ChipLinuxAppMainLoop();
+    return 0;
+}
+
+void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & attributePath, uint8_t type, uint16_t size,
+                                       uint8_t * value)
+{
+    // TODO: Watch ICDM Cluster changes
+}
