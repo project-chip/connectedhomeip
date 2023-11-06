ce5e7fbeaa30dd48ace440237c0fbbfd547a07d9
Andrei Litvin <andy314@gmail.com>
2023-11-06 10:00:27 -0500

    Match `Media Playback Cluster` to spec (#30217)
    * move MediaPlaybackStatusEnum to StatusEnum
    
    * Add compat shim
    
    * Zap regen
    
    * Replace enum name to the new name
    
    * Fix Darwin availability annotations.
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
+++ zzz_generated/app-common/app-common/zap-generated/cluster-enums.h
@@ -3801,22 +3801,6 @@ enum class TargetNavigatorStatusEnum : uint8_t
 
 namespace MediaPlayback {
 
-// Enum for MediaPlaybackStatusEnum
-enum class MediaPlaybackStatusEnum : uint8_t
-{
-    kSuccess                = 0x00,
-    kInvalidStateForCommand = 0x01,
-    kNotAllowed             = 0x02,
-    kNotActive              = 0x03,
-    kSpeedOutOfRange        = 0x04,
-    kSeekOutOfRange         = 0x05,
-    // All received enum values that are not listed above will be mapped
-    // to kUnknownEnumValue. This is a helper enum value that should only
-    // be used by code to process how it handles receiving and unknown
-    // enum value. This specific should never be transmitted.
-    kUnknownEnumValue = 6,
-};
-
 // Enum for PlaybackStateEnum
 enum class PlaybackStateEnum : uint8_t
 {
@@ -3829,6 +3813,22 @@ enum class PlaybackStateEnum : uint8_t
     // be used by code to process how it handles receiving and unknown
     // enum value. This specific should never be transmitted.
     kUnknownEnumValue = 4,
+};
+
+// Enum for StatusEnum
+enum class StatusEnum : uint8_t
+{
+    kSuccess                = 0x00,
+    kInvalidStateForCommand = 0x01,
+    kNotAllowed             = 0x02,
+    kNotActive              = 0x03,
+    kSpeedOutOfRange        = 0x04,
+    kSeekOutOfRange         = 0x05,
+    // All received enum values that are not listed above will be mapped
+    // to kUnknownEnumValue. This is a helper enum value that should only
+    // be used by code to process how it handles receiving and unknown
+    // enum value. This specific should never be transmitted.
+    kUnknownEnumValue = 6,
 };
 
 // Bitmap for Feature
