05260ab128c2f4b386f0c020b58923ccff85cf7b
Restyled.io <commits@restyled.io>
2023-11-11 06:39:23 +0000

    Restyled by shfmt

--- src/tools/interop/idt/capture/platform/android/streams/pcap/mac_build_tcpdump_64.sh
+++ src/tools/interop/idt/capture/platform/android/streams/pcap/mac_build_tcpdump_64.sh
@@ -16,20 +16,20 @@ OUT_DIR=$TMP_DIR/out
 
 # Function to download and extract archives
 download_and_extract() {
-  local url="$1"
-  local filepath="$2"
-  local tar_dir="$3"
+    local url="$1"
+    local filepath="$2"
+    local tar_dir="$3"
 
-  wget -O "$filepath" "$url"
-  tar -C "$tar_dir" -zxvf "$filepath"
+    wget -O "$filepath" "$url"
+    tar -C "$tar_dir" -zxvf "$filepath"
 }
 
 # Function to clean up downloaded and extracted files
 cleanup() {
-  local filepath="$1"
-  local dirpath="$2"
+    local filepath="$1"
+    local dirpath="$2"
 
-  rm -rf "$filepath" "$dirpath"
+    rm -rf "$filepath" "$dirpath"
 }
 
 # Cross-compile libpcap
@@ -38,7 +38,7 @@ LIBPCAP_DIR=$TMP_DIR/libpcap-$LIBPCAP_VERSION
 LIBPCAP_ARCHIVE=$TMP_DIR/libpcap-$LIBPCAP_VERSION.tar.gz
 
 download_and_extract "https://www.tcpdump.org/release/libpcap-$LIBPCAP_VERSION.tar.gz" "$LIBPCAP_ARCHIVE" "$TMP_DIR"
-( cd "$LIBPCAP_DIR" && ./configure --prefix="$OUT_DIR" --host="$TARGET_ARCH" --with-pcap=linux )
+(cd "$LIBPCAP_DIR" && ./configure --prefix="$OUT_DIR" --host="$TARGET_ARCH" --with-pcap=linux)
 make -C "$LIBPCAP_DIR" -j"$(nproc)"
 make -C "$LIBPCAP_DIR" install
 cleanup "$LIBPCAP_ARCHIVE" "$LIBPCAP_DIR"
@@ -49,7 +49,7 @@ TCPDUMP_DIR=$TMP_DIR/tcpdump-$TCPDUMP_VERSION
 TCPDUMP_ARCHIVE=$TMP_DIR/tcpdump-$TCPDUMP_VERSION.tar.gz
 
 download_and_extract "https://www.tcpdump.org/release/tcpdump-$TCPDUMP_VERSION.tar.gz" "$TCPDUMP_ARCHIVE" "$TMP_DIR"
-( cd "$TCPDUMP_DIR" && CFLAGS="-static -I$OUT_DIR/include" CPPFLAGS="-static" LDFLAGS="-static -L$OUT_DIR/lib" ./configure --prefix="$OUT_DIR" --host="$TARGET_ARCH" )
+(cd "$TCPDUMP_DIR" && CFLAGS="-static -I$OUT_DIR/include" CPPFLAGS="-static" LDFLAGS="-static -L$OUT_DIR/lib" ./configure --prefix="$OUT_DIR" --host="$TARGET_ARCH")
 make -C "$TCPDUMP_DIR"
 make -C "$TCPDUMP_DIR" install
 cleanup "$TCPDUMP_ARCHIVE" "$TCPDUMP_DIR"
