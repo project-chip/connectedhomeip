e427e21fc2410538896f2d574bcb3bbe8588db7d
Restyled.io <commits@restyled.io>
2023-11-14 20:11:31 +0000

    Restyled by whitespace

--- src/app/icd/ICDManager.h
+++ src/app/icd/ICDManager.h
@@ -47,7 +47,7 @@ public:
         ~ObserverPointer() { mObserver = nullptr; }
         ICDStateObserver * mObserver;
     };
-    
+
     enum class OperationalState : uint8_t
     {
         IdleMode,
