4d516e4274df564624ec13314b1d42d29b62cf5f
Sandeep Mistry <sandeep.mistry@arm.com>
2023-11-15 14:06:31 -0500

    Apply restyler patch

--- scripts/tests/avh/helpers/avh_lighting_app_instance.py
+++ scripts/tests/avh/helpers/avh_lighting_app_instance.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 import time
+from datetime import datetime
 
 import websocket
 
