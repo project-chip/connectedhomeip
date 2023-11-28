903710dc50462a520e3aa3bf031bf2babf2e95c1
Mathieu Kardous <mathieu.kardous@silabs.com>
2023-11-28 11:20:52 -0500

    fix ci

--- src/app/chip_data_model.cmake
+++ src/app/chip_data_model.cmake
@@ -140,6 +140,7 @@ function(chip_configure_data_model APP_TARGET)
         ${CHIP_APP_BASE_DIR}/util/binding-table.cpp
         ${CHIP_APP_BASE_DIR}/icd/ICDMonitoringTable.cpp
         ${CHIP_APP_BASE_DIR}/icd/ICDManagementServer.cpp
+        ${CHIP_APP_BASE_DIR}/icd/ICDNotifier.cpp
         ${CHIP_APP_BASE_DIR}/util/DataModelHandler.cpp
         ${CHIP_APP_BASE_DIR}/util/ember-compatibility-functions.cpp
         ${CHIP_APP_BASE_DIR}/util/generic-callback-stubs.cpp
