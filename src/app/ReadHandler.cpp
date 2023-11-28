6778d3e5088fb535238c5448889a47b0bf8db2d2
mkardous-silabs <84793247+mkardous-silabs@users.noreply.github.com>
2023-11-28 17:39:41 -0500

    [ICD] Create ICDConfigurationData class with associated refactor (#30618)

--- src/app/ReadHandler.cpp
+++ src/app/ReadHandler.cpp
@@ -36,7 +36,7 @@
 #include <app/reporting/Engine.h>
 
 #if CHIP_CONFIG_ENABLE_ICD_SERVER
-#include <app/icd/ICDManagementServer.h> // nogncheck
+#include <app/icd/ICDConfigurationData.h> //nogncheck
 #endif
 
 namespace chip {
@@ -46,7 +46,7 @@ using Status = Protocols::InteractionModel::Status;
 uint16_t ReadHandler::GetPublisherSelectedIntervalLimit()
 {
 #if CHIP_CONFIG_ENABLE_ICD_SERVER
-    return static_cast<uint16_t>(ICDManagementServer::GetInstance().GetIdleModeDurationSec());
+    return static_cast<uint16_t>(ICDConfigurationData::GetInstance().GetIdleModeDurationSec());
 #else
     return kSubscriptionMaxIntervalPublisherLimit;
 #endif
