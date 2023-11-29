fb97fd6791fea2bdd21c331b292f3256f1a8213f
Jeff Tung <100387939+jtung-apple@users.noreply.github.com>
2023-11-29 13:10:35 -0800

    Restyled fix

--- src/darwin/Framework/CHIP/MTRBaseDevice.mm
+++ src/darwin/Framework/CHIP/MTRBaseDevice.mm
@@ -332,7 +332,7 @@ public:
         ErrorCallback errorCallback, MTRDeviceResubscriptionScheduledHandler _Nullable resubscriptionScheduledHandler,
         MTRSubscriptionEstablishedHandler _Nullable subscriptionEstablishedHandler, OnDoneHandler _Nullable onDoneHandler)
         : MTRBaseSubscriptionCallback(attributeReportCallback, eventReportCallback, errorCallback, resubscriptionScheduledHandler,
-              subscriptionEstablishedHandler, onDoneHandler)
+            subscriptionEstablishedHandler, onDoneHandler)
     {
     }
 
