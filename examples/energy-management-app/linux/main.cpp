e12cf09b6470e67b3313fa933978f5107a2fcb32
James Harrow <james.harrow@gmail.com>
2023-12-04 10:44:05 +0000

    Fixes #30727 - Added initial EVSE cluster and Example Energy Managament app.

---
+++ examples/energy-management-app/linux/main.cpp
@@ -0,0 +1,97 @@
+/*
+ *
+ *    Copyright (c) 2023 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <AppMain.h>
+#include <EVSEManufacturerImpl.h>
+#include <EnergyEvseManager.h>
+#include <EnergyManagementManager.h>
+
+#include <app-common/zap-generated/ids/Attributes.h>
+#include <app-common/zap-generated/ids/Clusters.h>
+#include <app/ConcreteAttributePath.h>
+#include <app/clusters/network-commissioning/network-commissioning.h>
+#include <app/server/Server.h>
+#include <lib/support/logging/CHIPLogging.h>
+#include <platform/Linux/NetworkCommissioningDriver.h>
+
+#define ENERGY_EVSE_ENDPOINT 1
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+
+static EnergyEvseDelegate * gDelegate       = nullptr;
+static EnergyEvseManager * gInstance        = nullptr;
+static EVSEManufacturer * gEvseManufacturer = nullptr;
+
+void ApplicationInit()
+{
+    if ((gDelegate == nullptr) && (gInstance == nullptr))
+    {
+        gDelegate = new EnergyEvseDelegate();
+        if (gDelegate != nullptr)
+        {
+            gInstance = new EnergyEvseManager(EndpointId(ENERGY_EVSE_ENDPOINT), *gDelegate);
+            gInstance->Init(); /* Register Attribute & Command handlers */
+        }
+    }
+    else
+    {
+        ChipLogError(AppServer, "EVSE Instance or Delegate already exist.")
+    }
+
+    if (gEvseManufacturer == nullptr)
+    {
+        gEvseManufacturer = new EVSEManufacturer();
+        gEvseManufacturer->Init(gInstance);
+    }
+    else
+    {
+        ChipLogError(AppServer, "EVSEManufacturer already exists.")
+    }
+}
+
+void ApplicationShutdown()
+{
+    ChipLogDetail(AppServer, "Energy Management App: ApplicationShutdown()");
+
+    /* Shutdown the EVSEManufacturer*/
+    gEvseManufacturer->Shutdown(gInstance);
+
+    /* Shutdown the Instance - deregister attribute & command handler */
+    gInstance->Shutdown();
+
+    delete gEvseManufacturer;
+    delete gInstance;
+    delete gDelegate;
+    gEvseManufacturer = nullptr;
+    gInstance         = nullptr;
+    gDelegate         = nullptr;
+}
+
+int main(int argc, char * argv[])
+{
+    if (ChipLinuxAppInit(argc, argv) != 0)
+    {
+        return -1;
+    }
+
+    ChipLinuxAppMainLoop();
+
+    return 0;
+}
---
+++ src/app/clusters/energy-evse-server/energy-evse-server.cpp
@@ -0,0 +1,330 @@
+/*
+ *    Copyright (c) 2023 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "energy-evse-server.h"
+
+#include <app/AttributeAccessInterface.h>
+#include <app/ConcreteAttributePath.h>
+#include <app/InteractionModelEngine.h>
+#include <app/util/attribute-storage.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::EnergyEvse;
+using namespace chip::app::Clusters::EnergyEvse::Attributes;
+using chip::Protocols::InteractionModel::Status;
+
+namespace chip {
+namespace app {
+namespace Clusters {
+namespace EnergyEvse {
+
+Instance::Instance(EndpointId aEndpointId, Delegate & aDelegate) :
+    AttributeAccessInterface(MakeOptional(aEndpointId), Id), CommandHandlerInterface(MakeOptional(aEndpointId), Id),
+    mDelegate(aDelegate), mEndpointId(aEndpointId)
+{
+    /* set the base class delegates endpointId */
+    mDelegate.SetEndpointId(aEndpointId);
+}
+
+Instance::~Instance()
+{
+    Shutdown();
+}
+
+CHIP_ERROR Instance::Init()
+{
+    registerAttributeAccessOverride(this);
+    InteractionModelEngine::GetInstance()->RegisterCommandHandler(this);
+    return CHIP_NO_ERROR;
+}
+
+void Instance::Shutdown()
+{
+    unregisterAttributeAccessOverride(this);
+    InteractionModelEngine::GetInstance()->UnregisterCommandHandler(this);
+}
+
+bool Instance::HasFeature(Feature aFeature) const
+{
+    return mFeature.Has(aFeature);
+}
+
+// AttributeAccessInterface
+CHIP_ERROR Instance::Read(const ConcreteReadAttributePath & aPath, AttributeValueEncoder & aEncoder)
+{
+    switch (aPath.mAttributeId)
+    {
+    case State::Id:
+        return aEncoder.Encode(mDelegate.GetState());
+    case SupplyState::Id:
+        return aEncoder.Encode(mDelegate.GetSupplyState());
+    case FaultState::Id:
+        return aEncoder.Encode(mDelegate.GetFaultState());
+    case ChargingEnabledUntil::Id:
+        return aEncoder.Encode(mDelegate.GetChargingEnabledUntil());
+    case DischargingEnabledUntil::Id:
+        /* V2X */
+        return aEncoder.Encode(mDelegate.GetDischargingEnabledUntil());
+    case CircuitCapacity::Id:
+        return aEncoder.Encode(mDelegate.GetCircuitCapacity());
+    case MinimumChargeCurrent::Id:
+        return aEncoder.Encode(mDelegate.GetMinimumChargeCurrent());
+    case MaximumChargeCurrent::Id:
+        return aEncoder.Encode(mDelegate.GetMaximumChargeCurrent());
+    case MaximumDischargeCurrent::Id:
+        /* V2X */
+        return aEncoder.Encode(mDelegate.GetMaximumDischargeCurrent());
+    case UserMaximumChargeCurrent::Id:
+        /* PREF */
+        return aEncoder.Encode(mDelegate.GetUserMaximumChargeCurrent());
+    case RandomizationDelayWindow::Id:
+        /* Optional */
+        return aEncoder.Encode(mDelegate.GetRandomizationDelayWindow());
+
+    /* PREF attributes */
+    case NumberOfWeeklyTargets::Id:
+        return aEncoder.Encode(mDelegate.GetNumberOfWeeklyTargets());
+    case NumberOfDailyTargets::Id:
+        return aEncoder.Encode(mDelegate.GetNumberOfDailyTargets());
+    case NextChargeStartTime::Id:
+        return aEncoder.Encode(mDelegate.GetNextChargeStartTime());
+    case NextChargeTargetTime::Id:
+        return aEncoder.Encode(mDelegate.GetNextChargeTargetTime());
+
+    case NextChargeRequiredEnergy::Id:
+        return aEncoder.Encode(mDelegate.GetNextChargeRequiredEnergy());
+    case NextChargeTargetSoC::Id:
+        return aEncoder.Encode(mDelegate.GetNextChargeTargetSoC());
+    case ApproximateEVEfficiency::Id:
+        return aEncoder.Encode(mDelegate.GetApproximateEVEfficiency());
+
+    /* SOC attributes */
+    case StateOfCharge::Id:
+        return aEncoder.Encode(mDelegate.GetStateOfCharge());
+    case BatteryCapacity::Id:
+        return aEncoder.Encode(mDelegate.GetBatteryCapacity());
+
+    /* PNC attributes*/
+    case VehicleID::Id:
+        // TODO return aEncoder.Encode(mDelegate.GetVehicleID());
+
+    /* Session SESS attributes */
+    case SessionID::Id:
+        return aEncoder.Encode(mDelegate.GetSessionID());
+    case SessionDuration::Id:
+        return aEncoder.Encode(mDelegate.GetSessionDuration());
+    case SessionEnergyCharged::Id:
+        return aEncoder.Encode(mDelegate.GetSessionEnergyCharged());
+    case SessionEnergyDischarged::Id:
+        return aEncoder.Encode(mDelegate.GetSessionEnergyDischarged());
+    }
+    /* Allow all other unhandled attributes to fall through to Ember */
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR Instance::Write(const ConcreteDataAttributePath & aPath, AttributeValueDecoder & aDecoder)
+{
+    switch (aPath.mAttributeId)
+    {
+    case UserMaximumChargeCurrent::Id: {
+        int64_t newValue;
+        ReturnErrorOnFailure(aDecoder.Decode(newValue));
+        ReturnErrorOnFailure(mDelegate.SetUserMaximumChargeCurrent(newValue));
+        return CHIP_NO_ERROR;
+    }
+    case RandomizationDelayWindow::Id: {
+        uint32_t newValue;
+        ReturnErrorOnFailure(aDecoder.Decode(newValue));
+        ReturnErrorOnFailure(mDelegate.SetRandomizationDelayWindow(newValue));
+        return CHIP_NO_ERROR;
+    }
+    case ApproximateEVEfficiency::Id: {
+        uint16_t newValue;
+        ReturnErrorOnFailure(aDecoder.Decode(newValue));
+        ReturnErrorOnFailure(mDelegate.SetApproximateEVEfficiency(newValue));
+        return CHIP_NO_ERROR;
+    }
+
+    default:
+        // Unknown attribute; return error.  None of the other attributes for
+        // this cluster are writable, so should not be ending up in this code to
+        // start with.
+        return CHIP_IM_GLOBAL_STATUS(UnsupportedAttribute);
+    }
+    return CHIP_NO_ERROR;
+}
+
+// CommandHandlerInterface
+
+CHIP_ERROR Instance::EnumerateAcceptedCommands(const ConcreteClusterPath & cluster, CommandIdCallback callback, void * context)
+{
+    using namespace Commands;
+
+    for (auto && id : {
+             Disable::Id,
+             EnableCharging::Id,
+             EnableDischarging::Id,
+             StartDiagnostics::Id,
+             SetTargets::Id,
+             GetTargets::Id,
+             ClearTargets::Id,
+         })
+    {
+        if (callback(id, context) == Loop::Break)
+        {
+            return CHIP_NO_ERROR;
+        }
+    }
+
+    return CHIP_NO_ERROR;
+}
+
+void Instance::InvokeCommand(HandlerContext & handlerContext)
+{
+    using namespace Commands;
+
+    switch (handlerContext.mRequestPath.mCommandId)
+    {
+    case Disable::Id:
+        HandleCommand<Disable::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleDisable(ctx, commandData); });
+        return;
+    case EnableCharging::Id:
+        HandleCommand<EnableCharging::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleEnableCharging(ctx, commandData); });
+        return;
+    case EnableDischarging::Id:
+        HandleCommand<EnableDischarging::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleEnableDischarging(ctx, commandData); });
+        return;
+    case StartDiagnostics::Id:
+        HandleCommand<StartDiagnostics::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleStartDiagnostics(ctx, commandData); });
+        return;
+    case SetTargets::Id:
+        HandleCommand<SetTargets::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleSetTargets(ctx, commandData); });
+        return;
+    case GetTargets::Id:
+        HandleCommand<GetTargets::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleGetTargets(ctx, commandData); });
+        return;
+    case ClearTargets::Id:
+        HandleCommand<ClearTargets::DecodableType>(
+            handlerContext, [this](HandlerContext & ctx, const auto & commandData) { HandleClearTargets(ctx, commandData); });
+        return;
+    }
+}
+
+void Instance::HandleDisable(HandlerContext & ctx, const Commands::Disable::DecodableType & commandData)
+{
+    // No parameters for this command
+    // Call the delegate
+    Status status = mDelegate.Disable();
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+
+void Instance::HandleEnableCharging(HandlerContext & ctx, const Commands::EnableCharging::DecodableType & commandData)
+{
+    auto & chargingEnabledUntil = commandData.chargingEnabledUntil;
+    auto & minimumChargeCurrent = commandData.minimumChargeCurrent;
+    auto & maximumChargeCurrent = commandData.maximumChargeCurrent;
+
+    if ((minimumChargeCurrent < 0) || (minimumChargeCurrent > kMaximumChargeCurrent))
+    {
+        ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::ConstraintError);
+        return;
+    }
+
+    if ((maximumChargeCurrent < 0) || (maximumChargeCurrent > kMaximumChargeCurrent))
+    {
+        ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::ConstraintError);
+        return;
+    }
+
+    if (minimumChargeCurrent > maximumChargeCurrent)
+    {
+        ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::ConstraintError);
+        return;
+    }
+
+    // Call the delegate
+    Status status = mDelegate.EnableCharging(chargingEnabledUntil, minimumChargeCurrent, maximumChargeCurrent);
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+
+void Instance::HandleEnableDischarging(HandlerContext & ctx, const Commands::EnableDischarging::DecodableType & commandData)
+{
+
+    auto & dischargingEnabledUntil = commandData.dischargingEnabledUntil;
+    auto & maximumDischargeCurrent = commandData.maximumDischargeCurrent;
+
+    if ((maximumDischargeCurrent < 0) || (maximumDischargeCurrent > kMaximumChargeCurrent))
+    {
+        ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::ConstraintError);
+        return;
+    }
+
+    // Call the delegate
+    Status status = mDelegate.EnableDischarging(dischargingEnabledUntil, maximumDischargeCurrent);
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+void Instance::HandleStartDiagnostics(HandlerContext & ctx, const Commands::StartDiagnostics::DecodableType & commandData)
+{
+    // No parameters for this command
+    // Call the delegate
+    Status status = mDelegate.StartDiagnostics();
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+
+void Instance::HandleSetTargets(HandlerContext & ctx, const Commands::SetTargets::DecodableType & commandData)
+{
+    // Call the delegate
+    // TODO
+    // Status status = mDelegate.SetTargets();
+    Status status = Status::UnsupportedCommand;
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+void Instance::HandleGetTargets(HandlerContext & ctx, const Commands::GetTargets::DecodableType & commandData)
+{
+    // Call the delegate
+    // TODO
+    // Status status = mDelegate.GetTargets();
+    Status status = Status::UnsupportedCommand;
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+void Instance::HandleClearTargets(HandlerContext & ctx, const Commands::ClearTargets::DecodableType & commandData)
+{
+    // Call the delegate
+    // TODO
+    // Status status = mDelegate.ClearTargets();
+    Status status = Status::UnsupportedCommand;
+
+    ctx.mCommandHandler.AddStatus(ctx.mRequestPath, status);
+}
+
+} // namespace EnergyEvse
+} // namespace Clusters
+} // namespace app
+} // namespace chip
