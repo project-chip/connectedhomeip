4e354085f097ced88ac6b9fc822c4ab0d48318b8
yunhanw-google <yunhanw@google.com>
2023-12-05 12:17:16 -0800

    Update src/app/ReadClient.h
    Co-authored-by: mkardous-silabs <84793247+mkardous-silabs@users.noreply.github.com>

--- src/app/ReadClient.h
+++ src/app/ReadClient.h
@@ -645,7 +645,7 @@ private:
     System::Clock::Timeout mLivenessTimeoutOverride = System::Clock::kZero;
 
     // mPeerActiveUntilTimestamp represents the active time for subscription for ICD device, when the liveness timeout fired after
-    // mPeerActiveUntilTimestamp`, the ReadClient will enter "IdleSubscription" state
+    // the mPeerActiveUntilTimestamp, the ReadClient will enter "IdleSubscription" state
     // until OnActiveModeNotification() is called. The Zero value means the device should always be active.
     System::Clock::Timeout mPeerActivePeriod           = System::Clock::kZero;
     System::Clock::Timestamp mPeerActiveUntilTimestamp = System::Clock::kZero;
