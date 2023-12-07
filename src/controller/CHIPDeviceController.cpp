b9e801dac0f0d4e6a320a71fd8cf02f8c6c077e6
Song Guo <songguo@google.com>
2023-12-07 13:07:21 +0800

    [controller] fix 30260

--- src/controller/CHIPDeviceController.cpp
+++ src/controller/CHIPDeviceController.cpp
@@ -1177,18 +1177,25 @@ void DeviceCommissioner::OnICDManagementRegisterClientResponse(
     void * context, const app::Clusters::IcdManagement::Commands::RegisterClientResponse::DecodableType & data)
 {
     DeviceCommissioner * commissioner = static_cast<DeviceCommissioner *>(context);
+    VerifyOrReturn(commissioner != nullptr, ChipLogProgress(Controller, "Command response callback with null context. Ignoring"));
 
-    CommissioningDelegate::CommissioningReport report;
-    auto pairingDelegate         = commissioner->GetPairingDelegate();
-    auto deviceBeingCommissioned = commissioner->mDeviceBeingCommissioned;
-    if (pairingDelegate != nullptr && deviceBeingCommissioned != nullptr)
+    if (commissioner->mCommissioningStage != CommissioningStage::kICDRegistration)
     {
-        pairingDelegate->OnICDRegistrationComplete(deviceBeingCommissioned->GetDeviceId(), data.ICDCounter);
-        commissioner->CommissioningStageComplete(CHIP_NO_ERROR, report);
+        return;
     }
-    else
+
+    if (commissioner->mDeviceBeingCommissioned == nullptr)
     {
-        commissioner->CommissioningStageComplete(CHIP_ERROR_INCORRECT_STATE, report);
+        return;
+    }
+
+    if (commissioner->mPairingDelegate != nullptr)
+    {
+        CommissioningDelegate::CommissioningReport report;
+
+        commissioner->mPairingDelegate->OnICDRegistrationComplete(commissioner->mDeviceBeingCommissioned->GetDeviceId(),
+                                                                  data.ICDCounter);
+        commissioner->CommissioningStageComplete(CHIP_NO_ERROR, report);
     }
 }
 
