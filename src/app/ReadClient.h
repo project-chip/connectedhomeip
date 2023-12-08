df1285791afdaaedb0dff28fc20062b2789c38c0
yunhanw-google <yunhanw@google.com>
2023-12-08 14:41:14 -0800

    Update src/app/ReadClient.h
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/ReadClient.h
+++ src/app/ReadClient.h
@@ -326,7 +326,7 @@ public:
     /**
      *  Activate the idle subscription.
      *
-     *  When subscribing to ICD and liveness timeout reached, the read client will move to `InactiveICDSubscription` state and
+     *  When subscribing to ICD and liveness timeout reached, the read client will move to the `InactiveICDSubscription` state and
      * resubscription can be triggered via OnActiveModeNotification().
      *
      *  If the subscription is not in the`InactiveICDSubscription` state, this function will do nothing. So it is always safe to call
