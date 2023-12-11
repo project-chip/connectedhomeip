696975f5d89f1d4568bb5d3f625343fa1200abad
C Freeman <cecille@google.com>
2023-12-11 10:58:08 -0500

    Python script: start of TH integration code (#28708)
    * Python script: start of TH integration code
    
    - adds required command line functionality (endpoint, nodeID, timeout)
    - adds the ability to pass in a hooks object for use by the TH
    - adds an internal hooks object when scripts are being used on
      the command line
    - adds hook calls as appropriate at test start / end / steps
    
    To setup a test for use in the TH, define a steps_ and desc_
    function with the same name as the test, change print_step() calls
    to step() calls with the appropriate number, add pics_guard calls
    around pics checks.
    
    Changes the hello_test to demonstrate how to do this and also how
    the test works if the steps_ function is not defined.
    
    Added a demonstration of how the TH could call these to get the
    hooks back out.
    
    * Add pics_ handler, address review comments
    
    * lint
    
    * Restyled by isort
    
    * Update src/python_testing/matter_testing_support.py
    
    Co-authored-by: Carolina Lopes <116589288+ccruzagralopes@users.noreply.github.com>
    
    * Init stuff in setup_class in case setup_class fails
    
    * Make default timeout configurable
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Carolina Lopes <116589288+ccruzagralopes@users.noreply.github.com>

--- src/python_testing/hello_test.py
+++ src/python_testing/hello_test.py
@@ -19,11 +19,14 @@ import logging
 
 import chip.clusters as Clusters
 from chip.interaction_model import Status
-from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
+from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 
 class HelloTest(MatterBaseTest):
+    # This example test does not include the step_ and desc_ markers
+    # The MatterBaseTest will assume a single step and create a description
+    # based on the test name
     @async_test_body
     async def test_names_as_expected(self):
         dev_ctrl = self.default_controller
@@ -37,8 +40,24 @@ class HelloTest(MatterBaseTest):
         logging.info("Found VendorName: %s" % (vendor_name))
         asserts.assert_equal(vendor_name, "TEST_VENDOR", "VendorName must be TEST_VENDOR!")
 
+    # To include individual steps and description for the TH, define a steps_ and desc_ function
+    # for the test, then use self.step(#) to indicate how the test proceeds through the test plan.
+    # Support for keeping the TH up to date is built into MatterBaseTest when you use the step()
+    # function.
+    def steps_failure_on_wrong_endpoint(self) -> list[TestStep]:
+        steps = [TestStep(1, "Commissioning, already done", is_commissioning=True),
+                 TestStep(2, "Read ProductName on endpoint 9999"),
+                 ]
+        return steps
+
+    def desc_failure_on_wrong_endpoint(self) -> str:
+        return '#.#.#. [TC-HELLO-x.x] Test Failure On Wrong Endpoint'
+
     @async_test_body
     async def test_failure_on_wrong_endpoint(self):
+        self.step(1)  # Commissioning
+
+        self.step(2)
         dev_ctrl = self.default_controller
         result = await self.read_single_attribute(
             dev_ctrl,
@@ -48,6 +67,28 @@ class HelloTest(MatterBaseTest):
         )
         asserts.assert_true(isinstance(result, Clusters.Attribute.ValueDecodeFailure), "Should fail to read on endpoint 9999")
         asserts.assert_equal(result.Reason.status, Status.UnsupportedEndpoint, "Failure reason should be UnsupportedEndpoint")
+
+    def steps_pics(self) -> list[TestStep]:
+        steps = [TestStep(1, "Commissioning, already done", is_commissioning=True),
+                 TestStep(2, "Skip this step"),
+                 TestStep(3, "Run this step")
+                 ]
+        return steps
+
+    def desc_pics(self) -> str:
+        return "#.#.#. [TC-HELLO-x.x] Test pics"
+
+    @async_test_body
+    async def test_pics(self):
+        self.step(1)  # commissioning
+        print('This should be run')
+
+        self.step(2)
+        if self.pics_guard(self.check_pics('NON-EXISTANT_PICS')):
+            asserts.fail('This should not be run')
+
+        self.step(3)
+        print('This should also be run')
 
 
 if __name__ == "__main__":
