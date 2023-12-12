6630985205f85a2f993616c200d467121f13ad84
Andrei Litvin <andreilitvin@google.com>
2023-12-12 10:11:03 -0500

    zap regen

--- src/controller/java/generated/java/matter/controller/cluster/eventstructs/SampleMeiClusterPingCountEventEvent.kt
+++ src/controller/java/generated/java/matter/controller/cluster/eventstructs/SampleMeiClusterPingCountEventEvent.kt
@@ -14,9 +14,9 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package matter.devicecontroller.cluster.eventstructs
+package matter.controller.cluster.eventstructs
 
-import matter.devicecontroller.cluster.*
+import matter.controller.cluster.*
 import matter.tlv.ContextSpecificTag
 import matter.tlv.Tag
 import matter.tlv.TlvReader
