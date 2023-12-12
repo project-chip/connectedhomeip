258d447b282952daccf9e1b3aef0085468763367
Gibran Vargas <gvargas@csa-iot.org>
2023-12-12 00:26:00 -0800

    chore(python_testing): opencommissionwindow, oncommission methods, read currentfabric and steps 3, 4

--- src/python_testing/TC_ACE_1_5.py
+++ src/python_testing/TC_ACE_1_5.py
@@ -16,55 +16,87 @@
 #
 
 import logging
+import time
 
 import chip.clusters as Clusters
 from chip.interaction_model import Status
+from chip import ChipDeviceCtrl
+from chip.ChipDeviceCtrl import CommissioningParameters
 from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
 from mobly import asserts
 
 class TC_ACE_1_5(MatterBaseTest):
 
-    async def read_currentfabricindex_attribute(self, th, nodeid):
-        #cluster = Clusters.Objects.OperationalCredentials
-        attribute = Clusters.OperationalCredentials.Attributes.CurrentFabricIndex
-        await self.read_single_attribute(th, nodeid, 0, attribute)
+    def OpenCommissioningWindow(self) -> CommissioningParameters:
+        try:
+            params = self.th1.OpenCommissioningWindow(
+                nodeid=self.dut_node_id, timeout=600, iteration=10000, discriminator=self.matter_test_config.discriminators[0], option=1)
+            time.sleep(5)
+            return params
 
-    async def write_acl(self, acl):
-        # This returns an attribute status
-        result = await self.default_controller.WriteAttribute(self.dut_node_id, [(0, Clusters.AccessControl.Attributes.Acl(acl))])
-        asserts.assert_equal(result[0].Status, Status.Success, "ACL write failed")
+        except Exception as e:
+            logging.exception('Error running OpenCommissioningWindow %s', e)
+            asserts.assert_true(False, 'Failed to open commissioning window')
+    
+    # async def CommissionToStageSendCompleteAndCleanup(
+    #         self, stage: int, expectedErrorPart: chip.native.ErrorSDKPart, expectedErrCode: int):
+
+    #     logging.info("-----------------Fail on step {}-------------------------".format(stage))
+    #     params = self.OpenCommissioningWindow()
+    #     self.th2.ResetTestCommissioner()
+    #     # This will run the commissioning up to the point where stage x is run and the
+    #     # response is sent before the test commissioner simulates a failure
+    #     self.th2.SetTestCommissionerPrematureCompleteAfter(stage)
+    #     errcode = self.th2.CommissionOnNetwork(
+    #         nodeId=self.dut_node_id, setupPinCode=params.setupPinCode,
+    #         filterType=ChipDeviceCtrl.DiscoveryFilterType.LONG_DISCRIMINATOR, filter=self.matter_test_config.discriminators[0])
+    #     logging.info('Commissioning complete done. Successful? {}, errorcode = {}'.format(errcode.is_success, errcode))
+    #     asserts.assert_false(errcode.is_success, 'Commissioning complete did not error as expected')
+    #     asserts.assert_true(errcode.sdk_part == expectedErrorPart, 'Unexpected error type returned from CommissioningComplete')
+    #     asserts.assert_true(errcode.sdk_code == expectedErrCode, 'Unexpected error code returned from CommissioningComplete')
+    #     revokeCmd = Clusters.AdministratorCommissioning.Commands.RevokeCommissioning()
+    #     await self.th1.SendCommand(nodeid=self.dut_node_id, endpoint=0, payload=revokeCmd, timedRequestTimeoutMs=6000)
+    #     # The failsafe cleanup is scheduled after the command completes, so give it a bit of time to do that
+    #     time.sleep(1)
+
+    async def read_currentfabricindex_expected_success(self, th) -> str:
+        cluster = Clusters.Objects.OperationalCredentials
+        attribute = Clusters.OperationalCredentials.Attributes.CurrentFabricIndex
+        current_fabric_index = await self.read_single_attribute_check_success(dev_ctrl=th, endpoint=0, cluster=cluster, attribute=attribute)
+        print("Current Fabric Index: %s" % current_fabric_index)
+        return "100"
 
     @async_test_body
     async def test_TC_ACE_1_5(self):
         self.print_step(1, "Comissioning, already done ")
+        self.th1 = self.default_controller       
 
+        #new_certificate_authority = self.certificate_authority_manager.NewCertificateAuthority()
+        #new_fabric_admin = new_certificate_authority.NewFabricAdmin(vendorId=0xFFF1, fabricId=self.matter_test_config.fabric_id + 1)
+        #TH2 = new_fabric_admin.NewController(nodeId = 112233)
+
+        self.print_step(2, "TH1 opens the commissioning window on the DUT")
+        params = self.OpenCommissioningWindow()
+        #dev_ctrl = self.default_controller
+        #params = dev_ctrl.OpenComissioningWindow(nodeid=self.dut_node_id, timeout=900, iteration=1000, discriminator=1234, option=1)
+
+
+        new_certificate_authority = self.certificate_authority_manager.NewCertificateAuthority()
         fabric_admin = self.certificate_authority_manager.activeCaList[0].adminList[0]
-
-        TH1_nodeid = self.matter_test_config.controller_node_id + 1
+        new_fabric_admin = new_certificate_authority.NewFabricAdmin(vendorId=0xFFF1, fabricId=self.matter_test_config.fabric_id + 1)
         TH2_nodeid = self.matter_test_config.controller_node_id + 2
-
-        TH1 = fabric_admin.NewController(nodeId=TH1_nodeid,
-                                         paaTrustStorePath=str(self.matter_test_config.paa_trust_store_path))
-        TH2 = fabric_admin.NewController(nodeId=TH2_nodeid,
+        self.th2 = new_fabric_admin.NewController(nodeId=TH2_nodeid,
                                          paaTrustStorePath=str(self.matter_test_config.paa_trust_store_path))
 
-        TH2FabricIndex = await self.read_currentfabricindex_attribute(TH2, TH2_nodeid)
-
-        self.print_step(2, "TH1 writes DUT endpoint AccessControlEntryStruct")
-        admin_acl = Clusters.AccessControl.Structs.AccessControlEntryStruct(
-            fabricIndex = 1,
-            privilege = Clusters.AccessControl.Enums.AccessControlEntryPrivilegeEnum.kAdminister,
-            authMode = Clusters.AccessControl.Enums.AccessControlEntryAuthModeEnum.kCase,
-            subjects = [],
-            targets=[Clusters.AccessControl.Structs.AccessControlTargetStruct(endpoint=0, cluster=Clusters.AccessControl.id)])
-        all_view = Clusters.AccessControl.Structs.AccessControlEntryStruct(
-            fabricIndex = 1,
-            privilege=Clusters.AccessControl.Enums.AccessControlEntryPrivilegeEnum.kView,
-            authMode=Clusters.AccessControl.Enums.AccessControlEntryAuthModeEnum.kCase,
-            subjects=[],
-            targets=[Clusters.AccessControl.Structs.AccessControlTargetStruct(endpoint=0, cluster=Clusters.Descriptor.id)])
-        acl = [admin_acl, all_view]
-        await self.write_acl(acl)
+        errcode = self.th2.CommissionOnNetwork(
+            nodeId=self.dut_node_id, setupPinCode=params.setupPinCode,
+            filterType=ChipDeviceCtrl.DiscoveryFilterType.LONG_DISCRIMINATOR, filter=self.matter_test_config.discriminators[0])
+        logging.info('Commissioning complete done. Successful? {}, errorcode = {}'.format(errcode.is_success, errcode))
+        self.print_step(3, "TH2 commissions DUT using admin node ID N2")
+        
+        self.print_step(4, "TH2 Reads CurrentFabricIndex ")
+        th2FabricIndex = await self.read_currentfabricindex_expected_success(self.th2)
+        print(th2FabricIndex)
 
 if __name__ == "__main__":
     default_matter_test_main()
