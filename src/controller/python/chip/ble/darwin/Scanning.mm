6df320d96ff06ae654ac89be45e9c2e0e7e586e7
Arkadiusz Bokowy <a.bokowy@samsung.com>
2023-12-22 09:49:50 +0100

    Add missing license to Darwin source files

--- src/controller/python/chip/ble/darwin/Scanning.mm
+++ src/controller/python/chip/ble/darwin/Scanning.mm
@@ -1,3 +1,22 @@
+/**
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <Python.h>
+
 #include <ble/BleUUID.h>
 #include <ble/CHIPBleServiceData.h>
 #include <lib/support/CHIPMem.h>
@@ -7,7 +26,6 @@
 #import <CoreBluetooth/CoreBluetooth.h>
 
 namespace {
-struct PyObject;
 using DeviceScannedCallback
     = void (*)(PyObject * context, const char * address, uint16_t discriminator, uint16_t vendorId, uint16_t productId);
 using ScanCompleteCallback = void (*)(PyObject * context);
