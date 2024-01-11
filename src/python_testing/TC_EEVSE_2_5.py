bd83ba4f2dd8fbf49e508e7af59d4750a6c495c4
James Harrow <james.harrow@gmail.com>
2024-01-11 01:39:24 +0000

    Created a helper EEVSE base class to avoid repetition in the different test cases.

--- src/python_testing/TC_EEVSE_2_5.py
+++ src/python_testing/TC_EEVSE_2_5.py
@@ -27,70 +27,12 @@ from chip.interaction_model import InteractionModelError, Status
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
+from TC_EEVSE_Utils import EventChangeCallback, EEVSEBaseTestHelper
+
 logger = logging.getLogger(__name__)
 
 
-class EventChangeCallback:
-    def __init__(self, expected_cluster: ClusterObjects):
-        self._q = queue.Queue()
-        self._expected_cluster = expected_cluster
-
-    async def start(self, dev_ctrl, nodeid):
-        self._subscription = await dev_ctrl.ReadEvent(nodeid,
-                                                      events=[(1, self._expected_cluster, 1)], reportInterval=(1, 5),
-                                                      fabricFiltered=False, keepSubscriptions=True, autoResubscribe=False)
-        self._subscription.SetEventUpdateCallback(self.__call__)
-
-    def __call__(self, res: EventReadResult, transaction: SubscriptionTransaction):
-        if res.Status == Status.Success and res.Header.ClusterId == self._expected_cluster.id:
-            logging.info(
-                f'Got subscription report for event on cluster {self._expected_cluster}: {res.Data}')
-            self._q.put(res)
-
-    def WaitForEventReport(self, expected_event: ClusterObjects.ClusterEvent):
-        try:
-            res = self._q.get(block=True, timeout=10)
-        except queue.Empty:
-            asserts.fail("Failed to receive a report for the event {}".format(expected_event))
-
-        asserts.assert_equal(res.Header.ClusterId, expected_event.cluster_id, "Expected cluster ID not found in event report")
-        asserts.assert_equal(res.Header.EventId, expected_event.event_id, "Expected event ID not found in event report")
-        return res.Data
-
-
-class TC_EEVSE_2_5(MatterBaseTest):
-    async def read_evse_attribute_expect_success(self, endpoint, attribute):
-        full_attr = getattr(Clusters.EnergyEvse.Attributes, attribute)
-        cluster = Clusters.Objects.EnergyEvse
-        return await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=full_attr)
-
-    async def check_evse_attribute(self, attribute, expected_value):
-        value = await self.read_evse_attribute_expect_success(endpoint=1, attribute=attribute)
-        asserts.assert_equal(value, expected_value,
-                             f"Unexpected '{attribute}' value - expected {expected_value}, was {value}")
-
-    async def send_enable_charge_command(self, endpoint: int = 0, charge_until: int = None, timedRequestTimeoutMs: int = 3000,
-                                         min_charge: int = None, max_charge: int = None, expected_status: Status = Status.Success):
-        try:
-            await self.send_single_cmd(cmd=Clusters.EnergyEvse.Commands.EnableCharging(
-                chargingEnabledUntil=charge_until,
-                minimumChargeCurrent=min_charge,
-                maximumChargeCurrent=max_charge),
-                endpoint=1,
-                timedRequestTimeoutMs=timedRequestTimeoutMs)
-
-        except InteractionModelError as e:
-            asserts.assert_equal(e.status, expected_status, "Unexpected error returned")
-
-    async def send_start_diagnostics_command(self, endpoint: int = 0, timedRequestTimeoutMs: int = 3000,
-                                             expected_status: Status = Status.Success):
-        try:
-            await self.send_single_cmd(cmd=Clusters.EnergyEvse.Commands.StartDiagnostics(),
-                                       endpoint=1,
-                                       timedRequestTimeoutMs=timedRequestTimeoutMs)
-
-        except InteractionModelError as e:
-            asserts.assert_equal(e.status, expected_status, "Unexpected error returned")
+class TC_EEVSE_2_5(MatterBaseTest, EEVSEBaseTestHelper):
 
     def desc_TC_EEVSE_2_5(self) -> str:
         """Returns a description of this test"""
@@ -122,33 +64,6 @@ class TC_EEVSE_2_5(MatterBaseTest):
         ]
 
         return steps
-
-    async def send_test_event_triggers(self, enableKey=bytes([b for b in range(16)]), eventTrigger=0x0099000000000000):
-        try:
-            await self.send_single_cmd(endpoint=0,
-                                       cmd=Clusters.GeneralDiagnostics.Commands.TestEventTrigger(
-                                           enableKey,
-                                           eventTrigger)
-                                       )
-
-        except InteractionModelError as e:
-            asserts.fail(f"Unexpected error returned - {e.status}")
-
-    # TC_EEVSE_2_5 tests steps
-    async def check_test_event_triggers_enabled(self):
-        full_attr = Clusters.GeneralDiagnostics.Attributes.TestEventTriggersEnabled
-        cluster = Clusters.Objects.GeneralDiagnostics
-        test_event_enabled = await self.read_single_attribute_check_success(endpoint=0, cluster=cluster, attribute=full_attr)
-        asserts.assert_equal(test_event_enabled, True, "TestEventTriggersEnabled is False")
-
-    async def send_test_event_trigger_basic(self):
-        await self.send_test_event_triggers(eventTrigger=0x0099000000000000)
-
-    async def send_test_event_trigger_basic_clear(self):
-        await self.send_test_event_triggers(eventTrigger=0x0099000000000001)
-
-    async def send_test_event_trigger_evse_diagnostics_complete(self):
-        await self.send_test_event_triggers(eventTrigger=0x0099000000000020)
 
     @async_test_body
     async def test_TC_EEVSE_2_5(self):
