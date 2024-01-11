514e18e0fc09405c0c3aee0964ed13b318e3695a
Restyled.io <commits@restyled.io>
2024-01-11 01:40:19 +0000

    Restyled by isort

--- src/python_testing/TC_EEVSE_2_2.py
+++ src/python_testing/TC_EEVSE_2_2.py
@@ -27,8 +27,7 @@ from chip.clusters.Types import NullValue
 from chip.interaction_model import InteractionModelError, Status
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
-
-from TC_EEVSE_Utils import EventChangeCallback, EEVSEBaseTestHelper
+from TC_EEVSE_Utils import EEVSEBaseTestHelper, EventChangeCallback
 
 logger = logging.getLogger(__name__)
 
