34c077d0d015762e5f7d8c98e6a03bf5bb93c473
James Harrow <james.harrow@gmail.com>
2024-01-12 22:14:05 +0000

    Fixed another python lint issue.

--- src/python_testing/TC_EEVSE_2_2.py
+++ src/python_testing/TC_EEVSE_2_2.py
@@ -21,7 +21,6 @@ from datetime import datetime, timedelta, timezone
 
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from chip.interaction_model import Status
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 from TC_EEVSE_Utils import EEVSEBaseTestHelper, EventChangeCallback
