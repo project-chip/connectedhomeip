d6027f47e3ea35f615d8a91da99a1aa564b0fbe9
René Josefsen <rjosefsen@grundfos.com>
2024-01-12 10:31:44 +0100

    Remove unused imports

--- src/python_testing/TC_VALCC_3_4.py
+++ src/python_testing/TC_VALCC_3_4.py
@@ -15,12 +15,10 @@
 #    limitations under the License.
 #
 
-import time
 import logging
 
 import chip.clusters as Clusters
 from chip.interaction_model import InteractionModelError, Status
-from chip.clusters.Types import NullValue
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
