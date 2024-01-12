389e757785330a4c2b977ec7c2559dcd5695a1f5
yunhanw-google <yunhanw@google.com>
2024-01-12 08:26:13 -0800

    Update src/app/ReadClient.h
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/ReadClient.h
+++ src/app/ReadClient.h
@@ -167,7 +167,7 @@ public:
          *
          * If the peer is LIT ICD, and the timeout is reached, `aTerminationCause` will be
          * `CHIP_ERROR_LIT_SUBSCRIBE_INACTIVE_TIMEOUT`. In this case, returning `CHIP_NO_ERROR` will still trigger a resubscribe attempt,
-         * or return `CHIP_ERROR_LIT_SUBSCRIBE_INACTIVE_TIMEOUT` to put the subscription to `InactiveICDSubscription` state and this
+         * while returning `CHIP_ERROR_LIT_SUBSCRIBE_INACTIVE_TIMEOUT` will put the subscription in the `InactiveICDSubscription` state.  In the latter case, OnResubscriptionNeeded
          * will be called again when `OnActiveModeNotification` is called.
          *
          * If the method is over-ridden, it's the application's responsibility to take the appropriate steps needed to eventually
