23abc4806d7fe16cda4931ff4be4325da49ae662
Restyled.io <commits@restyled.io>
2024-01-15 02:41:04 +0000

    Restyled by clang-format

--- src/app/clusters/microwave-oven-control-server/microwave-oven-control-server.cpp
+++ src/app/clusters/microwave-oven-control-server/microwave-oven-control-server.cpp
@@ -312,8 +312,8 @@ void Instance::HandleSetCookingParameters(HandlerContext & ctx, const Commands::
     {
         // if using power as number, check if the param is invalid and set PowerSetting number.
         uint8_t reqPowerSettingNum;
-        uint8_t maxPowerNum = kDefaultMaxPowerNum;
-        uint8_t minPowerNum = kDefaultMinPowerNum;
+        uint8_t maxPowerNum  = kDefaultMaxPowerNum;
+        uint8_t minPowerNum  = kDefaultMinPowerNum;
         uint8_t powerStepNum = kDefaultPowerStepNum;
         VerifyOrExit(!wattSettingIndex.HasValue(), status = Status::InvalidCommand; ChipLogError(
             Zcl, "Microwave Oven Control: Failed to set cooking parameters, should have no value for wattSettingIndex"));
@@ -324,17 +324,18 @@ void Instance::HandleSetCookingParameters(HandlerContext & ctx, const Commands::
 
         if (HasFeature(MicrowaveOvenControl::Feature::kPowerNumberLimits))
         {
-            maxPowerNum = mDelegate->GetMaxPowerNum();
-            minPowerNum = mDelegate->GetMinPowerNum();
+            maxPowerNum  = mDelegate->GetMaxPowerNum();
+            minPowerNum  = mDelegate->GetMinPowerNum();
             powerStepNum = mDelegate->GetPowerStepNum();
         }
         reqPowerSettingNum = powerSetting.ValueOr(maxPowerNum);
         VerifyOrExit(IsPowerSettingNumberInRange(reqPowerSettingNum, minPowerNum, maxPowerNum), status = Status::ConstraintError;
                      ChipLogError(Zcl, "Microwave Oven Control: Failed to set PowerSetting, PowerSetting value is out of range"));
 
-
-        VerifyOrExit(reqPowerSettingNum % powerStepNum == 0, status = Status::InvalidCommand;
-                     ChipLogError(Zcl, "Microwave Oven Control: Failed to set PowerSetting, PowerSetting value must be multiple of PowerStep number"));
+        VerifyOrExit(
+            reqPowerSettingNum % powerStepNum == 0, status = Status::InvalidCommand; ChipLogError(
+                Zcl,
+                "Microwave Oven Control: Failed to set PowerSetting, PowerSetting value must be multiple of PowerStep number"));
 
         status = mDelegate->HandleSetCookingParametersCallback(reqCookMode, reqCookTimeSec, reqStartAfterSetting,
                                                                MakeOptional(reqPowerSettingNum), NullOptional);
