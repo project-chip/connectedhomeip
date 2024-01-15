7ce5c9131840ce8a98a348a391d404bdee16a62b
Arkadiusz Bokowy <a.bokowy@samsung.com>
2024-01-15 11:36:10 +0100

    Fix for Python < 3.9

--- src/controller/python/chip/native/__init__.py
+++ src/controller/python/chip/native/__init__.py
@@ -208,7 +208,7 @@ class _Handle:
     initialized: bool = False
 
 
-_nativeLibraryHandles: dict[Library, _Handle] = {}
+_nativeLibraryHandles: typing.Dict[Library, _Handle] = {}
 
 
 def _GetLibraryHandle(lib: Library, expectAlreadyInitialized: bool) -> ctypes.CDLL:
