dfe46531468b668838ef3d2469fc1c032a224c97
raul-marquez-csa <rmarquez@csa-iot.org>
2024-01-16 14:26:10 -0800

    Gets TXT record properties

--- src/python_testing/TC_SC_4_10.py
+++ src/python_testing/TC_SC_4_10.py
@@ -28,7 +28,7 @@ from chip.interaction_model import Status
 from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
 from mobly import asserts
 
-from zeroconf import Zeroconf, ServiceListener, ServiceBrowser, DNSText
+from zeroconf import DNSQuestionType, Zeroconf, ServiceListener, ServiceBrowser, DNSText
 from zeroconf.asyncio import (
     AsyncZeroconf,
     AsyncServiceBrowser,
@@ -51,50 +51,77 @@ https://github.com/CHIP-Specifications/chip-test-plans/blob/master/src/interacti
 
 MDNS_TYPE_OPERATIONAL = "_matter._tcp.local."
 
+'''
+A service listener required for the TXT record data to get populated and come back
+'''
+class DummyServiceListener(ServiceListener):
+
+    def add_service(self, zeroconf: Zeroconf, service_type: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zeroconf: Zeroconf, service_type: str, name: str) -> None:
+        pass
+
+    def update_service(self, zeroconf: Zeroconf, service_type: str, name: str) -> None:
+        pass
+
+
 class TC_SC_4_10(MatterBaseTest):
+    
+    @staticmethod
+    def get_property_value(properties, key: str):
+        # Convert the key from string to bytes, as the dictionary uses bytes
+        byte_key = key.encode('utf-8')
+
+        # Check if the key exists in the dictionary
+        if byte_key in properties:
+            value = properties[byte_key]
+
+            # Check if the value is not None
+            if value is not None:
+                # Convert the value from bytes to string
+                return value.decode('utf-8')
+            else:
+                asserts.fail(f"Value for property '{key}' not present")
+        else:
+            asserts.fail(f"Property '{key}' not found")
     
     @async_test_body
     async def test_TC_SC_4_10(self):
         print("\n" * 10)
-        azc = AsyncZeroconf()
+
+        # Zeroconf setup
+        azc: AsyncZeroconf = AsyncZeroconf()
+        listener = DummyServiceListener()
+        await azc.async_add_service_listener(MDNS_TYPE_OPERATIONAL, listener)
+        
+        # Build service name
         node_id = self.dut_node_id
         node_id_hex = str(hex(int(node_id))[2:].upper()).zfill(16)
         compressed_fabricid = self.default_controller.GetCompressedFabricId()
         compressed_fabricid_hex = hex(int(compressed_fabricid))[2:].upper()
         service_name = f"{compressed_fabricid_hex}-{node_id_hex}"
 
-        try:
-            service_types = list(await AsyncZeroconfServiceTypes.async_find())
+        service_types = list(await AsyncZeroconfServiceTypes.async_find())
 
-            for service_type in service_types:
-                if service_type == MDNS_TYPE_OPERATIONAL:
-                    name = f"{service_name}.{service_type}"
+        if MDNS_TYPE_OPERATIONAL in service_types:
+            name = f"{service_name}.{MDNS_TYPE_OPERATIONAL}"
 
-                    service_info: AsyncServiceInfo = await azc.async_get_service_info(
-                        name=name,
-                        type_=service_type
-                    )
+            service_info: AsyncServiceInfo = await azc.async_get_service_info(
+                name=name,
+                type_=MDNS_TYPE_OPERATIONAL
+            )
 
-                    if service_info is not None:
-                        for attribute_name in ['type', 'name', 'addresses',
-                                               'port', 'weight', 'priority',
-                                               'server', 'properties',
-                                               'interface_index']:
-                            attribute_value = getattr(service_info, attribute_name, None)
-                            print(f"{attribute_name}: {attribute_value}")
-                    else:
-                        print(f"Service info for {name} not found")
-        except Exception as e:
-            print(("\nError: " + str(e)) * 10)
+            if service_info is not None:
+                SII = self.get_property_value(service_info.properties, "SII")
+                SAI = self.get_property_value(service_info.properties, "SAI")
 
-
-
-
-
-
-
-
-
+                print(f" * get_property_value(SII): {SII}")
+                print(f" * get_property_value(SAI): {SAI}")
+            else:
+                asserts.fail(f"Service info for {name} not found")
+        else:
+            asserts.fail(f"The MDNS operational service type '{MDNS_TYPE_OPERATIONAL}' was not found")
 
 
         print("\n" * 10)
