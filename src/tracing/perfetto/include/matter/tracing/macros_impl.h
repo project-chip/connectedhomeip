6def37ca3fde558a3fd82eb63ef8e9e450375a15
shripad621git <shripad62001@gmail.com>
2024-01-17 16:50:32 +0530

    Added the support of counter macro in perfetto

--- src/tracing/perfetto/include/matter/tracing/macros_impl.h
+++ src/tracing/perfetto/include/matter/tracing/macros_impl.h
@@ -24,9 +24,16 @@
 
 #include <perfetto.h>
 
+
 PERFETTO_DEFINE_CATEGORIES(perfetto::Category("Matter").SetDescription("Matter trace events"));
 
 #define MATTER_TRACE_BEGIN(label, group) TRACE_EVENT_BEGIN("Matter", label, "class_name", group)
 #define MATTER_TRACE_END(label, group) TRACE_EVENT_END("Matter")
 #define MATTER_TRACE_INSTANT(label, group) TRACE_EVENT_INSTANT("Matter", label, "class_name", group)
 #define MATTER_TRACE_SCOPE(label, group) TRACE_EVENT("Matter", label, "class_name", group)
+
+#define MATTER_TRACE_COUNTER(label,group) \
+    do { \
+        static int count##_label = 0; \
+        TRACE_COUNTER("Matter", perfetto::CounterTrack(label), ++count##_label); \
+    } while(0)
--- src/tracing/perfetto/perfetto_tracing.h
+++ src/tracing/perfetto/perfetto_tracing.h
@@ -17,6 +17,7 @@
  */
 #pragma once
 
+#include <atomic>
 #include <tracing/backend.h>
 
 #include <memory>
