dee644895d049910a46d8629d35f5b3649093952
Song Guo <songguo@google.com>
2024-01-17 08:38:15 +0000

    Add tests

--- src/app/ReadClient.cpp
+++ src/app/ReadClient.cpp
@@ -461,6 +461,8 @@ void ReadClient::OnPeerTypeChange(PeerType aType)
 
     mIsPeerLIT = (aType == PeerType::kLITICD);
 
+    ChipLogProgress(DataManagement, "Peer is now %s LIT ICD.", mIsPeerLIT ? "a" : "not a");
+
     // If the peer is no longer LIT, try to wake up the subscription and do resubsribe when necessary.
     if (!mIsPeerLIT)
     {
