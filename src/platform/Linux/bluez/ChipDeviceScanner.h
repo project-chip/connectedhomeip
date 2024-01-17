8880e967d5a908b685ed53070356109bdabbbf18
tianfeng.yang <tianfeng.yang@tuya.com>
2024-01-17 12:32:44 +0800

    [Linux] fix bug: mutex is nonmoveable

--- src/platform/Linux/bluez/ChipDeviceScanner.h
+++ src/platform/Linux/bluez/ChipDeviceScanner.h
@@ -54,7 +54,7 @@ class ChipDeviceScanner
 {
 public:
     ChipDeviceScanner()                                      = default;
-    ChipDeviceScanner(ChipDeviceScanner &&)                  = default;
+    ChipDeviceScanner(ChipDeviceScanner &&)                  = delete;
     ChipDeviceScanner(const ChipDeviceScanner &)             = delete;
     ChipDeviceScanner & operator=(const ChipDeviceScanner &) = delete;
 
