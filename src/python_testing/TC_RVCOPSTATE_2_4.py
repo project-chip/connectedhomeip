deb5683312be1c1ac85d1854f2380df281dbcbc3
Restyled.io <commits@restyled.io>
2024-01-18 18:30:38 +0000

    Restyled by isort

--- src/python_testing/TC_RVCOPSTATE_2_4.py
+++ src/python_testing/TC_RVCOPSTATE_2_4.py
@@ -16,6 +16,7 @@
 #
 
 import logging
+
 import chip.clusters as Clusters
 from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main, type_matches
 from mobly import asserts
