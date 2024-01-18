4fb70fb292b394189856caa27e4ed5fd50f9004c
Andrei Litvin <andreilitvin@google.com>
2024-01-18 09:52:16 -0500

    Added a scoped value changer, mainly for unit test usage

---
+++ src/lib/support/tests/TestScoped.cpp
@@ -0,0 +1,103 @@
+/*
+ *
+ *    Copyright (c) 2024 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <lib/support/Scoped.h>
+#include <lib/support/UnitTestRegistration.h>
+
+#include <string.h>
+#include <nlunit-test.h>
+
+namespace {
+
+using namespace chip;
+
+void TestScopedVariableChange(nlTestSuite * inSuite, void * inContext)
+{
+    int x = 123;
+
+    {
+        ScopedChange<int> change1(x, 10);
+        NL_TEST_ASSERT(inSuite, x == 10);
+
+        x = 15;
+        {
+            ScopedChange<int> change1(x, 20);
+            NL_TEST_ASSERT(inSuite, x == 20);
+        }
+        NL_TEST_ASSERT(inSuite, x == 15);
+    }
+    NL_TEST_ASSERT(inSuite, x == 123);
+}
+
+void TestScopedChangeOnly(nlTestSuite * inSuite, void * inContext)
+{
+    ScopedChangeOnly<int> intValue(123);
+    ScopedChangeOnly<const char *> strValue("abc");
+
+
+    NL_TEST_ASSERT(inSuite, intValue == 123);
+    NL_TEST_ASSERT(inSuite, strcmp(strValue, "abc") == 0);
+
+    {
+        ScopedChange<int> change1(intValue, 234);
+
+        NL_TEST_ASSERT(inSuite, intValue == 234);
+        NL_TEST_ASSERT(inSuite, strcmp(strValue, "abc") == 0);
+
+        ScopedChange<const char *> change2(strValue, "xyz");
+        NL_TEST_ASSERT(inSuite, intValue == 234);
+        NL_TEST_ASSERT(inSuite, strcmp(strValue, "xyz") == 0);
+
+        {
+            ScopedChange<int> change3(intValue, 10);
+            ScopedChange<const char *> change4(strValue, "test");
+
+            NL_TEST_ASSERT(inSuite, intValue == 10);
+            NL_TEST_ASSERT(inSuite, strcmp(strValue, "test") == 0);
+        }
+
+        NL_TEST_ASSERT(inSuite, intValue == 234);
+        NL_TEST_ASSERT(inSuite, strcmp(strValue, "xyz") == 0);
+    }
+
+    NL_TEST_ASSERT(inSuite, intValue == 123);
+    NL_TEST_ASSERT(inSuite, strcmp(strValue, "abc") == 0);
+}
+
+} // namespace
+
+#define NL_TEST_DEF_FN(fn) NL_TEST_DEF("Test " #fn, fn)
+/**
+ *   Test Suite. It lists all the test functions.
+ */
+static const nlTest sTests[] = {
+    NL_TEST_DEF_FN(TestScopedVariableChange), //
+    NL_TEST_DEF_FN(TestScopedChangeOnly),     //
+    NL_TEST_SENTINEL()                        //
+};
+
+int TestScopedBuffer()
+{
+    nlTestSuite theSuite = { "CHIP Scoped tests", &sTests[0], nullptr, nullptr };
+
+    // Run test suite against one context.
+    nlTestRunner(&theSuite, nullptr);
+    return nlTestRunnerStats(&theSuite);
+}
+
+CHIP_REGISTER_TEST_SUITE(TestScopedBuffer)
