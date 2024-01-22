56ce93ba49521ec4e37dc768938343ac93243199
Michael Rupp <michael.rupp@silabs.com>
2024-01-22 09:56:01 -0500

    Allow sample apps to override BaseApplication's default lcd screen

--- examples/platform/silabs/BaseApplication.h
+++ examples/platform/silabs/BaseApplication.h
@@ -105,6 +105,11 @@ public:
      */
     static void PostEvent(const AppEvent * event);
 
+    /**
+     * @brief Overridable function used to update display on button press
+     */
+    static void UpdateDisplay();
+    
 #ifdef DISPLAY_ENABLED
     /**
      * @brief Return LCD object
