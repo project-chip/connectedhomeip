471fd351b25c26ab8914218542cd9ac840254a03
Restyled.io <commits@restyled.io>
2024-01-26 01:06:43 +0000

    Restyled by clang-format

--- src/app/clusters/microwave-oven-control-server/microwave-oven-control-server.cpp
+++ src/app/clusters/microwave-oven-control-server/microwave-oven-control-server.cpp
@@ -318,7 +318,7 @@ void Instance::HandleSetCookingParameters(HandlerContext & ctx, const Commands::
             mSupportedWattLevels > 0,
             ChipLogError(Zcl, "Microwave Oven Control: Failed to set wattSettingIndex, count of supported watt levels is 0"));
         uint8_t maxWattSettingIndex = static_cast<uint8_t>(mSupportedWattLevels - 1);
-        reqWattSettingIndex = wattSettingIndex.ValueOr(maxWattSettingIndex);
+        reqWattSettingIndex         = wattSettingIndex.ValueOr(maxWattSettingIndex);
         VerifyOrExit(reqWattSettingIndex <= maxWattSettingIndex, status = Status::ConstraintError;
                      ChipLogError(Zcl, "Microwave Oven Control: Failed to set wattSettingIndex, wattSettingIndex is out of range"));
 
