7dbc4c1d3a20c87b87d73cb7cc6b3f534f6db82e
Restyled.io <commits@restyled.io>
2024-02-04 21:12:26 +0000

    Restyled by isort

--- src/python_testing/TC_EEVSE_2_4.py
+++ src/python_testing/TC_EEVSE_2_4.py
@@ -20,7 +20,7 @@ import time
 
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main, EventChangeCallback
+from matter_testing_support import EventChangeCallback, MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from TC_EEVSE_Utils import EEVSEBaseTestHelper
 
 logger = logging.getLogger(__name__)
