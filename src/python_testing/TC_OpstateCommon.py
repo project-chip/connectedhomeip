f9f5577e6ba4cbea93b597b0ded42bf6e1a7ef77
LUCAS BECKER <lucas_becker1@whirlpool.com>
2024-02-07 08:47:17 -0300

    improvement base code

--- src/python_testing/TC_OpstateCommon.py
+++ src/python_testing/TC_OpstateCommon.py
@@ -48,6 +48,35 @@ class TC_OVENOPSTATE_BASE():
         self.test_info = test_info
         self.test_plan_config = test_plan_config
 
+    async def read_and_expect_value(self, endpoint, attribute, expected_value):
+        logging.info("\t##### Read %s" % attribute)
+        attr_value = await self.implementer.read_single_attribute_check_success(
+                                endpoint=endpoint,
+                                cluster=self.test_info.cluster,
+                                attribute=attribute)
+        logging.info("\t## %s: %s" % (attribute, attr_value))
+
+        asserts.assert_equal(attr_value, expected_value,
+                             "Current value not according expected. Expected %d, received %d!" %
+                             (expected_value, attr_value))
+
+    async def read_and_expect_array_contains(self, endpoint, attribute, expected_contains):
+        logging.info("\t##### Read %s" % attribute)
+        attr_value = await self.implementer.read_single_attribute_check_success(
+                                endpoint=endpoint,
+                                cluster=self.test_info.cluster,
+                                attribute=attribute)
+        attr_value.sort()
+        expected_contains.sort()
+
+        logging.info("\t## Current value: [%s]" % ','.join(map(str, attr_value)))
+        logging.info("\t## Expected value: [%s]" % ','.join(map(str, expected_contains)))
+
+        for item in expected_contains:
+            if item not in attr_value:
+                asserts.fail("""Entry (%s), not found! The returned value SHALL include all the entries: [%s]!""" %
+                             (item, ','.join(map(str, expected_contains))))
+
     def steps_TC_OPSTATE_BASE_1_1(self) -> list[TestStep]:
         steps = [TestStep(1, "Commissioning, already done", is_commissioning=True),
                  TestStep(2, "TH reads from the DUT the ClusterRevision attribute"),
@@ -58,7 +87,6 @@ class TC_OVENOPSTATE_BASE():
                  TestStep(7, "TH reads from the DUT the GeneratedCommandList attribute")
                  ]
         return steps
-
 
     async def test_TC_OPSTATE_BASE_1_1(self, endpoint=1):
         cluster = self.test_info.cluster
@@ -71,37 +99,18 @@ class TC_OVENOPSTATE_BASE():
 
         # STEP 2: TH reads from the DUT the ClusterRevision attribute
         self.implementer.step(2)
-
-        attr_value = await self.implementer.read_single_attribute_check_success(
-                                endpoint=endpoint,
-                                cluster=cluster,
-                                attribute=attribute.ClusterRevision)
-        logging.info("Cluster Revision: %s" % (attr_value))
-
-        asserts.assert_equal(attr_value, self.test_plan_config.cluster_revision,
-                             "Cluster Revision not according to the Test Plan. Expected %d, received %d!" %
-                             (self.test_plan_config.cluster_revision, attr_value))
+        await self.read_and_expect_value(endpoint=endpoint,
+                              attribute=attribute.ClusterRevision,
+                              expected_value=self.test_plan_config.cluster_revision)
 
         # STEP 3: TH reads from the DUT the FeatureMap attribute
         self.implementer.step(3)
-        attr_value = await self.implementer.read_single_attribute_check_success(
-                                endpoint=endpoint,
-                                cluster=cluster,
-                                attribute=attribute.FeatureMap)
-        logging.info("Feature Map: %s" % (attr_value))
-
-        # Check for feature map value
-        asserts.assert_equal(attr_value, self.test_plan_config.feature_map,
-                             "Feature Map not according to the Test Plan. Expected %d, received %d!" %
-                             (self.test_plan_config.feature_map, attr_value))
+        await self.read_and_expect_value(endpoint=endpoint,
+                              attribute=attribute.FeatureMap,
+                              expected_value=self.test_plan_config.feature_map)
 
         # STEP 4: TH reads from the DUT the AttributeList attribute
         self.implementer.step(4)
-        attr_value = await self.implementer.read_single_attribute_check_success(
-                                endpoint=endpoint,
-                                cluster=cluster,
-                                attribute=attribute.AttributeList)
-
         expected_value = [
             attribute.PhaseList.attribute_id,
             attribute.CurrentPhase.attribute_id,
@@ -118,25 +127,13 @@ class TC_OVENOPSTATE_BASE():
         if self.implementer.check_pics(("%s.S.A0002" % self.test_info.pics_name)):
             expected_value.append(attribute.CountdownTime.attribute_id)
 
-        attr_value.sort()
-        expected_value.sort()
-
-        logging.info("Current value: [%s]" % ','.join(map(str, attr_value)))
-        logging.info("Expected value: [%s]" % ','.join(map(str, expected_value)))
-
-        for item in expected_value:
-            if item not in attr_value:
-                asserts.fail("""Attribute (%s), not found! The list SHALL include all the entries: [%s]!""" %
-                             (item, ','.join(map(str, expected_value))))
+        await self.read_and_expect_array_contains(endpoint=endpoint,
+                              attribute=attribute.AttributeList,
+                              expected_contains=expected_value)
 
         # STEP 5: TH reads from the DUT the EventList attribute
         self.implementer.step(5)
         if self.implementer.pics_guard(self.implementer.check_pics("PICS_EVENT_LIST_ENABLED")):
-            attr_value = await self.implementer.read_single_attribute_check_success(
-                                    endpoint=endpoint,
-                                    cluster=cluster,
-                                    attribute=attribute.EventList)
-
             expected_value = [
                 events.OperationalError.event_id,
             ]
@@ -144,24 +141,12 @@ class TC_OVENOPSTATE_BASE():
             if self.implementer.check_pics(("%s.S.E01" % self.test_info.pics_name)):
                 expected_value.append(events.OperationCompletion.event_id)
 
-            attr_value.sort()
-            expected_value.sort()
-
-            logging.info("Current value: [%s]" % ','.join(map(str, attr_value)))
-            logging.info("Expected value: [%s]" % ','.join(map(str, expected_value)))
-
-            for item in expected_value:
-                if item not in attr_value:
-                    asserts.fail("""Event (%s), not found! The list SHALL include all the entries: [%s]!""" %
-                                (item, ','.join(map(str, expected_value))))
+            await self.read_and_expect_array_contains(endpoint=endpoint,
+                              attribute=attribute.EventList,
+                              expected_contains=expected_value)
 
         # STEP 6: TH reads from the DUT the AcceptedCommandList attribute
         self.implementer.step(6)
-        attr_value = await self.implementer.read_single_attribute_check_success(
-                                endpoint=endpoint,
-                                cluster=cluster,
-                                attribute=attribute.AcceptedCommandList)
-
         expected_value = []
 
         if self.implementer.check_pics(("%s.S.C00.Rsp" % self.test_info.pics_name)) or \
@@ -179,24 +164,12 @@ class TC_OVENOPSTATE_BASE():
             self.implementer.check_pics(("%s.S.C00.Rsp" % self.test_info.pics_name)):
             expected_value.append(commands.Resume.command_id)
 
-        attr_value.sort()
-        expected_value.sort()
-
-        logging.info("Current value: [%s]" % ','.join(map(str, attr_value)))
-        logging.info("Expected value: [%s]" % ','.join(map(str, expected_value)))
-
-        for item in expected_value:
-            if item not in attr_value:
-                asserts.fail("""Command (%s), not found! The list SHALL include all the entries: [%s]!""" %
-                            (item, ','.join(map(str, expected_value))))
+        await self.read_and_expect_array_contains(endpoint=endpoint,
+                              attribute=attribute.AcceptedCommandList,
+                              expected_contains=expected_value)
 
         # STEP 7: TH reads from the DUT the AcceptedCommandList attribute
         self.implementer.step(7)
-        attr_value = await self.implementer.read_single_attribute_check_success(
-                                endpoint=endpoint,
-                                cluster=cluster,
-                                attribute=attribute.GeneratedCommandList)
-
         expected_value = []
 
         if self.implementer.check_pics(("%s.S.C00.Rsp" % self.test_info.pics_name)) or \
@@ -205,13 +178,7 @@ class TC_OVENOPSTATE_BASE():
             self.implementer.check_pics(("%s.S.C03.Rsp" % self.test_info.pics_name)):
             expected_value.append(commands.OperationalCommandResponse.command_id)
 
-        attr_value.sort()
-        expected_value.sort()
+        await self.read_and_expect_array_contains(endpoint=endpoint,
+                              attribute=attribute.GeneratedCommandList,
+                              expected_contains=expected_value)
 
-        logging.info("Current value: [%s]" % ','.join(map(str, attr_value)))
-        logging.info("Expected value: [%s]" % ','.join(map(str, expected_value)))
-
-        for item in expected_value:
-            if item not in attr_value:
-                asserts.fail("""Command (%s), not found! The list SHALL include all the entries: [%s]!""" %
-                            (item, ','.join(map(str, expected_value))))
