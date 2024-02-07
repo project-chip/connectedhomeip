06dbc6c50c618f02cc196542535e3338c2726b07
LUCAS BECKER <lucas_becker1@whirlpool.com>
2024-02-07 08:10:48 -0300

    improvements opstte 1_1

--- src/python_testing/TC_OVENOPSTATE_1_1.py
+++ src/python_testing/TC_OVENOPSTATE_1_1.py
@@ -28,19 +28,19 @@ class TC_OVENOPSTATE_1_1(MatterBaseTest):
         super().__init__(*args)
 
         test_info = TestInfo(
-            "OVENOPSTATE",
-            Clusters.OvenCavityOperationalState
+            pics_name="OVENOPSTATE",
+            cluster=Clusters.OvenCavityOperationalState
         )
 
         test_plan_config = TestPlanConfig(
-            1,
-            0
+            cluster_revision=1,
+            feature_map=0
         )
 
         self.TC_BASE = TC_OVENOPSTATE_BASE(
-                            self,
-                            test_info,
-                            test_plan_config)
+                            implementer=self,
+                            test_info=test_info,
+                            test_plan_config=test_plan_config)
         self.endpoint = 0
 
 
@@ -53,71 +53,7 @@ class TC_OVENOPSTATE_1_1(MatterBaseTest):
     @async_test_body
     async def test_TC_OVENOPSTATE_1_1(self):
         self.endpoint = self.matter_test_config.endpoint
-
-
         tc_result = await self.TC_BASE.test_TC_OPSTATE_BASE_1_1(self.endpoint)
-
-
-
-        # # Verify that each ModeOptionsStruct entry has a unique Mode field value
-        # for m in supported_modes:
-        #     if m.mode in self.supported_modes_dut:
-        #         asserts.fail("SupportedModes must have unique mode values!")
-        #     else:
-        #         self.supported_modes_dut.append(m.mode)
-
-        # # Verify that each ModeOptionsStruct entry has a unique Label field value
-        # labels = []
-        # for m in supported_modes:
-        #     if m.label in labels:
-        #         asserts.fail("SupportedModes must have unique mode label values!")
-        #     else:
-        #         labels.append(m.label)
-
-        # # Verify that each ModeOptionsStruct entry's ModeTags field has:
-        # for m in supported_modes:
-        #     # * at least one entry
-        #     if len(m.modeTags) == 0:
-        #         asserts.fail("SupportedModes must have at least one mode tag!")
-
-        #     at_least_one_common_or_clean_tag = False
-        #     for t in m.modeTags:
-        #         # * the values of the Value fields that are not larger than 16 bits
-        #         if t.value > 0xFFFF or t.value < 0:
-        #             asserts.fail("Mode tag values must not be larger than 16 bits!")
-
-        #         # * for each Value field: {isCommonOrDerivedOrMfgTagsVal}
-        #         is_mfg = (0x8000 <= t.value <= 0xBFFF)
-        #         if (t.value not in self.commonTags and
-        #                 t.value not in self.cleanTags and
-        #                 not is_mfg):
-        #             asserts.fail("Mode tag value is not a common tag, clean tag or vendor tag!")
-
-        #         # * for at least one Value field: {isCommonOrDerivedTagsVal}
-        #         if not is_mfg:
-        #             at_least_one_common_or_clean_tag = True
-
-        #     if not at_least_one_common_or_clean_tag:
-        #         asserts.fail("At least one mode tag must be a common tag or clean tag!")
-
-        # # Verify that at least one ModeOptionsStruct entry includes either the
-        # # Vacuum(0x4001) mode tag or the Mop(0x4002)mode tag in the ModeTags field
-        # vacuum_mop_tags = [Clusters.RvcCleanMode.Enums.ModeTag.kVacuum, Clusters.RvcCleanMode.Enums.ModeTag.kMop]
-        # has_vacuum_or_mop_mode_tag = False
-        # for m in supported_modes:
-        #     has_vacuum_or_mop_mode_tag = any(t.value in vacuum_mop_tags for t in m.modeTags)
-        #     if has_vacuum_or_mop_mode_tag:
-        #         break
-
-        # asserts.assert_true(has_vacuum_or_mop_mode_tag,
-        #                     "At least one ModeOptionsStruct entry must include either the Vacuum or Mop mode tag")
-
-
-        #     self.print_step(3, "Read CurrentMode attribute")
-        #     current_mode = await self.read_mod_attribute_expect_success(endpoint=self.endpoint, attribute=attributes.CurrentMode)
-
-        #     logging.info("CurrentMode: %s" % (current_mode))
-        #     asserts.assert_true(current_mode in self.supported_modes_dut, "CurrentMode is not a supported mode!")
 
 
 if __name__ == "__main__":
