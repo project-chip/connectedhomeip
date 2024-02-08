89312013c3a006e267aaefd31409a05d0540a9d5
Lazar Kovacic <lkovacic@amazon.com>
2024-02-08 05:24:39 +0100

    TV Matter Media: Resolve cluster revisions (#31863)
    * Resolve the cluster revision issues
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Update per comments
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Update Content Launcher
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- examples/tv-app/android/java/ChannelManager.h
+++ examples/tv-app/android/java/ChannelManager.h
@@ -65,6 +65,7 @@ public:
                                    const chip::ByteSpan & data) override;
 
     uint32_t GetFeatureMap(chip::EndpointId endpoint) override;
+    uint16_t GetClusterRevision(chip::EndpointId endpoint) override;
 
 private:
     chip::JniGlobalReference mChannelManagerObject;
@@ -81,5 +82,6 @@ private:
     jmethodID mCancelRecordProgramMethod = nullptr;
 
     // TODO: set this based upon meta data from app
-    uint32_t mDynamicEndpointFeatureMap = 3;
+    static constexpr uint32_t kEndpointFeatureMap = 3;
+    static constexpr uint16_t kClusterRevision    = 2;
 };
