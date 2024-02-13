bac8f4e04f3fdebe89dbb67ee14e4b6ebf25372c
Restyled.io <commits@restyled.io>
2024-02-13 01:48:54 +0000

    Restyled by isort

--- src/python_testing/TC_OpstateCommon.py
+++ src/python_testing/TC_OpstateCommon.py
@@ -16,18 +16,18 @@
 #
 
 import logging
-import time
 import queue
+import time
+from dataclasses import dataclass
 
 import chip.clusters as Clusters
+import psutil
 from chip.clusters import ClusterObjects as ClusterObjects
 from chip.clusters.Attribute import EventReadResult, SubscriptionTransaction
 from chip.clusters.Types import NullValue
 from chip.interaction_model import InteractionModelError, Status
+from matter_testing_support import EventChangeCallback, TestStep
 from mobly import asserts
-from dataclasses import dataclass
-from matter_testing_support import TestStep, EventChangeCallback
-import psutil
 
 
 def get_pid(name):
