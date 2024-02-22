9b67bcab14a5062d3d0c55cbaf97cdd0f63fd4f1
Rohit Jadhav <rohit.jadhav@espressif.com>
2024-02-22 21:50:41 +0530

    Addressed review comments

--- examples/platform/esp32/time/TimeSync.cpp
+++ examples/platform/esp32/time/TimeSync.cpp
@@ -18,6 +18,7 @@
 
 #include "TimeSync.h"
 #include <esp_sntp.h>
+#include <lib/support/CHIPMemString.h>
 #include <lib/support/logging/CHIPLogging.h>
 
 #define REF_TIME 1546300800 /* 01-Jan-2019 00:00:00 */
@@ -34,12 +35,17 @@ static CHIP_ERROR GetLocalTimeString(char * buf, size_t buf_len)
     time_t now;
     time(&now);
     localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c %z[%Z]", &timeinfo);
+    if (strftime(strftime_buf, sizeof(strftime_buf), "%Y-%m-%dT%H:%M:%S%z", &timeinfo) == 0)
+    {
+        ChipLogError(DeviceLayer, "Buffer too small");
+        return CHIP_ERROR_BUFFER_TOO_SMALL;
+    }
     size_t print_size = snprintf(buf, buf_len, "%s, DST: %s", strftime_buf, timeinfo.tm_isdst ? "Yes" : "No");
     if (print_size >= buf_len)
     {
-        ChipLogError(DeviceLayer, "Buffer size %d insufficient for localtime string. Required size: %d", buf_len, print_size);
-        return CHIP_ERROR_INVALID_ARGUMENT;
+        ChipLogError(DeviceLayer, "Buffer size %d insufficient for localtime string. Required size: %d", static_cast<int>(buf_len),
+                     static_cast<int>(print_size));
+        return CHIP_ERROR_BUFFER_TOO_SMALL;
     }
     return CHIP_NO_ERROR;
 }
@@ -53,11 +59,12 @@ static bool ValidateTime()
 
 static CHIP_ERROR PrintCurrentTime()
 {
-    char local_time[64];
-    ReturnErrorCodeIf(GetLocalTimeString(local_time, sizeof(local_time)) != CHIP_NO_ERROR, CHIP_ERROR_INVALID_ARGUMENT);
+    char local_time[64] = { 0 };
+    ReturnErrorCodeIf(GetLocalTimeString(local_time, sizeof(local_time)) != CHIP_NO_ERROR, CHIP_ERROR_BUFFER_TOO_SMALL);
     if (!ValidateTime())
     {
         ChipLogProgress(DeviceLayer, "Time not synchronised yet.");
+        return CHIP_ERROR_INCORRECT_STATE;
     }
     ChipLogProgress(DeviceLayer, "The current time is: %s.", local_time);
     return CHIP_NO_ERROR;
@@ -66,7 +73,10 @@ static CHIP_ERROR PrintCurrentTime()
 static void TimeSyncCallback(struct timeval * tv)
 {
     ChipLogProgress(DeviceLayer, "SNTP Synchronised.");
-    PrintCurrentTime();
+    if (PrintCurrentTime() != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "SNTP time is not synchronised.");
+    }
 }
 
 } // anonymous namespace
@@ -75,7 +85,7 @@ namespace chip {
 
 void Esp32TimeSync::Init(const char * aSntpServerName, const uint16_t aSyncSntpIntervalDay)
 {
-    strcpy(mSntpServerName, aSntpServerName);
+    chip::Platform::CopyString(mSntpServerName, aSntpServerName);
     if (esp_sntp_enabled())
     {
         ChipLogProgress(DeviceLayer, "SNTP already initialized.");
