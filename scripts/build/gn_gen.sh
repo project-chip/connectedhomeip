89c830aae242bd2985a1fe5e2c6c2a9cb58b913c
Andrei Litvin <andy314@gmail.com>
2024-02-28 13:45:32 -0500

    Set a data model for gn gen

--- scripts/build/gn_gen.sh
+++ scripts/build/gn_gen.sh
@@ -21,8 +21,24 @@ set -e
 # GN gen script for GN build GitHub workflow.
 
 CHIP_ROOT="$(dirname "$0")/../.."
+INPUT_ARGS="$@"
+EXTRA_ARGS=""
 if [[ -z "${CHIP_ROOT_PATH}" ]]; then
     CHIP_ROOT_PATH=""
+    # If no app, build everything (e.g. tests or sample apps that do not care about the data model)
+    # with a fixed "include everything" data model
+    EXTRA_ARGS="chip_static_data_model_BACKEND=\"//src/controller/data_model:data_model\""
+fi
+
+if [[ -n "EXTRA_ARGS" ]]; then
+    echo "INPUT:"
+    echo "BEFORE: $INPUT_ARGS"
+    if [[ "$INPUT_ARGS" == *"--args="* ]]; then
+        INPUT_ARGS="${INPUT_ARGS/--args=/--args=$EXTRA_ARGS }"
+    else
+        INPUT_ARGS="--args=$EXTRA_ARGS $INPUT_ARGS"
+    fi
+    echo "AFTER:  $INPUT_ARGS"
 fi
 
 source "$CHIP_ROOT/scripts/activate.sh"
@@ -31,4 +47,5 @@ set -x
 
 env
 
-gn --root="$CHIP_ROOT/$CHIP_ROOT_PATH" gen --check --fail-on-unused-args "$CHIP_ROOT/out/$BUILD_TYPE" "$@"
+echo gn --root="$CHIP_ROOT/$CHIP_ROOT_PATH" gen --check --fail-on-unused-args "$CHIP_ROOT/out/$BUILD_TYPE" "$@" "$INPUT_ARGS"
+gn --root="$CHIP_ROOT/$CHIP_ROOT_PATH" gen --check --fail-on-unused-args "$CHIP_ROOT/out/$BUILD_TYPE" "$INPUT_ARGS"
