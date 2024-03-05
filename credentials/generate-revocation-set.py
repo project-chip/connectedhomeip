74210e68dfb62093435485397528ad2493db19c7
Vijay Selvaraj <vijay.selva@hotmail.com>
2024-03-05 13:12:03 -0500

    Updated the script to generate a basic RevocationSet from TestNet or MainNet

--- credentials/generate-revocation-set.py
+++ credentials/generate-revocation-set.py
@@ -30,6 +30,9 @@ import requests
 from click_option_group import RequiredMutuallyExclusiveOptionGroup, optgroup
 from cryptography import x509
 
+OID_VENDOR_ID = x509.ObjectIdentifier("1.3.6.1.4.1.37244.2.1")
+OID_PRODUCT_ID = x509.ObjectIdentifier("1.3.6.1.4.1.37244.2.2")
+
 PRODUCTION_NODE_URL = "https://on.dcl.csa-iot.org:26657"
 PRODUCTION_NODE_URL_REST = "https://on.dcl.csa-iot.org"
 TEST_NODE_URL_REST = "https://on.test-net.dcl.csa-iot.org"
@@ -76,33 +79,98 @@ def main(use_main_net_dcld, use_test_net_dcld, use_main_net_http, use_test_net_h
 
         revocation_point_list = json.loads(cmdpipe.stdout.read())["PkiRevocationDistributionPoint"]
 
-    crl_list = []
-
-    # TODO: Use the following two lines for testing purposes. Remove them once DCL is populated with valid CRLs.
-    # r = requests.get("<crl url>")
-    # crl_list.append(x509.load_der_x509_crl(r.content))
-
-    for revocation_point in revocation_point_list:
-        r = requests.get(revocation_point["dataURL"])
-        crl_list.append(x509.load_der_x509_crl(r.content))
-
     revocation_set = []
 
-    for crl in crl_list:
-        serialnumber_list = []
+    for revocation_point in revocation_point_list:
+        # 1. Validate Revocation Type
+        if revocation_point["revocationType"] != 1:
 
-        for r in crl:
-            serialnumber_list.append(bytes(str('{:02X}'.format(r.serial_number)), 'utf-8').decode('utf-8'))
+            # 2. Parse the certificate
+            crl_signer_certificate = x509.load_pem_x509_certificate(revocation_point["crlSignerCertificate"])
 
-        authority_key_id = crl.extensions.get_extension_for_oid(x509.OID_AUTHORITY_KEY_IDENTIFIER).value.key_identifier
-        issuer_subject_key_id = ''.join('{:02X}'.format(x) for x in authority_key_id)
+            vid = revocation_point["vid"]
+            pid = revocation_point["pid"]
+            is_paa = revocation_point["isPAA"]
 
-        issuer_name = base64.b64encode(crl.issuer.public_bytes()).decode('utf-8')
+            # 3. && 4. Validate VID/PID
+            crl_vid_list = crl_signer_certificate.subject.get_attributes_for_oid(OID_VENDOR_ID)
+            crl_pid_list = crl_signer_certificate.subject.get_attributes_for_oid(OID_PRODUCT_ID)
 
-        revocation_set.append({"type": "revocation_set",
-                               "issuer_subject_key_id": issuer_subject_key_id,
-                               "issuer_name": issuer_name,
-                               "revoked_serial_numbers": serialnumber_list})
+            if is_paa == True:
+                if len(crl_vid_list) == 1:
+                    if vid != int(crl_vid_list[0].value):
+                        continue
+            else:
+                if vid != int(crl_vid_list[0].value):
+                    continue
+                if len(crl_pid_list) == 1:
+                    if pid != int(crl_pid_list[0].value):
+                        continue
+
+            # TODO: 5.
+
+            # 6. Obtain the CRL
+            r = requests.get(revocation_point["dataURL"])
+            crl_file = x509.load_der_x509_crl(r.content)
+
+            # 7. Perform CRL File Validation
+            crl_authority_key_id = crl_file.extensions.get_extension_for_oid(x509.OID_AUTHORITY_KEY_IDENTIFIER).value.key_identifier
+            crl_signer_subject_key_id = crl_signer_certificate.extensions.get_extension_for_oid(
+                x509.OID_SUBJECT_KEY_IDENTIFIER).value.key_identifier
+            if crl_authority_key_id != crl_signer_subject_key_id:
+                continue
+
+            issuer_subject_key_id = ''.join('{:02X}'.format(x) for x in crl_authority_key_id)
+
+            same_issuer_points = None
+
+            if use_rest:
+                response = requests.get(
+                    f"{rest_node_url}/dcl/pki/revocation-points/{issuer_subject_key_id}").json()["pkiRevocationDistributionPointsByIssuerSubjectKeyID"]
+                same_issuer_points = response["points"]
+            else:
+                cmdlist = ['query', 'pki', 'revocation-points', '--issuer-subject-key-id', issuer_subject_key_id]
+                cmdpipe = subprocess.Popen(use_dcld(dcld, production, cmdlist), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                same_issuer_points = json.loads(cmdpipe.stdout.read())["pkiRevocationDistributionPointsByIssuerSubjectKeyID"]["points"]
+
+            matching_entries = False
+            for same_issuer_point in same_issuer_points:
+                if same_issuer_point["vid"] == vid:
+                    matching_entries = True
+                    break
+
+            if matching_entries:
+                try:
+                    issuing_distribution_point = crl_file.extensions.get_extension_for_oid(
+                        x509.OID_ISSUING_DISTRIBUTION_POINT).value
+                except Exception:
+                    continue
+
+                uri_list = issuing_distribution_point.full_name
+                if len(uri_list) == 1 and isinstance(uri_list[0], x509.UniformResourceIdentifier):
+                    if uri_list[0].value != revocation_point["dataURL"]:
+                        continue
+                else:
+                    continue
+
+            # TODO: 8.
+
+            # 9. Assign CRL File Issuer
+            certificate_authority_name = base64.b64encode(crl_file.issuer.public_bytes()).decode('utf-8')
+
+            serialnumber_list = []
+            # 10. Iterate through the Revoked Certificates List
+            for revoked_cert in crl_file:
+                # TODO: How to find certificate issuer extension in revoked cert?
+
+                serialnumber_list.append(bytes(str('{:02X}'.format(r.serial_number)), 'utf-8').decode('utf-8'))
+
+            issuer_name = base64.b64encode(crl_file.issuer.public_bytes()).decode('utf-8')
+
+            revocation_set.append({"type": "revocation_set",
+                                   "issuer_subject_key_id": issuer_subject_key_id,
+                                   "issuer_name": issuer_name,
+                                   "revoked_serial_numbers": serialnumber_list})
 
     with open(output, 'w+') as outfile:
         json.dump(revocation_set, outfile, indent=4)
