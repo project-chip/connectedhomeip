28da08fbdcba82144be73ebffc9cf2f9a0be660f
dinabenamar <108664279+dinabenamar@users.noreply.github.com>
2024-03-07 14:44:00 +0100

    [Zephyr] Make include of "zephyr/bluetooth/hci.h" explicit (#32461)

--- src/platform/Zephyr/BLEManagerImpl.cpp
+++ src/platform/Zephyr/BLEManagerImpl.cpp
@@ -39,6 +39,7 @@
 
 #include <zephyr/bluetooth/addr.h>
 #include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
 #include <zephyr/random/rand32.h>
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/sys/util.h>
