37c7a833ed3e7fae26e1b696c147a3b3d469054b
Restyled.io <commits@restyled.io>
2024-03-07 00:48:28 +0000

    Restyled by whitespace

--- src/controller/java/src/chip/devicecontroller/PairingHintBitmap.java
+++ src/controller/java/src/chip/devicecontroller/PairingHintBitmap.java
@@ -50,7 +50,7 @@ public enum PairingHintBitmap {
     public int getBitIndex() {
         return bitIndex;
     }
-    
+
     public boolean getRequirePairingInstruction() {
         return isRequirePairingInstruction;
     }
