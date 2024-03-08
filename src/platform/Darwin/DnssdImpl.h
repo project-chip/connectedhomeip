125f6c7006a5b3e0b4874b18f650840931b609b1
Boris Zbarsky <bzbarsky@apple.com>
2024-03-08 12:51:37 -0500

    Prioritize DNS-SD results from the network on tvOS.
    Since we can only deliver results from one interface given the current state of
    Matter SDK APIs (see
    https://github.com/project-chip/connectedhomeip/issues/32512), prioritize the
    interfaces that are more likely to have up-to-date results, not stale caches.

--- src/platform/Darwin/DnssdImpl.h
+++ src/platform/Darwin/DnssdImpl.h
@@ -251,6 +251,14 @@ struct ResolveContext : public GenericContext
                         const unsigned char * txtRecord);
     bool HasInterface();
     bool Matches(const char * otherInstanceName) const { return instanceName == otherInstanceName; }
+
+private:
+    /**
+     * Try reporting the results we got on the provided interface index.
+     * Returns true if information was reported, false if not (e.g. if there
+     * were no IP addresses, etc).
+     */
+    bool TryReportingResultsForInterfaceIndex(uint32_t interfaceIndex);
 };
 
 } // namespace Dnssd
