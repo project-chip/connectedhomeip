f06357c9e2a6e4133ac3c68886cd2fed6716a285
Andrei Litvin <andy314@gmail.com>
2024-03-18 12:44:54 -0400

    Create a ember-compatibility-functions header to define what the cpp contains

--- src/app/util/ember-compatibility-functions.cpp
+++ src/app/util/ember-compatibility-functions.cpp
@@ -14,12 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
-/**
- *    @file
- *          Contains the functions for compatibility with ember ZCL inner state
- *          when calling ember callbacks.
- */
+#include <app/ember-compatibility-functions.h>
 
 #include <access/AccessControl.h>
 #include <app/CommandHandlerInterface.h>
