e31e5c4d7a92a5a3cd581838d6c98969a2ff6247
Mathieu Kardous <mathieu.kardous@silabs.com>
2024-03-19 12:02:39 +0800

    restyle

--- src/app/tests/TestReadInteraction.cpp
+++ src/app/tests/TestReadInteraction.cpp
@@ -1373,7 +1373,8 @@ void TestReadInteraction::TestSetDirtyBetweenChunks(nlTestSuite * apSuite, void 
         public:
             DirtyingMockDelegate(AttributePathParams (&aReadPaths)[2], int & aNumAttributeResponsesWhenSetDirty,
                                  int & aNumArrayItemsWhenSetDirty) :
-                mReadPaths(aReadPaths), mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
+                mReadPaths(aReadPaths),
+                mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
                 mNumArrayItemsWhenSetDirty(aNumArrayItemsWhenSetDirty)
             {}
 
