30549f0bc913ebaabd0089d7ae4ac86c856bd8d6
Marcin Kajor <marcin.kajor@nordicsemi.no>
2024-03-21 10:36:37 +0100

    [nrfconnect] Wi-FI directed scan implementation
    This patch implements directed scanning and
    single ssid scanning during commissioning.
    
    Signed-off-by: Marcin Kajor <marcin.kajor@nordicsemi.no>

--- src/platform/nrfconnect/wifi/WiFiManager.cpp
+++ src/platform/nrfconnect/wifi/WiFiManager.cpp
@@ -189,7 +189,22 @@ CHIP_ERROR WiFiManager::Scan(const ByteSpan & ssid, ScanResultCallback resultCal
     mWiFiState          = WIFI_STATE_SCANNING;
     mSsidFound          = false;
 
-    if (0 != net_mgmt(NET_REQUEST_WIFI_SCAN, mNetIf, NULL, 0))
+    wifi_scan_params * scanParams{ nullptr };
+    size_t scanParamsSize{ 0 };
+
+    if (!ssid.empty())
+    {
+        /* We must assume that the ssid is handled as a NULL-terminated string.
+           Note that the mScanSsidBuffer is initialized with zeros. */
+        VerifyOrReturnError(ssid.size() < sizeof(mScanParams), CHIP_ERROR_INVALID_ARGUMENT);
+        memcpy(mScanSsidBuffer, ssid.data(), ssid.size());
+        mScanSsidBuffer[ssid.size()] = 0; // indicate the end of ssid string
+        mScanParams.ssids[0]         = mScanSsidBuffer;
+        mScanParams.ssids[1]         = nullptr; // indicate the end of ssids list
+        scanParams                   = &mScanParams;
+        scanParamsSize               = sizeof(*scanParams);
+    }
+    if (0 != net_mgmt(NET_REQUEST_WIFI_SCAN, mNetIf, scanParams, scanParamsSize))
     {
         ChipLogError(DeviceLayer, "Scan request failed");
         return CHIP_ERROR_INTERNAL;
