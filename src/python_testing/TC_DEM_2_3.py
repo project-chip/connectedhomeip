8332bad6fdf07b48d46d3d300b933d8af638ae0a
Restyled.io <commits@restyled.io>
2024-03-25 11:08:27 +0000

    Restyled by isort

--- src/python_testing/TC_DEM_2_3.py
+++ src/python_testing/TC_DEM_2_3.py
@@ -18,8 +18,8 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.interaction_model import Status
 from chip.clusters.Types import NullValue
+from chip.interaction_model import Status
 from matter_testing_support import EventChangeCallback, MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 from TC_DEM_Utils import DEMBaseTestHelper
