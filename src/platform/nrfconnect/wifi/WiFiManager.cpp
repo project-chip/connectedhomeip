c65e6f0d92be967a74c787474aceb4602181f5d1
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2024-03-26 11:46:35 +0100

    [nrfconnect] wifi: Fix 5GHz association
    Wi-Fi stack recently introduced a check for valid band value and the
    default value of 0 (memset) means only 2.4GHz, so, 5GHz Wi-Fi
    associations will fail.
    
    Fix the default to Unknown to scan all supported bands.

--- src/platform/nrfconnect/wifi/WiFiManager.cpp
+++ src/platform/nrfconnect/wifi/WiFiManager.cpp
@@ -352,6 +352,7 @@ void WiFiManager::ScanResultHandler(Platform::UniquePtr<uint8_t> data)
             Instance().mWiFiParams.mParams.timeout = Instance().mHandling.mConnectionTimeout.count();
             Instance().mWiFiParams.mParams.channel = WIFI_CHANNEL_ANY;
             Instance().mWiFiParams.mRssi           = scanResult->rssi;
+            Instance().mWiFiParams.mParams.band    = WIFI_FREQ_BAND_UNKNOWN;
             Instance().mSsidFound                  = true;
         }
     }
