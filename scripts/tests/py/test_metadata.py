e43ca09e4b34332bdbac0dfa1ea684c181984fb5
Vatsal Ghelani <152916324+vatsalghelani-csa@users.noreply.github.com>
2024-03-27 12:45:03 -0400

    Update test_metadata.py to correct the location for TC_RR* test script

--- scripts/tests/py/test_metadata.py
+++ scripts/tests/py/test_metadata.py
@@ -23,12 +23,12 @@ class TestMetadataReader(unittest.TestCase):
     def setUp(self):
 
         # build the reader object
-        self.reader=Metadata_Reader("/src/python_testing", "/scripts/tests/py/env.yaml")
+        self.reader=Metadata_Reader("/scripts/tests/py", "/scripts/tests/py/env.yaml")
         self.actual_runs_metadata = self.reader.get_runs_metadata()
         self.expected_runs_metadata = {}
 
-        self.expected_runs_metadata["/src/python_testing/TC_RR_1_1.py+run1"] = Metadata(app="out/linux-x64-all-clusters-ipv6only-no-ble-no-wifi-tsan-clang-test/chip-all-clusters-app",
-                   discriminator=1234, py_script_path="/src/python_testing/TC_RR_1_1.py", run="run1", passcode=20202021)
+        self.expected_runs_metadata["/scripts/tests/py/TC_RR_1_1_Test.py+run1"] = Metadata(app="out/linux-x64-all-clusters-ipv6only-no-ble-no-wifi-tsan-clang-test/chip-all-clusters-app",
+                   discriminator=1234, py_script_path="/scripts/tests/py/TC_RR_1_1_Test.py", run="run1", passcode=20202021)
         
     # gets the metadata associated with each run by parsing the test script and compares it to the expected metadata values
     def test_case1(self):
