fc38fb267ec48db6a1506eb44345b7147a6170b8
Vatsal Ghelani <152916324+vatsalghelani-csa@users.noreply.github.com>
2024-03-27 14:01:55 -0400

    Updated and rename TC_RR_1_1_Test.py to simple_run_args.py
    Just keeping the runner arguments required for testing purpose and removing all the unwanted test script code for this current testing use case

---
+++ scripts/tests/py/simple_run_args.py
@@ -0,0 +1,5 @@
+
+# run arguments:
+# test-runner-runs: run1
+# test-runner-run/run1/app: app/all-clusters discriminator KVS trace-to-appjson
+# test-runner-run/run1/script: storage-path commissioning-method discriminator passcode trace-to-testjson trace-to-testperfetto
