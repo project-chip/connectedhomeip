58222f1976f0d563b1d61db73be86bb64844fd76
Andrei Litvin <andreilitvin@google.com>
2024-03-28 13:39:40 -0400

    Restyle

--- examples/platform/silabs/SoftwareFaultReports.cpp
+++ examples/platform/silabs/SoftwareFaultReports.cpp
@@ -20,9 +20,9 @@
 #include "FreeRTOS.h"
 #include "silabs_utils.h"
 #include <app/clusters/software-diagnostics-server/software-diagnostics-server.h>
+#include <app/util/attribute-storage.h>
 #include <lib/support/CHIPMemString.h>
 #include <lib/support/CodeUtils.h>
-#include <app/util/attribute-storage.h>
 #include <platform/CHIPDeviceLayer.h>
 #include <platform/DiagnosticDataProvider.h>
 
