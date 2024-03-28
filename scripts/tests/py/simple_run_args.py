cf2768fcbbe6bc5aae2e0bd34212c17d86cd5521
Vatsal Ghelani <152916324+vatsalghelani-csa@users.noreply.github.com>
2024-03-28 11:54:16 -0400

    Updated the test script to be a .txt file and changed the designed to be all run arguments in one line

--- scripts/tests/py/simple_run_args.py
+++ scripts/tests/py/simple_run_args.py
@@ -1,5 +1,3 @@
-
-# run arguments:
+# runner arguments:
 # test-runner-runs: run1
-# test-runner-run/run1/app: app/all-clusters discriminator KVS trace-to-appjson
-# test-runner-run/run1/script: storage-path commissioning-method discriminator passcode trace-to-testjson trace-to-testperfetto
+# test-runner-run/run1: app/all-clusters discriminator KVS storage-path commissioning-method discriminator passcode
