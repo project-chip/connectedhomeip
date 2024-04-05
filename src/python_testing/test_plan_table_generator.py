415ee0a491ea7506a1cf7035309ab037d606ccec
cecille <cecille@google.com>
2024-04-05 17:52:17 -0400

    update spacing on expected outcome

--- src/python_testing/test_plan_table_generator.py
+++ src/python_testing/test_plan_table_generator.py
@@ -41,14 +41,18 @@ def main(filename, classname, test):
     test_instance = test_class(config)
     steps = test_instance.get_test_steps(test)
     indent = 6
+    header_num = f'{"**#**":<{indent}}'
+    header_num_step = f'|{header_num} |*TestStep*   '
     s = ('[cols="5%,45%,45%"]\n'
          '|===\n'
-         '|**#**  |*Test Step*|*Expected Outcome*\n')
+         f'{header_num_step}|*Expected Outcome*\n')
     for step in steps:
-        # add 2 to indent for a| and | at start
-        s += f'|{step.test_plan_number:<{indent}}a|{indent_multiline(step.description, indent+3)}\n'
-        padding = (indent + 1) * ' '
-        s += f'{padding}a|{indent_multiline(step.expectation, indent+3)}\n\n'
+        step_num = f'|{step.test_plan_number:<{indent}}a|'
+        s += f'{step_num}{indent_multiline(step.description, len(step_num))}\n'
+
+        padding = (len(header_num_step) - 1) * ' '
+        # add 2 to indent for a| at start
+        s += f'{padding}a|{indent_multiline(step.expectation, len(padding)+2)}\n\n'
     s += '|===\n'
 
     print(s)
