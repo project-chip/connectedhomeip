eb5941f8110eec3f5e05005de57e895c360acfe6
Rob Bultman <rob.bultman@gmail.com>
2024-04-05 09:01:12 -0400

    Fix lint error from CI

--- src/python_testing/TC_FAN_3_3.py
+++ src/python_testing/TC_FAN_3_3.py
@@ -19,7 +19,7 @@ import logging
 import time
 
 import chip.clusters as Clusters
-from chip.interaction_model import InteractionModelError, Status
+from chip.interaction_model import Status
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
