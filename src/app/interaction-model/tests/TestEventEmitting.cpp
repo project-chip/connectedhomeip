f855e93453bf4976a5ac644a9c3eda9e904b7220
Andrei Litvin <andy314@gmail.com>
2024-04-09 12:37:32 -0400

    Some tests actually pass

--- src/app/interaction-model/tests/TestEventEmitting.cpp
+++ src/app/interaction-model/tests/TestEventEmitting.cpp
@@ -17,7 +17,9 @@
  */
 
 #include <app-common/zap-generated/cluster-objects.h>
+#include <app/data-model/Decode.h>
 #include <app/interaction-model/events.h>
+#include <lib/support/CodeUtils.h>
 
 #include <gtest/gtest.h>
 
@@ -27,7 +29,8 @@ using namespace chip;
 using namespace chip::app;
 using namespace chip::app::InteractionModel;
 
-using StartUpEventType = chip::app::Clusters::BasicInformation::Events::StartUp::Type;
+using StartUpEventType              = chip::app::Clusters::BasicInformation::Events::StartUp::Type;
+using AccessControlEntryChangedType = chip::app::Clusters::AccessControl::Events::AccessControlEntryChanged::Type;
 
 constexpr uint32_t kFakeSoftwareVersion = 0x1234abcd;
 
@@ -37,17 +40,53 @@ public:
     virtual CHIP_ERROR EmitEvent(EventLoggingDelegate * eventContentWriter, const EventOptions & options,
                                  EventNumber & generatedEventNumber)
     {
+        TLV::TLVWriter writer;
+        TLV::TLVType outerType;
+        writer.Init(mLastEventEncodeBuffer);
+
+        ReturnErrorOnFailure(writer.StartContainer(TLV::AnonymousTag(), TLV::kTLVType_Structure, outerType));
+        ReturnErrorOnFailure(eventContentWriter->WriteEvent(writer));
+        ReturnErrorOnFailure(writer.EndContainer(outerType));
+        ReturnErrorOnFailure(writer.Finalize());
+        mLastEncodedSpan = ByteSpan(mLastEventEncodeBuffer, writer.GetLengthWritten());
+
         mLastOptions         = options;
         generatedEventNumber = ++mCurrentEventNumber;
+
         return CHIP_NO_ERROR;
     }
 
     EventNumber CurrentEventNumber() const { return mCurrentEventNumber; }
     const EventOptions & LastOptions() const { return mLastOptions; }
+    ByteSpan LastWrittenEvent() const { return mLastEncodedSpan; }
+
+    // This relies on the default encoding of events which uses
+    // DataModel::Encode on a EventDataIB::Tag::kData
+    template <typename T>
+    CHIP_ERROR DecodeLastEvent(T & dest)
+    {
+        // attempt to decode the last encoded event
+        TLV::TLVReader reader;
+        TLV::TLVType outerType;
+
+        reader.Init(LastWrittenEvent());
+
+        ReturnErrorOnFailure(reader.Next());
+        ReturnErrorOnFailure(reader.EnterContainer(outerType));
+
+        ReturnErrorOnFailure(reader.Next()); // MUST be positioned on the first element
+        ReturnErrorOnFailure(DataModel::Decode(reader, dest));
+
+        ReturnErrorOnFailure(reader.ExitContainer(outerType));
+
+        return CHIP_NO_ERROR;
+    }
 
 private:
     EventNumber mCurrentEventNumber = 0;
     EventOptions mLastOptions;
+    uint8_t mLastEventEncodeBuffer[128];
+    ByteSpan mLastEncodedSpan;
 };
 
 } // namespace
@@ -64,10 +103,60 @@ TEST(TestInteractionModelEventEmitting, TestBasicType)
     ASSERT_EQ(logOnlyEvents.LastOptions().mPath,
               ConcreteEventPath(0 /* endpointId */, StartUpEventType::GetClusterId(), StartUpEventType::GetEventId()));
 
+    chip::app::Clusters::BasicInformation::Events::StartUp::DecodableType decoded_event;
+    CHIP_ERROR err = logOnlyEvents.DecodeLastEvent(decoded_event);
+
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(EventLogging, "Decoding failed: %" CHIP_ERROR_FORMAT, err.Format());
+    }
+    ASSERT_EQ(err, CHIP_NO_ERROR);
+    ASSERT_EQ(decoded_event.softwareVersion, kFakeSoftwareVersion);
+
     EventNumber n2 = events->EmitEvent(event, 1);
     ASSERT_EQ(n2, logOnlyEvents.CurrentEventNumber());
     ASSERT_NE(n1, logOnlyEvents.CurrentEventNumber());
 
     ASSERT_EQ(logOnlyEvents.LastOptions().mPath,
               ConcreteEventPath(1 /* endpointId */, StartUpEventType::GetClusterId(), StartUpEventType::GetEventId()));
+}
+
+TEST(TestInteractionModelEventEmitting, TestFabricScoped)
+{
+    constexpr NodeId kTestNodeId           = 0x12ab;
+    constexpr uint16_t kTestPasscode       = 12345;
+    constexpr FabricIndex kTestFabricIndex = kMinValidFabricIndex + 10;
+    static_assert(kTestFabricIndex != kUndefinedFabricIndex);
+
+    LogOnlyEvents logOnlyEvents;
+    Events * events = &logOnlyEvents;
+
+    AccessControlEntryChangedType event;
+    event.adminNodeID     = chip::app::DataModel::MakeNullable(kTestNodeId);
+    event.adminPasscodeID = chip::app::DataModel::MakeNullable(kTestPasscode);
+
+    EventNumber n1 = events->EmitEvent(event, 0 /* EndpointId */);
+    // encoding without a fabric ID MUST fail for fabric events
+    ASSERT_EQ(n1, kInvalidEventId);
+
+    event.fabricIndex = kTestFabricIndex;
+    n1                = events->EmitEvent(event, 0 /* EndpointId */);
+
+    ASSERT_NE(n1, kInvalidEventId);
+    ASSERT_EQ(n1, logOnlyEvents.CurrentEventNumber());
+    ASSERT_EQ(logOnlyEvents.LastOptions().mPath,
+              ConcreteEventPath(0 /* endpointId */, AccessControlEntryChangedType::GetClusterId(),
+                                AccessControlEntryChangedType::GetEventId()));
+
+    chip::app::Clusters::AccessControl::Events::AccessControlEntryChanged::DecodableType decoded_event;
+    CHIP_ERROR err = logOnlyEvents.DecodeLastEvent(decoded_event);
+
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(EventLogging, "Decoding failed: %" CHIP_ERROR_FORMAT, err.Format());
+    }
+    ASSERT_EQ(err, CHIP_NO_ERROR);
+    ASSERT_EQ(decoded_event.adminNodeID.ValueOr(0), kTestNodeId);
+    ASSERT_EQ(decoded_event.adminPasscodeID.ValueOr(0), kTestPasscode);
+    ASSERT_EQ(decoded_event.fabricIndex, kTestFabricIndex);
 }
