1de8b475035521316b4b40cf8cd809131835a407
Arkadiusz Bokowy <a.bokowy@samsung.com>
2024-04-09 09:17:54 +0200

    Copyrights from origin project

--- src/ble/tests/TestBtpEngine.cpp
+++ src/ble/tests/TestBtpEngine.cpp
@@ -1,6 +1,8 @@
 /*
  *
  *    Copyright (c) 2024 Project CHIP Authors
+ *    Copyright (c) 2018 Google LLC.
+ *    Copyright (c) 2018 Nest Labs, Inc.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
