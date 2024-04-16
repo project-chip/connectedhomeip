bb627ae852560081a6fc607fdfb7df10dcd60641
Restyled.io <commits@restyled.io>
2024-04-16 16:51:58 +0000

    Restyled by isort

--- scripts/tests/py/test_metadata.py
+++ scripts/tests/py/test_metadata.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import tempfile
 import unittest
 from os import path
-import tempfile
 from typing import List
 
 from metadata import Metadata, MetadataReader
