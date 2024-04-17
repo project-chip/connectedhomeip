aafc3c4bfdeba4690726c26ba70ecd2571980391
Jakub Latusek <j.latusek@samsung.com>
2024-04-17 14:20:31 +0200

    Pigweed tests for platform

--- src/platform/tests/TestConnectivityMgr.cpp
+++ src/platform/tests/TestConnectivityMgr.cpp
@@ -30,8 +30,8 @@
 
 #include <lib/support/CHIPMem.h>
 #include <lib/support/CodeUtils.h>
-#include <lib/support/UnitTestRegistration.h>
-#include <nlunit-test.h>
+
+#include <gtest/gtest.h>
 
 #include <platform/CHIPDeviceLayer.h>
 #include <platform/DiagnosticDataProvider.h>
@@ -45,63 +45,34 @@ using namespace chip::DeviceLayer;
 //      Unit tests
 // =================================
 
-static void TestPlatformMgr_Init(nlTestSuite * inSuite, void * inContext)
+struct TestConnectivityMgr : public ::testing::Test
 {
-    // ConfigurationManager is initialized from PlatformManager indirectly
-    CHIP_ERROR err = PlatformMgr().InitChipStack();
-    NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
-}
 
-static void TestConnectivityMgr_GetNetworkInterfaces(nlTestSuite * inSuite, void * inContext)
+    static void SetUpTestSuite()
+    {
+        // ConfigurationManager is initialized from PlatformManager indirectly
+        CHIP_ERROR err = PlatformMgr().InitChipStack();
+        EXPECT_EQ(err, CHIP_NO_ERROR);
+        err = chip::Platform::MemoryInit();
+        ASSERT_EQ(err, CHIP_NO_ERROR);
+    }
+
+    static void TearDownTestSuite()
+    {
+        chip::Platform::MemoryShutdown();
+        chip::DeviceLayer::PlatformMgr().Shutdown();
+    }
+};
+
+TEST_F(TestConnectivityMgr, GetNetworkInterfaces)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
 
     NetworkInterface * netifs = nullptr;
 
     err = GetDiagnosticDataProvider().GetNetworkInterfaces(&netifs);
-    NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, netifs != nullptr);
+    EXPECT_EQ(err, CHIP_NO_ERROR);
+    ASSERT_NE(netifs, nullptr);
 
     GetDiagnosticDataProvider().ReleaseNetworkInterfaces(netifs);
 }
-
-/**
- *   Test Suite. It lists all the test functions.
- */
-static const nlTest sTests[] = {
-
-    NL_TEST_DEF("Test PlatformMgr::Init", TestPlatformMgr_Init),
-    NL_TEST_DEF("Test ConfigurationMgr::GetNetworkInterfaces", TestConnectivityMgr_GetNetworkInterfaces), NL_TEST_SENTINEL()
-};
-
-/**
- *  Set up the test suite.
- */
-int TestConnectivityMgr_Setup(void * inContext)
-{
-    CHIP_ERROR error = chip::Platform::MemoryInit();
-    if (error != CHIP_NO_ERROR)
-        return FAILURE;
-    return SUCCESS;
-}
-
-/**
- *  Tear down the test suite.
- */
-int TestConnectivityMgr_Teardown(void * inContext)
-{
-    PlatformMgr().Shutdown();
-    chip::Platform::MemoryShutdown();
-    return SUCCESS;
-}
-
-int TestConnectivityMgr()
-{
-    nlTestSuite theSuite = { "ConfigurationMgr tests", &sTests[0], TestConnectivityMgr_Setup, TestConnectivityMgr_Teardown };
-
-    // Run test suite against one context.
-    nlTestRunner(&theSuite, nullptr);
-    return nlTestRunnerStats(&theSuite);
-}
-
-CHIP_REGISTER_TEST_SUITE(TestConnectivityMgr)
