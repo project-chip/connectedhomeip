99cff599dd0e1c08b56b8ba80a9a0494fbad2f10
Kamil Kasperczyk <66371704+kkasperczyk-no@users.noreply.github.com>
2024-04-17 22:39:53 +0200

    [clusters] Implemented ThreadDiagnosticDelegate (#32964)
    Added ThreadDiagnosticDelegate to enable generation of optional
    events from ThreadNetworkDiagnostics cluster.
    
    Additionally implemented generation of ConnectionStatus
    and NetworkFaultChanged events when Thread link state
    is changed.

--- src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.h
+++ src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.h
@@ -40,6 +40,7 @@
 #include <app/icd/server/ICDServerConfig.h>
 #include <lib/dnssd/Advertiser.h>
 #include <lib/dnssd/platform/Dnssd.h>
+#include <platform/GeneralFaults.h>
 #include <platform/NetworkCommissioning.h>
 
 namespace chip {
@@ -231,6 +232,7 @@ private:
 
     DnsBrowseCallback mDnsBrowseCallback;
     DnsResolveCallback mDnsResolveCallback;
+    GeneralFaults<kMaxNetworkFaults> mNetworkFaults;
 
     struct DnsServiceTxtEntries
     {
