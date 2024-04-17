4a710230a4aa6c9a732f27bad3d24b33cb483552
Andrei Litvin <andy314@gmail.com>
2024-04-17 14:59:51 -0400

    move AttributeEncodeState to a separate file

--- src/app/AttributeValueEncoder.h
+++ src/app/AttributeValueEncoder.h
@@ -18,6 +18,7 @@
 
 #include <access/SubjectDescriptor.h>
 #include <app/AttributeReportBuilder.h>
+#include <app/AttributeEncodeState.h>
 #include <app/ConcreteAttributePath.h>
 #include <app/MessageDef/AttributeReportIBs.h>
 #include <app/data-model/FabricScoped.h>
@@ -27,60 +28,6 @@
 
 namespace chip {
 namespace app {
-
-/**
- * Maintains the internal state of list encoding
- */
-class AttributeEncodeState
-{
-public:
-    AttributeEncodeState() = default;
-
-    bool AllowPartialData() const { return mAllowPartialData; }
-    ListIndex CurrentEncodingListIndex() const { return mCurrentEncodingListIndex; }
-
-    AttributeEncodeState & SetAllowPartialData(bool allow)
-    {
-        mAllowPartialData = allow;
-        return *this;
-    }
-
-    AttributeEncodeState & SetCurrentEncodingListIndex(ListIndex idx)
-    {
-        mCurrentEncodingListIndex = idx;
-        return *this;
-    }
-
-    void Reset()
-    {
-        mCurrentEncodingListIndex = kInvalidListIndex;
-        mAllowPartialData         = false;
-    }
-
-private:
-    /**
-     * If set to kInvalidListIndex, indicates that we have not encoded any data for the list yet and
-     * need to start by encoding an empty list before we start encoding any list items.
-     *
-     * When set to a valid ListIndex value, indicates the index of the next list item that needs to be
-     * encoded (i.e. the count of items encoded so far).
-     */
-    ListIndex mCurrentEncodingListIndex = kInvalidListIndex;
-
-    /**
-     * When an attempt to encode an attribute returns an error, the buffer may contain tailing dirty data
-     * (since the put was aborted).  The report engine normally rolls back the buffer to right before encoding
-     * of the attribute started on errors.
-     *
-     * When chunking a list, EncodeListItem will atomically encode list items, ensuring that the
-     * state of the buffer is valid to send (i.e. contains no trailing garbage), and return an error
-     * if the list doesn't entirely fit.  In this situation, mAllowPartialData is set to communicate to the
-     * report engine that it should not roll back the list items.
-     *
-     * TODO: There might be a better name for this variable.
-     */
-    bool mAllowPartialData = false;
-};
 
 /**
  * The AttributeValueEncoder is a helper class for filling report payloads into AttributeReportIBs.
