5041d15fa45d9854e3f43a267811905bfe6051ec
cecille <cecille@google.com>
2024-04-18 15:01:05 -0400

    lint

--- src/python_testing/test_testing/test_TC_DGGEN_3_2.py
+++ src/python_testing/test_testing/test_TC_DGGEN_3_2.py
@@ -17,12 +17,10 @@
 #
 
 import sys
-import typing
 from dataclasses import dataclass
 
 import chip.clusters as Clusters
 from chip.clusters import Attribute
-from chip.clusters.Types import NullValue
 from MockTestRunner import MockTestRunner
 
 
