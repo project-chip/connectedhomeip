da86baba2af383e7ed7ace67f05ec5518695b3d1
Andrei Litvin <andy314@gmail.com>
2024-04-19 15:04:44 -0400

    Update examples/light-switch-app/infineon/cyw30739/src/BindingHandler.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- examples/light-switch-app/infineon/cyw30739/src/BindingHandler.cpp
+++ examples/light-switch-app/infineon/cyw30739/src/BindingHandler.cpp
@@ -306,7 +306,7 @@ void BindingHandler::PrintBindingTable()
             \t+ ClusterId %d \n \
             \t+ RemoteEndpointId %d \n \
             \t+ NodeId %d \n",
-                   (int) entry.fabricIndex, (int) entry.local, (int) entry.clusterId.value_or(-1), (int) entry.remote,
+                   (int) entry.fabricIndex, (int) entry.local, (int) entry.clusterId.value_or(kInvalidClusterId), (int) entry.remote,
                    (int) entry.nodeId);
             break;
         case MATTER_MULTICAST_BINDING:
