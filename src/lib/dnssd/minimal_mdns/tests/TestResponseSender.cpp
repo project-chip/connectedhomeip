7cdad21b500a7f37be3d002ad6805ffc97e6ab47
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2024-04-20 07:07:22 +0200

    Update src/lib/dnssd/minimal_mdns/tests/TestResponseSender.cpp

--- src/lib/dnssd/minimal_mdns/tests/TestResponseSender.cpp
+++ src/lib/dnssd/minimal_mdns/tests/TestResponseSender.cpp
@@ -87,7 +87,7 @@ struct CommonTestElements
 class TestResponseSender : public ::testing::Test
 {
 public:
-    static void SetUpTestSuite() { VerifyOrDie(chip::Platform::MemoryInit() == CHIP_NO_ERROR); }
+    static void SetUpTestSuite() { ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR); }
     static void TearDownTestSuite() { chip::Platform::MemoryShutdown(); }
 };
 
