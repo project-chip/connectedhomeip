107c627df6ae128b7d7d9abfd7ad356fe8b2a318
Andrei Litvin <andreilitvin@google.com>
2024-04-22 14:38:23 -0400

    Undo debug code

--- src/lib/dnssd/minimal_mdns/responders/QueryResponder.cpp
+++ src/lib/dnssd/minimal_mdns/responders/QueryResponder.cpp
@@ -96,13 +96,6 @@ size_t QueryResponderBase::MarkAdditional(const FullQName & qname)
             continue; // already marked
         }
 
-        // DO NOT SUBMIT: no addresses as additional data
-        if ((mResponderInfos[i].responder->GetQType() == QType::AAAA)
-      || (mResponderInfos[i].responder->GetQType() == QType::A))
-        {
-            continue;
-        }
-
         if (mResponderInfos[i].responder->GetQName() == qname)
         {
             mResponderInfos[i].reportNowAsAdditional = true;
