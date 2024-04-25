6164fc64ac42472f04c23aa178889e6888bb3916
Restyled.io <commits@restyled.io>
2024-04-25 17:30:38 +0000

    Restyled by isort

--- src/python_testing/test_plan_table_generator.py
+++ src/python_testing/test_plan_table_generator.py
@@ -15,8 +15,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-import logging
 import importlib
+import logging
 import os
 import sys
 from pathlib import Path
