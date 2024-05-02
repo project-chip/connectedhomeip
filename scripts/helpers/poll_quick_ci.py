5c186b82570e57bcc3ce9282f3e28b17d80a7888
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2024-05-02 11:34:08 -0700

    Update poll_quick_ci.py

--- scripts/helpers/poll_quick_ci.py
+++ scripts/helpers/poll_quick_ci.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import subprocess
 import time
 
@@ -9,7 +8,7 @@ def main():
   parser.add_argument("check", help="Pull request check to poll for")
   args = parser.parse_args()
   
-  logging.info(f"Gathering info on the {args.check} check being run for the current pull request.")
+  print(f"Gathering info on the {args.check} check being run for the current pull request.")
   polling = True
   poll_count = 0
   poll_max = 300
@@ -17,7 +16,7 @@ def main():
   while(polling):
     for line in subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr}", stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8").splitlines():
       if args.check in line:
-        logging.info(line)
+        print(line)
         if "pending" in line:
           if poll_count == poll_max:
             polling = false
@@ -27,9 +26,9 @@ def main():
           break
         elif "pass" in line:
           polling = False
-          logging.info(f"Fast CI {args.check} has passed; if all fast CI passes, builds and tests may commence.")
+          print(f"Fast CI {args.check} has passed; if all fast CI passes, builds and tests may commence.")
           exit(0)
-  logging.info(f"Polling for completion of fast CI {args.check} failed. Please ensure the name of the check was entered correctly and verify that it should take less than {poll_max} seconds to run.")
+  print(f"Polling for completion of fast CI {args.check} failed. Please ensure the name of the check was entered correctly and verify that it should take less than {poll_max} seconds to run.")
   exit(1)
 
 if __name__ == "__main__":
