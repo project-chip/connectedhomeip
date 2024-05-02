f784b693c915c660dac628905d93fb462ad80894
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2024-05-02 10:49:27 -0700

    Update poll_quick_ci.py

--- scripts/helpers/poll_quick_ci.py
+++ scripts/helpers/poll_quick_ci.py
@@ -7,7 +7,7 @@ def main():
   parser.add_argument("pr", help="Pull request number")
   args = parser.parse_args()
   logging.info("Gathering info on checks being run for the current pull request.")
-  subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr} -L 500", shell=True)
+  subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr}", shell=True)
 
 if __name__ == "__main__":
   main()
