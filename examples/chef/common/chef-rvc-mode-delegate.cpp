83bf599a9950ce1880ec3ae9a14276e30c80a794
Restyled.io <commits@restyled.io>
2024-05-02 06:58:31 +0000

    Restyled by clang-format

--- examples/chef/common/chef-rvc-mode-delegate.cpp
+++ examples/chef/common/chef-rvc-mode-delegate.cpp
@@ -155,8 +155,8 @@ void emberAfRvcRunModeClusterInitCallback(chip::EndpointId endpointId)
     VerifyOrDie(endpointId == 1); // this cluster is only enabled for endpoint 1.
     VerifyOrDie(gRvcRunModeDelegate == nullptr && gRvcRunModeInstance == nullptr);
     gRvcRunModeDelegate = new RvcRunMode::RvcRunModeDelegate;
-    gRvcRunModeInstance =
-        new ModeBase::Instance(gRvcRunModeDelegate, endpointId, RvcRunMode::Id, chip::to_underlying(RvcRunMode::Feature::kNoFeatures));
+    gRvcRunModeInstance = new ModeBase::Instance(gRvcRunModeDelegate, endpointId, RvcRunMode::Id,
+                                                 chip::to_underlying(RvcRunMode::Feature::kNoFeatures));
     gRvcRunModeInstance->Init();
 }
 
