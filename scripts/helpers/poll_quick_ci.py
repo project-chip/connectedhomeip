0789d50e2826fe769bc4a5cd0c93e56d157631b0
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2024-05-02 10:57:01 -0700

    Update poll_quick_ci.py

--- scripts/helpers/poll_quick_ci.py
+++ scripts/helpers/poll_quick_ci.py
@@ -5,9 +5,13 @@ import subprocess
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("pr", help="Pull request number")
+  parser.add_argument("check", help="Pull request check to poll for")
   args = parser.parse_args()
+  
   logging.info("Gathering info on checks being run for the current pull request.")
-  subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr}", shell=True)
+  for line in subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr}", stdout=subprocess.PIPE, shell=True).stdout.splitlines():
+    if args.check in line:
+      print(line)
 
 if __name__ == "__main__":
   main()
