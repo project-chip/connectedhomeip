4d11b8f46527509992e1a5b47cf7a032a60be8fd
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2024-05-02 10:10:20 -0700

    Update poll_quick_ci.py

--- scripts/helpers/poll_quick_ci.py
+++ scripts/helpers/poll_quick_ci.py
@@ -1,1 +1,8 @@
+import logging
 
+def main():
+  logging.info("Gathering info on checks being run for the current pull request.")
+  subprocess.run("gh pr checks -R project-chip/connectedhomeip ${{ github.event.number }} -L 500", shell=True)
+
+if __name__ == "__main__":
+  main()
