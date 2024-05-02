8162a4ed5a63a5df4d30c9bb0ff0d44dc01a8edc
Kai Liao <140431279+kliao-csa@users.noreply.github.com>
2024-05-02 10:37:03 -0700

    Update poll_quick_ci.py

--- scripts/helpers/poll_quick_ci.py
+++ scripts/helpers/poll_quick_ci.py
@@ -1,9 +1,13 @@
+import argparse
 import logging
 import subprocess
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("pr", help="Pull request number")
+  args = parser.parse_args()
   logging.info("Gathering info on checks being run for the current pull request.")
-  subprocess.run("gh pr checks -R project-chip/connectedhomeip ${{ github.event.number }} -L 500", shell=True)
+  subprocess.run(f"gh pr checks -R project-chip/connectedhomeip {args.pr} -L 500", shell=True)
 
 if __name__ == "__main__":
   main()
