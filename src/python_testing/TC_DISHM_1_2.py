64474e4b450a53343da900ad431827182c60b99f
Rob Bultman <rob.bultman@gmail.com>
2024-05-03 16:08:57 -0400

    Remove copy-paste error

--- src/python_testing/TC_DISHM_1_2.py
+++ src/python_testing/TC_DISHM_1_2.py
@@ -19,7 +19,6 @@ import logging
 
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from matter_testing_support import MatterBaseTest, async_test_body, default_matter_test_main
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
