380b9272fa87082292234b589af59d0788c5fbc4
Restyled.io <commits@restyled.io>
2024-05-15 13:29:24 +0000

    Restyled by whitespace

--- examples/tv-app/android/App/platform-app/src/main/java/com/matter/tv/server/MatterCommissioningPrompter.java
+++ examples/tv-app/android/App/platform-app/src/main/java/com/matter/tv/server/MatterCommissioningPrompter.java
@@ -50,7 +50,7 @@ public class MatterCommissioningPrompter extends UserPrompterResolver implements
     ContentResolver contentResolver = context.getContentResolver();
     boolean authorisationDialogDisabled =
         Settings.Secure.getInt(contentResolver, "matter_show_authorisation_dialog", 0) == 0;
-    // By default do not show authorisation dialog 
+    // By default do not show authorisation dialog
     // only do so if customer or OS explicitly ask for it, by updating matter_show_authorisation_dialog flag to 1
     if (authorisationDialogDisabled) {
       OnPromptAccepted();
