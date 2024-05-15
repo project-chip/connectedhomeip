319ec8f6acd54a4a4be5ff8b68f12634866f7b06
Kiel Oleson <kielo@apple.com>
2024-05-14 18:23:24 -0700

    namespace `UUIDHelper`

--- src/platform/Darwin/BleConnectionDelegateImpl.mm
+++ src/platform/Darwin/BleConnectionDelegateImpl.mm
@@ -35,7 +35,7 @@
 #include <tracing/metric_event.h>
 
 #import "PlatformMetricKeys.h"
-#import "UUIDHelper.h"
+#import "MTRUUIDHelper.h"
 
 using namespace chip::Ble;
 using namespace chip::DeviceLayer;
@@ -245,7 +245,7 @@ namespace DeviceLayer {
 {
     self = [super init];
     if (self) {
-        self.shortServiceUUID = [UUIDHelper GetShortestServiceUUID:&chip::Ble::CHIP_BLE_SVC_ID];
+        self.shortServiceUUID = [MTRUUIDHelper GetShortestServiceUUID:&chip::Ble::CHIP_BLE_SVC_ID];
         _chipWorkQueue = chip::DeviceLayer::PlatformMgrImpl().GetWorkQueue();
         _workQueue = queue;
         _centralManager = [CBCentralManager alloc];
