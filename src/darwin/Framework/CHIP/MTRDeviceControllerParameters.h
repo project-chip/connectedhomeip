264deab1add946542d0495f0f920c1d931d895a6
Justin Wood <woody@apple.com>
2024-05-16 08:50:18 -0700

    Update src/darwin/Framework/CHIP/MTRDeviceControllerParameters.h

--- src/darwin/Framework/CHIP/MTRDeviceControllerParameters.h
+++ src/darwin/Framework/CHIP/MTRDeviceControllerParameters.h
@@ -78,7 +78,7 @@ MTR_AVAILABLE(ios(17.6), macos(14.6), watchos(10.6), tvos(17.6))
 - (void)setOTAProviderDelegate:(id<MTROTAProviderDelegate>)otaProviderDelegate queue:(dispatch_queue_t)queue;
 
 /**
- * Sets the maximum subscriptions allowed for devices on Thread. This defaults to 3.
+ * Sets the maximum simultaneous subscriptions allowed to be established at one time for devices on Thread. This defaults to a large number.
  *
  * If this value is 0, the maximum subscriptions allowed will be set to 1.
  */
