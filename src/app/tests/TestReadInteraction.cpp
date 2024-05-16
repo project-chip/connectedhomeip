13e11e6524e9847a8f48f32f1cd2dbdcf07964b3
AYA <aalami90@gmail.com>
2024-05-16 19:30:05 +0200

    applying restyled patch

--- src/app/tests/TestReadInteraction.cpp
+++ src/app/tests/TestReadInteraction.cpp
@@ -1372,7 +1372,8 @@ void TestReadInteraction::TestSetDirtyBetweenChunks(nlTestSuite * apSuite, void 
         public:
             DirtyingMockDelegate(AttributePathParams (&aReadPaths)[2], int & aNumAttributeResponsesWhenSetDirty,
                                  int & aNumArrayItemsWhenSetDirty) :
-                mReadPaths(aReadPaths), mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
+                mReadPaths(aReadPaths),
+                mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
                 mNumArrayItemsWhenSetDirty(aNumArrayItemsWhenSetDirty)
             {}
 
