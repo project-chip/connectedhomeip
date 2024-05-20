443890307b2f9f3472891ff8d3e5ef4475376acb
raul-marquez-csa <rmarquez@csa-iot.org>
2024-05-20 16:29:57 -0700

    Removed unused imports

--- src/python_testing/TC_IDM_4_2.py
+++ src/python_testing/TC_IDM_4_2.py
@@ -19,14 +19,13 @@ import copy
 import logging
 import queue
 import time
-import traceback
 
 import chip.clusters as Clusters
 from chip.ChipDeviceCtrl import ChipDeviceController
 from chip.clusters import ClusterObjects as ClusterObjects
 from chip.clusters.Attribute import AttributePath, TypedAttributePath
 from chip.exceptions import ChipStackError
-from chip.interaction_model import Status, InteractionModelError
+from chip.interaction_model import Status
 from matter_testing_support import (AttributeChangeCallback, MatterBaseTest, async_test_body, default_matter_test_main,
                                     wait_for_attribute_report)
 from mobly import asserts
