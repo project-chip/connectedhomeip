5cdb95781f8b59767cf401b43a26580b0e050cb2
chbansal <chirag.bansal@silabs.com>
2024-05-21 18:58:02 +0530

    moving the server dependency from the src/platform

--- examples/platform/silabs/MatterConfig.cpp
+++ examples/platform/silabs/MatterConfig.cpp
@@ -57,6 +57,8 @@ static chip::DeviceLayer::Internal::Efr32PsaOperationalKeystore gOperationalKeys
 #endif
 
 #include "SilabsDeviceDataProvider.h"
+#include <app/server/Dnssd.h>
+#include <app/server/Server.h>
 #include <app/InteractionModelEngine.h>
 #include <app/TimerDelegates.h>
 
@@ -211,12 +213,18 @@ void SilabsMatterConfig::InitOTARequestorHandler(System::Layer * systemLayer, vo
 
 void SilabsMatterConfig::ConnectivityEventCallback(const ChipDeviceEvent * event, intptr_t arg)
 {
-    // Initialize OTA only when Thread or WiFi connectivity is established
     if (((event->Type == DeviceEventType::kThreadConnectivityChange) &&
          (event->ThreadConnectivityChange.Result == kConnectivity_Established)) ||
         ((event->Type == DeviceEventType::kInternetConnectivityChange) &&
          (event->InternetConnectivityChange.IPv6 == kConnectivity_Established)))
     {
+
+#if SL_WIFI
+        // Starting the MDNS server on the IP assignment
+        // TODO: Improve the connectivity event callback
+        chip::app::DnssdServer::Instance().StartServer(/*Dnssd::CommissioningMode::kEnabledBasic*/);
+#endif /* SL_WIFI */
+
 #if SILABS_OTA_ENABLED
         SILABS_LOG("Scheduling OTA Requestor initialization")
         chip::DeviceLayer::SystemLayer().StartTimer(chip::System::Clock::Seconds32(OTAConfig::kInitOTARequestorDelaySec),
--- src/platform/silabs/SiWx917/wifi/wfx_notify.cpp
+++ src/platform/silabs/SiWx917/wifi/wfx_notify.cpp
@@ -40,9 +40,6 @@ extern "C" {
 #endif // SL_ICD_ENABLED
 
 #include <platform/CHIPDeviceLayer.h>
-// #include <app/server/Mdns.h>
-#include <app/server/Dnssd.h>
-#include <app/server/Server.h>
 
 using namespace ::chip;
 using namespace ::chip::DeviceLayer;
@@ -143,16 +140,6 @@ void wfx_ipv6_notify(int got_ip)
     eventData.header.id     = got_ip ? IP_EVENT_GOT_IP6 : IP_EVENT_STA_LOST_IP;
     eventData.header.length = sizeof(eventData.header);
     PlatformMgrImpl().HandleWFXSystemEvent(IP_EVENT, &eventData);
-
-    /* So the other threads can run and have the connectivity OK */
-    if (got_ip)
-    {
-        /* Should remember this */
-        vTaskDelay(1);
-        chip::DeviceLayer::PlatformMgr().LockChipStack();
-        chip::app::DnssdServer::Instance().StartServer(/*Dnssd::CommissioningMode::kEnabledBasic*/);
-        chip::DeviceLayer::PlatformMgr().UnlockChipStack();
-    }
 }
 
 /**************************************************************************************
@@ -170,16 +157,6 @@ void wfx_ip_changed_notify(int got_ip)
     eventData.header.id     = got_ip ? IP_EVENT_STA_GOT_IP : IP_EVENT_STA_LOST_IP;
     eventData.header.length = sizeof(eventData.header);
     PlatformMgrImpl().HandleWFXSystemEvent(IP_EVENT, &eventData);
-
-    /* So the other threads can run and have the connectivity OK */
-    if (got_ip)
-    {
-        /* Should remember this */
-        vTaskDelay(1);
-        chip::DeviceLayer::PlatformMgr().LockChipStack();
-        chip::app::DnssdServer::Instance().StartServer(/*Dnssd::CommissioningMode::kEnabledBasic*/);
-        chip::DeviceLayer::PlatformMgr().UnlockChipStack();
-    }
 }
 
 /**************************************************************************************
