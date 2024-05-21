34b7667a7b46b3329b9f5f955ded16e5035b3fd3
raul-marquez-csa <rmarquez@csa-iot.org>
2024-05-21 10:32:10 -0700

    Fix restyle

--- src/python_testing/TC_IDM_4_3.py
+++ src/python_testing/TC_IDM_4_3.py
@@ -26,9 +26,7 @@ from chip.clusters import ClusterObjects as ClusterObjects
 from chip.clusters.Attribute import AttributePath, TypedAttributePath
 from chip.exceptions import ChipStackError
 from chip.interaction_model import Status
-from matter_testing_support import (MatterBaseTest, TestStep, async_test_body, default_matter_test_main)
-# from matter_testing_support import (AttributeChangeCallback, MatterBaseTest, async_test_body, default_matter_test_main,
-#                                     wait_for_attribute_report)
+from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 '''
@@ -138,9 +136,6 @@ class TC_IDM_4_3(MatterBaseTest):
             self.dut_node_id,
             [(0, node_label_attr(value=new_node_label_write))]
         )
-        
-        
-        
 
 
 if __name__ == "__main__":
