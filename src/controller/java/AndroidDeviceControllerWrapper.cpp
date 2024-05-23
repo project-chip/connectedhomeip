63c753d7f9a63838b979a699137ad57f708a471a
yunhanw-google <yunhanw@google.com>
2024-05-23 09:01:25 -0700

    Update AndroidDeviceControllerWrapper.cpp

--- src/controller/java/AndroidDeviceControllerWrapper.cpp
+++ src/controller/java/AndroidDeviceControllerWrapper.cpp
@@ -1065,7 +1065,7 @@ void AndroidDeviceControllerWrapper::OnICDRegistrationComplete(chip::NodeId icdN
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncGetKeyValue(const char * key, void * value, uint16_t & size)
 {
-    ChipLogProgress(chipTool, "KVS: Getting key %s", StringOrNullMarker(key));
+    ChipLogProgress(Controller, "KVS: Getting key %s", StringOrNullMarker(key));
 
     size_t read_size = 0;
 
@@ -1078,19 +1078,20 @@ CHIP_ERROR AndroidDeviceControllerWrapper::SyncGetKeyValue(const char * key, voi
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncSetKeyValue(const char * key, const void * value, uint16_t size)
 {
-    ChipLogProgress(chipTool, "KVS: Setting key %s", StringOrNullMarker(key));
+    ChipLogProgress(Controller, "KVS: Setting key %s", StringOrNullMarker(key));
     return chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr().Put(key, value, size);
 }
 
 CHIP_ERROR AndroidDeviceControllerWrapper::SyncDeleteKeyValue(const char * key)
 {
-    ChipLogProgress(chipTool, "KVS: Deleting key %s", StringOrNullMarker(key));
+    ChipLogProgress(Controller, "KVS: Deleting key %s", StringOrNullMarker(key));
     return chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr().Delete(key);
 }
 
 void AndroidDeviceControllerWrapper::StartDnssd()
 {
     FabricTable * fabricTable = DeviceControllerFactory::GetInstance().GetSystemState()->Fabrics();
+    VerifyOrReturn(fabricTable != nullptr, ChipLogError(controller, "Fail to get fabricTable in StartDnssd"));
     chip::app::DnssdServer::Instance().SetFabricTable(fabricTable);
     chip::app::DnssdServer::Instance().StartServer();
 }
