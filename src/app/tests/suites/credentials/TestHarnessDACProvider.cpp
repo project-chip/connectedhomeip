1378001f967d2d08f1621158ad40cbb7b4454f71
cecille <cecille@google.com>
2024-05-27 17:46:40 -0400

    Fix test to use pid from json

--- src/app/tests/suites/credentials/TestHarnessDACProvider.cpp
+++ src/app/tests/suites/credentials/TestHarnessDACProvider.cpp
@@ -177,6 +177,10 @@ bool ReadValue(Json::Value jsonValue)
     return false;
 }
 
+uint16_t ReadUint16(Json::Value jsonValue){
+    return static_cast<uint16_t>(jsonValue.asUInt());
+}
+
 // TODO: This should be moved to a method of P256Keypair
 CHIP_ERROR LoadKeypairFromRaw(ByteSpan private_key, ByteSpan public_key, Crypto::P256Keypair & keypair)
 {
@@ -205,6 +209,7 @@ void TestHarnessDACProvider::Init(const char * filepath)
     static constexpr char kFirmwareInfoKey[] = "firmware_information";
     static constexpr char kIsSuccessKey[]    = "is_success_case";
     static constexpr char kDescription[]     = "description";
+    static constexpr char kPid[]             = "basic_info_pid";
 
     std::ifstream json(filepath, std::ifstream::binary);
     if (!json)
@@ -272,6 +277,11 @@ void TestHarnessDACProvider::Init(const char * filepath)
         data.description.SetValue(ReadValue(root[kDescription], buf, sizeof(buf)));
     }
 
+    if (root.isMember(kPid))
+    {
+        data.pid.SetValue(ReadUint16(root[kPid]));
+    }
+
     Init(data);
 }
 
@@ -288,6 +298,8 @@ void TestHarnessDACProvider::Init(const TestHarnessDACProviderData & data)
 
     // TODO: We need a real example FirmwareInformation to be populated.
     mFirmwareInformation = data.firmwareInformation.HasValue() ? data.firmwareInformation.Value() : ByteSpan();
+
+    mPid = data.pid.ValueOr(0x8000);
 }
 
 CHIP_ERROR TestHarnessDACProvider::GetDeviceAttestationCert(MutableByteSpan & out_dac_buffer)
