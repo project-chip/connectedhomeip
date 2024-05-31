f330c0c2ca806bd11a78a5ecd9e0fee4fe5509cc
Thirupathi S <Thirupathi.S@silabs.com>
2024-05-31 18:13:57 +0530

    build error solved

--- examples/lighting-app/tizen/src/main.cpp
+++ examples/lighting-app/tizen/src/main.cpp
@@ -123,7 +123,7 @@ void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & 
 void emberAfColorControlClusterInitCallback(EndpointId endpoint)
 {
     // Set the color mode to color temperature.
-    Clusters::ColorControl::Attributes::ColorMode::Set(endpoint, ColorControl::ColorModeEnum::kColorTemperature);
+    Clusters::ColorControl::Attributes::ColorMode::Set(endpoint, Clusters::ColorControl::ColorModeEnum::kColorTemperature);
     // Preserve the state of the color temperature attribute across reboots.
     Clusters::ColorControl::Attributes::StartUpColorTemperatureMireds::SetNull(endpoint);
 }
