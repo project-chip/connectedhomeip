03edc520e016ac987b0136896d200978ccc48bdd
Thirupathi S <Thirupathi.S@silabs.com>
2024-05-31 18:30:58 +0530

    build error solved

--- examples/lighting-app/tizen/src/main.cpp
+++ examples/lighting-app/tizen/src/main.cpp
@@ -103,7 +103,7 @@ void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & 
         switch (attributePath.mAttributeId)
         {
         case Clusters::ColorControl::Attributes::ColorMode::Id:
-            sDBusInterface.SetColorMode(static_cast<Clusters::ColorControl::ColorMode>(*value));
+            sDBusInterface.SetColorMode(static_cast<Clusters::ColorControl::ColorModeEnum>(*value));
             break;
         case Clusters::ColorControl::Attributes::ColorTemperatureMireds::Id:
             VerifyOrDie(size == sizeof(uint16_t));
