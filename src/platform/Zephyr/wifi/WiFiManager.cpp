813b5a9aaf1c3fc4990be3f13ff64b83bbb84c0a
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2024-06-03 11:47:23 +0200

    Fix Zephyr WiFIManager build

--- src/platform/Zephyr/wifi/WiFiManager.cpp
+++ src/platform/Zephyr/wifi/WiFiManager.cpp
@@ -160,12 +160,12 @@ CHIP_ERROR WiFiManager::Init()
 {
     // TODO: consider moving these to ConnectivityManagerImpl to be prepared for handling multiple interfaces on a single device.
     Inet::UDPEndPointImplSockets::SetMulticastGroupHandler(
-        [](Inet::InterfaceId interfaceId, const Inet::IPAddress & address, UDPEndPointImplSockets::MulticastOperation operation) {
+        [](Inet::InterfaceId interfaceId, const Inet::IPAddress & address, Inet::UDPEndPointImplSockets::MulticastOperation operation) {
             const in6_addr addr = InetUtils::ToZephyrAddr(address);
             net_if * iface      = InetUtils::GetInterface(interfaceId);
             VerifyOrReturnError(iface != nullptr, INET_ERROR_UNKNOWN_INTERFACE);
 
-            if (operation == UDPEndPointImplSockets::MulticastOperation::kJoin)
+            if (operation == Inet::UDPEndPointImplSockets::MulticastOperation::kJoin)
             {
                 net_if_mcast_addr * maddr = net_if_ipv6_maddr_add(iface, &addr);
 
@@ -174,7 +174,7 @@ CHIP_ERROR WiFiManager::Init()
                     net_if_ipv6_maddr_join(iface, maddr);
                 }
             }
-            else if (operation == UDPEndPointImplSockets::MulticastOperation::kLeave)
+            else if (operation == Inet::UDPEndPointImplSockets::MulticastOperation::kLeave)
             {
                 VerifyOrReturnError(net_ipv6_is_addr_mcast_link_all_nodes(&addr) || net_if_ipv6_maddr_rm(iface, &addr),
                                     CHIP_ERROR_INVALID_ADDRESS);
