555bf19c73b10fc42f915ba9a3cd278937f91167
Thirupathi S <Thirupathi.S@silabs.com>
2024-06-04 10:14:49 +0530

    addressed review comments

--- zzz_generated/app-common/app-common/zap-generated/cluster-enums-check.h
+++ zzz_generated/app-common/app-common/zap-generated/cluster-enums-check.h
@@ -2962,7 +2962,7 @@ static auto __attribute__((unused)) EnsureKnownEnumValue(ColorControl::ColorMode
     {
     case EnumType::kCurrentHueAndCurrentSaturation:
     case EnumType::kCurrentXAndCurrentY:
-    case EnumType::kColorTemperature:
+    case EnumType::kColorTemperatureMireds:
         return val;
     default:
         return EnumType::kUnknownEnumValue;
@@ -3004,7 +3004,7 @@ static auto __attribute__((unused)) EnsureKnownEnumValue(ColorControl::EnhancedC
     {
     case EnumType::kCurrentHueAndCurrentSaturation:
     case EnumType::kCurrentXAndCurrentY:
-    case EnumType::kColorTemperature:
+    case EnumType::kColorTemperatureMireds:
     case EnumType::kEnhancedCurrentHueAndCurrentSaturation:
         return val;
     default:
