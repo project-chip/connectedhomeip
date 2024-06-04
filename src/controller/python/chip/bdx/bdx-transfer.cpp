beb87c5bba3b1d04901ac4146e44c343ab224014
Andy Salisbury <harimau@google.com>
2024-06-04 14:32:51 -0400

    Add the initial implementation of a BDX transfer.

---
+++ src/controller/python/chip/bdx/bdx-transfer.cpp
@@ -0,0 +1,178 @@
+/*
+ *
+ *    Copyright (c) 2024 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <controller/python/chip/bdx/bdx-transfer.h>
+
+#include <protocols/bdx/BdxTransferSession.h>
+
+namespace chip {
+namespace bdx {
+
+BdxTransfer::BdxTransfer(BdxTransfer::Delegate * delegate) : mDelegate(delegate)
+{
+    // TODO: Verify that delegate is non-null?
+}
+
+BdxTransfer::~BdxTransfer()
+{
+    if (mData != nullptr)
+    {
+        delete[] mData;
+        mData = nullptr;
+    }
+}
+
+CHIP_ERROR BdxTransfer::AcceptSend(BdxTransfer::DataCallback callback)
+{
+    VerifyOrReturnError(mAwaitingAccept, CHIP_ERROR_INCORRECT_STATE);
+    VerifyOrReturnError(callback != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
+    mAwaitingAccept = false;
+    mDataCallback = callback;
+
+    TransferSession::TransferAcceptData accept_data;
+    accept_data.ControlMode = mInitData.TransferCtlFlags;  // TODO: Is this value correct?
+    accept_data.MaxBlockSize = mInitData.MaxBlockSize;
+    // TODO: Check the acceptable control modes for a Diagnostic Logs cluster transfer.
+    return mTransfer.AcceptTransfer(accept_data);
+}
+
+CHIP_ERROR BdxTransfer::AcceptReceive(const ByteSpan data_to_send)
+{
+    VerifyOrReturnError(mAwaitingAccept, CHIP_ERROR_INCORRECT_STATE);
+    mAwaitingAccept = false;
+    mData = new uint8_t[data_to_send.size()];
+    if (mData == nullptr)
+    {
+        return CHIP_ERROR_NO_MEMORY;
+    }
+    memcpy(mData, data_to_send.data(), data_to_send.size());
+    mDataCount = data_to_send.size();
+
+    TransferSession::TransferAcceptData accept_data;
+    accept_data.ControlMode = mInitData.TransferCtlFlags;  // TODO: Is this value correct?
+    accept_data.MaxBlockSize = mInitData.MaxBlockSize;
+    return mTransfer.AcceptTransfer(accept_data);
+}
+
+CHIP_ERROR BdxTransfer::Reject()
+{
+    VerifyOrReturnError(mAwaitingAccept, CHIP_ERROR_INCORRECT_STATE);
+    mAwaitingAccept = false;
+    return mTransfer.RejectTransfer(StatusCode::kTransferFailedUnknownError);
+}
+
+void BdxTransfer::HandleTransferSessionOutput(TransferSession::OutputEvent & event)
+{
+    assertChipStackLockedByCurrentThread();
+
+    ChipLogDetail(BDX, "Received event %s", event.ToString(event.EventType));
+
+    switch (event.EventType)
+    {
+    case TransferSession::OutputEventType::kInitReceived:
+        mInitData = event.transferInitData;
+        mDelegate->InitMessageReceived(this, mInitData);
+        break;
+    case TransferSession::OutputEventType::kStatusReceived:
+        ChipLogError(BDX, "Received StatusReport %x", static_cast<uint16_t>(event.statusData.statusCode));
+        // TODO: Not a great error type. The issue isn't internal, it's external. We can check the status code, but I don't know
+        // if that would produce a better error type.
+        EndSession(CHIP_ERROR_INTERNAL);
+        break;
+    case TransferSession::OutputEventType::kInternalError:
+        EndSession(CHIP_ERROR_INTERNAL);
+        break;
+    case TransferSession::OutputEventType::kTransferTimeout:
+        EndSession(CHIP_ERROR_TIMEOUT);
+        break;
+    case TransferSession::OutputEventType::kBlockReceived:
+        ByteSpan data(event.blockdata.Data, event.blockdata.Length);
+        mDataCallback(data);
+        break;
+    case TransferSession::OutputEventType::kMsgToSend:
+        SendMessage(event);
+        if (event.msgTypeData.HasMessageType(MessageType::BlockAckEOF))
+        {
+            // TODO: Completing the session here means the ACK for the BlockAckEOF message hasn't been received.
+            EndSession(CHIP_NO_ERROR);
+        }
+        break;
+    case TransferSession::OutputEventType::kAckEOFReceived:
+        EndSession(CHIP_NO_ERROR);
+        break;
+    case TransferSession::OutputEventType::kQueryWithSkipReceived:
+        mDataTransferredCount += event.bytesToSkip.BytesToSkip;
+        mDataTransferredCount = std::min(mDataTransferredCount, mDataCount);
+        // Fallthrough intentional.
+    case TransferSession::OutputEventType::kQueryReceived:
+        SendBlock();
+        break;
+    case TransferSession::OutputEventType::kAckReceived:
+    case TransferSession::OutputEventType::kAcceptReceived:
+    case TransferSession::OutputEventType::kNone:
+        // Nothing to do.
+        break;
+    default:
+        // Should never happen.
+        chipDie();
+        break;
+    }
+}
+
+void BdxTransfer::EndSession(CHIP_ERROR result)
+{
+    mDelegate->TransferCompleted(this, result);
+    ResetTransfer();
+    if (mExchangeCtx)
+    {
+        mExchangeCtx->Close();
+    }
+}
+
+void BdxTransfer::OnExchangeClosing(Messaging::ExchangeContext * exchangeContext)
+{
+    mExchangeCtx = nullptr;
+}
+
+CHIP_ERROR BdxTransfer::SendMessage(TransferSession::OutputEvent & event)
+{
+    VerifyOrReturnError(mExchangeCtx != nullptr, CHIP_ERROR_INCORRECT_STATE);
+
+    ::chip::Messaging::SendFlags sendFlags;
+    if (!event.msgTypeData.HasMessageType(Protocols::SecureChannel::MsgType::StatusReport) &&
+        !event.msgTypeData.HasMessageType(MessageType::BlockAckEOF))
+    {
+        sendFlags.Set(Messaging::SendMessageFlags::kExpectResponse);
+    }
+    return mExchangeCtx->SendMessage(event.msgTypeData.ProtocolId, event.msgTypeData.MessageType, event.MsgData.Retain(), sendFlags);
+}
+
+CHIP_ERROR BdxTransfer::SendBlock()
+{
+    VerifyOrReturnError(mExchangeCtx != nullptr, CHIP_ERROR_INCORRECT_STATE);
+
+    TransferSession::BlockData block;
+    block.Data = mData + mDataCount;
+    block.Length = std::min<size_t>(mTransfer.GetTransferBlockSize(), mDataCount - mDataTransferredCount);
+    ReturnOnFailure(mTransfer.PrepareBlock(block));
+    mDataTransferredCount += block.Length;
+    ScheduleImmediatePoll();
+    return CHIP_NO_ERROR;
+}
+
+}  // namespace bdx
+}  // namespace chip
---
+++ src/controller/python/chip/bdx/bdx-transfer.h
@@ -0,0 +1,82 @@
+/*
+ *
+ *    Copyright (c) 2024 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+// TODO: Includes.
+#include <lib/support/Span.h>
+#include <messaging/ExchangeContext.h>
+
+#include <protocols/bdx/BdxTransferSession.h>
+#include <protocols/bdx/TransferFacilitator.h>
+
+#pragma once
+
+namespace chip {
+namespace bdx {
+
+// A class that represents a BDX transfer initiated by the other end of the transfer. This implements most of the transfer,
+// but uses a delegate to determine whether the transfer should be accepted and provide the data source or sink.
+class BdxTransfer : public Responder
+{
+public:
+    // The delegate is informed when specific events occur during the transfer.
+    class Delegate
+    {
+    public:
+        // Called when the SendInit or ReceiveInit message is received.
+        virtual void InitMessageReceived(BdxTransfer * transfer, TransferSession::TransferInitData init_data) = 0;
+        // Called when the transfer completes. The outcome of the transfer (successful or otherwise) is indicated by result.
+        virtual void TransferCompleted(BdxTransfer * transfer, CHIP_ERROR result) = 0;
+    };
+
+    // The callback used when a data block arrives. This is only used when the transfer is sending data to this controller.
+    using DataCallback = void(*)(const ByteSpan&);
+
+    BdxTransfer(Delegate * delegate);
+
+    // Accepts the transfer. When a block of data arrives the callback is invoked with the block. This must only be called if the
+    // transfer sends data to this controller.
+    CHIP_ERROR AcceptSend(DataCallback callback);
+
+    // Accepts the transfer. The data provided here will be sent over the transfer. This must only be called if the transfer
+    // receives data from this controller.
+    // TODO: Should the parameter be const&?
+    CHIP_ERROR AcceptReceive(const ByteSpan data_to_send);
+
+    // Rejects the transfer.
+    CHIP_ERROR Reject();
+
+    void HandleTransferSessionOutput(TransferSession::OutputEvent & event) override;
+    void OnExchangeClosing(Messaging::ExchangeContext * exchangeContext) override;
+
+private:
+    void EndSession(CHIP_ERROR result);
+    CHIP_ERROR SendMessage(TransferSession::OutputEvent & event);
+    CHIP_ERROR SendBlock();
+
+    Delegate * mDelegate = nullptr;
+    bool mAwaitingAccept = false;
+    DataCallback mDataCallback = nullptr;
+    TransferSession::TransferInitData mInitData;
+
+    // TODO: Request the data from a data source rather than copy the data here.
+    uint8_t * mData = nullptr;
+    size_t mDataCount = 0;
+    size_t mDataTransferredCount = 0;
+};
+
+}  // namespace bdx
+}  // namespace chip
