62d5e4d7e554951b551bfed6fd08613663c54b03
Restyled.io <commits@restyled.io>
2024-06-06 14:49:47 +0000

    Restyled by isort

--- src/python_testing/TestMatterTestingSupport.py
+++ src/python_testing/TestMatterTestingSupport.py
@@ -24,8 +24,7 @@ import chip.clusters as Clusters
 from chip.clusters.Types import Nullable, NullValue
 from chip.tlv import uint
 from matter_testing_support import (MatterBaseTest, async_test_body, compare_time, default_matter_test_main,
-                                    get_wait_seconds_from_set_time, type_matches,
-                                    utc_time_in_matter_epoch)
+                                    get_wait_seconds_from_set_time, type_matches, utc_time_in_matter_epoch)
 from mobly import asserts, signals
 from pics_support import parse_pics, parse_pics_xml
 from taglist_and_topology_test_support import (TagProblem, create_device_type_list_for_root, create_device_type_lists,
