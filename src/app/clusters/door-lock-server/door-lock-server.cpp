bdb1a75cd66b04837c9cef80911a4bcb962c0f80
Boris Zbarsky <bzbarsky@apple.com>
2024-06-06 22:39:53 -0400

    Generate LockUserChange events when clearing a credential.
    We were passing false for sendUserChangeEvent at all callsites of
    clearCredential(), which is not correct for the "clear a single credential"
    case.  The other cases generate their own LockUserChange events.

--- src/app/clusters/door-lock-server/door-lock-server.cpp
+++ src/app/clusters/door-lock-server/door-lock-server.cpp
@@ -951,7 +951,7 @@ void DoorLockServer::clearCredentialCommandHandler(
     }
 
     commandObj->AddStatus(commandPath,
-                          clearCredential(commandPath.mEndpointId, modifier, sourceNodeId, credentialType, credentialIndex, false));
+                          clearCredential(commandPath.mEndpointId, modifier, sourceNodeId, credentialType, credentialIndex, true));
 }
 
 void DoorLockServer::setWeekDayScheduleCommandHandler(chip::app::CommandHandler * commandObj,
