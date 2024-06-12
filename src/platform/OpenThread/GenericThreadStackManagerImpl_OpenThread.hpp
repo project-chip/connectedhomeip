a07b447e161bc5ce121399db044bb9f056e62cb3
chendejin <chendejin@espressif.com>
2024-06-12 12:37:15 +0800

    OpenThread: added Thread interface enabled support

--- src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.hpp
+++ src/platform/OpenThread/GenericThreadStackManagerImpl_OpenThread.hpp
@@ -1094,6 +1094,9 @@ CHIP_ERROR GenericThreadStackManagerImpl_OpenThread<ImplClass>::DoInit(otInstanc
     CHIP_ERROR err = CHIP_NO_ERROR;
     otError otErr  = OT_ERROR_NONE;
 
+    // If InterfaceEnabled is false, do not start Thread
+    bool InterfaceEnabled = true;
+
     // Arrange for OpenThread errors to be translated to text.
     RegisterOpenThreadErrorFormatter();
 
@@ -1131,8 +1134,12 @@ CHIP_ERROR GenericThreadStackManagerImpl_OpenThread<ImplClass>::DoInit(otInstanc
     memset(&mSrpClient, 0, sizeof(mSrpClient));
 #endif // CHIP_DEVICE_CONFIG_ENABLE_THREAD_SRP_CLIENT
 
+#ifndef _NO_NETWORK_COMMISSIONING_DRIVER_
+    InterfaceEnabled = sGenericThreadDriver.GetEnabled();
+#endif
+
     // If the Thread stack has been provisioned, but is not currently enabled, enable it now.
-    if (otThreadGetDeviceRole(mOTInst) == OT_DEVICE_ROLE_DISABLED && otDatasetIsCommissioned(otInst))
+    if (InterfaceEnabled && otThreadGetDeviceRole(mOTInst) == OT_DEVICE_ROLE_DISABLED && otDatasetIsCommissioned(otInst))
     {
         // Enable the Thread IPv6 interface.
         otErr = otIp6SetEnabled(otInst, true);
