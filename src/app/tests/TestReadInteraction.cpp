a621161966227bff453a5d727bbcab2039345dfc
Restyled.io <commits@restyled.io>
2024-06-12 14:00:39 +0000

    Restyled by clang-format

--- src/app/tests/TestReadInteraction.cpp
+++ src/app/tests/TestReadInteraction.cpp
@@ -1198,7 +1198,8 @@ TEST_F(TestReadInteraction, TestSetDirtyBetweenChunks)
         public:
             DirtyingMockDelegate(AttributePathParams (&aReadPaths)[2], int & aNumAttributeResponsesWhenSetDirty,
                                  int & aNumArrayItemsWhenSetDirty) :
-                mReadPaths(aReadPaths), mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
+                mReadPaths(aReadPaths),
+                mNumAttributeResponsesWhenSetDirty(aNumAttributeResponsesWhenSetDirty),
                 mNumArrayItemsWhenSetDirty(aNumArrayItemsWhenSetDirty)
             {}
 
