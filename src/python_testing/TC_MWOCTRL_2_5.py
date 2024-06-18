8461a18bf50c3f97b29efb3031565e1be49e0cf8
Rob Bultman <rob.bultman@gmail.com>
2024-06-18 12:59:44 -0400

    Removed unused import

--- src/python_testing/TC_MWOCTRL_2_5.py
+++ src/python_testing/TC_MWOCTRL_2_5.py
@@ -15,8 +15,6 @@
 #    limitations under the License.
 #
 
-import logging
-
 import chip.clusters as Clusters
 from chip.interaction_model import InteractionModelError, Status
 from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
