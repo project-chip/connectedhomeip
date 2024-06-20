b29e0cf324b9a52289f86f7cdf1f23128c6ae46e
Stefan Agner <stefan@agner.ch>
2024-06-20 14:00:18 +0200

    [Python] Use asyncio.run() to run async tests

--- scripts/tests/chiptest/yamltest_with_chip_repl_tester.py
+++ scripts/tests/chiptest/yamltest_with_chip_repl_tester.py
@@ -153,6 +153,4 @@ async def main(setup_code, yaml_path, node_id, pics_file):
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
