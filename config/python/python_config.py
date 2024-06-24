0eacd5b3f81f9af592b1fab74ef2cc93dac04053
Jakub Latusek <j.latusek@samsung.com>
2024-06-24 15:40:36 +0200

    restyle

--- config/python/python_config.py
+++ config/python/python_config.py
@@ -14,10 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import json
-import subprocess
+import os
 import shutil
+import subprocess
 
 
 class Config:
