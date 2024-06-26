7a9eed6296c7f2115a277f9d30301d7dc0332651
Restyled.io <commits@restyled.io>
2024-06-26 17:36:48 +0000

    Restyled by clang-format

--- examples/platform/silabs/MatterConfig.cpp
+++ examples/platform/silabs/MatterConfig.cpp
@@ -56,9 +56,9 @@
 static chip::DeviceLayer::Internal::Efr32PsaOperationalKeystore gOperationalKeystore;
 #endif
 
+#include <ProvisionManager.h>
 #include <app/InteractionModelEngine.h>
 #include <app/TimerDelegates.h>
-#include <ProvisionManager.h>
 
 #ifdef SL_MATTER_TEST_EVENT_TRIGGER_ENABLED
 #include "SilabsTestEventTriggerDelegate.h" // nogncheck
