27a0140351950c61cfee82789db2cdc622e0542b
Restyled.io <commits@restyled.io>
2024-06-26 17:47:25 +0000

    Restyled by autopep8

--- src/python_testing/TC_TIMESYNC_2_2.py
+++ src/python_testing/TC_TIMESYNC_2_2.py
@@ -81,7 +81,7 @@ class TC_TIMESYNC_2_2(MatterBaseTest):
         compare_time(received=utc_dut, utc=th_utc, tolerance=tolerance)
 
         self.print_step(5, "Read time source")
-        if timesource_attr_id in attribute_list:        
+        if timesource_attr_id in attribute_list:
             source = await self.read_ts_attribute_expect_success(endpoint=endpoint, attribute=attributes.TimeSource)
             if utc_dut_initial is NullValue:
                 asserts.assert_equal(source, Clusters.Objects.TimeSynchronization.Enums.TimeSourceEnum.kAdmin)
