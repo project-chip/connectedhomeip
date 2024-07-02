d0cc09c326d1a7bf42c0c7e0e0a4fdc705075e00
Andrei Litvin <andreilitvin@google.com>
2024-07-02 16:47:26 -0400

    Make clang-tidy happy

--- src/app/codegen-data-model/tests/TestCodegenModelViaMocks.cpp
+++ src/app/codegen-data-model/tests/TestCodegenModelViaMocks.cpp
@@ -817,20 +817,22 @@ TEST(TestCodegenModelViaMocks, GetAttributeInfo)
     EXPECT_FALSE(info->flags.Has(AttributeQualityFlags::kListAttribute)); // NOLINT(bugprone-unchecked-optional-access)
 
     // Mocks always set everything as R/W with administrative privileges
-    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister);
-    EXPECT_EQ(info->writePrivilege, chip::Access::Privilege::kAdminister);
+    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister); // NOLINT(bugprone-unchecked-optional-access)
+    EXPECT_EQ(info->writePrivilege, chip::Access::Privilege::kAdminister); // NOLINT(bugprone-unchecked-optional-access)
+
 
     info = model.GetAttributeInfo(ConcreteAttributePath(kMockEndpoint2, MockClusterId(2), MockAttributeId(2)));
     ASSERT_TRUE(info.has_value());
     EXPECT_TRUE(info->flags.Has(AttributeQualityFlags::kListAttribute)); // NOLINT(bugprone-unchecked-optional-access)
-    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister);
-    EXPECT_EQ(info->writePrivilege, chip::Access::Privilege::kAdminister);
+    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister); // NOLINT(bugprone-unchecked-optional-access)
+    EXPECT_EQ(info->writePrivilege, chip::Access::Privilege::kAdminister); // NOLINT(bugprone-unchecked-optional-access)
+
 
     // test a read-only attribute, which will not have a write privilege
     info = model.GetAttributeInfo(ConcreteAttributePath(kMockEndpoint3, MockClusterId(3), kReadOnlyAttributeId));
     ASSERT_TRUE(info.has_value());
     EXPECT_FALSE(info->flags.Has(AttributeQualityFlags::kListAttribute)); // NOLINT(bugprone-unchecked-optional-access)
-    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister);
+    EXPECT_EQ(info->readPrivilege, chip::Access::Privilege::kAdminister); // NOLINT(bugprone-unchecked-optional-access)
     EXPECT_FALSE(info->writePrivilege.has_value());
 }
 
