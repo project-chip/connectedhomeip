46b1cbf73934f06291403c5046f3fd1e824d21cc
Lazar Kovacic <lkovacic@amazon.com>
2024-07-02 18:33:27 +0200

    Add downloading state

--- examples/tv-app/android/java/src/com/matter/tv/server/tvapp/LauncherResponse.java
+++ examples/tv-app/android/java/src/com/matter/tv/server/tvapp/LauncherResponse.java
@@ -6,7 +6,8 @@ public class LauncherResponse {
   public static final int STATUS_APP_NOT_AVAILABLE = 1;
   public static final int STATUS_SYSTEM_BUSY = 2;
   public static final int STATUS_PENDING = 3;
-  public static final int STATUS_INSTALLING = 4;
+  public static final int STATUS_DOWNLOADING = 4;
+  public static final int STATUS_INSTALLING = 5;
 
   public LauncherResponse(int status, String data) {
     this.status = status;
