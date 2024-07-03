da1aae4e168d2c696b0710e82a04573e1e0d2c28
Andrei Litvin <andy314@gmail.com>
2024-07-03 15:34:09 -0400

    Updated encode/decode comment

--- src/app/codegen-data-model/CodegenDataModel_Write.cpp
+++ src/app/codegen-data-model/CodegenDataModel_Write.cpp
@@ -156,7 +156,7 @@ CHIP_ERROR DecodeIntoEmberBuffer(AttributeValueDecoder & decoder, bool isNullabl
         }
         else
         {
-            // This guards against trying to encode something that overlaps nullable, for example
+            // This guards against trying to decode something that overlaps nullable, for example
             // Nullable<uint8_t>(0xFF) is not representable because 0xFF is the encoding of NULL in ember
             // as well as odd-sized integers (e.g. full 32-bit value like 0x11223344 cannot be written
             // to a 3-byte odd-sized integger).
