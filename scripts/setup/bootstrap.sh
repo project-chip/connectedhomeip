04c8cc458635c89762cd2035cdc695c016970f6e
Jakub Latusek <j.latusek@samsung.com>
2024-07-09 09:46:04 +0200

    Formatting

--- scripts/setup/bootstrap.sh
+++ scripts/setup/bootstrap.sh
@@ -73,26 +73,26 @@ _install_additional_pip_requirements() {
 }
 
 _submodules_need_updating() {
-    # Validates if a set of submodules that should always be checked out are up to date.
+  # Validates if a set of submodules that should always be checked out are up to date.
 
-    # Pigweed will be up to date on an initial setup, however it may change over time.
-    # The rest are a small subset of things that are always checked out (have no platform attachment).
-    _SUBMODULE_PATHS=(
-        "third_party/pigweed/repo"
-        "third_party/openthread/repo"
-        "third_party/editline/repo"
-    )
+  # Pigweed will be up to date on an initial setup, however it may change over time.
+  # The rest are a small subset of things that are always checked out (have no platform attachment).
+  _SUBMODULE_PATHS=(
+    "third_party/pigweed/repo"
+    "third_party/openthread/repo"
+    "third_party/editline/repo"
+  )
 
-    for submodule_path in "${_SUBMODULE_PATHS[@]}"; do
-        if git submodule status "$submodule_path" | grep -E '^-' >/dev/null 2>&1; then
-            echo "git shows that $submodule_path has changes"
-            unset _SUBMODULE_PATHS
-            return 0 # Success
-        fi
-    done
+  for submodule_path in "${_SUBMODULE_PATHS[@]}"; do
+    if git submodule status "$submodule_path" | grep -E '^-' >/dev/null 2>&1; then 
+      echo "git shows that $submodule_path has changes"
+      unset _SUBMODULE_PATHS
+      return 0 # Success
+    fi
+  done
 
-    unset _SUBMODULE_PATHS
-    return 1 # Failure
+  unset _SUBMODULE_PATHS
+  return 1 # Failure
 }
 
 _bootstrap_or_activate() {
@@ -216,17 +216,17 @@ unset -f _install_additional_pip_requirements
 pw_cleanup
 
 if _submodules_need_updating; then
-    # yellow output
-    if which tput >/dev/null; then tput setaf 3; fi
+  # yellow output
+  if which tput >/dev/null; then tput setaf 3; fi
 
-    echo "Some submodules seem out of date."
-    echo "For a clean checkout, consider running:"
-    echo "   ./scripts/checkout_submodules.py --shallow --platform <your-platform>"
-    echo "OR for a full checkout:"
-    echo "   git submodule update -f --init --recursive"
+  echo "Some submodules seem out of date."
+  echo "For a clean checkout, consider running:"
+  echo "   ./scripts/checkout_submodules.py --shallow --platform <your-platform>"
+  echo "OR for a full checkout:"
+  echo "   git submodule update -f --init --recursive"
 
-    # reset output
-    if which tput >/dev/null; then tput sgr0; fi
+  # reset output
+  if which tput >/dev/null; then tput sgr0; fi
 fi
 
 unset _ACTION_TAKEN
