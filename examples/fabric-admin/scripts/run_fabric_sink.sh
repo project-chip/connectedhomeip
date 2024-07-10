0c6e281318e0b5e53aa27ff31d6d7884ee2055f1
Yufeng Wang <yufengwang@google.com>
2024-07-10 10:58:22 -0700

    Update scripts with mutiple pathes

--- examples/fabric-admin/scripts/run_fabric_sink.sh
+++ examples/fabric-admin/scripts/run_fabric_sink.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 
 # Default paths
-FABRIC_ADMIN_PATH="./fabric-admin"
+DEFAULT_CHOICES=(
+    "./fabric-admin"
+    "out/debug/standalone/fabric-admin"
+    "out/linux-x64-fabric-admin/fabric-admin"
+    "out/darwin-arm64-fabric-admin/fabric-admin"
+)
 FABRIC_ADMIN_LOG="/tmp/fabric_admin.log"
+FABRIC_ADMIN_PATH=""
 
-# Detect platform and set default paths
-if [[ $(uname -m) == "x86_64" ]]; then
-    FABRIC_ADMIN_PATH="out/debug/standalone/fabric-admin"
+# Function to find fabric-admin binary
+find_fabric_admin() {
+    local choices=("$@")
+    for path in "${choices[@]}"; do
+        if [[ -e "$path" ]]; then
+            echo "$path"
+            return 0
+        fi
+    done
+    return 1
+}
+
+# Parse arguments
+VERBOSE=false
+SPECIFIED_PATH=""
+
+for arg in "$@"; do
+    case $arg in
+        --verbose)
+            VERBOSE=true
+            ;;
+        --path=*)
+            SPECIFIED_PATH="${arg#*=}"
+            ;;
+    esac
+done
+
+# Use specified path if provided
+if [[ -n "$SPECIFIED_PATH" ]]; then
+    if [[ -e "$SPECIFIED_PATH" ]]; then
+        FABRIC_ADMIN_PATH="$SPECIFIED_PATH"
+    else
+        echo >&2 "Specified path does not exist: $SPECIFIED_PATH"
+        exit 1
+    fi
+else
+    FABRIC_ADMIN_PATH=$(find_fabric_admin "${DEFAULT_CHOICES[@]}")
+    if [[ $? -ne 0 ]]; then
+        echo >&2 "Could not find the fabric-admin binary"
+        exit 1
+    fi
 fi
 
-# Check if --verbose option is specified
-VERBOSE=false
-for arg in "$@"; do
-    if [ "$arg" == "--verbose" ]; then
-        VERBOSE=true
-        break
-    fi
-done
+echo "PATH IS: $FABRIC_ADMIN_PATH"
 
 # Kill fabric-admin if it is running
 echo "Checking for running fabric-admin process..."
 fabric_admin_pid=$(pgrep -f "$FABRIC_ADMIN_PATH")
-if [ ! -z "$fabric_admin_pid" ]; then
+if [[ -n "$fabric_admin_pid" ]]; then
     echo "Found fabric-admin with PID $fabric_admin_pid, attempting to kill..."
     kill -9 "$fabric_admin_pid"
     echo "Killed fabric-admin with PID $fabric_admin_pid"
