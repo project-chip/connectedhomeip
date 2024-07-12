0d29207177c13133a09af25975a92fe969bd26e4
Restyled.io <commits@restyled.io>
2024-07-12 18:16:25 +0000

    Restyled by clang-format

--- src/darwin/Framework/CHIP/MTRBaseDevice.mm
+++ src/darwin/Framework/CHIP/MTRBaseDevice.mm
@@ -283,7 +283,7 @@ public:
         ErrorCallback errorCallback, MTRDeviceResubscriptionScheduledHandler _Nullable resubscriptionScheduledHandler,
         MTRSubscriptionEstablishedHandler _Nullable subscriptionEstablishedHandler, OnDoneHandler _Nullable onDoneHandler)
         : MTRBaseSubscriptionCallback(attributeReportCallback, eventReportCallback, errorCallback, resubscriptionScheduledHandler,
-              subscriptionEstablishedHandler, onDoneHandler)
+            subscriptionEstablishedHandler, onDoneHandler)
     {
     }
 
