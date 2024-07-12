a951d47d9df93d711ed1d59ef5cb9c70c3b2bbd9
Justin Wood <woody@apple.com>
2024-07-12 08:29:19 -1000

    Ugh.

--- src/darwin/Framework/CHIP/MTRDeviceController.mm
+++ src/darwin/Framework/CHIP/MTRDeviceController.mm
@@ -295,7 +295,7 @@ using namespace chip::Tracing::DarwinFramework;
         return;
     }
 
-    MTR_LOG("Shutting down MTRDeviceController: %@", self)
+    MTR_LOG("Shutting down MTRDeviceController: %@", self);
     [self cleanupAfterStartup];
 }
 
@@ -307,7 +307,7 @@ using namespace chip::Tracing::DarwinFramework;
     // do the secure session shutdowns.  Since we don't want to hold the lock
     // while calling out into arbitrary invalidation code, snapshot the list of
     // devices before we start invalidating.
-    MTR_LOG("cleanupAfterStartup MTRDeviceController: %@", self)
+    MTR_LOG("cleanupAfterStartup MTRDeviceController: %@", self);
     os_unfair_lock_lock(&_deviceMapLock);
     NSEnumerator * devices = [_nodeIDToDeviceMap objectEnumerator];
     [_nodeIDToDeviceMap removeAllObjects];
@@ -325,7 +325,7 @@ using namespace chip::Tracing::DarwinFramework;
 // in a very specific way that only MTRDeviceControllerFactory knows about.
 - (void)shutDownCppController
 {
-    MTR_LOG("shutDownCppController MTRDeviceController: %p", self)
+    MTR_LOG("shutDownCppController MTRDeviceController: %p", self);
     assertChipStackLockedByCurrentThread();
 
     // Shut down all our endpoints.
@@ -627,7 +627,7 @@ using namespace chip::Tracing::DarwinFramework;
             });
         }];
     }
-    MTR_LOG("MTRDeviceController startup: %@", self)
+    MTR_LOG("MTRDeviceController startup: %@", self);
 
     return YES;
 }
@@ -952,8 +952,7 @@ static inline void emitMetricForSetupPayload(MTRSetupPayload * payload)
 
 - (MTRBaseDevice *)deviceBeingCommissionedWithNodeID:(NSNumber *)nodeID error:(NSError * __autoreleasing *)error
 {
-    auto block = ^MTRBaseDevice *
-    {
+    auto block = ^MTRBaseDevice * {
         chip::CommissioneeDeviceProxy * deviceProxy;
 
         auto errorCode = self->_cppCommissioner->GetDeviceBeingCommissioned(nodeID.unsignedLongLongValue, &deviceProxy);
@@ -1112,8 +1111,7 @@ static inline void emitMetricForSetupPayload(MTRSetupPayload * payload)
 
 - (NSData * _Nullable)attestationChallengeForDeviceID:(NSNumber *)deviceID
 {
-    auto block = ^NSData *
-    {
+    auto block = ^NSData * {
         chip::CommissioneeDeviceProxy * deviceProxy;
 
         auto errorCode = CHIP_NO_ERROR;
@@ -1935,8 +1933,7 @@ static inline void emitMetricForSetupPayload(MTRSetupPayload * payload)
         return nil;
     }
 
-    auto block = ^NSString *
-    {
+    auto block = ^NSString * {
         chip::SetupPayload setupPayload;
         errorCode = chip::Controller::AutoCommissioningWindowOpener::OpenCommissioningWindow(self->_cppCommissioner, deviceID,
             chip::System::Clock::Seconds16(static_cast<uint16_t>(duration)), chip::Crypto::kSpake2p_Min_PBKDF_Iterations,
