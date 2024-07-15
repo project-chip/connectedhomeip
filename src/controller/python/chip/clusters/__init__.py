be2a49c2dfb175c84fb6935f89ff76f77de23f68
Restyled.io <commits@restyled.io>
2024-07-15 20:02:48 +0000

    Restyled by isort

--- src/controller/python/chip/clusters/__init__.py
+++ src/controller/python/chip/clusters/__init__.py
@@ -46,8 +46,7 @@ from .Objects import (AccessControl, AccountLogin, Actions, ActivatedCarbonFilte
                       TemperatureControl, TemperatureMeasurement, Thermostat, ThermostatUserInterfaceConfiguration,
                       ThreadBorderRouterManagement, ThreadNetworkDiagnostics, TimeFormatLocalization, TimeSynchronization,
                       TotalVolatileOrganicCompoundsConcentrationMeasurement, UnitLocalization, UnitTesting, UserLabel,
-                      ValveConfigurationAndControl, WakeOnLan, WaterHeaterManagement, WiFiNetworkDiagnostics,
-                      WindowCovering)
+                      ValveConfigurationAndControl, WakeOnLan, WaterHeaterManagement, WiFiNetworkDiagnostics, WindowCovering)
 
 __all__ = [Attribute, CHIPClusters, Command, AccessControl, AccountLogin, Actions, ActivatedCarbonFilterMonitoring, AdministratorCommissioning, AirQuality,
            ApplicationBasic, ApplicationLauncher, AudioOutput, BallastConfiguration, BarrierControl, BasicInformation,
