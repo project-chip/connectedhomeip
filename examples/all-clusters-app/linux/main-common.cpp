2d212a0766b5fa3829ce27ddefab1dd1488f865b
James Harrow <james.harrow@gmail.com>
2024-07-17 17:24:02 +0100

    Added missing #include "water-heater-mode.h" to main-common.cpp

--- examples/all-clusters-app/linux/main-common.cpp
+++ examples/all-clusters-app/linux/main-common.cpp
@@ -37,6 +37,7 @@
 #include "rvc-modes.h"
 #include "rvc-operational-state-delegate-impl.h"
 #include "tcc-mode.h"
+#include "water-heater-mode.h"
 #include <Options.h>
 #include <app-common/zap-generated/attributes/Accessors.h>
 #include <app/CommandHandler.h>
