fc7188a761c29a6657869c13faaad3fb4d9d5ab6
pcoleman <pcoleman@geotogether.com>
2024-07-18 07:39:11 +0100

    Addressing review comments from Boris

--- src/app/clusters/water-heater-management-server/water-heater-management-server.cpp
+++ src/app/clusters/water-heater-management-server/water-heater-management-server.cpp
@@ -112,22 +112,22 @@ void Instance::InvokeCommand(HandlerContext & handlerContext)
 
 void Instance::HandleBoost(HandlerContext & ctx, const Commands::Boost::DecodableType & commandData)
 {
-    uint32_t duration                        = commandData.duration;
-    Optional<bool> oneShot                   = commandData.oneShot;
-    Optional<bool> emergencyBoost            = commandData.emergencyBoost;
-    Optional<int16_t> temporarySetpoint      = commandData.temporarySetpoint;
-    Optional<Percent> targetPercentage = commandData.targetPercentage;
-    Optional<Percent> targetReheat     = commandData.targetReheat;
+    uint32_t duration                   = commandData.duration;
+    Optional<bool> oneShot              = commandData.oneShot;
+    Optional<bool> emergencyBoost       = commandData.emergencyBoost;
+    Optional<int16_t> temporarySetpoint = commandData.temporarySetpoint;
+    Optional<Percent> targetPercentage  = commandData.targetPercentage;
+    Optional<Percent> targetReheat      = commandData.targetReheat;
 
     //  Notify the appliance if the appliance hardware cannot be adjusted, then return Failure
-    if (!HasFeature(WaterHeaterManagement::Feature::kTankPercent))
+    if (HasFeature(WaterHeaterManagement::Feature::kTankPercent))
     {
         if (targetPercentage.HasValue())
         {
             if (targetPercentage.Value() > 100)
             {
                 ChipLogError(Zcl, "Bad targetPercentage %u", targetPercentage.Value());
-                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::Failure);
+                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::InvalidCommand);
                 return;
             }
         }
@@ -137,24 +137,30 @@ void Instance::HandleBoost(HandlerContext & ctx, const Commands::Boost::Decodabl
             if (targetReheat.Value() > 100)
             {
                 ChipLogError(Zcl, "Bad targetReheat %u", targetReheat.Value());
-                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::Failure);
+                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::InvalidCommand);
                 return;
             }
 
             if (!targetPercentage.HasValue())
             {
                 ChipLogError(Zcl, "targetPercentage must be specified if targetReheat specified");
-                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::Failure);
+                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::InvalidCommand);
                 return;
             }
 
             if (oneShot.HasValue())
             {
                 ChipLogError(Zcl, "Cannot specify targetReheat with targetPercentage and oneShot. oneShot must be excluded");
-                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::Failure);
+                ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::InvalidCommand);
                 return;
             }
         }
+    }
+    else if (targetPercentage.HasValue() || targetReheat.HasValue())
+    {
+        ChipLogError(Zcl, "Cannot specify targetPercentage or targetReheat if the feature TankPercent is not supported");
+        ctx.mCommandHandler.AddStatus(ctx.mRequestPath, Status::InvalidCommand);
+        return;
     }
 
     Status status = mDelegate.HandleBoost(duration, oneShot, emergencyBoost, temporarySetpoint, targetPercentage, targetReheat);
