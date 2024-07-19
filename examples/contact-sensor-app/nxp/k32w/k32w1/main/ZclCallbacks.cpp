2ff77544531a565660f36952ccbe6a96a3a07329
marius-alex-tache <marius.tache@nxp.com>
2024-07-19 12:40:28 +0300

    [NXP][examples][k32w1] Update ZclCallbacks after transition to common code
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- examples/contact-sensor-app/nxp/k32w/k32w1/main/ZclCallbacks.cpp
+++ examples/contact-sensor-app/nxp/k32w/k32w1/main/ZclCallbacks.cpp
@@ -1,7 +1,6 @@
 /*
  *
- *    Copyright (c) 2022-2023 Project CHIP Authors
- *    Copyright (c) 2023 NXP
+ *    Copyright (c) 2024 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,97 +16,34 @@
  *    limitations under the License.
  */
 
+#include <app-common/zap-generated/cluster-objects.h>
+#include <app/EventLogging.h>
 #include <lib/support/logging/CHIPLogging.h>
 
-#include "AppTask.h"
-#include "ContactSensorManager.h"
-
-#include <app-common/zap-generated/cluster-objects.h>
-#include <app-common/zap-generated/ids/Attributes.h>
-#include <app-common/zap-generated/ids/Clusters.h>
-#include <app/ConcreteAttributePath.h>
-#include <app/EventLogging.h>
-#include <app/util/af-types.h>
-
-#if CONFIG_DIAG_LOGS_DEMO
-#include "DiagnosticLogsProviderDelegateImpl.h"
-#include <app/clusters/diagnostic-logs-server/diagnostic-logs-server.h>
+#if CONFIG_ENABLE_FEEDBACK
+#include "UserInterfaceFeedback.h"
 #endif
 
-using namespace ::chip;
-using namespace ::chip::app;
-using namespace ::chip::app::Clusters;
-using namespace ::chip::app::Clusters::BooleanState;
-#if CONFIG_DIAG_LOGS_DEMO
-using namespace ::chip::app::Clusters::DiagnosticLogs;
-#endif
-
-void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & path, uint8_t type, uint16_t size, uint8_t * value)
-{
-    ChipLogProgress(Zcl, "MatterPostAttributeChangeCallback, value:%d\n", *value);
-    if (path.mClusterId != BooleanState::Id)
-    {
-        ChipLogProgress(Zcl, "Unknown cluster ID: " ChipLogFormatMEI, ChipLogValueMEI(path.mClusterId));
-        return;
-    }
-
-    if (path.mAttributeId != BooleanState::Attributes::StateValue::Id)
-    {
-        ChipLogProgress(Zcl, "Unknown attribute ID: " ChipLogFormatMEI, ChipLogValueMEI(path.mAttributeId));
-        return;
-    }
-
-    AppTask & task = GetAppTask();
-    // If the callback is called after the cluster attribute was changed due to pressing a button,
-    // set the sync value to false. Both LED and attribute were updated at this point.
-    // On the other hand, if the cluster attribute was changed due to a cluster command,
-    // forward the request to AppTask in order to update the LED state.
-    if (task.IsSyncClusterToButtonAction())
-    {
-        task.SetSyncClusterToButtonAction(false);
-    }
-    else
-    {
-        task.PostContactActionRequest(*value ? ContactSensorManager::Action::kSignalDetected
-                                             : ContactSensorManager::Action::kSignalLost);
-    }
-}
-
-/** @brief OnOff Cluster Init
+/** @brief BooleanState Cluster Init
  *
  * This function is called when a specific cluster is initialized. It gives the
  * application an opportunity to take care of cluster initialization procedures.
  * It is called exactly once for each endpoint where cluster is present.
- *
- * @param endpoint   Ver.: always
- *
- * TODO Issue #3841
- * emberAfOnOffClusterInitCallback happens before the stack initialize the cluster
- * attributes to the default value.
- * The logic here expects something similar to the deprecated Plugins callback
- * emberAfPluginOnOffClusterServerPostInitCallback.
- *
  */
-void emberAfBooleanStateClusterInitCallback(EndpointId endpoint)
+void emberAfBooleanStateClusterInitCallback(chip::EndpointId endpoint)
 {
     ChipLogProgress(Zcl, "emberAfBooleanStateClusterInitCallback\n");
-    GetAppTask().UpdateClusterState();
+#if CONFIG_ENABLE_FEEDBACK
+    FeedbackMgr().RestoreState();
+#endif
 }
 
 void logBooleanStateEvent(bool state)
 {
-    EventNumber eventNumber;
-    Events::StateChange::Type event{ state };
-    if (CHIP_NO_ERROR != LogEvent(event, 1, eventNumber))
+    chip::EventNumber eventNumber;
+    chip::app::Clusters::BooleanState::Events::StateChange::Type event{ state };
+    if (CHIP_NO_ERROR != chip::app::LogEvent(event, 1, eventNumber))
     {
         ChipLogProgress(Zcl, "booleanstate: failed to reacord state-change event");
     }
 }
-
-#if CONFIG_DIAG_LOGS_DEMO
-void emberAfDiagnosticLogsClusterInitCallback(chip::EndpointId endpoint)
-{
-    auto & logProvider = LogProvider::GetInstance();
-    DiagnosticLogsServer::Instance().SetDiagnosticLogsProviderDelegate(endpoint, &logProvider);
-}
-#endif
