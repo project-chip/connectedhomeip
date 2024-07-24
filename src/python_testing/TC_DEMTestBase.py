ac24f5c92064d733d0532fc21dda7381de91f7ae
Restyled.io <commits@restyled.io>
2024-07-24 07:46:54 +0000

    Restyled by isort

--- src/python_testing/TC_DEMTestBase.py
+++ src/python_testing/TC_DEMTestBase.py
@@ -19,8 +19,8 @@ import logging
 
 import chip.clusters as Clusters
 from chip.interaction_model import InteractionModelError, Status
-from mobly import asserts
 from matter_testing_support import utc_time_in_matter_epoch
+from mobly import asserts
 
 logger = logging.getLogger(__name__)
 
