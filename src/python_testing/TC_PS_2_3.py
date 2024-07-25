64bf7338421f9c0c04c18458d6b12a7a68fce4cd
Restyled.io <commits@restyled.io>
2024-07-25 15:29:03 +0000

    Restyled by isort

--- src/python_testing/TC_PS_2_3.py
+++ src/python_testing/TC_PS_2_3.py
@@ -32,7 +32,8 @@ import time
 
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from matter_testing_support import ClusterAttributeChangeAccumulator, MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+from matter_testing_support import (ClusterAttributeChangeAccumulator, MatterBaseTest, TestStep, async_test_body,
+                                    default_matter_test_main)
 from mobly import asserts
 
 
