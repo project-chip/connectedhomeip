f0b46ca6267443ab41bbc4b4655a4c8b185c50d3
Cecille Freeman <cecille@google.com>
2024-07-25 21:01:07 -0400

    formatting of imports

--- src/python_testing/test_testing/TestDecorators.py
+++ src/python_testing/test_testing/TestDecorators.py
@@ -37,7 +37,8 @@ try:
                                         has_cluster, per_endpoint_test, per_node_test)
 except ImportError:
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-    from matter_testing_support import MatterBaseTest, async_test_body, has_cluster, has_attribute, get_accepted_endpoints_for_test, per_endpoint_test, per_node_test
+    from matter_testing_support import (MatterBaseTest, async_test_body, get_accepted_endpoints_for_test, has_attribute,
+                                        has_cluster, per_endpoint_test, per_node_test)
 
 from typing import Optional
 
