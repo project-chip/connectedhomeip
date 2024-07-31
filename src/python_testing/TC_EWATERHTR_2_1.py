5e5b22c25240bb0300420a44bc4c1209c5918988
James Harrow <james.harrow@gmail.com>
2024-07-31 18:19:05 +0100

    Corrected test-runner command linewrap issue

--- src/python_testing/TC_EWATERHTR_2_1.py
+++ src/python_testing/TC_EWATERHTR_2_1.py
@@ -18,7 +18,8 @@
 # See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
 # for details about the block below.
 #
-# === BEGIN CI TEST ARGUMENTS ===# test-runner-runs: run1
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs: run1
 # test-runner-run/run1/app: ${ALL_CLUSTERS_APP}
 # test-runner-run/run1/factoryreset: True
 # test-runner-run/run1/quiet: True
