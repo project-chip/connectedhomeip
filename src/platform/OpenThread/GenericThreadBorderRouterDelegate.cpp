150bd9f260034d25253fd127d976e229293a5af7
WanqQixiang <wangqixiang@espressif.com>
2024-07-31 15:18:59 +0800

    Move the sources of GenericThreadBorderRouterDelegate to src/platform

--- src/platform/OpenThread/GenericThreadBorderRouterDelegate.cpp
+++ src/platform/OpenThread/GenericThreadBorderRouterDelegate.cpp
@@ -209,8 +209,9 @@ CHIP_ERROR GenericOpenThreadBorderRouterDelegate::SetPendingDataset(const Thread
     VerifyOrReturnError(otInst, CHIP_ERROR_INCORRECT_STATE);
 
     otOperationalDatasetTlvs datasetTlvs;
+    VerifyOrDie(pendingDataset.AsByteSpan().size() <= OT_OPERATIONAL_DATASET_MAX_LENGTH);
     memcpy(datasetTlvs.mTlvs, pendingDataset.AsByteSpan().data(), pendingDataset.AsByteSpan().size());
-    datasetTlvs.mLength = pendingDataset.AsByteSpan().size();
+    datasetTlvs.mLength = static_cast<uint8_t>(pendingDataset.AsByteSpan().size());
     {
         ScopedThreadLock threadLock;
         ReturnErrorCodeIf(otDatasetSetPendingTlvs(otInst, &datasetTlvs) != OT_ERROR_NONE, CHIP_ERROR_INTERNAL);
