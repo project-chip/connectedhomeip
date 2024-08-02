d427ff2e36a8e07c179887297ab671a877b16ed0
Andrei Litvin <andreilitvin@google.com>
2024-08-02 15:03:14 -0400

    Undo header addition that seems wrong

--- src/app/data-model-provider/StringBuilderAdapters.cpp
+++ src/app/data-model-provider/StringBuilderAdapters.cpp
@@ -13,7 +13,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "app/data-model-provider/ActionReturnStatus.h"
 #include <app/data-model-provider/StringBuilderAdapters.h>
 
 #include <lib/core/StringBuilderAdapters.h>
