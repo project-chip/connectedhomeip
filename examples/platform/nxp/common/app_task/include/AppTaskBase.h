25d45ad780a09f127d0cb5717b098f79a2813444
Andrei Menzopol <andrei.menzopol@nxp.com>
2024-08-02 05:40:08 -0700

    [nxp][examples][platform] Add cluster-handling functions & remove RestoreState
     * Add virtual cluster-handling functions that must be implemented per application
       based on cluster configuration.
     * Remove RestoreState as it may read attributes from cluster before the enpoints
       are initialized.
    
    Signed-off-by: Andrei Menzopol <andrei.menzopol@nxp.com>

--- examples/platform/nxp/common/app_task/include/AppTaskBase.h
+++ examples/platform/nxp/common/app_task/include/AppTaskBase.h
@@ -177,6 +177,13 @@ public:
     virtual void SwitchCommissioningStateHandler(void);
     virtual void FactoryResetHandler(void);
 
+    /**
+     * Cluster-handling functions
+     * Must be overridden by a child class per cluster configuration
+     */
+    virtual bool CheckStateClusterHandler(void);
+    virtual CHIP_ERROR ProcessSetStateClusterHandler(void);
+
 private:
     inline static chip::CommonCaseDeviceServerInitParams initParams;
 
