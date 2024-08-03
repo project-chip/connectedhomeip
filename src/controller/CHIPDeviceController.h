5ae616dd562c90c0fa6d21884487aa6003cba386
Lo,Chin-Ran <chin-ran.lo@nxp.com>
2024-08-03 19:57:29 +0800

    * Fix the dependency error * Change the default setting of WiFiPAF as enabled if running on Linux system * Change to use C++ casting * Move the paf cancel function from desctructor to shutdown
    Signed-off-by: Lo,Chin-Ran <chin-ran.lo@nxp.com>

--- src/controller/CHIPDeviceController.h
+++ src/controller/CHIPDeviceController.h
@@ -478,7 +478,7 @@ class DLL_EXPORT DeviceCommissioner : public DeviceController,
 {
 public:
     DeviceCommissioner();
-    ~DeviceCommissioner() override;
+    ~DeviceCommissioner() override {}
 
 #if CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY // make this commissioner discoverable
     /**
@@ -730,6 +730,10 @@ public:
      */
     void CloseBleConnection();
 #endif
+#if CHIP_DEVICE_CONFIG_ENABLE_WIFIPAF
+    void ConnectWiFiPAFTransportToSelf();
+#endif
+
     /**
      * @brief
      *   Discover all devices advertising as commissionable.
