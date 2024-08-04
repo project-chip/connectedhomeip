9de1714f5dc226287dd825710ab9cbeb324f4290
Lo,Chin-Ran <chin-ran.lo@nxp.com>
2024-08-04 18:32:57 +0800

    * Fix the error found by the building script
    Signed-off-by: Lo,Chin-Ran <chin-ran.lo@nxp.com>

--- src/transport/raw/WiFiPAF.h
+++ src/transport/raw/WiFiPAF.h
@@ -55,7 +55,7 @@ public:
     CHIP_ERROR Init(const WiFiPAFListenParameters & param);
     CHIP_ERROR SendMessage(const Transport::PeerAddress & address, System::PacketBufferHandle && msgBuf) override;
     bool CanSendToPeer(const Transport::PeerAddress & address) override;
-    void OnWiFiPAFMessageReceived(System::PacketBufferHandle && buffer);
+    void OnWiFiPAFMessageReceived(System::PacketBufferHandle && buffer) override;
     void SetWiFiPAFLayerTransportToSelf() { mWiFiPAFLayer->mWiFiPAFTransport = this; }
     bool IsWiFiPAFLayerTransportSetToSelf() { return mWiFiPAFLayer->mWiFiPAFTransport == this; }
 
@@ -89,6 +89,7 @@ class WiFiPAFListenParameters
 public:
     WiFiPAFListenParameters() = default;
     explicit WiFiPAFListenParameters(WiFiPAFBase * layer) : mWiFiPAF(layer) {}
+    WiFiPAFBase * GetWiFiPAFTransport() const { return mWiFiPAF; }
 
 private:
     WiFiPAFBase * mWiFiPAF;
