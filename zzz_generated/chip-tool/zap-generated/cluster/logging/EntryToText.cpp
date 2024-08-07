2cdbd2ed55dfa5c2c0251751bae9f123a0163bcf
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2024-08-07 11:35:19 -0400

    Adding a Silabs dishwasher app, supports Electrical sensor and DEM
    Somewhat acceptable state, build & runs, got to try the tests

--- zzz_generated/chip-tool/zap-generated/cluster/logging/EntryToText.cpp
+++ zzz_generated/chip-tool/zap-generated/cluster/logging/EntryToText.cpp
@@ -6503,6 +6503,8 @@ char const * DeviceTypeIdToText(chip::DeviceTypeId id)
         return "Matter Humidity Sensor";
     case 0x0000050C:
         return "Matter EVSE";
+    case 0x0000050D:
+        return "Matter Device Energy Management";
     case 0x00000510:
         return "Matter Electrical Sensor";
     case 0x00000840:
