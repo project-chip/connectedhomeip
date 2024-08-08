1c01e9a0e236a0633b1818425020561f2f5e0980
Harshith-GRL <harshith.yadav@graniteriverlabs.in>
2024-08-08 17:06:40 +0530

    Made Fix for GIT jobs
    * Fixed CI Arguments, it will use CHIP_LOCK_APP

--- src/python_testing/TC_DRLK_2_13.py
+++ src/python_testing/TC_DRLK_2_13.py
@@ -19,7 +19,7 @@
 #
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs: run1
-# test-runner-run/run1/app: ${ALL_CLUSTERS_APP}
+# test-runner-run/run1/app: ${CHIP_LOCK_APP}
 # test-runner-run/run1/factoryreset: True
 # test-runner-run/run1/quiet: True
 # test-runner-run/run1/app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
