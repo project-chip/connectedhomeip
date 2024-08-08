f27d427c464aa86799d8ae2abe5d7acd189603b2
Harshith-GRL <harshith.yadav@graniteriverlabs.in>
2024-08-08 15:33:31 +0530

    Made Fix for GIT jobs * Used restyle to fix styling * Removed unused Variable 'response' * Added license Header and CI Argument header

--- src/python_testing/TC_DRLK_2_13.py
+++ src/python_testing/TC_DRLK_2_13.py
@@ -1,4 +1,31 @@
-import copy
+#
+#    Copyright (c) 2024 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs: run1
+# test-runner-run/run1/app: ${ALL_CLUSTERS_APP}
+# test-runner-run/run1/factoryreset: True
+# test-runner-run/run1/quiet: True
+# test-runner-run/run1/app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+# test-runner-run/run1/script-args: --storage-path admin_storage.json --commissioning-method on-network --discriminator 1234 --passcode 20202021 --endpoint 1 --trace-to json:${TRACE_TEST_JSON}.json --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+# === END CI TEST ARGUMENTS ===
+
 import logging
 import random
 
@@ -164,7 +191,7 @@ class TC_DRLK_2_13(MatterBaseTest):
 
     async def send_clear_user_cmd(self, user_index, expected_status: Status = Status.Success):
         try:
-            response = await self.send_single_cmd(cmd=Clusters.DoorLock.Commands.ClearUser(userIndex=user_index),
+            await self.send_single_cmd(cmd=Clusters.DoorLock.Commands.ClearUser(userIndex=user_index),
                                                   endpoint=1,
                                                   timedRequestTimeoutMs=1000)
             asserts.assert_equal(expected_status, Status.Success)
