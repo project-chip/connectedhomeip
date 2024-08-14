23b8ec2d963ce45148f0d3b210f3d62761c00268
Hasty Granbery <hasty@granbery.org>
2024-08-14 12:17:39 -0700

    Drop duplicate doc comments

--- examples/thermostat/thermostat-common/include/atomic-write-manager.h
+++ examples/thermostat/thermostat-common/include/atomic-write-manager.h
@@ -46,91 +46,18 @@ public:
      */
     void SetDelegate(AtomicWriteDelegate * delegate) override { mDelegate = delegate; };
 
-    /**
-     * @brief Check if there is an open atomic write on an endpoint, optionally associated with a specific attribute
-     *
-     * @param attributeId Optional attribute filter
-     * @param endpoint The endpoint to check atomic write state
-     * @return true if there is an open atomic write
-     * @return false if there is no open atomic write
-     */
+    // AtomicWriteManager
     bool InWrite(const std::optional<AttributeId> attributeId, EndpointId endpoint) override;
-
-    /**
-     * @brief Check if there is an open atomic write on an endpoint, associated with a given SubjectDescriptor, and optionally
-     * associated with a specific attribute
-     *
-     * @param attributeId Optional attribute filter
-     * @param subjectDescriptor The subject descriptor to check against
-     * @param endpoint The endpoint to check atomic write state
-     * @return true if there is an open atomic write
-     * @return false if there is no open atomic write
-     */
     bool InWrite(const std::optional<AttributeId> attributeId, const Access::SubjectDescriptor & subjectDescriptor,
                  EndpointId endpoint) override;
-
-    /**
-     * @brief Check if there is an open atomic write on an endpoint, associated with the source node for a given command invocation,
-     * and optionally associated with a specific attribute
-     *
-     * @param attributeId
-     * @param commandObj
-     * @param endpoint
-     * @return true if there is an open atomic write
-     * @return false if there is no open atomic write
-     */
     bool InWrite(const std::optional<AttributeId> attributeId, CommandHandler * commandObj, EndpointId endpoint) override;
-
-    /**
-     * @brief Attempt to start an atomic write
-     *
-     * @param commandObj The AtomicRequest command
-     * @param commandPath The path for the command
-     * @param commandData The payload of the command
-     * @return true if the atomic write was opened successfully
-     * @return false if the request to open atomic write failed
-     */
     bool BeginWrite(chip::app::CommandHandler * commandObj, const ConcreteCommandPath & commandPath,
                     const Commands::AtomicRequest::DecodableType & commandData) override;
-
-    /**
-     * @brief Attempt to commit an atomic write; returns true if the server is able to either commit or rollback successfully,
-     * returns false otherwise
-     *
-     * @param commandObj The AtomicRequest command
-     * @param commandPath The path for the command
-     * @param commandData The payload of the command
-     * @return true if the atomic write was either committed or rolled back
-     * @return false if the atomic write was not found
-     */
     bool CommitWrite(chip::app::CommandHandler * commandObj, const ConcreteCommandPath & commandPath,
                      const Commands::AtomicRequest::DecodableType & commandData) override;
-
-    /**
-     * @brief Attempt to roll back an atomic write; returns true if the server is able to rollback successfully, false if it was
-     * unable to find a matching atomic write
-     *
-     * @param commandObj The AtomicRequest command
-     * @param commandPath The path for the command
-     * @param commandData The payload of the command
-     * @return true if the atomic write was rolled back
-     * @return false if the atomic write was not found
-     */
     bool RollbackWrite(chip::app::CommandHandler * commandObj, const ConcreteCommandPath & commandPath,
                        const Commands::AtomicRequest::DecodableType & commandData) override;
-
-    /**
-     * @brief Reset any atomic writes associated with the given endpoint
-     *
-     * @param endpoint
-     */
     void ResetWrite(EndpointId endpoint) override;
-
-    /**
-     * @brief Reset any atomic writes originating from a given fabric index
-     *
-     * @param fabricIndex
-     */
     void ResetWrite(FabricIndex fabricIndex) override;
 
 private:
