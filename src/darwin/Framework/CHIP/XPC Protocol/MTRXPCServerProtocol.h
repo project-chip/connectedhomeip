8c88752d0412b2cfa2551d925f39749d152cee5d
Restyled.io <commits@restyled.io>
2024-08-16 01:47:02 +0000

    Restyled by clang-format

--- src/darwin/Framework/CHIP/XPC Protocol/MTRXPCServerProtocol.h
+++ src/darwin/Framework/CHIP/XPC Protocol/MTRXPCServerProtocol.h
@@ -17,13 +17,13 @@
 
 @protocol MTRXPCServerProtocol_MTRDevice <NSObject>
 
-- (oneway void)getStateWithReply:(void(^ _Nonnull)(MTRDeviceState state))reply;
-- (oneway void)getDeviceCachePrimedWithReply:(void(^ _Nonnull)(BOOL primed))reply;
-- (oneway void)getEstimatedStartTimeWithReply:(void(^ _Nonnull)(NSDate * _Nullable estimatedStartTime))reply;
-- (oneway void)getNodeIDWithReply:(void(^ _Nonnull)(NSNumber * _Nonnull nodeID))reply;
-- (oneway void)getEstimatedSubscriptionLatencyWithReply:(void(^ _Nonnull)(NSNumber * _Nullable estimatedSubscriptionLatency))reply;
+- (oneway void)getStateWithReply:(void (^_Nonnull)(MTRDeviceState state))reply;
+- (oneway void)getDeviceCachePrimedWithReply:(void (^_Nonnull)(BOOL primed))reply;
+- (oneway void)getEstimatedStartTimeWithReply:(void (^_Nonnull)(NSDate * _Nullable estimatedStartTime))reply;
+- (oneway void)getNodeIDWithReply:(void (^_Nonnull)(NSNumber * _Nonnull nodeID))reply;
+- (oneway void)getEstimatedSubscriptionLatencyWithReply:(void (^_Nonnull)(NSNumber * _Nullable estimatedSubscriptionLatency))reply;
 
-- (oneway void)readAttributeWithEndpointID:(NSNumber * _Nonnull)endpointID clusterID:(NSNumber * _Nonnull)clusterID attributeID:(NSNumber * _Nonnull)attributeID params:(MTRReadParams * _Nullable)params withReply:(void(^_Nonnull)(NSDictionary<NSString *, id> * _Nullable))reply;
+- (oneway void)readAttributeWithEndpointID:(NSNumber * _Nonnull)endpointID clusterID:(NSNumber * _Nonnull)clusterID attributeID:(NSNumber * _Nonnull)attributeID params:(MTRReadParams * _Nullable)params withReply:(void (^_Nonnull)(NSDictionary<NSString *, id> * _Nullable))reply;
 - (oneway void)writeAttributeWithEndpointID:(NSNumber * _Nonnull)endpointID clusterID:(NSNumber * _Nonnull)clusterID attributeID:(NSNumber * _Nonnull)attributeID value:(id _Nullable)value expectedValueInterval:(NSNumber * _Nullable)expectedValueInterval timedWriteTimeout:(NSNumber * _Nullable)timeout;
 
 - (oneway void)invokeCommandWithEndpointID:(NSNumber * _Nonnull)endpointID clusterID:(NSNumber * _Nonnull)clusterID commandID:(NSNumber * _Nonnull)commandID commandFields:(id _Nonnull)commandFields expectedValues:(NSArray<NSDictionary<NSString *, id> *> * _Nullable)expectedValues expectedValueInterval:(NSNumber * _Nullable)expectedValueInterval timedInvokeTimeout:(NSNumber * _Nullable)timeout completion:(MTRDeviceResponseHandler _Nonnull)completion;
@@ -31,12 +31,12 @@
 // Not Supported via XPC
 //- (oneway void)openCommissioningWindowWithSetupPasscode:(NSNumber * _Nonnull)setupPasscode discriminator:(NSNumber * _Nonnull)discriminator duration:(NSNumber * _Nonnull)duration completion:(MTRDeviceOpenCommissioningWindowHandler _Nonnull)completion;
 
-- (oneway void)getClientDataKeysWithReply:(void(^ _Nonnull)(NSArray * _Nullable))reply;
-- (oneway void)clientDataForKey:(NSString * _Nonnull)key withReply:(void(^ _Nonnull)(id<NSSecureCoding> _Nullable))reply;
+- (oneway void)getClientDataKeysWithReply:(void (^_Nonnull)(NSArray * _Nullable))reply;
+- (oneway void)clientDataForKey:(NSString * _Nonnull)key withReply:(void (^_Nonnull)(id<NSSecureCoding> _Nullable))reply;
 - (oneway void)setClientDataForKey:(NSString * _Nonnull)key value:(id<NSSecureCoding> _Nonnull)value;
 - (oneway void)removeClientDataForKey:(NSString * _Nonnull)key;
-- (oneway void)clientDataKeysForEndpointID:(NSNumber * _Nonnull)endpointID withReply:(void(^ _Nonnull)(NSArray * _Nullable))reply;
-- (oneway void)clientDataForKey:(NSString * _Nonnull)key endpointID:(NSNumber * _Nonnull)endpointID withReply:(void(^ _Nonnull)(id<NSSecureCoding> _Nullable))reply;
+- (oneway void)clientDataKeysForEndpointID:(NSNumber * _Nonnull)endpointID withReply:(void (^_Nonnull)(NSArray * _Nullable))reply;
+- (oneway void)clientDataForKey:(NSString * _Nonnull)key endpointID:(NSNumber * _Nonnull)endpointID withReply:(void (^_Nonnull)(id<NSSecureCoding> _Nullable))reply;
 - (oneway void)setClientDataForKey:(NSString * _Nonnull)key endpointID:(NSNumber * _Nonnull)endpointID value:(id<NSSecureCoding> _Nullable)value;
 - (oneway void)removeClientDataForKey:(NSString * _Nonnull)key endpointID:(NSNumber * _Nonnull)endpointID;
 
@@ -46,9 +46,9 @@
 
 @protocol MTRXPCServerProtocol_MTRDeviceController <NSObject>
 
-- (oneway void)getIsRunningWithReply:(void(^ _Nonnull)(BOOL response))reply;
-- (oneway void)getUniqueIdentifierWithReply:(void(^ _Nonnull)(NSUUID * _Nonnull))reply;
-- (oneway void)controllerNodeIDWithReply:(void(^ _Nonnull)(NSNumber * _Nonnull nodeID))reply;
+- (oneway void)getIsRunningWithReply:(void (^_Nonnull)(BOOL response))reply;
+- (oneway void)getUniqueIdentifierWithReply:(void (^_Nonnull)(NSUUID * _Nonnull))reply;
+- (oneway void)controllerNodeIDWithReply:(void (^_Nonnull)(NSNumber * _Nonnull nodeID))reply;
 
 // Not Supported via XPC
 // - (oneway void)setupCommissioningSessionWithPayload:(MTRSetupPayload * _Nonnull)payload newNodeID:(NSNumber * _Nonnull)newNodeID withReply:(void(^ _Nonnull)(BOOL success, NSError * _Nullable error))reply;
