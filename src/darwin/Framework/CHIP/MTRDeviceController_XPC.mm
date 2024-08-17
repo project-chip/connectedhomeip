8c026ead3b28f0164fda7ce15c3888e58cbc22e3
Kiel Oleson <kielo@apple.com>
2024-08-17 15:00:16 -0700

    [Darwin] New XPC Service (#34961)
    * empty xpc service files
    
    * XPC service needs to be C++ to use MTRLogging
    
    * add tests; stub method signature
    
    * start configuring test XPC listener
    
    * add first test method to XPC service protocol
    
    * tests: remove peformance test; add headers
    
    * make MTRXPCService testable
    
    * more working lines of XPC listener setup
    
    * add dummy XPC service for testing
    
    * add replying method to service protocol / implementation
    
    * remove log - reply contents can be asserted
    
    * make test interesting
    
    * linty formatty
    
    * remove personal development team from pbxproj
    
    * fix new file headers
    
    * less whitespace
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * add new XPC device controller
    
    * sketch out a first XPC method
    
    * store WIP for rebase
    
    * fix pbxproj merge mistake
    
    * make WIP slightly more sensible
    
    * WIP: remote proxy obj in `MTRDeviceController_XPC`
    
    * more experiments
    
    * WIP:  return type issue
    
    run `MTRXPCServiceTests testMTRXPCServiceSetup` and see selection of proxy object method in `MTRDeviceController_XPC.m`/`initWithXPCListenerEndpointForTesting` around line 44 to see the problem.
    
    * add `MTRDevice_XPC`
    
    nothing exciting yet
    
    * add device controller over XPC parameters stub
    
    * add init split for XPC device controllers
    
    not yet implemented, but there
    
    * rename new XPC parameters class
    
    `OverXPC` is already taken / means something else
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * remove failing exploratory test
    
    * lintfix
    
    * Examples
    
    * quickly attempt to fix test
    
    unused variable -> warning -> error
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * inevitably the file will need to be obj-C++
    
    * prepare to use mach service name to create XPC connection
    
    * allow for other XPC controller connection types for now
    
    once mach service is working, i don't expect to need others, but easier to collapse classes than raise.
    
    * constant for well-known matter plugin service name
    
    * note to self and re-ordering of this code now what `XPCParameters` is a subclass
    
    * XPC parameters should support secure coding
    
    * don't keep around object proxy; make a new one when needed.  try calling the `checkIn` method when connecting.  remove old test methods that served their purpose.
    
    * that doesn't need to be there.
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * no longer fighting with compiler, have some `instancetype`
    
    * extremely normal object construction
    
    * somehow this header got thrown out
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Updating XPC interfaces
    
    * hide `initWithMachServiceName:options:` constructor
    
    tvOS/iOS compilation issues need a look
    
    * make `MTRDeviceController_XPC` an XPC client
    
    via `MTRXPCClientProtocol_MTRDeviceController`, which for the moment is empty
    
    * remove initial tests
    
    they served their purpose well but are no longer relevant
    
    * call mach service constructor when appropriate
    
    also logging
    
    * remove some obsolete test methods
    
    * more obsoletes fixed
    
    * more obsolete removal and logging tweaks
    
    * buildability tweaks
    
    * Moving along
    
    * Fixing header
    
    * Fixing macros and codable
    
    * Adding invoke?
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Actually hooking up to XPCConnection
    
    * Actually hooking up to XPCConnection
    
    * Adding XPC Parameters
    
    * Adding XPC Parameter hookup
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * true => YES
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Justin Wood <woody@apple.com>

---
+++ src/darwin/Framework/CHIP/MTRDeviceController_XPC.mm
@@ -0,0 +1,196 @@
+/**
+ *    Copyright (c) 2024 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#import "MTRDeviceController_XPC.h"
+#import "MTRDefines_Internal.h"
+#import "MTRDeviceController_Internal.h"
+#import "MTRDevice_XPC.h"
+#import "MTRLogging_Internal.h"
+#import "MTRXPCClientProtocol.h"
+#import "MTRXPCServerProtocol.h"
+
+#define MTR_DEVICECONTROLLER_SIMPLE_REMOTE_XPC_GETTER(NAME, TYPE, DEFAULT_VALUE, GETTER_NAME)                 \
+    MTR_SIMPLE_REMOTE_XPC_GETTER(self.xpcConnection, NAME, TYPE, DEFAULT_VALUE, GETTER_NAME, deviceController \
+                                 : self.uniqueIdentifier)
+
+#define MTR_DEVICECONTROLLER_SIMPLE_REMOTE_XPC_COMMAND(METHOD_SIGNATURE, ADDITIONAL_ARGUMENTS)                 \
+    MTR_SIMPLE_REMOTE_XPC_COMMAND(self.xpcConnection, METHOD_SIGNATURE, ADDITIONAL_ARGUMENTS, deviceController \
+                                  : self.uniqueIdentifier)
+
+@interface MTRDeviceController_XPC ()
+
+@property (nonatomic, retain, readwrite) NSUUID * uniqueIdentifier;
+
+@end
+
+// #define MTR_HAVE_MACH_SERVICE_NAME_CONSTRUCTOR
+
+@implementation MTRDeviceController_XPC
+
+@synthesize uniqueIdentifier = _uniqueIdentifier;
+
+- (id)initWithUniqueIdentifier:(NSUUID *)UUID xpConnectionBlock:(NSXPCConnection * (^)(void) )connectionBlock
+{
+    if (self = [super initForSubclasses]) {
+        MTR_LOG("Setting up XPC Controller for UUID: %@  with connection block: %p", UUID, connectionBlock);
+
+        if (UUID == nil) {
+            MTR_LOG_ERROR("MTRDeviceController_XPC initWithUniqueIdentifier failed, nil UUID");
+            return nil;
+        }
+        if (connectionBlock == nil) {
+            MTR_LOG_ERROR("MTRDeviceController_XPC initWithUniqueIdentifier failed, nil connectionBlock");
+            return nil;
+        }
+
+        self.xpcConnection = connectionBlock();
+        self.uniqueIdentifier = UUID;
+
+        MTR_LOG("Set up XPC Connection: %@", self.xpcConnection);
+        if (self.xpcConnection) {
+            self.xpcConnection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(MTRXPCServerProtocol)];
+
+            self.xpcConnection.exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(MTRXPCClientProtocol)];
+            self.xpcConnection.exportedObject = self;
+
+            MTR_LOG("Resuming new XPC connection");
+            [self.xpcConnection resume];
+        } else {
+            MTR_LOG_ERROR("Failed to set up XPC Connection");
+            return nil;
+        }
+    }
+
+    return self;
+}
+
+#ifdef MTR_HAVE_MACH_SERVICE_NAME_CONSTRUCTOR
+- (id)initWithUniqueIdentifier:(NSUUID *)UUID machServiceName:(NSString *)machServiceName options:(NSXPCConnectionOptions)options
+{
+    if (self = [super initForSubclasses]) {
+        MTR_LOG("Setting up XPC Controller for UUID: %@  with machServiceName: %s options: %d", UUID, machServiceName, options);
+        self.xpcConnection = [[NSXPCConnection alloc] initWithMachServiceName:machServiceName options:options];
+        self.uniqueIdentifier = UUID;
+
+        MTR_LOG("Set up XPC Connection: %@", self.xpcConnection);
+        if (self.xpcConnection) {
+            self.xpcConnection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(MTRXPCServerProtocol)];
+
+            self.xpcConnection.exportedInterface = [NSXPCInterface interfaceWithProtocol:@protocol(MTRXPCClientProtocol)];
+            self.xpcConnection.exportedObject = self;
+
+            MTR_LOG("%s: resuming new XPC connection");
+            [self.xpcConnection resume];
+        } else {
+            MTR_LOG_ERROR("Failed to set up XPC Connection");
+            return nil;
+        }
+    }
+
+    return self;
+}
+#endif // MTR_HAVE_MACH_SERVICE_NAME_CONSTRUCTOR
+
+- (nullable instancetype)initWithParameters:(MTRDeviceControllerAbstractParameters *)parameters
+                                      error:(NSError * __autoreleasing *)error
+{
+    MTR_LOG_ERROR("%s: unimplemented method called", __PRETTY_FUNCTION__);
+    return nil;
+}
+
+#pragma mark - XPC Action Overrides
+
+MTR_DEVICECONTROLLER_SIMPLE_REMOTE_XPC_GETTER(isRunning, BOOL, NO, getIsRunningWithReply)
+MTR_DEVICECONTROLLER_SIMPLE_REMOTE_XPC_GETTER(controllerNodeID, NSNumber *, nil, controllerNodeIDWithReply)
+
+// Not Supported via XPC
+// - (oneway void)deviceController:(NSUUID *)controller setupCommissioningSessionWithPayload:(MTRSetupPayload *)payload newNodeID:(NSNumber *)newNodeID withReply:(void(^)(BOOL success, NSError * _Nullable error))reply;
+// - (oneway void)deviceController:(NSUUID *)controller setupCommissioningSessionWithDiscoveredDevice:(MTRCommissionableBrowserResult *)discoveredDevice payload:(MTRSetupPayload *)payload newNodeID:(NSNumber *)newNodeID withReply:(void(^)(BOOL success, NSError * _Nullable error))reply;
+// - (oneway void)deviceController:(NSUUID *)controller commissionNodeWithID:(NSNumber *)nodeID commissioningParams:(MTRCommissioningParameters *)commissioningParams withReply:(void(^)(BOOL success, NSError * _Nullable error))reply;
+// - (oneway void)deviceController:(NSUUID *)controller continueCommissioningDevice:(void *)opaqueDeviceHandle ignoreAttestationFailure:(BOOL)ignoreAttestationFailure withReply:(void(^)(BOOL success, NSError * _Nullable error))reply;
+// - (oneway void)deviceController:(NSUUID *)controller cancelCommissioningForNodeID:(NSNumber *)nodeID withReply:(void(^)(BOOL success, NSError * _Nullable error))reply;
+// - (nullable MTRBaseDevice *)deviceController:(NSUUID *)controller deviceBeingCommissionedWithNodeID:(NSNumber *)nodeID error:(NSError * __autoreleasing *)error;
+// - (oneway void)deviceController:(NSUUID *)controller startBrowseForCommissionables:(id<MTRCommissionableBrowserDelegate>)delegate withReply:(void(^)(BOOL success))reply;
+// - (oneway void)deviceController:(NSUUID *)controller stopBrowseForCommissionablesWithReply:(void(^)(BOOL success))reply;
+// - (oneway void)deviceController:(NSUUID *)controller attestationChallengeForDeviceID:(NSNumber *)deviceID withReply:(void(^)(NSData * _Nullable))reply;
+
+//- (oneway void)deviceController:(NSUUID *)controller addServerEndpoint:(MTRServerEndpoint *)endpoint withReply:(void(^)(BOOL success))reply;
+//- (oneway void)deviceController:(NSUUID *)controller removeServerEndpoint:(MTRServerEndpoint *)endpoint;
+
+MTR_DEVICECONTROLLER_SIMPLE_REMOTE_XPC_COMMAND(shutdown, shutdownDeviceController
+                                               : self.uniqueIdentifier)
+
+#pragma mark - MTRDeviceProtocol Client
+
+// All pass through, we could do some fancy redirection here based on protocol, but that's that for another day
+- (oneway void)device:(NSNumber *)nodeID stateChanged:(MTRDeviceState)state
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received device: %@ stateChanged: %lu   found device: %@", nodeID, (unsigned long) state, device);
+    [device device:nodeID stateChanged:state];
+}
+- (oneway void)device:(NSNumber *)nodeID receivedAttributeReport:(NSArray<NSDictionary<NSString *, id> *> *)attributeReport
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received device: %@ receivedAttributeReport: %@     found device: %@", nodeID, attributeReport, device);
+
+    [device device:nodeID receivedAttributeReport:attributeReport];
+}
+- (oneway void)device:(NSNumber *)nodeID receivedEventReport:(NSArray<NSDictionary<NSString *, id> *> *)eventReport
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received device: %@ receivedEventReport: %@     found device: %@", nodeID, eventReport, device);
+
+    [device device:nodeID receivedEventReport:eventReport];
+}
+- (oneway void)deviceBecameActive:(NSNumber *)nodeID
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received deviceBecameActive: %@ found device: %@", nodeID, device);
+
+    [device deviceBecameActive:nodeID];
+}
+- (oneway void)deviceCachePrimed:(NSNumber *)nodeID
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received deviceCachePrimed: %@ found device: %@", nodeID, device);
+
+    [device deviceCachePrimed:nodeID];
+}
+- (oneway void)deviceConfigurationChanged:(NSNumber *)nodeID
+{
+    MTRDevice_XPC * device = (MTRDevice_XPC *) [self deviceForNodeID:nodeID];
+    MTR_LOG("Received deviceConfigurationChanged: %@ found device: %@", nodeID, device);
+
+    [device deviceConfigurationChanged:nodeID];
+}
+
+#pragma mark - MTRDeviceController Protocol Client
+
+// Not Supported via XPC
+//- (oneway void)controller:(NSUUID *)controller statusUpdate:(MTRCommissioningStatus)status {
+//  }
+//- (oneway void)controller:(NSUUID *)controller commissioningSessionEstablishmentDone:(NSError * _Nullable)error {
+//
+//}
+//- (oneway void)controller:(NSUUID *)controller commissioningComplete:(NSError * _Nullable)error nodeID:(NSNumber * _Nullable)nodeID metrics:(MTRMetrics * _Nullable)metrics {
+//
+//}
+//- (oneway void)controller:(NSUUID *)controller readCommissioningInfo:(MTRProductIdentity *)info {
+//
+//}
+
+@end
--- src/darwin/Framework/CHIP/XPC Protocol/MTRXPCClientProtocol.h
+++ src/darwin/Framework/CHIP/XPC Protocol/MTRXPCClientProtocol.h
@@ -14,6 +14,9 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
+#import <Matter/MTRDevice.h> // for MTRDeviceState
+
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol MTRXPCClientProtocol_MTRDevice <NSObject>
