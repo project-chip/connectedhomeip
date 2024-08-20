09c719346d7ba70ed8a9f15e9d1d73daba3dbc8e
Restyled.io <commits@restyled.io>
2024-08-20 12:28:00 +0000

    Restyled by clang-format

--- examples/platform/silabs/efr32/sl_custom_board.h
+++ examples/platform/silabs/efr32/sl_custom_board.h
@@ -11,22 +11,19 @@
 #define WAKE_INDICATOR_PIN PIN(D, 2)
 #ifdef RS911X_WIFI
 // SPI ports and pins
-#define EUS1MOSI_PORT   gpioPortC
-#define EUS1MOSI_PIN    1
-#define EUS1MISO_PORT   gpioPortC
-#define EUS1MISO_PIN    2
-#define EUS1SCLK_PORT   gpioPortC
-#define EUS1SCLK_PIN    3
-#define EUS1CS_PORT     gpioPortC
-#define EUS1CS_PIN      0
-
+#define EUS1MOSI_PORT gpioPortC
+#define EUS1MOSI_PIN 1
+#define EUS1MISO_PORT gpioPortC
+#define EUS1MISO_PIN 2
+#define EUS1SCLK_PORT gpioPortC
+#define EUS1SCLK_PIN 3
+#define EUS1CS_PORT gpioPortC
+#define EUS1CS_PIN 0
 
 #define MY_USART EUSART1
 #define MY_USART_CLOCK cmuClock_EUSART1
-#define MY_USART_TX_SIGNAL                                                     \
-	  dmadrvPeripheralSignal_EUSART1_TXBL
-#define MY_USART_RX_SIGNAL                                                     \
-	  dmadrvPeripheralSignal_EUSART1_RXDATAV
+#define MY_USART_TX_SIGNAL dmadrvPeripheralSignal_EUSART1_TXBL
+#define MY_USART_RX_SIGNAL dmadrvPeripheralSignal_EUSART1_RXDATAV
 
 #define WFX_RESET_PIN PIN(A, 6)
 #define WFX_INTERRUPT_PIN PIN(A, 7)
@@ -34,18 +31,18 @@
 #define WFX_SLEEP_CONFIRM_PIN PIN(D, 2) /* Exp hdr 7 */
 #else
 #define WFX_SLEEP_CONFIRM_PIN PIN(A, 5) /* Exp hdr 7 */
-#endif /* EXP_BOARD */
+#endif                                  /* EXP_BOARD */
 #define SL_WFX_HOST_PINOUT_SPI_IRQ 5
 
 #else /* WF200 */
 
-#define PIN_OUT_SET	    	1
-#define PIN_OUT_CLEAR		0
+#define PIN_OUT_SET 1
+#define PIN_OUT_CLEAR 0
 
-#define MY_USART            USART0
-#define MY_USART_CLOCK      cmuClock_USART0
-#define MY_USART_TX_SIGNAL  dmadrvPeripheralSignal_USART0_TXBL
-#define MY_USART_RX_SIGNAL  dmadrvPeripheralSignal_USART0_RXDATAV
+#define MY_USART USART0
+#define MY_USART_CLOCK cmuClock_USART0
+#define MY_USART_TX_SIGNAL dmadrvPeripheralSignal_USART0_TXBL
+#define MY_USART_RX_SIGNAL dmadrvPeripheralSignal_USART0_RXDATAV
 
 #define SL_WFX_HOST_PINOUT_RESET_PORT gpioPortA
 #define SL_WFX_HOST_PINOUT_RESET_PIN 5
