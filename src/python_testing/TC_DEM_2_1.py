08fa86629a8cf9d51f27fc63c47e53489745bdd9
James Harrow <james.harrow@gmail.com>
2024-08-26 23:05:43 +0100

    Updated test steps to read the FeatureMap and try to align to the code below more closely. Improved / corrected power adjustment checking.

--- src/python_testing/TC_DEM_2_1.py
+++ src/python_testing/TC_DEM_2_1.py
@@ -60,22 +60,24 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
         steps = [
             TestStep("1", "Commissioning, already done",
                      is_commissioning=True),
-            TestStep("2", "TH reads from the DUT ESAType attribute.",
+            TestStep("2", "TH reads from the DUT FeatureMap attribute.",
+                     "Save this for later."),
+            TestStep("3", "TH reads from the DUT ESAType attribute.",
                      "Verify that the DUT response contains an a ESATypeEnum (enum8) value to match the DUT type."),
-            TestStep("3", "TH reads from the DUT the ESACanGenerate attribute.",
+            TestStep("4", "TH reads from the DUT the ESACanGenerate attribute.",
                      "Verify that the DUT response contains a boolean value to match the DUT capability."),
-            TestStep("4", "TH reads from the DUT ESAState attribute.",
+            TestStep("5", "TH reads from the DUT ESAState attribute.",
                      "Verify that the DUT response contains an a ESAStateEnum (enum8) value to match the DUT state (Online or Offline)."),
-            TestStep("5", "TH reads from the DUT the AbsMinPower attribute.",
-                     "Verify that the DUT response contains a power-mW value to match the DUT capability, and is negative if and only if the ESACanGenerate read in step 3 is TRUE."),
-            TestStep("6", "TH reads from the DUT the AbsMaxPower attribute.",
-                     "Verify that the DUT response contains a power-mW value to match the DUT capability, and greater than or equal to the AbsMinPower read in step 5."),
-            TestStep("7", "TH reads from the DUT the PowerAdjustmentCapability attribute.",
-                     "Verify that the DUT response contains a list of PowerAdjustStruct value."),
-            TestStep("8", "TH reads from the DUT the Forecast attribute.",
-                     "Verify that the DUT response contains a ForecastStruct value."),
-            TestStep("9", "TH reads from the DUT the OptOutState attribute.",
-                     "Verify that the DUT response contains an OptOutStateEnum (enum8) value 0 (NoOptOut)."),
+            TestStep("6", "TH reads from the DUT the AbsMinPower attribute.",
+                     "Verify that the DUT response contains a power-mW value to match the DUT capability, and is negative if and only if the ESACanGenerate read in step 4 is TRUE."),
+            TestStep("7", "TH reads from the DUT the AbsMaxPower attribute.",
+                     "Verify that the DUT response contains a power-mW value to match the DUT capability, and greater than or equal to the AbsMinPower read in step 6."),
+            TestStep("8", "TH reads from the DUT the PowerAdjustmentCapability attribute.",
+                     "Verify that the DUT response contains either a null value or a PowerAdjustCapabilityStruct value which contains a list of PowerAdjustStruct values and a valid Cause PowerAdjustReasonEnum (enum8) value."),
+            TestStep("9", "If FeatureMap from step 2 indicates support for PFR or SFR feature, TH reads from the DUT the Forecast attribute.",
+                     "Verify that the DUT response contains either a null value or a valid ForecastStruct value."),
+            TestStep("10", "If FeatureMap from step 2 indicates support for PA, STA, PAU, FA, CON features, TH reads from the DUT the OptOutState attribute.",
+                     "Verify that the DUT response contains an OptOutStateEnum (enum8) value."),
         ]
 
         return steps
@@ -85,14 +87,18 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
         # pylint: disable=too-many-locals, too-many-statements
         """Run the test steps."""
 
+        # Allow some user input steps to be skipped if running under CI
+        self.is_ci = self.check_pics("PICS_SDK_CI_ONLY")
+
         self.step("1")
         # Commission DUT - already done
 
         # Get the feature map for later
+        self.step("2")
         feature_map = await self.read_dem_attribute_expect_success(attribute="FeatureMap")
         logger.info(f"FeatureMap: {feature_map}")
 
-        self.step("2")
+        self.step("3")
         esa_type = await self.read_dem_attribute_expect_success(attribute="ESAType")
         asserts.assert_is_instance(esa_type, Clusters.DeviceEnergyManagement.Enums.ESATypeEnum, "Invalid type for ESAType")
 
@@ -102,7 +108,7 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
                                                      default_value="y")
             asserts.assert_equal(user_response.lower(), "y")
 
-        self.step("3")
+        self.step("4")
         esa_can_generate = await self.read_dem_attribute_expect_success(attribute="ESACanGenerate")
         asserts.assert_is_instance(esa_can_generate, bool)
 
@@ -112,12 +118,12 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
                                                      default_value="y")
             asserts.assert_equal(user_response.lower(), "y")
 
-        self.step("4")
+        self.step("5")
         esa_state = await self.read_dem_attribute_expect_success(attribute="ESAState")
         logger.info(f"ESAState is {esa_state}")
         asserts.assert_is_instance(esa_state, Clusters.DeviceEnergyManagement.Enums.ESAStateEnum)
 
-        self.step("5")
+        self.step("6")
         abs_min_power = await self.read_dem_attribute_expect_success(attribute="AbsMinPower")
         asserts.assert_is_instance(abs_min_power, int)
 
@@ -131,7 +137,7 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
             # ESA's that can't generate must have positive values
             asserts.assert_greater_equal(abs_min_power, 0)
 
-        self.step("6")
+        self.step("7")
         abs_max_power = await self.read_dem_attribute_expect_success(attribute="AbsMaxPower")
         asserts.assert_is_instance(abs_max_power, int)
         asserts.assert_greater_equal(abs_max_power, abs_min_power)
@@ -142,16 +148,25 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
                                                      default_value="y")
             asserts.assert_equal(user_response.lower(), "y")
 
-        self.step("7")
+        self.step("8")
         if Clusters.DeviceEnergyManagement.Bitmaps.Feature.kPowerAdjustment & feature_map:
             power_adjustment_capability = await self.read_dem_attribute_expect_success(attribute="PowerAdjustmentCapability")
             logger.info(f"PowerAdjustmentCapability is {power_adjustment_capability}")
 
             if power_adjustment_capability is not NullValue:
                 asserts.assert_is_instance(power_adjustment_capability,
-                                           Clusters.DeviceEnergyManagement.Structs.PowerAdjustStruct)
+                                           Clusters.DeviceEnergyManagement.Structs.PowerAdjustCapabilityStruct)
 
-        self.step("8")
+                power_adjustment_list = power_adjustment_capability.PowerAdjustCapability
+                if power_adjustment_list is not NullValue:
+                    asserts.assert_is_instance(power_adjustment_list, list)
+                    for entry in power_adjustment_list:
+                        asserts.assert_is_instance(entry, Clusters.DeviceEnergyManagement.Structs.PowerAdjustStruct)
+
+                power_adjustment_cause = power_adjustment_capability.Cause
+                asserts.assert_is_instance(power_adjustment_cause, Clusters.DeviceEnergyManagement.Enums.PowerAdjustReasonEnum)
+
+        self.step("9")
         if Clusters.DeviceEnergyManagement.Bitmaps.Feature.kPowerForecastReporting & feature_map or \
                 Clusters.DeviceEnergyManagement.Bitmaps.Feature.kStateForecastReporting & feature_map:
             forecast = await self.read_dem_attribute_expect_success(attribute="Forecast")
@@ -161,16 +176,14 @@ class TC_DEM_2_1(MatterBaseTest, DEMTestBase):
                 asserts.assert_is_instance(forecast,
                                            Clusters.DeviceEnergyManagement.Structs.ForecastStruct)
 
-        self.step("9")
+        self.step("10")
         if Clusters.DeviceEnergyManagement.Bitmaps.Feature.kPowerAdjustment & feature_map or \
                 Clusters.DeviceEnergyManagement.Bitmaps.Feature.kStartTimeAdjustment & feature_map or \
                 Clusters.DeviceEnergyManagement.Bitmaps.Feature.kPausable & feature_map or \
                 Clusters.DeviceEnergyManagement.Bitmaps.Feature.kForecastAdjustment & feature_map or \
                 Clusters.DeviceEnergyManagement.Bitmaps.Feature.kConstraintBasedAdjustment & feature_map:
             opt_out_state = await self.read_dem_attribute_expect_success(attribute="OptOutState")
-            logger.info(f"OptOutState is {opt_out_state}")
-            asserts.assert_equal(opt_out_state,
-                                 Clusters.DeviceEnergyManagement.Enums.OptOutStateEnum.kNoOptOut)
+            logger.info(f"OptOutState is {opt_out_state.name}:{opt_out_state}")
 
 
 if __name__ == "__main__":
