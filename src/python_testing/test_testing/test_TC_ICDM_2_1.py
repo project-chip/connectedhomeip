ea2de250fcd705680690e654f7b9016ec783558c
Restyled.io <commits@restyled.io>
2024-08-26 20:04:35 +0000

    Restyled by isort

--- src/python_testing/test_testing/test_TC_ICDM_2_1.py
+++ src/python_testing/test_testing/test_TC_ICDM_2_1.py
@@ -16,12 +16,13 @@
 #    limitations under the License.
 #
 
-from MockTestRunner import MockTestRunner
-from chip.clusters import Attribute
-import chip.clusters as Clusters
 import string
 import sys
 from dataclasses import dataclass
+
+import chip.clusters as Clusters
+from chip.clusters import Attribute
+from MockTestRunner import MockTestRunner
 
 # Add the parent directory to sys.path to allow importing
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
