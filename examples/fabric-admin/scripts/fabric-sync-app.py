b0e60d3fe2242c2b209e320e8cb3084a838a4d6c
Arkadiusz Bokowy <a.bokowy@samsung.com>
2024-08-27 14:41:23 +0200

    Terminate apps on SIGTERM and SIGINT

--- examples/fabric-admin/scripts/fabric-sync-app.py
+++ examples/fabric-admin/scripts/fabric-sync-app.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2024 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
 import contextlib
+import signal
 import sys
 import os
-import asyncio
 from argparse import ArgumentParser
 
 
@@ -102,6 +117,14 @@ async def main(args):
 
     if args.storage_dir is not None:
         os.makedirs(args.storage_dir, exist_ok=True)
+
+    def terminate(signum, frame):
+        admin.terminate()
+        bridge.terminate()
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, terminate)
+    signal.signal(signal.SIGTERM, terminate)
 
     admin, bridge = await asyncio.gather(
         run_admin(
