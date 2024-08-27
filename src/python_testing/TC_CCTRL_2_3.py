358ef26407c3433f3d54b9a88b36ac5bf7239280
Yufeng Wang <yufengwang@google.com>
2024-08-27 09:45:39 -0700

    Address review comments

--- src/python_testing/TC_CCTRL_2_3.py
+++ src/python_testing/TC_CCTRL_2_3.py
@@ -169,7 +169,6 @@ class TC_CCTRL_2_3(MatterBaseTest):
 
         self.step(12)
         th_server_fabrics_new = await self.read_single_attribute_check_success(cluster=Clusters.OperationalCredentials, attribute=Clusters.OperationalCredentials.Attributes.Fabrics, dev_ctrl=self.TH_server_controller, node_id=self.server_nodeid, endpoint=0, fabric_filtered=False)
-        # TODO: this should be mocked too.
         if not self.is_ci:
             asserts.assert_equal(len(th_server_fabrics) + 1, len(th_server_fabrics_new),
                                  "Unexpected number of fabrics on TH_SERVER")
