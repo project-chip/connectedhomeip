202148a646df5820f09944283b2bafb127f51e8f
Karsten Sperling <ksperling@apple.com>
2024-08-29 06:42:11 +1200

    Test tweaks to get CI to pass
    - Use pairing payload matching the other parameters
    - Check response of ArmFailSafe commands
    - Fix bad merge in commissioner_commands.py

--- scripts/py_matter_yamltests/matter_yamltests/pseudo_clusters/clusters/commissioner_commands.py
+++ scripts/py_matter_yamltests/matter_yamltests/pseudo_clusters/clusters/commissioner_commands.py
@@ -56,11 +56,6 @@ _DEFINITION = '''<?xml version="1.0"?>
       <arg name="RCAC" type="octet_string"/>
       <arg name="IPK" type="octet_string"/>
     </command>
-  
-    <command source="client" code="5" name="EstablishPASESession">
-      <arg name="nodeId" type="node_id"/>
-      <arg name="payload" type="char_string"/>
-    </command>
 
     <command source="client" code="5" name="EstablishPASESession">
       <arg name="nodeId" type="node_id"/>
