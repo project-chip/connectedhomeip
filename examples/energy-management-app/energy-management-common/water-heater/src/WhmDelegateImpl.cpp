4d1cd683ce01f649f5614f841d75cc3b113ad8b1
Restyled.io <commits@restyled.io>
2024-08-28 09:18:06 +0000

    Restyled by clang-format

--- examples/energy-management-app/energy-management-common/water-heater/src/WhmDelegateImpl.cpp
+++ examples/energy-management-app/energy-management-common/water-heater/src/WhmDelegateImpl.cpp
@@ -355,8 +355,8 @@ void WaterHeaterManagementDelegate::SetTargetWaterTemperature(uint16_t targetWat
 void WaterHeaterManagementDelegate::DrawOffHotWater(Percent percentageReplaced, uint16_t replacedWaterTemperature)
 {
     // First calculate the new average water temperature
-    mWaterTemperature =
-        static_cast<uint16_t>((mWaterTemperature * (100 - percentageReplaced) + replacedWaterTemperature * percentageReplaced) / 100);
+    mWaterTemperature = static_cast<uint16_t>(
+        (mWaterTemperature * (100 - percentageReplaced) + replacedWaterTemperature * percentageReplaced) / 100);
 
     // Replaces percentageReplaced% of the water in the tank with water of a temperature replacedWaterTemperature
     // Only supported if the kTankPercent feature is supported.
