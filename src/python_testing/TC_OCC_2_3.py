781e1aae11fa57041074cfd7d154f0e20782533b
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2024-08-29 18:27:52 -0500

    Update TC_OCC_2_3.py

--- src/python_testing/TC_OCC_2_3.py
+++ src/python_testing/TC_OCC_2_3.py
@@ -148,7 +148,7 @@ class TC_OCC_2_3(MatterBaseTest):
             occupancy_phy_otou_delay_dut = await self.read_occ_attribute_expect_success(attribute=attributes.PhysicalContactOccupiedToUnoccupiedDelay)
             holdtime_dut = await self.read_occ_attribute_expect_success(attribute=attributes.HoldTime)
             asserts.assert_equal(occupancy_phy_otou_delay_dut, holdtime_dut,
-				  "PhysicalContactOccupiedToUnoccupiedDelay has a different value from HoldTime in reverse testing.")
+				 "PhysicalContactOccupiedToUnoccupiedDelay has a different value from HoldTime in reverse testing.")
 
 
 if __name__ == "__main__":
