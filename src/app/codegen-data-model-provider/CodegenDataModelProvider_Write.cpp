ba6725deb7b70d55b992738b0a72c39c95d97220
Thomas Lea <thomas_lea@comcast.com>
2024-08-29 06:57:09 -0500

    fix return type in CodegenDataModelProvider_Write.cpp

--- src/app/codegen-data-model-provider/CodegenDataModelProvider_Write.cpp
+++ src/app/codegen-data-model-provider/CodegenDataModelProvider_Write.cpp
@@ -290,8 +290,7 @@ DataModel::ActionReturnStatus CodegenDataModelProvider::WriteAttribute(const Dat
             ReturnErrorCodeIf(err != CHIP_ERROR_ACCESS_DENIED && err != CHIP_ERROR_ACCESS_RESTRICTED_BY_ARL, err);
 
             // TODO: when wildcard/group writes are supported, handle them to discard rather than fail with status
-            return err == CHIP_ERROR_ACCESS_DENIED ? CHIP_IM_GLOBAL_STATUS(UnsupportedAccess)
-                                                   : CHIP_IM_GLOBAL_STATUS(AccessRestricted);
+            return err == CHIP_ERROR_ACCESS_DENIED ? Status::UnsupportedAccess : Status::AccessRestricted;
         }
     }
 
