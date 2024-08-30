ce258c0b6c05586d33859bc30cdf7c60e66c9b0d
Alex Tsitsiura <s07641069@gmail.com>
2024-08-30 15:53:35 +0300

    [Telink] Add TL3218X sceleton

--- scripts/build/build/targets.py
+++ scripts/build/build/targets.py
@@ -770,6 +770,8 @@ def BuildTelinkTarget():
         TargetPart('tlsr9528a_retention', board=TelinkBoard.TLSR9528A_RETENTION),
         TargetPart('tlsr9258a', board=TelinkBoard.TLSR9258A),
         TargetPart('tlsr9258a_retention', board=TelinkBoard.TLSR9258A_RETENTION),
+        TargetPart('tl3218x', board=TelinkBoard.TL3218X),
+        TargetPart('tl3218x_retention', board=TelinkBoard.TL3218X_RETENTION),
     ])
 
     target.AppendFixedTargets([
