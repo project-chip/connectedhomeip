eead0bc0665d22844b2a50a14ddb62c73884fbc8
William <hicklin@users.noreply.github.com>
2024-08-30 17:28:00 +0100

    Apply suggestions from code review
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/clusters/service-area-server/service-area-delegate.h
+++ src/app/clusters/service-area-server/service-area-delegate.h
@@ -55,8 +55,8 @@ public:
      * @brief Can the selected locations be set by the client in the current operating mode?
      * @param[out] statusText text describing why the selected locations cannot be set (if return is false).
      * Max size kMaxSizeStatusText.
-     * Note: If the return is false and statusText is not successfully edited, example due to a string that can be longer than
-     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid memory leaks.
+     * Note: If the return is false and statusText is not successfully set, for example due to a string that can be longer than
+     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid callers using un-initialized memory.
      * @return true if the current device state allows selected locations to be set by user.
      *
      * @note The statusText field SHOULD indicate why the request is not allowed, given the current mode
@@ -76,8 +76,8 @@ public:
      * @param[in] selectedAreas List of new selected locations.
      * @param[out] locationStatus Success if all checks pass, error code if failure.
      * @param[out] statusText text describing failure (see description above). Max size kMaxSizeStatusText.
-     * Note: If the return is false and statusText is not successfully edited, example due to a string that can be longer than
-     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid memory leaks.
+     * Note: If the return is false and statusText is not successfully set, for example due to a string that can be longer than
+     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid callers using un-initialized memory.
      * @return true if success.
      *
      * @note If the SelectAreas command is allowed when the device is operating and the selected locations change to none, the
@@ -91,8 +91,8 @@ public:
      * calling this method.
      * @param[in] skippedArea the area ID to skip.
      * @param[out] skipStatusText text describing why the current location cannot be skipped. Max size kMaxSizeStatusText.
-     * Note: If the return is false and skipStatusText is not successfully edited, example due to a string that can be longer than
-     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid memory leaks.
+     * Note: If the return is false and skipStatusText is not successfully set, for example due to a string that can be longer than
+     * kMaxSizeStatusText, the size of this value should be set to 0 with .reduce_size(0) to avoid callers using un-initialized memory.
      * @return true if command is successful, false if the received skip request cannot be handled due to the current mode of the
      * device.
      *
