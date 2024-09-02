dd94fbc6398dd37891faa8d17e3b64aaa31c338c
Rohan Sahay <rohan.sahay@silabs.com>
2024-09-02 15:45:53 +0530

    Sanitize provisioning WiFi network credentials

--- src/platform/silabs/SiWx917/wifi/wfx_host_events.h
+++ src/platform/silabs/SiWx917/wifi/wfx_host_events.h
@@ -159,7 +159,9 @@ typedef enum
 typedef struct
 {
     char ssid[WFX_MAX_SSID_LENGTH + 1];
+    size_t ssid_length;
     char passkey[WFX_MAX_PASSKEY_LENGTH + 1];
+    size_t passkey_length;
     wfx_sec_t security;
 } wfx_wifi_provision_t;
 
