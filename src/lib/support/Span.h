11401a00da493aaaed8fcfb2e03cd12172b18e57
William Hicklin <venturehicklin@gmail.com>
2024-09-02 16:26:24 +0100

    Added a CopyCharSpanToMutableCharSpanTruncate function.

--- src/lib/support/Span.h
+++ src/lib/support/Span.h
@@ -390,4 +390,18 @@ inline CHIP_ERROR CopyCharSpanToMutableCharSpan(CharSpan cspan_to_copy, MutableC
     return CHIP_NO_ERROR;
 }
 
+/**
+ * Copies a CharSpan into a MutableCharSpan.
+ * If the cspan_to_copy does not if in out_buf, cspan_to_copy in truncated to fit in out_buf.
+ * @param cspan_to_copy The CharSpan to copy.
+ * @param out_buf The MutableCharSpan in which cspan_to_copy is to be copied.
+ */
+inline void CopyCharSpanToMutableCharSpanTruncate(CharSpan cspan_to_copy, MutableCharSpan & out_buf)
+{
+    size_t size_to_copy = std::min(cspan_to_copy.size(), out_buf.size());
+
+    memcpy(out_buf.data(), cspan_to_copy.data(), size_to_copy);
+    out_buf.reduce_size(size_to_copy);
+}
+
 } // namespace chip
