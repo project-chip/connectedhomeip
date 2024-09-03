d7e1eb475ffb8de4d103d6f6cf5528c94625f4e4
cecille <cecille@google.com>
2024-09-03 17:50:48 -0400

    New DM XML drop from Sept 3

--- src/python_testing/spec_parsing_support.py
+++ src/python_testing/spec_parsing_support.py
@@ -398,6 +398,8 @@ class ClusterParser:
                 return CommandType.UNKNOWN
             if element.attrib['direction'].lower() == 'commandtoserver':
                 return CommandType.ACCEPTED
+            if element.attrib['direction'].lower() == 'responsefromclient':
+                return CommandType.UNKNOWN
             raise Exception(f"Unknown direction: {element.attrib['direction']}")
         except KeyError:
             return CommandType.UNKNOWN
