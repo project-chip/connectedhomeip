9c447eae3a02fd5ab4d68afcfc1e1c938f87fd40
René Josefsen <rjosefsen@grundfos.com>
2024-09-04 11:07:14 +0200

    Remove unused import

--- src/python_testing/TC_CC_2_2.py
+++ src/python_testing/TC_CC_2_2.py
@@ -35,7 +35,7 @@ from chip.clusters import ClusterObjects as ClusterObjects
 from matter_testing_support import (ClusterAttributeChangeAccumulator, MatterBaseTest, TestStep, default_matter_test_main,
                                     has_cluster, run_if_endpoint_matches)
 from mobly import asserts
-from test_plan_support import commission_if_required, if_feature_supported, read_attribute, verify_success
+from test_plan_support import commission_if_required, read_attribute, verify_success
 
 
 class TC_CC_2_3(MatterBaseTest):
