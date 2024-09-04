81ab73df4edd8437df33cc793dbae8036dd62d82
Andrei Litvin <andy314@gmail.com>
2024-09-04 17:33:58 -0400

    Restyle and update the image to 22.04 since this is what we use today

--- integrations/docker/images/stage-1/chip-build-crosscompile/start-sysroot-vm.sh
+++ integrations/docker/images/stage-1/chip-build-crosscompile/start-sysroot-vm.sh
@@ -11,38 +11,38 @@
 set -e
 
 if ! which qemu-system-aarch64 >/dev/null; then
-  echo "Cannot find 'qemu-system-aarch64'. Did you 'sudo apt-get install qemu-system-arm' ?"
-  exit 1
+    echo "Cannot find 'qemu-system-aarch64'. Did you 'sudo apt-get install qemu-system-arm' ?"
+    exit 1
 fi
 
 if ! which virt-install >/dev/null; then
-  echo "Cannot find 'virt-install'. Did you 'sudo apt-get install virtinst' ?"
-  exit 1
+    echo "Cannot find 'virt-install'. Did you 'sudo apt-get install virtinst' ?"
+    exit 1
 fi
 
 if ! [ -f /usr/sbin/libvirtd ]; then
-  echo "Cannot find 'virt-install'. Did you 'sudo apt-get install libvirt-daemon' ?"
-  exit 1
+    echo "Cannot find 'virt-install'. Did you 'sudo apt-get install libvirt-daemon' ?"
+    exit 1
 fi
 
 if ! which qemu-img >/dev/null; then
-  echo "Cannot find 'qemu-img'."
-  exit 1
+    echo "Cannot find 'qemu-img'."
+    exit 1
 fi
 
 if ! which genisoimage >/dev/null; then
-  echo "Cannot find 'genisoimage'."
-  exit 1
+    echo "Cannot find 'genisoimage'."
+    exit 1
 fi
 
 CLOUD_IMAGE_URL="https://cloud-images.ubuntu.com/releases/24.04/release-20240821/ubuntu-24.04-server-cloudimg-arm64.img"
 CLOUD_IMAGE_FILE=$(basename "$CLOUD_IMAGE_URL")
 
 if ! [ -f "$CLOUD_IMAGE_FILE" ]; then
-  echo "Image $CLOUD_IMAGE_FILE does not exist. Downloading ..."
-  wget "$CLOUD_IMAGE_URL"
+    echo "Image $CLOUD_IMAGE_FILE does not exist. Downloading ..."
+    wget "$CLOUD_IMAGE_URL"
 else
-  echo "Using existing $CLOUD_IMAGE_FILE ..."
+    echo "Using existing $CLOUD_IMAGE_FILE ..."
 fi
 
 rm -f sysrootsrv.img
@@ -51,7 +51,7 @@ qemu-img create -b "$CLOUD_IMAGE_FILE" -f qcow2 -F qcow2 sysrootsrv.img 10G
 # local user allowed to SSH in to the host directly without a password
 SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
 
-# set a login password 
+# set a login password
 PASSWORD_HASH=$(mkpasswd 1234)
 
 # Create cloud init files
@@ -95,7 +95,7 @@ packages:
   - unzip
 
 runcmd:
-  # Ubuntu cloud-init merges with a locked ubuntu, so the `lock_passwd` option does
+  # Ubuntu cloud-init merges with a locked ubuntu, so the $(lock_passwd) option does
   # not actually work. Unlock it here to make console work
   - [ passwd, --unlock, ubuntu ]
   # A marker saying that initial install is done. Installing all the packages above is VERY slow
@@ -104,7 +104,7 @@ runcmd:
 TEXTEND
 
 truncate -s 0 meta-data
-echo 'instance-id: sysrootsrv'    >>meta-data
+echo 'instance-id: sysrootsrv' >>meta-data
 echo 'local-hostname: sysrootsrv' >>meta-data
 
 genisoimage -output cidata.iso -V cidata -r -J user-data meta-data
@@ -133,7 +133,6 @@ genisoimage -output cidata.iso -V cidata -r -J user-data meta-data
 #     -device virtio-net-pci,netdev=netdev0                \
 #     ; # last line
 
-
 # EXAMPLE COMMANDS:
 #
 # View generated image:
@@ -144,20 +143,19 @@ genisoimage -output cidata.iso -V cidata -r -J user-data meta-data
 #
 # Delete:
 #   virsh destroy sysrootsrv && virsh undefine --nvram sysrootsrv
-virt-install                                         \
-  --name sysrootsrv                                  \
-  --memory 8192                                      \
-  --arch aarch64                                     \
-  --import                                           \
-  --disk path=sysrootsrv.img,format=qcow2            \
-  --disk path=cidata.iso,device=cdrom                \
-  --os-variant=ubuntu24.04                           \
-  --network=user                                     \
-  --xml "xpath.delete=./devices/input[@bus = 'usb']" \
-  --noautoconsole                                    \
-  ; # last-line
+virt-install \
+    --name sysrootsrv \
+    --memory 8192 \
+    --arch aarch64 \
+    --import \
+    --disk path=sysrootsrv.img,format=qcow2 \
+    --disk path=cidata.iso,device=cdrom \
+    --os-variant=ubuntu24.04 \
+    --network=user \
+    --xml "xpath.delete=./devices/input[@bus = 'usb']" \
+    --noautoconsole \
+    ;# last-line
 
 # Allow ssh via:
 #   ssh ubuntu@localhost -p 5555
 virsh qemu-monitor-command --hmp sysrootsrv 'hostfwd_add ::5555-:22'
-
