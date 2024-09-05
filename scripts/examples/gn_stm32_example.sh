dd5c3f22480b71f6caf30e90d6f966b5f5117c36
Restyled.io <commits@restyled.io>
2024-09-05 14:18:49 +0000

    Restyled by shfmt

--- scripts/examples/gn_stm32_example.sh
+++ scripts/examples/gn_stm32_example.sh
@@ -122,49 +122,49 @@ else
     shift
     while [ $# -gt 0 ]; do
         case $1 in
-        --wifi)
-            if [ -z "$2" ]; then
-                echo "--wifi requires mxchip"
-                exit 1
-            fi
-            if [ "$2" = "mxchip" ]; then
-                optArgs+="use_mxchip=true"
-            else
-                echo "Wifi usage: --wifi mxchip"
-                exit 1
-            fi
-            USE_WIFI=true
-            shift
-            shift
-            ;;
-        --sed)
-            optArgs+="enable_sleepy_device=true chip_openthread_ftd=false "
-            shift
-            ;;
-        --chip_enable_wifi_ipv4)
-            optArgs+="chip_enable_wifi_ipv4=true "
-            shift
-            ;;
-        --additional_data_advertising)
-            optArgs+="chip_enable_additional_data_advertising=true chip_enable_rotating_device_id=true "
-            shift
-            ;;
-        --use_ot_lib)
-            optArgs+="use_st_thread_lib=true chip_openthread_target=$ST_THREAD_TARGET openthread_external_platform=\"""\" "
-            shift
-            ;;
-        --use_ot_coap_lib)
-            optArgs+="use_st_thread_lib=true chip_openthread_target=$ST_THREAD_TARGET openthread_external_platform=\"""\" use_thread_coap_lib=true "
-            shift
-            ;;
-        *)
-            if [ "$1" =~ *"use_mxchip=true"* ]; then
+            --wifi)
+                if [ -z "$2" ]; then
+                    echo "--wifi requires mxchip"
+                    exit 1
+                fi
+                if [ "$2" = "mxchip" ]; then
+                    optArgs+="use_mxchip=true"
+                else
+                    echo "Wifi usage: --wifi mxchip"
+                    exit 1
+                fi
                 USE_WIFI=true
-            fi
+                shift
+                shift
+                ;;
+            --sed)
+                optArgs+="enable_sleepy_device=true chip_openthread_ftd=false "
+                shift
+                ;;
+            --chip_enable_wifi_ipv4)
+                optArgs+="chip_enable_wifi_ipv4=true "
+                shift
+                ;;
+            --additional_data_advertising)
+                optArgs+="chip_enable_additional_data_advertising=true chip_enable_rotating_device_id=true "
+                shift
+                ;;
+            --use_ot_lib)
+                optArgs+="use_st_thread_lib=true chip_openthread_target=$ST_THREAD_TARGET openthread_external_platform=\"""\" "
+                shift
+                ;;
+            --use_ot_coap_lib)
+                optArgs+="use_st_thread_lib=true chip_openthread_target=$ST_THREAD_TARGET openthread_external_platform=\"""\" use_thread_coap_lib=true "
+                shift
+                ;;
+            *)
+                if [ "$1" =~ *"use_mxchip=true"* ]; then
+                    USE_WIFI=true
+                fi
 
-            optArgs+=$1" "
-            shift
-            ;;
+                optArgs+=$1" "
+                shift
+                ;;
         esac
     done
 
