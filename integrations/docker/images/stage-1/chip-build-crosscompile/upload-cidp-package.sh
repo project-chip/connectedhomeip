4572b32bd17538c7469a64559d7f37a4709918fd
Andrei Litvin <andreilitvin@google.com>
2024-09-05 13:45:36 -0400

    Update tag to be unique, since I was missing a symlink before and that seemed more broken

--- integrations/docker/images/stage-1/chip-build-crosscompile/upload-cidp-package.sh
+++ integrations/docker/images/stage-1/chip-build-crosscompile/upload-cidp-package.sh
@@ -29,12 +29,13 @@ rsync \
     --exclude='lib/modules' \
     --exclude='lib/ssl/private' \
     --exclude='lib/systemd' \
-    ubuntu@localhost:/lib \
     ubuntu@localhost:/usr/include \
     ubuntu@localhost:/usr/lib \
-    "$OUTPUT_DIR"
+    "$OUTPUT_DIR/usr"
 
-TODAY=$(date '+%Y.%m.%d')
+# Ubuntu 24.04 lib is just a symlink
+rm -f "$OUTPUT_DIR/lib"
+ln -s usr/lib "$OUTPUT_DIR/lib"
 
 echo "Creating cipd definition"
 cat >cipd.yaml <<TEXTEND
@@ -45,10 +46,17 @@ data:
   - dir: "$OUTPUT_DIR"
 TEXTEND
 
+TODAY=$(date '+%Y.%m.%d')
+PACKAGE_VERSION="build-$TODAY"
+
+
 # NOTE: sufficient permissions are required to be able to upload this.
 #       Login is a prerequisite:
 #
 #    cipd auth-login
 #
-echo "Uploading package tagged 'v$TODAY' to $PACKAGE ..."
-cipd create -pkg-def=cipd.yaml -tag "version:v$TODAY"
+echo "Uploading package tagged '$PACKAGE_VERSION' to $PACKAGE ..."
+cipd create -pkg-def=cipd.yaml -tag "version:$PACKAGE_VERSION"
+
+echo "Updating version to latest"
+cipd set-ref $PACKAGE -version "version:$PACKAGE_VERSION" -ref latest
