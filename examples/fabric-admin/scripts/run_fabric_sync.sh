994d2110e622fe40aa59c007db3b008ee2f40c3c
Yufeng Wang <yufengwang@google.com>
2024-09-06 11:29:15 -0700

    Align the build images with CI

--- examples/fabric-admin/scripts/run_fabric_sync.sh
+++ examples/fabric-admin/scripts/run_fabric_sync.sh
@@ -14,7 +14,9 @@ DEFAULT_BRIDGE_CHOICES=(
     "./fabric-bridge-app"
     "out/debug/standalone/fabric-bridge-app"
     "out/linux-x64-fabric-bridge-rpc/fabric-bridge-app"
+    "out/linux-x64-fabric-bridge-rpc-no-ble/fabric-bridge-app"    
     "out/darwin-arm64-fabric-bridge-rpc/fabric-bridge-app"
+    "out/darwin-arm64-fabric-bridge-rpc-no-ble/fabric-bridge-app"    
 )
 FABRIC_ADMIN_LOG="/tmp/fabric_admin.log"
 FABRIC_BRIDGE_APP_LOG="/tmp/fabric_bridge_app.log"
