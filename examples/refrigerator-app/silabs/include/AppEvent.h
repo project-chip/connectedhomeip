d049031bdcb798f5eac5197044cd39e4f807dd61
shgutte <102281713+shgutte@users.noreply.github.com>
2024-09-12 15:26:07 +0530

    Updated task name and removed the incorrect data

--- examples/refrigerator-app/silabs/include/AppEvent.h
+++ examples/refrigerator-app/silabs/include/AppEvent.h
@@ -1,7 +1,6 @@
 /*
  *
  *    Copyright (c) 2024 Project CHIP Authors
- *    Copyright (c) 2018 Nest Labs, Inc.
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
