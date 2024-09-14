7fddbd946029a7261710363d4df01d011d1f8768
shgutte <102281713+shgutte@users.noreply.github.com>
2024-09-14 23:48:24 +0530

    Added changes for authors

--- examples/refrigerator-app/silabs/include/RefrigeratorManager.h
+++ examples/refrigerator-app/silabs/include/RefrigeratorManager.h
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2019 Google LLC.
+ *    Copyright (c) 2024 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
