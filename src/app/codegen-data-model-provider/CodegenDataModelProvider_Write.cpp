029f628275523ce595c8f1237a3dcfbbe3fa110e
Andrei Litvin <andy314@gmail.com>
2024-09-16 09:03:49 -0400

    Update src/app/codegen-data-model-provider/CodegenDataModelProvider_Write.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/codegen-data-model-provider/CodegenDataModelProvider_Write.cpp
+++ src/app/codegen-data-model-provider/CodegenDataModelProvider_Write.cpp
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "app/util/attribute-storage.h"
+#include <app/util/attribute-storage.h>
 #include <app/codegen-data-model-provider/CodegenDataModelProvider.h>
 
 #include <access/AccessControl.h>
