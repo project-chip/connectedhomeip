3ae7999c692e0aa28d58d3047ad7b3fd5b803d2f
Rohit Jadhav <rohit.jadhav@espressif.com>
2024-09-17 17:02:29 +0530

    Some example changes

--- examples/bridge-app/telink/src/DeviceCallbacks.cpp
+++ examples/bridge-app/telink/src/DeviceCallbacks.cpp
@@ -99,7 +99,7 @@ CHIP_ERROR ActionsAttrAccess::Read(const ConcreteReadAttributePath & aPath, Attr
 }
 } // anonymous namespace
 
-void MatterActionsPluginServerInitCallback(void)
-{
-    AttributeAccessInterfaceRegistry::Instance().Register(&gAttrAccess);
-}
+//void MatterActionsPluginServerInitCallback(void)
+//{
+//    AttributeAccessInterfaceRegistry::Instance().Register(&gAttrAccess);
+//}
