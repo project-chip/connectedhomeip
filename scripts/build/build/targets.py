d0b3ca01f879d08b7aa34a709a0112ef115a0db4
Sarthak Shaha <sarthak.shaha@silabs.com>
2024-09-17 14:43:23 -0400

    added build for app

--- scripts/build/build/targets.py
+++ scripts/build/build/targets.py
@@ -266,7 +266,8 @@ def BuildEfr32Target():
         TargetPart('light', app=Efr32App.LIGHT),
         TargetPart('lock', app=Efr32App.LOCK),
         TargetPart('thermostat', app=Efr32App.THERMOSTAT),
-        TargetPart('pump', app=Efr32App.PUMP)
+        TargetPart('pump', app=Efr32App.PUMP),
+        TargetPart('air-quality-sensor-app', app=Efr32App.AIR_QUALITY_SENSOR)
     ])
 
     target.AppendModifier('rpc', enable_rpcs=True)
