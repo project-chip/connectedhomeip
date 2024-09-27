6be91cd58b735e0073e91e76ecb5c14bfa42e37d
Cecille Freeman <cecille@google.com>
2024-09-27 10:19:18 -0700

    Add taglists to distinguish endpoints

--- examples/valve-app/linux/main.cpp
+++ examples/valve-app/linux/main.cpp
@@ -20,6 +20,7 @@
 #include <app/clusters/valve-configuration-and-control-server/valve-configuration-and-control-cluster-logic.h>
 #include <app/clusters/valve-configuration-and-control-server/valve-configuration-and-control-delegate.h>
 #include <app/clusters/valve-configuration-and-control-server/valve-configuration-and-control-server-disco.h>
+#include <app/util/attribute-storage.h>
 #include <lib/support/logging/CHIPLogging.h>
 #include <platform/KvsPersistentStorageDelegate.h>
 
@@ -89,6 +90,76 @@ NonLevelValveEndpoint ep3(3);
 NonLevelValveEndpoint ep4(4);
 NonLevelValveEndpoint ep5(5);
 NonLevelValveEndpoint ep6(6);
+
+// from https://github.com/CHIP-Specifications/connectedhomeip-spec/blob/master/src/namespaces/Namespace-Common-Number.adoc
+constexpr const uint8_t kNamespaceCommonNumber      = 0x7;
+constexpr const uint8_t kNamespaceCommonPosition    = 0x8;
+constexpr const uint8_t kNamespaceCommonPositionRow = 0x5;
+constexpr const uint8_t kLiquidIdentificationTag    = 0xAA;
+
+// TODO: Pull this from the CMD line - would need to be non-const, but that's fine
+// This wouldn't work on an embedded device though, so leaving for now.
+constexpr const char kEp1Label[] = "Bourbon";
+constexpr const char kEp2Label[] = "Campari";
+constexpr const char kEp3Label[] = "Vermouth";
+constexpr const char kEp4Label[] = "Gin";
+constexpr const char kEp5Label[] = "Sour mix";
+constexpr const char kEp6Label[] = "Simple syrup";
+
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp1TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 1 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("1"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp1Label))) },
+};
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp2TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 2 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("2"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp2Label))) },
+};
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp3TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 3 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("3"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp3Label))) },
+};
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp4TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 4 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("4"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp4Label))) },
+};
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp5TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 5 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("5"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp5Label))) },
+};
+const Clusters::Descriptor::Structs::SemanticTagStruct::Type gEp6TagList[] = {
+    { .namespaceID = kNamespaceCommonNumber, .tag = 6 },
+    { .namespaceID = kNamespaceCommonPosition,
+      .tag         = kNamespaceCommonPositionRow,
+      .label       = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>("6"_span)) },
+    { .mfgCode = DataModel::Nullable<chip::VendorId>(chip::VendorId::Google),
+      .tag     = kLiquidIdentificationTag,
+      .label   = chip::MakeOptional(DataModel::Nullable<chip::CharSpan>(CharSpan::fromCharString(kEp6Label))) },
+};
 } // namespace
 
 void ApplicationInit()
@@ -100,6 +171,13 @@ void ApplicationInit()
     ep4.Init();
     ep5.Init();
     ep6.Init();
+    // TODO: Can we pull these from the command line or something so these can be swapped on the fly?
+    SetTagList(/* endpoint= */ 1, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp1TagList));
+    SetTagList(/* endpoint= */ 2, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp2TagList));
+    SetTagList(/* endpoint= */ 3, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp3TagList));
+    SetTagList(/* endpoint= */ 4, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp4TagList));
+    SetTagList(/* endpoint= */ 5, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp5TagList));
+    SetTagList(/* endpoint= */ 6, Span<const Clusters::Descriptor::Structs::SemanticTagStruct::Type>(gEp6TagList));
 }
 
 void ApplicationShutdown()
