71119b52e6bed715d6835efa9ceb390bd2fc024c
Restyled.io <commits@restyled.io>
2024-09-27 12:35:48 +0000

    Restyled by autopep8

--- src/python_testing/TC_CADMIN_1_3.py
+++ src/python_testing/TC_CADMIN_1_3.py
@@ -41,7 +41,8 @@ from time import sleep
 
 opcreds = Clusters.OperationalCredentials
 nonce = random.randbytes(32)
-    
+
+
 class TC_CADMIN_1_3(MatterBaseTest):
     async def CommissionAttempt(
             self, setupPinCode: int, thnum: int, th: str, fail: bool):
@@ -65,7 +66,7 @@ class TC_CADMIN_1_3(MatterBaseTest):
             await th.CommissionOnNetwork(
                 nodeId=self.dut_node_id, setupPinCode=setupPinCode,
                 filterType=ChipDeviceCtrl.DiscoveryFilterType.LONG_DISCRIMINATOR, filter=self.discriminator)
-        
+
     async def get_fabrics(self, th: ChipDeviceCtrl) -> int:
         OC_cluster = Clusters.OperationalCredentials
         fabric_info = await self.read_single_attribute_check_success(dev_ctrl=th, fabric_filtered=True, cluster=OC_cluster, attribute=OC_cluster.Attributes.Fabrics)
@@ -80,8 +81,8 @@ class TC_CADMIN_1_3(MatterBaseTest):
     async def get_txt_record(self):
         discovery = mdns_discovery.MdnsDiscovery(verbose_logging=True)
         comm_service = await discovery.get_commissionable_service(
-            discovery_timeout_sec = 240,
-            log_output = False,
+            discovery_timeout_sec=240,
+            log_output=False,
         )
         return comm_service
 
@@ -121,28 +122,29 @@ class TC_CADMIN_1_3(MatterBaseTest):
         return [
             TestStep(1, "TH_CR1 starts a commissioning process with DUT_CE", is_commissioning=True),
             TestStep(2, "TH_CR1 reads the BasicCommissioningInfo attribute from the General Commissioning cluster and saves the MaxCumulativeFailsafeSeconds field as max_window_duration."),
-            TestStep("3a", "TH_CR1 opens a commissioning window on DUT_CE using a commissioning timeout of max_window_duration using ECM", 
-                    "DUT_CE opens its Commissioning window to allow a second commissioning."),
+            TestStep("3a", "TH_CR1 opens a commissioning window on DUT_CE using a commissioning timeout of max_window_duration using ECM",
+                     "DUT_CE opens its Commissioning window to allow a second commissioning."),
             TestStep("3b", "DNS-SD records shows DUT_CE advertising", "Verify that the DNS-SD advertisement shows CM=2"),
             TestStep("3c", "TH_CR1 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE",
-                    "Verify DUT_CE responds to both write/read with a success"),
+                     "Verify DUT_CE responds to both write/read with a success"),
             TestStep(4, "TH creates a controller (TH_CR2) on a new fabric and commissions DUT_CE using that controller. TH_CR2 should commission the device using a different NodeID than TH_CR1.",
-                    "Commissioning is successful"),
-            TestStep(5, "TH_CR1 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read", 
-                    "Verify that the RootPublicKey matches the root public key for TH_CR1 and the NodeID matches the node ID used when TH_CR1 commissioned the device."),
-            TestStep(6, "TH_CR2 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read", 
-                    "Verify that the RootPublicKey matches the root public key for TH_CR2 and the NodeID matches the node ID used when TH_CR2 commissioned the device."),
+                     "Commissioning is successful"),
+            TestStep(5, "TH_CR1 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read",
+                     "Verify that the RootPublicKey matches the root public key for TH_CR1 and the NodeID matches the node ID used when TH_CR1 commissioned the device."),
+            TestStep(6, "TH_CR2 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read",
+                     "Verify that the RootPublicKey matches the root public key for TH_CR2 and the NodeID matches the node ID used when TH_CR2 commissioned the device."),
             TestStep(7, "TH_CR1 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE",
-                    "Verify DUT_CE responds to both write/read with a success"),
+                     "Verify DUT_CE responds to both write/read with a success"),
             TestStep(8, "TH_CR2 reads, writes and then reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE",
-                    "Verify the initial read reflect the value written in the above step. Verify DUT_CE responds to both write/read with a success"),
+                     "Verify the initial read reflect the value written in the above step. Verify DUT_CE responds to both write/read with a success"),
             TestStep(9, "TH_CR2 opens a commissioning window on DUT_CE for 180 seconds using ECM"),
             TestStep(10, "Wait for the commissioning window in step 9 to timeout"),
-            TestStep(11, "TH_CR2 reads the window status to verify the DUT_CE window is closed", "DUT_CE windows status shows the window is closed"),
-            TestStep(12, "TH_CR2 opens a commissioning window on DUT_CE using ECM", 
-                    "DUT_CE opens its Commissioning window to allow a new commissioning"),
-            TestStep(13, "TH_CR1 starts a commissioning process with DUT_CE before the timeout from step 12", 
-                    "Since DUT_CE was already commissioned by TH_CR1 in step 1, AddNOC fails with NOCResponse with StatusCode field set to FabricConflict (9)"),
+            TestStep(11, "TH_CR2 reads the window status to verify the DUT_CE window is closed",
+                     "DUT_CE windows status shows the window is closed"),
+            TestStep(12, "TH_CR2 opens a commissioning window on DUT_CE using ECM",
+                     "DUT_CE opens its Commissioning window to allow a new commissioning"),
+            TestStep(13, "TH_CR1 starts a commissioning process with DUT_CE before the timeout from step 12",
+                     "Since DUT_CE was already commissioned by TH_CR1 in step 1, AddNOC fails with NOCResponse with StatusCode field set to FabricConflict (9)"),
         ]
 
     @async_test_body
@@ -151,7 +153,7 @@ class TC_CADMIN_1_3(MatterBaseTest):
 
         # Establishing TH1
         self.th1 = self.default_controller
-        
+
         self.step(2)
         GC_cluster = Clusters.GeneralCommissioning
         attribute = GC_cluster.Attributes.BasicCommissioningInfo
@@ -181,10 +183,10 @@ class TC_CADMIN_1_3(MatterBaseTest):
         await self.CommissionAttempt(setupPinCode, th=self.th2, fail=False, thnum=2)
 
         self.step(5)
-        #TH_CR1 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read
+        # TH_CR1 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read
         th1_fabric_info = await self.get_fabrics(th=self.th1)
 
-        #Verify that the RootPublicKey matches the root public key for TH_CR1 and the NodeID matches the node ID used when TH_CR1 commissioned the device.
+        # Verify that the RootPublicKey matches the root public key for TH_CR1 and the NodeID matches the node ID used when TH_CR1 commissioned the device.
         await self.send_single_cmd(dev_ctrl=self.th1, node_id=self.dut_node_id, cmd=Clusters.GeneralCommissioning.Commands.ArmFailSafe(10))
         th1_rcac_decoded = await self.get_rcac_decoded(th=self.th1)
         if th1_fabric_info[0].rootPublicKey != th1_rcac_decoded[9]:
@@ -196,43 +198,43 @@ class TC_CADMIN_1_3(MatterBaseTest):
         await self.th1.SendCommand(self.dut_node_id, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(0))
 
         self.step(6)
-        #TH_CR2 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read
+        # TH_CR2 reads the Fabrics attribute from the Node Operational Credentials cluster using a fabric-filtered read
         th2_fabric_info = await self.get_fabrics(th=self.th2)
 
-        #Verify that the RootPublicKey matches the root public key for TH_CR2 and the NodeID matches the node ID used when TH_CR2 commissioned the device.
+        # Verify that the RootPublicKey matches the root public key for TH_CR2 and the NodeID matches the node ID used when TH_CR2 commissioned the device.
         await self.send_single_cmd(dev_ctrl=self.th2, node_id=self.dut_node_id, cmd=Clusters.GeneralCommissioning.Commands.ArmFailSafe(self.max_window_duration))
         th2_rcac_decoded = await self.get_rcac_decoded(th=self.th2)
         if th2_fabric_info[0].rootPublicKey != th2_rcac_decoded[9]:
             asserts.fail(f"public keys from fabric and certs for TH2 are not the same")
-        if th2_fabric_info[0].nodeID != self.dut_node_id :
+        if th2_fabric_info[0].nodeID != self.dut_node_id:
             asserts.fail(f"DUT node ID from fabric does not match DUT node ID for TH2 during commissioning")
 
         await self.th2.SendCommand(self.dut_node_id, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(0))
 
         self.step(7)
-        #TH_CR1 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE
+        # TH_CR1 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE
         await self.write_nl_attr(th=self.th1, attr_val=nl_attribute)
         await self.read_nl_attr(th=self.th1, attr_val=nl_attribute)
 
         self.step(8)
-        #TH_CR2 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE
+        # TH_CR2 writes and reads the Basic Information Cluster’s NodeLabel mandatory attribute of DUT_CE
         await self.write_nl_attr(th=self.th2, attr_val=nl_attribute)
         await self.read_nl_attr(th=self.th2, attr_val=nl_attribute)
         sleep(1)
 
         self.step(9)
-        #TH_CR2 opens a commissioning window on DUT_CE for 180 seconds using ECM
+        # TH_CR2 opens a commissioning window on DUT_CE for 180 seconds using ECM
         await self.OpenCommissioningWindow(th=self.th2, duration=180)
 
         self.step(10)
-        #Wait for the commissioning window in step 9 to timeout
+        # Wait for the commissioning window in step 9 to timeout
         sleep(180)
-        
+
         self.step(11)
-        #TH_CR2 reads the window status to verify the DUT_CE window is closed
+        # TH_CR2 reads the window status to verify the DUT_CE window is closed
         await self.th2.SendCommand(self.dut_node_id, 0, Clusters.GeneralCommissioning.Commands.ArmFailSafe(0))
         window_status = await self.get_window_status()
-        #window_status = await self.th2.ReadAttribute(nodeid=self.dut_node_id, attributes=[(0, Clusters.AdministratorCommissioning.Attributes.WindowStatus)])
+        # window_status = await self.th2.ReadAttribute(nodeid=self.dut_node_id, attributes=[(0, Clusters.AdministratorCommissioning.Attributes.WindowStatus)])
         self.print_step(0, dir(AC_cluster.Enums))
         self.print_step(1, window_status)
 
@@ -241,6 +243,7 @@ class TC_CADMIN_1_3(MatterBaseTest):
 
         self.step(13)
         # TH_CR1 starts a commissioning process with DUT_CE before the timeout from step 12
+
 
 if __name__ == "__main__":
     default_matter_test_main()
