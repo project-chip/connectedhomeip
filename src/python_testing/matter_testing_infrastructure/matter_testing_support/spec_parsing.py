cb629161ff572d61d75a5f0d15d830c518b54218
Restyled.io <commits@restyled.io>
2024-10-04 19:57:01 +0000

    Restyled by isort

--- src/python_testing/matter_testing_infrastructure/matter_testing_support/spec_parsing.py
+++ src/python_testing/matter_testing_infrastructure/matter_testing_support/spec_parsing.py
@@ -26,8 +26,8 @@ from enum import Enum, auto
 from typing import Callable, Optional
 
 import chip.clusters as Clusters
-from chip.tlv import uint
 import matter_testing_support.conformance as conformance_support
+from chip.tlv import uint
 from matter_testing_support.conformance import (OPTIONAL_CONFORM, TOP_LEVEL_CONFORMANCE_TAGS, ConformanceDecision,
                                                 ConformanceException, ConformanceParseParameters, feature, is_disallowed, mandatory,
                                                 optional, or_operation, parse_callable_from_xml,
