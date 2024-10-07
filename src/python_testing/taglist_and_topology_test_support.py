ce5034ce2995a13dcede524d0ff2509ff9c6b5c0
cecille <cecille@google.com>
2024-10-07 13:04:54 -0400

    linter

--- src/python_testing/taglist_and_topology_test_support.py
+++ src/python_testing/taglist_and_topology_test_support.py
@@ -18,7 +18,7 @@
 import functools
 from collections import defaultdict
 from dataclasses import dataclass, field
-from chip.clusters.Types import Nullable, NullValue
+from chip.clusters.Types import Nullable
 from typing import Any
 
 import chip.clusters as Clusters
