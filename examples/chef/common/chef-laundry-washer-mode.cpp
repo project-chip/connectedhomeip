fe1949f842781c41886634cfecea658887218588
Sting Chang <houlunc@google.com>
2024-10-25 02:25:07 +0800

    return error

--- examples/chef/common/chef-laundry-washer-mode.cpp
+++ examples/chef/common/chef-laundry-washer-mode.cpp
@@ -100,7 +100,7 @@ chip::Protocols::InteractionModel::Status chefLaundryWasherModeWriteCallback(chi
                                                                              uint8_t * buffer)
 {
     VerifyOrDie(endpointId == 1); // this cluster is only enabled for endpoint 1
-    VerifyOrDie(gLaundryWasherModeInstance != nullptr);
+    VerifyOrReturnError(gLaundryWasherModeInstance != nullptr, chip::Protocols::InteractionModel::Status::Failure);
 
     chip::Protocols::InteractionModel::Status ret;
     chip::AttributeId attributeId = attributeMetadata->attributeId;
