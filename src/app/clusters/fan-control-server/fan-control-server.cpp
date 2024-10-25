feb6883f523fcec79280fae979d60105f6af53d7
Arun Padakanti <arunkumar.padakanti@silabs.com>
2024-10-25 13:19:45 +0530

    Modified return value for speedSetting attribute

--- src/app/clusters/fan-control-server/fan-control-server.cpp
+++ src/app/clusters/fan-control-server/fan-control-server.cpp
@@ -198,7 +198,7 @@ MatterFanControlClusterServerPreAttributeChangedCallback(const ConcreteAttribute
                 }
                 else
                 {
-                    res = Status::WriteIgnored;
+                    res = Status::InvalidInState;
                 }
             }
             else
