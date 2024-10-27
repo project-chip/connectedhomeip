9b66452472f7d0e96baf85d36fb302cbc7877235
Cecille Freeman <cecille@google.com>
2024-10-27 12:26:22 -0400

    Linter

--- src/python_testing/TC_VALCC_3_3.py
+++ src/python_testing/TC_VALCC_3_3.py
@@ -31,12 +31,8 @@
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
 
-import logging
-import time
-
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from chip.interaction_model import InteractionModelError, Status
 from chip.testing.matter_testing import (AttributeValue, ClusterAttributeChangeAccumulator, MatterBaseTest, TestStep,
                                          async_test_body, default_matter_test_main)
 from mobly import asserts
