d7a61beb9160e4ada117faa950a8694f828dc0ad
lian park <lian.park@lge.com>
2024-10-29 16:45:18 +0900

    [WebOS] Modify a script file for changing environment
    Changes
     - Modify webos script file which makes a build environment
     - Skip a git clone when the repository is existed
    
    Testing
     - Tested done on webOS

--- scripts/examples/webos_example.sh
+++ scripts/examples/webos_example.sh
@@ -18,15 +18,20 @@
 # Activating connectedhomeip build environment
 source scripts/activate.sh
 
-# Clone webos_sdk
-echo "##### Cloning webOS OSE SDK #####"
-git clone https://github.com/cabin15/webos-ose-ndk third_party/webos_sdk
+# Check if webos_sdk directory exists, clone if it doesn't
+if [ ! -d "third_party/webos_sdk/v2.14.1"] then
+    # Clone webos_sdk
+    echo "##### Cloning webOS OSE SDK #####"
+    git clone https://github.com/cabin15/webos-ose-ndk third_party/webos_sdk
 
-# Extract webOS SDK
-cat third_party/webos_sdk/v2.14.1/webos_sdk.tar* | (
-    cd third_party/webos_sdk/v2.14.1/
-    tar xvzf -
-)
+    # Extract webOS SDK
+    cat third_party/webos_sdk/v2.14.1/webos_sdk.tar* | (
+        cd third_party/webos_sdk/v2.14.1/
+        tar xvzf -
+    )
+else
+    echo "#### webOS OSE SDK already exists ####"
+fi
 
 # Grant execute permission for NDK install script
 chmod 555 third_party/webos_sdk/v2.14.1/webos-sdk-x86_64-cortexa7t2hf-neon-vfpv4-toolchain-2.14.1.g.sh
@@ -43,7 +48,7 @@ echo ""
 # Build webos example
 echo "##### Build webos example #####"
 echo "##### Performing gn gen #####"
-gn gen out/host --args="is_debug=false target_os=\"webos\" target_cpu=\"arm\" chip_enable_python_modules=false ar_webos=\"$AR\" cc_webos=\"$CC -Wno-format-security\" cxx_webos=\"$CXX\" webos_sysroot=\"$PKG_CONFIG_SYSROOT_DIR\" chip_build_tests=false enable_syslog=true treat_warnings_as_errors=false"
+gn gen out/webos --args="is_debug=false target_os=\"webos\" target_cpu=\"arm\" chip_enable_python_modules=false ar_webos=\"$AR\" cc_webos=\"$CC -Wno-format-security\" cxx_webos=\"$CXX\" webos_sysroot=\"$PKG_CONFIG_SYSROOT_DIR\" chip_build_tests=false enable_syslog=true treat_warnings_as_errors=false"
 
 echo "##### Building by ninja #####"
-ninja -C out/host
+ninja -C out/webos
