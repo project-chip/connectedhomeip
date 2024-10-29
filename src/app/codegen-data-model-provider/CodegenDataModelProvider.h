fa4664124960105b993b235604f43db03b5d17f7
Andrei Litvin <andy314@gmail.com>
2024-10-29 10:19:01 -0400

    Event validation simplification - pull logic into InteractionModelEngine.cpp only (#36251)
    * Pull back the event path validity mixin, start with a datamodel implementation
    
    * Fix compile logic after I moved things away
    
    * Add one more check
    
    * Restyle
    
    * Fix typo
    
    * Fix includes
    
    * Restyle
    
    * Update src/app/InteractionModelEngine.cpp
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    * Rename method
    
    * Restyle
    
    * More renames
    
    * Restyle
    
    * Fix some renames
    
    * Restyle
    
    * A few more renames
    
    * Restyle
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/codegen-data-model-provider/CodegenDataModelProvider.h
+++ src/app/codegen-data-model-provider/CodegenDataModelProvider.h
@@ -141,8 +141,6 @@ public:
         return CHIP_NO_ERROR;
     }
 
-    bool EventPathIncludesAccessibleConcretePath(const EventPathParams & path,
-                                                 const Access::SubjectDescriptor & descriptor) override;
     DataModel::ActionReturnStatus ReadAttribute(const DataModel::ReadAttributeRequest & request,
                                                 AttributeValueEncoder & encoder) override;
     DataModel::ActionReturnStatus WriteAttribute(const DataModel::WriteAttributeRequest & request,
