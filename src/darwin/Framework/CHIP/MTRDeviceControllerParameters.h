a6f016813e7d3b43c119bc70d9498788bd876ec5
Boris Zbarsky <bzbarsky@apple.com>
2024-10-30 18:56:02 -0400

    Update Darwin availability annotations to account for new release. (#36304)

--- src/darwin/Framework/CHIP/MTRDeviceControllerParameters.h
+++ src/darwin/Framework/CHIP/MTRDeviceControllerParameters.h
@@ -153,11 +153,11 @@ MTR_AVAILABLE(ios(17.6), macos(14.6), watchos(10.6), tvos(17.6))
 /**
  * The root certificate we were initialized with.
  */
-@property (nonatomic, copy, readonly) MTRCertificateDERBytes rootCertificate MTR_NEWLY_AVAILABLE;
+@property (nonatomic, copy, readonly) MTRCertificateDERBytes rootCertificate MTR_AVAILABLE(ios(18.2), macos(15.2), watchos(11.2), tvos(18.2));
 
 @end
 
-MTR_NEWLY_AVAILABLE
+MTR_AVAILABLE(ios(18.2), macos(15.2), watchos(11.2), tvos(18.2))
 @interface MTRXPCDeviceControllerParameters : MTRDeviceControllerAbstractParameters
 
 - (instancetype)init NS_UNAVAILABLE;
@@ -174,8 +174,8 @@ MTR_NEWLY_AVAILABLE
 - (instancetype)initWithXPConnectionBlock:(NSXPCConnection * (^)(void) )xpcConnectionBlock
                          uniqueIdentifier:(NSUUID *)uniqueIdentifier;
 
-@property (atomic, readonly, retain) NSUUID * uniqueIdentifier MTR_NEWLY_AVAILABLE;
-@property (readonly, strong, nonatomic) NSXPCConnection * (^xpcConnectionBlock)(void) MTR_NEWLY_AVAILABLE;
+@property (atomic, readonly, retain) NSUUID * uniqueIdentifier MTR_AVAILABLE(ios(18.2), macos(15.2), watchos(11.2), tvos(18.2));
+@property (readonly, strong, nonatomic) NSXPCConnection * (^xpcConnectionBlock)(void) MTR_AVAILABLE(ios(18.2), macos(15.2), watchos(11.2), tvos(18.2));
 
 @end
 
