1921d9f6bfbe4ac3e36db7c077c54a50f6f39228
lian park <lian.park@lge.com>
2024-10-30 11:02:50 +0900

    [WebOS] Change a script file to resolve webos build environment
    Changes
     - Change a script file to fix webOS build environment
    
    Testing
     - Done on webOS Platform

--- scripts/examples/webos_example.sh
+++ scripts/examples/webos_example.sh
@@ -1,36 +1,18 @@
-#!/usr/bin/env bash
-#
-#    Copyright (c) 2022 Project CHIP Authors
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-#
-
 # Activating connectedhomeip build environment
 source scripts/activate.sh
 
-# Check if webos_sdk directory exists, clone if it doesn't
-if [ ! -d "third_party/webos_sdk/v2.14.1"] then
-    # Clone webos_sdk
+# Check if webos_sdk directory exists, clone and extract if it doesn't
+if [ ! -d "third_party/webos_sdk/v2.14.1" ]; then
     echo "##### Cloning webOS OSE SDK #####"
     git clone https://github.com/cabin15/webos-ose-ndk third_party/webos_sdk
 
-    # Extract webOS SDK
+    echo "##### Extracting webOS SDK #####"
     cat third_party/webos_sdk/v2.14.1/webos_sdk.tar* | (
         cd third_party/webos_sdk/v2.14.1/
         tar xvzf -
     )
 else
-    echo "#### webOS OSE SDK already exists ####"
+    echo "##### webOS OSE SDK already exists #####"
 fi
 
 # Grant execute permission for NDK install script
