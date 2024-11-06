735fd76d97e61968f794ec4fcb9573fe3c7010d0
Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
2024-11-06 15:57:48 +0530

    Addressing comments

--- examples/platform/silabs/MatterConfig.cpp
+++ examples/platform/silabs/MatterConfig.cpp
@@ -329,7 +329,7 @@ CHIP_ERROR SilabsMatterConfig::InitWiFi(void)
 extern "C" void vApplicationIdleHook(void)
 {
 #if (SLI_SI91X_MCU_INTERFACE && CHIP_CONFIG_ENABLE_ICD_SERVER)
-    chip::DeviceLayer::Silabs::SiWxPlatformInterface::sl_si91x_btn_event_handler();
-    chip::DeviceLayer::Silabs::SiWxPlatformInterface::sl_si91x_uart_power_requirement_handler();
+    SiWxPlatformInterface::sl_si91x_btn_event_handler();
+    SiWxPlatformInterface::sl_si91x_uart_power_requirement_handler();
 #endif
 }
