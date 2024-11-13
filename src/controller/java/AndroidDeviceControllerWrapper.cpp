a4f64ace456a548373dabf4a7e9688fae2963c3b
yunhanw-google <yunhanw@google.com>
2024-11-12 21:50:35 -0800

    [ICD] Shutdown icd client storage when destroying android controller (#36348)
    * Shutdown DefautICDClientStorage when destorying Android controller
    
    * Restyled by whitespace
    
    * Update DefaultICDClientStorage.h
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/controller/java/AndroidDeviceControllerWrapper.cpp
+++ src/controller/java/AndroidDeviceControllerWrapper.cpp
@@ -52,6 +52,7 @@ using namespace TLV;
 
 AndroidDeviceControllerWrapper::~AndroidDeviceControllerWrapper()
 {
+    getICDClientStorage()->Shutdown();
     mController->Shutdown();
 
 #ifndef JAVA_MATTER_CONTROLLER_TEST
