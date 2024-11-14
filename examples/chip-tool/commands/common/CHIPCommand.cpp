9ac3b8fd5655055451aa54a0c43803a2897704bb
Vivien Nicolas <vnicolas@apple.com>
2024-11-14 23:49:58 +0100

    [darwin] Update the NetworkFramework UDP implementation (#36418)
    * [Network.framework] Add chip_system_config_use_network_framework
    
    * [Network.framework] Update src/lib/core/CHIPConfig.h file to enable CHIP_CONFIG_IsPlatformPOSIXErrorNonCritical
    
    * [Network.framework] Update src/system/SystemConfig.h file to enable CHIP_SYSTEM_CONFIG_USE_POSIX_SOCKETS
    
    * [Network.Framework] Update src/inet/InetInterface* files with some additional #ifdef for the network framework
    
    * [Network.Framework] Update src/inet/IPAddress* files with some additional #ifdef for the network framework
    
    * [Network.framework] Update src/inet/InetInterfaceImplDefault.cpp to enable the default implementation
    
    * [Network.framework] Update src/system to not use WakeEvent/Signal when using the network framework
    
    * [Network.framework] Convert UDPEndPointImplNetworkFramework.cpp to .mm with arc support
    
    * [chip-tool] Ensure each commissioner listen onto its own udc port since Network.framework won't allow SO_REUSEPORT
    
    * [Matter.framework][darwin-framework-tool] Add chip_system_config_use_network_framework flag support
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- examples/chip-tool/commands/common/CHIPCommand.cpp
+++ examples/chip-tool/commands/common/CHIPCommand.cpp
@@ -467,6 +467,11 @@ void CHIPCommand::ShutdownCommissioner(const CommissionerIdentity & key)
 CHIP_ERROR CHIPCommand::InitializeCommissioner(CommissionerIdentity & identity, chip::FabricId fabricId)
 {
     std::unique_ptr<ChipDeviceCommissioner> commissioner = std::make_unique<ChipDeviceCommissioner>();
+#if CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY
+    VerifyOrReturnError(chip::CanCastTo<uint16_t>(CHIP_UDC_PORT + fabricId), CHIP_ERROR_INVALID_ARGUMENT);
+    uint16_t udcListenPort = static_cast<uint16_t>(CHIP_UDC_PORT + fabricId);
+    commissioner->SetUdcListenPort(udcListenPort);
+#endif // CHIP_DEVICE_CONFIG_ENABLE_COMMISSIONER_DISCOVERY
     chip::Controller::SetupParams commissionerParams;
 
     ReturnLogErrorOnFailure(mCredIssuerCmds->SetupDeviceAttestation(commissionerParams, sTrustStore, sRevocationDelegate));
