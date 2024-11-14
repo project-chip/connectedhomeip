acea4646d980196f3298129d9fdef76238f2f238
Andrei Litvin <andy314@gmail.com>
2024-11-14 08:35:30 -0500

    Move ember-specific attribute persistence providers into `src/util/persistence` (#36473)
    * Move persistence providers code into util since they depend on ember.
    
    Unit tests pass, will need to fix up apps.
    
    * Fix up more includes
    
    * Restyled by clang-format
    
    * Fix up qpg build
    
    * Also fix nrfconnect deferred attribute persistence use
    
    * Fix path
    
    * Add persistence to esp32 CMakeLists.txt
    
    * Update examples/lighting-app/qpg/BUILD.gn
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/app/server/Server.h
+++ src/app/server/Server.h
@@ -24,7 +24,6 @@
 #include <access/examples/ExampleAccessControlDelegate.h>
 #include <app/CASEClientPool.h>
 #include <app/CASESessionManager.h>
-#include <app/DefaultAttributePersistenceProvider.h>
 #include <app/FailSafeContext.h>
 #include <app/OperationalSessionSetupPool.h>
 #include <app/SimpleSubscriptionResumptionStorage.h>
@@ -80,6 +79,11 @@
 #include <app/icd/server/ICDCheckInBackOffStrategy.h>        // nogncheck
 #endif                                                       // CHIP_CONFIG_ENABLE_ICD_CIP
 #endif                                                       // CHIP_CONFIG_ENABLE_ICD_SERVER
+
+// TODO: https://github.com/project-chip/connectedhomeip/issues/36472
+//       this strongly couples Server to Ember and this dependency should
+//       be removed
+#include <app/util/persistence/DefaultAttributePersistenceProvider.h>
 
 namespace chip {
 
