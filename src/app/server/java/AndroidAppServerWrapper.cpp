7cad7d6a477f42cf30930e9c4debfc8d5948667e
Andrei Litvin <andreilitvin@google.com>
2024-11-19 09:58:50 -0500

    Fix include

--- src/app/server/java/AndroidAppServerWrapper.cpp
+++ src/app/server/java/AndroidAppServerWrapper.cpp
@@ -17,7 +17,7 @@
  */
 
 #include "AndroidAppServerWrapper.h"
-#include <app/codegen-data-model-provider/Instance.h>
+
 #include <app/server/OnboardingCodesUtil.h>
 #include <app/server/Server.h>
 #include <credentials/DeviceAttestationCredsProvider.h>
