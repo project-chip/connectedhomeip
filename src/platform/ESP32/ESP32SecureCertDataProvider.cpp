ea1ba405b8312d08844f4327a537f7f5085122c9
Shubham Patil <shubham.patil@espressif.com>
2024-11-21 13:57:57 +0530

    ESP32: fix typo to include correct Span.h in secure cert data provider

--- src/platform/ESP32/ESP32SecureCertDataProvider.cpp
+++ src/platform/ESP32/ESP32SecureCertDataProvider.cpp
@@ -22,7 +22,7 @@
 #include <lib/core/CHIPEncoding.h>
 #include <lib/support/CodeUtils.h>
 #include <lib/support/logging/CHIPLogging.h>
-#include <lib/support/span.h>
+#include <lib/support/Span.h>
 #include <platform/ESP32/ESP32SecureCertDataProvider.h>
 
 namespace chip {
