f1d901610fe57d98b214476b5842f942a66fd957
Andrei Litvin <andy314@gmail.com>
2024-12-16 09:56:15 -0500

    Fix mangled license blurb

--- src/app/server/DefaultTermsAndConditionsProvider.cpp
+++ src/app/server/DefaultTermsAndConditionsProvider.cpp
@@ -1,5 +1,4 @@
 /*
- *
  *  Copyright (c) 2024 Project CHIP Authors
  *  All rights reserved.
  *
@@ -10,8 +9,10 @@
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #include "DefaultTermsAndConditionsProvider.h"
