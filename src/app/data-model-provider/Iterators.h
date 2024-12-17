7c8ec0fd80922db3ae1f362ff49c231ad0c899a2
Andrei Litvin <andreilitvin@google.com>
2024-12-17 15:45:00 -0500

    Start defining some iterator functions that may be used ... it is still somewhat unclear how efficient these are or how AttributePathExpandIterator can even work with these

---
+++ src/app/data-model-provider/Iterators.h
@@ -0,0 +1,73 @@
+/*
+ *    Copyright (c) 2024 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#pragma once
+
+#include <optional>
+
+namespace chip {
+namespace app {
+namespace DataModel {
+
+/// Supports one-way iteration over a specific element list.
+///
+template<typename T>
+class ElementIterator {
+public:
+    virtual ~ElementIterator() = default;
+
+    /// Return the next element in the given sequence.
+    ///
+    /// returns std::nullopt once the end is reached.
+    virtual std::optional<T> Next() = 0;
+};
+
+/// An iterator that supports seeking to a specific location.
+///
+/// Useful to directly locate a specific element in an iteration.
+template<typename T>
+class FastForwardIterator : public ElementIterator<T> {
+public:
+    virtual ~FastForwardIterator() = default;
+
+    /// Moves the iterator up to the value `T`. Use as it may be
+    /// more efficient than calling Next() repeateadly.
+    ///
+    /// Returns true if the seek was successful and the element is found.
+    /// Returns false otherwise and the iterator will be invalidated (next will return std::nullopt)
+    virtual bool Seek(const T& value) = 0;
+
+};
+
+/// An iterator that also supports returning extra associated
+/// metadata.
+template<typename T, typename Meta>
+class MetaDataIterator : public FastForwardIterator<T> {
+public:
+    virtual ~MetaDataIterator() = default;
+
+    /// Fetch the extra metadata associated with the current position
+    /// of the iterator.
+    ///
+    /// returns std::nullopt if the current iterator position is
+    /// before the start (i.e. was just created and Next was never called)
+    /// or if the iterator has reached its end.
+    virtual std::optional<Meta> GetMetadata() = 0;
+};
+
+} // DataModel
+} // namespace app
+} // namespace chip
