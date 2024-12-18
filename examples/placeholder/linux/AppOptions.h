852ca31170b6f0a3145aaa63339c6bd24d74fdf6
Shubham Patil <shubham.patil@espressif.com>
2024-12-18 17:55:47 +0530

    Move the --dac_provider option from all clusters apps to common place.
    all-clusters-app and all-clusters-minimal-app has the --dac_provider
    option which lets us inject the custom dacs in the applications.
    
    moved this option from these apps to examples/platform/linux so that
    wide range of applications can levarage this.
    
    This would be useful in testing device attestation revocation cases.

--- examples/placeholder/linux/AppOptions.h
+++ examples/placeholder/linux/AppOptions.h
@@ -20,13 +20,10 @@
 
 #include "AppMain.h"
 
-#include <app/tests/suites/credentials/TestHarnessDACProvider.h>
-
 class AppOptions
 {
 public:
     static chip::ArgParser::OptionSet * GetOptions();
-    static chip::Credentials::DeviceAttestationCredentialsProvider * GetDACProvider();
     static bool GetInteractiveMode();
     static chip::Optional<uint16_t> GetInteractiveModePort();
 
