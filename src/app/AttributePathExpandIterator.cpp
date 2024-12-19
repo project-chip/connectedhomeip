95a242f37768a23923c2c6690c810a188ef8f3bd
Andrei Litvin <andreilitvin@google.com>
2024-12-19 11:00:34 -0500

    Fix include

--- src/app/AttributePathExpandIterator.cpp
+++ src/app/AttributePathExpandIterator.cpp
@@ -14,7 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "lib/support/logging/TextOnlyLogging.h"
 #include <app/AttributePathExpandIterator.h>
 
 #include <app/GlobalAttributes.h>
