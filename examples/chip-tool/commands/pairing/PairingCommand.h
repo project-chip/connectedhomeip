c8ff0e04bf28d280a70ab0f4d6c989ca27a0f02b
James Swan <122404367+swan-amazon@users.noreply.github.com>
2024-12-19 22:25:39 +0000

    [doc] Clarify required arguments for T&C acknowledgements
    Update help text for require-tc-acknowledgements to explicitly state which
    arguments must be provided when T&C acknowledgements are required. Instead
    of the vague "valid T&C acknowledgements", now specifically mentions that
    both tc-acknowledgements and tc-acknowledgements-version arguments are
    mandatory for successful commissioning when this option is enabled.

--- examples/chip-tool/commands/pairing/PairingCommand.h
+++ examples/chip-tool/commands/pairing/PairingCommand.h
@@ -205,8 +205,8 @@ public:
 
             AddArgument("require-tc-acknowledgements", 0, 1, &mRequireTCAcknowledgements,
                         "Indicates whether Terms and Conditions acknowledgements are required during commissioning. If set to "
-                        "true, the commissioner must provide valid T&C acknowledgements or the commissioning will fail. If false, "
-                        "the T&C acknowledgement step will be skipped.");
+                        "true, the tc-acknowledgements and tc-acknowledgements-version arguments must be provided for the "
+                        "commissioning to succeed. If false, the T&C acknowledgement step will be skipped.");
 
             AddArgument("tc-acknowledgements", 0, UINT16_MAX, &mTCAcknowledgements,
                         "Bit-field value indicating which Terms and Conditions have been accepted by the user. This value is sent "
