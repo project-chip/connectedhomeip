0e5213d3eeb3133a99f2c438a7ea8dd422596de0
Restyled.io <commits@restyled.io>
2024-12-19 23:19:19 +0000

    Restyled by isort

--- scripts/spec_xml/spec_revision_diff_summary.py
+++ scripts/spec_xml/spec_revision_diff_summary.py
@@ -21,9 +21,8 @@
 # spec expectations before the 1.4 release and we should continue to do so going forward.
 
 import click
-
-from chip.testing.spec_parsing import build_xml_clusters, build_xml_device_types, PrebuiltDataModelDirectory
 from chip.testing.conformance import ConformanceDecision
+from chip.testing.spec_parsing import PrebuiltDataModelDirectory, build_xml_clusters, build_xml_device_types
 
 
 def get_changes(old, new):
