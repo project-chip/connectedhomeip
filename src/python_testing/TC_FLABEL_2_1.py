371f335fc914c2226f24212a1668d0b6cae851cd
Oleksandr Sirko <wisenss@gmail.com>
2024-12-19 16:43:04 +0100

    apply linter suggestion

--- src/python_testing/TC_FLABEL_2_1.py
+++ src/python_testing/TC_FLABEL_2_1.py
@@ -32,8 +32,8 @@
 # === END CI TEST ARGUMENTS ===
 
 import chip.clusters as Clusters
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from chip.interaction_model import Status
+from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 
