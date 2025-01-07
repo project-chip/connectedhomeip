f9d20d7d8dae8fd1d78f5155e5c12fbb77679c99
Mathieu Kardous <mathieu.kardous@silabs.com>
2025-01-07 15:06:03 -0500

    Refactor test event trigger to inject Storage interface

--- examples/platform/silabs/SilabsTestEventTriggerDelegate.cpp
+++ examples/platform/silabs/SilabsTestEventTriggerDelegate.cpp
@@ -17,24 +17,39 @@
  */
 
 #include "SilabsTestEventTriggerDelegate.h"
-#include <ProvisionManager.h>
 #include <lib/core/ErrorStr.h>
 
 using namespace ::chip::DeviceLayer;
 
 namespace chip {
 
+CHIP_ERROR SilabsTestEventTriggerDelegate::Init(DeviceLayer::Silabs::Provision::Storage * storage)
+{
+    VerifyOrReturnError(storage != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
+    mStorage = storage;
+
+    return CHIP_NO_ERROR;
+}
+
 bool SilabsTestEventTriggerDelegate::DoesEnableKeyMatch(const ByteSpan & enableKey) const
 {
+    CHIP_ERROR error = CHIP_NO_ERROR;
+
     uint8_t storedEnableKey[TestEventTriggerDelegate::kEnableKeyLength] = { 0 };
     MutableByteSpan storedEnableKeySpan(storedEnableKey);
 
-    CHIP_ERROR error = Silabs::Provision::Manager::GetInstance().GetStorage().GetTestEventTriggerKey(storedEnableKeySpan);
+    // If the mStorage is equal to nullptr, we still continue in the function to check if the requested enableKey matches the zero
+    // key.
+    if (mStorage != nullptr)
+    {
+        error = mStorage->GetTestEventTriggerKey(storedEnableKeySpan);
+    }
+
     if (error != CHIP_NO_ERROR)
     {
-        // If we fail to read the enableKey from storage, the MutableByteSpan is not modified by the getter which leaves the span equal to a zero bytepsan (size = 0).
-        // This guarantees that we will be able to inform the stack that the test event trigger is not enabled when the stack tries
-        // to match the zero bytespan to our enableKey.
+        // If we fail to read the enableKey from storage, the MutableByteSpan is not modified by the getter which leaves the span
+        // equal to a zero bytepsan (size = 0). This guarantees that we will be able to inform the stack that the test event trigger
+        // is not enabled when the stack tries to match the zero bytespan to our enableKey.
         ChipLogError(DeviceLayer, "Failed to get test event trigger key: %s", ErrorStr(error));
     }
 
