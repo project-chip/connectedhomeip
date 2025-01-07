e26a8a4994c9091cd6c05f688e8540209145213e
Yufeng Wang <yufengwang@google.com>
2025-01-07 10:39:29 -0800

    Fix lint error

--- src/python_testing/TC_DGSW_2_3.py
+++ src/python_testing/TC_DGSW_2_3.py
@@ -38,7 +38,6 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.interaction_model import InteractionModelError, Status
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
