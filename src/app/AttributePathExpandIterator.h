130c1f6bfb99858a64792485878f12b11c8c61c6
Andrei Litvin <andy314@gmail.com>
2025-01-08 12:57:53 -0500

    Update src/app/AttributePathExpandIterator.h
    Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

--- src/app/AttributePathExpandIterator.h
+++ src/app/AttributePathExpandIterator.h
@@ -65,7 +65,7 @@ public:
     public:
         // Position is treated as a direct member access by the AttributePathExpandIterator, however it is opaque (except copying)
         // for external code. We allow friendship here to not have specific get/set for methods (clearer interface and less
-        // likelyhood of extra code usage).
+        // likelihood of extra code usage).
         friend class AttributePathExpandIterator;
 
         /// External callers can only ever start iterating on a new path from the beginning
