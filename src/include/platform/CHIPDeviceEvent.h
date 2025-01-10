e61a316d7794d66ad681ca2013e34fa93d555b5f
Lo,Chin-Ran <chin-ran.lo@nxp.com>
2025-01-10 17:18:00 +0800

    Add PAFTP implementation
    Signed-off-by: Lo,Chin-Ran <chin-ran.lo@nxp.com>

--- src/include/platform/CHIPDeviceEvent.h
+++ src/include/platform/CHIPDeviceEvent.h
@@ -282,9 +282,10 @@ enum InternalEventTypes
      */
     kCHIPoBLEConnectionError,
     kCHIPoBLENotifyConfirm,
-    kCHIPoWiFiPAFWriteReceived,
+    kCHIPoWiFiPAFReceived,
     kCHIPoWiFiPAFConnected,
     kCHIPoWiFiPAFCancelConnect,
+    kCHIPoWiFiPAFWriteDone,
 };
 
 static_assert(kEventTypeNotSet == 0, "kEventTypeNotSet must be defined as 0");
@@ -391,6 +392,7 @@ typedef void (*AsyncWorkFunct)(intptr_t arg);
 #include <system/SystemEvent.h>
 #include <system/SystemLayer.h>
 #include <system/SystemPacketBuffer.h>
+#include <wifipaf/WiFiPAFRole.h>
 
 namespace chip {
 namespace DeviceLayer {
@@ -503,10 +505,8 @@ struct ChipDeviceEvent final
         struct
         {
             chip::System::PacketBuffer * Data;
-            uint32_t id;
-            uint32_t peer_id;
-            uint8_t peer_addr[6];
-        } CHIPoWiFiPAFWriteReceived;
+            chip::WiFiPAF::WiFiPAFSession SessionInfo;
+        } CHIPoWiFiPAFReceived;
 #endif
         struct
         {
