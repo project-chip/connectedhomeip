917d4a8abdea958a66b6b012e7bd3da914c8efda
Andrei Litvin <andy314@gmail.com>
2025-01-10 08:58:27 -0500

    Restyle

--- src/app/AttributePathExpandIterator.h
+++ src/app/AttributePathExpandIterator.h
@@ -176,7 +176,8 @@ private:
 ///        PeekAttributePathExpandIterator iterator(...., state);
 ///        if (iterator.Next(...)) { ... }
 ///      }
-///      // state here is ROLLED BACK (i.e. initializing a new iterator with it will start at the same place as the previous iteration attempt).
+///      // state here is ROLLED BACK (i.e. initializing a new iterator with it will start at the same place as the previous
+///      iteration attempt).
 ///
 ///
 class PeekAttributePathExpandIterator
