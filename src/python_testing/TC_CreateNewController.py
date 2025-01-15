2de9b9c22bd33e3ac8d8a1af9d9d60e010031e45
Jake Ororke <jororke@csa-iot.org>
2025-01-15 06:18:50 -0800

    Updating TC_CreateNewController python test module: - Resolving linting errors

--- src/python_testing/TC_CreateNewController.py
+++ src/python_testing/TC_CreateNewController.py
@@ -35,7 +35,6 @@
 
 import asyncio
 
-import chip.clusters as Clusters
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
