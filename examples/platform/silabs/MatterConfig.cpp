e01471d6fb7422021ee1bb3c92048239f3522f4a
bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>
2025-01-16 20:35:26 +0530

    Added changes to fix 917 SoC build when LED and button disabled (#37010)

--- examples/platform/silabs/MatterConfig.cpp
+++ examples/platform/silabs/MatterConfig.cpp
@@ -331,7 +331,9 @@ CHIP_ERROR SilabsMatterConfig::InitWiFi(void)
 extern "C" void vApplicationIdleHook(void)
 {
 #if (SLI_SI91X_MCU_INTERFACE && CHIP_CONFIG_ENABLE_ICD_SERVER)
+#ifdef SL_CATALOG_SIMPLE_BUTTON_PRESENT
     SiWxPlatformInterface::sl_si91x_btn_event_handler();
+#endif // SL_CATALOG_SIMPLE_BUTTON_PRESENT
     SiWxPlatformInterface::sl_si91x_uart_power_requirement_handler();
 #endif
 }
