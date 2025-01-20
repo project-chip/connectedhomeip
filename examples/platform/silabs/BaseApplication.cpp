f19f44aab092f7ead1060c7afafc0e05d93c0dc6
Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>
2025-01-20 12:08:27 -0500

    [Silabs] Continue WifiInterface headers clean up (#37107)
    * Clean up Wifi Interface headers
    
    * Rename SiWx function
    
    * fix comment
    
    * fix build
    
    * Address review comments

--- examples/platform/silabs/BaseApplication.cpp
+++ examples/platform/silabs/BaseApplication.cpp
@@ -261,7 +261,7 @@ CHIP_ERROR BaseApplication::Init()
      * Wait for the WiFi to be initialized
      */
     ChipLogProgress(AppServer, "APP: Wait WiFi Init");
-    while (!wfx_hw_ready())
+    while (!IsStationReady())
     {
         osDelay(pdMS_TO_TICKS(10));
     }
