5af9dc58899854a8728b7087072c3ff5331e89d5
Wang Qixiang <43193572+wqx6@users.noreply.github.com>
2025-01-20 10:05:27 +0800

    Update src/platform/PlatformLockSupport.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/platform/PlatformLockSupport.cpp
+++ src/platform/PlatformLockSupport.cpp
@@ -1,7 +1,5 @@
 /*
- *
- *    Copyright (c) 2020 Project CHIP Authors
- *    Copyright (c) 2018 Nest Labs, Inc.
+ *    Copyright (c) 2025 Project CHIP Authors
  *    All rights reserved.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
