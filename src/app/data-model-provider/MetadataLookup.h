297b0dada85d5adbbc6ef033720ff98aaa510845
Andrei Litvin <andreilitvin@google.com>
2025-01-21 12:27:52 -0500

    Fix/update some includes to make things compile - at least tests compile

--- src/app/data-model-provider/MetadataLookup.h
+++ src/app/data-model-provider/MetadataLookup.h
@@ -20,7 +20,7 @@
 #include <app/ConcreteAttributePath.h>
 #include <app/ConcreteClusterPath.h>
 #include <app/data-model-provider/MetadataList.h>
-#include <app/data-model-provider/MetadataTypes.h>
+#include <app/data-model-provider/ProviderMetadataTree.h>
 #include <lib/core/DataModelTypes.h>
 #include <lib/support/CodeUtils.h>
 
