b1e4eb1abd8fb61f604ddbac2237dd44e66131d2
Raul Marquez <rmarquez@csa-iot.org>
2025-01-28 23:27:19 -0800

    wip

--- src/python_testing/TC_FAN_3_1.py
+++ src/python_testing/TC_FAN_3_1.py
@@ -34,43 +34,23 @@
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
 
-import asyncio
-from enum import Enum
 import logging
 import time
-import random
-import queue
-from typing import Any
-
+from enum import Enum
 import chip.clusters as Clusters
-from chip.clusters.Types import NullValue
 from chip.interaction_model import Status
-from chip.testing.matter_testing import MatterBaseTest, AttributeValue, ClusterAttributeChangeAccumulator, TestStep, async_test_body, default_matter_test_main
+from chip.testing.matter_testing import MatterBaseTest, ClusterAttributeChangeAccumulator, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
-
-from matter_testing_infrastructure.chip.testing.matter_testing import AttributeChangeCallbackFan, AttributeValue, ClusterAttributeChangeAccumulator
-from chip.ChipDeviceCtrl import ChipDeviceController
-from chip.clusters.Attribute import AttributeStatus, AsyncReadTransaction, AttributePath, SubscriptionTransaction, TypedAttributePath
-
-
-# import chip.clusters as Clusters
-from chip.ChipDeviceCtrl import ChipDeviceController
+from matter_testing_infrastructure.chip.testing.matter_testing import ClusterAttributeChangeAccumulator
 from chip.clusters import ClusterObjects as ClusterObjects
-from chip.clusters.Attribute import AsyncReadTransaction, AttributePath, SubscriptionTransaction, TypedAttributePath
-from chip.clusters.enum import MatterIntEnum
-from chip.exceptions import ChipStackError
 from chip.interaction_model import Status
-from chip.testing.basic_composition import BasicCompositionTests
-from chip.testing.matter_testing import (AttributeChangeCallback, EventChangeCallback, MatterBaseTest, TestStep, async_test_body,
-                                         default_matter_test_main)
 
-# import pdb
-
-class DirectionEnum(Enum):
+class OrderEnum(Enum):
     Ascending = 1
     Descending = 2
 
 logger = logging.getLogger(__name__)
+
 
 class TC_FAN_3_1(MatterBaseTest):
     # def steps_TC_FAN_3_1(self):
@@ -81,53 +61,39 @@ class TC_FAN_3_1(MatterBaseTest):
         cluster = Clusters.Objects.FanControl
         return await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=attribute)
 
-    async def read_fan_mode(self, endpoint):
-        return await self.read_fc_attribute_expect_success(endpoint, Clusters.FanControl.Attributes.FanMode)
-
-    async def read_percent_current(self, endpoint):
-        return await self.read_fc_attribute_expect_success(endpoint, Clusters.FanControl.Attributes.PercentCurrent)
-
-    async def write_fan_mode(self, endpoint, fan_mode) -> Status:
-        result = await self.default_controller.WriteAttribute(self.dut_node_id, [(endpoint, Clusters.FanControl.Attributes.FanMode(fan_mode))])
-        return result[0].Status
+    async def read_setting(self, endpoint, attribute):
+        return await self.read_fc_attribute_expect_success(endpoint, attribute)
 
     async def write_setting(self, endpoint, attr_to_write, value) -> Status:
         result = await self.default_controller.WriteAttribute(self.dut_node_id, [(endpoint, attr_to_write(value))])
         return result[0].Status
-    
-    async def read_valcc_attribute_expect_success(self, endpoint, attribute):
-        cluster = Clusters.Objects.ValveConfigurationAndControl
-        return await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=attribute)
 
-    def pics_TC_FAN_3_1(self) -> list[str]:
-        return ["FAN.S"]
+    async def verify_fan_mode(self, endpoint, attr_to_write, order, timeout_sec):
+        logging.info(f"[FANS] Monitoring FanMode by writing to {attr_to_write.__name__} in {order.name} order")
 
-
-
-
-
-
-    async def verify_fan_mode(self, attr_to_write, endpoint, direction, timeout_sec):
-        logging.info(f"[FANS] Monitoring FanMode by writing to {attr_to_write.__name__} in {direction.name} order")
-        
         # Setup
         cluster = Clusters.FanControl
-        attr_to_monitor = cluster.Attributes.FanMode
+        fan_mode_attr = cluster.Attributes.FanMode
         attribute_subscription = ClusterAttributeChangeAccumulator(cluster)
         fan_mode_off = cluster.Enums.FanModeEnum.kOff
         fan_mode_high = cluster.Enums.FanModeEnum.kHigh
 
+        # As per the spec, the max values for PercentSetting and SpeedSetting are 100 and SpeedMax respectively
+        max_value = 100
+        if attr_to_write == cluster.Attributes.SpeedSetting:
+            max_value = await self.read_setting(endpoint, cluster.Attributes.SpeedMax)
+
         # Determine if values will be written ascending or descending
-        fan_mode_init = fan_mode_off if direction == DirectionEnum.Ascending else fan_mode_high
-        range_loop = range(1, 101) if direction == DirectionEnum.Ascending else range(100, -1, -1)
+        fan_mode_init = fan_mode_off if order == OrderEnum.Ascending else fan_mode_high
+        range_loop = range(1, max_value + 1) if order == OrderEnum.Ascending else range(max_value, -1, -1)
 
         # Write initial FanMode
-        write_result = await self.write_fan_mode(endpoint, fan_mode_init)
+        write_result = await self.write_setting(endpoint, fan_mode_attr, fan_mode_init)
         write_status_success = (write_result == Status.Success) or (write_result == Status.InvalidInState)
         asserts.assert_true(write_status_success, f"FanMode write did not return a value of either SUCCESS or INVALID_IN_STATE ({write_result.name})")
 
         # Read FanMode back and verify written value
-        fan_mode_initial = await self.read_fan_mode(endpoint=endpoint)
+        fan_mode_initial = await self.read_setting(endpoint, fan_mode_attr)
         asserts.assert_in(fan_mode_initial, cluster.Enums.FanModeEnum, "FanMode read response doesn't contain a FanModeEnum")
 
         await attribute_subscription.start(self.default_controller, self.dut_node_id, endpoint)
@@ -155,17 +121,17 @@ class TC_FAN_3_1(MatterBaseTest):
 
                 # Iterate through the queue items to search for the FanMode attribute
                 for q_item in list(q.queue):
-                    if q_item.attribute == attr_to_monitor:
+                    if q_item.attribute == fan_mode_attr:
                         fan_mode_current = q_item.value
 
-                        if direction == DirectionEnum.Ascending:
+                        if order == OrderEnum.Ascending:
                             # Verify the current FanMode is greater than the previous FanMode
                             asserts.assert_greater(fan_mode_current, fan_mode_previous, "Current FanMode must be greater than previous FanMode")
                         else:
                             # Verify the current FanMode is less than the previous FanMode
                             asserts.assert_less(fan_mode_current, fan_mode_previous, "Current FanMode must be less than previous FanMode")
                             
-                        logging.info(f"[FANS] FanMode change from {fan_mode_previous.name}({fan_mode_previous}) to {fan_mode_current.name}({fan_mode_current})")
+                        logging.info(f"[FANS] FanMode changed from {fan_mode_previous.name}({fan_mode_previous}) to {fan_mode_current.name}({fan_mode_current})")
                         fan_mode_previous = fan_mode_current
                         break_out = True
                         break
@@ -177,23 +143,29 @@ class TC_FAN_3_1(MatterBaseTest):
                     break
 
         await attribute_subscription.cancel()
-
-
-
+        
+    def pics_TC_FAN_3_1(self) -> list[str]:
+        return ["FAN.S"]
 
     @async_test_body
     async def test_TC_FAN_3_1(self):
-
+        # Setup
         endpoint = self.get_endpoint(default=1)
-        attr_to_write = Clusters.FanControl.Attributes.PercentSetting
+        percent_setting_attr = Clusters.FanControl.Attributes.PercentSetting
+        speed_setting_attr = Clusters.FanControl.Attributes.SpeedSetting
         timeout_sec = 0.2
 
-        await self.verify_fan_mode(attr_to_write, endpoint, DirectionEnum.Ascending, timeout_sec)
-        await self.verify_fan_mode(attr_to_write, endpoint, DirectionEnum.Descending, timeout_sec)
-        
-        
-        
-        
+        # TH writes to the DUT the PercentSetting iteratively within a range of 1 to 100 one at a time in ascending order
+        await self.verify_fan_mode(endpoint, percent_setting_attr, OrderEnum.Ascending, timeout_sec)
+
+        # TH writes to the DUT the PercentSetting iteratively within a range of 1 to 100 one at a time in descending order
+        await self.verify_fan_mode(endpoint, percent_setting_attr, OrderEnum.Descending, timeout_sec)
+
+        # TH writes to the DUT the SpeedSetting iteratively within a range of 1 to SpeedMax one at a time in ascending order
+        await self.verify_fan_mode(endpoint, speed_setting_attr, OrderEnum.Ascending, timeout_sec)
+
+        # TH writes to the DUT the SpeedSetting iteratively within a range of 1 to SpeedMax one at a time in descending order
+        await self.verify_fan_mode(endpoint, speed_setting_attr, OrderEnum.Descending, timeout_sec)
 
 
 if __name__ == "__main__":
