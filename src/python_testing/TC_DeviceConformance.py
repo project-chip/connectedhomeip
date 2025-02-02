289b46722cfdad57da4d1a28273ced96848c2b1c
cecille <cecille@google.com>
2025-02-01 21:36:32 -0500

    Auto-select data model set based on specification version

--- src/python_testing/TC_DeviceConformance.py
+++ src/python_testing/TC_DeviceConformance.py
@@ -42,21 +42,20 @@ import chip.clusters as Clusters
 from chip.testing.basic_composition import BasicCompositionTests
 from chip.testing.choice_conformance import (evaluate_attribute_choice_conformance, evaluate_command_choice_conformance,
                                              evaluate_feature_choice_conformance)
-from chip.testing.conformance import ConformanceDecision, conformance_allowed
+from chip.testing.conformance import ConformanceDecision, ConformanceException, conformance_allowed
 from chip.testing.global_attribute_ids import (ClusterIdType, DeviceTypeIdType, GlobalAttributeIds, cluster_id_type,
                                                device_type_id_type, is_valid_device_type_id)
 from chip.testing.matter_testing import (AttributePathLocation, ClusterPathLocation, CommandPathLocation, DeviceTypePathLocation,
                                          MatterBaseTest, ProblemNotice, ProblemSeverity, async_test_body, default_matter_test_main)
-from chip.testing.spec_parsing import CommandType, build_xml_clusters, build_xml_device_types
+from chip.testing.spec_parsing import CommandType
 from chip.tlv import uint
+from mobly import asserts
 
 
 class DeviceConformanceTests(BasicCompositionTests):
     async def setup_class_helper(self):
         await super().setup_class_helper()
-        self.xml_clusters, self.problems = build_xml_clusters()
-        self.xml_device_types, problems = build_xml_device_types()
-        self.problems.extend(problems)
+        self.build_spec_xmls()
 
     def _get_device_type_id(self, device_type_name: str) -> int:
         id = [id for id, dt in self.xml_device_types.items() if dt.name.lower() == device_type_name.lower()]
--- src/python_testing/TC_pics_checker.py
+++ src/python_testing/TC_pics_checker.py
@@ -31,10 +31,7 @@ class TC_PICS_Checker(MatterBaseTest, BasicCompositionTests):
     async def setup_class(self):
         super().setup_class()
         await self.setup_class_helper(False)
-        # build_xml_cluster returns a list of issues found when paring the XML
-        # Problems in the XML shouldn't cause test failure, but we want them recorded
-        # so they are added to the list of problems that get output when the test set completes.
-        self.xml_clusters, self.problems = build_xml_clusters()
+        self.build_spec_xmls()
 
     def _check_and_record_errors(self, location, required, pics):
         if required and not self.check_pics(pics):
