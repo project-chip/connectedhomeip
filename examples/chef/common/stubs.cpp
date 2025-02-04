a0f55b9cd12b3ca60d3008149232576285e9d341
erwinpan1 <erwinpan1@gmail.com>
2025-02-04 17:20:32 +0800

    Add semicolon to fix compilation failure on MacOS

--- examples/chef/common/stubs.cpp
+++ examples/chef/common/stubs.cpp
@@ -336,7 +336,7 @@ void emberAfWakeOnLanClusterInitCallback(EndpointId endpoint)
 
 void ApplicationInit()
 {
-    ChipLogProgress(NotSpecified, "Chef Application Init !!!")
+    ChipLogProgress(NotSpecified, "Chef Application Init !!!");
 
 #ifdef MATTER_DM_PLUGIN_REFRIGERATOR_ALARM_SERVER
         // set Parent Endpoint and Composition Type for an Endpoint
@@ -354,7 +354,7 @@ void ApplicationInit()
 
 void ApplicationShutdown()
 {
-    ChipLogProgress(NotSpecified, "Chef Application Down !!!")
+    ChipLogProgress(NotSpecified, "Chef Application Down !!!");
 }
 
 // No-op function, used to force linking this file,
