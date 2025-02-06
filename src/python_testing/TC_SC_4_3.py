d99447e8ec41847344b6419e49b2b043ed97ef0f
cecille <cecille@google.com>
2025-02-06 10:21:30 -0500

    Fix imports (changed post 1.4, before this test was written)

--- src/python_testing/TC_SC_4_3.py
+++ src/python_testing/TC_SC_4_3.py
@@ -39,7 +39,7 @@ import logging
 import re
 
 import chip.clusters as Clusters
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+from matter_testing_support import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mdns_discovery.mdns_discovery import DNSRecordType, MdnsDiscovery, MdnsServiceType
 from mobly import asserts
 from zeroconf.const import _TYPE_AAAA, _TYPES
