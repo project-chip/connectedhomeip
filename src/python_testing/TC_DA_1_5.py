bbb911adb0090f3794501b217bef828ba0238c47
Oleksandr Sirko <wisenss@gmail.com>
2025-02-06 18:43:06 +0100

    isort

--- src/python_testing/TC_DA_1_5.py
+++ src/python_testing/TC_DA_1_5.py
@@ -40,9 +40,9 @@ import random
 import chip.clusters as Clusters
 from chip import ChipDeviceCtrl
 from chip.interaction_model import InteractionModelError, Status
-from chip.testing.matter_testing import MatterBaseTest, async_test_body, default_matter_test_main
 from chip.testing.conversions import hex_from_bytes
 from chip.testing.matchers import type_matches
+from chip.testing.matter_testing import MatterBaseTest, async_test_body, default_matter_test_main
 from chip.tlv import TLVReader
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
