5fdbc2180f223dc7afde8dc6306077690a348dac
Oleksandr Sirko <wisenss@gmail.com>
2025-02-06 18:37:26 +0100

    style fix

--- src/python_testing/matter_testing_infrastructure/chip/testing/matchers.py
+++ src/python_testing/matter_testing_infrastructure/chip/testing/matchers.py
@@ -14,6 +14,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+
 """Type matching utilities for Matter testing infrastructure.
 
 This module provides functionality for validating type compatibility between
