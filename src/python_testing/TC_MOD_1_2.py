cabfd794144def3067f7050ba932decdff3d5a0b
jtrejoespinoza-grid <juanerasmo@google.com>
2025-02-10 13:40:53 -0700

    Fix 16bit assert. Update to use self.endpoint

--- src/python_testing/TC_MOD_1_2.py
+++ src/python_testing/TC_MOD_1_2.py
@@ -91,8 +91,10 @@ class MOD_1_2(MatterBaseTest):
             logger.info(
                 "SupportedMode.semanticTags contains values, verifying attributes for manufacturedcode and value are not longer than 16bits int")
             for semantictag in supported_mode.semanticTags:
-                asserts.assert_true(semantictag.mfgCode <= self._16bitshex, "Element semantictag.Mfgcode is greater than 16 bits")
-                asserts.assert_true(semantictag.value <= self._16bitshex, "Element semantictag.Value is greater than 16 bits")
+                asserts.assert_true(semantictag >= 0 and semantictag.mfgCode <= self._16bitshex,
+                                    "Element semantictag.Mfgcode is greater than 16 bits")
+                asserts.assert_true(semantictag >= 0 and semantictag.value <= self._16bitshex,
+                                    "Element semantictag.Value is greater than 16 bits")
 
     def _log_attribute(self, name, value):
         logger.info(f"{name} attribute {value} with type {type(value)}")
@@ -112,7 +114,7 @@ class MOD_1_2(MatterBaseTest):
         # Verify contains attributes label and mode
         # Verify if semantic tags has elements in list , and if there are values assert the values
         self.step(2)
-        supported_modes = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.SupportedModes)
+        supported_modes = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.SupportedModes)
         logger.info(f"Supported modes {supported_modes}")
         # List must not be empty
         asserts.assert_true(len(supported_modes) > 0, "Supported modes can not be empty.")
@@ -128,24 +130,24 @@ class MOD_1_2(MatterBaseTest):
 
         # Currentmode attribute check must be int and must be in the supported modes values.
         self.step(3)
-        current_mode = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.CurrentMode)
+        current_mode = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.CurrentMode)
         self._log_attribute('CurrentMode', current_mode)
         asserts.assert_true(isinstance(current_mode, int), "Current mode is not int")
         asserts.assert_in(current_mode, supported_modes_values, f"Current mode {current_mode} is not in {supported_modes_values}")
 
-        # Onmode can be null
         self.step(4)
-        on_mode = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.OnMode)
-        # on mode can be null
+        on_mode = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.OnMode)
+        # On mode can be null
         self._log_attribute("OnMode", on_mode)
         asserts.assert_true((isinstance(on_mode, int) or isinstance(on_mode, Nullable)),
                             "Onmode is not int or is not clusters.Types.Nullable")
+        # verify that OnMode is in the list of Supported Modes, but if null, cant be verified.
         if not isinstance(on_mode, Nullable):
             asserts.assert_in(on_mode, supported_modes_values, f"Onmode {current_mode} is not in {supported_modes_values}")
 
         # Validate startup mode
         self.step(5)
-        startup_mode = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.StartUpMode)
+        startup_mode = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.StartUpMode)
         self._log_attribute("StartupMode", startup_mode)
         asserts.assert_true(isinstance(startup_mode, int), "Startupmode is not int")
         asserts.assert_in(startup_mode, supported_modes_values, f"Startupmode {current_mode} is not in {supported_modes_values}")
@@ -153,7 +155,7 @@ class MOD_1_2(MatterBaseTest):
         # Verify the string for ci is larger that 1 char.
         # If is non ci ask the user if can read and understand the string.
         self.step(6)
-        description = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.Description)
+        description = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.Description)
         self._log_attribute("Description", description)
         if self.is_ci:
             asserts.assert_true(isinstance(description, str), "Description attribute is not str")
@@ -164,12 +166,12 @@ class MOD_1_2(MatterBaseTest):
                                                      default_value="y")
             asserts.assert_true(user_response.lower() == 'y', "The description is not understandable to the user.")
 
-        # Verify the StandardNamespace can be 16bits integer or null
+        # Verify the StandardNamespace can be 16 bits enum or null
         self.step(7)
-        standard_namepace = await self.read_single_attribute_check_success(endpoint=1, cluster=self.cluster, attribute=self.cluster.Attributes.StandardNamespace)
+        standard_namepace = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.StandardNamespace)
         self._log_attribute("StandardNamespace", standard_namepace)
-        asserts.assert_true((isinstance(standard_namepace, Nullable) or (isinstance(standard_namepace, int) and standard_namepace <= self._16bitshex)),
-                            "Standard namespace is not 16bit or not nullable")
+        asserts.assert_true((isinstance(standard_namepace, Nullable) or (isinstance(standard_namepace, int) and (standard_namepace >= 0 and standard_namepace <= self._16bitshex))),
+                            "Standard namespace is not 16bit enum or not Nullable")
 
 
 if __name__ == "__main__":
