1f580f60e99c4e5fe15ef8dab1093850786c955c
Restyled.io <commits@restyled.io>
2025-02-18 16:42:54 +0000

    Restyled by autopep8

--- credentials/generate_revocation_set.py
+++ credentials/generate_revocation_set.py
@@ -560,7 +560,7 @@ class RESTDCLDClient(DCLDClientInterface):
     def get_approved_certificate(self, subject_name: x509.name.Name, skid_hex: str) -> tuple[bool, x509.Certificate]:
         '''
         Get certificate from DCL
-        
+
         Parameters
         ----------
         subject_name: x509.name.Name
