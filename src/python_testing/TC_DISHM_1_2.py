a669bbfef576d1c449b42a217a884c7607bdfb62
juandediosg <jdelgado@griddynamics.com>
2025-02-19 19:23:18 -0600

    Updated: Removed validations for disallowed attributes (OnMode and StartupMode) based on specs from Test Plan.

--- src/python_testing/TC_DISHM_1_2.py
+++ src/python_testing/TC_DISHM_1_2.py
@@ -57,9 +57,7 @@ class TC_DISHM_1_2(MatterBaseTest, ModeBaseClusterChecks):
         steps = [
             TestStep(1, "Commissioning, already done", is_commissioning=True),
             TestStep(2, "TH reads from the DUT the SupportedModes attribute."),
-            TestStep(3, "TH reads from the DUT the CurrentMode attribute."),
-            TestStep(4, "TH reads from the DUT the OnMode attribute."),
-            TestStep(5, "TH reads from the DUT the StartUpMode attribute.")
+            TestStep(3, "TH reads from the DUT the CurrentMode attribute.")
         ]
         return steps
 
@@ -91,18 +89,6 @@ class TC_DISHM_1_2(MatterBaseTest, ModeBaseClusterChecks):
         # Verify that the CurrentMode attribute has a valid value.
         mode = self.cluster.Attributes.CurrentMode
         await self.read_and_check_mode(endpoint=endpoint, mode=mode, supported_modes=supported_modes)
-
-        self.step(4)
-        # Verify that the OnMode attribute has a valid value or null.
-        mode = self.cluster.Attributes.OnMode
-        await self.read_and_check_mode(endpoint=endpoint, mode=mode,
-                                       supported_modes=supported_modes, is_nullable=True)
-
-        self.step(5)
-        # Verify that the StartUpMode has a valid value or null
-        mode = self.cluster.Attributes.StartUpMode
-        await self.read_and_check_mode(endpoint=endpoint, mode=mode,
-                                       supported_modes=supported_modes, is_nullable=True)
 
 
 if __name__ == "__main__":
