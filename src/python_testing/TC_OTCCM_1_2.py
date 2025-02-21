5eeb93d824db315bbd0ebad6346ffa16c3790b4f
juandediosg <jdelgado@griddynamics.com>
2025-02-20 18:38:41 -0600

    Fixed: Renamed CLUSTER to cluster_otccm_mode for clarity.

--- src/python_testing/TC_OTCCM_1_2.py
+++ src/python_testing/TC_OTCCM_1_2.py
@@ -40,7 +40,7 @@ import chip.clusters as Clusters
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from modebase_cluster_check import ModeBaseClusterChecks
 
-CLUSTER = Clusters.OvenMode
+cluster_otccm_mode = Clusters.OvenMode
 
 
 class TC_OTCCM_1_2(MatterBaseTest, ModeBaseClusterChecks):
@@ -48,7 +48,7 @@ class TC_OTCCM_1_2(MatterBaseTest, ModeBaseClusterChecks):
     def __init__(self, *args):
         MatterBaseTest.__init__(self, *args)
         ModeBaseClusterChecks.__init__(self,
-                                       modebase_derived_cluster=CLUSTER)
+                                       modebase_derived_cluster=cluster_otccm_mode)
 
     def desc_TC_OTCCM_1_2(self) -> str:
         return "[TC-OTCCM-1.2] Cluster attributes with DUT as Server"
@@ -81,8 +81,8 @@ class TC_OTCCM_1_2(MatterBaseTest, ModeBaseClusterChecks):
         # According to the spec, there should be at least one like
         # Bake, Convection, Grill, Roast, Clean, Convection Bake, Convection Roast, Warming, Proofing
         # tag in the ones supported.
-        additional_tags = [CLUSTER.Enums.ModeTag.kBake,
-                           CLUSTER.Enums.ModeTag.kConvection]
+        additional_tags = [cluster_otccm_mode.Enums.ModeTag.kBake,
+                           cluster_otccm_mode.Enums.ModeTag.kConvection]
         self.check_tags_in_lists(supported_modes=supported_modes, required_tags=additional_tags)
 
         self.step(3)
