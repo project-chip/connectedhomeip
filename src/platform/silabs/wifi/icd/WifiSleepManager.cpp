0201fc5d5787818bf7e53acbe3ef4d9327ff7e40
Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>
2025-02-24 18:25:13 -0500

    [Silabs] Finish WifiInterface function API cleanup (#37746)
    * Finish WifiInterface cleanup
    
    * Use C-Like Array
    
    * Update impl files
    
    * Update NetworkCommissioningWifiDriver for C-Like arrays
    
    * fix copy string
    
    * Fix has ipv6 address check

--- src/platform/silabs/wifi/icd/WifiSleepManager.cpp
+++ src/platform/silabs/wifi/icd/WifiSleepManager.cpp
@@ -148,11 +148,7 @@ CHIP_ERROR WifiSleepManager::VerifyAndTransitionToLowPowerMode(PowerEvent event)
         return CHIP_NO_ERROR;
     }
 
-    // TODO: Clean this up when the Wi-Fi interface re-work is finished
-    wfx_wifi_provision_t wifiConfig;
-    wfx_get_wifi_provision(&wifiConfig);
-
-    if (!(wifiConfig.ssid[0] != 0))
+    if (!IsWifiProvisioned())
     {
         return ConfigureDeepSleep();
     }
