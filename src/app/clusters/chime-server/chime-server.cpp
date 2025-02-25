4a35ca5c9e8d6afe843f7e3cd6d874256b003bb1
marcos <15697303+gmarcosb@users.noreply.github.com>
2025-02-25 13:06:40 -0700

    Implement chime delegate instance for all-clusters-app

--- src/app/clusters/chime-server/chime-server.cpp
+++ src/app/clusters/chime-server/chime-server.cpp
@@ -69,7 +69,7 @@ void ChimeServer::LoadPersistentAttributes()
 {
     // Load Active Chime ID
     uint8_t storedActiveChimeID = 0;
-    CHIP_ERROR err = GetSafeAttributePersistenceProvider()->ReadScalarValue(
+    CHIP_ERROR err              = GetSafeAttributePersistenceProvider()->ReadScalarValue(
         ConcreteAttributePath(GetEndpointId(), Chime::Id, ActiveChimeID::Id), storedActiveChimeID);
     if (err == CHIP_NO_ERROR)
     {
