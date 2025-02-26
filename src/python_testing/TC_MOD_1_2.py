1e2ddf69213ff7ce2f62ad405819940f00325803
jtrejoespinoza-grid <juanerasmo@google.com>
2025-02-26 14:57:51 -0700

    Fix typos

--- src/python_testing/TC_MOD_1_2.py
+++ src/python_testing/TC_MOD_1_2.py
@@ -22,21 +22,20 @@
 # test-runner-runs:
 #   run1:
 #     app: ${ALL_CLUSTERS_APP}
-#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:TRACE_APP.json
+#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
 #     script-args: >
 #       --storage-path admin_storage.json
 #       --commissioning-method on-network
 #       --discriminator 1234
 #       --passcode 20202021
 #       --PICS src/app/tests/suites/certification/ci-pics-values
-#       --trace-to json:TRACE_TEST_JSON.json
-#       --trace-to perfetto:TRACE_TEST_PERFETTO.perfetto
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #     factory-reset: true
 #     quiet: true
-# === END CI TEST ARGUMENTS ===""import typing
+# === END CI TEST ARGUMENTS ===""
 
 import logging
-
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
