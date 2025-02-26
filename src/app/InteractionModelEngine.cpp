33a06639e84e857d328008ae59cdcaa83d18e0cc
Restyled.io <commits@restyled.io>
2025-02-26 02:23:50 +0000

    Restyled by clang-format

--- src/app/InteractionModelEngine.cpp
+++ src/app/InteractionModelEngine.cpp
@@ -130,8 +130,7 @@ bool MayHaveAccessibleEventPath(DataModel::Provider * aProvider, const EventPath
 class AutoReleaseSubscriptionInfoIterator
 {
 public:
-    AutoReleaseSubscriptionInfoIterator(SubscriptionResumptionStorage::SubscriptionInfoIterator * iterator) :
-        mIterator(iterator) {};
+    AutoReleaseSubscriptionInfoIterator(SubscriptionResumptionStorage::SubscriptionInfoIterator * iterator) : mIterator(iterator){};
     ~AutoReleaseSubscriptionInfoIterator() { mIterator->Release(); }
 
     SubscriptionResumptionStorage::SubscriptionInfoIterator * operator->() const { return mIterator; }
