9b50a087864c430ce66fd0818791b2d56564bf7b
chirag-silabs <100861685+chirag-silabs@users.noreply.github.com>
2025-03-05 22:13:41 +0530

    Update src/platform/silabs/wifi/wiseconnect-interface/WiseconnectWifiInterface.h
    Co-authored-by: Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>

--- src/platform/silabs/wifi/wiseconnect-interface/WiseconnectWifiInterface.h
+++ src/platform/silabs/wifi/wiseconnect-interface/WiseconnectWifiInterface.h
@@ -37,7 +37,7 @@ public:
         kAPStop              = 3,
         kScan                = 4, /* This combines the scan start and scan result events  */
         kStationStartJoin    = 5,
-        kStationNetworkEvent = 6, /* This combines the DHCP for RS9116 and Notify for SiWx917 */
+        kConnectionComplete = 6, /* This combines the DHCP for RS9116 and Notify for SiWx917 */
         kStationDhcpDone     = 7,
         kStationDhcpPoll     = 8,
     };
