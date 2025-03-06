00584c228647f6d2667d0e307a2bae0468642265
Jeff Tung <100387939+jtung-apple@users.noreply.github.com>
2025-03-06 13:48:23 -0800

    Address review comments

--- src/darwin/Framework/CHIP/MTRDevice_Concrete.mm
+++ src/darwin/Framework/CHIP/MTRDevice_Concrete.mm
@@ -3949,7 +3949,8 @@ static BOOL AttributeHasChangesOmittedQuality(MTRAttributePath * attributePath)
     // Update data version used for subscription filtering
     MTRDeviceClusterData * clusterData = [self _clusterDataForPath:clusterPath];
     if (!clusterData) {
-        clusterData = [[MTRDeviceClusterData alloc] initWithDataVersion:dataVersion attributes:nil];
+        clusterData = [[MTRDeviceClusterData alloc] init];
+        clusterData.pendingDataVersion = dataVersion;
         dataVersionChanged = YES;
     } else if (![clusterData.dataVersion isEqualToNumber:dataVersion]) {
         clusterData.pendingDataVersion = dataVersion;
