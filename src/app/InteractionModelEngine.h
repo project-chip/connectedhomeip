0eed7450c0c693c72225c59fdec66f6f9b70767e
Yunhan Wang <yunhanw@google.com>
2025-03-07 12:06:47 -0800

    address comments

--- src/app/InteractionModelEngine.h
+++ src/app/InteractionModelEngine.h
@@ -236,8 +236,7 @@ public:
     /**
      *  Activate the idle subscriptions.
      *
-     *  When subscribing to ICD and liveness timeout reached, the read client will move to `InactiveICDSubscription` state and
-     * resubscription can be triggered via OnActiveModeNotification().
+     *  See ReadClient::OnActiveModeNotification
      */
     void OnActiveModeNotification(ScopedNodeId aPeer);
 
