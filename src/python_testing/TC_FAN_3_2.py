bbb712fe4c67ae7254d35ee60d88c71b8d642930
Raul Marquez <rmarquez@csa-iot.org>
2025-03-13 11:59:53 -0700

    restyled/lint

--- src/python_testing/TC_FAN_3_2.py
+++ src/python_testing/TC_FAN_3_2.py
@@ -37,18 +37,15 @@
 
 import logging
 import math
-import random
-import time
 from typing import Any
 
 import chip.clusters as Clusters
 from chip.clusters import ClusterObjects as ClusterObjects
 from chip.interaction_model import Status
 from chip.testing.matter_asserts import is_valid_uint_value
-from chip.testing.matter_testing import (ClusterAttributeChangeAccumulator, MatterBaseTest, TestStep, AttributeValue,
-                                         async_test_body, default_matter_test_main)
+from chip.testing.matter_testing import (AttributeValue, ClusterAttributeChangeAccumulator, MatterBaseTest,
+                                         TestStep, async_test_body, default_matter_test_main)
 from mobly import asserts
-
 
 logger = logging.getLogger(__name__)
 
