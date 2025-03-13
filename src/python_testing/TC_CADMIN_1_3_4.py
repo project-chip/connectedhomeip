e903178db0bd226b2d61c1a02fd55b03e8e92800
Oleksandr Sirko <wisenss@gmail.com>
2025-03-13 14:54:51 +0100

    restyled

--- src/python_testing/TC_CADMIN_1_3_4.py
+++ src/python_testing/TC_CADMIN_1_3_4.py
@@ -51,9 +51,8 @@ import chip.clusters as Clusters
 from chip import ChipDeviceCtrl
 from chip.exceptions import ChipStackError
 from chip.interaction_model import Status
+from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from chip.tlv import TLVReader
-from chip.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body,
-                                                                       default_matter_test_main)
 from mdns_discovery import mdns_discovery
 from mobly import asserts
 
