ab1eb5dcd35a9d31bf34d31719535024b067fda6
Restyled.io <commits@restyled.io>
2025-03-18 14:34:29 +0000

    Restyled by clang-format

--- src/platform/nxp/common/ota/OTAImageProcessorImpl.cpp
+++ src/platform/nxp/common/ota/OTAImageProcessorImpl.cpp
@@ -396,8 +396,7 @@ void OTAImageProcessorImpl::HandleApply(intptr_t context)
      * into account queued actions, e.g. sending events to a subscription.
      */
     SystemLayer().StartTimer(
-        chip::System::Clock::Milliseconds32(imageProcessor->mDelayBeforeRebootSec * 1000
-                                            + CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY),
+        chip::System::Clock::Milliseconds32(imageProcessor->mDelayBeforeRebootSec * 1000 + CHIP_DEVICE_LAYER_OTA_REBOOT_DELAY),
         [](chip::System::Layer *, void *) { OtaHookReset(); }, nullptr);
 }
 
