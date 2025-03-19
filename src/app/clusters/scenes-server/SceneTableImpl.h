9934b774582063e3741e255521dacc09b034dcd1
marcos <15697303+gmarcosb@users.noreply.github.com>
2025-03-19 13:38:57 -0600

    Move out common fabric-scoped table-like storage from SceneTableImpl to a templatized class, chip::common::FabricTableImpl, to allow re-use in other clusters (specifically, TlsClientManagementServer)

--- src/app/clusters/scenes-server/SceneTableImpl.h
+++ src/app/clusters/scenes-server/SceneTableImpl.h
@@ -9,6 +9,7 @@
  *        http://www.apache.org/licenses/LICENSE-2.0
  *
  *    Unless required by applicable law or agreed to in writing, software
+ *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
@@ -19,6 +20,7 @@
 #include <app/clusters/scenes-server/ExtensionFieldSetsImpl.h>
 #include <app/clusters/scenes-server/SceneHandlerImpl.h>
 #include <app/clusters/scenes-server/SceneTable.h>
+#include <app/common/FabricTableImpl.h>
 #include <app/util/attribute-storage.h>
 #include <app/util/config.h>
 #include <lib/core/DataModelTypes.h>
@@ -49,25 +51,18 @@ static constexpr uint16_t kMaxScenesPerFabric = (kMaxScenesPerEndpoint - 1) / 2;
  * It is meant to be used exclusively when the scene cluster is enable for at least one endpoint
  * on the device.
  */
-class DefaultSceneTableImpl : public SceneTable<scenes::ExtensionFieldSetsImpl>
+using SceneTableBase = SceneTable<scenes::ExtensionFieldSetsImpl>;
+class DefaultSceneTableImpl
+    : public SceneTableBase,
+      public app::common::FabricTableImpl<SceneTableBase::SceneStorageId, SceneTableBase::SceneData, kIteratorsMax>
 {
 public:
-    DefaultSceneTableImpl() {}
+    using Super = app::common::FabricTableImpl<SceneTableBase::SceneStorageId, SceneTableBase::SceneData, kIteratorsMax>;
+
+    DefaultSceneTableImpl() : Super(kMaxScenesPerFabric, kMaxScenesPerEndpoint) {}
     ~DefaultSceneTableImpl() { Finish(); };
 
-    CHIP_ERROR Init(PersistentStorageDelegate * storage) override;
     void Finish() override;
-
-    // Scene count
-    CHIP_ERROR GetEndpointSceneCount(uint8_t & scene_count) override;
-    CHIP_ERROR GetFabricSceneCount(FabricIndex fabric_index, uint8_t & scene_count) override;
-
-    // Data
-    CHIP_ERROR GetRemainingCapacity(FabricIndex fabric_index, uint8_t & capacity) override;
-    CHIP_ERROR SetSceneTableEntry(FabricIndex fabric_index, const SceneTableEntry & entry) override;
-    CHIP_ERROR GetSceneTableEntry(FabricIndex fabric_index, SceneStorageId scene_id, SceneTableEntry & entry) override;
-    CHIP_ERROR RemoveSceneTableEntry(FabricIndex fabric_index, SceneStorageId scene_id) override;
-    CHIP_ERROR RemoveSceneTableEntryAtPosition(EndpointId endpoint, FabricIndex fabric_index, SceneIndex scene_idx) override;
 
     // Groups
     CHIP_ERROR GetAllSceneIdsInGroup(FabricIndex fabric_index, GroupId group_id, Span<SceneId> & scene_list) override;
@@ -82,58 +77,15 @@ public:
     CHIP_ERROR SceneSaveEFS(SceneTableEntry & scene) override;
     CHIP_ERROR SceneApplyEFS(const SceneTableEntry & scene) override;
 
-    // Fabrics
-    CHIP_ERROR RemoveFabric(FabricIndex fabric_index) override;
-    CHIP_ERROR RemoveEndpoint() override;
-
-    // Iterators
-    SceneEntryIterator * IterateSceneEntries(FabricIndex fabric_index) override;
-
-    void SetEndpoint(EndpointId endpoint);
-    void SetTableSize(uint16_t endpointSceneTableSize);
-    bool IsInitialized() { return (mStorage != nullptr); }
-
 protected:
     // This constructor is meant for test purposes, it allows to change the defined max for scenes per fabric and global, which
     // allows to simulate OTA where this value was changed
     DefaultSceneTableImpl(uint16_t maxScenesPerFabric, uint16_t maxScenesPerEndpoint) :
-        mMaxScenesPerFabric(maxScenesPerFabric), mMaxScenesPerEndpoint(maxScenesPerEndpoint)
+        FabricTableImpl(maxScenesPerFabric, maxScenesPerEndpoint)
     {}
 
     // Global scene count
-    CHIP_ERROR SetEndpointSceneCount(const uint8_t & scene_count);
-
-    // wrapper function around emberAfGetClustersFromEndpoint to allow override when testing
-    virtual uint8_t GetClustersFromEndpoint(ClusterId * clusterList, uint8_t listLen);
-
-    // wrapper function around emberAfGetClusterCountForEndpoint to allow override when testing
-    virtual uint8_t GetClusterCountFromEndpoint();
-
-    class SceneEntryIteratorImpl : public SceneEntryIterator
-    {
-    public:
-        SceneEntryIteratorImpl(DefaultSceneTableImpl & provider, FabricIndex fabricIdx, EndpointId endpoint,
-                               uint16_t maxScenesPerFabric, uint16_t maxScenesPerEndpoint);
-        size_t Count() override;
-        bool Next(SceneTableEntry & output) override;
-        void Release() override;
-
-    protected:
-        DefaultSceneTableImpl & mProvider;
-        FabricIndex mFabric  = kUndefinedFabricIndex;
-        EndpointId mEndpoint = kInvalidEndpointId;
-        SceneIndex mNextSceneIdx;
-        SceneIndex mSceneIndex = 0;
-        uint8_t mTotalScenes   = 0;
-        uint16_t mMaxScenesPerFabric;
-        uint16_t mMaxScenesPerEndpoint;
-    };
-
-    uint16_t mMaxScenesPerFabric               = kMaxScenesPerFabric;
-    uint16_t mMaxScenesPerEndpoint             = kMaxScenesPerEndpoint;
-    EndpointId mEndpointId                     = kInvalidEndpointId;
-    chip::PersistentStorageDelegate * mStorage = nullptr;
-    ObjectPool<SceneEntryIteratorImpl, kIteratorsMax> mSceneEntryIterators;
+    inline CHIP_ERROR SetEndpointSceneCount(const uint8_t & scene_count) { return SetEndpointEntryCount(scene_count); }
 }; // class DefaultSceneTableImpl
 
 /// @brief Gets a pointer to the instance of Scene Table Impl, providing EndpointId and Table Size for said endpoint
