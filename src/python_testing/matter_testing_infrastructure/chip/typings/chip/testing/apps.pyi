ab2986062d5ffb2ad3fa0148e7becb216514c619
Oleksandr Sirko <wisenss@gmail.com>
2025-03-20 17:00:13 +0100

    mypy type stubs for apps and tasks

---
+++ src/python_testing/matter_testing_infrastructure/chip/typings/chip/testing/apps.pyi
@@ -0,0 +1,29 @@
+from typing import Any, Callable, Optional
+
+import os
+import signal
+import tempfile
+from chip.testing.tasks import Subprocess
+
+
+class AppServerSubprocess(Subprocess):
+    PREFIX: bytes
+
+    def __init__(self, app: str, storage_dir: str, discriminator: int,
+                 passcode: int, port: int = ...) -> None: ...
+
+    def __del__(self) -> None: ...
+
+    kvs_fd: int
+
+
+class IcdAppServerSubprocess(AppServerSubprocess):
+    paused: bool
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
+
+    def pause(self, check_state: bool = ...) -> None: ...
+
+    def resume(self, check_state: bool = ...) -> None: ...
+
+    def terminate(self) -> None: ...
