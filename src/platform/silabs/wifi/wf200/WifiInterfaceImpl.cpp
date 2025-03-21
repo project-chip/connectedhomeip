6bc96334e2452199295d56631b806fc2c00a918e
Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>
2025-03-21 11:39:18 -0500

    [Silabs] Refactor WifiSleepManager dependecies to enable unit-tests (#37923)
    * Refactor WifiSleepManager dependecies to enable unit-tests on the manager
    
    * Update the wf200 and rs9116 header
    
    * Address review comments
    
    * Uncomment remove high performance req in configuration
    
    * Disable gn logs when using all platform tests
    
    * Remove unused inlude
    
    * Add missing define guards

--- src/platform/silabs/wifi/wf200/WifiInterfaceImpl.cpp
+++ src/platform/silabs/wifi/wf200/WifiInterfaceImpl.cpp
@@ -750,7 +750,7 @@ CHIP_ERROR WifiInterfaceImpl::GetAccessPointExtendedInfo(wfx_wifi_scan_ext_t & i
 }
 
 #if CHIP_CONFIG_ENABLE_ICD_SERVER
-CHIP_ERROR WifiInterfaceImpl::ConfigurePowerSave()
+CHIP_ERROR WifiInterfaceImpl::ConfigurePowerSave(PowerSaveInterface::PowerSaveConfiguration configuration, uint32_t listenInterval)
 {
     // TODO: Implement Power save configuration. We do a silent failure to avoid causing problems in higher layers.
     return CHIP_NO_ERROR;
@@ -833,12 +833,19 @@ CHIP_ERROR WifiInterfaceImpl::ConnectToAccessPoint(void)
     return CHIP_NO_ERROR;
 }
 
-#if CHIP_DEVICE_CONFIG_ENABLE_IPV4
 bool WifiInterfaceImpl::HasAnIPv4Address()
 {
-    return (sta_ip == STA_IP_FAIL) ? false : true;
-}
+    bool hasIPv4 = false;
+
+#if CHIP_DEVICE_CONFIG_ENABLE_IPV4
+    if (sta_ip != STA_IP_FAIL)
+    {
+        hasIPv4 = true;
+    }
 #endif // CHIP_DEVICE_CONFIG_ENABLE_IPV4
+
+    return hasIPv4;
+}
 
 bool WifiInterfaceImpl::HasAnIPv6Address()
 {
