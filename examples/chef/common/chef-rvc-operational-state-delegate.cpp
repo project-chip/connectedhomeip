1fb610cb99f9a483bc4ae38b9f3dddcac9794823
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-03-22 21:35:11 -0700

    Changes to this attribute merely due to the normal passage of time with no other dynamic change of device state SHALL NOT be reported.

--- examples/chef/common/chef-rvc-operational-state-delegate.cpp
+++ examples/chef/common/chef-rvc-operational-state-delegate.cpp
@@ -127,6 +127,7 @@ void RvcOperationalStateDelegate::HandleResumeStateCallback(GenericOperationalEr
     auto error = gRvcOperationalStateInstance->SetOperationalState(to_underlying(OperationalState::OperationalStateEnum::kRunning));
     if (error == CHIP_NO_ERROR)
     {
+        GetInstance()->UpdateCountdownTimeFromDelegate();
         err.Set(to_underlying(OperationalState::ErrorStateEnum::kNoError));
     }
     else
@@ -269,6 +270,7 @@ static void onOperationalStateTimerTick(System::Layer * systemLayer, void * data
                 static_cast<uint32_t>(gRvcOperationalStateDelegate->kExampleCountDown));
             gRvcOperationalStateDelegate->mRunningTime = 0;
             gRvcOperationalStateDelegate->mPausedTime  = 0;
+            instance->UpdateCountdownTimeFromDelegate();
         }
         else
         { // kPaused
@@ -291,11 +293,6 @@ static void onOperationalStateTimerTick(System::Layer * systemLayer, void * data
 
     ChipLogDetail(DeviceLayer, "RVC timer tick: Current state = %d. CountdownTime = %d. PauseTime = %d. RunningTime = %d.", state,
                   gRvcOperationalStateDelegate->mCountdownTime.Value(), mPausedTime, mRunningTime);
-    if (state == OperationalState::OperationalStateEnum::kRunning)
-    {
-        // Reported CountDownTime is the remaining time to run = mCountdownTime.Value() - mRunningTime.
-        GetInstance()->UpdateCountdownTimeFromDelegate();
-    }
 
     if (gRvcOperationalStateDelegate->mCountdownTime.Value() > mRunningTime)
     {
@@ -321,6 +318,7 @@ static void onOperationalStateTimerTick(System::Layer * systemLayer, void * data
             gRvcOperationalStateDelegate->mRunningTime = 0;
             gRvcOperationalStateDelegate->mPausedTime  = 0;
             gRvcOperationalStateDelegate->mCountdownTime.SetNull();
+            instance->UpdateCountdownTimeFromDelegate();
 
 #ifdef MATTER_DM_PLUGIN_RVC_RUN_MODE_SERVER
             getRvcRunModeInstance()->UpdateCurrentMode(RvcRunMode::ModeIdle);
