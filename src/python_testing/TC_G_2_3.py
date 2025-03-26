de6188c3173d30f5974915db0ec1c9834ed140dd
Gibran Vargas <gvargas@csa-iot.org>
2025-03-26 05:55:26 -0700

    chore(TC-G-2.3): restyled by isort

--- src/python_testing/TC_G_2_3.py
+++ src/python_testing/TC_G_2_3.py
@@ -39,10 +39,10 @@ import logging
 from typing import List
 
 import chip.clusters as Clusters
+from chip.clusters.Types import Nullable
 from chip.interaction_model import InteractionModelError, Status
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
-from chip.clusters.Types import Nullable
 
 class TC_G_2_3(MatterBaseTest):
 
