6170a1fbdea6ca0acf9ae75f36e13404b2022aa9
Anton Usmansky <j0tunn@ya.ru>
2025-03-28 19:20:40 +0300

    fix: avoid implicit deletion of ChipDeviceEvent default constructor

--- src/include/platform/CHIPDeviceEvent.h
+++ src/include/platform/CHIPDeviceEvent.h
@@ -408,7 +408,7 @@ struct ChipDeviceEvent final
 
     union
     {
-        ChipDevicePlatformEvent Platform;
+        ChipDevicePlatformEvent Platform = {};
         LambdaBridge LambdaEvent;
         struct
         {
