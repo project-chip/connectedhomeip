065bd97ee953ad0a6d0df465732bccef27db20fd
Alami-Amine <aalami90@gmail.com>
2025-03-31 18:14:55 +0000

    Adding comments

--- src/app/WriteClient.h
+++ src/app/WriteClient.h
@@ -429,8 +429,7 @@ private:
             outEncodedItemCount++;
         }
 
-        ReturnErrorOnFailure(EnsureListEnded());
-        return CHIP_NO_ERROR;
+        return EnsureListEnded();
     }
 
     /**
