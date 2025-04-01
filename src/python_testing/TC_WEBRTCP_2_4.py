43e8b3deab4d5d9a426f630019ffd6282179b8d4
Restyled.io <commits@restyled.io>
2025-04-01 19:38:39 +0000

    Restyled by autopep8

--- src/python_testing/TC_WEBRTCP_2_4.py
+++ src/python_testing/TC_WEBRTCP_2_4.py
@@ -265,7 +265,6 @@ from chip.webrtc.types import PeerDisconnectedCallback_t
 from chip.webrtc.types import StatsCallback_t
 
 
-
 class TC_WebRTCP_2_4(MatterBaseTest):
 
     def steps_TC_WEBRTC_2_4(self) -> list[TestStep]:
@@ -283,7 +282,7 @@ class TC_WebRTCP_2_4(MatterBaseTest):
 
     @async_test_body
     async def test_TC_WEBRTC_2_4(self):
-        
+
         def on_answer(answer, peer):
             print("on_answer called")
 
@@ -299,9 +298,9 @@ class TC_WebRTCP_2_4(MatterBaseTest):
 
         def on_disconnected(peer):
             print("on_disconnected called")
-            if(peer == 1):
+            if (peer == 1):
                 webrtc.GetStats(client1)
-            elif(peer == 2):
+            elif (peer == 2):
                 webrtc.GetStats(client2)
 
         def on_stats(stats, peer):
@@ -316,7 +315,8 @@ class TC_WebRTCP_2_4(MatterBaseTest):
 
         client = chip.webrtc.CreateWebrtcClient(1)
 
-        chip.webrtc.SetCallbacks(client, answer_callback, ice_callback, error_callback, peer_connected_callback, peer_disconnected_callback, stats_callback)
+        chip.webrtc.SetCallbacks(client, answer_callback, ice_callback, error_callback,
+                                 peer_connected_callback, peer_disconnected_callback, stats_callback)
 
         chip.webrtc.InitialiseConnection(client)
 
@@ -331,18 +331,19 @@ class TC_WebRTCP_2_4(MatterBaseTest):
         provider = Clusters.Objects.WebRTCTransportProvider
         current_sessions = await self.default_controller.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions)])
 
-        asserts.assert_equal(len(current_sessions[endpoint][Clusters.Objects.WebRTCTransportProvider][Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions]), 0, "All Webrtc sessions should be closed")
+        asserts.assert_equal(len(current_sessions[endpoint][Clusters.Objects.WebRTCTransportProvider]
+                             [Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions]), 0, "All Webrtc sessions should be closed")
 
-        self.print_step(2, "Send the ProvideOffer command with a valid WebRTCSessionID") 
+        self.print_step(2, "Send the ProvideOffer command with a valid WebRTCSessionID")
 
         try:
             provide_offer_response_2: Clusters.WebRTCTransportProvider.Commands.ProvideOfferResponse = await self.send_single_cmd(
                 cmd=Clusters.WebRTCTransportProvider.Commands.ProvideOffer(
-                    webRTCSessionID = 0,
+                    webRTCSessionID=0,
                     sdp="v=0\r\n",
                     streamUsage=Clusters.WebRTCTransportProvider.Enums.StreamUsageEnum.kLiveView,
-                    videoStreamID = NullValue,
-                    audioStreamID = NullValue,
+                    videoStreamID=NullValue,
+                    audioStreamID=NullValue,
                 ), endpoint=endpoint
             )
         except InteractionModelError as e:
@@ -350,37 +351,40 @@ class TC_WebRTCP_2_4(MatterBaseTest):
                 e.status, Status.NotFound, "Unexpected error returned")
             pass
 
-        self.print_step(3, "send the ProvideOffer command with a null WebRTCSessionID, a null VideoStreamID, a null AudioStreamID, and valid values for the other parameters") 
+        self.print_step(
+            3, "send the ProvideOffer command with a null WebRTCSessionID, a null VideoStreamID, a null AudioStreamID, and valid values for the other parameters")
 
         provide_offer_response_3: Clusters.WebRTCTransportProvider.Commands.ProvideOfferResponse = await self.send_single_cmd(
             cmd=Clusters.WebRTCTransportProvider.Commands.ProvideOffer(
-                webRTCSessionID = NullValue,
+                webRTCSessionID=NullValue,
                 sdp=offer,
                 streamUsage=Clusters.WebRTCTransportProvider.Enums.StreamUsageEnum.kLiveView,
-                videoStreamID = NullValue,
-                audioStreamID = NullValue,
+                videoStreamID=NullValue,
+                audioStreamID=NullValue,
             ), endpoint=endpoint
         )
 
         asserts.assert_equal(provide_offer_response_3.webRTCSessionID >= 0, True, "Invalid response")
         OldSessionId = provide_offer_response_3.webRTCSessionID
 
-        self.print_step(4, "Read CurrentSessions attribute from WebRTC Transport Provider Cluster on TH_SERVER") 
+        self.print_step(4, "Read CurrentSessions attribute from WebRTC Transport Provider Cluster on TH_SERVER")
 
         current_sessions_ = await self.default_controller.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions)])
 
-        asserts.assert_equal(bool(current_sessions_[endpoint][Clusters.Objects.WebRTCTransportProvider][Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions]), True, "No existing webrtc session")
+        asserts.assert_equal(bool(current_sessions_[endpoint][Clusters.Objects.WebRTCTransportProvider]
+                             [Clusters.Objects.WebRTCTransportProvider.Attributes.CurrentSessions]), True, "No existing webrtc session")
 
-        self.print_step(5, "send the ProvideOffer command with saved WebRTCSessionID in step 4 + 1, and valid values for the other parameters")
-        
+        self.print_step(
+            5, "send the ProvideOffer command with saved WebRTCSessionID in step 4 + 1, and valid values for the other parameters")
+
         try:
             provide_offer_response_5: Clusters.WebRTCTransportProvider.Commands.ProvideOfferResponse = await self.send_single_cmd(
                 cmd=Clusters.WebRTCTransportProvider.Commands.ProvideOffer(
-                    webRTCSessionID = OldSessionId + 1,
+                    webRTCSessionID=OldSessionId + 1,
                     sdp=offer,
                     streamUsage=Clusters.WebRTCTransportProvider.Enums.StreamUsageEnum.kLiveView,
-                    videoStreamID = NullValue,
-                    audioStreamID = NullValue,
+                    videoStreamID=NullValue,
+                    audioStreamID=NullValue,
                 ), endpoint=endpoint
             )
         except InteractionModelError as e:
@@ -392,11 +396,11 @@ class TC_WebRTCP_2_4(MatterBaseTest):
 
         provide_offer_response_6: Clusters.WebRTCTransportProvider.Commands.ProvideOfferResponse = await self.send_single_cmd(
             cmd=Clusters.WebRTCTransportProvider.Commands.ProvideOffer(
-                webRTCSessionID = OldSessionId,
+                webRTCSessionID=OldSessionId,
                 sdp=offer,
                 streamUsage=Clusters.WebRTCTransportProvider.Enums.StreamUsageEnum.kLiveView,
-                videoStreamID = NullValue,
-                audioStreamID = NullValue,
+                videoStreamID=NullValue,
+                audioStreamID=NullValue,
             ), endpoint=endpoint
         )
 
