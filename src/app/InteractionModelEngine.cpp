2d5341d71033ebbc7e400c106c74db238f876074
Restyled.io <commits@restyled.io>
2025-04-01 02:53:05 +0000

    Restyled by clang-format

--- src/app/InteractionModelEngine.cpp
+++ src/app/InteractionModelEngine.cpp
@@ -1080,8 +1080,8 @@ void InteractionModelEngine::OnActiveModeNotification(ScopedNodeId aPeer, uint64
 
         mpFabricTable->FetchCATs(pListItem->GetFabricIndex(), cats);
         if (ScopedNodeId(pListItem->GetPeerNodeId(), pListItem->GetFabricIndex()) == aPeer &&
-           (cats.CheckSubjectAgainstCATs(aMonitoredSubject) ||
-            aMonitoredSubject == mpFabricTable->FindFabricWithIndex(pListItem->GetFabricIndex()->GetNodeId())))
+            (cats.CheckSubjectAgainstCATs(aMonitoredSubject) ||
+             aMonitoredSubject == mpFabricTable->FindFabricWithIndex(pListItem->GetFabricIndex()->GetNodeId())))
         {
             pListItem->OnActiveModeNotification();
         }
