6750c78c6c79f25f9bf57e8a8e2e026c6fb4c3e8
Restyled.io <commits@restyled.io>
2025-04-01 16:35:27 +0000

    Restyled by autopep8

--- src/controller/python/chip/webrtc/callbacks.py
+++ src/controller/python/chip/webrtc/callbacks.py
@@ -24,25 +24,29 @@ def on_ice(candidates, peer):
     if future_ice is not None:
         future_ice.set_result(candidates)
 
+
 def on_offer(offer, peer):
     print("on_offer called")
     offer_string = offer.decode('utf-8')
     print(offer_string)
     if future_offer is not None:
         future_offer.set_result(offer_string)
-   
+
+
 def on_answer(answer, peer):
     print("on_answer called")
 
+
 def on_error(error, peer):
     print("on_error called")
+
 
 def on_stats(stats, peer):
     print(stats)
 
 
 offer_callback = SdpOfferCallback_t(on_offer)
-ice_callback = IceCallback_t(on_ice) 
+ice_callback = IceCallback_t(on_ice)
 answer_callback = SdpAnswerCallback_t(on_answer)
 error_callback = ErrorCallback_t(on_error)
-stats_callback = StatsCallback_t(on_stats) 
+stats_callback = StatsCallback_t(on_stats)
