857d30a4e8c07bb8e598cbbc9254d2f75f07dadd
Arkadiusz Bokowy <a.bokowy@samsung.com>
2025-04-01 11:11:14 +0200

    Update wheel names

--- src/test_driver/linux-cirque/MobileDeviceTest.py
+++ src/test_driver/linux-cirque/MobileDeviceTest.py
@@ -92,11 +92,11 @@ class TestPythonController(CHIPVirtualHome):
         req_device_id = req_ids[0]
 
         self.execute_device_cmd(req_device_id, "pip3 install --break-system-packages {}".format(os.path.join(
-            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/chip_clusters-0.0-py3-none-any.whl")))
+            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/matter_clusters-0.0-py3-none-any.whl")))
         self.execute_device_cmd(req_device_id, "pip3 install --break-system-packages {}".format(os.path.join(
-            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/chip_core-0.0-cp37-abi3-linux_x86_64.whl")))
+            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/matter_core-0.0-cp37-abi3-linux_x86_64.whl")))
         self.execute_device_cmd(req_device_id, "pip3 install --break-system-packages {}".format(os.path.join(
-            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/chip_repl-0.0-py3-none-any.whl")))
+            CHIP_REPO, "out/debug/linux_x64_gcc/controller/python/matter_repl-0.0-py3-none-any.whl")))
 
         command = ("gdb -batch -return-child-result -q -ex run -ex \"thread apply all bt\" "
                    "--args python3 {} -t 300 -a {} --paa-trust-store-path {}").format(
