ce2be3b0a90ebb10500c38d1ce70f0d75cbd1dad
abhishek.h <abhishek.h@samsung.com>
2025-04-04 15:57:05 +0530

    Support cross-compilation for arm
    - Needed to build camera controller for TH
    - need environment variable SYSROOT_AARCH64 to be declared
    
    Signed-off-by: abhishek.h <abhishek.h@samsung.com>

--- third_party/libdatachannel/scripts/build_libdatachannel.py
+++ third_party/libdatachannel/scripts/build_libdatachannel.py
@@ -2,9 +2,17 @@
 import os
 import subprocess
 import sys
+import optparse
 
+def main(argv):
+    parser = optparse.OptionParser()
+    parser.add_option('--target_cpu_type', type=str, default='x64')
 
-def main():
+    #get arguments
+    options, _ = parser.parse_args(argv)
+    target_cpu_type = options.target_cpu_type
+    print(f"Using CPU Type={target_cpu_type}")
+
     # Get the script directory
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -26,6 +34,27 @@ def main():
         "-DBUILD_SHARED_LIBS=OFF"
     ]
 
+    if target_cpu_type.startswith('arm'):
+        sysroot_aarch64 = SysRootPath('SYSROOT_AARCH64')
+        cmake_cmd = [
+            "cmake",
+            "-B", "build",
+            "-G", "Ninja",
+            "-DUSE_GNUTLS=0",
+            "-DUSE_NICE=0",
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DCMAKE_CXX_FLAGS=-Wno-shadow",
+            "-DBUILD_SHARED_LIBS=OFF",
+            "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+            "-DCMAKE_SYSTEM_NAME=Linux",
+            "-DCMAKE_SYSTEM_PROCESSOR=aarch64",
+            "-DCMAKE_C_COMPILER=clang",
+            "-DCMAKE_CXX_COMPILER=clang++",
+            "-DCMAKE_C_COMPILER_TARGET=aarch64-linux-gnu",
+            "-DCMAKE_CXX_COMPILER_TARGET=aarch64-linux-gnu",
+            f"-DCMAKE_SYSROOT={sysroot_aarch64}",
+        ]
+
     print(f"Running: {' '.join(cmake_cmd)}")
     result = subprocess.run(cmake_cmd, check=True)
 
@@ -35,15 +64,24 @@ def main():
     print(f"Changed directory to: {build_dir}")
 
     # Build with Make
-    make_cmd = ["make", "-j2"]
-    print(f"Running: {' '.join(make_cmd)}")
-    result = subprocess.run(make_cmd, check=True)
+    build_cmd = ["make", f"-j{os.cpu_count()}"]
+    if target_cpu_type.startswith('arm'):
+        #build with ninja
+        build_cmd = ["ninja", "-C", "."]
+
+    print(f"Running: {' '.join(build_cmd)}")
+    result = subprocess.run(build_cmd, check=True)
 
     print("libdatachannel build complete.")
     print(f"Artifacts are located in: {build_dir}")
 
     return 0
 
+def SysRootPath(name):
+    if name not in os.environ:
+        raise Exception('Missing environment variable "%s"' % name)
+    return os.environ[name]
+
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
