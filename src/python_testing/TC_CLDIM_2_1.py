b9f593de70ad399af2e2c9e8002340b4b6180a5a
Søren Møller Rasmussen <soeren.rasmussen@velux.com>
2025-04-04 13:55:04 +0200

    Updated variable naming

--- src/python_testing/TC_CLDIM_2_1.py
+++ src/python_testing/TC_CLDIM_2_1.py
@@ -95,106 +95,106 @@ class TC_CLDIM_2_1(MatterBaseTest):
         # STEP 3: Read CurrentState attribute
         self.step(3)
         if attributes.CurrentState.attribute_id in attribute_list:
-            kCurrentState = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.CurrentState)
-            if kCurrentState is not NullValue:
+            current_state = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.CurrentState)
+            if current_state is not NullValue:
                 if is_positioning_supported:
-                    asserts.assert_true(0 <= kCurrentState.position <= 10000, "Position is not in the expected range")
+                    asserts.assert_true(0 <= current_state.position <= 10000, "Position is not in the expected range")
                 if is_latching_supported:
-                    asserts.assert_is_instance(kCurrentState.Latch, bool, "Latch is not a boolean")
+                    asserts.assert_is_instance(current_state.Latch, bool, "Latch is not a boolean")
                 if is_speed_supported:
-                    asserts.assert_true(0 <= kCurrentState.speed <= 3, "Speed is not in the expected range")
+                    asserts.assert_true(0 <= current_state.speed <= 3, "Speed is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 4: Read Target attribute
         self.step(4)
         if attributes.Target.attribute_id in attribute_list:
-            kTarget = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Target)
-            if kTarget is not NullValue:
+            target = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Target)
+            if target is not NullValue:
                 if is_positioning_supported:
-                    asserts.assert_true(0 <= kTarget.position <= 10000, "Position is not in the expected range")
+                    asserts.assert_true(0 <= target.position <= 10000, "Position is not in the expected range")
                 if is_latching_supported:
-                    asserts.assert_is_instance(kCurrentState.Latch, bool, "Latch is not a boolean")
+                    asserts.assert_is_instance(current_state.Latch, bool, "Latch is not a boolean")
                 if is_speed_supported:
-                    asserts.assert_true(0 <= kTarget.speed <= 3, "Speed is not in the expected range")
+                    asserts.assert_true(0 <= target.speed <= 3, "Speed is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 5: Read Resolution attribute
         self.step(5)
         if attributes.Resolution.attribute_id in attribute_list:
-            kResolution = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Resolution)
-            asserts.assert_true(1 <= kResolution <= 10000, "Resolution is not in the expected range")
+            resolution = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Resolution)
+            asserts.assert_true(1 <= resolution <= 10000, "Resolution is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 6: Read StepValue attribute
         self.step(6)
         if attributes.StepValue.attribute_id in attribute_list:
-            kStepValue = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.StepValue)
-            asserts.assert_true(0 <= kStepValue <= 10000, "StepValue is not in the expected range")
-            asserts.assert_true(kStepValue % kResolution == 0, "StepValue is not a multiple of Resolution")
+            step_value = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.StepValue)
+            asserts.assert_true(0 <= step_value <= 10000, "StepValue is not in the expected range")
+            asserts.assert_true(step_value % resolution == 0, "StepValue is not a multiple of Resolution")
         else:
             logging.info("Test step skipped")
 
         # STEP 7: Read Unit attribute
         self.step(7)
         if attributes.Unit.attribute_id in attribute_list:
-            kUnit = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Unit)
-            asserts.assert_true(0 <= kUnit <= 1, "Unit is not in the expected range")
+            unit = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Unit)
+            asserts.assert_true(0 <= unit <= 1, "Unit is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 8: Read UnitRange attribute
         self.step(8)
         if attributes.UnitRange.attribute_id in attribute_list:
-            kUnitRange = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.UnitRange)
-            if kUnitRange is not NullValue:
-                asserts.assert_true(-32768 <= kUnitRange.Min <= kUnitRange.Max -1, "UnitRange.Min is not in the expected range")
-                asserts.assert_true(kUnitRange.Min +1  <= kUnitRange.Max <= 32767, "UnitRange.Max is not in the expected range")
+            unit_range = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.UnitRange)
+            if unit_range is not NullValue:
+                asserts.assert_true(-32768 <= unit_range.Min <= unit_range.Max -1, "UnitRange.Min is not in the expected range")
+                asserts.assert_true(unit_range.Min +1  <= unit_range.Max <= 32767, "UnitRange.Max is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 9: Read LimitRange attribute
         self.step(9)
         if attributes.LimitRange.attribute_id in attribute_list:
-            kLimitRange = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.LimitRange)
-            asserts.assert_true(0 <= kLimitRange.Min <= 10000, "LimitRange.Min is not in the expected range")
-            asserts.assert_true(kLimitRange.Min % kResolution == 0, "LimitRange.Min is not a multiple of Resolution")
-            asserts.assert_true(kLimitRange.Min <= kLimitRange.Max <= 10000, "LimitRange.Max is not in the expected range")
-            asserts.assert_true(kLimitRange.Max % kResolution == 0, "LimitRange.Max is not a multiple of Resolution")
+            limit_range = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.LimitRange)
+            asserts.assert_true(0 <= limit_range.Min <= 10000, "LimitRange.Min is not in the expected range")
+            asserts.assert_true(limit_range.Min % resolution == 0, "LimitRange.Min is not a multiple of Resolution")
+            asserts.assert_true(limit_range.Min <= limit_range.Max <= 10000, "LimitRange.Max is not in the expected range")
+            asserts.assert_true(limit_range.Max % resolution == 0, "LimitRange.Max is not a multiple of Resolution")
         else:
             logging.info("Test step skipped")
 
         # STEP 10: Read TranslationDirection attribute
         self.step(10)
         if attributes.TranslationDirection.attribute_id in attribute_list:
-            kTranslationDirection = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.TranslationDirection)
-            asserts.assert_true(0 <= kTranslationDirection <= 15, "TranslationDirection is not in the expected range")
+            translation_direction = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.TranslationDirection)
+            asserts.assert_true(0 <= translation_direction <= 15, "TranslationDirection is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 11: Read RotationAxis attribute
         self.step(11)
         if attributes.RotationAxis.attribute_id in attribute_list:
-            kRotationAxis = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.RotationAxis)
-            asserts.assert_true(0 <= kRotationAxis <= 10, "RotationAxis is not in the expected range")
+            rotation_axis = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.RotationAxis)
+            asserts.assert_true(0 <= rotation_axis <= 10, "RotationAxis is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 12: Read Overflow attribute
         self.step(12)
         if attributes.Overflow.attribute_id in attribute_list:
-            kOverflow = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Overflow)
-            asserts.assert_true(0 <= kOverflow <= 10, "Overflow is not in the expected range")
+            overflow = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.Overflow)
+            asserts.assert_true(0 <= overflow <= 10, "Overflow is not in the expected range")
         else:
             logging.info("Test step skipped")
 
         # STEP 13: Read ModulationType attribute
         self.step(13)
         if attributes.ModulationType.attribute_id in attribute_list:
-            kModulationType = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.ModulationType)
-            asserts.assert_true(0 <= kModulationType <= 4, "ModulationType is not in the expected range")
+            modulation_type = await self.read_cldim_attribute_expect_success(endpoint=endpoint, attribute=attributes.ModulationType)
+            asserts.assert_true(0 <= modulation_type <= 4, "ModulationType is not in the expected range")
         else:
             logging.info("Test step skipped")
 
