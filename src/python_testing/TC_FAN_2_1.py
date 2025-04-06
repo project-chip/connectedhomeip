f3b203c0f57eec29b6ac43b64321e0cc13e59e36
Raul Marquez <rmarquez@csa-iot.org>
2025-04-05 21:46:29 -0700

    update steps

--- src/python_testing/TC_FAN_2_1.py
+++ src/python_testing/TC_FAN_2_1.py
@@ -53,16 +53,16 @@ class Uint8Type(int):
 
 class TC_FAN_2_1(MatterBaseTest):
     def desc_TC_FAN_2_1(self) -> str:
-        return "[TC-FAN-3.1] Mandatory functionality with DUT as Server"
+        return "[TC-FAN-2.1] Mandatory functionality with DUT as Server"
 
     def steps_TC_FAN_2_1(self):
         return [TestStep(1, "[FC] Commissioning already done.", is_commissioning=True),
-                TestStep(2, "[FC] TH checks for support of the Auto FanMode feature.",
-                         "If supported, perform the next step. If unsupported, skip the next step."),
+                TestStep(2, "[FC] TH checks for support of the Auto feature.",
+                         "Save result for future use."),
                 TestStep(3, "[FC] TH reads from the DUT the FanModeSequence attribute.",
-                         "Verify that the DUT response contains an enum8 with value between 0 and 5 inclusive. Verify that the FanModeSequence attribute result contains one of the values that has the Auto FanMode option."),
+                         "Verify that the DUT response contains a FanModeSequenceEnum with value between 0 and 5 inclusive. If Auto is not supported, verify that the FanModeSequence attribute is a valid sequence (non-Auto). If Auto is supported, verify that the FanModeSequence attribute is a valid sequence (Auto or non-Auto)."),
                 TestStep(4, "[FC] TH reads from the DUT the FanMode attribute value.",
-                         "Verify that the DUT response contains an enum8 with a value between 0 and 5, excluding 4."),
+                         "Verify that the DUT response contains a FanModeEnum with a value between 0 and 5, excluding 4. Verify that if the FanMode attribute is set to Auto, the Auto feature is supported."),
                 TestStep(5, "[FC] TH reads from the DUT the PercentSetting attribute.",
                          "Verify that the DUT response contains a uint8 with value between 0 and 100 inclusive."),
                 TestStep(6, "[FC] TH reads from the DUT the PercentCurrent attribute.",
@@ -94,7 +94,7 @@ class TC_FAN_2_1(MatterBaseTest):
                                        f"[FC] {attribute.__name__} result ({value}) isn't of type {type.__name__}")
 
         # Verify response is valid (value is within expected range)
-        asserts.assert_in(value, range, f"[FC] {attribute.__name__} result ({value}) is invalid")
+        asserts.assert_in(value, range, f"[FC] {attribute.__name__} result ({value}) is out of range")
 
         return value
 
@@ -110,40 +110,49 @@ class TC_FAN_2_1(MatterBaseTest):
         feature = cluster.Bitmaps.Feature
         fm_enum = cluster.Enums.FanModeEnum
         fms_enum = cluster.Enums.FanModeSequenceEnum
+        non_auto_values = [fms_enum.kOffHigh, fms_enum.kOffLowHigh, fms_enum.kOffLowMedHigh]
+        auto_values = [fms_enum.kOffHighAuto, fms_enum.kOffLowHighAuto, fms_enum.kOffLowMedHighAuto]
 
         # *** STEP 1 ***
         # Commissioning already done
         self.step(1)
 
         # *** STEP 2 ***
-        # TH checks for support of the Auto FanMode feature
+        # TH checks for support of the Auto feature
         self.step(2)
         feature_map = await self.read_setting(attribute.FeatureMap)
-        supports_auto_fan_mode = bool(feature_map & feature.kAuto)
+        supports_auto = bool(feature_map & feature.kAuto)
+        logging.info(f"[FC] DUT supports Auto FanMode feature: {supports_auto}")
 
-        if not supports_auto_fan_mode:
-            # *** STEP 3 ***
-            # TH reads from the DUT the FanModeSequence attribute
-            # Verify that the DUT response contains an enum8 with value between 0 and 5 inclusive.
-            self.step(3)
-            fan_mode_sequence = await self.verify_setting(attribute.FanModeSequence, fms_enum, range(0, 6))
+        # *** STEP 3 ***
+        # TH reads from the DUT the FanModeSequence attribute
+        # Verify that the DUT response contains a FanModeEnum with value between 0 and 5 inclusive.
+        self.step(3)
+        fan_mode_sequence = await self.verify_setting(attribute.FanModeSequence, fms_enum, range(0, 6))
 
-            # FanModeSequence values that support Auto FanMode
-            fan_mode_sequence_non_auto_values = [fms_enum.kOffHigh, fms_enum.kOffLowHigh, fms_enum.kOffLowMedHigh]
-
-            # Verify that the FanModeSequence attribute is a non-Auto FanMode sequence
-            asserts.assert_in(fan_mode_sequence, fan_mode_sequence_non_auto_values,
-                              f"[FC] FanModeSequence ({fan_mode_sequence}:{fan_mode_sequence.name}) must be a non-Auto FanMode sequence.")
+        if not supports_auto:
+            # If Auto is not supported, verify that the FanModeSequence
+            # attribute is a valid sequence (non-Auto)
+            asserts.assert_in(fan_mode_sequence, non_auto_values,
+                              f"[FC] FanModeSequence ({fan_mode_sequence}:{fan_mode_sequence.name}) must be a valid FanMode sequence (non-Auto).")
         else:
-            logging.info("[FC] Auto FanMode feature is unsupported, skipping FanModeSequence Auto conformance step.")
-            self.skip_step(3)
+            # If Auto is supported, verify that the FanModeSequence
+            # attribute is a valid sequence (Auto or non-Auto)
+            asserts.assert_in(fan_mode_sequence, non_auto_values + auto_values,
+                              f"[FC] FanModeSequence ({fan_mode_sequence}:{fan_mode_sequence.name}) must be a valid FanMode sequence (Auto or non-Auto).")
 
         # *** STEP 4 ***
         # TH reads from the DUT the FanMode attribute value
         # Verify that the DUT response contains an enum8 with
         # a value between 0 and 5, excluding 4
         self.step(4)
-        await self.verify_setting(attribute.FanMode, fm_enum, [0, 1, 2, 3, 5])
+        fan_mode = await self.verify_setting(attribute.FanMode, fm_enum, [0, 1, 2, 3, 5])
+
+        # Verify that if the FanMode attribute is set to Auto,
+        # the Auto feature is supported
+        is_fan_mode_conformant = not ((fan_mode == fm_enum.kAuto) and not supports_auto)
+        asserts.assert_true(is_fan_mode_conformant,
+                            f"[FC] FanMode attribute ({fan_mode}:{fan_mode.name}) is set to Auto, but the DUT does not support the Auto feature.")
 
         # *** STEP 5 ***
         # TH reads from the DUT the PercentSetting attribute
