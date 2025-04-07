fa95b2cafd7e9960cb25a923051c23e8c7dc85ca
Raul Marquez <rmarquez@csa-iot.org>
2025-04-07 14:16:16 -0700

    updates rock feature check

--- src/python_testing/TC_FAN_2_3.py
+++ src/python_testing/TC_FAN_2_3.py
@@ -28,6 +28,7 @@
 #       --commissioning-method on-network
 #       --discriminator 1234
 #       --passcode 20202021
+#       --endpoint 1
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #     factory-reset: true
@@ -39,8 +40,8 @@ from typing import Any
 
 import chip.clusters as Clusters
 from chip.testing.matter_asserts import assert_valid_uint8
-from matter_testing_infrastructure.chip.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body,
-                                                                       default_matter_test_main)
+from matter_testing_infrastructure.chip.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main,
+                                                                       has_feature, run_if_endpoint_matches)
 from mobly import asserts
 
 logger = logging.getLogger(__name__)
@@ -52,13 +53,11 @@ class TC_FAN_2_3(MatterBaseTest):
 
     def steps_TC_FAN_2_3(self):
         return [TestStep(1, "[FC] Commissioning already done.", is_commissioning=True),
-                TestStep(2, "[FC] TH checks the DUT for support of the Rock feature (RCK).",
-                         "If the DUT does not support the feature, the test is skipped."),
-                TestStep(3, "[FC] TH reads from the DUT the RockSupport attribute.",
+                TestStep(2, "[FC] TH reads from the DUT the RockSupport attribute.",
                          "Verify that the RockSupport attribute value is of uint8 type. Verify that the RockSupport attribute's value is between 1 and 3 inclusive."),
-                TestStep(4, "[FC] TH reads from the DUT the RockSetting attribute.",
+                TestStep(3, "[FC] TH reads from the DUT the RockSetting attribute.",
                          "Verify that the RockSetting attribute value is of uint8 type. Verify that the RockSetting attribute's value is between 0 and 3 inclusive"),
-                TestStep(5, "[FC] TH checks that RockSetting is conformant with RockSupport.",
+                TestStep(4, "[FC] TH checks that RockSetting is conformant with RockSupport.",
                          "Verify that all bits set in RockSetting are also set in RockSupport."),
                 ]
 
@@ -79,32 +78,22 @@ class TC_FAN_2_3(MatterBaseTest):
         return await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=cluster, attribute=attribute)
 
     def pics_TC_FAN_2_3(self) -> list[str]:
-        return ["FAN.S"]
+        return ["FAN.S.F02"]
 
-    @async_test_body
+    @run_if_endpoint_matches(has_feature(Clusters.FanControl, Clusters.FanControl.Bitmaps.Feature.kRocking))
     async def test_TC_FAN_2_3(self):
         # Setup
         self.endpoint = self.get_endpoint(default=1)
         cluster = Clusters.FanControl
         attr = cluster.Attributes
-        rock_feature = cluster.Bitmaps.Feature.kRocking
 
         # *** STEP 1 ***
         # Commissioning already done
         self.step(1)
 
         # *** STEP 2 ***
-        # TH checks the DUT for support of the Rock feature (RCK)
-        #  - If the DUT does not support the feature, the test is skipped
-        self.step(2)
-        supports_rock = await self.feature_guard(self.endpoint, cluster, rock_feature)
-        if not supports_rock:
-            logger.info("[FC] The Rock (RCK) feature is not supported by the DUT for this test, skipping remaining steps.")
-            self.skip_all_remaining_steps()
-
-        # *** STEP 3 ***
         # TH reads from the DUT the RockSupport attribute
-        self.step(3)
+        self.step(2)
         rock_support = await self.read_setting(attr.RockSupport)
 
         # Verify that the RockSupport attribute value is of uint8 type
@@ -114,9 +103,9 @@ class TC_FAN_2_3(MatterBaseTest):
         asserts.assert_in(rock_support, range(
             1, 8), f"[FC] RockSupport attribute value ({rock_support}) is not between 1 and 7 inclusive")
 
-        # *** STEP 4 ***
+        # *** STEP 3 ***
         # TH reads from the DUT the RockSetting attribute
-        self.step(4)
+        self.step(3)
         rock_setting = await self.read_setting(attr.RockSetting)
 
         # Verify that the RockSetting attribute value is of uint8 type
@@ -126,10 +115,10 @@ class TC_FAN_2_3(MatterBaseTest):
         asserts.assert_in(rock_setting, range(
             0, 8), f"[FC] RockSetting attribute value ({rock_setting}) is not between 0 and 7 inclusive")
 
-        # *** STEP 5 ***
+        # *** STEP 4 ***
         # TH checks that RockSetting is conformant with RockSupport
         # Verify that all bits set in RockSetting are also set in RockSupport
-        self.step(5)
+        self.step(4)
         is_rock_conformant = (rock_setting & rock_support) == rock_setting
         asserts.assert_true(is_rock_conformant, "[FC] RockSetting contains unsupported bits; it is not conformant with RockSupport")
 
