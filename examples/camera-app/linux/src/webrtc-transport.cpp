ad3681d973575321aeef6b2db03393ebe05f5256
Raveendra Karu <r.karu@samsung.com>
2025-04-09 04:00:04 +0530

    Implement WebRTC Transport.
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

---
+++ examples/camera-app/linux/src/webrtc-transport.cpp
@@ -0,0 +1,60 @@
+/*
+ *
+ *    Copyright (c) 2025 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <transport/webrtc-transport.h>
+
+WebrtcTransport::WebrtcTransport(unsigned long peerID, uint16_t sessionID)
+{
+    peerID    = peerID;
+    sessionID = sessionID;
+}
+
+void WebrtcTransport::RegisterToMediaController()
+{
+    // Placeholder for actual WebRTC implementation to register to media controller
+}
+
+// Dummy implementation of SendVideo method
+void WebrtcTransport::SendVideo(const char * data, size_t size, uint16_t videoStreamID)
+{
+    // Placeholder for actual WebRTC implementation to send video data
+}
+
+// Dummy implementation of SendAudio method
+void WebrtcTransport::SendAudio(const char * data, size_t size, uint16_t audioStreamID)
+{
+    // Placeholder for actual WebRTC implementation to send audio data
+}
+
+// Dummy implementation of SendAudioVideo method
+void WebrtcTransport::SendAudioVideo(const char * data, size_t size, uint16_t videoStreamID, uint16_t audioStreamID)
+{
+    // Placeholder for actual WebRTC implementation to send synchronized audio/video data
+}
+
+// Dummy implementation of CanSendVideo method
+bool WebrtcTransport::CanSendVideo()
+{
+    return mCanSendVideo;
+}
+
+// Dummy implementation of CanSendAudio method
+bool WebrtcTransport::CanSendAudio()
+{
+    return mCanSendAudio;
+}
