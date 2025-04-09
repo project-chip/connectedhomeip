1e009bb79dd616068ab5fe822b9c981f5ca4d496
Arun Padakanti <arunkumar.padakanti@silabs.com>
2025-04-09 23:41:23 +0530

    Implementation of HandleStep added.

--- examples/closure-app/silabs/include/ClosureDimensionManager.h
+++ examples/closure-app/silabs/include/ClosureDimensionManager.h
@@ -21,6 +21,8 @@
 #include <app-common/zap-generated/cluster-objects.h>
 #include <app/clusters/closure-dimension-server/closure-dimension-delegate.h>
 #include <app/clusters/closure-dimension-server/closure-dimension-server.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-cluster-logic.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-matter-context.h>
 
 #include <lib/core/CHIPError.h>
 #include <protocols/interaction_model/StatusCode.h>
@@ -42,13 +44,11 @@ public:
 
 private:
     EndpointId mEndpoint;
-
 };
 
 class ClosureDimensionManager
 {
 public:
-
     enum Action_t
     {
         LOCK_ACTION = 0,
@@ -72,7 +72,24 @@ public:
         return CHIP_NO_ERROR;
     }
 
+    bool SupportsLimitMonitoring() const;
+    int32_t GetLimitRangeMin() const;
+    int32_t GetLimitRangeMax() const;
+    uint16_t GetCurrentPosition() const;
+    void MoveToPosition(uint16_t position);
+    void SetSpeed(Globals::ThreeLevelAutoEnum speedMode);
+    ClusterLogic getLogic(){
+        return mLogic;
+    }
+    ClusterConformance getConformance(){
+        return kConformance;
+    }
+    static ClosureDimensionManager GetManagerInstance(){
+        return sManager;
+    }
+
 private:
+    static ClosureDimensionManager sManager;
     const ClusterConformance kConformance = { .featureMap = 0, .supportsOverflow = false };
 
     EndpointId mEndpoint;
@@ -81,6 +98,10 @@ private:
     ClusterLogic mLogic;
     Interface mInterface;
 
+    int32_t mLimitMin = 0;
+    int32_t mLimitMax = 10000;
+    uint16_t mCurrentPosition = 0;
+    Globals::ThreeLevelAutoEnum mCurrentSpeed = Globals::ThreeLevelAutoEnum::kAuto;
     Callback_fn_initiated mActionInitiated_CB;
     Callback_fn_completed mActionCompleted_CB;
 };
