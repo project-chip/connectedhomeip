bf4823b2b9da6fe1131d69c13388f1bd0a116343
James Harrow <james.harrow@gmail.com>
2025-04-13 16:00:47 +0100

    Fix build issue caused by new spec update to make price optional

--- examples/energy-gateway-app/commodity-price/src/CommodityPriceEventTriggers.cpp
+++ examples/energy-gateway-app/commodity-price/src/CommodityPriceEventTriggers.cpp
@@ -52,11 +52,15 @@ void SetTestEventTrigger_PriceUpdate()
         ChipLogError(Support, "SetTestEventTrigger_PriceUpdate() could not get time");
     }
 
+    Globals::Structs::PriceStruct::Type price; // Contains .amount  and .currency
     newPriceStruct.periodStart = chipEpoch;
     newPriceStruct.periodEnd.SetNonNull(newPriceStruct.periodStart + 30 * 60);
-    newPriceStruct.price.currency.currency      = 981;
-    newPriceStruct.price.currency.decimalPoints = 5;
-    newPriceStruct.price.amount                 = 15916; // 15.916 p/kWh
+
+    price.amount                 = 15916; // 15.916 p/kWh
+    price.currency.currency      = 981;
+    price.currency.decimalPoints = 5;
+    newPriceStruct.price.SetValue(price);
+    newPriceStruct.priceLevel.SetValue(3);
 
     newPrice.SetNonNull(newPriceStruct);
 
@@ -77,10 +81,12 @@ void SetTestEventTrigger_ForecastUpdate()
     }
 
     Structs::CommodityPriceStruct::Type newPriceStruct;
+    Globals::Structs::PriceStruct::Type price; // Contains .amount  and .currency
 
     newPriceStruct.periodEnd.SetNonNull(chipEpoch - 1); // 1st time through the start period will be 'now' - see below
-    newPriceStruct.price.currency.currency      = 981;  // We'll always use GBP
-    newPriceStruct.price.currency.decimalPoints = 5;    // We'll always use 1000th of a pence
+
+    price.currency.currency      = 981; // We'll always use GBP
+    price.currency.decimalPoints = 5;   // We'll always use 1000th of a pence
 
     constexpr uint16_t kMinPrice        = 4000;    // 4 p/kWh
     constexpr uint16_t kMaxPrice        = 32000;   // 32 p/kWh
@@ -89,18 +95,20 @@ void SetTestEventTrigger_ForecastUpdate()
     for (uint8_t i = 0; i < 50; i++)
     {
         // Make up a price
-        newPriceStruct.price.amount = kMinPrice + rand() % (kMaxPrice - kMinPrice + 1);
+        price.amount = kMinPrice + rand() % (kMaxPrice - kMinPrice + 1);
+        newPriceStruct.price.SetValue(price);
+        newPriceStruct.priceLevel.SetValue(3);
 
         // Set the start and end time (non overlapping)
         newPriceStruct.periodStart = newPriceStruct.periodEnd.Value() + 1;
         newPriceStruct.periodEnd.SetNonNull(newPriceStruct.periodStart + (k30MinsInSeconds - 1)); // 29mins 59secs
 
         // Set a description based on price level
-        if (newPriceStruct.price.amount < 10000)
+        if (price.amount < 10000)
         {
             // priceDescription = "Low";
         }
-        else if (newPriceStruct.price.amount < 24000)
+        else if (price.amount < 24000)
         {
             // priceDescription = "Medium";
         }
@@ -119,17 +127,23 @@ void SetTestEventTrigger_ForecastUpdate()
         newPriceComponent.source = Globals::TariffPriceTypeEnum::kStandard;
         newPriceComponent.tariffComponentID.ClearValue(); // We don't have a tariff componentID
 
-        newPriceComponent.price = static_cast<Money>(newPriceStruct.price.amount * 95 / 100); // exVAT
+        newPriceComponent.price.SetValue(static_cast<Money>(price.amount * 95 / 100)); // exVAT
         // newPriceComponent.description.SetValue(//TODO ("ExVAT)"));
         memMgr.PreparePriceEntry(0);
         memMgr.AddPriceComponent(newPriceComponent);
 
-        newPriceComponent.price = static_cast<Money>(newPriceStruct.price.amount * 5 / 100); // VAT (5%)
+        newPriceComponent.price.SetValue(static_cast<Money>(price.amount * 5 / 100)); // VAT (5%)
         // newPriceComponent.description.SetValue(//TODO ("VAT)"));
 
         memMgr.PreparePriceEntry(1);
         memMgr.AddPriceComponent(newPriceComponent);
     }
+
+    //     DataModel::List<const Structs::CommodityPriceStruct::Type> myPriceForecast;
+    //     myPriceForecast = DataModel::List<const Structs::CommodityPriceStruct::Type>(HERE!!!!!!!!!!
+    //     This is wrong - we need to have an array here but it could be too big for the stack
+    // Consider another list management class.)
+    // dg->SetForecast()
 }
 
 bool HandleCommodityPriceTestEventTrigger(uint64_t eventTrigger)
