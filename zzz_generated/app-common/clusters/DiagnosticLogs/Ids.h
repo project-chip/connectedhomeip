372360c4ec05a139e577f6f5d3a28ef8d3af86db
Andrei Litvin <andy314@gmail.com>
2025-04-14 12:05:51 -0400

    Add individual cluster metadata generation (and build targets) that is generated based on controller-clusters (#38151)
    * Start to support some metadata generation
    
    * Fix import
    
    * Clean up sources
    
    * Add generated metadata
    
    * Remove extra comma to make this a valid build.gn
    
    * Restyle
    
    * Updated content a bit: cluster ID is also fixed
    
    * Temporary workaround...but will look for something better
    
    * Better code reuse
    
    * Restyle
    
    * Restyle and add missing file
    
    * Remove some todo comments
    
    * make linter happy
    
    * Fix one case for namespace end comment
    
    * move generation to zzz_generated
    
    * rename path a bit more
    
    * Updated structure to zzz_generated/clusters/<NAME>' to contain individual cluster information
    
    * Update generated code: generate IDs that are compatible with old IDs (naming and namespace wise)
    
    * Add extra files
    
    * Fix up data type for events
    
    * More arrangement of things ... it almost works
    
    * Things now compile with dependencies. Need to figure out globals and then use the IDs
    
    * Start adding some global attribute handling
    
    * Update the code generator file list
    
    * Global attributes work
    
    * Update naming and add pragma once
    
    * Add response command IDs
    
    * All clusters app now compiles
    
    * update paths to one more level of indirection
    
    * Update darwin projects to add data_model whenever app-common is there
    
    * Update more include paths, make ASR compile by fixing platform dependencies
    
    * Fix qpg compilation
    
    * Fix more builds: nrf, nxp, stm32, telink
    
    * Fix ameba compilation
    
    * Restyle
    
    * fix MW320 builds
    
    * Fix bouffalolab compiles
    
    * Fix TI compilation
    
    * Add ids dependencies to darwin
    
    * Add ids dependencies to NuttX
    
    * Fix tizen build
    
    * Fix android build
    
    * Restyle
    
    * fix zephyr
    
    * match zap gen
    
    * more nxp fixes for ids
    
    * more nxp fixes for ids
    
    * make silabs/efr32 compile
    
    * Bulk-add depdendencies on IDs on all platforms
    
    * Update more cmake files to include directories that include data_model whenever app-common was included
    
    * Cleanup NXP dependencies
    
    * NuttX code cleanup: generate does a real generate, cleanup the target boards usage
    
    * darwin framework tool compile fix
    
    * Disabled NuttX builds - platform maintainer has been notified
    
    * Look to move items to reduce blast radius of changes
    
    * Start updating the structure to reduce stutter
    
    * Move into clusters
    
    * Fix include path
    
    * Regenerate and better sorting
    
    * Fix build files
    
    * Now that paths moved, re-enable nuttx
    
    * Update namespacing for attribute metadata entries
    
    * Restyled by autopep8
    
    * Remove references to data_model name
    
    * Fix darwin framework tool build: its dependencies are just bad
    
    * Restyled by gn
    
    * Cleanup dependencies, we do not need util types
    
    * Fix path in Ids.h
    
    * Fix case typo in Clusters/clusters namespace
    
    * Zap regen
    
    * Address some code review comments: spacing, extra deps, format of cluster id
    
    * Fix typo
    
    * Update paths in metadata to pass compiler (at least LSP), update spacing a bit and regen
    
    * Fix namespace ordering
    
    ---------
    
    Co-authored-by: Andrei Litvin <andreilitvin@google.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ zzz_generated/app-common/clusters/DiagnosticLogs/Ids.h
@@ -0,0 +1,10 @@
+// DO NOT EDIT MANUALLY - Generated file
+//
+// Identifier constant values for cluster DiagnosticLogs (cluster code: 50/0x0432)
+// based on src/controller/data_model/controller-clusters.matter
+#pragma once
+
+#include <clusters/DiagnosticLogs/AttributeIds.h>
+#include <clusters/DiagnosticLogs/ClusterId.h>
+#include <clusters/DiagnosticLogs/CommandIds.h>
+#include <clusters/DiagnosticLogs/EventIds.h>
