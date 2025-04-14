bfae047ae24b8976ba69f654035aba4e5de524c8
Shubham Patil <shubham.patil@espressif.com>
2025-04-14 17:24:08 +0530

    Update src/app/data-model-provider/Provider.h
    Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

--- src/app/data-model-provider/Provider.h
+++ src/app/data-model-provider/Provider.h
@@ -116,7 +116,7 @@ public:
                                                      CommandHandler * handler) = 0;
 
 private:
-    InteractionModelContext mContext = {};
+    InteractionModelContext mContext;
 };
 
 } // namespace DataModel
