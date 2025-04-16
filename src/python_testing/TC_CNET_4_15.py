d6c83f334b0d6d85c20ce69f0192d1616e944823
Andrea Balestra <andreab@netapp.com>
2025-04-16 16:10:34 -0300

    Added fixes to file.

--- src/python_testing/TC_CNET_4_15.py
+++ src/python_testing/TC_CNET_4_15.py
@@ -1,5 +1,5 @@
 #
-#    Copyright (c) 2024 Project CHIP Authors
+#    Copyright (c) 2025 Project CHIP Authors
 #    All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,20 +14,37 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-
-import logging
-import random
-import string
-from typing import Optional
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs:
+#   run1:
+#     app: ${ALL_CLUSTERS_APP}
+#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#     script-args: >
+#       --endpoint 0
+#       --storage-path admin_storage.json
+#       --commissioning-method on-network
+#       --discriminator 1234
+#       --passcode 20202021
+#       --PICS src/app/tests/suites/certification/ci-pics-values
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#     factory-reset: true
+#     quiet: true
+# === END CI TEST ARGUMENTS ===
 
 import chip.clusters as Clusters
-from chip.clusters.Types import NullValue
-from matter_testing_infrastructure.chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main, type_matches
-from matter_testing_infrastructure.chip.testing.pixit import PIXIT
-from mobly import asserts
+from matter_testing_infrastructure.chip.testing.matter_testing import (
+    MatterBaseTest,
+    TestStep,
+    async_test_body,
+    default_matter_test_main,
+)
 
 
-class TC_CNET_4_4(MatterBaseTest):
+class TC_CNET_4_15(MatterBaseTest):
     def steps_TC_CNET_4_15(self):
         return [TestStep("precondition", "TH is commissioned", is_commissioning=True),
                 TestStep(1, 'TH sends ArmFailSafe command to the DUT with the ExpiryLengthSeconds field set to 900'),
@@ -43,7 +60,6 @@ class TC_CNET_4_4(MatterBaseTest):
     @async_test_body
     async def test_TC_CNET_4_15(self):
         cnet = Clusters.NetworkCommissioning
-        attr = cnet.Attributes
 
         # Commissioning is already done
         self.step("precondition")
@@ -63,9 +79,18 @@ class TC_CNET_4_4(MatterBaseTest):
         network_id = self.matter_test_config.global_test_params['PIXIT.CNET.WIFI_2ND_ACCESSPOINT_SSID']
         send_remove = await self.send_remove_network_command(network_id, 1)
 
-        # Verify that DUT sends RemoveNetworkResponse command to the TH
-        await self.expect_command(Clusters.GeneralCommissioning.Commands.RemoveNetworkResponse, send_remove)
+        # Verify that DUT sends NetworkConfigResponse command to the TH1 with NetworkingStatus field set as NetworkIDNotFound which is '3'
+        await self.expect_command(cnet.Commands.NetworkConfigResponse, send_remove)
         
+        self.step(3)
+        # TH sends ConnectNetwork Command to the DUT with NetworkID field set to PIXIT.CNET.WIFI_2ND_ACCESSPOINT_SSID, 
+        # which does not match the provisioned network, and Breadcrumb field set to 1
+        send_connect = await self.send_connect_network_command(network_id, 1)
+
+        # Verify that DUT sends ConnectNetworkResponse command to the TH with NetworkingStatus field set as NetworkIDNotFound which is '3'
+        await self.expect_command(Clusters.GeneralCommissioning.Commands.ConnectNetworkResponse, send_connect)
         
-        
+
+if __name__ == "__main__":
+    default_matter_test_main()
         
