2368a5925797b2dd9165b4f46470201708b9069b
James Harrow <james.harrow@gmail.com>
2025-04-18 21:41:11 +0100

    Fix Lint error

--- src/python_testing/TC_SEPR_2_3.py
+++ src/python_testing/TC_SEPR_2_3.py
@@ -46,7 +46,6 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.testing import matter_asserts
 from chip.testing.matter_testing import EventChangeCallback, MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 from TC_SEPRTestBase import CommodityPriceTestBaseHelper
