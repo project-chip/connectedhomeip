048b7a76e6de5a88cce972c9bd6103e682c1d54e
Andrey Khodyrev <khodyrev@google.com>
2025-04-17 23:15:25 -0600

    fix imports

--- src/python_testing/TC_CNET_4_2.py
+++ src/python_testing/TC_CNET_4_2.py
@@ -18,7 +18,6 @@ import logging
 
 import chip.clusters as Clusters
 import test_plan_support
-from chip.clusters.Types import NullValue
 from chip.testing import matter_asserts
 from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_feature, run_if_endpoint_matches
 from mobly import asserts
