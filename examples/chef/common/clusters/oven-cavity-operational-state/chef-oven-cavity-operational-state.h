0ad134067d85c1dd7c3973c67341111dcf14b120
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-04-18 15:03:08 -0700

    Fix compilation

--- examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.h
+++ examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.h
@@ -32,7 +32,7 @@ private:
 
 public:
     Delegate() { GenericOperationalStateDelegateImpl::mOperationalPhaseList = Span<const CharSpan>(kPhaseList); }
-}
+};
 
 void InitChefOvenCavityOperationalStateCluster();
 
