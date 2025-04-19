e3951adb22e4efd87695fd3ea19f2d2673284e02
Maciej Grela <m.grela@samsung.com>
2025-04-19 08:15:10 +0200

    TC_WEBRTCP_2_1: change chip -> matter in imports
    Signed-off-by: Maciej Grela <m.grela@samsung.com>

--- src/python_testing/TC_WEBRTCP_2_1.py
+++ src/python_testing/TC_WEBRTCP_2_1.py
@@ -36,11 +36,11 @@
 # === END CI TEST ARGUMENTS ===
 #
 
-import chip.clusters as Clusters
-from chip import ChipDeviceCtrl
-from chip.clusters.Types import NullValue
-from chip.interaction_model import InteractionModelError, Status
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+import matter.clusters as Clusters
+from matter import ChipDeviceCtrl
+from matter.clusters.Types import NullValue
+from matter.interaction_model import InteractionModelError, Status
+from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 
