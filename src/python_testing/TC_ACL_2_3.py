ceb293a84565b10d453aa5797c59385a10527436
Jake Ororke <jororke@csa-iot.org>
2025-04-20 03:22:40 -0700

    Resolving restylzer/autopep8 issue with script manually as provided CI patch failed

--- src/python_testing/TC_ACL_2_3.py
+++ src/python_testing/TC_ACL_2_3.py
@@ -41,17 +41,25 @@ from chip.interaction_model import Status
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
-# These below variables are used to test the AccessControl cluster Extension attribute and come from the test plan here: https://github.com/CHIP-Specifications/chip-test-plans/blob/59e8c45b8e7c24d5ce130b166520ff4f7bd935b6/src/cluster/AccessControl.adoc#tc-acl-2-6-accesscontrolentrychanged-event:~:text=D_OK_EMPTY%3A%20%221718%22%20which%20is%20an%20octstr%20of%20length%202%20containing%20valid%20TLV%3A
+# These below variables are used to test the AccessControl cluster
+# Extension attribute and come from the test plan here:
+# https://github.com/CHIP-Specifications/chip-test-plans/blob/59e8c45b8e7c24d5ce130b166520ff4f7bd935b6/src/cluster/AccessControl.adoc#tc-acl-2-6-accesscontrolentrychanged-event:~:text=D_OK_EMPTY%3A%20%221718%22%20which%20is%20an%20octstr%20of%20length%202%20containing%20valid%20TLV%3A
 D_OK_EMPTY = bytes.fromhex('1718')
-D_OK_SINGLE = bytes.fromhex('17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
+D_OK_SINGLE = bytes.fromhex(
+    '17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
 D_OK_FULL = bytes.fromhex('17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E6700D00000F1FF02003148656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E7420616761696E2E2E2E2E2E0018')
 D_BAD_LENGTH = bytes.fromhex('17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E6700D00000F1FF02003248656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E7420616761696E2E2E2E2E2E2E0018')
 D_BAD_STRUCT = bytes.fromhex('1518')
-D_BAD_LIST = bytes.fromhex('3701D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
-D_BAD_ELEM = bytes.fromhex('17103D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
-D_BAD_OVERFLOW = bytes.fromhex('17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018FF')
-D_BAD_UNDERFLOW = bytes.fromhex('17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E6700')
+D_BAD_LIST = bytes.fromhex(
+    '3701D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
+D_BAD_ELEM = bytes.fromhex(
+    '17103D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018')
+D_BAD_OVERFLOW = bytes.fromhex(
+    '17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E670018FF')
+D_BAD_UNDERFLOW = bytes.fromhex(
+    '17D00000F1FF01003D48656C6C6F20576F726C642E205468697320697320612073696E676C6520656C656D656E74206C6976696E6720617320612063686172737472696E6700')
 D_BAD_NONE = bytes.fromhex('')
+
 
 class TC_ACL_2_3(MatterBaseTest):
     async def read_currentfabricindex(self, th: ChipDeviceCtrl) -> int:
@@ -71,40 +79,40 @@ class TC_ACL_2_3(MatterBaseTest):
                      "Result is SUCCESS, value is stored as F1"),
             TestStep(3, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
                      "Result is SUCCESS, value is an empty list"),
-            TestStep(4, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_EMPTY", 
+            TestStep(4, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_EMPTY",
                      "Result is SUCCESS"),
             TestStep(5, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
                      "Result is SUCCESS, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_EMPTY, fabric index is F1"),
-            TestStep(6, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_SINGLE", 
+            TestStep(6, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_SINGLE",
                      "Result is SUCCESS"),
             TestStep(7, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
                      "Result is SUCCESS, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_SINGLE, fabric index is F1"),
-            TestStep(8, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_FULL", 
+            TestStep(8, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_FULL",
                      "Result is SUCCESS"),
             TestStep(9, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
                      "Result is SUCCESS, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_OK_FULL, fabric index is F1"),
-            TestStep(10, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_LENGTH", 
+            TestStep(10, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_LENGTH",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(11, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_STRUCT", 
+            TestStep(11, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_STRUCT",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(12, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_LIST", 
+            TestStep(12, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_LIST",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(13, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_ELEM", 
+            TestStep(13, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_ELEM",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(14, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_OVERFLOW", 
+            TestStep(14, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_OVERFLOW",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(15, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_UNDERFLOW", 
+            TestStep(15, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_UNDERFLOW",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(16, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_NONE", 
+            TestStep(16, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 1 element struct data field: D_BAD_NONE",
                      "Result is CONSTRAINT_ERROR (0x87)"),
-            TestStep(17, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 2 elements struct data field: D_OK_EMPTY, D_OK_SINGLE", 
+            TestStep(17, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is list of AccessControlExtensionStruct containing 2 elements struct data field: D_OK_EMPTY, D_OK_SINGLE",
                      "Result is SUCCESS for first struct, value is D_OK_EMPTY, fabric index is F1, CONTSTRAINT_ERROR for second struct"),
             TestStep(18, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
-                    "Result is Success, AccessControlExtensionStruct containing 1 element; which is the last successfully written extension from Test Step 8; value is struct with data field: D_OK_FULL, fabric index is F1"),
-            TestStep(19, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is an empty list", 
-                    "Result is SUCCESS"),
+                     "Result is Success, AccessControlExtensionStruct containing 1 element; which is the last successfully written extension from Test Step 8; value is struct with data field: D_OK_FULL, fabric index is F1"),
+            TestStep(19, "TH1 writes DUT Endpoint 0 AccessControl cluster Extension attribute, value is an empty list",
+                     "Result is SUCCESS"),
             TestStep(20, "TH1 reads DUT Endpoint 0 AccessControl cluster Extension attribute",
-                    "Result is Success, value is an empty list"),
+                     "Result is Success, value is an empty list"),
         ]
         return steps
 
@@ -185,7 +193,7 @@ class TC_ACL_2_3(MatterBaseTest):
         ac_extension_value3 = await self.read_single_attribute_check_success(endpoint=0, cluster=ac_cluster, attribute=ac_extension_attr)
         logging.info(f"AccessControlExtension: {str(ac_extension_value3)}")
         asserts.assert_equal(ac_extension_value3[0].data, D_OK_FULL, "AccessControlExtension is D_OK_FULL")
-        
+
         self.step(10)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_LENGTH
         extension4 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -198,7 +206,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result4)}")
         asserts.assert_equal(
             result4[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
-        
+
         self.step(11)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_STRUCT
         extension5 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -211,7 +219,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result5)}")
         asserts.assert_equal(
             result5[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
- 
+
         self.step(12)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_LIST
         extension6 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -224,7 +232,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result6)}")
         asserts.assert_equal(
             result6[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
-        
+
         self.step(13)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_ELEM
         extension7 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -237,7 +245,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result7)}")
         asserts.assert_equal(
             result7[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
-        
+
         self.step(14)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_OVERFLOW
         extension8 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -250,7 +258,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result8)}")
         asserts.assert_equal(
             result8[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
-        
+
         self.step(15)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_UNDERFLOW
         extension9 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -263,7 +271,7 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result9)}")
         asserts.assert_equal(
             result9[0].Status, Status.ConstraintError, f"Write should have returned a CONSTRAINT_ERROR")
-        
+
         self.step(16)
         # Write AccessControlExtension attribute for TH1 value is D_BAD_NONE
         extension10 = Clusters.AccessControl.Structs.AccessControlExtensionStruct(
@@ -290,13 +298,18 @@ class TC_ACL_2_3(MatterBaseTest):
         )
         logging.info(f"Write result: {str(result11)}")
         asserts.assert_equal(
-            result11[0].Status, Status.ConstraintError, f"Write should have returned a SUCCESS for struct 1, CONSTRAINT_ERROR for struct 2")
+            result11[0].Status,
+            Status.ConstraintError,
+            f"Write should have returned a SUCCESS for struct 1, CONSTRAINT_ERROR for struct 2")
 
         self.step(18)
         # Read AccessControlExtension attribute for TH1 value is D_OK_FULL
         ac_extension_value4 = await self.read_single_attribute_check_success(endpoint=0, cluster=ac_cluster, attribute=ac_extension_attr)
         logging.info(f"AccessControlExtension: {str(ac_extension_value4)}")
-        asserts.assert_equal(ac_extension_value4[0].data, D_OK_FULL, "AccessControlExtension is D_OK_FULL from test step 8 as last successfully written extension")
+        asserts.assert_equal(
+            ac_extension_value4[0].data,
+            D_OK_FULL,
+            "AccessControlExtension is D_OK_FULL from test step 8 as last successfully written extension")
 
         self.step(19)
         # Write AccessControlExtension attribute for TH1 value is an empty list
@@ -308,12 +321,13 @@ class TC_ACL_2_3(MatterBaseTest):
         logging.info(f"Write result: {str(result12)}")
         asserts.assert_equal(
             result12[0].Status, Status.Success, f"Write should have returned a SUCCESS")
-        
+
         self.step(20)
         # Read AccessControlExtension attribute for TH1 value is an empty list
         ac_extension_value5 = await self.read_single_attribute_check_success(endpoint=0, cluster=ac_cluster, attribute=ac_extension_attr)
         logging.info(f"AccessControlExtension: {str(ac_extension_value5)}")
         asserts.assert_equal(ac_extension_value5, [], "AccessControlExtension is empty list")
+
 
 if __name__ == "__main__":
     default_matter_test_main()
