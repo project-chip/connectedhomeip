19906d9c054c535d04a68b8e284bd9da3bc2668f
Restyled.io <commits@restyled.io>
2025-04-21 15:28:27 +0000

    Restyled by clang-format

--- src/app/data-model-provider/MetadataTypes.h
+++ src/app/data-model-provider/MetadataTypes.h
@@ -30,215 +30,205 @@
 // the narrowing and conversion of input values during the settings
 // of the variables inside of both 'AttributeEntry.mask' and 'AcceptedCommandEntry.mask'.
 #if defined(__clang__) || defined(__gcc__)
-#define  BitfieldAssignment(code)  _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wconversion\"") _Pragma("GCC diagnostic ignored \"-Wnarrowing\"") code _Pragma("GCC diagnostic pop")
+#define BitfieldAssignment(code)                                                                                                   \
+    _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wconversion\"")                                              \
+        _Pragma("GCC diagnostic ignored \"-Wnarrowing\"") code _Pragma("GCC diagnostic pop")
 #else
-#define  BitfieldAssignment(code)
+#define BitfieldAssignment(code)
 #endif
 
-            namespace chip
+namespace chip {
+namespace app {
+namespace DataModel {
+
+/// Represents various endpoint composition patterns as defined in the spec
+/// as `9.2.1. Endpoint Composition patterns`
+enum class EndpointCompositionPattern : uint8_t
 {
-    namespace app {
-    namespace DataModel {
+    // Tree pattern supports a general tree of endpoints. Commonly used for
+    // device types that support physical device composition (e.g. Refrigerator)
+    kTree = 0x1,
 
-    /// Represents various endpoint composition patterns as defined in the spec
-    /// as `9.2.1. Endpoint Composition patterns`
-    enum class EndpointCompositionPattern : uint8_t
+    // A full-family pattern is a list of all descendant endpoints, with no
+    // imposed hierarchy.
+    //
+    // For example the Root Node and Aggregator device types use the full-familiy
+    // pattern, as defined in their device type specification
+    kFullFamily = 0x2,
+};
+
+struct EndpointEntry
+{
+    EndpointId id;
+
+    // kInvalidEndpointId if there is no explicit parent endpoint (which means the parent is endpoint 0,
+    // for endpoints other than endpoint 0).
+    EndpointId parentId;
+    EndpointCompositionPattern compositionPattern;
+};
+
+enum class ClusterQualityFlags : uint32_t
+{
+    kDiagnosticsData = 0x0001, // `K` quality, may be filtered out in subscriptions
+};
+
+struct ServerClusterEntry
+{
+    ClusterId clusterId;
+    DataVersion dataVersion; // current cluster data version,
+    BitFlags<ClusterQualityFlags> flags;
+};
+
+struct ClusterInfo
+{
+    DataVersion dataVersion; // current cluster data version,
+    BitFlags<ClusterQualityFlags> flags;
+
+    /// Constructor that marks data version as mandatory
+    /// for this structure.
+    ClusterInfo(DataVersion version) : dataVersion(version) {}
+};
+
+enum class AttributeQualityFlags : uint32_t
+{
+    kNoAttribute     = 0x0000, // No attribute value
+    kListAttribute   = 0x0004, // This attribute is a list attribute
+    kFabricScoped    = 0x0008, // 'F' quality on attributes
+    kFabricSensitive = 0x0010, // 'S' quality on attributes
+    kChangesOmitted  = 0x0020, // `C` quality on attributes
+    kTimed           = 0x0040, // `T` quality on attributes (writes require timed interactions)
+};
+
+struct AttributeEntry
+{
+    AttributeId attributeId;
+
+    // Constructor
+    constexpr AttributeEntry(
+        AttributeId id = 0,                                                           // attributeId initial value,
+                                                                                      // this could be altered later
+        AttributeQualityFlags attrQualityFlags = AttributeQualityFlags::kNoAttribute, // mask.flags initial value
+        Access::Privilege readPriv             = Access::Privilege::kNoPrivilege,     // mask.readPrivilege initial value
+        Access::Privilege writePriv            = Access::Privilege::kNoPrivilege      // mask.writePrivilege initial value
+        ) :
+        attributeId{ id },
+        mask{ BitfieldAssignment(to_underlying(attrQualityFlags) & ((1 << 7) - 1)), // Narrowing expression to 7 bits
+              BitfieldAssignment(to_underlying(readPriv) & ((1 << 5) - 1)),         // Narrowing expression to 5 bits
+              BitfieldAssignment(to_underlying(writePriv) & ((1 << 5) - 1)) }       // Narrowing expression to 5 bits
+    {}
+
+    // Getter for mask.readPrivilege
+    constexpr Access::Privilege GetReadPrivilege() const { return static_cast<Access::Privilege>(mask.readPrivilege); }
+
+    // Getter for mask.writePrivilege
+    constexpr Access::Privilege GetWritePrivilege() const { return static_cast<Access::Privilege>(mask.writePrivilege); }
+
+    constexpr bool HasFlags(AttributeQualityFlags f) const { return (mask.flags & chip::to_underlying(f)) != 0; }
+
+    constexpr bool ReadAllowed() const { return mask.readPrivilege != to_underlying(Access::Privilege::kNoPrivilege); }
+    constexpr bool WriteAllowed() const { return mask.writePrivilege != to_underlying(Access::Privilege::kNoPrivilege); }
+
+private:
+    struct attribute_entry_mask_t
     {
-        // Tree pattern supports a general tree of endpoints. Commonly used for
-        // device types that support physical device composition (e.g. Refrigerator)
-        kTree = 0x1,
 
-        // A full-family pattern is a list of all descendant endpoints, with no
-        // imposed hierarchy.
+        // attribute quality flags
         //
-        // For example the Root Node and Aggregator device types use the full-familiy
-        // pattern, as defined in their device type specification
-        kFullFamily = 0x2,
+        // flags is a uint32_t bitfield of size 7, in order to accomodate all
+        // the different values of "enum class AttributeQualityFlags".
+        //
+        // Consider that any modification on the declaration of
+        // "enum class AttributeQualityFlags" will affect flags.
+        std::underlying_type_t<AttributeQualityFlags> flags : 7;
+
+        // read/write access privilege variables
+        //
+        // readPrivilege is a uint8_t bitfield of size 5, in order to accomodate all
+        // the different values of "enum class Privilege".
+        // Same case for writePrivilege.
+        //
+        // Consider that any modification on the declaration of "enum class Privilege"
+        // will affect both readPrivilege and writePrivilege.
+        //
+        // The use of bitfields means that each variable holds an individual
+        // Access::Privilege value, as a bitwise value. This allows us to
+        // handle Access::Privilege information without any bit fiddling.
+        std::underlying_type_t<Access::Privilege> readPrivilege : 5;
+        std::underlying_type_t<Access::Privilege> writePrivilege : 5;
     };
 
-    struct EndpointEntry
+    // Static ASSERT to check size of mask type "attribute_entry_mask_t"
+    static_assert(sizeof(attribute_entry_mask_t) <= 4, "Size of attribute_entry_mask_t is not as expected.");
+
+    attribute_entry_mask_t mask;
+};
+
+// Static ASSERT to check size of AttributeEntry
+static_assert(sizeof(AttributeEntry) <= 8, "Size of AttributeEntry is not as expected.");
+
+// Bitmask values for different Command qualities.
+enum class CommandQualityFlags : uint32_t
+{
+    kNoAttribute  = 0x0000, // No attribute value
+    kFabricScoped = 0x0001,
+    kTimed        = 0x0002, // `T` quality on commands
+    kLargeMessage = 0x0004, // `L` quality on commands
+};
+
+struct AcceptedCommandEntry
+{
+    CommandId commandId;
+
+    // Constructor
+    constexpr AcceptedCommandEntry(
+        CommandId id = 0,                                                        // commandId initial value,
+                                                                                 // this could be altered later
+        CommandQualityFlags cmdQualityFlags = CommandQualityFlags::kNoAttribute, // mask.flags initial value
+        Access::Privilege invokePriv        = Access::Privilege::kView           // mask.invokePrivilege initial value
+        ) :
+        commandId(id),
+        mask{ BitfieldAssignment(to_underlying(cmdQualityFlags) & ((1 << 3) - 1)), // Narrowing expression to 3 bits
+              BitfieldAssignment(to_underlying(invokePriv) & ((1 << 5) - 1)) }     // Narrowing expression to 5 bits
+    {}
+
+    // Getter for mask.invokePrivilege
+    constexpr Access::Privilege GetInvokePrivilege() const { return static_cast<Access::Privilege>(mask.invokePrivilege); }
+
+    constexpr bool HasFlags(CommandQualityFlags f) const { return (mask.flags & chip::to_underlying(f)) != 0; }
+
+private:
+    struct accepted_command_entry_mask_t
     {
-        EndpointId id;
 
-        // kInvalidEndpointId if there is no explicit parent endpoint (which means the parent is endpoint 0,
-        // for endpoints other than endpoint 0).
-        EndpointId parentId;
-        EndpointCompositionPattern compositionPattern;
+        // command quality flags
+        //
+        std::underlying_type_t<CommandQualityFlags> flags : 3; // generally defaults to View if readable
+
+        // invoke privilege variable
+        //
+        std::underlying_type_t<Access::Privilege> invokePrivilege : 5;
     };
 
-    enum class ClusterQualityFlags : uint32_t
+    // Static ASSERT to check size of mask type "accepted_command_entry_mask_t"
+    static_assert(sizeof(accepted_command_entry_mask_t) <= 4, "Size of accepted_command_entry_mask_t is not as expected.");
+
+    accepted_command_entry_mask_t mask;
+};
+
+/// Represents a device type that resides on an endpoint
+struct DeviceTypeEntry
+{
+    DeviceTypeId deviceTypeId;
+    uint8_t deviceTypeRevision;
+
+    bool operator==(const DeviceTypeEntry & other) const
     {
-        kDiagnosticsData = 0x0001, // `K` quality, may be filtered out in subscriptions
-    };
+        return (deviceTypeId == other.deviceTypeId) && (deviceTypeRevision == other.deviceTypeRevision);
+    }
+};
 
-    struct ServerClusterEntry
-    {
-        ClusterId clusterId;
-        DataVersion dataVersion; // current cluster data version,
-        BitFlags<ClusterQualityFlags> flags;
-    };
-
-    struct ClusterInfo
-    {
-        DataVersion dataVersion; // current cluster data version,
-        BitFlags<ClusterQualityFlags> flags;
-
-        /// Constructor that marks data version as mandatory
-        /// for this structure.
-        ClusterInfo(DataVersion version) : dataVersion(version) {}
-    };
-
-    enum class AttributeQualityFlags : uint32_t
-    {
-        kNoAttribute     = 0x0000, // No attribute value
-        kListAttribute   = 0x0004, // This attribute is a list attribute
-        kFabricScoped    = 0x0008, // 'F' quality on attributes
-        kFabricSensitive = 0x0010, // 'S' quality on attributes
-        kChangesOmitted  = 0x0020, // `C` quality on attributes
-        kTimed           = 0x0040, // `T` quality on attributes (writes require timed interactions)
-    };
-
-    struct AttributeEntry
-    {
-        AttributeId attributeId;
-
-            // Constructor
-            constexpr AttributeEntry(
-                AttributeId id = 0,                                                           // attributeId initial value,
-                                                                                              // this could be altered later
-                AttributeQualityFlags attrQualityFlags = AttributeQualityFlags::kNoAttribute, // mask.flags initial value
-                Access::Privilege readPriv             = Access::Privilege::kNoPrivilege,     // mask.readPrivilege initial value
-                Access::Privilege writePriv            = Access::Privilege::kNoPrivilege      // mask.writePrivilege initial value
-                ) :
-            attributeId{ id },
-            mask{ BitfieldAssignment(to_underlying(attrQualityFlags) & ((1 << 7) - 1)), // Narrowing expression to 7 bits
-                  BitfieldAssignment(to_underlying(readPriv) & ((1 << 5) - 1)),         // Narrowing expression to 5 bits
-                  BitfieldAssignment(to_underlying(writePriv) & ((1 << 5) - 1)) }       // Narrowing expression to 5 bits
-        {}
-
-
-            // Getter for mask.readPrivilege
-            constexpr Access::Privilege
-            GetReadPrivilege() const
-        {
-            return static_cast<Access::Privilege>(mask.readPrivilege);
-        }
-
-        // Getter for mask.writePrivilege
-        constexpr Access::Privilege GetWritePrivilege() const { return static_cast<Access::Privilege>(mask.writePrivilege); }
-
-        constexpr bool HasFlags(AttributeQualityFlags f) const { return (mask.flags & chip::to_underlying(f)) != 0; }
-
-        constexpr bool ReadAllowed() const { return mask.readPrivilege != to_underlying(Access::Privilege::kNoPrivilege); }
-        constexpr bool WriteAllowed() const { return mask.writePrivilege != to_underlying(Access::Privilege::kNoPrivilege); }
-
-    private:
-        struct attribute_entry_mask_t
-        {
-
-            // attribute quality flags
-            //
-            // flags is a uint32_t bitfield of size 7, in order to accomodate all
-            // the different values of "enum class AttributeQualityFlags".
-            //
-            // Consider that any modification on the declaration of
-            // "enum class AttributeQualityFlags" will affect flags.
-            std::underlying_type_t<AttributeQualityFlags> flags : 7;
-
-            // read/write access privilege variables
-            //
-            // readPrivilege is a uint8_t bitfield of size 5, in order to accomodate all
-            // the different values of "enum class Privilege".
-            // Same case for writePrivilege.
-            //
-            // Consider that any modification on the declaration of "enum class Privilege"
-            // will affect both readPrivilege and writePrivilege.
-            //
-            // The use of bitfields means that each variable holds an individual
-            // Access::Privilege value, as a bitwise value. This allows us to
-            // handle Access::Privilege information without any bit fiddling.
-            std::underlying_type_t<Access::Privilege> readPrivilege : 5;
-            std::underlying_type_t<Access::Privilege> writePrivilege : 5;
-        };
-
-        // Static ASSERT to check size of mask type "attribute_entry_mask_t"
-        static_assert(sizeof(attribute_entry_mask_t) <= 4, "Size of attribute_entry_mask_t is not as expected.");
-
-        attribute_entry_mask_t mask;
-    };
-
-    // Static ASSERT to check size of AttributeEntry
-    static_assert(sizeof(AttributeEntry) <= 8, "Size of AttributeEntry is not as expected.");
-
-    // Bitmask values for different Command qualities.
-    enum class CommandQualityFlags : uint32_t
-    {
-        kNoAttribute  = 0x0000, // No attribute value
-        kFabricScoped = 0x0001,
-        kTimed        = 0x0002, // `T` quality on commands
-        kLargeMessage = 0x0004, // `L` quality on commands
-    };
-
-    struct AcceptedCommandEntry
-    {
-        CommandId commandId;
-
-
-            // Constructor
-            constexpr AcceptedCommandEntry(
-                CommandId id = 0,                                                         // commandId initial value,
-                                                                                          // this could be altered later
-                CommandQualityFlags cmdQualityFlags = CommandQualityFlags::kNoAttribute,  // mask.flags initial value
-                Access::Privilege invokePriv        = Access::Privilege::kView            // mask.invokePrivilege initial value
-                ) :
-            commandId(id),
-            mask{ BitfieldAssignment(to_underlying(cmdQualityFlags) & ((1 << 3) - 1)), // Narrowing expression to 3 bits
-                  BitfieldAssignment(to_underlying(invokePriv) & ((1 << 5) - 1)) }     // Narrowing expression to 5 bits
-        {}
-
-
-            // Getter for mask.invokePrivilege
-            constexpr Access::Privilege
-            GetInvokePrivilege() const
-        {
-            return static_cast<Access::Privilege>(mask.invokePrivilege);
-        }
-
-        constexpr bool HasFlags(CommandQualityFlags f) const { return (mask.flags & chip::to_underlying(f)) != 0; }
-
-    private:
-        struct accepted_command_entry_mask_t
-        {
-
-            // command quality flags
-            //
-            std::underlying_type_t<CommandQualityFlags> flags : 3; // generally defaults to View if readable
-
-            // invoke privilege variable
-            //
-            std::underlying_type_t<Access::Privilege> invokePrivilege : 5;
-        };
-
-        // Static ASSERT to check size of mask type "accepted_command_entry_mask_t"
-        static_assert(sizeof(accepted_command_entry_mask_t) <= 4, "Size of accepted_command_entry_mask_t is not as expected.");
-
-        accepted_command_entry_mask_t mask;
-    };
-
-    /// Represents a device type that resides on an endpoint
-    struct DeviceTypeEntry
-    {
-        DeviceTypeId deviceTypeId;
-        uint8_t deviceTypeRevision;
-
-        bool operator==(const DeviceTypeEntry & other) const
-        {
-            return (deviceTypeId == other.deviceTypeId) && (deviceTypeRevision == other.deviceTypeRevision);
-        }
-    };
-
-    } // namespace DataModel
-    } // namespace app
+} // namespace DataModel
+} // namespace app
 } // namespace chip
 
 #undef BitfieldAssignment
