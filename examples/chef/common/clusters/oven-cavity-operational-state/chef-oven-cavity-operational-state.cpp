26b54a4d584cd22101f705964ad5d4bac036adf7
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-04-21 16:52:17 -0700

    Oven cavity operational state cluster delegate imlpementation with run cycle supported in three phases

--- examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.cpp
+++ examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.cpp
@@ -24,6 +24,7 @@
 #ifdef MATTER_DM_PLUGIN_OVEN_CAVITY_OPERATIONAL_STATE_SERVER
 
 using namespace chip;
+using namespace chip::app::Clusters;
 
 namespace chef {
 namespace OvenCavityOperationalState {
@@ -32,7 +33,267 @@ constexpr size_t kOvenCavityOperationalStateTableSize = MATTER_DM_OPERATIONAL_ST
 static_assert(kOvenCavityOperationalStateTableSize <= kEmberInvalidEndpointIndex, "OvenCavityOperationalState table size error");
 
 std::unique_ptr<Delegate> gDelegateTable[kOvenCavityOperationalStateTableSize];
-std::unique_ptr<chip::app::Clusters::OvenCavityOperationalState::Instance> gInstanceTable[kOvenCavityOperationalStateTableSize];
+std::unique_ptr<OvenCavityOperationalState::Instance> gInstanceTable[kOvenCavityOperationalStateTableSize];
+
+static void onOperationalStateTimerTick(System::Layer * systemLayer, void * data)
+{
+    Delegate * delegate = reinterpret_cast<Delegate *>(data);
+
+    uint8_t opState = delegate->GetCurrentOperationalState();
+    if (opState != to_underlying(OvenCavityOperationalState::OperationalStateEnum::kRunning) &&
+        opState != to_underlying(OvenCavityOperationalState::OperationalStateEnum::kPaused))
+    {
+        ChipLogError(DeviceLayer, "onOperationalStateTimerTick: Operational cycle can not be active in state %d", opState);
+        (void) DeviceLayer::SystemLayer().CancelTimer(onOperationalStateTimerTick, delegate);
+        return;
+    }
+
+    if (!delegate->CheckCycleActive())
+    {
+        (void) DeviceLayer::SystemLayer().CancelTimer(onOperationalStateTimerTick, delegate);
+        return;
+    }
+
+    delegate->CycleSecondTick();
+
+    if (delegate->CheckCycleComplete())
+    {
+        GenericOperationalError err;
+        delegate->HandleStopStateCallback(err);
+    }
+    else
+    {
+        (void) DeviceLayer::SystemLayer().StartTimer(System::Clock::Seconds16(1), onOperationalStateTimerTick, delegate);
+    }
+}
+
+app::DataModel::Nullable<uint32_t> Delegate::GetCountdownTime()
+{
+    if (mRunningTime.IsNull() || CheckCycleComplete())
+        return DataModel::NullNullable;
+
+    return DataModel::MakeNullable(kCycleSeconds - mRunningTime.Value());
+}
+
+bool Delegate::StartCycle()
+{
+    if (!mRunningTime.IsNull())
+    {
+        ChipLogError(DeviceLayer, "StartCycle: Cycle is in progress. Can not start new cycle.");
+        return false;
+    }
+    mRunningTime.SetNonNull(static_cast<uint32_t>(0));
+    mPausedTime.SetNonNull(static_cast<uint32_t>(0));
+    app::DataModel::Nullable<uint8_t> phase = GetRunningPhase();
+    if (phase.IsNull())
+    {
+        ChipLogError(DeviceLayer, "StartCycle: Phase did't update to non-NULL.");
+        return false;
+    }
+    if (phase.Value() != 0)
+    {
+        ChipLogError(DeviceLayer, "StartCycle: Unexpected start phase: %d.", phase.Value());
+        return false;
+    }
+    CHIP_ERROR err = GetInstance()->SetCurrentPhase(phase);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "StartCycle: Error setting current phase %" CHIP_ERROR_FORMAT, err.Format());
+    }
+    ChipLogDetail(DeviceLayer, "Oven Cycle Started. Current Phase: %d. Cycle time: %d", phase.Value(), kCycleSeconds);
+    return true;
+}
+
+void Delegate::CycleSecondTick()
+{
+    if (mRunningTime.IsNull() || CheckCycleComplete())
+        return;
+    uint8_t opState                            = GetCurrentOperationalState();
+    app::DataModel::Nullable<uint8_t> oldPhase = GetRunningPhase();
+    if (opState == to_underlying(OvenCavityOperationalState::OperationalStateEnum::kRunning))
+    {
+        mRunningTime.SetNonNull(mRunningTime.Value() + 1);
+    }
+    else if (opState == to_underlying(OvenCavityOperationalState::OperationalStateEnum::kPaused))
+    {
+        if (mPausedTime.IsNull())
+            mPausedTime.SetNonNull(static_cast<uint32_t>(0));
+        mPausedTime.SetNonNull(mPausedTime.Value() + 1);
+    }
+    else
+    {
+        ChipLogError(DeviceLayer, "CycleSecondTick: Invalid state %d", opState);
+        return;
+    }
+    app::DataModel::Nullable<uint8_t> newPhase = GetRunningPhase();
+    if (newPhase != oldPhase)
+    {
+        CHIP_ERROR err = GetInstance()->SetCurrentPhase(newPhase);
+        if (err != CHIP_NO_ERROR)
+        {
+            ChipLogError(DeviceLayer, "CycleSecondTick: Error setting current phase %" CHIP_ERROR_FORMAT, err.Format());
+        }
+    }
+    GetInstance()->UpdateCountdownTimeFromDelegate();
+
+    ChipLogDetail(DeviceLayer, "CycleSecondTick: Running time: %d. Paused time: %d", mRunningTime.Value(), mPausedTime.Value());
+    if (newPhase.IsNull())
+    {
+        ChipLogDetail(DeviceLayer, "CycleSecondTick: Phase: NULL");
+    }
+    else
+    {
+        ChipLogDetail(DeviceLayer, "CycleSecondTick: Phase: %d", newPhase.Value());
+    }
+}
+
+app::DataModel::Nullable<uint8_t> Delegate::GetRunningPhase()
+{
+    if (mRunningTime.IsNull() || CheckCycleComplete())
+        return DataModel::NullNullable;
+
+    if (mRunningTime.Value() >= kPreHeatingSeconds + kPreHeatedSeconds)
+        return 2; // cooling down
+    else if (mRunningTime.Value() >= kPreHeatingSeconds)
+        return 1; // pre-heated
+    else
+        return 0; // pre-heating
+}
+
+bool Delegate::CheckCycleComplete()
+{
+    return !mRunningTime.IsNull() && mRunningTime.Value() >= kCycleSeconds;
+}
+
+void Delegate::EndCycle()
+{
+    mRunningTime.SetNull();
+    mPausedTime.SetNull();
+    CHIP_ERROR err = GetInstance()->SetCurrentPhase(DataModel::NullNullable);
+    if (err != CHIP_NO_ERROR)
+    {
+        ChipLogError(DeviceLayer, "EndCycle: Error setting current phase %" CHIP_ERROR_FORMAT, err.Format());
+    }
+}
+
+bool Delegate::CheckCycleActive()
+{
+    return !mRunningTime.IsNull();
+}
+
+uint8_t Delegate::GetCurrentOperationalState()
+{
+    return GetInstance()->GetCurrentOperationalState();
+}
+
+void Delegate::HandleStartStateCallback(GenericOperationalError & err)
+{
+    OperationalState::GenericOperationalError current_err(to_underlying(OperationalState::ErrorStateEnum::kNoError));
+    GetInstance()->GetCurrentOperationalError(current_err);
+    uint8_t opState = GetCurrentOperationalState();
+
+    if (current_err.errorStateID != to_underlying(OperationalState::ErrorStateEnum::kNoError) ||
+        opState == OvenCavityOperationalState::OperationalStateEnum::kError)
+    {
+        err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToStartOrResume));
+        return;
+    }
+
+    if (opState == to_underlying(OvenCavityOperationalState::OperationalStateEnum::kRunning) ||
+        opState == to_underlying(OvenCavityOperationalState::OperationalStateEnum::kPaused))
+    {
+        err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kNoError));
+        return;
+    }
+
+    if (CheckCycleActive())
+    {
+        (void) DeviceLayer::SystemLayer().CancelTimer(onOperationalStateTimerTick, this);
+        EndCycle();
+    }
+
+    auto error = GetInstance()->SetOperationalState(to_underlying(OvenCavityOperationalState::OperationalStateEnum::kRunning));
+    if (error != CHIP_NO_ERROR)
+    {
+        err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToCompleteOperation));
+        return;
+    }
+    if (!StartCycle())
+    {
+        err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToStartOrResume));
+        return;
+    }
+    (void) DeviceLayer::SystemLayer().StartTimer(System::Clock::Seconds16(1), onOperationalStateTimerTick, this);
+    err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kNoError));
+}
+
+void Delegate::HandleStopStateCallback(GenericOperationalError & err)
+{
+    uint32_t RunningTime = mRunningTime.IsNull() ? 0 : mRunningTime.Value();
+    uint32_t PausedTime  = mPausedTime.IsNull() ? 0 : mPausedTime.Value();
+    uint8_t opState      = GetCurrentOperationalState();
+    if (CheckCycleActive())
+    {
+        (void) DeviceLayer::SystemLayer().CancelTimer(onOperationalStateTimerTick, this);
+        EndCycle();
+    }
+
+    if (opState != OvenCavityOperationalState::OperationalStateEnum::kRunning &&
+        opState != OvenCavityOperationalState::OperationalStateEnum::kPaused)
+    {
+        ChipLogDetail(DeviceLayer, "HandleStopStateCallback: Cycle not started. Current state = %d. Returning.",
+                      to_underlying(current_state));
+        err.Set(to_underlying(OperationalState::ErrorStateEnum::kNoError));
+        return;
+    }
+
+    auto error = GetInstance()->SetOperationalState(to_underlying(OvenCavityOperationalState::OperationalStateEnum::kStopped));
+    if (error != CHIP_NO_ERROR)
+    {
+        err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToCompleteOperation));
+        return;
+    }
+
+    OperationalState::GenericOperationalError current_err(to_underlying(OperationalState::ErrorStateEnum::kNoError));
+    GetInstance()->GetCurrentOperationalError(current_err);
+
+    Optional<DataModel::Nullable<uint32_t>> totalTime((DataModel::Nullable<uint32_t>(RunningTime + PausedTime)));
+    Optional<DataModel::Nullable<uint32_t>> pausedTime((DataModel::Nullable<uint32_t>(PausedTime)));
+
+    GetInstance()->OnOperationCompletionDetected(static_cast<uint8_t>(current_err.errorStateID), totalTime, pausedTime);
+    err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kNoError));
+}
+
+void Delegate::HandlePauseStateCallback(GenericOperationalError & err)
+{
+    err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToCompleteOperation)); // Not supported.
+}
+
+void Delegate::HandleResumeStateCallback(GenericOperationalError & err)
+{
+    err.Set(to_underlying(OvenCavityOperationalState::ErrorStateEnum::kUnableToCompleteOperation)); // Not supported.
+}
+
+CHIP_ERROR Delegate::GenericOperationalStateDelegateImpl::GetOperationalStateAtIndex(size_t index,
+                                                                                     GenericOperationalState & operationalState)
+{
+    if (index >= mOperationalStateList.size())
+    {
+        return CHIP_ERROR_NOT_FOUND;
+    }
+    operationalState = GenericOperationalState(to_underlying(mOperationalStateList[index]));
+    return CHIP_NO_ERROR;
+}
+
+CHIP_ERROR Delegate::GenericOperationalStateDelegateImpl::GetOperationalPhaseAtIndex(size_t index,
+                                                                                     MutableCharSpan & operationalPhase)
+{
+    if (index >= mOperationalPhaseList.size())
+    {
+        return CHIP_ERROR_NOT_FOUND;
+    }
+    return CopyCharSpanToMutableCharSpan(mOperationalPhaseList[index], operationalPhase);
+}
 
 /**
  * Initializes OvenCavityOperationalState cluster for the app (all endpoints).
@@ -50,15 +311,14 @@ void InitChefOvenCavityOperationalStateCluster()
         }
 
         // Check if endpoint has OvenCavityOperationalState cluster enabled
-        uint16_t epIndex = emberAfGetClusterServerEndpointIndex(endpointId, chip::app::Clusters::OvenCavityOperationalState::Id,
+        uint16_t epIndex = emberAfGetClusterServerEndpointIndex(endpointId, OvenCavityOperationalState::Id,
                                                                 MATTER_DM_OPERATIONAL_STATE_OVEN_CLUSTER_SERVER_ENDPOINT_COUNT);
         if (epIndex >= kOvenCavityOperationalStateTableSize)
             continue;
 
         gDelegateTable[epIndex] = std::make_unique<Delegate>();
 
-        gInstanceTable[epIndex] =
-            std::make_unique<chip::app::Clusters::OvenCavityOperationalState::Instance>(gDelegateTable[epIndex].get(), endpointId);
+        gInstanceTable[epIndex] = std::make_unique<OvenCavityOperationalState::Instance>(gDelegateTable[epIndex].get(), endpointId);
         gInstanceTable[epIndex]->Init();
 
         ChipLogProgress(DeviceLayer, "Endpoint %d OvenCavityOperationalState Initialized.", endpointId);
--- examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.h
+++ examples/chef/common/clusters/oven-cavity-operational-state/chef-oven-cavity-operational-state.h
@@ -18,26 +18,97 @@
 
 #pragma once
 
-#include "chef-operational-state-delegate-impl.h"
+#include <app-common/zap-generated/cluster-objects.h>
+#include <app/clusters/operational-state-server/operational-state-server.h>
+
+#include <app/util/attribute-metadata.h>
+#include <protocols/interaction_model/StatusCode.h>
 
 #ifdef MATTER_DM_PLUGIN_OVEN_CAVITY_OPERATIONAL_STATE_SERVER
 
 using namespace chip;
+using namespace chip::app::Clusters;
 
 namespace chef {
 namespace OvenCavityOperationalState {
 
-class Delegate : public chip::app::Clusters::OperationalState::OperationalStateDelegate
+/**
+ * @brief Delegate handles heating cycle in 3 phases: pre-heating (0), pre-heated (1) and cooling down (2).
+ * Cycle times are compile constants. TBD if cycle times need to be made run time modifiable.
+ */
+class Delegate : public OperationalState::Delegate
 {
 private:
-    const CharSpan kPhaseList[3] = { "pre-heating"_span, "pre-heated"_span, "cooling down"_span };
+    constexpr CharSpan kPhaseList[3]       = { "pre-heating"_span, "pre-heated"_span, "cooling down"_span };
+    constexpr uint32_t kPreHeatingSeconds  = 10;
+    constexpr uint32_t kPreHeatedSeconds   = 50;
+    constexpr uint32_t kCoolingDownSeconds = 10;
+    constexpr uint32_t kCycleSeconds       = kPreHeatingSeconds + kPreHeatedSeconds + kCoolingDownSeconds;
+    constexpr OvenCavityOperationalState::OperationalStateEnum kOpStateList[4] = {
+        OperationalStateEnum::kStopped,
+        OperationalStateEnum::kRunning,
+        OperationalStateEnum::kPaused,
+        OperationalStateEnum::kError,
+    };
+
+    Span<constexpr OvenCavityOperationalState::OperationalStateEnum> mOperationalStateList;
+    Span<constexpr CharSpan> mOperationalPhaseList;
+
+    // Non-null when cycle is in progress.
+    app::DataModel::Nullable<uint32_t> mRunningTime;
+    app::DataModel::Nullable<uint32_t> mPausedTime;
 
 public:
     Delegate()
     {
-        chip::app::Clusters::OperationalState::GenericOperationalStateDelegateImpl::mOperationalPhaseList =
-            Span<const CharSpan>(kPhaseList);
+        mRunningTime.SetNull();
+        mPausedTime.SetNull();
+        mOperationalStateList = Span<constexpr OvenCavityOperationalState::OperationalStateEnum>(kOpStateList);
+        mOperationalPhaseList = Span<constexpr CharSpan>(kPhaseList);
     }
+
+    app::DataModel::Nullable<uint32_t> GetCountdownTime() override;
+    CHIP_ERROR GetOperationalStateAtIndex(size_t index, GenericOperationalState & operationalState) override;
+    CHIP_ERROR GetOperationalPhaseAtIndex(size_t index, MutableCharSpan & operationalPhase) override;
+    void HandlePauseStateCallback(GenericOperationalError & err) override;
+    void HandleResumeStateCallback(GenericOperationalError & err) override;
+    void HandleStartStateCallback(GenericOperationalError & err) override;
+    void HandleStopStateCallback(GenericOperationalError & err) override;
+
+    /**
+     * @brief Starts a new cycle with Run/Pause times set to 0. Returns True if new cycle started successfully.
+     * Returns False on failure / current cycle in progress.
+     */
+    bool StartCycle();
+
+    /**
+     * @brief Increments run/paused (based on operational state) by one and updates current Phase if changed.
+     * NOP if no cycle is in progress / cycle has finished.
+     */
+    void CycleSecondTick();
+
+    /**
+     * @brief Gets the current phase based on current running time. NULL if cycle is not in progress or has
+     * completed.
+     */
+    app::DataModel::Nullable<uint8_t> GetRunningPhase();
+
+    /**
+     * @brief Returns True if an in-progress cycle run time has reached its cycle time. False otherwise.
+     */
+    bool CheckCycleComplete();
+
+    /**
+     * @brief Returns True if cycle was started but not ended.
+     */
+    bool CheckCycleActive();
+
+    /**
+     * @brief Sets run/pause times and current phase to NULL.
+     */
+    void EndCycle();
+
+    uint8_t GetCurrentOperationalState();
 };
 
 void InitChefOvenCavityOperationalStateCluster();
