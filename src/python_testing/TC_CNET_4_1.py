c7ca065bf837585a8e735f57710be73594f4cc38
jtrejoespinoza-grid <juanerasmo@google.com>
2025-04-21 12:58:28 -0700

    Lint fixes

--- src/python_testing/TC_CNET_4_1.py
+++ src/python_testing/TC_CNET_4_1.py
@@ -35,12 +35,14 @@
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
 import logging
+
 import chip.clusters as Clusters
 import test_plan_support
 from chip.clusters.Types import NullValue
 from chip.testing import matter_asserts
 from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_feature, run_if_endpoint_matches
 from mobly import asserts
+
 logger = logging.getLogger(__name__)
 
 
