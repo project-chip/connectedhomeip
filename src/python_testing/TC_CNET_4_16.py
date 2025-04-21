e34c17ecfc352bb114d3234f849aaaf6f9b2d85b
Alejandro Buezo <abuezo@griddynamics.com>
2025-04-21 10:45:33 -0300

    restyled imports

--- src/python_testing/TC_CNET_4_16.py
+++ src/python_testing/TC_CNET_4_16.py
@@ -30,13 +30,13 @@
 #     factory-reset: false
 #     quiet: true
 
+import logging
+import chip.clusters as Clusters
 
 from mobly import asserts
-import chip.clusters as Clusters
 from matter_testing_infrastructure.chip.testing.matter_asserts import assert_valid_uint8
 from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_feature, run_if_endpoint_matches
 
-import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
