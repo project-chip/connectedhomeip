822ba52b71de8812343ca439331c079342587fd8
James Harrow <james.harrow@gmail.com>
2025-04-22 10:20:36 +0100

    Cherry picked TC_EGC scripts from EGC_CLUSTER_SERVER branch

---
+++ src/python_testing/TC_EGC_2_3.py
@@ -0,0 +1,121 @@
+#
+#    Copyright (c) 2025 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs:
+#   run1:
+#     app: ${ENERGY_GATEWAY_APP}
+#     app-args: >
+#       --discriminator 1234
+#       --KVS kvs1
+#       --trace-to json:${TRACE_APP}.json
+#       --enable-key 000102030405060708090a0b0c0d0e0f
+#     script-args: >
+#       --storage-path admin_storage.json
+#       --commissioning-method on-network
+#       --discriminator 1234
+#       --passcode 20202021
+#       --hex-arg enableKey:000102030405060708090a0b0c0d0e0f
+#       --endpoint 1
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#     factory-reset: true
+#     quiet: true
+# === END CI TEST ARGUMENTS ===
+
+import logging
+
+import chip.clusters as Clusters
+from chip.clusters.Types import NullValue
+from chip.testing import matter_asserts
+from chip.testing.matter_testing import EventChangeCallback, TestStep, MatterBaseTest, default_matter_test_main, has_cluster, run_if_endpoint_matches
+from TC_EGCTestBase import ElectricalGridConditionsTestBaseHelper
+
+from mobly import asserts
+
+logger = logging.getLogger(__name__)
+
+cluster = Clusters.ElectricalGridConditions
+
+
+class EGC_2_3(ElectricalGridConditionsTestBaseHelper, MatterBaseTest):
+    """Implementation of test case TC_EGC_2_3."""
+
+    def desc_EGC_2_3(self) -> str:
+        """Returns a description of this test"""
+        return "[TC-EGC-2.3] Forecast feature functionality with DUT as Server"
+
+    def pics_EGC_2_3(self) -> list[str]:
+        """This function returns a list of PICS for this test case that must be True for the test to be run"""
+        return [
+            "EGC.S",
+            "EGC.F00(FORE)"
+        ]
+
+    def steps_EGC_2_3(self) -> list[TestStep]:
+        steps = [
+            TestStep("1", "Commission DUT to TH (can be skipped if done in a preceding test)."),
+            TestStep("2", "Set up a subscription to all ElectricalGridConditions cluster events"),
+            TestStep("3", "TH reads TestEventTriggersEnabled attribute from General Diagnostics Cluster",
+                     "Value has to be 1 (True)"),
+            TestStep("4", "TH sends TestEventTrigger command to General Diagnostics Cluster on Endpoint 0 with EnableKey field set to PIXIT.EGC.TESTEVENT_TRIGGERKEY and EventTrigger field set to PIXIT.EGC.TESTEVENTTRIGGER for Forecast Conditions Update Test Event",
+                     "Verify DUT responds w/ status SUCCESS(0x00) and event EGC.S.E0001(ForecastConditionsChanged) sent."),
+            TestStep("4a", "TH reads from the DUT the ForecastConditions attribute.",
+                     "Verify that the DUT response contains a list of  ElectricalGridConditionsStruct (or empty)."),
+        ]
+        return steps
+
+    @run_if_endpoint_matches(has_cluster(Clusters.ElectricalGridConditions))
+    async def test_EGC_2_3(self):
+        endpoint = self.get_endpoint()
+        attributes = cluster.Attributes
+
+        self.step("1")
+        # Commission DUT - already done
+
+        self.step("2")
+        events_callback = EventChangeCallback(cluster)
+        await events_callback.start(self.default_controller,
+                                    self.dut_node_id,
+                                    endpoint)
+
+        self.step("3")
+        # TH reads TestEventTriggersEnabled attribute from General Diagnostics Cluster
+        await self.check_test_event_triggers_enabled()
+
+        self.step("4")
+        # TH sends TestEventTrigger command to General Diagnostics Cluster on Endpoint 0 with EnableKey field set to PIXIT.EGC.TESTEVENT_TRIGGERKEY and EventTrigger field set to PIXIT.EGC.TESTEVENTTRIGGER for Forecast Conditions Update Test Event
+        # Verify DUT responds w/ status SUCCESS(0x00) and event EGC.S.E0001(ForecastConditionsChanged) sent.
+        await self.send_test_event_trigger_forecast_conditions_update()
+
+        events_callback.wait_for_event_report(
+            Clusters.ElectricalGridConditions.Events.ForecastConditionsChanged)
+
+        self.step("4a")
+        # TH reads from the DUT the ForecastConditions attribute.
+        # Verify that the DUT response contains a list of  ElectricalGridConditionsStruct (or empty).
+        val = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster,
+                                                             attribute=attributes.ForecastConditions)
+
+        await self.test_ForecastConditions(endpoint=endpoint, cluster=cluster,
+                                           forecastConditions=val)
+
+
+if __name__ == "__main__":
+    default_matter_test_main()
