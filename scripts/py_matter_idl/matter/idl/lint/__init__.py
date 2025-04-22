561c9638362e1f19c4da7ad8da0678441cb6d171
Restyled.io <commits@restyled.io>
2025-04-22 08:11:46 +0000

    Restyled by autopep8

--- scripts/py_matter_idl/matter/idl/lint/__init__.py
+++ scripts/py_matter_idl/matter/idl/lint/__init__.py
@@ -64,7 +64,8 @@ def main(log_level, rules, idl_path):
     lint_rules.extend(CreateParser(rules).parse())
 
     logging.info("Parsing idl from %s" % idl_path)
-    idl_tree = matter_idl_parser.CreateParser().parse(open(idl_path, "rt").read(), file_name=idl_path)
+    idl_tree = matter_idl_parser.CreateParser().parse(
+        open(idl_path, "rt").read(), file_name=idl_path)
 
     logging.info("Running %d lint rules" % len(lint_rules))
 
