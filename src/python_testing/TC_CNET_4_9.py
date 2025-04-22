d4c9676c8df7d24a207399ffd8d12c3598e32f73
Ramiro Recchia <rrecchia@google.com>
2025-04-22 10:00:31 -0300

    Fixes. Removed from CI, test updated

--- src/python_testing/TC_CNET_4_9.py
+++ src/python_testing/TC_CNET_4_9.py
@@ -25,9 +25,9 @@
 #     script-args: >
 #       --endpoint 0
 #       --storage-path admin_storage.json
-#       --commissioning-method on-network
 #       --discriminator 1234
 #       --passcode 20202021
+#       --commissioning_method ble-wifi
 #       --PICS src/app/tests/suites/certification/ci-pics-values
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
@@ -38,9 +38,10 @@
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
 from chip.testing.matter_asserts import is_valid_bool_value
-from chip.testing.matter_testing import MatterBaseTest, TestStep, run_if_endpoint_matches, default_matter_test_main, has_feature, type_matches, async_test_body
+from chip.testing.matter_testing import MatterBaseTest, TestStep, run_if_endpoint_matches, default_matter_test_main, has_feature, type_matches
 from mobly import asserts
 
+import logging
 
 class TC_CNET_4_9(MatterBaseTest):
     def steps_TC_CNET_4_9(self):
@@ -71,10 +72,9 @@ class TC_CNET_4_9(MatterBaseTest):
     def pics_TC_CNET_4_9(self):
         return ['CNET.S']
 
-    # @run_if_endpoint_matches(has_feature(Clusters.NetworkCommissioning, Clusters.NetworkCommissioning.Bitmaps.Feature.kWiFiNetworkInterface))
-    @async_test_body
+    @run_if_endpoint_matches(has_feature(Clusters.NetworkCommissioning, Clusters.NetworkCommissioning.Bitmaps.Feature.kWiFiNetworkInterface))
     async def test_TC_CNET_4_9(self):
-        ssid = b'en3'  # IS THIS CORRECT? PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID
+        ssid = self.get_wifi_ssid()
 
         # Commissioning is already done
         self.step("Precondition")
@@ -87,18 +87,15 @@ class TC_CNET_4_9(MatterBaseTest):
         # Verify that DUT sends ArmFailSafeResponse command to the TH
         asserts.assert_true(type_matches(result, Clusters.GeneralCommissioning.Commands.ArmFailSafeResponse),
                             "Unexpected value returned from ArmFailSafe")
-        asserts.assert_equal(result.errorCode, 0, "Error code is not 0")
-        asserts.assert_equal(result.debugText, "", "Debug text is not empty")
 
         # TH reads Networks attribute from the DUT and saves the number of entries as NumNetworks
         self.step(2)
         networks = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.Networks)
         num_networks = len(networks)
-        userwifi_netidx = next((i for i, network in enumerate(networks) if network.networkID == ssid), None)
+        userwifi_netidx = next((i for i, network in enumerate(networks) if network.networkID == ssid.encode("utf-8")), None)
 
         # Verify that the Networks attribute list has an entry with: NetworkID field value as PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID
-        if userwifi_netidx == None:
-            asserts.fail("There is not a NetworkID field equal to PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID in Networks attribute list")
+        asserts.assert_true(userwifi_netidx is not None, "There is not a NetworkID field equal to PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID in Networks attribute list")
 
         # Verify that the Networks attribute list has an entry with: Connected field value is of type bool and has the value true
         asserts.assert_true(is_valid_bool_value(networks[userwifi_netidx].connected),
@@ -106,12 +103,7 @@ class TC_CNET_4_9(MatterBaseTest):
         asserts.assert_true(networks[userwifi_netidx].connected, "Network Connected attribute field is not a true")
 
         # TH finds the index of the Networks list entry with NetworkID field value PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID and saves it as Userwifi_netidx
-        self.step(3)  # Done in previous step
-
-        # userwifi_netidx = next((i for i, network in enumerate(networks) if network.networkID == ssid), None) # Calculated in previous step
-
-        # if userwifi_netidx is None: # Calculated in previous stepx
-        #     asserts.fail("Could not find user WiFi network id")
+        self.step(3)  # Done in previous step. The value is userwifi_netidx
 
         # TH sends RemoveNetwork Command to the DUT with NetworkID field set to PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID and Breadcrumb field set to 1
         self.step(4)
@@ -119,19 +111,19 @@ class TC_CNET_4_9(MatterBaseTest):
         result = await self.send_single_cmd(cmd=cmd)
 
         # Verify that DUT sends NetworkConfigResponse to command with the following fields: NetworkingStatus is success and NetworkIndex is 'Userwifi_netidx'
-        asserts.assert_true(type_matches(result, Clusters.NetworkCommissioning.Commands.RemoveNetworkResponse),
+        asserts.assert_true(type_matches(result, Clusters.NetworkCommissioning.Commands.NetworkConfigResponse),
                             "Unexpected value returned from RemoveNetwork")
-        asserts.assert_equal(result.networkStatus, Clusters.NetworkCommissioning.Enums.kSuccess,
+        asserts.assert_equal(result.networkingStatus, Clusters.NetworkCommissioning.Enums.NetworkCommissioningStatusEnum.kSuccess,
                              "Network status was not successful")
         asserts.assert_equal(result.networkIndex, userwifi_netidx, "NetworkIndex does not match user WiFi network index")
 
         # TH reads Networks attribute from the DUT
         self.step(5)
-        networks = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.Networks)
+        networks_after_removal = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.Networks)
 
         # Verify that the Networks attribute list has 'NumNetworks' - 1 entries
         expected_num_networks = num_networks - 1
-        asserts.assert_equal(len(networks), expected_num_networks,
+        asserts.assert_equal(len(networks_after_removal), expected_num_networks,
                              f"Networks attribute length is not equal as NumNetworks {expected_num_networks}")
 
         # TH reads LastNetworkingStatus attribute from the DUT
@@ -139,18 +131,17 @@ class TC_CNET_4_9(MatterBaseTest):
         last_networking_status = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.LastNetworkingStatus)
 
         # Verify that DUT sends LastNetworkingStatus as Success which is 0 or null if 'NumNetworks' - 1 == 0 entries.
-        if len(networks) - 1 == 0:
-            asserts.assert_equal(last_networking_status.networkStatus, NullValue, "Network Status was not Null")
+        if len(networks_after_removal) == 0:
+            asserts.assert_equal(last_networking_status, NullValue, "LastNetworkingStatus should be Null when Networks list is empty")
         else:
-            asserts.assert_equal(last_networking_status.networkStatus,
-                                 Clusters.NetworkCommissioning.Enum.kSuccess, "Network Status was not successful")
+            asserts.assert_equal(last_networking_status, Clusters.NetworkCommissioning.Enums.NetworkCommissioningStatusEnum.kSuccess, "LastNetworkingStatus should be Success when Networks list is not empty")
 
         # TH reads LastNetworkID attribute from the DUT
         self.step(7)
         last_network_id = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.LastNetworkID)
 
         # Verify that DUT sends LastNetworkID as PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID or null if 'NumNetworks' - 1 == 0 entries.
-        if len(networks) - 1 == 0:
+        if len(networks_after_removal) == 0:
             asserts.assert_equal(last_network_id, NullValue, "Network ID was not Null")
         else:
             asserts.assert_equal(last_network_id, ssid, f"Network ID is {last_network_id} and not {ssid}")
@@ -166,13 +157,11 @@ class TC_CNET_4_9(MatterBaseTest):
 
         # TH sends ConnectNetwork command to the DUT with NetworkID field set to PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID and Breadcrumb field set to 2
         self.step(9)
-        cmd = Clusters.NetworkCommissioning.Commands.ConnectNetwork(networkID=ssid, breadcrumb=2)
+        cmd = Clusters.NetworkCommissioning.Commands.ConnectNetwork(networkID=ssid.encode("utf-8"), breadcrumb=2)
         result = await self.send_single_cmd(cmd=cmd)
 
         # Verify that the DUT sends a ConnectNetworkResponse to the command with the NetworkingStatus field set to NetworkIdNotFound
-        asserts.assert_true(type_matches(result, Clusters.NetworkCommissioning.Commands.ConnectNetworkResponse),
-                            "Unexpected value returned from ConnectNetwork")
-        asserts.assert_equal(result.NetworkingStatus, Clusters.NetworkCommissioning.Enum.kNetworkIDNotFound,
+        asserts.assert_equal(result.networkingStatus, Clusters.NetworkCommissioning.Enums.NetworkCommissioningStatusEnum.kNetworkIDNotFound,
                              "Should have received network status not found")
 
         # TH reads Breadcrumb attribute from the General Commissioning cluster of the DUT
@@ -203,8 +192,7 @@ class TC_CNET_4_9(MatterBaseTest):
         asserts.assert_equal(len(networks), num_networks, f"Network length is not equal to NumNetworks: {num_networks}")
 
         for network in networks:
-            asserts.assert_equal(network.networkID, ssid.encode().hex(
-            ), f"Network ID: {network.networkID} is not the hex representation of PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID: {ssid}")
+            asserts.assert_equal(network.networkID, ssid.encode('utf-8'), f"Network ID: {network.networkID.decode('utf-8')} is not the hex representation of PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID: {ssid}")
             asserts.assert_true(is_valid_bool_value(network.connected), "Network Connected attribute field is not a boolean")
             asserts.assert_true(network.connected, "Network Connected attribute field is not a true")
 
@@ -219,23 +207,23 @@ class TC_CNET_4_9(MatterBaseTest):
 
         # TH sends RemoveNetwork Command to the DUT with NetworkID field set to PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID and Breadcrumb field set to 1
         self.step(14)
-        cmd = Clusters.NetworkCommissioning.Commands.RemoveNetwork(networkID=ssid, breadcrumb=1)
+        cmd = Clusters.NetworkCommissioning.Commands.RemoveNetwork(networkID=ssid.encode('utf-8'), breadcrumb=1)
         result = await self.send_single_cmd(cmd=cmd)
 
         # Verify that DUT sends NetworkConfigResponse to command with the following fields: NetworkingStatus is success and NetworkIndex is 'Userwifi_netidx'
-        asserts.assert_true(type_matches(result, Clusters.NetworkCommissioning.Commands.RemoveNetworkResponse),
+        asserts.assert_true(type_matches(result, Clusters.NetworkCommissioning.Commands.NetworkConfigResponse),
                             "Unexpected value returned from RemoveNetwork")
-        asserts.assert_equal(result.networkStatus, Clusters.NetworkCommissioning.Enums.kSuccess,
+        asserts.assert_equal(result.networkingStatus, Clusters.NetworkCommissioning.Enums.NetworkCommissioningStatusEnum.kSuccess,
                              "Network status was not successful")
         asserts.assert_equal(result.networkIndex, userwifi_netidx, "NetworkIndex does not match user WiFi network index")
 
         # TH sends the CommissioningComplete command to the DUT
         self.step(15)
-        cmd = Clusters.GeneralCommissioning.Commands.CommissioningComplete
+        cmd = Clusters.GeneralCommissioning.Commands.CommissioningComplete()
         result = await self.send_single_cmd(cmd=cmd)
 
         # Verify that DUT sends CommissioningCompleteResponse with the ErrorCode field set to OK (0)
-        asserts.assert_equal(result.errorCode, Clusters.GeneralCommissioning.Enums.kOk, "CommissioningCompleteResponse was not OK")
+        asserts.assert_equal(result.errorCode, Clusters.GeneralCommissioning.Enums.CommissioningErrorEnum.kOk, "CommissioningCompleteResponse was not OK")
 
         # TH sends ArmFailSafe command to the DUT with ExpiryLengthSeconds set to 0 to ensure the CommissioningComplete call properly persisted the failsafe context. This call should have no effect if Commissioning Complete call is handled correctly
         self.step(16)
@@ -253,11 +241,9 @@ class TC_CNET_4_9(MatterBaseTest):
         # Verify that the Networks attribute list has 'NumNetworks' - 1 entries and does NOT contain an entry with the NetworkID for PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID
         asserts.assert_equal(len(networks), num_networks - 1, f"Networks length: {len(networks)} should be {num_networks - 1}")
 
-        userwifi_netidx = next((i for i, network in enumerate(networks) if network.networkID == ssid), None)
+        userwifi_netidx = next((i for i, network in enumerate(networks) if network.networkID == ssid.encode("utf-8")), None)
 
-        asserts.assert_equal(
-            userwifi_netidx, None, f"Networks should not contain an entry with the NetworkID for PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID: {ssid}")
-
+        asserts.assert_true(userwifi_netidx is None, f"Networks should not contain an entry with the NetworkID for PIXIT.CNET.WIFI_1ST_ACCESSPOINT_SSID: {ssid}")
 
 if __name__ == "__main__":
     default_matter_test_main()
