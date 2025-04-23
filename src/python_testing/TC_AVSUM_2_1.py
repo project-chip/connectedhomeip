4132d93e8ed4542cabe2a3454cef97b57e3a8377
Restyled.io <commits@restyled.io>
2025-04-23 02:49:51 +0000

    Restyled by isort

--- src/python_testing/TC_AVSUM_2_1.py
+++ src/python_testing/TC_AVSUM_2_1.py
@@ -38,8 +38,8 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body, default_matter_test_main, run_if_endpoint_matches,
-                                         has_cluster)
+from chip.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body, default_matter_test_main, has_cluster,
+                                         run_if_endpoint_matches)
 from mobly import asserts
 from TC_AVSUMTestBase import AVSUMTestBase
 
