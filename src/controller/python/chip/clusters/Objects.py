37948e2e0823534d9f843a0a842a42708cf234cd
Kiel Oleson <kielo@apple.com>
2025-04-24 09:54:14 -0700

    reconcile Common Area / Location namespaces to current spec (#38288)
    * reconcile Common Area namespace to updated spec
    
    remove duplicates with different names; add an item
    
    * Add LocationTag to namespace enums XML
    
    from 1.4 spec
    
    * remove defunct FloorSurfaceTag
    
    no longer in spec
    
    * codegen: zap for sample apps
    
    * zzz / python codegen
    
    * fix recently-added oven-cooktop

--- src/controller/python/chip/clusters/Objects.py
+++ src/controller/python/chip/clusters/Objects.py
@@ -222,7 +222,6 @@ class Globals:
             kGardenDoor = 0x25
             kGuestBathroom = 0x26
             kGuestBedroom = 0x27
-            kGuestRestroom = 0x28
             kGuestRoom = 0x29
             kGym = 0x2A
             kHallway = 0x2B
@@ -230,7 +229,6 @@ class Globals:
             kKidsRoom = 0x2D
             kKidsBedroom = 0x2E
             kKitchen = 0x2F
-            kLarder = 0x30
             kLaundryRoom = 0x31
             kLawn = 0x32
             kLibrary = 0x33
@@ -255,7 +253,6 @@ class Globals:
             kRamp = 0x46
             kReceptionRoom = 0x47
             kRecreationRoom = 0x48
-            kRestroom = 0x49
             kRoof = 0x4A
             kSauna = 0x4B
             kScullery = 0x4C
@@ -277,11 +274,12 @@ class Globals:
             kUtilityRoom = 0x5C
             kWard = 0x5D
             kWorkshop = 0x5E
+            kToilet = 0x5F
             # All received enum values that are not listed above will be mapped
             # to kUnknownEnumValue. This is a helper enum value that should only
             # be used by code to process how it handles receiving an unknown
             # enum value. This specific value should never be transmitted.
-            kUnknownEnumValue = 95
+            kUnknownEnumValue = 40
 
         class AtomicRequestTypeEnum(MatterIntEnum):
             kBeginWrite = 0x00
@@ -292,37 +290,6 @@ class Globals:
             # be used by code to process how it handles receiving an unknown
             # enum value. This specific value should never be transmitted.
             kUnknownEnumValue = 3
-
-        class FloorSurfaceTag(MatterIntEnum):
-            kCarpet = 0x00
-            kCeramic = 0x01
-            kConcrete = 0x02
-            kCork = 0x03
-            kDeepCarpet = 0x04
-            kDirt = 0x05
-            kEngineeredWood = 0x06
-            kGlass = 0x07
-            kGrass = 0x08
-            kHardwood = 0x09
-            kLaminate = 0x0A
-            kLinoleum = 0x0B
-            kMat = 0x0C
-            kMetal = 0x0D
-            kPlastic = 0x0E
-            kPolishedConcrete = 0x0F
-            kRubber = 0x10
-            kRug = 0x11
-            kSand = 0x12
-            kStone = 0x13
-            kTatami = 0x14
-            kTerrazzo = 0x15
-            kTile = 0x16
-            kVinyl = 0x17
-            # All received enum values that are not listed above will be mapped
-            # to kUnknownEnumValue. This is a helper enum value that should only
-            # be used by code to process how it handles receiving an unknown
-            # enum value. This specific value should never be transmitted.
-            kUnknownEnumValue = 24
 
         class LandmarkTag(MatterIntEnum):
             kAirConditioner = 0x00
@@ -381,6 +348,17 @@ class Globals:
             # be used by code to process how it handles receiving an unknown
             # enum value. This specific value should never be transmitted.
             kUnknownEnumValue = 51
+
+        class LocationTag(MatterIntEnum):
+            kIndoor = 0x00
+            kOutdoor = 0x01
+            kInside = 0x02
+            kOutside = 0x03
+            # All received enum values that are not listed above will be mapped
+            # to kUnknownEnumValue. This is a helper enum value that should only
+            # be used by code to process how it handles receiving an unknown
+            # enum value. This specific value should never be transmitted.
+            kUnknownEnumValue = 4
 
         class PositionTag(MatterIntEnum):
             kLeft = 0x00
