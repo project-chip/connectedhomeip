4ab11bb83708a4a13fa6adf05da667e29038d6fc
C Freeman <cecille@google.com>
2025-04-24 15:06:34 -0400

    Update src/python_testing/TestSpecParsingDeviceType.py
    Co-authored-by: Andrei Litvin <andy314@gmail.com>

--- src/python_testing/TestSpecParsingDeviceType.py
+++ src/python_testing/TestSpecParsingDeviceType.py
@@ -47,7 +47,7 @@ class TestSpecParsingDeviceType(MatterBaseTest):
         self.clusters = {0x0003: "Identify", 0x0004: "Groups"}
 
         # Let's just build a dictionary of device types to IDs becasue I need them and we don't have codegen
-        self.dt_ids = {dt.name.replace(' ', '').replace('/', '').replace('-', '').lower()
+        self.dt_ids = {re.replace('[ -/]*', '', dt.name.lower())
                                        : id for id, dt in self.xml_device_types.items()}
 
         # Conformance support tests the different types of conformance for clusters, so here we just want to ensure that we're correctly parsing the XML into python
