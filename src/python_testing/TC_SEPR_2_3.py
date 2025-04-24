363da69673c3436816520a7592540a059411cf7f
Restyled.io <commits@restyled.io>
2025-04-24 23:26:12 +0000

    Restyled by isort

--- src/python_testing/TC_SEPR_2_3.py
+++ src/python_testing/TC_SEPR_2_3.py
@@ -49,7 +49,6 @@ import chip.clusters as Clusters
 from chip import ChipDeviceCtrl
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
-
 from TC_SEPRTestBase import CommodityPriceTestBaseHelper
 
 logger = logging.getLogger(__name__)
