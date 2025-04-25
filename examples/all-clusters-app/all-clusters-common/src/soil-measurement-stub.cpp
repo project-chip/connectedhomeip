28ad016ff38bbf01bef48edbccf1fe04e3a36b4f
René Josefsen <rjosefsen@grundfos.com>
2025-04-25 18:34:46 +0200

    Added all-clusters stub and reworked server impl

---
+++ examples/all-clusters-app/all-clusters-common/src/soil-measurement-stub.cpp
@@ -0,0 +1,47 @@
+/*
+ *
+ *    Copyright (c) 2023 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <app/clusters/soil-measurement-server/soil-measurement-server.h>
+
+using namespace chip;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::SoilMeasurement;
+
+static std::unique_ptr<SoilMeasurementAttrAccess> gAttrAccess;
+
+void emberAfSoilMeasurementClusterInitCallback(chip::EndpointId endpointId)
+{
+    VerifyOrDie(endpointId == 1); // this cluster is only enabled for endpoint 1.
+    VerifyOrDie(!gAttrAccess);
+
+    gAttrAccess = std::make_unique<SoilMeasurementAttrAccess>();
+
+    if (gAttrAccess)
+    {
+        gAttrAccess->Init();
+    }
+}
+
+void emberAfSoilMeasurementClusterShutdownCallback(chip::EndpointId endpointId)
+{
+    if (gAttrAccess)
+    {
+        gAttrAccess->Shutdown();
+    }
+    gAttrAccess = nullptr;
+}
--- src/app/clusters/soil-measurement-server/soil-measurement-server.cpp
+++ src/app/clusters/soil-measurement-server/soil-measurement-server.cpp
@@ -21,40 +21,69 @@
 #include <app-common/zap-generated/ids/Attributes.h>
 #include <app-common/zap-generated/ids/Clusters.h>
 #include <app/AttributeAccessInterfaceRegistry.h>
+#include <app/util/attribute-storage.h>
 #include <lib/support/CodeUtils.h>
 #include <lib/support/logging/CHIPLogging.h>
+#include <zap-generated/gen_config.h>
+
+namespace chip {
+namespace app {
+namespace Clusters {
+namespace SoilMeasurement {
 
 using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
 using namespace chip::app::Clusters::SoilMeasurement;
 using namespace chip::app::Clusters::SoilMeasurement::Attributes;
+using namespace chip::app::Clusters::SoilMeasurement::Structs;
 
-SoilMeasurementServer SoilMeasurementServer::mInstance;
+MeasurementData gMeasurements[MATTER_DM_SOIL_MEASUREMENT_CLUSTER_SERVER_ENDPOINT_COUNT + CHIP_DEVICE_CONFIG_DYNAMIC_ENDPOINT_COUNT];
 
-SoilMeasurementServer & SoilMeasurementServer::Instance()
+MeasurementAccuracyRangeStruct::Type soilMoistureMeasurementLimitsAccuracyRange[] = {
+    { .rangeMin = 0, .rangeMax = 100, .percentMax = MakeOptional(static_cast<chip::Percent100ths>(10)) }
+};
+
+CHIP_ERROR SoilMeasurementAttrAccess::Init()
 {
-    return SoilMeasurementServer::mInstance;
+    VerifyOrReturnError(AttributeAccessInterfaceRegistry::Instance().Register(this), CHIP_ERROR_INCORRECT_STATE);
+
+    // Initialize the soil moisture measurement limits to default values
+    for (auto & measurement : gMeasurements)
+    {
+        measurement.soilMoistureMeasuredValue.SetNull();
+        measurement.soilMoistureMeasurementLimits = { .measurementType  = MeasurementTypeEnum::kSoilMoisture,
+                                                      .measured         = true,
+                                                      .minMeasuredValue = 0,
+                                                      .maxMeasuredValue = 100,
+                                                      .accuracyRanges = DataModel::List<const MeasurementAccuracyRangeStruct::Type>(
+                                                          soilMoistureMeasurementLimitsAccuracyRange) };
+    }
+
+    return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR SoilMeasurementServer::ReadClusterRevision(AttributeValueEncoder & aEncoder)
+void SoilMeasurementAttrAccess::Shutdown()
 {
-    return aEncoder.Encode(kClusterRevision);
+    AttributeAccessInterfaceRegistry::Instance().Unregister(this);
 }
 
-CHIP_ERROR SoilMeasurementServer::Read(const ConcreteReadAttributePath & aPath, AttributeValueEncoder & aEncoder)
+CHIP_ERROR SoilMeasurementAttrAccess::Read(const ConcreteReadAttributePath & aPath, AttributeValueEncoder & aEncoder)
 {
     VerifyOrDie(aPath.mClusterId == SoilMeasurement::Id);
 
+    MeasurementData * data = SoilMeasurementDataForEndpoint(aPath.mEndpointId);
+
     switch (aPath.mAttributeId)
     {
+    case Attributes::SoilMoistureMeasurementLimits::Id: {
+        return aEncoder.Encode(data->soilMoistureMeasurementLimits);
+    }
     case Attributes::SoilMoistureMeasuredValue::Id: {
-        DataModel::Nullable<uint16_t> soilMoistureMeasuredValue;
-        soilMoistureMeasuredValue.SetNonNull(1);
-        return aEncoder.Encode(soilMoistureMeasuredValue);
+        return aEncoder.Encode(data->soilMoistureMeasuredValue);
     }
     case ClusterRevision::Id: {
-        return ReadClusterRevision(aEncoder);
+        return aEncoder.Encode(kClusterRevision);
     }
     default: {
         break;
@@ -63,12 +92,37 @@ CHIP_ERROR SoilMeasurementServer::Read(const ConcreteReadAttributePath & aPath, 
     return CHIP_NO_ERROR;
 }
 
-void MatterSoilMeasurementPluginServerInitCallback()
+MeasurementData * SoilMeasurementDataForEndpoint(EndpointId endpointId)
 {
-    AttributeAccessInterfaceRegistry::Instance().Register(&SoilMeasurementServer::Instance());
+    auto index = emberAfGetClusterServerEndpointIndex(endpointId, app::Clusters::SoilMeasurement::Id,
+                                                      MATTER_DM_SOIL_MEASUREMENT_CLUSTER_SERVER_ENDPOINT_COUNT);
+
+    if (index == kEmberInvalidEndpointIndex)
+    {
+        return nullptr;
+    }
+
+    if (index >= MATTER_ARRAY_SIZE(gMeasurements))
+    {
+        ChipLogError(NotSpecified, "Internal error: invalid/unexpected energy measurement index.");
+        return nullptr;
+    }
+    return &gMeasurements[index];
 }
 
-void MatterSoilMeasurementPluginServerShutdownCallback()
+CHIP_ERROR SetSoilMeasurementAccuracy(EndpointId endpointId, const MeasurementAccuracyStruct::Type & measurementLimits)
 {
-    AttributeAccessInterfaceRegistry::Instance().Unregister(&SoilMeasurementServer::Instance());
+    MeasurementData * data = SoilMeasurementDataForEndpoint(endpointId);
+    VerifyOrReturnError(data != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
+
+    data->soilMoistureMeasurementLimits = measurementLimits;
+
+    // MatterReportingAttributeChangeCallback(endpointId, SoilMeasurement::Id, soilMoistureMeasurementLimits::Id);
+
+    return CHIP_NO_ERROR;
 }
+
+} // namespace SoilMeasurement
+} // namespace Clusters
+} // namespace app
+} // namespace chip
