a461c44f00c09f292985d31c0ad8800ae83f92ca
Raveendra Karu <r.karu@samsung.com>
2025-04-26 04:05:41 +0530

    Address review comments.
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- src/controller/python/chip/webrtc/callbacks.py
+++ src/controller/python/chip/webrtc/callbacks.py
@@ -15,29 +15,28 @@ candidates = []
 
 
 def on_ice(candidates, peer):
-    print("on_ice called")
+    logging.info(f"on_ice received ICE candidates: {candidates}")
     if future_ice is not None:
         future_ice.set_result(candidates)
 
 
 def on_offer(offer, peer):
-    print("on_offer called")
+    logging.info(f"on_offer received SDP offer: {offer}")
     offer_string = offer.decode('utf-8')
-    print(offer_string)
     if future_offer is not None:
         future_offer.set_result(offer_string)
 
 
 def on_answer(answer, peer):
-    print("on_answer called")
+    logging.info(f"on_answer received SDP answer: {answer}")
 
 
 def on_error(error, peer):
-    print("on_error called")
+    logging.info(f"on_error received error: {error}")
 
 
 def on_stats(stats, peer):
-    print(stats)
+    logging.info(f"on_stats received stats: {stats}")
 
 
 offer_callback = SdpOfferCallback_t(on_offer)
