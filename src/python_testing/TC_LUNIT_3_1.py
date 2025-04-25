3423e98f5753a7f9a7cb5b7ec444069b465d2c11
Clemens Knörzer <clemens@evehome.com>
2025-04-25 19:37:33 +0200

    Restyler fixes

--- src/python_testing/TC_LUNIT_3_1.py
+++ src/python_testing/TC_LUNIT_3_1.py
@@ -57,7 +57,8 @@ class TC_LUNIT_3_1(MatterBaseTest):
         cluster = Clusters.Objects.UnitLocalization
         result = await dev_ctrl.WriteAttribute(self.dut_node_id, [(endpoint, cluster.Attributes.TemperatureUnit(temp_unit))])
         status = result[0].Status
-        asserts.assert_equal(status, expected_status, f"TemperatureUnit write returned {status.name}; expected {expected_status.name}")
+        asserts.assert_equal(status, expected_status,
+                             f"TemperatureUnit write returned {status.name}; expected {expected_status.name}")
         return status
 
     def desc_TC_LUNIT_3_1(self) -> str:
@@ -88,28 +89,30 @@ class TC_LUNIT_3_1(MatterBaseTest):
         features = await self.read_lunit_attribute_expect_success(endpoint=endpoint, attribute=attributes.FeatureMap)
         self.supports_TEMP = bool(features & Clusters.UnitLocalization.Bitmaps.Feature.kTemperatureUnit)
 
-
         # Step 0 - Commissioning, already done", is_commissioning=True),
 
         self.step(0)  # commissioning - already done
-    
+
         # Step 1 - TH reads from the DUT the TemperatureUnit attribute"),
         self.step(1)
         if self.supports_TEMP:
             temperature_unit = await self.read_lunit_attribute_expect_success(endpoint=endpoint, attribute=attributes.TemperatureUnit)
-        
-            asserts.assert_greater_equal(temperature_unit, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kFahrenheit, "TemperatureUnit has to be Fahrenheit, Celsius or Kelvin")
-            asserts.assert_less_equal(temperature_unit, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kKelvin, "TemperatureUnit has to be Fahrenheit, Celsius or Kelvin")
+
+            asserts.assert_greater_equal(temperature_unit, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kFahrenheit,
+                                         "TemperatureUnit has to be Fahrenheit, Celsius or Kelvin")
+            asserts.assert_less_equal(temperature_unit, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kKelvin,
+                                      "TemperatureUnit has to be Fahrenheit, Celsius or Kelvin")
         else:
             logging.info("Test step skipped")
-
 
         # Step 2 - TH reads from the DUT the SupportedTemperatureUnits attribute"),
         self.step(2)
         if self.supports_TEMP:
             supported_temperature_units = await self.read_lunit_attribute_expect_success(endpoint=endpoint, attribute=attributes.SupportedTemperatureUnits)
-            asserts.assert_greater_equal(len(supported_temperature_units), 2, "SupportedTemperatureUnits must have at least two entry in the list")
-            asserts.assert_less_equal(len(supported_temperature_units), 3, "SupportedTemperatureUnits may have a maximum of three entries in the list")
+            asserts.assert_greater_equal(len(supported_temperature_units), 2,
+                                         "SupportedTemperatureUnits must have at least two entry in the list")
+            asserts.assert_less_equal(len(supported_temperature_units), 3,
+                                      "SupportedTemperatureUnits may have a maximum of three entries in the list")
         else:
             logging.info("Test step skipped")
 
@@ -121,20 +124,19 @@ class TC_LUNIT_3_1(MatterBaseTest):
         else:
             logging.info("Test step skipped")
 
-
         # Step 4 - Construct a list with valid TempUnitEnum values that are not contained in
 
         self.step(4)
         unsupported_temperature_units = []
         if self.supports_TEMP:
-            for unit in [Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kFahrenheit, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kCelsius, Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kKelvin]:
+            for unit in [Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kFahrenheit,
+                         Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kCelsius,
+                         Clusters.Objects.UnitLocalization.Enums.TempUnitEnum.kKelvin]:
                 if unit not in supported_temperature_units:
                     unsupported_temperature_units.append(unit)
         else:
             logging.info("Test step skipped")
 
-
-        
         # Step 5 - With each entry in UnsupportedUnitsList, TH writes to the DUT the TemperatureUnit attribute"),
         self.step(5)
         if self.supports_TEMP:
