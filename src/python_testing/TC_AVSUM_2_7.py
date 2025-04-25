deade43925877768377eded068c1cd810f5f11b8
Restyled.io <commits@restyled.io>
2025-04-25 19:57:58 +0000

    Restyled by isort

--- src/python_testing/TC_AVSUM_2_7.py
+++ src/python_testing/TC_AVSUM_2_7.py
@@ -37,7 +37,8 @@
 
 import chip.clusters as Clusters
 from chip.interaction_model import Status
-from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_cluster, has_feature, run_if_endpoint_matches
+from chip.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_cluster, has_feature,
+                                         run_if_endpoint_matches)
 from TC_AVSUMTestBase import AVSUMTestBase
 
 
