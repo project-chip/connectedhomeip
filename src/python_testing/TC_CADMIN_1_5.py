9e8c63addbae43cc0e6c2f5784d51316bde04304
Restyled.io <commits@restyled.io>
2025-04-25 01:13:01 +0000

    Restyled by autopep8

--- src/python_testing/TC_CADMIN_1_5.py
+++ src/python_testing/TC_CADMIN_1_5.py
@@ -62,7 +62,7 @@ class TC_CADMIN_1_5(MatterBaseTest):
                 except (ValueError, TypeError):
                     logging.warning(f"Could not convert CM value '{cm_value}' to integer")
                     self.cm = None
-            
+
             # Safely convert D value to int if present
             d_value = self.service.txt_record.get('D')
             if d_value is not None:
