2857ceabb027c362086e6c8e4d5ed9b1b785933e
sabollim <satyanaag.bollimpalli@silabs.com>
2025-04-26 01:08:10 +0530

    Closure app changes

--- examples/closure-app/closure-common/src/ClosureDimensionEndpoint.cpp
+++ examples/closure-app/closure-common/src/ClosureDimensionEndpoint.cpp
@@ -17,45 +17,155 @@
  */
 
 #include <ClosureDimensionEndpoint.h>
-#include <app-common/zap-generated/cluster-enums.h>
 #include <app-common/zap-generated/cluster-objects.h>
+#include <app-common/zap-generated/cluster-enums.h>
 #include <protocols/interaction_model/StatusCode.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-cluster-logic.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-delegate.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-matter-context.h>
+#include <app/clusters/closure-dimension-server/closure-dimension-server.h>
+#include <platform/CHIPDeviceLayer.h>
+#include <cmsis_os2.h>
+
+namespace {
+    constexpr chip::Percent100ths kLimitRangeMin = 0;
+    constexpr chip::Percent100ths kLimitRangaMax = 10000;
+    constexpr chip::Percent100ths kStep            = 1000;
+    const uint32_t kExampleMotionCountDown = 5;
+    const uint32_t kExampleStepCountDown   = 3000;
+} // namespace
 
 using namespace chip;
 using namespace chip::app::Clusters::ClosureDimension;
-
 using Protocols::InteractionModel::Status;
-
-Status PrintOnlyDelegate::HandleSetTarget(const Optional<Percent100ths> & pos, const Optional<bool> & latch,
-                                          const Optional<Globals::ThreeLevelAutoEnum> & speed)
+ 
+CHIP_ERROR ClosureDimensionDelegate::Init()
 {
-    ChipLogProgress(AppServer, "HandleSetTarget");
-    // Add the SetTarget handling logic here
-    return Status::Success;
+    GenericCurrentStateStruct currentState{ Optional<Percent100ths>(0), Optional<bool>(false),
+        Optional<Globals::ThreeLevelAutoEnum>(Globals::ThreeLevelAutoEnum::kAuto) };
+    ReturnErrorOnFailure(mLogic->SetCurrentState(chip::app::DataModel::MakeNullable(currentState)));
+    
+    GenericTargetStruct targetState{ Optional<Percent100ths>(0), Optional<bool>(false),
+        Optional<Globals::ThreeLevelAutoEnum>(Globals::ThreeLevelAutoEnum::kAuto) };
+    ReturnErrorOnFailure(mLogic->SetTarget(chip::app::DataModel::MakeNullable(targetState)));
+
+    Structs::RangePercent100thsStruct::Type limitRange;
+    limitRange.min = kLimitRangeMin;
+    limitRange.max = kLimitRangaMax;
+    ReturnErrorOnFailure(mLogic->SetLimitRange(limitRange));
+
+    ReturnErrorOnFailure(mLogic->SetStepValue(kStep));
+    
+    return CHIP_NO_ERROR;
 }
 
-Status PrintOnlyDelegate::HandleStep(const StepDirectionEnum & direction, const uint16_t & numberOfSteps,
-                                     const Optional<Globals::ThreeLevelAutoEnum> & speed)
+static void MotionTimerEventHandler(System::Layer * systemLayer, void * data)
 {
-    ChipLogProgress(AppServer, "HandleStep");
-    // Add the Step handling logic here
-    return Status::Success;
+    ClosureDimensionDelegate * delegate = reinterpret_cast<ClosureDimensionDelegate *>(data);
+    chip::app::DataModel::Nullable<GenericTargetStruct> target;
+    chip::app::DataModel::Nullable<GenericCurrentStateStruct> current;
+
+    delegate->GetLogic()->GetTarget(target);
+    current.Value().position.SetValue(static_cast<uint16_t>(target.Value().position.Value()));
+    if (target.Value().latch.HasValue())
+    {
+        current.Value().latch.SetValue(target.Value().latch.Value());
+    }
+
+    if (target.Value().speed.HasValue())
+    {
+        current.Value().speed.SetValue(target.Value().speed.Value());
+    }
+    
+    delegate->GetLogic()->SetCurrentState(current);
 }
 
+
+Status ClosureDimensionDelegate::HandleSetTarget(const Optional<Percent100ths> & pos, const Optional<bool> & latch,
+    const Optional<Globals::ThreeLevelAutoEnum> & speed)
+{
+    (void) DeviceLayer::SystemLayer().CancelTimer(MotionTimerEventHandler, this);
+    
+    (void) DeviceLayer::SystemLayer().StartTimer(System::Clock::Seconds16(kExampleMotionCountDown), MotionTimerEventHandler, this);
+    
+    // Trigger Motion Action
+
+    return Status::Success;;
+}
+
+static void HandleStepMotion(System::Layer * systemLayer, void * data)
+{
+    ClosureDimensionDelegate * delegate = reinterpret_cast<ClosureDimensionDelegate *>(data);
+    VerifyOrReturn(delegate != nullptr, void());
+
+    ClusterState state = delegate->GetLogic()->GetState();
+
+    StepDirectionEnum direction = delegate->GetTargetDirection();
+
+    uint32_t newPos;
+    if (direction == StepDirectionEnum::kDecrease)
+    {
+        newPos = std::max((state.target.Value().position.Value() + 0), (state.currentState.Value().position.Value() - state.stepValue));
+    }
+    else
+    {
+        newPos = std::min((state.target.Value().position.Value() + 0), (state.currentState.Value().position.Value() + state.stepValue));
+    }
+    state.currentState.Value().position.SetValue(newPos);
+    delegate->GetLogic()->SetCurrentState(state.currentState);
+    if (state.target.Value().position.Value() == state.currentState.Value().position.Value())
+    {
+        (void) DeviceLayer::SystemLayer().CancelTimer(HandleStepMotion, delegate);
+    }
+    else
+    {
+        (void) DeviceLayer::SystemLayer().StartTimer(System::Clock::Milliseconds16(kExampleStepCountDown),
+                                                     HandleStepMotion, delegate);
+        // Trigger Step Action
+    }
+}
+
+Status ClosureDimensionDelegate::HandleStep(const StepDirectionEnum & direction, const uint16_t & numberOfSteps,
+    const Optional<Globals::ThreeLevelAutoEnum> & speed)
+{
+    mTargetDirection = direction;
+    (void) DeviceLayer::SystemLayer().StartTimer(System::Clock::Milliseconds16(kExampleStepCountDown), HandleStepMotion, this);
+    
+    //Trigger Step Action
+    return Status::Success;
+    
+}
+
+bool ClosureDimensionDelegate::IsManualLatchingNeeded()
+{
+     // Check if closure needs manual latching.(manufacture specific)
+    return false;
+}
+ 
 CHIP_ERROR ClosureDimensionEndpoint::Init()
 {
+    ChipLogProgress(AppServer, "ClosureDimensionEndpoint::Init start");
+    osDelay(1000);
+    
     ClusterConformance conformance;
-    conformance.FeatureMap()
-        .Set(Feature::kPositioning)
-        .Set(Feature::kMotionLatching)
-        .Set(Feature::kUnit)
-        .Set(Feature::kLimitation)
-        .Set(Feature::kSpeed);
+    conformance.FeatureMap().Set(Feature::kPositioning)
+                            .Set(Feature::kMotionLatching)
+                            .Set(Feature::kUnit)
+                            .Set(Feature::kLimitation)
+                            .Set(Feature::kSpeed);
     conformance.OptionalAttributes().Set(OptionalAttributeEnum::kOverflow);
-
+    
     ClusterInitParameters clusterInitParameters;
-
+    
     ReturnErrorOnFailure(mLogic.Init(conformance, clusterInitParameters));
+    ChipLogProgress(AppServer, "mLogicInit done");
+    osDelay(1000);
     ReturnErrorOnFailure(mInterface.Init());
+    ChipLogProgress(AppServer, "interfaceInit done");
+    osDelay(1000);
+    ReturnErrorOnFailure(mDelegate.Init());
+      
+    ChipLogProgress(AppServer, "ClosureDimensionEndpoint::Init end");
+    osDelay(1000);
     return CHIP_NO_ERROR;
 }
--- examples/closure-app/silabs/src/DataModelCallbacks.cpp
+++ examples/closure-app/silabs/src/DataModelCallbacks.cpp
@@ -61,3 +61,9 @@ void MatterClosureDimensionClusterServerAttributeChangedCallback(const app::Conc
     ChipLogProgress(Zcl, "Closure cluster ID: " ChipLogFormatMEI, ChipLogValueMEI(attributePath.mAttributeId));
     // Attribute changes handling will be done in the next phase.
 }
+
+void MatterClosureDimensionClusterServerAttributeChangedCallback(const app::ConcreteAttributePath & attributePath)
+{
+    ChipLogProgress(Zcl, "Closure cluster ID: " ChipLogFormatMEI, ChipLogValueMEI(attributePath.mAttributeId));
+    // Attribute changes handling will done in next phase.
+}
