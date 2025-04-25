f8a11d06cfde052b2e0a63a50dd8f5c7d4e7a42c
Clemens Knörzer <clemens@evehome.com>
2025-04-25 18:54:30 +0200

    Make Restyler happy

--- src/python_testing/TC_LUNIT_3_1.py
+++ src/python_testing/TC_LUNIT_3_1.py
@@ -36,7 +36,8 @@ import logging
 
 import chip.clusters as Clusters
 from chip import ChipDeviceCtrl
-from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_attribute, has_cluster, run_if_endpoint_matches
+from chip.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_attribute, has_cluster,
+                                         run_if_endpoint_matches)
 from mobly import asserts
 from chip.interaction_model import Status
 
