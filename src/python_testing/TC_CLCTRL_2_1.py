fee0e77bdee3099873539d22dfa67662a08dcdc0
Fabian Wechselberger <fabian@innovation-matters.at>
2025-04-28 13:11:02 +0000

    Types import fix.

--- src/python_testing/TC_CLCTRL_2_1.py
+++ src/python_testing/TC_CLCTRL_2_1.py
@@ -34,7 +34,7 @@
 import logging
 
 import chip.clusters as Clusters
-import chip.Types as Types
+from chip.clusters.Types import NullValue
 
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
@@ -84,7 +84,7 @@ class TC_CLCTRL_2_1(MatterBaseTest):
         if attributes.CountdownTime.attribute_id in attribute_list:
             countdown_time = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.CountdownTime)
             logging.info(f"CountdownTime: {countdown_time}")
-            if countdown_time is not Types.NullValue:
+            if countdown_time is not NullValue:
                 asserts.assert_less_equal(countdown_time, 259200, "CountdownTime attribute is out of range")
                 asserts.assert_greater_equal(countdown_time, 0, "CountdownTime attribute is out of range")
         else:
@@ -120,20 +120,20 @@ class TC_CLCTRL_2_1(MatterBaseTest):
         is_positioning_supported = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kPositioning
         is_speed_supported = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kSpeed
 
-        if overall_state is not Types.NullValue:
+        if overall_state is not NullValue:
             # Check Positioning feature in OverallState - PS feature (bit 0)
-            if is_positioning_supported and overall_state.positioning is not Types.NullValue:
+            if is_positioning_supported and overall_state.positioning is not NullValue:
                 asserts.assert_less_equal(overall_state.positioning, Clusters.ClosureControl.Enums.PositioningEnum.kOpenedAtSignature, "OverallState.positioning is out of range")
                 asserts.assert_greater_equal(overall_state.positioning, Clusters.ClosureControl.Enums.PositioningEnum.kFullyClosed, "OverallState.positioning is out of range")
                 logging.info(f"OverallState.positioning: {overall_state.positioning}")
         
             # Check MotionLatching feature in OverallState - LT feature (bit 1)
-            if is_latching_supported and overall_state.latch is not Types.NullValue:
+            if is_latching_supported and overall_state.latch is not NullValue:
                 asserts.assert_true(isinstance(overall_state.latch, bool), "OverallState.latch is not a boolean value")
                 logging.info(f"OverallState.latch: {overall_state.latch}")
         
             # Check Speed feature in OverallState - SP feature (bit 3)
-            if is_speed_supported and overall_state.speed is not Types.NullValue:
+            if is_speed_supported and overall_state.speed is not NullValue:
                 asserts.assert_less_equal(overall_state.speed, Clusters.Globals.ThreeLevelAutoEnum.kHigh, "OverallState.speed is out of range")
                 asserts.assert_greater_equal(overall_state.speed, Clusters.Globals.ThreeLevelAutoEnum.kAuto, "OverallState.speed is out of range")
                 logging.info(f"OverallState.speed: {overall_state.speed}")
@@ -149,7 +149,7 @@ class TC_CLCTRL_2_1(MatterBaseTest):
         overall_target = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallTarget)
         logging.info(f"OverallTarget: {overall_target}")
         
-        if overall_target is not Types.NullValue:
+        if overall_target is not NullValue:
             # Check Positioning feature in OverallTarget
             if is_positioning_supported:  # PS feature (bit 0)
                 asserts.assert_less_equal(overall_target.position, Clusters.ClosureControl.Enums.TargetPositionEnum.kSignature, "OverallTarget.position is out of range")
