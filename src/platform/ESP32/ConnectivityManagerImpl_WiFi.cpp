1534dd679f5d8be08eee7ca46d994409ab99bd82
Restyled.io <commits@restyled.io>
2025-04-29 06:49:13 +0000

    Restyled by clang-format

--- src/platform/ESP32/ConnectivityManagerImpl_WiFi.cpp
+++ src/platform/ESP32/ConnectivityManagerImpl_WiFi.cpp
@@ -425,7 +425,7 @@ CHIP_ERROR ConnectivityManagerImpl::InitWiFi()
             wifiConfig.sta.scan_method = WIFI_ALL_CHANNEL_SCAN;
             wifiConfig.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
 #endif // CONFIG_WIFI_SCAN_ALL_CHANNEL_MODE
-            esp_err_t err              = esp_wifi_set_config(WIFI_IF_STA, &wifiConfig);
+            esp_err_t err = esp_wifi_set_config(WIFI_IF_STA, &wifiConfig);
             if (err != ESP_OK)
             {
                 ChipLogError(DeviceLayer, "esp_wifi_set_config() failed: %s", esp_err_to_name(err));
