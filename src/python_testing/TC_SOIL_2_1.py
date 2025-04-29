5355308d3e6c59d6be55e8bf41df16570da0c0bd
René Josefsen <69624991+ReneJosefsen@users.noreply.github.com>
2025-04-29 15:47:22 +0200

    Apply suggestions from code review

--- src/python_testing/TC_SOIL_2_1.py
+++ src/python_testing/TC_SOIL_2_1.py
@@ -25,6 +25,7 @@
 #       --commissioning-method on-network
 #       --discriminator 1234
 #       --passcode 20202021
+#       --endpoint 1
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #     factory-reset: true
