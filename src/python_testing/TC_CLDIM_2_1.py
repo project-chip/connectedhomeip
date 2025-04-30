5f137fd8882676b3c534ef0d8e1cf624cf59b27b
Søren Møller Rasmussen <soeren.rasmussen@velux.com>
2025-04-30 13:16:33 +0200

    fixed linter errors

--- src/python_testing/TC_CLDIM_2_1.py
+++ src/python_testing/TC_CLDIM_2_1.py
@@ -32,11 +32,10 @@
 # === END CI TEST ARGUMENTS ===
 
 import logging
-from random import choice
 
 import chip.clusters as Clusters
 from chip.clusters.Types import NullValue
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main, type_matches
+from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 class TC_CLDIM_2_1(MatterBaseTest):
