6ab01658a9f2cdf834a5007c9b89b5719198cd34
Sergio Soares <sergiosoares@google.com>
2025-04-30 15:17:48 +0000

    EventManagement: Make ctor public again

--- src/app/EventManagement.h
+++ src/app/EventManagement.h
@@ -90,7 +90,7 @@ public:
      * @brief
      *   A constructor for the CircularEventBuffer (internal API).
      */
-    CircularEventBuffer() : TLVCircularBuffer(nullptr, 0){};
+    CircularEventBuffer() : TLVCircularBuffer(nullptr, 0) {};
 
     /**
      * @brief
@@ -155,7 +155,7 @@ class CircularEventReader;
 class CircularEventBufferWrapper : public TLV::TLVCircularBuffer
 {
 public:
-    CircularEventBufferWrapper() : TLVCircularBuffer(nullptr, 0), mpCurrent(nullptr){};
+    CircularEventBufferWrapper() : TLVCircularBuffer(nullptr, 0), mpCurrent(nullptr) {};
     CircularEventBuffer * mpCurrent;
 
 private:
@@ -390,7 +390,9 @@ public:
                              EventNumber & generatedEventNumber) override;
 
 private:
-    constexpr EventManagement() = default;
+    // The EventManagement() constructor was made private in pull/38447 but we want the ability to
+    // instantiate this class in test code. Make it public for now until we find a cleaner solution.
+    // constexpr EventManagement() = default;
     static EventManagement sInstance;
 
     class InternalEventOptions : public EventOptions
