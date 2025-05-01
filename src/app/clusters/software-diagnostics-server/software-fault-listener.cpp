ec08d4957d9ec7a830127a377da4d2b43fb8b333
Andrei Litvin <andreilitvin@google.com>
2025-05-01 09:56:43 -0400

    Dependency update in BUILD.gn, fix jinja comments

--- src/app/clusters/software-diagnostics-server/software-fault-listener.cpp
+++ src/app/clusters/software-diagnostics-server/software-fault-listener.cpp
@@ -14,8 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#pragma once
-
 #include <app/clusters/software-diagnostics-server/software-fault-listener.h>
 
 namespace chip {
