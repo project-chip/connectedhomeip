915cccad579d09b992a9e9bcc95088750ec0b6ed
Søren Møller Rasmussen <126240309+sorenmr@users.noreply.github.com>
2025-05-05 10:16:48 +0200

    Updated CI Test Arguments

--- src/python_testing/TC_CLDIM_3_1.py
+++ src/python_testing/TC_CLDIM_3_1.py
@@ -21,13 +21,14 @@
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs:
 #    run1:
-#      app: ${TYPE_OF_APP}
+#      app: ${ALL_CLUSTERS_APP}
 #      app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
 #      script-args: >
 #        --storage-path admin_storage.json
 #        --commissioning-method on-network
 #        --discriminator 1234
 #        --passcode 20202021
+#        --int-arg PIXIT.CLDIM.FullMotionDuration:1
 #        --trace-to json:${TRACE_TEST_JSON}.json
 #        --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #      factory-reset: true
