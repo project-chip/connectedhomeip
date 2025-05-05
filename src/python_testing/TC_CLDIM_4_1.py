7f00ce1221a7ef3c124da5986d62418058ed4940
Søren Møller Rasmussen <soeren.rasmussen@velux.com>
2025-05-05 11:44:46 +0200

    Added CI Test Arguments

--- src/python_testing/TC_CLDIM_4_1.py
+++ src/python_testing/TC_CLDIM_4_1.py
@@ -15,6 +15,27 @@
 #    limitations under the License.
 #
 
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs:
+#    run1:
+#      app: ${ALL_CLUSTERS_APP}
+#      app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#      script-args: >
+#        --storage-path admin_storage.json
+#        --commissioning-method on-network
+#        --discriminator 1234
+#        --passcode 20202021
+#        --int-arg PIXIT.CLDIM.FullMotionDuration:1
+#        --int-arg PIXIT.CLDIM.StepMotionDuration:1
+#        --trace-to json:${TRACE_TEST_JSON}.json
+#        --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#      factory-reset: true
+#      quiet: true
+# === END CI TEST ARGUMENTS ===
+
 import logging
 import time
 
