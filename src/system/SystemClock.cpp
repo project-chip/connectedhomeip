5a9611b6358add8dd45d34c4689881c01cad063e
James Harrow <james.harrow@gmail.com>
2025-05-06 17:45:30 +0100

    Fixed build issue with logging

--- src/system/SystemClock.cpp
+++ src/system/SystemClock.cpp
@@ -297,7 +297,7 @@ CHIP_ERROR GetClock_MatterEpochS(uint32_t & aMatterEpoch)
     auto unixEpoch = std::chrono::duration_cast<Seconds32>(cTMs).count();
     if (!UnixEpochToChipEpochTime(unixEpoch, aMatterEpoch))
     {
-        ChipLogError(DeviceLayer, "Unable to convert Unix Epoch time to Matter Epoch Time: unixEpoch (%ld)", unixEpoch);
+        ChipLogError(DeviceLayer, "Unable to convert Unix Epoch time to Matter Epoch Time: unixEpoch (%u)", unixEpoch);
         return CHIP_ERROR_INCORRECT_STATE;
     }
 
