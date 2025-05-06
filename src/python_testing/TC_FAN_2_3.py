1d7686c27413111d993382dd69606713664a48af
Restyled.io <commits@restyled.io>
2025-05-06 18:01:09 +0000

    Restyled by isort

--- src/python_testing/TC_FAN_2_3.py
+++ src/python_testing/TC_FAN_2_3.py
@@ -43,7 +43,6 @@ import chip.clusters as Clusters
 from chip.interaction_model import Status
 from chip.testing.matter_asserts import assert_valid_map8
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
-
 from mobly import asserts
 
 logger = logging.getLogger(__name__)
