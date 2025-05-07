6cf6d5a184d8926546635347c0eebbad79628a65
Martin Girardot <martin.girardot@nxp.com>
2025-05-07 14:19:33 +0200

    update src/platform/nxp/common/NetworkCommissioningWiFiDriver.cpp
    Signed-off-by: Martin Girardot <martin.girardot@nxp.com>

--- src/platform/nxp/common/NetworkCommissioningWiFiDriver.cpp
+++ src/platform/nxp/common/NetworkCommissioningWiFiDriver.cpp
@@ -203,9 +203,11 @@ CHIP_ERROR NXPWiFiDriver::ConnectWiFiNetwork(const char * ssid, uint8_t ssidLen,
 
 void NXPWiFiDriver::OnConnectWiFiNetwork(Status commissioningError, CharSpan debugText, int32_t connectStatus)
 {
-    /* Commit wifi network credentials in flash only if the connexion succeeded */
+    /* Commit wifi network credentials in flash only if the connection succeeded */
     if (commissioningError == NetworkCommissioning::Status::kSuccess)
+    {
         CommitConfiguration();
+    }
 
     if (mpConnectCallback != nullptr)
     {
