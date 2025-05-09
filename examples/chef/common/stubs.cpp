7a909a9ceb78411ad04dbc93c733040fe7e0585e
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-05-08 21:16:40 -0700

    Fix NRF break.

--- examples/chef/common/stubs.cpp
+++ examples/chef/common/stubs.cpp
@@ -502,7 +502,12 @@ void ApplicationInit()
 
     RefrigeratorTemperatureControlledCabinetInit();
     OvenTemperatureControlledCabinetCooktopCookSurfaceInit();
+
+#ifdef MATTER_DM_PLUGIN_PUMP_CONFIGURATION_AND_CONTROL_SERVER
+#ifdef MATTER_DM_PLUGIN_ON_OFF_SERVER
     chef::pump::init();
+#endif // MATTER_DM_PLUGIN_ON_OFF_SERVER
+#endif // MATTER_DM_PLUGIN_PUMP_CONFIGURATION_AND_CONTROL_SERVER
 
 #ifdef MATTER_DM_PLUGIN_WINDOW_COVERING_SERVER
     ChipLogProgress(NotSpecified, "Initializing WindowCovering cluster delegate.");
