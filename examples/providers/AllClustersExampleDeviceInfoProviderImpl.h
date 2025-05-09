4a2b3df1dc10313c5d4260d88fbd32c012e48126
Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
2025-05-09 13:26:36 -0400

    Remove garbage default data from DeviceInfoProviderImpl (#38754)
    * Remove garbage default data from DeviceInfoProviderImpl
    
    - Sample version was copied by many manufacturers/platforms, but
      contained a lot of "overly sample data" which, when copied
      by manufacturers, led to just unusable data in the field.
    - This PR removes all garbage default data that doesn't make
      sense, and replaces it with more likely data.
    - This PR makes a version for all-clusters-app that has more
      data to help test the SDK.
    - Updated some docs and renamed the target so that builds
      need to be updated on roll.
    
    * Restyled by clang-format
    
    * Restyled by prettier-yaml
    
    * Update examples/platform/linux/AppMain.cpp
    
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>
    
    * Improve docs on ESP32
    
    * Address review comments
    
    * Restyled by clang-format
    
    * Fix java test
    
    * Fix java test
    
    * Fix java test
    
    * Fix build on NXP
    
    * Fix cyw30739 platform
    
    * kick CI
    
    * Revert "kick CI"
    
    This reverts commit 95d7819eab93be45d61127ebdc3d0c20ddb8f94a.
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- examples/providers/AllClustersExampleDeviceInfoProviderImpl.h
+++ examples/providers/AllClustersExampleDeviceInfoProviderImpl.h
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2022 Project CHIP Authors
+ *    Copyright (c) 2025 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -16,31 +16,18 @@
  */
 #pragma once
 
-#include <lib/support/EnforceFormat.h>
+#include <lib/core/DataModelTypes.h>
+#include <lib/support/CHIPMem.h>
 #include <platform/DeviceInfoProvider.h>
 
 namespace chip {
 namespace DeviceLayer {
 
-// !!!!!!!!!!!!!!!!!!!!!!!! WARNING WARNING WARNING !!!!!!!!!!!!!!!!!!!!
-// WARNING: DO NOT USE THESE DEFAULT IMPLEMENTATIONS WITH DEFAULT VALUES
-// IN PRODUCTION PRODUCTS WITHOUT AUDITING THEM! See
-// `AllClustersExampleDeviceInforProviderImpl.h` for an example provider
-// that has constant values. Here, all providers have empty implementations
-// to force empty lists which prevent bad values from leaking into products
-// like happened before Matter 1.5. If you really are using these clusters,
-// then please re-implement the provider as needed.
-//
-// The FixedLabel, LocalizationConfigurationand and Time Format localization
-// clusters, if used, should have values that have been vetted
-// for correctness in the product !!! DO NOT USE SAMPLE DEFAULTS IN PRODUCTS.
-// !!!!!!!!!!!!!!!!!!!!!!!! WARNING WARNING WARNING !!!!!!!!!!!!!!!!!!!!
-
-class DeviceInfoProviderImpl : public DeviceInfoProvider
+class AllClustersExampleDeviceInfoProviderImpl : public DeviceInfoProvider
 {
 public:
-    DeviceInfoProviderImpl() = default;
-    ~DeviceInfoProviderImpl() override {}
+    AllClustersExampleDeviceInfoProviderImpl() = default;
+    ~AllClustersExampleDeviceInfoProviderImpl() override {}
 
     // Iterators
     FixedLabelIterator * IterateFixedLabel(EndpointId endpoint) override;
@@ -48,19 +35,33 @@ public:
     SupportedLocalesIterator * IterateSupportedLocales() override;
     SupportedCalendarTypesIterator * IterateSupportedCalendarTypes() override;
 
-    static DeviceInfoProviderImpl & GetDefaultInstance();
+    static AllClustersExampleDeviceInfoProviderImpl & GetDefaultInstance();
 
 protected:
+    class FixedLabelIteratorImpl : public FixedLabelIterator
+    {
+    public:
+        FixedLabelIteratorImpl(EndpointId endpoint);
+        size_t Count() override;
+        bool Next(FixedLabelType & output) override;
+        void Release() override { chip::Platform::Delete(this); }
+
+    private:
+        static constexpr size_t kNumSupportedFixedLabels = 1;
+        EndpointId mEndpoint                             = 0;
+        size_t mIndex                                    = 0;
+    };
+
     class UserLabelIteratorImpl : public UserLabelIterator
     {
     public:
-        UserLabelIteratorImpl(DeviceInfoProviderImpl & provider, EndpointId endpoint);
+        UserLabelIteratorImpl(AllClustersExampleDeviceInfoProviderImpl & provider, EndpointId endpoint);
         size_t Count() override { return mTotal; }
         bool Next(UserLabelType & output) override;
         void Release() override { chip::Platform::Delete(this); }
 
     private:
-        DeviceInfoProviderImpl & mProvider;
+        AllClustersExampleDeviceInfoProviderImpl & mProvider;
         EndpointId mEndpoint = 0;
         size_t mIndex        = 0;
         size_t mTotal        = 0;
@@ -77,7 +78,7 @@ protected:
         void Release() override { chip::Platform::Delete(this); }
 
     private:
-        static constexpr size_t kNumSupportedLocales = 1;
+        static constexpr size_t kNumSupportedLocales = 8;
         size_t mIndex                                = 0;
     };
 
