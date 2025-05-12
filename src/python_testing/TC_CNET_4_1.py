c6d8de1f9b4cdea10126a91577f8b3374c23f319
jtrejoespinoza-grid <juanerasmo@google.com>
2025-05-12 16:06:02 -0700

    Removed step 1 and 2. Updated step number accordning the test plan.

--- src/python_testing/TC_CNET_4_1.py
+++ src/python_testing/TC_CNET_4_1.py
@@ -43,29 +43,27 @@ class TC_CNET_4_1(MatterBaseTest):
 
     def steps_TC_CNET_4_1(self) -> list[TestStep]:
         steps = [
-            TestStep(2, "TH reads Descriptor Cluster from the DUT with EP0. TH reads ServerList from the DUT",
-                     "Verify for the presence of an element with value 49 (0x0031) in the ServerList"),
-            TestStep(3, "TH reads the MaxNetworks attribute from the DUT",
+            TestStep(1, "TH reads the MaxNetworks attribute from the DUT",
                      "Verify that MaxNetworks attribute value is within a range of 1 to 255"),
-            TestStep(4, "TH reads the Networks attribute list from the DUT on all available endpoints",
+            TestStep(2, "TH reads the Networks attribute list from the DUT on all available endpoints",
                      "Verify that each element in the Networks attribute list has the following fields: 'NetworkID', 'connected'.\n\
                       NetworkID field is of type octstr with a length range 1 to 32 \n\
                       The connected field is of type bool \n\
                       Verify that only one entry has connected status as TRUE \n\
                       Verify that the number of entries in the Networks attribute is less than or equal to 'MaxNetworksValue'"),
-            TestStep(5, "TH reads ScanMaxTimeSeconds attribute from the DUT",
+            TestStep(3, "TH reads ScanMaxTimeSeconds attribute from the DUT",
                      "Verify that ScanMaxTimeSeconds attribute value is within the range of 1 to 255 seconds"),
-            TestStep(6, "TH reads ConnectMaxTimeSeconds attribute from the DUT",
+            TestStep(4, "TH reads ConnectMaxTimeSeconds attribute from the DUT",
                      "Verify that ConnectMaxTimeSeconds attribute value is within the range of 1 to 255 seconds"),
-            TestStep(7, "TH reads InterfaceEnabled attribute from the DUT", "Verify that InterfaceEnabled attribute value is true"),
-            TestStep(8, "TH reads LastNetworkingStatus attribute from the DUT",
+            TestStep(5, "TH reads InterfaceEnabled attribute from the DUT", "Verify that InterfaceEnabled attribute value is true"),
+            TestStep(6, "TH reads LastNetworkingStatus attribute from the DUT",
                      "LastNetworkingStatus attribute value will be within any one of the following values \
                       Success, NetworkNotFound, OutOfRange, RegulatoryError, UnknownError, null"),
-            TestStep(9, "TH reads the LastNetworkID attribute from the DUT. TH reads the Networks attribute from the DUT",
+            TestStep(7, "TH reads the LastNetworkID attribute from the DUT. TH reads the Networks attribute from the DUT",
                      "Verify that LastNetworkID attribute matches the NetworkID value of one of the entries in the Networks attribute list"),
-            TestStep(10, "TH reads the LastConnectErrorValue attribute from the DUT",
+            TestStep(8, "TH reads the LastConnectErrorValue attribute from the DUT",
                      "Verify that LastConnectErrorValue attribute value is null"),
-            TestStep(11, "TH reads the SupportedWiFiBands attribute from the DUT",
+            TestStep(9, "TH reads the SupportedWiFiBands attribute from the DUT",
                      "Verify that SupportedWiFiBands attribute value has 1 or more entries, all of which are in the range of WiFiBandEnum.")
         ]
         return steps
@@ -74,21 +72,14 @@ class TC_CNET_4_1(MatterBaseTest):
                                          Clusters.NetworkCommissioning.Bitmaps.Feature.kWiFiNetworkInterface))
     async def test_TC_CNET_4_1(self):
         # Commissioning already done
-        self.step(2)
-        server_list = await self.read_single_attribute_check_success(
-            cluster=Clusters.Descriptor,
-            attribute=Clusters.Descriptor.Attributes.ServerList)
-        asserts.assert_true(49 in server_list,
-                            msg="Verify for the presence of an element with value 49 (0x0031) in the ServerList")
-
-        self.step(3)
+        self.step(1)
         max_networks_count = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.MaxNetworks)
 
         matter_asserts.assert_int_in_range(max_networks_count, min_value=1, max_value=255, description="MaxNetworks")
 
-        self.step(4)
+        self.step(2)
         network = await self.read_single_attribute_check_success(cluster=Clusters.NetworkCommissioning, attribute=Clusters.NetworkCommissioning.Attributes.Networks)
         logger.info(f"network single {network}")
         networks_dict = await self.read_single_attribute_all_endpoints(
@@ -120,33 +111,33 @@ class TC_CNET_4_1(MatterBaseTest):
         asserts.assert_less_equal(connected_networks, max_networks_count,
                                   "Number of entries in the Networks attribute is less than or equal to 'MaxNetworksValue'")
 
-        self.step(5)
+        self.step(3)
         scan_max_time_seconds = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.ScanMaxTimeSeconds)
         matter_asserts.assert_int_in_range(scan_max_time_seconds, min_value=1, max_value=255, description="ScanMaxTimeSeconds")
 
-        self.step(6)
+        self.step(4)
         connect_max_time_seconds = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.ConnectMaxTimeSeconds)
         matter_asserts.assert_int_in_range(connect_max_time_seconds, min_value=1,
                                            max_value=255, description="ConnectMaxTimeSeconds")
 
-        self.step(7)
+        self.step(5)
         interface_enabled = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.InterfaceEnabled)
         asserts.assert_true(interface_enabled, "Verify that InterfaceEnabled attribute value is true")
 
-        self.step(8)
+        self.step(6)
         last_networking_status = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.LastNetworkingStatus)
         expected_status = Clusters.NetworkCommissioning.Enums.NetworkCommissioningStatusEnum.kSuccess
         asserts.assert_is(last_networking_status, expected_status, "Verify that LastNetworkingStatus attribute value is success")
 
-        self.step(9)
+        self.step(7)
         last_network_id = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.LastNetworkID)
@@ -159,13 +150,13 @@ class TC_CNET_4_1(MatterBaseTest):
         asserts.assert_true(isinstance(last_network_id, bytes) and 1 <= len(last_network_id) <= 32,
                             "Verify LastNetworkID attribute value will be of type octstr with a length range of 1 to 32")
 
-        self.step(10)
+        self.step(8)
         last_connect_error_value = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.LastConnectErrorValue)
         asserts.assert_is(last_connect_error_value, NullValue, "Verify that LastConnectErrorValue attribute value is null")
 
-        self.step(11)
+        self.step(9)
         supported_wifi_bands = await self.read_single_attribute_check_success(
             cluster=Clusters.NetworkCommissioning,
             attribute=Clusters.NetworkCommissioning.Attributes.SupportedWiFiBands)
