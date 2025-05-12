7ba17d8c4da51cfaf12562fc92b441bae5c31822
gabc-cpp <cgarciaballesteros@griddynamics.com>
2025-05-11 23:36:28 -0600

    Moved AttributeEntry constructor inside of "for" loop.

--- src/data-model-providers/codegen/CodegenDataModelProvider.cpp
+++ src/data-model-providers/codegen/CodegenDataModelProvider.cpp
@@ -366,20 +366,19 @@ CHIP_ERROR CodegenDataModelProvider::Attributes(const ConcreteClusterPath & path
         ReturnErrorOnFailure(builder.Append(AttributeEntryFrom(path, attribute)));
     }
 
-    // This "GlobalListEntry" is specific for metadata that ember does not include
-    // in its attribute list metadata.
-    //
-    // By spec these Attribute/AcceptedCommands/GeneratedCommants lists are:
-    //   - lists of elements
-    //   - read-only, with read privilege view
-    //   - fixed value (no such flag exists, so this is not a quality flag we set/track)
-    DataModel::AttributeEntry globalListEntry(0, /* default initial value, attributeId is assigned later */
-                                              DataModel::AttributeQualityFlags::kListAttribute, Access::Privilege::kView,
-                                              std::nullopt);
-
     for (auto & attribute : GlobalAttributesNotInMetadata)
     {
-        globalListEntry.attributeId = attribute;
+
+        // This "GlobalListEntry" is specific for metadata that ember does not include
+        // in its attribute list metadata.
+        //
+        // By spec these Attribute/AcceptedCommands/GeneratedCommants lists are:
+        //   - lists of elements
+        //   - read-only, with read privilege view
+        //   - fixed value (no such flag exists, so this is not a quality flag we set/track)
+        DataModel::AttributeEntry globalListEntry(attribute, DataModel::AttributeQualityFlags::kListAttribute,
+                                                  Access::Privilege::kView, std::nullopt);
+
         ReturnErrorOnFailure(builder.Append(globalListEntry));
     }
 
