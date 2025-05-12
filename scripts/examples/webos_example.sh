b4ff8f7820f57845331d6c9a9100ffd62a28e152
jungwook-park <lian.park@lge.com>
2025-05-12 10:26:48 +0900

    Update webos_example.sh

--- scripts/examples/webos_example.sh
+++ scripts/examples/webos_example.sh
@@ -14,6 +14,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+
 # Activating connectedhomeip build environment
 source scripts/activate.sh
 
