3fdedf1c8997714cf741cd58c4270c0b4e7b71e9
Raveendra Karu <r.karu@samsung.com>
2025-05-16 01:08:51 +0530

    Add LICENSE and cleanup.
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- examples/camera-app/linux/include/uploader/pushav-uploader.h
+++ examples/camera-app/linux/include/uploader/pushav-uploader.h
@@ -18,29 +18,29 @@
 
 #pragma once
 
+#include <atomic>
+#include <curl/curl.h>
+#include <mutex>
 #include <queue>
 #include <string>
-#include <mutex>
-#include <atomic>
 #include <thread>
-#include <curl/curl.h>
 
-class PushAVUploader {
+class PushAVUploader
+{
 public:
     PushAVUploader();
     ~PushAVUploader();
 
     void start();
     void stop();
-    void add_uploadData(std::string& filename, std::string& url);
+    void add_uploadData(std::string & filename, std::string & url);
 
 private:
     void process_queue();
     void upload_data(std::pair<std::string, std::string> data);
 
-    std::queue<std::pair <std::string, std::string>> av_data;
+    std::queue<std::pair<std::string, std::string>> av_data;
     std::mutex queue_mutex;
     std::atomic<bool> running;
     std::thread uploader_thread;
 };
-
