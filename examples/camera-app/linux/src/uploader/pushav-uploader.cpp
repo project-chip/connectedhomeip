cc3e110f0f1d196cc3d12a8693e48c63612e120e
Raveendra Karu <r.karu@samsung.com>
2025-05-16 01:08:51 +0530

    Add LICENSE and cleanup.
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- examples/camera-app/linux/src/uploader/pushav-uploader.cpp
+++ examples/camera-app/linux/src/uploader/pushav-uploader.cpp
@@ -19,69 +19,80 @@
 #include "pushav-uploader.h"
 #include <lib/support/logging/CHIPLogging.h>
 
-static size_t pushAVCurlCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-  return 0;
-}
-
 PushAVUploader::PushAVUploader() : running(false) {}
 
-PushAVUploader::~PushAVUploader() {
+PushAVUploader::~PushAVUploader()
+{
     stop();
 }
 
-void PushAVUploader::process_queue() {
-    while (running) {
-	std::pair<std::string, std::string> data;
+void PushAVUploader::process_queue()
+{
+    while (running)
+    {
+        std::pair<std::string, std::string> data;
         {
-           std::lock_guard<std::mutex> lock(queue_mutex);
-           if (!av_data.empty()) {
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            if (!av_data.empty())
+            {
                 data = av_data.front();
                 av_data.pop();
             }
         }
-        if (!av_data.empty()) {
+        if (!av_data.empty())
+        {
             upload_data(data);
-        } else {
+        }
+        else
+        {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
 }
 
-void PushAVUploader::start() {
-    if (!running) {
-        running = true;
+void PushAVUploader::start()
+{
+    if (!running)
+    {
+        running         = true;
         uploader_thread = std::thread(&PushAVUploader::process_queue, this);
     }
 }
 
-void PushAVUploader::stop() {
-    if (running) {
+void PushAVUploader::stop()
+{
+    if (running)
+    {
         running = false;
-        if (uploader_thread.joinable()) {
+        if (uploader_thread.joinable())
+        {
             uploader_thread.join();
         }
     }
 }
 
-void PushAVUploader::add_uploadData(std::string& filename, std::string& url) {
+void PushAVUploader::add_uploadData(std::string & filename, std::string & url)
+{
     ChipLogError(Camera, "Added file name %s to queue", filename.c_str());
     std::lock_guard<std::mutex> lock(queue_mutex);
     auto data = make_pair(filename, url);
     av_data.push(data);
 }
 
-void PushAVUploader::upload_data(std::pair<std::string, std::string> data) {
-    CURL* curl = curl_easy_init();
-    if (!curl) {
+void PushAVUploader::upload_data(std::pair<std::string, std::string> data)
+{
+    CURL * curl = curl_easy_init();
+    if (!curl)
+    {
         ChipLogError(Camera, "Failed to initialize CURL");
         return;
     }
-    curl_mime *form;
-    curl_mimepart *field;
-    struct curl_slist *header;
+    curl_mime * form;
+    curl_mimepart * field;
+    struct curl_slist * header;
     std::string response;
 
-    form = curl_mime_init(curl);
+    form  = curl_mime_init(curl);
     field = curl_mime_addpart(form);
     curl_mime_name(field, "file");
     curl_mime_filedata(field, data.first.c_str());
@@ -90,18 +101,18 @@ void PushAVUploader::upload_data(std::pair<std::string, std::string> data) {
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
     curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
     curl_easy_setopt(curl, CURLOPT_URL, data.second.c_str());
-    //TODO:Enable below code for saving the response header
-    //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, pushAVCurlCallback);
-    //curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);    
     curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 1);    
+    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 1);
 
     CURLcode res = curl_easy_perform(curl);
 
-    if (res != CURLE_OK) {
+    if (res != CURLE_OK)
+    {
         ChipLogError(Camera, "CURL upload failed %s", curl_easy_strerror(res));
-    } else {
+    }
+    else
+    {
         ChipLogError(Camera, "CURL uploaded file  %s", data.first.c_str());
     }
 
@@ -109,4 +120,3 @@ void PushAVUploader::upload_data(std::pair<std::string, std::string> data) {
     curl_easy_cleanup(curl);
     curl_slist_free_all(header);
 }
-
