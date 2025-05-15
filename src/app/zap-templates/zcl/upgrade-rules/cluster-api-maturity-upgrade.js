15244bfeb368d1668349707bb8cc50d23f89dc8c
brdandu <brdandu@silabs.com>
2025-05-15 15:15:10 -0400

    Restyle changes

--- src/app/zap-templates/zcl/upgrade-rules/cluster-api-maturity-upgrade.js
+++ src/app/zap-templates/zcl/upgrade-rules/cluster-api-maturity-upgrade.js
@@ -1,32 +1,35 @@
 /**
  * Upgrade rule to notify users that api maturity has been changed for clusters
  * and it is no longer provisional.
- * @param {*} api 
- * @param {*} context 
+ * @param {*} api
+ * @param {*} context
  * @returns A string mentioning the API changes made to .zap file
  */
-async function postLoad(api, context) {
+async function postLoad(api, context)
+{
   let resMsg = ''
-  let epts = await api.endpoints(context)
-  for (let i = 0; i < epts.length; i++) {
+  let epts   = await api.endpoints(context)
+  for (let i = 0; i < epts.length; i++)
+  {
     let clusters = await api.clusters(context, epts[i])
-    for (let j = 0; j < clusters.length; j++) {
+    for (let j = 0; j < clusters.length; j++)
+    {
       /* The following clusters are no longer provisional
       - WIFI_NETWORK_MANAGEMENT_CLUSTER
       - THREAD_BORDER_ROUTER_MANAGEMENT_CLUSTER
       - THREAD_NETWORK_DIRECTORY_CLUSTER
       */
-      if (
-        parseInt(clusters[j].code) == 0x0451
-        || parseInt(clusters[j].code) == 0x0452
-        || parseInt(clusters[j].code) == 0x0453) {
+      if (parseInt(clusters[j].code) == 0x0451 || parseInt(clusters[j].code) == 0x0452 || parseInt(clusters[j].code) == 0x0453) {
         resMsg += `${clusters[j].name} cluster on endpoint ${epts[i].endpointIdentifier} is no longer provisional.\n`
       }
     }
   }
   // Status can be 'nothing', 'automatic', 'user_verification', 'impossible'.
   if (resMsg) {
-    return { message: resMsg, status: 'automatic' }
+    return
+    {
+      message: resMsg, status: 'automatic'
+    }
   }
 }
 
