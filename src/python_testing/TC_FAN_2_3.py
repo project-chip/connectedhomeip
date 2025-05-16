91f6681b09472b3247937c928bcab43d7d4d9739
Arkadiusz Bokowy <a.bokowy@samsung.com>
2025-05-16 09:27:35 +0200

    Fix flake8 warnings after merge with master

--- src/python_testing/TC_FAN_2_3.py
+++ src/python_testing/TC_FAN_2_3.py
@@ -43,7 +43,7 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.interaction_model import Status
-from matter.testing.matter_asserts import assert_valid_map8, assert_valid_uint8
+from matter.testing.matter_asserts import assert_valid_map8
 from matter.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_feature, run_if_endpoint_matches
 
 logger = logging.getLogger(__name__)
