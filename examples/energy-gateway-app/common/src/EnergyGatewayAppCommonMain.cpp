c47e5b1dc98d61405aa6e3ccf4249b94c8f18e7a
dsavitsky-dsr <dmitry.savitskiy@dsr-corporation.com>
2025-05-19 18:13:02 +0300

    :The MeterId cluster moved from all-clusters-app sample to energy-gateway-app sample

--- examples/energy-gateway-app/common/src/EnergyGatewayAppCommonMain.cpp
+++ examples/energy-gateway-app/common/src/EnergyGatewayAppCommonMain.cpp
@@ -19,6 +19,7 @@
 #include "EnergyGatewayAppCommonMain.h"
 #include "CommodityPriceMain.h"
 #include "ElectricalGridConditionsMain.h"
+#include "meter-identification-instance.h"
 
 #include <app-common/zap-generated/ids/Attributes.h>
 #include <app-common/zap-generated/ids/Clusters.h>
@@ -71,6 +72,7 @@ void ElectricalEnergyTariffInit()
 
     VerifyOrDie(CommodityPriceInit(kElectricalEnergyTariffEndpointId) == CHIP_NO_ERROR);
     VerifyOrDie(ElectricalGridConditionsInit(kElectricalEnergyTariffEndpointId) == CHIP_NO_ERROR);
+    VerifyOrDie(MeterIdentificationInit(kElectricalEnergyTariffEndpointId) == CHIP_NO_ERROR);
 
     // set the descriptor TagList to include "ElectricalEnergy" and "Current" (to indicate the ActiveTariff)
     SetTagList(kElectricalEnergyTariffEndpointId,
@@ -79,5 +81,6 @@ void ElectricalEnergyTariffInit()
 
 void ElectricalEnergyTariffShutdown()
 {
+    MeterIdentificationShutdown();
     ChipLogDetail(AppServer, "Energy Gateway App : ElectricalEnergyTariffShutdown()");
 }
--- examples/energy-gateway-app/meter-identification/include/meter-identification-instance.h
+++ examples/energy-gateway-app/meter-identification/include/meter-identification-instance.h
@@ -2,13 +2,16 @@
 
 #include <app/clusters/meter-identification-server/meter-identification-server.h>
 
+CHIP_ERROR MeterIdentificationInit(chip::EndpointId endpointId);
+CHIP_ERROR MeterIdentificationShutdown();
+
 namespace chip {
 namespace app {
 namespace Clusters {
 namespace MeterIdentification {
 
 Instance * GetInstance();
-
+void Init();
 void Shutdown();
 
 } // namespace MeterIdentification
