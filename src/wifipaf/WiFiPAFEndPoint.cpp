6d7cdac5ad2db1eb3c01b3cf5ecb162ce62adde8
Lo,Chin-Ran <chin-ran.lo@nxp.com>
2025-05-20 06:46:26 +0200

    Change the description of the comment
    Signed-off-by: Lo,Chin-Ran <chin-ran.lo@nxp.com>

--- src/wifipaf/WiFiPAFEndPoint.cpp
+++ src/wifipaf/WiFiPAFEndPoint.cpp
@@ -72,7 +72,7 @@
 #define WIFIPAF_ACK_SEND_TIMEOUT_MS 2500
 #define WIFIPAF_WAIT_RES_TIMEOUT_MS 1000
 // Drop the connection if network resources remain unavailable for the period.
-// Known condition: If the remote site is awaiting an ACK packet, the wait time must not exceed PAFTP_ACK_TIMEOUT_MS.
+// Known condition: If the remote side is awaiting an ACK packet, the wait time must not exceed PAFTP_ACK_TIMEOUT_MS.
 #define WIFIPAF_MAX_RESOURCE_BLOCK_COUNT (PAFTP_ACK_TIMEOUT_MS / WIFIPAF_WAIT_RES_TIMEOUT_MS)
 
 /**
