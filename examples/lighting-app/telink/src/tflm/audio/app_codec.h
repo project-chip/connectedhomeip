1e845ee5c4212297ad42f4833ce36734bb261e6c
Jinmiao Yu <jinmiao.yu@telink-semi.com>
2025-05-20 15:06:29 +0800

    [Telink] added tflm feature to lighting-app
    - added tflm feature to tl721x lighting-app
    - compile command: west build -b tl7218x -- -DCONFIG_TFLM_FEATURE=y
    
    Signed-off-by: Jinmiao Yu <jinmiao.yu@telink-semi.com>

---
+++ examples/lighting-app/telink/src/tflm/audio/app_codec.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Telink Semiconductor (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef VENDOR_AUDIO_DEMO_APP_CODEC_H_
+#define VENDOR_AUDIO_DEMO_APP_CODEC_H_
+
+/* Enable C linkage for C++ Compilers: */
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+#include "app_audio.h"
+#include "../micro_speech/tflite_micro_micro_speech.h"
+
+void app_codec_init(void);
+
+/* Disable C linkage for C++ Compilers: */
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* VENDOR_AUDIO_DEMO_APP_CODEC_H_ */
