5c69a30773fdd2650619c496ccad0dba46be3997
m.trayer <m.trayer@samsung.com>
2025-05-20 16:16:05 -0500

    Fix linter errors

--- src/python_testing/TC_AVSUM_2_7.py
+++ src/python_testing/TC_AVSUM_2_7.py
@@ -37,7 +37,7 @@
 
 import chip.clusters as Clusters
 from chip.interaction_model import Status
-from chip.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_cluster, has_feature,
+from chip.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_feature,
                                          run_if_endpoint_matches)
 from mobly import asserts
 from TC_AVSUMTestBase import AVSUMTestBase
