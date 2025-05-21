13f6813b6765c24fe47521480aff74c6acadc20e
Cecille Freeman <cecille@google.com>
2025-05-21 09:48:52 -0400

    Add script

---
+++ src/python_testing/generate_test_case_list.py
@@ -0,0 +1,31 @@
+#
+#    Copyright (c) 2025 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+import subprocess
+
+
+def main():
+    # TODO: glob
+    case_list = ['TC_DeviceBasicComposition.py', 'TC_DeviceConformance.py',
+                 'TC_FAN_3_2.py', 'TC_VALCC_4_5.py', 'TC_CNET_4_3.py', 'TC_DRLK_2_5.py']
+    for file in case_list:
+        cmd = f'python src/python_testing/{file} --storage-path admin_storage.json --bool-arg test_case_list:True'
+        subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
