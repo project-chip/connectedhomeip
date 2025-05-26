b719eabd3d4a69bd9501b61bca56ff645a4b5422
Restyled.io <commits@restyled.io>
2025-05-26 16:41:56 +0000

    Restyled by clang-format

--- src/platform/Tizen/DnssdImpl.cpp
+++ src/platform/Tizen/DnssdImpl.cpp
@@ -407,7 +407,8 @@ namespace Dnssd {
 DnssdTizen DnssdTizen::sInstance;
 
 RegisterContext::RegisterContext(DnssdTizen * instance, const char * type, const DnssdService & service,
-                                 DnssdPublishCallback callback, void * context) : GenericContext(ContextType::Register, instance)
+                                 DnssdPublishCallback callback, void * context) :
+    GenericContext(ContextType::Register, instance)
 {
     Platform::CopyString(mName, service.mName);
     Platform::CopyString(mType, type);
@@ -431,7 +432,8 @@ RegisterContext::~RegisterContext()
 }
 
 BrowseContext::BrowseContext(DnssdTizen * instance, const char * type, Dnssd::DnssdServiceProtocol protocol, uint32_t interfaceId,
-                             DnssdBrowseCallback callback, void * context) : GenericContext(ContextType::Browse, instance)
+                             DnssdBrowseCallback callback, void * context) :
+    GenericContext(ContextType::Browse, instance)
 {
     Platform::CopyString(mType, type);
     mProtocol    = protocol;
@@ -451,7 +453,8 @@ BrowseContext::~BrowseContext()
 }
 
 ResolveContext::ResolveContext(DnssdTizen * instance, const char * name, const char * type, uint32_t interfaceId,
-                               DnssdResolveCallback callback, void * context) : GenericContext(ContextType::Resolve, instance)
+                               DnssdResolveCallback callback, void * context) :
+    GenericContext(ContextType::Resolve, instance)
 {
     Platform::CopyString(mName, name);
     Platform::CopyString(mType, type);
