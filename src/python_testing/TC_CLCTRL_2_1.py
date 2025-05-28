29945bb4d20ffccb041c92ea3cc357ad92bfb849
Fabian Wechselberger <fabian@innovation-matters.at>
2025-05-28 15:03:24 +0000

    TC-CLCTRL-2.1 Add LatchControlModes check, some fixes due to execution issues

--- src/python_testing/TC_CLCTRL_2_1.py
+++ src/python_testing/TC_CLCTRL_2_1.py
@@ -38,7 +38,6 @@ from chip.clusters.Types import NullValue
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
-
 class TC_CLCTRL_2_1(MatterBaseTest):
     async def read_closurecontrol_attribute_expect_success(self, endpoint, attribute):
         cluster = Clusters.Objects.ClosureControl
@@ -50,13 +49,14 @@ class TC_CLCTRL_2_1(MatterBaseTest):
     def steps_TC_CLCTRL_2_1(self) -> list[TestStep]:
         steps = [
             TestStep(1, "Commissioning, already done", is_commissioning=True),
-            TestStep(2, "Read AttributeList attribute to determine supported attributes"),
-            TestStep(3, "Read FeatureMap attribute to determine supported features"),
-            TestStep(4, "Read CountdownTime attribute if supported"),
-            TestStep(5, "Read MainState attribute"),
-            TestStep(6, "Read CurrentErrorList attribute"),
-            TestStep(7, "Read OverallState attribute"),
-            TestStep(8, "Read OverallTarget attribute"),
+            TestStep(2, "Read the AttributeList attribute to determine supported attributes"),
+            TestStep(3, "Read the FeatureMap attribute to determine supported features"),
+            TestStep(4, "Read the CountdownTime attribute, if it is supported"),
+            TestStep(5, "Read the MainState attribute"),
+            TestStep(6, "Read the CurrentErrorList attribute"),
+            TestStep(7, "Read the OverallState attribute"),
+            TestStep(8, "Read the OverallTarget attribute"),
+            TestStep(9, "Read the LatchControlModes attribute, if LT is supported")
         ]
         return steps
 
@@ -77,86 +77,95 @@ class TC_CLCTRL_2_1(MatterBaseTest):
 
         # STEP 2: Read AttributeList attribute to determine supported attributes
         self.step(2)
-        attribute_list = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.AttributeList)
-        logging.info(f"Attribute list: {attribute_list}")
+        attribute_list: typing.List[uint] = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.AttributeList)
+        logging.info(f"AttributeList: {attribute_list}")
 
         # STEP 3: Read FeatureMap attribute to determine supported features
         self.step(3)
-        feature_map = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.FeatureMap)
-
-        is_latching_supported = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kMotionLatching
-        is_positioning_supported = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kPositioning
-        is_speed_supported = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kSpeed
+        feature_map: uint = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.FeatureMap)
+        
+        is_positioning_supported: bool = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kPositioning
+        is_latching_supported: bool = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kMotionLatching
+        is_speed_supported: bool = feature_map & Clusters.ClosureControl.Bitmaps.Feature.kSpeed
+        
+        logging.info(f"FeatureMap: {feature_map}")
+        logging.info(f"-> Positioning supported: \t\t {bool(is_positioning_supported)}")
+        logging.info(f"-> MotionLatching supported:\t {bool(is_latching_supported)}")
+        logging.info(f"-> Speed supported: \t\t {bool(is_speed_supported)}")
 
         # STEP 4: Read CountdownTime attribute if supported
         self.step(4)
         if attributes.CountdownTime.attribute_id in attribute_list:
-            countdown_time = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.CountdownTime)
+            countdown_time: typing.Union[None, Nullable, uint] = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.CountdownTime)
             logging.info(f"CountdownTime: {countdown_time}")
+            
             if countdown_time is not NullValue:
                 asserts.assert_less_equal(countdown_time, 259200, "CountdownTime attribute is out of range")
-                asserts.assert_greater_equal(countdown_time, 0, "CountdownTime attribute is out of range")
+                asserts.assert_greater_equal(countdown_time, 0, "CountdownTime attribute is out of range")    
         else:
-            logging.info("CountdownTime attribute not supported, skipping step")
+            logging.info("CountdownTime attribute not supported")
 
         # STEP 5: Read MainState attribute
         self.step(5)
-        main_state = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.MainState)
+        main_state: ClosureControl.Enums.MainStateEnum = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.MainState)
+        logging.info(f"MainState: {main_state, Clusters.ClosureControl.Enums.MainStateEnum(main_state).name}")
         asserts.assert_less_equal(main_state, Clusters.ClosureControl.Enums.MainStateEnum.kSetupRequired,
                                   "MainState attribute is out of range")
         asserts.assert_greater_equal(main_state, Clusters.ClosureControl.Enums.MainStateEnum.kStopped,
                                      "MainState attribute is out of range")
-        logging.info(f"MainState: {main_state}")
 
         # STEP 6: Read CurrentErrorList attribute
         self.step(6)
-        current_error_list = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.CurrentErrorList)
+        current_error_list: typing.List[ClosureControl.Enums.ClosureErrorEnum] = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.CurrentErrorList)
+        logging.info(f"CurrentErrorList: {current_error_list}")
         asserts.assert_less_equal(len(current_error_list), 10, "CurrentErrorList length is out of range")
         for error in current_error_list:
             # Check if error is a valid value, if the list is not empty
             asserts.assert_less_equal(error, 0xBF, "CurrentErrorList value is out of range")
             asserts.assert_greater_equal(error, 0x00, "CurrentErrorList value is out of range")
-        logging.info(f"CurrentErrorList: {current_error_list}")
 
         # STEP 7: Read OverallState attribute
         self.step(7)
-        overall_state = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallState)
+        overall_state: typing.Union[Nullable, ClosureControl.Structs.OverallStateStruct] = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallState)
         logging.info(f"OverallState: {overall_state}")
 
-        if overall_state is not NullValue:
+        if overall_state is NullValue:
+            logging.info("OverallState is NULL")
+        else:
             # Check Positioning feature in OverallState - PS feature (bit 0)
             if is_positioning_supported and overall_state.positioning is not NullValue:
+                logging.info(f"OverallState.positioning: {overall_state.positioning}")
                 asserts.assert_less_equal(
                     overall_state.positioning, Clusters.ClosureControl.Enums.PositioningEnum.kOpenedAtSignature, "OverallState.positioning is out of range")
                 asserts.assert_greater_equal(
                     overall_state.positioning, Clusters.ClosureControl.Enums.PositioningEnum.kFullyClosed, "OverallState.positioning is out of range")
-                logging.info(f"OverallState.positioning: {overall_state.positioning}")
 
             # Check MotionLatching feature in OverallState - LT feature (bit 1)
             if is_latching_supported and overall_state.latch is not NullValue:
-                asserts.assert_true(isinstance(overall_state.latch, bool), "OverallState.latch is not a boolean value")
                 logging.info(f"OverallState.latch: {overall_state.latch}")
-
+                asserts.assert_true(isinstance(overall_state.latch, bool), "OverallState.latch is not a boolean value")
+            
             # Check Speed feature in OverallState - SP feature (bit 3)
             if is_speed_supported and overall_state.speed is not NullValue:
+                logging.info(f"OverallState.speed: {overall_state.speed}")
                 asserts.assert_less_equal(overall_state.speed, Clusters.Globals.ThreeLevelAutoEnum.kHigh,
                                           "OverallState.speed is out of range")
                 asserts.assert_greater_equal(overall_state.speed, Clusters.Globals.ThreeLevelAutoEnum.kAuto,
                                              "OverallState.speed is out of range")
-                logging.info(f"OverallState.speed: {overall_state.speed}")
 
             # Check SecureState attribute in OverallState
             if is_positioning_supported or is_latching_supported:
-                asserts.assert_true(isinstance(overall_state.secureState, bool), "OverallState.SecureState is not a boolean value")
-        else:
-            logging.info("OverallState is NULL, skipping steps")
-
+                logging.info(f"OverallState.secureState: {overall_state.secureState}")
+                asserts.assert_true(isinstance(overall_state.secureState, bool), "OverallState.secureState is not a boolean value")
+            
         # STEP 8: Read OverallTarget attribute
         self.step(8)
-        overall_target = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallTarget)
+        overall_target: typing.Union[Nullable, ClosureControl.Structs.OverallTargetStruct] = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallTarget)
         logging.info(f"OverallTarget: {overall_target}")
 
-        if overall_target is not NullValue:
+        if overall_target is NullValue:
+            logging.info("OverallTarget is NULL")
+        else:
             # Check Positioning feature in OverallTarget
             if is_positioning_supported:  # PS feature (bit 0)
                 asserts.assert_less_equal(
@@ -176,10 +185,18 @@ class TC_CLCTRL_2_1(MatterBaseTest):
                                           "OverallTarget.speed is out of range")
                 asserts.assert_greater_equal(overall_target.speed, Clusters.Globals.ThreeLevelAutoEnum.kAuto,
                                              "OverallTarget.speed is out of range")
-                logging.info(f"OverallTarget.speed: {overall_target.speed}")
+                logging.info(f"OverallTarget.speed: {overall_target.speed}")            
+            
+        # STEP 9: Read LatchControlModes attribute if LT is supported
+        self.step(9)
+        if not is_latching_supported:
+            logging.info("LatchControlModes attribute not supported")
         else:
-            logging.info("OverallTarget is NULL, skipping steps")
-
+            latch_control_modes: uint = await self.read_closurecontrol_attribute_expect_success(endpoint=endpoint, attribute=attributes.LatchControlModes)
+            logging.info(f"LatchControlModes: {latch_control_modes}")
+            
+            asserts.assert_less_equal(latch_control_modes, 3, "LatchControlModes attribute is out of range")
+            asserts.assert_greater_equal(latch_control_modes, 0, "LatchControlModes attribute is out of range")    
 
 if __name__ == "__main__":
     default_matter_test_main()
