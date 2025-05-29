652c95ba5b2843edffb95922a2cd066082ed029b
Andrei Litvin <andreilitvin@google.com>
2025-05-29 16:39:48 -0400

    Fix wrong pragma in cpp file

--- src/app/clusters/network-commissioning/CodegenInstance.cpp
+++ src/app/clusters/network-commissioning/CodegenInstance.cpp
@@ -14,8 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#pragma once
-
 #include "CodegenInstance.h"
 
 #include <data-model-providers/codegen/CodegenDataModelProvider.h>
