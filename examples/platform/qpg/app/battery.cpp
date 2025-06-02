7d0215a9ac8fb0483d6c270b728d07a0d50f9789
Kliff Wong <kliff.wong@qorvo.com>
2025-06-02 15:08:37 +0100

    [QPG] Fix file header from Qorvo to Project CHIP

--- examples/platform/qpg/app/battery.cpp
+++ examples/platform/qpg/app/battery.cpp
@@ -1,28 +1,19 @@
 /*
- * Copyright (c) 2024, Qorvo Inc
  *
+ *    Copyright (c) 2024 Project CHIP Authors
+ *    All rights reserved.
  *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * This software is owned by Qorvo Inc
- * and protected under applicable copyright laws.
- * It is delivered under the terms of the license
- * and is intended and supplied for use solely and
- * exclusively with products manufactured by
- * Qorvo Inc.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *
- * THIS SOFTWARE IS PROVIDED IN AN "AS IS"
- * CONDITION. NO WARRANTIES, WHETHER EXPRESS,
- * IMPLIED OR STATUTORY, INCLUDING, BUT NOT
- * LIMITED TO, IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- * QORVO INC. SHALL NOT, IN ANY
- * CIRCUMSTANCES, BE LIABLE FOR SPECIAL,
- * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * FOR ANY REASON WHATSOEVER.
- *
- *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 /** @file "mainBattery.cpp"
