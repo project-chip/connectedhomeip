88ffc8644f6afe0c6adb61bc4159712c68deca99
James Harrow <james.harrow@gmail.com>
2025-06-06 15:08:49 +0100

    Added redirection of old YAML tests to Energy Management App

--- src/python_testing/TC_PWRTL_2_1.py
+++ src/python_testing/TC_PWRTL_2_1.py
@@ -21,7 +21,7 @@
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs:
 #   run1:
-#     app: ${ALL_CLUSTERS_APP}
+#     app: ${ENERGY_MANAGEMENT_APP}
 #     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
 #     script-args: >
 #       --storage-path admin_storage.json
