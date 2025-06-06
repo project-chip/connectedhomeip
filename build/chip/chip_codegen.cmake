e3faf73a20b77fa239d89827cef936dfdadca01d
Gatien Chapon <gatien.chapon@nxp.com>
2025-06-06 09:18:47 +0200

    [CMake] Refactor Matter-related CMake calls for cross-platform compatibility, including Windows support
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- build/chip/chip_codegen.cmake
+++ build/chip/chip_codegen.cmake
@@ -65,7 +65,7 @@ function(chip_codegen TARGET_NAME)
         # find_package(Python3 REQUIRED)
         add_custom_command(
             OUTPUT ${OUT_NAMES}
-            COMMAND "${CHIP_ROOT}/scripts/codegen.py"
+            COMMAND ${Python3_EXECUTABLE} ${CHIP_ROOT}/scripts/codegen.py
             ARGS "--generator" "${ARG_GENERATOR}"
                  "--output-dir" "${GEN_FOLDER}"
                  "--expected-outputs" "${GEN_FOLDER}/expected.outputs"
@@ -190,7 +190,7 @@ function(chip_zapgen TARGET_NAME)
         #    Error: EEXIST: file already exists, mkdir '/var/folders/24/8k48jl6d249_n_qfxwsl6xvm0000gn/T/pkg/465fcc8a6282e28dc7a166859d5814d34e2fb94249a72fa9229033b5b32dff1a'
         add_custom_command(
             OUTPUT ${OUT_NAMES}
-            COMMAND "${CHIP_ROOT}/scripts/tools/zap/generate.py"
+            COMMAND ${Python3_EXECUTABLE} ${CHIP_ROOT}/scripts/tools/zap/generate.py
             ARGS
                 "--no-prettify-output"
                 "--templates" "${TEMPLATE_PATH}"
