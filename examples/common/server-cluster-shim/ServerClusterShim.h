c631367ca36312fa3f2660f92f7d5172fc67a64c
Restyled.io <commits@restyled.io>
2025-06-06 21:05:47 +0000

    Restyled by clang-format

--- examples/common/server-cluster-shim/ServerClusterShim.h
+++ examples/common/server-cluster-shim/ServerClusterShim.h
@@ -83,9 +83,10 @@ public:
 
     void ListAttributeWriteNotification(const ConcreteAttributePath & aPath, DataModel::ListWriteOperation opType) override;
 
-CHIP_ERROR EventInfo(const ConcreteEventPath & path, DataModel::EventEntry & eventInfo) override;
+    CHIP_ERROR EventInfo(const ConcreteEventPath & path, DataModel::EventEntry & eventInfo) override;
 
-    private : bool ContainsClusterPath(const ConcreteClusterPath & path) const;
+private:
+    bool ContainsClusterPath(const ConcreteClusterPath & path) const;
 
     ServerClusterContext * mContext = nullptr;
     const std::vector<ConcreteClusterPath> mPaths;
