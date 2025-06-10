9ed28cb2136b1dbb9b4226f31b68832a7ca1dd48
Andrei Litvin <andreilitvin@google.com>
2025-06-10 10:33:13 -0400

    one more typo

--- src/app/clusters/administrator-commissioning-server/CodegenIntegration.cpp
+++ src/app/clusters/administrator-commissioning-server/CodegenIntegration.cpp
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include <app/clusters/administrator-commissioning-server/AdministratorCommimssioningCluster.h>
+#include <app/clusters/administrator-commissioning-server/AdministratorCommissioningCluster.h>
 #include <app/static-cluster-config/AdministratorCommissioning.h>
 #include <data-model-providers/codegen/CodegenDataModelProvider.h>
 
