a69d075d567a801e2015b2bcd12e7c72aee95e4e
erwinpan1 <erwinpan1@gmail.com>
2025-06-10 22:27:59 +0800

    restyle

--- examples/chef/common/clusters/switch/SwitchManager.cpp
+++ examples/chef/common/clusters/switch/SwitchManager.cpp
@@ -153,7 +153,6 @@ static SwitchActionsDelegate * gSwitchActionsDelegate = new SwitchActionsDelegat
 
 #endif // CONFIG_ENABLE_PW_RPC
 
-
 const Clusters::Descriptor::Structs::SemanticTagStruct::Type gLatchingSwitch[] = {
     { .namespaceID = kNamespaceCommonLevel,
       .tag         = kTagCommonLow,
