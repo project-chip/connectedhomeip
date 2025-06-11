2eb89cb9238b3ddf4e87984a65a1b55012c71bf8
Andrei Litvin <andy314@gmail.com>
2025-06-11 13:16:21 -0400

    Add a stack lock for AddDeviceEndpoint

--- examples/fabric-bridge-app/linux/RpcServer.cpp
+++ examples/fabric-bridge-app/linux/RpcServer.cpp
@@ -117,12 +117,15 @@ pw::Status FabricBridge::AddSynchronizedDevice(const chip_rpc_SynchronizedDevice
     device->SetBridgedAttributes(attributes);
     device->SetIcd(request.has_is_icd && request.is_icd);
 
-    auto result = BridgedDeviceManager::Instance().AddDeviceEndpoint(std::move(device), 1 /* parentEndpointId */);
-    if (!result.has_value())
     {
-        ChipLogError(NotSpecified, "Failed to add device with Id=[%d:0x" ChipLogFormatX64 "]", scopedNodeId.GetFabricIndex(),
-                     ChipLogValueX64(scopedNodeId.GetNodeId()));
-        return pw::Status::Unknown();
+        DeviceLayer::StackLock lock;
+        auto result = BridgedDeviceManager::Instance().AddDeviceEndpoint(std::move(device), 1 /* parentEndpointId */);
+        if (!result.has_value())
+        {
+            ChipLogError(NotSpecified, "Failed to add device with Id=[%d:0x" ChipLogFormatX64 "]", scopedNodeId.GetFabricIndex(),
+                         ChipLogValueX64(scopedNodeId.GetNodeId()));
+            return pw::Status::Unknown();
+        }
     }
 
     BridgedDevice * addedDevice = BridgedDeviceManager::Instance().GetDeviceByScopedNodeId(scopedNodeId);
