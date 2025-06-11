e2bd0dfd1dbd1e33041ac27a5e8ec53e5f0d71df
tianfeng.yang <tianfeng.yang@tuya.com>
2025-06-11 21:33:44 +0800

    bugs fix

--- src/controller/python/chip/clusters/Attribute.py
+++ src/controller/python/chip/clusters/Attribute.py
@@ -567,7 +567,7 @@ class SubscriptionTransaction:
     def SetSubscriptionStillActiveCallback(self, callback: Callable[[SubscriptionTransaction], None]):
         '''Sets the callback function for the subscription still active notification.'''
         if callback is not None:
-            self._OnSubscriptionStillActiveCallback = callback
+            self._onSubscriptionStillActiveCallback = callback
 
     @property
     def OnAttributeChangeCb(self) -> Callable[[TypedAttributePath, SubscriptionTransaction], None]:
@@ -587,7 +587,7 @@ class SubscriptionTransaction:
 
     @property
     def OnSubscriptionStillActiveCb(self) -> Callable[[SubscriptionTransaction], None]:
-        return self._OnSubscriptionStillActiveCallback
+        return self._onSubscriptionStillActiveCallback
 
     def Shutdown(self):
         if self._isDone:
