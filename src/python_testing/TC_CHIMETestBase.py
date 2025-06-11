7ad781e61a8f481413d7ddbaf5efa10b3f1f84e2
m.trayer <m.trayer@samsung.com>
2025-06-10 20:02:16 -0500

    Handle linter errors

--- src/python_testing/TC_CHIMETestBase.py
+++ src/python_testing/TC_CHIMETestBase.py
@@ -31,11 +31,9 @@ class CHIMETestBase:
         return await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=attribute)
 
     async def write_chime_attribute_expect_success(self, endpoint, attribute, value):
-        cluster = Clusters.Objects.Chime
         return await self.write_single_attribute(attribute_value=attribute(value), endpoint_id=endpoint)
 
     async def write_chime_attribute_expect_failure(self, endpoint, attribute, value, status):
-        cluster = Clusters.Objects.Chime
         response = await self.write_single_attribute(attribute_value=attribute(value), endpoint_id=endpoint, expect_success=False)
 
         asserts.assert_equal(response, status, "Unexpected error returned")
