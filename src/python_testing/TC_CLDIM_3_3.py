07be5e73eb687de8a56cadfa3ddca072e6ec7e90
Restyled.io <commits@restyled.io>
2025-06-20 09:17:47 +0000

    Restyled by isort

--- src/python_testing/TC_CLDIM_3_3.py
+++ src/python_testing/TC_CLDIM_3_3.py
@@ -35,11 +35,12 @@
 # === END CI TEST ARGUMENTS ===
 
 import logging
+
 import chip.clusters as Clusters
 from chip.clusters import Globals
-from chip.interaction_model import (InteractionModelError, Status)
-from chip.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body,
-                                         default_matter_test_main, AttributeMatcher, AttributeValue, ClusterAttributeChangeAccumulator)
+from chip.interaction_model import InteractionModelError, Status
+from chip.testing.matter_testing import (AttributeMatcher, AttributeValue, ClusterAttributeChangeAccumulator, MatterBaseTest,
+                                         TestStep, async_test_body, default_matter_test_main)
 from mobly import asserts
 
 
