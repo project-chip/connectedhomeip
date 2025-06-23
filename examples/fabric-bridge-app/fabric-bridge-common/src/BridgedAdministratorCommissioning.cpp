370db3d875b53183175c8678935bb976e8566860
Jenny Gallegos <72410844+jennygaz@users.noreply.github.com>
2025-06-23 10:26:04 -0600

    Update BridgedAdministratorCommissioning due to conflicts
    Roll back the comment changes

--- examples/fabric-bridge-app/fabric-bridge-common/src/BridgedAdministratorCommissioning.cpp
+++ examples/fabric-bridge-app/fabric-bridge-common/src/BridgedAdministratorCommissioning.cpp
@@ -30,7 +30,7 @@ namespace bridge {
 
 CHIP_ERROR BridgedAdministratorCommissioning::Init()
 {
-    // We expect initialization after `emberAfInit`. This allows us to unregister the existing
+    // We expect initialization after emberAfInit(). This allows us to unregister the existing
     // AccessAttributeInterface for AdministratorCommissioning and register ourselves, ensuring we
     // get the callback for reading attribute. If the read is not intended for a bridged device we will
     // forward it to the original attribute interface that we are unregistering.
