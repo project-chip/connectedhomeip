e735775e65e51a29a42c9f28fbdbd9474e0442bc
Andrei Litvin <andreilitvin@google.com>
2025-06-24 15:31:40 -0400

    Fix includes

--- src/app/util/attribute-storage.cpp
+++ src/app/util/attribute-storage.cpp
@@ -14,8 +14,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
-#include "app/util/attribute-metadata.h"
 #include <app/util/attribute-storage.h>
 
 #include <app/util/attribute-storage-detail.h>
