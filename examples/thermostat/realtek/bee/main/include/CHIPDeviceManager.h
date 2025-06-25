76b4aef59a12d521885a62d192f5dd1db803fde6
pankore <86098180+pankore@users.noreply.github.com>
2025-06-25 13:36:52 +0800

    [Realtek] fix bugs

--- examples/thermostat/realtek/bee/main/include/CHIPDeviceManager.h
+++ examples/thermostat/realtek/bee/main/include/CHIPDeviceManager.h
@@ -117,7 +117,7 @@ public:
     static void CommonDeviceEventHandler(const chip::DeviceLayer::ChipDeviceEvent * event, intptr_t arg);
 
 private:
-    mIsInitDone                      = false;
+    bool mIsInitDone                 = false;
     CHIPDeviceManagerCallbacks * mCB = nullptr;
     CHIPDeviceManager() {}
 };
