e518a562372e9ac14a89ed227f31940a2e7c7576
pankore <86098180+pankore@users.noreply.github.com>
2025-06-25 09:14:42 +0800

    [Realtek] Add software-string-version in DeviceInfo

--- src/platform/realtek/BEE/FactoryDataProvider.cpp
+++ src/platform/realtek/BEE/FactoryDataProvider.cpp
@@ -637,6 +637,16 @@ exit:
     return err;
 }
 
+CHIP_ERROR FactoryDataProvider::GetSoftwareVersionString(char * buf, size_t bufSize)
+{
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    VerifyOrReturnError(bufSize >= sizeof(CHIP_DEVICE_CONFIG_DEVICE_SOFTWARE_VERSION_STRING), CHIP_ERROR_BUFFER_TOO_SMALL);
+    strcpy(buf, CHIP_DEVICE_CONFIG_DEVICE_SOFTWARE_VERSION_STRING);
+
+    return err;
+}
+
 CHIP_ERROR FactoryDataProvider::GetHardwareVersion(uint16_t & hardwareVersion)
 {
     CHIP_ERROR err = CHIP_DEVICE_ERROR_CONFIG_NOT_FOUND;
