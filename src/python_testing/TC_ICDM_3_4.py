b2eee576f07f22ba9befb405709559e6d5f095ce
juandediosg <jdelgado@griddynamics.com>
2025-06-25 13:32:16 -0600

    Fixed: Restyled errors on TC_ICDM_3_4

--- src/python_testing/TC_ICDM_3_4.py
+++ src/python_testing/TC_ICDM_3_4.py
@@ -40,8 +40,8 @@ import logging
 import time
 
 import chip.clusters as Clusters
-from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, default_matter_test_main
 from chip.testing.matter_stack_state import MatterStackState
+from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 logger = logging.getLogger(__name__)
