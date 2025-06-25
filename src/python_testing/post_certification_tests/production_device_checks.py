bb96144e693a06803d224882219d21b53eb0b830
juandediosg <jdelgado@griddynamics.com>
2025-06-25 13:34:01 -0600

    Fixed: Restyled errors on production_device_checks.py

--- src/python_testing/post_certification_tests/production_device_checks.py
+++ src/python_testing/post_certification_tests/production_device_checks.py
@@ -61,14 +61,14 @@ DEFAULT_CHIP_ROOT = os.path.abspath(
 
 try:
     from chip.testing.basic_composition import BasicCompositionTests
-    from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, run_tests_no_exit
     from chip.testing.matter_stack_state import MatterStackState
+    from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, run_tests_no_exit
 except ImportError:
     sys.path.append(os.path.abspath(
         os.path.join(os.path.dirname(__file__), '..')))
     from chip.testing.basic_composition import BasicCompositionTests
-    from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, run_tests_no_exit
     from chip.testing.matter_stack_state import MatterStackState
+    from chip.testing.matter_testing import MatterBaseTest, MatterTestConfig, TestStep, async_test_body, run_tests_no_exit
 
 try:
     import fetch_paa_certs_from_dcl
