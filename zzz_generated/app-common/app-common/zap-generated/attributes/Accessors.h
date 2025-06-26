d791b38691b9716357d47c286b3b9e068b123a6b
Yufeng Wang <yufengwang@google.com>
2025-06-25 23:49:46 -0700

    Rerun codegen

--- zzz_generated/app-common/app-common/zap-generated/attributes/Accessors.h
+++ zzz_generated/app-common/app-common/zap-generated/attributes/Accessors.h
@@ -958,21 +958,7 @@ Protocols::InteractionModel::Status Set(EndpointId endpoint, uint16_t value, Mar
 } // namespace WiFiNetworkDiagnostics
 
 namespace EthernetNetworkDiagnostics {
-namespace Attributes {
-
-namespace FeatureMap {
-Protocols::InteractionModel::Status Get(EndpointId endpoint, uint32_t * value); // bitmap32
-Protocols::InteractionModel::Status Set(EndpointId endpoint, uint32_t value);
-Protocols::InteractionModel::Status Set(EndpointId endpoint, uint32_t value, MarkAttributeDirty markDirty);
-} // namespace FeatureMap
-
-namespace ClusterRevision {
-Protocols::InteractionModel::Status Get(EndpointId endpoint, uint16_t * value); // int16u
-Protocols::InteractionModel::Status Set(EndpointId endpoint, uint16_t value);
-Protocols::InteractionModel::Status Set(EndpointId endpoint, uint16_t value, MarkAttributeDirty markDirty);
-} // namespace ClusterRevision
-
-} // namespace Attributes
+namespace Attributes {} // namespace Attributes
 } // namespace EthernetNetworkDiagnostics
 
 namespace TimeSynchronization {
