4b3d022be32cf46b81f7189d167e5687feb3a05e
dsavitsky-dsr <dmitry.savitskiy@dsr-corporation.com>
2025-07-01 11:52:01 +0300

    Code style update

--- examples/energy-gateway-app/commodity-metering/include/CommodityMeteringInstance.h
+++ examples/energy-gateway-app/commodity-metering/include/CommodityMeteringInstance.h
@@ -15,7 +15,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
- 
+
 #pragma once
 
 #include <app/clusters/commodity-metering-server/commodity-metering-server.h>
