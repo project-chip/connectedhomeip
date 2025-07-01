25fa257c040667d3e3f1eba31cb0b832f58418b5
Restyled.io <commits@restyled.io>
2025-07-01 16:13:13 +0000

    Restyled by clang-format

--- src/controller/python/chip/webrtc/RTCClient.cpp
+++ src/controller/python/chip/webrtc/RTCClient.cpp
@@ -65,6 +65,6 @@ void pychip_webrtc_client_set_local_description_callback(void * client, LocalDes
 
 void pychip_webrtc_client_set_ice_candidate_callback(void * client, IceCandidateCallback cb, void * user_data)
 {
-    chip::webrtc:: webrtc_client_set_ice_candidate_callback(client, cb, user_data);
+    chip::webrtc::webrtc_client_set_ice_candidate_callback(client, cb, user_data);
 }
 }
