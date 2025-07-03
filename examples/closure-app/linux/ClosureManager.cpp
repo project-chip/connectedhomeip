e051a29aec1598c9236c32e419a7fcf709733a95
Restyled.io <commits@restyled.io>
2025-07-03 09:10:46 +0000

    Restyled by clang-format

--- examples/closure-app/linux/ClosureManager.cpp
+++ examples/closure-app/linux/ClosureManager.cpp
@@ -142,8 +142,8 @@ CHIP_ERROR ClosureManager::SetClosurePanelInitialState(ClosureDimensionEndpoint 
 {
     ChipLogProgress(AppServer, "ClosurePanelEndpoint SetInitialState");
     DataModel::Nullable<GenericDimensionStateStruct> currentState(
-        GenericDimensionStateStruct(MakeOptional(DataModel::MakeNullable<Percent100ths>(10000)), MakeOptional(DataModel::MakeNullable(true)),
-                                    MakeOptional(Globals::ThreeLevelAutoEnum::kAuto)));
+        GenericDimensionStateStruct(MakeOptional(DataModel::MakeNullable<Percent100ths>(10000)),
+                                    MakeOptional(DataModel::MakeNullable(true)), MakeOptional(Globals::ThreeLevelAutoEnum::kAuto)));
     ReturnErrorOnFailure(closurePanelEndpoint.GetLogic().SetCurrentState(currentState));
 
     DataModel::Nullable<GenericDimensionStateStruct> targetState(
