0ddc3dd33b9d2a5d033c633a83b286fee86169e2
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-07-06 22:17:19 -0700

    level control and toggle tests

--- examples/chef/tests/dimmablelight_test.py
+++ examples/chef/tests/dimmablelight_test.py
@@ -17,6 +17,7 @@
 
 import logging
 
+import chip.clusters as Clusters
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
@@ -26,17 +27,60 @@ logger = logging.getLogger(__name__)
 class TC_DIMMABLELIGHT(MatterBaseTest):
     """Tests for chef dimmablelight device."""
 
+    _DIMMABLELIGHT_ENDPOINT = 1
+    _MAX_BRIGHTNESS_LEVEL = 254
+    _MID_BRIGHTNESS_LEVEL = 127
+    _MIN_BRIGHTNESS_LEVEL = 1
+
+    async def _read_on_off(self):
+        return await self.read_single_attribute_check_success(
+            endpoint=self._PUMP_ENDPOINT, cluster=Clusters.Objects.OnOff, attribute=Clusters.Objects.OnOff.Attributes.OnOff)
+
+    async def _read_current_level(self):
+        return await self.read_single_attribute_check_success(
+            endpoint=self._PUMP_ENDPOINT, cluster=Clusters.Objects.LevelControl, attribute=Clusters.Objects.LevelControl.Attributes.CurrentLevel)
+
     def desc_TC_DIMMABLELIGHT(self) -> str:
         return "[TC_DIMMABLELIGHT] chef dimmablelight functionality test."
 
     def steps_TC_DIMMABLELIGHT(self):
-        return [TestStep(1, "[TC_DIMMABLELIGHT] Commissioning already done.", is_commissioning=True)]
+        return [TestStep(1, "[TC_DIMMABLELIGHT] Commissioning already done.", is_commissioning=True),
+                TestStep(2, "[TC_DIMMABLELIGHT] Test level control."),
+                TestStep(3, "[TC_DIMMABLELIGHT] Test toggle.")]
 
     @async_test_body
     async def test_TC_DIMMABLELIGHT(self):
-        # *** STEP 1 ***
-        # Commissioning already done.
+        """Run all steps."""
+
         self.step(1)
+        # Commissioning already done.
+
+        self.step(2)
+        await self.send_single_cmd(
+            cmd=Clusters.Objects.OnOff.Commands.On(),
+            dev_ctrl=self.default_controller,
+            node_id=self.dut_node_id,
+            endpoint=self._DIMMABLELIGHT_ENDPOINT,
+        )
+        asserts.assert_equal(await self._read_on_off(), True)
+        for level in [self._MIN_BRIGHTNESS_LEVEL, self._MID_BRIGHTNESS_LEVEL, self._MAX_BRIGHTNESS_LEVEL]:
+            await self.send_single_cmd(
+                cmd=Clusters.Objects.LevelControl.Commands.MoveToLevel(level=level),
+                dev_ctrl=self.default_controller,
+                node_id=self.dut_node_id,
+                endpoint=self._DIMMABLELIGHT_ENDPOINT,
+            )
+            asserts.assert_equal(await self._read_current_level(), level)
+
+        self.step(3)
+        before = await self._read_on_off()
+        await self.send_single_cmd(
+            cmd=Clusters.Objects.OnOff.Commands.Toggle(),
+            dev_ctrl=self.default_controller,
+            node_id=self.dut_node_id,
+            endpoint=self._DIMMABLELIGHT_ENDPOINT,
+        )
+        asserts.assert_equal(await self._read_on_off(), not before)
 
 
 if __name__ == "__main__":
