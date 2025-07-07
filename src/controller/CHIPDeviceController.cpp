project-chip/connectedhomeip

commit 0657d7d4e34bb285461310d5c39518f594ea5e21

[E2E][JF] Add jcm/TrustedIcacPublicKeyB parameters for OwnershipTransferContext (#39243)
jcm boolean signals if the ownership transfer from jfc to jfa is for a regular
commissioning session or for the joint commissioning method.

if jcm is true, TrustedIcacPublicKeyB (see spec around its usage) is also transferred.

Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

src/controller/CHIPDeviceController.cpp
@@ -2045,7 +2045,13 @@ void DeviceCommissioner::SendCommissioningCompleteCallbacks(NodeId nodeId, const
         return;
     }
 
-    mPairingDelegate->OnCommissioningComplete(nodeId, completionStatus.err);
+#if CHIP_DEVICE_CONFIG_ENABLE_JOINT_FABRIC
+    mPairingDelegate->OnCommissioningComplete(nodeId, mTrustedIcacPublicKeyB, completionStatus.err);
+    mTrustedIcacPublicKeyB.ClearValue();
+#else
+    mPairingDelegate->OnCommissioningComplete(nodeId, NullOptional, completionStatus.err);
+#endif
+
     PeerId peerId(GetCompressedFabricId(), nodeId);
     if (completionStatus.err == CHIP_NO_ERROR)
     {
@@ -3568,17 +3574,32 @@ void DeviceCommissioner::PerformCommissioningStep(DeviceProxy * proxy, Commissio
             return;
         }
 
-        CHIP_ERROR err = ValidateJFAdminNOC(params.GetJFAdminNOC().Value());
+        P256PublicKeySpan jfAdminICACPKSpan;
+        CHIP_ERROR err = CHIP_NO_ERROR;
+
+        /* check the peer Admin NOC */
+        err = ValidateJFAdminNOC(params.GetJFAdminNOC().Value());
 
         if (err != CHIP_NO_ERROR)
         {
             ChipLogError(Controller, "JCM: Cannot validate JFAdminNOC");
+            CommissioningStageComplete(err);
+            return;
         }
 
-        CommissioningStageComplete(err);
+        /* extract and save the public key of the peer Admin ICAC */
+        err = ExtractPublicKeyFromChipCert(params.GetJFAdminICAC().Value(), jfAdminICACPKSpan);
+        if (err != CHIP_NO_ERROR)
+        {
+            ChipLogError(Controller, "JCM: Error parsing JFAdminICAC Public Key");
+            CommissioningStageComplete(err);
+            return;
+        }
+        mTrustedIcacPublicKeyB.Emplace(jfAdminICACPKSpan);
 
-        break;
+        CommissioningStageComplete(err);
     }
+    break;
 #endif
 
     case CommissioningStage::kSendVIDVerificationRequest:

examples/common/pigweed/protos/joint_fabric_service.options
@@ -1,0 +1,1 @@
+OwnershipContext.trustedIcacPublicKeyB max_size:65    // Maximum size of trustedIcacPublicKeyB

examples/common/pigweed/protos/joint_fabric_service.proto
@@ -19,7 +19,9 @@ syntax = "proto3";
 import 'pw_protobuf_protos/common.proto';
 
 message OwnershipContext {
-  uint64 node_id = 1;
+  uint64 node_id = 1; // jf-admin-app should finalize commissioning with this specific node_id
+  bool jcm = 2; // should be set to true if jf-admin-app has to finalize the jcm process
+  bytes trustedIcacPublicKeyB = 3; // see spec., JCM steps
 }
 
 service JointFabric {

examples/common/pigweed/rpc_services/JointFabric.h
@@ -1,9 +1,11 @@
 #include "JFAManager.h"
 #include "joint_fabric_service/joint_fabric_service.rpc.pb.h"
 
+#include <crypto/CHIPCryptoPAL.h>
 #include <platform/CHIPDeviceLayer.h>
 
 using namespace chip;
+using namespace chip::Crypto;
 
 namespace joint_fabric_service {
 
@@ -15,15 +17,23 @@ public:
 private:
     struct OwnershipTransferContext
     {
-        OwnershipTransferContext(uint64_t nodeId) : mNodeId(nodeId) {}
+        OwnershipTransferContext(uint64_t nodeId, bool jcm, ByteSpan trustedIcacPublicKeyB) : mNodeId(nodeId), mJCM(jcm)
+        {
+            memcpy(keyRawBytes, trustedIcacPublicKeyB.data(), kP256_PublicKey_Length);
+            mTrustedIcacPublicKeyBSerialized = keyRawBytes;
+        }
 
         uint64_t mNodeId;
+        bool mJCM;
+
+        uint8_t keyRawBytes[kP256_PublicKey_Length] = { 0 };
+        P256PublicKey mTrustedIcacPublicKeyBSerialized;
     };
 
     static void FinalizeCommissioningWork(intptr_t arg)
     {
         OwnershipTransferContext * data = reinterpret_cast<OwnershipTransferContext *>(arg);
-        JFAMgr().FinalizeCommissioning(data->mNodeId);
+        JFAMgr().FinalizeCommissioning(data->mNodeId, data->mJCM, data->mTrustedIcacPublicKeyBSerialized);
         Platform::Delete(data);
     }
 };

examples/jf-admin-app/linux/JFAManager.cpp
@@ -30,6 +30,7 @@ using namespace chip;
 using namespace chip::app;
 using namespace chip::app::Clusters;
 using namespace chip::Controller;
+using namespace chip::Crypto;
 
 JFAManager JFAManager::sJFA;
 
@@ -41,12 +42,15 @@ CHIP_ERROR JFAManager::Init(Server & server)
     return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR JFAManager::FinalizeCommissioning(NodeId nodeId)
+CHIP_ERROR JFAManager::FinalizeCommissioning(NodeId nodeId, bool isJCM, P256PublicKey & trustedIcacPublicKeyB)
 {
     if (jfFabricIndex == kUndefinedFabricId)
     {
         return CHIP_ERROR_INCORRECT_STATE;
     }
+
+    ChipLogProgress(JointFabric, "FinalizeCommissioning for NodeID: 0x" ChipLogFormatX64 ", isJCM = %d", ChipLogValueX64(nodeId),
+                    isJCM);
 
     ScopedNodeId scopedNodeId = ScopedNodeId(nodeId, jfFabricIndex);
 

examples/jf-admin-app/linux/include/JFAManager.h
@@ -36,7 +36,7 @@ public:
 
     CHIP_ERROR Init(Server & server);
     void HandleCommissioningCompleteEvent();
-    CHIP_ERROR FinalizeCommissioning(NodeId nodeId);
+    CHIP_ERROR FinalizeCommissioning(NodeId nodeId, bool isJCM, chip::Crypto::P256PublicKey & trustedIcacPublicKeyB);
 
 private:
     // Various actions to take when OnConnected callback is called

examples/jf-admin-app/linux/rpc/RpcServer.cpp
@@ -10,9 +10,22 @@ namespace joint_fabric_service {
 
 ::pw::Status JointFabric::TransferOwnership(const ::OwnershipContext & request, ::pw_protobuf_Empty & response)
 {
-    ChipLogProgress(chipTool, "Ownership Transfer for NodeId: " ChipLogFormatX64, ChipLogValueX64(request.node_id));
+    ChipLogProgress(JointFabric, "Ownership Transfer for NodeId: 0x" ChipLogFormatX64 ", jcm=%d", ChipLogValueX64(request.node_id),
+                    request.jcm);
 
-    OwnershipTransferContext * data = Platform::New<OwnershipTransferContext>(request.node_id);
+    if (request.jcm && (Crypto::kP256_PublicKey_Length != request.trustedIcacPublicKeyB.size))
+    {
+        return pw::Status::OutOfRange();
+        ChipLogError(JointFabric, "Invalid ICAC Public Key Size");
+    }
+
+    for (size_t i = 0; i < Crypto::kP256_PublicKey_Length; ++i)
+    {
+        ChipLogProgress(JointFabric, "trustedIcacPublicKeyB[%li] = %02X", i, request.trustedIcacPublicKeyB.bytes[i]);
+    }
+
+    OwnershipTransferContext * data = Platform::New<OwnershipTransferContext>(
+        request.node_id, request.jcm, ByteSpan(request.trustedIcacPublicKeyB.bytes, request.trustedIcacPublicKeyB.size));
     VerifyOrReturnValue(data, pw::Status::Internal());
     DeviceLayer::PlatformMgr().ScheduleWork(FinalizeCommissioningWork, reinterpret_cast<intptr_t>(data));
 

examples/jf-control-app/commands/pairing/PairingCommand.cpp
@@ -524,7 +524,8 @@ void OnOwnershipTransferDone(const _pw_protobuf_Empty & response, ::pw::Status s
 
 } // namespace
 
-void PairingCommand::OnCommissioningComplete(NodeId nodeId, CHIP_ERROR err)
+void PairingCommand::OnCommissioningComplete(NodeId nodeId, const Optional<Crypto::P256PublicKey> & trustedIcacPublicKeyB,
+                                             CHIP_ERROR err)
 {
     if (err == CHIP_NO_ERROR)
     {
@@ -539,6 +540,31 @@ void PairingCommand::OnCommissioningComplete(NodeId nodeId, CHIP_ERROR err)
 
             memset(&request, 0, sizeof(request));
             request.node_id = nodeId;
+            request.jcm     = false;
+
+            if (mExecuteJCM.ValueOr(false))
+            {
+                request.jcm = true;
+
+                if (trustedIcacPublicKeyB.HasValue())
+                {
+                    memcpy(request.trustedIcacPublicKeyB.bytes, trustedIcacPublicKeyB.Value().ConstBytes(),
+                           Crypto::kP256_PublicKey_Length);
+                    request.trustedIcacPublicKeyB.size = Crypto::kP256_PublicKey_Length;
+
+                    for (size_t i = 0; i < Crypto::kP256_PublicKey_Length; ++i)
+                    {
+                        ChipLogProgress(JointFabric, "trustedIcacPublicKeyB[%li] = %02X", i,
+                                        request.trustedIcacPublicKeyB.bytes[i]);
+                    }
+                }
+                else
+                {
+                    SetCommandExitStatus(CHIP_ERROR_INVALID_ARGUMENT);
+                    ChipLogError(chipTool, "JCM requested but peer Admin ICAC not found");
+                    return;
+                }
+            }
 
             auto call = rpcClient.TransferOwnership(request, OnOwnershipTransferDone);
             if (!call.active())

commit f94ba7b087e33a9256e77fdb9d2cd66e7bb88f61

Fix CI/CV issue
Problem introduced by https://github.com/project-chip/connectedhomeip/pull/39243

Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

src/controller/CHIPDeviceController.cpp
@@ -2049,7 +2049,7 @@ void DeviceCommissioner::SendCommissioningCompleteCallbacks(NodeId nodeId, const
     mPairingDelegate->OnCommissioningComplete(nodeId, mTrustedIcacPublicKeyB, completionStatus.err);
     mTrustedIcacPublicKeyB.ClearValue();
 #else
-    mPairingDelegate->OnCommissioningComplete(nodeId, NullOptional, completionStatus.err);
+    mPairingDelegate->OnCommissioningComplete(nodeId, completionStatus.err);
 #endif
 
     PeerId peerId(GetCompressedFabricId(), nodeId);