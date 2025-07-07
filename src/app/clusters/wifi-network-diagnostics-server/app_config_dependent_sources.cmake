3ae6d45b2710c8432051cd1479eedc019b61a524
Rohit <rohit.jadhav@espressif.com>
2025-07-07 12:54:47 +0530

    Decouple wifi network diagnostics cluster.

--- src/app/clusters/wifi-network-diagnostics-server/app_config_dependent_sources.cmake
+++ src/app/clusters/wifi-network-diagnostics-server/app_config_dependent_sources.cmake
@@ -16,6 +16,13 @@
 TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
+    "${CLUSTER_DIR}/CodegenIntegration.cpp"
+)
+
+# These are the things that BUILD.gn dependencies would pull
+TARGET_SOURCES(
+  ${APP_TARGET}
+  PRIVATE
     "${CLUSTER_DIR}/WiFiDiagnosticsTestEventTriggerHandler.h"
     "${CLUSTER_DIR}/wifi-network-diagnostics-server.cpp"
     "${CLUSTER_DIR}/wifi-network-diagnostics-server.h"
