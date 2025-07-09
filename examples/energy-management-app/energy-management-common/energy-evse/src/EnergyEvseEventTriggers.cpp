335d6e6d5a0bd3e5e126b0b19a8063682167933f
jamesharrow <93921463+jamesharrow@users.noreply.github.com>
2025-07-09 13:22:18 +0100

    Update examples/energy-management-app/energy-management-common/energy-evse/src/EnergyEvseEventTriggers.cpp
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- examples/energy-management-app/energy-management-common/energy-evse/src/EnergyEvseEventTriggers.cpp
+++ examples/energy-management-app/energy-management-common/energy-evse/src/EnergyEvseEventTriggers.cpp
@@ -169,7 +169,7 @@ void SetTestEventTrigger_EVSETriggerRFID()
     EnergyEvseDelegate * dg = GetEvseDelegate();
 
     uint8_t rfidData[10] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99 };
-    chip::ByteSpan rfidSpan(rfidData, sizeof(rfidData));
+    ByteSpan rfidSpan(rfidData);
     dg->HwSetRFID(rfidSpan);
 }
 
