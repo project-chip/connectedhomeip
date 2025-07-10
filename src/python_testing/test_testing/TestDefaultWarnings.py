5f25027ee5f88a25792c8a4dda052d4803ac1a7e
jtrejoespinoza-grid <juanerasmo@google.com>
2025-07-10 10:50:27 -0700

    Refactor class DefaultChecker into its own file on test_testing as is a dependency for two Tests

--- src/python_testing/test_testing/TestDefaultWarnings.py
+++ src/python_testing/test_testing/TestDefaultWarnings.py
@@ -18,16 +18,16 @@
 from typing import Callable
 
 import chip.clusters as Clusters
-import TC_DefaultWarnings
+import DefaultChecker as DefaultChecker
 from chip.testing.matter_testing import MatterBaseTest, default_matter_test_main
 from mobly import asserts
 
 
-class TestDefaultChecker(MatterBaseTest):
+class DefaultChecker(MatterBaseTest):
     ''' This is a unit-style test of the defaults test. It runs the individual test functions against mocks.'''
 
     def setup_test(self):
-        self.test = TC_DefaultWarnings.DefaultChecker()
+        self.test = DefaultChecker.DefaultChecker()
         self.test.mark_current_step_skipped = self.mark_current_step_skipped
         self.skipped = 0
         super().setup_test()
@@ -38,7 +38,7 @@ class TestDefaultChecker(MatterBaseTest):
 
     def test_product_name_is_not_default_check(self):
         def run_check(product_name: str, set_override: bool, expect_problem: bool):
-            self.test.user_params = {TC_DefaultWarnings.FLAG_PRODUCT_NAME: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_PRODUCT_NAME: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.BasicInformation: {Clusters.BasicInformation.Attributes.ProductName: product_name}}}
             self.test.check_default_product_name()
@@ -62,7 +62,7 @@ class TestDefaultChecker(MatterBaseTest):
 
     def test_vendor_name_is_not_default_check(self):
         def run_check(vendor_name: str, set_override: bool, expect_problem: bool):
-            self.test.user_params = {TC_DefaultWarnings.FLAG_VENDOR_NAME: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_VENDOR_NAME: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.BasicInformation: {Clusters.BasicInformation.Attributes.VendorName: vendor_name}}}
             self.test.check_default_vendor_name()
@@ -86,7 +86,7 @@ class TestDefaultChecker(MatterBaseTest):
 
     def test_vendor_id_is_not_default_check(self):
         def run_check(vendor_id: int, set_override: bool, expect_problem: bool):
-            self.test.user_params = {TC_DefaultWarnings.FLAG_VENDOR_ID: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_VENDOR_ID: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.BasicInformation: {Clusters.BasicInformation.Attributes.VendorID: vendor_id}}}
             self.test.check_default_vendor_id()
@@ -112,7 +112,7 @@ class TestDefaultChecker(MatterBaseTest):
         enum = Clusters.TimeFormatLocalization.Enums.CalendarTypeEnum
 
         def run_check(calendar_type: Clusters.TimeFormatLocalization.Enums.CalendarTypeEnum, set_override: bool, expect_problem: bool):
-            self.test.user_params = {TC_DefaultWarnings.FLAG_DEFAULT_CALENDAR_FORMAT: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_DEFAULT_CALENDAR_FORMAT: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.TimeFormatLocalization: {
                 Clusters.TimeFormatLocalization.Attributes.ActiveCalendarType: calendar_type}}}
@@ -144,7 +144,7 @@ class TestDefaultChecker(MatterBaseTest):
 
         # Flag should also work here
         self.skipped = 0
-        self.test.user_params = {TC_DefaultWarnings.FLAG_DEFAULT_CALENDAR_FORMAT: True}
+        self.test.user_params = {DefaultChecker.FLAG_DEFAULT_CALENDAR_FORMAT: True}
         self.test.check_default_calendar_format()
         asserts.assert_equal(len(self.test.problems), 0,
                              "Unexpected problem when testing device with no TimeFormatLocalization cluster and override")
@@ -164,7 +164,7 @@ class TestDefaultChecker(MatterBaseTest):
 
         # Flag should also work here
         self.skipped = 0
-        self.test.user_params = {TC_DefaultWarnings.FLAG_DEFAULT_CALENDAR_FORMAT: True}
+        self.test.user_params = {DefaultChecker.FLAG_DEFAULT_CALENDAR_FORMAT: True}
         self.test.check_default_calendar_format()
         asserts.assert_equal(len(self.test.problems), 0,
                              "Unexpected problem when testing device with no ActiveCalendarType cluster and override")
@@ -213,19 +213,19 @@ class TestDefaultChecker(MatterBaseTest):
 
     def test_unit_testing_is_not_present_check(self):
         self._cluster_presence_test(Clusters.UnitTesting, self.test.check_unit_testing_cluster_presence,
-                                    TC_DefaultWarnings.FLAG_UNIT_TESTING)
+                                    DefaultChecker.FLAG_UNIT_TESTING)
 
     def test_fault_injection_is_not_present_check(self):
         self._cluster_presence_test(Clusters.FaultInjection, self.test.check_fault_injection_cluster_presence,
-                                    TC_DefaultWarnings.FLAG_FAULT_INJECTION)
+                                    DefaultChecker.FLAG_FAULT_INJECTION)
 
     def test_sample_mei_is_not_present_check(self):
         self._cluster_presence_test(Clusters.SampleMei, self.test.check_sample_mei_cluster_presence,
-                                    TC_DefaultWarnings.FLAG_SAMPLE_MEI)
+                                    DefaultChecker.FLAG_SAMPLE_MEI)
 
     def test_fixed_label_is_not_empty_check(self):
         def run_check(label_list: list[Clusters.FixedLabel.Structs.LabelStruct], set_override: bool, expect_problem: bool):
-            self.test.user_params = {TC_DefaultWarnings.FLAG_FIXED_LABEL_EMPTY: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_FIXED_LABEL_EMPTY: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.FixedLabel: {Clusters.FixedLabel.Attributes.LabelList: label_list}}}
             self.test.check_fixed_label_cluster_empty()
@@ -256,7 +256,7 @@ class TestDefaultChecker(MatterBaseTest):
 
         # Flag should also work here
         self.skipped = 0
-        self.test.user_params = {TC_DefaultWarnings.FLAG_FIXED_LABEL_EMPTY: True}
+        self.test.user_params = {DefaultChecker.FLAG_FIXED_LABEL_EMPTY: True}
         self.test.check_fixed_label_cluster_empty()
         asserts.assert_equal(len(self.test.problems), 0,
                              "Unexpected problem when testing device with no TimeFormatLocalization cluster and override")
@@ -266,7 +266,7 @@ class TestDefaultChecker(MatterBaseTest):
     def test_fixed_label_is_not_default_check(self):
         def run_check(label_list: list[Clusters.FixedLabel.Structs.LabelStruct], set_override: bool, expect_problem: bool):
             self.skipped = 0
-            self.test.user_params = {TC_DefaultWarnings.FLAG_FIXED_LABEL_DEFAULT_VALUES: set_override}
+            self.test.user_params = {DefaultChecker.FLAG_FIXED_LABEL_DEFAULT_VALUES: set_override}
             self.test.problems = []
             self.test.endpoints = {0: {Clusters.FixedLabel: {Clusters.FixedLabel.Attributes.LabelList: label_list}}}
             self.test.check_fixed_label_cluster_defaults()
@@ -280,12 +280,12 @@ class TestDefaultChecker(MatterBaseTest):
                 asserts.assert_equal(self.skipped, 1, "Test with override did not mark steps skipped")
 
         asserts.assert_equal(self.skipped, 0, "Skip not reset properly")
-        for default_label in TC_DefaultWarnings.DEFAULT_FIXED_LABEL_VALUES:
+        for default_label in DefaultChecker.DEFAULT_FIXED_LABEL_VALUES:
             run_check([default_label], set_override=False, expect_problem=True)
             run_check([default_label], set_override=True, expect_problem=False)
 
-        run_check(TC_DefaultWarnings.DEFAULT_FIXED_LABEL_VALUES, set_override=False, expect_problem=True)
-        run_check(TC_DefaultWarnings.DEFAULT_FIXED_LABEL_VALUES, set_override=True, expect_problem=False)
+        run_check(DefaultChecker.DEFAULT_FIXED_LABEL_VALUES, set_override=False, expect_problem=True)
+        run_check(DefaultChecker.DEFAULT_FIXED_LABEL_VALUES, set_override=True, expect_problem=False)
 
         run_check([Clusters.FixedLabel.Structs.LabelStruct("test", "val")], set_override=False, expect_problem=False)
         run_check([Clusters.FixedLabel.Structs.LabelStruct("test", "val")], set_override=True, expect_problem=False)
@@ -303,7 +303,7 @@ class TestDefaultChecker(MatterBaseTest):
 
         # Flag should also work here
         self.skipped = 0
-        self.test.user_params = {TC_DefaultWarnings.FLAG_FIXED_LABEL_DEFAULT_VALUES: True}
+        self.test.user_params = {DefaultChecker.FLAG_FIXED_LABEL_DEFAULT_VALUES: True}
         self.test.check_fixed_label_cluster_defaults()
         asserts.assert_equal(len(self.test.problems), 0,
                              "Unexpected problem when testing device with no FixedLabel cluster and override")
