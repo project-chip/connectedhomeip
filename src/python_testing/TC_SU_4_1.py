eb5c1e4f4e18023d8d69b53cb407417618718ab8
juandediosg <jdelgado@griddynamics.com>
2025-07-11 17:54:57 -0600

    Initial commit with partial implementation.

--- src/python_testing/TC_SU_4_1.py
+++ src/python_testing/TC_SU_4_1.py
@@ -28,10 +28,66 @@
 #       --commissioning-method on-network
 #       --discriminator 1234
 #       --passcode 20202021
-#       --PICS src/app/tests/suites/certification/ci-pics-values
-#       --int-arg PIXIT.CGEN.FailsafeExpiryLengthSeconds:1
+#       --endpoint 1
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #     factory-reset: true
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
+
+import chip.clusters as Clusters
+from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+from mobly import asserts
+import logging
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+
+class TC_SU_4_1(MatterBaseTest):
+    def desc_TC_SU_4_1(self) -> str:
+        return "[TC-SU-4.1] Verifying Cluster Attributes on OTA-R(DUT)"
+
+    def pics_TC_SU_4_1(self):
+        """Return the PICS definitions associated with this test."""
+        pics = [
+            "MCORE.S",      # Pics
+        ]
+        return pics
+
+    def steps_TC_SU_4_1(self) -> list[TestStep]:
+        steps = [
+            TestStep(0, "Commissioning, already done", is_commissioning=True),
+            TestStep(1, """TH sends a write request for the DefaultOTAProviders Attribute on the first fabric to the DUT. 
+                     TH2 is set as the default Provider for the fabric."""),
+            # TestStep(2, "TH..."),
+            # TestStep(3, "TH..."),
+            # TestStep(4, "TH..."),
+            # TestStep(5, "TH..."),
+            # TestStep(6, "TH..."),
+            # TestStep(7, "TH..."),
+            # TestStep(8, "TH..."),
+            # TestStep(9, "TH..."),
+        ]
+        return steps
+
+    @async_test_body
+    async def test_TC_SU_4_1(self):
+        cluster_otap = Clusters.OtaSoftwareUpdateProvider
+        cluster_otar = Clusters.OtaSoftwareUpdateRequestor
+
+        self.step(0)
+
+    # Read the Steps
+        self.step(1)
+
+        # self.step(2)
+        # self.step(3)
+        # self.step(4)
+        # self.step(5)
+        # self.step(6)
+        # self.step(7)
+        # self.step(8)
+        # self.step(9)
+if __name__ == "__main__":
+    default_matter_test_main()
