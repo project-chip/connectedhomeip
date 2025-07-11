9a2dd38ffaaf67bbef86f68032b304884b91686f
Charles Kim <chulspro.kim@samsung.com>
2025-07-11 10:50:01 +0900

    Enable TC WEBRTC 1.1, 1.2, 1.3, 1.4, 1.5 in CI
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>

--- src/python_testing/TC_WEBRTC_1_3.py
+++ src/python_testing/TC_WEBRTC_1_3.py
@@ -13,6 +13,28 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs:
+#   run1:
+#     app: ${CAMERA_APP}
+#     app-args: --camera-deferred-offer --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#     script-args: >
+#       --PICS src/app/tests/suites/certification/ci-pics-values
+#       --storage-path admin_storage.json
+#       --commissioning-method on-network
+#       --discriminator 1234
+#       --passcode 20202021
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#     factory-reset: true
+#     quiet: true
+# === END CI TEST ARGUMENTS ===
+#
+
 import logging
 
 from chip.ChipDeviceCtrl import TransportPayloadCapability
