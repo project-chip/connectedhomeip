02778f8ba4d22dd09af7819c8cfbe775b1d62242
Kiel Oleson <kielo@apple.com>
2025-07-14 09:12:38 -0700

    add app pipe arguments for CI

--- src/python_testing/TC_RVCOPSTATE_2_5.py
+++ src/python_testing/TC_RVCOPSTATE_2_5.py
@@ -19,7 +19,7 @@
 # test-runner-runs:
 #   run1:
 #     app: ${ALL_CLUSTERS_APP}
-#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json --app-pipe /tmp/rvcopstate_2_5_fifo
 #     script-args: >
 #       --storage-path admin_storage.json
 #       --commissioning-method on-network
@@ -28,6 +28,7 @@
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #       --endpoint 1
+#       --app-pipe /tmp/rvcopstate_2_5_fifo
 #       --PICS examples/rvc-app/rvc-common/pics/rvc-app-pics-values
 #       --int-arg runmode_cleanmode:1
 #     factory-reset: true
