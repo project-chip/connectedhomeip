d134294856ddced3fde5e17e7d6df097f700ec41
Raul Marquez <130402456+raul-marquez-csa@users.noreply.github.com>
2025-07-15 11:20:53 -0700

    Update src/python_testing/TC_FAN_3_5.py
    Co-authored-by: C Freeman <cecille@google.com>

--- src/python_testing/TC_FAN_3_5.py
+++ src/python_testing/TC_FAN_3_5.py
@@ -69,7 +69,7 @@ class TC_FAN_3_5(MatterBaseTest):
                          "- Setup a Step command with: -- Direction=Decrease -- Wrap=False -- LowestOff=True."),
                 TestStep(7, "[FC] Initialize the PercentSetting attribute to 100.",
                          "- Verify that the SpeedSetting attribute value is set to SpeedMax. - Verify that the FanMode attribute value is set to High."),
-                TestStep(8, "[FC] Subscribe to PercentSetting, SpeedSetting, and FanMode attributes.",
+                TestStep(8, "[FC] Subscribe to PercentSetting, SpeedSetting, and FanMode attributes.")
                 TestStep(9, "[FC] TH sends Step commands iteratively.",
                          "- Verify that the PercentSetting attribute value reaches 0. - When the PercentSetting attribute value reaches 0, send an additional Step command to veiryf that the PercentSetting attribute value stays at 0."),
                 TestStep(10, "[FC] Read the resulting attribute reports from each subscription after the Step commands.",
