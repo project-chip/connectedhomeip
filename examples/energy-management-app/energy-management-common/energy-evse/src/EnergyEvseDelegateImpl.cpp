0dcf0f9f269119b690e4bdcba649caa37a8adec0
James Harrow <james.harrow@gmail.com>
2025-07-16 21:49:57 +0100

    Proper fix for issue raised during review of https://github.com/project-chip/connectedhomeip/pull/39641#discussion_r2211520266

--- examples/energy-management-app/energy-management-common/energy-evse/src/EnergyEvseDelegateImpl.cpp
+++ examples/energy-management-app/energy-management-common/energy-evse/src/EnergyEvseDelegateImpl.cpp
@@ -730,8 +730,9 @@ Status EnergyEvseDelegate::HwSetRFID(ByteSpan uid)
 Status EnergyEvseDelegate::HwSetVehicleID(const CharSpan & newValue)
 {
 
-    if (!mVehicleID.IsNull() && newValue.data_equal(mVehicleID.Value()))
+    if (!mVehicleID.IsNull() && newValue.data_equal(mVehicleID.Value()) || (mVehicleID.IsNull() && newValue.empty()))
     {
+        // No change in VehicleID, nothing to do
         return Status::Success;
     }
 
