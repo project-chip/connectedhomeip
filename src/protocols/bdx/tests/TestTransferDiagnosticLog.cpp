1260c20bdf3b76495db6f9e523b4160d4b211d98
Mauricio Costa <mcosta@griddynamics.com>
2025-07-16 10:42:22 -0300

    incresing UT coverage for src/protocols/bdx/BdxTransferProxyDiagnosticLog.

--- src/protocols/bdx/tests/TestTransferDiagnosticLog.cpp
+++ src/protocols/bdx/tests/TestTransferDiagnosticLog.cpp
@@ -2,9 +2,9 @@
 
 #include <pw_unit_test/framework.h>
 
-#include <functional>
-#include <optional>
 #include <protocols/bdx/BdxTransferDiagnosticLog.h>
+#include <system/SystemClock.h>
+#include <system/SystemTimer.h>
 
 using namespace ::chip;
 using namespace ::chip::bdx;
@@ -15,10 +15,297 @@ namespace {
 
 class TestTransferDiagnosticLog : public ::testing::Test
 {
+public:
+static void SetUpTestSuite()
+    {
+        ASSERT_EQ(Platform::MemoryInit(), CHIP_NO_ERROR);
+    }
+
+    static void TearDownTestSuite()
+    {
+        Platform::MemoryShutdown();
+    }    
 protected:
     TransferSession mTransferSession{};
 };
 
-TEST_F(TestTransferDiagnosticLog, TestTransferDiagnosticLog) {}
+TEST_F(TestTransferDiagnosticLog, InitsDiagnosticLog) {
+    System::Clock::Internal::MockClock clock;
+    System::Clock::ClockBase * realClock = &System::SystemClock();
+    System::Clock::Internal::SetSystemClockForTesting(&clock);
+
+    mTransferSession.WaitForTransfer(TransferRole::kReceiver, TransferControlFlags::kSenderDrive, uint16_t{512}, 1000_ms);
+
+    auto transferInit = TransferInit();
+
+    transferInit.TransferCtlOptions.ClearAll().Set(TransferControlFlags::kSenderDrive, true);
+    transferInit.Version = 1;
+
+    transferInit.MaxLength = static_cast<uint64_t>(std::numeric_limits<uint32_t>::max()) + 1;
+
+    transferInit.StartOffset  = 42;
+    transferInit.MaxBlockSize = 256;
+
+    char testFileDes[9]    = { "test.txt" };
+    transferInit.FileDesLength  = 9;
+    transferInit.FileDesignator = reinterpret_cast<uint8_t *>(testFileDes);
+
+    uint8_t fakeData[5]    = { 7, 6, 5, 4, 3 };
+    transferInit.MetadataLength = 5;
+    transferInit.Metadata       = reinterpret_cast<uint8_t *>(fakeData);
+
+    size_t msgSize = transferInit.MessageSize();
+    Encoding::LittleEndian::PacketBufferWriter bbuf(System::PacketBufferHandle::New(msgSize));
+    ASSERT_FALSE(bbuf.IsNull());
+    transferInit.WriteToBuffer(bbuf);
+
+    System::PacketBufferHandle msgBuf = bbuf.Finalize();
+    ASSERT_FALSE(msgBuf.IsNull());
+    System::PacketBufferHandle rcvBuf = System::PacketBufferHandle::NewWithData(msgBuf->Start(), msgSize);
+    ASSERT_FALSE(rcvBuf.IsNull());
+
+    PayloadHeader payloadHeader;
+    payloadHeader.SetMessageType(Protocols::BDX::Id , to_underlying(MessageType::SendInit));
+
+    auto r = mTransferSession.HandleMessageReceived(payloadHeader, std::move(rcvBuf), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    auto proxyDiagnosticLog = BDXTransferProxyDiagnosticLog();
+
+    r = proxyDiagnosticLog.Init(&mTransferSession);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    System::Clock::Internal::SetSystemClockForTesting(realClock);
+}
+
+TEST_F(TestTransferDiagnosticLog, AccpetsTransferActingAsReceiverWhileInititatorDrivesTransfer) {
+    
+    auto proxyDiagnosticLog = BDXTransferProxyDiagnosticLog();
+
+    TransferSession initiator;
+    TransferSession::TransferInitData transferInitData;
+    transferInitData.TransferCtlFlags =TransferControlFlags::kSenderDrive;
+    transferInitData.MaxBlockSize = 512;
+    transferInitData.StartOffset  = 0;
+    transferInitData.Length       = 1024;
+    char testFileDes[9]    = { "test.txt" };
+    transferInitData.FileDesLength  = 9;
+    transferInitData.FileDesignator = reinterpret_cast<uint8_t *>(testFileDes);
+    uint8_t fakeData[5]    = { 7, 6, 5, 4, 3 };
+    transferInitData.MetadataLength = 5;
+    transferInitData.Metadata       = reinterpret_cast<uint8_t *>(fakeData);
+
+    /// Init initiator (and sender) transfer session
+    auto r = initiator.StartTransfer(TransferRole::kSender, transferInitData, System::Clock::Seconds16(10));
+    
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    TransferSession::OutputEvent initiatorEvent;
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kMsgToSend);
+
+    /// Init responder (and receiver) transfer session
+    r = mTransferSession.WaitForTransfer(TransferRole::kReceiver, TransferControlFlags::kSenderDrive, 512, System::Clock::Seconds16(20));
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    TransferSession::OutputEvent responderEvent;
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kNone);
+
+    chip::PayloadHeader payloadHeader;
+    payloadHeader.SetMessageType(initiatorEvent.msgTypeData.ProtocolId, initiatorEvent.msgTypeData.MessageType);
+
+    r = mTransferSession.HandleMessageReceived(payloadHeader, std::move(initiatorEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kInitReceived);
+
+    r = proxyDiagnosticLog.Init(&mTransferSession);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    proxyDiagnosticLog.SetFabricIndex(1);
+    proxyDiagnosticLog.SetPeerNodeId(2);
+    r = proxyDiagnosticLog.Accept();
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kMsgToSend); // responder sends Accept message
+
+}
+
+
+TEST_F(TestTransferDiagnosticLog, RejectsInTheMiddleOfTransfer) {
+    
+    auto proxyDiagnosticLog = BDXTransferProxyDiagnosticLog();
+
+    TransferSession initiator;
+    TransferSession::TransferInitData transferInitData;
+    transferInitData.TransferCtlFlags =TransferControlFlags::kSenderDrive;
+    transferInitData.MaxBlockSize = 512;
+    transferInitData.StartOffset  = 0;
+    transferInitData.Length       = 1024;
+    char testFileDes[9]    = { "test.txt" };
+    transferInitData.FileDesLength  = 9;
+    transferInitData.FileDesignator = reinterpret_cast<uint8_t *>(testFileDes);
+    uint8_t fakeData[5]    = { 7, 6, 5, 4, 3 };
+    transferInitData.MetadataLength = 5;
+    transferInitData.Metadata       = reinterpret_cast<uint8_t *>(fakeData);
+
+    /// Init initiator (and sender) transfer session
+    auto r = initiator.StartTransfer(TransferRole::kSender, transferInitData, System::Clock::Seconds16(10));
+    
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    TransferSession::OutputEvent initiatorEvent;
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kMsgToSend);
+
+    /// Init responder (and receiver) transfer session
+    r = mTransferSession.WaitForTransfer(TransferRole::kReceiver, TransferControlFlags::kSenderDrive, 512, System::Clock::Seconds16(20));
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    TransferSession::OutputEvent responderEvent;
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kNone);
+
+    chip::PayloadHeader payloadHeader;
+    payloadHeader.SetMessageType(initiatorEvent.msgTypeData.ProtocolId, initiatorEvent.msgTypeData.MessageType);
+
+    r = mTransferSession.HandleMessageReceived(payloadHeader, std::move(initiatorEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kInitReceived);
+
+    r = proxyDiagnosticLog.Init(&mTransferSession);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    proxyDiagnosticLog.SetFabricIndex(1);
+    proxyDiagnosticLog.SetPeerNodeId(2);
+    r = proxyDiagnosticLog.Accept();
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kMsgToSend); // responder sends Accept message
+
+    /// Now, initiator handles SendAccept message
+    payloadHeader.SetMessageType(responderEvent.msgTypeData.ProtocolId, responderEvent.msgTypeData.MessageType);
+
+    r = initiator.HandleMessageReceived(payloadHeader, std::move(responderEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kAcceptReceived);
+
+    /// initiator prepares block to send
+    TransferSession::BlockData blockData;
+    blockData.Data         = reinterpret_cast<const uint8_t *>("hi there");
+    blockData.Length       = 8;
+    blockData.IsEof        = false;
+    blockData.BlockCounter = 0;
+    
+    initiator.PrepareBlock(blockData);
+
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kMsgToSend);
+
+    EXPECT_EQ(initiatorEvent.msgTypeData.MessageType, to_underlying(MessageType::Block));
+
+    payloadHeader.SetMessageType(initiatorEvent.msgTypeData.ProtocolId, initiatorEvent.msgTypeData.MessageType);
+
+    r = mTransferSession.HandleMessageReceived(payloadHeader, std::move(initiatorEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kBlockReceived);
+    
+    /// proxy acknowledges block
+    r = proxyDiagnosticLog.Continue();
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kMsgToSend); // responder sends Ack message
+
+    EXPECT_EQ(responderEvent.msgTypeData.MessageType, to_underlying(MessageType::BlockAck));
+
+    /// Now, initiator handles Block Ack message
+
+    payloadHeader.SetMessageType(responderEvent.msgTypeData.ProtocolId, responderEvent.msgTypeData.MessageType);
+
+    r = initiator.HandleMessageReceived(payloadHeader, std::move(responderEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kAckReceived);
+
+    /// initiator prepares next block to send
+    blockData.BlockCounter = 10; // while respodner expects 1
+
+    initiator.PrepareBlock(blockData);
+
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kMsgToSend);
+
+    EXPECT_EQ(initiatorEvent.msgTypeData.MessageType, to_underlying(MessageType::Block));
+
+    /// responder receives 2nd block
+    payloadHeader.SetMessageType(initiatorEvent.msgTypeData.ProtocolId, initiatorEvent.msgTypeData.MessageType);
+
+    r = mTransferSession.HandleMessageReceived(payloadHeader, std::move(initiatorEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kBlockReceived);
+
+    /// proxy rejects transfer
+    r = proxyDiagnosticLog.Reject(CHIP_ERROR_INVALID_ARGUMENT);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    mTransferSession.PollOutput(responderEvent, System::Clock::kZero);
+
+    EXPECT_EQ(responderEvent.EventType, TransferSession::OutputEventType::kMsgToSend); // responder sends StatusReport message
+
+    /// Now, initiator handles StatusReport message
+    payloadHeader.SetMessageType(responderEvent.msgTypeData.ProtocolId, responderEvent.msgTypeData.MessageType);
+
+    r = initiator.HandleMessageReceived(payloadHeader, std::move(responderEvent.MsgData), System::Clock::kZero);
+
+    EXPECT_EQ(r, CHIP_NO_ERROR);
+
+    initiator.PollOutput(initiatorEvent, System::Clock::kZero);
+
+    EXPECT_EQ(initiatorEvent.EventType, TransferSession::OutputEventType::kStatusReceived); /// this implies transfer rejection got received
+
+    initiator.Reset();
+    mTransferSession.Reset();
+    proxyDiagnosticLog.Reset();
+}
+
 
 } // namespace
