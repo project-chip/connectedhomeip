499afd0992c76499a9016b2a4ebfde6e9b907548
Oleksandr Sirko <wisenss@gmail.com>
2025-07-16 13:58:36 +0200

    fix casing issue

--- src/python_testing/matter_testing_infrastructure/chip/testing/runner.py
+++ src/python_testing/matter_testing_infrastructure/chip/testing/runner.py
@@ -964,7 +964,7 @@ def json_named_arg(s: str) -> Tuple[str, object]:
 
 def bool_named_arg(s: str) -> Tuple[str, bool]:
     regex = r"^(?P<name>[a-zA-Z_0-9.]+):((?P<truth_value>true|false)|(?P<decimal_value>[01]))$"
-    match = re.match(regex, s.lower())
+    match = re.match(regex, s, re.IGNORECASE)
     if not match:
         raise ValueError("Invalid bool argument format, does not match %s" % regex)
 
