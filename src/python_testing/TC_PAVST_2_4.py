06d2e850e1e7893d3521b09f9f66eb5e9f52d44e
Restyled.io <commits@restyled.io>
2025-07-17 16:15:37 +0000

    Restyled by isort

--- src/python_testing/TC_PAVST_2_4.py
+++ src/python_testing/TC_PAVST_2_4.py
@@ -38,12 +38,7 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.testing.matter_testing import (
-    MatterBaseTest,
-    TestStep,
-    async_test_body,
-    default_matter_test_main,
-)
+from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 logger = logging.getLogger(__name__)
