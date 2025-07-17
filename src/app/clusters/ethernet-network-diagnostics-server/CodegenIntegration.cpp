bc3f520070db9b5deba63a3db13d42dd885d6311
Yufeng Wang <yufengwang@google.com>
2025-07-17 11:24:40 -0700

    Address review comments

--- src/app/clusters/ethernet-network-diagnostics-server/CodegenIntegration.cpp
+++ src/app/clusters/ethernet-network-diagnostics-server/CodegenIntegration.cpp
@@ -17,6 +17,7 @@
 #include <app/clusters/ethernet-network-diagnostics-server/ethernet-diagnostics-cluster.h>
 #include <app/static-cluster-config/EthernetNetworkDiagnostics.h>
 #include <app/util/attribute-storage.h>
+#include <app/util/util.h>
 #include <data-model-providers/codegen/CodegenDataModelProvider.h>
 #include <platform/DiagnosticDataProvider.h>
 
@@ -25,53 +26,65 @@ using namespace chip::app;
 using namespace chip::app::Clusters;
 using namespace chip::app::Clusters::EthernetNetworkDiagnostics;
 
-// the static assert is skipped in case of dynamic endpoints.
-static_assert((EthernetNetworkDiagnostics::StaticApplicationConfig::kFixedClusterConfig.size() == 1 &&
-               EthernetNetworkDiagnostics::StaticApplicationConfig::kFixedClusterConfig[0].endpointNumber == kRootEndpointId) ||
-              EthernetNetworkDiagnostics::StaticApplicationConfig::kFixedClusterConfig.size() == 0);
-
 namespace {
 
-LazyRegisteredServerCluster<EthernetDiagnosticsServerCluster> gServer;
+static constexpr size_t kEthernetNetworkDiagnosticsFixedClusterCount =
+    EthernetNetworkDiagnostics::StaticApplicationConfig::kFixedClusterConfig.size();
+static constexpr size_t kEthernetNetworkDiagnosticsMaxClusterCount =
+    kEthernetNetworkDiagnosticsFixedClusterCount + CHIP_DEVICE_CONFIG_DYNAMIC_ENDPOINT_COUNT;
 
-// compile-time evaluated method if "is <EP>::EthernetNetworkDiagnostics::<ATTR>" enabled
-constexpr bool IsAttributeEnabled(EndpointId endpointId, AttributeId attributeId)
+LazyRegisteredServerCluster<EthernetDiagnosticsServerCluster> gServers[kEthernetNetworkDiagnosticsMaxClusterCount];
+
+// Find the 0-based array index corresponding to the given endpoint id.
+// Log an error if not found.
+bool findEndpointWithLog(EndpointId endpointId, uint16_t & outArrayIndex)
 {
-    for (auto & config : EthernetNetworkDiagnostics::StaticApplicationConfig::kFixedClusterConfig)
+    uint16_t arrayIndex = emberAfGetClusterServerEndpointIndex(endpointId, EthernetNetworkDiagnostics::Id,
+                                                               kEthernetNetworkDiagnosticsFixedClusterCount);
+
+    if (arrayIndex >= kEthernetNetworkDiagnosticsMaxClusterCount)
     {
-        if (config.endpointNumber != endpointId)
-        {
-            continue;
-        }
-        for (auto & attr : config.enabledAttributes)
-        {
-            if (attr == attributeId)
-            {
-                return true;
-            }
-        }
+        ChipLogError(AppServer, "Could not find endpoint index for endpoint %u", endpointId);
+        return false;
     }
-    return false;
+    outArrayIndex = arrayIndex;
+    return true;
+}
+
+// Runtime method to check if an attribute is enabled using Ember functions
+bool IsAttributeEnabled(EndpointId endpointId, AttributeId attributeId)
+{
+    return emberAfContainsAttribute(endpointId, EthernetNetworkDiagnostics::Id, attributeId);
 }
 
 } // namespace
 
 void emberAfEthernetNetworkDiagnosticsClusterInitCallback(EndpointId endpointId)
 {
-    VerifyOrReturn(endpointId == kRootEndpointId);
+    uint16_t arrayIndex = 0;
+    if (!findEndpointWithLog(endpointId, arrayIndex))
+    {
+        return;
+    }
 
     const EthernetDiagnosticsEnabledAttributes enabledAttributes{
-        .enableCarrierDetect  = IsAttributeEnabled(kRootEndpointId, Attributes::CarrierDetect::Id),
-        .enableFullDuplex     = IsAttributeEnabled(kRootEndpointId, Attributes::FullDuplex::Id),
-        .enablePacketCount    = IsAttributeEnabled(kRootEndpointId, Attributes::PacketRxCount::Id),
-        .enablePHYRate        = IsAttributeEnabled(kRootEndpointId, Attributes::PHYRate::Id),
-        .enableTimeSinceReset = IsAttributeEnabled(kRootEndpointId, Attributes::TimeSinceReset::Id),
-        .enableErrCount       = IsAttributeEnabled(kRootEndpointId, Attributes::TxErrCount::Id),
+        .enableCarrierDetect = IsAttributeEnabled(endpointId, Attributes::CarrierDetect::Id),
+        .enableFullDuplex    = IsAttributeEnabled(endpointId, Attributes::FullDuplex::Id),
+        .enablePacketCount   = IsAttributeEnabled(endpointId, Attributes::PacketRxCount::Id) ||
+            IsAttributeEnabled(endpointId, Attributes::PacketTxCount::Id),
+        .enablePHYRate        = IsAttributeEnabled(endpointId, Attributes::PHYRate::Id),
+        .enableTimeSinceReset = IsAttributeEnabled(endpointId, Attributes::TimeSinceReset::Id),
+        .enableErrCount       = IsAttributeEnabled(endpointId, Attributes::TxErrCount::Id) ||
+            IsAttributeEnabled(endpointId, Attributes::CollisionCount::Id) ||
+            IsAttributeEnabled(endpointId, Attributes::OverrunCount::Id),
     };
 
-    gServer.Create(DeviceLayer::GetDiagnosticDataProvider(), enabledAttributes);
+    // NOTE: Currently, diagnostics only support a single provider (DeviceLayer::GetDiagnosticDataProvider())
+    // and do not properly support secondary network interfaces or per-endpoint diagnostics.
+    // See issue:#40175
+    gServers[arrayIndex].Create(DeviceLayer::GetDiagnosticDataProvider(), enabledAttributes);
 
-    CHIP_ERROR err = CodegenDataModelProvider::Instance().Registry().Register(gServer.Registration());
+    CHIP_ERROR err = CodegenDataModelProvider::Instance().Registry().Register(gServers[arrayIndex].Registration());
     if (err != CHIP_NO_ERROR)
     {
         ChipLogError(AppServer, "Failed to register EthernetNetworkDiagnostics on endpoint %u: %" CHIP_ERROR_FORMAT, endpointId,
@@ -81,14 +94,19 @@ void emberAfEthernetNetworkDiagnosticsClusterInitCallback(EndpointId endpointId)
 
 void emberAfEthernetNetworkDiagnosticsClusterShutdownCallback(EndpointId endpointId)
 {
-    VerifyOrReturn(endpointId == kRootEndpointId);
-    CHIP_ERROR err = CodegenDataModelProvider::Instance().Registry().Unregister(&gServer.Cluster());
+    uint16_t arrayIndex = 0;
+    if (!findEndpointWithLog(endpointId, arrayIndex))
+    {
+        return;
+    }
+
+    CHIP_ERROR err = CodegenDataModelProvider::Instance().Registry().Unregister(&gServers[arrayIndex].Cluster());
     if (err != CHIP_NO_ERROR)
     {
         ChipLogError(AppServer, "Failed to unregister EthernetNetworkDiagnostics on endpoint %u: %" CHIP_ERROR_FORMAT, endpointId,
                      err.Format());
     }
-    gServer.Destroy();
+    gServers[arrayIndex].Destroy();
 }
 
 void MatterEthernetNetworkDiagnosticsPluginServerInitCallback() {}
