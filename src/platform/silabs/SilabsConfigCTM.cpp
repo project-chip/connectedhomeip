db1d0f6fe554800cb2c61cb43f36ec135854085e
lpbeliveau-silabs <112982107+lpbeliveau-silabs@users.noreply.github.com>
2025-07-22 10:15:55 -0400

    Update src/platform/silabs/SilabsConfigCTM.cpp
    Co-authored-by: Sergio Soares <sergiosoares@google.com>

--- src/platform/silabs/SilabsConfigCTM.cpp
+++ src/platform/silabs/SilabsConfigCTM.cpp
@@ -38,8 +38,10 @@
 #include <sl_token_manager_interface.h>
 // Substitute the GSDK weak nvm3_lockBegin and nvm3_lockEnd
 // for an application controlled re-entrance protection
-static SemaphoreHandle_t nvm3_Sem;
-static StaticSemaphore_t nvm3_SemStruct;
+namespace {
+SemaphoreHandle_t nvm3_Sem;
+StaticSemaphore_t nvm3_SemStruct;
+}
 
 void nvm3_lockBegin(void)
 {
