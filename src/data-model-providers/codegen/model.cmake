f4a7fa0d7ea78c66f5024b25dead0d6ea3587c63
Sergio Soares <sergiosoares@google.com>
2025-07-30 17:24:18 +0000

    Fix path on model.cmake

--- src/data-model-providers/codegen/model.cmake
+++ src/data-model-providers/codegen/model.cmake
@@ -22,8 +22,6 @@ SET(CODEGEN_DATA_MODEL_SOURCES
   "${BASE_DIR}/EmberAttributeDataBuffer.cpp"
   "${BASE_DIR}/EmberAttributeDataBuffer.h"
   "${BASE_DIR}/Instance.cpp"
-  "${BASE_DIR}/ServerClusterInterfaceRegistry.cpp"
-  "${BASE_DIR}/ServerClusterInterfaceRegistry.h"
 
   # These are dependencies from model.gni that are not included directly in cmake
   # "${chip_root}/src/app/server-cluster",
@@ -34,4 +32,6 @@ SET(CODEGEN_DATA_MODEL_SOURCES
   "${BASE_DIR}/../../app/server-cluster/ServerClusterContext.h"
   "${BASE_DIR}/../../app/server-cluster/ServerClusterInterface.cpp"
   "${BASE_DIR}/../../app/server-cluster/ServerClusterInterface.h"
+  "${BASE_DIR}/../../app/server-cluster/ServerClusterInterfaceRegistry.cpp"
+  "${BASE_DIR}/../../app/server-cluster/ServerClusterInterfaceRegistry.h"
 )
