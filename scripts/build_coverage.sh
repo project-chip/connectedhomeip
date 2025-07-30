af6699ebbeff57d8a40761564967294d34d1f4b9
Diego Maranges <dmaranges@griddynamics.com>
2025-07-30 18:56:59 -0300

    edit codecov.yml to include patial coverage lines as hit and build_coverage.sh to skip test files on parcer proccess

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -225,7 +225,7 @@ fi
 # ------------------------------------------------------------------------------
 # Coverage Generation
 # ----------------------------------------------------------------------------
-LCOV_IGNORE_ERRORS="format,unsupported,inconsistent,inconsistent,unused,unused,gcov,gcov" # Some error types mentioned twice is needed to suppress the warnings.
+LCOV_IGNORE_ERRORS="format,unsupported,inconsistent,inconsistent,unused,unused,gcov" # Some error types mentioned twice is needed to suppress the warnings.
 LCOV_EXCLUDE_INCLUDE_OPTIONS=()
 
 # Exclude files we do NOT want included in coverage
@@ -299,6 +299,8 @@ if [ "$GENERATE_XML" == true ]; then
 
     gcovr --exclude=zzz_generated/ \
         --exclude=third_party/ \
+        --exclude=".*tests/.*" \
+        --exclude=".*testing/.*" \
         --include=src/ \
         --gcov-ignore-parse-errors \
         --xml="$COVERAGE_ROOT"/coverage.xml
