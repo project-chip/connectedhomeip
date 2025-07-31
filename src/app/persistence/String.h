dae2d83d4819486107568146a203e960ee38789c
Restyled.io <commits@restyled.io>
2025-07-31 19:38:30 +0000

    Restyled by clang-format

--- src/app/persistence/String.h
+++ src/app/persistence/String.h
@@ -103,10 +103,10 @@ public:
     // internal shortstring is self-referencing pointers. That cannot be copied, so we assume no copy for now
     // These could be implemented, however for now we assume people should just use the underlying Span() to set
     // the values.
-    String(String &&)                   = delete;
-    String & operator=(String &&) = delete;
-    String(const String &)              = delete;
-    String & operator=(const String &)  = delete;
+    String(String &&)                  = delete;
+    String & operator=(String &&)      = delete;
+    String(const String &)             = delete;
+    String & operator=(const String &) = delete;
 
 private:
     //   - 1 byte to null-terminate to allow for a c_str() implementation
