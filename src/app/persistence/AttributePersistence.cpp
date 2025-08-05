6b02af3a2ac81c2475a39beaa410c5df6988cfcb
Andrei Litvin <andreilitvin@google.com>
2025-08-05 11:21:54 -0400

    Fix include path

--- src/app/persistence/AttributePersistence.cpp
+++ src/app/persistence/AttributePersistence.cpp
@@ -13,11 +13,11 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "app/persistence/String.h"
 #include <app/ConcreteAttributePath.h>
 #include <app/data-model/Nullable.h>
 #include <app/persistence/AttributePersistence.h>
 #include <app/persistence/AttributePersistenceProvider.h>
+#include <app/persistence/String.h>
 #include <lib/core/CHIPError.h>
 #include <lib/support/Span.h>
 
