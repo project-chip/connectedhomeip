d42a4aecf8fc72b2f53a15eb62849e14d4de41a3
Alejandro Buezo <abuezo@griddynamics.com>
2025-08-06 16:29:49 -0300

    Removed MAX_RETRIES from test script

--- src/python_testing/TC_CNET_4_11.py
+++ src/python_testing/TC_CNET_4_11.py
@@ -20,7 +20,7 @@ import logging
 
 import chip.clusters as Clusters
 from chip.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_feature, run_if_endpoint_matches
-from matter_testing_infrastructure.chip.testing.wifi import (MAX_RETRIES, TIMED_REQUEST_TIMEOUT_MS, TIMEOUT, WIFI_WAIT_SECONDS,
+from matter_testing_infrastructure.chip.testing.wifi import (TIMED_REQUEST_TIMEOUT_MS, TIMEOUT, WIFI_WAIT_SECONDS,
                                                              change_networks, connect_host_wifi, find_network_and_assert, verify_operational_network)
 from mobly import asserts
 
