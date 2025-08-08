b89a652604958be403af4dd1a8d04812ee477ccc
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2025-08-07 17:00:52 -0700

    Fix module name

--- src/python_testing/TC_COLORCONTROL.py
+++ src/python_testing/TC_COLORCONTROL.py
@@ -39,8 +39,8 @@
 
 import logging
 
-import chip.clusters as Clusters
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+import matter.clusters as Clusters
+from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
 logger = logging.getLogger(__name__)
