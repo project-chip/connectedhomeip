04dcf03c00d88891e0d4e5f3eba321582cd5f91d
Restyled.io <commits@restyled.io>
2025-08-08 11:37:29 +0000

    Restyled by isort

--- src/python_testing/TC_CLCTRL_3_1.py
+++ src/python_testing/TC_CLCTRL_3_1.py
@@ -35,10 +35,10 @@
 
 import logging
 
-from matter.clusters.Types import NullValue
 from mobly import asserts
 
 import matter.clusters as Clusters
+from matter.clusters.Types import NullValue
 from matter.interaction_model import InteractionModelError, Status
 from matter.testing.event_attribute_reporting import AttributeSubscriptionHandler
 from matter.testing.matter_testing import (AttributeMatcher, AttributeValue, MatterBaseTest, TestStep, async_test_body,
