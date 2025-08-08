5062aa3464dd0ef5baa33c1a273b3467d517ad0b
Fabian Wechselberger <fabian@innovation-matters.at>
2025-08-08 14:39:26 +0200

    fix typo

--- src/python_testing/TC_CLCTRL_4_3.py
+++ src/python_testing/TC_CLCTRL_4_3.py
@@ -607,7 +607,7 @@ class TC_CLCTRL_4_3(MatterBaseTest):
             overall_current_state: typing.Union[Nullable, Clusters.ClosureControl.Structs.OverallCurrentStateStruct] = await self.read_clctrl_attribute_expect_success(endpoint=endpoint, attribute=attributes.OverallCurrentState)
             current_position: Clusters.ClosureControl.Enums.CurrentPositionEnum = overall_current_state.position
             current_latch: bool = overall_current_state.latch if overall_current_state is not NullValue else NullValue
-            logging.info(f"current_speed: {current_speed}, current_latch: {current_latch}")
+            logging.info(f"current_position: {current_position}, current_latch: {current_latch}")
             
             self.step("9c")
             if is_latching_supported and current_latch is True:
