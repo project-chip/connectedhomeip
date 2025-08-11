f9d27e5ff298b67c11994743f966c87755185cf3
Rajashreekalmane <95841834+Rajashreekalmane@users.noreply.github.com>
2025-08-11 17:23:16 +0530

    Update TC_APPLAUNCHER_3_5.py

--- src/python_testing/TC_APPLAUNCHER_3_5.py
+++ src/python_testing/TC_APPLAUNCHER_3_5.py
@@ -15,7 +15,8 @@
 # === END CI TEST ARGUMENTS ===
 
 import logging
-import chip.clusters as Clusters
+
+import matter.clusters as Clusters
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 
