5b106d4e1949b3cebc0e0e7308bf8a42c1e77581
Rajashreekalmane <rajashree@graniteriverlabs.in>
2025-08-11 21:11:35 +0530

    minor changes

--- src/python_testing/TC_AUDIOOUTPUT_7_5.py
+++ src/python_testing/TC_AUDIOOUTPUT_7_5.py
@@ -26,11 +26,13 @@
 #       --endpoint 1
 # === END CI TEST ARGUMENTS ===
 
+
 import logging
 
-import chip.clusters as Clusters
-from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
+import matter.clusters as Clusters
+from matter.interaction_model import Status
+from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 
 
 class TC_AUDIOOUTPUT_7_5(MatterBaseTest):
