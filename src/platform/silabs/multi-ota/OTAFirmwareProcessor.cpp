SiliconLabsSoftware/matter_sdk

[SL-UP] Adds support for PKCS7 padding in OTA flow (#511)
Co-authored-by: Restyled.io <commits@restyled.io>

src/platform/silabs/multi-ota/OTAFirmwareProcessor.cpp
@@ -72,6 +72,12 @@ CHIP_ERROR OTAFirmwareProcessor::ProcessInternal(ByteSpan & block)
     }
 
     OTATlvProcessor::vOtaProcessInternalEncryption(byteBlock);
+    if (IsLastBlock() == true)
+    {
+        // Remove padding from the last block since if the file was padded, last block will contain padding bytes.
+        VerifyOrReturnError(OTATlvProcessor::RemovePadding(byteBlock) == CHIP_NO_ERROR, CHIP_ERROR_WRONG_ENCRYPTION_TYPE,
+                            ChipLogError(SoftwareUpdate, "Failed to remove padding"));
+    }
     block = byteBlock;
 #endif
 

project-chip/connectedhomeip

src/platform/silabs/multi-ota/OTAFirmwareProcessor.cpp
@@ -96,6 +96,12 @@ CHIP_ERROR OTAFirmwareProcessor::ProcessInternal(ByteSpan & block)
     }
 
     OTATlvProcessor::vOtaProcessInternalEncryption(mBlock);
+    if (IsLastBlock() == true)
+    {
+        // Remove padding from the last block since if the file was padded, last block will contain padding bytes.
+        VerifyOrReturnError(OTATlvProcessor::RemovePadding(mBlock) == CHIP_NO_ERROR, CHIP_ERROR_WRONG_ENCRYPTION_TYPE,
+        ChipLogError(SoftwareUpdate, "Failed to remove padding"));
+    }
     block = mBlock;
 #endif
 