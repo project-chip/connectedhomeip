04567c60f8999ed28a5ef4ae5a7902ffb973b10d
Nivi Sarkar <nivedita_sarkar@apple.com>
2025-08-18 11:45:56 -0700

    Update thermostat cluster xml with Alchemy generated xml from spec
    Add zap generated files for the updates

--- src/controller/python/matter/clusters/Objects.py
+++ src/controller/python/matter/clusters/Objects.py
@@ -31876,6 +31876,7 @@ class Thermostat(Cluster):
             kMatterScheduleConfiguration = 0x80
             kPresets = 0x100
             kEvents = 0x200
+            kThermostatSuggestions = 0x400
 
         class HVACSystemTypeBitmap(IntFlag):
             kCoolingStage = 0x3
@@ -31937,6 +31938,7 @@ class Thermostat(Cluster):
             kVacationMode = 0x10
             kTimeOfUseCostSavings = 0x20
             kPreCoolingOrPreHeating = 0x40
+            kConflictingSuggestions = 0x80
 
     class Structs:
         @dataclass
