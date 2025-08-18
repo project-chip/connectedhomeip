e4929f28fef475d5d7a0567791955716c1531c80
Raveendra Karu <r.karu@samsung.com>
2025-08-18 16:24:29 +0530

    Rebased to latest code.
    Increase timeout to get pipeline state.
    Co-Author: Dhruvkumar <kakadiya.b@samsung.com>
    
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- examples/camera-app/linux/src/camera-device.cpp
+++ examples/camera-app/linux/src/camera-device.cpp
@@ -673,7 +673,7 @@ CameraError CameraDevice::StartVideoStream(const VideoStreamStruct & allocatedSt
 
     // Wait for the pipeline to reach the PLAYING state
     GstState state;
-    gst_element_get_state(videoPipeline, &state, nullptr, 5 * GST_SECOND);
+    gst_element_get_state(videoPipeline, &state, nullptr, 6 * GST_SECOND);
     if (state != GST_STATE_PLAYING)
     {
         ChipLogError(Camera, "Video pipeline did not reach PLAYING state.");
