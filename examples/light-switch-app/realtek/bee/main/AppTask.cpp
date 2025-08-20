08b323e5e775f0f30ecda4af7213cb07dbc8c49a
pankore <86098180+pankore@users.noreply.github.com>
2025-08-20 15:23:37 +0800

    [Realtek] Fix some error

--- examples/light-switch-app/realtek/bee/main/AppTask.cpp
+++ examples/light-switch-app/realtek/bee/main/AppTask.cpp
@@ -73,7 +73,7 @@ using namespace app::Clusters::Descriptor::Structs;
 
 namespace {
 
-#if (CONFIG_1_TO_2_ZAP || CONFIG_1_TO_8_ZAP || CONFIG_1_TO_12_ZAP)
+#if (CONFIG_1_TO_2_ZAP || CONFIG_1_TO_8_ZAP || CONFIG_1_TO_11_ZAP)
 
 // Switches Namespace: 0x43, tag 0x08 (Custom)
 constexpr const uint8_t kNamespaceSwitches = 0x43;
