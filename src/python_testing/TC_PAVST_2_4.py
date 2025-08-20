037e48d1fde433efcab8c0ced15f4d9d1c32a30f
Raveendra Karu <r.karu@samsung.com>
2025-08-20 17:55:46 +0530

    Fix LINT error
    Co-author: Sambhavi <sambhavi.1@samsung.com>
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- src/python_testing/TC_PAVST_2_4.py
+++ src/python_testing/TC_PAVST_2_4.py
@@ -38,7 +38,7 @@
 import logging
 
 import chip.clusters as Clusters
-from chip.interaction_model import InteractionModelError, Status
+from chip.interaction_model import Status
 from chip.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from mobly import asserts
 from TC_PAVSTTestBase import PAVSTTestBase
