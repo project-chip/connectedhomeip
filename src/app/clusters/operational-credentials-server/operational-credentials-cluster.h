9e4911e365e88b109968128b08d5077dc67c1ae1
Moises Terrones <moises.tersal@gmail.com>
2025-08-24 22:20:40 -0600

    Add decouple files

---
+++ src/app/clusters/operational-credentials-server/operational-credentials-cluster.h
@@ -0,0 +1,44 @@
+/*
+ *    Copyright (c) 2021-2025 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#pragma once
+
+#include <app/server-cluster/DefaultServerCluster.h>
+#include <clusters/OperationalCredentials/ClusterId.h>
+
+namespace chip {
+namespace app {
+namespace Clusters {
+
+class OperationalCredentialsCluster : public DefaultServerCluster
+{
+public:
+    OperationalCredentialsCluster(EndpointId endpoint) : DefaultServerCluster({endpoint, }) {}
+
+    // Server cluster implementation
+    DataModel::ActionReturnStatus ReadAttribute(const DataModel::ReadAttributeRequest & request,
+                                                AttributeValueEncoder & encoder) override;
+    std::optional<DataModel::ActionReturnStatus> InvokeCommand(const DataModel::InvokeRequest & request,
+                                                               TLV::TLVReader & input_arguments, CommandHandler * handler) override;
+    CHIP_ERROR AcceptedCommands(const ConcreteClusterPath & path,
+                                ReadOnlyBufferBuilder<DataModel::AcceptedCommandEntry> & builder) override;
+    CHIP_ERROR Attributes(const ConcreteClusterPath & path, ReadOnlyBufferBuilder<DataModel::AttributeEntry> & builder) override;
+}
+
+
+} // namespace Clusters
+} // namespace app
+} // namespace chip
