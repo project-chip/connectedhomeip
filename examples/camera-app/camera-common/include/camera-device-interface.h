98e7daeedc3d7698643f009416d91591f2092d4c
Yufeng Wang <yufengwang@google.com>
2025-08-25 14:49:58 -0700

    Migrate WebRTC Provider Cluster to Server Cluster Interface

--- examples/camera-app/camera-common/include/camera-device-interface.h
+++ examples/camera-app/camera-common/include/camera-device-interface.h
@@ -23,7 +23,7 @@
 #include <app/clusters/camera-av-stream-management-server/camera-av-stream-management-server.h>
 #include <app/clusters/chime-server/chime-server.h>
 #include <app/clusters/push-av-stream-transport-server/push-av-stream-transport-cluster.h>
-#include <app/clusters/webrtc-transport-provider-server/webrtc-transport-provider-server.h>
+#include <app/clusters/webrtc-transport-provider-server/webrtc-transport-provider-cluster.h>
 #include <app/clusters/zone-management-server/zone-management-server.h>
 
 using chip::app::Clusters::CameraAvStreamManagement::AudioCapabilitiesStruct;
