edbe7df932eac0b8507d2fb22a9b61507a1ba349
Ramiro Recchia <rrecchia@google.com>
2025-08-26 15:33:39 -0300

    Removing TC_CNET_1_4.py from CI, it was not running because of PICS value CNET.S

--- src/python_testing/TC_CNET_1_4.py
+++ src/python_testing/TC_CNET_1_4.py
@@ -18,22 +18,6 @@
 # See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
 # for details about the block below.
 #
-# === BEGIN CI TEST ARGUMENTS ===
-# test-runner-runs:
-#   run1:
-#     app: ${ALL_CLUSTERS_APP}
-#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
-#     script-args: >
-#       --storage-path admin_storage.json
-#       --commissioning-method on-network
-#       --discriminator 1234
-#       --passcode 20202021
-#       --PICS src/app/tests/suites/certification/ci-pics-values
-#       --trace-to json:${TRACE_TEST_JSON}.json
-#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
-#     factory-reset: true
-#     quiet: true
-# === END CI TEST ARGUMENTS ===
 
 import logging
 
