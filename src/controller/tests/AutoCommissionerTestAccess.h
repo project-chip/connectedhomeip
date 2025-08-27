5faf51b9f473dd9665edb21c24d370ebe93bc288
marybadalyan <marybadalyangdh@gmail.com>
2025-08-27 15:30:30 +0000

    Add new AutoCommissioner tests

--- src/controller/tests/AutoCommissionerTestAccess.h
+++ src/controller/tests/AutoCommissionerTestAccess.h
@@ -47,6 +47,13 @@ public:
     }
     void SetBreadcrumb(uint64_t value) { mCommissioner->mDeviceCommissioningInfo.general.breadcrumb = value; }
     void SetUTCRequirements(bool requiresUTC) { mCommissioner->mDeviceCommissioningInfo.requiresUTC = requiresUTC; }
+    bool AccessIsScanNeeded() { return mCommissioner->IsScanNeeded(); }
+    bool AccessIsSecondaryNetworkSupported() const { return mCommissioner->IsSecondaryNetworkSupported(); }
+    Controller::ReadCommissioningInfo & GetDeviceCommissioningInfo() { return mCommissioner->mDeviceCommissioningInfo; }
+    void AccessResetTryingSecondaryNetwork() { mCommissioner->ResetTryingSecondaryNetwork(); }
+
+    bool AccessTryingSecondaryNetwork() { return mCommissioner->TryingSecondaryNetwork(); }
+    void AccessTrySecondaryNetwork() { mCommissioner->TrySecondaryNetwork(); }
 
     CHIP_ERROR NOCChainGenerated(ByteSpan noc, ByteSpan icac, ByteSpan rcac, Crypto::IdentityProtectionKeySpan ipk,
                                  NodeId adminSubject)
