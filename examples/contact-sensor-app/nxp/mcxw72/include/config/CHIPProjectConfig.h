4f62d98af1d7321016b4765cedba0e2cf3bddd4c
Andrei Menzopol <andrei.menzopol@nxp.com>
2025-08-27 14:45:09 +0300

    [NXP][platform][mcxw72] Remove references to obsolete K32W1 platform
    Signed-off-by: Andrei Menzopol <andrei.menzopol@nxp.com>

--- examples/contact-sensor-app/nxp/mcxw72/include/config/CHIPProjectConfig.h
+++ examples/contact-sensor-app/nxp/mcxw72/include/config/CHIPProjectConfig.h
@@ -174,7 +174,7 @@
 /**
  * CONFIG_CHIP_NFC_COMMISSIONING, CHIP_DEVICE_CONFIG_ENABLE_NFC
  *
- * NFC commissioning is not supported on K32W1
+ * NFC commissioning is not supported
  */
 #define CONFIG_CHIP_NFC_COMMISSIONING 0
 #define CHIP_DEVICE_CONFIG_ENABLE_NFC 0
