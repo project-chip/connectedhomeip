a7846e82098916586e64ec7d9411b3f98148ad30
Alami-Amine <aalami90@gmail.com>
2025-08-29 16:17:41 +0200

    integrating copilot suggestion

--- src/platform/Linux/ConnectivityManagerImpl.cpp
+++ src/platform/Linux/ConnectivityManagerImpl.cpp
@@ -832,10 +832,10 @@ CHIP_ERROR ConnectivityManagerImpl::_WiFiPAFCancelPublish(uint32_t PublishId)
     gboolean result =
         wpa_supplicant_1_interface_call_nancancel_publish_sync(mWpaSupplicant.iface.get(), PublishId, nullptr, &err.GetReceiver());
 
-    if (!result)
-    {
-        ChipLogError(DeviceLayer, "WiFi-PAF: Failed to Cancel Publish");
-    }
+    // TODO: make sure that the callers do check the return values. This doesn't seem to be happening now.
+    VerifyOrReturnError(
+        result, CHIP_ERROR_INTERNAL,
+        ChipLogError(DeviceLayer, "WiFi-PAF: Failed to Cancel Publish with Error: %s", err ? err->message : "unknown error"));
 
     return CHIP_NO_ERROR;
 }
