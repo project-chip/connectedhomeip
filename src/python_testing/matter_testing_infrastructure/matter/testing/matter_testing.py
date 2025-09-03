44b6ad139fa62e6b060fa70b4f00d56ab12fdd6c
Jake Ororke <jororke@csa-iot.org>
2025-09-03 13:23:19 -0700

    Updating matter_testing support module to help resolve mypy restart_flag_file type issue

--- src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
@@ -511,8 +511,10 @@ class MatterBaseTest(base_test.BaseTestClass):
         except AttributeError:
             return None
 
-    def get_restart_flag_file(self) -> str:
-        return self.matter_test_config.restart_flag_file
+    def get_restart_flag_file(self) -> Optional[str]:
+        if self.matter_test_config.restart_flag_file is None:
+            return None
+        return str(self.matter_test_config.restart_flag_file)
 
     def get_test_pics(self, test: str) -> list[str]:
         ''' Retrieves a list of top-level PICS that should be checked before running this test
