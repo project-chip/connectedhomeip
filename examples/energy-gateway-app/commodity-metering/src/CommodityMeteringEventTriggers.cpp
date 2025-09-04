a2dbfd022536cd98a3d293dc985cb48100406ed7
dsavitsky-dsr <dmitry.savitskiy@dsr-corporation.com>
2025-09-04 12:55:26 +0300

    Fixed potential type conversion issue

--- examples/energy-gateway-app/commodity-metering/src/CommodityMeteringEventTriggers.cpp
+++ examples/energy-gateway-app/commodity-metering/src/CommodityMeteringEventTriggers.cpp
@@ -219,7 +219,7 @@ private:
 
         std::array<Structs::MeteredQuantityStruct::Type, MAX_MQ_SAMPLES> mqBuffer;
 
-        std::copy(MQSamplePair.first.begin(), MQSamplePair.first.begin() + MQSamplePair.second, mqBuffer.data());
+        std::copy_n(MQSamplePair.first.begin(), MQSamplePair.second, mqBuffer.data());
 
         DataModel::List<Structs::MeteredQuantityStruct::Type> tmpList(mqBuffer.data(), MQSamplePair.second);
         DataModel::Nullable<DataModel::List<Structs::MeteredQuantityStruct::Type>> nullableList;
