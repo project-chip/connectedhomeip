294dc4d05ea22a857c567d0e6b6de9e4c8b6476a
Restyled.io <commits@restyled.io>
2025-09-08 11:50:32 +0000

    Restyled by clang-format

--- src/include/platform/tests/TestAttributeList.cpp
+++ src/include/platform/tests/TestAttributeList.cpp
@@ -2,18 +2,18 @@
 
 #include <platform/AttributeList.h>
 
-namespace chip
-{
-namespace DeviceLayer
-{
+namespace chip {
+namespace DeviceLayer {
 
-class TestAttributeList : public ::testing::Test {};
+class TestAttributeList : public ::testing::Test
+{
+};
 
 // Test an empty list
 TEST_F(TestAttributeList, TestEmpty)
 {
     AttributeList<int, 5> list;
-    EXPECT_EQ(list.size(), (size_t)0);
+    EXPECT_EQ(list.size(), (size_t) 0);
     EXPECT_EQ(list.begin(), list.end());
 
     // This will abort/die
@@ -26,22 +26,22 @@ TEST_F(TestAttributeList, TestAddAndOverflow)
     AttributeList<int, 5> list;
 
     EXPECT_EQ(list.add(10), CHIP_NO_ERROR);
-    EXPECT_EQ(list.size(), (size_t)1);
+    EXPECT_EQ(list.size(), (size_t) 1);
 
     EXPECT_EQ(list.add(20), CHIP_NO_ERROR);
-    EXPECT_EQ(list.size(), (size_t)2);
+    EXPECT_EQ(list.size(), (size_t) 2);
 
     EXPECT_EQ(list.add(30), CHIP_NO_ERROR);
-    EXPECT_EQ(list.size(), (size_t)3);
+    EXPECT_EQ(list.size(), (size_t) 3);
 
     EXPECT_EQ(list.add(40), CHIP_NO_ERROR);
-    EXPECT_EQ(list.size(), (size_t)4);
+    EXPECT_EQ(list.size(), (size_t) 4);
 
     EXPECT_EQ(list.add(50), CHIP_NO_ERROR);
-    EXPECT_EQ(list.size(), (size_t)5);
+    EXPECT_EQ(list.size(), (size_t) 5);
 
     EXPECT_EQ(list.add(60), CHIP_ERROR_NO_MEMORY);
-    EXPECT_EQ(list.size(), (size_t)5);
+    EXPECT_EQ(list.size(), (size_t) 5);
 }
 
 // Test operator[]
@@ -53,7 +53,7 @@ TEST_F(TestAttributeList, TestIndexing)
     EXPECT_EQ(list.add(20), CHIP_NO_ERROR);
     EXPECT_EQ(list.add(30), CHIP_NO_ERROR);
 
-    ASSERT_EQ(list.size(), (size_t)3);
+    ASSERT_EQ(list.size(), (size_t) 3);
 
     EXPECT_EQ(list[0], 10);
     EXPECT_EQ(list[1], 20);
@@ -79,7 +79,7 @@ TEST_F(TestAttributeList, TestIterator)
     EXPECT_EQ(list.add(30), CHIP_NO_ERROR);
     EXPECT_NE(list.begin(), list.end());
 
-    ASSERT_EQ(list.size(), (size_t)3);
+    ASSERT_EQ(list.size(), (size_t) 3);
 
     auto it = list.begin();
     EXPECT_EQ(*it, 10);
@@ -95,5 +95,5 @@ TEST_F(TestAttributeList, TestIterator)
     EXPECT_EQ(it, list.end());
 };
 
-}
-}
+} // namespace DeviceLayer
+} // namespace chip
