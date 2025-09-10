40664de741ac625e6cea2850bd12e7ef578d515b
Restyled.io <commits@restyled.io>
2025-09-10 20:14:52 +0000

    Restyled by clang-format

--- src/app/clusters/descriptor/CodegenIntegration.cpp
+++ src/app/clusters/descriptor/CodegenIntegration.cpp
@@ -42,7 +42,8 @@ public:
         return gServers[emberEndpointIndex].Registration();
     }
 
-    ServerClusterInterface & FindRegistration(unsigned emberEndpointIndex) override {
+    ServerClusterInterface & FindRegistration(unsigned emberEndpointIndex) override
+    {
         return gServers[emberEndpointIndex].Cluster();
     }
     void ReleaseRegistration(unsigned emberEndpointIndex) override { gServers[emberEndpointIndex].Destroy(); }
