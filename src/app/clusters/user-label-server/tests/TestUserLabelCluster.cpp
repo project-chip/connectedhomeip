42c48b92e1eafae5c7e5f60f9ca4eba0511592de
arielsz71 <asztejnberg@griddynamics.com>
2025-09-11 16:56:28 -0300

    Decouple user-label cluster

---
+++ src/app/clusters/user-label-server/tests/TestUserLabelCluster.cpp
@@ -0,0 +1,130 @@
+/*
+ *    Copyright (c) 2025 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <pw_unit_test/framework.h>
+
+#include <app/clusters/user-label-server/user-label-cluster.h>
+
+#include <app/clusters/testing/AttributeTesting.h>
+#include <app/server-cluster/AttributeListBuilder.h>
+#include <app/server-cluster/DefaultServerCluster.h>
+#include <clusters/UserLabel/Attributes.h>
+#include <clusters/UserLabel/Metadata.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::UserLabel;
+using namespace chip::app::Clusters::UserLabel::Attributes;
+
+namespace {
+
+struct TestUserLabelCluster : public ::testing::Test
+{
+    static void SetUpTestSuite() { ASSERT_EQ(Platform::MemoryInit(), CHIP_NO_ERROR); }
+    static void TearDownTestSuite() { Platform::MemoryShutdown(); }
+};
+
+constexpr size_t kUserLabelFixedClusterCount = 2;
+
+class UserLabelClusterTest
+{
+    UserLabelCluster userLabel;
+
+public:
+    template <typename... Args>
+    UserLabelClusterTest(Args &&... args) : userLabel(std::forward<Args>(args)...)
+    {}
+
+    template <typename F>
+    void Check(F check)
+    {
+        check(userLabel);
+    }
+};
+
+class ReadAttribute
+{
+    UserLabelCluster & mCluster;
+    DataModel::ActionReturnStatus mStatus;
+
+public:
+    ReadAttribute(UserLabelCluster & cluster) : mCluster(cluster), mStatus(CHIP_NO_ERROR) {}
+
+    DataModel::ActionReturnStatus GetStatus() const { return mStatus; }
+
+    void operator()(DataModel::ReadAttributeRequest & request)
+    {
+        Platform::ScopedMemoryBufferWithSize<uint8_t> buffer;
+        ASSERT_NE(buffer.Alloc(1024).Get(), nullptr);
+
+        AttributeReportIBs::Builder attributeReportIBsBuilder;
+        TLV::TLVWriter reportWriter;
+        reportWriter.Init(buffer.Get(), buffer.AllocatedSize());
+        ASSERT_EQ(attributeReportIBsBuilder.Init(&reportWriter), CHIP_NO_ERROR);
+
+        AttributeValueEncoder encoder(attributeReportIBsBuilder, Access::SubjectDescriptor{}, request.path, 0 /* dataVersion */);
+
+        mStatus = mCluster.ReadAttribute(request, encoder);
+    }
+};
+
+} // namespace
+
+TEST_F(TestUserLabelCluster, AttributeTest)
+{
+    for (EndpointId endpoint = 0; endpoint < kUserLabelFixedClusterCount; ++endpoint)
+    {
+        UserLabelClusterTest(endpoint).Check([&](UserLabelCluster & userLabel) {
+            ReadOnlyBufferBuilder<DataModel::AttributeEntry> attributes;
+            ASSERT_EQ(userLabel.Attributes(ConcreteClusterPath(endpoint, UserLabel::Id), attributes), CHIP_NO_ERROR);
+
+            ReadOnlyBufferBuilder<DataModel::AttributeEntry> expected;
+            AttributeListBuilder listBuilder(expected);
+            ASSERT_EQ(listBuilder.Append(Span(UserLabel::Attributes::kMandatoryMetadata), {}), CHIP_NO_ERROR);
+            ASSERT_TRUE(Testing::EqualAttributeSets(attributes.TakeBuffer(), expected.TakeBuffer()));
+        });
+    }
+}
+
+TEST_F(TestUserLabelCluster, ReadAttributeTest)
+{
+    for (EndpointId endpoint = 0; endpoint < kUserLabelFixedClusterCount; ++endpoint)
+    {
+        UserLabelClusterTest(endpoint).Check([&](UserLabelCluster & userLabel) {
+            DataModel::ReadAttributeRequest request;
+            request.path.mEndpointId  = endpoint;
+            request.path.mClusterId   = UserLabel::Id;
+            request.path.mAttributeId = Globals::Attributes::ClusterRevision::Id;
+
+            ReadAttribute readAttribute(userLabel);
+            readAttribute(request);
+            EXPECT_TRUE(readAttribute.GetStatus().IsSuccess());
+
+            request.path.mAttributeId = FeatureMap::Id;
+            readAttribute(request);
+            EXPECT_TRUE(readAttribute.GetStatus().IsSuccess());
+
+            request.path.mAttributeId = LabelList::Id;
+            readAttribute(request);
+            EXPECT_TRUE(readAttribute.GetStatus().IsSuccess());
+
+            request.path.mAttributeId = 0xFFFF;
+            readAttribute(request);
+            EXPECT_TRUE(readAttribute.GetStatus().IsError());
+        });
+    }
+}
---
+++ src/app/clusters/user-label-server/user-label-cluster.cpp
@@ -0,0 +1,166 @@
+/*
+ *    Copyright (c) 2025 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <app/clusters/user-label-server/user-label-cluster.h>
+#include <app/server-cluster/AttributeListBuilder.h>
+#include <clusters/UserLabel/Metadata.h>
+
+namespace chip::app::Clusters {
+
+using namespace UserLabel;
+using namespace UserLabel::Attributes;
+
+namespace {
+
+CHIP_ERROR ReadLabelList(EndpointId endpoint, AttributeValueEncoder & encoder)
+{
+    DeviceLayer::DeviceInfoProvider * provider = DeviceLayer::GetDeviceInfoProvider();
+
+    if (provider)
+    {
+        DeviceLayer::DeviceInfoProvider::UserLabelIterator * it = provider->IterateUserLabel(endpoint);
+
+        if (it)
+        {
+            CHIP_ERROR err = encoder.EncodeList([&it](const auto & encod) -> CHIP_ERROR {
+                UserLabel::Structs::LabelStruct::Type userlabel;
+
+                while (it->Next(userlabel))
+                {
+                    ReturnErrorOnFailure(encod.Encode(userlabel));
+                }
+
+                return CHIP_NO_ERROR;
+            });
+
+            it->Release();
+            return err;
+        }
+    }
+
+    return encoder.EncodeEmptyList();
+}
+
+/// Matches constraints on a LabelStruct.
+bool IsValidLabelEntry(const Structs::LabelStruct::Type & entry)
+{
+    constexpr size_t kMaxLabelSize = 16;
+    constexpr size_t kMaxValueSize = 16;
+
+    // NOTE: spec default for label and value is empty, so empty is accepted here
+    return (entry.label.size() <= kMaxLabelSize) && (entry.value.size() <= kMaxValueSize);
+}
+
+bool IsValidLabelEntryList(const LabelList::TypeInfo::DecodableType & list)
+{
+    auto iter = list.begin();
+    while (iter.Next())
+    {
+        if (!IsValidLabelEntry(iter.GetValue()))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+CHIP_ERROR WriteLabelList(const ConcreteDataAttributePath & path, AttributeValueDecoder & decoder)
+{
+    DeviceLayer::DeviceInfoProvider * provider = DeviceLayer::GetDeviceInfoProvider();
+
+    VerifyOrReturnError(provider != nullptr, CHIP_ERROR_NOT_IMPLEMENTED);
+
+    EndpointId endpoint = path.mEndpointId;
+
+    if (!path.IsListItemOperation())
+    {
+        DeviceLayer::AttributeList<Structs::LabelStruct::Type, DeviceLayer::kMaxUserLabelListLength> labelList;
+        LabelList::TypeInfo::DecodableType decodablelist;
+
+        ReturnErrorOnFailure(decoder.Decode(decodablelist));
+        VerifyOrReturnError(IsValidLabelEntryList(decodablelist), CHIP_IM_GLOBAL_STATUS(ConstraintError));
+
+        auto iter = decodablelist.begin();
+        while (iter.Next())
+        {
+            auto & entry = iter.GetValue();
+            ReturnErrorOnFailure(labelList.add(entry));
+        }
+        ReturnErrorOnFailure(iter.GetStatus());
+
+        return provider->SetUserLabelList(endpoint, labelList);
+    }
+
+    if (path.mListOp == ConcreteDataAttributePath::ListOperation::AppendItem)
+    {
+        Structs::LabelStruct::DecodableType entry;
+
+        ReturnErrorOnFailure(decoder.Decode(entry));
+        VerifyOrReturnError(IsValidLabelEntry(entry), CHIP_IM_GLOBAL_STATUS(ConstraintError));
+
+        // Append the single user label entry
+        CHIP_ERROR err = provider->AppendUserLabel(endpoint, entry);
+        if (err == CHIP_ERROR_NO_MEMORY)
+        {
+            return CHIP_IM_GLOBAL_STATUS(ResourceExhausted);
+        }
+
+        return err;
+    }
+
+    return CHIP_ERROR_UNSUPPORTED_CHIP_FEATURE;
+}
+
+} // namespace
+
+UserLabelCluster::UserLabelCluster(EndpointId endpoint) : DefaultServerCluster({ endpoint, UserLabel::Id }) {}
+
+DataModel::ActionReturnStatus UserLabelCluster::ReadAttribute(const DataModel::ReadAttributeRequest & request,
+                                                              AttributeValueEncoder & encoder)
+{
+    switch (request.path.mAttributeId)
+    {
+    case LabelList::Id:
+        return ReadLabelList(mPath.mEndpointId, encoder);
+    case ClusterRevision::Id:
+        return encoder.Encode(UserLabel::kRevision);
+    case FeatureMap::Id:
+        return encoder.Encode<uint32_t>(0);
+    default:
+        return Protocols::InteractionModel::Status::UnsupportedAttribute;
+    }
+}
+
+DataModel::ActionReturnStatus UserLabelCluster::WriteAttribute(const DataModel::WriteAttributeRequest & request,
+                                                               AttributeValueDecoder & decoder)
+{
+    switch (request.path.mAttributeId)
+    {
+    case LabelList::Id:
+        return WriteLabelList(request.path, decoder);
+    default:
+        return Protocols::InteractionModel::Status::UnsupportedWrite;
+    }
+}
+
+CHIP_ERROR UserLabelCluster::Attributes(const ConcreteClusterPath & path,
+                                        ReadOnlyBufferBuilder<DataModel::AttributeEntry> & builder)
+{
+    AttributeListBuilder listBuilder(builder);
+    return listBuilder.Append(Span(UserLabel::Attributes::kMandatoryMetadata), {});
+}
+
+} // namespace chip::app::Clusters
