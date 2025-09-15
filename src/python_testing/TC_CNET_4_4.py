c8179304e7a3e3f3cb2572a95214ab5cc698229a
Restyled.io <commits@restyled.io>
2025-09-15 19:41:06 +0000

    Restyled by isort

--- src/python_testing/TC_CNET_4_4.py
+++ src/python_testing/TC_CNET_4_4.py
@@ -24,7 +24,8 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.clusters.Types import NullValue
-from matter.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, matchers, run_if_endpoint_matches, has_feature
+from matter.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_feature, matchers,
+                                           run_if_endpoint_matches)
 
 
 class TC_CNET_4_4(MatterBaseTest):
