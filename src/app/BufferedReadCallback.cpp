da1e2d96124194369ae98193dde268447ad0ff9c
Abdul Samad <abdul.samad@smartthings.com>
2025-09-15 11:21:16 -0500

    Revert TLV writer init with chained buffer from #40552

--- src/app/BufferedReadCallback.cpp
+++ src/app/BufferedReadCallback.cpp
@@ -121,12 +121,7 @@ CHIP_ERROR BufferedReadCallback::BufferListItem(TLV::TLVReader & reader)
     handle               = System::PacketBufferHandle::New(bufSize);
     VerifyOrReturnError(!handle.IsNull(), CHIP_ERROR_NO_MEMORY);
 
-#if INET_CONFIG_ENABLE_TCP_ENDPOINT
-    // Support chained buffer
-    writer.Init(std::move(handle), true);
-#else
     writer.Init(std::move(handle), false);
-#endif
 
     ReturnErrorOnFailure(writer.CopyElement(TLV::AnonymousTag(), reader));
     ReturnErrorOnFailure(writer.Finalize(&handle));
