79edc4e04e84900918900729218ae39ecd4db221
Zaid Omer <zaidomer@google.com>
2025-09-16 11:41:08 -0400

    Fix build errors

--- src/app/util/generic-callbacks.h
+++ src/app/util/generic-callbacks.h
@@ -147,5 +147,5 @@ chip::Protocols::InteractionModel::Status MatterPreAttributeChangeCallback(const
 void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & attributePath, uint8_t type, uint16_t size,
                                        uint8_t * value);
 
-void MatterCodedrivenClusterServerInitCallback(EndpointId endpoint, ClusterId clusterId);
-void MatterCodedrivenClusterServerShutdownCallback(EndpointId endpoint, ClusterId clusterId);
+void MatterCodedrivenClusterServerInitCallback(chip::EndpointId endpoint, chip::ClusterId clusterId);
+void MatterCodedrivenClusterServerShutdownCallback(chip::EndpointId endpoint, chip::ClusterId clusterId);
