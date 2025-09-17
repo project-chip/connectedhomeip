b676c3ff49099b012eae80ef7770526ce1a961bb
mahesh <maheshpimple2002@gmail.com>
2025-09-17 16:23:39 +0530

    feat(insights_trace): add version-independent generic APIs

---
+++ src/tracing/esp32_trace/insights_api.h
@@ -0,0 +1,35 @@
+/*
+ *
+ *    Copyright (c) 2025 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ */
+
+#pragma once
+#include <esp_diagnostics_metrics.h>
+
+esp_err_t chip_esp_insights_add_uint(const char * tag, const char * key, uint32_t u)
+{
+#ifdef CONFIG_ESP_INSIGHTS_META_VERSION_10
+    (void) tag; // tag is not used in this version
+    return esp_diag_metrics_add_uint(key, u);
+#else
+    return esp_diag_metrics_report_uint(tag, key, u);
+#endif
+}
+
+esp_err_t chip_esp_insights_add_int(const char * tag, const char * key, int32_t i)
+{
+#ifdef CONFIG_ESP_INSIGHTS_META_VERSION_10
+    (void) tag; // tag is not used in this version
+    return esp_diag_metrics_add_int(key, i);
+#else
+    return esp_diag_metrics_report_int(tag, key, i);
+#endif
+}
