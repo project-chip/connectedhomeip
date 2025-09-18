6557f6c5270622f9d85537a301e2128705e19cff
Ramiro Recchia <rrecchia@google.com>
2025-09-18 11:25:27 -0300

    Reused same variable and skip step 7

--- src/python_testing/TC_DISHM_2_1.py
+++ src/python_testing/TC_DISHM_2_1.py
@@ -106,6 +106,8 @@ class TC_DISHM_2_1(MatterBaseTest):
             INVALID_IN_MODE = 0x03
 
         cluster = Clusters.Objects.DishwasherMode
+        supported_modes_attribute = cluster.Attributes.SupportedModes
+        current_mode_attribute = cluster.Attributes.CurrentMode
         endpoint = self.get_endpoint(default=1)
         self.mode_fail = self.matter_test_config.global_test_params['PIXIT.DISHM.MODE_CHANGE_FAIL']
         self.mode_ok = self.matter_test_config.global_test_params['PIXIT.DISHM.MODE_CHANGE_OK']
@@ -116,7 +118,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the SupportedModes attribute
         self.step(2)
-        supported_modes_attribute = cluster.Attributes.SupportedModes
         supported_modes_dut = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=supported_modes_attribute)
         modes = [m.mode for m in supported_modes_dut]
 
@@ -136,7 +137,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(3)
-        current_mode_attribute = cluster.Attributes.CurrentMode
         old_current_mode_dut = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value
@@ -177,7 +177,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(6)
-        current_mode_attribute = cluster.Attributes.CurrentMode
         old_current_mode_dut = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value
@@ -186,32 +185,28 @@ class TC_DISHM_2_1(MatterBaseTest):
         # TH sends a ChangeToMode command to the DUT with NewMode set to PIXIT.DISHM.MODE_CHANGE_FAIL
         # NOTE: Skip this step as SDK is not enabled with this failure response
 
-        if True:
-            self.skip_step(7)
-        else:
-            self.step(7)
-            cmd = cluster.Commands.ChangeToMode(newMode=self.mode_fail)
-            change_to_mode_response = await self.send_single_cmd(cmd=cmd, endpoint=endpoint)
+        self.skip_step(7)
+        cmd = cluster.Commands.ChangeToMode(newMode=self.mode_fail)
+        change_to_mode_response = await self.send_single_cmd(cmd=cmd, endpoint=endpoint)
 
-            # DUT responds contains a ChangeToModeResponse command with Status field is set to GenericFailure(0x02), InvalidInMode(0x03), or in the MfgCodes (0x80 to 0xBF) range and StatusText field has a length between 1 and 64
-            asserts.assert_true(matchers.is_type(change_to_mode_response, cluster.Commands.ChangeToModeResponse),
-                                "Unexpected return type for ChangeToMode")
+        # DUT responds contains a ChangeToModeResponse command with Status field is set to GenericFailure(0x02), InvalidInMode(0x03), or in the MfgCodes (0x80 to 0xBF) range and StatusText field has a length between 1 and 64
+        asserts.assert_true(matchers.is_type(change_to_mode_response, cluster.Commands.ChangeToModeResponse),
+                            "Unexpected return type for ChangeToMode")
 
-            logging.info(f"response: {change_to_mode_response}")
+        logging.info(f"response: {change_to_mode_response}")
 
-            st = change_to_mode_response.status
+        st = change_to_mode_response.status
 
-            is_mfg_code = st in range(0x80, 0xC0)
-            is_err_code = (st == CommonCodes.GENERIC_FAILURE.value) or (st == CommonCodes.INVALID_IN_MODE.value) or is_mfg_code
+        is_mfg_code = st in range(0x80, 0xC0)
+        is_err_code = (st == CommonCodes.GENERIC_FAILURE.value) or (st == CommonCodes.INVALID_IN_MODE.value) or is_mfg_code
 
-            asserts.assert_true(is_err_code, f"Changing to mode {self.mode_fail} must fail due to the current state of the device")
+        asserts.assert_true(is_err_code, f"Changing to mode {self.mode_fail} must fail due to the current state of the device")
 
-            st_text_len = len(change_to_mode_response.statusText)
-            asserts.assert_true(st_text_len in range(1, 65), f"StatusText length {st_text_len} must be between 1 and 64")
+        st_text_len = len(change_to_mode_response.statusText)
+        asserts.assert_true(st_text_len in range(1, 65), f"StatusText length {st_text_len} must be between 1 and 64")
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(8)
-        current_mode_attribute = cluster.Attributes.CurrentMode
         old_current_mode_dut_2 = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value
@@ -231,7 +226,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(10)
-        current_mode_attribute = cluster.Attributes.CurrentMode
         old_current_mode_dut = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value
@@ -250,7 +244,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(12)
-        current_mode_attribute = cluster.Attributes.CurrentMode
         current_mode = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value and it is the same as step 11
@@ -269,8 +262,6 @@ class TC_DISHM_2_1(MatterBaseTest):
 
         # TH reads from the DUT the CurrentMode attribute
         self.step(14)
-
-        current_mode_attribute = cluster.Attributes.CurrentMode
         current_mode = await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=current_mode_attribute)
 
         # CurrentMode attribute value is an integer value and same as step 12
