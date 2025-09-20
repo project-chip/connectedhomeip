1a2894127e04139eb94f95d0a971a0c0818512a1
Raveendra Karu <r.karu@samsung.com>
2025-09-20 15:30:41 +0530

    Add clip recorder file management utilities
    Author: Tushant Yadav <t.yadav@samsung.com>
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- examples/camera-app/linux/include/pushav-clip-recorder.h
+++ examples/camera-app/linux/include/pushav-clip-recorder.h
@@ -206,8 +206,19 @@ private:
     /// @name Internal Methods
     /// @{
     bool FileExists(const std::string & path);
+
+    bool IsOutputDirectoryValid(const std::string & path);
+
+    /**
+     * @brief Removes files from previous recordings in the specified directory.
+     * @param path The directory path to clean.
+     */
+    void RemovePreviousRecordingFiles(const std::string & path);
+
     bool CheckAndUploadFile(std::string path);
+
     bool IsH264IFrame(const uint8_t * data, unsigned int length);
+
     AVPacket * CreatePacket(const uint8_t * data, int size, bool isVideo);
 
     /**
