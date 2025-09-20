745485a95be0c0e37848c452a28caa3f8ffd39e5
ehsanziyaee <ehsan.ziyaee@gmail.com>
2025-09-20 19:00:49 +0330

    cach endpointId in Init func and delete it from other places

--- examples/chef/common/clusters/oven-mode/chef-oven-mode.cpp
+++ examples/chef/common/clusters/oven-mode/chef-oven-mode.cpp
@@ -79,19 +79,18 @@ void InitChefOvenModeCluster()
 
 CHIP_ERROR OvenMode::ChefDelegate::Init()
 {
+    endpointId = GetInstance()->GetEndpointId();
     return CHIP_NO_ERROR;
 }
 
 void OvenMode::ChefDelegate::HandleChangeToMode(uint8_t NewMode, ModeBase::Commands::ChangeToModeResponse::Type & response)
 {
-    EndpointId endpointId = GetInstance()->GetEndpointId();
     ChipLogDetail(DeviceLayer, "HandleChangeToMode: Endpoint %d", endpointId);
     response.status = to_underlying(ModeBase::StatusCode::kSuccess);
 }
 
 CHIP_ERROR OvenMode::ChefDelegate::GetModeLabelByIndex(uint8_t modeIndex, chip::MutableCharSpan & label)
 {
-    EndpointId endpointId = GetInstance()->GetEndpointId();
     ChipLogDetail(DeviceLayer, "GetModeLabelByIndex: Endpoint %d", endpointId);
     if (modeIndex >= MATTER_ARRAY_SIZE(kModeOptions))
     {
@@ -102,7 +101,6 @@ CHIP_ERROR OvenMode::ChefDelegate::GetModeLabelByIndex(uint8_t modeIndex, chip::
 
 CHIP_ERROR OvenMode::ChefDelegate::GetModeValueByIndex(uint8_t modeIndex, uint8_t & value)
 {
-    EndpointId endpointId = GetInstance()->GetEndpointId();
     ChipLogDetail(DeviceLayer, "GetModeValueByIndex: Endpoint %d", endpointId);
     if (modeIndex >= MATTER_ARRAY_SIZE(kModeOptions))
     {
@@ -114,7 +112,6 @@ CHIP_ERROR OvenMode::ChefDelegate::GetModeValueByIndex(uint8_t modeIndex, uint8_
 
 CHIP_ERROR OvenMode::ChefDelegate::GetModeTagsByIndex(uint8_t modeIndex, List<ModeTagStructType> & tags)
 {
-    EndpointId endpointId = GetInstance()->GetEndpointId();
     ChipLogDetail(DeviceLayer, "GetModeTagsByIndex: Endpoint %d", endpointId);
     if (modeIndex >= MATTER_ARRAY_SIZE(kModeOptions))
     {
