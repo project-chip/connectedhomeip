ed14d33138750949cd5e3e3313d8a23bf2d8a82d
ehsanziyaee <ehsan.ziyaee@gmail.com>
2025-09-21 14:57:56 +0330

    update doxygen

--- src/app/clusters/mode-base-server/mode-base-server.h
+++ src/app/clusters/mode-base-server/mode-base-server.h
@@ -260,9 +260,9 @@ private:
 
 protected:
     /**
-     * @brief Provides access to the Instance pointer.
+     * @brief Provides access to the const Instance pointer.
      *
-     * @return A pointer to the Instance object associated with this delegate.
+     * @return A xonst pointer to the Instance object associated with this delegate.
      */
     const Instance * GetInstance() const { return mInstance; }
 
