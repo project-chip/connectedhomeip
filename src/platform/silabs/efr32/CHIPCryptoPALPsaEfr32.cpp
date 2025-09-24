566157c8a426e9b4fb3b611fae38c93a69bd0fe5
Mathieu Kardous <mathieu.kardous@silabs.com>
2025-09-24 11:53:24 -0400

    Update specific changes

--- src/platform/silabs/efr32/CHIPCryptoPALPsaEfr32.cpp
+++ src/platform/silabs/efr32/CHIPCryptoPALPsaEfr32.cpp
@@ -2219,7 +2219,7 @@ CHIP_ERROR ExtractRawDNFromX509Cert(bool extractSubject, const ByteSpan & certif
     size_t len       = 0;
     mbedtls_x509_crt mbedCertificate;
 
-    ReturnErrorCodeIf(certificate.empty(), CHIP_ERROR_INVALID_ARGUMENT);
+    VerifyOrReturnError(!certificate.empty(), CHIP_ERROR_INVALID_ARGUMENT);
 
     mbedtls_x509_crt_init(&mbedCertificate);
     result = mbedtls_x509_crt_parse(&mbedCertificate, Uint8::to_const_uchar(certificate.data()), certificate.size());
@@ -2275,7 +2275,7 @@ CHIP_ERROR ReplaceCertIfResignedCertFound(const ByteSpan & referenceCertificate,
 
     outCertificate = referenceCertificate;
 
-    ReturnErrorCodeIf(candidateCertificates == nullptr || candidateCertificatesCount == 0, CHIP_NO_ERROR);
+    VerifyOrReturnError(candidateCertificates != nullptr && candidateCertificatesCount != 0, CHIP_NO_ERROR);
 
     ReturnErrorOnFailure(ExtractSubjectFromX509Cert(referenceCertificate, referenceSubject));
     ReturnErrorOnFailure(ExtractSKIDFromX509Cert(referenceCertificate, referenceSKID));
--- src/platform/silabs/efr32/OTAImageProcessorImpl.cpp
+++ src/platform/silabs/efr32/OTAImageProcessorImpl.cpp
@@ -224,18 +224,11 @@ void OTAImageProcessorImpl::HandleApply(intptr_t context)
         return;
     }
 #endif // SL_BTLCTRL_MUX
-
-#if defined(_SILICON_LABS_32B_SERIES_3) && CHIP_PROGRESS_LOGGING
-    osDelay(100); // sl-temp: delay for uart print before verifyImage
-#endif            // _SILICON_LABS_32B_SERIES_3 && CHIP_PROGRESS_LOGGING
-    LockRadioProcessing();
 #if defined(SL_TRUSTZONE_NONSECURE)
-    WRAP_BL_DFU_CALL(err = bootloader_verifyImage(mSlotId))
+    CORE_CRITICAL_SECTION(err = bootloader_verifyImage(mSlotId);)
 #else
-    WRAP_BL_DFU_CALL(err = bootloader_verifyImage(mSlotId, NULL))
+    CORE_CRITICAL_SECTION(err = bootloader_verifyImage(mSlotId, NULL);)
 #endif
-    UnlockRadioProcessing();
-
     if (err != SL_BOOTLOADER_OK)
     {
         ChipLogError(SoftwareUpdate, "bootloader_verifyImage() error: %ld", err);
