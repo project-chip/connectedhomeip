bbc8fed25db07dd566ebb0e0e04491a11a6d0c13
Moises Terrones <moises.tersal@gmail.com>
2025-09-25 10:38:32 -0600

    Regenerate zap files

--- scripts/tools/zap/tests/outputs/lighting-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/lighting-app/app-templates/endpoint_config.h
@@ -1339,7 +1339,7 @@
 // This is an array of EmberAfEndpointType structures.
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
-        { ZAP_CLUSTER_INDEX(0), 21, 111 },                                                                                         \
+        { ZAP_CLUSTER_INDEX(0), 21, 105 },                                                                                         \
         { ZAP_CLUSTER_INDEX(21), 8, 109 },                                                                                         \
     }
 
@@ -1352,7 +1352,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (220)
+#define ATTRIBUTE_MAX_SIZE (214)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (2)
