87d7bbe8f733cc297fccb15a0f81c2c1e31790cb
dsavitsky-dsr <dmitry.savitskiy@dsr-corporation.com>
2025-09-25 18:12:34 +0300

    Changes caused by trying of use Scoped Memory Buffers in implementation

--- src/app/clusters/commodity-tariff-server/commodity-tariff-server.cpp
+++ src/app/clusters/commodity-tariff-server/commodity-tariff-server.cpp
@@ -521,25 +521,37 @@ CHIP_ERROR UpdateTariffComponentAttrsDayEntryById(Instance * aInstance, CurrentT
     const Structs::TariffPeriodStruct::Type * period = FindTariffPeriodByDayEntryId(aCtx, dayEntryID);
 
     // Use a fixed-size array with maximum expected components
-    constexpr size_t MAX_COMPONENTS = 16; // Adjust this based on your maximum expected components
-    std::array<Structs::TariffComponentStruct::Type, MAX_COMPONENTS> tempArray;
-    size_t componentCount = 0;
+    Platform::ScopedMemoryBufferWithSize<Structs::TariffComponentStruct::Type> tempBuffer;
+    Platform::ScopedMemoryBufferWithSize<char> tempLabelBuffers[kTariffPeriodItemMaxIDs];
 
     if (period == nullptr)
     {
         return CHIP_ERROR_NOT_FOUND;
     }
     const DataModel::List<const uint32_t> & componentIDs = period->tariffComponentIDs;
+    const size_t componentCount = componentIDs.size();
 
-    for (const auto & entryID : componentIDs)
+    // Validate component count
+    if (componentCount == 0 || componentCount > kTariffPeriodItemMaxIDs)
+    {
+        return CHIP_ERROR_INVALID_LIST_LENGTH;
+    }
+
+    // Allocate memory for the component array
+    if (!tempBuffer.Calloc(componentCount))
+    {
+        return CHIP_ERROR_NO_MEMORY;
+    }
+
+    for (size_t i = 0; i < componentIDs.size(); i++)
     {
         Structs::TariffComponentStruct::Type entry;
         auto current =
-            GetListEntryById<Structs::TariffComponentStruct::Type>(aCtx.mTariffProvider->GetTariffComponents().Value(), entryID);
+            GetListEntryById<Structs::TariffComponentStruct::Type>(aCtx.mTariffProvider->GetTariffComponents().Value(), componentIDs[i]);
         if (current == nullptr)
         {
             err = CHIP_ERROR_NOT_FOUND;
-            goto exit;
+            break;
         }
         entry = *current;
         if (current->label.HasValue())
@@ -549,20 +561,17 @@ CHIP_ERROR UpdateTariffComponentAttrsDayEntryById(Instance * aInstance, CurrentT
             if (!current->label.Value().IsNull())
             {
                 chip::CharSpan srcLabelSpan = current->label.Value().Value();
-                if (CHIP_NO_ERROR !=
-                    (err = CommodityTariffAttrsDataMgmt::SpanCopier<char>::CopyToNullable(current->label.Value().Value(),
-                                                                                          tmpNullLabel, srcLabelSpan.size())))
-                {
-                    goto exit;
-                }
+                tempLabelBuffers[i].CopyFromSpan(srcLabelSpan);
+                tmpNullLabel.SetNonNull(chip::CharSpan(tempLabelBuffers[i].Get(), srcLabelSpan.size()));
             }
             entry.label = MakeOptional(tmpNullLabel);
         }
-        tempArray[componentCount++] = entry;
+        tempBuffer[i] = entry;
     }
+    SuccessOrExit(err);
 
     err =
-        mgmtObj.SetNewValue(MakeNullable(DataModel::List<Structs::TariffComponentStruct::Type>(tempArray.data(), componentCount)));
+        mgmtObj.SetNewValue(MakeNullable(DataModel::List<Structs::TariffComponentStruct::Type>(tempBuffer.Get(), componentCount)));
     SuccessOrExit(err);
 
     err = mgmtObj.UpdateBegin(nullptr);
@@ -574,11 +583,6 @@ CHIP_ERROR UpdateTariffComponentAttrsDayEntryById(Instance * aInstance, CurrentT
     }
 
 exit:
-    for (size_t i = 0; i < componentCount; i++)
-    {
-        mgmtObj.CleanupExtListEntry(tempArray[i]);
-    }
-
     return err;
 }
 } // namespace Utils
