12e284b120260987d0a86b4f1dd369155ff4e444
marybadalyan <marybadalyangdh@gmail.com>
2025-09-27 14:47:45 +0000

    Refactor

--- src/controller/tests/AutoCommissionerTestAccess.h
+++ src/controller/tests/AutoCommissionerTestAccess.h
@@ -97,9 +97,9 @@ public:
     bool IsScanNeeded() { return mCommissioner->IsScanNeeded(); }
     bool IsSecondaryNetworkSupported() const { return mCommissioner->IsSecondaryNetworkSupported(); }
     Controller::ReadCommissioningInfo & GetDeviceCommissioningInfo() { return mCommissioner->mDeviceCommissioningInfo; }
-    void ResetTryingSecondaryNetwork() { mCommissioner->ResetTryingSecondaryNetwork(); }
+    void ResetNetworkAttemptType() { mCommissioner->ResetNetworkAttemptType(); }
 
-    bool TryingSecondaryNetwork() { return mCommissioner->TryingSecondaryNetwork(); }
+    bool TryingSecondaryNetwork() const { return mCommissioner->TryingSecondaryNetwork(); }
     void TrySecondaryNetwork() { mCommissioner->TrySecondaryNetwork(); }
 
     void TrySecondaryNetwork() { mCommissioner->TrySecondaryNetwork(); }
