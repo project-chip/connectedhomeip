1de2422547daf8ded060cee27933ad80e4d81f81
Restyled.io <commits@restyled.io>
2025-09-29 15:43:01 +0000

    Restyled by isort

--- src/python_testing/TC_IDM_3_2.py
+++ src/python_testing/TC_IDM_3_2.py
@@ -42,8 +42,8 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.clusters import ClusterObjects as ClusterObjects
-from matter.interaction_model import InteractionModelError, Status
 from matter.exceptions import ChipStackError
+from matter.interaction_model import InteractionModelError, Status
 from matter.testing import global_attribute_ids
 from matter.testing.basic_composition import BasicCompositionTests
 from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
