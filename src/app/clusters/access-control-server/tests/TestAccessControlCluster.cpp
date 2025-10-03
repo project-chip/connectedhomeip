d7b71438bdaacce7efb72380404b5d7279d8095b
Zaid Omer <zaidomer@google.com>
2025-10-03 15:02:18 -0400

    Migrate Access Control Cluster to be Code Driven (#41064)
    * Code driven access control cluster code
    
    * Cleanup build file
    
    * Codegen
    
    * Remove old cluster impl
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Restyled by gn
    
    * Various build fixes
    
    * Restyled by clang-format
    
    * Remove non-existent header from cmake
    
    * Add/remove listener on start/shutdown
    
    * Update CodegenIntegration to use new init/shutdown callbacks
    
    * Codegen
    
    * Restyled by clang-format
    
    * Add correct error mapping
    
    * Restyled by clang-format
    
    * Fix typo
    
    * Fix EventInfo and logging
    
    * Remove unused variable
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Remove unused variable
    
    * Add Access Control to CommandHandlerInterfaceOnlyClusters
    
    * Codegen
    
    * Apply suggestions from code review
    
    Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>
    
    * Restyled by whitespace
    
    * Restyled by clang-format
    
    * Cleanup after applying suggestions
    
    * Remove return
    
    * Address review comments
    
    * Update config_data
    
    * Update return statement
    
    * Flash reduction changes
    
    * Restyled by clang-format
    
    * More flash reduction steps and address review comments
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Tennessee Carmel-Veilleux <tennessee.carmelveilleux@gmail.com>

---
+++ src/app/clusters/access-control-server/tests/TestAccessControlCluster.cpp
@@ -0,0 +1,103 @@
+/*
+ *    Copyright (c) 2025 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#include <pw_unit_test/framework.h>
+
+#include <app/clusters/access-control-server/access-control-cluster.h>
+#include <app/clusters/testing/AttributeTesting.h>
+#include <app/data-model-provider/MetadataTypes.h>
+#include <app/server-cluster/DefaultServerCluster.h>
+#include <clusters/AccessControl/Enums.h>
+#include <clusters/AccessControl/Metadata.h>
+#include <lib/core/CHIPError.h>
+#include <lib/core/DataModelTypes.h>
+#include <lib/support/BitFlags.h>
+#include <lib/support/ReadOnlyBuffer.h>
+#include <platform/NetworkCommissioning.h>
+
+namespace {
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using namespace chip::app::DataModel;
+
+struct TestAccessControlCluster : public ::testing::Test
+{
+    static void SetUpTestSuite() { ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR); }
+    static void TearDownTestSuite() { chip::Platform::MemoryShutdown(); }
+};
+
+TEST_F(TestAccessControlCluster, CompileTest)
+{
+    AccessControlCluster cluster;
+    ASSERT_EQ(cluster.GetClusterFlags({ kRootEndpointId, AccessControl::Id }), BitFlags<ClusterQualityFlags>());
+}
+
+TEST_F(TestAccessControlCluster, CommandsTest)
+{
+    AccessControlCluster cluster;
+    ConcreteClusterPath accessControlPath = ConcreteClusterPath(kRootEndpointId, AccessControl::Id);
+
+    ReadOnlyBufferBuilder<DataModel::AcceptedCommandEntry> acceptedCommandsBuilder;
+    ASSERT_EQ(cluster.AcceptedCommands(accessControlPath, acceptedCommandsBuilder), CHIP_NO_ERROR);
+    ReadOnlyBuffer<DataModel::AcceptedCommandEntry> acceptedCommands = acceptedCommandsBuilder.TakeBuffer();
+
+    ReadOnlyBufferBuilder<chip::CommandId> generatedCommandsBuilder;
+    ASSERT_EQ(cluster.GeneratedCommands(accessControlPath, generatedCommandsBuilder), CHIP_NO_ERROR);
+    ReadOnlyBuffer<chip::CommandId> generatedCommands = generatedCommandsBuilder.TakeBuffer();
+
+#if CHIP_CONFIG_USE_ACCESS_RESTRICTIONS
+    // Check accepted commands
+    ASSERT_EQ(acceptedCommands.size(), AccessControl::Commands::kAcceptedCommandsCount);
+    ASSERT_EQ(acceptedCommands[0].commandId, AccessControl::Commands::ReviewFabricRestrictions::Id);
+    ASSERT_EQ(acceptedCommands[0].GetInvokePrivilege(),
+              AccessControl::Commands::ReviewFabricRestrictions::kMetadataEntry.GetInvokePrivilege());
+
+    // Check generated commands
+    ASSERT_EQ(generatedCommands.size(), AccessControl::Commands::kGeneratedCommandsCount);
+    ASSERT_EQ(generatedCommands[0], AccessControl::Commands::ReviewFabricRestrictionsResponse::Id);
+#else
+    ASSERT_EQ(acceptedCommands.size(), (size_t) (0));
+    ASSERT_EQ(generatedCommands.size(), (size_t) (0));
+#endif
+}
+
+TEST_F(TestAccessControlCluster, AttributesTest)
+{
+    AccessControlCluster cluster;
+    ConcreteClusterPath accessControlPath = ConcreteClusterPath(kRootEndpointId, AccessControl::Id);
+
+    ReadOnlyBufferBuilder<DataModel::AttributeEntry> attributesBuilder;
+    ASSERT_EQ(cluster.Attributes(accessControlPath, attributesBuilder), CHIP_NO_ERROR);
+
+    ReadOnlyBufferBuilder<DataModel::AttributeEntry> expectedBuilder;
+    ASSERT_EQ(expectedBuilder.ReferenceExisting(DefaultServerCluster::GlobalAttributes()), CHIP_NO_ERROR);
+
+    ASSERT_EQ(expectedBuilder.AppendElements({
+#if CHIP_CONFIG_ENABLE_ACL_EXTENSIONS
+        AccessControl::Attributes::Extension::kMetadataEntry,
+#endif
+
+#if CHIP_CONFIG_USE_ACCESS_RESTRICTIONS
+            AccessControl::Attributes::CommissioningARL::kMetadataEntry, AccessControl::Attributes::Arl::kMetadataEntry
+#endif
+    }),
+              CHIP_NO_ERROR);
+    ASSERT_EQ(expectedBuilder.AppendElements(AccessControl::Attributes::kMandatoryMetadata), CHIP_NO_ERROR);
+    ASSERT_TRUE(Testing::EqualAttributeSets(attributesBuilder.TakeBuffer(), expectedBuilder.TakeBuffer()));
+}
+
+} // namespace
