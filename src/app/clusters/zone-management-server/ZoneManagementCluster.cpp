539cdcad4ebaff3824cb78c6c6005cf877816e32
Robert Szewczyk <szewczyk@google.com>
2025-10-05 17:03:05 -0700

    Restyle

--- src/app/clusters/zone-management-server/ZoneManagementCluster.cpp
+++ src/app/clusters/zone-management-server/ZoneManagementCluster.cpp
@@ -479,8 +479,8 @@ void ZoneManagementCluster::PersistZones()
     err = mContext->attributeStorage.WriteValue(ConcreteAttributePath(mPath.mEndpointId, ZoneManagement::Id, Attributes::Zones::Id),
                                                 bufferSpan);
     LogAndReturnOnFailure(err, Zcl, "ZoneManagement[ep=%d] failure saving zone: %s", mPath.mEndpointId, "write");
-    ChipLogProgress(Zcl, "ZoneManagement[ep=%d] persisted %u zones, wrote %d bytes", mPath.mEndpointId, static_cast<uint8_t>(mZones.size()),
-                    writer.GetLengthWritten());
+    ChipLogProgress(Zcl, "ZoneManagement[ep=%d] persisted %u zones, wrote %d bytes", mPath.mEndpointId,
+                    static_cast<uint8_t>(mZones.size()), writer.GetLengthWritten());
 }
 
 void ZoneManagementCluster::PersistTriggers()
@@ -509,8 +509,8 @@ void ZoneManagementCluster::PersistTriggers()
     err = mContext->attributeStorage.WriteValue(
         ConcreteAttributePath(mPath.mEndpointId, ZoneManagement::Id, Attributes::Triggers::Id), bufferSpan);
     LogAndReturnOnFailure(err, Zcl, "ZoneManagement[ep=%d] failure saving trigger: %s", mPath.mEndpointId, "write");
-    ChipLogProgress(Zcl, "ZoneManagement[ep=%d] persisted %u triggers, wrote %d bytes", mPath.mEndpointId, static_cast<uint8_t>(mTriggers.size()),
-                    writer.GetLengthWritten());
+    ChipLogProgress(Zcl, "ZoneManagement[ep=%d] persisted %u triggers, wrote %d bytes", mPath.mEndpointId,
+                    static_cast<uint8_t>(mTriggers.size()), writer.GetLengthWritten());
 }
 
 CHIP_ERROR ZoneManagementCluster::AddZone(const ZoneInformationStorage & zone)
