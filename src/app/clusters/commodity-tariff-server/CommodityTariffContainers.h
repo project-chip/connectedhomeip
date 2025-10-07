26cf78317d49a286f0305139972f10bd2ebbe49c
dsavitsky-dsr <dmitry.savitskiy@dsr-corporation.com>
2025-10-07 15:28:27 +0300

    Added few helper classes for data collections

---
+++ src/app/clusters/commodity-tariff-server/CommodityTariffContainers.h
@@ -0,0 +1,350 @@
+/**
+ *
+ *    Copyright (c) 2025 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include "CommodityTariffAttrsDataMgmt.h"
+#include <algorithm>
+#include <cstddef>
+#include <utility>
+
+namespace chip {
+namespace app {
+namespace CommodityTariffContainers {
+
+template<typename T>
+class CTC_ContainerClassBase
+{
+protected:
+    CTC_ContainerClassBase(size_t aCapacity, size_t aMaxLimit = 0) : mMaxLimit(aMaxLimit), mCapacity(aCapacity)
+    {
+        createBuffer();
+
+        if (mMaxLimit <= aCapacity)
+        {
+            mMaxLimit = mCapacity;
+        }
+    }
+    
+public:
+    virtual ~CTC_ContainerClassBase() = default;
+
+    enum class RetCode : uint8_t
+    {
+        kSuccess,
+        kNoInit,
+        kInUse,
+        kNoMem,
+        kGenericFail,
+        kNotFound,
+        kDuplicate,
+    };   
+
+    virtual bool insert(const T& item) = 0;
+    virtual void remove(const T& item) = 0;
+    virtual void clear()    { mCount = 0; };
+    virtual void sort() = 0;    
+    virtual bool contains(const T& item) const = 0;
+    virtual T* find(const T& item) = 0;
+    virtual const T* find(const T& item) const = 0;
+
+    size_t size() const { return mCount; }
+    size_t capacity() const { return mCapacity; }
+    bool empty() const { return (mCount == 0); }
+
+    // Safe accessors without exceptions
+    T* at(size_t index) 
+    { 
+        return (index < mCount) ? &mBuffer[index] : nullptr;
+    }
+    
+    const T* at(size_t index) const 
+    { 
+        return (index < mCount) ? &mBuffer[index] : nullptr;
+    }
+
+    // Array-style access
+    T& operator[](size_t index) { return mBuffer[index]; }
+    const T& operator[](size_t index) const { return mBuffer[index]; }
+
+    // Iterator support
+    T* begin() { return mBuffer.Get(); }
+    T* end() { return mBuffer.Get() + mCount; }
+    const T* begin() const { return mBuffer.Get(); }
+    const T* end() const { return mBuffer.Get() + mCount; }
+
+protected:
+    size_t mMaxLimit = 0;
+    size_t mCapacity = 0;
+    size_t mCount = 0;
+    Platform::ScopedMemoryBuffer<T> mBuffer;
+
+    RetCode createBuffer() {
+        if (mCapacity == 0) {
+            return RetCode::kNoInit;
+        }
+
+        if (!mBuffer.Calloc(mCapacity)) {
+            return RetCode::kNoMem;
+        }
+
+        mCount = 0;
+        return RetCode::kSuccess;
+    }
+
+    RetCode resize(size_t newCapacity) {
+        if (newCapacity == mCapacity) {
+            return RetCode::kSuccess;
+        }
+        
+        if (newCapacity < mCount) {
+            return RetCode::kInUse;
+        }
+        
+        chip::Platform::ScopedMemoryBuffer<T> newBuffer;
+        if (!newBuffer.Calloc(newCapacity)) {
+            return RetCode::kNoMem;
+        }
+        
+        // Use std::move for efficient transfer
+        if (mBuffer.Get() && mCount > 0) {
+            std::move(mBuffer.Get(), mBuffer.Get() + mCount, newBuffer.Get());
+        }
+        
+        mBuffer = std::move(newBuffer);
+        mCapacity = newCapacity;
+        return RetCode::kSuccess;
+    }
+
+    RetCode ensureCapacity(size_t requiredCapacity) {
+        if (requiredCapacity > mMaxLimit) 
+        {
+            return RetCode::kNoMem;
+        }
+
+        if (requiredCapacity <= mCapacity)
+        {
+            return RetCode::kSuccess;
+        }
+        
+        size_t newCapacity = std::max(requiredCapacity, mCapacity * 2);
+        return resize(newCapacity);
+    }
+
+    RetCode insertAt(size_t index, const T& item) {
+        if (index > mMaxLimit-1 ) {
+            return RetCode::kGenericFail;
+        }
+        
+        RetCode ret = ensureCapacity(mCount + 1);
+        if (ret != RetCode::kSuccess) {
+            return ret;
+        }
+        
+        // Shift items using std::move_backward
+        if (index < mCount) {
+            std::move_backward(mBuffer.Get() + index, mBuffer.Get() + mCount, 
+                              mBuffer.Get() + mCount + 1);
+        }
+        
+        mBuffer[index] = item;
+        ++mCount;
+        return RetCode::kSuccess;
+    }
+
+    void removeAt(size_t index) {
+        if (index >= mCount) return;
+        
+        // Shift items using std::move
+        if (index < mCount - 1) {
+            std::move(mBuffer.Get() + index + 1, mBuffer.Get() + mCount, 
+                     mBuffer.Get() + index);
+        }
+        
+        --mCount;
+    }
+};
+
+template<typename ItemType>
+class CTC_UnorderedSet : public CTC_ContainerClassBase<ItemType>
+{
+public:
+    using Base = CTC_ContainerClassBase<ItemType>;
+    using Base::Base;
+
+    CTC_UnorderedSet(size_t aCapacity) : Base(aCapacity) {}
+
+    bool insert(const ItemType& key) override {
+        // Check for duplicate using std::find
+        if (this->find(key) != nullptr) {
+            return false;
+        }
+        
+        // Append to the end (unordered)
+        return (this->insertAt(this->mCount, key) == Base::RetCode::kSuccess);
+    }
+
+    void remove(const ItemType& key) override {
+        auto* found = this->find(key);
+        if (found) {
+            size_t index = static_cast<size_t>(found - this->mBuffer.Get());
+            this->removeAt(index);
+        }
+    }
+
+    void sort() override {
+        // Sort the entire buffer
+        if (this->mCount > 0) {
+            std::sort(this->mBuffer.Get(), this->mBuffer.Get() + this->mCount);
+        }
+    }
+
+    bool contains(const ItemType& key) const override {
+        return this->find(key) != nullptr;
+    }
+
+    ItemType* find(const ItemType& key) override {
+        // Linear search using std::find
+        auto it = std::find(this->begin(), this->end(), key);
+        return (it != this->end()) ? it : nullptr;
+    }
+
+    const ItemType* find(const ItemType& key) const override {
+        // Const version of linear search
+        auto it = std::find(this->begin(), this->end(), key);
+        return (it != this->end()) ? it : nullptr;
+    }
+};
+
+template<typename KeyType, typename ValueType>
+class CTC_UnorderedMap
+{
+public:
+    using PairType = std::pair<KeyType, ValueType>;
+
+private:
+    // Custom set that only compares keys for uniqueness
+    class PairSet : public CTC_UnorderedSet<PairType>
+    {
+    public:
+        using Base = CTC_UnorderedSet<PairType>;
+        using Base::Base;
+
+        // Override find to only compare keys
+        PairType* find(const PairType& pair) override {
+            return findByKey(pair.first);
+        }
+
+        const PairType* find(const PairType& pair) const override {
+            return findByKey(pair.first);
+        }
+
+        PairType* findByKey(const KeyType& key) {
+            auto it = std::find_if(this->begin(), this->end(),
+                                  [&key](const PairType& item) {
+                                      return item.first == key;
+                                  });
+            return (it != this->end()) ? it : nullptr;
+        }
+
+        const PairType* findByKey(const KeyType& key) const {
+            auto it = std::find_if(this->begin(), this->end(),
+                                  [&key](const PairType& item) {
+                                      return item.first == key;
+                                  });
+            return (it != this->end()) ? it : nullptr;
+        }
+
+        // Override contains to only check key
+        bool contains(const PairType& pair) const override {
+            return containsKey(pair.first);
+        }
+
+        bool containsKey(const KeyType& key) const {
+            return findByKey(key) != nullptr;
+        }
+    };
+
+    PairSet mPairStorage;
+
+public:
+    CTC_UnorderedMap(size_t aCapacity) : mPairStorage(aCapacity) 
+    {
+    }
+
+    bool insert(const KeyType& key, const ValueType& value) {
+        return mPairStorage.insert(std::make_pair(key, value));
+    }
+
+    bool insert(const PairType& pair) {
+        return mPairStorage.insert(pair);
+    }
+
+    void remove(const KeyType& key) {
+        // We need to find the actual pair to remove
+        if (auto* pair = mPairStorage.findByKey(key)) {
+            mPairStorage.remove(*pair);
+        }
+    }
+
+    ValueType* getValue(const KeyType& key) {
+        auto* pair = mPairStorage.findByKey(key);
+        return pair ? &pair->second : nullptr;
+    }
+
+    const ValueType* getValue(const KeyType& key) const {
+        auto* pair = mPairStorage.findByKey(key);
+        return pair ? &pair->second : nullptr;
+    }
+
+    ValueType& operator[](const KeyType& key) {
+        auto* pair = mPairStorage.findByKey(key);
+        if (!pair) {
+            insert(key, ValueType{});
+            pair = mPairStorage.findByKey(key);
+        }
+        return pair->second;
+    }
+
+    bool contains(const KeyType& key) const {
+        return mPairStorage.containsKey(key);
+    }
+
+    size_t size() const { return mPairStorage.size(); }
+    size_t capacity() const { return mPairStorage.capacity(); }
+    bool empty() const { return mPairStorage.empty(); }
+
+    // Direct access to pairs
+    PairType* find(const KeyType& key) {
+        return mPairStorage.findByKey(key);
+    }
+
+    const PairType* find(const KeyType& key) const {
+        return mPairStorage.findByKey(key);
+    }
+
+    // Iterator support
+    auto begin() { return mPairStorage.begin(); }
+    auto end() { return mPairStorage.end(); }
+    auto begin() const { return mPairStorage.begin(); }
+    auto end() const { return mPairStorage.end(); }
+};
+
+} // namespace CommodityTariffContainers
+} // namespace app
+} // namespace chip
---
+++ src/app/clusters/commodity-tariff-server/tests/TestCommodityTariffContainers.cpp
@@ -0,0 +1,205 @@
+/*
+ *
+ *    Copyright (c) 2025 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ *    @file
+ *      This file implements a unit test suite for testing different zone
+ *      polygons for self-intersection.
+ *
+ */
+
+#include "pw_unit_test/framework.h"
+#include <app/clusters/commodity-tariff-server/CommodityTariffContainers.h>
+
+#include <cstdint>
+#include <mutex>
+#include <thread>
+
+#include <lib/support/CodeUtils.h>
+
+using namespace chip;
+using namespace chip::app;
+using namespace chip::app::Clusters;
+using namespace chip::app::CommodityTariffContainers;
+
+// =================================
+//      Unit tests
+// =================================
+
+namespace chip {
+namespace app {
+
+class TestCommodityTariffContainers : public ::testing::Test
+{
+public:
+    static void SetUpTestSuite() {}
+    static void TearDownTestSuite() {}
+
+protected:
+    void SetUp() override
+    {
+        // Initialize CHIP stack if needed
+        Platform::MemoryInit();
+    }
+
+    void TearDown() override { Platform::MemoryShutdown(); }
+};
+
+TEST_F(TestCommodityTariffContainers, CreateEmptyAndDestroy) {
+    {
+        CTC_UnorderedSet<int> sample(10);
+        EXPECT_EQ(sample.capacity(), 10u);
+        EXPECT_EQ(sample.size(), 0u);
+        EXPECT_TRUE(sample.empty());
+    }
+}
+
+TEST_F(TestCommodityTariffContainers, CreateAndPutItems) {
+    {
+        CTC_UnorderedSet<uint32_t> sample(10);
+        EXPECT_EQ(sample.capacity(), 10u);
+        EXPECT_EQ(sample.size(), 0u);
+        EXPECT_TRUE(sample.empty());
+
+        EXPECT_TRUE(sample.insert(0xAA550000));
+        EXPECT_TRUE(sample.insert(0x55AA0001));
+
+        EXPECT_EQ(sample.size(), 2u);
+        EXPECT_FALSE(sample.empty());
+
+        for (size_t i=sample.size(); i <= sample.capacity() + 1; i++)
+        {
+            bool ret = sample.insert( (uint32_t)( (i%2 ? 0x55AA : 0xAA55) << 16 ) | (uint32_t) i );
+
+            if (i < sample.capacity())
+            {
+                EXPECT_TRUE(ret);                
+            }
+            else 
+            {
+                EXPECT_FALSE(ret);
+            }
+        }
+
+        for (size_t i=0; i < sample.capacity(); i++)
+        {
+            uint32_t entry = sample[i];
+           if (i%2)
+           {
+                EXPECT_EQ( ( entry >> 16 ), 0x55AAu);
+           }
+           else {
+                EXPECT_EQ( ( entry >> 16 ), 0xAA55u);
+           }
+
+           EXPECT_EQ( ( entry & 0x0000ffff ), (uint32_t) i);
+        }
+    }
+}
+
+TEST_F(TestCommodityTariffContainers, UnorderedSet_BasicInsertAndContains) {
+    CTC_UnorderedSet<uint32_t> sample(12);
+
+    for (size_t i = 0; i <= sample.capacity() - 3; i++) {
+        bool ret = sample.insert((uint32_t)((i % 2 ? 0x55AA : 0xAA55) << 16) | (uint32_t)i);
+
+        if (i < sample.capacity()) {
+            EXPECT_TRUE(ret);
+        } else {
+            EXPECT_FALSE(ret);
+        }
+    }
+
+    EXPECT_TRUE(sample.contains(0x55AA0001));
+    EXPECT_TRUE(sample.contains(0xAA550002));
+    EXPECT_TRUE(sample.contains(0x55AA0007));
+    EXPECT_FALSE(sample.contains(0x55AA0008));
+
+    EXPECT_FALSE(sample.insert(0xAA550000));
+    EXPECT_FALSE(sample.insert(0x55AA0005));
+
+    EXPECT_TRUE(sample.insert(0xAA550010));
+    EXPECT_TRUE(sample.insert(0x55AA0011));
+}
+
+TEST_F(TestCommodityTariffContainers, UnorderedSet_DuplicatePrevention) {
+    CTC_UnorderedSet<int> set(10);
+    
+    EXPECT_TRUE(set.insert(100));
+    EXPECT_TRUE(set.insert(200));
+    EXPECT_TRUE(set.insert(300));
+    
+    EXPECT_FALSE(set.insert(100));
+    EXPECT_FALSE(set.insert(200));
+    EXPECT_FALSE(set.insert(300));
+    
+    EXPECT_EQ(set.size(), 3u);
+}
+
+TEST_F(TestCommodityTariffContainers, UnorderedSet_Iteration) {
+    CTC_UnorderedSet<int> set(5);
+    std::vector<int> expected = {10, 20, 30};
+    
+    for (int value : expected) {
+        EXPECT_TRUE(set.insert(value));
+    }
+    
+    EXPECT_EQ(set.size(), 3u);
+    
+    // Test range-based for loop
+    std::vector<int> actual;
+    for (const auto& item : set) {
+        actual.push_back(item);
+    }
+    
+    // Since it's unordered, we only check that all expected items are present
+    for (int expected_value : expected) {
+        EXPECT_NE(std::find(actual.begin(), actual.end(), expected_value), actual.end());
+    }
+}
+
+TEST_F(TestCommodityTariffContainers, UnorderedSet_CapacityLimits) {
+    CTC_UnorderedSet<int> set(3);
+    
+    EXPECT_TRUE(set.insert(1));
+    EXPECT_TRUE(set.insert(2));
+    EXPECT_TRUE(set.insert(3));
+    
+    // Should fail to insert beyond capacity
+    EXPECT_FALSE(set.insert(4));
+    EXPECT_EQ(set.size(), 3u);
+}
+
+TEST_F(TestCommodityTariffContainers, UnorderedMap_BasicOperations) {
+    CTC_UnorderedMap<uint8_t, uint32_t> map(10);
+    
+    EXPECT_TRUE(map.insert(1, 0x11));
+    EXPECT_TRUE(map.insert(2, 0x12));
+    EXPECT_TRUE(map.insert(3, 0x13));
+    
+    EXPECT_EQ(map.size(), 3u);
+    EXPECT_TRUE(map.contains(1));
+    EXPECT_TRUE(map.contains(2));
+    EXPECT_FALSE(map.contains(4));
+    
+    EXPECT_FALSE(map.insert(1, 100)); // Duplicate key
+    EXPECT_EQ(map.size(), 3u);
+}
+
+} // namespace app
+} // namespace chip
