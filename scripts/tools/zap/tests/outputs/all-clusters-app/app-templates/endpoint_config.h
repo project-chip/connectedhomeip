67f65f5fa12bce6000cacfaf880edde0134369b5
arielsz71 <asztejnberg@griddynamics.com>
2025-10-08 15:36:47 -0300

    Update endpoint_config.h

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4949,7 +4949,7 @@
 // This is an array of EmberAfEndpointType structures.
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
-        { ZAP_CLUSTER_INDEX(0), 28, 197 },                                                                                         \
+        { ZAP_CLUSTER_INDEX(0), 28, 195 },                                                                                         \
         { ZAP_CLUSTER_INDEX(28), 73, 3430 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 110 },                                                                                        \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
@@ -4964,7 +4964,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3737)
+#define ATTRIBUTE_MAX_SIZE (3735)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
