8d95d1a3243001f2c7d555843f2c2327ba4346ac
Raveendra Karu <r.karu@samsung.com>
2025-10-13 12:24:23 +0530

    Updating status code to valid code
    Co-author: Sambhavi<sambhavi.1@samsung.com>
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- src/python_testing/TC_PAVST_2_10.py
+++ src/python_testing/TC_PAVST_2_10.py
@@ -160,8 +160,8 @@ class TC_PAVST_2_10(MatterBaseTest, PAVSTTestBase, PAVSTIUtils):
         for idx, (desc, url) in enumerate(invalid_cases, start=5):
             self.step(idx)
             status = await self.allocate_one_pushav_transport(
-                endpoint, tlsEndPoint=tlsEndpointId, url=url, expected_cluster_status=pvcluster.Enums.StatusCodeEnum.InvalidURL, expiryTime=30)
-            asserts.assert_equal(status, pvcluster.Enums.StatusCodeEnum.InvalidURL,
+                endpoint, tlsEndPoint=tlsEndpointId, url=url, expected_cluster_status=pvcluster.Enums.StatusCodeEnum.kInvalidURL, expiryTime=30)
+            asserts.assert_equal(status, pvcluster.Enums.StatusCodeEnum.kInvalidURL,
                                  f"Push AV Transport should return InvalidURL for {desc}")
 
 
