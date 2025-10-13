2707e235f374a33d7c92bcc77bfebaaf953c878a
Restyled.io <commits@restyled.io>
2025-10-13 14:28:31 +0000

    Restyled by clang-format

--- src/platform/nxp/common/crypto/S200/CHIPCryptoPalS200.cpp
+++ src/platform/nxp/common/crypto/S200/CHIPCryptoPalS200.cpp
@@ -39,8 +39,8 @@
 #if defined(MBEDTLS_X509_CRT_PARSE_C)
 #include <mbedtls/x509_crt.h>
 #endif // defined(MBEDTLS_X509_CRT_PARSE_C)
-#include <mbedtls/version.h>
 #include <mbedtls/oid.h>
+#include <mbedtls/version.h>
 #include <mbedtls/x509.h>
 #include <mbedtls/x509_csr.h>
 
