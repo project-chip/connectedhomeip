54f813470a97a2347807c9d2965e22ee4e4893af
Raveendra Karu <r.karu@samsung.com>
2025-10-13 11:03:26 +0530

    Pass expected cluster status.
    Signed-off-by: Raveendra Karu <r.karu@samsung.com>

--- src/python_testing/TC_PAVST_2_10.py
+++ src/python_testing/TC_PAVST_2_10.py
@@ -160,7 +160,7 @@ class TC_PAVST_2_10(MatterBaseTest, PAVSTTestBase, PAVSTIUtils):
         for idx, (desc, url) in enumerate(invalid_cases, start=5):
             self.step(idx)
             status = await self.allocate_one_pushav_transport(
-                endpoint, tlsEndPoint=tlsEndpointId, url=url, expiryTime=30)
+                endpoint, tlsEndPoint=tlsEndpointId, url=url, expected_cluster_status=pvcluster.Enums.StatusCodeEnum.InvalidURL, expiryTime=30)
             asserts.assert_equal(status, pvcluster.Enums.StatusCodeEnum.InvalidURL, f"Push AV Transport should return InvalidURL for {desc}")
 
 
