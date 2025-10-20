d01115dc17f18d6801a15c00c29ed3e8a78a1ed4
Moises Terrones <moises.tersal@gmail.com>
2025-10-20 10:13:39 -0600

    Generate WasherControl cluster using Alchemy

--- zzz_generated/app-common/clusters/LaundryWasherControls/Metadata.h
+++ zzz_generated/app-common/clusters/LaundryWasherControls/Metadata.h
@@ -17,7 +17,7 @@ namespace app {
 namespace Clusters {
 namespace LaundryWasherControls {
 
-inline constexpr uint32_t kRevision = 1;
+inline constexpr uint32_t kRevision = 2;
 
 namespace Attributes {
 
