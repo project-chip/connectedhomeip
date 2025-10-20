46de4efc929a6ba32c26dbe25c1087f2313af760
Miłosz Tomkiel <m.tomkiel@samsung.com>
2025-10-20 09:41:22 +0000

    Add import

--- scripts/tests/run_darwin_framework_ota_test.py
+++ scripts/tests/run_darwin_framework_ota_test.py
@@ -11,6 +11,7 @@ from chiptest.accessories import AppsRegister
 from chiptest.runner import Application, Runner
 from chiptest.test_definition import App, ExecutionCapture
 from chipyaml.paths_finder import PathsFinder
+from pathlib import Path
 
 TEST_NODE_ID = '0x12344321'
 TEST_VID = '0xFFF1'
