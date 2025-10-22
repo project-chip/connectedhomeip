719ba95712b7ece4ca627f4855c3c4556e929b13
Raul Marquez <130402456+raul-marquez-csa@users.noreply.github.com>
2025-10-22 15:22:46 -0700

    Update src/python_testing/TC_SC_4_1.py
    Co-authored-by: C Freeman <cecille@google.com>

--- src/python_testing/TC_SC_4_1.py
+++ src/python_testing/TC_SC_4_1.py
@@ -126,7 +126,7 @@ class TC_SC_4_1(MatterBaseTest):
                         """See the 'Commissionable Subtypes Verifications' table in the Notes/Testing considerations
                         section of the Test Plan for the list of verifications to be performed"""),
 
-            TestStep(10, """VVerify SRV record advertisements""",
+            TestStep(10, """Verify SRV record advertisements""",
                      """See the 'SRV Record Verifications' table in the Notes/Testing considerations
                         section of the Test Plan for the list of verifications to be performed"""),
 
