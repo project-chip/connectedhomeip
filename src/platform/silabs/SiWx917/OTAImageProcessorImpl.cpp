4bdd883bb6ad6f5295d5e5bb7bce891461bd7e9a
Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>
2025-10-22 11:19:07 -0400

    [Silabs] Cherry-pick changes to support PSA keys for the SessionKeyStore (#41143)
    * [Silabs] PSA Crypto PAL customized for EFR32. (#36575)
    
    * [Silabs] PSA Crypto PAL customized for EFR32.
    
    * Pull request #2067: Provision SPAM (#36404)
    
    Merge in WMN_TOOLS/matter from provision_SPAM to dev/provision_source_code
    
    Squashed commit of the following:
    
    commit ae6d6bd09b963838fa8ae70efefa0770c33c5b73
    Author: lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
    Date:   Wed Sep 25 11:08:23 2024 -0400
    
        Updated the GsdkSpam for series 3 now that provisioning relies on it
    
    commit fb0cb797ca3eea51856e2b62e5b2c0a4dfb8cac3
    Author: lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
    Date:   Tue Sep 24 17:53:54 2024 -0400
    
        Updated provisioning to use Silabs Platform Abstraction Manager
    
    Co-authored-by: Andrei Litvin <andy314@gmail.com>
    
    * [Silabs] Trustzone-compatibility. (#36643)
    
    * [Silabs] Bugfix: PSA Crypto keystore for Wi-fi. (#36963)
    
    * [Silabs] Provision: Dynamic buffer allocation. (#36964)
    
    * [Silabs] Provision: Dynamic buffer allocation.
    
    * Code review.
    
    * [Silabs] AES_CCM_encrypt/decrypt output buffers fixed. (#37580)
    
    * [Silabs] AES_CCM_decrypt output buffer fixed.
    
    * Code review.
    
    * [Silabs] Bugfix: PSA AEAD encryption/decryption fixed. (#37663)
    
    * [Silabs] Update silabs sdks versions (#37034)
    
    * Bump submodule for sdk updates and docker image version
    
    * Update files and libs pulled from sisdk. add -fno-lto ld flag. update mg24 linkerfile
    
    * Bring PSA crypto changes from sisdk
    
    Co-authored-by: Ricardo Casallas <77841255+rcasallas-silabs@users.noreply.github.com>
    
    * fixup merge conflict on CHIPCryptoPALPsaEfr32.cpp
    
    * Update mgm24 linkerfile. add no-lto to test-driver
    
    * update mg26 linkerfile
    
    * Adds mbedTLS 3.x support with tinycrypt uECC APIs (#132)
    
    * [SL-UP] Add BRD4338A support for Wiseconnect 3.4 in GN (#104)
    
    * Update for siwx917
    
    * Changes for wifi sdk 3.4.0 (#89)
    
    * fix slc-gen build
    
    * fix wifi ncp build
    
    * Fixup nxp submodule desync
    
    * Fix ot_lib builds
    
    * Fix linking issue with coapi lib. address comments
    
    * Cleanup commented lines and bump merged matter_support sha
    
    * move some psa define from coap config to the right header
    
    * Add segger_rtt reference in linkerfile
    
    * fix rebase to master conflict issue
    
    ---------
    
    Co-authored-by: Ricardo Casallas <77841255+rcasallas-silabs@users.noreply.github.com>
    Co-authored-by: Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
    Co-authored-by: Mathieu Kardous <84793247+mkardous-silabs@users.noreply.github.com>
    Co-authored-by: bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>
    
    * Split UniqueId and PersistentUniqueId (#36292)
    
    * Update specific changes
    
    * Update src/platform/silabs/platformAbstraction/WiseMcuSpam.cpp
    
    ---------
    
    Co-authored-by: Ricardo Casallas <77841255+rcasallas-silabs@users.noreply.github.com>
    Co-authored-by: lpbeliveau-silabs <112982107+lpbeliveau-silabs@users.noreply.github.com>
    Co-authored-by: Andrei Litvin <andy314@gmail.com>
    Co-authored-by: Junior Martinez <67972863+jmartinez-silabs@users.noreply.github.com>
    Co-authored-by: Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
    Co-authored-by: bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>

--- src/platform/silabs/SiWx917/OTAImageProcessorImpl.cpp
+++ src/platform/silabs/SiWx917/OTAImageProcessorImpl.cpp
@@ -238,7 +238,7 @@ void OTAImageProcessorImpl::HandleApply(intptr_t context)
         // send system reset request to reset the MCU and upgrade the m4 image
         ChipLogProgress(SoftwareUpdate, "SoC Soft Reset initiated!");
         // Reboots the device
-        sl_si91x_soc_soft_reset();
+        sl_si91x_soc_nvic_reset();
     }
 }
 
