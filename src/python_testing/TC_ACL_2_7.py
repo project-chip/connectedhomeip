a8f7a006171929053deeaad048fe5adff2d07665
Restyled.io <commits@restyled.io>
2025-10-23 22:13:08 +0000

    Restyled by isort

--- src/python_testing/TC_ACL_2_7.py
+++ src/python_testing/TC_ACL_2_7.py
@@ -41,7 +41,8 @@ import matter.clusters as Clusters
 from matter import ChipDeviceCtrl
 from matter.clusters.Types import Nullable
 from matter.interaction_model import Status
-from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main, run_if_endpoint_matches, has_attribute
+from matter.testing.matter_testing import (MatterBaseTest, TestStep, async_test_body, default_matter_test_main, has_attribute,
+                                           run_if_endpoint_matches)
 
 # These below variables are used to test the AccessControl cluster Extension attribute and come from the test plan here: https://github.com/CHIP-Specifications/chip-test-plans/blob/59e8c45b8e7c24d5ce130b166520ff4f7bd935b6/src/cluster/AccessControl.adoc#tc-acl-2-6-accesscontrolentrychanged-event:~:text=D_OK_EMPTY%3A%20%221718%22%20which%20is%20an%20octstr%20of%20length%202%20containing%20valid%20TLV%3A
 D_OK_EMPTY = bytes.fromhex('1718')
