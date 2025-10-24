13ed85a225629811a0b005a5ac446f469dbd48c5
Junior Martinez <67972863+jmartinez-silabs@users.noreply.github.com>
2025-10-24 12:44:10 -0400

    Update src/app/clusters/groupcast/tests/TestGroupcastCluster.cpp
    Co-authored-by: Andrei Litvin <andy314@gmail.com>

--- src/app/clusters/groupcast/tests/TestGroupcastCluster.cpp
+++ src/app/clusters/groupcast/tests/TestGroupcastCluster.cpp
@@ -176,7 +176,7 @@ TEST_F(TestGroupcastCluster, TestJoinGroupCommand)
     cmdData.endpoints       = { 1 };
     cmdData.keyID           = 0xAABBCCDD;
     const uint8_t keyData[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
-    cmdData.key             = MakeOptional(Span<const uint8_t>(keyData, sizeof(keyData)));
+    cmdData.key             = MakeOptional(ByteSpan(keyData));
     cmdData.gracePeriod     = MakeOptional(0U);
     cmdData.useAuxiliaryACL = MakeOptional(true);
 
