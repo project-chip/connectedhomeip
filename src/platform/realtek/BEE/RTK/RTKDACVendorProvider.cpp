9fd429ebb0a6ebbacd2df11fb5de7ae212034d3a
pankore <86098180+pankore@users.noreply.github.com>
2025-10-24 23:32:24 +0800

    [Realtek] Optimize code

--- src/platform/realtek/BEE/RTK/RTKDACVendorProvider.cpp
+++ src/platform/realtek/BEE/RTK/RTKDACVendorProvider.cpp
@@ -190,22 +190,20 @@ CHIP_ERROR RTKDACVendorProvider::GetProductAttestationIntermediateCert(MutableBy
 #if FEATURE_TRUSTZONE_ENABLE && CONFIG_DAC_KEY_ENC
 CHIP_ERROR RTKDACVendorProvider::ImportDACKey()
 {
-    DAC_IMPORT_PARAM key_param       = {};
-    key_param.encrypted_priv_key     = const_cast<uint8_t *>(pFactoryData->dac.dac_key.value);
-    key_param.encrypted_priv_key_len = pFactoryData->dac.dac_key.len;
-
     ByteSpan dacCertSpan{ pFactoryData->dac.dac_cert.value, pFactoryData->dac.dac_cert.len };
     chip::Crypto::P256PublicKey dacPublicKey;
     ReturnErrorOnFailure(chip::Crypto::ExtractPubkeyFromX509Cert(dacCertSpan, dacPublicKey));
-    key_param.public_key     = dacPublicKey.Bytes();
-    key_param.public_key_len = dacPublicKey.Length();
-    ChipLogDetail(DeviceLayer, "ImportDACKey: encrypted_priv_key %p, encrypted_priv_key_len %d, public_key %p, public_key_len %d",
-                  key_param.encrypted_priv_key, key_param.encrypted_priv_key_len, key_param.public_key, key_param.public_key_len);
 
-    // Assuming secure_app_function_call returns a status that can be checked.
-    if (!secure_app_function_call(SECURE_APP_FUNCTION_DAC_KEY_IMPORT, &key_param))
+    DAC_IMPORT_PARAM key_param       = {};
+    key_param.encrypted_priv_key     = pFactoryData->dac.dac_key.value;
+    key_param.encrypted_priv_key_len = pFactoryData->dac.dac_key.len;
+    key_param.public_key             = dacPublicKey.Bytes();
+    key_param.public_key_len         = dacPublicKey.Length();
+
+    secure_app_function_call(SECURE_APP_FUNCTION_DAC_KEY_IMPORT, &key_param);
+    if (key_param.ret)
     {
-        ChipLogError(DeviceLayer, "Failed to import DAC key into secure world");
+        ChipLogError(DeviceLayer, "secure_app_function_call DAC key import %d", key_param.ret);
         return CHIP_ERROR_INTERNAL;
     }
 
@@ -225,24 +223,32 @@ CHIP_ERROR RTKDACVendorProvider::SignWithDeviceAttestationKey(const ByteSpan & m
 
 #if CONFIG_FACTORY_DATA
 #if FEATURE_TRUSTZONE_ENABLE && CONFIG_DAC_KEY_ENC
-    uint8_t sig_tmp_buf[Crypto::kP256_ECDSA_Signature_Length_Raw] = {};
-    uint32_t sig_len                                              = 0;
-    ReturnErrorOnFailure(ImportDACKey());
-
-    DAC_SIGN_PARAM param = {};
-    param.msg            = const_cast<uint8_t *>(messageToSign.data());
-    param.msg_len        = messageToSign.size();
-    param.sig            = sig_tmp_buf;
-    param.p_sig_len      = &sig_len;
-
-    // Assuming secure_app_function_call returns a status that can be checked.
-    if (!secure_app_function_call(SECURE_APP_FUNCTION_DAC_KEY_SIGN, &param))
+    if (!mDACKeyImported)
     {
-        ChipLogError(DeviceLayer, "Failed to sign with DAC key in secure world");
+        ReturnErrorOnFailure(ImportDACKey());
+        mDACKeyImported = true;
+    }
+
+    uint8_t sig_tmp_buf[Crypto::kP256_ECDSA_Signature_Length_Raw] = {};
+    DAC_SIGN_PARAM param                                          = {};
+    param.msg                                                     = messageToSign.data();
+    param.msg_len                                                 = messageToSign.size();
+    param.sig                                                     = sig_tmp_buf;
+    param.p_sig_len                                               = sizeof(sig_tmp_buf);
+
+    secure_app_function_call(SECURE_APP_FUNCTION_DAC_KEY_SIGN, &param);
+    if (param.ret)
+    {
+        ChipLogError(DeviceLayer, "secure_app_function_call DAC key sign %d", param.ret);
+        return CHIP_ERROR_INTERNAL;
+    }
+    if (param.sig_len > sizeof(sig_tmp_buf))
+    {
+        ChipLogError(DeviceLayer, "Signature length out of bounds: %d", param.sig_len);
         return CHIP_ERROR_INTERNAL;
     }
 
-    return CopySpanToMutableSpan(ByteSpan{ sig_tmp_buf, static_cast<size_t>(sig_len) }, outSignBuffer);
+    return CopySpanToMutableSpan(ByteSpan{ sig_tmp_buf, static_cast<size_t>(param.sig_len) }, outSignBuffer);
 #else
     VerifyOrReturnError(pFactoryData->dac.dac_cert.value, CHIP_ERROR_PERSISTED_STORAGE_VALUE_NOT_FOUND);
     VerifyOrReturnError(pFactoryData->dac.dac_key.value, CHIP_ERROR_PERSISTED_STORAGE_VALUE_NOT_FOUND);
