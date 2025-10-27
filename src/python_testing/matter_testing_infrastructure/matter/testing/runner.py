ae0a62e61cb233ab7dc97057f2b034ec46783071
Harshith-GRL <harshith.yadav@graniteriverlabs.in>
2025-10-27 21:31:33 +0530

    Added check for qr_code presence in command line args

--- src/python_testing/matter_testing_infrastructure/matter/testing/runner.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/runner.py
@@ -757,13 +757,13 @@ def convert_args_to_matter_config(args: argparse.Namespace):
 
     if args.commissioning_method and "nfc" in args.commissioning_method:
         if ("NFC_Reader_index" in config.global_test_params and
-                not any([args.passcodes, args.discriminators, args.manual_code])):
+                not any([args.passcodes, args.discriminators, args.manual_code,args.qr_code])):
             from matter.testing.matter_nfc_interaction import connect_read_nfc_tag_data
             nfc_tag_data = connect_read_nfc_tag_data(config.global_test_params.get("NFC_Reader_index"))
             args.qr_code.append(nfc_tag_data)
         else:
             print("Error: Missing required argument --int-arg NFC_Reader_index:<int-value> for NFC tests. "
-                  "Do not provide discriminator, passcode, or manual code, as the commissioning "
+                  "Do not provide discriminator, passcode, manual code or qr-code as the commissioning "
                   "payload is read directly from the NFC tag.")
             sys.exit(1)
 
