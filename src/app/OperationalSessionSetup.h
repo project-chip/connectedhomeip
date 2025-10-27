48facd3ddc8dc2bf43947844557d6838dfd714eb
yunhanw-google <yunhanw@google.com>
2025-10-27 09:30:52 -0700

    Update OperationalSessionSetup.h

--- src/app/OperationalSessionSetup.h
+++ src/app/OperationalSessionSetup.h
@@ -396,6 +396,7 @@ private:
 
     void DequeueConnectionCallbacks(CHIP_ERROR error, ReleaseBehavior releaseBehavior = ReleaseBehavior::Release)
     {
+        // NOLINTNEXTLINE(core.StackAddressEscape): TODO #41631
         this->DequeueConnectionCallbacks(error, SessionEstablishmentStage::kNotInKeyExchange, releaseBehavior);
     }
 
