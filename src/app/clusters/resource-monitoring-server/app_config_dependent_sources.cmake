ab797fbded642c98a99a6fb13b4893588c7865bf
Mauricio Costa <mcosta@griddynamics.com>
2025-10-28 13:40:01 -0300

    removing errouneos ','s from .cmake file

--- src/app/clusters/resource-monitoring-server/app_config_dependent_sources.cmake
+++ src/app/clusters/resource-monitoring-server/app_config_dependent_sources.cmake
@@ -16,8 +16,8 @@
 TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
-    "${CLUSTER_DIR}/CodegenIntegration.cpp",
-    "${CLUSTER_DIR}/CodegenIntegration.h",
+    "${CLUSTER_DIR}/CodegenIntegration.cpp"
+    "${CLUSTER_DIR}/CodegenIntegration.h"
 )
 
 # These are the things that BUILD.gn dependencies would pull
