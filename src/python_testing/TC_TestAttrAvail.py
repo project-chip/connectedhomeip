7fdbe04d29ec865febd695470298744bdb1a80f4
Restyled.io <commits@restyled.io>
2025-10-30 23:09:41 +0000

    Restyled by isort

--- src/python_testing/TC_TestAttrAvail.py
+++ src/python_testing/TC_TestAttrAvail.py
@@ -64,8 +64,8 @@ import asyncio
 from mobly import asserts
 
 import matter.clusters as Clusters
-from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 from matter.testing.basic_composition import BasicCompositionTests
+from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
 
 
 class TC_TestAttrAvail(MatterBaseTest, BasicCompositionTests):
