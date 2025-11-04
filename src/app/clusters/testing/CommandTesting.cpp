2bda6d5dd6d9c91053574b0583508142e669156f
Lyudmila Kostanyan <119743330+LyudmilaKostanyan@users.noreply.github.com>
2025-11-04 13:46:45 +0400

    Update src/app/clusters/testing/CommandTesting.cpp
    Co-authored-by: Andrei Litvin <andy314@gmail.com>

--- src/app/clusters/testing/CommandTesting.cpp
+++ src/app/clusters/testing/CommandTesting.cpp
@@ -33,8 +33,7 @@ CHIP_ERROR MockCommandHandler::FallibleAddStatus(const ConcreteCommandPath & aRe
                                                  const Protocols::InteractionModel::ClusterStatusCode & aStatus,
                                                  const char * context)
 {
-    StatusRecord record{ aRequestCommandPath, aStatus, context };
-    mStatuses.push_back(std::move(record));
+    mStatuses.emplace_back(aRequestCommandPath, aStatus, context);
     return CHIP_NO_ERROR;
 }
 
