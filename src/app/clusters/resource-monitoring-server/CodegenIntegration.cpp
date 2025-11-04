cf222fd437cc4430e1f85e4d72bfc452373f710d
Mauricio Costa <mcosta@griddynamics.com>
2025-11-04 18:39:35 -0300

    removed intermediary header file.

--- src/app/clusters/resource-monitoring-server/CodegenIntegration.cpp
+++ src/app/clusters/resource-monitoring-server/CodegenIntegration.cpp
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "resource-monitoring-server.h"
+#include "CodegenIntegration.h"
 #include <app/clusters/resource-monitoring-server/resource-monitoring-cluster.h>
 #include <app/static-cluster-config/ActivatedCarbonFilterMonitoring.h>
 #include <app/static-cluster-config/HepaFilterMonitoring.h>
