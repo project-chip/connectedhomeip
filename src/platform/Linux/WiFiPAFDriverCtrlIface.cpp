b270fb044d16d8f3e17daec074dcbc8108a39843
Restyled.io <commits@restyled.io>
2025-11-04 19:35:42 +0000

    Restyled by clang-format

--- src/platform/Linux/WiFiPAFDriverCtrlIface.cpp
+++ src/platform/Linux/WiFiPAFDriverCtrlIface.cpp
@@ -211,8 +211,8 @@ CHIP_ERROR WiFiPAFDriverCtrlIface::Publish(std::unique_ptr<uint16_t[]> freq_list
     VerifyOrDie(DeviceLayer::GetCommissionableDataProvider()->GetSetupDiscriminator(PafPublish_ssi.DevInfo) == CHIP_NO_ERROR);
 
     std::ostringstream oss;
-    oss << "NAN_PUBLISH " << "service_name=" << srv_name << " srv_proto_type=" << srv_proto_type << " ttl=" << ttl
-        << " freq=" << freq;
+    oss << "NAN_PUBLISH "
+        << "service_name=" << srv_name << " srv_proto_type=" << srv_proto_type << " ttl=" << ttl << " freq=" << freq;
 
     oss << " ssi=" << std::hex << std::setfill('0');
     uint8_t * ssi_array = reinterpret_cast<uint8_t *>(&PafPublish_ssi);
@@ -402,8 +402,8 @@ CHIP_ERROR WiFiPAFDriverCtrlIface::Subscribe(const uint16_t & connDiscriminator,
 
     std::ostringstream oss;
 
-    oss << "NAN_SUBSCRIBE " << "service_name=" << srv_name << " srv_proto_type=" << srv_proto_type << " active=" << is_active
-        << " ttl=" << ttl
+    oss << "NAN_SUBSCRIBE "
+        << "service_name=" << srv_name << " srv_proto_type=" << srv_proto_type << " active=" << is_active << " ttl=" << ttl
 #if !CHIP_DEVICE_CONFIG_ENABLE_WIFIPAF_HOSTAPD
         << " freq=" << freq
 #endif
@@ -491,8 +491,8 @@ CHIP_ERROR WiFiPAFDriverCtrlIface::Send(const WiFiPAF::WiFiPAFSession & TxInfo, 
     auto ssi_str = std::string(ssi_hex_buffer.data());
 
     std::ostringstream oss;
-    oss << "NAN_TRANSMIT " << "handle=" << TxInfo.id << " req_instance_id=" << TxInfo.peer_id << " address=" << peer_mac
-        << " ssi=" << ssi_str;
+    oss << "NAN_TRANSMIT "
+        << "handle=" << TxInfo.id << " req_instance_id=" << TxInfo.peer_id << " address=" << peer_mac << " ssi=" << ssi_str;
 
     std::string cmd_str = oss.str();
     const char * cmd    = cmd_str.c_str();
