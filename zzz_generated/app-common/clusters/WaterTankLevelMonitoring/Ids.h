b478ae02674ed3e6fade1d25bb5883da7cfa185c
Moises Terrones <moises.tersal@gmail.com>
2025-11-06 14:20:44 -0600

    Fix typo in Ids.h generator that added a 4 to each header cluster code (#41826)

--- zzz_generated/app-common/clusters/WaterTankLevelMonitoring/Ids.h
+++ zzz_generated/app-common/clusters/WaterTankLevelMonitoring/Ids.h
@@ -1,6 +1,6 @@
 // DO NOT EDIT MANUALLY - Generated file
 //
-// Identifier constant values for cluster WaterTankLevelMonitoring (cluster code: 121/0x0479)
+// Identifier constant values for cluster WaterTankLevelMonitoring (cluster code: 121/0x0079)
 // based on src/controller/data_model/controller-clusters.matter
 #pragma once
 
