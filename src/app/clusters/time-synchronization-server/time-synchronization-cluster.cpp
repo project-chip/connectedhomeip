7f00a4c419bfec812e9b4d4bbc46c4c23fc30065
Restyled.io <commits@restyled.io>
2025-11-07 19:39:38 +0000

    Restyled by clang-format

--- src/app/clusters/time-synchronization-server/time-synchronization-cluster.cpp
+++ src/app/clusters/time-synchronization-server/time-synchronization-cluster.cpp
@@ -177,9 +177,9 @@ TimeSynchronization::Delegate * TimeSynchronizationCluster::GetDelegate()
 TimeSynchronizationCluster::TimeSynchronizationCluster(EndpointId endpoint, const BitFlags<TimeSynchronization::Feature> features,
                                                        const OptionalAttributeSet & optionalAttributeSet,
                                                        const StartupConfiguration & config) :
-    DefaultServerCluster({ endpoint, TimeSynchronization::Id }), mFeatures(features), mOptionalAttributeSet(optionalAttributeSet),
-    mSupportsDNSResolve(config.supportsDNSResolve), mNTPServerAvailable(config.ntpServerAvailable),
-    mTimeZoneDatabase(config.timeZoneDatabase), mTimeSource(config.timeSource),
+    DefaultServerCluster({ endpoint, TimeSynchronization::Id }),
+    mFeatures(features), mOptionalAttributeSet(optionalAttributeSet), mSupportsDNSResolve(config.supportsDNSResolve),
+    mNTPServerAvailable(config.ntpServerAvailable), mTimeZoneDatabase(config.timeZoneDatabase), mTimeSource(config.timeSource),
 #if TIME_SYNC_ENABLE_TSC_FEATURE
     mOnDeviceConnectedCallback(OnDeviceConnectedWrapper, this),
     mOnDeviceConnectionFailureCallback(OnDeviceConnectionFailureWrapper, this),
