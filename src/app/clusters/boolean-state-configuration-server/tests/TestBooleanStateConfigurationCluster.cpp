70cf0f745f9648bd0ecdc9ce82f2090cfdaf8244
Andrei Litvin <andy314@gmail.com>
2025-11-07 16:14:03 +0000

    Start adding tests.

---
+++ src/app/clusters/boolean-state-configuration-server/tests/TestBooleanStateConfigurationCluster.cpp
@@ -0,0 +1,64 @@
+/*
+ *    Copyright (c) 2025 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#include <pw_unit_test/framework.h>
+
+#include <app/clusters/boolean-state-configuration-server/boolean-state-configuration-cluster.h>
+#include <app/clusters/testing/AttributeTesting.h>
+#include <app/clusters/testing/ClusterTester.h>
+#include <lib/core/CHIPError.h>
+#include <lib/core/DataModelTypes.h>
+#include <lib/support/ReadOnlyBuffer.h>
+
+namespace {
+
+using namespace chip;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::BooleanStateConfiguration;
+
+using chip::app::DataModel::AcceptedCommandEntry;
+using chip::app::DataModel::AttributeEntry;
+using chip::Test::ClusterTester;
+
+// initialize memory as ReadOnlyBufferBuilder may allocate
+struct TestBooleanStateConfigurationCluster : public ::testing::Test
+{
+    static void SetUpTestSuite() { ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR); }
+    static void TearDownTestSuite() { chip::Platform::MemoryShutdown(); }
+};
+
+constexpr EndpointId kTestEndpointId = 1;
+
+TEST_F(TestBooleanStateConfigurationCluster, TestAttributeList)
+{
+    // cluster without any attributes
+    {
+        BooleanStateConfigurationCluster cluster(kTestEndpointId, {}, {},
+                                                 {
+                                                     .supportedSensitivityLevels = 7,
+                                                     .defaultSensitivityLevel    = 4,
+                                                     .alarmsSupported            = 0,
+                                                 });
+
+        ReadOnlyBufferBuilder<AttributeEntry> builder;
+        ASSERT_EQ(cluster.Attributes({ kTestEndpointId, Id }, builder), CHIP_NO_ERROR);
+
+        ReadOnlyBufferBuilder<AttributeEntry> expectedBuilder;
+        ASSERT_EQ(expectedBuilder.ReferenceExisting(app::DefaultServerCluster::GlobalAttributes()), CHIP_NO_ERROR);
+        ASSERT_TRUE(Testing::EqualAttributeSets(builder.TakeBuffer(), expectedBuilder.TakeBuffer()));
+    }
+}
+
+} // namespace
