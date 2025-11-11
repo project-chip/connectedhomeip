ea19cde12ad97ae638ade8d383ec1638a7d243ec
Marcos Boyington <15697303+gmarcosb@users.noreply.github.com>
2025-11-11 09:49:09 -0700

    Fix CI after clang tidy fix

--- src/data-model-providers/codedriven/tests/TestCodeDrivenDataModelProvider.cpp
+++ src/data-model-providers/codedriven/tests/TestCodeDrivenDataModelProvider.cpp
@@ -39,6 +39,7 @@
 
 using namespace chip;
 using namespace chip::app;
+using namespace chip::app::DataModel;
 using namespace chip::app::Testing;
 
 class TestProviderChangeListener : public DataModel::ProviderChangeListener
