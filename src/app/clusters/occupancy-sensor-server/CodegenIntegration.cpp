68c2ba8134bc1f3309ef21d1a893bc23045191bc
Sergio Soares <sergiosoares@google.com>
2025-11-12 21:36:41 +0000

    Fix include after merging with master

--- src/app/clusters/occupancy-sensor-server/CodegenIntegration.cpp
+++ src/app/clusters/occupancy-sensor-server/CodegenIntegration.cpp
@@ -19,12 +19,12 @@
 #include "CodegenIntegration.h"
 #include <app-common/zap-generated/attributes/Accessors.h>
 #include <app-common/zap-generated/cluster-objects.h>
-#include <app/DefaultTimerDelegate.h>
 #include <app/clusters/occupancy-sensor-server/OccupancySensingCluster.h>
 #include <app/static-cluster-config/OccupancySensing.h>
 #include <app/util/attribute-storage.h>
 #include <data-model-providers/codegen/ClusterIntegration.h>
 #include <data-model-providers/codegen/CodegenDataModelProvider.h>
+#include <platform/DefaultTimerDelegate.h>
 
 using namespace chip;
 using namespace chip::app;
