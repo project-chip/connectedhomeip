77c653cbd2dca849d75f79535c71d5f931f56c5a
Restyled.io <commits@restyled.io>
2025-11-14 10:19:09 +0000

    Restyled by autopep8

--- src/python_testing/TestRevokeCommissioningClearsPASE.py
+++ src/python_testing/TestRevokeCommissioningClearsPASE.py
@@ -102,8 +102,8 @@ class TestRevokeCommissioningClearsPASE(MatterBaseTest):
 
         # ---------------------------- Repeat test, but sending RevokeCommissioning over PASE this time --------------------------------
 
-        self.print_step(6, "recreate Second Controller; to establish a new PASE session and repeat test, but sending RevokeCommissioning over PASE this time")
-
+        self.print_step(
+            6, "recreate Second Controller; to establish a new PASE session and repeat test, but sending RevokeCommissioning over PASE this time")
 
         self.TH2.Shutdown()
         fabric_admin = self.certificate_authority_manager.activeCaList[0].adminList[0]
@@ -112,7 +112,6 @@ class TestRevokeCommissioningClearsPASE(MatterBaseTest):
             nodeId=self.TH2_nodeId,
             paaTrustStorePath=str(self.matter_test_config.paa_trust_store_path),
         )
-
 
         self.print_step(7, "TH1 sends an OpenCommissioningWindow command to DUT to allow it to be commissioned by TH2")
         resp = await self.open_commissioning_window()
@@ -123,7 +122,6 @@ class TestRevokeCommissioningClearsPASE(MatterBaseTest):
         self.print_step(9, "TH2 Sends RevokeCommissioning (Over PASE) to clear PASE session on DUT")
 
         await self.TH2.SendCommand(nodeId=pase_node_id, endpoint=0, payload=revokeCmd, timedRequestTimeoutMs=6000)
-
 
         self.print_step(
             10, "Ensure that the PASE Session got cleared, by attempting to read VendorName using TH2 (over PASE) and Ensuring that the Command times out")
