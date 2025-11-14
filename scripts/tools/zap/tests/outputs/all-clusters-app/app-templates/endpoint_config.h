ddad8da480e3f05c567fbe28972ab3c3ba21f2d4
Mauricio Costa <mcosta@griddynamics.com>
2025-11-14 17:16:33 -0300

    fix for zap generated code

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4955,7 +4955,7 @@
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
         { ZAP_CLUSTER_INDEX(0), 28, 187 },                                                                                         \
-        { ZAP_CLUSTER_INDEX(28), 73, 3415 },                                                                                       \
+        { ZAP_CLUSTER_INDEX(28), 73, 3411 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 101 },                                                                                        \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
     }
@@ -4969,7 +4969,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3703)
+#define ATTRIBUTE_MAX_SIZE (3699)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
