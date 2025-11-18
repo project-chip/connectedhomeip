ea23c5bee7fa7dddd30f9b07a92417cd197b4681
Restyled.io <commits@restyled.io>
2025-11-18 22:14:58 +0000

    Restyled by isort

--- src/python_testing/TC_DefaultWarnings.py
+++ src/python_testing/TC_DefaultWarnings.py
@@ -38,8 +38,8 @@ from mobly import asserts
 from test_testing.DefaultChecker import (FLAG_DEFAULT_CALENDAR_FORMAT, FLAG_FAULT_INJECTION, FLAG_FIXED_LABEL_DEFAULT_VALUES,
                                          FLAG_FIXED_LABEL_EMPTY, FLAG_PRODUCT_NAME, FLAG_SAMPLE_MEI, FLAG_UNIT_TESTING,
                                          FLAG_VENDOR_ID, FLAG_VENDOR_NAME, DefaultChecker)
-from matter.testing.basic_composition import BasicCompositionTests
 
+from matter.testing.basic_composition import BasicCompositionTests
 from matter.testing.matter_testing import TestStep, async_test_body, default_matter_test_main
 
 
