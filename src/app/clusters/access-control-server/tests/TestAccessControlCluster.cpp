1decfce2d409c4941bed00ba83cb232c2cce1c3a
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-11-19 19:05:06 +0400

    Enhance unit tests for Access Control Cluster by adding initialization and attribute reading tests.
    Introduced a TestDeviceTypeResolver for better test isolation and ensured mandatory and optional attributes can be read successfully. Improved setup and teardown procedures for the test suite.

--- src/app/clusters/access-control-server/tests/TestAccessControlCluster.cpp
+++ src/app/clusters/access-control-server/tests/TestAccessControlCluster.cpp
@@ -15,8 +15,11 @@
  */
 #include <pw_unit_test/framework.h>
 
+#include <access/AccessControl.h>
+#include <access/examples/ExampleAccessControlDelegate.h>
 #include <app/clusters/access-control-server/access-control-cluster.h>
 #include <app/clusters/testing/AttributeTesting.h>
+#include <app/clusters/testing/ClusterTester.h>
 #include <app/data-model-provider/MetadataTypes.h>
 #include <app/server-cluster/DefaultServerCluster.h>
 #include <clusters/AccessControl/Enums.h>
@@ -34,10 +37,29 @@ using namespace chip::app;
 using namespace chip::app::Clusters;
 using namespace chip::app::DataModel;
 
+// Simple DeviceTypeResolver for tests
+class TestDeviceTypeResolver : public chip::Access::AccessControl::DeviceTypeResolver
+{
+public:
+    bool IsDeviceTypeOnEndpoint(DeviceTypeId deviceType, EndpointId endpoint) override { return false; }
+};
+
+static TestDeviceTypeResolver gTestDeviceTypeResolver;
+
 struct TestAccessControlCluster : public ::testing::Test
 {
-    static void SetUpTestSuite() { ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR); }
-    static void TearDownTestSuite() { chip::Platform::MemoryShutdown(); }
+    static void SetUpTestSuite()
+    {
+        ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR);
+        // Initialize AccessControl for reading mandatory attributes
+        chip::Access::AccessControl::Delegate * delegate = chip::Access::Examples::GetAccessControlDelegate();
+        ASSERT_EQ(chip::Access::GetAccessControl().Init(delegate, gTestDeviceTypeResolver), CHIP_NO_ERROR);
+    }
+    static void TearDownTestSuite()
+    {
+        chip::Access::GetAccessControl().Finish();
+        chip::Platform::MemoryShutdown();
+    }
 };
 
 TEST_F(TestAccessControlCluster, CompileTest)
@@ -88,16 +110,73 @@ TEST_F(TestAccessControlCluster, AttributesTest)
 
     ASSERT_EQ(expectedBuilder.AppendElements({
 #if CHIP_CONFIG_ENABLE_ACL_EXTENSIONS
-        AccessControl::Attributes::Extension::kMetadataEntry,
+                  AccessControl::Attributes::Extension::kMetadataEntry,
 #endif
 
 #if CHIP_CONFIG_USE_ACCESS_RESTRICTIONS
-            AccessControl::Attributes::CommissioningARL::kMetadataEntry, AccessControl::Attributes::Arl::kMetadataEntry
+                  AccessControl::Attributes::CommissioningARL::kMetadataEntry, AccessControl::Attributes::Arl::kMetadataEntry
 #endif
-    }),
+              }),
               CHIP_NO_ERROR);
     ASSERT_EQ(expectedBuilder.AppendElements(AccessControl::Attributes::kMandatoryMetadata), CHIP_NO_ERROR);
-    ASSERT_TRUE(Testing::EqualAttributeSets(attributesBuilder.TakeBuffer(), expectedBuilder.TakeBuffer()));
+    ASSERT_TRUE(chip::Testing::EqualAttributeSets(attributesBuilder.TakeBuffer(), expectedBuilder.TakeBuffer()));
+}
+
+// Helper function to count elements in a decodable list
+template <typename DecodableListType>
+size_t CountListElements(DecodableListType & list)
+{
+    size_t count = 0;
+    auto it      = list.begin();
+    while (it.Next())
+    {
+        ++count;
+    }
+    return count;
+}
+
+TEST_F(TestAccessControlCluster, ReadAttributesTest)
+{
+    // Test that all available attributes (mandatory and optional) can be read
+    AccessControlCluster cluster;
+    chip::Test::ClusterTester tester(cluster);
+
+    ASSERT_EQ(cluster.Startup(tester.GetServerClusterContext()), CHIP_NO_ERROR);
+
+    // Test mandatory attributes (always available)
+    uint16_t subjectsPerEntry = 0;
+    auto status = tester.ReadAttribute(AccessControl::Attributes::SubjectsPerAccessControlEntry::Id, subjectsPerEntry);
+    ASSERT_TRUE(status.IsSuccess());
+
+    uint16_t targetsPerEntry = 0;
+    status                   = tester.ReadAttribute(AccessControl::Attributes::TargetsPerAccessControlEntry::Id, targetsPerEntry);
+    ASSERT_TRUE(status.IsSuccess());
+
+    uint16_t entriesPerFabric = 0;
+    status = tester.ReadAttribute(AccessControl::Attributes::AccessControlEntriesPerFabric::Id, entriesPerFabric);
+    ASSERT_TRUE(status.IsSuccess());
+
+#if CHIP_CONFIG_ENABLE_ACL_EXTENSIONS
+    // Read Extension attribute (optional)
+    AccessControl::Attributes::Extension::TypeInfo::DecodableType extension;
+    status = tester.ReadAttribute(AccessControl::Attributes::Extension::Id, extension);
+    ASSERT_TRUE(status.IsSuccess());
+    ASSERT_EQ(CountListElements(extension), 0u);
+#endif // CHIP_CONFIG_ENABLE_ACL_EXTENSIONS
+
+#if CHIP_CONFIG_USE_ACCESS_RESTRICTIONS
+    // Read CommissioningARL attribute (optional)
+    AccessControl::Attributes::CommissioningARL::TypeInfo::DecodableType commissioningArl;
+    status = tester.ReadAttribute(AccessControl::Attributes::CommissioningARL::Id, commissioningArl);
+    ASSERT_TRUE(status.IsSuccess());
+    ASSERT_EQ(CountListElements(commissioningArl), 0u);
+
+    // Read Arl attribute (optional)
+    AccessControl::Attributes::Arl::TypeInfo::DecodableType arl;
+    status = tester.ReadAttribute(AccessControl::Attributes::Arl::Id, arl);
+    ASSERT_TRUE(status.IsSuccess());
+    ASSERT_EQ(CountListElements(arl), 0u);
+#endif // CHIP_CONFIG_USE_ACCESS_RESTRICTIONS
 }
 
 } // namespace
