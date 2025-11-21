af1e9db80567e86337b48a2a80203a9e14a3602f
Andrei Litvin <andy314@gmail.com>
2025-11-21 10:06:26 -0500

    Remove useless variable.

--- src/app/clusters/boolean-state-configuration-server/boolean-state-configuration-cluster.cpp
+++ src/app/clusters/boolean-state-configuration-server/boolean-state-configuration-cluster.cpp
@@ -44,8 +44,8 @@ BooleanStateConfigurationCluster::BooleanStateConfigurationCluster(EndpointId en
                                                                    BitMask<BooleanStateConfiguration::Feature> features,
                                                                    OptionalAttributesSet optionalAttributes,
                                                                    const StartupConfiguration & config) :
-    DefaultServerCluster({ endpointId, BooleanStateConfiguration::Id }), mFeatures(features),
-    mOptionalAttributes([&features, &optionalAttributes]() -> FullOptionalAttributesSet {
+    DefaultServerCluster({ endpointId, BooleanStateConfiguration::Id }),
+    mFeatures(features), mOptionalAttributes([&features, &optionalAttributes]() -> FullOptionalAttributesSet {
         // constructs the attribute set, that once constructed stays const
         AttributeSet enabledOptionalAttributes;
 
@@ -160,7 +160,6 @@ BooleanStateConfigurationCluster::InvokeCommand(const DataModel::InvokeRequest &
 
         if (mAlarmsEnabled != alarms)
         {
-            AttributePersistence attributePersistence(mContext->attributeStorage);
             mAlarmsEnabled = alarms;
 
             AlarmModeBitMask::IntegerType rawAlarmsEnabled = mAlarmsEnabled.Raw();
