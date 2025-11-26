5f4045481628be7b8bece376959d4297369931d3
Andrei Litvin <andy314@gmail.com>
2025-11-26 17:03:03 -0500

    Fix build failure for general-diagnostics-server

--- examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.h
+++ examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.h
@@ -21,7 +21,7 @@
 #include <app/clusters/basic-information/BasicInformationCluster.h>
 #include <app/clusters/general-commissioning-server/BreadCrumbTracker.h>
 #include <app/clusters/general-commissioning-server/general-commissioning-cluster.h>
-#include <app/clusters/general-diagnostics-server/general-diagnostics-cluster.h>
+#include <app/clusters/general-diagnostics-server/GeneralDiagnosticsCluster.h>
 #include <app/clusters/group-key-mgmt-server/group-key-mgmt-cluster.h>
 #include <app/clusters/network-commissioning/NetworkCommissioningCluster.h>
 #include <app/clusters/operational-credentials-server/operational-credentials-cluster.h>
