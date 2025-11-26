4377ed8abbc266a27a1d61ce16b6677eec7dfb98
Andrei Litvin <andy314@gmail.com>
2025-11-26 17:06:27 -0500

    Fix build failure for operational-credentials-server

--- examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.h
+++ examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.h
@@ -24,7 +24,7 @@
 #include <app/clusters/general-diagnostics-server/general-diagnostics-cluster.h>
 #include <app/clusters/group-key-mgmt-server/group-key-mgmt-cluster.h>
 #include <app/clusters/network-commissioning/NetworkCommissioningCluster.h>
-#include <app/clusters/operational-credentials-server/operational-credentials-cluster.h>
+#include <app/clusters/operational-credentials-server/OperationalCredentialsCluster.h>
 #include <app/clusters/software-diagnostics-server/software-diagnostics-cluster.h>
 #include <app/clusters/wifi-network-diagnostics-server/wifi-network-diagnostics-cluster.h>
 #include <app/server-cluster/ServerClusterInterfaceRegistry.h>
