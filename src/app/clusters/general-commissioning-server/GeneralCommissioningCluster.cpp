f0188ee3d151f82c310d79f696352d702c013a16
Andrei Litvin <andy314@gmail.com>
2025-11-26 15:55:05 -0500

    Rename files to use UpperCamelCaseCluster/Logic for general-commissioning-server

--- src/app/clusters/general-commissioning-server/GeneralCommissioningCluster.cpp
+++ src/app/clusters/general-commissioning-server/GeneralCommissioningCluster.cpp
@@ -15,7 +15,7 @@
  *    limitations under the License.
  */
 
-#include "general-commissioning-cluster.h"
+#include "GeneralCommissioningCluster.h"
 
 #include <app/AppConfig.h>
 #include <app/reporting/reporting.h>
