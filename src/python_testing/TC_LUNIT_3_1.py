080c2bdd366ae48ad95679ec311d44d865d54b2c
Ricardo Torres Guzman <torresguzman@xwf.google.com>
2025-12-02 03:44:31 +0000

    adding has_attribute to python includes

--- src/python_testing/TC_LUNIT_3_1.py
+++ src/python_testing/TC_LUNIT_3_1.py
@@ -37,7 +37,7 @@ from mobly import asserts
 import matter.clusters as Clusters
 from matter import ChipDeviceCtrl
 from matter.interaction_model import Status
-from matter.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_cluster, run_if_endpoint_matches
+from matter.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main, has_cluster, has_attribute, run_if_endpoint_matches
 
 
 class TC_LUNIT_3_1(MatterBaseTest):
