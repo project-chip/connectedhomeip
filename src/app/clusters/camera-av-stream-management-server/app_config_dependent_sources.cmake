b381839699fa43f7d135921d2927524d0e1d5a7e
Pradip De <pradipd@google.com>
2025-12-02 12:56:32 -0800

    Renames and fixes

--- src/app/clusters/camera-av-stream-management-server/app_config_dependent_sources.cmake
+++ src/app/clusters/camera-av-stream-management-server/app_config_dependent_sources.cmake
@@ -17,11 +17,6 @@ TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
     "${CLUSTER_DIR}/CodegenIntegration.cpp"
-)
-
-TARGET_SOURCES(
-  ${APP_TARGET}
-  PRIVATE
-    "${CLUSTER_DIR}/camera-av-stream-management-cluster.cpp"
-    "${CLUSTER_DIR}/camera-av-stream-management-cluster.h"
+    "${CLUSTER_DIR}/CameraAVStreamManagementCluster.cpp"
+    "${CLUSTER_DIR}/CameraAVStreamManagementCluster.h"
 )
