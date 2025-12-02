26861db9ca678109cd9e9a1bc18a3322f33ab036
Moises Terrones <moises.tersal@gmail.com>
2025-12-02 00:25:14 -0600

    Resolve merge conflicts with ZAP generation

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4933,7 +4933,7 @@
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
         { ZAP_CLUSTER_INDEX(0), 28, 187 },                                                                                         \
-        { ZAP_CLUSTER_INDEX(28), 73, 3404 },                                                                                       \
+        { ZAP_CLUSTER_INDEX(28), 73, 3378 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 101 },                                                                                        \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
     }
@@ -4947,7 +4947,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3692)
+#define ATTRIBUTE_MAX_SIZE (3666)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
