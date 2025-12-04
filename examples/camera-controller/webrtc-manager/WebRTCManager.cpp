bb9fb797b1dbb1d5a1750b35312acd1f6043d334
Yufeng Wang <yufengwang@google.com>
2025-12-04 10:01:31 -0800

    Save the candidates that were sent and only clear those specific ones

--- examples/camera-controller/webrtc-manager/WebRTCManager.cpp
+++ examples/camera-controller/webrtc-manager/WebRTCManager.cpp
@@ -483,7 +483,12 @@ CHIP_ERROR WebRTCManager::ProvideICECandidates(uint16_t sessionId)
         return CHIP_ERROR_INVALID_ARGUMENT;
     }
 
-    CHIP_ERROR err = mWebRTCProviderClient.ProvideICECandidates(sessionId, mLocalCandidates);
+    // Make a copy of candidates to send to avoid race condition with onLocalCandidate callback
+    // which can asynchronously add new candidates while we're sending
+    std::vector<ICECandidateInfo> candidatesToSend = mLocalCandidates;
+    size_t candidateCount                          = candidatesToSend.size();
+
+    CHIP_ERROR err = mWebRTCProviderClient.ProvideICECandidates(sessionId, candidatesToSend);
 
     if (err != CHIP_NO_ERROR)
     {
@@ -491,8 +496,24 @@ CHIP_ERROR WebRTCManager::ProvideICECandidates(uint16_t sessionId)
     }
     else
     {
-        ChipLogProgress(Camera, "Sent %lu ICE candidate(s), clearing list", mLocalCandidates.size());
-        mLocalCandidates.clear();
+        ChipLogProgress(Camera, "Sent %lu ICE candidate(s)", candidateCount);
+
+        // Remove only the candidates that were successfully sent
+        // New candidates may have arrived during transmission, so we remove from the front
+        if (mLocalCandidates.size() >= candidateCount)
+        {
+            mLocalCandidates.erase(mLocalCandidates.begin(), mLocalCandidates.begin() + candidateCount);
+            if (!mLocalCandidates.empty())
+            {
+                ChipLogProgress(Camera, "%lu new candidate(s) arrived during transmission, keeping for next batch",
+                                mLocalCandidates.size());
+            }
+        }
+        else
+        {
+            ChipLogProgress(Camera, "Sent %lu ICE candidate(s), clearing list", mLocalCandidates.size());
+            mLocalCandidates.clear();
+        }
     }
 
     return err;
