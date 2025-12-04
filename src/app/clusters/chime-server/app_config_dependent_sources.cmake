bae77ee8cdc53cd98b5b67a56926190e83abda67
Sergio Soares <sergiosoares@google.com>
2025-12-04 16:23:32 +0000

    Move legacy chime-server into codegen sub-directory

--- src/app/clusters/chime-server/app_config_dependent_sources.cmake
+++ src/app/clusters/chime-server/app_config_dependent_sources.cmake
@@ -16,6 +16,7 @@
 TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
-    "${CLUSTER_DIR}/chime-server.cpp"
+    "${CLUSTER_DIR}/codegen/chime-server.cpp"
+    "${CLUSTER_DIR}/codegen/chime-server.h"
     "${CLUSTER_DIR}/chime-server.h"
 )
