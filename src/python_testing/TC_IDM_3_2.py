47331f463f694f408751a9c28cdb917635258c50
Restyled.io <commits@restyled.io>
2025-12-06 02:22:44 +0000

    Restyled by isort

--- src/python_testing/TC_IDM_3_2.py
+++ src/python_testing/TC_IDM_3_2.py
@@ -39,6 +39,7 @@
 import logging
 
 from mobly import asserts
+from support_modules.idm_support import IDMBaseTest
 
 import matter.clusters as Clusters
 from matter.clusters import ClusterObjects as ClusterObjects
@@ -47,7 +48,6 @@ from matter.interaction_model import InteractionModelError, Status
 from matter.testing import global_attribute_ids
 from matter.testing.basic_composition import BasicCompositionTests
 from matter.testing.matter_testing import TestStep, async_test_body, default_matter_test_main
-from support_modules.idm_support import IDMBaseTest
 
 log = logging.getLogger(__name__)
 
