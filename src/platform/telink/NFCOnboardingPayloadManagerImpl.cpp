ddec85a0f214e1cdfb9efe44cd766937e6bca70d
Restyled.io <commits@restyled.io>
2025-12-09 10:33:40 +0200

    Restyled by clang-format

--- src/platform/telink/NFCOnboardingPayloadManagerImpl.cpp
+++ src/platform/telink/NFCOnboardingPayloadManagerImpl.cpp
@@ -21,8 +21,8 @@
 
 #include <lib/support/logging/CHIPLogging.h>
 
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/nfc/nfc_tag.h>
+#include <zephyr/kernel.h>
 
 void nfc_callback(const struct device * dev, enum nfc_tag_event event, const uint8_t * data, size_t data_len)
 {
@@ -114,23 +114,24 @@ CHIP_ERROR NFCOnboardingPayloadManagerImpl::_StopTagEmulation()
     return CHIP_NO_ERROR;
 }
 
-size_t NFCOnboardingPayloadManagerImpl::_EncodeNDEFURI(const uint8_t* uri, size_t uri_len, uint8_t* out_buf)
+size_t NFCOnboardingPayloadManagerImpl::_EncodeNDEFURI(const uint8_t * uri, size_t uri_len, uint8_t * out_buf)
 {
     size_t max_len = 128;
     // NDEF Record Header for short record, TNF = Well Known, Type = 'U'
-    const uint8_t type_field = 'U';
-    const uint8_t tnf = 0xD1; // MB=1, ME=1, SR=1, TNF=1
+    const uint8_t type_field     = 'U';
+    const uint8_t tnf            = 0xD1; // MB=1, ME=1, SR=1, TNF=1
     const uint8_t payload_prefix = 0x00; // No prefix, full URI follows
 
-    if (max_len < 5 + uri_len) {
+    if (max_len < 5 + uri_len)
+    {
         ChipLogError(DeviceLayer, "NDEF message is too long, (5 + uri_len) required for TNF URI NDEF record");
         return 0;
     }
 
-    size_t offset = 0;
+    size_t offset     = 0;
     out_buf[offset++] = tnf;
-    out_buf[offset++] = 1;               // Type length
-    out_buf[offset++] = uri_len + 1;     // Payload length (1 prefix + uri)
+    out_buf[offset++] = 1;           // Type length
+    out_buf[offset++] = uri_len + 1; // Payload length (1 prefix + uri)
     out_buf[offset++] = type_field;
     out_buf[offset++] = payload_prefix;
     memcpy(out_buf + offset, uri, uri_len);
