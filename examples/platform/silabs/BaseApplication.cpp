752bac22a9280ca7b558cd572eb90e67bb9a1909
Sarthak Shaha <sarthak.shaha@silabs.com>
2025-12-12 12:15:15 -0500

    fixes from sl-up

--- examples/platform/silabs/BaseApplication.cpp
+++ examples/platform/silabs/BaseApplication.cpp
@@ -393,51 +393,57 @@ bool BaseApplication::ActivateStatusLedPatterns()
     bool isPatternSet = false;
 #if (defined(ENABLE_WSTK_LEDS) && (defined(SL_CATALOG_SIMPLE_LED_LED1_PRESENT)))
 #ifdef MATTER_DM_PLUGIN_IDENTIFY_SERVER
+    bool isIdentifyActive = false;
     for (const auto & obj : IdentifyPool)
     {
         if (obj->mActive)
         {
-            // Identify in progress
-            // Do a steady blink on the status led
-            sStatusLED.Blink(250, 250);
-            isPatternSet = true;
+            isIdentifyActive = true;
+            break;
         }
-        else if (sIdentifyEffect != Clusters::Identify::EffectIdentifierEnum::kStopEffect)
+    }
+    if (isIdentifyActive)
+    {
+        // Identify in progress
+        // Do a steady blink on the status led
+        sStatusLED.Blink(250, 250);
+        isPatternSet = true;
+    }
+    else if (sIdentifyEffect != Clusters::Identify::EffectIdentifierEnum::kStopEffect)
+    {
+        // Identify trigger effect received. Do some on/off patterns on the status led
+        if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kBlink)
         {
-            // Identify trigger effect received. Do some on/off patterns on the status led
-            if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kBlink)
-            {
-                // Fast blink
-                sStatusLED.Blink(50, 50);
-            }
-            else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kBreathe)
-            {
-                // Slow blink
-                sStatusLED.Blink(1000, 1000);
-            }
-            else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kOkay)
-            {
-                // Pulse effect
-                sStatusLED.Blink(300, 700);
-            }
-            else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kChannelChange)
-            {
-                // Alternate between Short and Long pulses effect
-                static uint64_t mLastChangeTimeMS = 0;
-                static bool alternatePattern      = false;
-                uint32_t onTimeMS                 = alternatePattern ? 50 : 700;
-                uint32_t offTimeMS                = alternatePattern ? 950 : 300;
-
-                uint64_t nowMS = chip::System::SystemClock().GetMonotonicMilliseconds64().count();
-                if (nowMS >= mLastChangeTimeMS + 1000) // each pattern is done over a 1 second period
-                {
-                    mLastChangeTimeMS = nowMS;
-                    alternatePattern  = !alternatePattern;
-                    sStatusLED.Blink(onTimeMS, offTimeMS);
-                }
-            }
-            isPatternSet = true;
+            // Fast blink
+            sStatusLED.Blink(50, 50);
         }
+        else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kBreathe)
+        {
+            // Slow blink
+            sStatusLED.Blink(1000, 1000);
+        }
+        else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kOkay)
+        {
+            // Pulse effect
+            sStatusLED.Blink(300, 700);
+        }
+        else if (sIdentifyEffect == Clusters::Identify::EffectIdentifierEnum::kChannelChange)
+        {
+            // Alternate between Short and Long pulses effect
+            static uint64_t mLastChangeTimeMS = 0;
+            static bool alternatePattern      = false;
+            uint32_t onTimeMS                 = alternatePattern ? 50 : 700;
+            uint32_t offTimeMS                = alternatePattern ? 950 : 300;
+
+            uint64_t nowMS = chip::System::SystemClock().GetMonotonicMilliseconds64().count();
+            if (nowMS >= mLastChangeTimeMS + 1000) // each pattern is done over a 1 second period
+            {
+                mLastChangeTimeMS = nowMS;
+                alternatePattern  = !alternatePattern;
+                sStatusLED.Blink(onTimeMS, offTimeMS);
+            }
+        }
+        isPatternSet = true;
     }
 #endif // MATTER_DM_PLUGIN_IDENTIFY_SERVER
 
@@ -750,6 +756,12 @@ void BaseApplication::OnTriggerIdentifyEffect(Identify * identify)
         ChipLogProgress(Zcl, "No identifier effect");
     }
 }
+
+void emberAfIdentifyClusterInitCallback(chip::EndpointId endpoint)
+{
+    IdentifyPool.CreateObject(endpoint, BaseApplication::OnIdentifyStart, BaseApplication::OnIdentifyStop,
+                              Clusters::Identify::IdentifyTypeEnum::kVisibleIndicator, BaseApplication::OnTriggerIdentifyEffect);
+}
 #endif // MATTER_DM_PLUGIN_IDENTIFY_SERVER
 
 void BaseApplication::LightTimerEventHandler(void * timerCbArg)
@@ -1007,9 +1019,4 @@ void BaseApplication::OutputQrCode(bool refreshLCD)
 bool BaseApplication::GetProvisionStatus()
 {
     return BaseApplication::sIsProvisioned;
-}
-void emberAfIdentifyClusterInitCallback(chip::EndpointId endpoint)
-{
-    IdentifyPool.CreateObject(endpoint, BaseApplication::OnIdentifyStart, BaseApplication::OnIdentifyStop,
-                              Clusters::Identify::IdentifyTypeEnum::kVisibleIndicator, BaseApplication::OnTriggerIdentifyEffect);
 }
