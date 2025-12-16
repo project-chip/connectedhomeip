4b0a6d895cdce27caecb45d6d15abab903319682
Marek Pikuła <m.pikula@partner.samsung.com>
2025-12-16 22:42:12 +0100

    Use communicate instead of wait
    Signed-off-by: Marek Pikuła <m.pikula@partner.samsung.com>

--- scripts/tests/chiptest/accessories.py
+++ scripts/tests/chiptest/accessories.py
@@ -149,7 +149,11 @@ class AppsRegister:
         s = subprocess.Popen(cmd)
         # We need to have some timeout so that in case the process hangs we don't wait infinitely in CI. 60 seconds is large enough
         # for the OTA tool.
-        s.wait(60)
+        try:
+            s.communicate(timeout=60)
+        except subprocess.TimeoutExpired:
+            s.kill()
+            raise RuntimeError('OTA image tool timed out')
         if s.returncode != 0:
             raise RuntimeError('Cannot create OTA image file')
         return True
