b1637c6fcd2b66d8d13e21f2e4af93c0dc902a44
Restyled.io <commits@restyled.io>
2025-12-16 16:24:27 +0000

    Restyled by isort

--- src/python_testing/TC_IDM_9_1.py
+++ src/python_testing/TC_IDM_9_1.py
@@ -36,17 +36,18 @@
 # === END CI TEST ARGUMENTS ===
 
 import logging
-from mobly import asserts
+import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import Optional
-import xml.etree.ElementTree as ET
+
+from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.interaction_model import InteractionModelError, Status
-from matter.tlv import uint
 from matter.testing.basic_composition import BasicCompositionTests
 from matter.testing.global_attribute_ids import GlobalAttributeIds, is_standard_attribute_id, is_standard_cluster_id
 from matter.testing.matter_testing import MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+from matter.tlv import uint
 
 log = logging.getLogger(__name__)
 
