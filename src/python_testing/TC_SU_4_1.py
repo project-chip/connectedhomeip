0f88e7941291d4b8f88fd4d621519949dd11119a
juandediosg <jdelgado@griddynamics.com>
2025-12-16 20:28:10 -0600

    Update: Code-lints and restyled.

--- src/python_testing/TC_SU_4_1.py
+++ src/python_testing/TC_SU_4_1.py
@@ -47,7 +47,6 @@ import matter.clusters as Clusters
 from matter import ChipDeviceCtrl
 from matter.interaction_model import Status
 from matter.testing.matter_testing import TestStep, async_test_body, default_matter_test_main
-from test_plan_support import remove_fabric
 
 # Create a logger
 logger = logging.getLogger(__name__)
