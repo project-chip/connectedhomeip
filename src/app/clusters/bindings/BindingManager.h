40adb117a7f00ee28ad6d5d947b4079b7de23096
Ernesto Messina <emessina@griddynamics.com>
2025-12-17 11:47:38 -0300

    moving BindingManager CreateBindingEntry logic into BindingCluster.

--- src/app/clusters/bindings/BindingManager.h
+++ src/app/clusters/bindings/BindingManager.h
@@ -183,17 +183,6 @@ private:
 
 } // namespace Binding
 
-/**
- * @brief appends a binding to the list of bindings
- *        This function is to be used when a device wants to add a binding to its own table
- *        If entry is a unicast binding, BindingManager will be notified and will establish a case session with the peer device
- *        Entry will be added to the binding table and persisted into storage
- *        BindingManager will be notified and the binding added callback will be called if it has been set
- *
- * @param entry binding to add
- */
-CHIP_ERROR AddBindingEntry(const Binding::TableEntry & entry);
-
 } // namespace Clusters
 } // namespace app
 } // namespace chip
