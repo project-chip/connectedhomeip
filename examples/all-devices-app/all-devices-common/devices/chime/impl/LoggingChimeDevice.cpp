3b80d8de6449ebb008eb413f66f061e130267d2c
Sergio Soares <sergiosoares@google.com>
2025-12-19 16:35:44 +0000

    fix include

--- examples/all-devices-app/all-devices-common/devices/chime/impl/LoggingChimeDevice.cpp
+++ examples/all-devices-app/all-devices-common/devices/chime/impl/LoggingChimeDevice.cpp
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "LoggingChimeDevice.h"
+#include <devices/chime/impl/LoggingChimeDevice.h>
 #include <lib/support/logging/CHIPLogging.h>
 
 using namespace chip::app::Clusters;
