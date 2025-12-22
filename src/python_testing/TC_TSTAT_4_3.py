6006666fbf60b39bb76830a8819798097dfe2cc4
Ramiro Recchia <rrecchia@google.com>
2025-12-22 16:21:50 -0300

    Missed one import

--- src/python_testing/TC_TSTAT_4_3.py
+++ src/python_testing/TC_TSTAT_4_3.py
@@ -44,7 +44,8 @@ from mobly import asserts
 import matter.clusters as Clusters
 from matter.interaction_model import InteractionModelError, Status
 from matter.testing.decorators import async_test_body
-from matter.testing.matter_testing import MatterBaseTest, TestStep, default_matter_test_main
+from matter.testing.matter_testing import MatterBaseTest, TestStep
+from matter.testing.runner import default_matter_test_main
 
 log = logging.getLogger(__name__)
 
