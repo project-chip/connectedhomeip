781a4346dee7ce112784c08edc4d01a48fea271a
Moises Terrones <moises.tersal@gmail.com>
2025-12-23 10:16:35 -0600

    Apply suggestions from code review
    Co-authored-by: Boris Zbarsky <bzbarsky@apple.com>

--- src/app/clusters/valve-configuration-and-control-server/TimeSyncTracker.h
+++ src/app/clusters/valve-configuration-and-control-server/TimeSyncTracker.h
@@ -23,7 +23,7 @@ namespace chip::app::Clusters {
 /// of the TimeSynchronization cluster and UTC time.
 ///
 /// This is needed for the Feature TimeSync of the ValveConfigurationAndControl
-/// cluster, according to spec this Feature shall only be enabled if the
+/// cluster. According to spec this Feature shall only be enabled if the
 /// TimeSynchronization cluster is available.
 /// For the AutoCloseTime, the TimeSynchronization cluster should also have a
 /// valid UTC time.
