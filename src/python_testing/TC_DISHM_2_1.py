049aac39ce38d671729641b55ec0f6b35d0ff2b3
Ramiro Recchia <rrecchia@google.com>
2026-01-06 14:15:55 -0300

    Update imports

--- src/python_testing/TC_DISHM_2_1.py
+++ src/python_testing/TC_DISHM_2_1.py
@@ -48,8 +48,9 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.testing.matter_asserts import is_valid_int_value
-from matter.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_cluster, matchers,
-                                           run_if_endpoint_matches)
+from matter.testing.decorators import has_cluster, run_if_endpoint_matches
+from matter.testing.matter_testing import MatterBaseTest, TestStep, matchers
+from matter.testing.runner import default_matter_test_main
 
 logger = logging.getLogger(__name__)
 
