4bac33c021ff7688d24f3240bd13ce608aeb887b
Zaid Omer <zaidomer@google.com>
2026-01-06 12:36:46 -0500

    Fix const attribute sets in tests

--- src/app/clusters/basic-information/tests/TestBasicInformationReadWrite.cpp
+++ src/app/clusters/basic-information/tests/TestBasicInformationReadWrite.cpp
@@ -235,7 +235,7 @@ TEST_F(TestBasicInformationReadWrite, TestAllAttributesSpecCompliance)
 {
     using namespace chip::app::Clusters::BasicInformation;
 
-    const BasicInformationCluster::OptionalAttributesSet optionalAttributeSet;
+    BasicInformationCluster::OptionalAttributesSet optionalAttributeSet;
     optionalAttributeSet.Set<Attributes::ManufacturingDate::Id>();
     BasicInformationCluster cluster(optionalAttributeSet, &gMockDeviceInstanceInfoProvider);
     chip::Testing::ClusterTester tester(cluster);
@@ -407,7 +407,7 @@ TEST_F(TestBasicInformationReadWrite, TestWriteLocalConfigDisabled)
 {
     bool readValue{};
 
-    const BasicInformationCluster::OptionalAttributesSet optionalAttributeSet;
+    BasicInformationCluster::OptionalAttributesSet optionalAttributeSet;
     optionalAttributeSet.Set<Attributes::LocalConfigDisabled::Id>();
     BasicInformationCluster cluster(optionalAttributeSet, &gMockDeviceInstanceInfoProvider);
     ASSERT_EQ(cluster.Startup(testContext.Get()), CHIP_NO_ERROR);
