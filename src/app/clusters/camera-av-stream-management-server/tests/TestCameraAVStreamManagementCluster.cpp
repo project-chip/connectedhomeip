e9c9ba75672b94f3ec9253d555d35869088576b3
Pradip De <pradipd@google.com>
2026-01-07 18:53:52 -0800

    Add more attribute read tests to read initialized values.

--- src/app/clusters/camera-av-stream-management-server/tests/TestCameraAVStreamManagementCluster.cpp
+++ src/app/clusters/camera-av-stream-management-server/tests/TestCameraAVStreamManagementCluster.cpp
@@ -15,6 +15,7 @@
  */
 #include <pw_unit_test/framework.h>
 
+#include <app/AttributeValueDecoder.h>
 #include <app/CommandHandler.h>
 #include <app/clusters/camera-av-stream-management-server/CameraAVStreamManagementCluster.h>
 #include <app/data-model-provider/MetadataTypes.h>
@@ -314,7 +315,7 @@ TEST_F(TestCameraAVStreamManagementCluster, TestReadMaxEncodedPixelRate)
 
 TEST_F(TestCameraAVStreamManagementCluster, TestReadVideoSensorParams)
 {
-    Attributes::VideoSensorParams::TypeInfo::Type videoSensorParams;
+    Attributes::VideoSensorParams::TypeInfo::DecodableType videoSensorParams;
     EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::VideoSensorParams::Id, videoSensorParams), CHIP_NO_ERROR);
     EXPECT_EQ(videoSensorParams.sensorWidth, 1920);
     EXPECT_EQ(videoSensorParams.sensorHeight, 1080);
@@ -333,29 +334,17 @@ TEST_F(TestCameraAVStreamManagementCluster, TestReadMinViewportResolution)
 
 TEST_F(TestCameraAVStreamManagementCluster, TestReadRateDistortionTradeOffPoints)
 {
-    // Create a mock attribute request
-    chip::app::DataModel::ReadAttributeRequest request;
-    request.path.mEndpointId  = kTestEndpointId;
-    request.path.mClusterId   = CameraAvStreamManagement::Id;
-    request.path.mAttributeId = Attributes::RateDistortionTradeOffPoints::Id;
-
-    // Create a buffer for encoding
-    chip::Platform::ScopedMemoryBufferWithSize<uint8_t> buffer;
-    ASSERT_TRUE(buffer.Alloc(1024));
-
-    // Create AttributeReportIBs::Builder for the encoder
-    chip::app::AttributeReportIBs::Builder attributeReportIBsBuilder;
-    chip::TLV::TLVWriter reportWriter;
-    reportWriter.Init(buffer.Get(), buffer.AllocatedSize());
-    CHIP_ERROR err = attributeReportIBsBuilder.Init(&reportWriter);
-    ASSERT_EQ(err, CHIP_NO_ERROR);
-
-    chip::app::AttributeValueEncoder encoder(attributeReportIBsBuilder, chip::Access::SubjectDescriptor{}, request.path,
-                                             0 /* dataVersion */);
-
-    // Read the attribute
-    auto status = mServer.ReadAttribute(request, encoder);
-    EXPECT_TRUE(status.IsSuccess());
+    Attributes::RateDistortionTradeOffPoints::TypeInfo::DecodableType rateDistortionTradeOffPoints;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::RateDistortionTradeOffPoints::Id, rateDistortionTradeOffPoints),
+              CHIP_NO_ERROR);
+    auto it       = rateDistortionTradeOffPoints.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 1u);
 }
 
 TEST_F(TestCameraAVStreamManagementCluster, TestReadMaxContentBufferSize)
@@ -363,6 +352,276 @@ TEST_F(TestCameraAVStreamManagementCluster, TestReadMaxContentBufferSize)
     uint32_t maxContentBufferSize = 0;
     EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MaxContentBufferSize::Id, maxContentBufferSize), CHIP_NO_ERROR);
     EXPECT_EQ(maxContentBufferSize, 4096u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneCapabilities)
+{
+    Attributes::MicrophoneCapabilities::TypeInfo::DecodableType microphoneCapabilities;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneCapabilities::Id, microphoneCapabilities), CHIP_NO_ERROR);
+
+    EXPECT_EQ(microphoneCapabilities.maxNumberOfChannels, 2);
+
+    auto it       = microphoneCapabilities.supportedCodecs.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 2u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSpeakerCapabilities)
+{
+    Attributes::SpeakerCapabilities::TypeInfo::DecodableType speakerCapabilities;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SpeakerCapabilities::Id, speakerCapabilities), CHIP_NO_ERROR);
+
+    EXPECT_EQ(speakerCapabilities.maxNumberOfChannels, 2);
+
+    auto it       = speakerCapabilities.supportedCodecs.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 2u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadTwoWayTalkSupport)
+{
+    Attributes::TwoWayTalkSupport::TypeInfo::Type twoWayTalkSupport = TwoWayTalkSupportTypeEnum::kNotSupported;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::TwoWayTalkSupport::Id, twoWayTalkSupport), CHIP_NO_ERROR);
+    EXPECT_EQ(twoWayTalkSupport, TwoWayTalkSupportTypeEnum::kFullDuplex);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSnapshotCapabilities)
+{
+    Attributes::SnapshotCapabilities::TypeInfo::DecodableType snapshotCapabilities;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SnapshotCapabilities::Id, snapshotCapabilities), CHIP_NO_ERROR);
+    auto it       = snapshotCapabilities.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 2u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMaxNetworkBandwidth)
+{
+    uint32_t maxNetworkBandwidth = 0;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MaxNetworkBandwidth::Id, maxNetworkBandwidth), CHIP_NO_ERROR);
+    EXPECT_EQ(maxNetworkBandwidth, 128000000u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadCurrentFrameRate)
+{
+    Attributes::CurrentFrameRate::TypeInfo::Type currentFrameRate = 0;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::CurrentFrameRate::Id, currentFrameRate), CHIP_NO_ERROR);
+    EXPECT_EQ(currentFrameRate, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSupportedStreamUsages)
+{
+    Attributes::SupportedStreamUsages::TypeInfo::DecodableType supportedStreamUsages;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SupportedStreamUsages::Id, supportedStreamUsages), CHIP_NO_ERROR);
+    auto it       = supportedStreamUsages.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 2u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadAllocatedVideoStreams)
+{
+    Attributes::AllocatedVideoStreams::TypeInfo::DecodableType allocatedVideoStreams;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::AllocatedVideoStreams::Id, allocatedVideoStreams), CHIP_NO_ERROR);
+    auto it       = allocatedVideoStreams.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 0u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadAllocatedAudioStreams)
+{
+    Attributes::AllocatedAudioStreams::TypeInfo::DecodableType allocatedAudioStreams;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::AllocatedAudioStreams::Id, allocatedAudioStreams), CHIP_NO_ERROR);
+    auto it       = allocatedAudioStreams.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 0u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadAllocatedSnapshotStreams)
+{
+    Attributes::AllocatedSnapshotStreams::TypeInfo::DecodableType allocatedSnapshotStreams;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::AllocatedSnapshotStreams::Id, allocatedSnapshotStreams), CHIP_NO_ERROR);
+    auto it       = allocatedSnapshotStreams.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 0u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadStreamUsagePriorities)
+{
+    Attributes::StreamUsagePriorities::TypeInfo::DecodableType streamUsagePriorities;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::StreamUsagePriorities::Id, streamUsagePriorities), CHIP_NO_ERROR);
+    auto it       = streamUsagePriorities.begin();
+    uint8_t count = 0;
+    while (it.Next())
+    {
+        ++count;
+    }
+    EXPECT_EQ(it.GetStatus(), CHIP_NO_ERROR);
+    EXPECT_EQ(count, 2u);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSoftRecordingPrivacyModeEnabled)
+{
+    bool softRecordingPrivacyModeEnabled = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SoftRecordingPrivacyModeEnabled::Id, softRecordingPrivacyModeEnabled),
+              CHIP_NO_ERROR);
+    EXPECT_FALSE(softRecordingPrivacyModeEnabled);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSoftLivestreamPrivacyModeEnabled)
+{
+    bool softLivestreamPrivacyModeEnabled = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SoftLivestreamPrivacyModeEnabled::Id, softLivestreamPrivacyModeEnabled),
+              CHIP_NO_ERROR);
+    EXPECT_FALSE(softLivestreamPrivacyModeEnabled);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadHardPrivacyModeOn)
+{
+    bool hardPrivacyModeOn = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::HardPrivacyModeOn::Id, hardPrivacyModeOn), CHIP_NO_ERROR);
+    EXPECT_FALSE(hardPrivacyModeOn);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadViewport)
+{
+    Attributes::Viewport::TypeInfo::Type viewport;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::Viewport::Id, viewport), CHIP_NO_ERROR);
+    EXPECT_EQ(viewport.x1, 0);
+    EXPECT_EQ(viewport.y1, 0);
+    EXPECT_EQ(viewport.x2, 0);
+    EXPECT_EQ(viewport.y2, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSpeakerMuted)
+{
+    bool speakerMuted = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SpeakerMuted::Id, speakerMuted), CHIP_NO_ERROR);
+    EXPECT_FALSE(speakerMuted);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSpeakerVolumeLevel)
+{
+    uint8_t speakerVolumeLevel = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SpeakerVolumeLevel::Id, speakerVolumeLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(speakerVolumeLevel, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSpeakerMaxLevel)
+{
+    uint8_t speakerMaxLevel = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SpeakerMaxLevel::Id, speakerMaxLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(speakerMaxLevel, kMaxSpeakerLevel);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadSpeakerMinLevel)
+{
+    uint8_t speakerMinLevel = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::SpeakerMinLevel::Id, speakerMinLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(speakerMinLevel, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneMuted)
+{
+    bool microphoneMuted = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneMuted::Id, microphoneMuted), CHIP_NO_ERROR);
+    EXPECT_FALSE(microphoneMuted);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneVolumeLevel)
+{
+    uint8_t microphoneVolumeLevel = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneVolumeLevel::Id, microphoneVolumeLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(microphoneVolumeLevel, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneMaxLevel)
+{
+    uint8_t microphoneMaxLevel = 0;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneMaxLevel::Id, microphoneMaxLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(microphoneMaxLevel, kMaxMicrophoneLevel);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneMinLevel)
+{
+    uint8_t microphoneMinLevel = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneMinLevel::Id, microphoneMinLevel), CHIP_NO_ERROR);
+    EXPECT_EQ(microphoneMinLevel, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadMicrophoneAGCEnabled)
+{
+    bool microphoneAGCEnabled = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::MicrophoneAGCEnabled::Id, microphoneAGCEnabled), CHIP_NO_ERROR);
+    EXPECT_FALSE(microphoneAGCEnabled);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadImageRotation)
+{
+    uint16_t imageRotation = 1;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::ImageRotation::Id, imageRotation), CHIP_NO_ERROR);
+    EXPECT_EQ(imageRotation, 0);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadImageFlipHorizontal)
+{
+    bool imageFlipHorizontal = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::ImageFlipHorizontal::Id, imageFlipHorizontal), CHIP_NO_ERROR);
+    EXPECT_FALSE(imageFlipHorizontal);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadImageFlipVertical)
+{
+    bool imageFlipVertical = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::ImageFlipVertical::Id, imageFlipVertical), CHIP_NO_ERROR);
+    EXPECT_FALSE(imageFlipVertical);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadStatusLightEnabled)
+{
+    bool statusLightEnabled = true;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::StatusLightEnabled::Id, statusLightEnabled), CHIP_NO_ERROR);
+    EXPECT_FALSE(statusLightEnabled);
+}
+
+TEST_F(TestCameraAVStreamManagementCluster, TestReadStatusLightBrightness)
+{
+    Attributes::StatusLightBrightness::TypeInfo::Type statusLightBrightness = Globals::ThreeLevelAutoEnum::kAuto;
+    EXPECT_EQ(mClusterTester.ReadAttribute(Attributes::StatusLightBrightness::Id, statusLightBrightness), CHIP_NO_ERROR);
+    EXPECT_EQ(statusLightBrightness, Globals::ThreeLevelAutoEnum::kMedium);
 }
 
 } // namespace
