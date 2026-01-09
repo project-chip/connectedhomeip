e413d57ce141953245c61fbcb3f80b86d806d053
Restyled.io <commits@restyled.io>
2026-01-09 20:13:51 +0000

    Restyled by isort

--- src/python_testing/test_testing/TestConformanceSupport.py
+++ src/python_testing/test_testing/TestConformanceSupport.py
@@ -20,9 +20,10 @@ from typing import Callable
 
 from mobly import asserts
 
-from matter.testing.conformance import (Choice, Conformance, ConformanceAssessmentData, ConformanceDecision, ConformanceException,
-                                        ConformanceParseParameters, EMPTY_CLUSTER_GLOBAL_ATTRIBUTES, deprecated, disallowed, mandatory, optional,
-                                        parse_basic_callable_from_xml, parse_callable_from_xml, provisional, zigbee)
+from matter.testing.conformance import (EMPTY_CLUSTER_GLOBAL_ATTRIBUTES, Choice, Conformance, ConformanceAssessmentData,
+                                        ConformanceDecision, ConformanceException, ConformanceParseParameters, deprecated,
+                                        disallowed, mandatory, optional, parse_basic_callable_from_xml, parse_callable_from_xml,
+                                        provisional, zigbee)
 from matter.testing.matter_testing import MatterBaseTest
 from matter.testing.runner import default_matter_test_main
 from matter.tlv import uint
