ad8253115a0bbd678a40e8d4389b8a280369e271
Olivier LORENTE <olivier.lorente@st.com>
2026-01-12 10:28:33 +0100

    Changes following code review.

--- src/python_testing/matter_testing_infrastructure/matter/testing/nfc.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/nfc.py
@@ -1,9 +1,26 @@
+#
+#    Copyright (c) 2026 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
 import asyncio
 import logging
 import threading
 
-import ndef
-import smartcard
+import ndef         # From 'ndeflib' library
+import smartcard    # From 'pyscard' library
 import smartcard.CardMonitoring
 from mobly import asserts
 
@@ -147,7 +164,7 @@ class NFCReader:
             ndef_uri (str): The URI string extracted from the NFC tag.
 
         Returns:
-            bool: True if the string starts with 'mt:' (case-insensitive), False otherwise.
+            bool: True if the string starts with 'MT:', False otherwise.
 
         Example:
             >>> is_onboarding_data("mt:ABC123")
@@ -157,7 +174,7 @@ class NFCReader:
         """
         if ndef_uri is None:
             return False
-        return ndef_uri.lower().startswith("mt:")
+        return ndef_uri.startswith("MT:")
 
     def activate_tag_monitoring(self):
         """
