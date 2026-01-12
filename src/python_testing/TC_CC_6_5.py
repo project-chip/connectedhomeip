214bb909a314dcae79c43a3d385eefd7f8dd8411
Fran <franluntref@gmail.com>
2026-01-12 14:30:45 -0300

    Fixed imports and PICS

--- src/python_testing/TC_CC_6_5.py
+++ src/python_testing/TC_CC_6_5.py
@@ -41,8 +41,9 @@ from mobly import asserts
 import matter.clusters as Clusters
 from matter.clusters.Types import NullValue
 from matter.testing import matter_asserts
-from matter.testing.matter_testing import (MatterBaseTest, TestStep, default_matter_test_main, has_attribute, has_command,
-                                           run_if_endpoint_matches)
+from matter.testing.decorators import has_attribute, has_command, run_if_endpoint_matches
+from matter.testing.matter_testing import MatterBaseTest
+from matter.testing.runner import default_matter_test_main, TestStep
 
 log = logging.getLogger(__name__)
 
@@ -58,6 +59,9 @@ class TC_CC_6_5(MatterBaseTest):
             "Cluster server."
         )
 
+    def pics_TC_CC_6_5(self) -> list[str]:
+        return ["CC.S"]
+    
     def steps_TC_CC_6_5(self) -> list[TestStep]:
         return [
             TestStep("0", "Commissioning, already done", is_commissioning=True),
