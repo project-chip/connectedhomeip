f8bac945f40decde5651a610fb60588c35e9c1c4
Ramiro Recchia <rrecchia@google.com>
2026-01-13 11:44:21 -0300

    fix imports

--- src/python_testing/TC_SU_2_5.py
+++ src/python_testing/TC_SU_2_5.py
@@ -57,8 +57,9 @@ from TC_SUTestBase import SoftwareUpdateBaseTest
 import matter.clusters as Clusters
 from matter import ChipDeviceCtrl
 from matter.clusters.Types import NullValue
+from matter.testing.decorators import async_test_body
 from matter.testing.event_attribute_reporting import AttributeSubscriptionHandler
-from matter.testing.matter_testing import AttributeMatcher, TestStep, async_test_body
+from matter.testing.matter_testing import AttributeMatcher, TestStep
 from matter.testing.runner import default_matter_test_main
 
 logger = logging.getLogger(__name__)
