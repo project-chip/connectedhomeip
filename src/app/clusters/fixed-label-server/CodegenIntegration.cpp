32f07ad12235e18db77ac7065675992d86507c40
AniDashyan <ani.dashyan77@gmail.com>
2026-01-15 17:48:41 +0400

    Address comments

--- src/app/clusters/fixed-label-server/CodegenIntegration.cpp
+++ src/app/clusters/fixed-label-server/CodegenIntegration.cpp
@@ -41,7 +41,7 @@ public:
     ServerClusterRegistration & CreateRegistration(EndpointId endpointId, unsigned clusterInstanceIndex,
                                                    uint32_t optionalAttributeBits, uint32_t featureMap) override
     {
-        gServers[clusterInstanceIndex].Create(endpointId);
+        gServers[clusterInstanceIndex].Create(endpointId, DeviceLayer::GetDeviceInfoProvider());
         return gServers[clusterInstanceIndex].Registration();
     }
 
