364698ef0b1487e4cb573cfb0406d8bb3e02f33a
Andrei Litvin <andy314@gmail.com>
2026-01-16 08:56:32 -0500

    Support test definitions to have an array of targets. Make targets be more than just a string.

--- scripts/tests/chiptest/__init__.py
+++ scripts/tests/chiptest/__init__.py
@@ -24,7 +24,7 @@ from pathlib import Path
 from typing import Iterator, Set
 
 from . import runner
-from .test_definition import TestDefinition, TestTag, TestTarget
+from .test_definition import TestDefinition, TestTag, TestTarget, StandardTargets
 
 log = logging.getLogger(__name__)
 
@@ -278,77 +278,6 @@ def _AllYamlTests():
         yield path
 
 
-def target_for_name(name: str):
-    if (name.startswith("TV_") or name.startswith("Test_TC_MC_") or
-            name.startswith("Test_TC_LOWPOWER_") or name.startswith("Test_TC_KEYPADINPUT_") or
-            name.startswith("Test_TC_APPLAUNCHER_") or name.startswith("Test_TC_MEDIAINPUT_") or
-            name.startswith("Test_TC_WAKEONLAN_") or name.startswith("Test_TC_CHANNEL_") or
-            name.startswith("Test_TC_MEDIAPLAYBACK_") or name.startswith("Test_TC_AUDIOOUTPUT_") or
-            name.startswith("Test_TC_TGTNAV_") or name.startswith("Test_TC_APBSC_") or
-            name.startswith("Test_TC_CONTENTLAUNCHER_") or name.startswith("Test_TC_ALOGIN_")):
-        return TestTarget.TV
-    if name.startswith("DL_") or name.startswith("Test_TC_DRLK_"):
-        return TestTarget.LOCK
-    if name.startswith("TestFabricSync"):
-        return TestTarget.FABRIC_SYNC
-    if name.startswith("OTA_"):
-        return TestTarget.OTA
-    if name.startswith("Test_TC_BRBINFO_") or name.startswith("Test_TC_ACT_"):
-        return TestTarget.BRIDGE
-    if name.startswith("TestIcd") or name.startswith("Test_TC_ICDM_"):
-        return TestTarget.LIT_ICD
-    if name.startswith("Test_TC_MWOCTRL_") or name.startswith("Test_TC_MWOM_"):
-        return TestTarget.MWO
-    if name.startswith("Test_TC_RVCRUNM_") or name.startswith("Test_TC_RVCCLEANM_") or name.startswith("Test_TC_RVCOPSTATE_"):
-        return TestTarget.RVC
-    if name.startswith("Test_TC_TBRM_") or name.startswith("Test_TC_THNETDIR_") or name.startswith("Test_TC_WIFINM_"):
-        return TestTarget.NETWORK_MANAGER
-    if name.startswith("Test_TC_MTRID_"):
-        return TestTarget.ENERGY_GATEWAY
-    if (name.startswith("Test_TC_DEM_") or name.startswith("Test_TC_DEMM_") or
-            name.startswith("Test_TC_EEVSE_") or name.startswith("Test_TC_EEVSEM_")):
-        return TestTarget.ENERGY_MANAGEMENT
-    if name.startswith("Test_TC_CLCTRL_") or name.startswith("Test_TC_CLDIM_"):
-        return TestTarget.CLOSURE
-    return TestTarget.ALL_CLUSTERS
-
-
-def tests_with_command(chip_tool: str, is_manual: bool):
-    """Executes `chip_tool` binary to see what tests are available, using cmd
-    to get the list.
-    """
-    cmd_list = "list"
-    if is_manual:
-        cmd_list += "-manual"
-
-    cmd = [chip_tool, "tests", cmd_list]
-    result = subprocess.run(cmd, capture_output=True, encoding="utf-8")
-    if result.returncode != 0:
-        log.error("Failed to run %s:", shlex.join(cmd))
-        log.error("STDOUT: %s", result.stdout)
-        log.error("STDERR: %s", result.stderr)
-        result.check_returncode()
-
-    test_tags: set[TestTag] = set()
-    if is_manual:
-        test_tags.add(TestTag.MANUAL)
-
-    in_development_tests = [s.replace(".yaml", "") for s in _GetInDevelopmentTests()]
-
-    for name in result.stdout.split("\n"):
-        if not name:
-            continue
-
-        target = target_for_name(name)
-        tags = test_tags.copy()
-        if name in in_development_tests:
-            tags.add(TestTag.IN_DEVELOPMENT)
-
-        yield TestDefinition(
-            run_name=name, name=name, target=target, tags=tags
-        )
-
-
 def _AllFoundYamlTests(treat_repl_unsupported_as_in_development: bool, treat_dft_unsupported_as_in_development: bool, treat_chip_tool_unsupported_as_in_development: bool, use_short_run_name: bool):
     """
     use_short_run_name should be true if we want the run_name to be "Test_ABC" instead of "some/path/Test_ABC.yaml"
@@ -400,10 +329,13 @@ def _AllFoundYamlTests(treat_repl_unsupported_as_in_development: bool, treat_dft
         if treat_chip_tool_unsupported_as_in_development and run_name in chip_tool_unsupported_as_in_development_tests:
             tags.add(TestTag.IN_DEVELOPMENT)
 
+        # TODO: figure out the targets
+        targets = [StandardTargets.for_test_name(path.name)]
+
         yield TestDefinition(
             run_name=run_name,
             name=path.stem,  # `path.stem` converts "some/path/Test_ABC_1.2.yaml" to "Test_ABC.1.2"
-            target=target_for_name(path.name),
+            targets=targets,
             tags=tags,
         )
 
@@ -422,10 +354,3 @@ def AllDarwinFrameworkToolYamlTests():
     for test in _AllFoundYamlTests(treat_repl_unsupported_as_in_development=False, treat_dft_unsupported_as_in_development=True, treat_chip_tool_unsupported_as_in_development=False, use_short_run_name=True):
         yield test
 
-
-def AllChipToolTests(chip_tool: str):
-    for test in tests_with_command(chip_tool, is_manual=False):
-        yield test
-
-    for test in tests_with_command(chip_tool, is_manual=True):
-        yield test
