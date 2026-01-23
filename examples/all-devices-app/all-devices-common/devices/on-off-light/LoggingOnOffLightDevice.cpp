c4cd856b064b209c3e0d5d0df36553a837b9e22e
Andrei Litvin <andy314@gmail.com>
2026-01-23 18:56:28 +0000

    Add an identify delegate: on/off light mandates that we have a trigger effect supported.

--- examples/all-devices-app/all-devices-common/devices/on-off-light/LoggingOnOffLightDevice.cpp
+++ examples/all-devices-app/all-devices-common/devices/on-off-light/LoggingOnOffLightDevice.cpp
@@ -64,10 +64,70 @@ public:
     }
 };
 
+class LoggingIdentifyDelegate : public IdentifyDelegate
+{
+public:
+    void OnIdentifyStart(IdentifyCluster & cluster) override { ChipLogProgress(DeviceLayer, "Identify: START"); }
+
+    void OnIdentifyStop(IdentifyCluster & cluster) override { ChipLogProgress(DeviceLayer, "Identify: STOP"); }
+
+    void OnTriggerEffect(IdentifyCluster & cluster) override
+    {
+        StringBuilder<48> msg;
+
+        switch (cluster.GetEffectIdentifier())
+        {
+
+        case Identify::EffectIdentifierEnum::kBlink:
+            msg.Add("BlinkEffect");
+            break;
+        case Identify::EffectIdentifierEnum::kBreathe:
+            msg.Add("BreatheEffect");
+            break;
+        case Identify::EffectIdentifierEnum::kOkay:
+            msg.Add("OkayEffect");
+            break;
+        case Identify::EffectIdentifierEnum::kChannelChange:
+            msg.Add("ChannelChangeEffect");
+            break;
+        case Identify::EffectIdentifierEnum::kFinishEffect:
+            msg.Add("FinishEffect");
+            break;
+        case Identify::EffectIdentifierEnum::kStopEffect:
+            msg.Add("StopEffect");
+            break;
+        default:
+            msg.AddFormat("UnknownEffect(%d)", static_cast<int>(cluster.GetEffectIdentifier()));
+            break;
+        }
+        msg.Add(" / ");
+
+        switch (cluster.GetEffectVariant())
+        {
+        case Identify::EffectVariantEnum::kDefault:
+            msg.Add("DefaultVariant");
+            break;
+        default:
+            msg.AddFormat("UnknownVariant(%d)", static_cast<int>(cluster.GetEffectVariant()));
+            break;
+        }
+
+        ChipLogProgress(DeviceLayer, "Identify: Trigger effect: %s", msg.c_str());
+    }
+
+    bool IsTriggerEffectEnabled() const override { return true; }
+};
+
 LoggingOnOffEffectDelegate & LoggingDelegate()
 {
     static LoggingOnOffEffectDelegate gOnOffEffectDelegate;
     return gOnOffEffectDelegate;
+}
+
+LoggingIdentifyDelegate & IdentifyDelegate()
+{
+    static LoggingIdentifyDelegate gIdentifyDelegate;
+    return gIdentifyDelegate;
 }
 
 } // namespace
@@ -76,7 +136,7 @@ CHIP_ERROR LoggingOnOffLightDevice::Register(chip::EndpointId endpoint, CodeDriv
 {
     ReturnErrorOnFailure(SingleEndpointRegistration(endpoint, provider, parentId));
 
-    mIdentifyCluster.Create(IdentifyCluster::Config(endpoint, mContext.timerDelegate));
+    mIdentifyCluster.Create(IdentifyCluster::Config(endpoint, mContext.timerDelegate).WithDelegate(&IdentifyDelegate()));
     ReturnErrorOnFailure(provider.AddCluster(mIdentifyCluster.Registration()));
 
     mScenesTableProvider.SetEndpoint(endpoint);
