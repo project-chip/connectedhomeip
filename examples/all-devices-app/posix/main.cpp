11b2b909b4a2d84ece883211c96da62f105d7c9a
Andrei Litvin <andy314@gmail.com>
2026-01-26 12:16:39 -0500

    Fix bad restyle in main.

--- examples/all-devices-app/posix/main.cpp
+++ examples/all-devices-app/posix/main.cpp
@@ -89,9 +89,13 @@ public:
         mContext(context), mDataModelProvider(mContext.storageDelegate, mAttributePersistence), mRootNode({
             .commissioningWindowManager = mContext.commissioningWindowManager,
             .configurationManager = mContext.configurationManager, .deviceControlServer = mContext.deviceControlServer,
+
             .fabricTable = mContext.fabricTable, .failsafeContext = mContext.failsafeContext,
+
             .platformManager = mContext.platformManager, .groupDataProvider = mContext.groupDataProvider,
+
             .sessionManager = mContext.sessionManager, .dnssdServer = mContext.dnssdServer,
+
 #if CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
             .termsAndConditionsProvider = mContext.termsAndConditionsProvider,
 #endif // CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
@@ -149,16 +153,19 @@ void RunApplication(AppMainLoopImplementation * mainLoop = nullptr)
     Credentials::SetGroupDataProvider(&gGroupDataProvider);
 
     static CodeDrivenDataModelDevices devices({
-        .storageDelegate            = *initParams.persistentStorageDelegate,
-        .commissioningWindowManager = Server::GetInstance().GetCommissioningWindowManager(),
-        .configurationManager       = DeviceLayer::ConfigurationMgr(),
-        .deviceControlServer        = DeviceLayer::DeviceControlServer::DeviceControlSvr(),
-        .fabricTable = Server::GetInstance().GetFabricTable(), .failsafeContext = Server::GetInstance().GetFailSafeContext(),
-        .platformManager = DeviceLayer::PlatformMgr(), .groupDataProvider = gGroupDataProvider,
-        .sessionManager = Server::GetInstance().GetSecureSessionManager(), .dnssdServer = DnssdServer::Instance(),
+        .storageDelegate                = *initParams.persistentStorageDelegate,                 //
+            .commissioningWindowManager = Server::GetInstance().GetCommissioningWindowManager(), //
+            .configurationManager       = DeviceLayer::ConfigurationMgr(),                       //
+            .deviceControlServer        = DeviceLayer::DeviceControlServer::DeviceControlSvr(),  //
+            .fabricTable                = Server::GetInstance().GetFabricTable(),                //
+            .failsafeContext            = Server::GetInstance().GetFailSafeContext(),            //
+            .platformManager            = DeviceLayer::PlatformMgr(),                            //
+            .groupDataProvider          = gGroupDataProvider,                                    //
+            .sessionManager             = Server::GetInstance().GetSecureSessionManager(),       //
+            .dnssdServer                = DnssdServer::Instance(),                               //
 
 #if CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
-        .termsAndConditionsProvider = TermsAndConditionsManager::GetInstance(),
+            .termsAndConditionsProvider = TermsAndConditionsManager::GetInstance(),
 #endif // CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
     });
 
