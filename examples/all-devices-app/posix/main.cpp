9424ae07f463328f12cf67d0c95fe2790ed807ec
Andrei Litvin <andy314@gmail.com>
2026-01-27 15:23:21 -0500

    Ensure our tests can see the main starting.

--- examples/all-devices-app/posix/main.cpp
+++ examples/all-devices-app/posix/main.cpp
@@ -222,6 +222,13 @@ void RunApplication(AppMainLoopImplementation * mainLoop = nullptr)
 
     chip::app::SetTerminateHandler(StopSignalHandler);
 
+    // This message is used as a marker for when the application process has started.
+    // See: scripts/tests/chiptest/test_definition.py
+    // TODO: A cleaner and more generic mechanism needs to be developed as a follow-up.
+    // Currently other places (OTA, TV) also scrape logs for information and a better way should be
+    // possible.
+    ChipLogProgress(DeviceLayer, "===== APP STATUS: Starting event loop =====");
+
     if (mainLoop != nullptr)
     {
         mainLoop->RunMainLoop();
