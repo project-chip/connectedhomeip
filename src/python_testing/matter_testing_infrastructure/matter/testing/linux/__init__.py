b11ef166aebe426bf5e35980d96203c6632d005a
Restyled.io <commits@restyled.io>
2026-01-30 10:23:04 +0000

    Restyled by autopep8

--- src/python_testing/matter_testing_infrastructure/matter/testing/linux/__init__.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/linux/__init__.py
@@ -1,7 +1,7 @@
 from .bluetooth import BluetoothMock
 from .dbus import DBusTestSystemBus
 from .namespace import (IsolatedNetworkNamespace, LinuxNamespacedExecutor, ensure_network_namespace_availability,
-                             ensure_private_state)
+                        ensure_private_state)
 from .wifi import WpaSupplicantMock
 
 __all__ = [
