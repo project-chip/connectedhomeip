01dc171d78b8434f08161fec946e95c485926f1d
marybadalyan <marybadalyangdh@gmail.com>
2026-02-03 13:34:37 +0400

    dereference pointer

--- examples/all-devices-app/esp32/main/main.cpp
+++ examples/all-devices-app/esp32/main/main.cpp
@@ -236,7 +236,6 @@ chip::app::DataModel::Provider * PopulateCodeDrivenDataModelProvider(PersistentS
                 .diagnosticDataProvider     = DeviceLayer::GetDiagnosticDataProvider(),              //
                 .testEventTriggerDelegate   = testEventTriggerDelegate,                              //
                 .dacProvider            = Credentials::GetDeviceAttestationCredentialsProvider(), //
-                .accessControl          = Access::GetAccessControl(),              //
                 .eventManagement        = EventManagement::GetInstance(), //
                 .safeAttributePersistenceProvider = gSafeAttributePersistenceProvider,                     //
 #if CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
