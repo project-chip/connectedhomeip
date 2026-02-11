892fd37216c85d632764de034b72ed734e4610a5
Andrei Litvin <andy314@gmail.com>
2026-02-11 12:15:07 -0500

    Fix arguments.

--- src/python_testing/TC_DeviceBasicComposition.py
+++ src/python_testing/TC_DeviceBasicComposition.py
@@ -20,7 +20,7 @@
 #
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs:
-#   run1: # Runs through all tests
+#   run1: # Runs through all tests with debug mode enabled (dumps attribute data on failure)
 #     app: ${ALL_CLUSTERS_APP}
 #     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
 #     script-args: >
@@ -29,6 +29,7 @@
 #       --PICS src/app/tests/suites/certification/ci-pics-values
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#       --debug
 #     factory-reset: true
 #     quiet: true
 #   run2: # tests PASE connection using manual code (12.1 only)
@@ -158,19 +159,17 @@
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #     factory-reset: true
 #     quiet: true
-#   run16: # Tests against all-devices-app - default (contactsensor) with debug mode enabled (dumps attribute data on failure)
+#   run16: # Tests against all-devices-app - default (contactsensor)
 #     app: ${ALL_DEVICES_APP}
 #     app-args: >
 #       --discriminator 1234
 #       --KVS kvs1
-#       --trace-to json:${TRACE_APP}.json
 #     script-args: >
 #       --storage-path admin_storage.json
 #       --manual-code 10054912339
 #       --PICS src/app/tests/suites/certification/ci-pics-values
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
-#       --debug
 #     factory-reset: true
 #     quiet: true
 #   run17: # Tests against all-devices-app - on/off light
