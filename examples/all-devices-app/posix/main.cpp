d849977e5080690b819f979cb8c4ac801ccda6a7
Restyled.io <commits@restyled.io>
2026-02-11 18:52:33 +0000

    Restyled by clang-format

--- examples/all-devices-app/posix/main.cpp
+++ examples/all-devices-app/posix/main.cpp
@@ -214,8 +214,8 @@ void RunApplication(AppMainLoopImplementation * mainLoop = nullptr)
             .deviceLoadStatusProvider   = *InteractionModelEngine::GetInstance(),                //
             .diagnosticDataProvider     = DeviceLayer::GetDiagnosticDataProvider(),              //
             .testEventTriggerDelegate   = initParams.testEventTriggerDelegate,                   //
-            .dacProvider = *Credentials::Examples::GetExampleDACProvider(), //                                           //
-            .eventManagement = EventManagement::GetInstance(),                      //
+            .dacProvider     = *Credentials::Examples::GetExampleDACProvider(), //                                           //
+            .eventManagement = EventManagement::GetInstance(),                  //
 
 #if CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
             .termsAndConditionsProvider = TermsAndConditionsManager::GetInstance(),
