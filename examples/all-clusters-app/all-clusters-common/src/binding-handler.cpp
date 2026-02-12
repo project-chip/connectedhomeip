6ff5bdaad4eeded498c2ade957c0779762e58b71
AniDashyan <ani.dashyan77@gmail.com>
2026-02-12 12:17:19 +0000

    Fix namespace issue

--- examples/all-clusters-app/all-clusters-common/src/binding-handler.cpp
+++ examples/all-clusters-app/all-clusters-common/src/binding-handler.cpp
@@ -116,8 +116,8 @@ static void BoundDeviceContextReleaseHandler(void * context)
 static void InitBindingHandlerInternal(intptr_t arg)
 {
     auto & server = chip::Server::GetInstance();
-    TEMPORARY_RETURN_IGNORED Binding::Manager::GetInstance().Init({ &app::Clusters::Binding::Table::GetInstance(),
-                                                                    &server.GetFabricTable(), server.GetCASESessionManager(),
+    TEMPORARY_RETURN_IGNORED Binding::Manager::GetInstance().Init({ &Binding::Table::GetInstance(), &server.GetFabricTable(),
+                                                                    server.GetCASESessionManager(),
                                                                     &server.GetPersistentStorage() });
     Binding::Manager::GetInstance().RegisterBoundDeviceChangedHandler(BoundDeviceChangedHandler);
     Binding::Manager::GetInstance().RegisterBoundDeviceContextReleaseHandler(BoundDeviceContextReleaseHandler);
