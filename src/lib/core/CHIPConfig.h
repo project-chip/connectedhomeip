1c3d6640cbf6135238b150c16d980e6790971196
Junior Martinez <junior.Martinez@silabs.com>
2026-02-16 16:19:27 -0500

    Update all-cluster-app config to set CHIP_CONFIG_MAX_GROUPCAST_MEMBERSHIP_COUNT at 24 for backward compatibility of test TestGroupKeyManagementCluster that still usese Groups cluster. Add one additinal key creation in the test inline with groupdataprovider changes.

--- src/lib/core/CHIPConfig.h
+++ src/lib/core/CHIPConfig.h
@@ -1155,7 +1155,7 @@ extern const char CHIP_NON_PRODUCTION_MARKER[];
  * Binds to number of KeySet entries to support per fabric (Need at least 1 for Identity Protection Key)
  */
 #ifndef CHIP_CONFIG_MAX_GROUP_KEYS_PER_FABRIC
-#define CHIP_CONFIG_MAX_GROUP_KEYS_PER_FABRIC (CHIP_CONFIG_MAX_GROUPCAST_MEMBERSHIP_COUNT / 2)
+#define CHIP_CONFIG_MAX_GROUP_KEYS_PER_FABRIC (3 + 1) // support 3 KeySets + IPK per fabric
 #endif
 
 #if CHIP_CONFIG_MAX_GROUP_KEYS_PER_FABRIC < 1
