a19dbdd473e819a938ebf766846bbe200d744555
Arkadiusz Bokowy <a.bokowy@samsung.com>
2026-02-16 09:22:44 +0100

    Replace all usages of ChipLogFailure with ChipLogError
    The ChipLogFailure() suggests that there is another logging level.
    However, ChipLogFailure simply calls ChipLogError internally. Since the
    ChipLogFailure is used only in few places, replace it with ChipLogError
    to make the codebase consistent.

--- examples/closure-app/silabs/src/ClosureManager.cpp
+++ examples/closure-app/silabs/src/ClosureManager.cpp
@@ -123,7 +123,7 @@ void ClosureManager::Init()
     if (pTestEventDelegate != nullptr)
     {
         CHIP_ERROR err = pTestEventDelegate->AddHandler(&mClosureEndpoint1.GetDelegate());
-        ChipLogFailure(err, AppServer, "Failed to add handler for delegate");
+        ChipLogError(AppServer, "Failed to add handler for delegate: %" CHIP_ERROR_FORMAT, err.Format());
     }
     else
     {
