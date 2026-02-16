75548daa3845d5414fafc7337aeb6cdd806a7db0
jtrejoespinoza-grid <juanerasmo@google.com>
2026-02-16 12:23:51 -0700

    test: check if this solve the CI issue

--- src/python_testing/TC_CC_6_5.py
+++ src/python_testing/TC_CC_6_5.py
@@ -19,7 +19,7 @@
 # test-runner-runs:
 #   run1:
 #     app: ${LIGHTING_APP_NO_UNIQUE_ID}
-#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#     app-args: --discriminator 1234 --KVS /tmp/chip_kvs --trace-to json:${TRACE_APP}.json
 #     app-ready-pattern: "Server initialization complete"
 #     script-args: >
 #       --storage-path admin_storage.json
