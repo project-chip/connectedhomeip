e76b2d92f110c5f76cc9bba1b7fe1e0b351caa2c
Andrei Litvin <andy314@gmail.com>
2026-02-17 18:06:39 +0000

    Restyle

--- src/app/clusters/bridged-device-basic-information-server/BridgedDeviceBasicInformationCluster.cpp
+++ src/app/clusters/bridged-device-basic-information-server/BridgedDeviceBasicInformationCluster.cpp
@@ -37,7 +37,7 @@ constexpr size_t kNodeLabelMaxLength = 32;
 
 CharSpan ToSpan(const std::string & s)
 {
-    return {s.data(), s.size()};
+    return { s.data(), s.size() };
 }
 
 CharSpan ToSpan(const std::optional<std::string> & s)
