567844a5bfa10ccd6a914febda90809767b238bb
Restyled.io <commits@restyled.io>
2026-02-17 19:28:42 +0000

    Restyled by clang-format

--- examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.cpp
+++ examples/all-devices-app/all-devices-common/devices/root-node/RootNodeDevice.cpp
@@ -78,8 +78,7 @@ CHIP_ERROR RootNodeDevice::Register(EndpointId endpointId, CodeDrivenDataModelPr
                                                     .failSafeContext = mContext.failSafeContext });
     ReturnErrorOnFailure(provider.AddCluster(mAdministratorCommissioningCluster.Registration()));
 
-    mGeneralDiagnosticsCluster.Create(GeneralDiagnosticsCluster::OptionalAttributeSet{},
-                                      BitFlags<GeneralDiagnostics::Feature>{},
+    mGeneralDiagnosticsCluster.Create(GeneralDiagnosticsCluster::OptionalAttributeSet{}, BitFlags<GeneralDiagnostics::Feature>{},
                                       GeneralDiagnosticsCluster::Context{
                                           .deviceLoadStatusProvider = mContext.deviceLoadStatusProvider,
                                           .diagnosticDataProvider   = mContext.diagnosticDataProvider,
