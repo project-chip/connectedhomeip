653a1853cd30d2ec782c1e8143072c62398c8f79
Ben Humphries <iMacHumphries@me.com>
2026-02-23 15:03:13 -0500

    [Matter Casting][iOS] Add build_xcframework.sh

---
+++ examples/tv-casting-app/darwin/MatterTvCastingBridge/build_xcframework.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+xcodebuild archive \
+    -project MatterTvCastingBridge.xcodeproj \
+    -scheme MatterTvCastingBridge \
+    -configuration Release \
+    -destination "generic/platform=iOS" \
+    -archivePath "archives/MatterTvCastingBridge-iOS" \
+    SKIP_INSTALL=0
+
+xcodebuild archive \
+    -project MatterTvCastingBridge.xcodeproj \
+    -scheme MatterTvCastingBridge \
+    -configuration Release \
+    -destination "generic/platform=iOS Simulator" \
+    -archivePath "archives/MatterTvCastingBridge-iOS_Simulator" \
+    SKIP_INSTALL=0
+
+xcodebuild -create-xcframework \
+    -archive archives/MatterTvCastingBridge-iOS.xcarchive -framework MatterTvCastingBridge.framework \
+    -archive archives/MatterTvCastingBridge-iOS_Simulator.xcarchive -framework MatterTvCastingBridge.framework \
+    -output xcframeworks/MatterTvCastingBridge.xcframework
