2ba36cbb312a019fee94ea223f3e71523ea39a25
arielsz71 <asztejnberg@griddynamics.com>
2026-02-24 16:58:07 -0300

    Address review comment

--- src/app/clusters/temperature-measurement-server/app_config_dependent_sources.cmake
+++ src/app/clusters/temperature-measurement-server/app_config_dependent_sources.cmake
@@ -18,11 +18,3 @@ TARGET_SOURCES(
    "${CLUSTER_DIR}/CodegenIntegration.cpp"
    "${CLUSTER_DIR}/CodegenIntegration.h"
 )
-
-# These are the things that BUILD.gn dependencies would pull
-TARGET_SOURCES(
-  ${APP_TARGET}
-  PRIVATE
-    "${CLUSTER_DIR}/TemperatureMeasurementCluster.cpp"
-    "${CLUSTER_DIR}/TemperatureMeasurementCluster.h"
-)
