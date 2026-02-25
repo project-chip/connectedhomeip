4790237b9012f48865df7acdced960f8f2ac9877
Andrei Litvin <andy314@gmail.com>
2026-02-25 14:36:09 -0500

    Fix include and a stray include by chance

--- src/app/clusters/bridged-device-basic-information-server/tests/TestBridgedDeviceBasicInformationCluster.cpp
+++ src/app/clusters/bridged-device-basic-information-server/tests/TestBridgedDeviceBasicInformationCluster.cpp
@@ -13,6 +13,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+#include <pw_unit_test/framework.h>
+
 #include <app/clusters/basic-information/BasicInformationCluster.h>
 #include <app/clusters/bridged-device-basic-information-server/BasicInformationClusterProxy.h>
 #include <app/clusters/bridged-device-basic-information-server/BridgedDeviceBasicInformationCluster.h>
