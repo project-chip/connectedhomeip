72c3626cc860978f5780574d8d4cabffb1bb2fd3
Restyled.io <commits@restyled.io>
2026-02-26 08:52:58 +0000

    Restyled by autopep8

--- src/python_testing/provisional/TC_SC_TC_4_1.py
+++ src/python_testing/provisional/TC_SC_TC_4_1.py
@@ -61,10 +61,10 @@ class TC_SC_TC_4_1(MatterBaseTest):
         self.step(2)
         commissioner: ChipDeviceCtrl.ChipDeviceController = self.default_controller
         commissioner.SetThreadOperationalDataset(self.matter_test_config.thread_operational_dataset)
-        await commissioner.EstablishPASESessionThreadMeshcop(baAddr = self.matter_test_config.thread_ba_host,
-                                                       setupCode = self.matter_test_config.qr_code_content[0],
-                                                       nodeId = self.matter_test_config.dut_node_ids[0],
-                                                       baPort = self.matter_test_config.thread_ba_port)
+        await commissioner.EstablishPASESessionThreadMeshcop(baAddr=self.matter_test_config.thread_ba_host,
+                                                             setupCode=self.matter_test_config.qr_code_content[0],
+                                                             nodeId=self.matter_test_config.dut_node_ids[0],
+                                                             baPort=self.matter_test_config.thread_ba_port)
 
 
 if __name__ == "__main__":
