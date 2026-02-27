2801d4d0fbc50e86e5eb08a9049ab3909231f9b2
Restyled.io <commits@restyled.io>
2026-02-27 11:24:29 +0000

    Restyled by isort

--- scripts/tests/chiptest/linux.py
+++ scripts/tests/chiptest/linux.py
@@ -32,8 +32,8 @@ root_dir = os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 with PythonPath(os.path.join(root_dir, 'src/python_testing/matter_testing_infrastructure'), relative_to=__file__):
-    from matter.testing.linux import (BluetoothMock, DBusTestSystemBus, IsolatedNetworkNamespace, ThreadBorderRouter, WpaSupplicantMock,
-                                      ensure_network_namespace_availability, ensure_private_state)
+    from matter.testing.linux import (BluetoothMock, DBusTestSystemBus, IsolatedNetworkNamespace, ThreadBorderRouter,
+                                      WpaSupplicantMock, ensure_network_namespace_availability, ensure_private_state)
 
 __all__ = [
     "ensure_network_namespace_availability",
