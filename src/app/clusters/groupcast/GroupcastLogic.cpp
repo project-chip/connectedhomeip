9ec724292fa678c6899efe04e8b6aa039a80ae47
Restyled.io <commits@restyled.io>
2026-02-27 21:38:01 +0000

    Restyled by clang-format

--- src/app/clusters/groupcast/GroupcastLogic.cpp
+++ src/app/clusters/groupcast/GroupcastLogic.cpp
@@ -148,7 +148,6 @@ Status GroupcastLogic::JoinGroup(FabricIndex fabric_index, const Groupcast::Comm
     // ReplaceEndpoints can only be present if kListener feature is supported
     VerifyOrReturnError(!data.replaceEndpoints.HasValue() || mFeatures.Has(Groupcast::Feature::kListener), Status::ConstraintError);
 
-
     // Check endpoints
     size_t endpoint_count = 0;
     err                   = data.endpoints.ComputeSize(&endpoint_count);
