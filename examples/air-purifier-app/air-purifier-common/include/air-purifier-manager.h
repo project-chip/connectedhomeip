754242097bb46ffc2b19965d09672b51236caadb
LyudmilaKostanyan <lyudakstnn@gmail.com>
2026-03-03 20:40:16 +0400

    Refactor AirPurifierManager to use emberAfWriteAttribute for attribute updates

--- examples/air-purifier-app/air-purifier-common/include/air-purifier-manager.h
+++ examples/air-purifier-app/air-purifier-common/include/air-purifier-manager.h
@@ -164,7 +164,7 @@ private:
     void SetSpeedSetting(DataModel::Nullable<uint8_t> aNewSpeedSetting);
     DataModel::Nullable<uint8_t> GetSpeedSetting();
     DataModel::Nullable<Percent> GetPercentSetting();
-    uint8_t GetSpeedMax();
+    std::optional<uint8_t> GetSpeedMax() override;
 
     void HandleThermostatAttributeChange(AttributeId attributeId, uint8_t type, uint16_t size, uint8_t * value);
     void ThermostatHeatingSetpointWriteCallback(int16_t aNewHeatingSetpoint);
