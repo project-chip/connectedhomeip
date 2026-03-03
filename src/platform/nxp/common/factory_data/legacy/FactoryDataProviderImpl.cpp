6e30cf317dde7a99c68f162d8cba9db0f76f977e
Martin Girardot <martin.girardot@nxp.com>
2026-03-03 18:41:51 +0100

    [NXP] Add PSA spport

--- src/platform/nxp/common/factory_data/legacy/FactoryDataProviderImpl.cpp
+++ src/platform/nxp/common/factory_data/legacy/FactoryDataProviderImpl.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *    Copyright (c) 2023 Project CHIP Authors
+ *    Copyright (c) 2023-2026 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,6 +18,8 @@
 #include <platform/nxp/common/factory_data/legacy/FactoryDataProviderImpl.h>
 
 #include "fsl_adapter_flash.h"
+#include <psa/crypto.h>
+#include "mcux_psa_s2xx_key_locations.h"
 
 #if CONFIG_CHIP_OTA_FACTORY_DATA_PROCESSOR
 extern "C" WEAK CHIP_ERROR FactoryDataDefaultRestoreMechanism();
@@ -26,10 +28,11 @@ extern "C" WEAK CHIP_ERROR FactoryDataDefaultRestoreMechanism();
 namespace chip {
 namespace DeviceLayer {
 
-#if !CHIP_USE_PLAIN_DAC_KEY
-// SSS adds 24 bytes of metadata when creating the blob
-static constexpr size_t kSssBlobMetadataLength = 24;
-static constexpr size_t kPrivateKeyBlobLength  = Crypto::kP256_PrivateKey_Length + kSssBlobMetadataLength;
+#if CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+static constexpr size_t kPrivateKeyBlobLength  = PSA_S200_NON_EL2GO_BLOB_EXPORT_SIZE(PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1), 256);
+
+static_assert (kPrivateKeyBlobLength >= Crypto::kP256_PrivateKey_Length);
+static constexpr size_t kSEBlobAdditionalLength = kPrivateKeyBlobLength - Crypto::kP256_PrivateKey_Length;
 #endif
 
 uint32_t FactoryDataProvider::kFactoryDataMaxSize = 0x800;
@@ -43,8 +46,12 @@ FactoryDataProviderImpl::FactoryDataProviderImpl()
 
 FactoryDataProviderImpl::~FactoryDataProviderImpl()
 {
-#if !CHIP_USE_PLAIN_DAC_KEY
-    SSS_KEY_OBJ_FREE(&mContext);
+#if CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+    if (dac_private_key_id != PSA_KEY_ID_NULL)
+    {
+        psa_destroy_key(dac_private_key_id);
+        dac_private_key_id = PSA_KEY_ID_NULL;
+    }
 #endif
 }
 
@@ -64,8 +71,8 @@ CHIP_ERROR FactoryDataProviderImpl::Init()
     ReturnErrorOnFailure(PostResetCheck());
 #endif
 
-#if CHIP_DEVICE_CONFIG_ENABLE_SSS_API_TEST && !CHIP_USE_PLAIN_DAC_KEY
-    SSS_RunApiTest();
+#if CHIP_DEVICE_CONFIG_ENABLE_API_TEST && CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+    RunApiTest();
 #endif
 
 #if CONFIG_CHIP_OTA_FACTORY_DATA_PROCESSOR
@@ -78,154 +85,156 @@ CHIP_ERROR FactoryDataProviderImpl::Init()
         ChipLogError(DeviceLayer, "Factory data init failed with: %" CHIP_ERROR_FORMAT, error.Format());
     }
 
-#if !CHIP_USE_PLAIN_DAC_KEY
-    ReturnErrorOnFailure(SSS_InitContext());
-    ReturnErrorOnFailure(SSS_ConvertDacKey());
-    ReturnErrorOnFailure(SSS_ImportPrivateKeyBlob());
+#ifdef CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+    ReturnErrorOnFailure(InitContext());
+    ReturnErrorOnFailure(ConvertDacKey());
+    ReturnErrorOnFailure(ImportPrivateKeyBlob());
 #endif
 
     return error;
 }
 
-#if CHIP_USE_PLAIN_DAC_KEY
-CHIP_ERROR FactoryDataProviderImpl::SignWithDacKey(const ByteSpan & messageToSign, MutableByteSpan & outSignBuffer)
+void FactoryDataProviderImpl::UpdateKeyAttributes(psa_key_attributes_t & attrs)
 {
-    return FactoryDataProvider::SignWithDacKey(messageToSign, outSignBuffer);
+    if (psa_get_key_lifetime(&attrs) == PSA_KEY_LIFETIME_VOLATILE)
+    {
+        psa_set_key_lifetime(&attrs, PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
+                                PSA_KEY_LIFETIME_VOLATILE, PSA_KEY_LOCATION_S200_KEY_STORAGE_NON_EL2GO));
+    }
 }
 
-#else
-CHIP_ERROR FactoryDataProviderImpl::SignWithDacKey(const ByteSpan & messageToSign, MutableByteSpan & outSignBuffer)
+#if CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+CHIP_ERROR FactoryDataProviderImpl::InitContext()
 {
-    Crypto::P256ECDSASignature signature;
-
-    VerifyOrReturnError(IsSpanUsable(outSignBuffer), CHIP_ERROR_INVALID_ARGUMENT);
-    VerifyOrReturnError(IsSpanUsable(messageToSign), CHIP_ERROR_INVALID_ARGUMENT);
-    VerifyOrReturnError(outSignBuffer.size() >= signature.Capacity(), CHIP_ERROR_BUFFER_TOO_SMALL);
-    VerifyOrReturnError(messageToSign.data() != nullptr, CHIP_ERROR_INVALID_ARGUMENT);
-    VerifyOrReturnError(messageToSign.size() > 0, CHIP_ERROR_INVALID_ARGUMENT);
-
-    uint8_t digest[Crypto::kSHA256_Hash_Length];
-    memset(&digest[0], 0, sizeof(digest));
-
-    ReturnErrorOnFailure(Crypto::Hash_SHA256(messageToSign.data(), messageToSign.size(), digest));
-    ReturnErrorOnFailure(SSS_Sign(digest, signature));
-
-    return CopySpanToMutableSpan(ByteSpan{ signature.ConstBytes(), signature.Length() }, outSignBuffer);
-}
-
-CHIP_ERROR FactoryDataProviderImpl::SSS_InitContext()
-{
-    auto res = sss_sscp_key_object_init(&mContext, &g_keyStore);
-    VerifyOrReturnError(res == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
-    res = sss_sscp_key_object_allocate_handle(&mContext, 0x0u, kSSS_KeyPart_Private, kSSS_CipherType_EC_NIST_P,
-                                              Crypto::kP256_PrivateKey_Length, SSS_KEYPROP_OPERATION_ASYM);
-    VerifyOrReturnError(res == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
+    dac_private_key_id = PSA_KEY_ID_NULL;
 
     return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR FactoryDataProviderImpl::SSS_ImportPrivateKeyBlob()
+CHIP_ERROR FactoryDataProviderImpl::ImportPrivateKeyBlob()
 {
+    psa_status_t status;
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
     uint8_t blob[kPrivateKeyBlobLength] = { 0 };
     uint16_t blobSize                   = 0;
+
     ReturnErrorOnFailure(SearchForId(FactoryDataId::kDacPrivateKeyId, blob, kPrivateKeyBlobLength, blobSize));
 
-    auto res = sss_sscp_key_store_import_key(&g_keyStore, &mContext, blob, kPrivateKeyBlobLength, kPrivateKeyBlobLength * 8,
-                                             kSSS_blobType_ELKE_blob);
-    VerifyOrReturnError(res == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
+    // Define key usage and type
+    psa_key_usage_t usage = PSA_KEY_USAGE_SIGN_HASH | PSA_KEY_USAGE_SIGN_MESSAGE;
+    psa_key_type_t key_type = PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1);
+
+    // Set key attributes for P256 private key
+    psa_set_key_type(&attributes, key_type);
+    psa_set_key_bits(&attributes, 256);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+    psa_set_key_usage_flags(&attributes, usage);
+    psa_set_key_lifetime(&attributes, PSA_KEY_LIFETIME_VOLATILE);
+    UpdateKeyAttributes(attributes);
+
+    // Import the key blob
+    status = psa_import_key(&attributes, blob, blobSize, &dac_private_key_id);
+
+    psa_reset_key_attributes(&attributes);
+
+    VerifyOrReturnError(status == PSA_SUCCESS, CHIP_ERROR_INTERNAL);
 
     return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR FactoryDataProviderImpl::SSS_Sign(uint8_t * digest, Crypto::P256ECDSASignature & signature)
-{
-    CHIP_ERROR error     = CHIP_NO_ERROR;
-    size_t signatureSize = Crypto::kP256_ECDSA_Signature_Length_Raw;
-    sss_status_t res     = kStatus_SSS_Fail;
-    sss_sscp_asymmetric_t asyc;
-
-    res = sss_sscp_asymmetric_context_init(&asyc, &g_sssSession, &mContext, kAlgorithm_SSS_ECDSA_SHA256, kMode_SSS_Sign);
-    VerifyOrExit(res == kStatus_SSS_Success, error = CHIP_ERROR_INTERNAL);
-    res = sss_sscp_asymmetric_sign_digest(&asyc, digest, Crypto::kP256_PrivateKey_Length, signature.Bytes(), &signatureSize);
-    VerifyOrExit(res == kStatus_SSS_Success, error = CHIP_ERROR_INTERNAL);
-    error = signature.SetLength(signatureSize);
-
-exit:
-    sss_sscp_asymmetric_context_free(&asyc);
-    return error;
-}
-
-CHIP_ERROR FactoryDataProviderImpl::SSS_ConvertDacKey()
+CHIP_ERROR FactoryDataProviderImpl::ConvertDacKey()
 {
     size_t blobSize                     = kPrivateKeyBlobLength;
-    size_t newSize                      = sizeof(FactoryDataProvider::Header) + mHeader.size + kSssBlobMetadataLength;
+    size_t newSize                      = sizeof(FactoryDataProvider::Header) + mHeader.size + kSEBlobAdditionalLength;
     uint8_t blob[kPrivateKeyBlobLength] = { 0 };
-    extendedAppFactoryData_t * data     = static_cast<extendedAppFactoryData_t *>(
-        chip::Platform::MemoryAlloc(offsetof(extendedAppFactoryData_t, app_factory_data) + newSize));
+    extendedAppFactoryData_t * data     = nullptr;
     uint32_t offset = 0;
     bool convNeeded = true;
     uint8_t status  = 0;
 
-    VerifyOrReturnError(data != nullptr, CHIP_ERROR_INTERNAL);
-    VerifyOrReturnError(mFactoryData != nullptr, CHIP_ERROR_INTERNAL);
+     VerifyOrReturnError(mFactoryData != nullptr, CHIP_ERROR_INTERNAL);
 
-    ReturnErrorOnFailure(SSS_ExportBlob(blob, &blobSize, offset, convNeeded));
+     data     = static_cast<extendedAppFactoryData_t *>(
+        chip::Platform::MemoryAlloc(offsetof(extendedAppFactoryData_t, app_factory_data) + newSize));
+    VerifyOrReturnError(data != nullptr, CHIP_ERROR_INTERNAL);
+
+    ReturnErrorOnFailure(ExportBlob(blob, &blobSize, offset, convNeeded));
     if (!convNeeded)
     {
-        ChipLogError(DeviceLayer, "SSS: DAC private key already converted to blob");
+        ChipLogError(DeviceLayer, "DAC private key already converted to blob");
         chip::Platform::MemoryFree(data);
         return CHIP_NO_ERROR;
     }
-    ChipLogError(DeviceLayer, "SSS: extracted blob from DAC private key");
+    ChipLogError(DeviceLayer, "extracted blob from DAC private key");
 
     memcpy(data, mFactoryData, offsetof(extendedAppFactoryData_t, app_factory_data) + mFactoryData->extendedDataLength);
-    ChipLogError(DeviceLayer, "SSS: cached factory data in RAM");
+    ChipLogError(DeviceLayer, "cached factory data in RAM");
 
     ReturnErrorOnFailure(ReplaceWithBlob(&data->app_factory_data[0], blob, blobSize, offset));
-    ChipLogError(DeviceLayer, "SSS: replaced DAC private key with secured blob");
+    ChipLogError(DeviceLayer, "replaced DAC private key with secured blob");
 
     data->extendedDataLength = newSize;
     status                   = Nv_WriteAppFactoryData(data, newSize);
     VerifyOrReturnError(status == 0, CHIP_ERROR_INTERNAL);
-    ChipLogError(DeviceLayer, "SSS: updated factory data");
+    ChipLogError(DeviceLayer, "updated factory data");
 
     memset(data, 0, newSize);
     chip::Platform::MemoryFree(data);
-    ChipLogError(DeviceLayer, "SSS: sanitized RAM cache");
+    ChipLogError(DeviceLayer, "sanitized RAM cache");
 
     ReturnErrorOnFailure(Validate());
 
     return CHIP_NO_ERROR;
 }
 
-CHIP_ERROR FactoryDataProviderImpl::SSS_ExportBlob(uint8_t * data, size_t * dataLen, uint32_t & offset, bool & isNeeded)
+CHIP_ERROR FactoryDataProviderImpl::ExportBlob(uint8_t * data, size_t * dataLen, uint32_t & offset, bool & isNeeded)
 {
     CHIP_ERROR error = CHIP_NO_ERROR;
-    auto res         = kStatus_SSS_Success;
+    psa_status_t status = PSA_SUCCESS;
 
     uint8_t keyBuf[kPrivateKeyBlobLength];
     MutableByteSpan dacPrivateKeySpan(keyBuf);
     uint16_t keySize = 0;
     isNeeded         = true;
 
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_key_id_t temp_key_id = PSA_KEY_ID_NULL;
+
+    // Search for the DAC private key in factory data
     error = SearchForId(FactoryDataId::kDacPrivateKeyId, dacPrivateKeySpan.data(), dacPrivateKeySpan.size(), keySize, &offset);
     SuccessOrExit(error);
     dacPrivateKeySpan.reduce_size(keySize);
 
+    // Check if already a blob (blob length = plain key length + metadata)
     if (keySize == kPrivateKeyBlobLength)
     {
         isNeeded = false;
         return CHIP_NO_ERROR;
     }
 
-    res = SSS_KEY_STORE_SET_KEY(&mContext, dacPrivateKeySpan.data(), Crypto::kP256_PrivateKey_Length, keySize * 8,
-                                kSSS_KeyPart_Private);
-    VerifyOrExit(res == kStatus_SSS_Success, error = CHIP_ERROR_INTERNAL);
+    psa_set_key_type(&attributes, PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1));
+    psa_set_key_bits(&attributes, 256);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+    psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_EXPORT);
+    psa_set_key_lifetime(&attributes, PSA_KEY_LIFETIME_VOLATILE);
+    UpdateKeyAttributes(attributes);
 
-    res = sss_sscp_key_store_export_key(&g_keyStore, &mContext, data, dataLen, kSSS_blobType_ELKE_blob);
-    VerifyOrExit(res == kStatus_SSS_Success, error = CHIP_ERROR_INTERNAL);
+    // Import the plain key
+    status = psa_import_key(&attributes, dacPrivateKeySpan.data(), Crypto::kP256_PrivateKey_Length, &temp_key_id);
+    psa_reset_key_attributes(&attributes);
+    VerifyOrExit(status == PSA_SUCCESS, error = CHIP_ERROR_INTERNAL);
+
+    // Export as blob (platform-specific secure blob format)
+    status = psa_export_key(temp_key_id, data, kPrivateKeyBlobLength, dataLen);
+    VerifyOrExit(status == PSA_SUCCESS, error = CHIP_ERROR_INTERNAL);
 
 exit:
+    // Clean up temporary key if it was created
+    if (temp_key_id != PSA_KEY_ID_NULL)
+    {
+        psa_destroy_key(temp_key_id);
+    }
+
     /* Sanitize temporary buffer */
     memset(keyBuf, 0, Crypto::kP256_PrivateKey_Length);
     return error;
@@ -233,12 +242,12 @@ exit:
 
 CHIP_ERROR FactoryDataProviderImpl::ReplaceWithBlob(uint8_t * data, uint8_t * blob, size_t blobLen, uint32_t offset)
 {
-    size_t newSize                       = mHeader.size + kSssBlobMetadataLength;
+    size_t newSize                       = mHeader.size + kSEBlobAdditionalLength;
     FactoryDataProvider::Header * header = reinterpret_cast<FactoryDataProvider::Header *>(data);
     uint8_t * payload                    = data + sizeof(FactoryDataProvider::Header);
     size_t subsequentDataOffset          = offset + kValueOffset + Crypto::kP256_PrivateKey_Length;
 
-    memmove(payload + subsequentDataOffset + kSssBlobMetadataLength, payload + subsequentDataOffset,
+    memmove(payload + subsequentDataOffset + kSEBlobAdditionalLength, payload + subsequentDataOffset,
             mHeader.size - subsequentDataOffset);
     header->size = newSize;
     memcpy(payload + offset + kLengthOffset, (uint16_t *) &blobLen, sizeof(uint16_t));
@@ -251,23 +260,23 @@ CHIP_ERROR FactoryDataProviderImpl::ReplaceWithBlob(uint8_t * data, uint8_t * bl
     return CHIP_NO_ERROR;
 }
 
-#if CHIP_DEVICE_CONFIG_ENABLE_SSS_API_TEST
+#if CHIP_DEVICE_CONFIG_ENABLE_API_TEST
 
 #define _assert(condition)                                                                                                         \
     if (!condition)                                                                                                                \
     {                                                                                                                              \
-        ChipLogError(DeviceLayer, "Condition failed" #condition);                                                                  \
+        ChipLogError(DeviceLayer, "Condition failed: " #condition);                                                                \
         while (1)                                                                                                                  \
             ;                                                                                                                      \
     }
 
-void FactoryDataProviderImpl::SSS_RunApiTest()
+void FactoryDataProviderImpl::RunApiTest()
 {
     uint8_t privateKey[Crypto::kP256_PrivateKey_Length] = { 0x18, 0xfe, 0x9a, 0xd9, 0x30, 0xdd, 0x2f, 0x62, 0xbe, 0x99, 0x43,
                                                             0x93, 0xe8, 0xbe, 0x47, 0x28, 0x7f, 0xda, 0x5a, 0x71, 0x86, 0x1b,
                                                             0x0e, 0x3f, 0x91, 0x27, 0x52, 0xd0, 0xba, 0xa7, 0x40, 0x02 };
 
-    auto error = SSS_InitContext();
+    auto error = InitContext();
     _assert((error == CHIP_NO_ERROR));
 
     // Simulate factory data in RAM: create the header + dummy data + DAC private key entry + dummy data
@@ -317,7 +326,7 @@ void FactoryDataProviderImpl::SSS_RunApiTest()
         entry += dummyLength;
     }
 
-    FactoryDataProvider::kFactoryDataPayloadStart = (uint32_t) factoryData + sizeof(FactoryDataProvider::Header);
+    mConfig.payload = (uint32_t) factoryData + sizeof(FactoryDataProvider::Header);
 
     uint8_t keyBuf[Crypto::kP256_PrivateKey_Length];
     MutableByteSpan dacPrivateKeySpan(keyBuf);
@@ -329,28 +338,72 @@ void FactoryDataProviderImpl::SSS_RunApiTest()
     _assert((memcmp(dacPrivateKeySpan.data(), privateKey, Crypto::kP256_PrivateKey_Length) == 0));
 
     size_t blobSize                     = kPrivateKeyBlobLength;
-    size_t newSize                      = sizeof(FactoryDataProvider::Header) + mHeader.size + kSssBlobMetadataLength;
+    size_t newSize                      = sizeof(FactoryDataProvider::Header) + mHeader.size + kSEBlobAdditionalLength;
     uint8_t blob[kPrivateKeyBlobLength] = { 0 };
 
     uint32_t offset = 0;
-    error           = SSS_ExportBlob(blob, &blobSize, offset);
+    bool convNeeded = true;
+    error           = ExportBlob(blob, &blobSize, offset, convNeeded);
     _assert((error == CHIP_NO_ERROR));
+    _assert((convNeeded == true));
+    _assert((blobSize == kPrivateKeyBlobLength));
+
     error = ReplaceWithBlob(factoryData, blob, blobSize, offset);
     _assert((error == CHIP_NO_ERROR));
+
     FactoryDataProvider::Header * header = reinterpret_cast<FactoryDataProvider::Header *>(factoryData);
-    _assert((header->size == (mHeader.size + kSssBlobMetadataLength)));
+    _assert((header->size == (mHeader.size + kSEBlobAdditionalLength)));
     _assert((header->hash[0] != 0xde));
     _assert((header->hash[1] != 0xad));
     _assert((header->hash[2] != 0xbe));
     _assert((header->hash[3] != 0xef));
 
+    // Test importing the blob
+    psa_status_t status;
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_key_id_t test_key_id = PSA_KEY_ID_NULL;
+
+    psa_key_usage_t usage = PSA_KEY_USAGE_SIGN_MESSAGE | PSA_KEY_USAGE_SIGN_HASH;
+    psa_key_type_t key_type = PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1);
+
+    psa_set_key_type(&attributes, key_type);
+    psa_set_key_bits(&attributes, 256);
+    psa_set_key_algorithm(&attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+    psa_set_key_usage_flags(&attributes, usage);
+    psa_set_key_lifetime(&attributes, PSA_KEY_LIFETIME_VOLATILE);
+    UpdateKeyAttributes(attributes);
+
+    status = psa_import_key(&attributes, blob, blobSize, &test_key_id);
+    psa_reset_key_attributes(&attributes);
+    _assert((status == PSA_SUCCESS));
+    _assert((test_key_id != PSA_KEY_ID_NULL));
+
+    // Test signing with the imported key
+    uint8_t test_hash[Crypto::kSHA256_Hash_Length] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                                                        0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+                                                        0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+                                                        0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20 };
+    uint8_t signature[PSA_SIGNATURE_MAX_SIZE] = { 0 };
+    size_t signature_length = 0;
+
+    status = psa_sign_hash(test_key_id, PSA_ALG_ECDSA(PSA_ALG_SHA_256),
+                          test_hash, sizeof(test_hash),
+                          signature, sizeof(signature), &signature_length);
+    _assert((status == PSA_SUCCESS));
+    _assert((signature_length > 0));
+
+    // Clean up test key
+    psa_destroy_key(test_key_id);
+
+    // Clean up
     memset(factoryData, 0, size);
     chip::Platform::MemoryFree(factoryData);
-    FactoryDataProvider::kFactoryDataPayloadStart = FactoryDataProvider::kFactoryDataStart + sizeof(FactoryDataProvider::Header);
-    SSS_KEY_OBJ_FREE(&mContext);
+    mConfig.payload = mConfig.start + sizeof(FactoryDataProvider::Header);
+
+    ChipLogProgress(DeviceLayer, "RunApiTest: All tests passed!");
 }
-#endif // CHIP_DEVICE_CONFIG_ENABLE_SSS_API_TEST
-#endif // CHIP_USE_PLAIN_DAC_KEY
+#endif // CHIP_DEVICE_CONFIG_ENABLE_API_TEST
+#endif // CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
 
 #ifndef CONFIG_CHIP_FACTORY_DATA_PROVIDER_CUSTOM_SINGLETON_IMPL
 FactoryDataProvider & FactoryDataPrvdImpl()
