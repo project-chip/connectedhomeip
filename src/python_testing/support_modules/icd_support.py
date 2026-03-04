d76a8ddff6e119c1d11a9652e7c6ca38c0199a5a
Raul Marquez <rmarquez@csa-iot.org>
2026-03-03 20:06:47 -0800

    updates icd support

--- src/python_testing/support_modules/icd_support.py
+++ src/python_testing/support_modules/icd_support.py
@@ -19,6 +19,7 @@ Support module for ICD test modules containing shared functionality.
 """
 
 import logging
+import re
 from enum import IntEnum
 
 import matter.clusters as Clusters
@@ -41,3 +42,46 @@ class ICDTestEventTriggerOperations(IntEnum):
     kForceMaximumCheckInBackOffState = _ICD_CLUSTER_CODE | 0x0000_00000005
     kDSLSForceSitMode = _ICD_CLUSTER_CODE | 0x0000_00000006
     kDSLSWithdrawSitMode = _ICD_CLUSTER_CODE | 0x0000_00000007
+
+
+# *** UserActiveModeTriggerBitmap helpers ***
+_cluster = Clusters.Objects.IcdManagement
+uat = _cluster.Bitmaps.UserActiveModeTriggerBitmap
+
+# BitMask for hints that are dependent on UserActiveModeTriggerInstruction
+kUatInstructionDependentBitMask = (
+    uat.kCustomInstruction | uat.kActuateSensorSeconds | uat.kActuateSensorTimes |
+    uat.kActuateSensorLightsBlink | uat.kResetButtonLightsBlink | uat.kResetButtonSeconds |
+    uat.kResetButtonTimes | uat.kSetupButtonSeconds | uat.kSetupButtonLightsBlink |
+    uat.kSetupButtonTimes | uat.kAppDefinedButton)
+
+# BitMask for hints that REQUIRE the presence of UserActiveModeTriggerInstruction
+kUatInstructionMandatoryBitMask = (
+    uat.kCustomInstruction | uat.kActuateSensorSeconds | uat.kActuateSensorTimes |
+    uat.kResetButtonSeconds | uat.kResetButtonTimes | uat.kSetupButtonSeconds |
+    uat.kSetupButtonTimes | uat.kAppDefinedButton)
+
+# BitMask for hints where UserActiveModeTriggerInstruction is a uint (number)
+kUatNumberInstructionBitMask = (
+    uat.kActuateSensorSeconds | uat.kActuateSensorTimes |
+    uat.kResetButtonSeconds | uat.kResetButtonTimes |
+    uat.kSetupButtonSeconds | uat.kSetupButtonTimes)
+
+# BitMask for hints where UserActiveModeTriggerInstruction is a color
+kUatColorInstructionBitMask = (
+    uat.kActuateSensorLightsBlink | uat.kResetButtonLightsBlink |
+    uat.kSetupButtonLightsBlink)
+
+
+def uat_bit_name(bit):
+    """Derive a readable name from the SDK enum, e.g. 'kResetButtonLightsBlink' → 'Reset Button Lights Blink'."""
+    return re.sub(r'([a-z])([A-Z])', r'\1 \2', bit.name.removeprefix('k'))
+
+
+def uat_set_hints(hint_bitmap):
+    """Return a list of individual UAT bits set in the given bitmap, logging each one."""
+    hints = [bit for bit in uat if hint_bitmap & bit.value]
+    log.info(f"UserActiveModeTriggerHint has {len(hints)} bit(s) set:")
+    for bit in hints:
+        log.info(f"  - {uat_bit_name(bit)} (0x{bit.value:05X})")
+    return hints
