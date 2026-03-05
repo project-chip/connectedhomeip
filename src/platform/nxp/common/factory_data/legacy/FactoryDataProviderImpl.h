9f065058958dab00d59d3c592bd452231979ef30
Doru-Cristian Gucea <doru-cristian.gucea@nxp.com>
2026-03-05 11:05:35 +0000

    [nxp toup] Fix build issue when CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION is enabled
    Signed-off-by: Doru-Cristian Gucea <doru-cristian.gucea@nxp.com>
    (cherry picked from commit e02a9ad504f60f4b4cf2b5b2033d47c872582ebc)

--- src/platform/nxp/common/factory_data/legacy/FactoryDataProviderImpl.h
+++ src/platform/nxp/common/factory_data/legacy/FactoryDataProviderImpl.h
@@ -20,6 +20,9 @@
 #include <lib/core/CHIPPersistentStorageDelegate.h>
 #include <lib/support/DefaultStorageKeyAllocator.h>
 #include <platform/nxp/common/factory_data/legacy/FactoryDataProvider.h>
+#if CONFIG_NXP_FACTORY_DAC_BLOB_GENERATION
+#include <psa/crypto.h>
+#endif
 
 #if !CHIP_USE_PLAIN_DAC_KEY
 #include "sss_crypto.h"
