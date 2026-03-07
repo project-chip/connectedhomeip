3e45e2be9bf7aef78505f8c7ca435f6cbbc1b49a
Shreyas Balakrishna Bhandare <shreyasbalakrishna19@gmail.com>
2026-03-07 16:10:42 +0000

    [Chef] Enable more cluster support for Mounted On/Off and Water valve devices (#43512)
    * Enable level control cluster for mounted On/Off control device
    
    * Add back mounted on off and water valve to build pipeline
    
    * Fix zap initialisations
    
    * Pw access for ValveConfigurationAndControl::Attributes::CurrentLevel
    
    * Compile fix
    
    * Compile fix
    
    * Add watervalve tests
    
    * Remove check for default open duration
    
    * debug log
    
    * Debug logs
    
    * Debug logs
    
    * Debug logs
    
    * Do not check autoclose
    
    * Remove time sync feature from valve configuration and control
    
    * fix test
    
    * Remove debug logs
    
    * Remove stray spaces
    
    * Restyled by ruff
    
    * Restyled by autopep8
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- examples/chef/chef.py
+++ examples/chef/chef.py
@@ -44,8 +44,6 @@ _EXCLUDE_DEVICE_FROM_LINUX_CI = [
     "noip_rootnode_dimmablelight_bCwGYSDpoe",  # Broken.
     "rootnode_genericswitch_2dfff6e516",  # not actively developed,
     "rootnode_mounteddimmableloadcontrol_a9a1a87f2d",  # not actively developed,
-    "rootnode_mountedonoffcontrol_ec30c757a6",  # not actively developed,
-    "rootnode_watervalve_6bb39f1f67",  # not actively developed,
 ]
 # Pattern to filter (based on device-name) devices that need ICD support.
 _ICD_DEVICE_PATTERN = "^icd_"
