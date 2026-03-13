1514e3a8c8bdf023041ee32460561cbb02cdd6eb
Marek Pikuła <m.pikula@partner.samsung.com>
2026-03-13 05:43:32 +0000

    Move logging to log_config
    Signed-off-by: Marek Pikuła <m.pikula@partner.samsung.com>

--- scripts/tests/run_test_suite.py
+++ scripts/tests/run_test_suite.py
@@ -32,7 +32,7 @@ import chiptest
 import click
 from chiptest.accessories import AppsRegister
 from chiptest.glob_matcher import GlobMatcher
-from chiptest.logging import LOG_LEVELS, LogConfig
+from chiptest.log_config import LOG_LEVELS, LogConfig
 from chiptest.runner import Executor, SubprocessKind
 from chiptest.test_definition import TEST_THREAD_DATASET, SubprocessInfoRepo, TestDefinition, TestRunTime, TestTag
 from chipyaml.paths_finder import PathsFinder
