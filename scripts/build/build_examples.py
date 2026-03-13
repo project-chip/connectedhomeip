b4d9bc784964c084a225592b3af8183630b7ba67
Restyled.io <commits@restyled.io>
2026-03-13 06:07:48 +0000

    Restyled by isort

--- scripts/build/build_examples.py
+++ scripts/build/build_examples.py
@@ -14,12 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import build
 import json
 import logging
 import os
 import sys
 
+import build
 import click
 import coloredlogs
 from builders.builder import BuilderOptions
