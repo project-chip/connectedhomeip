45f966a6fcee79ded0ae5eb680924d6e2655ef3a
Restyled.io <commits@restyled.io>
2026-03-19 21:08:01 +0000

    Restyled by isort

--- src/python_testing/test_testing/DefaultChecker.py
+++ src/python_testing/test_testing/DefaultChecker.py
@@ -16,17 +16,7 @@
 #
 
 # Re-export from the canonical location in the matter.testing package.
-from matter.testing.default_checker import (  # noqa: F401
-    DEFAULT_FIXED_LABEL_VALUES,
-    FLAG_DEFAULT_CALENDAR_FORMAT,
-    FLAG_FAULT_INJECTION,
-    FLAG_FIXED_LABEL_DEFAULT_VALUES,
-    FLAG_FIXED_LABEL_EMPTY,
-    FLAG_PRODUCT_NAME,
-    FLAG_SAMPLE_MEI,
-    FLAG_UNIT_TESTING,
-    FLAG_VENDOR_ID,
-    FLAG_VENDOR_NAME,
-    DefaultChecker,
-    warning_wrapper,
-)
+from matter.testing.default_checker import (DEFAULT_FIXED_LABEL_VALUES, FLAG_DEFAULT_CALENDAR_FORMAT,  # noqa: F401
+                                            FLAG_FAULT_INJECTION, FLAG_FIXED_LABEL_DEFAULT_VALUES, FLAG_FIXED_LABEL_EMPTY,
+                                            FLAG_PRODUCT_NAME, FLAG_SAMPLE_MEI, FLAG_UNIT_TESTING, FLAG_VENDOR_ID, FLAG_VENDOR_NAME,
+                                            DefaultChecker, warning_wrapper)
