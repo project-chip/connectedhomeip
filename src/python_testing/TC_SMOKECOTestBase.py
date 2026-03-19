523428c67c82a2d34cd339d3acdd7cdb7568a90a
jtrejoespinoza-grid <juanerasmo@google.com>
2026-03-19 15:56:29 -0700

    Added base class fo smokeco. Reafctor 2.1

---
+++ src/python_testing/TC_SMOKECOTestBase.py
@@ -0,0 +1,70 @@
+#
+#    Copyright (c) 2026 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import logging
+
+from mobly import asserts
+
+import matter.clusters as Clusters
+from matter.testing.matter_asserts import assert_int_in_range, assert_is_unixtimestamp, assert_valid_bool, assert_valid_uint32
+from matter.testing.matter_testing import MatterBaseTest
+
+log = logging.getLogger(__name__)
+
+class SmokeCoBaseTest(MatterBaseTest):
+
+    smokeco_cluster = Clusters.SmokeCoAlarm
+
+    async def read_smokeco_attribute_expect_success(self, attribute):
+        return await self.read_single_attribute_check_success(cluster=self.smokeco_cluster, endpoint=self.get_endpoint(), attribute=attribute)
+
+    async def read_smokeco_event(self,smokeco_event):
+        urgent = 1
+        #smoke_alarm_event = event
+        events_response = await self.default_controller.ReadEvent(
+            self.dut_node_id,
+            events=[(self.get_endpoint(), smokeco_event, urgent)],
+            fabricFiltered=True
+        )
+        log.info(f"Events gathered {events_response}")
+        # Only UpdateAppliedEvent should be in the list
+        if len(events_response) == 0:
+            asserts.fail("Failed to read events")
+        smoke_alarm_event_data = None
+        # Reads for the VersionAppliedEvent
+        for event in events_response:
+            if event.Header.EventId == smokeco_event.event_id:
+                log.info(f"Event retrieved {event}")
+                smoke_alarm_event_data = event.Data
+        if smoke_alarm_event_data is None :
+            asserts.fail(f"Failed to retrieve event for {smokeco_event}")
+        return smoke_alarm_event_data
+
+    async def read_attribute_check_range(self, attribute, range_low: int, range_high: int):
+        """Reads an attribute from the SmokeCluster and validate against a range."""
+        attr = await self.read_smokeco_attribute_expect_success(attribute=attribute)
+        assert_int_in_range(attr, range_low, range_high, f"Attritute {attribute} is out of range {attr}")
+
+    async def read_attribute_check_bool(self, attribute):
+        """Reads an attribute from the SmokeCluster and validate againts a boolean value."""
+        attr = await self.read_smokeco_attribute_expect_success(attribute=attribute)
+        assert_valid_bool(value=attr, description=f"Attribute {attribute} is not a bool instance {attr}")
+
+    async def read_attribute_check_epoch(self, attribute):
+        """Reads an attribute from the SmokeCluster and validate againts is valid timestmap value."""
+        attr = await self.read_smokeco_attribute_expect_success(attribute=attribute)
+        assert_valid_uint32(attr, "Attribute is not in uint range")
+        assert_is_unixtimestamp(attr, f"Attribute with value: {attr}")
--- src/python_testing/TC_SMOKECO_2_1.py
+++ src/python_testing/TC_SMOKECO_2_1.py
@@ -37,44 +37,16 @@
 
 import logging
 
+from TC_SMOKECOTestBase import SmokeCoBaseTest
+
 import matter.clusters as Clusters
-from matter.testing.decorators import async_test_body
-from matter.testing.matter_asserts import assert_int_in_range, assert_is_unixtimestamp, assert_valid_bool, assert_valid_uint32
-from matter.testing.matter_testing import MatterBaseTest
+from matter.testing.decorators import async_test_body, has_cluster, run_if_endpoint_matches
 from matter.testing.runner import TestStep, default_matter_test_main
 
 log = logging.getLogger(__name__)
 
 
-class TC_SMOKECO_2_1(MatterBaseTest):
-
-    @async_test_body
-    async def setup_test(self):
-        super().setup_test()
-        self.smoke_cluster = Clusters.SmokeCoAlarm
-
-    async def _read_attribute_check_range(self, attribute, range_low: int, range_high: int):
-        """Reads an attribute from the SmokeCluster and validate against a range
-        """
-        if self.attribute_guard(self.get_endpoint(), attribute=attribute):
-            attr = await self.read_single_attribute_check_success(cluster=self.smoke_cluster, attribute=attribute, dev_ctrl=self.default_controller, endpoint=self.get_endpoint())
-            # Validate
-            assert_int_in_range(attr, range_low, range_high, f"Attritute {attribute} is out of range {attr}")
-
-    async def _read_attribute_check_bool(self, attribute):
-        """Reads an attribute from the SmokeCluster and validate againts a boolean value
-        """
-        if self.attribute_guard(self.get_endpoint(), attribute=attribute):
-            attr = await self.read_single_attribute_check_success(cluster=self.smoke_cluster, attribute=attribute, dev_ctrl=self.default_controller, endpoint=self.get_endpoint())
-            assert_valid_bool(attr, f"Attritute {attribute} is not a bool instance {attr}")
-
-    async def _read_attribute_check_epoch(self, attribute):
-        """Reads an attribute from the SmokeCluster and validate againts a boolean value
-        """
-        if self.attribute_guard(self.get_endpoint(), attribute=attribute):
-            attr = await self.read_single_attribute_check_success(cluster=self.smoke_cluster, attribute=attribute, dev_ctrl=self.default_controller, endpoint=self.get_endpoint())
-            assert_valid_uint32(attr, "Attribute is not in uint range")
-            assert_is_unixtimestamp(attr, f"Attribute with value: {attribute} ")
+class TC_SMOKECO_2_1(SmokeCoBaseTest):
 
     def desc_TC_SMOKECO_2_1(self) -> str:
         return "[TC-SMOKECO-2.7] Unmount Attribute with DUT as Server"
@@ -115,53 +87,58 @@ class TC_SMOKECO_2_1(MatterBaseTest):
             "SMOKECO.S.A000d",
         ]
 
-    @async_test_body
+    @run_if_endpoint_matches(has_cluster(Clusters.SmokeCoAlarm))
     async def test_TC_SMOKECO_2_1(self):
 
         # Step 1, "Commission DUT to TH."
         self.step(1)  # Commissioning already done
 
         self.step(2)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.ExpressedState, 0, 9)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.ExpressedState, 0, 9)
 
         self.step(3)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.SmokeState, 0, 2)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.SmokeState, 0, 2)
 
         self.step(4)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.COState, 0, 2)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.COState, 0, 2)
 
         self.step(5)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.BatteryAlert, 0, 2)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.BatteryAlert, 0, 2)
 
         self.step(6)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.DeviceMuted, 0, 2)
+        if self.attribute_guard(endpoint=self.get_endpoint(), attribute=self.smokeco_cluster.Attributes.DeviceMuted):
+            await self.read_attribute_check_range(self.smokeco_cluster.Attributes.DeviceMuted, 0, 2)
 
         self.step(7)
-        await self._read_attribute_check_bool(self.smoke_cluster.Attributes.TestInProgress)
+        await self.read_attribute_check_bool(self.smokeco_cluster.Attributes.TestInProgress)
 
         self.step(8)
-        await self._read_attribute_check_bool(self.smoke_cluster.Attributes.HardwareFaultAlert)
+        await self.read_attribute_check_bool(self.smokeco_cluster.Attributes.HardwareFaultAlert)
 
         self.step(9)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.EndOfServiceAlert, 0, 1)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.EndOfServiceAlert, 0, 1)
 
         self.step(10)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.InterconnectSmokeAlarm, 0, 2)
+        if self.attribute_guard(endpoint=self.get_endpoint(), attribute=self.smokeco_cluster.Attributes.InterconnectSmokeAlarm):
+            await self.read_attribute_check_range(self.smokeco_cluster.Attributes.InterconnectSmokeAlarm, 0, 2)
 
         self.step(11)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.InterconnectCOAlarm, 0, 2)
+        if self.attribute_guard(endpoint=self.get_endpoint(), attribute=self.smokeco_cluster.Attributes.InterconnectCOAlarm):
+            await self.read_attribute_check_range(self.smokeco_cluster.Attributes.InterconnectCOAlarm, 0, 2)
 
         self.step(12)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.ContaminationState, 0, 3)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.ContaminationState, 0, 3)
 
         self.step(13)
-        await self._read_attribute_check_range(self.smoke_cluster.Attributes.SmokeSensitivityLevel, 0, 2)
+        await self.read_attribute_check_range(self.smokeco_cluster.Attributes.SmokeSensitivityLevel, 0, 2)
 
         self.step(14)
-        await self._read_attribute_check_epoch(self.smoke_cluster.Attributes.ExpiryDate)
+        if self.attribute_guard(endpoint=self.get_endpoint(), attribute=self.smokeco_cluster.Attributes.ExpiryDate):
+            await self.read_attribute_check_epoch(self.smokeco_cluster.Attributes.ExpiryDate)
 
         self.step(15)
-        await self._read_attribute_check_bool(self.smoke_cluster.Attributes.Unmounted)
+        if self.attribute_guard(endpoint=self.get_endpoint(), attribute=self.smokeco_cluster.Attributes.Unmounted):
+            await self.read_attribute_check_bool(self.smokeco_cluster.Attributes.Unmounted)
 
 
 if __name__ == "__main__":
