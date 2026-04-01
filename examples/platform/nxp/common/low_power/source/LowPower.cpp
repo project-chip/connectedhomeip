b81304d7509b93177c613d7580e4353ec078751d
Marian Chereji <marian.chereji@nxp.com>
2026-04-01 19:09:39 +0300

    [nxp toup][mcxw72] Add external flash management for low power scenarios
    The external flash memory is only used during Matter OTA hence it is
    pointless to have it enabled all the time as it constantly consumes a
    certain amount of power. This upgrade improves the management of the
    external flash memory in low power scenarios so that it is enabled only
    when it is actually used (during OTA).
    
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- examples/platform/nxp/common/low_power/source/LowPower.cpp
+++ examples/platform/nxp/common/low_power/source/LowPower.cpp
@@ -17,7 +17,15 @@
 
 #include "LowPower.h"
 #include "fsl_pm_core.h"
+#include "fwk_platform_lowpower.h"
+// The "fwk_platform_extflash.h" header file does not export its API for C++
+// linkage so we have to force it here until it does
+extern "C" {
+#include "fwk_platform_extflash.h"
+}
 
+#include <platform/PlatformManager.h>
+#include <platform/nxp/common/PlatformManagerImpl.h>
 #include <lib/support/CodeUtils.h>
 
 #define APP_ERROR_PM_REGISTER_LP_CALLBACK_FAILED CHIP_APPLICATION_ERROR(0x01)
@@ -31,7 +39,28 @@ static pm_notify_element_t appNotifyElement = {
 
 static status_t LowPowerCallback(pm_event_type_t type, uint8_t state, void * data)
 {
-    // This is just an example. It does nothing by default.
+    if (state < PLATFORM_DEEP_SLEEP_STATE)
+    {
+        /* Nothing to do when entering WFI or Sleep low power state
+           NVIC fully functionnal to trigger upcoming interrupts */
+        return kStatus_Success;
+    }
+
+    // Do nothing if the OTA storage is not enabled
+    if (chip::DeviceLayer::PlatformMgrImpl().GetOTAStorageState() != chip::DeviceLayer::PlatformManagerImpl::OTAStorageEnabled)
+    {
+        return kStatus_Success;
+    }
+
+    if (type == kPM_EventEnteringSleep)
+    {
+        PLATFORM_UninitExternalFlash();
+    }
+    else
+    {
+        PLATFORM_ReinitExternalFlash();
+    }
+
     return kStatus_Success;
 }
 
