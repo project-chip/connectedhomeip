33dc5dc6d588535ac9fb43db020f24cb8c916d53
Petru Lauric <plauric@irobot.com>
2026-04-10 17:04:47 -0400

    fix Python method name

--- src/python_testing/support_modules/binfo_attributes_verification.py
+++ src/python_testing/support_modules/binfo_attributes_verification.py
@@ -361,7 +361,7 @@ class BasicInformationAttributesVerificationBase(MatterBaseTest):
         # Step 25: Write empty DeviceLocation
         self.step(25)
         if hasattr(cluster.Attributes, 'DeviceLocation') and await self.attribute_guard(endpoint=self.endpoint, attribute=cluster.Attributes.DeviceLocation):
-            await self.write_single_attribute_check_success(cluster=cluster, attribute=cluster.Attributes.DeviceLocation, value=Globals.Structs.LocationDescriptorStruct(locationName="", floorNumber=-1, areaType=None))
+            await self.write_single_attribute(cluster=cluster, attribute=cluster.Attributes.DeviceLocation, value=Globals.Structs.LocationDescriptorStruct(locationName="", floorNumber=-1, areaType=None))
         elif not hasattr(cluster.Attributes, 'DeviceLocation'):
             self.mark_current_step_skipped()
 
@@ -380,7 +380,7 @@ class BasicInformationAttributesVerificationBase(MatterBaseTest):
         # Step 27: Write DeviceLocation with location name of 128 characters
         self.step(27)
         if hasattr(cluster.Attributes, 'DeviceLocation') and await self.attribute_guard(endpoint=self.endpoint, attribute=cluster.Attributes.DeviceLocation):
-            await self.write_single_attribute_check_success(cluster=cluster, attribute=cluster.Attributes.DeviceLocation, value=Globals.Structs.LocationDescriptorStruct(locationName="location" * 16, floorNumber=200, areaType=0x0002))
+            await self.write_single_attribute(cluster=cluster, attribute=cluster.Attributes.DeviceLocation, value=Globals.Structs.LocationDescriptorStruct(locationName="location" * 16, floorNumber=200, areaType=0x0002))
         elif not hasattr(cluster.Attributes, 'DeviceLocation'):
             self.mark_current_step_skipped()
 
