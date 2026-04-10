91874bd3f31fd24094190fcbd3939af6bcb543fd
Petru Lauric <plauric@irobot.com>
2026-04-10 16:15:53 -0400

    fix style issue

--- src/python_testing/support_modules/binfo_attributes_verification.py
+++ src/python_testing/support_modules/binfo_attributes_verification.py
@@ -22,8 +22,8 @@ import validators
 from mobly import asserts
 
 from matter.clusters import Globals
-from matter.clusters.Types import Nullable
 from matter.clusters.ClusterObjects import Cluster
+from matter.clusters.Types import Nullable
 from matter.testing.conformance import ConformanceException
 from matter.testing.matter_testing import MatterBaseTest, TestStep
 from matter.testing.spec_parsing import dm_from_spec_version
