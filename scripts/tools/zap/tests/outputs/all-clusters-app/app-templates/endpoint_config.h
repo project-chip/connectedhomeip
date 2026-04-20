946d4ddd54289c4e13e6055f62b3c0e16894794b
LyudmilaKostanyan <lyudakstnn@gmail.com>
2026-04-20 21:08:13 +0400

    Zap regen

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4951,7 +4951,7 @@
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
         { ZAP_CLUSTER_INDEX(0), 28, 177 },                                                                                         \
-        { ZAP_CLUSTER_INDEX(28), 73, 3350 },                                                                                       \
+        { ZAP_CLUSTER_INDEX(28), 73, 3342 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 99 },                                                                                         \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
     }
@@ -4965,7 +4965,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3626)
+#define ATTRIBUTE_MAX_SIZE (3618)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
