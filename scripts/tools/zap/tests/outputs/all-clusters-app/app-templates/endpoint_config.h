ee7289138cf5be15a6355eedcd05511d697fc57a
LyudmilaKostanyan <lyudakstnn@gmail.com>
2026-04-21 17:45:45 +0400

    Zap regen

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4955,7 +4955,7 @@
 // This is an array of EmberAfEndpointType structures.
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
-        { ZAP_CLUSTER_INDEX(0), 28, 177 },                                                                                         \
+        { ZAP_CLUSTER_INDEX(0), 28, 168 },                                                                                         \
         { ZAP_CLUSTER_INDEX(28), 73, 3342 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 99 },                                                                                         \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
@@ -4970,7 +4970,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3618)
+#define ATTRIBUTE_MAX_SIZE (3609)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
