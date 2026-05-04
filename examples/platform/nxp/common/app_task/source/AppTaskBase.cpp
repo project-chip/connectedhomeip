34a958ee2590365f2bf94ad65a9125a48a4db124
Doru-Cristian Gucea <doru-cristian.gucea@nxp.com>
2026-05-04 15:30:33 +0100

    [nxp noup] Fix build error when CONFIG_CHIP_APP_FACTORY_DATA=n
    Signed-off-by: Doru-Cristian Gucea <doru-cristian.gucea@nxp.com>

--- examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
+++ examples/platform/nxp/common/app_task/source/AppTaskBase.cpp
@@ -276,6 +276,7 @@ CHIP_ERROR chip::NXP::App::AppTaskBase::Init()
     TEMPORARY_RETURN_IGNORED chip::NXP::App::LowPower::Init();
 #endif
 
+#if CONFIG_CHIP_APP_FACTORY_DATA
     /* Initialize Matter factory data before initializing the Matter stack */
     err = AppFactoryData_PreMatterStackInit();
 
@@ -284,6 +285,7 @@ CHIP_ERROR chip::NXP::App::AppTaskBase::Init()
         ChipLogError(DeviceLayer, "Pre Factory Data Provider init failed");
         goto exit;
     }
+#endif
 
     /*
      * Initialize the CHIP stack.
@@ -296,6 +298,7 @@ CHIP_ERROR chip::NXP::App::AppTaskBase::Init()
         goto exit;
     }
 
+#if CONFIG_CHIP_APP_FACTORY_DATA
     /* Initialize Matter factory data after initializing the Matter stack */
     err = AppFactoryData_PostMatterStackInit();
     if (err != CHIP_NO_ERROR)
@@ -303,6 +306,7 @@ CHIP_ERROR chip::NXP::App::AppTaskBase::Init()
         ChipLogError(DeviceLayer, "Post Factory Data Provider init failed");
         goto exit;
     }
+#endif
 
     /*
      * Register all application callbacks allowing to be informed of stack events
