faa431e84f8e9f488a34c357c350411da429fe3c
Hayk10002 <hayk.g.khachatryan@gmail.com>
2026-05-05 11:13:11 +0000

    Implement `ReadAttribute` on `DummyPowerSourceCluster`

--- src/app/clusters/power-source-server/CodegenIntegration.h
+++ src/app/clusters/power-source-server/CodegenIntegration.h
@@ -90,6 +90,9 @@ public:
     DummyPowerSourceCluster(const DummyPowerSourceCluster &)             = delete;
     DummyPowerSourceCluster & operator=(const DummyPowerSourceCluster &) = delete;
 
+    DataModel::ActionReturnStatus ReadAttribute(const DataModel::ReadAttributeRequest & request,
+                                                        AttributeValueEncoder & encoder) override { return {}; }
+
     PowerSourceStatusEnum GetStatus() const { return {}; }
     uint8_t GetOrder() const { return {}; }
     CharSpan GetDescription() const { return {}; }
