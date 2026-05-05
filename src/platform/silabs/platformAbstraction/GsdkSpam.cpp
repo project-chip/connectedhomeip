3130c42fcef7f7fc70b7e7038d90cac6ae21e286
chirag-silabs <chirag.bansal@silabs.com>
2026-05-05 11:45:37 +0530

    Fixing the build for the boards without the button

--- src/platform/silabs/platformAbstraction/GsdkSpam.cpp
+++ src/platform/silabs/platformAbstraction/GsdkSpam.cpp
@@ -303,15 +303,17 @@ uint8_t SilabsPlatform::GetButtonState(uint8_t button)
     const sl_button_t * handle = SL_SIMPLE_BUTTON_INSTANCE(button);
     return nullptr == handle ? 0 : sl_button_get_state(handle);
 }
-#ifdef SL_ICD_ENABLED
-void SilabsPlatform::SleepButtonActionHandler() {}
-#endif // SL_ICD_ENABLED
 #else
 uint8_t SilabsPlatform::GetButtonState(uint8_t button)
 {
     return 0;
 }
 #endif // SL_CATALOG_SIMPLE_BUTTON_PRESENT
+
+// Sleep Button Action Handler does nothing for now, but it is here to be overridden by the platform if needed.
+#ifdef SL_ICD_ENABLED
+void SilabsPlatform::SleepButtonActionHandler() {}
+#endif // SL_ICD_ENABLED
 
 #if defined(SL_MATTER_USE_SI70XX_SENSOR) && SL_MATTER_USE_SI70XX_SENSOR
 sl_status_t SilabsPlatform::EnableSi70xxSensorGpio()
