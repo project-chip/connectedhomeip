0a25fd0dda6f4e04cba005454bf19343d6db7280
Marian CHEREJI <122872376+marian-chereji-nxp@users.noreply.github.com>
2026-05-05 22:43:17 +0300

    [nxp fromtree][nxp][platform] Remove support for the NXP MCXW71 platform (#71806)
    The NXP MCXW71 platform is no longer supported in Matter 1.6.
    
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>
    (cherry picked from commit 919d310)

--- examples/platform/nxp/common/app_common.cmake
+++ examples/platform/nxp/common/app_common.cmake
@@ -266,8 +266,8 @@ if (CONFIG_CHIP_APP_OTA_REQUESTOR)
     endif()
     if (CONFIG_CHIP_APP_PLATFORM_OTA_UTILS)
         target_sources(app PRIVATE
-            # Use the example provided by mcxw71 platform until a common solution is proposed.
-            ${EXAMPLE_PLATFORM_NXP_COMMON_DIR}/../mcxw71/ota/OtaUtils.cpp
+            # Use the example provided by mcxw72 platform until a common solution is proposed.
+            ${EXAMPLE_PLATFORM_NXP_COMMON_DIR}/../mcxw72/ota/OtaUtils.cpp
         )
     endif()
 endif()
@@ -292,8 +292,8 @@ if (CONFIG_CHIP_APP_UI_FEEDBACK)
             )
         else()
             target_sources(app PRIVATE
-                # Use the example provided by mcxw71 platform until a common solution is proposed.
-                ${EXAMPLE_PLATFORM_NXP_COMMON_DIR}/../mcxw71/util/LedOnOff.cpp
+                # Use the example provided by mcxw72 platform until a common solution is proposed.
+                ${EXAMPLE_PLATFORM_NXP_COMMON_DIR}/../mcxw72/util/LedOnOff.cpp
             )
         endif()
     endif()
--- examples/platform/nxp/mcxw71/app_ble/include/extra_gatt_db.h
+++
@@ -1,4 +0,0 @@
-PRIMARY_SERVICE_UUID128(service_wireless_uart, uuid_service_wireless_uart)
-CHARACTERISTIC_UUID128(char_uart_stream, uuid_uart_stream, (gGattCharPropWriteWithoutRsp_c))
-VALUE_UUID128_VARLEN(value_uart_stream, uuid_uart_stream, (gPermissionFlagWritable_c), gAttMaxWriteDataSize_d(gAttMaxMtu_c), 1,
-                     0x00)
