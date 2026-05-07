cfc9075226bd030151ea943d02ba27899dc69f30
LyudmilaKostanyan <lyudakstnn@gmail.com>
2026-05-07 20:31:04 +0400

    Add CI test arguments for AIR_PURIFIER_APP in multiple fan test cases

--- src/python_testing/TC_FAN_3_1.py
+++ src/python_testing/TC_FAN_3_1.py
@@ -21,6 +21,21 @@
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs:
 #   run1:
+#     app: ${AIR_PURIFIER_APP}
+#     app-args: --discriminator 1234 --KVS kvs1 --trace_file json:${TRACE_APP}.json
+#     script-args: >
+#       --storage-path admin_storage.json
+#       --commissioning-method on-network
+#       --discriminator 1234
+#       --passcode 20202021
+#       --PICS src/app/tests/suites/certification/ci-pics-values
+#       --endpoint 1
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#       --timeout 600
+#     factory-reset: true
+#     quiet: true
+#   run2:
 #     app: ${ALL_DEVICES_APP}
 #     app-args: --discriminator 1234 --KVS kvs1 --device fan
 #     script-args: >
