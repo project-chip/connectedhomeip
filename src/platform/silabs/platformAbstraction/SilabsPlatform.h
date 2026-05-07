f6709566e48874085ae81c36723067fec01d38d9
chirag-silabs <chirag.bansal@silabs.com>
2026-05-07 10:32:46 +0530

    Refactor the btns

--- src/platform/silabs/platformAbstraction/SilabsPlatform.h
+++ src/platform/silabs/platformAbstraction/SilabsPlatform.h
@@ -50,12 +50,14 @@ public:
 #endif
 
     // Buttons
+#if defined(SL_CATALOG_SIMPLE_BUTTON_PRESENT)
     inline void SetButtonsCb(SilabsButtonCb callback) override { mButtonCallback = callback; }
     static SilabsButtonCb mButtonCallback;
     uint8_t GetButtonState(uint8_t button) override;
-#if defined(SL_ICD_ENABLED) && SL_ICD_ENABLED == 1 && defined(SL_CATALOG_SIMPLE_BUTTON_PRESENT)
+#if defined(SL_ICD_ENABLED) && SL_ICD_ENABLED == 1
     void SleepButtonActionHandler(void) override;
-#endif // defined(SL_ICD_ENABLED) && SL_ICD_ENABLED == 1 && defined(SL_ICD_SLEEP_BUTTON_ENABLE)
+#endif // defined(SL_ICD_ENABLED) && SL_ICD_ENABLED == 1
+#endif // defined(SL_CATALOG_SIMPLE_BUTTON_PRESENT)
 
 #if defined(SL_CATALOG_CUSTOM_MAIN_PRESENT)
     void StartScheduler(void) override;
