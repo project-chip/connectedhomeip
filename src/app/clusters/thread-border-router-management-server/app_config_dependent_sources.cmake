924343dd184da02f59e456b8453b65c9bbdcface
Sergio Soares <sergiosoares@google.com>
2026-05-12 20:11:14 +0000

    Added CodegenIntegration.h to app_config_dependent_sources
    - Followed pattern of other clusters to list header in build files.
    
    ### Testing
    Verified that network-manager-app built successfully in previous step.

--- src/app/clusters/thread-border-router-management-server/app_config_dependent_sources.cmake
+++ src/app/clusters/thread-border-router-management-server/app_config_dependent_sources.cmake
@@ -17,6 +17,7 @@ TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
     "${CLUSTER_DIR}/CodegenIntegration.cpp"
+    "${CLUSTER_DIR}/CodegenIntegration.h"
     "${CLUSTER_DIR}/thread-border-router-management-server.h"
     "${CLUSTER_DIR}/thread-br-delegate.h"
 )
