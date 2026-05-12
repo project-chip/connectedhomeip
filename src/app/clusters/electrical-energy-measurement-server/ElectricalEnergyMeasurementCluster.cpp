cfc292930309d79ef8923f3fca6243a3fedccdf6
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2026-05-12 10:31:21 -0400

    Fix missing gn dependency

--- src/app/clusters/electrical-energy-measurement-server/ElectricalEnergyMeasurementCluster.cpp
+++ src/app/clusters/electrical-energy-measurement-server/ElectricalEnergyMeasurementCluster.cpp
@@ -15,7 +15,6 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#include "app/cluster-building-blocks/QuieterReporting.h"
 #include <app/clusters/electrical-energy-measurement-server/ElectricalEnergyMeasurementCluster.h>
 
 #include <protocols/interaction_model/StatusCode.h>
