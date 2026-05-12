9d8c7a399d788ded1278a1de1aa7cbdd6344e355
AniDashyan <ani.dashyan77@gmail.com>
2026-05-12 19:14:24 +0400

    Fix build issues and address review comments

--- src/app/clusters/window-covering-server/WindowCoveringCluster.h
+++ src/app/clusters/window-covering-server/WindowCoveringCluster.h
@@ -281,62 +281,23 @@ private:
     chip::BitMask<SafetyStatus> mSafetyStatus;
 };
 
-// These functions are only kept for backwards compatibility with example apps and should not be used by new code.
-// New code should interact with the cluster directly through the WindowCoveringCluster API.
-void TypeSet(chip::EndpointId endpoint, Type type);
-Type TypeGet(chip::EndpointId endpoint);
-
-void ConfigStatusPrint(const chip::BitMask<ConfigStatus> & configStatus);
-void ConfigStatusSet(chip::EndpointId endpoint, const chip::BitMask<ConfigStatus> & status);
-chip::BitMask<ConfigStatus> ConfigStatusGet(chip::EndpointId endpoint);
-void ConfigStatusUpdateFeatures(chip::EndpointId endpoint);
-
-void OperationalStatusPrint(const chip::BitMask<OperationalStatus> & opStatus);
-void OperationalStatusSet(chip::EndpointId endpoint, chip::BitMask<OperationalStatus> newStatus);
-chip::BitMask<OperationalStatus> OperationalStatusGet(chip::EndpointId endpoint);
-void OperationalStateSet(chip::EndpointId endpoint, const chip::BitMask<OperationalStatus> field, OperationalState state);
-OperationalState OperationalStateGet(chip::EndpointId endpoint, const chip::BitMask<OperationalStatus> field);
+// Helper functions
+LimitStatus CheckLimitState(uint16_t position, AbsoluteLimits limits);
+bool IsPercent100thsValid(Percent100ths percent100ths);
+bool IsPercent100thsValid(NPercent100ths npercent100ths);
 
 OperationalState ComputeOperationalState(uint16_t target, uint16_t current);
 OperationalState ComputeOperationalState(NPercent100ths target, NPercent100ths current);
 Percent100ths ComputePercent100thsStep(OperationalState direction, Percent100ths previous, Percent100ths delta);
-
-void EndProductTypeSet(chip::EndpointId endpoint, EndProductType type);
-EndProductType EndProductTypeGet(chip::EndpointId endpoint);
-
-void ModePrint(const chip::BitMask<Mode> & mode);
-void ModeSet(chip::EndpointId endpoint, chip::BitMask<Mode> & mode);
-chip::BitMask<Mode> ModeGet(chip::EndpointId endpoint);
-
-void SafetyStatusSet(chip::EndpointId endpoint, const chip::BitMask<SafetyStatus> & status);
-chip::BitMask<SafetyStatus> SafetyStatusGet(chip::EndpointId endpoint);
-
-LimitStatus CheckLimitState(uint16_t position, AbsoluteLimits limits);
-bool IsPercent100thsValid(Percent100ths percent100ths);
-bool IsPercent100thsValid(NPercent100ths npercent100ths);
 
 uint16_t Percent100thsToValue(AbsoluteLimits limits, Percent100ths relative);
 Percent100ths ValueToPercent100ths(AbsoluteLimits limits, uint16_t absolute);
 
 uint16_t LiftToPercent100ths(chip::EndpointId endpoint, uint16_t lift);
 uint16_t Percent100thsToLift(chip::EndpointId endpoint, uint16_t percent100ths);
-void LiftPositionSet(chip::EndpointId endpoint, NPercent100ths position);
 
 uint16_t TiltToPercent100ths(chip::EndpointId endpoint, uint16_t tilt);
 uint16_t Percent100thsToTilt(chip::EndpointId endpoint, uint16_t percent100ths);
-void TiltPositionSet(chip::EndpointId endpoint, NPercent100ths position);
-
-/**
- * @brief PostAttributeChange is called when an Attribute is modified.
- *
- * The method is called by MatterWindowCoveringClusterServerAttributeChangedCallback
- * to update cluster attributes values. If the application overrides MatterWindowCoveringClusterServerAttributeChangedCallback,
- * it should call the PostAttributeChange on its own.
- *
- * @param[in] endpoint
- * @param[in] attributeId
- */
-void PostAttributeChange(chip::EndpointId endpoint, chip::AttributeId attributeId);
 
 } // namespace WindowCovering
 } // namespace Clusters
