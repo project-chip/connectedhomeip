0e57423a1ab12b66690cf3528446facf7280dbb6
Dheeraj <kuppamsaidheeraj.singh@nxp.com>
2026-05-15 17:25:03 +0530

    [nxp noup][SE05x][MATTER-4224]Optimise factory reset to reset only relevant data for nfc-comm
    [nxp noup][SE05x][MATTER-4224]Optimise factory reset to reset wifi/thread and rootca data for nfc-comm
    
    [nxp noup][SE05x][MATTER-4224] Remove unused functions
    
    (cherry picked from commit 7c5bb9f24557db76277490e8496f1bb1df697291)

--- src/platform/nxp/crypto/se05x/CHIPCryptoPALHsm_se05x_utils.cpp
+++ src/platform/nxp/crypto/se05x/CHIPCryptoPALHsm_se05x_utils.cpp
@@ -392,25 +392,6 @@ CHIP_ERROR se05x_set_binary_data(uint32_t keyId, const uint8_t * buf, size_t buf
     return CHIP_NO_ERROR;
 }
 
-/* Set EC key in se05x */
-CHIP_ERROR se05x_set_ec_key(uint32_t keyId, const uint8_t * buf, size_t buflen)
-{
-    sss_object_t keyObject = { 0 };
-    sss_status_t status    = kStatus_SSS_Fail;
-
-    status = sss_key_object_init(&keyObject, &gex_sss_chip_ctx.ks);
-    VerifyOrReturnError(status == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
-
-    status = sss_key_object_allocate_handle(&keyObject, keyId, kSSS_KeyPart_Pair, kSSS_CipherType_EC_NIST_P, buflen,
-                                            kKeyObject_Mode_Persistent);
-    VerifyOrReturnError(status == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
-
-    status = sss_key_store_set_key(&gex_sss_chip_ctx.ks, &keyObject, buf, buflen, 256, NULL, 0);
-    VerifyOrReturnError(status == kStatus_SSS_Success, CHIP_ERROR_INTERNAL);
-
-    return CHIP_NO_ERROR;
-}
-
 /* Perform internal sign in se05x (only on SE051H) */
 CHIP_ERROR se05x_perform_internal_sign(uint32_t keyId, uint8_t * sigBuf, size_t * sigBufLen)
 {
