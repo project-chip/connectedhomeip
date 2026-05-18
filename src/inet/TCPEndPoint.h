e0b74d8ad402ed2273a84b40f274217c04888bf9
Dominik Dess <dominik@4rooks.com>
2026-05-18 13:45:11 +0000

    [inet] Fix C++20 reversed-argument operator== in UDP/TCP Endpoint handles
    C++20 added reversed-argument lookup for operator==. Inside
    UDPEndPoint::operator==(const UDPEndPointHandle&) and its TCP
    equivalent, the expression `other == *this` is now ambiguous: the
    compiler considers calling the enclosing function recursively with
    swapped arguments, triggering -Werror on the resulting diagnostic.
    
    Replace `other == *this` with `other.operator==(*this)` to invoke
    ReferenceCountedPtr::operator==(const T&) directly, bypassing C++20
    reversed-argument resolution.

--- src/inet/TCPEndPoint.h
+++ src/inet/TCPEndPoint.h
@@ -522,8 +522,8 @@ public:
     static bool sForceEarlyFailureIncomingConnection;
 #endif
 
-    inline bool operator==(const TCPEndPointHandle & other) const { return other == *this; }
-    inline bool operator!=(const TCPEndPointHandle & other) const { return other != *this; }
+    inline bool operator==(const TCPEndPointHandle & other) const { return other.operator==(*this); }
+    inline bool operator!=(const TCPEndPointHandle & other) const { return other.operator!=(*this); }
 
 protected:
     friend class TCPTest;
