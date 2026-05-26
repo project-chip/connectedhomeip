93c01be1a19450a410b16b29386c69e66592f4a5
Simon Morris <simon.morris@morsemicro.com>
2026-05-26 13:47:54 +0100

    Fix ids/types for kProximityRanger

--- zzz_generated/app-common/devices/Ids.h
+++ zzz_generated/app-common/devices/Ids.h
@@ -230,6 +230,12 @@ constexpr uint8_t kCameraControllerDeviceTypeRevision = 1;
 constexpr DeviceTypeId kDoorbellDeviceTypeId  = 0x00000148;
 constexpr uint8_t kDoorbellDeviceTypeRevision = 1;
 
+constexpr DeviceTypeId kAmbientContextSensorDeviceTypeId  = 0x00000150;
+constexpr uint8_t kAmbientContextSensorDeviceTypeRevision = 1;
+
+constexpr DeviceTypeId kProximityRangerDeviceTypeId  = 0x00000152;
+constexpr uint8_t kProximityRangerDeviceTypeRevision = 1;
+
 constexpr DeviceTypeId kWindowCoveringDeviceTypeId  = 0x00000202;
 constexpr uint8_t kWindowCoveringDeviceTypeRevision = 4;
 
