e2a47cc3fe604d452418097b2d11250b1066bd31
Marek Pikuła <m.pikula@partner.samsung.com>
2026-05-27 09:00:37 +0000

    Move process start to resource_start in DBusTestSystemBus

--- src/python_testing/matter_testing_infrastructure/matter/testing/linux/dbus.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/linux/dbus.py
@@ -21,25 +21,31 @@ import subprocess
 from matter.testing.concurrency.context import TerminableResource
 
 
-class DBusTestSystemBus(TerminableResource, subprocess.Popen[bytes]):
+class DBusTestSystemBus(TerminableResource):
     """Run a dbus-daemon in a subprocess as a test system bus."""
 
     SOCKET = pathlib.Path(f"/tmp/chip-dbus-{os.getpid()}")
     ADDRESS = f"unix:path={SOCKET}"
 
-    def __init__(self):
-        super().__init__(["dbus-daemon", "--session", "--print-address", "--address", self.ADDRESS],
-                         stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        os.environ["DBUS_SYSTEM_BUS_ADDRESS"] = self.ADDRESS
-        # Wait for the bus to start (it will print the address to stdout).
-        assert self.stdout is not None, "stdout should have been set to subprocess.PIPE"
-        self.stdout.readline()
+    def __init__(self) -> None:
+        self._process: subprocess.Popen[bytes] | None = None
 
-    def resource_terminate(self):
-        try:
-            super().terminate()
-            self.wait()
-        finally:
-            if self.stdout is not None:
-                self.stdout.close()
-            self.SOCKET.unlink(missing_ok=True)
+    def resource_start(self) -> None:
+        self._process = subprocess.Popen(["dbus-daemon", "--session", "--print-address", "--address", self.ADDRESS],
+                                         stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        os.environ["DBUS_SYSTEM_BUS_ADDRESS"] = self.ADDRESS
+
+        # Wait for the bus to start (it will print the address to stdout).
+        assert self._process.stdout is not None, "stdout should have been set to subprocess.PIPE"
+        self._process.stdout.readline()
+
+    def resource_terminate(self) -> None:
+        if self._process is not None:
+            try:
+                self._process.terminate()
+                self._process.wait()
+            finally:
+                if self._process.stdout is not None:
+                    self._process.stdout.close()
+
+        self.SOCKET.unlink(missing_ok=True)
