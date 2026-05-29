cf1ba248e249d086f1e3ca669a0f6971d80e8b17
Olivier LORENTE <olivier.lorente@st.com>
2026-05-29 14:41:33 +0200

    Fix from Gemini

--- src/controller/CHIPDeviceController.cpp
+++ src/controller/CHIPDeviceController.cpp
@@ -2640,7 +2640,10 @@ CHIP_ERROR DeviceCommissioner::ParseNetworkCommissioningInfo(ReadCommissioningIn
             {
                 ChipLogProgress(Controller, "NetworkCommissioning Features: has Ethernet. endpointid = %u", path.mEndpointId);
                 info.network.eth.endpoint = path.mEndpointId;
-                mDefaultCommissioner->SetNetworkSetupNeeded(false);
+                if (mDefaultCommissioner != nullptr)
+                {
+                    mDefaultCommissioner->SetNetworkSetupNeeded(false);
+                }
             }
         }
         return CHIP_NO_ERROR;
