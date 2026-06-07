83c921b6486386d0ca3c076aa6ff0f4d4946fe90
Shreyas Bhandare <bhandares@google.com>
2026-06-07 15:53:07 -0700

    Update test to use water leak detector on endpoint 2

--- examples/all-devices-app/all-devices-common/devices/boolean-state-sensor/test.py
+++ examples/all-devices-app/all-devices-common/devices/boolean-state-sensor/test.py
@@ -21,10 +21,7 @@ source ./scripts/bootstrap.sh -p linux
 scripts/run_in_build_env.sh './scripts/build_python.sh --install_virtual_env out/python_env -pw true'
 
 # Build app
-cd examples/all-devices-app/posix
-gn gen --add-export-compile-commands="*" out
-ninja -C out
-cd ../../..
+cd examples/all-devices-app/posix && gn gen --add-export-compile-commands="*" out && ninja -C out && cd ../../..
 
 # Run test
 scripts/run_in_python_env.sh out/python_env \
@@ -32,7 +29,7 @@ scripts/run_in_python_env.sh out/python_env \
     --app examples/all-devices-app/posix/out/all-devices-app \
     --app-args "\
         --device contact-sensor:1 \
-        --device contact-sensor:2" \
+        --device water-leak-detector:2" \
     --factory-reset \
     --script examples/all-devices-app/all-devices-common/devices/boolean-state-sensor/test.py \
     --script-args "\
