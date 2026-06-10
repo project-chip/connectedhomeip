97b040c8d1848e80625c7dcb628ba69babb90ed6
Ramiro Recchia <rrecchia@google.com>
2026-06-10 16:05:00 -0300

    Validating None case for stored global wildcard

--- src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
@@ -657,6 +657,10 @@ class MatterBaseTest(base_test.BaseTestClass):
         """
         tls_cluster_id = Clusters.TlsClientManagement.id
         found_any = False
+        if self.stored_global_wildcard is None:
+            LOGGER.info("[CLN] wildcard not available, skipping TLS endpoint cleanup")
+            return
+        
         for endpoint_id, clusters in self.stored_global_wildcard.attributes.items():
             server_list = clusters.get(Clusters.Descriptor, {}).get(Clusters.Descriptor.Attributes.ServerList)
             if server_list is None or tls_cluster_id not in server_list:
