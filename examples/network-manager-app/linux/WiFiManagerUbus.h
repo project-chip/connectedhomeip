76fc841e7b46a607a1d3ae5af1221c6569b88950
Raphaël Mélotte <raphael.melotte@mind.be>
2026-06-16 15:22:10 +0200

    examples: network-manager-app: add ubus WiFi management
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

---
+++ examples/network-manager-app/linux/WiFiManagerUbus.h
@@ -0,0 +1,44 @@
+/*
+ *    Copyright (c) 2026 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include "UbusManager.h"
+#include <app/clusters/wifi-network-management-server/wifi-network-management-server.h>
+
+namespace chip {
+
+class WiFiManagerUbus
+{
+public:
+    WiFiManagerUbus(ubus::UbusManager & ubusManager, app::Clusters::WiFiNetworkManagementServer & server) :
+        mUbusManager(ubusManager), mServer(server)
+    {}
+
+    void Init();
+
+private:
+    void InvokeUciGetWifiIfaces();
+    void OnUciGetResponse(blob_attr * msg);
+
+    ubus::UbusManager & mUbusManager;
+    app::Clusters::WiFiNetworkManagementServer & mServer;
+    ubus::UbusWatch mUci{ "uci", this };
+    ubus::UbusWatch mService{ "service", this };
+};
+
+} // namespace chip
