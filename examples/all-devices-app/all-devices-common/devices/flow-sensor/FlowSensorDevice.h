66029a88e48c38041073ce19e9d27cba4169cd0b
Andrei Litvin <andy314@gmail.com>
2026-06-17 15:36:30 +0000

    Integrate water-freeze-detector test runner and apply styling fixes to sensor headers

--- examples/all-devices-app/all-devices-common/devices/flow-sensor/FlowSensorDevice.h
+++ examples/all-devices-app/all-devices-common/devices/flow-sensor/FlowSensorDevice.h
@@ -14,6 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
 #pragma once
 
 #include <app/clusters/flow-measurement-server/FlowMeasurementCluster.h>
