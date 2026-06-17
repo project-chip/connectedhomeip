c32d53bf564f8b419befdc65cdc79de2508684b8
Jake Ororke <jororke@csa-iot.org>
2026-06-17 15:46:06 -0700

    Reapply "[Add Test Cluster] Adding test hidden manufacturer-specific cluster and attribute for non-controller integrated cluster testing (#42642)" (#72614)
    This reverts commit a3953d81c4f9452fd6aaa3dcc900698e8a8236f9.

--- zzz_generated/app-common/clusters/MetadataQuery.h
+++ zzz_generated/app-common/clusters/MetadataQuery.h
@@ -268,6 +268,8 @@
 #include <clusters/TemperatureControlledCabinetTopology/MetadataProvider.h>
 #include <clusters/TemperatureMeasurement/Ids.h>
 #include <clusters/TemperatureMeasurement/MetadataProvider.h>
+#include <clusters/TestHiddenManufacturerSpecific/Ids.h>
+#include <clusters/TestHiddenManufacturerSpecific/MetadataProvider.h>
 #include <clusters/Thermostat/Ids.h>
 #include <clusters/Thermostat/MetadataProvider.h>
 #include <clusters/ThermostatUserInterfaceConfiguration/Ids.h>
@@ -982,6 +984,11 @@ std::optional<DataModel::AcceptedCommandEntry> AcceptedCommandEntryFor(ClusterId
     {
         if (id == TemperatureMeasurement::Id)
             return ClusterMetadataProvider<DataModel::AcceptedCommandEntry, TemperatureMeasurement::Id>::EntryFor(command);
+    }
+    if constexpr (sizeof...(TClusterIds) == 0 || ((TClusterIds == TestHiddenManufacturerSpecific::Id) || ...))
+    {
+        if (id == TestHiddenManufacturerSpecific::Id)
+            return ClusterMetadataProvider<DataModel::AcceptedCommandEntry, TestHiddenManufacturerSpecific::Id>::EntryFor(command);
     }
     if constexpr (sizeof...(TClusterIds) == 0 || ((TClusterIds == Thermostat::Id) || ...))
     {
