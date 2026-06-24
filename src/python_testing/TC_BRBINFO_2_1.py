f89135c5f27955e65b3dfe77c13952e3377bb68e
Gatien Chapon <gatien.chapon@nxp.com>
2026-06-24 18:41:40 +0200

    Revert "[nxp fromtree][Fix] Update test step numbering for BRBINFO_2_1 test module to match with test plan and makes minor improvement to test step skips (#43756)"
    This reverts commit bc6308376ccdfa456fc66319fb167bd47b49126c.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/python_testing/TC_BRBINFO_2_1.py
+++ src/python_testing/TC_BRBINFO_2_1.py
@@ -37,7 +37,8 @@ from support_modules.binfo_attributes_verification import BasicInformationAttrib
 
 import matter.clusters as Clusters
 from matter.testing.decorators import async_test_body
-from matter.testing.runner import TestStep, default_matter_test_main
+from matter.testing.matter_testing import TestStep
+from matter.testing.runner import default_matter_test_main
 
 
 class TC_BRBINFO_2_1(BasicInformationAttributesVerificationBase):
@@ -45,9 +46,9 @@ class TC_BRBINFO_2_1(BasicInformationAttributesVerificationBase):
         return "[TC-BRBINFO-2.1] Attributes [DUT-Server]"
 
     def steps_TC_BRBINFO_2_1(self) -> list[TestStep]:
-        # BRBINFO test plan omits Basic Information attributes not present on Bridged Device Basic Information:
-        # DataModelRevision (1), Location (7), LocalConfigDisabled (17), CapabilityMinima (20),
-        # SpecificationVersion (22), MaxPathsPerInvoke (23).
+        # BRBINFO doesn't have DataModelRevision, Location, LocalConfigDisabled, CapabilityMinima, SpecificationVersion, MaxPathsPerInvoke
+        # and ConfigurationVersion (29) as mandatory attributes
+        # so skip steps 1, 7, 17, 20, 22, 23
         return self.steps()
 
     def pics_TC_BRBINFO_2_1(self) -> list[str]:
