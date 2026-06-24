33485fc0c833eea07efe678197306da31732fbe4
Gatien Chapon <gatien.chapon@nxp.com>
2026-06-24 18:41:36 +0200

    Reapply "[nxp toup][rw612][keyAllocator] Ignoring PSA_ALG_ECDH since ECDH keys are not supported in S50"
    This reverts commit b90e50064d18ed0d92dc3f27709dc0249ba81540.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/platform/nxp/common/crypto/S50/S50KeyAllocator.h
+++ src/platform/nxp/common/crypto/S50/S50KeyAllocator.h
@@ -36,11 +36,14 @@ public:
                                  PSA_KEY_LIFETIME_PERSISTENT, PSA_CRYPTO_ELS_PKC_LOCATION_S50_RFC3394_STORAGE));
         }
 
-        /* NON_EL2GO storage for CASE Ephemeral Keys and pending operational keys */
+        /* NON_EL2GO storage for CASE Ephemeral Keys and pending operational keys
+        + Ignoring PSA_ALG_ECDH since ECDH keys are not supported in S50 and should not be allocated in this storage.
+        */
         if ((lifetime == PSA_KEY_LIFETIME_VOLATILE) &&
             PSA_KEY_TYPE_IS_ECC_KEY_PAIR(keyType) &&
             (PSA_ECC_FAMILY_SECP_R1 == PSA_KEY_TYPE_ECC_GET_FAMILY(keyType)) &&
-            (psa_get_key_bits(&attrs) == kP256_PrivateKey_Length * 8))
+            (psa_get_key_bits(&attrs) == kP256_PrivateKey_Length * 8) &&
+            (psa_get_key_algorithm(&attrs) != PSA_ALG_ECDH))
         {
             psa_set_key_lifetime(&attrs, PSA_KEY_LIFETIME_FROM_PERSISTENCE_AND_LOCATION(
                 PSA_KEY_LIFETIME_VOLATILE, PSA_CRYPTO_ELS_PKC_LOCATION_S50_RFC3394_STORAGE));
