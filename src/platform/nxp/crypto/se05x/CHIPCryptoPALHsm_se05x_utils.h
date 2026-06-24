707d0913684e37815f507485cd9bd766472977a8
Gatien Chapon <gatien.chapon@nxp.com>
2026-06-24 18:41:38 +0200

    Revert "[nxp noup][SE05x][MATTER-4224]Optimise factory reset to reset only relevant data for nfc-comm"
    This reverts commit 7c5bb9f24557db76277490e8496f1bb1df697291.
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- src/platform/nxp/crypto/se05x/CHIPCryptoPALHsm_se05x_utils.h
+++ src/platform/nxp/crypto/se05x/CHIPCryptoPALHsm_se05x_utils.h
@@ -153,6 +153,16 @@ CHIP_ERROR se05x_set_certificate(uint32_t keyId, const uint8_t * buf, size_t buf
 CHIP_ERROR se05x_set_binary_data(uint32_t keyId, const uint8_t * buf, size_t buflen);
 
 /**
+ * @brief Set EC key in se05x.
+ * The Key is stored with Persistent option.
+ * @param[in] keyid - Key id of the object.
+ * @param[in] buf - Buffer containing Key data.
+ * @param[in] buflen - Buffer length.
+ * @return CHIP_ERROR_INTERNAL on error, CHIP_NO_ERROR otherwise
+ */
+CHIP_ERROR se05x_set_ec_key(uint32_t keyId, const uint8_t * buf, size_t buflen);
+
+/**
  * @brief Perform internal sign in se05x (only on SE051H).
  * Used to Perform ECDSA internal sign
  * ECDSA sign is performed on concatenated data of BinaryFile Secure Object,
