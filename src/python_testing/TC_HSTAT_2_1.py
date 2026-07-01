8575ff12be2c3d6b6d90a13f04e1fc2b3c1f5635
Rob Bultman <Rob.bultman@gmail.com>
2026-07-01 14:42:25 -0400

    Fix tests

--- src/python_testing/TC_HSTAT_2_1.py
+++ src/python_testing/TC_HSTAT_2_1.py
@@ -92,8 +92,8 @@ class TC_HSTAT_2_1(MatterBaseTest):
                 TestStep(next(step), "TH reads from the DUT the MistType attribute.",
                          "Verify that the DUT response contains a value or 0 or 1"),
                 TestStep(next(step), "TH reads from the DUT the Continuous attribute.",
-                         "Verify that the DUT response contains a value or True or False."),
-                TestStep(next(step), "Verify that the DUT response contains a value or True or False",
+                         "Verify that the DUT response contains a Boolean."),
+                TestStep(next(step), "TH reads from the DUT the Sleep attribute.",
                          "Verify that the DUT response contains a value or True or False."),
                 TestStep(next(step), "TH reads from the DUT the Optimal attribute.",
                          "Verify that the DUT response contains a value or True or False"),
@@ -307,17 +307,17 @@ class TC_HSTAT_2_1(MatterBaseTest):
         self.step(next(step))  # Check Continuous attribute
         if supports_continuous:
             dut_Continuous = await self.read_hstat_attribute_expect_success(endpoint=endpoint, attribute=attributes.Continuous)
-            asserts.assert_true(dut_Continuous or not dut_Continuous, "Continuous attribute must be True or False")
+            asserts.assert_true(isintance(dut_Continuous, bool), "Continuous attribute must be a Boolean")
 
         self.step(next(step))  # Check Sleep attribute
         if attributes.Sleep.attribute_id in supported_attributes:
             dut_Sleep = await self.read_hstat_attribute_expect_success(endpoint=endpoint, attribute=attributes.Sleep)
-            asserts.assert_true(dut_Sleep or not dut_Sleep, "Sleep attribute must be True or False")
+            asserts.assert_true(isintance(dut_Sleep, bool), "Sleep attribute must be a Boolean")
 
         self.step(next(step))  # Check Optimal attribute
         if supports_optimal:
             dut_Optimal = await self.read_hstat_attribute_expect_success(endpoint=endpoint, attribute=attributes.Optimal)
-            asserts.assert_true(dut_Optimal or not dut_Optimal, "Optimal attribute must be True or False")
+            asserts.assert_true(isintance(dut_Optimal, bool), "Optimal attribute must be a Boolean")
 
         self.step(next(step))  # Write Mode to Off
         await self.write_single_attribute(attribute_value=attributes.Mode(modeOff), endpoint_id=endpoint)
