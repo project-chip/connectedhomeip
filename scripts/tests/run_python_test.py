d7fc99ea91ceb91f1642f0cffcd630f83a5ce118
Maciej Grela <m.grela@samsung.com>
2026-07-10 13:07:12 +0200

    Warn if script timeout is more than run timeout

--- scripts/tests/run_python_test.py
+++ scripts/tests/run_python_test.py
@@ -186,13 +186,22 @@ class IpPacketCaptureManager:
 
 
 def run_timeout(run: Metadata) -> float:
-    if run.timeout is not None:
-        return run.timeout
+    script_timeout = None
+    
     if run.script_args is not None:
         p = matter_test_args_parser()
         (args, _) = p.parse_known_args(shlex.split(run.script_args))
-        if args.timeout is not None:
-            return args.timeout + TestingDefaults.TEST_RUNNER_SLACK_S
+        script_timeout = args.timeout
+
+    if run.timeout is not None and script_timeout is not None:
+        if run.timeout < script_timeout:
+            log.warning(f"Framework timeout for run '{run.run}' ({run_timeout} s) will expire earlier than script timeout ({script_timeout} s)")
+
+    if run.timeout is not None:
+        return run.timeout
+    if script_timeout is not None:
+        return script_timeout + TestingDefaults.TEST_RUNNER_SLACK_S
+    
     return TestingDefaults.DEFAULT_TIMEOUT_S
 
 
