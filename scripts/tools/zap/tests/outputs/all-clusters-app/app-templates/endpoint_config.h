eb6cd3c2fa2d879e2f290a4c3fd0e121941406ea
AniDashyan <ani.dashyan77@gmail.com>
2026-07-13 15:43:03 +0400

    zap_regen for resolving conflicts in `endpoint_config.h`

--- scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
+++ scripts/tools/zap/tests/outputs/all-clusters-app/app-templates/endpoint_config.h
@@ -4962,7 +4962,7 @@
 #define GENERATED_ENDPOINT_TYPES                                                                                                   \
     {                                                                                                                              \
         { ZAP_CLUSTER_INDEX(0), 28, 168 },                                                                                         \
-        { ZAP_CLUSTER_INDEX(28), 73, 3325 },                                                                                       \
+        { ZAP_CLUSTER_INDEX(28), 73, 3322 },                                                                                       \
         { ZAP_CLUSTER_INDEX(101), 7, 99 },                                                                                         \
         { ZAP_CLUSTER_INDEX(108), 2, 0 },                                                                                          \
     }
@@ -4976,7 +4976,7 @@ static_assert(ATTRIBUTE_LARGEST <= CHIP_CONFIG_MAX_ATTRIBUTE_STORE_ELEMENT_SIZE,
 #define ATTRIBUTE_SINGLETONS_SIZE (0)
 
 // Total size of attribute storage
-#define ATTRIBUTE_MAX_SIZE (3592)
+#define ATTRIBUTE_MAX_SIZE (3589)
 
 // Number of fixed endpoints
 #define FIXED_ENDPOINT_COUNT (4)
