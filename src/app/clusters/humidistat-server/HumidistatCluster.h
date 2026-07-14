fdfdfb8237075f37958efcd821391f0b1db03307
Daniel <daniel.nordahl@inter.ikea.com>
2026-07-14 15:22:48 +0200

    humidistat: Added write attribute support for attributes in HumidistatCluster.

--- src/app/clusters/humidistat-server/HumidistatCluster.h
+++ src/app/clusters/humidistat-server/HumidistatCluster.h
@@ -138,6 +138,9 @@ public:
     DataModel::ActionReturnStatus ReadAttribute(const DataModel::ReadAttributeRequest & request,
                                                 AttributeValueEncoder & encoder) override;
 
+    DataModel::ActionReturnStatus WriteAttribute(const DataModel::WriteAttributeRequest & request,
+                                                 AttributeValueDecoder & decoder) override;
+
     std::optional<DataModel::ActionReturnStatus> InvokeCommand(const DataModel::InvokeRequest & request,
                                                                chip::TLV::TLVReader & input_arguments,
                                                                CommandHandler * handler) override;
