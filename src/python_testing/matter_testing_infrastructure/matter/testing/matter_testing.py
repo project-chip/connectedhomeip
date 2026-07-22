8052c7f38b59493ab6d94d1d3dc9bd66a8d42b74
Raul Marquez <rmarquez@csa-iot.org>
2026-07-22 13:28:36 -0700

    mypy

--- src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
@@ -107,8 +107,12 @@ def compute_mrp_retransmission_timeout_sec(dev_ctrl: ChipDeviceCtrl.ChipDeviceCo
     session_params = dev_ctrl.GetRemoteSessionParameters(node_id)
     # Default local MRP intervals from ReliableMessageProtocolConfig.h for Linux controller builds:
     # idle=500ms, active=300ms.
-    negotiated_idle_interval_ms = session_params.sessionIdleInterval if session_params else 500
-    negotiated_active_interval_ms = session_params.sessionActiveInterval if session_params else 300
+    negotiated_idle_interval_ms = session_params.sessionIdleInterval if session_params else None
+    negotiated_active_interval_ms = session_params.sessionActiveInterval if session_params else None
+    if negotiated_idle_interval_ms is None:
+        negotiated_idle_interval_ms = 500
+    if negotiated_active_interval_ms is None:
+        negotiated_active_interval_ms = 300
 
     # Defaults: 500ms idle (IP) / 2000ms (Thread) / 4000ms (fallback).
     base_interval_ms = max(negotiated_idle_interval_ms, negotiated_active_interval_ms, 4000)
