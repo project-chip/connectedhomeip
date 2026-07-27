fd3ce4fc06c9e6c6b3166eb8a33379f77e8d37d0
LyudmilaKostanyan <lyudakstnn@gmail.com>
2026-07-27 18:33:52 +0400

    Add dependences to cmake

--- examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
+++ examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
@@ -114,6 +114,8 @@ set(ALL_DEVICES_DEVICE_SOURCES
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/on-off-load/impl/LoggingOnOffLoad.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/operational-state/impl/LoggingOperationalStateDelegate.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/operational-state/impl/LoggingRvcOperationalStateDelegate.cpp"
+    "${ALL_DEVICES_COMMON_DIR}/device/capabilities/service-area/impl/LoggingServiceAreaDelegate.cpp"
+    "${ALL_DEVICES_COMMON_DIR}/device/capabilities/service-area/impl/LoggingServiceAreaStorageDelegate.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/api/allocator/DynamicEndpointIdAllocator.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/api/Interface.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/api/SingleEndpoint.cpp"
