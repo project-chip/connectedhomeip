c13b9458aabca962cdb98f6aaf497f9995902c71
Olivier LORENTE <olivier.lorente@st.com>
2026-07-27 14:32:19 +0200

    Fix from Copilot

--- src/controller/java/CHIPDeviceController-JNI.cpp
+++ src/controller/java/CHIPDeviceController-JNI.cpp
@@ -1081,7 +1081,10 @@ exit:
     if (err != CHIP_NO_ERROR)
     {
         ChipLogError(Controller, "Failed to set WiFiCredentialsNeeded Listener. : %" CHIP_ERROR_FORMAT, err.Format());
-        JniReferences::GetInstance().ThrowError(env, sChipDeviceControllerExceptionCls, err);
+        if (err != CHIP_JNI_ERROR_EXCEPTION_THROWN)
+        {
+            JniReferences::GetInstance().ThrowError(env, sChipDeviceControllerExceptionCls, err);
+        }
     }
 }
 
