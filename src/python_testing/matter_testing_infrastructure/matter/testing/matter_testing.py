e225a9931614df9e57fab582b8726a41038f213e
Olivier LORENTE [ST] <34505085+OlivierGre@users.noreply.github.com>
2026-07-28 13:19:27 +0200

    Apply suggestion from @OlivierGre

--- src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/matter_testing.py
@@ -2155,10 +2155,6 @@ class MatterBaseTest(base_test.BaseTestClass):
             self._dut_confirmed_available = True
         return result
 
-
-<< << << < HEAD
-    async def open_commissioning_window(self, dev_ctrl: Optional[ChipDeviceCtrl.ChipDeviceController] = None, node_id: Optional[int] = None, timeout: int = 900) -> CustomCommissioningParameters:
-== == == =
     async def commission_ntl_device(self, setup_payload: SetupPayload) -> bool:
         """Commission a single DUT devices over NTL.
         The discovery_cap_bitmask is patched to keep only the NTL bit ON.
