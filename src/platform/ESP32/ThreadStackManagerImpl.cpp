db947a167a4e9e13313f5b875e9244580da61c3e
liyashuai <liyashuai@espressif.com>
2026-07-31 09:32:51 +0800

    [v1.5][backport][ESP32]remove duplicate Thread stack lock

--- src/platform/ESP32/ThreadStackManagerImpl.cpp
+++ src/platform/ESP32/ThreadStackManagerImpl.cpp
@@ -54,9 +54,7 @@ CHIP_ERROR ThreadStackManagerImpl::_InitThreadStack()
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
     openthread_init_stack();
-    _LockThreadStack();
     err = GenericThreadStackManagerImpl_OpenThread<ThreadStackManagerImpl>::DoInit(esp_openthread_get_instance());
-    _UnlockThreadStack();
     return err;
 }
 
