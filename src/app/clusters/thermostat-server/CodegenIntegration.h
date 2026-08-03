00425e3c35ae39b393c32ca0c58090a04bd6839f
Hasty Granbery <hasty@granbery.org>
2026-08-03 08:56:02 -0700

    Remove dynamic_cast requirement

--- src/app/clusters/thermostat-server/CodegenIntegration.h
+++ src/app/clusters/thermostat-server/CodegenIntegration.h
@@ -85,7 +85,8 @@ public:
         return nullptr;
     }
 
-    Protocols::InteractionModel::Status SetDelegate(EndpointId endpoint, Thermostat::Delegate * delegate)
+    template <typename DelegateT>
+    Protocols::InteractionModel::Status SetDelegate(EndpointId endpoint, DelegateT * delegate)
     {
         Cluster * cluster = FindClusterOnEndpoint(endpoint);
         if (cluster == nullptr)
@@ -94,30 +95,7 @@ public:
             return Protocols::InteractionModel::Status::Failure;
         }
         cluster->SetDelegate(delegate);
-        return Protocols::InteractionModel::Status::Success;
     }
-
-   /* void ServerInit(EndpointId endpointId)
-    {
-        for (auto & cluster : mClusters)
-        {
-            if (cluster.IsConstructed() && cluster.Cluster().GetPaths()[0].mEndpointId == endpointId)
-            {
-                cluster.Cluster().ServerInit();
-            }
-        }
-    }
-
-    void ServerShutdown(EndpointId endpointId, MatterClusterShutdownType clusterShutdownType)
-    {
-        for (auto & cluster : mClusters)
-        {
-            if (cluster.IsConstructed() && cluster.Cluster().GetPaths()[0].mEndpointId == endpointId)
-            {
-                cluster.Cluster().ServerShutdown(clusterShutdownType);
-            }
-        }
-    }*/
 };
 
 template <typename Cluster>
@@ -159,21 +137,11 @@ ThermostatCluster * FindClusterOnEndpoint(EndpointId endpointId)
     return integrationDelegate.FindClusterOnEndpoint(endpointId);
 }
 
-inline ThermostatCluster * FindClusterOnEndpoint(EndpointId endpointId)
-{
-    return FindClusterOnEndpoint<DefaultThermostatCluster>(endpointId);
-}
-
-template <typename Cluster>
-Protocols::InteractionModel::Status SetDefaultDelegate(EndpointId endpoint, Delegate * delegate)
+template <typename Cluster, typename DelegateT>
+Protocols::InteractionModel::Status SetDefaultDelegate(EndpointId endpoint, DelegateT * delegate)
 {
     IntegrationDelegate<kThermostatEndpointCount, Cluster> integrationDelegate;
     return integrationDelegate.SetDelegate(endpoint, delegate);
-}
-
-inline Protocols::InteractionModel::Status SetDefaultDelegate(EndpointId endpoint, Delegate * delegate)
-{
-    return SetDefaultDelegate<DefaultThermostatCluster>(endpoint, delegate);
 }
 
 } // namespace chip::app::Clusters::Thermostat
