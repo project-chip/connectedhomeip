d106c33108b06e1c2f8ed0a0b978035705b64ef3
Moises Terrones <moises.tersal@gmail.com>
2026-08-03 19:57:58 -0600

    Remove unused files

--- src/app/clusters/laundry-dryer-controls-server/laundry-dryer-controls-server.h
+++ src/app/clusters/laundry-dryer-controls-server/laundry-dryer-controls-server.h
@@ -1,6 +1,5 @@
-/**
- *
- *    Copyright (c) 2023 Project CHIP Authors
+/*
+ *    Copyright (c) 2023-2026 Project CHIP Authors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,47 +16,4 @@
 
 #pragma once
 
-#include "laundry-dryer-controls-delegate.h"
-#include <app-common/zap-generated/cluster-objects.h>
-#include <app/AttributeAccessInterface.h>
-#include <protocols/interaction_model/StatusCode.h>
-
-namespace chip {
-namespace app {
-namespace Clusters {
-namespace LaundryDryerControls {
-
-/**
- * @brief LaundryDryerControls Server Plugin class
- */
-class LaundryDryerControlsServer : public AttributeAccessInterface
-{
-public:
-    LaundryDryerControlsServer() : AttributeAccessInterface(Optional<EndpointId>::Missing(), LaundryDryerControls::Id) {}
-    static LaundryDryerControlsServer & Instance();
-
-    /**
-     * Set the default delegate of laundry dryer server at endpoint x
-     * @param endpoint ID of the endpoint
-     * @param delegate The default delegate at the endpoint
-     */
-    static void SetDefaultDelegate(EndpointId endpoint, Delegate * delegate);
-
-    /**
-     * API to set/get the SelectedDrynessLevel attribute
-     */
-    Protocols::InteractionModel::Status SetSelectedDrynessLevel(EndpointId endpointId, DrynessLevelEnum newSelectedDrynessLevel);
-    Protocols::InteractionModel::Status GetSelectedDrynessLevel(EndpointId endpointId,
-                                                                DataModel::Nullable<DrynessLevelEnum> & selectedDrynessLevel);
-
-private:
-    CHIP_ERROR Read(const ConcreteReadAttributePath & aPath, AttributeValueEncoder & aEncoder) override;
-    CHIP_ERROR ReadSupportedDrynessLevels(const ConcreteReadAttributePath & aPath, AttributeValueEncoder & aEncoder);
-
-    static LaundryDryerControlsServer sInstance;
-};
-
-} // namespace LaundryDryerControls
-} // namespace Clusters
-} // namespace app
-} // namespace chip
+#include <app/clusters/laundry-dryer-controls-server/CodegenIntegration.h>
