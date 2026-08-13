06fa1fae4a6097b1d7299022ca7c76e3e7e218a5
Andrei Litvin <andy314@gmail.com>
2026-08-13 19:25:39 +0000

    python_testing: revert test logic modification in TC_MWOCTRL_2_4.py

--- src/python_testing/TC_MWOCTRL_2_4.py
+++ src/python_testing/TC_MWOCTRL_2_4.py
@@ -128,10 +128,7 @@ class TC_MWOCTRL_2_4(MatterBaseTest):
             supportedWattsList), "SelectedWattIndex is out of range")
 
         self.step(4)
-        if len(supportedWattsList) > 1:
-            newWattIndex = (selectedWattIndex + 1) % len(supportedWattsList)
-        else:
-            newWattIndex = selectedWattIndex
+        newWattIndex = (selectedWattIndex + 1) % (len(supportedWattsList) - 1)
         try:
             await self.send_single_cmd(cmd=commands.SetCookingParameters(wattSettingIndex=newWattIndex), endpoint=endpoint)
         except InteractionModelError as e:
