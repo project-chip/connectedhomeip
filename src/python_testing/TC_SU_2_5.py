34c2fe9d288a8e4b07452dfe5bdba4d59450ab95
jtrejoespinoza-grid <juanerasmo@google.com>
2026-08-14 11:59:09 -0700

    Moved TC_SUTestBase to support module. References updated.

--- src/python_testing/TC_SU_2_5.py
+++ src/python_testing/TC_SU_2_5.py
@@ -53,7 +53,7 @@ import asyncio
 import logging
 
 from mobly import asserts
-from TC_SUTestBase import SoftwareUpdateBaseTest
+from support_modules.su_support import SoftwareUpdateBaseTest
 
 import matter.clusters as Clusters
 from matter import ChipDeviceCtrl
