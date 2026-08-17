04631124df16751e64961269a22dc6fb685f6750
Rob Bultman <Rob.bultman@gmail.com>
2026-08-17 09:29:09 -0400

    Add missing import

--- src/python_testing/TC_TSTATM_3_2.py
+++ src/python_testing/TC_TSTATM_3_2.py
@@ -36,7 +36,7 @@
 
 from mobly import asserts
 import matter.clusters as Clusters
-from matter.interaction_model import Status
+from matter.interaction_model import InteractionModelError, Status
 from matter.testing.decorators import async_test_body
 from matter.testing.matter_testing import MatterBaseTest
 from matter.testing.matter_testing import matter_asserts
