cf669ba6ccbda1925d19b0f5ac92444372d17320
Andrei Litvin <andy314@gmail.com>
2026-08-18 13:44:37 +0000

    [all-devices-app] Refactor EmulatedLaundryWasher to a single unified device class

--- examples/all-devices-app/all-devices-common/device/types/laundry-washer/impl/EmulatedLaundryWasher.h
+++ examples/all-devices-app/all-devices-common/device/types/laundry-washer/impl/EmulatedLaundryWasher.h
@@ -16,13 +16,16 @@
 
 #pragma once
 
-#include <device/capabilities/operational-state/impl/EmulatedOperationalStateDelegate.h>
 #include <device/types/laundry-washer/LaundryWasher.h>
 #include <lib/support/TimerDelegate.h>
 
 namespace chip::app {
 
-class EmulatedLaundryWasher : public LaundryWasher
+class EmulatedLaundryWasher : public LaundryWasher,
+                              public Clusters::OperationalState::Delegate,
+                              public Clusters::LaundryWasherControls::Delegate,
+                              public Clusters::ModeBase::AppDelegate,
+                              public TimerContext
 {
 public:
     struct Context
@@ -31,35 +34,56 @@ public:
         DeviceLayer::DiagnosticDataProvider & diagnosticDataProvider;
     };
 
-    explicit EmulatedLaundryWasher(const Context & context) :
-        LaundryWasher(LaundryWasher::Config{
-            .operationalStateDelegate = mOpStateDelegate,
-            .controlsDelegate         = mControlsDelegate,
-            .modeDelegate             = mModeDelegate,
-            .diagnosticDataProvider   = context.diagnosticDataProvider,
-        }),
-        mOpStateDelegate(context.timerDelegate)
-    {}
+    explicit EmulatedLaundryWasher(const Context & context);
+    ~EmulatedLaundryWasher() override;
 
-    ~EmulatedLaundryWasher() override = default;
+    void Unregister(CodeDrivenDataModelProvider & provider) override;
 
-    CHIP_ERROR Register(EndpointId endpoint, CodeDrivenDataModelProvider & provider, EndpointComposition composition = {}) override
-    {
-        ReturnErrorOnFailure(LaundryWasher::Register(endpoint, provider, composition));
-        mOpStateDelegate.SetCluster(&OperationalState());
-        return CHIP_NO_ERROR;
-    }
+    // -- TimerContext Interface --
+    void TimerFired() override;
 
-    void Unregister(CodeDrivenDataModelProvider & provider) override
-    {
-        mOpStateDelegate.SetCluster(nullptr);
-        LaundryWasher::Unregister(provider);
-    }
+    // -- OperationalState::Delegate Interface --
+    DataModel::Nullable<uint32_t> GetCountdownTime() override { return mCountdownTime; }
+    CHIP_ERROR GetOperationalStateAtIndex(size_t index, Clusters::OperationalState::GenericOperationalState & operationalState) override;
+    CHIP_ERROR GetOperationalPhaseAtIndex(size_t index, MutableCharSpan & operationalPhase) override;
+    void HandlePauseStateCallback(Clusters::OperationalState::GenericOperationalError & err) override;
+    void HandleResumeStateCallback(Clusters::OperationalState::GenericOperationalError & err) override;
+    void HandleStartStateCallback(Clusters::OperationalState::GenericOperationalError & err) override;
+    void HandleStopStateCallback(Clusters::OperationalState::GenericOperationalError & err) override;
+
+    // -- LaundryWasherControls::Delegate Interface --
+    CHIP_ERROR GetSpinSpeedAtIndex(size_t index, MutableCharSpan & spinSpeed) override;
+    CHIP_ERROR GetSupportedRinseAtIndex(size_t index, Clusters::LaundryWasherControls::NumberOfRinsesEnum & supportedRinse) override;
+
+    // -- ModeBase::AppDelegate Interface --
+    CHIP_ERROR Init() override { return CHIP_NO_ERROR; }
+    CHIP_ERROR GetModeLabelByIndex(uint8_t modeIndex, MutableCharSpan & label) override;
+    CHIP_ERROR GetModeValueByIndex(uint8_t modeIndex, uint8_t & value) override;
+    CHIP_ERROR GetModeTagsByIndex(uint8_t modeIndex,
+                                  DataModel::List<Clusters::detail::Structs::ModeTagStruct::Type> & modeTags) override;
+    void HandleChangeToMode(uint8_t newMode, Clusters::ModeBase::Commands::ChangeToModeResponse::Type & response) override;
 
 private:
-    Clusters::OperationalState::EmulatedOperationalStateDelegate mOpStateDelegate;
-    EmulatedLaundryWasherControlsDelegate mControlsDelegate;
-    LaundryWasherModeDelegate mModeDelegate;
+    static constexpr uint32_t kEmulatedOperationDurationSec = 30;
+
+    static constexpr CharSpan kSpinSpeeds[] = { "Off"_span, "Low"_span, "Medium"_span, "High"_span };
+    static constexpr Clusters::LaundryWasherControls::NumberOfRinsesEnum kRinses[] = {
+        Clusters::LaundryWasherControls::NumberOfRinsesEnum::kNone,
+        Clusters::LaundryWasherControls::NumberOfRinsesEnum::kNormal,
+        Clusters::LaundryWasherControls::NumberOfRinsesEnum::kExtra
+    };
+
+    static constexpr CharSpan kModeLabels[]    = { "Normal"_span, "Delicate"_span, "Heavy"_span };
+    static constexpr uint16_t kModeTagValues[] = { to_underlying(Clusters::LaundryWasherMode::ModeTag::kNormal),
+                                                   to_underlying(Clusters::LaundryWasherMode::ModeTag::kDelicate),
+                                                   to_underlying(Clusters::LaundryWasherMode::ModeTag::kHeavy) };
+
+    void CancelTimer();
+    void StartEmulatedOperationTimer();
+
+    TimerDelegate & mTimerDelegate;
+    Clusters::OperationalState::OperationalStateEnum mOperationalState = Clusters::OperationalState::OperationalStateEnum::kStopped;
+    DataModel::Nullable<uint32_t> mCountdownTime;
 };
 
 } // namespace chip::app
