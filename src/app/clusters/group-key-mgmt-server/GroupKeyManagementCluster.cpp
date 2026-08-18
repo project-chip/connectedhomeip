9c45d5f5672facaedcba303db01785d313009505
Zaid Omer <zaidomer@google.com>
2026-08-18 16:08:40 -0400

    Dont set GCAST feature as it is Provisional (#73624)

--- src/app/clusters/group-key-mgmt-server/GroupKeyManagementCluster.cpp
+++ src/app/clusters/group-key-mgmt-server/GroupKeyManagementCluster.cpp
@@ -647,10 +647,6 @@ DataModel::ActionReturnStatus GroupKeyManagementCluster::ReadAttribute(const Dat
         return encoder.Encode(kRevision);
     case Attributes::FeatureMap::Id: {
         BitFlags<GroupKeyManagement::Feature> features;
-        if (mContext.groupDataProvider.IsGroupcastEnabled())
-        {
-            features.Set(Clusters::GroupKeyManagement::Feature::kGroupcast);
-        }
         if (IsMCSPSupported())
         {
             features.Set(Clusters::GroupKeyManagement::Feature::kCacheAndSync);
