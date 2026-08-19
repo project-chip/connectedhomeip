b6c9eb8ac88c068c6ada40e57fd4f2020edc834b
Ramiro Recchia <rrecchia@google.com>
2026-08-19 16:00:53 -0300

    Remove unused variable

--- src/python_testing/matter_testing_infrastructure/matter/testing/runner.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/runner.py
@@ -455,8 +455,6 @@ def run_tests_no_exit(
         runner = TestRunner(log_dir=test_config.log_path,
                             testbed_name=test_config.testbed_name)
 
-        stored_global_wildcard = None
-
         with runner.mobly_logger():
             if matter_test_config.commissioning_method is not None:
                 # Populate XML data model
