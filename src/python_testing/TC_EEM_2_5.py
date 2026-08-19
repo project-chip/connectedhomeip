10be37713fc672ad4bf2b7782d18de32fc5ee158
Hayk Khachatryan <69189182+Hayk10002@users.noreply.github.com>
2026-08-19 16:54:59 +0400

    test fix

--- src/python_testing/TC_EEM_2_5.py
+++ src/python_testing/TC_EEM_2_5.py
@@ -44,7 +44,6 @@
 #       --discriminator 1234
 #       --KVS kvs1
 #       --trace-to json:${TRACE_APP}.json
-#       --enable-key 000102030405060708090a0b0c0d0e0f
 #     script-args: >
 #       --storage-path admin_storage.json
 #       --commissioning-method on-network
