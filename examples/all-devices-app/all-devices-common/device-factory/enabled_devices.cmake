2ee92453d247f1879805cb4e2b5af4dd6e3bc5de
Hayk10002 <hayk.g.khachatryan@gmail.com>
2026-08-21 16:40:16 +0000

    fix cmake

--- examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
+++ examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
@@ -56,6 +56,7 @@ set(ALL_DEVICES_DEVICE_SOURCES
     "${ALL_DEVICES_COMMON_DIR}/device/types/dishwasher/impl/EmulatedDishwasher.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/types/extractor-hood/ExtractorHood.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/types/electrical-sensor/ElectricalSensor.cpp"
+    "${ALL_DEVICES_COMMON_DIR}/device/types/electrical-sensor/impl/LoggingElectricalSensor.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/types/fan/Fan.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/types/fan/impl/LoggingFan.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/types/flow-sensor/FlowSensor.cpp"
