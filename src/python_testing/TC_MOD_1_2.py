8177d892bfa5da0c1191362f108170cd31f9b9c8
Elen Soghomonyan <43118262+Elen777300@users.noreply.github.com>
2026-08-28 22:38:15 +0400

    Update src/python_testing/TC_MOD_1_2.py
    Co-authored-by: Juan Erasmo Trejo <jtrejoespinoza@griddynamics.com>

--- src/python_testing/TC_MOD_1_2.py
+++ src/python_testing/TC_MOD_1_2.py
@@ -158,7 +158,7 @@ class TC_MOD_1_2(MatterBaseTest):
         if await self.attribute_guard(endpoint=self.endpoint, attribute=self.cluster.Attributes.StartUpMode):
             startup_mode = await self.read_single_attribute_check_success(endpoint=self.endpoint, cluster=self.cluster, attribute=self.cluster.Attributes.StartUpMode)
             self._log_attribute("StartupMode", startup_mode)
-            asserts.assert_true((isinstance(startup_mode, int) or startup_mode is NullValue),
+            asserts.assert_true((matter_asserts.is_valid_uint_value(startup_mode, 8) or startup_mode is NullValue,"Value for StartupMode is not uint8 or Null"),
                                 "StartupMode is not int or NullValue")
             if startup_mode is not NullValue:
                 asserts.assert_in(startup_mode, supported_modes_values,
