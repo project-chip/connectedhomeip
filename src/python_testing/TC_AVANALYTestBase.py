58b36a00ad107d089276563a78b240a76a78dc47
marktrayer <m.trayer@samsung.com>
2026-08-31 10:51:18 -0500

    [AvAnalysis] Test scripts for AVANALY 2.1 and 2.2 (#73776)
    * Test scripts for AVANLY 2.1 and 2.2
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Exclude the helper class from CI
    
    * Address Code Comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address code comments, fix bad cut-n-paste
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address code comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address code comment re: finally block
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit 82839ba387034725bacd2093b0a5f867e60f6ba3)

---
+++ src/python_testing/TC_AVANALYTestBase.py
@@ -0,0 +1,48 @@
+#
+#    Copyright (c) 2025 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from mobly import asserts
+
+import matter.clusters as Clusters
+
+
+class AVANALYTestBase:
+    SPEC_MAX_COUNT_SUPPORTEDAMBIENTCONTEXTS = 50
+    SPEC_MAX_COUNT_ANALYSIS_STREAMS = 255
+
+    has_feature_lclcondetect = False
+    has_feature_remcondetect = False
+    has_feature_perzonedetect = False
+
+    async def read_avanaly_attribute_expect_success(self, endpoint, attribute):
+        """Read the provided attribute from the cluster instance on the provided endpoint.
+
+        Args:
+            attribute: The attribute to be read, must be a valid AvAnalysis cluster attribute
+        """
+        cluster = Clusters.Objects.AvAnalysis
+        return await self.read_single_attribute_check_success(endpoint=endpoint, cluster=cluster, attribute=attribute)
+
+    async def check_avanaly_attribute(self, attribute, expected_value, endpoint):
+        """Validate that the current instance of an attribute has the value provided
+
+        Args:
+            attribute:      The attribute to be read and validated, must be a valid AvAnalysis cluster attribute.
+            expected_value: The value to be matched, assert failure if this is not equal to the current attribute value
+        """
+        value = await self.read_avanaly_attribute_expect_success(endpoint=endpoint, attribute=attribute)
+        asserts.assert_equal(value, expected_value,
+                             f"Unexpected '{attribute}' value - expected {expected_value}, was {value}")
---
+++ src/python_testing/TC_AVANALY_2_2.py
@@ -0,0 +1,100 @@
+#
+#    Copyright (c) 2025 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# See https://github.com/project-chip/connectedhomeip/blob/master/docs/testing/python.md#defining-the-ci-test-arguments
+# for details about the block below.
+#
+# === BEGIN CI TEST ARGUMENTS ===
+# test-runner-runs:
+#   run1:
+#     app: ${CAMERA_APP}
+#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json
+#     script-args: >
+#       --storage-path admin_storage.json
+#       --commissioning-method on-network
+#       --discriminator 1234
+#       --passcode 20202021
+#       --PICS src/app/tests/suites/certification/ci-pics-values
+#       --bool-arg PIXIT.CANBEMADEBUSY:False
+#       --trace-to json:${TRACE_TEST_JSON}.json
+#       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
+#       --endpoint 1
+#     factory-reset: true
+#     quiet: true
+# === END CI TEST ARGUMENTS ===
+
+
+from mobly import asserts
+from TC_AVANALYTestBase import AVANALYTestBase
+
+import matter.clusters as Clusters
+from matter.interaction_model import Status
+from matter.testing.decorators import has_cluster, run_if_endpoint_matches
+from matter.testing.matter_testing import MatterBaseTest
+from matter.testing.runner import TestStep, default_matter_test_main
+
+
+class TC_AVANALY_2_2(MatterBaseTest, AVANALYTestBase):
+
+    def desc_TC_AVANALY_2_2(self) -> str:
+        return "[TC-AVANALY-2.2] Validate writeable attributes with Server as DUT"
+
+    def steps_TC_AVANALY_2_2(self) -> list[TestStep]:
+        return [
+            TestStep(1, "Commissioning, already done", is_commissioning=True),
+            TestStep(2, "TH reads the TrackingEnabled attribute. Verify that it is a bool with a value of false. Store in aTrackingEnabled"),
+            TestStep(3, "TH writes to TrackingEnabled the value of !aTrackingEnabled. Verify success response."),
+            TestStep(4, "TH reads the TrackingEnabled attribute. Verify that it is set to !aTrackingEnabled,",
+                        "then reset the TrackingEnabled attribute to its original value.")
+        ]
+
+    def pics_TC_AVANALY_2_2(self) -> list[str]:
+        return [
+            "AVANALY.S",
+        ]
+
+    @run_if_endpoint_matches(has_cluster(Clusters.AvAnalysis))
+    async def test_TC_AVANALY_2_2(self):
+        cluster = Clusters.Objects.AvAnalysis
+        attributes = cluster.Attributes
+        endpoint = self.get_endpoint()
+
+        self.step(1)  # Already done, immediately go to step 2
+
+        self.step(2)
+        tracking_enabled_dut = await self.read_avanaly_attribute_expect_success(endpoint, attributes.TrackingEnabled)
+        asserts.assert_false(tracking_enabled_dut, "TrackingEnabled should be false on startup")
+
+        # Wrap in try .. finally to make sure that even if there is an error, we revert the attribute value
+        try:
+            self.step(3)
+            result = await self.write_single_attribute(attributes.TrackingEnabled(not tracking_enabled_dut),
+                                                       endpoint_id=endpoint)
+            asserts.assert_equal(result, Status.Success, "Error when trying to write TrackingEnabled")
+
+            self.step(4)
+            tracking_enabled_dut_new = await self.read_avanaly_attribute_expect_success(endpoint, attributes.TrackingEnabled)
+
+            asserts.assert_equal(tracking_enabled_dut_new, not tracking_enabled_dut,
+                                 "Value does not match what was written in step 3")
+
+        finally:
+            result = await self.write_single_attribute(attributes.TrackingEnabled(tracking_enabled_dut),
+                                                       endpoint_id=endpoint)
+
+
+if __name__ == "__main__":
+    default_matter_test_main()
