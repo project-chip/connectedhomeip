2aaa3f57c260168afe4b8bf2e13fd585b0b8cfb5
Jean-Francois Penven <jean-francois.penven@silabs.com>
2026-09-03 19:46:40 -0400

    fix ci and ai comments

--- examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
+++ examples/all-devices-app/all-devices-common/device-factory/enabled_devices.cmake
@@ -113,6 +113,7 @@ set(ALL_DEVICES_DEVICE_SOURCES
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/dimmable-load/impl/LoggingDimmableLoad.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/fan-load/FanLoad.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/fan-load/impl/LoggingFanLoad.cpp"
+    "${ALL_DEVICES_COMMON_DIR}/device/capabilities/identify/LoggingIdentifyDelegate.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/on-off-load/OnOffLoad.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/on-off-load/impl/LoggingOnOffLoad.cpp"
     "${ALL_DEVICES_COMMON_DIR}/device/capabilities/operational-state/impl/LoggingOperationalStateDelegate.cpp"
--- examples/all-devices-app/all-devices-common/device/capabilities/on-off-load/impl/LoggingOnOffLoad.cpp
+++ examples/all-devices-app/all-devices-common/device/capabilities/on-off-load/impl/LoggingOnOffLoad.cpp
@@ -30,7 +30,7 @@ LoggingOnOffLoad::LoggingOnOffLoad(Span<const DataModel::DeviceTypeEntry> device
                                    Clusters::OnOffDelegate * customOnOff, Clusters::OnOffEffectDelegate * customEffect,
                                    Clusters::IdentifyDelegate * customIdentify) :
     OnOffLoad(deviceTypes, customOnOff ? *customOnOff : static_cast<Clusters::OnOffDelegate &>(*this),
-              customEffect ? *customEffect : static_cast<Clusters::OnOffEffectDelegate &>(*this), context.identifyDelegate, context)
+              customEffect ? *customEffect : static_cast<Clusters::OnOffEffectDelegate &>(*this), customIdentify ? *customIdentify : context.identifyDelegate, context)
 {}
 
 // OnOffDelegate
