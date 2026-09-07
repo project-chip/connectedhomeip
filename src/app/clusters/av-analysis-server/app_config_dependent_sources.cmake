fba334ff14fc916ef36d88703c8f71f2c8d69a2c
sayondeep <sayondeep0710@rediffmail.com>
2026-09-07 14:18:50 +0530

    [AVAnalysis] Sort the WebRTC client header in the build listings

--- src/app/clusters/av-analysis-server/app_config_dependent_sources.cmake
+++ src/app/clusters/av-analysis-server/app_config_dependent_sources.cmake
@@ -24,7 +24,6 @@ TARGET_SOURCES(
   ${APP_TARGET}
   PRIVATE
     "${CLUSTER_DIR}/AvAnalysisCameraClient.h"
-    "${CLUSTER_DIR}/AvAnalysisWebRTCClient.h"
     "${CLUSTER_DIR}/AvAnalysisCameraInteraction.h"
     "${CLUSTER_DIR}/AvAnalysisCluster.cpp"
     "${CLUSTER_DIR}/AvAnalysisCluster.h"
@@ -32,6 +31,7 @@ TARGET_SOURCES(
     "${CLUSTER_DIR}/AvAnalysisLogic.h"
     "${CLUSTER_DIR}/AvAnalysisStorage.h"
     "${CLUSTER_DIR}/AvAnalysisStreamTable.h"
+    "${CLUSTER_DIR}/AvAnalysisWebRTCClient.h"
     "${CLUSTER_DIR}/DefaultAvAnalysisCameraClient.cpp"
     "${CLUSTER_DIR}/DefaultAvAnalysisCameraClient.h"
     "${CLUSTER_DIR}/DefaultAvAnalysisWebRTCClient.cpp"
