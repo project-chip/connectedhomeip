68477a7c1e550afc577322990311f7dba695016e
Rob Bultman <Rob.bultman@gmail.com>
2026-09-08 13:47:00 -0400

    Add test for water tank level and media

--- src/python_testing/matter_testing_infrastructure/matter/testing/spec_parsing.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/spec_parsing.py
@@ -390,6 +390,7 @@ ALIAS_PICS = {0x040C: 'CMOCONC',
               0x042F: 'RNCONC',
               0x0071: 'HEPAFREMON',
               0x0072: 'ACFREMON',
+              0x0079: 'WTLREPMON',
               0x0405: 'RH',
               0x001C: 'PWM'}
 
