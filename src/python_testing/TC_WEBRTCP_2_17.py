d902fd51dc75b7a56d5e056f0257d7353f022c99
m.trayer <m.trayer@samsung.com>
2026-09-09 17:15:46 -0500

    Set received SDP as the remote on the webrtc peer

--- src/python_testing/TC_WEBRTCP_2_17.py
+++ src/python_testing/TC_WEBRTCP_2_17.py
@@ -174,6 +174,9 @@ class TC_WEBRTCP_2_17(MatterBaseTest, WEBRTCPTestBase):
 
         log.info("SDP answer validated successfully. Answer length: %s bytes", len(answer_sdp))
         log.info("SDP answer preview: %s...", answer_sdp[:400])
+        
+        # SDP valid, set it as the remote
+        webrtc_peer.set_remote_answer(answer_sdp)
 
         self.step(5)
         # Send EndSession command to terminate the WebRTC session
