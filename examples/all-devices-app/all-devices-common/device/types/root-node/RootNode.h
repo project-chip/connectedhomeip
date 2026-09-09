6ba93709d2e3cc5fee0e5b3e32a9d41ba71e027f
Matthew Albert <matthew.albert@silabs.com>
2026-09-09 10:05:34 -0400

    [all-device-app] Add OTA Requestor device type to RootNode (#73879)
    * Add OTA to RootNode
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * github review
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit d07927ef9feed07d37758a8a7b85c8f0af2d5c5c)

--- examples/all-devices-app/all-devices-common/device/types/root-node/RootNode.h
+++ examples/all-devices-app/all-devices-common/device/types/root-node/RootNode.h
@@ -67,9 +67,7 @@ public:
 #endif // CHIP_CONFIG_TERMS_AND_CONDITIONS_REQUIRED
     };
 
-    RootNode(const Context & context) :
-        SingleEndpoint(Span<const DataModel::DeviceTypeEntry>(&Device::Type::kRootNode, 1)), mContext(context)
-    {}
+    RootNode(const Context & context) : SingleEndpoint(Span<const DataModel::DeviceTypeEntry>(kDeviceTypes)), mContext(context) {}
     ~RootNode() override = default;
 
     CHIP_ERROR Register(EndpointId endpoint, CodeDrivenDataModelProvider & provider, EndpointComposition composition = {}) override;
@@ -77,12 +75,22 @@ public:
 
     Clusters::BasicInformationCluster & BasicInformation() { return mBasicInformationCluster.Cluster(); }
 
+#if CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
+    CHIP_ERROR ClientClusters(ReadOnlyBufferBuilder<ClusterId> & out) const override;
+#endif // CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
+
 protected:
     Context mContext;
 
     LazyRegisteredServerCluster<Clusters::GeneralCommissioningCluster> mGeneralCommissioningCluster;
 
 private:
+#if CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
+    static constexpr DataModel::DeviceTypeEntry kDeviceTypes[] = { Device::Type::kRootNode, Device::Type::kOtaRequestor };
+#else
+    static constexpr DataModel::DeviceTypeEntry kDeviceTypes[] = { Device::Type::kRootNode };
+#endif // CHIP_DEVICE_CONFIG_ENABLE_OTA_REQUESTOR
+
     LazyRegisteredServerCluster<Clusters::BasicInformationCluster> mBasicInformationCluster;
     LazyRegisteredServerCluster<Clusters::AdministratorCommissioningWithBasicCommissioningWindowCluster>
         mAdministratorCommissioningCluster;
