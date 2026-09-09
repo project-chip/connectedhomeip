6a4ae977d0627971f584920449f1be38ffa08642
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2026-09-09 11:08:23 -0500

    Update TC_ACS_3_1.py

--- src/python_testing/TC_ACS_3_1.py
+++ src/python_testing/TC_ACS_3_1.py
@@ -20,8 +20,8 @@
 # === BEGIN CI TEST ARGUMENTS ===
 # test-runner-runs:
 #   run1:
-#     app: ${ALL_CLUSTERS_APP}
-#     app-args: --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json --app-pipe /tmp/acs_fifo_3_1
+#     app: ${ALL_DEVICES_APP}
+#     app-args: --device ambient-context-sensor --discriminator 1234 --KVS kvs1 --trace-to json:${TRACE_APP}.json --app-pipe /tmp/acs_fifo_3_1
 #     script-args: >
 #       --storage-path admin_storage.json
 #       --commissioning-method on-network
