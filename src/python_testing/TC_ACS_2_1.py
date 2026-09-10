be53d72aa80959e91363758fc7db7915b858c87e
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2026-09-10 16:29:44 -0500

    Update TC_ACS_2_1.py

--- src/python_testing/TC_ACS_2_1.py
+++ src/python_testing/TC_ACS_2_1.py
@@ -40,7 +40,7 @@ from mobly import asserts
 
 import matter.clusters as Clusters
 from matter.clusters.Types import NullValue
-from matter.testing.decorators import has_cluster, pics, run_if_endpoint_matches
+from matter.testing.decorators import async_test_body, has_cluster, pics, run_if_endpoint_matches
 from matter.testing.matter_testing import MatterBaseTest
 from matter.testing.runner import default_matter_test_main
 
