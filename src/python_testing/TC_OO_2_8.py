b27deac2528dbe3433e1ae5bf63a9e339eaf2b92
René Josefsen <rjosefsen@grundfos.com>
2026-09-10 19:18:13 +0200

    Reworked test script to latest style

--- src/python_testing/TC_OO_2_8.py
+++ src/python_testing/TC_OO_2_8.py
@@ -1,5 +1,5 @@
 #
-#    Copyright (c) 2024 Project CHIP Authors
+#    Copyright (c) 2026 Project CHIP Authors
 #    All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
@@ -54,189 +54,131 @@
 import asyncio
 import logging
 
-import test_plan_support
 from mobly import asserts
 
 import matter.clusters as Clusters
-from matter.testing.decorators import has_feature, run_if_endpoint_matches
+from matter.testing.decorators import async_test_body, has_feature, pics, run_if_endpoint_matches
 from matter.testing.event_attribute_reporting import AttributeSubscriptionHandler
 from matter.testing.matter_testing import MatterBaseTest
-from matter.testing.runner import TestStep, default_matter_test_main
+from matter.testing.runner import default_matter_test_main
 
 log = logging.getLogger(__name__)
 
 
 class TC_OO_2_8(MatterBaseTest):
 
-    def desc_TC_OO_2_8(self) -> str:
-        """Returns a description of this test"""
-        return "[TC-OO-2.8] Reporting requirements with (DUT as Server)"
-
-    def pics_TC_OO_2_8(self):
-        """This function returns a list of PICS for this test case that must be True for the test to be run"""
-        return ["OO.S", "OO.S.F00"]
-
-    def steps_TC_OO_2_8(self) -> list[TestStep]:
-        THcommand = "TH sends the command"
-        return [TestStep(1, test_plan_support.commission_if_required(), is_commissioning=True),
-                TestStep(2, f"{THcommand} Off command", test_plan_support.verify_success()),
-                TestStep(3, "Set up a subscription wildcard subscription for the On/Off Cluster, with MinIntervalFloor set to 0, MaxIntervalCeiling set to 30 and KeepSubscriptions set to false",
-                         "Subscription successfully established"),
-                TestStep(4, f"	{THcommand} OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 5 (0.5s) and OffWaitTime set to 5 (0.5s)",
-                         test_plan_support.verify_success()),
-                TestStep(5, f"	{THcommand} OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 100 (10s) and OffWaitTime set to 20 (2s)",
-                         test_plan_support.verify_success()),
-                TestStep(6, "Wait for 5 seconds"),
-                TestStep(7, f"{THcommand} OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 150 (15s) and OffWaitTime set to 100 (10s)",
-                         test_plan_support.verify_success()),
-                TestStep(8, "Wait for 20 seconds"),
-                TestStep(9, "TH stores the reported values of OnTime in all incoming reports for OnTime attribute, that contains data in reportedOnTimeValuesList and verifies reportedOnTimeValueList contains three entries",
-                         "reportedOnTimeValueList has 3 entries in the list"),
-                TestStep(10, "TH verifies the first entry in reportedOnTimeValueList is approximately 100",
-                         "The first entry in reportedOnTimeValueList is in the range 95 - 100"),
-                TestStep(11, "TH verifies the second entry in reportedOnTimeValueList is approximately 150",
-                         "The second entry in reportedOnTimeValueList is in the range 145 - 150"),
-                TestStep(12, "TH verifies the third entry in reportedOnTimeValueList is 0",
-                         "The third entry in reportedOnTimeValueList is equal to 0"),
-                TestStep(13, "TH stores the reported values of OffWaitTime in all incoming reports for OffWaitTime attribute, that contains data in reportedOffWaitTimeValueList and verifies reportedOffWaitTimeValueList contains three entries",
-                         "reportedOffWaitTimeValueList has 3 entries in the list"),
-                TestStep(14, "TH verifies the first entry in reportedOffWaitTimeValueList is approximately 20",
-                         "The first entry in reportedOffWaitTimeValueList is in the range 15 - 20"),
-                TestStep(15, "TH verifies the second entry in reportedOffWaitTimeValueList is approximately 100",
-                         "The second entry in reportedOffWaitTimeValueList is in the range 95 - 100"),
-                TestStep(16, "TH verifies the third entry in reportedOffWaitTimeValueList is 0",
-                         "The third entry in reportedOffWaitTimeValueList is equal to 0"),
-                TestStep(17, "Clear the received reports on the subscription"),
-                TestStep(18, f"{THcommand} OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 100 (10s) and OffWaitTime set to 50 (5s)",
-                         test_plan_support.verify_success()),
-                TestStep(19, "Wait for 5 seconds"),
-                TestStep(20, f"{THcommand} Off command", test_plan_support.verify_success()),
-                TestStep(21, "Wait for 10 seconds"),
-                TestStep(22, "TH stores the reported values of OnTime in all incoming reports for OnTime attribute, that contains data in reportedOnTimeValuesList and verifies reportedOnTimeValueList contains two entries",
-                         "reportedOnTimeValueList has 2 entries in the list"),
-                TestStep(23, "TH verifies the first entry in reportedOnTimeValueList is approximately 100",
-                         "The first entry in reportedOnTimeValueList is in the range 95 - 100"),
-                TestStep(24, "TH verifies the second entry in reportedOnTimeValueList is 0",
-                         "The second entry in reportedOnTimeValueList is equal to 0"),
-                TestStep(25, "TH stores the reported values of OffWaitTime in all incoming reports for OffWaitTime attribute, that contains data in reportedOffWaitTimeValueList and verifies reportedOffWaitTimeValueList contains two entries",
-                         "reportedOffWaitTimeValueList has 2 entries in the list"),
-                TestStep(26, "TH verifies the first entry in reportedOffWaitTimeValueList is approximately 50",
-                         "The first entry in reportedOffWaitTimeValueList is in the range 45 - 50"),
-                TestStep(27, "TH verifies the second entry in reportedOffWaitTimeValueList is 0",
-                         "The second entry in reportedOffWaitTimeValueList is equal to 0"),
-                ]
-
-    @run_if_endpoint_matches(has_feature(Clusters.OnOff, Clusters.OnOff.Bitmaps.Feature.kLighting))
+    @pics('OO.S', 'OO.S.F00')
+    @async_test_body
     async def test_TC_OO_2_8(self):
+        """[TC-OO-2.8] Reporting requirements with (DUT as Server)"""
         # Commissioning - already done
-        self.step(1)
+        self.step(1, "Commission DUT if not already done", is_commissioning=True)
 
-        self.step(2)
+        self.step(2, "TH sends the command Off command")
         await self.send_single_cmd(Clusters.OnOff.Commands.Off())
 
         # NOTE: added this sleep to let the DUT have some time to move
         log.info("Test waits for 1 seconds")
         await asyncio.sleep(1)
 
-        self.step(3)
+        self.step(3, "Set up a subscription wildcard subscription for the On/Off Cluster, with MinIntervalFloor set to 0, MaxIntervalCeiling set to 30 and KeepSubscriptions set to false")
         sub_handler = AttributeSubscriptionHandler(expected_cluster=Clusters.OnOff)
-        await sub_handler.start(self.default_controller, self.dut_node_id, self.get_endpoint())
+        await sub_handler.start(self.default_controller, self.dut_node_id, self.get_endpoint(), min_interval_sec=0, max_interval_sec=30, keepSubscriptions=False)
 
-        self.step(4)
+        self.step(4, "TH sends the command OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 5 (0.5s) and OffWaitTime set to 5 (0.5s)")
         cmd = Clusters.OnOff.Commands.OnWithTimedOff(onOffControl=0, onTime=5, offWaitTime=5)
         await self.send_single_cmd(cmd)
 
-        self.step(5)
+        self.step(5, "TH sends the command OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 100 (10s) and OffWaitTime set to 20 (2s)")
         cmd = Clusters.OnOff.Commands.OnWithTimedOff(onOffControl=0, onTime=100, offWaitTime=20)
         await self.send_single_cmd(cmd)
 
-        self.step(6)
+        self.step(6, "Wait for 5 seconds")
         log.info("Test waits for 5 seconds")
         await asyncio.sleep(5)
 
-        self.step(7)
+        self.step(7, "TH sends the command OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 150 (15s) and OffWaitTime set to 100 (10s)")
         cmd = Clusters.OnOff.Commands.OnWithTimedOff(onOffControl=0, onTime=150, offWaitTime=100)
         await self.send_single_cmd(cmd)
 
-        self.step(8)
+        self.step(8, "Wait for 20 seconds")
         log.info("Test waits for 20 seconds")
         await asyncio.sleep(20)
 
-        self.step(9)
+        self.step(9, "TH stores the reported values of OnTime in all incoming reports for OnTime attribute, that contains data in reportedOnTimeValuesList and verifies reportedOnTimeValueList contains three entries")
         count = sub_handler.attribute_report_counts[Clusters.OnOff.Attributes.OnTime]
         reportedOnTimeValueList = sub_handler.attribute_reports[Clusters.OnOff.Attributes.OnTime]
         asserts.assert_equal(count, 3, "Unexpected number of OnTime reports")
 
-        self.step(10)
+        self.step(10, "TH verifies the first entry in reportedOnTimeValueList is approximately 100")
         asserts.assert_less_equal(reportedOnTimeValueList[0].value, 100, "Unexpected first OnTime report")
-        asserts.assert_almost_equal(reportedOnTimeValueList[0].value, 100, delta=10, msg="Unexpected first OnTime report")
+        asserts.assert_greater_equal(reportedOnTimeValueList[0].value, 95, msg="Unexpected first OnTime report")
 
-        self.step(11)
+        self.step(11, "TH verifies the second entry in reportedOnTimeValueList is approximately 150")
         asserts.assert_less_equal(reportedOnTimeValueList[1].value, 150, "Unexpected second OnTime report")
-        asserts.assert_almost_equal(reportedOnTimeValueList[1].value, 150, delta=10, msg="Unexpected second OnTime report")
+        asserts.assert_greater_equal(reportedOnTimeValueList[1].value, 145, msg="Unexpected second OnTime report")
 
-        self.step(12)
+        self.step(12, "TH verifies the third entry in reportedOnTimeValueList is 0")
         asserts.assert_equal(reportedOnTimeValueList[2].value, 0, "Unexpected last OnTime report")
 
-        self.step(13)
+        self.step(13, "TH stores the reported values of OffWaitTime in all incoming reports for OffWaitTime attribute, that contains data in reportedOffWaitTimeValueList and verifies reportedOffWaitTimeValueList contains three entries")
         count = sub_handler.attribute_report_counts[Clusters.OnOff.Attributes.OffWaitTime]
         reportedOffWaitTimeValueList = sub_handler.attribute_reports[Clusters.OnOff.Attributes.OffWaitTime]
         asserts.assert_equal(count, 3, "Unexpected number of OffWaitTime reports")
 
-        self.step(14)
+        self.step(14, "TH verifies the first entry in reportedOffWaitTimeValueList is approximately 20")
         asserts.assert_less_equal(reportedOffWaitTimeValueList[0].value, 20, "Unexpected first OffWaitTime report")
-        asserts.assert_almost_equal(reportedOffWaitTimeValueList[0].value, 20, delta=10, msg="Unexpected first OffWaitTime report")
+        asserts.assert_greater_equal(reportedOffWaitTimeValueList[0].value, 15, msg="Unexpected first OffWaitTime report")
 
-        self.step(15)
+        self.step(15, "TH verifies the second entry in reportedOffWaitTimeValueList is approximately 100")
         asserts.assert_less_equal(reportedOffWaitTimeValueList[1].value, 100, "Unexpected second OffWaitTime report")
-        asserts.assert_almost_equal(reportedOffWaitTimeValueList[1].value,
-                                    100, delta=10, msg="Unexpected second OffWaitTime report")
+        asserts.assert_greater_equal(reportedOffWaitTimeValueList[1].value, 95, msg="Unexpected second OffWaitTime report")
 
-        self.step(16)
+        self.step(16, "TH verifies the third entry in reportedOffWaitTimeValueList is 0")
         asserts.assert_equal(reportedOffWaitTimeValueList[2].value, 0, "Unexpected last OffWaitTime report")
 
-        self.step(17)
+        self.step(17, "Clear the received reports on the subscription")
         sub_handler.reset()
         reportedOnTimeValueList = []
         reportedOffWaitTimeValueList = []
 
-        self.step(18)
+        self.step(18, "TH sends the command OnWithTimedOff command with AcceptOnlyWhenOn field set to 0, OnTime field set to 100 (10s) and OffWaitTime set to 50 (5s)")
         cmd = Clusters.OnOff.Commands.OnWithTimedOff(onOffControl=0, onTime=100, offWaitTime=50)
         await self.send_single_cmd(cmd)
 
-        self.step(19)
+        self.step(19, "Wait for 5 seconds")
         log.info("Test waits for 5 seconds")
         await asyncio.sleep(5)
 
-        self.step(20)
+        self.step(20, "TH sends the command Off command")
         await self.send_single_cmd(Clusters.OnOff.Commands.Off())
 
-        self.step(21)
+        self.step(21, "Wait for 10 seconds")
         log.info("Test waits for 10 seconds")
         await asyncio.sleep(10)
 
-        self.step(22)
+        self.step(22, "TH stores the reported values of OnTime in all incoming reports for OnTime attribute, that contains data in reportedOnTimeValuesList and verifies reportedOnTimeValueList contains two entries")
         count = sub_handler.attribute_report_counts[Clusters.OnOff.Attributes.OnTime]
         reportedOnTimeValueList = sub_handler.attribute_reports[Clusters.OnOff.Attributes.OnTime]
         asserts.assert_equal(count, 2, "Unexpected number of OnTime reports")
 
-        self.step(23)
+        self.step(23, "TH verifies the first entry in reportedOnTimeValueList is approximately 100")
         asserts.assert_less_equal(reportedOnTimeValueList[0].value, 100, "Unexpected first OnTime report")
-        asserts.assert_almost_equal(reportedOnTimeValueList[0].value, 100, delta=10, msg="Unexpected first OnTime report")
+        asserts.assert_greater_equal(reportedOnTimeValueList[0].value, 95, msg="Unexpected first OnTime report")
 
-        self.step(24)
+        self.step(24, "TH verifies the second entry in reportedOnTimeValueList is 0")
         asserts.assert_equal(reportedOnTimeValueList[1].value, 0, "Unexpected last OnTime report")
 
-        self.step(25)
+        self.step(25, "TH stores the reported values of OffWaitTime in all incoming reports for OffWaitTime attribute, that contains data in reportedOffWaitTimeValueList and verifies reportedOffWaitTimeValueList contains two entries")
         count = sub_handler.attribute_report_counts[Clusters.OnOff.Attributes.OffWaitTime]
         reportedOffWaitTimeValueList = sub_handler.attribute_reports[Clusters.OnOff.Attributes.OffWaitTime]
         asserts.assert_equal(count, 2, "Unexpected number of OffWaitTime reports")
 
-        self.step(26)
+        self.step(26, "TH verifies the first entry in reportedOffWaitTimeValueList is approximately 50")
         asserts.assert_less_equal(reportedOffWaitTimeValueList[0].value, 50, "Unexpected first OffWaitTime report")
-        asserts.assert_almost_equal(reportedOffWaitTimeValueList[0].value, 50, delta=10, msg="Unexpected first OffWaitTime report")
+        asserts.assert_greater_equal(reportedOffWaitTimeValueList[0].value, 45, msg="Unexpected first OffWaitTime report")
 
-        self.step(27)
+        self.step(27, "TH verifies the second entry in reportedOffWaitTimeValueList is 0")
         asserts.assert_equal(reportedOffWaitTimeValueList[1].value, 0, "Unexpected last OffWaitTime report")
 
 
