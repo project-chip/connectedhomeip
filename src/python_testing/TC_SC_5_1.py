a129903a090b2e26ba166b17c429cabb73a3e98d
Arya Hassanli <aryahassanli@google.com>
2026-09-11 20:14:37 +0000

    Fix groupcast test

--- src/python_testing/TC_SC_5_1.py
+++ src/python_testing/TC_SC_5_1.py
@@ -261,13 +261,19 @@ class TC_SC_5_1(MatterBaseTest):
                 groupID=0x0104, endpoints=join_endpoints, keySetID=0x01a4,
                 key=bytes.fromhex("e0e1e2e3e4e5e6e7e8e9eaebecedeeef")))
 
-            # Step 9b: Groupcast-created key set is visible in GroupKeyMap
+            # Step 9b: Groupcast-created key set is visible in GroupKeyMap (if Groupcast not adopted)
             self.step("9b")
             group_key_map = await self.read_single_attribute_check_success(
                 cluster=Clusters.GroupKeyManagement, attribute=Clusters.GroupKeyManagement.Attributes.GroupKeyMap, endpoint=0)
-            mappings = [(entry.groupId, entry.groupKeySetID) for entry in group_key_map]
-            asserts.assert_in((0x0104, 0x01a4), mappings,
-                              "GroupKeyMap missing Groupcast-created GroupID 0x0104 -> KeySetID 0x01a4")
+            groups_cluster_rev = await self.read_single_attribute_check_success(
+                cluster=Clusters.Groups, attribute=Clusters.Groups.Attributes.ClusterRevision, endpoint=groups_endpoint)
+            if groups_cluster_rev <= 4:
+                mappings = [(entry.groupId, entry.groupKeySetID) for entry in group_key_map]
+                asserts.assert_in((0x0104, 0x01a4), mappings,
+                                  "GroupKeyMap missing Groupcast-created GroupID 0x0104 -> KeySetID 0x01a4")
+            else:
+                # When Groups cluster revision > 4, Groupcast is adopted and GroupKeyMap is empty
+                asserts.assert_equal(len(group_key_map), 0, "GroupKeyMap should be empty when Groupcast is adopted")
 
             # Step 9c: KeySetRead returns the auto-created GroupKeySet
             self.step("9c")
