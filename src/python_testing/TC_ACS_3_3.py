318a20fb1f91700d897c9b326b0306a3835bdf5d
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2026-09-11 10:29:01 -0500

    Update TC_ACS_3_3.py

--- src/python_testing/TC_ACS_3_3.py
+++ src/python_testing/TC_ACS_3_3.py
@@ -77,7 +77,7 @@ SOUND_IDENTIFICATION_NAMESPACE_ID = 74  # 0x4A
 class TC_ACS_3_3(MatterBaseTest):
 
     def pics_TC_ACS_3_3(self):
-    return ['ACS.S']
+        return ['ACS.S']
 
     def setup_test(self):
         super().setup_test()
