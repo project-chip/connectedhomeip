48d370f9b37a6e8d2925d64e2f42d24b3ba659d3
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2026-09-13 14:59:26 -0500

    Update TC_ACS_3_1.py

--- src/python_testing/TC_ACS_3_1.py
+++ src/python_testing/TC_ACS_3_1.py
@@ -295,10 +295,10 @@ class TC_ACS_3_1(MatterBaseTest):
                 await asyncio.sleep(holdTime_input - elapsed_time + 1)
 
             # The last end event
-            event = event_listener.get_last_event()
-            # event = event_listener.wait_for_event_report(cluster.Events.AmbientContextDetectEnded, timeout_sec=30.0)
-            asserts.assert_equal(event.Header.EventId, cluster.Events.AmbientContextDetectEnded.event_id,
-                                 f"Wrong event, {event.Header.EventId}, {cluster.Events.AmbientContextDetectStarted.event_id}")
+            #event = event_listener.get_last_event()
+            event_listener.wait_for_event_report(cluster.Events.AmbientContextDetectEnded, timeout_sec=30.0)
+            #asserts.assert_equal(event.Header.EventId, cluster.Events.AmbientContextDetectEnded.event_id,
+            #                     f"Wrong event, {event.Header.EventId}, {cluster.Events.AmbientContextDetectStarted.event_id}")
 
             self.skip_step("6a")
             self.skip_step("6b")
@@ -479,10 +479,10 @@ class TC_ACS_3_1(MatterBaseTest):
                 await asyncio.sleep(holdTime_input - elapsed_time + 3)
 
             # The last end event
-            event = event_listener.get_last_event()
-            # event = event_listener.wait_for_event_report(cluster.Events.AmbientContextDetectEnded, timeout_sec=30.0)
-            asserts.assert_equal(event.Header.EventId, cluster.Events.AmbientContextDetectEnded.event_id,
-                                 f"Wrong event, {event.Header.EventId}, {cluster.Events.AmbientContextDetectStarted.event_id}")
+            # event = event_listener.get_last_event()
+            event_listener.wait_for_event_report(cluster.Events.AmbientContextDetectEnded, timeout_sec=30.0)
+            #asserts.assert_equal(event.Header.EventId, cluster.Events.AmbientContextDetectEnded.event_id,
+            #                     f"Wrong event, {event.Header.EventId}, {cluster.Events.AmbientContextDetectStarted.event_id}")
 
         self.step("7", "TH reads the AmbientContextType attribute. Verify that the AmbientContextType attribute contains an empty list and the Boolean attributes related the step 5a or 6a are False.")
         # Check the boolean attributes are set to False
