f436180701d79c83b237ccda4feb0ed6721eb276
Jae Son <12516308+jaehs6sam@users.noreply.github.com>
2026-09-14 00:20:27 -0500

    Update TC_ACS_3_1.py

--- src/python_testing/TC_ACS_3_1.py
+++ src/python_testing/TC_ACS_3_1.py
@@ -203,7 +203,7 @@ class TC_ACS_3_1(MatterBaseTest):
 
             # Second PIXIT input
             pixit2_nsid = self.user_params.get("PIXIT.ACS.Event2_NSID", "0x49")
-            pixit2_tagid = self.user_params.get("PIXIT.ACS.Event2_TAGID", "0x03")
+            pixit2_tagid = self.user_params.get("PIXIT.ACS.Event2_TAGID", "0x04")
             log.info("pixit1_nsid: %s", pixit2_nsid)
             log.info("pixit1_tagid: %s", pixit2_tagid)
             # expecting PIXIT to be like "0x4B" hex string and convert string hex to decimal
