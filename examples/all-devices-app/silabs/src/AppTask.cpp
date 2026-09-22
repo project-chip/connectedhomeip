a6bc0262da4dbff3cb00ade8118066e69fe34800
Jean-Francois Penven <jean-francois.penven@silabs.com>
2026-09-21 21:14:00 -0400

    fix build

--- examples/all-devices-app/silabs/src/AppTask.cpp
+++ examples/all-devices-app/silabs/src/AppTask.cpp
@@ -323,7 +323,9 @@ CHIP_ERROR AppTask::InitCodeDrivenDataModel(chip::PersistentStorageDelegate & st
 #if ALL_DEVICES_ENABLE_POWER_SOURCE
     // Override the generic DecreasingBatteryPowerSource with a silabs-specific
     // implementation tuned for the platform.
-    deviceFactory.RegisterCreator("power-source", []() { return std::make_unique<chip::app::SilabsBatteryPowerSource>(); });
+    deviceFactory.RegisterCreator("power-source", []() {
+        return chip::app::NoHooksDeviceFactory::MakeDevice<chip::app::SilabsBatteryPowerSource>();
+    });
 #endif
 
     ConsecutiveEndpointIdAllocator allocator(kDeviceEndpointId);
