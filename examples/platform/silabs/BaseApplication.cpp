4a9b2ff50cf0baeb0d19e87fa87f04b8853f15b1
Ricardo Casallas <ricardo.casallas@silabs.com>
2026-09-24 11:39:38 -0400

    Code review.

--- examples/platform/silabs/BaseApplication.cpp
+++ examples/platform/silabs/BaseApplication.cpp
@@ -173,6 +173,7 @@ ObjectPool<Identify, MATTER_DM_IDENTIFY_CLUSTER_SERVER_ENDPOINT_COUNT + CHIP_DEV
 
 int sCodeDrivenIdentifyActiveCount                                 = 0;
 Clusters::Identify::EffectIdentifierEnum sCodeDrivenIdentifyEffect = Clusters::Identify::EffectIdentifierEnum::kStopEffect;
+[[maybe_unused]] Clusters::Identify::EffectVariantEnum sCodeDrivenIdentifyVariant   = Clusters::Identify::EffectVariantEnum::kDefault;
 
 // Protects the three sCodeDrivenIdentify* variables above.
 osSemaphoreId_t sCodeDrivenIdentifyLock = nullptr;
@@ -853,6 +854,7 @@ void BaseApplication::NotifyCodeDrivenTriggerEffect(Clusters::Identify::EffectId
 {
     osSemaphoreAcquire(sCodeDrivenIdentifyLock, osWaitForever);
     sCodeDrivenIdentifyEffect  = effect;
+    sCodeDrivenIdentifyVariant = variant;
     osSemaphoreRelease(sCodeDrivenIdentifyLock);
 
     if (variant != Clusters::Identify::EffectVariantEnum::kDefault)
